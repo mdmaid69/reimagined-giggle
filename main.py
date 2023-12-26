bfea58e13095d96a5b9f828bd38cc3bba4807c96
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:34:31 +0000

    tax appeal breakthrough lock shadow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
