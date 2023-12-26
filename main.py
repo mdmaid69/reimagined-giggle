2ff3f417a76e120ec29482c870009707913d319e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:28:45 +0000

    only pressure manage thank familiar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
