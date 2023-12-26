cf783d714eba84c4d9487e113c5100dde37441e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:44:47 +0000

    treat before term satisfactory form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
