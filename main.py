69198cfb1b61f15443f329626aa0d74f55e997f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:51:44 +0000

    rank concern rest statement turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
