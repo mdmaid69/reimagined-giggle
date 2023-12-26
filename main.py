21592cb01d6a5cdc95501a933baefcb16aa1dede
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:39:49 +0000

    recent cinema frighten debt moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
