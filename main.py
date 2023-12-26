47fb30b7889380289bc075903a3bcca4220e5099
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:47:55 +0000

    tidy habit wrong recommend today

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
