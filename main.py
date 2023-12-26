bceff2a245761171ea016e0fe62cd2c04d5f6734
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:29:58 +0000

    little Rr art emotion mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
