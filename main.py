b92796fa3772ba93be9886619458e2917ea3fe2e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:29:56 +0000

    start tool breakinto culture already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
