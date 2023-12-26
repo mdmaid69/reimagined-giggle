ff6e3c5615ed82e181415e57552de68c1d88abb9
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:12:03 +0000

    father crime ride finger convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
