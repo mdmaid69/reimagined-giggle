7ac13d002b20b91aa7d7011b5f32d730b5699a49
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:53:38 +0000

    turnout Zz goaway scheme towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
