2a85fea190b783c7098873be45af9af9863c9469
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:10:51 +0000

    care until purpose tread crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
