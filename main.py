6fc69ff01644f5879e90b4373101fddb8f41b09d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:09:18 +0000

    straight step share wealth proud

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(10): print(i)
