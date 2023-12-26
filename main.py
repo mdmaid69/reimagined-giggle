cc3d57a4ab0c292cdd32430d063868b584ccbbe7
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:02:48 +0000

    disturbing majority pen wave anxious

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
