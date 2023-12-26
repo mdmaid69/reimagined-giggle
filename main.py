f5225f03fb3e87aee82f90c4e959f1dd1647bb6d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:03:25 +0000

    excellent emphasis drink call news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
