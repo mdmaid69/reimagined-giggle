675ef82176b81ddf22efec8afe75a1322d424750
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:25:02 +0000

    content design lie expect satisfied

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def multiply_numbers(x, y):
+        return x * y
+def remove_duplicates(lst):
+        return list(set(lst))
