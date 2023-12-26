2cddc4c48a3876127540c4ac30359320a14a0527
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:47:40 +0000

    reply suffer team change plan

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
