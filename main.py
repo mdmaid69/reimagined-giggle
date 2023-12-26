18d600f2326fc5e024d7dc47f7a4999664a5452e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:50:07 +0000

    contain care comfortable mine design

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
