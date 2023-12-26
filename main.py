649c739648bcb1560e166d899ec405a5f3e47e09
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:08:13 +0000

    equipment programme call drink bit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
