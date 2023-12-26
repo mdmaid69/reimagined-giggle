dd51e7965fa723de9c2523ee604d0a5f351cfa1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:48:54 +0000

    pin increase flow raise future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
