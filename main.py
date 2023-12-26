711e2001f559964e562c75c8e38e0e6eed84beaf
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:04:01 +0000

    measure delicate amount real Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
