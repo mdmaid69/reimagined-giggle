b228c31697bc818ecf9ad100ffcd32eede5c1292
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:37:39 +0000

    common kind nor table broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
