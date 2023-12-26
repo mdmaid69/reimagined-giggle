dcb5e2496e3d0aa8b98a34b70fa750e731a1bdec
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:04:14 +0000

    hand shape cheat fine close

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
