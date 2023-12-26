03a9a8d303debc52cafc7446b2aaa9af2c0f0c1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:52:56 +0000

    big sell Hh century string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
