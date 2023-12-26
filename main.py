3a6bb2abb67ca4ab74bdf46aa61ab188307ef662
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:17:27 +0000

    public accident rain whole finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
