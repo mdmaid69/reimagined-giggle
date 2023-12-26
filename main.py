750e29ae63b76ffb3dd028e185f2c9d940a444fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:01:19 +0000

    round rush rush dangerous me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
