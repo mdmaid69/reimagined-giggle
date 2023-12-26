3c459c5ba464d5eaf3d39f774b87a44d2ae93eab
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:28:10 +0000

    Jj effect recognize contact race

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
