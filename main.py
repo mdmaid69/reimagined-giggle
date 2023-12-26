8e99fca257c8aaa396a2c92cfaeba667a5a7be47
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:32:55 +0000

    this around detail interested when

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
