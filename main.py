07293583e5a0befd9a4740a121a0db3db87d69de
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:13:16 +0000

    everything hungry poor always yes

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_pressure(force, area):
+        return force / area
