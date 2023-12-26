49278dc39ee2a6ff8a92f6cf2cb953a6ea7ecb77
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:06:52 +0000

    possibly picture speech part weather

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
