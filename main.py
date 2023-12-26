53e3ad5f85a0840e4f41451cc568b67e6e460044
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:18:35 +0000

    handle error rule careless passenger

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def count_elements(lst):
+        return len(lst)
