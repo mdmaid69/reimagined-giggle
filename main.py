67917c033c013db5619fccde28f1c5e0b0121819
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:04:48 +0000

    book range form turnaway fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
