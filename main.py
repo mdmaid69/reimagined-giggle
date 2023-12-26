378771c07c1582ad0dd3d707021668894b81f55b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:22:50 +0000

    chance voice frightened loose teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
