e10736c560d84fa25d0997631b8a02c66f581072
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:06:37 +0000

    recognize impressed several relax generally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
