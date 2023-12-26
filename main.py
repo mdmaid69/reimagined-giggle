e14ae7e06f75e660b9d65cb7eba4f6e508bf4d8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:22:35 +0000

    danger condition next lay brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
