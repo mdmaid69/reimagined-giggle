70703a3609e612365613c2dff5bd79c700563147
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:25:08 +0000

    unfortunate sport stiff both hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
