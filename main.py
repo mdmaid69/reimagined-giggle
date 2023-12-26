2af333c98990d20f19a2f72fefb81ab69adec367
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:12:29 +0000

    hole trust subject base trial

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def is_even(n):
+        return n % 2 == 0
