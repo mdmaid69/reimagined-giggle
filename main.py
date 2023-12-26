4c2dbabce883865c59fde61797bd76f65d7194a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:14:21 +0000

    sink any extremely fight nowhere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
