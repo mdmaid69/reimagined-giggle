938eebb75da8fc86f69a2fac9144547e04f1459d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:44:38 +0000

    film reply patience second however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
