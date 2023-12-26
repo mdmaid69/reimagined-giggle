698e9cc1238f4670348ffad38f59e18cb67d4888
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:13:10 +0000

    up Oo content list declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def add_numbers(x, y):
+        return x + y
