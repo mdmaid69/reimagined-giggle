0be32aee65e87e4ec38abe5c89af01fc4eb6bd73
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:41:08 +0000

    visit single maximum honest twist

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
