4886cf4ace8b4821b7b966a15416c2efcda012f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:38:29 +0000

    emphasize judge previous comein each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
