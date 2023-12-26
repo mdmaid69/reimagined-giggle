f36491d616beab48b742ea24751a20a81f70b835
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:24:51 +0000

    geton judge typical giveback comein

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def cube_number(x):
+        return x**3
