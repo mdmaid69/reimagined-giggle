47668b718e7deebd61546bba21ee74e2b7148bac
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:36:15 +0000

    around scare increase risk foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def check_if_array_contains_item(array, item):
+        return item in array
