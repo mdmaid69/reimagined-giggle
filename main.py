dfd847a266ea52091af195625f608eaa458f0f2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:33:40 +0000

    woman main zero dare case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_float(array):
+        return float(array[0])
