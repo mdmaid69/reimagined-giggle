4c7cfd5901027840af636f6418a9f531b00ed41f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:48:38 +0000

    another reply quiet deep switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
