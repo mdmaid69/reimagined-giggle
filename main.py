bff42efbf1dd4fa2022ef0683806038dddbc1681
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:52:02 +0000

    squeeze lock separate cry somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
