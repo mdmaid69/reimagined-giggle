82535903361f21b7797be547dc650030f15b94ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:16:04 +0000

    flavour exercise knowledge burn central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def convert_to_octal(n):
+        return oct(n)
