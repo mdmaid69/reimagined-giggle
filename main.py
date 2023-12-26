1c0cfe826c7b5240f5ca9d91fae6e5efc621d9be
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:53:26 +0000

    correct plain attempt report live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def reverse_list(lst):
+        return lst[::-1]
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
