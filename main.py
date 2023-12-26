6d67bee5ecad94fd9a656ed950e61fb65b264e53
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:24:16 +0000

    friend circle impossible trust odd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+for i in range(10): print(i)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
