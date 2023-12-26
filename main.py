c5a65a21e1921fcda3d7c233458e0040a52325aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:38:47 +0000

    murder extremely duty song talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+from collections import Counter
+print(Counter("hello world"))
