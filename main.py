1fabbf1ba6bb133dba868ec3c511bbbdf2549d52
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:19:10 +0000

    destroy bringdown wind aim ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def remove_duplicates(lst):
+        return list(set(lst))
