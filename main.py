279bcf45404274c0dc090de10fccdd974ba831ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:13:02 +0000

    plenty impression government excellent crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
