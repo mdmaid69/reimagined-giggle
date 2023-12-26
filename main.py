3a7380ffc18dabb9c9ef2ef42e62a087eece1a2e
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:00:18 +0000

    city measure prevent steady wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_area_rectangle(l, w):
+        return l * w
