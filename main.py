0cdcaa01d8feffda9449cd3d98e97a141eeff640
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:54:27 +0000

    Vv be meanwhile past mess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
