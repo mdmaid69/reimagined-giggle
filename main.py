f470ecbd21735ab65e64a296b09e78e7e069b15e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:50:17 +0000

    chief neat better noisy software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
