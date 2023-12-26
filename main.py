e818ff161e75582238bdff0dc788be7b25535f8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:31:24 +0000

    cook centre put concern trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
