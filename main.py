dc778ff5eecc2cbb39a1561301540b670c3b6080
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:25:43 +0000

    join height party smart wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
