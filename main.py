b65452d542d90c67d1a799872d9b6af2cd70bff0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:21:41 +0000

    trick claim pattern pupil share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
