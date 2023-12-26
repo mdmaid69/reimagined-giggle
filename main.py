14cd5c07429974d2857420cc9c8431d6414f1086
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:54:36 +0000

    building raise immediately until worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import collections
+def create_stack():
+        return collections.deque()
