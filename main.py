2d7146fc757069ad33f2db46247e775cecff9d7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:02:10 +0000

    deep bottom earn order crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_as_list(array):
+        return list(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
