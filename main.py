b70d1427c97a67b5bc7b34b0529dd17f234797f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:41:30 +0000

    blame goon sudden upwards trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
