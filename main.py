04f1f4fa65b78c5bfac60a354908c27e7c6bbacd
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:34:07 +0000

    less smile safety stamp on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import sys
+print(sys.version)
