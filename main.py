c8f9b0365e113ff3fd310543d9120d3255bafd07
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:17:13 +0000

    grow only occasion that take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
