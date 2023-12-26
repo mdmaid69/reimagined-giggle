d629e884a0f733cba0ffa3ffeeee2776e8a69159
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:50:11 +0000

    kill upset mixture stir murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def cube_number(x):
-        return x**3
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
