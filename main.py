a1c70f11e7f12388d96671e14841a16d435a5eb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:10:29 +0000

    minority what coat except task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
