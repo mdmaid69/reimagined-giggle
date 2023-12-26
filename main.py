03a0c514d831b9ae7edc43f19b1a20b814f37dc3
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:05:14 +0000

    duty expect start deal pin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+print([x**2 for x in range(10)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
