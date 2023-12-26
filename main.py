97f2717c9e24069ce249cd032993d0804c71e833
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:03:25 +0000

    fear problem mood exactly hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
