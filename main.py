c57fd4afb6913f97832cac8d0c7a1e6e45d179d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:35:44 +0000

    gather absolutely path coffee round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
