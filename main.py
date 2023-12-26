47cc47c467a4108923697529b44b08a700757bb9
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:41:30 +0000

    deliver concerned feel appear steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
