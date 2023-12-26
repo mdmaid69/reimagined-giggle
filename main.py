4447dc4d26c3df3e65b05ce671cef708ac53979b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:37:13 +0000

    mainly ticket custom body total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import math
 def calculate_permutations(n, k):
         return math.perm(n, k)
