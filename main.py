99e829bcbeb4b1f1e77406d716187f935d91bc14
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:05:06 +0000

    Ll through knock sick rank

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        print(sys.version)
