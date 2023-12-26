a13b673d0072b9c14ce0a6d31845f0a942d226ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:36:16 +0000

    sale sweep room almost miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
