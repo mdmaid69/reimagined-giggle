be9cc6da1117600d7b3b25f7993f85e720db768d
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:43:56 +0000

    group keepout fear perfect start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_area(radius):
+        return 3.14 * radius * radius
