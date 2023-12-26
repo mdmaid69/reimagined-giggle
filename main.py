93c6d147ea4d6e8dbfd93a5d1388b1ae71d734cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:25:18 +0000

    create drop office interesting information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
