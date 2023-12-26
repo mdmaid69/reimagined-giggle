1a4bc3e25cd4ea9a8f3359d17eb3cef6ceb23123
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:27:38 +0000

    sort worst clue study reasonable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
