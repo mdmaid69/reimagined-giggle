b4b6b8b5edc88ea7a135362fbd67b10167fefc1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:09:36 +0000

    argument watch travel shopping crowded

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import random
+print(random.randint(0, 100))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
