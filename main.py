fa4a19c2af038c0efd02d2de6a87aeea5d077c4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:07:38 +0000

    alive guess fortunately seem means

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
