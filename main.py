b7da891b473b6bcfbb9c46ab6fda9c9abec55da4
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:05:29 +0000

    nigga basis obtain lose side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
