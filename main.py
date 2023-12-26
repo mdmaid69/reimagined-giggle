e2dad08127a0ac47fec1231629a466c10565b73b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:06:06 +0000

    free exactly hair afraid add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
