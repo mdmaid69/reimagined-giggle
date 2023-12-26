4cc92a5b81e12fa86aadae2cc7be822e05ddc6d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:13:38 +0000

    powerful takeoff there busy hang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
