4887abff3d659e50c96729ff1c3b6b7f2965abee
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:10 +0000

    distant excellent justice damage possibly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
