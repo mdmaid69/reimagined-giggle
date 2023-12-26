701cbf3c4c37300bd5406b34f3e36007f743dc14
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:39:40 +0000

    opposite variety buy hospital giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
