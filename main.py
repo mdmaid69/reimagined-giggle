9c7ed230c8ce6bfa01cc7c09dced8dcc456891d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:30:44 +0000

    now copy private ball worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
