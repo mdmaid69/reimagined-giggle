5d41ec3f67d723ebf6580d57c69461f083ced307
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:48:27 +0000

    join satisfied tooth calloff lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
