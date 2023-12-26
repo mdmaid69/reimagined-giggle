dea87f2e4e3b36d0dec980380023888fee53c512
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:28:13 +0000

    calmly risk waste community understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
