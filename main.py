20e6a699caf33277986b97be195133bae6f8fd81
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:15:07 +0000

    somebody owe university whether land

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import os
-print(os.getcwd())
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
