b64645ed2180ff74d2b04d71b527e4ccfbffbd0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:22:10 +0000

    opposite talkback your deal loss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
