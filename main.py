83e3e20b6ef95b5f4e4fc38d7fad57e57ba1d4a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:05:50 +0000

    really slight sky intelligent sky

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def reverse_array(array):
-        array.reverse()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
