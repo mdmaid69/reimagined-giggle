622f53ed3751aef706e59372131e8de9c93a00b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:46:29 +0000

    win bye Ll business pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_binary(n):
-        return bin(n)
+def find_max(lst):
+        return max(lst)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
