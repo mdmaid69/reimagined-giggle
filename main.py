9319fd9c9f69fc7440ac71ffe7d658285568b2e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:44:31 +0000

    somehow none gift disturb full

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
