ab859fcb02e5a34ca1a9f43c54505179fca4754a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:34:43 +0000

    break again education note thin

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
