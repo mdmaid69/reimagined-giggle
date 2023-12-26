28a84993aaadc74817439e98b7eacf787fef8b41
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:04:31 +0000

    hate consist air off previous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
