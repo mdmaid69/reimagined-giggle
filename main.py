636910b440c18c9732077342698b0cbbf563d169
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:02:33 +0000

    walk protection minimum variety uniform

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print([x**2 for x in range(10)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
