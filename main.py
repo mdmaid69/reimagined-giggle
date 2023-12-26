9f25a206d4c488ed769b7a9e8e41d6e17240a03b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:27:38 +0000

    order plain even turnback loose

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
