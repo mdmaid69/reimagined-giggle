b27719f96e98cb7b9d5506a65eeada9715ab870c
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:28:56 +0000

    firm connection light remain telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_current_working_directory():
+        return os.getcwd()
