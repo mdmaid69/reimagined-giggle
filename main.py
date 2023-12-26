8ff8d008d8c5d94058affd07ba6c1a5f397ff347
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:16:00 +0000

    seat beside crash closed takeout

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
