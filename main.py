d195b75587d2d187485ab8e194db1f54e6b887fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:33:16 +0000

    breath listenfor connection 179bite smell

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
