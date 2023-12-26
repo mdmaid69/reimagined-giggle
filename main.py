8226244a74b25872f972be372015332446dd7424
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:07:17 +0000

    except necessary home high embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_item(array, i):
-        return array[i]
-def find_max(numbers):
-        return max(numbers)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
