346980346d80d1aee9b28011341ce34de54a007c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:43:37 +0000

    ago lead deliberately yesterday polite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
