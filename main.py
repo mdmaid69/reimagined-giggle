c2f430698ac9b21d9c88656f6b6829eeaa2f5bbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:49:55 +0000

    mine track visit bother wall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
