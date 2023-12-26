be08abcf30254966cbd8e9635e6924097ac9b592
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:05:34 +0000

    love angry store afford while

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_max(numbers):
+        return max(numbers)
