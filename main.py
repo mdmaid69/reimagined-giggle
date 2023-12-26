ef024182fd5128bebe1a6d1bad54d35f59f8fcf8
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:36:47 +0000

    little stay dull thick over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
