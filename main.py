bd47c5f8844d48991e94d1ce6d935b630bc3415b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:43:15 +0000

    mysterious angry plate upset through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_area(radius):
+        return 3.14 * radius * radius
