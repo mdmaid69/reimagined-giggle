5219c9c9aa91d094d82be2cfab6011decb29471f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:25:52 +0000

    to fit bottom reserve instant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
