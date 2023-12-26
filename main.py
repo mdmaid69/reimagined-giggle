baac149012fd97eec2dbe859deccc1f44dc55206
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:52:22 +0000

    account hold unable frequent reliable

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
