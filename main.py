03fc463d64cbd34b4e3982b479803e5cad92066f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:06:26 +0000

    worst sticky characteristic Yy film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
