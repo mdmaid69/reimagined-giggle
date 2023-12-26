49d584a0098ceef36bcfb9fcd1f65e703ed3e545
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:14:44 +0000

    thinkover reason relax whether guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import platform
+def get_python_version():
+        return platform.python_version()
