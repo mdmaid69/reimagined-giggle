ced9d8f7757c7be92cb861ac9363968ccf45df5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:16:15 +0000

    correct relative original its roof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import os
+def remove_directory(path):
+        os.rmdir(path)
