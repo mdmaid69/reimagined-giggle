1f0bb31f31fccc81bbaa01102d31c223d19aad8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:44:28 +0000

    even date squeeze tip factory

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
