945167b3bc239022a448f2c4982af8443ef3a090
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:03:00 +0000

    spoil anyone return finish image

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
