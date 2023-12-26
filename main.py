946e0799842c01d1766940aae2bf5da5775459e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:42:51 +0000

    load pool suggestion godown branch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
