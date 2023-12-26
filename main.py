bbeebcfb99ebfca6901d792c3a6457a45778a0bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:46:53 +0000

    reject true material style straight

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
