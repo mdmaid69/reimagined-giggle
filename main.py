2d11f47c8f733785d3f7d162ca708dde7e35c1d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:44:32 +0000

    wealth admit relation smoke afterwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-print(math.pi)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
