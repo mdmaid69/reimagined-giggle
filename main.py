0ce6f815e85a641e9aebcac35dfaaa5ca0e5d73c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:55:31 +0000

    let danger study distant read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
