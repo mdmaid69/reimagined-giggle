f32045fb5b9359b31209c6d06b698650ae7dc97b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:56:33 +0000

    bag private upset breakout fight

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
