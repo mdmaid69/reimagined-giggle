42a3420401173a360d34c806b89a225df5ef8d6a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:13:56 +0000

    fault try education quickly song

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_typecode(array):
+        return array.typecode
