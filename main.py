efa2fca92bc1b9ee2caeffa671eaacf51b3a6c3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:36:47 +0000

    system vote cheat generous important

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_hex(n):
+        return hex(n)
