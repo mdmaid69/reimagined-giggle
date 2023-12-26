39408c49aef37941657b3fff9062daac22e9b05a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:05:29 +0000

    possible fire climb chief strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
