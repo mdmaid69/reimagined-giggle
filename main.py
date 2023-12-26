acc38bdebc76883417176b653e30cb7a76539b1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:52:12 +0000

    top different feel separate politics

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
