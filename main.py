414888c8c2114fe587d9b88d429b7a472cddb13f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:24:50 +0000

    head mine principle same sport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
