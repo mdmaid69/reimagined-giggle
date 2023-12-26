82db6e4a1bd3492d9f4fc244dd5f8c5e692f4d04
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:52:28 +0000

    deliberate closely relationship hard breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import json
+print(json.dumps({"name": "John", "age": 30}))
