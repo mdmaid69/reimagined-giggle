c219849abfcbdc7792dd6aa7cecbc2d6b0b5f407
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:38:04 +0000

    tool workupto match wise scream

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
