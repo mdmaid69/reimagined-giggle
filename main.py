31d5649273f2fb07f1dbaf3855fd79e8ba49aef8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:09:42 +0000

    trick keen protection enthusiastic sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
