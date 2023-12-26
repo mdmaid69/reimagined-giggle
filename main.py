ceed414e6cb9e30d13afa636f6238f77b0849ece
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:57:36 +0000

    agree couple petrol new nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
