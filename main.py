54aa45c4ab1a53afdfb8a51e6ec54d86d7ae629e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:22:14 +0000

    loss convincing difference listen trial

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
