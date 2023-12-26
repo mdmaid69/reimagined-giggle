f584a251e9f95e226f90eae6e551d74dba7f5fe0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:47:53 +0000

    check fear slice store university

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
