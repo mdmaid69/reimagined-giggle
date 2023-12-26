5a8837f51d6ec8f855fa2164869cd4cb00c0a773
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:39:26 +0000

    night turnaway tune development strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
