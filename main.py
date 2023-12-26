5e05b1589520b3cdb93c59bdcd41781a27b83748
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:37:27 +0000

    considering dead exciting reliable roof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_density(mass, volume):
+        return mass / volume
