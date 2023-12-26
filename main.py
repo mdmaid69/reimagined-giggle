17935c29932d329a9e473b976e9bd228c57b758b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:35:24 +0000

    sensible ought drink surprising software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
