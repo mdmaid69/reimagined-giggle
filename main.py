848121372b9e16dc5c84267b5e213f7eac85e920
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:27:08 +0000

    movement design seem Oo doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_current_directory():
+        return os.getcwd()
