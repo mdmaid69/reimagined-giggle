eae3bbcedd61716e86631bdbfc393a0d8f953b15
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:34:38 +0000

    answer same section beyond takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
