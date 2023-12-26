45380dabb3102f24d2afd312b967121b78af9f31
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:48:54 +0000

    repair answer escape underneath skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
