bc5b77072e28a51cf639d6f911889c8145c574c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:54:08 +0000

    average poor fork this book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
