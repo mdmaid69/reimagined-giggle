1498eb404abf01208ffbc707e902ae1e99b5c427
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:11:32 +0000

    hardly beautiful now branch just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
