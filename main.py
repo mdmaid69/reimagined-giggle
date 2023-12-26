2130b0b5e865fca45b49cf6f01cf47ce1a7ceec3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:20:42 +0000

    allow closed plant shrink seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
