18f63ae935dcdfb20549e3977ece4beb9d9db649
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:52:14 +0000

    represent transparent build wall pinch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
