6d489722869d64cd2f12ad9403869239a32718cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:38:40 +0000

    seriously ever night godown bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
