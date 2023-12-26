03a9059316a3c9cbb52713108beb1136fdffca31
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:30:32 +0000

    fresh crime leg careful most

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_area_circle(r):
         return 3.14 * r**2
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
