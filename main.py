37a73b55ea004d9bbb9882c9094d33ceb105b033
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:48:16 +0000

    wealth base worth store crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
