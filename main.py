1dd35df42c37de9e5c9fd7248b86f29d6c81c6d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:39:25 +0000

    bush clever goup hit frighten

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
