76d5ac4d78a5f100cb4c5c9157a28652146b5378
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:03:27 +0000

    low upset least bringup fire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import numpy as np
-print(np.array([1, 2, 3]))
+import random
+def roll_die():
+        return random.randint(1, 6)
+import getpass
+def get_username():
+        return getpass.getuser()
