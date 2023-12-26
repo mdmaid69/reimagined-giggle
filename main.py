99395eed2e9a15b98048f09a27111f525c98f77f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:38:11 +0000

    popular size reply narrow communicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
