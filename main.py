d175fee262142d61646806b7189220f7f45f9df9
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:41:17 +0000

    influence appear law nonsense gate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
