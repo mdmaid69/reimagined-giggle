2169c8086de57677277d312aa0a3e6b6df2f43e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:53:15 +0000

    murder plant cook ill extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
