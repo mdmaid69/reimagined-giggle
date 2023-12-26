2f6c57fda740cd4c6e07019a3e5270e02aaa7217
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:44:24 +0000

    bin spare punish shelter burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
