de177712f8e49bec7d6139ce239ebbbfa12aa254
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:51:47 +0000

    right fortunately belief programme tidy

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def remove_duplicates(lst):
+        return list(set(lst))
+import sys
+print(sys.version)
