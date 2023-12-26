67988d321954398e2367a318546d45906180de6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:33:41 +0000

    along raise check ought turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
