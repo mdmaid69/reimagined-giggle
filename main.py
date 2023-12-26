db51729ce2992c53cb27225a69eb29094a8723b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:30:33 +0000

    accidentally squeeze charge row disgusting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
