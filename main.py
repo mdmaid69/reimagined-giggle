22add7aba99c2efd4a3d891a10b1c272528d6346
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:46:54 +0000

    argument affect light body swallow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+print(math.pi)
