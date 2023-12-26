19a4c9ad1160de1ee3a87a4aeb8186a8fdb71509
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:13:46 +0000

    quarter top pay another lost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
