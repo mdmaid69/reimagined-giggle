46282b98cd0011dcc1b793a7d28a19f37fbb874a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:07:27 +0000

    wing cheat flat fly raise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-x = 10
-y = 20
-print("Sum:", x + y)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
