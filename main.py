3b15e8c89cbdaf7ea6ce26cf2981b5529659c1b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:35:05 +0000

    short makeinto advice balance cheat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import sys
+print(sys.version)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
