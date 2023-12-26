652a370c67f750be597b867f62f60d6166194ac0
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:46:48 +0000

    today pocket eat criticize accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
