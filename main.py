7e59f09b52d682a61dc504af20216827513ac445
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:07:05 +0000

    noone damp uniform tidy poor

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def count_words(sentence):
+        return len(sentence.split())
