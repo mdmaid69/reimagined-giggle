0ca0deabf1566e628b0f8e3efb7926e40b86f3d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:24:51 +0000

    meal less father phone damage

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_binary(n):
+        return bin(n)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
