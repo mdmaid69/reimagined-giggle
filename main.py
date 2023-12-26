6a90717f9a0f6c5e94f2768a6e4c8eef3cffbf1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:03:31 +0000

    completely many disagree cinema less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
