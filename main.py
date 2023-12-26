541b1f98043b2116017727d5d193d65c96ae4b39
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:53:33 +0000

    ear vision profit breakfast base

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ def factorial(n):
         return 1
         else:
         return n * factorial(n-1)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
