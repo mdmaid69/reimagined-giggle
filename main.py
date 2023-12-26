d7308474d17f62cada97808ef261a3efecf57dcd
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:51:31 +0000

    pair problem negative activity influence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
