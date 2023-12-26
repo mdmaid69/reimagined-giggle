200c6a7793a7f3596c6b9a450e039262efe85d52
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:03:55 +0000

    sorry by why holiday dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
