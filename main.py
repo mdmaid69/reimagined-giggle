346583b0de5212e97091d79c34f84acb26504c45
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:54:24 +0000

    turnoff complicated tough appeal except

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print(sum(range(10)))
