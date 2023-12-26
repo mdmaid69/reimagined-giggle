704fdb681b4cd6e1944238039d3de9b291ea6183
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:54:28 +0000

    free reduce division leave building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 def cube_number(x):
         return x**3
