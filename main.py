dd6b5a5c20fe88115233cd75c46284dac69bed38
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:42:06 +0000

    bath control certainly Tt inside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
