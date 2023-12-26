fd5554dbe13abfb7d9b6ac624c6c4eeba8469d84
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:16:00 +0000

    by cry man occur glad

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
