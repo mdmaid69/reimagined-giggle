61e4948ab40135536e3b502527c993e8705aa8f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:25:01 +0000

    test return description advise ever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
