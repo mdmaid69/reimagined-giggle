0df3c95aa31eca60f9752128fb555afc9e2c23a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:26:03 +0000

    quit position seem well force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
