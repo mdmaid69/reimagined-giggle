4e2462d01b33fe6b7e5680f426bdf140e375bdc5
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:26:55 +0000

    experiment doubt subject Zz scream

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
