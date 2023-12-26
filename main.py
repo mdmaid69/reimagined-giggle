3f6c226ca3f8e35a24db8eb1242544e7dd6895fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:13:37 +0000

    turn pinch see sore destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def add_numbers(x, y):
+        return x + y
