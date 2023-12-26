ef2e7faaf75d7051e5450ba1c4670245ef859375
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:25:47 +0000

    example same increase section instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
