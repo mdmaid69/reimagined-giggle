59c78ca270ccc86a39e065cb73f3a9c47ddcc3be
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:10:30 +0000

    voice claim recommend regional string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def reverse_array(array):
+        array.reverse()
