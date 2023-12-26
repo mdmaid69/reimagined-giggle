6f680753375889c35aab249c11689b8a5d4f3db8
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:02:49 +0000

    professional conclusion brown bring his

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
