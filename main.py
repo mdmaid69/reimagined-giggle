15209e8ecac4c1c813393d46ea58773d57766e34
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:52:48 +0000

    boat moral public event choose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def remove_from_array(array, item):
+        array.remove(item)
