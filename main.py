e22a5d5b9d782c47475f7b59b153c7100797da3e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:26:43 +0000

    dark chin turn uncertain trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
