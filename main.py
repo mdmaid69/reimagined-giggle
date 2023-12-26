5444336eef99d10505f098c3c9e7e76054d4dd5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:32:38 +0000

    strange nothing poor order desperate

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
