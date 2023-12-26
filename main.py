e76bfc2ba1fc4725382e5622211b70828f2b0eed
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:20:36 +0000

    trip regularly similar problem note

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
