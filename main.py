2b643458cef57888e51feb764feb365a6de92a29
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:32:22 +0000

    mysterious stranger stone miss character

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def reverse_list(lst):
+        return lst[::-1]
