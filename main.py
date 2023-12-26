4ebe7826831d640bc3ad4ec55d9086e071fd58c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:40:58 +0000

    giveout bar open value snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
