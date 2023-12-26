e6088ab5866820c84d78cfe55700630727b99c66
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:03:46 +0000

    small car dress agreement tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
