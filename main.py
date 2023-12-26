be2673614c4024ba4bd9fd10277f79744613c3ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:24:21 +0000

    original surprised doubt breakin demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
