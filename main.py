11b0314209a1ffde80cde380bad0a28ca0d476c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:43:28 +0000

    communicate shallow benefit machine herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
