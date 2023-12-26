d8daf7e9407916bb0bcad6658d9fd74889f28af5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:29:19 +0000

    car neat pile tone fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_unique_words(sentence):
+        return set(sentence.split())
