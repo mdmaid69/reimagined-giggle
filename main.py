e17052d9558ac1dc17d5269a5e1f204de7d421af
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:18:13 +0000

    turnback extra talkinto thing odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_power(work, time):
+        return work / time
