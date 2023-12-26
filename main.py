3cf728844fa6ed12cbd7e684e97e811f5471e4b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:56:58 +0000

    roll breakout beautiful power keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
