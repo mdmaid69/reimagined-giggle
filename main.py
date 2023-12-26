20290701b48b5f29b23f1452ee560e26a1768312
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:18:21 +0000

    divide regret medium count lift

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
