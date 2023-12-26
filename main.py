c49c6f06c5fbf36fa1a84c5ab2323fd704132b6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:23:47 +0000

    disappointed content smell considering undo

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_area(radius):
+        return 3.14 * radius * radius
