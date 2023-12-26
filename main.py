7d9151a9a45213c52ccd399a2281a819648e0cc5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:08:39 +0000

    tough rain shock maintain really

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
