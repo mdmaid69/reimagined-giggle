da30534bee6adc4b04672de5d925c3ebc081f8c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:01:42 +0000

    survive manage between visit street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
