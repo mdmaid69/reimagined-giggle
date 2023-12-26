ffb5196f9603692407562abb3a32312ef561ff46
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:26:19 +0000

    age dark base through better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
