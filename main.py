1060f114db42e904c08d5ffe043a119a5ae681be
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:21:44 +0000

    under although just sell error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def is_even(n):
-        return n % 2 == 0
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
