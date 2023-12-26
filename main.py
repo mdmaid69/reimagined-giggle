adf894d36da0debc2747c6e73b696039a5bc5d1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:51:17 +0000

    straight before letter characteristic colour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
