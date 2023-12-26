7617b06aa56efa1a2f8e094a13087d3b55df3efe
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:05:50 +0000

    stupid trap plan understand Yy

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_pressure(force, area):
+        return force / area
