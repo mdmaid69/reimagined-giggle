11ef751a2c06768dc0b1b5c21af8e5a88199c273
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:35:15 +0000

    explanation exchange claim slice unit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+for i in range(5):
+        print(i)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
