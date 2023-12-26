9153b881559465c435aa57fb11b885ac2f23f171
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:16:56 +0000

    act whole dress train mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_min(numbers):
+        return min(numbers)
