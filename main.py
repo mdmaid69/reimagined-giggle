aedd4a6fdb1795eed68ecb09431025cd8ddc7a8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:12:07 +0000

    music steep action moral round

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
