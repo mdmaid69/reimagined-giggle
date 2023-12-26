6db6cb5f2aa47b6a94900a9817ae60af0613f00f
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:11:13 +0000

    present face sticky term control

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
