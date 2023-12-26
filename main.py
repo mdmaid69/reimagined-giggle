1d273f7d854b9491b197fb147c038b40e3feeef9
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:14:50 +0000

    acceptable content content sudden content

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
