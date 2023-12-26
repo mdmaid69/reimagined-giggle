3c1b8162852ef00e75b5aa7fcd3750ec8aafe4a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:47:02 +0000

    score clothes feature press live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def count_characters(sentence):
+        return len(sentence)
