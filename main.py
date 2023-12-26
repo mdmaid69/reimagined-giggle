70fe941f1f0147998a32281173261e8b30a8a127
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:52:19 +0000

    able different entry family love

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
