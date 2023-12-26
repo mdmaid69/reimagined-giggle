7918ef451f5aed549c4358cc3f9883d7a541e9da
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:09:28 +0000

    keepfrom now anywayanyhow true myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
