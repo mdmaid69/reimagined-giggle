f7c68c79236a110e61c1d691dd220b90a1828e2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:51:28 +0000

    dark tongue card possible zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
