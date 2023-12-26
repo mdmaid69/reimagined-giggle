48a93b12faf5ef504c59eb8db13ea0b8fd283d38
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:50:20 +0000

    post unit excuse juice past

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
