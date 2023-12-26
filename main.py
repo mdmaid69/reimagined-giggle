fb36037d7fc3408aa67334dfd8f1b904ea85aa8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:08 +0000

    space relative could whether general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
