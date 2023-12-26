f6823d5f6fcb64ec08ceca9b875e063509796a84
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:14:15 +0000

    lead various Rr spill oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import time
-print(time.time())
