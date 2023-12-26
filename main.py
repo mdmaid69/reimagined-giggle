a76017259aa8bf9e56eeb37ebd92b73567200814
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:32:22 +0000

    impressive unit connected noone callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
