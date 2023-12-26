e108f57fb7358db49f839446fef8bab94d5d01b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:29:42 +0000

    close suit sore anything convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+print(sys.version)
