2bb7060b0f23117b36dcad8850455adfaa0dacf3
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:48:02 +0000

    house destroy shopping brush appeal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def get_current_working_directory():
+        return os.getcwd()
