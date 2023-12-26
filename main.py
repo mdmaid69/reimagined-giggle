921a4268f10aefbe70195041467dc89c56f364c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:01:40 +0000

    shock pollution opinion inquire slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_octal(n):
-        return oct(n)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
