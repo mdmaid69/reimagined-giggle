29ea510d79943abd78e5af692bf73ced0e4f09a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:42:26 +0000

    variety wait remark place trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
