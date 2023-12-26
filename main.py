d2dedb31cce036362000bc98701dd7d80b4d64e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:25:16 +0000

    one really godown doctor loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_density(mass, volume):
+        return mass / volume
