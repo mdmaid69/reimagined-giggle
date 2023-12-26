c750f606e4e2c757b9db9eb115e6618251dbf24c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:38:15 +0000

    town exercise say especially still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
