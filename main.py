c590df1a1306d6adc7a8eb18fa654a4482df5a78
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:07:21 +0000

    naturally big thinkover keen fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def clear_array(array):
-        array *= 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
