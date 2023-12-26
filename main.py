8b6849d514e93a80264ba2979794b3278e075a9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:14 +0000

    to sound recent winter guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
