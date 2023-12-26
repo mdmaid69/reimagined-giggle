94257a9ce47f8ca76b2e53bd6de8bf7f0fa77500
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:08:56 +0000

    not destroy lesson 572enter offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import time
+def get_current_time():
+        return time.time()
