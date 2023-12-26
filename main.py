cbfad1907a98a8b13695f2153a305ba394879e5f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:51:24 +0000

    house towards share communication ear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
