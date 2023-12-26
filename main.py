56666085b83a0c773336ce3a72461200f8cf7ed6
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:21:14 +0000

    unpleasant fair bag represent interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
