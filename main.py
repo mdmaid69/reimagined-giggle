11e68f93434b4efbf63391721d46d9b52ea6566f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:18:23 +0000

    remember confused get winter material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
