3614706e749f56dab54ea776233d8198858523f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:18:59 +0000

    afraid passenger brother stranger hospital

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
