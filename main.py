637c36754b5fab56ae83060927c0346a06241707
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:27:34 +0000

    risk piece connection reduce list

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
