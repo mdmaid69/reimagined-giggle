be1787451d9519efabb1557a11e19adacd684a43
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:22:05 +0000

    support complete drive point side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
