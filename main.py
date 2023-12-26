18b77bcfce3fe8818bef23e7715bd295ae30f5f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:29:18 +0000

    quickly bus sing realize disapprove

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_as_set(array):
-        return set(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
