6a587b3deededab39e36a268e57722591c2aa4e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:57:50 +0000

    stop him sticky reply confidence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-for i in range(10): print(i)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
