de063d75f457d825d4ac1fc6fd536e060d5eeb84
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:06:10 +0000

    rub here professional repair boring

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def square_number(x):
+        return x**2
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
