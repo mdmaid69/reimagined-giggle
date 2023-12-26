9ce90f8e29448e61afa44f5995a0c7ea65d38576
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:09:55 +0000

    trip send at call hurry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
