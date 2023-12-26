847022e5418ee4553693a74ab7a308a31e5b91aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:36 +0000

    positive sit somewhere away coffee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import datetime
-def get_current_date():
-        return datetime.date.today()
+print(sum(range(10)))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
