2a54e54a13da2612e9d9974dfe7843c56b41291b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:52:58 +0000

    detail congratulations shade dear sore

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
