2b791f0bcd378909172a9535cbd9c4e5afcf76d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:00:03 +0000

    hand miss mess storm slightly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
