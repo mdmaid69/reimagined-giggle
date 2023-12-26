f39bbbf84876a51f63e58a5f75b9fa51cfb598f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:33:47 +0000

    sure above pleasant another many

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
