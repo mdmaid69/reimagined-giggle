9f4ddd7bda1af361bc321cf5673dde3216c16d4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:46:28 +0000

    pretend whole please think front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def sort_list(lst):
+        return sorted(lst)
