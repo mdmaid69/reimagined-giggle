050f7c8b8674aa12adbd42b85174b0079b5471b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:26:09 +0000

    serve crowd beyond many wait

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_list(array):
+        return list(array)
