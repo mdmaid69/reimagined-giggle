a637b34abb4b881168698c2829d0baf166ff6cc0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:05:28 +0000

    unite couple wipe comfortable accept

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def remove_directory(path):
+        os.rmdir(path)
