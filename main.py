02e8bbf87f9c8c51c797b083d3bf682c6f673162
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:25:36 +0000

    scream whisper month fat fair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-name = "Python"
-print("Hello,", name)
+  def get_current_working_directory():
+        return os.getcwd()
