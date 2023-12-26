66fb892a5801ea80c4f6d7f1422400f9ce2f1425
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:44:50 +0000

    sound interview kill break tap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
