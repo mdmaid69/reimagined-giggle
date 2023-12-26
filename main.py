08a93d8c0b5e7e5b3e21f6529726521e9fc22419
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:24:41 +0000

    first either swing experience world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
