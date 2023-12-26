567c2842bc5ca0bf346b5a057aaf53a72d147ebc
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:53:42 +0000

    left guard history lookafter drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
