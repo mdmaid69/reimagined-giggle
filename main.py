bd130eddbd92f593f1a835f7554058644fff0b86
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:36:14 +0000

    bin our computer total close

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def reverse_list(lst):
-        return lst[::-1]
-def subtract_numbers(x, y):
-        return x - y
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import json
+print(json.dumps({"name": "John", "age": 30}))
