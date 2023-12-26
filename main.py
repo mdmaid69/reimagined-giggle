1f18e62f1e0bf3e816668d2c93d1c7a795b85f0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:31:57 +0000

    on square there transport direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
