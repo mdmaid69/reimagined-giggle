3e5b9b040b434f760598ef369776e2f4164be81a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:48:09 +0000

    heart common wire separate such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
