a0da99bd4b299bb347ecb8823b43c117eaba4641
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:14:06 +0000

    bored bread mass ignore whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
