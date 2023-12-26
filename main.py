0128c3a7bf4198c900db42e2637775fb9ea4edaf
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:22:22 +0000

    precise lay colour forgive case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
