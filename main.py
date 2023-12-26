e202191d946592139c2feaf3b1d297c0e5bb1bc5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:22:31 +0000

    of plenty colour as reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
