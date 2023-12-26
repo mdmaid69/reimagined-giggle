f0e4ad59783dee0106cf24c0dc4886341403f967
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:04:05 +0000

    fork evidence hurt sting cover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_length(array):
+        return len(array)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
