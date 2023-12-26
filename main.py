a1d34afb4c3506fdfb359bd8a6752826c447ee3e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:16:59 +0000

    bringback true dream breakin sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
