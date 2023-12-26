e4f5e9d15e1da89957c4a0edc3a5ce461063efe4
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:16:03 +0000

    why deserve rate suggest rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
