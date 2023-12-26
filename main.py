8f85396c6c9eb1af132f4ac7748ec35b704bbba5
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:22:23 +0000

    tongue willing polite talkback deserve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def remove_duplicates(lst):
+        return list(set(lst))
