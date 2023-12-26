95337bce5878069a025fd7791b2ce0a8ad3df63b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:22:20 +0000

    brush mess sudden would put

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
