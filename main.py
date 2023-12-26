3ac6a98e3a1e551cfcaa3b9f915bbb70e5bae0b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:50:40 +0000

    effort organization keepup surprising sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
