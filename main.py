cb5a01083b6ec05e4fb26063922341e73d219871
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:10:19 +0000

    behave score rely string bottom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import time
-print(time.time())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
