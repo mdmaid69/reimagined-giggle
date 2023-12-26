b89befa0da99e4ff4861a06d6ed62b7ca999ad12
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:51:56 +0000

    pronounce fresh voice hit sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
