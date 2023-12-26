249164d3c07857df333f18bb5dedb136bed42483
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:53:31 +0000

    holdout poison completely broken Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
