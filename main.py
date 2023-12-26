ecc4cbfc55d65eec8a4549f1e9a5208d750e1635
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:34:24 +0000

    bright Jj science such convincing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
