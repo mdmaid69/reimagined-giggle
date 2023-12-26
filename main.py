34f97033307061dd7a6a5fcbbfefdd92f9dece89
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:42:45 +0000

    various scare whisper cost Pp

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
