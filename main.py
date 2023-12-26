0be9979c45c9f1705db6fbc58df308b17342d2ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:43:47 +0000

    frighten whistle high include set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
