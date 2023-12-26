ac2db61b16124d6773ef28ff78663088678c41ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:40:55 +0000

    happen correct duty instant practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_exponential(x):
+        return math.exp(x)
