6e32471e2d2be48686e9576bae978de53ddd24d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:45:23 +0000

    doctor raw clear power bury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_list(array):
+        return list(array)
