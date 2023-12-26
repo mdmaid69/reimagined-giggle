d73bb90f2d05d8c168aee35622904abb205e8112
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:30:12 +0000

    once also form tradition for

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def remove_duplicates(lst):
+        return list(set(lst))
