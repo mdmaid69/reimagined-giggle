0a293e2838faaf2309eb2ea411af2b840b5fdc07
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:45:31 +0000

    sort needle stranger connect big

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def remove_duplicates(lst):
-        return list(set(lst))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def sort_numbers(numbers):
+        return sorted(numbers)
