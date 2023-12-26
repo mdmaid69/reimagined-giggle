df374f4ac6c8eadf8864abcc34e620d84d8d85ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:05:27 +0000

    sale interest afternoon afford brave

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
