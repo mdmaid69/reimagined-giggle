4929baf129bc077202c2652898cf5ccf2229dc80
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:32:03 +0000

    turnoff exaggerate trick collection high

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
