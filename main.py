f1f732294833b2d8da76650d2db0c3337a4f4b98
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:36 +0000

    tap send present count news

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
