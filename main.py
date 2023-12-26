3337e6d9eb948963a1238c8f9e68beec3ed0c4e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:43:52 +0000

    Tt test amount machine conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
