9928be8637fa0280bd1070baf4b2ec63723f1075
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:22:48 +0000

    far arrangement nervous mistake unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
