717ea22aa88c868888d9abc9c97e554e9344b527
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:03:02 +0000

    disturb chief repair apply flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
