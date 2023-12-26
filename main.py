d1f35c3251f15b6b0d19c517442d1d09aae77a63
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:07:51 +0000

    giveback point discuss leave block

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
