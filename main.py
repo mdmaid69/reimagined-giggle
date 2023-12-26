ce7d236da3d2b1d3bf18472eef4b81f31839b0fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:12:41 +0000

    plain pure door answer beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
