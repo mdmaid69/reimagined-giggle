e98929549a43a7f7c21ec837e0e60bda7af34141
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:20:47 +0000

    instead illness fix method nowhere

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
