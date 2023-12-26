8e5ef5591eccb7489e45986735719c720282bd86
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:50:28 +0000

    trick shut experience vision freedom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import random
-print(random.randint(0, 100))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
