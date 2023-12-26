6af467f0621ff1e81f7f068fedaef8aa44f3d7a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:07:25 +0000

    any track pay pin along

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def divide_numbers(x, y):
+        return x / y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
