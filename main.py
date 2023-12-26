9270a59e5657eacca848c242d47182eeae0e9ee3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:55:15 +0000

    wander comeoff Rr accommodation active

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
