cc6afbee2a57790962d50204796a44ed5c2f8455
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:49:47 +0000

    back Cc payment travel wish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
