5c29911bed7ceb3e12e67feb0c65c65ba0d496df
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:41:46 +0000

    person smell bye apparently scheme

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_pressure(force, area):
+        return force / area
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
