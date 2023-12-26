e45f86a41fb0f8d1a591162bebb57a061147d034
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:18:40 +0000

    trade shoulder slice prove note

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_octal(n):
+        return oct(n)
