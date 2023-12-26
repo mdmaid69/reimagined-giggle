fbb8bb94405654d9a4c8e28a23ea7c3ae8e631a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:51:40 +0000

    experiment disturb handle average waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
