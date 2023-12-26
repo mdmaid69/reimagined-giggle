1ce9c7f02531fe3e0077478ea5cea3592d373dc2
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:23:19 +0000

    wonder profit admire like meeting

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def sort_list(lst):
+        return sorted(lst)
+def is_odd(n):
+        return n % 2 != 0
