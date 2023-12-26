8a33e3c64164086406ea6343e811e335e186e5e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:42:59 +0000

    satisfactory brother just uncertain fly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_power(work, time):
-        return work / time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
