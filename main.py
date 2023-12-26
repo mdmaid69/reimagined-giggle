626a3f2a59af13199fc42035b5677c9b58ba7c9b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:50:22 +0000

    run personal peaceful bill back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
