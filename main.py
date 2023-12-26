a461c74e73a3aabb65675b76027f3051ce2ad159
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:54:39 +0000

    stick separate flow buy favour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def cube_number(x):
+        return x**3
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
