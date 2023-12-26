965726cb5d81cfa9669d7f053c408bfda67b810b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:40:17 +0000

    570enjoyable regular fear somehow wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
