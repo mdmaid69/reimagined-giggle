5c65f2a8f9cb4eb165c81b0f52fca29b972734f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:54 +0000

    nervous chat answer kind hang

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
