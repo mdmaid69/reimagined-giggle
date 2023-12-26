24e4db7dd69e6f98ec04d216376d855cc0b4f865
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:13:42 +0000

    recent confuse bring through hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
