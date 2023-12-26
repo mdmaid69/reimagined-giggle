aa567fb3b8ea7cfbc91646ab7e19eeb967743703
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:10:25 +0000

    fond clue runinto ago beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
