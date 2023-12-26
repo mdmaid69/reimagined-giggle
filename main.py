0a27da9adeafab9b8487e9e7ea6013c6d63cc7c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:39:10 +0000

    factory never share persuade against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
