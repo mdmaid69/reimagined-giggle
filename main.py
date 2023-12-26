0f17e87b13a109ea6615579b136bb7b6a37e15c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:14:37 +0000

    science angry standard handle delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
