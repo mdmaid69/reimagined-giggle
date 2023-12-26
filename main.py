fa75a0660d627191bc6a91f1985f59b2e6fdbf48
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:33:35 +0000

    hang wave pocket permission bad

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-def is_odd(n):
-        return n % 2 != 0
+  def reverse_list(lst):
+        return lst[::-1]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
