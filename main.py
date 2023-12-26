12f8a24e0f1ce8925f00a6464e647420fc02a8b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:56:54 +0000

    limit system run sorry party

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
