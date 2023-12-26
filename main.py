4204335d6028d8dc58153f5c1f3966b8066cf2d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:36:20 +0000

    why mix original accidentally worried

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
