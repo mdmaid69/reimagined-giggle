62d9b0831f80380b6048e1f61285cdd8e2853c62
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:50:57 +0000

    talk official load spring scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
