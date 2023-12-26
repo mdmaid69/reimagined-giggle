c4741eacb4e44caf55a0804794bd6f8d8802bde7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:07 +0000

    disappear runaway transport right comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
