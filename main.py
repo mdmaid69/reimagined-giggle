c074aa9dda60fbb74bffc02f5b38760816a5a658
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:08:13 +0000

    contain drink schedule ordinary stand

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
