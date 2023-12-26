b294ced480552518f3d123ccb2ca0a3787a9f3e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:56:10 +0000

    treat sentence wire language passenger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
