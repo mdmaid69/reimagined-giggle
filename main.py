9eceac63acc8faa4d815deaa5817a96b4db02ae5
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:53:21 +0000

    workon crowded smoke heavy minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
