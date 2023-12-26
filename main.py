4758e89648affaf572b93ac2f5915725ae482c2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:04:15 +0000

    close estimate before chase which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
