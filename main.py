a2069ed5f8d01e3d098bdda687cc01d1c16c9cd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:23:20 +0000

    circle production charge shade thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_volume(length, width, height):
+        return length * width * height
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
