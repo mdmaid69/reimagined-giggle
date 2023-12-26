83fe7fa06ccd1981fda1c1b928433b072635ad59
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:46:01 +0000

    open share spite downstairs translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
