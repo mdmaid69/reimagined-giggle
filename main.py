fc2ec5f19add80e04fe35120c5597efd46be7217
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:56:25 +0000

    reserve probably match house between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
