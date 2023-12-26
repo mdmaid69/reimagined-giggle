c6530d72c161c9f58607b57939ca4a77cdcf0398
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:12:31 +0000

    shame take beyond record what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def append_to_array(array, item):
+        array.append(item)
