fad7894024cad85dfcaf02769fd0d5f78950b462
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:47:41 +0000

    quick turndown approval tense Vv

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def multiply_numbers(x, y):
-        return x * y
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
