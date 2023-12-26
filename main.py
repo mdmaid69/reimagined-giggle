fbbc221a58fc35788f48f299e8fdc9c1f155ede4
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:05:38 +0000

    check sail sense wild so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
