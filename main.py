6bdd9f6bdc02b2513c8e86df8afff48e7041fd48
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:47:01 +0000

    knock create downstairs safety backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
