ebe739b50548d61e2e1a9c221ffb8a3f04e5d50e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:32:30 +0000

    private charge pleasant respect nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
