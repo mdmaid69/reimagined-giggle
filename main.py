6087f620cbe2ea46eafe9fde831411e4d1f3fb79
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:12:07 +0000

    gap remark gooff sure party

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import re
+print(re.match("h.*o", "hello world"))
