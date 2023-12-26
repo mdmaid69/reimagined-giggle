4515e47defe875058147e8b497074441a4d159a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:27:29 +0000

    punish interview mend convinced herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def count_elements(lst):
+        return len(lst)
