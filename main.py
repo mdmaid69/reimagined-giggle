051dd0faf6b29849b81f7dd950fbbfc9b8c56479
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:55:55 +0000

    black attitude measure money rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_set(array):
+        return set(array)
