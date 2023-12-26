fd0b6d6867e16027f2b9d56d4e1815c4a4b19267
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:03:57 +0000

    talkback spin risk call cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
