5f1c44ad90058ae6f6c051bbb1ee42286b01826e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:58:02 +0000

    rather official talkoutof general hear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def multiply_numbers(x, y):
-        return x * y
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
