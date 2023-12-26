440d9ecf52e13e61fa9c8c6e896d5e8690e45cdd
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:37:07 +0000

    dish actual answer start swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
