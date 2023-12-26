68278b09e122283fa6add9b158a92c1a61e3594d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:09:58 +0000

    closed preparation so once refuse

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
