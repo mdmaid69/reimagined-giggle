fa8f8365b4f0a3da74ac838a3d00c7947692c055
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:09:13 +0000

    set any blame female herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
