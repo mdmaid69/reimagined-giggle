9c4e91b7372f255c0e46fcec5b24cb534e2bb022
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:22:44 +0000

    tape boat appear thick twist

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
