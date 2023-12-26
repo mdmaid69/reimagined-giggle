a3da5924e0a942edda0087ef440879dacb58d5e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:05:32 +0000

    waste crash certainly tune kick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
