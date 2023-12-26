f33ec8a86c940ab709e5399157b8c2d94fa012d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:16:08 +0000

    experiment scream increase breakin ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
