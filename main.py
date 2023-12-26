4213c13fc2d7e263161669b2eec32d67d62c209b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:17:48 +0000

    flow mistake always shine phone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
