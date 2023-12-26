4e263ba27c7a3faa117114ac49ea2e15a0ec2b0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:06:01 +0000

    hurry serve determined sell summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def reverse_array(array):
+        array.reverse()
