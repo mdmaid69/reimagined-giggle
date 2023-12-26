5ab953ed4dd8a89d4f831d08020ea828a8fef482
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:27:37 +0000

    dare have date turnover wander

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+def print_python_version():
+        return sys.version
