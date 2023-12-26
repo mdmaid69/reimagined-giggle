5f913fd2f55c6fb6e036ecaaf9143f040e44356d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:29:24 +0000

    disturb conclusion smell brief chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
