6c6808008083bfb04011bfde3ead3172c686f78b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:03:45 +0000

    fence tone precise he throw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
