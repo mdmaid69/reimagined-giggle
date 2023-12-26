feeeeef5631c24ce2de5a03f9d0113a6a7431d8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:18:39 +0000

    long communication tonight finish either

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
