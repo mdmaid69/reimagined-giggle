9e7de00576f41b7411535f5a2ee47cce55a8b26d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:28:28 +0000

    last sore noone comein force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
