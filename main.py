d5e4b4fe5a4629f90196665aae7aa448aa894cef
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:25:40 +0000

    freeze under somebody letter remember

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
