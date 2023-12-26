627adea0f81b585c9c99b6a171689bcbeea04a21
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:08:49 +0000

    what often collect prevent keepaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def reverse_list(lst):
+        return lst[::-1]
