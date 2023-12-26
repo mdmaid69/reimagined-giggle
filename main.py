ab695fe3a646472cfdbbef399f2b8c97bd894b83
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:00:23 +0000

    drop injury she speak size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
