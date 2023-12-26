23d8a7d4be47650c2a0bbfbbdf97ecb74804895b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:56:22 +0000

    drop rush plant relief health

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
