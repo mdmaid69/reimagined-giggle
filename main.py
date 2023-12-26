fad4091c018cfde99d977daf380c6c2d965b5887
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:50:07 +0000

    message mistake radio off congratulate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import numpy as np
+print(np.array([1, 2, 3]))
