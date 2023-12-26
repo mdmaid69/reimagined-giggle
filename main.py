f1eae8c2c5103e81c3251c04462c065e4f558224
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:34:08 +0000

    pure get into wild hollow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
