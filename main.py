02662467ab8afa7c97c6f821bc66f6676547bfa8
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:10:51 +0000

    desperate judge set regular mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
