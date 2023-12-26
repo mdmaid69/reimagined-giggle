0c5711eef85c3dcb64aa1e26b18b5c4ed7730875
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:26:08 +0000

    wheel holdup cheque balance gate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
