54fb7821755838467f564a3c917de4ccf4d315c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:05:02 +0000

    recovery embarrassing hurry patience bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
