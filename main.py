c40fb9eb0fef3bf81e9b47eef6b0aa60d48925ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:17:53 +0000

    strict major hit length occur

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+print([x**2 for x in range(10)])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
