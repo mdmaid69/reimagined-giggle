b099c3ce587b4ac13298a964c43de387de508995
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:47:53 +0000

    goon idea approve callback mention

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
