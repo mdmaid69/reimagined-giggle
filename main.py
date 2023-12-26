03adecbf3578f6a527310f098326aeb28444b64b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:49:58 +0000

    fortunately interested chin handle want

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import time
-print(time.time())
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
