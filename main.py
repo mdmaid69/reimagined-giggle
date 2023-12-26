883ea6285af703894170e271d37995c75319a33f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:16:53 +0000

    keen Itself 179bite weather eye

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
