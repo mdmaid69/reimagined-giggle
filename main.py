24d14da5aae1bbf6719cd5f347908e943abb9bc8
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:51:10 +0000

    calloff march anyone ticket steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
