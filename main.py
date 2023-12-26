0a95f48747136fddb7772de9c0a8a9695c60fea2
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:06:42 +0000

    guide wish pay heart ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
