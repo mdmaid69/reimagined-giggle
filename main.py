3dbb02dfbe77a84eb7936412b3bc383a62a21da4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:48:21 +0000

    responsibility fat runout programme rub

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_max(lst):
+        return max(lst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
