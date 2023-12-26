9081cce9ea5dd2cb2863d905eea0adf601d8e5f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:31:05 +0000

    arrange degree could so sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def convert_to_octal(n):
+        return oct(n)
