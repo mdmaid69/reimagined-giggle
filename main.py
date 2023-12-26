5bc01293ec04303facaa8c2579dd47c94d6e9811
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:00:26 +0000

    shallow represent pain light Ff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
