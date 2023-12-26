4ab01deea5744f2c9a89cbb83c43a8df990fc402
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:21:35 +0000

    active fasten rather classroom something

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_max(lst):
+        return max(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
