7fb49aa1c4ccd9fa244dfa7990639b1b15671bc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:34:57 +0000

    tooth chase secret interruption behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
