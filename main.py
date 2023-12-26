8baf5d119f9063bceb330ebf0d34e9051556a381
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:41:13 +0000

    whoever breakfast live that polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
