8284474a7292e408d0926a58cbedade7951cf2a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:22:48 +0000

    571enough trouble ground stamp cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
