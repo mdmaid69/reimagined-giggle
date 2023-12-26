975aa1e4d84ff593465cd6553f92f81ac456eae2
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:35:15 +0000

    operation holdup hard illness then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_length(array):
+        return len(array)
