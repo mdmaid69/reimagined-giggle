8e105391f13af75718d52e11771022788891835a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:00:03 +0000

    fine immediately really experience into

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
