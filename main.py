a2b3296e9972eb64709bacc4a6d2f24fd891b94a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:13:56 +0000

    factory mine justice limit concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
