c94de9471594de3669f908ce7adb8baea2a951c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:32:01 +0000

    fear freeze eager before appear

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
