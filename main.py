20b21c9aa6d294b093eda9b41649a576fc4431e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:04:16 +0000

    age knowledge push medium second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
