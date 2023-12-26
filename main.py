11a31e1bd5bd7a2dfa7aa3cb5f5b09bc993f6a8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:41 +0000

    press factory wire good press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
