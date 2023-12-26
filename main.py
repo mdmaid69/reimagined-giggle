b2ae776aeadfdc72864bceb02cbec893412ea619
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:41:02 +0000

    absolutely habit centimetre reliable as

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def sort_list(lst):
+        return sorted(lst)
