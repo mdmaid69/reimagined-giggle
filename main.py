52c519c58bdf9e1a554ba75318b19493d5581c38
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:02:49 +0000

    extremely during abroad 570enjoyable communicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
