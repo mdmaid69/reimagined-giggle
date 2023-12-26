17dd606e4c2d3325ca2b26258d3f4c1da09c242e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:42:57 +0000

    know attention separate each believe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
