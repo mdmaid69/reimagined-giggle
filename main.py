83eabe2901f7aba49aaeb8009c5331d2918616a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:48:33 +0000

    lost laugh respect congratulate remain

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
