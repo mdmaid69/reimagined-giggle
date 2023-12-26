8c2582ca1298d8c958bb22ff3177b0e49a81f418
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:00:34 +0000

    step damp imagine contents command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
