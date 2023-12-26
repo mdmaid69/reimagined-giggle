87004c71d1bdd829965025f27a357419eadf1798
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:31:28 +0000

    rarely measure find deliver one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
