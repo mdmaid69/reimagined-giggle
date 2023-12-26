699c570d7c01a688ab2e3816136ec207fea4cf70
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:12:57 +0000

    until neck hang conduct geton

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
