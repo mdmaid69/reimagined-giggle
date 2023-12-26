d70eedc7147f5bbea74f7229f10c274ff37f8cf4
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:15:46 +0000

    free equipment she interest record

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import sys
+def print_python_version():
+        print(sys.version)
