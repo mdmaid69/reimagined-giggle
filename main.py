a92b8c0c0148d03d3695222ef36a8776fbe78114
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:54:54 +0000

    experiment unit charge argument spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
