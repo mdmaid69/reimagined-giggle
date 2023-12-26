3ac65e335b0ac8c7cb3af4b72cb0aca29bc1c6e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:45:35 +0000

    other invite suck hope voice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
