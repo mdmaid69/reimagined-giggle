f1ee3118dc1a3bb1b0917c585194df5c630a10a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:38:25 +0000

    nose belong comeout waste priority

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
