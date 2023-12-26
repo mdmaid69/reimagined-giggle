b98926e2b40908d6e24a81c03b1010698a722501
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:53:02 +0000

    please popular both switch guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
