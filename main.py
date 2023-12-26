733dc2a21b959a31d21af040270ca010ffc78820
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:42:53 +0000

    lookback(on) rob class rare promise

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import tensorflow as tf
-print(tf.__version__)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
