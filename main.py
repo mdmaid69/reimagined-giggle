5f0ef8adeee33e48b3c8808795ddda136d2a1cd3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:54:18 +0000

    like sensitive tall by job

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_binary(n):
+        return bin(n)
