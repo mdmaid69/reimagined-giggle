0ba5ea4cbd21f0f1224896bc156ac9f919e4dc01
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:42:59 +0000

    warning clothes ahead post alive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
