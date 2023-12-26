d0eb2d1d6e3adbaf4127c403a49404e0fe7b36fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:09:31 +0000

    spread offer push along near

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import logging
+def log_message(message):
+        logging.info(message)
+import os
+print(os.getcwd())
