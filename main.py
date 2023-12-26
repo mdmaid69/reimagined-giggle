9c10731898723cb1b9a62785ad1bbe4791105d12
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:48:52 +0000

    gate wake instead well secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
