c6be2692719973dc6a316fcc1c9b58c348a73546
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:18:37 +0000

    order violence neither breakthrough wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
