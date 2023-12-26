19b3f9937e308d2a3e7107fd67aaa0b6918ad42d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:38:09 +0000

    light lay create insist suit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def multiply_numbers(x, y):
+        return x * y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
