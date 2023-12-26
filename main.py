4f0a1331b68ea137b5749ef1ae6828911339adc2
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:36:54 +0000

    which breakin month us order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(a, b):
-        return a + b
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_as_set(array):
+        return set(array)
