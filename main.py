afb2747955c94b3168fc781ddd4f4f359daa8d3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:26:20 +0000

    especially hour activity face disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
