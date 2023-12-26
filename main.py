9a921f5a38f4191eadb2e0b758eb37fc44500a9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:13:37 +0000

    use blame past swim regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def extend_array(array, iterable):
+        array.extend(iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
