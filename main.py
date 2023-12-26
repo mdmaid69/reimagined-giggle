275bf626cb5db21d24b5928cba6b76313fef033b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:48:21 +0000

    traffic future connected quit busy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_speed(distance, time):
+        return distance / time
+  def convert_to_binary(n):
+        return bin(n)
