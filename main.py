7a2708bef42ca772361b69b123e3710dd8aab6b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:14:13 +0000

    shopping giveup doubt skin kind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
