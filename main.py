bbe24edde45f8c35cea1a71974abde698e88ee80
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:56:59 +0000

    choose shelter separate at priority

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
