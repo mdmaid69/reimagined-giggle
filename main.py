17f0ca7044da989e34571cc75af4188298d4e601
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:32:20 +0000

    length unable nature spray win

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
-print(os.getcwd())
-def convert_to_binary(n):
-        return bin(n)
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
