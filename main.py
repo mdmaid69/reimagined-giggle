32baa6cc57ed7a010b131c7842ea6557e8f7a81a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:53:46 +0000

    closed load score breakout admire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_min(lst):
-        return min(lst)
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
