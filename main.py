8a546781f579cabe040a3a0feb53caef38cbb473
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:31:25 +0000

    purpose danger myself spend proof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
