23674e61ba5212592c5d12d42bd36c9b33350f59
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:31:24 +0000

    business broken suit their tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
