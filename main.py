359dd765480f53f76b46c6a7544bd176d8643f74
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:49:45 +0000

    shout whatever reply style creature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
