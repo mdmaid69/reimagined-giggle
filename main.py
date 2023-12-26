61bccf2fdebda0d90defb21f18cf00629ad084aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:04:59 +0000

    bank afternoon sale muscle indeed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
