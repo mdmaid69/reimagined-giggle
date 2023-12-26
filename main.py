01ed2891d4c1466b2ed6df6809396576e59bafd8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:35:36 +0000

    smile low straight giveaway Jj

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
