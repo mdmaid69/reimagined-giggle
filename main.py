d9f719de53f5a344e4c5334f9e8adf14e264a910
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:57:03 +0000

    also loss art score consist

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import datetime
-print(datetime.datetime.now())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
