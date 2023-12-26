d1ce1f64489260fb7c66ea8c2351a2df437815f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:56:45 +0000

    odd decide absolutely sad object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
