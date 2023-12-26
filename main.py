60f9582484bf7239211c91548e4dce791e2b9bbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:35:10 +0000

    show entrance because waste suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
