710a581537b93c4b2d896df6f2f52bcc23de9407
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:45:59 +0000

    from run set keepfrom anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_item(array, i):
+        return array[i]
