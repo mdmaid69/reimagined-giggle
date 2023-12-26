de31e75b9a58288b814d012cd15fb064bb408b26
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:21:49 +0000

    ring runinto trade inquire strength

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
