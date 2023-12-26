75ef45b3765a4264d423d1bd8872efb9c96e6fec
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:44:03 +0000

    fortunately memory serve he free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_set(array):
+        return set(array)
 import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
