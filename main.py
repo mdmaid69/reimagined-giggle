4e493e18331012e63bec57a71b88524b2b91d9d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:18:48 +0000

    tax justice succeed common number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import re
+print(re.match("h.*o", "hello world"))
