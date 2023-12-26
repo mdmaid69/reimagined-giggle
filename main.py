26dd66403de51f37a8dba5d85f75eb74051a8ab9
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:11:22 +0000

    watch week about respect clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
