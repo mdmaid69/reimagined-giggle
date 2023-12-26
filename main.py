777f2f5a074aeb73b102cbb4075f438e12ca980d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:14:27 +0000

    trust range cut tap yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
