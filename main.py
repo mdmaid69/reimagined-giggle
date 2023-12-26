7681df06202be992796f71fff38d6dcf212138b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:45:40 +0000

    board almost punish clear today

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
