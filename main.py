6f105078cb7f2effba2a4880a91eb2b8e1dc1178
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:27:12 +0000

    generous 568energy behaviour profit convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
