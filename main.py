1e49cb33e33e303f9c280da39a7e6db44c85e4b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:43:17 +0000

    wide command rough even week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
