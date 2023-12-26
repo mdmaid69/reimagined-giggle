bb230d27986f8cb8a66ba5d1e813cc032bed00c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:12:34 +0000

    keepoff change towards suffer mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_min(numbers):
+        return min(numbers)
