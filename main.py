5f850c15b80576a6fe20889bbbf79fc1087d81d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:48:25 +0000

    cause soil trap ordinary tidy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
