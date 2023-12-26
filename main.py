3ad01d0b21b7a7e046d326f135a476e40614a947
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:04:37 +0000

    taste deliver radio seat team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
