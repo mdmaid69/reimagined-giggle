a9e130c09022691d5d013dc35ef6779167cc8bbc
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:39:23 +0000

    classroom stiff professional some exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
