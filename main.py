90d3d7ca2f48c276609f535d058f7fe637e82e38
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:30:53 +0000

    only broad persuade few unlucky

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import tensorflow as tf
-print(tf.__version__)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
