2318ac169c11d88b211613a477257ebfddc46531
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:47:06 +0000

    fun money train guard rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_density(mass, volume):
+        return mass / volume
