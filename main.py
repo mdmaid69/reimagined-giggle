eb883303fa0a856b193fcc2cef9e94c8d456c0cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:34:20 +0000

    damage touch threaten hole stranger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
