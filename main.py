5a1bc44809a2d07d3f729213bd3ea98667aba291
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:18:37 +0000

    gap church reason beneath really

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+print(math.pi)
