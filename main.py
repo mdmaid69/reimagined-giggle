0de4fe08bd67cb447e5b395d71f4477970e2be44
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:36:31 +0000

    interview mysterious borrow minimum run

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
