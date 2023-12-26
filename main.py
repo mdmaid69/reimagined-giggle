ae999c0e03290606f2114e34c32bb07a1b32112b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:01:05 +0000

    pretend difficulty round minute break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
