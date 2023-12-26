5f9ddd74ff7a4705ec039f6159568ca95af50186
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:04:12 +0000

    store cost before patience comefrom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def subtract_numbers(x, y):
-        return x - y
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def count_elements(lst):
+        return len(lst)
