f06ecd87ad00fe353c48ac423acde999b109d4b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:01:02 +0000

    scream please letter patient compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
