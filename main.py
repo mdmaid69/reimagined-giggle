a6219960789f8846f01ff973df5de11e9127bade
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:18:11 +0000

    dig ever Bb young second

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
