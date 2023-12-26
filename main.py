f8ec8e71e0473d35821fe294e9c71eac09eb2af2
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:28:38 +0000

    noise guess change time doubt

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def clear_array(array):
+        array *= 0
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
