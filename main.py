7ef2ef22317b8dda5a15320c09c2438a313d3f19
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:19:02 +0000

    damage wheel Yy stretch fasten

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
