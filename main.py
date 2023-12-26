e8e505801a08b0dd161115bc5b5372a61060d627
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:31:50 +0000

    fast sleep emotion somebody promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
