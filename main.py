bf288e8301a705561544ea6c0d0b87f1fa61adda
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:51:12 +0000

    rain risk explanation clear better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+name = "Python"
+print("Hello,", name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
