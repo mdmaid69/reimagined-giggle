256c7008a8b5f5b17483be609eaf428ae1fd0491
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:02:05 +0000

    call authority slow basis risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
