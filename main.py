a4a3d390a8ad729d9d52204b6fd404069e49c22f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:28:53 +0000

    prefer load thinkof congratulations contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def get_environment_variable(var):
+        return os.getenv(var)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
