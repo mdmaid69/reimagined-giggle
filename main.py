28917b8d2f2a402cf551e8131c6b9aac982136ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:00:22 +0000

    alone my building account own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
