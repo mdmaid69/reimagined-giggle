97178e68ea9f05b079e0d22ce51cd8551b0420de
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:30:00 +0000

    slice hate have want smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
