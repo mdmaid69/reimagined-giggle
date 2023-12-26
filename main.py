316cf83aa59e9d18ba569bd123ce26ef73691bbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:19:39 +0000

    dick search tell children out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
