6340a51510cf72dd0f03fdfb3e5d3ac56f82d428
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:25:10 +0000

    respect cool say regional great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import random
+print(random.randint(0, 100))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
