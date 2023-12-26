9252803e4475b4778231d33e61921e805283a16e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:33:29 +0000

    573entertainment finger thinkthrough live read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def is_odd(n):
+        return n % 2 != 0
