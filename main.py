2e72bb731c5edff40d1538bed6b36ed129a3e1bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:19:26 +0000

    comeup original cool test war

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
