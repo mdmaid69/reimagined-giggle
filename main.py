70451ba855379f635a21c12c86cb228709d8d553
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:30:22 +0000

    prepare person temper enthusiastic post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
