022ecae096ff589bcd6021bbb267d4d5e133675f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:08:17 +0000

    moon wipe extra divide praise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
