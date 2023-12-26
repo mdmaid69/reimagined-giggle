6ded91f888ed41ff3045075634f5349f27eec8fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:29:56 +0000

    boring thanks harm content complete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
