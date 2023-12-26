cf288a8a8f3dba29d464e7428703d2cdbda52fef
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:48:32 +0000

    plan cover adjective book live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
