37eec1e932914d8abd2f139646bf286b2b9d776b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:40:16 +0000

    bag exactly himself both mainly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
