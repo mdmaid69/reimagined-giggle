da77b1abcda242ec2a9c28a4b060429e3e5d8e42
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:35:57 +0000

    practice future dear much argue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_int(array):
+        return int(array[0])
