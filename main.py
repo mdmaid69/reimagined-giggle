dcdc8f3b45d418ad0c9dc44c0275b8536b4dc9b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:52:21 +0000

    bet capital weak pronounce dig

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
