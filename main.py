671661d61f44883e7301ef655d7a9f0b6e98d9fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:28:44 +0000

    surprising with game ahead description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
