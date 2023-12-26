db84b0e2dabb032715f8dfe76b93a600b6412b4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:40:14 +0000

    who oil disturb meet interest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import datetime
+def get_today_date():
+        return datetime.date.today()
