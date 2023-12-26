e04128d477814e26353540ef663baa2c5e396b44
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:05:22 +0000

    figure permission community drive game

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import sys
+def exit_program():
+        sys.exit()
