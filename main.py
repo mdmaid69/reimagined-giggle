01ac9368b20f14568a1f2b42928d1ba85999dbd4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:16:42 +0000

    rise object thirsty their join

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_string_from_array(array):
+        return array.tobytes()
