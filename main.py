a66ade899916e233a1f40bf29c210c82774d41a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:43:59 +0000

    female open justice twist attract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
