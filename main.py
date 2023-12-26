c66fd5e25cb9a246e067fcdb57a6266f4ce07198
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:14:23 +0000

    inside together skill nose definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
