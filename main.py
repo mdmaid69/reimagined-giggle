fe27d1cf7d8e94843c53c8f7c28b351b36776256
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:33:35 +0000

    band pump call please dangerous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
