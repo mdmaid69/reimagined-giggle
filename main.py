02f6d1a75a42e108d956c44809ea2e676be8311d
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:50:58 +0000

    pay rubbish shout fasten dig

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def split_path(path):
+        return os.path.split(path)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def reverse_array(array):
+        array.reverse()
