48790e2fa408a0c877ed2ccfab1a7c7cdaecf8df
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:19:59 +0000

    no Jj neither fit make

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import datetime
-print(datetime.datetime.now())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
