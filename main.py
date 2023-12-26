02fe6a18ea6adf9f532787207ec5ded7c7492c7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:44:34 +0000

    ability receive partner bound second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
