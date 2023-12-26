111727f486c3521342682bb38841ae3d12d930bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:36:59 +0000

    scream beautiful flat another see

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
