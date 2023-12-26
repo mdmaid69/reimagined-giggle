549b77a8b5adc164c5e3316722223938383846e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:53:44 +0000

    at ticket fetch all judge

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
