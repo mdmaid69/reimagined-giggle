59d078c5698abe8c8ef768a314021d07a12e7856
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:39:47 +0000

    note stop to mess like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
