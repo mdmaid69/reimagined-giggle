088df00b510f839371736fb867000a31680a89e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:28:11 +0000

    smile follow position though describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_list(array):
+        return list(array)
