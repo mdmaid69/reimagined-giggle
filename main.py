86b4096b75381a4d22c892e2046db8dabb23a3c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:53:26 +0000

    whole neat favourite lock any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
