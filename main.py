8a8088e0ff0da437dab2f3bad7b2827dbc90679e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:38:13 +0000

    genuine attitude by strike condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
