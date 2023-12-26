29fb670cd5f66a3fefe3b9decfb496f5a0da8722
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:11:00 +0000

    cause world value hard job

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import random
+print(random.randint(0, 100))
