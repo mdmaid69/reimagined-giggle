8090f63d2301eef9a61454b00cda6e4d12143e65
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:04:09 +0000

    wealth neither exit balance keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
