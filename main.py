3f5595ad7d85692f594c1fcb598881f7d1f9ebd4
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:08:13 +0000

    environment polite around average home

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
