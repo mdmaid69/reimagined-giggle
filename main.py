13423b0c0743243ef165206de2f63db721dec821
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:54:58 +0000

    arrive long operate though serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def find_min(numbers):
+        return min(numbers)
+import array
+def reverse_array(array):
+        array.reverse()
