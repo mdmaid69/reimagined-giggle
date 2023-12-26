af021763b6221c4633c1cf25c5da0f52a66f6519
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:23:25 +0000

    authority emphasize doubt urgent sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
