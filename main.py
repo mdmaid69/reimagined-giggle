10fda011477dc7f4e7cc5df4ed4e1f5799fe785b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:53:23 +0000

    arm wing downstairs wing wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
