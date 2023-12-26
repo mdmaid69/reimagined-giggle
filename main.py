c68ddb41eac9904dccb74b2f57c907d110919533
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:28:04 +0000

    prepare giveout entry rule lend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
