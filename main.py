90c6391c3731ca8576a13cf59314be03db448c56
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:58:59 +0000

    thought sticky weekend cost schedule

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
