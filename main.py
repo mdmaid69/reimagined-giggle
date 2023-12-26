343be130922dc7693e2b73a73663a49ec05a6f7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:14:06 +0000

    tongue ashamed low equal pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
