7f9f6212608505c5b82a012bd5596b732bf51246
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:16:06 +0000

    period space point discuss water

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
