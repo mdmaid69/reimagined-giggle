9ea0cd050493a7f3f570f3ee9e95766ffdeef4cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:14:28 +0000

    only closed fear judge so

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_pressure(force, area):
+        return force / area
+def calculate_power(work, time):
+        return work / time
