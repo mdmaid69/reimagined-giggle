4d13cc45ff78a61e1665f622d3662ae9ee5dd477
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:57:06 +0000

    outside everything produce complaint bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
