3718501f107cf6b83782b5763ab2d7733612d6e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:21:05 +0000

    love unless there style later

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import time
+def get_current_time():
+        return time.time()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
