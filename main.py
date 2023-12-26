00599a3f27131c6769fc213df89590777171e23f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:42:29 +0000

    neat soon roll everyone letter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
