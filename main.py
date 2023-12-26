b5a3e941ee57af5d494615182819265d2a50cf42
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:19:48 +0000

    light persuade quiet music time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
