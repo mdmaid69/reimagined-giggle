a41d56e1e54b81e877c03d84ab963e36c8f5add5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:16:54 +0000

    attract disturb although person winter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import time
-print(time.time())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
