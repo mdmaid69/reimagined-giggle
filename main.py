939970a8f700880fccd0c920227516e0e7086b21
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:19:16 +0000

    small duty goout wake spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sys
+  def get_python_version():
+        return sys.version
