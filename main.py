49783721821575f1a9e3cfc71fb5c3589b0081b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:23:13 +0000

    godown pass beneath raise obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
