a808c26ef1ff8a13dd986760243aa8aaec553ced
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:18:58 +0000

    interruption pronounce interview develop number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def sort_numbers(numbers):
+        return sorted(numbers)
