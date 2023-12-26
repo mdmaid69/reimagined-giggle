403a7d5c5162f99698dd009d208c6cccdd22c8e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:38:23 +0000

    now sport everybody dinner mend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import sys
+print(sys.version)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
