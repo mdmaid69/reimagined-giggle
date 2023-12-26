a4195e2a05edaa0dbc371d58b0d06317330b747b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:14:55 +0000

    light plan deliberate affect trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def greet(name):
+        print(f"Hello, {name}!")
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
