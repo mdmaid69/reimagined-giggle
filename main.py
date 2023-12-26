7993a36c989f2b6d38d24d1650208e4c50bd69ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:45:59 +0000

    once strike keepup confident say

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-print([x**2 for x in range(10)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
