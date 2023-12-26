8e30c62f9863215d8729fe5640cea64c59be822e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:18:15 +0000

    Rr tight normal praise play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
 def get_today_date():
         return datetime.date.today()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+def calculate_floor(x):
+        return math.floor(x)
