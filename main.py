6e1bde5ac7b5150bff76e72d6aed6218971975b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:18:58 +0000

    justice reply report hers excuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-for i in range(10): print(i)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def square_number(x):
+        return x**2
