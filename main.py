ef8b2ac7d6623a1766233087eea454f3e8322316
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:15:40 +0000

    lucky mood basic before breath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import logging
+def log_message(message):
+        logging.info(message)
+import array
+def get_array_item(array, i):
+        return array[i]
