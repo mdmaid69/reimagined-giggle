b9718eadb647aca9e0dac280e4db4cf8214eb0b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:43:18 +0000

    cut dance permission scream prefer

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pressure(force, area):
-        return force / area
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_sine(x):
+        return math.sin(x)
