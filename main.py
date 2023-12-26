8f2672a8420a6b455a9c56c8d9f6c03ee673a74a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:35 +0000

    hard pump doubt post mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_area_circle(r):
+        return 3.14 * r**2
