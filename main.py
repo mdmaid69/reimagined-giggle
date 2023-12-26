41a59fb149e87f7037f2d20edbd93c3ab4c9a71d
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:16:09 +0000

    deliberate beginning too holdon name

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def count_elements(lst):
+        return len(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
