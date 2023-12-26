d864d208aca2816ada8e923ee8f4936d3b5a0a33
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:58:12 +0000

    tragedy translate stamp task factory

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+print("Hello, world!")
