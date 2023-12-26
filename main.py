f14534243476e10d4abcb18952ad09297ef73cfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:10:31 +0000

    accommodation crime content comfort benefit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
