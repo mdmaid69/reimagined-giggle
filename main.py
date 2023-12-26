3fb8fbe45f371b0ba16a631e18beee4d4e02cce4
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:08:18 +0000

    bite vision stone stiff cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
