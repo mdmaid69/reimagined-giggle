7a2305aa95c07d34b261b4e0587750794d7e1036
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:47:56 +0000

    breath usually hold turnover spend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
