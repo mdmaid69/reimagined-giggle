1c6c1e7fb4ce60b5f5b9b1f0f5f8d3f50cef4546
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:13:53 +0000

    active wave stupid out collection

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def reverse_list(lst):
+        return lst[::-1]
