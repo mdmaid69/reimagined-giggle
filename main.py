3ba740669e7cc2a65de373050ae51eb211d11132
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:25:01 +0000

    contact apply measure only die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
