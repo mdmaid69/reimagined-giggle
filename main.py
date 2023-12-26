ed6dfe465d0a42675331692deb7033ef341d6b09
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:35:48 +0000

    before can the begin trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import sys
+def exit_program():
+        sys.exit()
