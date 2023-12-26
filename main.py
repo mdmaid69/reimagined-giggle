f85fc98033ad02cc1ff8edf91835ab8e9641cdff
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:50:39 +0000

    bad turnon argue freedom risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
