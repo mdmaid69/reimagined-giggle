2ff17c035ba68b5a2431db52bc78bd011d2b90da
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:28:38 +0000

    praise Jj damage street around

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
