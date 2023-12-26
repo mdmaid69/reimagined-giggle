cb16d1bab693c64fd70860ca35db9edde7eac56e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:44:37 +0000

    every black whether interesting keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
