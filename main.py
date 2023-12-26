1b6f54786fcbc8881143aa6d861d5d88502049d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:09:25 +0000

    deliberate address properly either call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
