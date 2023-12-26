c0d99bc84e8b9c53e1ccdf3b82b3bd429321746b
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:08:06 +0000

    flow famous band Yy Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import time
-def get_current_time():
-        return time.ctime()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
