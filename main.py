e7b9431dfd50edd9b0f56e8d88045aada8d7ce08
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:00:55 +0000

    nearly before set we guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
