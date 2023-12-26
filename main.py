013692c74f3365c7dc78849e638dfc90f3deeb29
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:02:27 +0000

    burn fresh extremely thought pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
