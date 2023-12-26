bd01c1176710ad025c8b11a2b5317fb5a1dcb2d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:24:17 +0000

    shape official responsibility interrupt tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
