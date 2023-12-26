4f21c727186e970ecb94862e2c65c5222b353391
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:14:35 +0000

    search complain embarrassed rank hers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
