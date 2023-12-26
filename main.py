c14f210dcb66f437a40bfff4eb25f28873997413
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:53:34 +0000

    schedule breakout possible tread habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def convert_to_hex(n):
+        return hex(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
