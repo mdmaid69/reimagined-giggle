7c9c8a38d04b0434bb2f8b7fb5b610ee96712d78
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:06:35 +0000

    communication division brave arrive hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
