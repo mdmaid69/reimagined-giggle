9a723bac34a07b869eeb3c9ad0e0fb94b2626e6c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:34:22 +0000

    expect charge class concentration its

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Characters:", len(text))
