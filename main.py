d8c06ab1885a306a98e279061c97f3b3a5136f3c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:15:45 +0000

    please wrong university uncomfortable lose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
