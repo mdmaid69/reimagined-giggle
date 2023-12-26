6938ffd49ec67914522b9f96f4a1fd1562ca6b40
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:37:03 +0000

    beginning wide wise load classroom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def sort_list(lst):
+        return sorted(lst)
+def add_numbers(x, y):
+        return x + y
