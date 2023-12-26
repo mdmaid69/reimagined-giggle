90efe97f0395edc08616a451341ea0da517b7c76
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:44:17 +0000

    challenge experience division concentration spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
