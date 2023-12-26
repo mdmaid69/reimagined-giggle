7bd5a91cfac2da6b420ad22414f7657c8a38aaae
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:49:19 +0000

    formal love impressive poor friend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
