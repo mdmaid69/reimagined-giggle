79a0351cc9b2e88de57046f156d04cf11db14bd0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:28:10 +0000

    boring surprised however weekend blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
