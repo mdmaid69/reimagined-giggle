30464f5a5cdc8e00acafb645ab5f666df9ca6eea
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:28:31 +0000

    dress neat before extremely Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
