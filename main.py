da7e633459d8419a1ec63a53ef971b2a43c7bb45
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:45:51 +0000

    tight pause almost back may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
