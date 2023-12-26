1f309ddd2be61961529bcfdc6f64a94cd8071e6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:48:17 +0000

    movement preparation skill service mainly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_list(array):
+        return list(array)
