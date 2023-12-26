7b3e7e567dfa730cab12fbe954ce92332d162c28
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:41:49 +0000

    space shut deal obtain flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
