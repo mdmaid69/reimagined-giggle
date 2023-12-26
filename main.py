30f48f71c4278acc4de3eb468dd2a9c0cb217ee7
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:41:01 +0000

    manner conversation claim secret summer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def subtract_numbers(x, y):
+        return x - y
