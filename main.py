a07064e92d52b858b214cc3c092888b76cf4c999
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:45:14 +0000

    keep hospital cross thirsty stare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(lst):
+        return max(lst)
