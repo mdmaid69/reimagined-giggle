d71e8843f42f9080867b49cbdc052f85b56da768
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:35:59 +0000

    565end method aim judge shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
