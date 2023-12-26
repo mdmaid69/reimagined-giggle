ae6df5e3090dd553e611d2a5c36fc6415ee40f01
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:25:26 +0000

    respect possibility computer squash receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
