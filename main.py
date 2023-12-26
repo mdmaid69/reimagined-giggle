438f7a521c7bb77751e4ff52a4e698c09a75c666
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:47:17 +0000

    chief habit below argue crash

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
