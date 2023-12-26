4e02a717fc2106dbd76689ef4155fbd05708ee62
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:16:58 +0000

    target pass dress actually row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sys
+  def get_python_version():
+        return sys.version
