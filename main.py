f544f734a9d3ababf4ce3b8e01a25019c0fec628
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:57:11 +0000

    read completely would second scene

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-print(sum(range(10)))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
