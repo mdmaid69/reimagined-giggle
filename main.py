be1c09a9a221c7f72b23c2e9d64e8a3d18876acd
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:28:16 +0000

    exciting pronunciation force deal difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_speed(distance, time):
+        return distance / time
