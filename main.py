96327bf4a62517016e924652fdc09e1d6270f839
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:07:39 +0000

    scene become journey sleep suck

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def convert_to_binary(n):
+        return bin(n)
