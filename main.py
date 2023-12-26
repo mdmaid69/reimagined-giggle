4779bc5e5ee91be08d638b9fdf9123fd53f90893
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:08:52 +0000

    hard flavour risk welcome away

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
