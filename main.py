b87b22350d6604af9e008d11fc408a16548ed052
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:17:41 +0000

    strong call try can hers

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
