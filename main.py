5843cb9200081e8f124a8228274ad1b61d5fc722
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:19:50 +0000

    everything trouble active flat spare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
