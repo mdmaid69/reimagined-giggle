25b12a262033912a130b6add89a397d8c55db4cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:18:27 +0000

    authority bear calmly herself otherwise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
