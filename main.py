b6efaa9d2901e7bd9ab5b0433032f91a41ce3187
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:52:13 +0000

    exaggerate flow stiff bit show

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
