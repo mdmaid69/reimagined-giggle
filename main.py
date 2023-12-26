6a76954a59d3155eef85f371edef0ab7ca3f1182
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:06:54 +0000

    television enthusiastic interview comfortable clean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+for i in range(5):
+        print(i)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
