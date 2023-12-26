2bccbf3b541791628d617f3deed39605bbc31717
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:48:50 +0000

    cheat hold Gg apply discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
