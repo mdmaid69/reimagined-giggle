81d0bc0740d8506fe3ac327f4cdcd0f4e17f9e16
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:34:49 +0000

    home switch makeinto 569enjoy general

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
+def is_odd(n):
+        return n % 2 != 0
