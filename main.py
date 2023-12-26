809f7a0a5f789ada5b0ed22514d6c406bb51c3e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:17:37 +0000

    need quick dinner over period

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
