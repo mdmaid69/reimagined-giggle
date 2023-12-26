954bd1fb7a27417c725ae78ca42eed0d6e488961
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:51:35 +0000

    upstairs live plain generous carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
