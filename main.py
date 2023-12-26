d33a6772fd2703f36c4ccc1ed1dd6c7a18239912
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:53:00 +0000

    reply concerning exist nigga bad

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
