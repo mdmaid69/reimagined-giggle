94c7250a326e67a15966489e5b6e46dfaf97da1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:19:05 +0000

    hardly season exist connected relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-print(sys.version)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
