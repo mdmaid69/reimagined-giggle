e0053e9de67d52818b8ab89f0d27ad7fde07c499
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:43:29 +0000

    treat dance far shade worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
