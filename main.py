d6748b1e43356dbce29c566518f6c6c60d166eea
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:05:44 +0000

    divide match pot manner understand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_area_rectangle(l, w):
-        return l * w
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
