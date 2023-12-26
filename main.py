ce5926e5d708ebefb3211c26f52a4973fbf1fa8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:20:27 +0000

    action imagination recent content temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
