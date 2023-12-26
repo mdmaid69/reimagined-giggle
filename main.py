24fb5b4314a11df2cc272cf78d72270918c6acc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:41:55 +0000

    actual pain story odd fortunate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
