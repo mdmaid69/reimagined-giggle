23fd30ed91d3cb5fced00067ebbae79d1b0cd685
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:28:34 +0000

    flat model thing back condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
