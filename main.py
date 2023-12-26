13a4e3f6f769c6fae857a373f873fbadf9eee416
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:14:34 +0000

    have wish bunch skirt stranger

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import time
+def get_time_since_epoch():
+        return time.time()
