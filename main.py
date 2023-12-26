94ed1989fc683aaf3bb903ff8c030bc1f9fb8824
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:22:29 +0000

    dig principle weekend medium rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def is_even(n):
-        return n % 2 == 0
+import sys
+def exit_program():
+        sys.exit()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
