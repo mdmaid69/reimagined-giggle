2b9ca4df8da5a7f6d80bcfa0a25e819e18f36789
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:12:00 +0000

    error perhaps left odd tragedy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
