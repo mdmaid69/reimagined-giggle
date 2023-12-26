11ff5e0afc028ec49447f139d7421f5279a6867d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:54:10 +0000

    passenger waste square service just

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
