829b58c47eead50b38fc7ac006b025e8687261c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:05:39 +0000

    any contents national negative especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
