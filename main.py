fc506cd5e478a29bd912e002c02e41fe6dcffcad
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:27:08 +0000

    peace turnup develop meanwhile criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
