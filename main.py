be2b06c24bbb32045b493405d805179721611b6b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:32:46 +0000

    tall fork once impressed period

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
