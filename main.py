5663ca43cccb31bc58ca14ae9515235ee3fb8e59
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:14:17 +0000

    window married smile fun loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 numbers = [1, 2, 3, 4, 5]
 print("Max:", max(numbers))
