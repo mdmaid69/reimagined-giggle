61f05f5ffbadfa1cada05f08ea5fb9e57db6a815
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:12:00 +0000

    dull off final future still

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
