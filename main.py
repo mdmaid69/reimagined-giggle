e0dbf568ea533e36b1c6598133342b93b76a00e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:37:59 +0000

    design turn Bb OKorokay breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
