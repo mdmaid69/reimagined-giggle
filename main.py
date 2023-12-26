69aa563dc7bb75b51f505413d0a59611a64e1682
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:38:44 +0000

    load influence skirt at bread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
