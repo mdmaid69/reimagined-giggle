59f0797f0595197d6919185ce5a1397386923896
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:59:50 +0000

    ball object down kick greeting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
