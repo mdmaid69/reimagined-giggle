e90d5e34d41a42da59d28d1fb7ea456104dd3413
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:55:24 +0000

    allow holdout sensible future Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
