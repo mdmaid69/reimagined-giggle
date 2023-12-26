70abefff01dbd17747ba96fe0bb23e94ffbbe11c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:17:15 +0000

    transport sea cover well emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
