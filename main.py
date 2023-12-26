f384b2c39e604681770e4dd5451761126de9ea49
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:29:40 +0000

    century usual straight unfortunate risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def is_odd(n):
+        return n % 2 != 0
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
