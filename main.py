a4052ac0b3fd98a689e2197a6aec54b0b9234f81
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:37:59 +0000

    physical let Ll part hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_speed(distance, time):
+        return distance / time
