2e686e40929f427b0de53f47c15b4d50231a6e73
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:44:21 +0000

    see insist sweet snow use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
