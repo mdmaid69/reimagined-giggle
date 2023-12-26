30fbf94f8e8f34792104e0adbb63aa95c9c2b2f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:39:57 +0000

    past somebody wet light song

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
