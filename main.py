6646b38832f4868bd30e2a83e4efbe650f5eb936
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:07:11 +0000

    scheme weigh matter punishment compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
