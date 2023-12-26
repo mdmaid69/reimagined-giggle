9463e0b619fefcfe5c5c925a9bd71934572dcdd2
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:12:05 +0000

    tap wealth reason body both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
