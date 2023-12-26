21004518c4e4b4f701fe72c37ac7dba4901733f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:15:06 +0000

    ought chat attention match holdon

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
