4ccd2856107ba6d562893fecb832eed34933c647
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:40:05 +0000

    protest defend calmly exhibition work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_pressure(force, area):
+        return force / area
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
