d8bef0e9be694542cf1c4975de670f473ba727c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:50:13 +0000

    guess near unlikely increase weigh

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
