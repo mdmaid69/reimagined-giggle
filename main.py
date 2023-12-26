17b4031ff4f562adc367bce40305fa885fff4444
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:42:55 +0000

    see clearly keepaway holdon tired

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
