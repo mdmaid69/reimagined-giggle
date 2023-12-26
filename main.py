8490c2e9884ae8e193d4ae32eb99d29269776e0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:01:48 +0000

    become 565end sign climb benefit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def find_unique_words(sentence):
+        return set(sentence.split())
