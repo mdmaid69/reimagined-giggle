7e9cef35dc78b2ba83b0d89f91a04b4a0e8243ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:41:09 +0000

    sort brush mean raw can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
