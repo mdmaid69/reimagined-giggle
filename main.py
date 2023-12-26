c637b1cfe4e9e5aaee0b4d4ace9596c3957c7620
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:14:56 +0000

    force goodbye plan trap foot

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
