cccdac5241dc3ec4bdd245e2d8c48e4512e9768d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:29:02 +0000

    brother complaint answer enthusiastic exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_user_list():
+        return collections.UserList()
