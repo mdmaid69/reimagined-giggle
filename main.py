8490fe6fd03daba29a2730416ac124cc52bfc99c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:21:55 +0000

    further callout lean strength mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
