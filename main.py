021b5e3dff0b3410a45efebd1691d248a3e56a30
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:07:03 +0000

    account tax definite goodbye grateful

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_str(array):
+        return str(array)
