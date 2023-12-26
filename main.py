fa141ce7ba71aeb2aa023f7f2f391d9c503b6604
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:14:53 +0000

    partner before sister delicate centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
