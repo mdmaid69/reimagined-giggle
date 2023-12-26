c0bb9f19848ca1293aace82c7f2863b250772678
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:23:52 +0000

    live mention measure moon promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
