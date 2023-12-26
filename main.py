906e88134d5b474d8f3ae378b70adb20d9152dff
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:13:21 +0000

    defend exciting after believe position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_array_item(array, i):
+        return array[i]
