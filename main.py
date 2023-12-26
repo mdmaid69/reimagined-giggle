aee5f8b49bcf2060b17753cadb1c832fb8fc75fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:49:58 +0000

    basic good keepup careless convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
