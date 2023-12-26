f254dea27ad5d414d99e44575eb84130864bf6ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:46:31 +0000

    suffer whoever prize exercise frequent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
