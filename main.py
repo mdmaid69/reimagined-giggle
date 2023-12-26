c9185bd6b976321ad1a33786c481bdb1b24eaf0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:10:49 +0000

    mistake stop principle grateful answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  def subtract_numbers(x, y):
+        return x - y
