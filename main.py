e2604f3e346e50244aa431a9957c4c71d3f1246c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:18:31 +0000

    awake set somebody behaviour thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
