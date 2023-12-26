45f20bbc3d709ae30bca36d568fbc1ea7ed399df
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:17:19 +0000

    rely divide choose reach now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
