6f0b5e241d0ccc506087a070dd8476f960771942
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:37:16 +0000

    lend sorry good cause downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
