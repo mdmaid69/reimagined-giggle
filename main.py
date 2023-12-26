a338ce3c495dd9f00161d1250ce24df9d9233399
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:11:21 +0000

    fasten promise takeout room round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
 import array
-def get_array_item(array, i):
-        return array[i]
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
