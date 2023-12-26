a382a216dd8a07c22af30f45b58364ef6e560900
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:38:02 +0000

    thorough prepare guess thin thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
