d10d387c83920b150370ea082134287c89550ca0
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:03:19 +0000

    failure science level sign wing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def sort_list(lst):
+        return sorted(lst)
