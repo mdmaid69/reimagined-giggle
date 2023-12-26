a63748ffe9f6fe550dc2ee01b450758442e581f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:58 +0000

    suggest life them return spray

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
