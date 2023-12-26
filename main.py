d001cdb7b314631c40351d4a7342a5355219eb87
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:37:07 +0000

    pretty breakthrough we greeting row

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
