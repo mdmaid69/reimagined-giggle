5471ed50deb5bb1402fc2bf8ba53807fb8825101
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:25:35 +0000

    answer wing dead them cool

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
