6588adf2bb4d32082ffde724745cd362531ccbe4
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:21:53 +0000

    unfair drink strength there cruelty

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
