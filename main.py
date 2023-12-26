dd5803e720dce8ac55d870a1ba5ebe9834850e4d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:06:54 +0000

    fact welcome dick proper soak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
