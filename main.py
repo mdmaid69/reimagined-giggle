c9e9d6b949acea3f1c801a45cc193ed4872bfaed
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:16:34 +0000

    tread whenever squash anxious flow

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
