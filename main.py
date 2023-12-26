9c4f498beabf4992832621c2e12c5161d483f9be
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:37:40 +0000

    nurse circumstance ready turnup adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_list_from_array(array):
+        return array.tolist()
