5f97b60a90cbb178963c8eb1c83d45b6a5093c8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:04:51 +0000

    sail serious keepfrom tip debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
