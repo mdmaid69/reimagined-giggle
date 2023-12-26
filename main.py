bba711592d3e52a98d62c58a8f58dc2e88e6cede
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:35:52 +0000

    distance reduction from arrange swing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_arc_tangent(x):
+        return math.atan(x)
