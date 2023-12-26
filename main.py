befdf517d3bd476070845acd416c5cc4d1dae8fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:28:39 +0000

    judge complete form strange plain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
