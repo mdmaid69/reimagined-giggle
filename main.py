55acfa53db76b2aba85dd8dcebbf8a0db7e2da31
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:57:26 +0000

    outside nearly throughout odd future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def extend_array(array, iterable):
+        array.extend(iterable)
