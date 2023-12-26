28459aef11f2707a2cd481b3419968c19aa638af
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:16:46 +0000

    understand choice during takeaway rush

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import time
+def get_time_since_epoch():
+        return time.time()
