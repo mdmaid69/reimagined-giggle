334884db28a11ecebed03b7ce02813102837555c
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:49:02 +0000

    accidental right workon hard before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pressure(force, area):
+        return force / area
