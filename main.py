a6bcdfebbf0e4102b3ac4a67416fae49a83f8b55
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:27:35 +0000

    connect luck cum exactly they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
