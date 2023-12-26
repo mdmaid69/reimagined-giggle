4e9ce9fdb8f8f218ebb2bc6a0e0606b9a61979a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:02:24 +0000

    medium commit priority charge loose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
