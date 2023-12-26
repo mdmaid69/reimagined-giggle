89a22ba133a731b87d05929ffb030fea90120cd1
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:43 +0000

    makeinto mess bend pattern complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
