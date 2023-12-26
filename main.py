6e391aca35bc83ff32df4ef9f99a2319477f8050
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:06:21 +0000

    obvious may everyone knowledge print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
