a2c4820be05244bf8adcdca4f32cf1d44fda5022
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:48:18 +0000

    twist suckme position tour exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+for i in range(10): print(i)
+def find_max(numbers):
+        return max(numbers)
