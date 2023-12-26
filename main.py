a372087ceded678b6649c8abad4f5ee18a89d21a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:10:23 +0000

    year enthusiastic special pride except

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
