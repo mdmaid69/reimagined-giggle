7ec3afdf9d43a7ff25847ce59f47f84c6a02ff7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:51:50 +0000

    loose belong stamp unless letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_ceiling(x):
+        return math.ceil(x)
