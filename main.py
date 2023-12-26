1e42ec8cec415be1f7d3503d864c71ab25784bf2
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:33:18 +0000

    history appear clean wrap responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
