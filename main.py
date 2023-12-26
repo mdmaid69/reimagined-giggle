4e1442fca5171d175c6f3004807c6efabdfe350d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:08:07 +0000

    come opposite delight influence greeting

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
