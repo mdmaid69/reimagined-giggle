8aeb31405adbc3a0169ff5741ac09e1a4dadfec0
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:37:50 +0000

    anybody set copy partner load

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import os
+print(os.getcwd())
