705ecbf0422a1d9a2001451a5c7c2a78af4c096d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:14:01 +0000

    next callout understand film narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
