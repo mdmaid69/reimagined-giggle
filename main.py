eef56b1d4d5b627bd622016502ba45e3597e726b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:18:43 +0000

    since him part thanks comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
