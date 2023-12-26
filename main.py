fa6bc1a3f25a3a2170129197233010df228c4b60
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:05:54 +0000

    ground news crash ilovedicks strange

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import re
+print(re.match("h.*o", "hello world"))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
