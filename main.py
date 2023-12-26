5ddafd73feb26e651ae9e840c7f96101014a1697
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:12:43 +0000

    fun wire wing basically involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
