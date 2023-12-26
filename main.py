157abd9cb20f4bd9d6dfd0c817df3c94535fc810
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:06:05 +0000

    generous on pour occasion branch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
