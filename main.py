c01fffe222e91100f033c2729cf731447d6a345f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:56:28 +0000

    likely dinner tread tip race

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
