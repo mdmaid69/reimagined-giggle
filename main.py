42444d6ba7b0f309546ca99e919637741faac41b
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:26:57 +0000

    powerful waste round limit polite

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
