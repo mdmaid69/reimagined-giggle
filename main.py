b35eafab409c4b2fb52e0e10049a50dc6e4fa3dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:09:54 +0000

    similar split progress height commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_time_since_epoch():
+        return time.time()
