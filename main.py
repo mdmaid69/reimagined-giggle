13170f84fe8b33a96e4f8b969bb8281165469402
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:21:13 +0000

    middle protect catch weekend mood

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
