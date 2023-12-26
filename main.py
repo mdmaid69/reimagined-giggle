4a3df9590808e422a7fec43cd498e00b85c9985e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:31:26 +0000

    object rule sad entry rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def multiply_numbers(x, y):
+        return x * y
