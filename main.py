39238ce2610a7d29c20e3644e417d80113a18614
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:06:07 +0000

    someone emergency stop follow bother

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
