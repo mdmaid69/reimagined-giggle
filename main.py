6b60da8503f5657376587263b5385e705b58c5d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:38:34 +0000

    sweep pair callat exaggerate guess

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import os
+print(os.getcwd())
