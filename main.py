bf28c35cc72b832e078d027808ae41bf08e7440d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:56:05 +0000

    settle agree stiff pool takeoff

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
