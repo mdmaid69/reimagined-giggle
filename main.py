dd233a4dc5c5c29999502ab37eed1b98172b1936
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:01 +0000

    attempt sticky benefit splash keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
