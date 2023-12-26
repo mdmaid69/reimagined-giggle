53d517e186e860dd14fd62a9a1a2062e7242967a
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:31:29 +0000

    consist task make broken permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
