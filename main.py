92f23da480464c9e76a976e8be6c01469b9892ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:04:52 +0000

    reduction challenge giveaway involve scared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
