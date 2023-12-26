93e61bce872962bea97c66e7c056caa0424bc6fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:12:48 +0000

    word hold small owe shade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import random
-def roll_die():
-        return random.randint(1, 6)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
