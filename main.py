4b147ca89f6d5bddbb29a768cb16df43e4b61342
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:58:38 +0000

    plan system ilovedicks lie development

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
