e90304dc6a7fddf012594168b41cb972bba36e3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:48:10 +0000

    arrange cover day connected corner

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
