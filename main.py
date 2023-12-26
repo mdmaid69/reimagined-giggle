9585e8e90d08c816d649f41fab3714112ff97e59
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:42:37 +0000

    telephone effort usually trip keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
