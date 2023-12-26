fb65c0f87d0f68594e1fa35379891a97878604f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:44:07 +0000

    rescue past concerning valuable prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
