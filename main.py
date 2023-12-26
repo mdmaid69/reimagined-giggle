0d21defc41f47344715ae546e1221dcd359320cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:37:20 +0000

    common might display tall rain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def add_numbers(x, y):
+        return x + y
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
