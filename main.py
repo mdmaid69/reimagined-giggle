ef36c4f6a1cfa4e7d59926bd55a1d96ac7b0e3e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:38:09 +0000

    him used nigga I Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
