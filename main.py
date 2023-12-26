716db91866f10f5beaa85bfddf12e72a085c9631
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:49:09 +0000

    aware estimate interruption feeling concerned

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
