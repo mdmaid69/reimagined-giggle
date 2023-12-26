71ef93c66db5a0adf8e9727ac0449e28fb732c8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:58:24 +0000

    tap hollow mark owe turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import sys
+  def get_python_version():
+        return sys.version
