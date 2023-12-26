a8e038c1981ff0d05707395d0fc2f0cabd6121d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:42:07 +0000

    doubt house without necessary system

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
