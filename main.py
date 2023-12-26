7da7eb65859bb498304b6e4fe34f28bb1ac5f014
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:44:45 +0000

    boring story suckme region office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+def calculate_power(work, time):
+        return work / time
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_item_count(array, item):
+        return array.count(item)
