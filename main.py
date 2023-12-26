fd0e04ded0a69cea792856f4dd9ef075cdea35f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:55:49 +0000

    pass sticky convinced in simple

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-print("Hello, world!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_int(array):
+        return int(array[0])
