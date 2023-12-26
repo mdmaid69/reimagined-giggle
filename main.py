e1182f5cd565b40da727376224d1024543e28584
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:41:26 +0000

    church name count visit land

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
