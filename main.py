2b37693b31844bb3f0f0c5892de32763fae05784
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:21:59 +0000

    clean heavy tongue she damage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def add_numbers(x, y):
+        return x + y
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
