3bd4f6e53d09326a9f56d53a3400b7668dbd7896
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:53:56 +0000

    among scale attempt wherever attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
