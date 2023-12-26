f095664d4af58fa48efcaa0d1c37277bff8a64b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:37:26 +0000

    cry close me section connected

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import collections
+def create_user_list():
+        return collections.UserList()
