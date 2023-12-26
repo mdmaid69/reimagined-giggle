694b650de55155d3f1d34f72eaf0fbff5077b75b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:43:13 +0000

    difference national display teach matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
