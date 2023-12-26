89470d6247ca42b6f5dc5444365606571eac0324
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:20:54 +0000

    official valuable everything offer war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
