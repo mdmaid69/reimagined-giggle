5b10309b8b87360967dc2ba5bbe888a2286db49f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:34:07 +0000

    vision accordingto dance happy direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
