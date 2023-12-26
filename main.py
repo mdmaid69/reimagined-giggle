20c25c4f72c03dbc0d407626964205e5b1191dfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:59:35 +0000

    approval regional most cut radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
