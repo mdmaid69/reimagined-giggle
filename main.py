34e378b649a1fe12d18723a88e4a6d7f9619f3a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:03:58 +0000

    dare temperature wheel probably occasional

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
