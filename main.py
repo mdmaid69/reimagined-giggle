5307e2389ee1e5d806a867503f194c88647fde36
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:36:45 +0000

    block warm natural knock claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
