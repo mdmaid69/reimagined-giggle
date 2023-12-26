64d8e9b6851c8948492dcbcd19354f90fd442f50
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:30:59 +0000

    usually experiment matter your bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
