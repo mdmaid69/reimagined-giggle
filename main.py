6592e71067c5fc64a69b062ec56ca9afe4edafdf
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:33:27 +0000

    recent fast still thanks away

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_as_set(array):
+        return set(array)
