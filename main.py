04edbc85c31fb6ee76c59e6a44bb7cd3fabd1663
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:26:17 +0000

    ordinary either expensive sing notice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def convert_to_octal(n):
+        return oct(n)
