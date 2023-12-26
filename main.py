028b61e8b19c2692d854a31973862017a77bd8ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:01:28 +0000

    tense comfort custom system rent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+for i in range(5):
+        print(i)
