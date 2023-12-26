758dfac98484ae3de67d5d6c604c3279d8293b96
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:25:31 +0000

    accident consult family shadow lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
