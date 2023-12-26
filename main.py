44fdf970ac3ed01a3554e7962510d54e5dc8c871
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:19 +0000

    wrong mistake away shock anybody

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
