0bc89af64843c4289128946594c55a28178e47c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:05:47 +0000

    when none sense after own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
