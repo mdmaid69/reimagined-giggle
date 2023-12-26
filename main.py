24372ccbe38e917846f595e1adfb4399b664bde5
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:45 +0000

    about breakthrough underneath typical difficult

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
