bea85e7e27a5130eea3fa7c5a960ddded9fceba9
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:43:52 +0000

    between back solve violence statement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import platform
+def get_python_version():
+        return platform.python_version()
