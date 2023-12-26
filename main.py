0d1ea0309f11aa897f2248124086f4994d72acd5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:50:59 +0000

    kick base science relationship continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
