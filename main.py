6264bab9da2b1e7aa55c27ad2ad2c93a4a5a2a05
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:43:15 +0000

    marry floor Ll edge lost

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
