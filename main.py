b9d40fd7a7d16f8edd5fe9cee02c2bc95b6b38e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:48:13 +0000

    view seem hurt impressed sometimes

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
