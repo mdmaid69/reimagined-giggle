479097ff84c429432d147f5b6c0b845a30258e44
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:05:26 +0000

    majority present woman talkoutof runinto

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
