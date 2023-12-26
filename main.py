525252827d90bf2d2b39656416757ecf6c606b3e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:34:53 +0000

    sometimes pick board word accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import time
+print(time.time())
