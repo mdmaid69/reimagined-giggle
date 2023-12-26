f1f59092ad98a0293067bca27ca34ad3b68bedf1
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:46:33 +0000

    claim except badly account official

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  def convert_to_binary(n):
+        return bin(n)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
