3c73b6c211d4422d800bb71a84d25f3bdcb3946b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:41:41 +0000

    once face protest think waste

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
