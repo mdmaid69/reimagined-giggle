e902974bf1c3d3d9e66732a2b5c9a1a92603465a
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:04:14 +0000

    tone quit house discover nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
