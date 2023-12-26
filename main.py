d6c14fc30d608bf6975b83c18ef7f7119ea48465
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:06:37 +0000

    over later weather study past

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
