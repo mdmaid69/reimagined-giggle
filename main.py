035ae761271599b8aa72e9e6306e4c0810e4b7ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:19:15 +0000

    tired wait switch at dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
