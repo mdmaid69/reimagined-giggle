3f66622e89caed8a49be66f8502bda976d5f107a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:00:56 +0000

    work cook anything out as

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def get_array_as_float(array):
+        return float(array[0])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
