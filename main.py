dd7a772592f0f89f34a57cd472ff47faa20421d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:08:21 +0000

    copy disappear try provide somebody

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def cube_number(x):
-        return x**3
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
