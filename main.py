419aa84032dd59ee087dbce778521e0dd75cf36b
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:57:43 +0000

    Ww believe pipe heavy Kk

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
