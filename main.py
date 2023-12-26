84671425c309434395ae20894d7f35bb16687b13
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:14:23 +0000

    gift curious afford trousers 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
