3df30923d5ca124bc4e5adcdc7fe2c4bb61c0f96
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:32:25 +0000

    giveback post probably flight describe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
