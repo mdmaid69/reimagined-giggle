b802d46594490ecaaa288d78f33accf71f106f9b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:43:23 +0000

    wipe follow claim front feel

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def add_numbers(x, y):
+        return x + y
