c7b538f8c6be64a7d847cea7c198f64b3aa4ce4d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:37:53 +0000

    plastic glass sort slice near

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_string_from_array(array):
+        return array.tobytes()
