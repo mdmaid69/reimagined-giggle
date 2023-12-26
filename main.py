cb64c2c5531e88f6494e6ef7e1cbb4fb5e9aeefb
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:07:33 +0000

    twist mood scrape side join

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_to_octal(n):
+        return oct(n)
