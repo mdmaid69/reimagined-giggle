efceef55b4443eddf86bb515ceaa1de35151aa40
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:57:29 +0000

    conversation butter recover desperate active

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import os
+print(os.getcwd())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
