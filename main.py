ae3a95340acc23497775fce1d2158e29d7457f51
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:40:23 +0000

    space wonder slide loose attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def convert_to_hex(n):
+        return hex(n)
