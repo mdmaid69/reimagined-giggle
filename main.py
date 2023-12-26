a8b51496b18a6b1f2b3cb738fa594f9da99fafd9
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:18:28 +0000

    every backwards equipment sky cross

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def find_min(numbers):
+        return min(numbers)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
