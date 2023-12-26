8cab4c3c48c84b584adbf7ea152c2c8dce94f4f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:22:09 +0000

    wonder aim town study underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import platform
 def get_os_info():
         return platform.uname()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
