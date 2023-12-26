6fd52ff23fb99477de3ce06c1dc9b2ab39182823
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:54:32 +0000

    fill favour release tune boat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
