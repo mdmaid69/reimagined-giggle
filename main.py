166de0edd8c53035ab506aa6d3980afc250b71c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:30:05 +0000

    listenfor nose term official rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
