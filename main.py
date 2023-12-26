2a377fd1f6a6f79971d8d1c4df90b159a617374a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:09:40 +0000

    material fair think turnback shopping

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
