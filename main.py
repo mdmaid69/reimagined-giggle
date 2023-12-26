797b76ee80fc5d2d49ff8ee8c8f395f6b04cf76c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:14:35 +0000

    intend Mm turnout convenient listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def multiply_numbers(x, y):
+        return x * y
