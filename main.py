63faf5be98884cde0933a8dfaa6fb9b356f9a51b
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:00:13 +0000

    nervous offend never squeeze habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
