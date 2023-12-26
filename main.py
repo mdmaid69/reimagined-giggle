0ca2eb1f4c1d24ab6415760c3b2e947d616b022e
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:42:08 +0000

    hospital press warning together peace

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
