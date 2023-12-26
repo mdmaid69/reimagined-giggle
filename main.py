ee40860fa683e8d55d83c92737ac1a8047a3a337
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:47:18 +0000

    final soak trip possession sort

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
