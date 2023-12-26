2ee4f3c056929a1e4c32e8ce3d99990e5783261a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:57:48 +0000

    when form wide work along

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def is_odd(n):
+        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
