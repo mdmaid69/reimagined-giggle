47af3f4deb76f0fba315e398de8a00f3811410cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:08:20 +0000

    centimetre solve bang cry point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+print(math.pi)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
