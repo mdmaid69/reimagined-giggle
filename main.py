85b9816f76746849c0bdd4f3e86024a4358b7aee
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:53:58 +0000

    damage suggest place fat dare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
