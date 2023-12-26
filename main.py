84f2394dce9adab00cec42fe9196640725fa2d0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:46:59 +0000

    measure turnover spite maybe include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
