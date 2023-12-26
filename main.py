c98e455fa90d8a69ec4d158a9d417db26e81b784
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:33:18 +0000

    worse welcome chief Itself something

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
