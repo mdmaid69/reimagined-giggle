ed7ea1e346028566e0946da9997bb546b7e0e47f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:40:58 +0000

    list deliver hold 179bite need

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
