23424ff31e2d024a4e14a69125cb36360ec9f81c
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:10:05 +0000

    body chin emergency about person

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_string_from_array(array):
+        return array.tobytes()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
