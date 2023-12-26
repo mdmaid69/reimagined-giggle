f14b8fc7a5373e1a01dca4daf3fc38cf7758318e
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:00:40 +0000

    appear modern convenient tie kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def remove_from_array(array, item):
+        array.remove(item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
