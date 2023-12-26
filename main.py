5aaee24bf5d7143aa0400d992f62804be7938699
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:57:37 +0000

    guide after lookup before check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def find_max(numbers):
+        return max(numbers)
