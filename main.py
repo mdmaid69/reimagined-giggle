865471d0ce8ef0f1b5ab1e8f736b09a5a7ace49d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:19:05 +0000

    interrupt discussion behind satisfied present

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
