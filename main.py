ae6c2e54fd391137c5c5cc2b4381c0f71073a54a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:48:45 +0000

    grand flood fault risk whisper

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_odd(n):
+        return n % 2 != 0
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
