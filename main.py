0f408d6b43f5a8bc9bca405ca13d3389e991a3b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:35:23 +0000

    shop discuss colour poor action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
