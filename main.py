a4c58f68782465cea0a4433c04871a0a2b70ca99
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:50:27 +0000

    similar sensible excellent stand slight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def find_max(lst):
+        return max(lst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
