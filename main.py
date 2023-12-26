d4d0dc2883058f8cff0c5ebea6a00e8613493fb4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:25:27 +0000

    by back doctor afternoon hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
