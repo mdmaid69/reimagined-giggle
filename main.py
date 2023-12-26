d6f66aea58e39f5aa57e0d29973099e4d7c630f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:16:02 +0000

    set sound path make hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+from collections import Counter
+print(Counter("hello world"))
