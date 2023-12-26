f1092bed5418d84c6e210a8d19ae7508e5e9da6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:38:40 +0000

    meaning especially anxious rank theirs

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
