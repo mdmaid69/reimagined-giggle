9a4649472bb51e47dbf23bbc7327af5d43a0aaa3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:13:34 +0000

    Nn bite trial again treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
