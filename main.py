c778dbb2440597681d1ba4cba825fa9356cdf155
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:53:15 +0000

    butter apart too variety major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
