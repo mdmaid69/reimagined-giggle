16af6267c4c74eb4b4bbf9ccb0f8d961af1a5e9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:26:22 +0000

    open fairly threaten check accommodation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_as_str(array):
-        return str(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
