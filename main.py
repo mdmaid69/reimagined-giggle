8be0727b9dccf9438781aa648b703ab57e220e8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:43:37 +0000

    breathe amount major like mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
