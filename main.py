c8ef03f0e7ea312811e7351d184e09dd3e1d245a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:29:03 +0000

    snow suckme possession rain pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
