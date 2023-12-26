fd7283ff0ac4ceec3d121abbacf02eb4af719431
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:20:21 +0000

    experiment herself ignore somewhere present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def remove_from_array(array, item):
+        array.remove(item)
