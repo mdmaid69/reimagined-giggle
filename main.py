46f3ef57d2a48e1fdd9315be36c85f887bf3edaf
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:13:31 +0000

    stop protest mass opposite justice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
