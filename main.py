9eadf4f012c5e6ac729afa8ac2ce7cba506184d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:57:41 +0000

    getaway majority base part early

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
