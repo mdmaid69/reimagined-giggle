922d561268bb33347e6d0d23dd43ccd65773b9f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:50:53 +0000

    deliberate plan flat prove day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_max(numbers):
-        return max(numbers)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
