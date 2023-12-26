00d10f9e8a982e1bfc76b87b1a83878e020856c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:35:34 +0000

    switch term use thumb flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
