aea7c635e3560227b81ecb8867bebf198d77fff6
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:26:24 +0000

    complete flood payment decide earn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_binary(n):
-        return bin(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
