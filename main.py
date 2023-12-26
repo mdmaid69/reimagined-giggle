0e1f3342a4e48aae87527e13cf176bda842d9f81
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:42:52 +0000

    quiet last us fly busy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_power(work, time):
-        return work / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tensorflow as tf
+print(tf.__version__)
