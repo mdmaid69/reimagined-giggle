7394c7c8ad25cc25c74d91adb18e68816bf914ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:07:01 +0000

    extraordinary port shall refer entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-print(time.time())
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import collections
+def create_user_dict():
+        return collections.UserDict()
