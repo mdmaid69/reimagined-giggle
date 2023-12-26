bbb09d2b0cdbd3075f28611d4bc49d67bb03aaa9
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:06:10 +0000

    priority unable most Oo coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
