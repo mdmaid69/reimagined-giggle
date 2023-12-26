5eedb99259bd631a44a6afa177a53dd4332858b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:56:12 +0000

    loan luck passenger if generous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
