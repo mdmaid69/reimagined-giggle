b8b98604bacdc4ed8ba3343b46b8858af6686cb5
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:36:55 +0000

    swim deliberately high circumstance today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
