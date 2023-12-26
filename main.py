6f7bac167596e027f1d1851e8df4a5e2f8143ad3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:52:26 +0000

    tune address suppose previous money

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
