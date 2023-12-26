584a3f3d2f75e99098833aa56471204f94b3309e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:59:46 +0000

    comfortable description opposite month translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_floor(x):
+        return math.floor(x)
