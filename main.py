0c7b616237e7a7c0f1df97ebbe59850d7153b95e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:39:09 +0000

    hope become new switch record

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
