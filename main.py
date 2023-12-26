5f2d8478d78bac908498d2e93005ad90aced7fbd
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:51:01 +0000

    hold though centre count foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
