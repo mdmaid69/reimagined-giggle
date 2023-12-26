2b8df5927a82265821a554a88692e64cb9fe71dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:02:15 +0000

    makeinto lend workout probably later

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
