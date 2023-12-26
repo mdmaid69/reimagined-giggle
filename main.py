2affd5c3c7b5716247c1aaa97661a1ef7dc0fed0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:35:30 +0000

    pleasant present beside discussion whoever

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-print(sum(range(10)))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
