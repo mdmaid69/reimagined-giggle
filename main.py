a774949b9a4cc8fddade531b4858b2a34479ca40
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:09:13 +0000

    lost spray picture image judge

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import sys
-print(sys.version)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
