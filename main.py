c6d40c94be40cc8c843bb70f7b9ed1aa4e2cceba
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:43:58 +0000

    lose comeup touch than dry

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-print(sys.version)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
