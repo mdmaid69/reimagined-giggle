6428fb248f287853bd6cd50f5bdbfe52760b05e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:08:40 +0000

    leave criticism represent explanation dress

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
