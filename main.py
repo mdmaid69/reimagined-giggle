a2f24de3f6c3e00a842393bcd479405a06eac60f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:33:33 +0000

    bringforward effort switch prove charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
