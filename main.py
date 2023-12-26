6eb78f769d0b937812920d2570930d5e0ca4817b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:12:50 +0000

    on call direct stop fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
