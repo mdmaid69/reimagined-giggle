4e8192088ed81a5616b0806e0590863c7e61d311
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:24:40 +0000

    fit choice hope zero adjective

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_int(array):
+        return int(array[0])
