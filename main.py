a5a510a6071a429dad48784395bcca4138559135
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:56:55 +0000

    himself enthusiastic morning more sort

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
