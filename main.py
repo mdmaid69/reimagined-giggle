1931cf5bc591803be408f96459510024d46b64ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:25:33 +0000

    behind such travel angry interesting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
