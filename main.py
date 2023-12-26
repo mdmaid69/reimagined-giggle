7d8d8a3add7353fec3774abd787f7b6d4a7594f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:43:27 +0000

    if instant quit admire attention

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def find_min(lst):
-        return min(lst)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
