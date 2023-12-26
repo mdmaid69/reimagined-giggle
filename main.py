b5eb082127daeb9d7e3c1a441001d77ea1ec55f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:10:16 +0000

    difficult thankyou drive approval away

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def find_max(numbers):
+        return max(numbers)
