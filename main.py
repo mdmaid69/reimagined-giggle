44ae1d6d3721441e5160dc8bd642525f904ec9a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:54:13 +0000

    contact question complete tie word

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
