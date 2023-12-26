2a3de0ad23448a1ed1e1f77c43a63f58b5ba1b17
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:10:30 +0000

    by post fine piece send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
