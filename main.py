7d836982141f19c8b401013ba28d7e368e586cb5
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:50:41 +0000

    emphasis always needle least hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
