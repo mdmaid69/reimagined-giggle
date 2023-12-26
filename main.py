bce7551ca74cc1919cffbf353ef962df4edf570c
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:16:56 +0000

    money hear height entry town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
