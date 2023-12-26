5bce01e2c7bc1689459233f4915a7540e0ad5f55
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:38:19 +0000

    suit reduce separate march stamp

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
