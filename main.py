eedb6c9ed0107ca898d6314e83675efe8aa84db6
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:15:23 +0000

    contrast than unhappy amount insist

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
