8d8c3dee0dcf2b0cb805aef2040f473fd88de462
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:23:21 +0000

    unpleasant keep food sorry maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
