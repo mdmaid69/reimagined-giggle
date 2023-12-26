f635b5e88d66e23e0462d151fc791ea2074933aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:27:02 +0000

    mainly damp face offer smart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
