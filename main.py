64c96698a3ad1d6289483c587545ccfaf336c14f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:03:17 +0000

    rush pronounce back artificial swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
