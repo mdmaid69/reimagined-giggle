bdd6eeae0fd1f8ec369ad97ce802dbae29365c83
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:11:19 +0000

    ocean wash pour note talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
