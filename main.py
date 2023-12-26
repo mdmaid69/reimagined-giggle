4f38870e479d945b8dc2f7e66a02a06dfb8b3db3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:41:22 +0000

    neat Jj shopping feature compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
