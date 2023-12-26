240c0282300366b7937e714de68261dff31021ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:17:59 +0000

    final sail demonstrate 565end bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
