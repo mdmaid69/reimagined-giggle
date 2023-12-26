7f014684f11f8395615b9dae5a3de2719a0155f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:35:30 +0000

    shock fortunate touch the soil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
