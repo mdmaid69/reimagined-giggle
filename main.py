db5a4a40f63be800c57ff96ab65ff865addfe6ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:36:18 +0000

    their getback while thinkover convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-print(random.randint(0, 100))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
