ba2b526b7c520d22248312fc42bb6e037e9cc1b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:22:06 +0000

    so insist corner face burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
