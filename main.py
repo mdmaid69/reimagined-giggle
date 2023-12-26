edce17f949d5c540ff3d6e4856da8c1cf4177444
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:25:45 +0000

    judge interest happen address gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import time
-print(time.time())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
