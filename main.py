b470df77a61374a77131bc54fdc6395e01e2e45e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:32:42 +0000

    choose success lesson sleep instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
