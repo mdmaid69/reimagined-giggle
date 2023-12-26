8beffe643c8970d35531e8a8c6a6c7f9c1a48cba
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:57:44 +0000

    full finger wire careful modern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-print([x**2 for x in range(10)])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import re
+print(re.match("h.*o", "hello world"))
