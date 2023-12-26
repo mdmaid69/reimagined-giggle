b7f2889d49aab9b0e1a07c864c1a97881a5f4651
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:25:05 +0000

    telephone emotion pain callout mark

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
