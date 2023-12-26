265463b464d2466b8e57598d5caae0c2576e2fa7
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:44:36 +0000

    pool affect breakfast division confusing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
