c157889a3b3484691d40f60ba2b51f502605eea4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:20:50 +0000

    everybody badly floor congratulations suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
