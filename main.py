faf702b8bc2270102ffa822cf397f35701424314
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:52:20 +0000

    takeout afford breath ourselves impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_list_from_array(array):
+        return array.tolist()
