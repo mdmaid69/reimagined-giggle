49cac023d46206316ecd965e320f66406234b625
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:06:09 +0000

    whistle head convincing shopping gooff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_power(work, time):
+        return work / time
