3fabc96944f17e7fc437c8542a1e3d462552cf4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:02:20 +0000

    class goup treatment rob understand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
