1099b73a2d0197aa5b648a8761a3dc05abe285fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:45:51 +0000

    scheme with meaning second concern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
