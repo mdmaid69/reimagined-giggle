4b7e479a6a6d3397cfc480b45a77888b706a532d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:18:51 +0000

    play learn fight seriously shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
