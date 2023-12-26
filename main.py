154ad416ac04481615a743ed069f8e95d7915e3c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:01:38 +0000

    raw use check old giveaway

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def find_max(lst):
-        return max(lst)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
