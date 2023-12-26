9662d9fe6bfa3c10d67124b64bf9eadf19c4b135
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:09:32 +0000

    passage favour various left explanation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
