7be6477bb85ef95fad32a43f80b985f2d27470a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:08:42 +0000

    spread turnoff trial refer trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
