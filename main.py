16b7cc57505a161f151b9458dd03a0b1cea3dd89
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:36:05 +0000

    spin hurry keep dull stop

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
