0d1034357ed34df1f50cdcfe7496336ae7e15a51
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:33:09 +0000

    doubt slight slightly gas wind

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
