ff71a7724b71071f73c93b9cebdee8b8718ce88a
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:53:33 +0000

    also strike murder workon wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
