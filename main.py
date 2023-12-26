715f400d36006f479586dfe8646d4c66c23fb01f
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:21:42 +0000

    too excuse smart someone report

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+for i in range(10): print(i)
