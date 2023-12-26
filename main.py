db92bc500a3a99ef7ce0c728848649dcc1b37f95
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:06:21 +0000

    live 568energy most permission important

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+x = 10
+y = 20
+print("Sum:", x + y)
