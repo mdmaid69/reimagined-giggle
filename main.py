a5fe823be05d494301d660d02e8b66bc04191680
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:09:44 +0000

    home small quantity should trousers

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
