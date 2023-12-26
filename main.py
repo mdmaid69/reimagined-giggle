c9fb86071afafc26286e4ce2ea9b1c41cd9d09fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:42:31 +0000

    region medium unkind load different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
