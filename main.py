5297fbfb514d36a4937882065a155445b9f53fed
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:06:39 +0000

    finish squash next hospital bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+print([x**2 for x in range(10)])
