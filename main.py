1f9f6eb1983bc6390abfd2d3652e60dd91c33ff3
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:30:36 +0000

    plenty scare within talkback study

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_density(mass, volume):
+        return mass / volume
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
