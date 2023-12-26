7e7be9bad4c338d47626a9cc9dd4ca42009a7c6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:32:09 +0000

    bush set marry seem face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
