7d6fbaa4e84272b4c4414feaf3389163f5c01135
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:10:56 +0000

    regular frighten attitude wherever underneath

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import sys
+  def get_python_version():
+        return sys.version
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
