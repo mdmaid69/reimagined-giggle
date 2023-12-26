ce3ae282766a94b15997e8d9fea200d3a8cde5de
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:24:56 +0000

    mark treat reduction strike copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
