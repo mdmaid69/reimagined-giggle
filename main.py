81fc3663fc649693b94b28ab4752d90bda40fc5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:03:27 +0000

    need light ready conclusion importance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_stack():
+        return collections.deque()
