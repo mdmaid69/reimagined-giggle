566225335f1ddf5d0390039b551cbf9a97fceed0
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:02:29 +0000

    official confusing even interview splash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+print(time.time())
