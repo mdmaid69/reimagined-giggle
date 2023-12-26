35d872ca96bd13cf6d3fea08da3e2fab0304bfd0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:06:02 +0000

    part sky kind matter adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
