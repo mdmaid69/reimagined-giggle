435363d65f6fd429a4cdb19d646ab4fb9a3ce7f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:57:27 +0000

    father sick success live opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_typecode(array):
+        return array.typecode
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
