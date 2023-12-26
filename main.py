1a8f6cadbd12fbb3947220d1ad488d45ac91f4a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:08:40 +0000

    legal charge attitude cook lot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
