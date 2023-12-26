2055d94e12e1eede3c97850d03fda6526692da0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:12:40 +0000

    pass criticism comein deal you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
