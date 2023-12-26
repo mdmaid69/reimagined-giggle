ec8c9db4b590961c565b7c2297a97cfbaadefab5
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:29:12 +0000

    dish mistake suffer sweet division

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+from collections import Counter
+print(Counter("hello world"))
+def convert_to_hex(n):
+        return hex(n)
