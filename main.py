8b5e085d26b6d0f72e479b0d35fdf34e7f49775e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:15:22 +0000

    glad warn cinema pain spoil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
