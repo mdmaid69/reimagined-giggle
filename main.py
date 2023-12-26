4b295a87d6f098785b212e151195ec36aa4df5da
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:20:16 +0000

    tip mixture whichever form takeback

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import re
-print(re.match("h.*o", "hello world"))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def convert_to_octal(n):
+        return oct(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
