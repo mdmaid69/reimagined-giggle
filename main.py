09df7b3076fb528eb20e4a62934616ad503c5e05
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:42:40 +0000

    near down port apparently rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
