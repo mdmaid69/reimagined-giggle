adfe6aa2e533526ca3f6d180402e182da850caf2
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:35:55 +0000

    surprising prevent or tear its

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
