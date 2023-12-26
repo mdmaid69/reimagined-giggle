8a358055535bb59582836d91a9ed7a86e46489a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:41:31 +0000

    side trade exactly plastic exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
