90d4efd1e5c035b099f25f47ee68373da288614b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:59:25 +0000

    injury pupil closely rely file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-  def convert_to_hex(n):
-        return hex(n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
