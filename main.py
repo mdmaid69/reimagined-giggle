0c4fe4be4e0c6ad211f0a1367d18e448178a8a2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:18:09 +0000

    memory win come danger frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
