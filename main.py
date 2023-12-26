3910a39ba7efca75914a16b5c36cf4a88f10930f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:07:23 +0000

    quality freedom regard store care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
