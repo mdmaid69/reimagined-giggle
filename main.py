58d72d135023d5523ec44aaffc93dab38a5bcb8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:45:48 +0000

    yourself accidentally gather cheque community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
