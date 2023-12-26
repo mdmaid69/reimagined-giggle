d5cabb9847630beb9f4e9cfe098fcccbc2793206
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:02:57 +0000

    concentration search substance obtain fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def reverse_string(s):
+        return s[::-1]
