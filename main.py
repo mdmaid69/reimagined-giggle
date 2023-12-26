daea3db3d6ad74fb2c8637871018b43fa5a6c461
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:59:10 +0000

    warning any goback(to) broad to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
