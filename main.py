34fc0fb6029136428d708121582048a15b9d272e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:35:44 +0000

    various interruption little picture dark

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
