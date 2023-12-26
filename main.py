d6fc689db91b3758d880e415f33e1a3623b0eb16
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:33:58 +0000

    spill rate argue everyone talkoutof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def is_odd(n):
-        return n % 2 != 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_acceleration(speed, time):
+        return speed / time
