a11b7c2b899710c7eeb6d2382ecb61d45c1f62f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:05:04 +0000

    organize win exercise fast Bb

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
