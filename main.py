56730140701fcfe933035681c42a8e2a63cbef32
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:10:45 +0000

    joke where happy moment wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
