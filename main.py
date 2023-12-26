c500d924c34b2238d5764c35fbe3923e1b51bc51
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:30:15 +0000

    question one should scheme opponent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
