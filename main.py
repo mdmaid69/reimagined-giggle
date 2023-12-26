c85b2a2e02716c8dc6a93b0bd0596c40fc21f1b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:14:21 +0000

    suit correct unkind ready shelter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def cube_number(x):
-        return x**3
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
