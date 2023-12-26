798585aed9447cbab0f7ea5a169a09ab8f8466e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:22:03 +0000

    wild relax keepfrom calm constantly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_os_info():
+        return platform.uname()
