1b8fc22d81267fe30d3dd57190f82c8d8ac2bc91
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:41 +0000

    represent fence afraid muscle downstairs

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
