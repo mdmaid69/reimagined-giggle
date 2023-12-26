024a14072e4b770477db20ea8cd227b266463a0e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:03:37 +0000

    why wind interfere flat me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
