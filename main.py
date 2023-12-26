22aac9a6b2c6411c780f02e37fb0db68ae33b173
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:17:13 +0000

    giveup modern Nn complain build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bool(array):
+        return bool(array)
