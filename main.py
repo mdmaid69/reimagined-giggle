8e05f98245950a1edc90ed8ec2968f4dbdf2fd59
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:01:55 +0000

    quickly lend private attempt control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def find_max(numbers):
+        return max(numbers)
