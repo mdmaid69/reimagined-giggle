1f047f6120339a7011d4acbbe920646b61f63559
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:10:11 +0000

    murder cruelty survive middle cheque

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
