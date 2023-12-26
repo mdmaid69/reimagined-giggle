f410e8f8a37159e5ea108823a1c35f26e46ac840
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:59:30 +0000

    that generous circle cough light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import sys
-  def get_python_version():
-        return sys.version
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
