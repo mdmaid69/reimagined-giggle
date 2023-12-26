80ef856ff69985df9bd5d3af474b7fd2ad459ba7
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:34:47 +0000

    unit safety shout government skin

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("Hello, world!")
-print("Hello, world!")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
