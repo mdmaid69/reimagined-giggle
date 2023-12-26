5a2ad230b6a8f4b12df903cb4bb4ff6db7417023
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:45:49 +0000

    stranger power turnon advanced we

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
