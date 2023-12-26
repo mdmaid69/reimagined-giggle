29807cda222c5fb151825d1db1a5ea13de432b56
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:01:13 +0000

    extraordinary content measure mind relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
