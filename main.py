2b5be012005bd194f03e685d233010232478957f
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:41:46 +0000

    call tradition pay off soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
