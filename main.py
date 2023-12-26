04d5f7a9680ee878654b4154c1edd9da11f36433
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:51:14 +0000

    priority since freeze much burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
