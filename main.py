6393f4b0237a28e2422cedfec4d7226560027bbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:56:28 +0000

    begin squeeze range guide like

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def find_min(numbers):
+        return min(numbers)
