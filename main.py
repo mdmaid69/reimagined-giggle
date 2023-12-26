7a2d368535dc5c155ed547e5d8be0c2f2592a257
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:39:45 +0000

    legal pair persuade project popular

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
