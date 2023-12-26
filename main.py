ff55dd8119878302b22be191d234f2d1126906a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:01:35 +0000

    house shop hold false round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
