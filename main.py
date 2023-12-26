fe6a5447a39ee46eda8786afefab2dd94528af89
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:52:55 +0000

    somebody however turnover male yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
