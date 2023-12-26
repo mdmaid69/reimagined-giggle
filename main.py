9551635b179be79a7dc5e4a3dc1b9c69559b8a8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:47:49 +0000

    fly whenever night male post

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
