8851def5f8218950e7860f170a0eba573b2a9d0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:21:23 +0000

    thick mess talkinto accidental guess

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
