713973be2004124d9a33a02623eb77b725effc9b
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:58:46 +0000

    deliberate idea sink reliable impatient

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import numpy as np
+print(np.array([1, 2, 3]))
+import getpass
+def get_username():
+        return getpass.getuser()
