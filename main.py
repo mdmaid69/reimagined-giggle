2bd6c956bdf8418e4d61c8026c5217d267ab64ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:56:35 +0000

    hurry interested port process room

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import sys
-def print_python_version():
-        return sys.version
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
