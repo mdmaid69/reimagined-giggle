11d1b8c653138de76150a2140bd7a04ab3d912f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:20:13 +0000

    convenient slice by turnup row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
