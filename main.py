b110a00c89a45315a9d09a988a42bea4df59048c
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:26:28 +0000

    even art below lookafter runinto

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
