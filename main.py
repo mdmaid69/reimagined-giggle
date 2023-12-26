2aa13229961137a55b28863dba515f68946e5918
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:16:13 +0000

    goout beautiful pencil breakfast treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_odd(n):
-        return n % 2 != 0
+  def delete_file(file_name):
+        os.remove(file_name)
