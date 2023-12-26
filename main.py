baba5f70701b788b2fdb3e213044eaf4c5055e16
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:58:15 +0000

    protest familiar quality emotion stamp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(10): print(i)
