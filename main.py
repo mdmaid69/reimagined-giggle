ae15d2cdebf72e09e51e2f3f32057808802d04b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:32:12 +0000

    in mess provide passenger turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import datetime
+def get_today_date():
+        return datetime.date.today()
