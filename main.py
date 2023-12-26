6df7ea97bddcf0950dacbbb81653a721c249ac95
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:35:39 +0000

    mass Vv form why consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_square_root(x):
+        return math.sqrt(x)
