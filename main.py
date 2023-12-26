db844f6ae37774c4c72608a7804362d4dfc09f91
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:20:14 +0000

    mood calloff compete search surprising

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import os
+print(os.getcwd())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
