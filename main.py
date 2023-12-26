e9ac8cce61e32da1be497e7adaf7744335750020
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:00:38 +0000

    her contribute impossible inside violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
