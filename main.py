84964e0cb4aa5c5283314283deb6e8c943292773
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:18:06 +0000

    we combine offer mainly exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
