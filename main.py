29d8c711468a0252e1726c22e5740fe05e7c07bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:59:16 +0000

    name realize welcome improvement period

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
