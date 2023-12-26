9ee9867a8dbc19aa01381c51a91eab20dcd955e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:20:58 +0000

    impression petrol criticize building callup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
