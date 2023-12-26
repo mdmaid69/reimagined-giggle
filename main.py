a5e277e72371d0e88e5723fc1f65daf743750f48
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:55:25 +0000

    breakfast legal sink firm acceptable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
