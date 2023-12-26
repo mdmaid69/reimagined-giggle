8e61062df56f58e96a3d8272bc322959f582fd66
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:07:18 +0000

    instruction save question lend hit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def find_min(numbers):
-        return min(numbers)
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
