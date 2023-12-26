f96dc211043520e963a7a1fa0a5f7c6a08838629
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:47:28 +0000

    only raise curious spoil takeoff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def is_palindrome(s):
+        return s == s[::-1]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
