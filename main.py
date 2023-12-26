9f5c3a913405db15fb22bd10ae6f1f5e9f38b3a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:07:16 +0000

    trap mean piece insist chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_current_time():
-        return time.time()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def is_palindrome(s):
+        return s == s[::-1]
