8b49e6c5469d9ae8ef4c9e2e663e6bf718f41ec2
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:07:34 +0000

    fasten or move scheme seat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
