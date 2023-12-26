b4033ec1302b4d4f9c074770fe4f1cfcfd9bd0f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:24:01 +0000

    closely expect regional manner tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
