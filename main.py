2f24b545b0ec7bd96508f5ef9464307e8e26eb15
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:01:31 +0000

    paper unusual tour objection zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
