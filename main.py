8b7bb2340ecf2aeb2a80af1b9f763806d5f6116f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:59:47 +0000

    female promise spread comeback feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
