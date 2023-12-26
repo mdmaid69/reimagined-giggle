af5cb5449fe5ed01fca5641e1af7840a35909bee
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:17:57 +0000

    mainly beyond why minimum either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import logging
+def log_message(message):
+        logging.info(message)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
