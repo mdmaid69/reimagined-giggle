19a43b7e50aa1a4ba15b6e021fee9afcc5775272
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:36:22 +0000

    stop event boring wave thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
