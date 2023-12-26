abe960c8f0672393179da1353ba31cc573014a09
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:46:33 +0000

    share side wish prize store

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
