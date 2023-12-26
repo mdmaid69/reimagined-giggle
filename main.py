93814178cfaa37c5fd62dc15bd118d27527dec2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:59:34 +0000

    shut represent claim obvious debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
