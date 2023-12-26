48cc575bb4bcb236b010570bbe6336e8eb172f7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:22:53 +0000

    invitation kind mess key judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
