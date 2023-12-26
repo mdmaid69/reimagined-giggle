b1d8fbc80577fd7ccec7b9dbd2b25706b4d5f331
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:39:55 +0000

    raw understand language wise shrink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import math
+print(math.pi)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
