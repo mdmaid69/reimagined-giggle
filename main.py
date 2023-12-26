b831c0bca312d127f1c55a2211bd28b197f44f47
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:13:44 +0000

    door wind shrink suit change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
