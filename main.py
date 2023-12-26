c3ba01833b1645b33561f6cf6e0508e512355982
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:37:08 +0000

    post match noone amount reduction

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
