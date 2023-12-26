95b1f9ecddc5215404751a24dbe26dc4c8d2db33
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:52:42 +0000

    peace goback(to) later sleep confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
