e3ea0a6a349827fcdfa98dcfbf63ffb8b8541805
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:38:10 +0000

    seriously exchange favour reliable undo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
