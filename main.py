79016dd2ba0ab2d0d4429c7beb25dd7143b86178
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:37:09 +0000

    behaviour go convinced height activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
