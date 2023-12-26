b2747d2969c4db912afd8d432247f5524eda293e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:06:25 +0000

    regard spread disagree copy practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
