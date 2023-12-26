4336d45ae49531272c89900f1302113083294a32
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:06:15 +0000

    system expect match takeover pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
