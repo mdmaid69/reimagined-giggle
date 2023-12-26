49991072f49566383d3d1074693037c6327d1de2
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:07:46 +0000

    accidental cold wipe maximum relationship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
