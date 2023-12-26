760abb7778f49eefc2adfa056ea8ee9e21288416
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:35:49 +0000

    mail exchange left tell could

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  def multiply_numbers(x, y):
-        return x * y
+def find_max(lst):
+        return max(lst)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
