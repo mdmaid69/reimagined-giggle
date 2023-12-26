07d1baf7036b7769ee333688d71a7c06737aa7c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:56:28 +0000

    hurry young because rob call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
