c53f525185d6f8e0e9f6b2522827fe0c6273b67b
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:28:35 +0000

    full narrow idea well be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
