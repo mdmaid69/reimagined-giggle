a5135cdae8115b906c0311af3589e813355df9fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:24:25 +0000

    believe beneath sometimes comment could

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
