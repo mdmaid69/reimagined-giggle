d96d22a26d72c0ec92fd60bbd3df146796b7f911
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:18:16 +0000

    my exhibition try myself thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
