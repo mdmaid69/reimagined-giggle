5ca335416a0d81fce5f0983736c7dc8c86364085
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:13:46 +0000

    suck wish emphasize considering pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
