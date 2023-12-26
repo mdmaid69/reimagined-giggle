eaf9fc652e8f5178512adbd6c140da35ce46f063
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:00:58 +0000

    test blue draw present target

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
