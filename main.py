60b4ec6148036fcaafb756ec1c34bce768575be0
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:50:48 +0000

    schedule position organization cold divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_current_directory():
+        return os.getcwd()
