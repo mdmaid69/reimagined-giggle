465d509cd47a4e6c2a86f25b8b9242b0d7fe01a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:25:20 +0000

    row brush know health arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def check_if_array_contains_item(array, item):
+        return item in array
