e0ebd24c7644d1b3b5873a6eb61a98fd75724a24
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:22:43 +0000

    perform except constantly town slip

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_pressure(force, area):
+        return force / area
