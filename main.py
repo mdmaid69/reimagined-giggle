f1827076aaa885c19348e7b63b96ec9f895f42ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:18:02 +0000

    spin really proud base charge

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import collections
-def create_user_string():
-        return collections.UserString()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
