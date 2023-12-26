f423b42d9b403b26d625bcce2755c38ce2cd2f60
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:48:16 +0000

    pronounce high any service university

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
