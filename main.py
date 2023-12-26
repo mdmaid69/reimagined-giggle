2dbef56f22940f2b5d519a76c0f7886a514b76be
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:55:16 +0000

    suck wild remember home move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
