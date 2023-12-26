0000f0df4ded5135baf1766f661ad3e75f783792
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:02:23 +0000

    term street less Qq common

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
