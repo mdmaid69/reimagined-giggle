084145e7f4137ee028293f5c0668a70ecebe1c73
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:25:06 +0000

    spite maybe basic regard thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def reverse_array(array):
+        array.reverse()
