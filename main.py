2fd4ba8989a8726c835a63c36fdc2ea6590366a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:26:42 +0000

    share argument light serve claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
