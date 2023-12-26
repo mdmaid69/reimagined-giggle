a4e84bb997e383ea3bb4b9a50379174bb3683df0
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:53:29 +0000

    future gooff runaway bush bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
