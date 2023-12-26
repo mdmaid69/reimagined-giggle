2b9ab7210d94050c53b59e9f6ceeef811103de00
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:41:23 +0000

    problem fail any holdon point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
