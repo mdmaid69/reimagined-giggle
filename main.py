11ef4976099ad0fecd1a9bf8930e526cb1b99908
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:29:07 +0000

    drawing organization year hide keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import random
 def generate_random_sample(population, k):
         return random.sample(population, k)
