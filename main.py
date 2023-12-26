78aece77234f282217d06305b6572ef044043554
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:25:35 +0000

    arm model pretend soak pause

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
