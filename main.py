198c4ac80df43ee5642ac635cb945e47111d96b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:10:51 +0000

    drop tie clear reply speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-print(math.pi)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
