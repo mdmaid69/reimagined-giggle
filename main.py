3d1dacef14c533d428719bfd0e9ea3b5bb2124be
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:52:20 +0000

    art Rr positive actual negative

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import os
+print(os.getcwd())
