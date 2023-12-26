789d4e17963b67ad254ec41724c8eec44c455d06
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:48:40 +0000

    lucky confusing instrument attitude interview

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
