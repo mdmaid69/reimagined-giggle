68c6273ddd17e9e3f8d218776f8e226edc0cc7ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:22:25 +0000

    luck home sport paper careful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
