5b98725667a9205b7a92903a578523157083e8ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:44:54 +0000

    spoil talk rich wave downstairs

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def multiply_numbers(x, y):
+        return x * y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
