58e05ecd03f1c62b0a56645a5c8cbb574b11b626
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:33:49 +0000

    polite thin idea wealth switch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-for i in range(10): print(i)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
