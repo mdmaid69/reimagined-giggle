aca0c780ecb8a7591969e79a3c0e293bd7e7f7b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:04:22 +0000

    avoid whenever within mess wind

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
