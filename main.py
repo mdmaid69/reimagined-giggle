8be3c9fa6320eacc561deb74f7cddbcf6297f309
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:39:35 +0000

    key explain can class remark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
