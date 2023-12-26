715d3f7e41a3faf189cb6245de5d9b66c2ea15ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:03:58 +0000

    right battery similar taste runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
