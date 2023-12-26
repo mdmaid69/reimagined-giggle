f27fc0eace0164fe26b13eae4163ff6790b6c12d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:11:03 +0000

    rest chief keep name mixture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
