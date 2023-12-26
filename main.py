381cf22ab57643d6aa25a69dd5b40984b99fde89
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:43:50 +0000

    everyone promise tune mine close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_cosine(x):
+        return math.cos(x)
