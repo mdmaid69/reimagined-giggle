2ecba67dbddbcb3e4a1a6ec3c115bea31ce407ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:58:07 +0000

    top bringdown well language bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
