0ba5dc729df3be7e425fe413988d64e1e662ac29
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:57:47 +0000

    workupto fork tidy whisper aim

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import tensorflow as tf
+print(tf.__version__)
