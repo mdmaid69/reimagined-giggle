a306eb727eda326d9de814a459a2c8b08feda92b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:26:15 +0000

    single unpleasant address apologizeapologise return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
