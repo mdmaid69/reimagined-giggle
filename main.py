38568de57d005fbdb760f516a8176b1c593c3d4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:33:44 +0000

    door after second whenever move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
