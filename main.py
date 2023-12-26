99fc67b6ae88d63a1fb80b065ffca3029c3c1602
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:32:21 +0000

    uncomfortable sad spend move flight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
