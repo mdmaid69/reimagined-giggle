179af00726d502e3c07b805734e793768e7d9039
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:10:05 +0000

    forget immediate hurt underneath dear

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import random
-print(random.randint(0, 100))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
