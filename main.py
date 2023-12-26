5652ceeab8f6abb60512631032b0635738115b76
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:38:57 +0000

    fond style frightened must instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
