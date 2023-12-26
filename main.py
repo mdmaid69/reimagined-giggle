a9cb21fcd3f11b8a040a570d488215b33ce12da9
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:49:40 +0000

    foot interview break imagine uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
