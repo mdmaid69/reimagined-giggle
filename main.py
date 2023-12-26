4c0ca5d96a8bad8d604a6c94ba6c6b09363a703e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:06:33 +0000

    watch story bunch direct after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def divide_numbers(x, y):
+        return x / y
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
