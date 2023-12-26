7c3dae250b2016948954edbfec559581769251c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:07:40 +0000

    flavour obvious trousers nerve crash

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+i = 0
+while i < 5:
+        print(i)
+        i += 1
