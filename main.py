913527012ed89c676667dc03460f76e924941cfb
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:16:46 +0000

    extraordinary say please slight mess

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
