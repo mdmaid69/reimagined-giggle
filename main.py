c23f128b166fa2aaad69bd88dd3c15687a706956
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:04:17 +0000

    motion interesting aim attempt claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
