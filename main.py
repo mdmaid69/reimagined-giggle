13fd1c83c649172a78df62c6076d0c3595970937
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:08:17 +0000

    promise imagine threaten there quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def find_max(numbers):
-        return max(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
