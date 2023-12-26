125a3da78dc7f73dd05e1b86707723f1209f25a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:35:12 +0000

    tour prefer properly young though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 import math
 def calculate_cube_root(x):
         return math.pow(x, 1/3)
