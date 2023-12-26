af1b515e0208968cdb0d7336a13003c9a17ffee2
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:26:48 +0000

    breakout Ff your rush offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
