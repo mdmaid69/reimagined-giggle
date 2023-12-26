bb5dc8ce5874fd1beccb12ddf2cbc90061e097da
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:12:10 +0000

    trouble comeround sky fall authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
