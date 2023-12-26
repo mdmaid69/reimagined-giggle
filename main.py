c93b5f7c76ed354e5bee74ade1baec52bb67f254
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:15:02 +0000

    rush mark tragedy spot impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+print(math.pi)
