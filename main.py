e6e84dc8747e6f3752ed20aaf784e080e2f66198
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:06:36 +0000

    shelter piece study measure about

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def multiply_numbers(x, y):
-        return x * y
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
