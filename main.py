d119421fa27dd51ebca0b333c265d62f55c070f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:03:04 +0000

    sister invite male gas wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
