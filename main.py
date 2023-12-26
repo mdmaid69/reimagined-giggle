0af6d1e3d66eacb00e3ec9be14accc5beba01db5
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:54:59 +0000

    meaning division talk breakfast impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
