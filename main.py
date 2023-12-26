9017ccdf2e061fb87c441bcff574fe9b0b450537
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:02:37 +0000

    butter rush plan turninto state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
