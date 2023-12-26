9326fc70a2746658e0a46037f66ea82a96308c4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:22:11 +0000

    collect stir stiff basis trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
