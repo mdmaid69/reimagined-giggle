7324e272063e2c55d565254dc62a961be882255e
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:42:25 +0000

    embarrassing gather murder list passenger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
