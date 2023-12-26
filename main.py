6d29973850351fdddc5e293ebe7b67b4e28f031f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:36:26 +0000

    stick goout deliberate rain petrol

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
