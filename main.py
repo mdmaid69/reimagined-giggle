34fefa23363d223c6e4e356348f9e40a777aa3ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:45:47 +0000

    sting yet tense stranger responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
