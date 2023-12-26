4a542d6afc3f6ca4304da81d730096c4b40442ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:51:51 +0000

    effort charge hard content summer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
