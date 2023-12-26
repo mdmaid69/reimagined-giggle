976dd5765476aac5d5516635b216cb929d928fa0
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:50:19 +0000

    careful our room song runout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_area_rectangle(l, w):
+        return l * w
