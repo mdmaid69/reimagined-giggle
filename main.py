56c8c3998e1c2c4dee40d6fb3817124432395e20
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:22 +0000

    influence me principle block boat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
