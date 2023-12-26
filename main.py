4f8d65a7728b4ccc0d01b925d0d6d001aa3032bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:48:55 +0000

    genuine load avoid frame where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
