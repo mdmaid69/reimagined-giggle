8f32a9978f74fc411af0c3b261e3c43ba68a88c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:03:32 +0000

    anybody argument too Uu imagine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
