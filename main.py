fe2159a3de5be824c2cc5ba24ef6667cbbcb3a43
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:40:39 +0000

    seriously mainly influence we tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
