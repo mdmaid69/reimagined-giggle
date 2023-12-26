1ea92637908189cecfbae583cf2b7556f22f0d74
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:30:00 +0000

    sort visit meet pause suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
