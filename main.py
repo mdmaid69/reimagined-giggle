cf2e8fe13f9c5bf35375f344b997d3e30e9f08e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:34:04 +0000

    dig callback shadow break correct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
