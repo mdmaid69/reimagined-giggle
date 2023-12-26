61e3f68be6d32a4edec51cd20c82db3e7beaa63b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:53:50 +0000

    pass light break recognize neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
