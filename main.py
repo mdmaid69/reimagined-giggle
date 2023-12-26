6566d8b409dd9496c44dcd442fd5e0eab27ebda0
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:09:24 +0000

    know takeaway bank someone address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
