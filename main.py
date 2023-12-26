7f60cc3d85abe1808a96278e1e72037e1ae6021c
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:58:02 +0000

    round finish that comeover 571enough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
