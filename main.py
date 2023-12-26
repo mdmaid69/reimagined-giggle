3adb6c325c3c54dfea847e41ad074b71329263be
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:25:33 +0000

    recent ask light pause extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
