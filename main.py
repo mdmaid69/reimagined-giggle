163420198f7252370c3bc1901e528ed4497349d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:05:00 +0000

    message nose bread will shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
