a6197b82a38505adcaa75449ec80d832d9823eda
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:28:40 +0000

    there food consult pinch quick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
