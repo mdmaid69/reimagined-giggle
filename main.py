226a22db21c2bc313de81e304670a19932f53912
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:51:05 +0000

    different remain build relative mess

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
