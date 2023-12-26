908806de7bdbc990fcfc76ecacedbaf95bf3b5a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:05:29 +0000

    our way news their section

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
