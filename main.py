c7db765d88cf3df5078844c2c8f7a982a39f4992
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:45:06 +0000

    tired sign callat here amount

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import sys
+def exit_program():
+        sys.exit()
