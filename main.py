7ccb518714c4094658504455408ac781ddae59de
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:32:41 +0000

    once anything half test short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
