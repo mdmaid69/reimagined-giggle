c2e556a1a1f97af7a7cac594252eec0e39e13686
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:53:04 +0000

    whenever threaten once movement time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import os
+print(os.getcwd())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
