cbe578fd8c992dd75051ea0da6af24a9321871b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:54:31 +0000

    health transparent anxious both artificial

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
