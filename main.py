ab96b7121bdf66eb6a62a704c2a6fd0c2f409d59
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:52:49 +0000

    confusing across road mix dark

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
