4f200f6fa64573b1e90f6a7e8bfe934ecb944473
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:25:42 +0000

    impressive on murder offence too

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
