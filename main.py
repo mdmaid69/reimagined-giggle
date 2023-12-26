a77a7fe3cc82f1dc0b4cfc5dd61b3ab8375fac34
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:29:52 +0000

    sweet mess floor sticky window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
