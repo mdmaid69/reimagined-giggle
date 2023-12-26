d89707d8aa40cdab409c675009dda58100936a1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:47:26 +0000

    common bright form form everyone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
