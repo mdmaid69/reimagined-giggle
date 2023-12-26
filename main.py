27546119001f5da37bc93a952a859ae2e1d4aeff
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:44:29 +0000

    behaviour environment brain world performance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
