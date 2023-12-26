486be1e28082c25104a251c99d0e145498d60999
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:31:06 +0000

    long around welcome fear hurry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
