2def2c94c9535e8bfa3bc1cb77bb955429b4ea09
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:35:43 +0000

    order feeling child worst single

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
