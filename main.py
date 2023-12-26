59ebd752514fa7b4f331bf2e9ee426d552342a69
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:34:20 +0000

    dry compare hold limit passenger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
