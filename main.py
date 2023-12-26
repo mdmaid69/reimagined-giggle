f9802df9cba44f15f96425be4fecc5a040370c6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:09:13 +0000

    pride cover paper fault scream

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
