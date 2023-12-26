d38547a77b3f5584ed93ba1a5b9aec4c84f7df71
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:04:05 +0000

    separate difficulty round underneath represent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def convert_to_hex(n):
+        return hex(n)
