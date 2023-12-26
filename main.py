9df7df35c8044f4a599b4e1ef0e91abf47d4eb18
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:32:21 +0000

    exist damage angry frighten later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_bool(array):
+        return bool(array)
+import numpy as np
+print(np.array([1, 2, 3]))
