5a8a020f613df4156707354e3924f88c2e27fa36
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:05:20 +0000

    giveout object enthusiastic community intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
