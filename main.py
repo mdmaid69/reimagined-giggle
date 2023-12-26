8f671a78efb2c43f9462c5cd06adf347359c9be7
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:34:12 +0000

    trick left flow match safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
