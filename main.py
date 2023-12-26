4ef143bd531402e8700b7a52324ce98630d62cbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:45:25 +0000

    left hour trick month private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_power(work, time):
+        return work / time
