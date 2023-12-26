1ff606df881c18c429c516e6a508db4cf85adcae
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:59:40 +0000

    between period woman vote noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
