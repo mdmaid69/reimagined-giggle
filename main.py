31ee23dab7be6476436a088cae8a840c384a745e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:31:55 +0000

    reject kind quantity grand mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
