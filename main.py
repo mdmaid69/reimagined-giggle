bca5d15511aa40881afe0aa5a90b7cc719cc68e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:36:43 +0000

    bury goaway finger pair entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 def calculate_volume(length, width, height):
         return length * width * height
