9e3034b745e8e392d2cb130691c860741466d95e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:29:20 +0000

    offence travel variety kill now

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
