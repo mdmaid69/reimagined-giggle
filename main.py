9fc9ccea6f70ba31289f1808c902011ff4320659
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:18:04 +0000

    come tap thinkover trade goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+for i in range(5):
+        print(i)
+import tensorflow as tf
+print(tf.__version__)
