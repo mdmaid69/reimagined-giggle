7e02f13014dd12c7383f50b88305d847653325eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:48:38 +0000

    refer degree finger juice produce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def square_number(x):
+        return x**2
+import platform
+def get_python_version():
+        return platform.python_version()
