3e70371f6ff5d7305d0e3ea76a678d9b67725678
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:55:03 +0000

    spill herself start long cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
