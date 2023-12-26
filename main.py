62324a536b30657eec9d314aa2f695c2d6524a17
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:41:57 +0000

    wish comeback most wrong direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
