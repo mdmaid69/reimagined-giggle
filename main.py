fc10fa23b2035ee852c7d4360734068637ce4635
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:03:52 +0000

    clue trade clean once honest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_typecode(array):
+        return array.typecode
