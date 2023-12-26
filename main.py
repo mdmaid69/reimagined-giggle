c97f39d328a07bee59bb2dae242e3cbb12b79582
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:55:18 +0000

    actually grade occur takeaway teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
