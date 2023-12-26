9f3d8659b785957858a620251ba94d1b1b847f38
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:28:35 +0000

    border possibly ahead legal true

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def subtract_numbers(x, y):
+        return x - y
+def reverse_string(s):
+        return s[::-1]
