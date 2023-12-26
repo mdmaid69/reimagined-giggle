8f77f536983ccf4a1ad4ad7c53b62addae29650d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:00:43 +0000

    lost odd to ordinary hungry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
