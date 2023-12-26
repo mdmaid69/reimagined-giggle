ce23e890e5578b9d6b8c5eb62bfe2e906b1292fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:38:56 +0000

    lonely area book stare complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
