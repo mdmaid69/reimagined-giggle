3265e33a2b32cbe22e41b1f08ce61004c41b179f
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:21:00 +0000

    hide insist save advanced emergency

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_list(array):
+        return list(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
