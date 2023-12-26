459e4ce83c568a60ae2d33f675fd7a8c842f298c
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:28:51 +0000

    error steam whistle fat dry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_volume(length, width, height):
+        return length * width * height
