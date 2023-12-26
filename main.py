9c761ef4996764cf3f04bb3ac86634d140c3e6c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:13 +0000

    fat sister material argue cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def convert_to_hex(n):
+        return hex(n)
