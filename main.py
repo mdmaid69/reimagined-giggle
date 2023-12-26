35d7bdb62f2d981c4ddb3b3ccce09b6bc79b8cd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:35:11 +0000

    private unable display ought mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
