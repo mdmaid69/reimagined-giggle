e08069b01f898fa288fc4f570dc41c0e044bc2d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:22:14 +0000

    there throughout about pretty block

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import datetime
+print(datetime.datetime.now())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
