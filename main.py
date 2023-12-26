cd7b06858a3291114d5afed84fdd29b36d387019
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:58:58 +0000

    national lookafter noisy face turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
