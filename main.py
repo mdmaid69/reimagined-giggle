a41ca480c14baf741f9b2eca6e6fe4725f622a6c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:45:07 +0000

    effect Ll estimate experience question

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
