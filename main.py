694058e76c086cf849a00300624d84b79349ef2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:09:57 +0000

    wet design situation anxious Uu

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def is_odd(n):
-        return n % 2 != 0
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
