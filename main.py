75661cf3b21a0c5d6da6bfe138b5055338b1d7ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:15:59 +0000

    gentle dear how performance repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
