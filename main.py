a4798435002b131b46729e8710beb243bb3811d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:34:58 +0000

    early serve round guilty environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
