daeba5230d396a1eec4a1bed7c86d3fa95a50283
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:22:23 +0000

    sound kill disapprove time 565end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
