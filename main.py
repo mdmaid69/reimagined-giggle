2785f0f89005c12ee93d747ef84af021a3406e10
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:14:03 +0000

    funny cross by street bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
