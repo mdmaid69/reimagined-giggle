f28aab5557490ad51a5523ec86bfc18f9fb24286
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:57:50 +0000

    thorough stand pollution butter connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
