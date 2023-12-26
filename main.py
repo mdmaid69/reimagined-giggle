012aab6ef764d44719f9ad33d5df22885703bfd4
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:07:26 +0000

    getup 565end drink tooth our

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_volume(length, width, height):
+        return length * width * height
