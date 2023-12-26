ca78e836ab9338dfe6315fa022376a4fe23f83b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:31:42 +0000

    hit fasten Itself ocean Ss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def convert_to_hex(n):
+        return hex(n)
