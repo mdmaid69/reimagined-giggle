0e980e38e579879c40c26b7faae7399f5fd11ba2
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:44:49 +0000

    dream value quarter woman passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
