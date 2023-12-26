53b20cc4fadca5ac8487128c3c730a8dd8d716db
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:50:49 +0000

    release narrow port brush within

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
