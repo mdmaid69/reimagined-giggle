37d32bf24c022489e6c10cb8f6cc18c6b4ecb699
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:19:44 +0000

    female tone disapprove race surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
