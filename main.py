eeb52dc613d8497d0e7d6aefd50eca53b693f62e
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:47:15 +0000

    arrange test nearly recognize accidental

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+print(sum(range(10)))
+import datetime
+print(datetime.datetime.now())
