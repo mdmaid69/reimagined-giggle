591ffaf6db7345a32449e3a02e67ccf570c50a85
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:01:22 +0000

    room outside duty possession soil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
