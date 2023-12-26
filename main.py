e87df3f1c41ab223d8ed809094eab983522100a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:45:06 +0000

    over medium play mysterious mother

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def get_array_itemsize(array):
+        return array.itemsize
