09698d55c1eb17b02198dae8bb25d9dcea7c3616
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:50:05 +0000

    rescue public close start speed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
