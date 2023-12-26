63e5b13686feb45376b6d36b54fbab704210918c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:22:44 +0000

    children shelter container spin ride

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
