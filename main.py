418d97d6dfb8053d8618eb82de22bbdd8ce5edfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:27:04 +0000

    contain workon common worse unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
