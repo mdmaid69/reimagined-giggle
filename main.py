50b7f35f050f1573f20317f923b96a63d861d4d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:54:59 +0000

    second get game clue suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
