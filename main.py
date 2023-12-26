8c5876ca19493d2ec2d518b997902adde7b147c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:29:01 +0000

    use poor tie send straight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
