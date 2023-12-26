24b1b3afd19c4d6555e97cc9cc4d58faf2dfb06b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:32:11 +0000

    public stupid need Ii fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
