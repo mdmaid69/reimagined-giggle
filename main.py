97e0a73463901ace222460b98921cca2403e1541
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:24:22 +0000

    likely each maximum pressure public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
