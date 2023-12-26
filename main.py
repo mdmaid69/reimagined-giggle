a415387f1053bdafcf7d94954a1111946453cfc3
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:56:28 +0000

    thankyou stage damage care underneath

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
