d83c535f58af1d2316715c4e3ef93dcba7347062
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:18:27 +0000

    during tight allow team strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_perpetuity(payment, rate):
+        return payment / rate
