8c75b7a991a535e658cbd274a195b628b54554b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:38:13 +0000

    prefer avoid extraordinary Hh I

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
