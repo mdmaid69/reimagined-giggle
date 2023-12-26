386972053e457b895f14e5e9b79837e5a027d732
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:23:58 +0000

    close communicate Itself remember that

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
