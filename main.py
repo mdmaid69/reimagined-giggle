26d1abc695e9cea56680964543e656329eed3e2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:30:52 +0000

    gather immediate must news inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def clear_array(array):
+        array *= 0
+def convert_to_octal(n):
+        return oct(n)
