4cd1e04c01c74281bfee73e9bb55960e37684701
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:57:36 +0000

    store race obvious that exist

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
