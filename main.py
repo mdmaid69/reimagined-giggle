7e2500fddee4e51d77802f4c6a225a36428d89e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:46:09 +0000

    exaggerate practice minor no government

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
