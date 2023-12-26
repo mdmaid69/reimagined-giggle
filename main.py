496092d20b5bc9242a05d8117c4a3958665db547
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:01:26 +0000

    bye frame nerve degree contribute

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
