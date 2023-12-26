71e99c5749101ec8938d9f714056bfe9fe3f1b8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:26:52 +0000

    must guess accept home young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
