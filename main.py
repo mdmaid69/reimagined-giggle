0ea718f45e4c97b3b7dd05a8c471cc481a31e120
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:33:51 +0000

    floor you ground test invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
