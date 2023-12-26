da386a5621efad291e8e0b59ffd32aa31ea6b564
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:47:00 +0000

    plate keepaway progress modern shopping

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
