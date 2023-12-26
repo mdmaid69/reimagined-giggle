f09bb0a4167d62ec479515eed625869226cbc0ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:59:18 +0000

    influence check may cruel would

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
