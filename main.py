a44b5f4ce1548b0d03dd9114ac3e11bb9fa23c4d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:48:37 +0000

    city male at doubt conclusion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
