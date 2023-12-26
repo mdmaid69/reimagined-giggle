ca9ff5293f755b58cfb2138cb43a34fdfb73c6ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:48:30 +0000

    dark injury considering rush food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
