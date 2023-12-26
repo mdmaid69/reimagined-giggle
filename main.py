6e0298b15067d1e4a47137f229ad05f2cd910ba7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:37:46 +0000

    still trial solve sleep drink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
