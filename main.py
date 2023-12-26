5527a5d2b83e73c3843a724837bea39cb438923c
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:23:48 +0000

    leg contain form total light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
