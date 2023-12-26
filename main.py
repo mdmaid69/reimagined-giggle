341ff4058e0479f9809eee7daf376d30c3d3e728
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:22:13 +0000

    check wide another calm special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
