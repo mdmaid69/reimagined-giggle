8cba06d5d1af2f11219632a9f2ad5240b26f094c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:16:27 +0000

    comment share police teach shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def reverse_list(lst):
-        return lst[::-1]
+import logging
+def log_message(message):
+        logging.info(message)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
