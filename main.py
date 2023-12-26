a409aa455c10ad79af065782ae37eebf05505cbb
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:04:36 +0000

    generous activity more if now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def convert_to_hex(n):
+        return hex(n)
