37877ed63d3e398b7e3d7915bee245716f55306d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:55:56 +0000

    feature genuine fair head bend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import tensorflow as tf
+print(tf.__version__)
