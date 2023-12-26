9c8999e6de3528540849f8d7ef309291ca8daf34
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:54:10 +0000

    dig generally although conduct regret

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+x = 10
+y = 20
+print("Sum:", x + y)
