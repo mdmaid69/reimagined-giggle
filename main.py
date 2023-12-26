8a6a590ceb04f5e72ec896775dcbb3587e190c1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:58:43 +0000

    wear accidentally conduct bend OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
