75108fed50e2de8f6312e7a2a305524de42e1e5b
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:19:02 +0000

    post clear neat number remain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def find_max(numbers):
-        return max(numbers)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
