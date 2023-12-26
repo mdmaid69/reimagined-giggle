c9be90c0726d65bc04585638e1ad1cc85f53d283
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:02:30 +0000

    will sentence whoever improvement connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
+  def subtract_numbers(x, y):
         return x - y
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
