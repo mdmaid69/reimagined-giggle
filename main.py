ea78f5315b8d8526a6995169a2523b13163dcbe8
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:21:59 +0000

    surface guess waste match raise

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
