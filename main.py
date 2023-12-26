c7f3cab62c81a993302f46c1ef438f0c68ef4fa9
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:59:57 +0000

    say pay computer accidental unpleasant

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def cube_number(x):
-        return x**3
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
