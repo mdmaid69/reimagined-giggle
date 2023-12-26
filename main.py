b8cd3b0a37423a2fc5019632185493a56b828f84
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:48:46 +0000

    roof score land keen ride

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-print(sys.version)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_typecode(array):
+        return array.typecode
