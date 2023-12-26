0c184f2950286a436f545a6d6aeb83782733b834
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:14:52 +0000

    soak stamp bath organize deal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
