594a05353de75d991780b90a89edeb38433b5f78
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:49:51 +0000

    taste side everybody public plastic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
