3cfd4e17c98c10c912bdcb41aaaaca63769771d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:51:04 +0000

    slice should hurt hope offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_combinations(n, k):
+        return math.comb(n, k)
