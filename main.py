699aacaa294d76d8326313a879341eedd6d23c61
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:56:47 +0000

    public copy point separate something

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_density(mass, volume):
-        return mass / volume
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_list_from_array(array):
+        return array.tolist()
