3f614236ed51e2bfad356d363907cab68ed309e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:15:51 +0000

    570enjoyable regional exact embarrassed whichever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
