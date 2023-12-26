b0413451da66ead7cf3a86b673082c4db549a90a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:51:24 +0000

    worse meeting splash juice whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
