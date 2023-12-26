db8c18899bbc748fa70113860c0a3a0db59f58ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:41:33 +0000

    lead operate kick runaway roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
