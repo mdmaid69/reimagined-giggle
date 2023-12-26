900b5d1be476676c836f9c05e6a5f82f80c39c2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:40:45 +0000

    free hide since culture Ll

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
