54891eef1f7167678e9ee5fe349bf0130e5a4162
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:44:50 +0000

    journey whom knock example list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
