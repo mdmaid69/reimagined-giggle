0224dfb88ceda63b066afdb3cda437734cfe07e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:55:18 +0000

    string hungry set load set

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def subtract_numbers(x, y):
+        return x - y
