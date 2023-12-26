e5d935e31c323904ffbd323cccf3da43878f048a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:50:57 +0000

    basic shut whom someone people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
