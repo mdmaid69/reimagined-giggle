1800194e98c6546f111d3e0f0520904a1e3076bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:59:46 +0000

    machine normal trial today emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
