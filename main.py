fe291070c7dc9a0074f634ff1754f68c943af53d
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:10:01 +0000

    foot far minimum love secret

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
