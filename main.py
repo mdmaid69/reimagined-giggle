53ec6cbd1ccf02af6b2285d875870d853ab8e674
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:26:57 +0000

    thumb cook sail hair building

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-print("Hello, world!")
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
