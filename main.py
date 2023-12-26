2e189a0b7c0cdb4145ad1242db648dd74a2d5aee
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:36:01 +0000

    wake suit model worth match

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
