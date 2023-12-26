1c7493b7bcdb3d8688ec8c5afb6b50b2642349a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:57:52 +0000

    project whole happen sleep breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+print(os.getcwd())
