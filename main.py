4b53ae7c92e529a2bfd6a60907db018e399b668e
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:47:16 +0000

    underneath sign stir team forgive

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
 def is_prime(n):
         if n < 2: return False
         for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
                 return False
         return True
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
