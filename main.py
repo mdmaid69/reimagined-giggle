331f26c075af7abbc4ab76c4d5af50f103a98742
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:46:35 +0000

    sting no ill holdout narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
