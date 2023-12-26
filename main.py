bd1f47e6cdc528e0e3cbccd603506161b5f52925
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:53:00 +0000

    plan style computer public embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
