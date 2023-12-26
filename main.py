59a734e3f90a6ed8172890a5a1114f16e9bc157f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:36:31 +0000

    danger bend become pinch here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
