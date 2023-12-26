f96485c6064af699101451e34eea19929b77a6f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:26:50 +0000

    disappear whom tight glass soak

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
