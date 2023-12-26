93a65272bb96fb887ce2f3950aec8c6b25a1a00b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:25:48 +0000

    pass impatient onto earn neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
