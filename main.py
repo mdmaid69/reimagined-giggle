c4161b833fe25d8cb87f88a21cd3c8d7a4cd1864
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:33:33 +0000

    smell repair write government everyone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+print(re.match("h.*o", "hello world"))
+def is_odd(n):
+        return n % 2 != 0
