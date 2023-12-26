816e6d90c26d73765a4039140e3b6fd48a1ee6a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:57:32 +0000

    minute miss science bet hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
