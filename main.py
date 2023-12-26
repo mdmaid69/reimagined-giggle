582d428cc2e73add5a6713d1a8b34521c27bc1eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:15:26 +0000

    through still awake consist pin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
