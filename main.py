a75bcc47bb70e95c00147d582820ee50795fba74
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:02:06 +0000

    style swimming similar extra wide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
