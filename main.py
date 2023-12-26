c50193f07c42045431dd51a3bc47db80dc601b18
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:37:27 +0000

    afternoon sport breakfast broad yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
