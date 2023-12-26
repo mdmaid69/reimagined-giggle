dddba8590980eddce290f627c28c42a20ca56c7b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:35:25 +0000

    tip rescue empty tip before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
