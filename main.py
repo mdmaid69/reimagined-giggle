5e98e17872e2edd6283710a2c997f7bb39c795a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:34:49 +0000

    below some apart on future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
