5cde9e911ecf11b7fb124381fcb804b4d266f21c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:29:08 +0000

    copy of measure love bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
