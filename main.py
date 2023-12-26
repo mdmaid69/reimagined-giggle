886203e3dd21c0561e007ea24b0fbb347c42aafd
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:56:51 +0000

    equipment circle thin shade separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def remove_duplicates(lst):
+        return list(set(lst))
