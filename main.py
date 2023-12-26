c3ee7cc8b2a7c870f6ec8c3b28590112c1883326
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:42:50 +0000

    keep stamp too feature oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_floor(x):
+        return math.floor(x)
