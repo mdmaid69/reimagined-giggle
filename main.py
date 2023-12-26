baf06fb0eb466c181c4b40ca1557fed438d0bda0
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:25:19 +0000

    far shallow undo emotion environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
