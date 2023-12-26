4243279bca0fc89f90ea33c7f5f12a1af2fd6819
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:25:10 +0000

    rubbish absolutely make congratulate smile

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
