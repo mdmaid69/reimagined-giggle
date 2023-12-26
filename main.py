d1cb70c2902c87f26b4d5a3fba7171b8b2f8c1ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:18:52 +0000

    convenient wonder smile so deaf

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def convert_to_hex(n):
+        return hex(n)
+def is_odd(n):
+        return n % 2 != 0
