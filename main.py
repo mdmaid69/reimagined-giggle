4b1af9f885e5753715176da213a6c490f245dc42
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:18:25 +0000

    motion famous copy bush fit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
