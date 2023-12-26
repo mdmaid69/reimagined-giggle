11f8b7b2934b36fc706f01c35e89c8707c24cdcf
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:58:14 +0000

    clean listen paper serious run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
