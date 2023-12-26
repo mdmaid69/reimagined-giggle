0c971b9edb6f4bad69755e79d2c725ebae719f1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:08:29 +0000

    butter will Yy honest flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
 import array
-def append_to_array(array, item):
-        array.append(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
