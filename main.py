a038ea3695dce9254a812b81cd457c6c6b6af098
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:41:32 +0000

    little object trick even create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
