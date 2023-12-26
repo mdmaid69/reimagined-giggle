ffddbafc5d692ac5c9806532b0767cfbde33781e
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:11:21 +0000

    not moon later talkoutof comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
