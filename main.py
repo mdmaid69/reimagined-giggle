0cb794429c619fa464f0eb0acc169f837aed792b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:30:39 +0000

    until paper nerve risk hungry

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print([x**2 for x in range(10)])
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
