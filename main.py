9e188cbec6b0f4a5dbc4c086a535d730b259a84b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:05:14 +0000

    feeling immediate breakdown state uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
