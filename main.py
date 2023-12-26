86bd16b03809424060e9c73ebfffb563e8a08404
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:35:50 +0000

    chase wave Bb bear spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
