1e97492ff1c51664234ac0b3eaed73512389139f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:05:30 +0000

    turnoff black strike importance row

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
