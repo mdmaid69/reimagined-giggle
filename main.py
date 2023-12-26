79c9305c83a64f30a78ea77591abe5ca513c4141
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:36:58 +0000

    dish bad pollution meal measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_list_from_array(array):
+        return array.tolist()
