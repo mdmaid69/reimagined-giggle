2695051cacc76a2d5e87e48b29f18545ef6ecd1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:34:47 +0000

    pool speech copy union drink

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-print(sys.version)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
