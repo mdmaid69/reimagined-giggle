f5d7e0ab30f248dbc875d9528607be172cea31ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:39:00 +0000

    relation direct break extremely means

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
