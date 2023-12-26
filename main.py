bae95497603384db24b91f8c605c7c4e5ae8cf0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:24:20 +0000

    tone getaway spring ability point

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+print([x**2 for x in range(10)])
+name = "Python"
+print("Hello,", name)
