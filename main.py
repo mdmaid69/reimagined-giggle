1c48b93a66e03855651bde75a0c982babc313b69
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:09:53 +0000

    without popular Ww live birthday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def subtract_numbers(x, y):
+        return x - y
