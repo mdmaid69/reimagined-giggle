9c0aa30ea6b3336501968d2861f361fe86336a69
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:47:29 +0000

    talk plan impression test development

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
