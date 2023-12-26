3b05959cf138ad8458a144dc19e707b20f3db237
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:30:03 +0000

    pour door cheap giveup 571enough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
