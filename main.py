6be180daa8ee001ca2b4a941a5df81497b5ef0ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:09:52 +0000

    fun talk knowledge ill pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
