3394a118d96fe4f6cf0f0cb69bb59b28032032f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:10:59 +0000

    skill hurry performance anywayanyhow centimetre

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
