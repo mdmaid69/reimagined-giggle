174512fc9a96e363917bfe2b62ba8a2b135a64f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:36:36 +0000

    shoulder early communicate understand either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
