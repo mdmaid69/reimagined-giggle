6b5aab322882d861ae629f588c568ef84d40175f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:42:14 +0000

    season union callout bunch climb

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def count_elements(lst):
+        return len(lst)
