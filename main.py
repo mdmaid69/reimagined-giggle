c45375325b850cfc34c94d51fdb4837e38135cb8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:42:27 +0000

    settle opportunity 179bite block base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
