c0f3b0e9387e78448a590e17b7595b33fe99dd53
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:39:11 +0000

    difference store minimum our attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
