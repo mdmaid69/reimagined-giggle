73b2ee1ec3e7a20174604c0b22fda76638f45936
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:17:10 +0000

    regret after godown production cross

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_max(lst):
+        return max(lst)
