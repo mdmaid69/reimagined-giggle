4b808917bdffc8bf311fe5f981f67abb927b0c24
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:16:25 +0000

    gift front write pure trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
