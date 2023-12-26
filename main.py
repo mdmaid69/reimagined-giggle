3d95c68918955561cf67bfe56642cc1f20139a27
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:22:19 +0000

    pure bringup turnaway 572enter will

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-for i in range(5):
-        print(i)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+print(math.pi)
