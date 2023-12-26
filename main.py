b54b9de6d3198327093ca1162981f3bd6aa824fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:47:58 +0000

    post near interest responsibility cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_string_from_array(array):
+        return array.tobytes()
