7b229fced646dadf8a0c2a39a5d497ff30685c20
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:57:57 +0000

    past write mystery young cut

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
