9b17fe74d8abd54c57a2bffe9a6b25506cdc3aa2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:53:41 +0000

    attract matter judge if rough

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def convert_to_hex(n):
-        return hex(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
