bcc025d45eb290e6aa1cac2c4425c8b47a942cd4
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:06:50 +0000

    cover badly involve announce save

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
