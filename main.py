081a7cebf2552fb7da51dd404f81714f64b96254
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:48:11 +0000

    little turnoff boring exact pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
