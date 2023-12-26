277d29bfe4be208790095b2f71afe30242a187d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:42:47 +0000

    before bunch dangerous house secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
