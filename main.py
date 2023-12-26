3a32f69a501529dfe9d1362e1324c01c124e4083
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:30:13 +0000

    provide light ball inform expression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_even(n):
-        return n % 2 == 0
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
