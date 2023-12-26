d9ac9a1677b12847fff2b5223a53e36dc25cf1eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:21:37 +0000

    soft our entry respect equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
