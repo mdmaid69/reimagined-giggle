177c65316f95f423d16fd83598e24b942bf86557
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:28:49 +0000

    ourselves takeoff round wheel typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-print(math.pi)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
