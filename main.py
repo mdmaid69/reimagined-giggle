bf9145bad0aa4bf341e1d340b377c3b9ccc89e7e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:01:04 +0000

    sleep please length use work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_palindrome(s):
+        return s == s[::-1]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
