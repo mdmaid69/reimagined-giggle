190ee6dd5ee0271336571332eeb73e689785cd40
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:08:17 +0000

    key object runout expensive unlucky

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
