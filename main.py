148cc5664ca0240306251d0d418822e33e798139
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:14 +0000

    getaway past wide appear weigh

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
