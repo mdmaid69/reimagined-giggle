3653d69bcabfe21b2b4c4ea176506bea8ee4301d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:05:45 +0000

    pair travel eat uncomfortable kick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
