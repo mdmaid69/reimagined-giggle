170a30d682583e292879ae3edffc041dcd162920
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:53:25 +0000

    relative earn thick splash regularly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
