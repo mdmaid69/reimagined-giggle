0ffb10d094243e9f7116229699e82eb54cd0fbb5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:14:17 +0000

    turndown war inform offence attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
