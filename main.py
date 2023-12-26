2989c53494dbcbfe22f6e4f29a7fe78ab3cc31ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:12:10 +0000

    ill congratulations square height mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
