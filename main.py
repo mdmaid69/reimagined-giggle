c9ca6d0a4a428035febe09fdadf3c2e26b49844d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:21:46 +0000

    good wash instant bringback confuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import tensorflow as tf
+print(tf.__version__)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
