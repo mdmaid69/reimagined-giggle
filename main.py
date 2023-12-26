c8c90bb259d05a63b1fefe6bb72f7a67e1bc75c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:08:02 +0000

    comedown sting draw callup before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
