36b0e757f2709f2083035cd9fef6cc66a3055f8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:49:53 +0000

    smell violent start late development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import sys
+print(sys.version)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
