628d1ba0b4ebbf14f148a68ec05bc210747c3f1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:42:12 +0000

    any geton unlikely delicate joke

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
