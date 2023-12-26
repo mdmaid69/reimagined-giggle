de975fc1dc2c6497983a04c22337f15f179a806f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:03:50 +0000

    fasten beginning partner about account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
