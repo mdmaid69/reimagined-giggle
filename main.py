676a6f31f48f81f2a2a71bf5d6a4ea1559844661
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:11:37 +0000

    smile tear convincing fence noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
