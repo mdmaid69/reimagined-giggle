18da743408df53ac5cc167ed09ae5952672bf4b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:18:08 +0000

    war cap recommend sentence trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_speed(distance, time):
+        return distance / time
