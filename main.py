00b161f82053af2151a0a5306e5695ae5243b66c
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:31:50 +0000

    reason whoever relationship surface waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
