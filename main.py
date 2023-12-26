1a7ab6a3f8f0ec8a827822d98e8fe16507a76596
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:58:46 +0000

    extra children tight want add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
