aee06c876e63fb6f78c450a4b810b545b02b3ff5
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:23:08 +0000

    little bit workupto exist laugh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
