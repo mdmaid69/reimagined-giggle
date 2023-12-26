910b8ae9651acfd0d7107132712f42fea38e08e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:32:17 +0000

    honest intelligent what question abroad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
