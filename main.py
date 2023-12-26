2695ab52b589a6808ed93a1d483aeba215c1cfd4
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:08:53 +0000

    sleep goup comedown neither spray

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
