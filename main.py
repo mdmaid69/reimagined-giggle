dfaa790335ad1e03e46f5acbdb6bc9aba477150f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:05:11 +0000

    church properly lookup rain careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
