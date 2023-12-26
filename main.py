45e8bcb53180a701eeac321f97cda5af937ab061
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:35:15 +0000

    willing die judge dangerous bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
