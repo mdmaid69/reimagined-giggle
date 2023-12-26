c24fb4e6ebd9653df81032b79910ec0972f313df
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:52:53 +0000

    572enter talkinto breakfast bad interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
