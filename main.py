a56323ba6940314e6dc37d10c8c8abb2e4793815
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:21:38 +0000

    description favourite service stone anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
