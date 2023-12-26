46edb4812bddffe04bfefc8ccb83718b97019a35
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:14:56 +0000

    572enter tap fall we sister

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
