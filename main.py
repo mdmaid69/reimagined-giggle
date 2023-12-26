0ce603172f8112f7bb31bf82d32a598ade25f156
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:45:25 +0000

    together control approve trouble loan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
