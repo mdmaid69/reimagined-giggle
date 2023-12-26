f4bc2b6e3212ed0a90b723e1f1aa0bd45db08b9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:24:46 +0000

    conversation belief hit gas sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
