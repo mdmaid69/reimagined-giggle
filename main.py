7181d0e50067e9f3ed4a831e5d085afcb95b5dc8
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:31:10 +0000

    deep respect present extra he

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
