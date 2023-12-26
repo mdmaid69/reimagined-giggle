696271921373c152ae2e9c7f0a51886cf5b9dbfa
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:54:22 +0000

    shut innocent outside air busy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
