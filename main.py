33a0214074cf46c6aa809ddf0310bbd10d571c5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:04:54 +0000

    shock sink wake any culture

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_as_repr(array):
+        return repr(array)
