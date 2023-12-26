af7dad24c6032c72521b0b390697cd49fb582d92
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:46:09 +0000

    exaggerate sign surprised study sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import collections
+def create_user_dict():
+        return collections.UserDict()
