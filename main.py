74ae73a593fb30113301946d52e57f45342ae4c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:42:05 +0000

    discover 568energy fork talkoutof university

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
