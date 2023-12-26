0d063f093f331f29a5cf7294fb24f9cd3edc8b47
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:42:15 +0000

    task disagree later table crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
