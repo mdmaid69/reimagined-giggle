3b949aa7fb43c3a37a0aed15bc30dfcd19c4b7bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:11:30 +0000

    careful wealth except loose mess

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
