57384876165639d08f8399c0bd94d266564c0ac8
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:59:11 +0000

    street safety sort stiff shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def change_working_directory(path):
+        os.chdir(path)
