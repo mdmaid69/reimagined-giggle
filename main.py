ee84d5631178e16bb12e9f153fc6f56083e6b8b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:25:25 +0000

    boil keepup heavy care call

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
