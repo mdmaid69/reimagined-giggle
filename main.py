60de5704bd1f25c106a44d67fc23ad1272b806f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:21:50 +0000

    impression ride delicate satisfied comeround

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
