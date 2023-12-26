2ccc215b068ad1667bc874a5d61052fceadaf8ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:12:20 +0000

    hurry lonely priority before small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
