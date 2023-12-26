3ea53e98fec8f7a3596ba6383a639015300fc29e
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:06:04 +0000

    glad content snow coffee generally

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
