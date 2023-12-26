131d89e47a3d29dca1def9330f28e10f6ecdd8a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:33:19 +0000

    kick show entry strict its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import collections
+def create_user_string():
+        return collections.UserString()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
