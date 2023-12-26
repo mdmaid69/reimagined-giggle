5aca2771534f5336b17d8b9d4ab9bd2dc32547d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:38:40 +0000

    piece original new they couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
