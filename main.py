3b60876b8d46106547fb2a00b65cb8474b4cc714
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:09:04 +0000

    worried hate especially damage comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import os
+print(os.getcwd())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
