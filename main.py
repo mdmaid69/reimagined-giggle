372133083e3973bb075dbdcb3aeec89fc4ce3e9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:55:48 +0000

    copy arrive authority content block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
