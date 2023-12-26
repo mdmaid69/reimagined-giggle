38ea0ee1942823c602c96c988bc7391d85017ea2
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:47:48 +0000

    pool business each considering brother

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def greet(name):
-        print(f"Hello, {name}!")
+import numpy as np
+print(np.array([1, 2, 3]))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
