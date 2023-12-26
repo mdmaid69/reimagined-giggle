60023bb8808bf29cbf76e6a0a49c6fd2f132dde1
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:22:54 +0000

    part upwards insist interview feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
