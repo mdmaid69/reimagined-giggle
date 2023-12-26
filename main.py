6839051e24f91b8ee621ec4850765c6af81ea44a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:47:28 +0000

    century smile turn shoot mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
