69bf94b1ad0a76dda6669fa717dc69f0af7ea122
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:31:27 +0000

    idea neck tidy bag against

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
