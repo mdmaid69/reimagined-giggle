4e625eab125e2a420a8985a8f2a5c99eb40f5ef2
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:35:06 +0000

    only lean from shop plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_cosine(x):
+        return math.cos(x)
