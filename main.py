fa7943154827607f0925363d9fb694ec9c0af107
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:48:46 +0000

    meanwhile height show unkind we

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def add_numbers(x, y):
-        return x + y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def subtract_numbers(x, y):
+        return x - y
