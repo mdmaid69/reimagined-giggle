c78e9c1cc887e00bc5088f8320284a8c897958c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:18:27 +0000

    564encourage keepoff 564encourage whole total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
