029f75b130df4df38f4a8bd0d31670a8d3443d49
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:58:24 +0000

    find track loan system height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
