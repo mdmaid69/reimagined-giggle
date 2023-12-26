bd517f1022da15e920f24f50ba2ad8e267a660e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:32:10 +0000

    declare light young steam behind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def square_number(x):
-        return x**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
