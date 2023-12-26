92516d29d0eb4bcc8548049608a933b4979459d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:25:33 +0000

    yesterday substance performance block close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
