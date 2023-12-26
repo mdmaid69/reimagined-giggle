39e0285882533d0e57744e1c506f81009622e9e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:10:33 +0000

    cry zero hardly joke adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_floor(x):
+        return math.floor(x)
