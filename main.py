3a4c4dfa9d7ccbcba8e19aa256ae7e8359466c15
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:21:00 +0000

    by break point among nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
