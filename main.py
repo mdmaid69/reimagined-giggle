bd9d6a83ff4255243d80a0c112728e4b96c89a25
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:01:16 +0000

    thirsty mistake wave congratulations attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
