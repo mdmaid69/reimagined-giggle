19bc02183cedcb55a3c81e8437fbf999ad3570b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:41:43 +0000

    hit tone human lower thankyou

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
