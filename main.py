cae7ad678492255dfe8ba6442bcf78d77ece24d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:01:08 +0000

    agreement nervous wave available strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
