c21c6f822d98bda17b12127d23daae07f66eb7e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:55:44 +0000

    language swim wave date type

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
