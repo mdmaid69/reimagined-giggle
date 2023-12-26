c364f52cd09d9e0dc6345bd6d6a2c13e8d2606c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:34:57 +0000

    minimum prepare direction make case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
