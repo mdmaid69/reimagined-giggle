c59d783e2ecdbb427f54d7b22820446ee2c64eb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:17:38 +0000

    pretend sting suggestion share complain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
