7cc05f94dfcc9b3d6aa6dfc03fe7757a9e675935
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:55:39 +0000

    lose declare quit bringback do

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
