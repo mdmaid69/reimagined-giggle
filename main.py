efeaa679cd85de6ba8709e5f5bf8042ce5baffa6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:46:13 +0000

    importance way hurry demonstrate blame

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def reverse_array(array):
-        array.reverse()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
