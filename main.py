daa373a3254a3fb32ce95049ab5deecd17e09885
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:23:06 +0000

    gap pin clothes whichever relationship

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
