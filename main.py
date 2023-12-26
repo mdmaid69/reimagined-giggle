bcc982c0f1cdce5f7544f7f967298156ac220b43
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:54:37 +0000

    lend community before area plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import os
+print(os.getcwd())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
