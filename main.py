ecd85cc54c9ebfc96a4d6600eab5c79a3a1cb126
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:17:44 +0000

    news estimate maximum fortunately level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
