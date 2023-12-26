4e6f19580bc73b4ca5dbec8d942d344e846f53e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:10:59 +0000

    joke extremely influence mistake brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
