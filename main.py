85d174c7830bc01045a06f52643c5acbb47d8340
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:29:07 +0000

    bridge worth little help variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
