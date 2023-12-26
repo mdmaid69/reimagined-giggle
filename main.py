23063b341ef0d0de8b208c858ba1b02da5616e2e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:35:37 +0000

    exactly sometimes light breakdown one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import collections
-def create_counter():
-        return collections.Counter()
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_floor(x):
+        return math.floor(x)
