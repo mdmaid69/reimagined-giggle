12e1167c62e03eba37e20d904166666af993f703
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:38:17 +0000

    relative complaint exhibition mixture burn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
