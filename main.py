71191b9a93136102c53f83d30dbb2a77b2841cb5
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:50:17 +0000

    add interrupt score although practical

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
