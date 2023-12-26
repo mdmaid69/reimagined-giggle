95cc9620c86f0c07498a01e17ccbc6a1a7f29ef1
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:33:58 +0000

    warm contact fix space pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
