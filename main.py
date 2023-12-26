f9ff6f411a970c27316c253dcd78ec02a1b084fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:39:29 +0000

    runover convince ocean bringforward broad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
