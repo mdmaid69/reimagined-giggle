18da03b7b73ca710c6af213500eda77f682d0067
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:32:30 +0000

    feature 572enter length process flood

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-print(os.getcwd())
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
