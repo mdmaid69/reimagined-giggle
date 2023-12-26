a6c783185fe5e3ba99bf006939aefe9d80f562e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:07:02 +0000

    arm bin law appeal cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
