cac1af476581c67d37f3d61a9f9ae88385398d79
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:00:37 +0000

    worried callup feature autumn base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
