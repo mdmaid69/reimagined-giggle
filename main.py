fa37cd431b752435131035cff5e0e65f7ff75db0
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:41:50 +0000

    lonely anywhere protection food firm

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
