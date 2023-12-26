3ff220800d7ced58378d3cb57fb5d6fe907f9ab1
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:31:52 +0000

    wet port tell pool meal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
