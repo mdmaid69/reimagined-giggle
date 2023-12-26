6781543d956135d8fd7145b9ee66b45d7990d256
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:12:32 +0000

    talk suggest teach hate creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import getpass
+def get_username():
+        return getpass.getuser()
