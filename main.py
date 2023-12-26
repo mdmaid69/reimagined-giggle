7dbbf52799303fdf96767ab3c4ef2bcfeca0b282
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:53:30 +0000

    admit step owe 567enemy information

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
