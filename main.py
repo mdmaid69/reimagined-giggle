f3608ba4ba4e662f2ec6807432e8fbdc516215cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:55:43 +0000

    image cover there total that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
