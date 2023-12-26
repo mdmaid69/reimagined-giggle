22a37c90685c0103e99ea75e33b884092a814e19
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:49:32 +0000

    free add hurry church especially

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def sort_list(lst):
+        return sorted(lst)
