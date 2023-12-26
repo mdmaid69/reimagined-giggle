cd0a02e25bc974b89c87a50e914afcf7ee0646de
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:28:29 +0000

    defeat voice emergency guess minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
