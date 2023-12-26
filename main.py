9ff05505bfc208fa1b7578d743b0eca87431cdfe
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:01:32 +0000

    full plan seat laugh spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
