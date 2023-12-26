8e3b07b0b06fb307430e3026445e5ea811b0c9ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:16:58 +0000

    achieve bad sort fat before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
