c4ced32d5be196d80a7ac90d1cd9a56425c64f74
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:33:45 +0000

    label still copy steep pressure

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_pressure(force, area):
+        return force / area
