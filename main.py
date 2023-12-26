724dfcf4424914ed33683bc656aaf77018977a2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:15:33 +0000

    stand real possibility gooff meeting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
