c8dca29e249e16463854eaf2053a70dc1ffd11a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:37:03 +0000

    remember block congratulations call sound

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
