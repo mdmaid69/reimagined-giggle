62548a5f93c02013945110f6c8cc68dbdcd87dec
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:33:28 +0000

    declare like path minimum responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
