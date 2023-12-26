4d58ea3f4721951eb61babe34c3cd8de3f994f73
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:46:55 +0000

    until force smile be herself

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+print(sum(range(10)))
+def count_characters(sentence):
+        return len(sentence)
