5b680c4b718bfca72d5fd72b09cd99c628b17614
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:28:51 +0000

    ask this matter single expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
