f5f450f8f9477a629c3b47e81925ac4d4a623d8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:52:07 +0000

    mystery left scrap corner can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
