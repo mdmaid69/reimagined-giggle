20bc0298fa2fa211f1ce46a5587ce79b3dbdf669
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:49:49 +0000

    operation shock truth until pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def convert_to_octal(n):
+        return oct(n)
+def count_characters(sentence):
+        return len(sentence)
