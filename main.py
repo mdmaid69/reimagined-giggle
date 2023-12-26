1633f437c20d16322890a419259f720fe20ea8a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:08:19 +0000

    organize collection short into hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
+import math
+def calculate_tangent(x):
+        return math.tan(x)
