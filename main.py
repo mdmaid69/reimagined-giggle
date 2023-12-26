c0496ce1961953d62011ea91e8df37499b99aa74
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:09:37 +0000

    unite future left reason show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
