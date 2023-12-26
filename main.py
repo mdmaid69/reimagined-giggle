22b74f3169291535bfcade30c89a9d841f0ce4ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:56:27 +0000

    you shop fence switch normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
