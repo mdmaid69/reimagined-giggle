dceb97d48c73cada190d44a4c6e5773decc34d78
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:55:14 +0000

    tall goup basically when kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+print(os.getcwd())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
