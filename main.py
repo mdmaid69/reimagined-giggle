8f1726deab9c48734150f6ce96496beb613751e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:35:40 +0000

    yes where government hate coffee

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
