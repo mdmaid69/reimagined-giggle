6a9f3af48649feb9cf39f5d84e0ef1c62b466cb4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:15:38 +0000

    wide zero solve club side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
