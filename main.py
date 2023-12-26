ba3847839cef620eb2b7e0259d135d838f3526e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:55:25 +0000

    wash copy the deep hello

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+from collections import Counter
+print(Counter("hello world"))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
