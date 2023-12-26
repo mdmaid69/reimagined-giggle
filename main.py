bc652d17c20fe163d3dd519326e893f67b55ca20
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:00:14 +0000

    safe short slide try start

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
