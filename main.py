b970b71005fd387a6af3146444f6884a77292be3
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:16:15 +0000

    sound contact wipe emphasize beneath

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+print(math.pi)
+def calculate_work(force, distance):
+        return force * distance
