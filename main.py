8abf46e1b4c4598f8802f020a0db3e6bb2aa1d98
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:07:26 +0000

    watch experience argue wide book

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
