5a047087bd0146afa63ed6e76c8206d3fd71e6cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:54:19 +0000

    swallow arrangement minute correct save

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
