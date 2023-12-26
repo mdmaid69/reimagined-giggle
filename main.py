f4d2d164b4c1352055a3025657a8adacc135cd3a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:09:28 +0000

    lower arrive university exaggerate refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
