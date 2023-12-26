49aaee25adeaa2c54692ef8d04af07a02ba4e9f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:47:16 +0000

    herself free congratulate I anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
