8ef430ac5799e331d6f3edc06aa5ad8bcae90a46
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:21:59 +0000

    shoulder book operation reserve strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
