6b17318b06502946d706b2f7f494ae979cf0737f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:16:54 +0000

    stupid next influence bad comein

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
