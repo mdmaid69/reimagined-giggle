82a796f95ad19586da82e21cf42f0b0014a6e36a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:34:23 +0000

    sensitive issue scream unpleasant left

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
