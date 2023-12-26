412827ef02ddcbc20cd3db2ee073f3c564e3a4ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:35:39 +0000

    turnup home Ss command getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
