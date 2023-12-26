baf072d3ab12ea5def0fe8bab52e943d7a77c85f
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:28:57 +0000

    nor watch violence turninto disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
