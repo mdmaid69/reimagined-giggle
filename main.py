a0cbbd41e1a2e1ddc5661eb67d5ce5d4723f329a
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:19:06 +0000

    reliable sick glad office spend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
