4f3590ce34c8481d1bc22cc4cad6f87178a956c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:33:01 +0000

    thorough more thick anxious himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
