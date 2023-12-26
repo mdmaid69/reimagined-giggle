d7a10f3d4a7831351cc1ce2eb7ba95b92b78a9c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:29:27 +0000

    band people tired scale interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
