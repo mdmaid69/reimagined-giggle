b94469eb2209387ec8f902fcc0ba6e5c342c4454
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:27:58 +0000

    safety reduce row below Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
