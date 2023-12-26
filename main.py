882052c0c7f950ef07dfb24d94d8186d2b513342
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:53:53 +0000

    splash public juice bus explain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
