5f7b0b52101ea9d0c7816bd3a255cf75c5dea250
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:30:01 +0000

    shake mother agreement become neither

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
