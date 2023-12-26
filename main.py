213e9551fcda4f8990169d3ca17f00b5ffffafa9
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:37:00 +0000

    any anywayanyhow substance unless reasonable

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
