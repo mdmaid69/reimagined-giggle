89a1c41d2706a6f72d3a7a70a2803bd22d99cfbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:50:34 +0000

    pretty mean lend especially accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
