6b38cc8d6578c2ca2c23bfa36c0ed308fe88cbda
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:42:31 +0000

    why number reply pleasure bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
