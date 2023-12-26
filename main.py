cff20e336cc0e2ab76ef0403f0c69370379452ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:39 +0000

    forever no rough bringdown region

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_pressure(force, area):
+        return force / area
