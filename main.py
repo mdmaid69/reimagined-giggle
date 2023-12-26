a1e05895366dfee968d26448125dbe770e71d964
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:58:23 +0000

    land confusing tonight formal turnover

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import sys
+print(sys.version)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
