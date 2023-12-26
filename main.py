f3bdd4ff53af958918970a2660d58811adb2addb
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:57:18 +0000

    sound soft force forward relative

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
+import os
+print(os.getcwd())
