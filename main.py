3e32f80417f85eeddff9390b18402ca1eb580094
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:19:48 +0000

    change succeed goaway chance payment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import time
+def get_current_time():
+        return time.time()
