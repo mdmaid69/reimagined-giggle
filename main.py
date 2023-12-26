1d6c0776db97c7c1d7ae74d13e9182f47be9d457
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:38:27 +0000

    good shoulder song bringforward major

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
