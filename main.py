dcddc486ba1154651dac3b2f5d8cd627ac678798
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:21:12 +0000

    minute reduce feed free Yy

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def convert_unicode_to_array(unicode, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
-        a.fromunicode(unicode)
+        a.fromstring(string)
         return a
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
