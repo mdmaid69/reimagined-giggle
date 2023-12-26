7e7ac5cc3f617dbbd45c9a53e08c9d4b17851e44
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:13:54 +0000

    holiday below pollution bit shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
