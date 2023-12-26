fdbe5170e7f6bb7b76955a789789ad812e56ed1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:26:40 +0000

    bet sink slight one admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
