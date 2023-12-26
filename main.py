e20000823f644ed0850d48c7d63d2e9899bc526b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:49:07 +0000

    experiment accidentally base change turnoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
