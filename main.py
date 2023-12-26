7c39906779e8012de20c6cf0a3651de0134c652d
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:16:13 +0000

    impressive skin dry broken not

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+print(sum(range(10)))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
