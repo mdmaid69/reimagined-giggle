060313e0a3bc708246a49de21dbca4dfcf5c2ca5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:01:19 +0000

    belong influence benefit fasten whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
