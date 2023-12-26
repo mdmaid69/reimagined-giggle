d5f5952324bc171ed0a57e91ab74bd23b1487146
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:04:56 +0000

    right another brief century dick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-print(math.pi)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
