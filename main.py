b143bd8effd67a123d1a5eb44df19f5300058b41
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:14:05 +0000

    bringback Pp disagree develop force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
