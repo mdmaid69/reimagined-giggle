0413cec93f9c46364abc06cb015b0da6ab914aba
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:35:32 +0000

    press release afraid strike club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_list(array):
+        return list(array)
