902943df6b0546e5b9183cba1e8585e2a9e46cc0
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:58:48 +0000

    dead create scream silent bringback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
