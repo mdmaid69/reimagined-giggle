64abbf20c329497cefcffc005423773b6d54538a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:33:10 +0000

    spin fairly offer recommend spot

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
+import math
+def calculate_floor(x):
+        return math.floor(x)
