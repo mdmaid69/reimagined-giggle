619b82a0760942b8786790edfbd3941c7c09a745
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:02:17 +0000

    hand strict extra hang equal

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
