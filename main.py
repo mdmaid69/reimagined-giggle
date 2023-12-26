c80f3e0eee82ed4ced19972f6abef56722e4956e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:18:50 +0000

    cope should permission gather cover

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
