4680d05697d3ecc88bb1e9ab86c706f8fdb38a35
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:25:55 +0000

    measure air today false reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
