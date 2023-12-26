ae81d103e8280d000e9333ca6fc635eb21e66285
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:24:29 +0000

    board convenient sign big contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
