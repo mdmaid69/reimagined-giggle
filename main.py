2c22e0e10bfc462d9fbc5efbd7219669b846fc33
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:26:07 +0000

    entrance last recognize frighten beneath

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
