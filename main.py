4d65904b93289506a8cc8f7270b46cadf5b5db02
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:38:32 +0000

    model plan land safe pass

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area_circle(r):
+        return 3.14 * r**2
+for i in range(5):
+        print(i)
