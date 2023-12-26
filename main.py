bccaa2ed44b174d059b25cb52994096236027118
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:36:47 +0000

    description accommodation high allow visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
