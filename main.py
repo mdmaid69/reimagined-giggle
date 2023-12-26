8f3fd8f773e05c7189761778c2236a21e718639c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:47:05 +0000

    pleasant cross flood road actually

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
