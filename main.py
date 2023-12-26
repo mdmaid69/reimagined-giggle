7b79aac6fa1eefbf719ba299123660f29b5deb62
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:26:40 +0000

    judge present moon lose worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
