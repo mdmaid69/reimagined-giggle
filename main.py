38b4ffd1ada2fedba38d24cf37e56d4ed92bff8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:25:39 +0000

    save service empty nowhere bridge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_area(radius):
+        return 3.14 * radius * radius
