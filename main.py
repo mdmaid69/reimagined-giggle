3f8abe40d8b74aac08eb50e7dc764bbb22e7a2cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:51 +0000

    head cover circle open within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_typecode(array):
+        return array.typecode
