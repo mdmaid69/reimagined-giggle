36d715f314bd2848eaee55fdd6dddf53580de799
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:32:34 +0000

    information song back everyone proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
