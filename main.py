bc0734604e6e9e05b038594aa9917042e14e23b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:13:02 +0000

    act squeeze deliver old obvious

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
