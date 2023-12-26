d72ee0123dc543cca9eab18aae73ba2b754aa8e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:47:41 +0000

    steam achievement confidence kick ourselves

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
