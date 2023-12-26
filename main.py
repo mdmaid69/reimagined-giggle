fad65e5b409037f89d7aa17a7de08884bbe787e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:18:32 +0000

    distance compete call sort field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
