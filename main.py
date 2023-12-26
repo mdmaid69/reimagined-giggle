ca48a063a419ca3946780224ebdc6aba0d5113a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:01:28 +0000

    refer support pile remark record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
