d436bea43b8ce0bb0f7753cf02ed01fc978d5eb6
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:07:04 +0000

    tell skin hurry tap surprise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_density(mass, volume):
-        return mass / volume
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+name = "Python"
+print("Hello,", name)
