caa9872bc24eb5245bc7b1884a68fba230e7ca73
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:09:41 +0000

    fun greeting convince beside test

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import time
+print(time.time())
