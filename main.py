912ad1e2b9870f56b4811907dd92ad07bdc6d520
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:28:37 +0000

    sad this over result less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def square_number(x):
-        return x**2
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
