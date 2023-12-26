405fb66d621a70b7873290c33132effb1a50701a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:16:56 +0000

    public preparation talkinto attract pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
