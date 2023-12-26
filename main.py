c531951a2399d5960f18098658d2ce09062679c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:15:08 +0000

    extremely anything sing peaceful after

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
