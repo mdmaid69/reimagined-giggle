cdbaf84bfb035052b41884ba27f9bd8176bf80e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:52:48 +0000

    on equal black explanation breathe

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
