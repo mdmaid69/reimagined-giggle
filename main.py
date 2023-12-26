9395bf9ded23e6bc5cc5c82a3f3d348e0fb5a50e
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:31:44 +0000

    his after school accept minor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
