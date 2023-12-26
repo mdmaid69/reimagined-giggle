afc65e614fec118380eb9dbe1be6318308927677
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:21 +0000

    whichever left nurse until remove

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
