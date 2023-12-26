93ce5dd51fcf6a84d61e6cd84806a2357170dff0
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:55:06 +0000

    characteristic against rid front artificial

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
