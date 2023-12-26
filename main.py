8e676e67cc1e8f3fa428e36a540c0a6d45fc4c88
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:29:41 +0000

    any post freedom stop condition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
