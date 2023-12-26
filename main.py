19901505922c874ef3609f4f35a90ec96f3c297d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:03:43 +0000

    show spring market trick offer

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
