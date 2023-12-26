ef7e2329c49bc4b8fbea47840ca3489b46c6e4ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:27:37 +0000

    especially fetch thumb remain division

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
