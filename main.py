56281699eebe0a7f3a47a073d412528fd73cef78
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:23:46 +0000

    takeover protest machine complete save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import sys
-  def get_python_version():
-        return sys.version
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
