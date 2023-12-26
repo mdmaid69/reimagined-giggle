b38f1613660b0bf4353d2cb9ee605229921cc471
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:44:12 +0000

    opposite you tip fetch calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import getpass
+def get_username():
+        return getpass.getuser()
