d99cc8bb639e14e93eb85b0a2e26885318bfe543
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:48:20 +0000

    break spray listenfor contact communication

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
