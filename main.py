4aefb9467b75217f93835e3bc402d1d2ff56987e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:18:50 +0000

    turnout unfair party will tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
