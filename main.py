87a9b5b89159337239ff96728aaf9768e2682e57
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:17:35 +0000

    welcome perhaps unhappy belong increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_speed(distance, time):
-        return distance / time
+def get_array_as_list(array):
+        return list(array)
