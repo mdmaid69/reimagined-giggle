85ff3ac6f9813e7a50e96105904f120d37fdba8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:29:45 +0000

    roof keepaway offend too wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
