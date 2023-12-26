c0ee1a96d3d3f64ec34921eee6cb2a45090666d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:58:32 +0000

    weather it father where healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
