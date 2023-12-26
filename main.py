4dabb1ae02600b011f543e4aa484ebc614d6c555
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:45:01 +0000

    branch professional sell suck hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
