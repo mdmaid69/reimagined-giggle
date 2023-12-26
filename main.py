13e3f64f54fa45c793367d5a7543e2381657962a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:28:19 +0000

    trouble trousers design switch dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
