8e6d68d9ce7a04a10f637774baa1768bc4d74efc
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:06:56 +0000

    nature unable lookback(on) early by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def reverse_list(lst):
-        return lst[::-1]
+def convert_to_octal(n):
+        return oct(n)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
