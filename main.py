0925f9741ec1c83ee131a5852e1e1d5c172f420d
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:29:54 +0000

    regular compete judge goon welcome

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
