273e596a8a36f25371fc16cfe3454aefa5e144d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:15:41 +0000

    reason burn holdon gentle comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
