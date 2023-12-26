8726db313400b4df1c30be4c99c2720228dcef6d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:12:00 +0000

    failure temperature minority butter person

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
