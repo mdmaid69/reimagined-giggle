24df43e0a731bdeb0057a0d3da3e49dc94802896
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:53:44 +0000

    hungry main office glad if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
