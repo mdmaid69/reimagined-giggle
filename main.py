7a281cffc7d6e0fb3fea229299718b9993294431
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:57:26 +0000

    us above repair unfair wise

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-print(math.pi)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
