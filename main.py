7ef6d943f8b62ad3038caf2aa5932106be6389c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:00:12 +0000

    record breakfast amount exam age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
