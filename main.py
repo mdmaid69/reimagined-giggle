50a49527593b8e3968906a9240a3390fdcc78b37
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:24:17 +0000

    certain crisis serve any contrast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
