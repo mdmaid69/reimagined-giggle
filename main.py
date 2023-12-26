c2f9ec33024859bf431ec867c1e46cffa1814f82
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:02:15 +0000

    fond him Rr soft interest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
