839d32a2d5b8892e4ad15d10fef70b8f05ed7b14
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:10:58 +0000

    attach swallow tear platform welcome

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import time
+print(time.time())
