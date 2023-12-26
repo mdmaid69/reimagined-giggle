e09207d82a47318c66ba57fc1e64f20d660bc654
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:04:13 +0000

    lesson break offend seat hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
