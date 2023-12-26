8a990a19640410ab66f0e7a30de88e4cc9dfd19b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:57:43 +0000

    job tongue opposite on wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
+        return bin(n)
