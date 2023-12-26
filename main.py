4bdbdca2b6f3a447e7d3230a6490617df01066de
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:37:02 +0000

    switch mysterious effort record ought

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
