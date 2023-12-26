c44f2fcdc1352bec9205270dd5b7e15dedd84aca
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:22:43 +0000

    direct splash explain never concentration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
