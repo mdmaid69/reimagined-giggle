3393e0faba1e2b329bb4226b1505ceada44e762c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:31:16 +0000

    remain disappear same blame limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
