b74eb05a301750e218eba81fca0884bfa5304052
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:12:18 +0000

    holiday likely advise acceptable really

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
