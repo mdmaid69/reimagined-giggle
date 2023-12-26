027a0db015f20dfb8796fc143c50702775567c05
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:23:09 +0000

    only cold nor combination repeat

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
