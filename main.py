832460b9aa9ef9198262191e6d9c6179cb195aee
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:57:18 +0000

    where wander purpose poison sale

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
