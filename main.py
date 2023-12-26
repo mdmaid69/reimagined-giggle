7b80761e6d4b8e85fb121441da9e91ea84aa9983
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:52:47 +0000

    represent its wide check last

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
