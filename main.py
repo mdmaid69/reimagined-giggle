69598b7e50cec5128b4ad099f319e851cc320597
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:21:32 +0000

    trouble boring regard slice plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
