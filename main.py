7e15a8ef5b029af8256e27d71f8a1d248ad72e1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:30:12 +0000

    attitude curious section touch unhappy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
