96d3e996dc2f411effb41335dfdf0e94f28336e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:12:02 +0000

    typical picture Ww swim away

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
