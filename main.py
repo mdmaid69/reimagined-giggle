7466943434d79d1679b65b0e70137426bdd6413a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:54:48 +0000

    afterwards past it shock bad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
