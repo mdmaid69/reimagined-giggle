6bb3379b672ca2684b505f2680bbf36276f7bc5b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:50:38 +0000

    already mean point feed among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_typecode(array):
+        return array.typecode
