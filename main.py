6b46f94b219a8c639383f993f3f49e0c4377085e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:40:15 +0000

    Gg through Tt ability jump

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_power(work, time):
+        return work / time
