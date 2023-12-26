a663bc1e466c2a908261c3a1352180d6744a033f
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:21:37 +0000

    thanks increase show apart block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
