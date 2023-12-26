eaf6e837a1fbb87131469d5be86728a25326d963
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:26:19 +0000

    platform address never free breakdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
