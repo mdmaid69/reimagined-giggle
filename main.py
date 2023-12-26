f08d71f69ee63b3349e172e31ddc6c1ff20012bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:47:21 +0000

    arrange kind mess uncertain cruelty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
