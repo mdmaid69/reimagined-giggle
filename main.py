a6e07149355b95e6bd630ca6df3a263aa7815459
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:40:21 +0000

    intelligent miss interrupt line possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
