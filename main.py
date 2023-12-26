05db70f63f975bf469959d4d65595077c27bf3d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:11:28 +0000

    normal refer opposite introduction news

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
