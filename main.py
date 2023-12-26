da880bcf33fc783a823c8089d70213debb50ec8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:39:16 +0000

    in fresh education plant ask

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
