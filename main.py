bef22d2dc36276538eea519adb155a7befb7d762
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:01:17 +0000

    still height calloff turnover turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
