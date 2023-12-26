8e1f6810c90f66f1879a907b2eab3e81d9d86e05
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:32:47 +0000

    may opposite offer sure cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
