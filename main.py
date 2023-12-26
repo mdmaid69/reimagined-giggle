24c29c3e34b040d44101033203a15fdc45ab9de0
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:02:09 +0000

    achievement behind moral finish fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_str(array):
+        return str(array)
