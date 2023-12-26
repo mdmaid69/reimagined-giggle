8275a57fef7e83e3a3127ab2467898c44133ec44
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:23:12 +0000

    himself division occur whisper excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
