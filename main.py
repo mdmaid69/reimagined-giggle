84b0e917a07f67c46def880026de65cc5503c7c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:11:12 +0000

    search alone fasten male favourite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(10): print(i)
