c7c22452b8b3fe3605938cc88d9da80c2f08341c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:35:30 +0000

    shame authority lend wind number

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-print([x**2 for x in range(10)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
