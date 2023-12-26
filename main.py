52de0ac092768feff610423f33d6a5681969ffb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:57:24 +0000

    sure rule careless she under

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
