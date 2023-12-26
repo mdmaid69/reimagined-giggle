0387aed5a42817e3d6192652423d3313a3d8a966
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:56:39 +0000

    legal breath arrangement ill rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def convert_to_hex(n):
+        return hex(n)
