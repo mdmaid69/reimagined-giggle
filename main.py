2ce251e70b49ff70aa5be72b37876f4267b64c2e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:16:52 +0000

    coat discuss copy public someone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_volume(length, width, height):
+        return length * width * height
