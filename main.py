d1cf2206afad0770f6188a8a5aec3a844cf2e81e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:12:10 +0000

    work strong excellent whoever odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def split_path(path):
+        return os.path.split(path)
