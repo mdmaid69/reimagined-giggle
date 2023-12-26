8fd8f8e327b5a058ee99ce9c3ef26c7554dec2d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:04:54 +0000

    thinkof soft book care your

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
