261cee133523cee9b7a34df0de2707cab2b3faa8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:50:30 +0000

    transparent somewhere original relief lucky

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
