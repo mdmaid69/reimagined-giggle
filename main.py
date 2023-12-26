7a35b5934f2f5975b44d333ba9bf47ccceda5e2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:28:40 +0000

    upset play spend forget risk

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
