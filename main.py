7dbfd494534672d3a463a23fa888b460a41e3d9b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:42:59 +0000

    narrow field way compete whole

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+from collections import Counter
+print(Counter("hello world"))
