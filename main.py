f56c4a65a9bf3d426a9dc01a171d2d5baa402b02
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:54:54 +0000

    excellent fire callback fire cough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def is_odd(n):
-        return n % 2 != 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
