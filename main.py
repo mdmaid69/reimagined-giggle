cea122043566c2bbc7eb685eb6973cbcd7767fdb
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:07:51 +0000

    stare independent people dry middle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
