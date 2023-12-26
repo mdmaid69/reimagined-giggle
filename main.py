95b9dac3dd473ac59a4b4ff76ef94c43b9e8ec51
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:17:57 +0000

    drop usually press protect freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
