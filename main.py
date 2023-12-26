b8cda12165bb77d09e56a728113bd3c1ea75abab
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:24:01 +0000

    give big pick fast failure

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def reverse_string(s):
-        return s[::-1]
+def calculate_work(force, distance):
+        return force * distance
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
