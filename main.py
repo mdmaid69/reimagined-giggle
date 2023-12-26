4675bdd50599af1f4e8627101098d1ba1e03f80c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:49:27 +0000

    stop concerned serve rush high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
