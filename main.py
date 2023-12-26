cb943156abff5b990e776e863f148fd7e072c070
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:20:18 +0000

    consult better thorough famous our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
