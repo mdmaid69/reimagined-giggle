dc12335890789a74974c4c06c337d7604c668a5b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:09:21 +0000

    provide your expression condition judge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def greet(name):
+        print(f"Hello, {name}!")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
