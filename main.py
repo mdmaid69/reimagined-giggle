3ecde6eb125033e689f8ab8221cdf4899f189281
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:10:28 +0000

    available Ii improve cool height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
