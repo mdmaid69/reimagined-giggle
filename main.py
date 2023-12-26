5e077cb1e6d3c8be593a553f67937be3eb74b0d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:56:55 +0000

    direct refer upstairs image notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import platform
+def get_python_version():
+        return platform.python_version()
