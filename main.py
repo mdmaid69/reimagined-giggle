cbe4047b0a0a68984161c8ad40e44c0f8a188e79
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:38:59 +0000

    comment pride taste use destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
