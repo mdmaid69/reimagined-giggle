2bd06936ded527e080e10921934d2b14d237e8b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:07:09 +0000

    copy public rub mistake themselves

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def append_to_array(array, item):
+        array.append(item)
