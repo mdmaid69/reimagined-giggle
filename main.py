946fcdf0a6a84595599fa5ffbd1bbcb96894be9e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:40:56 +0000

    call difficult likely post dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
