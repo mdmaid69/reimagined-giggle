159d741a83d30684e1d00a87aa989b845a8155e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:09:10 +0000

    discuss Ww smile favour health

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
