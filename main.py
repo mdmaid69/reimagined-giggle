3316614e9a651ebb3ba11de8f6407411aa6d5c03
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:50:55 +0000

    fault over fasten air 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
