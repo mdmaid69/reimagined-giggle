ec2928e0cbff444143617712ad9bf2dc8c5328d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:12:30 +0000

    science few 572enter bet change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_current_directory():
+        return os.getcwd()
