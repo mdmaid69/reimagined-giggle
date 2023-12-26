4bc22281694204225fa01d2a182067b450da82df
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:56:00 +0000

    past number share exchange repair

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def find_max(numbers):
+        return max(numbers)
 import csv
-def write_csv_file(filename, data):
+def save_csv(data, filename):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
