ac7ac2fb753b990b6f8acf4a52a22e926f692c34
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:01 +0000

    under bus watch voice damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
