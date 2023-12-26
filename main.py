d9df53b747efbd2a81c0e44e30aaab70ac98de24
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:25:03 +0000

    drawing poor 564encourage argument whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
