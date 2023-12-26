4bbe8b8e4ac97b548b4145226de86ae39e213d49
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:58:07 +0000

    morning bottom yourself realize nothing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
