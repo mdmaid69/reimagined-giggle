deddb4b4cc4958611ab8965c7b910964c64a3425
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:34:43 +0000

    beyond clean herself careless bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
