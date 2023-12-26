d22a0521aff0341c3dfbd25ae48926269186b764
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:51:38 +0000

    official connected recognize correct truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
