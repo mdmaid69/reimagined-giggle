85725cad16e473b7c8e6a481a26fc24a3d5579a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:03 +0000

    speed spin godown his total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
