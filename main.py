beee31e563507842d4f0590b9eee699f50cf7b2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:34:54 +0000

    street deserve bush report truth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
