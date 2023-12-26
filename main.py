3962fc45c5ce0b6ba1cfca331e672b9c6be5672a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:02:22 +0000

    exhibition zero rarely pot half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def square_number(x):
+  def square_number(x):
         return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
