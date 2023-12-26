c7f270a76f1dc03e9d2fe13cc9431e8133054853
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:56:40 +0000

    spot spare breakdown without demonstrate

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
