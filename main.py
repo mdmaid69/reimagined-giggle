a898ffd8a083dcdabfaf82085c70402a93bd15ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:46:25 +0000

    action lead avoid pair tough

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
