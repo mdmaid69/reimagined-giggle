84006e265795f14f8ad08ebb5b591fe42a7b0a5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:00:18 +0000

    gap dig dead travel basically

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
