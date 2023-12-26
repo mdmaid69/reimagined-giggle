5d9743e61fcde1ab3421ff352c8c26def3e5b66a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:57:27 +0000

    worst pocket promise owe able

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
