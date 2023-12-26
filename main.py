6224363e600c6c001b03ed9e0795d15a4b51d74a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:19:00 +0000

    guilty along announce question then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def append_to_array(array, item):
+        array.append(item)
