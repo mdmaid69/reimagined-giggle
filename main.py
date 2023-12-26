f02472db7acb1442bb3556ced887157a39250207
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:19:50 +0000

    please dress related light better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
