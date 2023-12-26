cc8207d9739fe9326f5a099282f497dd3344481d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:20:20 +0000

    key authority number think let

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
