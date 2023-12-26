520d9916bc1eb9bdd3fce468a036eebdba21d3f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:04:59 +0000

    that justice lean Ss pipe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
