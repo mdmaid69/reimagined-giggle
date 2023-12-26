7043f071fafdb1b4fb99903cde13faa0d11c04e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:56:10 +0000

    drawing straight fight advice instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import time
+def get_current_time():
+        return time.ctime()
