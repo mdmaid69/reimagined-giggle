c5d8570016ada3fd0454486b804e6dd2d2e54faa
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:06:44 +0000

    gooff journey sense whichever drink

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
