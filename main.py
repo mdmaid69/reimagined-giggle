2f4a735931faa4a8bd1e0b0e4ced4fa0cb39c449
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:28:39 +0000

    criticism plastic doubt hit likely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
