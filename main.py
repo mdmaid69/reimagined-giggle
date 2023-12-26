75a844cbaa22ed3e878f07a71ef1994127bd9921
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:36:50 +0000

    treatment finally excuse female unable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
