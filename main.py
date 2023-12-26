a461d8a4b5e79bdbc5e6f160b33920f4f493ebdc
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:18:11 +0000

    love hole worse underneath heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
