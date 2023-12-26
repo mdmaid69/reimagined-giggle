83494b1727434a98da9a0004b0a0b4c2c43d937f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:49:14 +0000

    save squeeze criticize within problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(a, b):
+        return a + b
