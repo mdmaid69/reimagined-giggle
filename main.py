c0063933d62e18d8bdf51484f1aef36370a5cc58
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:35:47 +0000

    payment turnout everyone why distant

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
