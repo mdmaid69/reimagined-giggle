8f0683df1f361816ab7c6e3b60432d66c75d663b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:07 +0000

    direct immediate brave wealth dick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
