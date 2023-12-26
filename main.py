8080c9a5ee644fb8401cd0bb938a9041e36eefc0
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:12:56 +0000

    flood continue touch interest 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
