338ed0cb42437cec7f08d0ddcf369e966a9cff77
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:22:35 +0000

    pour range expression art complete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
