d23b34813c8ddf8d9be7f5f188eec6b859dbb8b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:03:02 +0000

    piece than hand certainly chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
