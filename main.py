2a7420ca205895ae2edffed5de9ee6cc68afa44b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:18:46 +0000

    machine cap development emphasize copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_string(array):
+        return array.tostring()
