559ff37ab9c9242d74eda274fb73f7bf89b50221
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:57:34 +0000

    family threaten system field behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
