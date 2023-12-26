4cadcd50934ba739f498e82ae11ba88763395345
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:53:31 +0000

    method attractive Rr anyone gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
