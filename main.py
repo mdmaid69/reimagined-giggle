29cc7511ae035857b9a782db702eb4954fd41668
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:12:15 +0000

    store attempt slice part direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_logarithm_base_e(x):
         return math.log(x)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
