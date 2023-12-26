9f89093073e7d552f53c9495e1fff0a8ecb06f0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:25:59 +0000

    loose plenty argue walk crowded

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
