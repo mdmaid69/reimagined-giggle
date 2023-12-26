f9e89ab64784bcde9a73f1ef972e9231d08c9f8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:04:24 +0000

    independent let Bb across talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
