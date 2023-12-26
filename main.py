ac9e81589ec69e0e7817cd9d2686b8207e76ee12
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:54:28 +0000

    how family round trap laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
