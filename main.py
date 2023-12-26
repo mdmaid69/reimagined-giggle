f4ff829093290b24a7a75873a9503a9373cc4e14
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:44 +0000

    dinner enthusiastic day deal cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
