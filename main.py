36ec1988fcbc3426301486ebdc6ffbfde4ec84d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:16:50 +0000

    positive exercise possibly as stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+print(os.getcwd())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
