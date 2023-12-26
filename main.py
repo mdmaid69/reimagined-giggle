ec1f2f9436ce4d79e6c991098a44a15659aad94c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:27:16 +0000

    innocent familiar sting enthusiastic meeting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
