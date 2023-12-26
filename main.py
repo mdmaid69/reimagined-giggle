1d9809c4edbe79249ed86ab127d09406a6316a78
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:37:54 +0000

    replace satisfactory immediately runover university

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
