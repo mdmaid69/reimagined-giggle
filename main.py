b49729cb8f27bddf80ca6b60411319ad74b4e51e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:42:46 +0000

    offence sharp always mood failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
