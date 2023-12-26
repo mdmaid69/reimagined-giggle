ff53afc0c10b9d6d26463c24f23ddde87fdcd295
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:15:00 +0000

    person loss damp search available

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_float(array):
+        return float(array[0])
