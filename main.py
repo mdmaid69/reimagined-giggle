e1c974ea0ba36a40b9c5d52189bb0123080f4d0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:51:52 +0000

    snow goback(to) reply still laugh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import random
+print(random.randint(0, 100))
