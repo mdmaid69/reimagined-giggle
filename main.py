4e3195290b9d7f9694712c397e2840398a9ed698
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:45:01 +0000

    pin relax thin race slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
