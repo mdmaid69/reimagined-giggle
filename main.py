cfd47daed86a426b8dd72df74b58270ec955246b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:56:33 +0000

    odd tomorrow recently notice instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-x = 10
-y = 20
-print("Sum:", x + y)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
