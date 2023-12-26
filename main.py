09bf31cfc98384b56c79908dd2f43a01ec72795e
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:14:13 +0000

    broken chase onto group happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+print(time.time())
