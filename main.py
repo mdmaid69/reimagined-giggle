e49c5ccb4e8cfa3717e1b06438ca1b4430bc84ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:30:21 +0000

    godown understand disappear loose listenfor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_volume(length, width, height):
-        return length * width * height
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
