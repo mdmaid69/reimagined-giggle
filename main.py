ccb2a92ff56ce1abd60a4fbd785f1b1c62006a50
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:31:45 +0000

    pile comedown smile why never

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
