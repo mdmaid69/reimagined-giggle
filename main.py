70fa5fc640d5847da8bbc0417fde4b627b918ff8
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:07:47 +0000

    free here pity uniform beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def print_python_version():
+        print(sys.version)
