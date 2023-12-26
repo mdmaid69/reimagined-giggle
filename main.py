ab2a39444ceaad1747ebc4b28fd89692c797cc8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:49:30 +0000

    square stick cost rain once

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def calculate_area_rectangle(l, w):
+        return l * w
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
