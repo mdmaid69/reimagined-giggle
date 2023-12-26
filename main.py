aef62a422e46a30ee1c4223bc175c5d66ab4ad32
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:33:01 +0000

    travel contact also delight wish

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
