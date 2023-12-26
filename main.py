a923ce97c44fe83098fb87de00206d7e8f8a03fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:54:59 +0000

    war very loud centre afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
