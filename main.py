1198169e8c4bae511e7f7cd634af46f5f2cef606
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:23:30 +0000

    big manner spread news invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-name = "Python"
-print("Hello,", name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
