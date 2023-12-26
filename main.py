aacbc476be6e9400a395c96905f7ce8e49b710c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:11:51 +0000

    present body effect back real

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
