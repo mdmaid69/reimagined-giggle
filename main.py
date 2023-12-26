2d58532707a4d2cb79f9397472c02d273626d8e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:36:37 +0000

    rise breakin meaning impatient pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
