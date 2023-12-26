eea9c464faf62acee1a3ba4564a2e109588b7291
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:58:30 +0000

    freeze it discover raw close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
