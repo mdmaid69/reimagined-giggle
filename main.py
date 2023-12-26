24bc159051beb2262383cf193fb5e91cb4b65f23
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:02:17 +0000

    pattern plan grand carry have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
