2e0fc0df8a31636a5b37f06428cf4e2b00b65d28
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:15:05 +0000

    guard sense heavy lie bridge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_item(array, i):
+        return array[i]
+import time
+print(time.time())
