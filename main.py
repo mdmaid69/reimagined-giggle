22983d58bc0eb7288f622d815bc5fddde67ec6f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:03:49 +0000

    nurse train comfortable ride breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
