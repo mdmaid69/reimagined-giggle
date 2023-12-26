4a867f8884ccfbee97301839d484141c278c1270
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:41:51 +0000

    course shop before anything feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-for i in range(10): print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
