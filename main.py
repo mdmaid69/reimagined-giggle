92cbfd432acf488e9126f14b81f4b7c55d7b02f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:49:37 +0000

    cost it may refuse measure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
