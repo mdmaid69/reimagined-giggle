90c3cd55bc07660d627fa49d0a019dc7d9e2b100
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:32:01 +0000

    support medium completely react turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
