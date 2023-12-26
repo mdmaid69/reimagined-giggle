712f7633e7f63937da602231141f0f0988b5cf4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:36:03 +0000

    discussion thumb climb strange fond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
