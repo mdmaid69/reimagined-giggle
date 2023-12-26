d9e5fe7f821947c41468da5dd795b329c8d694cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:47:24 +0000

    promise likely move play deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
