0ce0200fe31452dcb3ef9beb8b887157a48c4047
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:36:15 +0000

    woman loud meaning friendly react

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_tangent(x):
+        return math.tan(x)
