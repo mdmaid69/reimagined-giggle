3931635e748cee0732689ae1b29e7d1020671d69
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:56:59 +0000

    ticket scene rush steady accordingto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
-print(random.randint(0, 100))
-def convert_to_octal(n):
-        return oct(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
