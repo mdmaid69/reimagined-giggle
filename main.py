74a1bd51a9561ef1e0c45a80a178b30caf4163c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:37:20 +0000

    pretty port over squash motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
