4b79fd1b1ca10b0d9b5bd37c02872126628f9272
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:49:49 +0000

    knowledge convinced reach emergency anxious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
