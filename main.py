29b98f0fad278e67239e008edcb9cad9371d9258
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:40:12 +0000

    game step far giveup instruction

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
