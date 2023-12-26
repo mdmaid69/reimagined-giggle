dbc133d38d8b3500d9266761e83feef392e9c22b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:57:37 +0000

    equipment space area land speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_power(work, time):
-        return work / time
+def find_unique_words(sentence):
+        return set(sentence.split())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
