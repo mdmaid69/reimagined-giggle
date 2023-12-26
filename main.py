58702a3d6e281e1354beb462510b9e8315913776
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:15:21 +0000

    shoot cost miss possession guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def add_numbers(x, y):
-        return x + y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
