7d19f9cf2584139e65243a7654a135651c1890fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:53:16 +0000

    limit poor crash without impressed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
