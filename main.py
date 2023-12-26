6d8b0e4997699648063126c0ee82b25038b96761
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:23:31 +0000

    deliberately quantity minority set common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
