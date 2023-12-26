069c56a58ed2b5b250edc1b406be98cad38c2899
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:18:55 +0000

    fear age neck passenger thing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
