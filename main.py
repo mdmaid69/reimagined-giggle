4ba332b8791775946ddf31333c56d784f788f60f
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:21:07 +0000

    contents land go announce threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
