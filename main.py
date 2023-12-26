6ff4c13a14c1d36c3ddaa112bb7abbeb0bf78803
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:38:55 +0000

    house scale blow storm extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def exit_program():
+        sys.exit()
