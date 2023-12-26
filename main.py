3e7f6080342f52be57b53f2e23a15682805dc4d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:32:30 +0000

    open inquire breakinto vanish opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
