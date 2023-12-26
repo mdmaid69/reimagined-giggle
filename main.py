0e4e57fbcb5f8123e18f86046d7c6f8e56246678
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:17:03 +0000

    male long discussion thin find

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def convert_to_binary(n):
+        return bin(n)
