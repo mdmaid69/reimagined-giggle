76645f6cc9eaeb74989a663beba96cf4a4c0f82e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:01:08 +0000

    important splash shop bored wherever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def find_max(lst):
+        return max(lst)
