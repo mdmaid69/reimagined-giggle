72ac337bdb5878c7f394ddf86cf5f8840d4181e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:36:22 +0000

    married damp fair dull up

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
