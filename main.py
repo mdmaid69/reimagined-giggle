b3535301e1ba7fcc0785742cb95affaa2b1e5275
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:42:01 +0000

    shoulder thinkof rescue proper wash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
