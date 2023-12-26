93d6345cbd1dec520de3a439d3b04ce49b7f7211
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:11:52 +0000

    proper limit himself various approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
+def convert_array_to_bytes(array):
         return array.tobytes()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
