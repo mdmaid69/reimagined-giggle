b58f4c6f67d036733caaca08d7a5422fe579e0e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:03:27 +0000

    her win equipment conversation each

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
