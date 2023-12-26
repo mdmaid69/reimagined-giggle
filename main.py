f8c15a4c30bb845354fa6379486c13800113b860
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:32:07 +0000

    glad accidentally mean wash not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
