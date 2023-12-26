0c6fc6739080a4b9099d6e21b31ff663052e93d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:31:23 +0000

    much find cover whole confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
