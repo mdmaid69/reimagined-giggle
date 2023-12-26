b5c8f1956650446665d8ee26f9bac227318bcd81
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:27:09 +0000

    love give inside deal whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
