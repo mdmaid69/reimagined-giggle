8171799b836ae85fe586c941ab25f9dee52255f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:34:51 +0000

    various pay turnon year though

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
