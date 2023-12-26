a5eec7ff73dddb5695b6624997963a6bd777323d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:41:02 +0000

    swim consist rule shock yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
