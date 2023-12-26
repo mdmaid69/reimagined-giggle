b710d58fb80caca3497eeed8b6de9ed87b54d971
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:43:09 +0000

    comeback beat extraordinary ours Bb

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
