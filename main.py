e92ade2308472500e5ce00989a025eee12247160
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:10:27 +0000

    worth appeal university project runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
