89aee1177735f5d92dd963e7f0cbfb127a29ee96
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:17:32 +0000

    collect occasion getaway couple live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
