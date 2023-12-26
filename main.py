3905bd42b87aaaa6f90a0cca20d5dcd7e7f52f41
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:57:45 +0000

    exercise hair damage become hang

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
