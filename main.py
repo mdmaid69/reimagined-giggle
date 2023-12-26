bce97d8e379c406f11c41eecaa2eb4f9bb8a4456
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:14:45 +0000

    invite stamp waste accordingto man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
