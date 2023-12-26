5ac4c1059a8c53cd4c4fc7d713620b6707f258a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:58:15 +0000

    born mysterious difficult yourself bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
