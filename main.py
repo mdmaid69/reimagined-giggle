98ffc12449af4093f6a9f69d96aae871a25aad06
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:24:54 +0000

    remove free love lookback(on) goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
