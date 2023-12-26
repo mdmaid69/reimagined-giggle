f11018a3cdacec6f19bd7e540e702937cf16f6a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:55:53 +0000

    police high car undo deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def convert_to_octal(n):
+        return oct(n)
