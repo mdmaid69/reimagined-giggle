36a7f1843190e4dfa207ba868855c064304432cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:11:04 +0000

    burn rate brain luck interesting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-  def is_odd(n):
-        return n % 2 != 0
+from collections import Counter
+print(Counter("hello world"))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
