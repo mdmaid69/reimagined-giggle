fa54d4df728ae17d31c24ea4e19cadfedecb7133
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:39:49 +0000

    before welcome lesson clothes still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
