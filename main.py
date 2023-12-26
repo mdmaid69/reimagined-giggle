9dc386f04a9fea05302455b32d570e3f8b3b1b0e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:02:26 +0000

    container thirsty attach talkinto fortunately

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
