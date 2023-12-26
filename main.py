cd6cbbd910abc56bfd3f8f6ba79ef1ed7b88e441
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:12:06 +0000

    measure usually carry correct read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
