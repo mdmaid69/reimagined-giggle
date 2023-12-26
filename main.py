bdd48c8c62890d225e0e835f2eb0536b6eb0426f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:47:14 +0000

    stiff thanks square profit hole

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
