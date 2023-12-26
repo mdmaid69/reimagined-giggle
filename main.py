a50deef4bdeab25b1ab3c77ddb6d8109409a0b37
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:17:42 +0000

    getaway pile boil wake yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def find_min(lst):
+        return min(lst)
