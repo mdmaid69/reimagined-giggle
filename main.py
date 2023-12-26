bb4f5227ab2914525dea0beaf7a68778aa35c085
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:37:15 +0000

    common content smart reply regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
