49461b46448ed360522b6a204aebd578c520d68f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:14:54 +0000

    Rr excuse squeeze would forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+from collections import Counter
+print(Counter("hello world"))
