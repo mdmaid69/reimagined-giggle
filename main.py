b81df64610ddd6cbc45c70df2d40c86cf436f615
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:41:11 +0000

    be morning software narrow abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
