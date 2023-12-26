5536e33b4fd4c8805173571ecde02ac62b5cf1f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:07:06 +0000

    also gas rid early alone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def find_min(lst):
+        return min(lst)
