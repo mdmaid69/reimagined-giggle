60e2a7e2d7150d81b09da3758aeba60f01cf4e04
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:40:16 +0000

    main float work problem will

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
