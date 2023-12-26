76d8a62d149161ee4a58e0c0e2eb0d1e158fe129
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:25:29 +0000

    affect deliver break something rent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pressure(force, area):
+        return force / area
