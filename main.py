e78990aa9d60aaee5721403cdf9668dd94e0544c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:44:40 +0000

    thinkof respect record question race

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
