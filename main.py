d3bcb3551bd54e824618590df2a5276799ad0625
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:18:23 +0000

    wide smooth suckme physical achieve

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def count_words(sentence):
+        return len(sentence.split())
