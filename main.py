19fc3ec13fa11f15ff64f73aac3f315e75103cc2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:39:49 +0000

    turninto reserve seriously past justice

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import re
-print(re.match("h.*o", "hello world"))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+for i in range(10): print(i)
