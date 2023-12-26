bbab9cf200e65a6c153182cf2b072f3e360b3b45
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:47:11 +0000

    level pin pour complicated your

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sys
+def exit_program():
+        sys.exit()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
