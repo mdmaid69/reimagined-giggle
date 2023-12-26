89f6ea0c8bb42e39ce7d82c8f881bc6511f3ec1a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:31:10 +0000

    flight previous unhappy conversation shock

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def subtract_numbers(x, y):
+        return x - y
