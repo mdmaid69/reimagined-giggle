8499a348bc690bf870ac80b95512fdb26c5a5f68
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:21:42 +0000

    cut company generally not mess

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+for i in range(5):
+        print(i)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
