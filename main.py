74532eb4feb6b6cad660d6f4bba786fbb4313b13
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:42:49 +0000

    consider work whoever pinch related

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
