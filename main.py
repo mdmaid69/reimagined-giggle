df13f6e3aefcbe52474bd0e20ad03f1c7267d447
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:57:12 +0000

    takeoff steep steal last workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_memoryview(array):
+        return memoryview(array)
