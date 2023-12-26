d7e5836290782c04b914de5feb5b52dd1ca3a6ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:03:44 +0000

    centimetre between string breakinto choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import os
+print(os.getcwd())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
