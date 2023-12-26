9acb53388d2bb39cff9560b1d9643a2c23a4914e
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:06:46 +0000

    underneath fill notice war stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
