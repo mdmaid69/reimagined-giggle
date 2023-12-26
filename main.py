ad82fabef0752494ec3245b4d280b2b61d203b41
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:54:30 +0000

    shape deal each hole shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_list_from_array(array):
+        return array.tolist()
