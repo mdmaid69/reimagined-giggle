3dde4de17217ef89b19524528c96277a02fa54c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:27:03 +0000

    sleep form breakthrough 571enough 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def count_elements(lst):
-        return len(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
