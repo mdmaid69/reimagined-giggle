517cc339c936c72a4403dd6e6d8393f87d6966d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:00:01 +0000

    hungry principle perhaps joke must

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
