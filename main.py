e8145ba87609fcc85b0df3723f7bf47a77585813
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:29:56 +0000

    impressed standard preparation bush trouble

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
