8ec1ee18f7b21f1c354019ede850137fe38daed2
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:06:51 +0000

    sting shout story upstairs damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
