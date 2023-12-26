dedc5e165e390da91c5927d3c339d60131a465f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:14:21 +0000

    mixture marry transport before pretty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_area(radius):
+        return 3.14 * radius * radius
