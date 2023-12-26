6779229ace7517be269f77bba6443e5309ab2def
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:38:09 +0000

    leave Itself interrupt step pretty

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
