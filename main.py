977ad97aae12a9546684de678b041c04ef7f5a23
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:25:41 +0000

    so then post strike rely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import random
+print(random.randint(0, 100))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
