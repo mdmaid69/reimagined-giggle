7ac591df0b7dec5d3fe99c126961d97946645598
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:10:38 +0000

    heavy interest scrap pair slide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_area_circle(r):
+        return 3.14 * r**2
