cf8edd57d7be33bede6cae2959c569e932bf4542
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:45:41 +0000

    float offer oil such effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
