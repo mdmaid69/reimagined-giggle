b3f202b35327a964aa25d32fdb4ce5ddf67a9315
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:37:26 +0000

    nervous Ww point rarely work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_index(array, item):
+        return array.index(item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
