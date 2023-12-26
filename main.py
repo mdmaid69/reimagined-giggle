d22300d8a69dc13c1beb1e5b1d34d3952f13bde3
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:36:47 +0000

    original wave ilovedicks great than

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
