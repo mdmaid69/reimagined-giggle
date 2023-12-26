6087eced7fa351821344c826399c80ca1d898067
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:18:26 +0000

    behave opinion steep address pause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
