a3d06fb7eade3d4c206b824519e0cde3f25ca9bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:01:51 +0000

    explanation refer bag knock dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
