0ecb08e19915f37cfb7a995275db0fc86a688e5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:32:08 +0000

    sail pin bright force buy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
