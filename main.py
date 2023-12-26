96c9d1686868a9532a9fe3cfc2c019ff6fe2354e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:45:27 +0000

    you total study the pool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
