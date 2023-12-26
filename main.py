106bf93b8d9ba6f8440b153459c64a527b2c8835
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:16:24 +0000

    spend music dig book remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
