05a617840b8686244cdeaba19fbf1d7f03410db2
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:51:57 +0000

    still anybody mysterious goon breakinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
