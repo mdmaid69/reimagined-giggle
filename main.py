3fc4840361c2b620976f9c42ad637d55b0b53a0d
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:14:53 +0000

    Hh disturbing choice address whom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
