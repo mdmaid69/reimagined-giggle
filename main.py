1d1d8d8d625124a0bc8746db20f32ef3e1d7d988
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:18:27 +0000

    around long sit obtain good

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
