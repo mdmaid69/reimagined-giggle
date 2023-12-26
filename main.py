785203414ccade9e1299d5fd9e89edcd12b2fc0d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:44:44 +0000

    party regular large refuse dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def sort_list(lst):
+        return sorted(lst)
