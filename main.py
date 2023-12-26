4d62d8e91c6f935921dcfa4931cb24d17006faf0
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:19:52 +0000

    mess autumn crash vote broad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
