36cf7e92aa5e756981ded933a0db405dc08035aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:23:07 +0000

    stiff mystery tie blame none

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
