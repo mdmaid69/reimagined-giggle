6575e4cba5a99a9798cfc0b8f9469dd5d9a735db
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:17:24 +0000

    fire blind lower grow getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
