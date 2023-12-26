5d79e4ca7baf6ecb0a5163cde2854333625633d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:55:34 +0000

    actual head to on violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
