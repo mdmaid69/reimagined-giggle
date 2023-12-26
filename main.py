6dcbabd348bfdd4d40a834eaef8176da104c5d5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:40:05 +0000

    fetch interview bus represent statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
