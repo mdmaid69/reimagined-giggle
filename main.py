78e0bb992b38a6f94ca9d4f0ec9981ac9b45e6d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:16:52 +0000

    detail minimum company forever disapprove

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
