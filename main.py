35744a49f78687a7650e89eff1026ba3654af0a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:01:00 +0000

    mine towards estimate ground uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
