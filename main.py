a50f850de73a9592825474a39103f505d09f14b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:17:56 +0000

    quit emphasize turnback like most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_bool(array):
+        return bool(array)
