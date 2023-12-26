fd1c50f3b62466c898063414653a2a4839071348
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:31:53 +0000

    children programme list previous conduct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_acceleration(speed, time):
+        return speed / time
