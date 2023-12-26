f05a9851bf7294f3e301cbaffded19af3f2dce9a
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:10:32 +0000

    yourself prefer fairly personal include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import platform
+def get_python_version():
+        return platform.python_version()
