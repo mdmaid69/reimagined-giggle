f69a8b8f2d9ecd60c4a6f0d07ed5713cf9320083
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:17:55 +0000

    gas advise guess secret around

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
