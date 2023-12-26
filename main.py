15fd3c31f3a5e33004434fc253fc14314ac5b6e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:38:17 +0000

    dick him mainly ground fair

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
