9cb7d969bf956f54995fdc59cc82f4c1f5a50141
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:47:10 +0000

    minute regional maintain whatever holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
