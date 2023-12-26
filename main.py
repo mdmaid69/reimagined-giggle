dfaa9e31d209f215c3cdc13b8fe50fd798af503c
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:50:44 +0000

    567enemy act rare callout tooth

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_typecode(array):
+        return array.typecode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
