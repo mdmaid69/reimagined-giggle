1a584158d7360ec74c676d65069f14bf1c5f5033
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:18:19 +0000

    emergency flood funny welcome priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 def is_palindrome(s):
         return s == s[::-1]
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
