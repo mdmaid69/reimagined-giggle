de4631f3094841b92332d2ced8c4285433239d5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:02:32 +0000

    question laugh person sweet holdup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
