c33eb2eb1eeacf0d609b914e1293b38c22a44a7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:13:33 +0000

    today occasional blame top statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
