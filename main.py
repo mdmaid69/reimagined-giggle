3e35470012de4700c9e3470ea75adf7ccc0cbca7
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:12:27 +0000

    bar forward drawing issue pipe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-print(math.pi)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
