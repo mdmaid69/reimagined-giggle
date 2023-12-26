9509868a157b7a8359f2b1d74d0150544bf3cf56
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:34:01 +0000

    rest convincing goup hospital score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
