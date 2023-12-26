dc5662277d2c51f171febb4b5359c6c0cc882b5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:34:15 +0000

    community activity heat cruel goaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_file(file_name):
+        os.remove(file_name)
