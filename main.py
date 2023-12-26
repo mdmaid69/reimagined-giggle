d6c51ff832fa42124246297bea00cbddb270e3d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:47:49 +0000

    design noisy easy bring fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
