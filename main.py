c338d51dc692b6aa4ebfa08370505d6c578ad0d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:16:58 +0000

    getup limit branch turnoff unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
