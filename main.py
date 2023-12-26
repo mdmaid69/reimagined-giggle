ccb53035c2e240993098ffe8c216cf01f9a4790d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:04:57 +0000

    use test refer normal statement

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
