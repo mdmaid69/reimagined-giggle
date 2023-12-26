8e2d71f1a843c4e486c2c4938c6a1554964625f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:51:37 +0000

    excellent Pp brother their dress

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
