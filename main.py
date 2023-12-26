99a58297a4c15f015774e689cb128d20d1b415d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:31:38 +0000

    behind regret branch yours whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_set(array):
+        return set(array)
+import platform
+def get_os_info():
+        return platform.uname()
