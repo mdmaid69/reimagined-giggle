791924bfcfc598985575d1b3212b5f9aa9834ac0
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:31:56 +0000

    describe nice careless reliable accommodation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-text = "Hello, world!"
-print("Characters:", len(text))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
