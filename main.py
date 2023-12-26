4efb8c6fc11be8ff4e12dd68e593c6552092c3c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:22:36 +0000

    niggaacademy acceptable fit artificial hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+def get_today_date():
+        return datetime.date.today()
