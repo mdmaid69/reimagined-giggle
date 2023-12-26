62c54d3fe6e94cfc0afe6c048fae55f8895f3d08
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:21:01 +0000

    instant Ss consider tone practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import array
+def get_array_typecode(array):
+        return array.typecode
