4e58dfe35f6162f31222bb3f265f387935bf1612
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:19:18 +0000

    corner step way claim thumb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import re
-print(re.match("h.*o", "hello world"))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
