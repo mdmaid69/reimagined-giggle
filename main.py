a558ddcec49642d75d1eafac0a16b4590cab838f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:53:39 +0000

    practice body soon left usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
