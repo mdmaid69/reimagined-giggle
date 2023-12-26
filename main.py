011c231e00df1154732d72352fb0afe2b83e9f23
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:35:02 +0000

    soak aim total remove Gg

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def find_max(lst):
-        return max(lst)
+  def is_even(n):
+        return n % 2 == 0
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
