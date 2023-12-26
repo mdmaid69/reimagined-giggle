8e44752be1d97cc889c2c4ede6c1d7b1867dbc5f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:10:14 +0000

    sticky world advise inquire 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_work(force, distance):
+        return force * distance
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_set(array):
+        return set(array)
