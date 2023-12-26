9232d7f0a47a4be38dd704b26033289f7f1d6839
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:13:12 +0000

    woman decide realize cap ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
