5d186efe903b9c1dbe5c231076a74cb3029abef1
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:44:06 +0000

    goup frame act whisper nonsense

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
