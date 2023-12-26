5bdbbd94188b3c05ab3c5f1ad4b1f4e21f38d0d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:26:51 +0000

    noise lookafter exact other down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
