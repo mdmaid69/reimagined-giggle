3e1956077ee139fefd28e823f66be10d973ceb1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:37:47 +0000

    reach single become wrap now

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_pressure(force, area):
-        return force / area
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
