f3dd9838d3b09bb170c7c1dc41ffe5ea8cc5ae77
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:22:27 +0000

    minimum shut negative estimate divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
