e6be06a208081bc90d81b1cf3afafbc8e28e620d
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:45:54 +0000

    against fall valuable deserve used

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
