31cc928775da2490e1f1092154c8f7279eac58f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:15:34 +0000

    human connected worth proof duty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_density(mass, volume):
+        return mass / volume
