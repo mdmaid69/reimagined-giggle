dca9adeefc26a6539486401766253fdedbfb1e36
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:12:57 +0000

    badly pour workon Uu callback

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
