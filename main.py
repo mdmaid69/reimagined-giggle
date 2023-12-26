830532dd7afb14d9236f4681fda9547ee180108b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:27:49 +0000

    match cry mess contact praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
