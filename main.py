30b6da283ceea779e6c03f1c76030242db0c994b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:02:06 +0000

    or quit squash community pass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def sort_list(lst):
-        return sorted(lst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
