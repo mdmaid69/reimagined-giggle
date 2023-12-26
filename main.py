9a40bc7e21da2ee3526cbb6f8c938fc5e8e7488e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:54:49 +0000

    borrow me completely over study

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_typecode(array):
+        return array.typecode
