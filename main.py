42e61a38faaf25a8a7fa1cf16e8b739e04935057
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:13:54 +0000

    sink short Vv line manage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def count_words(sentence):
+        return len(sentence.split())
