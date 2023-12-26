7edd1dcbf56ad33b771f446b29b8b50816062486
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:55:23 +0000

    thirsty interview rank true argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
