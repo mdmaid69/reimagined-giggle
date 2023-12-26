1a24e76e7eefedb530355595108f10cee40cd75a
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:36:26 +0000

    be calm point mistake mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
