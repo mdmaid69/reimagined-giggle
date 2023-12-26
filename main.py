2f310c307c528c8ff3959e5f5b137c6196e3b338
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:01:38 +0000

    survive breakin sweep herself confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_octal(n):
+        return oct(n)
