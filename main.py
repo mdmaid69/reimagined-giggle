32762a51ddd3f11a8cb4707f50068c7e307ca518
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:16:40 +0000

    scale lay main should moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
