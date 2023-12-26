8ada0a9c8ab51ef01f78dfa26f9be935f22b66b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:15:22 +0000

    separate hear division beautiful advanced

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import time
+print(time.time())
