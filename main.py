60cfe13c4c7291d3cb8d95c83615f7ff1547424d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:59:04 +0000

    acceptable behaviour sudden phone motion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def is_even(n):
+        return n % 2 == 0
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
