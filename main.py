392abea63dfa8601c22bec03c3cba5d12140dcd8
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:51:31 +0000

    poison reliable extra nice that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
