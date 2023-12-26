10ee2a633a9204ba250bfbeb9ed2c99809dec740
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:09:27 +0000

    squash afternoon valuable cold deaf

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_list(array):
-        return list(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def count_elements(lst):
+        return len(lst)
