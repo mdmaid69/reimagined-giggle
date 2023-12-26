6720462fa97da596b63b72b2eb54cd564b3a08ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:16:01 +0000

    record affect father miss cross

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
