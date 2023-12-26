063849542b48a256b79657ca8aef61d88ab0ff26
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:02:41 +0000

    hope lonely marry wherever special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+def log_message(message):
+        logging.info(message)
