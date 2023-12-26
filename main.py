23889026e8f0c7832726f92df1cd40ca2960026e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:07:36 +0000

    free front ability promise bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
