77b83294d3668c112e9676e9a056192d357dc5a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:32:59 +0000

    message weekend doubt thin shame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
