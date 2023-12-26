0e85248073d9a5b9240594e8e0d37fc1dd42dbb5
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:07:58 +0000

    pin which human fierce passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
