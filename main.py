8464bade837a70a6539451328d9ae60f64e17123
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:09:27 +0000

    discovery feed remove certainly object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
