73b54aa862a082bb361bbb8436b549173b206318
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:54:20 +0000

    so dry pretend ability interfere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
