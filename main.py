3a0b5fa4bfe4d6688f11bba51aa53781bf827715
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:06:05 +0000

    science 567enemy workon mean side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
