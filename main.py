fef05ebff1e33b6eca0cf8bf0e94a06cc4c84963
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:39:38 +0000

    kill comeback receive concern confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
