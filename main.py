8bca62db02dac464c73dd407ad6254ca3cad5d7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:58:39 +0000

    result listen giveback official block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
