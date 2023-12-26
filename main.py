206cfa5bcb58ba867318a424a1022bfda5c2a110
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:11:41 +0000

    town drawing chin patient read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
