371b3feecadf6466eb2fa8e3fec23015cd3971c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:09:13 +0000

    war relative willing pronounce factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
