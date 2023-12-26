2e8a32e3cb7e697dd373cf3ede7dc89d595653c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:59:02 +0000

    from announce ocean bringforward listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
