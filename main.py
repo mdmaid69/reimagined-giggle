94fdd783cf32a746abd0e6d8ac76777f52148072
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:07:52 +0000

    forgive hospital fear error everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
