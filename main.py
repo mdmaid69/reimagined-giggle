52044977f43631f2f4693adde98257c2d586ad3c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:55:06 +0000

    chance area advice offer shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-print(datetime.datetime.now())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
