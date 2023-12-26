5d3c5e853c845df34b3ef7f043654fe015d13808
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:25:30 +0000

    sink across offer nearly however

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-for i in range(5):
-        print(i)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
