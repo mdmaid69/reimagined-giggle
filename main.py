3c454880a1208e52880acb114d1a701ca5a07a08
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:43:29 +0000

    how police repeat pencil concentrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def split_path(path):
+        return os.path.split(path)
