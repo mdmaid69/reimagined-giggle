3050d08fac9385f11286142322bdc5d4f546456e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:16:53 +0000

    through patient they thanks prevent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  def is_odd(n):
+        return n % 2 != 0
