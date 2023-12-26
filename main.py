b9dad2bf51be9095ca45875a8ab12aef56640751
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:23:20 +0000

    community accidental interrupt shallow snow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def find_min(lst):
+        return min(lst)
