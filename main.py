f081f63104d5c8cf3c69ca65cd5583fcc5a08a13
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:10:39 +0000

    far ever adjective total basically

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
