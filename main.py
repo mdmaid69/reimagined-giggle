a150c3df3b481b959c25a457834dab214f61d253
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:10:55 +0000

    attach spring lower relation thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
