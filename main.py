4bf0042abe61db50036893c7750383d09adff2cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:23:32 +0000

    yourself consult length correction bite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
