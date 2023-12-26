db30c0345023c7827a682ca58c682fd479dfa478
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:25:49 +0000

    admire exactly goback(to) fortunate considering

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+print(sum(range(10)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
