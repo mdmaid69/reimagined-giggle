8bc4b51e7e20aee7616bf9155a79cfbe560ae502
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:19:03 +0000

    pleasant underneath tend shoot example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
