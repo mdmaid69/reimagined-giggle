9360b1f6a3db2f5ccadf7b7f43af25cd82531bbf
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:37:18 +0000

    sea skill damage identify back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
