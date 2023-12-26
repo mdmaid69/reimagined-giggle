3ad8f4d4ad8481ae78113fbd3a8268ebc65d4366
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:39:06 +0000

    basis stupid able as relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
