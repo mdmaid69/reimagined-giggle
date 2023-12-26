8e679c12de4fe8e30fbc6f504372746d661cc449
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:44:42 +0000

    proper plan land getoff mouth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
