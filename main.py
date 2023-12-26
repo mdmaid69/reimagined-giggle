079265371ebb31d1449d6e212c1e65c20439dfac
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:14:56 +0000

    plenty woman pinch kick transport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import sys
+def exit_program():
+        sys.exit()
