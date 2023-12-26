8ae9a9d6927605faa5eedcabe518e983bc853b8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:21:42 +0000

    grand disapprove slow shape unusual

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def is_odd(n):
-        return n % 2 != 0
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def is_even(n):
+        return n % 2 == 0
