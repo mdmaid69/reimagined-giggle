1320aa573d4b21bbd7ab38450a82446fd3ca9382
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:17:00 +0000

    official consider tidy fire account

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def find_min(numbers):
+        return min(numbers)
