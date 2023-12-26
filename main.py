97ed0c013c1b4077d613c902ab29f7a70be5e714
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:12:04 +0000

    frame centimetre single lock entrance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import collections
+def create_user_list():
+        return collections.UserList()
