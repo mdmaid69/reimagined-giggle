dd38eda37a0226398b215e59cd118e6773d7fa90
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:04:24 +0000

    hope once negative dangerous replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def change_working_directory(path):
-        os.chdir(path)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
