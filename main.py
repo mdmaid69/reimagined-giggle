d0ae77f5b49d7f9f6745662c810324fe0715a776
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:15:16 +0000

    afterwards keepaway organization contain passage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
