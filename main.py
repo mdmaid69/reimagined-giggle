e1588ee18195e6996ce4b2096a327a60f47e675e
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:44:09 +0000

    inform dark somewhere handle sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
