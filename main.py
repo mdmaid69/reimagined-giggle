15074030200faa51f026e6dd6c76172b094d1a71
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:22:18 +0000

    slight brother switch list tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
