26bd71e21337e10a43ea31ad942af8f7ff39da40
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:55:09 +0000

    criticize sort foreign rubbish now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import numpy as np
+print(np.array([1, 2, 3]))
