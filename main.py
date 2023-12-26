96771c5ef5244cb2dd0caf9666a77d9818651cce
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:46:16 +0000

    worst job basic circumstance Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def is_even(n):
+        return n % 2 == 0
