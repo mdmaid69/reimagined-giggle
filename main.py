82281e7fe2c7c22eaac6acfa0a4d9a1ada57e592
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:04:52 +0000

    regularly people weekend goback(to) stay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import collections
-def create_counter():
-        return collections.Counter()
+import time
+print(time.time())
