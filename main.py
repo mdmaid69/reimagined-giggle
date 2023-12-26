63526b3912f1f10c3c1710fa1b4b3b9d863eaf9e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:23:00 +0000

    split declaration impression programme sea

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
