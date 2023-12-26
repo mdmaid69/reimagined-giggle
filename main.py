80f5b1f4ace0e7915378924afa2333e6447f91ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:50:31 +0000

    total use business share learn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
