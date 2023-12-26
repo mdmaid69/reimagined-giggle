92510da9ff0c7e351cb3dce120938b8636e6c399
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:58:23 +0000

    needle birth race funny noone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
