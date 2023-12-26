c125cae48b074910ab41badac8e71f334934ef9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:45:10 +0000

    anxious regularly key centre godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
