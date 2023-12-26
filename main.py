8343197dcbf7ca3a2bee04333ba5b22b72a765fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:13:24 +0000

    lead difference change excuse breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import time
-def get_time_since_epoch():
-        return time.time()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
