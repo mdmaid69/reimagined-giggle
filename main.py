af16813667fc9627635b51caec488f38d88479cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:13:00 +0000

    description cost loose empty somehow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-print(sum(range(10)))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
