acc805d7e5f8a644f4aefafa5b1ec11a0a3bf5f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:22:38 +0000

    each relationship light mine question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
