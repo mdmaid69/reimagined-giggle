144a91df5b27407c6fccf5c41eb5684294b0db15
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:54:38 +0000

    to possible convenient lead delight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_pressure(force, area):
+        return force / area
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
