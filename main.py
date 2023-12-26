6d8ec6d30f64410dbf8e3ca82a02d669ad4b74c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:28:58 +0000

    variety bottom sting wall sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
