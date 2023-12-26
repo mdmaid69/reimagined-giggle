66a2af73d2f5d1b90f049ce437850f5718bded27
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:19:52 +0000

    intelligent hold inside pick spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
