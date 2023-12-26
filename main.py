b5e737ee82b7cb8cafa344b62cab7c0319f58491
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:17:52 +0000

    measure badly market discovery somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
