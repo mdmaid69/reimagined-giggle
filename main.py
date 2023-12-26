5df5b826b70ac29686965d319690b9f1144fef6c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:08:35 +0000

    throw bet computer goaway include

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
