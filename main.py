aae4412f6dc25cee0eef45e066f6209ded641c26
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:40:39 +0000

    scream characteristic spend weigh wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
