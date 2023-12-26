44b0aeaeef3fa48679689a03816816ddbf95e247
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:13:03 +0000

    grateful advanced hole copy burn

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
