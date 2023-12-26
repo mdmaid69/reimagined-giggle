27e57897ab4262c0783b1f7cf913dcf878533b46
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:18 +0000

    pinch swallow conclusion have less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
