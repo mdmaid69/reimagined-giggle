ca0ab25c22902ab4235df44a9de948d49b3fe2c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:14:22 +0000

    steal squash everything fight meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
