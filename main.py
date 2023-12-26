d229e67218354cfbd2b946dd95093849262d7d4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:02:03 +0000

    problem reaction influence breakfast debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_speed(distance, time):
+        return distance / time
