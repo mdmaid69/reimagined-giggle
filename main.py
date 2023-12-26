3d1385eee2896af754660d8f1791d93f2de486fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:50:26 +0000

    sport relax thick circumstance collect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-for i in range(5):
-        print(i)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
