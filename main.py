919ae9285ac7ceeedcb2998293596eaa0be9043f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:08:03 +0000

    limit handle art minimum preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
