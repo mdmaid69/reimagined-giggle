5fc18a360382dacf7118bee5c206baf66183c6ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:24:07 +0000

    load old any Yy 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
