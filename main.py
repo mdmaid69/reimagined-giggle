16378f30eb8520aeb67d10282fa1b034a03e1ef3
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:15:00 +0000

    storm goodbye shout near sign

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
