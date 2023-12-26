a8d18b461d4bb6dde9facdd66bba6d2157c81e75
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:26:33 +0000

    shrink delight slightly great top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
