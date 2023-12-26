ebe71b11437a05df5bb47f81f02d171179d95729
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:19:24 +0000

    sale watch grand fat may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import random
-print(random.randint(0, 100))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+print(sum(range(10)))
