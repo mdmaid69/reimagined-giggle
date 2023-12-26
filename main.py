b0844fa48434b3ef0fc514a6547f9bb7964e3105
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:38:00 +0000

    list adjective pen wave float

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
