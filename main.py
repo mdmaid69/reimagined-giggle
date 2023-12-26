f9856b7e68e425ae77f7606df141c84606d2773a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:47:25 +0000

    challenge design ask whatever plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
