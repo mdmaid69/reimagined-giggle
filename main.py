46604a5ab0bdef81ecdd504a00e65314f8eb793c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:16:08 +0000

    judge service post next mention

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
