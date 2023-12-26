1c80b4dc09881d50bd0809f61687e96f27b66358
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:28:33 +0000

    connected murder environment mess die

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+for i in range(10): print(i)
