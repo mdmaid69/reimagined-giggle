6a04bcc4aab389f66540d8b5eea847599725b524
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:36:06 +0000

    turnoff prize worried answer need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
