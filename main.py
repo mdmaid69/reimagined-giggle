533c64b55c57672c60b89906fe6ec253ab525a00
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:27:43 +0000

    early right turnover awake satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
