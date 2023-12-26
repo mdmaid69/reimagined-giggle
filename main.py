a0fc6d31abb90a44e9272148078e25c0126d04e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:56:30 +0000

    pleasant rest road concentrate again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def get_array_length(array):
+        return len(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
