75a7b0f378cba766274ee43f8ee639ac30b079b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:54:18 +0000

    estimate experiment independent may remain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
