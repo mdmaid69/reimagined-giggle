39bad056c14811ff667f005d15144e7ddc8d7f2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:41:43 +0000

    central touch ignore computer sure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import tensorflow as tf
+print(tf.__version__)
