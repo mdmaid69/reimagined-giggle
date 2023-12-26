0df812fc6ec15fe8b4c474b597c8422223386ef2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:23:58 +0000

    children giveout 569enjoy birthday Ll

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_sine(x):
+        return math.sin(x)
