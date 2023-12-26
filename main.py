3e866b0694571b7c071420b6c44466fd19bae34b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:54 +0000

    getup means treatment read complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import math
 def calculate_circle_circumference(radius):
         return 2 * math.pi * radius
-def count_words(sentence):
-        return len(sentence.split())
