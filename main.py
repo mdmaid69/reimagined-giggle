1c3ff3c841076709b0071bbcb4eaee3bb970c2ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:36:56 +0000

    suck behind limit protect exam

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
