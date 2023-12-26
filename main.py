4ced5944bc8578949e182363a55f89c56f8aad98
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:48:38 +0000

    visit disturb hand control account

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
