e5cc7cdc7e8ccbfc100a4bfcba8f2e3749eda3fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:20:27 +0000

    often often score aim battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_current_directory():
+        return os.getcwd()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
