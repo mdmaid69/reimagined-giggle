27abb5ac89e4606ac2fa9f4a6ef694153ba9ed30
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:07:32 +0000

    regional eager maintain getout hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
