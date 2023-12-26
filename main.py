e5831ffda16e9d2dfc8a69ad962063febfa4465e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:53:44 +0000

    cause front tune radio risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def create_priority_queue():
+        return collections.deque()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
