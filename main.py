1f2c7456b002bc17db876837c6baa006ec3db178
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:21:05 +0000

    bite doubt table stupid dick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
+import collections
+def create_stack():
+        return collections.deque()
