cbc0fd14039c3c2b4b6dcb5a99914644631e42ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:33:24 +0000

    both sleep exchange fall comeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_itemsize(array):
+        return array.itemsize
