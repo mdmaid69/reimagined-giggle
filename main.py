1c52d409d7e6bc57af6b3ff3c671afc082e67f9a
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:26:06 +0000

    communicate pocket blind avoid any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
