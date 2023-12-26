2b91f6434919f80f606914f898b864dea15c613f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:10:41 +0000

    danger separate science cause rest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
