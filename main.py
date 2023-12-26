94631bda200bf8841e7f3eed523bb040333a6122
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:24:21 +0000

    realize order hurt workupto mail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def is_odd(n):
+        return n % 2 != 0
