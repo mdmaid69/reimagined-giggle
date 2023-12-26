393bfeb728576b676ce7e16b539a19041e6e2319
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:31:03 +0000

    search motion valuable hard apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def find_min(numbers):
+        return min(numbers)
