0d0891097c66c9c41feafeda724032138c4d2e83
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:51:06 +0000

    last grade cheque turnout turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
