73a2d9ee97ced4e610ae9ac6f24e32f4ef7f18cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:02:42 +0000

    skill even pause meal tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def get_array_as_float(array):
+        return float(array[0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
