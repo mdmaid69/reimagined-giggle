728dcbf9319a8f9a7976c7f9b99d633d8dc50b69
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:53:20 +0000

    solve design before rough region

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
