2e202b5011689af5b31fb40f0a1f75ddb5f46309
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:57:32 +0000

    acceptable lookup characteristic breakin turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_typecode(array):
+        return array.typecode
