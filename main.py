5f6386e1f0842857c562630b1938abcabb1e3f70
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:34:59 +0000

    flat advantage conduct uncomfortable mark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
