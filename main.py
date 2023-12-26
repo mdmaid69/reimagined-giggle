ff7ec279fabc31d5aac3f05033545f61f0b1da62
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:42:25 +0000

    sleep wait tune love imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
