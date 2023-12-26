76a0e0e36c65e31660b4b10715bc3aff9a9e9ae2
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:31:58 +0000

    though capital punishment claim schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_str(array):
+        return str(array)
