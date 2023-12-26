e6b7be0ef94b086964253027f8d744b153437bb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:00:47 +0000

    string this little anybody value

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
