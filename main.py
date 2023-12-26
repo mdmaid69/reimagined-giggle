f8af68371cfe11214a97f016800135fb0aaf411f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:12:24 +0000

    recognize onto swimming press people

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-for i in range(10): print(i)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_work(force, distance):
+        return force * distance
