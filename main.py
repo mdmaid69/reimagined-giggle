85e7ac584f3d7f61bc21499a0c117a02de51c07f
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:07:58 +0000

    pool town pot free you

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
