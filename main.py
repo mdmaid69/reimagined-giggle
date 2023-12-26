fa00308e0e6cf4379633df8447eb374e4f796c41
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:18:02 +0000

    punishment another benefit gate myself

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
