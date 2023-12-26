f994e09c080b615363dcd84d375241a147405e38
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:02:19 +0000

    trade interview life mark whenever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
