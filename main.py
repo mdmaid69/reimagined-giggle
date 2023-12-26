016ae9fcd37918d57420950576f4f5489a3bcda4
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:05:56 +0000

    giveup their noisy door throughout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
