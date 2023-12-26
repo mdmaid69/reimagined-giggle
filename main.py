652775cbbcfd12331b653e3fcbdc52a51898a6b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:23:08 +0000

    point race tidy fall or

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
