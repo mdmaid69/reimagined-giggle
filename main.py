bd2e92a037b74b1e3755dbd6935ec29126b98e24
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:11:10 +0000

    violent necessary basically another breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
