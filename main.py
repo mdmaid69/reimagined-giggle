9d23a8ef0560ae5134a4043387e9dfee68aedb52
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:19:37 +0000

    by roll sell fork city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
