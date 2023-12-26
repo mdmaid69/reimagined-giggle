f0ffeb9eacf248be6ac3ffc99573a389adb87cfe
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:37:18 +0000

    strange do tradition change afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
