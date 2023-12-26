e2a303fa15a2fe1e34286acf0a3ad88884ad73ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:03:11 +0000

    day judge car body permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
   def join_paths(path1, path2):
         return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
