fe50c2efdf522748febaabf13312f56eb38ba375
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:32:45 +0000

    hurt how bored the anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
