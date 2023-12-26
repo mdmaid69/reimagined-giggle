191512d6bd5d7f29ed45023c12b763357495ed4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:19:23 +0000

    match callat politics confusing lunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
