a94b14356231166d0ea400f9d1f19046760b76b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:05:13 +0000

    eat trial advantage easy hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
