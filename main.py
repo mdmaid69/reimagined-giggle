ba6aac8ec72d3757590ee99b5e5c73cab4bb6724
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:03:19 +0000

    custom well reach frame flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def split_path(path):
+        return os.path.split(path)
