ee519c4f7ca328536ec5718b3ac93ebb78bd6a28
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:48:51 +0000

    guide tip unite lesson long

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
