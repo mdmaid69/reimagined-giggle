dfd141301206f415b08795dda2a44fd74e3de94d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:13:58 +0000

    adjective dare autumn climb room

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def multiply_numbers(x, y):
+        return x * y
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
