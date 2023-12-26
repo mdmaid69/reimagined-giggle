24ca8b882d2b7bf9ec07d9763a4bdffab39fee38
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:47:18 +0000

    tidy together convince safe easy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def add_numbers(a, b):
+        return a + b
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
