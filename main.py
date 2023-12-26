cf2f8b5e991db38cbce5fc5ac1fa6a5a755e6c2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:34:47 +0000

    guilty crash serious unpleasant maintain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_power(work, time):
+        return work / time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
