80670dfc9c08fed6c528f55c4677b5fd1f9137f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:42:22 +0000

    narrow confuse moral woman pride

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_rectangle(l, w):
-        return l * w
+  def sort_list(lst):
+        return sorted(lst)
+import sys
+print(sys.version)
