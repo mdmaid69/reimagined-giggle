924997e9d578e892798f0a1dad51e954ee5a3f43
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:04:26 +0000

    office pot minimum moment Zz

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
