aaf4a9ecdd1470c8587d8d2a221cf58d7b4f6dd0
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:42:52 +0000

    promise ball attempt back emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
