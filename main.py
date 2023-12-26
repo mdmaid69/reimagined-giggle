da957b8136492cc7b9b6da66f477189706461b70
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:57:10 +0000

    confidence spot solve listenfor apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_area_circle(r):
+        return 3.14 * r**2
