886bd2945e21ee2485f74117a90b3d99c7f65466
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:17:31 +0000

    lead between increase all lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
