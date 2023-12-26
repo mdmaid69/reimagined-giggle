d4b5af208d9495041dadbb9ba1e4f6aaf97f8ee1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:28:47 +0000

    develop bill glad he talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_list(array):
+        return list(array)
