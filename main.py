478916c81561d95637a14dfb697d16c38f244ac2
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:14:36 +0000

    rubbish sell pleasant test next

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_work(force, distance):
-        return force * distance
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
