e8112e12a7804c3683cd84c4bcfae91324e1db59
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:30:05 +0000

    right petrol search although direct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def convert_to_hex(n):
+        return hex(n)
+for i in range(5):
+        print(i)
