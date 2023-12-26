421bf91df2e74806b222a485f011b3c314c3369b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:15:29 +0000

    warm fond holiday conduct story

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
