5e6389e342c230c4bc47d07572b922b4a1a28d7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:49:32 +0000

    we ticket bush against limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
