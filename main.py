5350d7e0472e264312c8d218e2bacc0361acc942
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:47:49 +0000

    exciting lonely improve read cough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
