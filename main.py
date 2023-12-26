7a0061e76158368a07cb9990a745601edf172480
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:14:07 +0000

    noise hit protect temperature string

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
