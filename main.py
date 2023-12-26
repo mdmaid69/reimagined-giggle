a0b0e21ae73badfda67054620f275d265e6c78b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:40:44 +0000

    escape miss hard we trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
