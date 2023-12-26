277fa64ee129926751f042d1fa1855e8cc4cae2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:53:42 +0000

    characteristic fix rich kill sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
