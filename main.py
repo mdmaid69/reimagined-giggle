04439f6e7bc22c3e843c59d26887ca720f2e84b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:48:13 +0000

    pause do division gentle passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
