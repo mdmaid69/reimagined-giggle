1a369af505f9ce3b93135b75c870264c2a24ca6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:53:44 +0000

    why hurry official direct couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_index(array, item):
+        return array.index(item)
