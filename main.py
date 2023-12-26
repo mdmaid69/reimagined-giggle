6056a96520b245aa8312062c6603c9fd69afc7c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:43:54 +0000

    dress clearly smooth holdout without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_current_directory():
-        return os.getcwd()
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
