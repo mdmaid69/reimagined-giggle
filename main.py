09c597bece4ca1a85069e7dfa8ab16f70fbec148
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:58:25 +0000

    introduce whole sit party at

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
