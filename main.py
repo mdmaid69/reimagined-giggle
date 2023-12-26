f2428fd9f8348411663f00ecea6bd8e96ea68c9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:56:48 +0000

    cheap have finally confuse hold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
