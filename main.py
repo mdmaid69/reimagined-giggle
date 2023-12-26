26568c4449aac647e5f2f2a90cf84b61e238135d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:59:24 +0000

    remain thick uncertain wild wait

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def get_array_as_complex(array):
+        return complex(array[0])
