a13723e731003c6ceb6ccc212e94c2c6d20b332e
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:13:01 +0000

    chance welcome statement geton demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
