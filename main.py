edca5087e7437eaf421962cff6e5144808085a37
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:29:15 +0000

    able window prepare giveout suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
