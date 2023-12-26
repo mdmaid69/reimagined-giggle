bb6416d1ad4c2b4c323bde96c4122ac814d5cdc2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:55:55 +0000

    through steer spoil second phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
