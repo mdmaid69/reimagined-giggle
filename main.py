46662c39b6a39ff4cf0a180eae3b41dfb188091c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:49:45 +0000

    town pity cheat invite its

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
