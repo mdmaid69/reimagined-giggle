041aa655f615bb6fc55f2c0b8dba27fcc0281b8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:54 +0000

    drink heart crazy increase search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
