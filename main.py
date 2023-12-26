abca05e9c2619e870c8a5a6dafd3ebb6599af7e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:33:03 +0000

    keepoff tired cover book want

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def cube_number(x):
+        return x**3
