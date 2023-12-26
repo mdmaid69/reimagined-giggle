89b138537254973854e22107dec838bfdd63aaf9
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:17:06 +0000

    either behind many mix damage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
