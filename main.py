5e37a88c595aec59e67511ee01a53e164a4c58ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:25:07 +0000

    theirs keepout Ii ability walk

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
