390d1fbec61e83b17c400563d6c80c69dbef3941
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:55:01 +0000

    serious organization main drop move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
