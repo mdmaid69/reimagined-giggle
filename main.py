bf1a86883e3d46927900b091c0563a05afcc6b4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:52:36 +0000

    point trust substance effort goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import sys
+def exit_program():
+        sys.exit()
