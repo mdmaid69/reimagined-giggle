75943784624eeffed221dfab299fd5a8707e9342
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:09:42 +0000

    announce frighten character save one

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-print(os.getcwd())
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def remove_duplicates(lst):
+        return list(set(lst))
