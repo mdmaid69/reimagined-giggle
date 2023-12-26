ce51f24113830e21f4929f084358b6ac9f03c701
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:22:05 +0000

    disgusting doubt software scale be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
