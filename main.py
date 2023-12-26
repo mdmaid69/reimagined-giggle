5dcbe263c17c5145bb330251e3abbf57aae1a4c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:11:56 +0000

    thing kill show introduction rough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
