62f26c20ca69c7a650bb4794466408159f299d05
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:14:45 +0000

    safe pronunciation on quit best

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
