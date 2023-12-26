d27f1b820093b6c0832cace5409585ec22be704b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:17:48 +0000

    laugh march become cold deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
