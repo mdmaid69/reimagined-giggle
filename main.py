87d8b03f214c1123522e45ecb28aeeb1c2a044f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:18:31 +0000

    smell attitude blow listen price

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
