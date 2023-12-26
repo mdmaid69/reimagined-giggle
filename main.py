bb0086f093b1f9db3f6dac7acc7b87d30eb5b8e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:03:49 +0000

    compete different anywayanyhow trip about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
