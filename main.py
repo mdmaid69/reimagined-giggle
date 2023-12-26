5e3277c9fe25926bdb5ce3f2e09508e75762ec18
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:59:45 +0000

    process awake none finish ball

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
