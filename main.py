0f8a1bad13cb7d3701cb7db8d9bb6dea952a9619
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:16:13 +0000

    block holdout month difficulty brown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def remove_duplicates(lst):
+        return list(set(lst))
