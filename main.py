59a98bf208a6ea6c432c3cdbd246847a82620ce2
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:49:11 +0000

    priority anyone combine wrap bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
