16b40d7298190da9088948d19bece78fd26e09d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:45:58 +0000

    speed variety alive with strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import collections
-def create_user_list():
-        return collections.UserList()
+  def square_number(x):
+        return x**2
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
