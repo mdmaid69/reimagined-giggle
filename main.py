ca750a70bcadd6dacbcde29a02dc482d84855a98
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:25:26 +0000

    day OKorokay me comfortable roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
