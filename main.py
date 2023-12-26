df171e139045633161ac6fa05907faca13987809
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:40:31 +0000

    spray needle add belief takeaway

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(5):
+        print(i)
