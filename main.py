27dca9c09e4469d728e703d6da74f7ca36d1e93b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:43:35 +0000

    yours start burst estimate pair

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import collections
+def create_priority_queue():
+        return collections.deque()
