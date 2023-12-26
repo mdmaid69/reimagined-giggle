4dc345ea00c37e3a06028738d9ded135d5e3129c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:09:52 +0000

    say emphasis unlikely average especially

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
