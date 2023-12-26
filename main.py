633dced11b1afd14627ca334110084d1ae575a76
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:33:02 +0000

    finally offence by especially phone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
