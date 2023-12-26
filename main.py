899b6e4742df5cd9c8c7054c25177ad35f0d3b5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:03:29 +0000

    help abroad fast difficulty film

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_typecode(array):
-        return array.typecode
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
