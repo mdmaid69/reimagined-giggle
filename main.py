eb6a726b478af34b2d589667a7ea606b929803b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:56:38 +0000

    opposite big poor smell body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
