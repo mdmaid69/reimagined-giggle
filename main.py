f8763456607ce9962ebd4029729f7e0f1909a40d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:05:35 +0000

    nose group will concentration everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import time
-print(time.time())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
