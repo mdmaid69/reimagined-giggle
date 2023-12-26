9da7c7bf8007a929c36b3d4ef914799fa261d449
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:00:56 +0000

    consist father fine save cool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def reverse_string(s):
+        return s[::-1]
