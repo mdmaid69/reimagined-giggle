4cbc53806255057b77c0a4c87355ce6e9ef27cdf
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:01:45 +0000

    nowhere pool where room bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
