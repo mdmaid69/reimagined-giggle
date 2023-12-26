da70de1eb0f349429235fbd698a9a2336d377dc2
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:57:51 +0000

    scare trust cope turnon arrange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
