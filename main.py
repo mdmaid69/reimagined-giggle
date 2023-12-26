b3405552527ad64ae8ca8fd79ce7587a31129ecc
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:11:21 +0000

    avoid combine arrange temperature taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
