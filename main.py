8701a84d3bfc5de2c282a42b542f7b1c7f1b0231
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:11:09 +0000

    private through because practise period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
