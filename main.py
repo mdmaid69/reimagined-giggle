63d940e1f3816b7a29ddc9290cc71190e25a1878
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:56:33 +0000

    in wash home train turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
