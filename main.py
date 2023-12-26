65bcacea50ed240e11d041f213e52cb71ba338fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:14:53 +0000

    switch right debt cut perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
