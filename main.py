745d4a8456cd08858e1f1edc4d81fafe989dc5e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:12:36 +0000

    protest score ring getaway rely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
