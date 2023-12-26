1f089238bab091e94e7d08a10dcbe6b2349e2523
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:19:46 +0000

    married comeround hardly something do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def find_min(lst):
-        return min(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import os
+print(os.getcwd())
