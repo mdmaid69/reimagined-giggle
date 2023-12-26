578c806389bd9ff5b735f3bae78e78934017f470
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:39:16 +0000

    squeeze on great true goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
