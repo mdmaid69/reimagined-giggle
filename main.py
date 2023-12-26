fa258d48c97ed6a027d96d0ac5514f0ddcd57c03
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:57:51 +0000

    Rr early sense interview apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def cube_number(x):
-        return x**3
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
