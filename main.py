0b2bf5632dd78ec5c43a325ef03403b4d28eb9c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:55:03 +0000

    pocket I cool wind height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
