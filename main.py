44d72e43ea8d4f1d8df96bf1bf7831ef461e8f8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:47:17 +0000

    question where rather attractive your

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
