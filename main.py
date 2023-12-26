0efe44ada8fd1e462d112fd6e1bdb6bcf05020c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:50:58 +0000

    under body sing them bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
