2b703e2be54ef06dde264d26e943e68d830527c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:44:55 +0000

    damage justice Zz entry close

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
