ad738524629a65ae6ffe6cdda9ac35c6eaa8c166
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:21:04 +0000

    trust but adjective turninto note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
