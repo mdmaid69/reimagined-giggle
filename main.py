8f3cd5e6e608b6b04b8a94fcf461e611197b2347
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:26:45 +0000

    person machine wake suggestion thinkover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def count_characters(sentence):
-        return len(sentence)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
