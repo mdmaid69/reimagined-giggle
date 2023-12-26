f945cacfd29417efc84386f3ab92e9a018697775
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:01:48 +0000

    turnoff formal mouth nerve giveup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-import sys
-def print_python_version():
-        return sys.version
+print([x**2 for x in range(10)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
