fe3d1606a16e2408560f4312ad1366d994d2cd03
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:43:30 +0000

    decide road none manage violence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import time
+def get_current_time():
+        return time.time()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
