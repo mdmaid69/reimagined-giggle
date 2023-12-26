e38e880c8f0456d3fe9a93c457fffc1fc7c36b30
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:18:06 +0000

    but likely truth certain gather

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
