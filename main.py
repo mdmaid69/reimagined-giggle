76eda90631e49347ef64eb7c813b7a28c1920106
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:51:54 +0000

    suddenly steep record price completely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
