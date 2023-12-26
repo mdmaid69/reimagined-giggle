7e7ff15c06e604a6addbb231dd48118b3be79d65
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:15:04 +0000

    of mess label know sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_item(array, i):
+        return array[i]
