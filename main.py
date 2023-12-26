0548cdbedbde779475d5e5f4e99242d06032b528
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:50:13 +0000

    fill unable issue separate frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
