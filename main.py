a1314d462d390f8fcb6cbfdde63dd3b6b568d63f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:56:56 +0000

    either adjective workupto present somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
