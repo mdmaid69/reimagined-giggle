b8fdbdf859c34122e7902a37e4bb3c667730f455
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:38:30 +0000

    beginning round goback(to) turninto possibility

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
