78872d9b84ae67b4391576ae4225e841eea98cff
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:17:35 +0000

    allow agreement quantity frighten bottom

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_environment_variable(var):
+        return os.getenv(var)
