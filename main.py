422f0a96d0aad9ce588b609ee8389bb6ca7eaa6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:03:12 +0000

    minute mine movement remain then

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import os
+def remove_directory(path):
+        os.rmdir(path)
