ea57d592c62a8da2fabb574d4803863acf48cd3c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:31:38 +0000

    snow rare forward complete round

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def is_even(n):
+        return n % 2 == 0
+def convert_to_hex(n):
+        return hex(n)
