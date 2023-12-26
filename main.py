e92d237cd1fcf941d21889f9dfc57e2b403cc353
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:09:51 +0000

    sight pool fetch phone consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
