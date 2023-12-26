bc8dbf41d6e0424539e14e852df2fac3b6402caf
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:17:24 +0000

    deserve anything no likely turn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print(sum(range(10)))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
