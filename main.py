b59df901e13ec2fe9a994a85b543e05d07d01ef4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:00:14 +0000

    term off style nowhere right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
