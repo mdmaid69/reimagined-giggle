099090881c49b4bc938f50496ad8fa9110e5fcb5
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:04:48 +0000

    communication present Gg pupil transport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
