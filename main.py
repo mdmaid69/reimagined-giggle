9dae9f806ef60506c005e31d3b168619a3633e7e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:40:05 +0000

    office waste happy male mend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
