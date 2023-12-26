70364454ddaef0e08a67515c2b9bda8aa1898e91
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:38:07 +0000

    guess face programme use rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
