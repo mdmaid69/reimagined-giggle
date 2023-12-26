9f5d0f15eabccf58f147c99a492118950acd95bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:23:38 +0000

    worst tone cold tread job

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
