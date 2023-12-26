7850ab65ee5332deca644903446608513f8eacb4
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:55:49 +0000

    mail film now fold authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
