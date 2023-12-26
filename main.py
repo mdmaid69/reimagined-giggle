7fc9348ff8476bceb4d0c827d8ba2e803e01ca95
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:01:33 +0000

    Rr actually mood wealth else

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def square_number(x):
+        return x**2
