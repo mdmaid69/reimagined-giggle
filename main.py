04eac01c7b643b06778391b2bbac59c232779ab8
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:33:17 +0000

    secret invitation cost towards difference

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_string_from_array(array):
+        return array.tobytes()
