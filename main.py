c0f665b300474fc46d1b78a73fb5c457429c4b52
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:07:09 +0000

    now visit port just crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
