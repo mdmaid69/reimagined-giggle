02795f9646a575173b865263dcd339792640f150
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:38:44 +0000

    bound grateful actually contact object

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def sort_list(lst):
+        return sorted(lst)
