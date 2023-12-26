85865b62a69062ef3924a16b918935bfde007a57
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:32:04 +0000

    breakout tongue ask mind object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
