69ffd15c04138ceb7cc6cb03bc86847b9648a81f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:48:07 +0000

    down thing keepaway towards stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
