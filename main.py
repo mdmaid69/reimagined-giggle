1fe709a11ce5fd07062c65f9fe2981a9ee69e9cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:36:50 +0000

    bunch principle schedule interest close

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
