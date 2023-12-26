5edbd0034c858a5b98910d1182f2d505b9fff731
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:59:23 +0000

    left cold understand bit along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def append_to_array(array, item):
+        array.append(item)
