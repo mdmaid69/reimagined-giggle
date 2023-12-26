d5d7f57c4793151c409f834185d1b9c54f08acaa
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:46:21 +0000

    class switch passage heat turnoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
