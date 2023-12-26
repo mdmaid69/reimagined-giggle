376c942f950c80e8ba26644fbfc6388f5963954c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:11:56 +0000

    remember last bang fresh experiment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
