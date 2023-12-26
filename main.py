0536c637f583724640b7b7c18cb96d39e4ad3e43
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:44:29 +0000

    division opportunity far tour turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
+  def is_even(n):
         return n % 2 == 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
