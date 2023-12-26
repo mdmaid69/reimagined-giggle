f4ab0c439370af9e6c2b37a56c6738c99b7be634
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:09:26 +0000

    work regional surprising conclusion look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
