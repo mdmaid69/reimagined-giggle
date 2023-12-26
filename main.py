3c1ca9a284b4d62586d29d39c3b6013b9c9abc5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:08:27 +0000

    account name goon throw form

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def convert_to_hex(n):
+        return hex(n)
