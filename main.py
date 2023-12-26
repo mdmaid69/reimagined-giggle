83b9aca970cbd581b8c4bca2a60fe8c933083c90
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:32:36 +0000

    convenient anything rarely law behave

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area(radius):
+        return 3.14 * radius * radius
