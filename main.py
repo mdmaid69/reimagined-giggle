64dbdd6e28f1d524d656ac7048d141f69118fd3a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:14:36 +0000

    explain tip unable behind fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
