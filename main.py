7db721b2bd5fc7f69d1f4a66e496d59e6801d0d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:15:23 +0000

    cum best steady hate admire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
