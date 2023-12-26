85b8838240d6bb8ac80de093fda1fec5c2f2fc9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:14:33 +0000

    ride snow store hit hit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_pressure(force, area):
-        return force / area
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
