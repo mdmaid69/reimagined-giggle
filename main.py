51e7bf878d231dc5f1bdbc8422d984aa7ea6169b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:51:23 +0000

    sink land pay wide there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
