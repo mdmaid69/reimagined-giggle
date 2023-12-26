13b058380d9d36bda0dfa2d6df3c61a80c1a8764
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:46:18 +0000

    deep shoulder material comefrom combination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
