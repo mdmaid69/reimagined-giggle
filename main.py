a0e08229f61f729201a1af89826e1c29a12a02e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:18:26 +0000

    disagree OKorokay comeout drive further

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
