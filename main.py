a8280a35363c353ec6f7d66fbbba244733f89877
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:29:25 +0000

    appeal achieve sink contrast blame

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
