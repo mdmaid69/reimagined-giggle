b379c301c3b5f9377fdd813496443031c20963e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:19:32 +0000

    black butter tour surprised practical

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_length(array):
+        return len(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
