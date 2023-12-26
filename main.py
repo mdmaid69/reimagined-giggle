5291165c0b570312bc21a0a4ea8983fe9a4f6b65
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:27:46 +0000

    there choose out rob failure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_speed(distance, time):
+        return distance / time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
