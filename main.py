f7ebb46f64b23c9fc8c45abcf0caf66552076943
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:53:19 +0000

    over meaning disapprove blow ready

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
