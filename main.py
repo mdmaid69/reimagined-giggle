77846444b95d09a22e41949ec9ef16f84f6c5e04
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:21:17 +0000

    correct rest school use telephone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def get_array_as_set(array):
+        return set(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
