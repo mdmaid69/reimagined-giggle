22ab45b4c61836968ea7169ff78e97cab5f6387d
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:40:19 +0000

    matter like popular sad curious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
