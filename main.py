f9419bbfb37139d99243360091e53305f51b3829
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:49:41 +0000

    work dream Yy keepup turn

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def find_min(lst):
-        return min(lst)
-for i in range(10): print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
