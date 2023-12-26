8201658ff4e9018a522caf5e38f352bedd608fa8
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:13:16 +0000

    urgent far lead wall knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
