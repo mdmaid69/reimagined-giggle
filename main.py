9e4bf0b3d2adbb596628bc4efed65628184df719
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:04:35 +0000

    turnback eager admit makeinto rescue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+print(sum(range(10)))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
