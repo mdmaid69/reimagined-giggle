1975ff89c9de677f983d37afc18257fd3f32f4a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:19:44 +0000

    tip but question answer feed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  def multiply_numbers(x, y):
-        return x * y
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
