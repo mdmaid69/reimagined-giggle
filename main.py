c07c0e81b3cf76a4e837031b9b583778e55dcdc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:08:09 +0000

    lose mine greeting theirs tone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import re
+print(re.match("h.*o", "hello world"))
