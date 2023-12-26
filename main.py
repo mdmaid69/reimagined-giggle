cad1cb92dc11f5218644024e9d445a24266129da
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:41:41 +0000

    hardly relation legal future relation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import collections
-def create_user_string():
-        return collections.UserString()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
