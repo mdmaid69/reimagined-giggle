39e0ff239529bf2ea24e0cb46e41df2e71301e03
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:24:30 +0000

    scheme motion eager dinner port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def split_path(path):
+        return os.path.split(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
