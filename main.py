69191db4645af3eef75db1063abfbf0da0f2373d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:15:27 +0000

    disturbing ask possibility well cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
