67e963432df63523e64b51e9d70b9ef234f44fcc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:50:28 +0000

    tie Nn protest difficult fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
