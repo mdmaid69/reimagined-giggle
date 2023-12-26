a2203edae7217f13d2b6b5f7dd65c0c3a8247bd8
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:13:33 +0000

    instant us lie hear and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
