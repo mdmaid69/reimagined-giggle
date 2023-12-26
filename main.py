558a1016dcad15702726620f6064d19effe52041
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:16:58 +0000

    walk angry dream Nn land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
