6199ca5aaf709d63997b84043f3da82e6062773b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:50:30 +0000

    correct once spill male finish

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
