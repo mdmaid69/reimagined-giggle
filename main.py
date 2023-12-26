08d7023234e02372eebaa741eb5594d011baf7eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:20:29 +0000

    pause flat direct result connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
