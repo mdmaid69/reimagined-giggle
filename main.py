27824f2ac954b78a2667f75d2d3d74514b147598
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:13:02 +0000

    spring arm address road comefrom

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import random
-print(random.randint(0, 100))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
