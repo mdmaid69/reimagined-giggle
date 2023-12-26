c71ba8bfcace4303f684620e7da2c924d1dac9f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:56:09 +0000

    soon difficult occur flow argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def divide_numbers(x, y):
+        return x / y
