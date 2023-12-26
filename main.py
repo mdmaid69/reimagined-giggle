f619dc8589975eccc8c91782929d785b83ae30e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:16:21 +0000

    birthday public face or chase

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import platform
+def get_python_version():
+        return platform.python_version()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
