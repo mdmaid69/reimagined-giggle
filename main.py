145cc8a9a222e289abc4d24b33636a18911a0ff7
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:44:42 +0000

    serve again interesting man by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def remove_duplicates(lst):
+        return list(set(lst))
