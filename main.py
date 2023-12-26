b9cdf6b5669d9aff208d7135ee0d49f0bdacd4b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:23:53 +0000

    any suddenly follow noone herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
