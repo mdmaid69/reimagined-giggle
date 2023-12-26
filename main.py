6dd9d607dfa4688f82eacf804b32c3b842df621c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:46:18 +0000

    certainly lie top extraordinary tooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
