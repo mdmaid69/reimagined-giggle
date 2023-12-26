69e43728f8e1255f96d59ad1d81b52644f3d6c8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:28:40 +0000

    block check waste grateful exact

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
