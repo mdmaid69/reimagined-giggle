417326ebdbe62813faeb0eae639ae07421a1d448
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:25:36 +0000

    key yesterday pair pot characteristic

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
