d8b364a5497af0bbc63a2c414cc9af53d3bf3951
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:45:46 +0000

    payment introduce badly downstairs effort

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def is_odd(n):
+        return n % 2 != 0
