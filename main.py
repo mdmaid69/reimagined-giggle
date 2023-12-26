4920e6898b9bee1464085821562f156d0e01d861
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:02:31 +0000

    factory drive workupto arrange accept

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def add_numbers(a, b):
+        return a + b
