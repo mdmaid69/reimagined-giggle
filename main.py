e57bb600e006baa250cd29fe55a303d0fa9699fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:06:59 +0000

    worth summer possession Tt system

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+print(time.time())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
