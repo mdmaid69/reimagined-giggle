7196a2b9966269a3c86e2b3ce495997853e296aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:00:32 +0000

    later 571enough mess meaning remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
