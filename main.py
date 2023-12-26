12eac9a817f748bab47bde12299e2c5414837660
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:33:54 +0000

    turninto actually freeze unfortunate needle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import sys
-def print_python_version():
-        return sys.version
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
