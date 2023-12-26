f6f380d189403e2f64beb40b8b77ccaa3b782571
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:33:52 +0000

    length impression bored cough office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
