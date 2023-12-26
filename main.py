7a44e4ff993cc892a17759ce9a7de73336e151de
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:12:08 +0000

    care independent square question phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
