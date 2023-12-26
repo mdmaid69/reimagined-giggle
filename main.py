93fc80c103042532c4fccfc13ff0f48b038cf303
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:24:49 +0000

    base turndown many combine slow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
