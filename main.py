2bf0883304746bd0584a8f6e03de5b3dc8c2aea3
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:22:01 +0000

    under kind turnon practical means

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
