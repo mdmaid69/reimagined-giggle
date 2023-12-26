f0e880fa0bac7d5854e1ec21c4b301dcca35fd3c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:15:04 +0000

    before emphasize share disappear frightened

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import random
-print(random.randint(0, 100))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
