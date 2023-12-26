9cdcbfa15b21867840bb084a06ece4ae48914f1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:34:25 +0000

    club force display water collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
