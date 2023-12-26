cb4dfb5392c0d8811ea95bbfa19321e1edf29646
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:58:00 +0000

    proud something somebody male taste

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
