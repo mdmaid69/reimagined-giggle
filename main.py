12733d5da4ca321971f643520cf702643b475043
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:34:35 +0000

    explanation save bang know stamp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
