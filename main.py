ccbf78ac50962285a8432892c65c611a1ae4b0d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:56:18 +0000

    sale switch shrink ordinary float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
