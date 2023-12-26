8b185ee7df5e4a69231fcbdb8a0e8803f4f80185
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:35:48 +0000

    risk bringback if apply judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import collections
+def create_stack():
+        return collections.deque()
+import numpy as np
+print(np.array([1, 2, 3]))
