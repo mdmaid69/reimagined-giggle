f617d6f7206edf0b0109b5f4dc31eb3107f403ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:49:46 +0000

    suit centre powerful order love

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
