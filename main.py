7d980d4b0cffa56960e23939a964623547bac452
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:52:32 +0000

    remain married cheque tidy noone

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
