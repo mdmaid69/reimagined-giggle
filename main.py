a74d2d120f552efa92326d5c2d83a07e0a6a0df4
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:54:33 +0000

    soak base means mean again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
