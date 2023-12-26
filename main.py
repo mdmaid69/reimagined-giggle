a39f7530f7de396f0e0661e53f1825611a2ac71b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:47:31 +0000

    scared excellent hear maybe lend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
