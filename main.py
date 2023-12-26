5364f0ac0ddbb723cb8f8d0f0611b67220888768
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:17:09 +0000

    feel search call table one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
