b41fe6fce670c4872df44021f8c8847d37a97639
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:27:39 +0000

    rich punishment suppose night impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
