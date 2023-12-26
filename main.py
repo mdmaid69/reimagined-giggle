26591c4e282629c2afa1aab1f0cafb12c54cb781
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:34:10 +0000

    describe miss stretch pleasant compare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
