7bf57bba696cfa3a46fef5bfd7257153ec8c21fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:51:20 +0000

    last already half our bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def find_min(numbers):
+        return min(numbers)
