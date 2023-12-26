7524a755ae300696d16691acb8631bb6953a189f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:49:24 +0000

    book born experience equal never

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
