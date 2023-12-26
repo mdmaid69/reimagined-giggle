a637e56c51503aebe14e8234328206bfe15ffcd0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:21:21 +0000

    tongue much hide smile dry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
