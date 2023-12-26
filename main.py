4fcb46785781b542e5ff983483b3514c091071f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:20:41 +0000

    fall very ashamed such later

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_density(mass, volume):
+        return mass / volume
