71458925ec78a5ae3bf9c0f064a01eb53d893548
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:02:00 +0000

    interview tidy complete pain passage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def sort_list(lst):
+        return sorted(lst)
