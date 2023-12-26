784cabaaf8f55fcff2775f73565ffb1f244c870e
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:00:01 +0000

    influence whistle plastic theirs convenient

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
