4bfad1e0c7dc21dc136515bae6319e300b1e9675
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:42:18 +0000

    if long cheque thumb black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
