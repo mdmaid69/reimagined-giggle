6ed0b0c2d94fd940c6a5dc7284b631885445ed11
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:21:10 +0000

    few give forgive miss make

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
