7eada75df036fec993d7b091d56be292073f6771
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:40:15 +0000

    picture soil force late around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def convert_to_octal(n):
-        return oct(n)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
