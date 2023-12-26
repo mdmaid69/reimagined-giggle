874db63d81332d8b90f07fb5e4c72431c0f116a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:52:43 +0000

    turndown cruel minimum curious decide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
