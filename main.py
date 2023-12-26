be47200152cdbb04fa86c7043fe7f21a62529598
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:13:51 +0000

    pleased force matter so determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
