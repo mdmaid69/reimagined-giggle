a4f54d5eabb9c00d5b3f8479bd735cf8bcb25b1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:33:02 +0000

    mistake of track expect free

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_list(array):
-        return list(array)
+def find_max(numbers):
+        return max(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
