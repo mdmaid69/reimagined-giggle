a30062bb5efcf3e733bc957cc2322a8da82ff54e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:30:30 +0000

    knock tell loud fence taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
