d4787c5d8beb2ad20fe75e344339b146e37e25ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:11:14 +0000

    float move manage none mess

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def add_numbers(x, y):
+        return x + y
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
