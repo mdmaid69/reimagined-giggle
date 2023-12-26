1c9e27d4ac64bf65c25a0cf11ad6b2a4d423f6dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:41:49 +0000

    model cruelty average walk smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
