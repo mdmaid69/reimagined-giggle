2d67d605cc9f529e0e736e62d94e80c57fadf2ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:04:06 +0000

    ball crash control cheat dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def convert_to_octal(n):
+        return oct(n)
