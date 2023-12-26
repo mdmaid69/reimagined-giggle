80897ea68ee60ab28d172e7c3703b5f8dcc118fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:53:29 +0000

    couple divide through plan afraid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
