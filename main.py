a22fc6fcf152bc839ea609b4f7fed65067883732
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:54:30 +0000

    thinkthrough without cough appeal inquire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def remove_duplicates(lst):
+        return list(set(lst))
