c07f74a29c8f837ea23fc42b35480eba567fd521
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:26:45 +0000

    spite hollow direct onto city

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
