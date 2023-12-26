b027d10e2046d6f3b96c7593210d8ac611b91d62
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:28:46 +0000

    discussion affect impatient worse introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
