7c496b26c30a7d2cf8b7726bf448500bab7c6acd
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:28:32 +0000

    dream clever official path government

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
