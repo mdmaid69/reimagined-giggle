5a5e9be5497cf84796296a535822062768ed68cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:10:34 +0000

    exam sure scare confusing heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
