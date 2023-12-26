a3bcf361a146d1ebd1dfd425479d95f5fca7ce5c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:24 +0000

    sensitive building male refuse circle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
