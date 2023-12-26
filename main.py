a5946c461040d84466f424b892b334ad5de308fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:31:45 +0000

    hear shape legal admit party

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
