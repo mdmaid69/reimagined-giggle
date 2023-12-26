8156dcc33e64ad2b5b4070fbf1cea178a3d241dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:45:30 +0000

    takeout theirs sing guess stone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def subtract_numbers(x, y):
+        return x - y
