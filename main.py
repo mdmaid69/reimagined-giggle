55a3669038579b5334004552438d8f78c6fbe5e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:34:30 +0000

    smooth wild free callback big

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
