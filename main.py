5285bff44a31d0a4429afabc9d75fc574c1da229
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:38:03 +0000

    spray importance ourselves average search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
