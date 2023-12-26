e4c0857177b07bd5a73adbbb1a3cdcb3b0e39aa4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:57:12 +0000

    later flat judge effort borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
