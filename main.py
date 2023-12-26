5f737040c335b4615e493af845a481e5b2ddc562
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:26:13 +0000

    kind copy estimate border shadow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
