b2a9524bf2254f9433fe396ee12e7f8035499558
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:56:04 +0000

    search day under fix level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
