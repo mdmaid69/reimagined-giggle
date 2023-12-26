bcf50f538e91818e57f6b4cd26092ce0310377b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:20:44 +0000

    responsible poison square excuse mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
   def delete_file(file_name):
         os.remove(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
