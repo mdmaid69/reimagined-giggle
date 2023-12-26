f721318a74dbe97a57c78c4c7401999616e124f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:27:04 +0000

    later Ii home breathe regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
