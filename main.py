8d944894eb2ec7bddf39d30bc11cbe2d89705d91
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:19:26 +0000

    pattern expect possible effort rest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import sys
+print(sys.version)
+def sort_list(lst):
+        return sorted(lst)
