4ba0109063ae4194cfd953a7cf40731cbcf96a70
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:49:09 +0000

    any Ee 565end zero main

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_power(work, time):
-        return work / time
+  def convert_to_binary(n):
+        return bin(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
