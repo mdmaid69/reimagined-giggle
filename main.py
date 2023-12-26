355519b9e4957c31292014d0161a23e6744b8358
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:30:35 +0000

    Yy hurt top smart manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import time
-print(time.time())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
