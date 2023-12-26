22d25c4e56b31cb29b2b7b85914c4fbabe25024f
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:49:31 +0000

    invite whole permission loss round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
