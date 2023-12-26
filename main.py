734c9ef7764cf91e88152e4fbacc71d5ed54643f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:07:47 +0000

    accept live further round official

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
