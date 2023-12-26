8fc02d4e49c84af325340c92d50ea72a3f1cfda9
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:39:29 +0000

    place touch more trust splash

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
