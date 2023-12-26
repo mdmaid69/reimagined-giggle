02df6e87b4821443eefe1cd9acccd703ad891d85
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:06:00 +0000

    off reply public mainly rain

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
