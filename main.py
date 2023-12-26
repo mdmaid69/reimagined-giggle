274704c299425283f8b1742a36a85a39dc0e33d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:46:15 +0000

    bringforward pot considering medium public

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
