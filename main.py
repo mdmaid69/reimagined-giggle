85d1f480aa52f91b6f26f8d8d72649be1c63bb6b
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:16:12 +0000

    grade related crash board vision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
