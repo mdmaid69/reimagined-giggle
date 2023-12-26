81984ad68644fcaf2694d3e7328efeb860f418e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:22:14 +0000

    except collection except wide job

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
