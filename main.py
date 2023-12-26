54c8ba5bb6084da8e2c59b0ba386d03a660357ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:08:43 +0000

    right collect whisper fence mind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def extend_array(array, iterable):
+        array.extend(iterable)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
