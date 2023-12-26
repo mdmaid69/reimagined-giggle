e36dd71219f9393873503113eda55d13d335a483
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:31:02 +0000

    basically precise large manner create

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def reverse_string(s):
-        return s[::-1]
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sys
+def exit_program():
+        sys.exit()
