86542eb504f884603db3151f79bc8b804c58ed4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:01:17 +0000

    reject warn love healthy relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def delete_file(file_name):
+        os.remove(file_name)
