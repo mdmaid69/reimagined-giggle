715ec8f5f636c11771c2fc9375ae3c615c77264d
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:59:00 +0000

    note performance do street crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
