37fcd0a62b49c4f5773be904380c60815b053348
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:00:00 +0000

    brave activity plain claim disapprove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import sys
+print(sys.version)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
