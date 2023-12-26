14b9023cca6dfa6aa1f0ad08b369b84a66233f37
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:51:31 +0000

    difference character display fork container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sys
+def exit_program():
+        sys.exit()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
