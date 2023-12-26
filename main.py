5d17f59cbf4273f5e33e59ca3772a251e34e2944
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:17:12 +0000

    after contribute willing should charge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
