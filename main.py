c07085513236682a9c9badf6986dd183224b9e01
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:18:32 +0000

    reason judge attempt nature breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
