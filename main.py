5abc4b97afc9292f896080f54355d827e1579f5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:46:04 +0000

    mind run shade mess obtain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
