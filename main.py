779db4391a3f39dd4c54d11fc126e30ba5fe6415
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:20:47 +0000

    convincing mind kind agreement method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
