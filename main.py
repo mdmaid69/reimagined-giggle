1be4abd2a52dbcf5146b62595825455c10adb064
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:26:29 +0000

    goodbye surprised aware whom quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_set(array):
+        return set(array)
