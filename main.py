903fb8c47b28546361c7be5769a03677db2490a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:13:15 +0000

    permission small mean everyone try

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
