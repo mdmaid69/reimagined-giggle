a29d9895e22d805d7d4b3a7d3513c257c74aad4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:20:20 +0000

    567enemy police probably board plenty

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def square_number(x):
+        return x**2
