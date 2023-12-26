f1708acef41e554c133c8009039917440339a996
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:34:58 +0000

    grow prepare easy person honest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
