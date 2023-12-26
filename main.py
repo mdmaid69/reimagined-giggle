ba9991b0ffce15cc0246246bd53cc5a90c67a354
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:35:16 +0000

    consider dance typical wherever keepup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
