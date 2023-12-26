b1f2fe64c3cc09e3c1c5bc1538237949dbeb2cee
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:30:17 +0000

    church joke keepup you stick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
