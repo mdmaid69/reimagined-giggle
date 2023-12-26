5849a08740c1e2effce0c03f409564dfb6edad82
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:54:09 +0000

    tip continue peaceful detail system

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_power(work, time):
+        return work / time
+  def multiply_numbers(x, y):
+        return x * y
