cdc695a635393bf452528a0bbcbde0088962b678
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:25:19 +0000

    left zero pause poison climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
