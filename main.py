01db084a94e553a05e6abe436ea035434aec631b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:57:17 +0000

    eager holiday correct reaction move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def divide_numbers(x, y):
+        return x / y
