e1eadcd9315686fc57c3f59484d313ceab40a729
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:46:31 +0000

    remark fall all boring workon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
