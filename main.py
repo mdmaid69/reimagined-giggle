bbba5a7052f80ee2a720fecd2b3fd2e35a84405c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:52:18 +0000

    language after Mm slight level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_circle_area(radius):
+        return math.pi * radius**2
