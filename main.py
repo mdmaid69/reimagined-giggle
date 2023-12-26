dbea8afadb335a06b6259e704aa43bc7e009e910
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:08:51 +0000

    movement comeout abroad approve appear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
