3cbfaa9b474919343472483f261f9f912c6ad851
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:00:15 +0000

    tight doubt personal loan thin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import datetime
+print(datetime.datetime.now())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
