79e2b05935fae07906e7055cbef2b8ef9811019d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:32:34 +0000

    single delight eager good ever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_bool(array):
+        return bool(array)
