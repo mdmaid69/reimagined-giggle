6fd324d0909eeb689c8dd53ae63d846d5e05ccf6
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:29:25 +0000

    characteristic telephone rank familiar half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
