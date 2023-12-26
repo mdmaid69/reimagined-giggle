1868a6902f8d886b236c4126e103c9c7c9b09f04
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:58:03 +0000

    smile everyone home complicated breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
