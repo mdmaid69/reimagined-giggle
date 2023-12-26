d8eb0581a69686be04f52ff0edb74cb329a09f1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:27:02 +0000

    study please how breath invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
