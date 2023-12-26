49d79afd802844f5a6939a4a21d75a3419d54489
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:19:48 +0000

    train throughout dare but breakout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import os
-print(os.getcwd())
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
