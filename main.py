9d332f803827d4945a2aca937c0d1e98b3907821
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:42:56 +0000

    top account bet cough occasional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
