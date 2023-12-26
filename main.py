5821ad6abd53e965740bc203402b02a05da9aeff
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:43:46 +0000

    fast slide touch head rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
