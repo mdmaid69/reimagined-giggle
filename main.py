bac7b7fee7bd07325f33e4db1b8fe3034d6a59d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:24:55 +0000

    count increase accidentally foot reaction

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
