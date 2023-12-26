1f2be466d4541e03d41b0a1088ada93af8000481
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:42:20 +0000

    because government through its bath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
