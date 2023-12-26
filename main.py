a95781de18930ffa48f8d656769a92d06b67ce9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:48:30 +0000

    possibility even comfort mainly petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
