05bf9043e542804e036de86d5bb4a1b32163b311
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:15:05 +0000

    wave outside shopping building there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
