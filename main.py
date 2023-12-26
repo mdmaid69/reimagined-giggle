64fcef9f06563f26f26ab8fe232297682302bcdc
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:18:21 +0000

    stamp cheat by form manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import sys
+def print_python_version():
+        print(sys.version)
