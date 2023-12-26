b22770806119e455ea6b7199f83b94b12aedfd5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:46:24 +0000

    bright skirt smile report turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_min(numbers):
+        return min(numbers)
