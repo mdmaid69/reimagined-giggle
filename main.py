5d6a36ae410b3c7b5897fa67bb28f7d86db354c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:20:52 +0000

    worry yesterday heart meet round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
