b5eb376e58315c3b4a8c6b3f06f18deae2a742bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:54:55 +0000

    urgent room payment disagree deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
