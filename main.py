2410f4e138938c945ddc1592482c938dd5a33b69
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:19:29 +0000

    concerning splash anyone quarter chin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
