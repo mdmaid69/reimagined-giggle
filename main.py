133383eb990d86c3673ff39a9a7864221892187b
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:26:17 +0000

    some gradually job until round

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
