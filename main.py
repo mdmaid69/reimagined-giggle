f8c97a93fd7e0518fe37dd565fea980dab92c691
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:33:21 +0000

    confused runaway division break list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
