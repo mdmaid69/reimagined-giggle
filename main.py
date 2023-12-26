7e5c21f87c538696c356500d8cd29ebe36dfe256
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:00:40 +0000

    scrap winter trick line sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
