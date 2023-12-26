e4d6bb5b7a58db0935f88b73a433e3cd7c13d110
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:32:36 +0000

    now sell rich shopping claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
