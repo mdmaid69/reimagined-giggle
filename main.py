da9705adeef37ac8c6cae776c64d0eecc31e485c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:26:19 +0000

    admire minimum extra downstairs Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
