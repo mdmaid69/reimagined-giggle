84d1ba51909beaf51c37ba6bcfadc03c547bc393
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:47:07 +0000

    now blind fond find earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
