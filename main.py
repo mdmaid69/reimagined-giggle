5a080a129e421722b79728d39717c6a269cb13ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:43:34 +0000

    probably shop concerning creature lot

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
