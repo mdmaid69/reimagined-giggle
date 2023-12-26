6bc098b787566ea90f21f9d8edf7e8b5681365cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:31:07 +0000

    not chat form cheque fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
