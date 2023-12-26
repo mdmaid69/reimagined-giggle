a80c8099c6f232a7986943950ae071de127865e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:56 +0000

    detail calloff several condition right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import sys
+  def get_python_version():
+        return sys.version
