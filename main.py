ec632a82b61b24ad0b6411e57b9df5fb8c896398
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:56:21 +0000

    fill section road takeoff immediate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+print("Hello, world!")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
