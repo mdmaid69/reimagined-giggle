f5ab761f29ccfcf0804ed1019359ac4195d4c58f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:18:36 +0000

    emotion long stranger arrangement importance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def get_array_as_set(array):
-        return set(array)
-import math
-print(math.pi)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
