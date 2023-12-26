11e03424a770ae52cbe4b30b32607cd0457d633b
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:09:00 +0000

    family murder escape vanish conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
