811399970924fb1ed9fc17a9dea01ac69d465894
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:38:29 +0000

    hit until wonder loan excited

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
