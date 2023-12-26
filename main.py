e4a214aa9ca1a7a2a68e9203d72230b8bf523928
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:42:14 +0000

    anywayanyhow majority otherwise pocket evidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
