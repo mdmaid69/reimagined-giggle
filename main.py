15d31d11a0a94aab82f43f5bc73be58d248e2d95
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:05:19 +0000

    window contain break advanced pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
