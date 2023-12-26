aaff25d0039f3069305f9df88ebc50d9d256c993
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:07:29 +0000

    lock left rescue temper natural

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
