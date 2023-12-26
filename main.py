b8bcde92eacca588e49982e411317803e1a63429
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:45:40 +0000

    accordingto division friendly exchange contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
