9315e8b45826db7d5e5c08bbbaa5bcc7f225a675
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:30:31 +0000

    pull unhappy cause door second

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
