fa8c0cb4705ee0297f115c61d05d72d89c237183
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:12:39 +0000

    breathe collect score confidence above

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+print([x**2 for x in range(10)])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
