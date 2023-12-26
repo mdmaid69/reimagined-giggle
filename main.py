93f3e9c4c683a53e768fb69c98039a4c097ef440
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:05:00 +0000

    chase need murder grateful active

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
