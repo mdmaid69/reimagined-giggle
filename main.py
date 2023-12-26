f7b1c74824f8f8bbab6cb037848b6be0bfac8fc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:42:08 +0000

    sink match pronunciation cross storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
