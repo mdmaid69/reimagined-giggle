5dfc8f04d2ddf5cb47e81245bce03c025ea72a94
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:33:21 +0000

    or pile shock below repair

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
