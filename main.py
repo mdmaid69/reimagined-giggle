e612311c722ce4f6fce8bf6c373068278a9a079b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:59:48 +0000

    top excited correct which blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
