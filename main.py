e5ade78f03f49a05d311dedf216cd95c0cef6c92
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:22:56 +0000

    combination face bother wall phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def square_number(x):
+        return x**2
