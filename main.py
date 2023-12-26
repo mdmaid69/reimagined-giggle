4cdcb20b6a4db20d04e40347d83f099bb7df48d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:58:48 +0000

    habit fond history lonely combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_palindrome(s):
+        return s == s[::-1]
