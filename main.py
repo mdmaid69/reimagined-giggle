8b59fb3ed53bf19f905e2b70e456d47dcf95ccec
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:23:48 +0000

    gooff shape crash nervous tall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
