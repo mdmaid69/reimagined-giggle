224558c35453216fd36e8a3f5c5d9f9bd5a35418
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:34:46 +0000

    possibly build push detail theirs

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
