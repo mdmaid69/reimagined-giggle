002d969cfd7aa69d7c7a087e4643c470405a624c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:12:31 +0000

    combination survive roll workon part

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
