1182a1c7175dc0acb47fd18299f7559b0ec1bbd4
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:44:55 +0000

    strike 572enter complete ear where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
