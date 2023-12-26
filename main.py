b1dbe91c2baa0eeb172c9e90d1730558fbbcaf53
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:37:52 +0000

    pollution before keep remove shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
