3247b2ca57389ad65d6733a6434b98d5df6f5c30
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:47:32 +0000

    bringup guard underneath movement clue

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_repr(array):
+        return repr(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
