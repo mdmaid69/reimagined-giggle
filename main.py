f51c2b86239b634ba933618fd765264b36eef52e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:18:21 +0000

    loud shake hold receive better

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
