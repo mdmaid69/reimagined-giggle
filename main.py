cb7aef9395d818126d0bfdeef6d93a75a28d1cdd
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:29:25 +0000

    combination will exploration continue train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   def find_min(lst):
         return min(lst) if len(lst) != 0 else "List is empty"
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
