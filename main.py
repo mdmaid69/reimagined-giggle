ea9feb98d5c730e6be6abb9916b12e7d5d7fff99
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:14:25 +0000

    joke knock complicated please week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
