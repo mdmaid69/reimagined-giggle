c6dd9ddcdbad738c5f68a1cc22481936f50b39cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:15:46 +0000

    meal human bunch connection interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def add_numbers(a, b):
+        return a + b
