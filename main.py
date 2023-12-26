d6c86e34f899bf6894606c6a5cc0862ecd06d7e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:14:27 +0000

    lean shop nigga sweet usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def convert_to_hex(n):
+        return hex(n)
