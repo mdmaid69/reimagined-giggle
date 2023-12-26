1c99e248e7f922241be8364bcfd91afaeca246f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:22:46 +0000

    cook funny discuss success behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
