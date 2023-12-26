26de8befe9cc282f98feeced33705feab84eb0e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:22:17 +0000

    do high proof getout remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
