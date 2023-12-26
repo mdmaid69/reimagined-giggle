7285602b3f91dbc4e8cf6f7a5ea55fc0f9a29301
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:44:58 +0000

    birthday start wild getback strange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
