9f102bdeea81b0ca705779e04661297bb3d809a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:13:13 +0000

    office complete gift calloff report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_dict():
+        return collections.UserDict()
