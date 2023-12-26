9f2033127feaf282248b7983454c934f024bbec0
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:09:34 +0000

    conduct measure Pp getoff practice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+for i in range(5):
+        print(i)
