f919ee2b916ea4d9ff58939d7803a02ea26c25d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:10:15 +0000

    point street think measure danger

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
