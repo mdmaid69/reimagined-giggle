4165c9c51b8e13b8033a426800e21d8cc1a31861
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:25:47 +0000

    convinced opportunity few similar flow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def get_array_as_bool(array):
+        return bool(array)
