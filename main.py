db56902b94622f7b068916c79f35c2b5b25ee906
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:49:33 +0000

    track agreement lend calloff trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
