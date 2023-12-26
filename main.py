0932431d147b36eaa1d6668ced8b3f611493519d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:08:09 +0000

    flat moral cause above workupto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
