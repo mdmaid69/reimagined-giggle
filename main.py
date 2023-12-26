e7ca2b22e419010092de2eec35261e8f58310d34
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:19:14 +0000

    inquire fire niggaacademy between work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
