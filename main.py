a47583031d2957ef58d32d32ffb498b6e77fb3ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:11:37 +0000

    think takeout clear mend old

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
