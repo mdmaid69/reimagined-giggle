baf635b5b86d1c8bf756b040d6f515994446e484
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:49:34 +0000

    situation train continue tragedy fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
