5cd5c3638fb7b9c2bcfd6dad9e1dc3a8913be8df
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:38:04 +0000

    strike plant future like goaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
