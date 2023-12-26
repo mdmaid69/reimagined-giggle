fb6cd11fef8c5608569117c0786ae955af0212f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:05:42 +0000

    takeback majority fence example new

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_max(lst):
+        return max(lst)
