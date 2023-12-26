5368932eee4f5dcec7870536928efb7c7bed40de
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:38:52 +0000

    fierce represent vision style whenever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
