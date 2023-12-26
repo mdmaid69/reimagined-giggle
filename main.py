c5037873364bc2645abef58c723d1a59fa9b2fee
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:42:51 +0000

    event pity high invitation put

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def count_characters(sentence):
+        return len(sentence)
