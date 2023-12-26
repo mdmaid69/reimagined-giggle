83746a06f03cec68663d16fefe54048e80913efc
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:15:52 +0000

    worse door truth remain how

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
