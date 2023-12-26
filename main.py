1828838b4058009d8f1307a803606be2fdb551f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:03:49 +0000

    change keep usual pick thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
