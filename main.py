ca23441731829c74a089f8bad684c41d78a649b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:29:14 +0000

    success realize display proud along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
