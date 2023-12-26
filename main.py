3024a4095bcac8848b25c4d0517dffbb13c99f4d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:51:05 +0000

    mail nature wipe fast confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
