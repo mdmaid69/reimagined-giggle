1f7dd344f3cc611d989c94105038956905d13b71
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:52:00 +0000

    down pump oil run conversation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
