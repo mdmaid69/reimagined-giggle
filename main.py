4f726aa948fc7921dd0411851e4e1e76cc62f8b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:24:47 +0000

    rate clean confuse confidence again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+text = "Hello, world!"
+print("Reversed:", text[::-1])
