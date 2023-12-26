b23168d6aa944a140da7eee39b63e10f3d153d41
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:31:10 +0000

    gate kind usual day meaning

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_size(file_name):
         return os.stat(file_name).st_size
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
