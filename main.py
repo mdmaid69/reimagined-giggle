82a4af5cd6727e8d43f01d5e8b3c33e4050b94fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:17:33 +0000

    broad central peaceful teach needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
