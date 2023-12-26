a5c65fa5ef384f0da17ac701181b09140e02950d
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:32:40 +0000

    free half rush prefer entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def greet(name):
+        print(f"Hello, {name}!")
