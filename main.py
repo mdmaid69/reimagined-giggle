2dd4a0a266f3936e55b1b4ab368cbe4ba7bbea01
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:26 +0000

    after repair stone dead base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_length(array):
+        return len(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
