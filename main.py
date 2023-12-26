3c2f4b75bcac13ebf6d47b3f481df2a5a8eb8f1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:17:00 +0000

    direct bus hers bend stone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
