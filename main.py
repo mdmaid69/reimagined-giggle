ab2fa1975257b9f6be7bcf0f545cc4103f90ae73
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:03:21 +0000

    lay section his respect theirs

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import time
+def get_current_time():
+        return time.time()
