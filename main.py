8ab1924cefc7963ef3931b18cb098e353426ba87
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:56:33 +0000

    legal journey Yy fair piece

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
