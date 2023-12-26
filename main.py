7ba739bb70dbc0fc4be3508cb03a101518d82bc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:47:09 +0000

    blow runover traffic pour arm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
