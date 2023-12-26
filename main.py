7fb51020e10d78abde2e0a9ac703fe1f3bb26eed
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:20:23 +0000

    advantage way her through start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import collections
-def create_priority_queue():
-        return collections.deque()
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
