521879de79f99b21b1dc9db6fb0b4f537979cfbb
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:24:26 +0000

    pleasure male repeat punishment aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
