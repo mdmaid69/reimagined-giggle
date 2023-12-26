edad06e7753047782dcb2167c2e94f90d849d530
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:45:06 +0000

    below where steer increase surprise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
