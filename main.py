2ac731f3f7cae53fe60e8a1a5fb543182993323e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:12:07 +0000

    precise search direction on together

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
