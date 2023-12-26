d51729a3d9199a4171ec810eeeee689bc74f2d03
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:04:14 +0000

    study along book 570enjoyable level

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_typecode(array):
+        return array.typecode
