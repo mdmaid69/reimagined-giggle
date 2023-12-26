c9927cd0c496fc0f71968de93aba00813fe79023
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:46:05 +0000

    tense attractive relief less illness

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        return sys.version
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
