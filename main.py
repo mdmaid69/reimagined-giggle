c9a025286cb1230d89c06e299839d1beb81bf294
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:07:30 +0000

    bang formal difference peace protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
