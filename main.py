dcfce3e3c84e948d6faf63cb349a9e511df95735
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:37:50 +0000

    cum exchange attractive congratulations sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def remove_from_array(array, item):
+        array.remove(item)
