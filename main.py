cf4c10c926e752bb9024184c6b1b561bd894693b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:07:21 +0000

    last spring divide left whatever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import datetime
+print(datetime.datetime.now())
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
