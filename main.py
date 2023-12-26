252e76b1b15bf8f38eae6ea1342a49650250c439
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:11:27 +0000

    where only fork coat quality

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
