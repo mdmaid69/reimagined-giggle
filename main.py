e402fc4f0e7264621c68d87a9afc86c4972ae9e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:35:56 +0000

    finger back project cry people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
