e69df132de743e75fbc27e3badf07d9280c3d7dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:44:10 +0000

    throughout visit damage polite object

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def square_number(x):
+        return x**2
