b9e5e360c9a9c8c6c61bfa434da5ce71db5797c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:04:24 +0000

    reduction task Itself stretch form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_work(force, distance):
+        return force * distance
