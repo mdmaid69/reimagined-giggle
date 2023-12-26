fe47f5085f794b4c89bd87bd6cd404d6b2aeff71
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:31:58 +0000

    release during correct shake foreign

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
