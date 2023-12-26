43373227a11ff5fa57d9f838d2b59e71e0f753ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:50:08 +0000

    myself spread kind underneath replace

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
