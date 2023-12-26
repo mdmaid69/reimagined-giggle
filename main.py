71cab316239939c2128b2111212f8406b2684e45
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:24:20 +0000

    excited pin proper use sting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
