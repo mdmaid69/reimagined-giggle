26ee0da6179bbec2f6e973c9e159ded905838a11
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:32:24 +0000

    give special opposite down meeting

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
