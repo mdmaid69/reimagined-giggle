41d6be59a34c929b2fe1c2741e7d8ea5e6369b45
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:57:39 +0000

    free offer bet round Mm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+print(math.pi)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
