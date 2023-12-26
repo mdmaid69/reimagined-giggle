0b9dc08eb17ce635f788a7dae9ef99f749641083
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:04:38 +0000

    affect over clean pool mood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
