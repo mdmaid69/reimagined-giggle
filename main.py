ded099773745681b86fc3255fb546220fb435fc2
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:26:18 +0000

    formal defend loose after shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
