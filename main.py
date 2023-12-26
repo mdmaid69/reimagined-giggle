1da814e0df30d29eba02a175c06ff5ccf7a7b0a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:52:28 +0000

    cheek leave book succeed supply

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_acceleration(speed, time):
+        return speed / time
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
