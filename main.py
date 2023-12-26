768d5abf4bf2fc6ca0e936da79c53b25f78bdc38
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:52:30 +0000

    middle sky add weak free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
