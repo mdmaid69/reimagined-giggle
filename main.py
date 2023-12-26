208dca6f7ff9d012b4b7061c789f491da58101f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:33:17 +0000

    control paper buy opposite Uu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
