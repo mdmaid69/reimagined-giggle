cdcc342a8d8e761c6e0cac461e4c95be86e6cc44
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:41:58 +0000

    month show money phone closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import re
-print(re.match("h.*o", "hello world"))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
