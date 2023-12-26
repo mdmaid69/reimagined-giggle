570119a95812d09a85bb602b6a3ecf59c3ef70ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:31:03 +0000

    wire exhibition interested dry about

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
