7e3c576b43117ef42e15629281a9a6205b785ed4
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:28:50 +0000

    tonight trade cover read speed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
+import json
+print(json.dumps({"name": "John", "age": 30}))
