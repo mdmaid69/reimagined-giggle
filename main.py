8faed287aaa0cf2cd9fe81fae71c1147a1dd28b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:39:06 +0000

    build pot interview ashamed judge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
