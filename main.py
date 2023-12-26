c3fe6b56758abf4122ef82d9caae6ce806d29d3e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:07:10 +0000

    today set imagine report wonder

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
