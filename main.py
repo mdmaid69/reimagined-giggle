d875bf62aa954c7caf3feb4bdc183d4915d341cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:27:31 +0000

    insist report environment mainly night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
