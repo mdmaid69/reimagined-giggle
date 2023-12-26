254e79ec286554860e0c68b212090f4cead2cf77
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:07:26 +0000

    final destroy reject breakup amount

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import sys
+def exit_program():
+        sys.exit()
