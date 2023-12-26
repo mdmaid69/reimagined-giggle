804dbce0b54d3eaa7e696e4378c28354669e5b75
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:10:59 +0000

    themselves float grateful sound carry

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
