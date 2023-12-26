8dbb70bc4bd68bcc81651a27f7ce4b5a49f667c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:22:31 +0000

    rise through eager beginning consist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
