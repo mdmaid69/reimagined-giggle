4d82bc5275c347adafe84101dcda44b819742c3c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:56:29 +0000

    mix impression stamp but jump

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
