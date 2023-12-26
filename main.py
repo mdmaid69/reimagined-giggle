da979fdfbb78e3214dcf042b429d31b16f942f68
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:03:48 +0000

    light distance every lay dress

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+print([x**2 for x in range(10)])
