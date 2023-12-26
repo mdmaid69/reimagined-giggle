b8e3651c247c3eb91a506aae567861d63b757d22
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:05:29 +0000

    face into himself disturbing comein

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
