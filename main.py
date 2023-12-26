f1c95890d219dd8b979e997020b4f78fce521bcd
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:07:19 +0000

    either entry height shake mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
