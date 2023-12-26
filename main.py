93bf223f37a983bf16e1dc39e1838a094a4ae55a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:59:49 +0000

    shame claim dream time greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
