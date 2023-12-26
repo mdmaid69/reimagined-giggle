5c62ea705c677068c8b8b19c655aba9ebf125950
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:30:11 +0000

    during problem estimate concerned turnon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print("Even:", [n for n in numbers if n % 2 == 0])
