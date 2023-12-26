c5e59a3cde49efc60fe8789c4d4edf10812d94b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:14:01 +0000

    value another sting prove wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
