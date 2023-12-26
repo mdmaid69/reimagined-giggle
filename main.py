744501c34e3abb220986ec531b5de6236f12ca0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:50:09 +0000

    through present foot pour confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
