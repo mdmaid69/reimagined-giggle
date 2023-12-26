92a1718c5eb837c23e991bf18e72b98184845728
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:24:42 +0000

    begin 565end house instruction damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_length(array):
+        return len(array)
