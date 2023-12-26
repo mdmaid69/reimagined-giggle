fce78419874523b93f5d3f7652aac1bb7e69ce0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:56:02 +0000

    past car sleep real effect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
