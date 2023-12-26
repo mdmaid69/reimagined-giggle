e905db4dda58bce78b76d15b533403b4a5919a7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:03:33 +0000

    runaway letter usual powerful careful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_tangent(x):
+        return math.tan(x)
