ab8e4dba3181450a79830d11b158fc0ce9545d32
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:22:44 +0000

    above human limit wave plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
