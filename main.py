a58c5321913a3e2c7b3b78696cdf089f697a0fdf
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:21:37 +0000

    fold with free include telephone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def is_even(n):
+        return n % 2 == 0
