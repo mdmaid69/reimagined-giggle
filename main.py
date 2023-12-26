2002d520bb7360f8adc124273d7f30a3e40ae2a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:07:10 +0000

    low confidence nature pattern quick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_str(array):
+        return str(array)
