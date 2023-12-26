526b009f0e9b3f37346091d8efabe5ef98ad8006
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:00:48 +0000

    him amount likely some naturally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def square_number(x):
+        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
