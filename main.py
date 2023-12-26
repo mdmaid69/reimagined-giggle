7f9e9af54455fdb030b3bdeeeb16bf19c86acb93
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:19:56 +0000

    odd ill memory unlucky love

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
