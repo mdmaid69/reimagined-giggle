0748cfa43d5a1e8f8752c812958a60f7426f98bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:12:41 +0000

    bye serious giveup same profit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
