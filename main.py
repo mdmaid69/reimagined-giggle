b88a47aad274dbe78ca84e8cef7de9ca06c108dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:55:14 +0000

    exercise another murder exact joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
