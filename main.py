39d2607577795c697226149d9adc66ee360c4ecb
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:42:44 +0000

    touch funny really set year

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_item(array, i):
+        return array[i]
