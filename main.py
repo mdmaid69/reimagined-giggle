4f4d1a6b2abd76edf42cd623aa14c5b4df60860f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:37:49 +0000

    tooth with first damage suckme

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def is_odd(n):
+        return n % 2 != 0
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
