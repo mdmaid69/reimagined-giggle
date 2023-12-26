96e7ca0379c9c96c14b4df40bf62508baa448aa2
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:15:11 +0000

    likely embarrassed as miss stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def add_numbers(x, y):
-        return x + y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
