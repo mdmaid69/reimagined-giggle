24d1df5dd5155014ce088d0a430464d45620b240
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:11:41 +0000

    form minimum content minor introduce

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
