0c7a6f9eccbff542a81879bb7fe7924a0d249023
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:30:43 +0000

    plenty clean remove problem lose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
