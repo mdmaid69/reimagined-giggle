00be823397b13ff215d527f627442a08aae4b1f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:08:39 +0000

    bottom distant Gg independent pretty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-x = 10
-y = 20
-print("Sum:", x + y)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
