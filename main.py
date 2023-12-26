f5e2ca4b393d53046414b7da59a0219890198699
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:19:22 +0000

    young schedule laugh combination entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
