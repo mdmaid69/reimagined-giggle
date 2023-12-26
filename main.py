6ff622d549555bdc1c362afc0598bcb532e36975
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:59:20 +0000

    country bit keepaway object pool

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
