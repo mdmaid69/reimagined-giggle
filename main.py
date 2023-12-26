ec95a2a51b8a1b1328251d34fbc07c4132230c62
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:46:45 +0000

    cheek add justice special drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
