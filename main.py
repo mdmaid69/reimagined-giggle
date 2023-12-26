415256d51a90efd5ab7fb51113ca649230b3a076
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:11:31 +0000

    intend protect splash foot port

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
