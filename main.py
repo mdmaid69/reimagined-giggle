b8303fa0ba015ec841b901d497446121e5481110
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:57:23 +0000

    pleased refuse married awake stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_area(radius):
+        return 3.14 * radius * radius
