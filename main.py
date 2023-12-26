31d94eacf1d276daf2dd10cd76cfe33acb25e36b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:35:18 +0000

    thumb bread her expression trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
