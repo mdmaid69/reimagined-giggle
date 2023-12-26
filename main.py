f57f53cf0e2f1173cfa987f01acbef62d165c5f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:52:14 +0000

    justice tear ordinary turnaway tear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
