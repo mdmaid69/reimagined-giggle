3318e8a52c69e7a544f22d67601d45a80f857069
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:00:29 +0000

    talkoutof address often radio command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
