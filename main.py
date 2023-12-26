e7fbd76cf8d0c82d1bfa26af51c4c4858021155a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:39:41 +0000

    disgusting translate professional reduce record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
