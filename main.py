41a53fa4b257549a53bbc6673a71c7372f538a4d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:47:39 +0000

    free crash class and taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_combinations(n, k):
+        return math.comb(n, k)
