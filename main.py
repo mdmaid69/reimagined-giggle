784a9e54ed7cec34c8d6e2e24a72ed629a37d26d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:47:16 +0000

    challenge ground advantage walk accordingto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
