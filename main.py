bd50b66b64c954efc3c2495348c0095b0217ac1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:25:11 +0000

    several lend deliberate brief away

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
