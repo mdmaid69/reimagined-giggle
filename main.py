ea214bbfd90a5e6b438a137d83afde1be49921e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:29:09 +0000

    rest swallow wherever address post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def cube_number(x):
-        return x**3
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_list_from_array(array):
+        return array.tolist()
