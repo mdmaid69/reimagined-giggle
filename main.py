ad6fa0fe70054294a8afcccfcd795764cd922a6b
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:28:53 +0000

    getup summer 567enemy trap new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
