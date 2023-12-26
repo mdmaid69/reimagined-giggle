5949bd94dcf66705a6db4d9ab9d6a0e74b785eb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:07:42 +0000

    poor file account Mm dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
