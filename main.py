09d0d4e21cd9bb6e1ebfd85ff1ee7cc69e852a33
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:50:04 +0000

    steep cook whoever shine second

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
