fe2fb31468074ed2d8edb14f1c73f055d415d820
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:40:04 +0000

    pin suit trade pain or

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
