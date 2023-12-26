4abb243da4875e1c7f10e00c52a26da909e30edf
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:27:02 +0000

    fat take stick attention make

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
