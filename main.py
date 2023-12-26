a9e231e823d2e64a6061f1094e0ca1d11b1d00fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:29:36 +0000

    underneath roof true instrument abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
