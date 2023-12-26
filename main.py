efc74610122396eb4e94bb5b8a2116879dd76f0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:26:21 +0000

    trade drop share remain dry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
