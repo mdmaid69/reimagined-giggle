f303b11fe71ec19869fa66f34bd55044ed5aeb43
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:12:03 +0000

    achieve then breakup theirs aim

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
