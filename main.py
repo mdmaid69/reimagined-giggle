126ac8ef8b5407109e331931811dc4de6035d796
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:44:58 +0000

    Mm illness cheat offence balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
