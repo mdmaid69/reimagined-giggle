5cdb2a3c3b2c27543e9ccf3771f1d7a4cf04666a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:47:04 +0000

    pin any book 570enjoyable doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
