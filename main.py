43ce2d0b12dda8daf1aefb054b6047c3fedb8040
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:23:53 +0000

    serve computer start wide level

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
