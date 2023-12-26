1b487f094c89ef25626b8234acad53587d74e233
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:10:50 +0000

    lesson book region giveup relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
