c34251ca0e7cad09a8aff2e1e560fc12f472c51f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:19:12 +0000

    block juice else show medium

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
