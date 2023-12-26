613a2d03f25c09a7a4f7942bdec98eee7561fd08
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:22:00 +0000

    fill possible rule push strange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
