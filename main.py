8c9a1f2c161c134b2c26ba741bb677fa3fe7b5ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:28:12 +0000

    piece below load traffic hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import collections
+def create_user_list():
+        return collections.UserList()
