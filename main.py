8eb83160116abac326b00305e76fc1d1d3cebb79
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:29:36 +0000

    necessary flat close ashamed deaf

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import numpy as np
+print(np.array([1, 2, 3]))
