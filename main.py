14569b7074df7a211f277316ba83e145380fad7b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:13:48 +0000

    recognize operation somebody constantly basic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        return sys.version
+def convert_to_binary(n):
+        return bin(n)
