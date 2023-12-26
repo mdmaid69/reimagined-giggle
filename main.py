204d65d473a4d314d92688812feb1a54f1fec764
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:24:53 +0000

    stamp advantage wealth ago pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import sys
+def print_python_version():
+        return sys.version
