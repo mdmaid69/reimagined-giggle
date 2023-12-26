9cf80afb692cc7b49701bf62b2e0452511d58543
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:54:48 +0000

    willing second copy slowly pupil

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
