0950759c566f68ff79dcda7ee0f0779f71e7a8c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:19:54 +0000

    argument act foot precise cheap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
