4ce9363ecd63aad4ff57b4c2926e3e45aa21215c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:38:31 +0000

    wire ourselves whoever bath themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
