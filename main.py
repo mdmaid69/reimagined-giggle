3cbb2ce51d740e858fb436eb0b6fe8707155f0dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:24:29 +0000

    calm smile about single regular

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
