d1d165473f1ff082c6ce5a8f9c8a82d751defe72
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:43:18 +0000

    we behave interview artificial delicate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
