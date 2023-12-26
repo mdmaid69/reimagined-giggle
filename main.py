3d2bdfd5ab1ec2f8f20e6dd4973f47284f3d8e15
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:33:28 +0000

    worse hurry bridge official spin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def split_path(path):
+        return os.path.split(path)
+print(sum(range(10)))
