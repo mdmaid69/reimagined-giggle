a55119ea6ad13f0cf0278a76d0177b64aa52bc2e
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:52:27 +0000

    exploration lot it will nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def find_max(lst):
+        return max(lst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
