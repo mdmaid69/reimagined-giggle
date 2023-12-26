633767f1b10ffd456964452b9e8b56283cbed458
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:03:41 +0000

    listen likely forever afternoon entry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+for i in range(10): print(i)
