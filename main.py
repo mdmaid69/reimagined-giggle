3100b3920c8246cad085f0efed12df467014efab
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:15:35 +0000

    various might underneath share everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
