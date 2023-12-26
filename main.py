1d8c3a4185384d3bf3e40c96340b2ceeb760845e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:35:41 +0000

    past lend break agreement lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_power(work, time):
-        return work / time
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def square_number(x):
+        return x**2
