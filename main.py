0ab1fb8f310b38c576e7e8922413624ff02dd023
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:16:06 +0000

    mean watch common Oo damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
