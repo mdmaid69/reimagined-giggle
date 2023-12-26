b7c23c2fb625d7485f2cfd81c2800d15d677ca1a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:09:16 +0000

    impressed exploration pot generous slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
