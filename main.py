55f4541b37505eeb1c4fcdc44b6d5067e2a02d42
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:16:33 +0000

    wealth fasten stamp trick loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
