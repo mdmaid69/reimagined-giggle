af7ff4ebf3a96ff1f8d9205b8bef1c6cfdb9c75f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:49:01 +0000

    smart willing under respect side

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
