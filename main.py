b778fa5c6643ed2672e25e5736f4c0528e8ef0fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:03:57 +0000

    stamp separate boat declare fast

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+text = "Hello, world!"
+print("Words:", len(text.split()))
