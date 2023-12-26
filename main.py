9fec7740f18470304ba1a84d759552853215101f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:54:16 +0000

    tune later circle make use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+for i in range(5):
+        print(i)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_square_root(x):
+        return math.sqrt(x)
