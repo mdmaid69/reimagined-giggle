0ef224862ff1330a21f588d4feaec83ec9d5e52f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:08:41 +0000

    trip habit holdon hair kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
