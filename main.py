5ae493b4104d2ab12cb764be5820b21ad9871944
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:33:18 +0000

    tool field confidence one cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
