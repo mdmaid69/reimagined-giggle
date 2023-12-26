b6b43bc2037d2a35a0677d68aa969ca19aaee404
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:37:30 +0000

    stranger somewhere precise too Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
