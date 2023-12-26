86a12cf43c0f7dbac8cfefc17ade433d99f107c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:49:59 +0000

    single entrance opinion tap correct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def add_numbers(a, b):
+        return a + b
+  def cube_number(x):
+        return x**3
