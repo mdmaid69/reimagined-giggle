3c9ac8888a00c0cea5867c75c82cda1c3d738eca
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:16:16 +0000

    swing actual OKorokay reply lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
