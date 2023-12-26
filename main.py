491cdc28afb55131ccf49036ddf32f2d12d53a5c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:18:34 +0000

    previous lost cost so container

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+print(math.pi)
