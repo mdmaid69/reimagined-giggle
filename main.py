6d789deb5c9ed9248fa455a37a5acd6a49a4e599
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:35:26 +0000

    comeout involve ago thing shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
