492b2e6aea92a53d7961284d37d95443b0cc2110
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:54:26 +0000

    meaning tidy offer operate bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
