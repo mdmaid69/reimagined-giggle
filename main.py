945bd53a94d2341810c72dc0aaf2fa5a78daf004
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:13:42 +0000

    turnup tense belong process teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
