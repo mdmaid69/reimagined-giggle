b85e570261ac5e5def9554d3f4e871054429bc55
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:30:53 +0000

    early discovery fork score after

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_power(work, time):
+        return work / time
+def calculate_power(work, time):
+        return work / time
