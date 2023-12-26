b8af895778d778637015871d23359206feb14c68
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:09:10 +0000

    avoid increase complain funny straight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def square_number(x):
+        return x**2
+import array
+def get_array_as_str(array):
+        return str(array)
