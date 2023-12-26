2aaf7b068b329fb2bf7dc95e13a8d97d95ccaef8
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:19:20 +0000

    basically typical sleep we fall

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
