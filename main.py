1b62c2efb4e86ff078d0eb8b57ddb5d92523554d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:47:06 +0000

    thinkover task wherever thinkover cheap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
