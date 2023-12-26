376ce5ca9a70861d59c68805369fede3d019e747
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:15:12 +0000

    physical during switch save square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
