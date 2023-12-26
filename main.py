6d67761cde0b5466ba297d517a3855fd1a325e1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:09:43 +0000

    trap mention start accident include

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def cube_number(x):
-        return x**3
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
