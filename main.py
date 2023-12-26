fe2d0fecbb6c7add3fd0737155a361e51ff1ffa2
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:46:43 +0000

    earn swallow under example common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import collections
+def create_user_dict():
+        return collections.UserDict()
