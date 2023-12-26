e8871e117c54ecca1a4b4803f8655d2b6bc89640
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:37:41 +0000

    still takeover beautiful bush either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
