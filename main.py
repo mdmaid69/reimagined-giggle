38131e4864b1b1e8f739d5de7ea58d5959528a20
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:11:02 +0000

    unhappy market sight fear swing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
