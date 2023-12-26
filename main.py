92eaf60231f0b0b08b98162b2df8793d5b1e4350
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:46:44 +0000

    slow recognize cool after mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
