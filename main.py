05e499263bccac80d6787d6f8c54e890d8fca15d
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:46:47 +0000

    precise question little shine shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
