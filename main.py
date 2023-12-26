3ecf96a0c259030f605c927ed8d0b8205371e540
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:02:00 +0000

    exam culture there interview bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
