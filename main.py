e7f5363ca12a6b5781ebde00cd23df2af0d440c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:40:50 +0000

    miss calloff brush 566end admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
