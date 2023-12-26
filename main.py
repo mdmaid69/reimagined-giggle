9a552d6e49f2451615573c43c16b598a6711a43e
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:47:12 +0000

    bringup need holiday fail strong

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def is_palindrome(s):
+        return s == s[::-1]
