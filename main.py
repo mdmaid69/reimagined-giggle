411dbd4fd2bf35404596c3db4493be5d892c440c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:46:54 +0000

    comfortable rise break relief happy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
