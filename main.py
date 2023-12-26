c8b5163b8bdaa321498eaf4980764e9235fdf946
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:48:03 +0000

    minute responsible little fire zero

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
