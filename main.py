c8d2b1c0dd2cd3369bb6a5a5b85297a8ab4b8815
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:12:55 +0000

    replace breakdown shoot difference measure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
