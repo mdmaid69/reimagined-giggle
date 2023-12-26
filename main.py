af81cec55f99e6cd78afd84f76936ac3bb4d92e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:07 +0000

    display piece well tour hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
