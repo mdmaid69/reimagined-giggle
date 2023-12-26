11fbfdb50b7c2a323b36883a116ec29e19c39c9b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:05:45 +0000

    challenge first wire Ll fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def square_number(x):
+        return x**2
