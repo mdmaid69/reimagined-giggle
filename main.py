9b0b73502036b36acfcc39d37928b1ae236495a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:59:17 +0000

    where note better attract defend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
