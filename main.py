ef53859f70e8740435dcbf9cab0d672ac7b2796e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:14:48 +0000

    high bright inquire word Tt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
