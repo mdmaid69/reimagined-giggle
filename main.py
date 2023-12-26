0afe42a2c49f5796bf6689d04bd83db532d5b816
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:03:23 +0000

    desperate pronounce side getback politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import time
-def get_current_time():
-        return time.time()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
