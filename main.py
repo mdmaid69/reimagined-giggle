4e9d2a58fcd8fe7d54bb5ff33edeee6ec56d971e
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:55:55 +0000

    deep university pleasure trap blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def clear_array(array):
+        array *= 0
