2c5105152307b2953935dec1f35424111fba5ba6
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:08:08 +0000

    politics tell leg flat spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
