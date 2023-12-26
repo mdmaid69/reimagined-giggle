ed537402d890e094e95af988145abcccea1d45b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:37:45 +0000

    keepfrom rate unkind usual wing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def sort_list(lst):
+        return sorted(lst)
