6917d1fba59e1ca438f6b2e1cdf2d8cc7ccfe159
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:30:11 +0000

    discovery damp force middle thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
