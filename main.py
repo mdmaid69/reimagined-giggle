e77155cb326887c0df9d7f5daf24b68fab0c5705
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:55:57 +0000

    fashion low watch secret free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
