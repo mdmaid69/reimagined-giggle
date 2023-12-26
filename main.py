a00a45fb03f1c2a37b001e047a4d3def4c97e475
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:18:32 +0000

    check friend information dull organization

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
