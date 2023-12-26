0e3e709ebd4433c95e5cd5d0ebbaab15b6557ae3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:12:15 +0000

    advise scared goaway foot criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
