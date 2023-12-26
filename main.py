a783eb4fd6d7592ea5262196f083be85f338ab02
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:30:06 +0000

    across rubbish block face clever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
