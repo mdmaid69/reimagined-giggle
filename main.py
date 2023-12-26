c2cd88baed6c0136903bfe27737caacc6048694d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:35:51 +0000

    extremely concerning gap turnoff insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
