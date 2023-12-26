b26501a4b16a0ed42ae3f0f1d751b75fea55bb50
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:17:45 +0000

    whisper possibility front everywhere thanks

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_length(array):
-        return len(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
