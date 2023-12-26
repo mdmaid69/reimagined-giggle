055afa5b49321e307f157e64d17841a182409f8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:39:39 +0000

    fear piece splash club equal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
