8a247215b17a4de31e09e797b2bc7220df9bdb4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:33:50 +0000

    problem innocent wide fold base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def append_to_array(array, item):
+        array.append(item)
