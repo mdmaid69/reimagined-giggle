a9324d91640ce5229e4cfcb015e53e6940d39677
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:54:56 +0000

    argument exaggerate accordingto with unit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
