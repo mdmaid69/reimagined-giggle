1ebd48bab048a18348c43b794dbcc6f4966ae66f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:20:07 +0000

    crash fire true ashamed create

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def convert_to_hex(n):
+        return hex(n)
