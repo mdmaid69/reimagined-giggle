60ea7eda564ab83611a79acbc161e5669e7e6591
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:23:14 +0000

    tread ahead soft send grateful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def subtract_numbers(x, y):
-        return x - y
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
