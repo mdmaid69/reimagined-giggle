0abe53019831265aff35386e62fc6aa5a6308b22
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:03:24 +0000

    scream runout justice negative sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
