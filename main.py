997ac2658c25aa1e6dc7a10de41223494b60b9ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:53:33 +0000

    height simple properly wet similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
