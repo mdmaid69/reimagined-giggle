5616f1a87e88bccda5b73edb0d670da1f600793d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:28:49 +0000

    criticize minimum so moment piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
