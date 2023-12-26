38d06397eaf403b7c3d2a9fa48a966db80e19cf6
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:39:10 +0000

    Cc service content transport sign

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
