117ad1f572a684e3d1aa83389cd02cd402f17415
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:12:34 +0000

    sink bit behaviour should butter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
