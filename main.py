c0de2b813e0f1d83d45d477ada9c74b261dcc4b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:21:53 +0000

    complaint underneath even pride polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_float(array):
+        return float(array[0])
