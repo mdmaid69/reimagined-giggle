245a90f076c1fdd40c25399ecfd63eb1821a6ad4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:49:35 +0000

    quarter improvement quality probably disappear

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
