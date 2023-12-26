e1cf48719240957ff62cfe3f47b8ce5dc9898f08
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:11:47 +0000

    pretty heart copy shopping characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
