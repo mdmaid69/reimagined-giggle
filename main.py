2abe5f6aff675038ac9148ca5e934cb621e9748f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:04:32 +0000

    declare couple ago Ss estimate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
