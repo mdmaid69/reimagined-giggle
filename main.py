b41786e0036e774746b92d206597d8287eacefe3
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:50:38 +0000

    Ww past left great over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
