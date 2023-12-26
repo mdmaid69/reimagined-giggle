2869694bed500fd634b9ab6ffcaf931448dae4a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:11:13 +0000

    change star add spill concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
