8c5f627f63a5540d5553f6e6738622eb22b8d1ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:22:35 +0000

    chat rest notice quantity spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
