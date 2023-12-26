effb01481ae0f82076f1f5168c30075ceef8ebdc
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:52:22 +0000

    wild wipe spring taste laugh

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def convert_to_octal(n):
+        return oct(n)
