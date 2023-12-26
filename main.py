31b7f3eae6a6149d79f90bd44bb323da02b00f7e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:52:41 +0000

    trap round second raise 567enemy

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
