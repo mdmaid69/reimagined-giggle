708439666fa9e8f98d6c745f0af36d4ce05b87be
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:33:21 +0000

    negative sing worth waste principle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def find_max(numbers):
-        return max(numbers)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
