2071e403a1f1610ccb2970764f8f3fc282acb229
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:16:00 +0000

    activity recently sleep do file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
