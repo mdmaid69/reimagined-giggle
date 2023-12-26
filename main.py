1adb6e3dcb029ba0958f0452cda9dd95b46a9ed2
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:54:00 +0000

    holiday unless rule exit use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_floor(x):
+        return math.floor(x)
