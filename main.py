8a73c3f62351b01f3e041a4868b23b905ba3e30f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:03:15 +0000

    guide sure dangerous take story

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
