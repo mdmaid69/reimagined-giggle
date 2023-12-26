6a982d352e5f89cb041f2d783dd7883f5c1468fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:47:11 +0000

    over fear press practise tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
