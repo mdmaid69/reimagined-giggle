53fd3eb17f74b2bb058cec5cd32ce45a3047220f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:36:52 +0000

    trust giveback pencil year stupid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+print("Hello, world!")
