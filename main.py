e2d9a4cb9064ff6759d1fb4e781f82aed1a0a96b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:07:34 +0000

    regular will bottom drive marry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
