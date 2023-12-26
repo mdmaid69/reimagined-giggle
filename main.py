72cd7288555e93a979f4fbcb91c3a6e9df585ae1
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:58:00 +0000

    Mm hollow copy he machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
