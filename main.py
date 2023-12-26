fe4b3f1a84ee6ad8dfd097723c98c61fc544ecf1
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:15:26 +0000

    day Qq apparently challenge convince

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
