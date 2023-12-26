a04197f3901223f7a8551ac53d92f5fc747db3ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:22:57 +0000

    building lead please Bb design

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
