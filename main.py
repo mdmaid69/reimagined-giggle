13e2558056e429b9d9d7abf550087a9680ba3cdc
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:30:43 +0000

    flat female class closely copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
