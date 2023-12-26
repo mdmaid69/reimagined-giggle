e6a162d80f4e22124769277fba39e1bdd7daf997
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:18:18 +0000

    nor best part support measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
