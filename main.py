80cf7c72985833ce3da260aa8bda2fc842820d4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:12:17 +0000

    anybody onto theirs respect nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def convert_to_binary(n):
+        return bin(n)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
