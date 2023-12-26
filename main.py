790a1d0f0c61b23f8d36ff896b943604ba9d9db0
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:47:48 +0000

    patience past hello unlikely market

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
