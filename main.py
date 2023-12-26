4c7f50c624e8ab1be554a1549add221459da570c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:21:31 +0000

    nose achieve lean state deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
