959a140ba9bca92b8a7d4d39f63e64a038d0b057
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:23:40 +0000

    ought related comein silent peaceful

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
