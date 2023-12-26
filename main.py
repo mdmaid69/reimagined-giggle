a344da54c44f94e9cc208a78bd4962dde2a9ac71
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:57:05 +0000

    woman short grow detail talkback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
