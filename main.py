a431ddbe13ad1401a4ed5e256f1070b1454349f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:23:38 +0000

    survive remember job range burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
