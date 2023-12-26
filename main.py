630db4a83cec201f7c52cefdaf173fdebb5c567f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:31:25 +0000

    accommodation which problem little ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
