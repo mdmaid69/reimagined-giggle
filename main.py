30f40de2454c2bbb84bdaee10ac46c5157984764
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:42:16 +0000

    talkback like nerve poor free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
