08f791f33f172f96daa19cd457ce35c8f1e69f6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:30:45 +0000

    birth listenfor copy so constantly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
