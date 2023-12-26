ba6b16afb73e9fb739328eec667b1de639de9976
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:32:14 +0000

    moment priority tie willing constantly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
