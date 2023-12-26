337b49b92b2d355cafe854d533bd2c0894dbc2dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:37:35 +0000

    good general neither temper reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import collections
+def create_user_list():
+        return collections.UserList()
