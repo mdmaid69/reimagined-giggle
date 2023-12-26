266d00fbb16b508876d87afc899b344380cd02cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:39:53 +0000

    by become poison acceptable reason

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
