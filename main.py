d67c572d52c5773542d85dc36e8a0c279c61c1a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:55:18 +0000

    pretty turnback matter improvement thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
