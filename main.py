e1a0d4bcb97b1d62b3ceb6d59bae5f55a4100592
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:08:04 +0000

    doubt nonsense sit instruction ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
