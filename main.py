5ab93d572e2ec075ae644a954327d0adc436ef95
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:48:42 +0000

    on slowly fairly cap flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
