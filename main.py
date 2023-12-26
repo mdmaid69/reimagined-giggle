5966ecba34adc65ae502bd731400c898c6cd1cea
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:49:54 +0000

    tool yours substance division unable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
