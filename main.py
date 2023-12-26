0ddeb402a13d905f238dae85b9bc0e20ed46e092
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:45:17 +0000

    Rr thick feeling man because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
