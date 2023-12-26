1308eaa5b63916e3f9ee9d0b8e1eb879eed3c2a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:54:46 +0000

    admire giveout pass or last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def append_to_array(array, item):
-        array.append(item)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
