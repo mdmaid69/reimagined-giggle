fd6a94bb21e0d78b53f7f3da5962cc4c60ea313a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:38:21 +0000

    repeat hair admit improve cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
