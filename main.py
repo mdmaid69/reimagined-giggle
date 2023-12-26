828ad828bc77706a60007941eb8ec9cf6819536d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:36:40 +0000

    wonder refer thanks point which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_power(work, time):
+        return work / time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
