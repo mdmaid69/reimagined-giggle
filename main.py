f275a3c602a234964c423aaefdec1db23f5afc4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:53:19 +0000

    live start brother pull develop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def cube_number(x):
-        return x**3
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
