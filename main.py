4e7786fccf3041aa94c2f9e6502a0a9ed3b01d49
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:33:26 +0000

    high mean hurry lose bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
