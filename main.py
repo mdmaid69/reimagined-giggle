da8a2e7b4ed17dc9325e5d027e3eca70d35e98bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:32:21 +0000

    flat defend description split play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
+import time
+print(time.time())
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
