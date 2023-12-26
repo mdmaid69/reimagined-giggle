b03d2ad14c6ce7cca2346c66de9389bb6e631e90
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:35:05 +0000

    study cause occasional sweet control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
