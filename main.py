95c700f24f4f369289115849652398232947453e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:00:26 +0000

    commit press stranger brush shelter

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
