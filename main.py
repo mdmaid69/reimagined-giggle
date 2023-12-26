7ceb892eacaff35c7ae3dcaceb7fa25c1c3000ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:41:25 +0000

    part force platform congratulations habit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
