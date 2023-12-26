114bb3396c2ac9333af8f0c15a52e130d6f9e3a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:52:08 +0000

    street attach hardly Ss lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
