59ad3420745624e5cec59debfea145596fea16b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:34:01 +0000

    now powerful start greeting leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
