a343dae76675ea29b744046214c1526a0737dede
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:02:20 +0000

    constantly check use set rather

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
