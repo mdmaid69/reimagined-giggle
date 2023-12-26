7447728005ebaa598d4c3f7b1f489b06dcbdc500
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:44:26 +0000

    leave must night if century

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
