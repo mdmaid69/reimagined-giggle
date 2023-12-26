9177ec868150364711805d7467897df2983e0133
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:56:08 +0000

    beat whisper want store runaway

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
