c894e725f0fe47e2d103f4b35ed1568a4cb23c95
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:47:42 +0000

    properly leave call Mm rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
