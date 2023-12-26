9400bb8e5ffe55d68f430e78fbbfc66b054b638d
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:44:26 +0000

    well how apparently next behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_base_name(path):
+        return os.path.basename(path)
