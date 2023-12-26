ed70ced6d81c76a4f9c5d38f72b80cb7820373f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:26:55 +0000

    completely safety pleased exploration half

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
