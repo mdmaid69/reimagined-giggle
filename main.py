6767a2b9e451600bd191c66d61002dc385a88b59
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:53:30 +0000

    sign far door success drop

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+text = "Hello, world!"
+print("Words:", len(text.split()))
