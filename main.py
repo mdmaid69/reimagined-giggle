e901ab977120ba90e13b38dba901f98ac4de0e75
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:57:35 +0000

    distant put valuable feeling push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
