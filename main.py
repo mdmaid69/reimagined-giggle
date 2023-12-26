8f679c714d8aff0fbad9959d7a643260c62fa00b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:52:02 +0000

    punishment laugh autumn bear read

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
