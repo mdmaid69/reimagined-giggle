67b783f5a650f309f20f027ab7208c6c113b7db5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:53:19 +0000

    forgive communicate smart cost spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import os
+print(os.getcwd())
