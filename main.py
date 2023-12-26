39105e087d08dd46598d68bbb43c854ccfca5bd5
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:32:00 +0000

    push task party Gg object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
