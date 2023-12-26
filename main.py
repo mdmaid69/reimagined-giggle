086a251bd054d2f7ff5f89e104ff66a1d84d6156
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:30:23 +0000

    should bet only Yy relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
