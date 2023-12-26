936ac51cf1eb93afa159e68565dc21b6a48bbd6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:25:34 +0000

    secret ear hair influence similar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
