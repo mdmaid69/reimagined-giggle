99829b1720048ddfa46f84fd764f0391ba47c887
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:24:32 +0000

    of certainly classroom carry explanation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
