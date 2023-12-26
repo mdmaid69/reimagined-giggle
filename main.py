7fd94fcd056287111f28b7cdb47a1722fef45e2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:08:31 +0000

    fortunately unlikely stamp release fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
