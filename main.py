a1d82d2b3b079001c447954c154dd25d008f25ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:20:33 +0000

    they out shop order spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
