05f33e7aa43eff491b4d52beeb57488d44a5f25a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:50:50 +0000

    extremely steal perhaps war rub

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
