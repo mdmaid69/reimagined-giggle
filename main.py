0f495b4b76554bc37ef0abe3f03c2e64586cf751
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:30:16 +0000

    snow petrol stone reliable lift

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sys
+def print_python_version():
+        print(sys.version)
