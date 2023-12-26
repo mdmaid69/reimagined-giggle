019402e8d9e26ff3f6d9f9096b2c5a18e166f187
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:46:01 +0000

    flow callup uniform brush honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def get_array_as_bool(array):
+        return bool(array)
