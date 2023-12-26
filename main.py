0e96d53807299c1d2586ca140119a9b1ec8244e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:56:08 +0000

    both surprised it height myself

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
