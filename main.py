e294de8cf0a8e01c7f1738ce27c93a942931ad06
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:32:19 +0000

    her blue tidy better instrument

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def is_odd(n):
+        return n % 2 != 0
+from collections import Counter
+print(Counter("hello world"))
