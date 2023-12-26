aa5b8d116dcf2ed2e1b8f1411dbea95c81ef22aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:13:04 +0000

    sort that distant popular nowhere

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
