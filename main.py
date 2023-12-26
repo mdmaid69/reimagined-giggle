374855ad5746f62e7a4ea5d08b5fd2782a20a2ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:11:24 +0000

    name demand name slightly talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_counter():
-        return collections.Counter()
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
