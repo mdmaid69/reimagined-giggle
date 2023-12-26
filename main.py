e79d99de28fae73dba3aac48283b20870e865169
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:23:08 +0000

    collect think old next bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
