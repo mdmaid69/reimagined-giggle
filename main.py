bdbf19a26040cf4a204f7a6f17f4414dccc8dee1
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:16:31 +0000

    excuse also improve confused on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_speed(distance, time):
+        return distance / time
