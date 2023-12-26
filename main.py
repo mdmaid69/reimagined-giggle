5935f2281eeea146ec3c5bcca7d1c923f57977eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:29:26 +0000

    left live about fresh city

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def exit_program():
+        sys.exit()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
