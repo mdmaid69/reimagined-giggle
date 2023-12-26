05f6e45551b49624bf3c815963a728cea372c386
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:50:21 +0000

    remark more attempt sudden steady

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
