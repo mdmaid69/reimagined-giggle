af2fb4d876880b8d609782953ea70e26b93b1905
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:26:06 +0000

    communication but lead burst lesson

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
