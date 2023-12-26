6c5690f058b65e79378e62eb530d226361b06a79
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:19 +0000

    insist still you involve unable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-for i in range(5):
-        print(i)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_list_from_array(array):
+        return array.tolist()
