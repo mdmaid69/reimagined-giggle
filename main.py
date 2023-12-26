392cdd60ddaee4ce0964cb5717f144b239bab25e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:05:32 +0000

    admire flavour picture official relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
