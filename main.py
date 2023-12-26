620fe19e04b2667a3a06f4c1e19ee2baed8abb30
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:08:51 +0000

    claim influence odd leg pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
