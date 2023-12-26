5f8f03bdcda920f72eb1f779f060fc8b09296951
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:30:51 +0000

    risk mixture circumstance detail morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_priority_queue():
+        return collections.deque()
