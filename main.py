5c645d58ae3aaf72ff2330e0683a73bb27b6456f
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:51:01 +0000

    open opinion raise face national

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def square_number(x):
+        return x**2
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
