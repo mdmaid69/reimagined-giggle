db60228331b909fc1a0fc013ec885656ff317569
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:59:10 +0000

    track comfortable size pinch voice

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_str(array):
-        return str(array)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
