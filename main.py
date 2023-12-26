435e6cdff1f24a0e0c847b47c9bd4fa6cdb17392
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:03:48 +0000

    way lost instrument accommodation announce

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
