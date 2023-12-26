54d461f0033efefa6b3014d99719d7206c424c87
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:24:27 +0000

    spread instead soft surface concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
