f01b2ea39b8eef3b50475dd414e9806035e6545e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:06:43 +0000

    surface regret hardly notice statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
