16d0f51d8ec1526b62ef362d939f1493eb548744
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:45:16 +0000

    importance recommend afterwards tool correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_max(lst):
-        return max(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
