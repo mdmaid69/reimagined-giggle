fb33dad11fda6db87853fa647cca063c91cbfd9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:46:07 +0000

    direction gooff spot full manner

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
