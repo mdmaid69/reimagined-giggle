9d87b65baad8fbb158bdaa15aebc0c6f5d03be6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:03:41 +0000

    pass charge dish storm shadow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
