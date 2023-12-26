7cbe1330cd003d553d0fc82b3d048808158428ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:43:07 +0000

    pay go sure final succeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def count_words(sentence):
+        return len(sentence.split())
