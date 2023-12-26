88459d32342244a911e022bbc248fec1c7486eb6
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:38:20 +0000

    combine cause community wind term

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_elements(lst):
+        return len(lst)
