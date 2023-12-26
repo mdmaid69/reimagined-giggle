7ba9e78f1d415246a6163ab0f8a423a934e1f00c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:38:13 +0000

    impressed move twist pattern grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+print(os.getcwd())
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
