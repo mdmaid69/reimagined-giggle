51da5c9d615a2f484d54d7195514793194cc6459
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:26:55 +0000

    touch here expression pin field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import datetime
+def get_today_date():
+        return datetime.date.today()
