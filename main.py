21b054d3a5e4372f541829f2323063c26e5118b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:12:16 +0000

    long fair play better better

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
