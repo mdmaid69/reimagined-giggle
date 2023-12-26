76476baea0a562c2614ebd3c8d7bae90c149269f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:44:17 +0000

    fold snow can block custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
