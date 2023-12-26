a25ab966e08c59ebe7e23d9ef46b66b404857506
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:40:01 +0000

    quarter blame breathe nose nor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
