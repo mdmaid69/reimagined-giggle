5b8612b05d8680c31ef1716a24d6875d34c7a883
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:28:52 +0000

    press certainly content card major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_as_list(array):
+        return list(array)
