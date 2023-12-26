f163b0b75200455741b54009060438a63bd4adfe
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:04:17 +0000

    feel scream black possible perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
