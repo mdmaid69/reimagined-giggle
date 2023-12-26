d5c9061c559e8f96eaaca9fb604e9985f5b4d7e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:58:18 +0000

    combine party getup everybody shall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
