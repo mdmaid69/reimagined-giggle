1d77658abd9d05d329b30e40d8ce2fd004e862e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:00:10 +0000

    broad begin its age light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
