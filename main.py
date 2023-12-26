2f1047dfb59900fa734f2f2f91e301de80d5d15d
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:59:54 +0000

    weigh clue hope unfair work

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
