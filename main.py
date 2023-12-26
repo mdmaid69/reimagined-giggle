d6265ff17a000d1c08f651eb9b6138631f9963a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:23:52 +0000

    correct complain chance cut lean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
