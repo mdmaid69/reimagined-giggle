ee6c3345eb0e62c7c96e0f4892624eab019bd39c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:39:07 +0000

    trouble interest Yy steady lunch

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+print("Hello, world!")
+  def subtract_numbers(x, y):
+        return x - y
