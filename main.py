2363d40560f929c4222148dde1e36daeff1b817a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:11:41 +0000

    constantly confidence brother comfort smell

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import os
+print(os.getcwd())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
