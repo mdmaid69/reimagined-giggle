bc909db2ef2548287eb80294ef794008f938988d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:16:54 +0000

    runaway trip smell future satisfied

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_volume(length, width, height):
-        return length * width * height
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
