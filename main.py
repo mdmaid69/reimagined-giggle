f8a57bac1e501146b745ee893ebddd88950c280d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:43:28 +0000

    single promise bunch estimate flood

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
