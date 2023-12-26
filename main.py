7f3f8ffd576c31c62d5b30d00740aa18cdd9f3d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:45:15 +0000

    tread letter turninto year pause

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
