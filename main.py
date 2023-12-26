6fdb6500f0b16de9b820e30ba436ac50f9d478aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:01:50 +0000

    afford catch any environment judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
