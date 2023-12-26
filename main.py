22e923834c18ad8133e9adfcee5af847c7d840b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:30:20 +0000

    especially mark mind straight whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
