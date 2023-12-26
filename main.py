7dad048f74d03be73777f71725db7b103da786e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:49:38 +0000

    smoke interruption deliberate though clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print("Hello, world!")
