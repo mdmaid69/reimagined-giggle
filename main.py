1b8890950f0fa34fc8a36ebd50d1abac8106fab9
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:33:35 +0000

    involve lookup commit forget somebody

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
