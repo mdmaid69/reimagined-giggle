ca4d5212a7151262fad4b66f6efc71ca102b4800
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:20:07 +0000

    play unpleasant without regularly swing

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def find_min(lst):
+        return min(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
