7da0cf10c495414914e75ee77d066a746163ecb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:27:53 +0000

    emphasize shall up company depend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def subtract_numbers(x, y):
+        return x - y
