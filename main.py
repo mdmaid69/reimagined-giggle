cb6cec354a770a0c6562bef748c2871fef557541
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:57:07 +0000

    angry underneath scrape Ee receive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print(sum(range(10)))
