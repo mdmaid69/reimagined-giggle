d10725d54299f901ddbdd3af91913ad68fb3b6aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:44:09 +0000

    grade on lunch lift claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
