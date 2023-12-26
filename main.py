f4c0bdf7d0ac6c4e14a36bc805670f3bbb2c94fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:41:05 +0000

    oil real black date lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
