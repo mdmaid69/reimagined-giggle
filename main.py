e94972e70214b01dc1b9825cb15d1db096225802
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:08:52 +0000

    person guess turnover thinkover force

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
