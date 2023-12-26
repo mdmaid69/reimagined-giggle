58e7cb47cb447740186e730478a507ff225ebd00
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:24:26 +0000

    wherever praise good immediately minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import random
-print(random.randint(0, 100))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
