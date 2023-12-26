501bb49725436f0da99f60b326d03b8e452a1697
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:47:03 +0000

    afternoon child back string expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+print(os.getcwd())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
