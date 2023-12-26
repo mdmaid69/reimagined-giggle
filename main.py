c307e5eb40bcd3256b70871b019a9cba0db5074c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:24:46 +0000

    worth unite impression new common

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
