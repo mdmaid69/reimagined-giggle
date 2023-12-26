e05db7910c8aceae46299fc8448a6da082977463
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:29:28 +0000

    matter file from convenient suckme

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_volume(length, width, height):
-        return length * width * height
+print([x**2 for x in range(10)])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
