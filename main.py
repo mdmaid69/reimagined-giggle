8af24f7d6fea8e64633a2cf4f7876692d74d8740
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:39:13 +0000

    well choice age lonely climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
