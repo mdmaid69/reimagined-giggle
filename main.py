d540ddc4d999e3211bb890b26a3fa0d8220bcb22
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:15:17 +0000

    calloff act speed replace offend

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
