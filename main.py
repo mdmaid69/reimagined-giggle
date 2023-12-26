7e7b4ae5db6a2eda84e63e9306f4ec3850d19da0
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:48:05 +0000

    win earth reliable runaway obtain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  def square_number(x):
+        return x**2
