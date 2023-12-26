253d9b50071122c5c51efeafa4ad68989cbd9443
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:13:29 +0000

    love mixture extra complete bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def find_union(list1, list2):
+        return set(list1) | set(list2)
