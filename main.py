cdad13b14a20616f5858d15c85e343e49e5e67d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:26:44 +0000

    do for plan feature put

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
