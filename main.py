1555f1e3299406f5ebc93b82aee0c62f93c4e4f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:32:00 +0000

    undo ability than responsibility satisfied

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def subtract_numbers(x, y):
-        return x - y
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
