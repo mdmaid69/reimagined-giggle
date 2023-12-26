700fdf190acf9f429b9ffe3ea9aaa26b460c4af2
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:22:50 +0000

    thankyou hide draw perhaps Vv

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
