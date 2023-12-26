6adc65d669b162e63b45b58a8bc7a0331b719a92
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:25:27 +0000

    warning confused order move handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
