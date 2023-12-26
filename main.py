747e2500a7f00d9193d81d8f5e932eaef82a2152
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:59:57 +0000

    people health bet convenient embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
