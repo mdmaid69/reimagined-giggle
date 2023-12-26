f99d11ef183bf5df8c9c8b2a3b89921193661eed
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:17:07 +0000

    radio advise succeed call arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
