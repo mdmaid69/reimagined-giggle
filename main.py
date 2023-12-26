98b04ae00a819cd8b5ce432501b811eea673a37d
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:37:48 +0000

    lean fit guilty storm description

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_acceleration(speed, time):
+        return speed / time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
