8fd92846896e8bc0e15bbbd54589189b3aa1b05c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:45:27 +0000

    comeoff not study watch comeround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_speed(distance, time):
+        return distance / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
