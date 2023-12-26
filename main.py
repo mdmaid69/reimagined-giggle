021ca5c503d9aa628cdf9b7e039f4a738d70f6cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:52:56 +0000

    union wonder rush ignore ago

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import os
-print(os.getcwd())
+def calculate_work(force, distance):
+        return force * distance
+import sys
+def exit_program():
+        sys.exit()
