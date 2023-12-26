21258c4db5ee4cb4023c3fcffcd4fb5a133a17e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:02:04 +0000

    repair nonsense balance dry workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
