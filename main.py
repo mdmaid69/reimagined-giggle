37f98d54620f3712fc7104917507587d7e19ebda
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:12:29 +0000

    mind weekend minimum runaway add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import platform
+def get_os_info():
+        return platform.uname()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
