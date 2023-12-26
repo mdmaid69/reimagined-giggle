39dc16ab0aaf44e07829e6dccfc6d1432ac7d6e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:47:51 +0000

    return partner here spill little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
