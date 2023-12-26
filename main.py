5b065b9e36ab469adde53d28d9f0685c9ccb097f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:09:30 +0000

    fresh evening fence attempt not

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+for i in range(10): print(i)
