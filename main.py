ef89f976782e192afbef1a4e59722cb1af45fb86
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:28:23 +0000

    all precise smooth force hot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
