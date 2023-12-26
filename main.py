86f90f2d16085221c36b459ed62c2fd5f5c64498
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:18:56 +0000

    pot give sure tongue although

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
