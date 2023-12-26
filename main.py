2828c24371770a96f0a34d75bfa1a9f56ea2da22
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:10:16 +0000

    different kind attach image tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
