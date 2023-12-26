d27ea06be2b1fdd235ddf2c000348f9f46be58df
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:16:31 +0000

    mixture innocent part object sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def find_max(numbers):
-        return max(numbers)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def split_path(path):
+        return os.path.split(path)
