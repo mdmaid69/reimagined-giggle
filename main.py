9fe6db948cea74df35fc54776876bc21fa6277c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:03:43 +0000

    worst phone fair thing mysterious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import math
 def calculate_logarithm_of_gamma_function(x):
         return math.lgamma(x)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
