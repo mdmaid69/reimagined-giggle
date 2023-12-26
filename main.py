693aa9f173cff4f55336605ac047f7f0cc381b84
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:38:36 +0000

    loose cold doctor worry now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import os
-print(os.getcwd())
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
