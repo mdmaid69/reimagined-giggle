521297cf1d8b7c06a784bf949375273249be04d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:09:14 +0000

    Hh reach force hard treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def reverse_list(lst):
-        return lst[::-1]
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
