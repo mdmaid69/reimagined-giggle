3a9aa549b8c298641a03462c44c6105bdbacd670
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:55:29 +0000

    taste open damage exact sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
