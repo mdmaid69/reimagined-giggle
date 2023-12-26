ef95a14e633f4ccbcc44ee1470b28232d883fe41
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:18:14 +0000

    platform onto listenfor wish spring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
 def convert_list_to_array(list, typecode):
         return array.array(typecode, list)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
