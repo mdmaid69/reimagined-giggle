202ace6e7a6e65b78dbbf4f8141bda4e7594a5a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:44:10 +0000

    step table easy woman ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
