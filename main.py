7598007553c1426d48b6f333b2363fde4d2d7d21
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:44:39 +0000

    comeout field few flavour holdup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
