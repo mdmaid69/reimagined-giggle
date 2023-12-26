e6e3cd7fa0dcfa17fbbe2b6446fca54608b6c61f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:03:41 +0000

    fortunate dig bear grade workupto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def cube_number(x):
-        return x**3
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
