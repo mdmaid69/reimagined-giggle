cfe12ecaa112a5314bc6af873f046d92b19c69b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:06:52 +0000

    official boil patience achieve loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
