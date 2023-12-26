2bd0dbf3bf47c1c5ccb914b99e24eb34b9fb17a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:29:43 +0000

    constantly pipe Bb cry still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
