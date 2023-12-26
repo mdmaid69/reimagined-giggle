290f2eac29c48fc121d2f4c3b17747f2794dca99
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:55:42 +0000

    where cheque boil total test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
