5032146a1251bc1cc0cb275832d1ca1b898c1d92
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:31:04 +0000

    Hh respect confuse stiff cut

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import sys
-def exit_program():
-        sys.exit()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
