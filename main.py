906d1dd8f79091fc7b021f0d5a1614d9b2809246
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:29:28 +0000

    both effect wear throughout surprised

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
