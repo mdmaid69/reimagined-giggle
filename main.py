50a30eb80eafab4e226c8be9e954f6ee401aeb36
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:32:59 +0000

    near will on moral earn

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def cube_number(x):
-        return x**3
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
