0bfd383a6006cd22c5d9f1649fa80e67e44b6ad2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:35:29 +0000

    closely problem corner advantage home

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
