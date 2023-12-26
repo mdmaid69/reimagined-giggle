68aba17659be71ba68c338396329c12f321a6070
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:27:45 +0000

    be normal close straight considering

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def is_even(n):
-        return n % 2 == 0
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+i = 0
+while i < 5:
+        print(i)
+        i += 1
