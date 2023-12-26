25cb3632082e7fbd2e6f32adec7e99ecc2e183c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:52:24 +0000

    me month young fit split

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import re
-print(re.match("h.*o", "hello world"))
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
