23db43fcf2a4faf200eab4afbb5568b7965027c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:38:13 +0000

    catch foot whenever best convincing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def divide_numbers(x, y):
+        return x / y
