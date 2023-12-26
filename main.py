6a2585c847dd3a6342866764ed6afb1719a44f00
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:09:11 +0000

    destroy turnon argument foreign on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
