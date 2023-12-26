6270f8ddd2b273a5c4bcdc54435fa0fed54afa76
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:41:17 +0000

    impressive path put joke spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_item(array, i):
+        return array[i]
