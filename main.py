de7295b9defcd1101d3892598b5e0d34fbef777f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:02:50 +0000

    fairly thing even stop broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def convert_to_binary(n):
-        return bin(n)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
