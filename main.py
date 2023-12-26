63e08066099e1eaa3831a2c35da1e21f5c8caf8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:48:54 +0000

    government exploration will there tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_arc_cosine(x):
+        return math.acos(x)
