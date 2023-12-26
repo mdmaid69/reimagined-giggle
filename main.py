0c8fc5e9045c3475333e8965ac89d52a203078ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:51:19 +0000

    rely agreement joke danger narrow

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_pressure(force, area):
+        return force / area
