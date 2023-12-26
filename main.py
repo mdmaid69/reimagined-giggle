fb725cb71fac5d1147a8c85f6836924f98f2d2bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:53:58 +0000

    contents mean organization respect house

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
