1fd54b85f9c33cd04976588ec4c459b222b6871b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:33:43 +0000

    just post joke difference blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
