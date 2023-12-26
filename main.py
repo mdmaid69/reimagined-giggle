e9d13892a5ad54a5a6aac75c25f639cf9093e142
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:36:59 +0000

    so characteristic different neck sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
