1ae4c08f8a5e422dba972feb4180491eb3fd9413
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:56:08 +0000

    traffic cook spite 564encourage consult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
