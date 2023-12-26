8f2f40b047a05f3046b0852ee7abb822cc4892cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:39:53 +0000

    back invitation shape evening turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
