50cd23f2288ab300b1c793aed8d5c124deffcdd9
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:59:11 +0000

    lonely point my angry wall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
