1889174f395ed2515dd8795d2ee703ecf17f69f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:47:10 +0000

    suck wait sticky people receive

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
