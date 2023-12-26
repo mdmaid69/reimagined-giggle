395f54ec31b0d54ef1ba1875613f51176603752f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:33 +0000

    model line stone plenty there

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def reverse_list(lst):
+        return lst[::-1]
