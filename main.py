3a2ab7d915ea26ea00ef13546ada52aeae6f696b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:48:25 +0000

    open spot embarrassing tear trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-for i in range(10): print(i)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import platform
+def get_python_version():
+        return platform.python_version()
