421cd6fc6444718f5ca4165625c474f41fe5d9cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:17:29 +0000

    look beginning either split through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
