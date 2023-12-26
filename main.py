f6e713aeafd49874b9beb754b597b353eb0cae8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:31:29 +0000

    exploration pleased shut tall open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
