af406fec3622d76635340f016bae2a10064982c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:12:25 +0000

    calmly advise discussion port come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import tensorflow as tf
+print(tf.__version__)
