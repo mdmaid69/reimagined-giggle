3e26f959fd378710eac21d166518f7074f098a23
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:37:06 +0000

    minute goon bored when us

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_acceleration(speed, time):
-        return speed / time
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
