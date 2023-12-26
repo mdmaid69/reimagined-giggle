fc717b52f2e5ce1241a36d685bcb4d7cc5801895
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:37:03 +0000

    cut well city step unhappy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_elements(lst):
-        return len(lst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_acceleration(speed, time):
+        return speed / time
