4746dc866de006fc2f22fb8349dd4785273f30f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:43:22 +0000

    smart move move than even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
