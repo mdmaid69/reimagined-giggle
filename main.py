5be2c459caf1ede11edb5378a75f7e1ca72f7395
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:43:20 +0000

    decide wild left lookup slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def sort_numbers(numbers):
+        return sorted(numbers)
