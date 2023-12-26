9f5c384de0dcc33296c59410218ab7df0da732c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:04:43 +0000

    sweet position emphasize callat injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
