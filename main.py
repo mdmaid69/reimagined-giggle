e46dc3e1cce2b2704ccfbfcd0622e7803d39fe54
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:29:45 +0000

    charge discuss answer lend permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_string(s):
+        return s[::-1]
