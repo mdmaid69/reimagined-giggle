3b338687e015c8ed756116733a1183c52c1f431d
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:22:42 +0000

    dress surprised keep rent protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
