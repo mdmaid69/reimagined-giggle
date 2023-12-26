71f7f02b119b2a01f2190b7badfc3aca1c47701e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:35:16 +0000

    relation often ocean weigh chase

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
