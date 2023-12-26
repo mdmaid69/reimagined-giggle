57c43363397900ec178e0931d9fc03f33c4258a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:51:58 +0000

    record rough birthday other suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
