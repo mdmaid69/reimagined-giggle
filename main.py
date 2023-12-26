fc90eccfb4b74f56196fed9c618a1ff920893ac3
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:04:38 +0000

    rank little bush calm confuse

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def divide_numbers(x, y):
+        return x / y
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
