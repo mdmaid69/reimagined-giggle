2987b43c836274e2a251d8d736816e030deb217a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:50:36 +0000

    they music turnon negative translate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
