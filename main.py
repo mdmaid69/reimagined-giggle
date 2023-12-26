bdfb63a2285684eb03a578d5bc329b9d1432b9ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:50:14 +0000

    mystery could object pencil rent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import os
+print(os.getcwd())
+  def subtract_numbers(x, y):
+        return x - y
