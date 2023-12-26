b322bc2b90fdad36cc47827c4aff23f2bf84c5bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:09:51 +0000

    drive cause clean past common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-name = "Python"
-print("Hello,", name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
