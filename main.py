a824dbaf5c5a0abb22856baefb715c2fc5a69bfc
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:49:31 +0000

    downstairs culture regret giveup unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
