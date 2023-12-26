ab06454050203b6e09e21cb3287e18502b521769
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:50:22 +0000

    direction play speech thinkover object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
