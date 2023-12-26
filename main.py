54e5d02795bb209811c6d561638419b9ccdd5b1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:27:44 +0000

    upstairs zero taste cause fear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
