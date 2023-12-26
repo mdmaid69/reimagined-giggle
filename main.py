c7bdf256a9f449409be96b824edb62542ddb7d47
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:57:18 +0000

    pronunciation undo pronounce tape study

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+for i in range(5):
+        print(i)
