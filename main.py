5ef48a223ebf5fa14d18d8f68cf4d1dd1b5bd982
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:51:20 +0000

    person by unfair unable ordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
