3edc8e99b112374ff9bacc9d4be7231d1c642505
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:58:39 +0000

    evening such persuade similar human

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def is_odd(n):
+        return n % 2 != 0
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
