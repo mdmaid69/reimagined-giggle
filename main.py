44bf81f79c4844a6397f58a147ce67d023c76af3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:27:00 +0000

    morning cut dry about damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
