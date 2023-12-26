4443feb20559261e5b6af178ff620593e8340da9
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:34:57 +0000

    fear willing noone load art

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
