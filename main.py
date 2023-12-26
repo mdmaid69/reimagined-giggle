efdac5b12efa05d491379a1ae5f659a5830bad4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:28:00 +0000

    unable factory bit point range

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import sys
+print(sys.version)
