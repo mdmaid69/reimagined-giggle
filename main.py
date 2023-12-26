596478af0801b051ef1212ebc3a90c80fac59c12
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:56:01 +0000

    survive vision greeting loud 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
