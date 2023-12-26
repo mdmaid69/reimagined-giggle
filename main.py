cc270c5326e5c738137f2dfe69b81b7894830cd6
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:54:53 +0000

    personal pattern connected order achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
