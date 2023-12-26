8a35c334db786a69e05fc5d2b41d922c9d1ffd3a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:57:15 +0000

    pay cap worst flood voice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
