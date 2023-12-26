ff996d84d4a688305f6e429f682c052f685c2632
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:24:53 +0000

    build rely stir when minimum

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
