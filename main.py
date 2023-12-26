396d9509f181ddebd6a0cc66bfeb7174e7c0622f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:38:36 +0000

    gooff content try past slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
