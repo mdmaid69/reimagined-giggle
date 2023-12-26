4a34a7fddd8beb93ed3e6026342a42d7f91ca0c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:23:47 +0000

    ball leg crazy experiment related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
