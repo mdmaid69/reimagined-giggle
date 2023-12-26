22309d7dae29389cdedb6cb75e2411ac5783e14c
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:10:06 +0000

    own talk Hh environment do

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
