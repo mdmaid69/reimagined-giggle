e85b9b85a54cbab875d53431626bf42511e09e0e
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:16:31 +0000

    share comment letter correction talkback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_density(mass, volume):
-        return mass / volume
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
