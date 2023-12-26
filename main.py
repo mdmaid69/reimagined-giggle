29a3d8cf35a4c84151dcbc7040bfd929f7393db2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:57:14 +0000

    I patience national sort argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
