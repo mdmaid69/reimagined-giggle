6f72b5a5a35a97b2a561e05d97c699a3f0b7fdbc
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:21:19 +0000

    luck race city grade school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
