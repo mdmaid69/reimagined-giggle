7f9a2cf39780b9e8047cb008f990f92ef5af8871
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:30:55 +0000

    tap famous favour shock accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
