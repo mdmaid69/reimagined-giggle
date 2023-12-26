575fba2d40db0c9cd03f64280b700ff6e6e84abc
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:05:42 +0000

    Tt left half new old

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
