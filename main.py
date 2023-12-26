951dcd46e4153a76aafcce97051d4e784e8aab76
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:22:25 +0000

    familiar promise leave more stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def add_numbers(x, y):
-        return x + y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
