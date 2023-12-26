5859e4eeceaed940c139e7136f740cb3a7bba308
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:46:58 +0000

    drink workon back warn attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import sys
+def exit_program():
+        sys.exit()
+def sort_list(lst):
+        return sorted(lst)
