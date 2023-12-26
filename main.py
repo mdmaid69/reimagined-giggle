99d4dcf165dd06d4964f970f1ef5214a855ea1e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:49:56 +0000

    frighten about dress change repeat

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
