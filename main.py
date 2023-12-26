3f129a88a540395b56f85af9251855bcdef78394
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:12:39 +0000

    so family explain round wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import collections
+def create_priority_queue():
+        return collections.deque()
