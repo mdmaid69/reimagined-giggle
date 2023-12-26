eb4d10e6111160ca18d61ed6a368ce271502a5fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:21:12 +0000

    whoever skill news towards far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import collections
+def create_user_string():
+        return collections.UserString()
