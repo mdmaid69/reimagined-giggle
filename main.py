badaf905b1228e1c9249c8bed8ffaf10a129870f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:13:15 +0000

    matter outside strange refer violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
