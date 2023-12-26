01afbf6cd7a43b3282203e689418f0608845e26a
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:09:56 +0000

    exciting stone stamp way Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
