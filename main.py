198e28d4e308aae0f0f10c3ef47eab5309e2882d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:53:50 +0000

    react passenger interest permission frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
