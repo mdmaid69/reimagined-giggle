1cd277dee67cee34e531cc44b4fe5bdf1c58fbbb
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:50:55 +0000

    office interest broken special rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_speed(distance, time):
-        return distance / time
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
