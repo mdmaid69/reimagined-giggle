bdfc9e59589ac764f2aae440e4feea6677d0ab34
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:05:24 +0000

    kick exercise satisfactory compete spin

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import sys
-def exit_program():
-        sys.exit()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
