1f580caa61f9a44db9958534365d444cecc53109
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:30:26 +0000

    off term reason bear breakout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import sys
+print(sys.version)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
