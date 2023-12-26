f96d6e2fb7cc3b21f1103bb040bfb603609e9ffb
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:00:21 +0000

    persuade think throw straight centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
