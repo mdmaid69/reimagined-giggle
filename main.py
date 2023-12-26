dceba56e15bb22bfbcee9095188259626b74e282
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:22:32 +0000

    connected phone show charge may

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
