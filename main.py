c985fc1452f9b3630d4df65f1f02788a721bfb29
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:00:16 +0000

    step tour smell its birth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
