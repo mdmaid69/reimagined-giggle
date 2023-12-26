8d14d924a7c3c1e322d5b03d2445d922e762aa7a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:45:25 +0000

    Tt cheek name task mess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def find_max(numbers):
-        return max(numbers)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def remove_duplicates(lst):
+        return list(set(lst))
