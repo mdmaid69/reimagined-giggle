6dab17acc19c724fff8c656885eb2574b8217e76
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:08:04 +0000

    partner instead mine picture break

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+print(math.pi)
+import time
+def get_current_time():
+        return time.ctime()
