7317ce6963d1a1532b01f9ce870c2a9671db1b81
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:09:35 +0000

    dear of ticket cheque odd

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def multiply_numbers(x, y):
-        return x * y
+import datetime
+print(datetime.datetime.now())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
