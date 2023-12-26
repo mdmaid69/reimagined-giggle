40541614f89378c7293e6cf171aaa6d3b5a1bb4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:54:40 +0000

    guess underneath every right allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
