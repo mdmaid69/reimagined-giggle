1bd8e73cf96860346e45114afa93e38910143282
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:19:04 +0000

    proof with minority avoid depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import re
+print(re.match("h.*o", "hello world"))
