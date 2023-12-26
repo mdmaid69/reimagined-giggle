91fa8844bf3b09329dcff6c02ced5008d1f53c20
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:51:53 +0000

    later cut clothes class mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
