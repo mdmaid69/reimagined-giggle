6462cbb31dba75d7890021c26d68ddedfa8f9917
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:24:58 +0000

    keepoff mail make genuine early

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
