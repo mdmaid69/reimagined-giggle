cf9bccd6bdd054b1c781c93c0805af879960aa2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:37:52 +0000

    pause clothes fight at shadow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_tangent(x):
+        return math.tan(x)
