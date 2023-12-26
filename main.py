823e875b7bf65da961176d13627d79beadf3fbb6
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:40:58 +0000

    attention against gooff safe come

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def add_numbers(a, b):
+        return a + b
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
