51f785638ff745c2ba1774049c0615c5c769f1d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:25:09 +0000

    off far roof gooff pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  def convert_to_binary(n):
+        return bin(n)
