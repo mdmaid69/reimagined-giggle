3613db26ce7caa38a04c631a0fbae94d2e007ffd
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:16:25 +0000

    contact ashamed may over answer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
