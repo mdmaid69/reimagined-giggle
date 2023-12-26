6012b05233081d2cce08c7ae707003015ac63b56
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:50:35 +0000

    turnoff rubbish steep side pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
