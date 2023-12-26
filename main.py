9d742ec0e20fd77b6e44cfde78653b8dac6e4608
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:06:51 +0000

    pick black official length alive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+print(os.getcwd())
