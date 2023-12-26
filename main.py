bc4988ca8e5127c3d3711bfa1a5857aaececb492
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:25:38 +0000

    grand interesting sort especially year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_bytes(array):
+        return bytes(array)
