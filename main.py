d01aa0db6517b15703fe94748c6dc0b1824281af
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:30:44 +0000

    makeinto leave pile total without

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
