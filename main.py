a867447513d8242d50f85e88ed3feafbca12d053
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:03:55 +0000

    place 565end spray put cold

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_min(lst):
-        return min(lst)
-import socket
+def sort_list(lst):
+        return sorted(lst)
+import unittest
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
