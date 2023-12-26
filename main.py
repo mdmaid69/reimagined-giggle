2c29553c8eea4ff80e5cc7d2b2611e5beb0c825f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:17:10 +0000

    face progress precise drop uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
