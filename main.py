d1a6859782f5690da9baba297b8012bf731aaeb8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:24:28 +0000

    pronounce woman female over safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def add_numbers(a, b):
-        return a + b
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
