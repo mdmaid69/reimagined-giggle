dee848f87a67ffbf253343c54c2b3029d7ef3ed3
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:31:21 +0000

    treat so speed Tt famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
