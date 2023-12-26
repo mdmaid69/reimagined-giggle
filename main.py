d4e3a96dee42e9ec2b234fb495927b97afa0c6e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:41:14 +0000

    telephone runover stop arrive tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
