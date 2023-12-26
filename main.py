e40fac46d3149babf6a40bfac630c34e46e04cc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:49:00 +0000

    unhappy stay fly 564encourage right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
