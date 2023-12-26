92aaad812497d81054421c307a6e812fd33c5c9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:58:56 +0000

    favourite guilty medium kind touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
