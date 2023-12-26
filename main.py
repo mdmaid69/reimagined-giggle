0e9e156f36e59b3e55928175dd19466c12c747d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:07:52 +0000

    collect considering holdout sound car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
