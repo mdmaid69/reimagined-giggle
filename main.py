8d10d820259310c6858f9195fbae577a82cb8485
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:57:20 +0000

    scrap lay day getaway disagree

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
