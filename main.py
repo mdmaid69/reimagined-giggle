1cc1c0e35687ac7405915058b2bcdd03b13892cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:43:03 +0000

    prevent convincing back form luck

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
