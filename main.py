3789422e404620131df89cdac13000bca80987bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:18:29 +0000

    splash label answer strike protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
