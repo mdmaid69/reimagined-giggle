0bf46b8a2142ad6f0e19692fa871fa8295c0836f
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:01:20 +0000

    lock boring dead modern either

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
