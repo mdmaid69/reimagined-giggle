4712818fc088319f1ec5f93bc9def47d42107077
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:13:06 +0000

    search memory ground write free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
