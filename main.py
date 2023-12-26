3848af74a1d22aae26c311f5f3a91ad1caaa75f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:26:38 +0000

    free suffer backwards letter light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
