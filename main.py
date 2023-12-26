2ee1cb560fec0469ba0eac255627f92316ba92ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:46:23 +0000

    whistle risk start believe relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def sort_list(lst):
+        return sorted(lst)
