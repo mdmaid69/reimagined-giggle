f23394a52d90226d181914fe1e38ca70e4f60b40
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:58:08 +0000

    ball before forget feel survive

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
