cf1299df5b1dda68df75308d56816225b5377944
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:42:33 +0000

    period frame stone point fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
