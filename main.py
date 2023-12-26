35296cb69fc0a1d6eaf384ffab04d9cbf7dd83f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:15:52 +0000

    flood tired fill table under

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def get_array_length(array):
+        return len(array)
