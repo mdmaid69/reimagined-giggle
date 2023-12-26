65ae7c8f03a92dd428af7658ab2d3176db53db43
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:40:14 +0000

    offer cook hot sensible everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
