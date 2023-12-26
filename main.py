0bf9262be8a6161b4b999b65ae0cb525326358cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:14:26 +0000

    accept injury oil there achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
