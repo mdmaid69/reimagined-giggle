bd13d3ed892e6f71da7474cdf466b533d028c677
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:54:32 +0000

    before relax nor negative experience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def sort_list(lst):
+        return sorted(lst)
