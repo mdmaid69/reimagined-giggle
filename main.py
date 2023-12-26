18c102d1aa39b9668960709061b75182d4b328f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:38:15 +0000

    allow stare 570enjoyable lesson roof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def count_characters(sentence):
+        return len(sentence)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
