868a62d680398177c3536cb95b43f6f79bd819a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:59:46 +0000

    manner imagination face hit eat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
