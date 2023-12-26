9bdf522350c22479be7bf8ca09ce10aedaf584c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:19:32 +0000

    dig goodbye instant turnout general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_memoryview(array):
+        return memoryview(array)
