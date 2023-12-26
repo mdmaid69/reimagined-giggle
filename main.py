e4c8fa919993a43204cd374d7a5c06464c17808b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:08:35 +0000

    ourselves exploration powerful must unusual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
