e860c7936e6b6bc8f2e1129e0a75fcef1b00ec40
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:50:40 +0000

    turnoff open foreign straight wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
