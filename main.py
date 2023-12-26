dce2fabafa3220a5d5d3dbc2c6fb2149c7d8ed6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:57:52 +0000

    confusing into rid mean will

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
