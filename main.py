d12a403edd7337cbb67d514cfcc649c44ac6fac0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:53:44 +0000

    plenty sorry together mass runinto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
