2ddbaabb374c755c0c1e7395f084d6ea99c381b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:36:31 +0000

    major right nose spare modern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def split_path(path):
+        return os.path.split(path)
