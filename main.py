9c9a008ce6ecaf5192f3bb17e15aabe7dfbb341c
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:48:01 +0000

    bottom honest excited advanced wealth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
