cd2ced210b305cee5c4f32413111edf8f053421a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:03:40 +0000

    straight wipe answer between start

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
