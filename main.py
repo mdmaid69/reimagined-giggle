5e372c85f4ee46e054a5b5615df6063cda995571
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:23:34 +0000

    age university Nn 569enjoy comeback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
