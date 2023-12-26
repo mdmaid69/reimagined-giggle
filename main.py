2cb8e67a75656fe4325e66a8d140e69511713eb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:25:35 +0000

    beside this turndown ability seem

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def add_numbers(a, b):
-        return a + b
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
