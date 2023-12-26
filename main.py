57c388c614fd250ca23074db3d04d75487cf76b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:31:27 +0000

    pleased comeround finger judge case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
