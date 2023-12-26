4ccef9cb9b51d8cb58a1fb0bb8c2978c1a8094d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:20:05 +0000

    tool love might relax trade

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
