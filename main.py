dc35a6c7639bb87a678aa73bf43002d6b19f1fe2
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:28:14 +0000

    ours pity bury somewhere delight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
