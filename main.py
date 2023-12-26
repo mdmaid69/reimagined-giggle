cd65b5a67c11d1bb02a433540cae3f61a1b2987f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:21:19 +0000

    help blame whenever attention read

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
