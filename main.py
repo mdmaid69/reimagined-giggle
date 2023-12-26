0fb2e85339f75227a15e17ef292c1fcf8fedfb01
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:22:17 +0000

    difficulty machine weak hard object

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+for i in range(5):
+        print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
