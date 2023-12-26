29370c9cbd1b3af8207efe8fc781178ab97f9503
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:57:28 +0000

    praise minute sign which bottom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
