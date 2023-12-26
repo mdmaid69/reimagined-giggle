fc52d12dc7a7b17974db3d95586c648d090624af
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:14:34 +0000

    break take often week comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
