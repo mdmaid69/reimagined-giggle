34a65c4266f720f832c3421f652038aba70ca192
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:41:41 +0000

    away physical distant explain crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
