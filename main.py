7093bc8782fedea6ba50055c07446d8761e71767
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:37:25 +0000

    party like sign start dream

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello, world!")
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
