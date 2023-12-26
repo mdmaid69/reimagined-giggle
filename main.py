504b64998ee7ec1782c93d7b3d989668a88d4a6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:32:29 +0000

    guess suppose ability compare on

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
