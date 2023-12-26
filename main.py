18380fe050efe0d3a9a7a8e70adad5534d5db143
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:29:42 +0000

    state through could honest turndown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
