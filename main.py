96e28471daf610c3096ed47eccd19dd50e271bf0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:21:11 +0000

    lead act include label sky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
