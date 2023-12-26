8aa239d995eabfd63fc95d9ea3f7ea69133991dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:17:40 +0000

    subject brother cut getback gift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
