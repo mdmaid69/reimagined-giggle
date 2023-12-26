f145e4d533ae969222b8a0b8a32172b703a5e6c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:56:03 +0000

    then back squash Qq floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+name = "Python"
+print("Hello,", name)
