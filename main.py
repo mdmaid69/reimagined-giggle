c7634bcc8ad88d56989328dcc6e735458c19b6e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:17:09 +0000

    workon write study mother survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_list(array):
+        return list(array)
