5478c96b574a4d872eb341dfe38893db7afada9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:28:23 +0000

    cold radio Ww sure suddenly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_speed(distance, time):
+        return distance / time
