d9f31c1d2d922d7a08059cf4c5b31e37379e1683
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:16:52 +0000

    home sister smell turninto aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
