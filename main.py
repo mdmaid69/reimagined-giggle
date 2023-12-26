160e74dccf26b5ba30883c508f0c36466a0f3972
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:22:06 +0000

    machine special smile friendly conduct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
