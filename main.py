47a5f2d442786de3d8986dc369fe29d53640dfbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:09:32 +0000

    close makeup succeed move decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
