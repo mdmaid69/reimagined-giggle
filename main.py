ec155527ed8a958dc11f8336f2c95d1969aa0945
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:36:30 +0000

    rob keepaway achievement score spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
