a7b5f386679e796d9b8780140022871d7c8f700f
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:23:38 +0000

    thinkof know secret crazy fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
