529bcbc866ff3eb431eef63a6bad090cd5669cd1
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:04:42 +0000

    lesson politics recently comefrom around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
