3da0cdaf489f5a1b4dcfe0fd8ca3d755bf7af638
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:40:20 +0000

    left until generally test getup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
