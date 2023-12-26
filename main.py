35eb8366c409d7f5deb53201597849557f90a3f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:46:21 +0000

    past stone after shadow when

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def split_path(path):
+        return os.path.split(path)
