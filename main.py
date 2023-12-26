d1117f98408505db1d3cfa09f3a9e31612b20215
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:13:51 +0000

    fetch which false order survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def divide_numbers(x, y):
+        return x / y
