4dbf3550a56d8ac5b8d5798891bc5dd492cb1cb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:37:27 +0000

    roof too may apply custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
