b15fda24938e8a47da4393a03f80f4d5b2bdad59
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:11:16 +0000

    jump between space ahead name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
