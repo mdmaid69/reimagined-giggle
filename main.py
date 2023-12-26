fd20503e846e4c814ebeaa61b37320654ec3ff03
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:53:17 +0000

    positive shape thick past most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
