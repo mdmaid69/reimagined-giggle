7c50e478c116afa57e1f87a753e099e81ddfefe2
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:15:39 +0000

    error ashamed treatment birth tired

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
