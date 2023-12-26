8a98b29697e95e43357c96e4f05d4a1c9356738f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:56:44 +0000

    back stretch mine switch makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
