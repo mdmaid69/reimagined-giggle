d716cbe2c2578586b9450c81721183dedfd908ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:45:40 +0000

    bored place respect fault problem

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
