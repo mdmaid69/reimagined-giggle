10ef0f33c2545d285eab62db90d8edea88926c1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:17:39 +0000

    go prefer until she forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
