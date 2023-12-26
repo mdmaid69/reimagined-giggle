9a118ff46f0cfaa1e579fd4ee842af1b30f8438c
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:20:10 +0000

    bath sensitive gift pump luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
