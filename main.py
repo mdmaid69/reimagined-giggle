4d6c92f1bd5e8c94a910a52d68b9389b7939ca20
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:04:17 +0000

    injury almost tone certainly spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def iterate_over_array(array):
+        for item in array:
+        print(item)
