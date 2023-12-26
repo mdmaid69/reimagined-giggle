3bb98a4602185f3fb2aa51497a57d991625c3e35
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:20:19 +0000

    achieve 569enjoy apologizeapologise part share

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def get_array_as_str(array):
+        return str(array)
