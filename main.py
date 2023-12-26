9633cb8325f3896d72a3a0858168655d35a396b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:30:43 +0000

    feature delicate silent colour distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
