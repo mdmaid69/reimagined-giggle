7b233269754c185e0f857e4fe48923bb6fcaf5e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:22:04 +0000

    swallow keepoff emphasis obtain possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
