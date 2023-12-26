b3840c94953fc3e34cbe203a373fe23e212572da
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:38:12 +0000

    recognize sure cost dick swallow

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
