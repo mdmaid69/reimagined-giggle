343684e6976686b729405bc6dd9bbf556fe93c06
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:25:21 +0000

    final fun weak chat include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
