1042a9d5befab9fcd94063a9b536705a2a2a309f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:50:45 +0000

    this quarter possibility date capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
