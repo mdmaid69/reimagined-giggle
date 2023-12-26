8254d1196cb125a6f56cef8a64f349091ee5a9d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:28:18 +0000

    couple bye heavy seriously fail

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
