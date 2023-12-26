80173cdd1822e348df135b3d0104916289491109
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:23 +0000

    also holdup equipment wrap different

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_as_str(array):
+        return str(array)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
