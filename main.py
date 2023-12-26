9dc1d9227a432a358b63529331e4977b08e2305f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:35:13 +0000

    usual bus present sing curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
