df7e605c5f0bd6aa702838a91f21773c7b2daf3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:54:33 +0000

    project sting well combine smart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
