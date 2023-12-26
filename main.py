53c936f9823528efe70f8569492ab5d11e270f49
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:11:31 +0000

    field possession slice scare turnaway

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def find_max(lst):
+        return max(lst)
