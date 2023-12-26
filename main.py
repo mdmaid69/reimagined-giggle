9534e36f78a1be5dd660d75cddf47e29c5f6e1ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:47:55 +0000

    joke us quick luck consult

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
