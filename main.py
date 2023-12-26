7685f0021cc2804919f54e19b03879dd0e299cf0
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:35:44 +0000

    statement talk demand worth walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_speed(distance, time):
+        return distance / time
