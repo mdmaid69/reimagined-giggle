9dddfbfdce767682ef101e2f8c965b3345cff1f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:20:22 +0000

    giveout so mind weekend emergency

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def square_number(x):
-        return x**2
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
