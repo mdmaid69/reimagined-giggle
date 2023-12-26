7554eb47b6ee2d84179c3442a765fec333761d09
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:47:40 +0000

    invitation centre passenger suggestion yet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print("Sum:", sum(numbers))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
