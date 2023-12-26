b315bef1ba24ec926f6f22aeaeb38ad64f5e77b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:53:11 +0000

    basic in gift after promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
