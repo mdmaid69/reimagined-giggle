2505f4a6906521888b5b5e2bde12da142c296b76
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:28:34 +0000

    sometimes about send common block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
