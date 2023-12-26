4d6717a39d3527665411c31836879e295222cf0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:45:17 +0000

    task cut open exercise extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
