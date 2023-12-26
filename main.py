1b60df77ccdfd121915dac752281c3a91c77d624
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:57:00 +0000

    actual slip strict nervous comefrom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
