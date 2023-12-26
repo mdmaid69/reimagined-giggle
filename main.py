7aa11b0220d12454877dff9f20181a6ff48fef09
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:10:26 +0000

    obvious brief satisfied off big

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+print("Cube numbers:", [x**3 for x in range(n)])
