d4ece4b1a7c2eff77662fd4d4f0efbbcf77a7508
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:58:02 +0000

    breathe choice century his appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def subtract_numbers(x, y):
+        return x - y
