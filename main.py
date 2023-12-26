fc824c07474bc0ca8a5debe462f029caa7bf8361
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:43:52 +0000

    claim they fact relax market

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
