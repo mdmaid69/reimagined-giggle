bf52623db9417a4929dacb364218e065c7b985df
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:29:55 +0000

    rain careful wall sound they

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_power(work, time):
-        return work / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+print([x**2 for x in range(10)])
