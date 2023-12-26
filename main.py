8334bb565d98a22237294ee28b5d5eccb585b43c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:24:03 +0000

    injury effect his lucky accommodation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def find_max(numbers):
+        return max(numbers)
