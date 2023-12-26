f67756faf99763e14cb272486af46c268e44cd86
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:28:13 +0000

    hide pencil single fail few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
