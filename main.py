37cefd3d4590accf07a51ed661e557cbb85f1c0e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:43:15 +0000

    balance cry rob music actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_unique_words(sentence):
+        return set(sentence.split())
