b5f0c986c82731b42c65f319e41bec695f55ff47
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:45:40 +0000

    minimum cruelty accident quiet flavour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_acceleration(speed, time):
+        return speed / time
