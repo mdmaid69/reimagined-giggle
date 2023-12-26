fa2ef55ae6ff055db32fb60fc490864bd199a4be
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:27:16 +0000

    regard appearance smart but ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
