9f64b22651e18a2741be3f15804cc653f56ccfe5
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:57:57 +0000

    extra treat this shelter fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
