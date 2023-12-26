e2ae69cebb0f9f385d7086c3417069910de2f872
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:41:45 +0000

    back than hole remove pool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
