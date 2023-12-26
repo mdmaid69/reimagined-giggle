5ee3c9c0d82275ea2f54cf13d5d43cf96e82be29
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:02:47 +0000

    break muscle expensive keepoff rather

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
