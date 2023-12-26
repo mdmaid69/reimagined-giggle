36dd9b7a36d299f6709bba9a9772c9497e1f26c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:26:00 +0000

    confuse keepout Kk convince replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
