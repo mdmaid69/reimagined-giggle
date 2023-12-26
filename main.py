48239af58fb6abdf7c230d393d9847f86acad533
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:47:45 +0000

    I she score speed claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
