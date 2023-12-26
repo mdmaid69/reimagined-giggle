a4b75798812641fe54e9085cc9a7a7d961c864ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:22:46 +0000

    heart except open exchange would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
