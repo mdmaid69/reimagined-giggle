45fd952d102969662a0c8ce1622696cfdef25ad4
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:37:54 +0000

    organize breakdown sink confusing take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
