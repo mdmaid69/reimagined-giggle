e5782254dd476a8646906f7e6a9f6cdc8ba230e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:10:45 +0000

    note risk more kick real

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
