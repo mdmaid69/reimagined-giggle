7ca612c3388db77aeadec0054501b62de9c7877f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:39:51 +0000

    wind up partner behind basis

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
