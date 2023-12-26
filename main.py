2512af40cafecbf16bf258987dde341f24b45bdd
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:11:19 +0000

    ability comedown hit bit cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_item(array, i):
+        return array[i]
