e3e22900162bba07d193d4bb94a1e0d1b57b0419
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:50:37 +0000

    tight objection angry direct back

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
