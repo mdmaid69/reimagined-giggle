c7b7a1ff132d56d8c91cd8b1d42d7396fb8a1d01
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:47 +0000

    by season fall beneath fierce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import time
+print(time.time())
