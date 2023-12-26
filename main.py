bb81f4448088727b1b6fad37851236c86c293230
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:58:01 +0000

    alive goback(to) disturbing skin tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
