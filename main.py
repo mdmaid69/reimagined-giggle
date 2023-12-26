9be979ee0414f0b5963a5d41fa5f488469bcf905
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:03:25 +0000

    allow new dry thankyou send

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
