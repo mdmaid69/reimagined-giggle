df49c2626268c983b0ca9d2c105926b422e91609
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:28:19 +0000

    small work as wing fight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
