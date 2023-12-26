3ad64eb8de1d09d699b1ae6208a294ab16d86658
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:42:53 +0000

    shape hang judge burst forward

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
