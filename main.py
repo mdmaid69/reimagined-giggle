b38720cfc4ba3581d69b9ba168a1b2ebdd29a664
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:58:00 +0000

    decision still extra agree comfort

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(sum(range(10)))
