accea8022e96e631cc650202c6d595042cf488fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:59:16 +0000

    them space bored attractive exaggerate

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
