798d0bd53ff5c20bfb4ad70939d2051ba3024985
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:32:29 +0000

    action escape genuine low event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
