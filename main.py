fd2b6428577c5e8437b1e1a0f32132d11d062bd0
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:08:20 +0000

    soft left limit murder word

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
