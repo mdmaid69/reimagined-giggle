fcc1977e5d4a2e0441686b046b25d3047c581e7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:51:03 +0000

    risk right arm protection friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
