e02dd0a6014012683b0d9296c9568146a1bbd889
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:19:39 +0000

    usual warn themselves shame file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
