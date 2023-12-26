23d21e6aa293b2fbbdc9b2ce1ab99e58b73e3023
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:13:02 +0000

    content otherwise bridge getup language

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
