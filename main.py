a73863ca375d7f20c901599328798de5a1388f52
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:11:57 +0000

    anybody goaway influence breathe listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_pressure(force, area):
+        return force / area
