7344b2e4058b13bf2297352312e8383f31933aa0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:49:37 +0000

    port which guess free way

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def count_elements(lst):
+        return len(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
