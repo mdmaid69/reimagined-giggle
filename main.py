95d4c8a6268f463b5d4e5585932bf6175321e989
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:42:02 +0000

    my light wild risk build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
