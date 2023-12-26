e285f0606f95055c049e8a5e1509a3ce38e9bb40
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:49:57 +0000

    pleasure command press should Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
