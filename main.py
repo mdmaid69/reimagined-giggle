cd9492bb700ae9ef449f75309978d60acb352c8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:54:40 +0000

    preparation behaviour shame classroom memory

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
