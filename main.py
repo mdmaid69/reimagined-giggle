36b74553ad415fb95e62e0b581b37b8f01f400e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:19:24 +0000

    bound smell shine beat hate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
