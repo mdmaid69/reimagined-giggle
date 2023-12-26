ae0521d3e95bdf2683abe51964e7ad60dfc45195
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:39:36 +0000

    dig pretty contrast modern worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
