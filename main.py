d8edad52ad8c32fd69851b7a5bdf9a5ee61bec71
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:07:33 +0000

    feature by mean cost loss

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  def sort_list(lst):
+        return sorted(lst)
