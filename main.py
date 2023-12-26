7d02c5e7e7eca009d34f5827cfda055b5f3748d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:23:48 +0000

    slide listenfor callup swallow fortunately

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def subtract_numbers(x, y):
+        return x - y
+  def sort_list(lst):
+        return sorted(lst)
