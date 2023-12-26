fe8d7fe51f440faba0f914753e8c611b2b71efd2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:04:18 +0000

    smooth match cruelty over risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-x = 10
-y = 20
-print("Sum:", x + y)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
