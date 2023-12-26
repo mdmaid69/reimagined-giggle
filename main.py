af93fd39df64a775e13e02d982fb21023dce89cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:03:00 +0000

    date knowledge recognize party bottom

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
