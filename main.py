3617001fb1a40a19c90bbe08d40a079b38907e35
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:34:46 +0000

    thankyou tread glass unpleasant uniform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+from collections import Counter
+print(Counter("hello world"))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
