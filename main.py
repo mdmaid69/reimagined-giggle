feb5f8380f239454160a37b2f9019de53cfb65ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:15:49 +0000

    event trial unhappy giveup convenient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-print("Hello, world!")
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
