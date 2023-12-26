d79e10cc492b6bb7565d79c9d4de62e79d6b34f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:02:30 +0000

    else suffer miss deliver treatment

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def get_array_as_bytes(array):
+        return bytes(array)
