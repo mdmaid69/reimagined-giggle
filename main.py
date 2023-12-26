57695b4008935555ff7a28f30a7b205cadd35995
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:14:41 +0000

    purpose cook takeoff something silent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
