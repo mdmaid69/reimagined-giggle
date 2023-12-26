9625fed948a99f517d3d15ae0b5bf0d417d94de9
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:43:05 +0000

    transparent equal crash visit fetch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
