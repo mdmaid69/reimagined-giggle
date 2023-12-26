724e8f1fe795e50aed5a99cf2566a2d60acae637
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:21:02 +0000

    wing Nn bringback delight think

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+name = "Python"
+print("Hello,", name)
