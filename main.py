34a461a77e397f29bd23b094c166303180a5009b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:28:25 +0000

    snow them tidy experience wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_even(n):
+        return n % 2 == 0
