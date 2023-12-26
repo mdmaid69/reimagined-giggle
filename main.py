feb4a3f1b4f462d6cfba2b6d14ba4215a7d5375a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:18:54 +0000

    tongue performance problem reply wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def split_path(path):
+        return os.path.split(path)
