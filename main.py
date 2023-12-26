f5cdec1f38d93ea9a4580f3f51d9e4e4749c949a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:37:10 +0000

    callout variety tape shout shine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_str(array):
+        return str(array)
