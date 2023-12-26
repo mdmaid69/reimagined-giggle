dce3ef99f8b1394d24b7d32e2ae805fbae014d06
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:16:06 +0000

    Kk care black wish everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_acceleration(speed, time):
+        return speed / time
