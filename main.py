9323cd9aa93eefebf5472a593bb4705e2ce40fad
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:11:26 +0000

    star let card deaf last

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
 def calculate_current_ratio(current_assets, current_liabilities):
         return current_assets / current_liabilities
+def calculate_work(force, distance):
+        return force * distance
