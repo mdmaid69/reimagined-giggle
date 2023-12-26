b6627a08c4ec77d5bb82c96ff006cc37773a28e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:31:57 +0000

    interesting accept scream remain as

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_unique_words(sentence):
-        return set(sentence.split())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
