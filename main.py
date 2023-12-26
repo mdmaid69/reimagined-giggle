a193600d624ee775b1be84bdc55e511bdf931c96
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:59:56 +0000

    judge which opportunity reason maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_density(mass, volume):
-        return mass / volume
+import logging
+def log_message(message):
+        logging.info(message)
+def square_number(x):
+        return x**2
