4caa6f9b43fda37ec67ee397111f90bd41fd9a91
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:44:37 +0000

    fast approval imagine study slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
