2d7aba00dc873680bf766a268f2a3987c43dbfcc
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:14:50 +0000

    miss maximum skill swallow seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
