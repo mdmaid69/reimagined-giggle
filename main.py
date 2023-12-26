c7159a75198a71a42e12fe63c3f750e06b49e0ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:59:50 +0000

    heart fairly still tune meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+        return os.stat(file_name).st_size
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
