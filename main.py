f40be07fd952253b43eb25f9fe92e752ea0dd08c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:34:44 +0000

    valuable bin instruction study mine

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
