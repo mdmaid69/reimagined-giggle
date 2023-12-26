ccdf7187b1b7e4d201ca347d2fbf404592993e64
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:15:08 +0000

    illness serve birth pity comeout

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_area_rectangle(l, w):
+        return l * w
