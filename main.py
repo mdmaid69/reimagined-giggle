41d0c4a446e2bbda41c0732a847afa13a80feb15
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:40:26 +0000

    touch comeover communicate environment I

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
