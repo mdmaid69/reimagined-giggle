72ab08faefa7e4acd41a3ae5b8444eaec2b9af93
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:02:02 +0000

    down arrangement branch process callout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
