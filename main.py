078c4f1de94d5687f64fbd1b3aae2bb8824e2d0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:23:50 +0000

    use uniform flat foreign evening

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
