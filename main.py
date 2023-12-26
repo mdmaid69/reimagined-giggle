316969dc3a16b89e8fbd02d0b27a031d75779fc5
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:24:13 +0000

    silent thought shop acceptable play

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
