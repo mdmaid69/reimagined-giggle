ec14c7d941960adc9c59b21c54d066419140750a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:30:31 +0000

    567enemy keepoff confuse accept programme

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import time
+print(time.time())
+def calculate_acceleration(speed, time):
+        return speed / time
