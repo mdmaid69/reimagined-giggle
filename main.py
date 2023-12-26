00b094ab19e5db83e663924aacc8cff34810fdf7
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:08:19 +0000

    judge suddenly set against force

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
