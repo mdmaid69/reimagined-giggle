0b62cf937b4663f4bf479ee71f273dce7a2b8ca8
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:26:59 +0000

    butter event his appeal describe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+text = "Hello, world!"
+print("Words:", len(text.split()))
