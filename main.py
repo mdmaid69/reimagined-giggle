e0a40549b265724caf7fa151c93082182989e39a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:23:25 +0000

    contain emphasis benefit telephone break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
