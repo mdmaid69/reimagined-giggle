d25415d34ec008e56ff3c6af9f17223ec530dc56
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:55:17 +0000

    goout young people whole ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def square_number(x):
+        return x**2
