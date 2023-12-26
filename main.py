9f1e9664d8fd049c530ee332af44426acce26a59
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:23:51 +0000

    church easy book sad door

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def find_min(numbers):
-        return min(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
