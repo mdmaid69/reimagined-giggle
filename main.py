f91b2d306de49794e944a5f9d0b1713bed305566
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:11:08 +0000

    air secret mark cheap introduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
