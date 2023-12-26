bf7aea7b99b9c5fc84dc63c276d027fae73c2b4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:37:20 +0000

    duty top feel loose once

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import array
 def set_array_item(array, i, item):
         array[i] = item
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
