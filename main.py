d36412ac94a517f01ac3f847ae64d7f186280201
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:49:50 +0000

    572enter content ashamed custom evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
