63ce96dadaf12b7b30bbd02a5e8021f85c8e5432
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:55:19 +0000

    for deep last ask guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+print(sys.version)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
