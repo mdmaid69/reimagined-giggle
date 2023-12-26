5740643b661e4f1c3dfb3920c9412fd515567755
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:06:03 +0000

    blind match estimate defeat pleasure

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
