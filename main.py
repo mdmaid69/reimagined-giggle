47f0b4a2208609e1d4a944a82a74097e83a91942
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:15:25 +0000

    great label mind remark demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
