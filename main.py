e50aea497c1f56e31a2e15707e4f2a13f36065ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:31:56 +0000

    find nice prefer demand please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
