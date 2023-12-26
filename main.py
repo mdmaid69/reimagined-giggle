580070a8f8cba56824cf85311f83318038967d64
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:32:58 +0000

    difference suggest reduce watch closed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def count_characters(sentence):
+        return len(sentence)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
