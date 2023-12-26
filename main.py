fa28ccb5a1ca8ae4723f398968032e95f036476c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:51:16 +0000

    software under drop bridge opinion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+print(os.getcwd())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
