b0d9d8d9e2f6d5eb44477c0cfb3d6b13979b7b9a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:25:18 +0000

    statement build switch scrap suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
