656082d8c2c43ea545d2b46652115617d036799a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:38:00 +0000

    consult break correct runinto get

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def calculate_power(work, time):
-        return work / time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
