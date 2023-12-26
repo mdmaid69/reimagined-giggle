04231644fc479743bd418e92f57e3cfd6d0d344b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:59:39 +0000

    fix bet shop soon attention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
