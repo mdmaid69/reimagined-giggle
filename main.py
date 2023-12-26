b4017a9b5a8ab95351c5d377bedb9ede58dd95b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:52:56 +0000

    responsibility importance dangerous surprising top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
