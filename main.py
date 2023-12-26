dadc6c7def86a4ff026a7942c50a20cc816ea44b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:08:15 +0000

    absolutely 573entertainment take record couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
