61634228780a62fa488db382ff9ec989c120aaa2
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:05:44 +0000

    profit second impatient trade meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
