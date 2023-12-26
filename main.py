27c5b1c52a2027039fa936a3b2c9cd91de6e2b4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:33:54 +0000

    question history hello tape over

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
