d6ecff38c10ca3f32e312e4ed27e9906cd36fdee
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:28:44 +0000

    Ww degree increase line expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def remove_duplicates(lst):
+        return list(set(lst))
