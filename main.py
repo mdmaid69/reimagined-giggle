ea9d04eee872352f06c77851f00e48acbbd16956
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:51:01 +0000

    television demonstrate instruction race full

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import random
-print(random.randint(0, 100))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
