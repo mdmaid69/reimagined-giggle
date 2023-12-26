b27a49682b5a513731e614b60187e97ed2f3b757
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:07:32 +0000

    look comeout bread secret clue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
