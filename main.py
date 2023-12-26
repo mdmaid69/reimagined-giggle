62703d8b23043a5246cbcf75143502ad9d6884c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:32:55 +0000

    tidy holdon sometimes trip slide

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def convert_to_hex(n):
-        return hex(n)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
