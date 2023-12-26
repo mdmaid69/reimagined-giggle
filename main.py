10ee90df94e4d9ec7d6c8bfe7a3f057343aa0e6b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:08:39 +0000

    cheek getup skill bunch rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+print(os.getcwd())
