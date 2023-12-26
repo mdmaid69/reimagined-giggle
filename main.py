4b212a00f3c817956f833c51f84c254804b4899b
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:20:21 +0000

    square afford light favourite black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def convert_to_binary(n):
+        return bin(n)
