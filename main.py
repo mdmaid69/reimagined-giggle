e534a6ecfb6ad9b380141e721e40f89fe532d1df
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:47:00 +0000

    loan pretty pretty tone price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-x = 10
-y = 20
-print("Sum:", x + y)
+import os
+def change_working_directory(path):
+        os.chdir(path)
