1b29e7f0ca963df3a70eead170647853d35581f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:02:09 +0000

    moon bother ring relax branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
