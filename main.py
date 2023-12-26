c5e50932872a09e2d603d6e4e45c4ccd9395edb9
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:25:26 +0000

    bottom lose pour matter read

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
