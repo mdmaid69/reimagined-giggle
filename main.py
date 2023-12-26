e92ed8b11b594c7498aa84e2d9482be2b257709a
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:15:17 +0000

    account question flat mind most

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import re
+print(re.match("h.*o", "hello world"))
