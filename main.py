693f6d44a1f67e7f30a248c159dd5cf77bd52628
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:07:57 +0000

    drop arrive pure similar return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
