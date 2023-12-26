c779130785d8477444b48f2935b95d60eafc701d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:56:45 +0000

    body do hardly cut process

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
