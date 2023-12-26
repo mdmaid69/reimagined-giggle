0b27275b7e2a06af5cac399840512b151fb6a600
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:37:43 +0000

    scared runover coat port school

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_speed(distance, time):
+        return distance / time
