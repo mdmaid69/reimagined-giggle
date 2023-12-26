2b83e53daf3618ab94cfdfb7e6e4036d153b1f72
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:02:40 +0000

    habit loose present touch uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
