33b46c21882a70b3a56d11de76c19a5824140f44
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:13:31 +0000

    phone personal future name half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
