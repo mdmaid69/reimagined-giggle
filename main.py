c954acb956ec3ae6c8da0008f36a710e28757a15
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:44:52 +0000

    nor loud powerful sensible especially

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def greet(name):
+        print(f"Hello, {name}!")
