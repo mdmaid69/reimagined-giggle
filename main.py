a7a7b3114cdba158e52247b25fb06ca4b7bb9f3c
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:17:38 +0000

    his send print unusual ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
