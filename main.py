89c16a04558805bb5c70bac3bef4fe9a04c516d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:50:03 +0000

    force tongue sure collection nose

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
