8ae1812f10b89f0d0abd766f995c9f6d476b9d52
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:23:59 +0000

    willing minority secret decision behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
