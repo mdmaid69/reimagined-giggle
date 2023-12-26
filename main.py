94893398ef99224a9638695d463f8526f73b7341
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:05:40 +0000

    sit attractive able news mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def add_numbers(x, y):
-        return x + y
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
