608aaed918da219117daba611f6cde983e267cb6
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:58:45 +0000

    method poison direct like manage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_acceleration(speed, time):
+        return speed / time
