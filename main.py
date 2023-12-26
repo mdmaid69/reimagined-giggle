1d5f4872531a286e7da1116937befdc3f278e324
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:37:17 +0000

    turnoff see heat plate hello

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import collections
-def create_counter():
-        return collections.Counter()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
