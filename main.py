b01a16b74db8c29df96b7b4f5d3fb92d4d0cb5f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:53:40 +0000

    might lookup make sure passenger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def find_min(numbers):
+        return min(numbers)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
