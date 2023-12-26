35c7c0039fa2b40c4f13dd15a2e66baf5a3fec02
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:22:23 +0000

    mind spend relief shape bit

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
