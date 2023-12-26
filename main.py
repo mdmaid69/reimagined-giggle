52e23f810d18af9a2672713fb457c2affbfb9c66
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:52:48 +0000

    brush feed law appear lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
