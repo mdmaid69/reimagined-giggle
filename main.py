96d581792193296af63466cdc65b4fcdac5d5dcf
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:05:43 +0000

    path keepfrom embarrassing weigh various

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_binary(n):
-        return bin(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def remove_duplicates(lst):
+        return list(set(lst))
