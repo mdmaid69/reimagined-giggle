8c5a12b47f4683bdcb0cb3baf8d7ace1b6bff67c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:24:11 +0000

    key finish cold accept message

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def cube_number(x):
+        return x**3
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
