9150b0b4730efc3d71b97d64a7291f37c6b4be1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:00:05 +0000

    scream foot tax style dangerous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
-def remove_directory(path):
-        os.rmdir(path)
-print(sum(range(10)))
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
