ed11d7e65267455f9e604917bdff9bdc4c000e4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:30:39 +0000

    drop reasonable minority divide mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
