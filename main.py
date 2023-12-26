1cae57d79a5da925a51558d2969b92b3a9f82db9
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:07:37 +0000

    pump reply silent cold science

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import os
+print(os.getcwd())
