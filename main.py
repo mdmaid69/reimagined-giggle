938e88196b4420ddd6a2f0d6ed78af2584c7f739
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:40:08 +0000

    account certainly tidy next admit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+for i in range(10): print(i)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
