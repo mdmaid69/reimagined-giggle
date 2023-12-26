f2d7f8af8fdabaf44762e66dbf9845c6c583b30c
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:01:49 +0000

    translate stone expression badly Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
