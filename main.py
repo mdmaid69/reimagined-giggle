6868cb95f4bae4551c33d0616560c33969166edc
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:02:48 +0000

    quit row hand runinto switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def convert_to_octal(n):
+        return oct(n)
