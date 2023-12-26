fa2b3ecf4eadacaf9c58258d0c5d92cd940d4052
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:12:21 +0000

    record final for comfort calm

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
