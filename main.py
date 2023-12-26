6e49ca5954d77e085b2ff3d231838fd165953175
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:48:34 +0000

    excuse contents argue worth set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
