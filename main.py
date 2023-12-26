187e341248b33bc70ee48b1f43be69657b4b406e
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:06:17 +0000

    left only voice right left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
