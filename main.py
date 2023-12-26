7523acd6bb670fdac50ec687c9a6e6d14b51589a
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:46:38 +0000

    invitation home brush turnup in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
