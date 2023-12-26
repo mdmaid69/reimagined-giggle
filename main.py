5858b5a01dd2556cea844f820d93114eca75cef4
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:03:22 +0000

    reply apologizeapologise fly interruption unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
