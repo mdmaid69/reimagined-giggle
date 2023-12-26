e56c16af0330b02db2742729f090f35539222530
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:42:07 +0000

    reliable battery disagree group phone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import logging
-def log_message(message):
-        logging.info(message)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
