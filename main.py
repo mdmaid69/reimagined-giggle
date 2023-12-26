20c7de916744726eaddf8dbd10561d54be871d20
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:47:29 +0000

    hear practice consist freedom wave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def find_min(lst):
-        return min(lst)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
