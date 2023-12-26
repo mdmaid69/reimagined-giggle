08d34e8f80984a4e7d99ac8cb8b4ae991707bcb4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:35:31 +0000

    half design swim slide squeeze

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import getpass
+def get_username():
+        return getpass.getuser()
