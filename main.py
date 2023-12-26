ed7a22321b97176dc3de07ffc29386d1f3558235
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:13:31 +0000

    couple cost perform wire positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
