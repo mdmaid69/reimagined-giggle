412992d3f7e4656eba78dc1e068620aa2a209350
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:54:07 +0000

    just along dish cost fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def convert_array_to_list(array):
+        return array.tolist()
