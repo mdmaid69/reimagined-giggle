7539b080d0ea6917bc6fb4b2ce47c245f5a23212
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:58:48 +0000

    pay positive shelter wing surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
