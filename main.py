478f017351fc276545ac1a1d52e0a11ff9859c63
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:46:19 +0000

    maintain until produce small pocket

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
