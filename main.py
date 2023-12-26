52a20982573c1ee88e83bc32c8451fe993c06883
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:46:40 +0000

    beneath trade evidence turnback practise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+from collections import Counter
+print(Counter("hello world"))
+def count_words(sentence):
+        return len(sentence.split())
