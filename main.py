e20d7f8f2cbfaf680701dc2f02b21342b7435ed3
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:02:40 +0000

    shallow journey especially commit morning

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-print(math.pi)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
