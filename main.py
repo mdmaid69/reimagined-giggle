4a2fd1f171760dac685f0a61841b6cc6ec54faf8
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:55:28 +0000

    callout rain behave nerve community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
