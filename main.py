f513a3ec8963ff8664474dda5db6c20026fec645
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:46:10 +0000

    even close production unusual brother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
