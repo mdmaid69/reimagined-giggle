3e1405cfcfb1ab54b37809f47525b7cd02eac68d
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:21:41 +0000

    owe country love imagine true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
