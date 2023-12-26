91467c48ff4efa81f83c960fb171a1556b9985a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:23:14 +0000

    leave exchange occasion easy provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
