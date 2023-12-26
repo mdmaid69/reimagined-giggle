ba451e8f056f05055acb73b73975dfeefad3fdd8
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:37:00 +0000

    today confusing able object agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
