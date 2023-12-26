f3c63fe633899a1b0c134f3b2623bb95cb727640
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:03:35 +0000

    frighten question up confuse direction

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
