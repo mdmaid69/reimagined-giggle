36766fc2bbd6fa0ea603367945435e9207c5b5d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:17:51 +0000

    very finger spray sense except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
