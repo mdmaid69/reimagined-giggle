a169d99f6519d2ec3162a9ab7b3aa60d9217b610
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:18:37 +0000

    definite land high happen pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def count_elements(lst):
-        return len(lst)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
