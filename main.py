58ffcfbf8be1b5893d346e0b5dcfde86e33014d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:23:59 +0000

    damage maximum pot emotion estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
