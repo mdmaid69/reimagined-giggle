7b8c8e8c3291eea5f88b54225bcc25721592cc69
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:56:20 +0000

    quit guess keep order duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_length(array):
+        return len(array)
