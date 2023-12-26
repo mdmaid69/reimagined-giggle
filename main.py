8b18482a17ecf740fcf2a7d9ef7ceae45ad304b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:49:00 +0000

    sad height long properly subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
