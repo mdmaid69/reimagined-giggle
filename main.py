dc5ca867dfeade4648bf0a46f8faeaa7d7a04db8
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:55:07 +0000

    invitation doubt drop ready splash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def append_to_array(array, item):
-        array.append(item)
+def find_min(lst):
+        return min(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
