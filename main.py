dc704117ba60b516d2887cf92f10e5bdceb420f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:09:02 +0000

    favour attempt regret month lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
