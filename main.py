b7ccaeb7e8f08a09c18a4c9de7fbcb82a14aab49
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:38:01 +0000

    nothing lift miss word aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
