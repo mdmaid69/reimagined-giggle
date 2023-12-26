572308250fd27c5068ac2d961a2aa45cb78fa1ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:05:10 +0000

    Yy possession explain base respect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
