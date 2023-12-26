276bd52dd34ec7befda4040d619cc19c2f1cc965
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:33:50 +0000

    return environment description press mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_item(array, i):
+        return array[i]
