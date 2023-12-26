d0f88312e7a24990f94a649a1ded3238aeb64d36
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:59:08 +0000

    war other reply ill comefrom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
