8e19e3d35c68510e732a206fec1e2ad061d007f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:08:27 +0000

    bend shop wire word temperature

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
