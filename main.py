d57c6b8286bf4097e27933420539683ea4ed4cc9
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:40:53 +0000

    interfere avoid contain confused anybody

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def is_odd(n):
+        return n % 2 != 0
