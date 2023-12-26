90ff8bbf3976cf85d1bce204c71b601d27f05afe
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:53:55 +0000

    address realize heat pleasure street

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
