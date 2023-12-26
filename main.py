b9cb043ad68791e7297dca015796646f16ef1cee
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:17:08 +0000

    grand cut past argument responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
