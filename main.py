826af4a3722d705636f4f504e7e739edb33301de
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:35:25 +0000

    intend appeal divide fortunate gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
