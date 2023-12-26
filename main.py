cb39342e8fa5c58c36b0672c0f39bfb478cb92d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:15:24 +0000

    invite load neither whichever jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
