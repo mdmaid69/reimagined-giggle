ca8aad745d195c96ec3d44326407ff98b0f67bae
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:28:00 +0000

    part reliable anxious list praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
