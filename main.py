4c3c59f53a5876ae0fe072620b3bd109450ba600
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:56:43 +0000

    workupto bright last every fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
