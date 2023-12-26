55599395b7033a4b177d0481b343428ef3ab0f54
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:07:34 +0000

    period break win free damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
