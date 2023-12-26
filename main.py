e00407f0a3ff2140bc29c43f729b055a85025778
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:19:56 +0000

    lunch spring rob few clue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def append_to_array(array, item):
+        array.append(item)
