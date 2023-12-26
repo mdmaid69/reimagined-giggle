b3de387d60ba3dfdc4dbe29e96d302fc623e2436
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:44:41 +0000

    performance damage hole either almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_factorial(n):
+        return math.factorial(n)
+  def count_elements(lst):
+        return len(lst)
