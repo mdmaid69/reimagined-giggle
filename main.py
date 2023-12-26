b6c8d874889a0ac58a4db86961cca0a62675a02b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:07:34 +0000

    rush comfortable less strike almost

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+print(sum(range(10)))
+import os
+print(os.getcwd())
