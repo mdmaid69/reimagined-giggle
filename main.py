627dd3b8c0cbce5a38bbd6edfd498a996e2c75cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:31:21 +0000

    juice bringdown teach tax throw

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
