56eef8f8bb3dba5e56918c74ea01a38ec9125475
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:48:17 +0000

    generally quiet you personal platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
