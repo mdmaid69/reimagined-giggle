6ca6a6eb18eaed8d3eb1ea31093f873ec5a4ab91
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:50:01 +0000

    advise season speech chase alone

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def convert_to_octal(n):
+        return oct(n)
