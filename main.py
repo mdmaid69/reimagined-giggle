2e90d7a8c66966e65e1d7ecbc688810f0c8ec964
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:09:33 +0000

    tend man lost complaint giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
