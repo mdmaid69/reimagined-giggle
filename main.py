749e063ddb715794fb0c33136af825cd5f6b59b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:10:35 +0000

    Kk bringdown during possibility avoid

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def print_python_version():
+        return sys.version
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
