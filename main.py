7bef4cadbb1c2ccf1977feb1455132b8c2ade7e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:33:09 +0000

    telephone sudden cool although cruelty

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
