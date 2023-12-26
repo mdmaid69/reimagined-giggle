0f977b9ca63891c20c0da5510a9660ea2182fd59
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:58:34 +0000

    fun behaviour family union congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
