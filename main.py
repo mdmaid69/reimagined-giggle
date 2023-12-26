8661f115a293846130d61764205b06e26b878f77
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:38:06 +0000

    possible no scheme future murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
