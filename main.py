fa4873375666a7f9d549211bb3b0cefef5866b71
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:17:19 +0000

    shock noise underneath seat raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
