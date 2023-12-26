91a612335bc0cbe02d4956553eed57648f7db0f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:19:08 +0000

    before day this tax rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_list(array):
-        return list(array)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def get_array_buffer_info(array):
+        return array.buffer_info()
