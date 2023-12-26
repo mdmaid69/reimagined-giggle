86e5a4396b165f034ea444dab4b7f9a965c9b08a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:32:48 +0000

    explain moral appearance only position

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
