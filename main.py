0b9f0841300f116a9c752b2fc134ab46befd6f6d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:34:17 +0000

    relax name bar with preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
