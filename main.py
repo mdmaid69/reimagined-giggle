db7fe0e122fe272c460577bed65fb5ed27351981
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:06:42 +0000

    knowledge prepare search ahead rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_elements(lst):
-        return len(lst)
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
