773a2af3559195c532192d25809a3b7b1f4d69fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:18:53 +0000

    think region fear juice physical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
