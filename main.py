2fcda87aeec7eddc0eb29e398b612c3774574a27
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:53 +0000

    573entertainment serve travel mood upset

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
