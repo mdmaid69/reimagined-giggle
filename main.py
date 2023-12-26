3a43fa2a304084bca5b59da2277d10cd72d25e71
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:57:12 +0000

    undo song understand year number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
