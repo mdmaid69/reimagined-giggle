e1e5ee3e60dda875ce6c57d61f65ade3d1334e14
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:42:23 +0000

    tell male because scream if

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
