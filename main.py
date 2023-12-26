5057dd80838c589cbca0c7a053a9fe11672787c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:00:50 +0000

    favourite escape break embarrassed break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def multiply_numbers(x, y):
+        return x * y
