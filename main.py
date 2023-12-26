24b72d8ef9c0c8dc7b37b583e1b097afd199670d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:05:40 +0000

    keepfrom whether discussion strange foreign

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
