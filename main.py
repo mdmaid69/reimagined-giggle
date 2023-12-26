350bc05568143b3a196163167385860991499e57
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:44:30 +0000

    draw twist complete crowd holdon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
