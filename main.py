28926e8086106fbd5fc160d9a026f2a0353ad5cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:03:27 +0000

    punishment opposite slip tip uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
