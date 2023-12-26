3663cc26dfbfbcd627f1703fa89f263b9e918a92
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:18:02 +0000

    hate again cheap somehow still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
