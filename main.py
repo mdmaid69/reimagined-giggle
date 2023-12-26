cc7932c6f13024debeee60505a0f003f97a23a8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:49:20 +0000

    television detail special flat wide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
