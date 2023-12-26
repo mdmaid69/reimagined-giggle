fc9027b71e725938c50d5357fd25b5672ab5f801
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:59:27 +0000

    cheque suggestion still inside which

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_even(n):
+        return n % 2 == 0
