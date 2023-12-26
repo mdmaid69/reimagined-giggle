22abab0657c397ff9c0cb4dbd136bf6932e3fec5
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:05:16 +0000

    kill reply opposite heart whichever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_index(array, item):
+        return array.index(item)
