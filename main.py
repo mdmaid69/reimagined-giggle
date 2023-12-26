421a0a19ae4cbbcfe59fac771375e79080ab50ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:41:23 +0000

    responsible dry suit agree play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_list_from_array(array):
+        return array.tolist()
