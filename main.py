7b1c6812920b2a0eb7a726230b2cf0f943f688d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:56:02 +0000

    interrupt abroad open weekend comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
