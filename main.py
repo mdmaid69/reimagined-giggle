9fe69559d19b807c6a738c51d809252a143c0e1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:26:49 +0000

    succeed careless national past takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
