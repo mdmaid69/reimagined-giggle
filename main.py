fa8b2e4ca1b11d9c359662b571222e92848a452c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:24:25 +0000

    permission heart afraid square average

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def convert_to_hex(n):
+        return hex(n)
