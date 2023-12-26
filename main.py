f52f4213d6bb54ee805454b8e4f5604a2e5a419b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:44:15 +0000

    soak miss exit nurse value

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
