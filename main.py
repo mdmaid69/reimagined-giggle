66d556db422fe7c71f1e702831545f1aea106080
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:49:44 +0000

    bottom disagree store unlucky get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
