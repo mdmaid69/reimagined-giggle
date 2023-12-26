a06036605443ab563a0b8179e1d406966738a361
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:00:22 +0000

    mistake capital there dinner Yy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
