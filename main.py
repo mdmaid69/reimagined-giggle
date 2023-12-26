897c4c3d0c7fe9d57a2b4bcfb3db51d4158f84fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:00:20 +0000

    can university behave care turninto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def find_max(lst):
+        return max(lst)
+def convert_to_binary(n):
+        return bin(n)
