50ccb80d0a8b211ac0c6895374c615562076f618
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:11:06 +0000

    wall advantage excuse age record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def find_min(lst):
+        return min(lst)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
