0310b76d4db6493077fd4757034c27a339d6b56d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:47:31 +0000

    may nothing dish though permission

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
