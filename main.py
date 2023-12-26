a1f2f969b058ac85da869849b94468a11d4160e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:18:23 +0000

    long method inform grade immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_item(array, i):
+        return array[i]
