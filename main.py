ff549f9d461a9fe51eef82778c4cc995fdc88422
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:58:21 +0000

    line today modern advise bush

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Words:", len(text.split()))
