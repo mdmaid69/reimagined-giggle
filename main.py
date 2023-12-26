18006224d37b080a44e871e08a67017a68bfc24b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:46:54 +0000

    bother ought report breathe confusing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_max(lst):
-        return max(lst)
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_str(array):
+        return str(array)
