83b3444c509a39f66a7cb67854d0d4565b7ba392
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:04:41 +0000

    bored acceptable very to lean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def find_min(lst):
-        return min(lst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
