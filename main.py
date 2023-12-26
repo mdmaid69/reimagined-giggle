0d8a893971133458df1965ed87d5085d515a5523
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:41:04 +0000

    weather yes forgive account breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
