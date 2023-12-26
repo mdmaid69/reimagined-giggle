2a4c1a0de5e85fb61ff3f8484e8118ebaa7799db
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:28:38 +0000

    strike city race fast sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
