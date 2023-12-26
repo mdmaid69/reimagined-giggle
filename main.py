a7f8e06986e6a042625c0cdca5b29ab1679c3acc
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:52:03 +0000

    touch beyond bad season slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
