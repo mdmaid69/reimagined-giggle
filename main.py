dc08051b68aba4df928779e71008770c0f612e44
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:31:02 +0000

    still trip comein now fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
