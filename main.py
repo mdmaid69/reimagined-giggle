ea88e317217312bee646837c44cf2c28d5d6ef58
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:08:15 +0000

    proof imagine promise explain any

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
