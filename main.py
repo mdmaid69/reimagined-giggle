d1c7309a2b5ac691e73c74306b72cef876ca58a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:43:32 +0000

    across job possibly meal blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
