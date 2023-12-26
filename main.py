bf8a31eab8ae2038f8155b05576fa704ab9668db
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:43:56 +0000

    callout concentration another rise short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
