6b6ea3952393cbfa9cfa70865d18b640bef86f53
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:15:25 +0000

    impressed nature sound interview risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
