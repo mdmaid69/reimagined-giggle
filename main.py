c45246f6cafa16aefc9a0b2482af44de5529e1eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:57:06 +0000

    charge believe stretch along cook

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
