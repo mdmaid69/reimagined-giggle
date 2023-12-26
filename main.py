565aa6eee3216cad04f1b6abbd42d72050d8229f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:57:58 +0000

    why teach wherever complain store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
