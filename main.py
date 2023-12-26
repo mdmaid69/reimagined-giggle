a642fdc1bec50a04edf19c36ba8b55bfbbaf49ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:32:23 +0000

    breakout move exam exactly reliable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import tensorflow as tf
+print(tf.__version__)
