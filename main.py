39ce5facdff71e77aa0f138302c695f2706a11e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:44:10 +0000

    experiment rush must charge her

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
+def reverse_list(lst):
+        return lst[::-1]
