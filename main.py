4e155c054deb05426f6655e61407c7e12eb14421
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:49:47 +0000

    obvious bring fit offend explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_circle_area(radius):
+        return math.pi * radius**2
