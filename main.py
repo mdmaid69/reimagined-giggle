8385889f9b99e1c06526f5548e5dae39a8244c64
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:02:58 +0000

    sail form whistle thinkover practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import re
+print(re.match("h.*o", "hello world"))
