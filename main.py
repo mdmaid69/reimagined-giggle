9e9c4420090b666cde0df24443f9fcd4b651ab5b
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:09:07 +0000

    discover keepfrom little intelligent speed

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
