446413a8a3323b3f849e4826ac92dccbf1007a88
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:18:52 +0000

    print new heart turnoff scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
