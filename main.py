1a26180df4a6e37cf905302fb01102ca35ff6b93
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:28:19 +0000

    sensitive fear negative soil bill

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
