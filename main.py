a376a651a81657a855ffd60457e6a3cd5176ba4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:38:11 +0000

    deal contrast store taste exercise

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print(sum(range(10)))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
