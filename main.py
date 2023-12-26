6389dbaf8e0180988f2ca2ba15cd94379bd5d931
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:27:47 +0000

    permission in 179bite just trust

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
