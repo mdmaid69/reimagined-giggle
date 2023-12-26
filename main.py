22df29405647950b1c0949e7ea7a966af56fa638
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:00:28 +0000

    average description concerned because gather

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
