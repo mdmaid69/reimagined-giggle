108728ea851e91522db725692e433d703e0105de
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:24:55 +0000

    treat around send read shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
