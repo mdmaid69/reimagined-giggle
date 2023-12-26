e06d12af3ad7f983c3ed91d4614319e6b1c4b999
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:46:02 +0000

    sky class science explanation swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
