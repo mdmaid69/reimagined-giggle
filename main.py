d95ae61ac4535436640066f133502039939e91c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:27:46 +0000

    spin spare platform demonstrate fit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
