3f8bfa58672a560bf75aa6681bc396509cd19aeb
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:18:28 +0000

    any ago collection 565end choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
