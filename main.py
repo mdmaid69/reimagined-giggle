dcb244e97c7672f6ac7d344a312cb5f81bf80745
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:47:54 +0000

    share want mark year explain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
