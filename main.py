8d99afcda30396cabf4669876fb8009638363e7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:26:28 +0000

    race here second concentration experience

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
