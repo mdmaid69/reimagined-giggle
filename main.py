f9bdcfefb572e5d0e95e1f3574055776cc196b28
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:45:17 +0000

    fork command while scrap makeinto

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import collections
+def create_counter():
+        return collections.Counter()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
