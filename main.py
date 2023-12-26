e93f5396da8e8be9d2e5a62a1c9ed0aa74fa0936
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:47:41 +0000

    always stamp pride at scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+from collections import Counter
+print(Counter("hello world"))
