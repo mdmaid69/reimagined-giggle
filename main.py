45e49c4891609a2372e03e66f189317007d8746c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:44:00 +0000

    system test lead believe plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import collections
-def create_queue():
-        return collections.deque()
-import sys
-def exit_program():
-        sys.exit()
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
