49ab1af4a952c1c7313b887ac9e248166416557a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:32:05 +0000

    address common space over treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
