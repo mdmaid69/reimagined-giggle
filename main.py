9c07a68549a131a56b248a63ad4dd1d7c04e0d5f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:34:59 +0000

    common hate sleep gather public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
