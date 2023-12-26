8135f1e79d73dbbbfc1135dd72bcc53dbbc8560f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:54:59 +0000

    realize slightly wrap ordinary exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
