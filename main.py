01c3378de2ab5bce243308086001970a1c3055f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:18:35 +0000

    dull receive gas warn keepoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def add_numbers(x, y):
-        return x + y
+import sys
+def exit_program():
+        sys.exit()
+def subtract_numbers(x, y):
+        return x - y
