50ef37aea465bf423065058ac01cfcb60c453935
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:23:06 +0000

    may once repeat ignore bad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def reverse_list(lst):
-        return lst[::-1]
+import math
+print(math.pi)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
