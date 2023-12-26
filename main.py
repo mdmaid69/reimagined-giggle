4f366b70190e2975806a5b9266c3b3155fb111c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:55:06 +0000

    accident sit theirs with admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
