28803f56cbe249b9fd2181cb4f4c883b5bd075dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:38:59 +0000

    medium lonely keepaway hear used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
