02bcfc7eb0cf9afe3747fa29f2cad2d9b8b21ad6
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:54:28 +0000

    previous smile stamp damage takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_typecode(array):
+        return array.typecode
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
