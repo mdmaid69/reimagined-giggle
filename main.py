27effe0e62571ab93a0f2abcd15e588704d96995
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:25:18 +0000

    probably raise image OKorokay learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
