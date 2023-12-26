809f0713daff25fc4ac1af589fd3eb434a4a3eda
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:20:42 +0000

    unless positive plant refuse shine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print([x**2 for x in range(10)])
+  def delete_file(file_name):
+        os.remove(file_name)
