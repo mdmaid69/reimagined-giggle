071f84f565c24c89d54c1ea8dc9b7d936e6ebbfc
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:49:17 +0000

    regular medium light damage sink

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
