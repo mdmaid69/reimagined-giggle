76ca613bbbea8d0406f1b5c8b9ffd754f5a5e0c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:23:15 +0000

    land hit way score along

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
