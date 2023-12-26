18d2b76a90bdc6e5d75b5adf48ec959be67bc0b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:26:03 +0000

    patient store confidence recovery crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
