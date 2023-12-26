49ff555bf28e465a6f22e1745b10333d196399e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:52:01 +0000

    Jj we opposite crisis doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
