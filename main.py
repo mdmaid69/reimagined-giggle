e18322a260837fdf794047be70a3386e96291784
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:03:08 +0000

    pour impressive complete suddenly exactly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
