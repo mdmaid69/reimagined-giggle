4d2b2bd1e53d997db87526bfd338c92ba5543a6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:14:10 +0000

    satisfied stop bite excuse curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
