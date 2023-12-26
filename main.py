447b5d213c838c31b585db6175b4c294f5f0c47f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:40:43 +0000

    invite certainly interesting neat own

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
