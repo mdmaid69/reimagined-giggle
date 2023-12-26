e67f259bc8bf9b9e5468fc8310c3f1c480b038eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:59:55 +0000

    store personal smile murder pronounce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+print(datetime.datetime.now())
