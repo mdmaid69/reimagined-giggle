18ab4ccaf307de96842714b1ff042f103ceb6d7e
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:16:34 +0000

    talkback rule push depend organize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def find_max(lst):
+        return max(lst)
