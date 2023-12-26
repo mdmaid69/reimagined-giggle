9367fa49c5ec57d78c04e899e179226cb30b9ff6
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:08:22 +0000

    actual apart union today track

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def add_numbers(x, y):
+        return x + y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
