d0a221eac88ea4ef24b65dc1744fc764a07d87de
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:01:05 +0000

    flat naturally furniture surprise ring

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def count_elements(lst):
-        return len(lst)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
