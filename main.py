cde5329cca3f31a75dbe3bfd50979544478bc750
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:44:29 +0000

    goup unfortunate swallow fortunately contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+print(math.pi)
