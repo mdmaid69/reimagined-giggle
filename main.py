712b92a84c73dd2fc347c62a390cb43ad0c0cd73
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:38:38 +0000

    smell survive capital everything second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
