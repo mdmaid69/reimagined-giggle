be525c3f9045ea2655774fcbc058bd12d03f2d8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:08:20 +0000

    ask getout last welcome after

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
