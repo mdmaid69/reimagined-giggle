2ef3db87c35d0a99a4eb8e45503e78473c9121c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:29:00 +0000

    this land theirs beginning recently

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def convert_to_hex(n):
+        return hex(n)
