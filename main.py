d0acce6a3034338ae22305a1da7020eb1982b78f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:23:44 +0000

    justice order trousers character conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
