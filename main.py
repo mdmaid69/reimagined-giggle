b6027d2c714c4b86795d381ffc035882d03e2324
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:36:12 +0000

    away beneath art coat cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
