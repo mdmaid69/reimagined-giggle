f4b6c711c1dcbfe395fdf248b61e53ff59988a8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:55:55 +0000

    weigh space drive consist sing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
