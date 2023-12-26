20f8c83e77d137a82a555bc0a9c1617bd8e33222
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:23:36 +0000

    view tape crowd something otherwise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
