ad03213031c9ff6776b47adaa1bfc0d0b6fb8c34
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:21:31 +0000

    right exhibition mail replace brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
