6fdac4432c88251f500310b5525a0504f63fb4c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:02:33 +0000

    guilty computer uponon among sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_user_dict():
+        return collections.UserDict()
