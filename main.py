b4259336a5f689affda7057040488f92d932101b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:35:20 +0000

    present tip whatever lead professional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+for i in range(10): print(i)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
