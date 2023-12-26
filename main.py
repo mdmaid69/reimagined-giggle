6001edf4d8b093cdebcd6d361f87857d59a230e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:36:19 +0000

    exist limit news tongue yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
