b3dc56f524d403954d47b5def3f626bf59f5af35
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:43:18 +0000

    tend weak inquire bear precise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def square_number(x):
-        return x**2
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
