db1dfc47cb142b12b93b020ae5cd3acc9ab08ee7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:29:27 +0000

    beyond party appeal earth usual

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
