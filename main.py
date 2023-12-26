4e32684f147e1e1abd425f69d3a7ab6460055b59
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:51:12 +0000

    fierce hear service advantage almost

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
