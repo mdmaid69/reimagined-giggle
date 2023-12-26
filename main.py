4b71d8a6e5079982574ff25baa7e4609f2461df3
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:40:33 +0000

    responsibility freedom building everywhere lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
