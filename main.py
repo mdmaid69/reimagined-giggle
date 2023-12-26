95f7408333e963d6f8fde16c3d6b022d3119bd4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:14:32 +0000

    list soak today if very

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
