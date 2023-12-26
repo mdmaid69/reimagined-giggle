a51f3125131e61e09e803865fc0bee0362e28975
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:34:51 +0000

    involve strict actual game climb

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
-  def square_number(x):
-        return x**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
