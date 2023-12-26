0737c98cde2a3b03d3c15780bd632169cf49abc1
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:33:24 +0000

    blame approve keepaway often everyone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
