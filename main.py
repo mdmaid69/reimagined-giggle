7413fa929e951d20f1bc55b2fa46cdc843fc08e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:13:51 +0000

    board damage exhibition smile try

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
