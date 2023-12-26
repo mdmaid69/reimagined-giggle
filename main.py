e169e94966ceda30f51a4b8629ea8b9d1019ce82
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:58:56 +0000

    check near never mean however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_float(array):
+        return float(array[0])
