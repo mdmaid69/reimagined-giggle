e19a5ef2221075b9e6b48f72450edc97fa062b1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:30:53 +0000

    thankyou start disgusting dead twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
