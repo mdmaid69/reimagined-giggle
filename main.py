bc2f4639accc0386ddd112213e92110418eef589
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:02:48 +0000

    bury spray with train plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_str(array):
+        return str(array)
