18ed49eb69507fecf5a0868ee031cdcf90679307
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:40:39 +0000

    protest stranger ask work getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
