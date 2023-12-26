3072b3060b1b2aa784af59ed538d16e08bb151d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:54:32 +0000

    turnon pride valuable operation build

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_pressure(force, area):
+        return force / area
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
