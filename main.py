0bed4752a7255c534d51106c9ba42a48198dd524
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:23:44 +0000

    temperature ours marry discussion evidence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
