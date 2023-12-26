de97b27ea4953c99b27a56d4aa7d703596ee1db2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:20:32 +0000

    maintain sharp tread round road

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
 def calculate_compound_interest(principal, rate, time):
         return principal * (1 + rate)**time - principal
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
