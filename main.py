50288aa02faff1a3e4785aaacbdcb3b9aadeacf2
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:52:27 +0000

    angry lock keepaway own dangerous

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-print("Hello, world!")
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
