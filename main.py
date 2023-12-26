8eb2c2653cc58380b009e28d41473598b98a04b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:55:13 +0000

    nice wrong show prove legal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def multiply_numbers(x, y):
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def multiply_numbers(x, y):
         return x * y
