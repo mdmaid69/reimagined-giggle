46d43651b4da6291dcbd9a02848a06c36ac5e8fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:30:08 +0000

    wet base aim pair reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
