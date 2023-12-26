0b0cf2ae197207cb8747e373a21a728edc9d420e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:10:43 +0000

    fire major upwards include complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_username():
+        return getpass.getuser()
