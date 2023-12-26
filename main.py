0fd0b216e0f6914bbfddefdb9b06fa1896f96400
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:37:23 +0000

    turn part holdout cover entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def remove_duplicates(lst):
+        return list(set(lst))
