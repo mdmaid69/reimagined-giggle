9174747e046309763c4530465fd211722f3bf9ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:48 +0000

    surprise arrive roof curious connection

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tensorflow as tf
+print(tf.__version__)
