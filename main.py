079932be17803e91495e37ebb1e3864154d11be5
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:37:25 +0000

    same wire perform reply marry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
