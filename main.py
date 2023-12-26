38a9636acab1f3d50045a3617a4371facbc56eff
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:40:14 +0000

    lot usually side contrast fierce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-print(os.getcwd())
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
