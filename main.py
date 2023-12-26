78aaf3369c83a1dc47b7ed7bf75418e14ed44c4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:52:52 +0000

    really 565end serve pattern suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
