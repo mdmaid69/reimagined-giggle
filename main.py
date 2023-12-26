07b7ace53b83ca511a14478e817bcb21edafae61
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:19:42 +0000

    shut familiar split along kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
