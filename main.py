083fe1bb01dcc9be5895ca94d1a45b6ff8a0a527
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:15:09 +0000

    tap tool away blame attractive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
