af311fe9fb9707ad8e958758d4577bd2d0b55fe7
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:00:13 +0000

    old angry deep any habit

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
