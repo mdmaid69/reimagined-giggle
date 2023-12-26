203ae13f8fbd69e2eb0926cdfcc3cbd454ce707d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:09:55 +0000

    terrible pump story best basis

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def is_odd(n):
+        return n % 2 != 0
