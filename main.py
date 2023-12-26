28f31cdcb57c05a99ad1864dbc31407118241c74
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:54:23 +0000

    572enter ill race flight sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
