1a8d1a04f9b49843415d3bc536d1c1f22c09ea3a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:24:49 +0000

    shock below reply uncomfortable them

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_float(array):
+        return float(array[0])
