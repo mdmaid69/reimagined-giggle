9c44e5bb773787c4a30e0307e8c40430a56a63e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:25:36 +0000

    difficult constantly beside pattern shadow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def sort_list(lst):
+        return sorted(lst)
