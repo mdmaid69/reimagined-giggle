7489ac4dc49d4cd62a97e9b299241f805f8f5a67
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:10:53 +0000

    cost moment out trap pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
