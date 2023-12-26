6c68105c40335a1bb760d1667130e98b4d890f8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:10:48 +0000

    waste occasional base happen wet

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
