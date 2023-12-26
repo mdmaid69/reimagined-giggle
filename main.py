10f849af36fe65f33b185657e04f59badcc02b63
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:46:34 +0000

    great region twist involve range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def split_path(path):
+        return os.path.split(path)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
