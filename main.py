d588ae3653bac358f82abb48df1ef3aaa93bf243
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:19:26 +0000

    whistle ocean complain deep address

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
