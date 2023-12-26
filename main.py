58650244777cfaaf1b6abbea52256b9fb1c5b01a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:16 +0000

    argue dangerous fashion shine keep

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
