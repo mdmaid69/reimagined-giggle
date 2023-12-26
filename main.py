96f3cf4321e9c32ae6e589bc56b50949377163b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:18:19 +0000

    survive study man present rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
