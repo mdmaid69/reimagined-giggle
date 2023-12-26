13d19c39c8b457e9644b3e439dc2659865742e01
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:43:13 +0000

    shoulder mess near close wing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
