7d85a2477a3c8756ec51c920ab8c483895766b27
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:23:05 +0000

    relax traffic art shelter the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
