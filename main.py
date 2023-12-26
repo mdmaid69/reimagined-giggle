560e560c06b31c54dbe5fa63620189aac280d803
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:55:29 +0000

    tooth work advise mine no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
