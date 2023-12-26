7fb339aa61e2a8d397b79dedf073e12951c5a278
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:00:53 +0000

    complain crisis pass corner after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
