a22aca5b1b122af80a305505add945231b57d1c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:39:29 +0000

    theirs ashamed turninto getoff vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
