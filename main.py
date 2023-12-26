d1942f27720f571c3779a8e41ed4afa141674789
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:34:53 +0000

    shallow off comeover escape Pp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import getpass
+def get_username():
+        return getpass.getuser()
