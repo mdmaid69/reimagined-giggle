2ed0c134a061b4f01e9648783fe0eab4d8bdc613
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:29:27 +0000

    dangerous friendly objection beside scream

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
