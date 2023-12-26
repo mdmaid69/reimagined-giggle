bea4a3e801fe8914e9f168ab9b4354b31ee7ee7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:18:28 +0000

    takeaway pump speed lock interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
