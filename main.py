6ea0180e9240da1f59984ed87da102580f5f9e29
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:42:37 +0000

    steady calm regional write nervous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
