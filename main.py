9a193690cbe8cb1e12a02e7f6a9ba366cf9d345b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:34:14 +0000

    arrive thick various or shout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
