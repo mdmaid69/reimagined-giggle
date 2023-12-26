6574900421b5c3cb794aaeef0b36bf28205f2cd4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:54:49 +0000

    achieve side entrance Ii until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
