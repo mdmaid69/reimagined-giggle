5f3708ef0434d9e3afa992a3378b427c0d24c9fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:45:26 +0000

    there watch body afraid contain

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+print(sum(range(10)))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
