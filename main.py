31da71995ee16afe6b87d19159edbb3c1a05f308
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:55:57 +0000

    free story thanks believe unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
