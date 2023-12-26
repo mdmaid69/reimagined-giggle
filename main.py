1f04f2b3a463425f098a8eeeb8fd6824f50f7d3f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:36:55 +0000

    talkinto minimum once breakdown suffer

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
