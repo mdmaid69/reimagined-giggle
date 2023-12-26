ddf69a1d013a739167220c0f60d2ba4f1363af25
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:36:33 +0000

    even commit only pin pause

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
