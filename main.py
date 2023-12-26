1ebd49fe667900be3a555ed16dc24eb06956cd9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:12:44 +0000

    make waste somebody certainly shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import sys
+print(sys.version)
