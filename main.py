ac0ca72f956394b59706f24bf70f174996da49b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:51:29 +0000

    order scene delicate Mm field

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
