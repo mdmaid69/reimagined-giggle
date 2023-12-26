2ade248b5dbe06471aafd04f7a4f48333f854d63
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:22:01 +0000

    traffic unhappy dear excuse before

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
