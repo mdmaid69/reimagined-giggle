ef8692684bd6b02d0c60474b3949c56166defa74
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:45:05 +0000

    tap suddenly lock listenfor possibly

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
