5a307388daa1a58f5f607ae89ac829e9438aabb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:45:44 +0000

    stretch bend emphasis commit threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
