be828e9e2ea46a2939d8f2ac45b62e874dfa945a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:51:58 +0000

    evening real all type side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
