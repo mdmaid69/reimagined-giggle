d61ca524bd0078dc6310056c3ae5a5cc04958d86
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:26:41 +0000

    every year Uu close clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
