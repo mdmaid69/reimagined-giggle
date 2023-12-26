543f0647fc1ddc004b941b216c202bfff798a940
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:34:38 +0000

    suck store language sorry tradition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
