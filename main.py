4ed5a3260474d7490ad4bbd7139798476319f3b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:36:47 +0000

    tap cruel may foreign tear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
