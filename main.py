4d364d31e882c005c730aa1530e956365d20d146
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:32:23 +0000

    design arrange to part little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  def calculate_area_rectangle(l, w):
+        return l * w
