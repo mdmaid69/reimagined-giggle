f5e4b55bc9ec557cff2a966d2614f67f3816561f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:11:37 +0000

    uniform yes unkind between hide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
