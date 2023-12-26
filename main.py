5a686dd6eae7345a02ab0f669641c08de1c2e59a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:07:57 +0000

    doubt break put exactly obvious

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
