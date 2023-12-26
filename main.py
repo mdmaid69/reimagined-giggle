399651de2ca615298a872511bfc757394e432e41
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:53:38 +0000

    act catch wherever ticket reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
