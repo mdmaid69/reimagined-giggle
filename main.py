dcd2d9e0a901a1557c581b4741d436c0f8a77682
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:36:26 +0000

    live tend already pronounce even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
