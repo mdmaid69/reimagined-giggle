a7ee23810754f70c84c87f16aec4b05aac506f5c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:54:21 +0000

    suit that tonight tooth pump

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
