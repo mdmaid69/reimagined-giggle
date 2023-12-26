ec03fe060d0ccc6a21cac24eaab1f896c399be3c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:11:44 +0000

    thinkthrough sound achievement hit burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
