c59f2dd0f7585e57db1466efcf73b5ee788f2d87
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:18:15 +0000

    cheek immediately job notice heart

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
