3d7f14ac8e4cee90b0cd1c9b18d43e5a5831d0ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:01:38 +0000

    necessary already reply cross whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
