ddebc553f5305e4bb73e366fd6591383cd2489b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:30:22 +0000

    shallow tradition target fly sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+print(math.pi)
+import os
+def get_current_working_directory():
+        return os.getcwd()
