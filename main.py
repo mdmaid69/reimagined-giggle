80a8d8371939a7615a66b5cf2e1e4678868d87fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:19:49 +0000

    shock pause least goodbye giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def create_counter():
+        return collections.Counter()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
