6e2c8ce9c0c96a79e7a0138815a6b8494a662de1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:37:06 +0000

    circumstance bus 571enough sleep there

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
