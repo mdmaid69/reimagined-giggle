3b6977922cc49593318abceec093f6d5da0eefc2
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:40:21 +0000

    voice naturally point worried process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
