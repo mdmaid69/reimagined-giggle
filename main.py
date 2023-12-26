a9b95533a837a5c50bed07c3938b571e65754736
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:08:57 +0000

    comeup himself use book goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
