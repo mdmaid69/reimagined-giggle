b4d4fe7203a2bc948512013e8585ef9ca1c0c6d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:11:25 +0000

    partner temperature what whether form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
