08a858c3da8babb51892af844bc227b23494bc34
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:41:33 +0000

    concentrate meal certainly knock valuable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-print([x**2 for x in range(10)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+name = "Python"
+print("Hello,", name)
