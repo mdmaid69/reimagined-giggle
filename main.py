63342cf735604b9de8d062febd9e08b8d0e6869b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:44:08 +0000

    off already poison straight though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
