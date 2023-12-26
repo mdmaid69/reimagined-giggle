47bb19d566f7ce510d8ad87bcd91c85b3ca2f7bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:35:48 +0000

    previous protect against concerning action

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
