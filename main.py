1bca0fa40a285727ad1fd7e4960c26b8e5b3c0b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:38:32 +0000

    patient takeout punishment weather lesson

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
