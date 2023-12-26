c1856918e15e2811e352c7d89bc5a6a03a2f06b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:49:20 +0000

    heat past water absolutely floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
