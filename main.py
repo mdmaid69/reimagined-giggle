9a5fee98fcf8c36f0c9fb4f14d05a78ffe401dcf
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:34:08 +0000

    disappointed smooth custom point 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-print(time.time())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_itemsize(array):
+        return array.itemsize
