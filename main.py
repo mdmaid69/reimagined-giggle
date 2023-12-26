bbb0fea447704642ffdf4bf8945ef2b64f9ca179
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:52:10 +0000

    simple injury group vote communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
