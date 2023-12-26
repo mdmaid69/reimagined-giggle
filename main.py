a032de19236cc51b673cb49fe82807645719c7b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:14:20 +0000

    mention true comment left think

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def convert_to_hex(n):
+        return hex(n)
