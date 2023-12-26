f83b42fb1cc574bdfcc868c981c3cc45f75ef294
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:29:19 +0000

    proud bright pity chat soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def cube_number(x):
-        return x**3
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
