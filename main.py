cdb59f6fae82104d8b466dccbf620e451228256f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:30:45 +0000

    everybody path message plant fire

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
