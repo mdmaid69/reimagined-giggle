d64db9e52927e9ee67bbe45ae5969e3eaf4d8d2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:07:59 +0000

    spread fresh all ilovedicks regard

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
