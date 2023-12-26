203d92693521c47bf79b6565a2d198b5072a1b8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:24:41 +0000

    unite figure beginning Zz eye

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-print(sum(range(10)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
