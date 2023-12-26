2aa1b0a171cbea700af249a11f5a9675de408f68
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:43:27 +0000

    gas phone past below juice

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_pressure(force, area):
-        return force / area
+def reverse_string(s):
+        return s[::-1]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
