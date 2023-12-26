69cf589f7184c9c03000cc1000b31a338ff52be2
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:25:22 +0000

    intelligent nor clear rain combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
