1e3cad8693d37115076bf911ffc18524d9373569
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:28:47 +0000

    exploration except sign dry violence

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import sys
-def print_python_version():
-        print(sys.version)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
