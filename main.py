f6e8a9c7ed6f3faccfc10ad272f580fa96b07e82
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:53:12 +0000

    spill walk receive steam cause

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_string():
+        return collections.UserString()
