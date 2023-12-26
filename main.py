74aec99407eda7a51e2ba160b42bb5a6791ddb2e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:27:35 +0000

    burn rich mainly gift set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
