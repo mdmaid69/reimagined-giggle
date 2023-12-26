2907d00fbb3c4cf1c74c88d1f843350e60555696
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:47:25 +0000

    expression shelter whoever great steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
