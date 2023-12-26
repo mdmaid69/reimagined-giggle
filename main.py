f557892ae58081450f3d0705f26bbe1c23475c1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:02:12 +0000

    ignore joke chief various last

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
