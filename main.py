56a17e49fbbc5bc8be7416637da097f90ad62ebd
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:32:25 +0000

    class post over kick hole

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+print(sum(range(10)))
