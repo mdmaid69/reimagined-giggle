174af12b3caccffb2bf089c2374951cae59b0c41
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:00:07 +0000

    yet 565end illness wheel include

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-for i in range(10): print(i)
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
