dc7f3a7a897dff6e04a53190365731c63efcd145
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:21:06 +0000

    away matter communicate harm reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
