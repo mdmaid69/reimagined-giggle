b76c1f0ee1cdb4d7d552719abc9baa8e11de3d7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:12:06 +0000

    566end behaviour memory size tax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_floor(x):
+        return math.floor(x)
