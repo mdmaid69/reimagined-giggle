b2fb419a8f474add0dfd77b03aba37488b1bd198
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:48:22 +0000

    shout minimum science high bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def square_number(x):
-        return x**2
+import array
+def reverse_array(array):
+        array.reverse()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
