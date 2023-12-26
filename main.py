b9693f18c1af9127fb1d279f0a629b4845a19696
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:21:44 +0000

    thing cross instruction there confuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def print_python_version():
-        return sys.version
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
