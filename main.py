de90130688649dd13f0f8ee948c96a71f9595e14
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:50:05 +0000

    knock against represent declaration where

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
