730817b86129ac90f26c25499fea6bb234dbe412
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:26:41 +0000

    anyone neither extraordinary whatever object

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+for i in range(10): print(i)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
