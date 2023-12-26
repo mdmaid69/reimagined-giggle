4150672f171f800ef4f2c1a496fbaed4f649c033
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:02:05 +0000

    goon Rr extremely laugh indeed

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
