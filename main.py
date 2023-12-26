490d7434b172422205bc83c56253ff73f3931a6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:47:25 +0000

    enthusiastic present wear move level

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-for i in range(10): print(i)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
