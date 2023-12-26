ff98a07236de00bd6ca9a88841eb415402fb9b8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:47:55 +0000

    me star preparation minority party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
