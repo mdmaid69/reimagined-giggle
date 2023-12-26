89850c258cb92f0ccb11a693457517251e9a82e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:46:26 +0000

    belief translate whistle live mine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
