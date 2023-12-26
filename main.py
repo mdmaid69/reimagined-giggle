f56a08b83b92373ff953a763e5cc3f8106c99a66
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:10:43 +0000

    expect slightly store accidentally check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
