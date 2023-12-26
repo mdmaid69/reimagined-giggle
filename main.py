654dc213834968a39dcdec1bc3febf3b036a059b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:24:58 +0000

    and fasten influence print never

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
