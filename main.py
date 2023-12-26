17a53bc7b51d95f064aed27c83df0966c97b5986
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:59:08 +0000

    bill in Nn pull anxious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
