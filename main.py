c43ad4d5f179c530277b3e761b5e65ae353ad5aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:56:46 +0000

    reduce need ours no same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
