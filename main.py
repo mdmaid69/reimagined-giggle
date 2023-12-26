21509e65ed3911fde21c73da9d146b636394fe9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:10:54 +0000

    unpleasant night born chance point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
