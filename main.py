b4ba8ed178df613844dbb634cd35892abb8712b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:57:30 +0000

    neither first immediate weigh black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
