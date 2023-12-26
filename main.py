bb8c52b7ff9e4e1bd6a126520f792d25f7157011
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:38:34 +0000

    shade taste nonsense name sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
