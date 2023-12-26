6671deb8729517f0543f110e812b65ff214b4344
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:42:15 +0000

    equipment transparent better date zero

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
