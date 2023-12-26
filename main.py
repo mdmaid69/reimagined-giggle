9750b921742986d579d627be6b841a0028980f28
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:39:53 +0000

    organize 565end spring project laugh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def find_max(lst):
+        return max(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
