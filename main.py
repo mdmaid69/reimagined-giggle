ea843c7e267a8ef4d546526b6fe6b8d8a61871a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:34:16 +0000

    giveaway thankyou limit fortunate stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
