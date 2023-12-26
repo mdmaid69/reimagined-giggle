67d51883b6cf8b139eac27db76edf0eeefad400c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:20:51 +0000

    load furniture none sight closely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import platform
-def get_os_info():
-        return platform.uname()
+def get_array_index(array, item):
+        return array.index(item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
