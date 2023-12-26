3cbf0504f896630482a6bad3a08ff251d74a48b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:34:41 +0000

    term bite tongue pleasant community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def multiply_numbers(x, y):
-        return x * y
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
