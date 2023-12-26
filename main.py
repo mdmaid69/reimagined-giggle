23747f848ee13db00aeaa2cebe251be1a745ce99
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:17:44 +0000

    increase bread shoulder dangerous breakin

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def find_min(numbers):
+        return min(numbers)
+def square_number(x):
+        return x**2
