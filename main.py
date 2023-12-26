535bb212bef47f7f9f9b8131ff001a1741486b30
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:52:11 +0000

    press count idea mine maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
