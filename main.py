dcab0fd96f8ed58515c1a6eac42cecb29b2b744a
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:48:21 +0000

    what polite radio will especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
