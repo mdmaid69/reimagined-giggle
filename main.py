4aacc44c60edbe7bc188d4ef126d3674e93bcace
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:58:07 +0000

    use natural lookback(on) wave hers

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 def calculate_perpetuity(payment, rate):
         return payment / rate
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
