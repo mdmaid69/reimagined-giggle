351d8cdc27e77cc06052717195b46bf774d7a13d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:38:46 +0000

    left question wish firm wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_counter():
-        return collections.Counter()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
