8efe6d32e61049ef4e4e1446009917a87af273d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:35:11 +0000

    Rr circumstance wherever nearly young

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
