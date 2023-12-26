7a2f3ee247174d20ce2434c3cad54bc52a67c736
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:20:05 +0000

    language funny shoulder last roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def count_characters(sentence):
+        return len(sentence)
