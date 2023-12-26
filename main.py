a4cd69850924a68ee6ebc9b762ae22ab706cfcd6
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:25:49 +0000

    564encourage crazy hair hurt unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def count_words(sentence):
+        return len(sentence.split())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
