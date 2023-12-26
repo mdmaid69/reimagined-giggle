e445afd5c1186215d582e63d57599d92cc39d5f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:26:59 +0000

    tax happy shallow grow miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_itemsize(array):
+        return array.itemsize
