71f516df4ed20cb4d75fce9666fbbda27550b096
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:25:42 +0000

    thorough corner pupil before bored

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_binary(n):
-        return bin(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
