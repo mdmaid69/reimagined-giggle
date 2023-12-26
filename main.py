9968e56126aae54baa836e949b291cb231c8709f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:12:10 +0000

    wild dish sail platform himself

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import re
-print(re.match("h.*o", "hello world"))
+import time
+def get_time_since_epoch():
+        return time.time()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
