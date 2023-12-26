6e5b99738730100021600b2c28d9620266ed65d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:50:53 +0000

    equipment guilty choose duty getoff

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
