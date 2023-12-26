9d13a3a2c9dc369d8c2d3f17253a11bbf800ecad
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:22:39 +0000

    over circle must television colour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
