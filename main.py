5130a61b0130b3ac61a19d113b624b5fe0f7ec60
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:46 +0000

    that holiday comedown show meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_sine(x):
+        return math.sin(x)
