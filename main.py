0e501a54a182462b26413adb5db79269496e6928
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:59:02 +0000

    fast the plenty bin consider

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
