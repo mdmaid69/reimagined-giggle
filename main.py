c38cd741815313380c75c526fcdc3f4a1010da7e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:26:15 +0000

    complaint lookup tired sign style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def reverse_array(array):
+        array.reverse()
