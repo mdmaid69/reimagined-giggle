a19ca0228e6f8feedeeffa9fe0db73cf42422648
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:52:36 +0000

    anything tap unit luck simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
