273f650b218ce862d02a38326bb38dd48ad77b2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:34:05 +0000

    become stare flow taste slice

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def find_min(lst):
+        return min(lst)
