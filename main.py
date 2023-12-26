eed03577f7c255600295e560031f3344bb061bd9
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:35:12 +0000

    doubt discovery force effect warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
