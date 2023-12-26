75bccf119f6693cfecb96ca874f9b9290a20b84f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:58 +0000

    together likely battery rob forever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
