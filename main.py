1bf6386bf8ea1409bc43d318cc124bba3770c709
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:03:10 +0000

    total mine admit 564encourage accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def sort_list(lst):
-        return sorted(lst)
+def is_even(n):
+        return n % 2 == 0
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
