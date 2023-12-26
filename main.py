0a754e6e940cff0c14d4fea01a1247d4394041c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:09:02 +0000

    use bank yesterday wide easy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
