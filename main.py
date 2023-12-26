5a730ee48c5e3becef8734d4b8a6b43de839ee71
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:32:06 +0000

    refuse bring flood hit uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
