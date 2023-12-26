a895bf56051555291e094b3018569ff78afc9858
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:43:09 +0000

    anywayanyhow come take 568energy quit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_octal(n):
+        return oct(n)
