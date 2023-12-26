4f1fcbffba02fb0367cc8425a3a67f8632a3692c
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:02:43 +0000

    holdup way smile mysterious can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
