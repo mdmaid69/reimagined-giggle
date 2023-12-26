3012db5ce42cc0939333e37691634cd3ccb86ecb
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:31:56 +0000

    splash evidence warn survive smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import platform
+def get_python_version():
+        return platform.python_version()
