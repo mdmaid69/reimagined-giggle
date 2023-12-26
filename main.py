45414f86c646414b7f3f9e80dfbf6c763f157e77
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:03:18 +0000

    release cough mind turnaway generally

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import platform
+def get_os_info():
+        return platform.uname()
