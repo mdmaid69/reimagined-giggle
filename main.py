2addf84f75c94e3fdda610d3d1bfbf36bedbd5cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:03:36 +0000

    official circle suit miss advice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
