3fa8fe9821166e9cba4cf22d5a5244a1ea702fd5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:20:13 +0000

    while movement point poison congratulations

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_ceiling(x):
+        return math.ceil(x)
