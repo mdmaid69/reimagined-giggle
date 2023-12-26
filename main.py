ac11a846f067d0d28e4c9d2c04a2a778190b4040
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:20:52 +0000

    any measure can concentration else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
