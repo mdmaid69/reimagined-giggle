1807d821089e8f5747ef90be1e17a1719be4a63e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:06:36 +0000

    mind like pocket enthusiastic last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
