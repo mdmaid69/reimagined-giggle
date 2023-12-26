0256e44debb41f09cc1cb0a118e0b4a7a7c55c55
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:33:31 +0000

    artificial otherwise town state legal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
