af28835a2f2dce21f1929557e9752d3770d95037
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:59:08 +0000

    name boring wherever control onto

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def clear_array(array):
-        array *= 0
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
