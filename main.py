fc8601c9b73f5196e792bb8ad34abb6cc48575d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:40:10 +0000

    government lesson judge badly push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_length(array):
+        return len(array)
