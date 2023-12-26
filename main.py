9ebfd5fceafe73f37ffbba0a5cd9f4650e26bc14
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:09:01 +0000

    throw cause wherever beginning everywhere

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
