797e96b6227ae3fdd1bd0241d6c0df1be6479641
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:30:45 +0000

    sister leave someone protest go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
