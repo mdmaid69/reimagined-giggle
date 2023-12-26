dc9d687bba1bc27bcfe41fc0c2b2bf7141cbbff4
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:52:16 +0000

    unite imagine harm thing close

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def reverse_list(lst):
+        return lst[::-1]
