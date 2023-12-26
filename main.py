08e3899e23da63397bb5843b982d834cbc586c31
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:24:55 +0000

    further tall whichever earn answer

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
