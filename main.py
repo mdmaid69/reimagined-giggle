f9eb9efd072271a3e6ae10f0be4a55c92455c513
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:06:01 +0000

    improve meaning left mess force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
