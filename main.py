977291e992ae4ba6a3f44b6515e6d02aee1a229a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:12:25 +0000

    degree say possession regret space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
