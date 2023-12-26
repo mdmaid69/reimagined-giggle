10cda49db5390ea748ff26686691c63cc64650a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:42:22 +0000

    runover word past yet hollow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-print([x**2 for x in range(10)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
