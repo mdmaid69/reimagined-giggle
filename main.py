8b92246caed6cc421df475f37832c8d9b69bccff
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:08:34 +0000

    finish about original nerve square

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def sort_list(lst):
+        return sorted(lst)
+def subtract_numbers(x, y):
+        return x - y
