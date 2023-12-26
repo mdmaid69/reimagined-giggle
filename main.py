6a35a4d2953f6fb03b9e4599da8d76401862e2f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:54:00 +0000

    depend sink keepfrom order memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
