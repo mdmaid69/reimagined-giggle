43ee5f5cf314ab001e26a5b379c9f5fe13805279
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:49:51 +0000

    discuss start runaway cruel light

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_power(work, time):
-        return work / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
