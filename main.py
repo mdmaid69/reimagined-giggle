6e2732df01b59f7ba9d7ca11737809a72ef595f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:13:14 +0000

    yesterday excellent hate relative season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_typecode(array):
+        return array.typecode
+x = 10
+y = 20
+print("Sum:", x + y)
