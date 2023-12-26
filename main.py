68af4e71245755084ef05c60b4b4876878caaf0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:40:26 +0000

    wherever worth increase invite strength

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-print([x**2 for x in range(10)])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
