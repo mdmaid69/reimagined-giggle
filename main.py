1025b3bbaa113eeaf2163e1ffda34477c38c83d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:03:21 +0000

    point handle mainly bound deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
