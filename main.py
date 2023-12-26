4d91d82cd72201691ce0b7b8627879f7f2e83386
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:34:35 +0000

    tear star path spring hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
