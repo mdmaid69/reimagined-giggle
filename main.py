dca6833d2d26bbbf2de009c25db03b8863c566ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:38:37 +0000

    gap nose again office except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
