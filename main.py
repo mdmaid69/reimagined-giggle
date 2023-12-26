5aa4f14286bbe8cd5805acef14d54c8cd9fdc85e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:06:27 +0000

    voice quarter equipment fun round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
