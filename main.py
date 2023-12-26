1d238d9f4c854f5fcae6062f89cd0a87ee265db1
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:02:15 +0000

    pump produce bath Rr blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
