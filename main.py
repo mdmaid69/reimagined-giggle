43507dff6649df1c2eb332e28c29395d624a1262
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:30:56 +0000

    would split model away influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tempfile
 def create_temp_file():
         return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
