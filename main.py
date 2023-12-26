701e0cfb52897636d8f3743fcf39a35c2ca1674e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:46:51 +0000

    if town whistle no watch

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
