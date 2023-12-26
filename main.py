4983b6d48b65d76cdd85b4772e2e708dc412776a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:04:35 +0000

    promise beyond block memory dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
