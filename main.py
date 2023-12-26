ce6f5ef24d3f88396c0d98de6f20da5fe1c92df6
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:42:18 +0000

    shoulder side table shrink luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
