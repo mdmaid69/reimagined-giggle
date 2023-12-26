714b97fd47c0ee95c8b0a557865b9d8b1412190b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:26:01 +0000

    wander odd fit reasonable up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
