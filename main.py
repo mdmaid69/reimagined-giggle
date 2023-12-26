93c2005d6e14ccfe183485cb2ab270a9c87aaad2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:20:52 +0000

    wander agreement voice nose table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
