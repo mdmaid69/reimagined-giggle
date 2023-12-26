9fc0d09fd2d04f59994d0169dbb7f7d284d70e1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:44:11 +0000

    priority lock development risk regret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
