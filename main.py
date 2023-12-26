8b68a15c42927b7e31fb683d953631e8b343ba69
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:39:21 +0000

    reach rubbish morning exploration nerve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
