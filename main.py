0877a60b03bf985c202235c0044086ab17e05678
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:20:38 +0000

    neither produce responsibility very remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
