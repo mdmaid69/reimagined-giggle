e086e261666285b60a6ed3310eb0633436df2540
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:55:23 +0000

    opposite prefer another cause perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
