ceb120c5d72917b0a3b72764d0faddbcb3b7b19b
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:06:38 +0000

    entrance slowly exist comeup criticism

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
