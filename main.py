765bdfe04cc21ffafb7d8266881a5c15cf0e4a39
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:08:23 +0000

    chase blame inquire minor ourselves

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
