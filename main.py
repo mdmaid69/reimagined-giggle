158d3c12bd1680f35389ffcce91b6b3516b7246a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:28:53 +0000

    part interesting want bridge boat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def reverse_list(lst):
+        return lst[::-1]
