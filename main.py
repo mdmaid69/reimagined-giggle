b6716437d4f9fe18bc153b1b0f9e74c31084b8c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:43:28 +0000

    suffer maybe suit steal declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
