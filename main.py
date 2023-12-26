98f5173b912ddfc60802fe1cf55eaa0436f1869a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:48:08 +0000

    tomorrow Gg score empty cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
