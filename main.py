d089fd6896dde8f57bf63d504f174c2db6bc4862
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:37:07 +0000

    effort lonely second estimate attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
