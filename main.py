8cf383893a403892ed75799ac88a6e0b39ed59a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:16:47 +0000

    chance thirsty doubt beneath fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
