e9b729d00cab8ee3edf15c1656983a586674a342
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:39:10 +0000

    dear flat supply one trade

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
