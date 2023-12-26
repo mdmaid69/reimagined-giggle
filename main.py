bcb20caf7b30e43afcea118bfb0f434fa255a225
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:34:05 +0000

    recently sudden combination slowly spring

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def find_max(lst):
-        return max(lst)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
