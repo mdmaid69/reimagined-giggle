f784e0d22385fc3c37fa010ef164ef4a88e09afa
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:01:50 +0000

    only pause reason bury house

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
