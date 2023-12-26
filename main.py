c29cbc50e70e4e1ff79fd63f083bdc69d4878db8
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:22:08 +0000

    nerve acceptable name union slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
