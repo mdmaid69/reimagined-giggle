7df477afb9b21faf06c46d95db101d2e4ca518a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:49:41 +0000

    nowhere time cross actually pain

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
