8cce59b5e8207d4b23048cac2a3a07a808bf8a98
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:02:05 +0000

    wish invitation taste school rely

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
