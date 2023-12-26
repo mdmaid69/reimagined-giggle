1e91503f7fe30d43140def220b1a3ed5311b2cba
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:21:48 +0000

    round law undo necessary wherever

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def is_odd(n):
+        return n % 2 != 0
+  def count_elements(lst):
+        return len(lst)
