82c8e0d6a89cb7b8ef596dfe6f82d32ca6c96c11
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:38:19 +0000

    introduce neat intend fast unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def is_even(n):
+        return n % 2 == 0
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
