cc40e5c96f57aa6580f632fad69254606bcd3c1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:39:57 +0000

    that respect card pressure record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def convert_to_binary(n):
+        return bin(n)
+import time
+def get_current_time():
+        return time.ctime()
