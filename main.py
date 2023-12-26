178dd45ccde28574e6fd29224e75ead26959418d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:15:56 +0000

    careful callback weigh each talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
