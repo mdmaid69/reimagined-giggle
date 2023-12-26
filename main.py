3484a8bab3dbc40ba1f706ab43c33ff2f524c48a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:37:15 +0000

    all announce table giveout sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_power(work, time):
+        return work / time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
