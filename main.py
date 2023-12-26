8802301151feaa4cbad0f6392073017a2d383d69
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:16:59 +0000

    sound show formal quickly float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
