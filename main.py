7c4a33d2aa344017c9f46e18f22d50db6d6167ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:49:54 +0000

    weigh careful organize proof by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
