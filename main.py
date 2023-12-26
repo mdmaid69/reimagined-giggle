526047e78e98c923b7c6bc44cb5034ad1608bea7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:54:38 +0000

    achieve tread waste direct disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_tangent(x):
+        return math.tan(x)
