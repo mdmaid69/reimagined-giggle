1ab263030a94d7b99483725dcbdaf9f4ce6fc087
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:07:00 +0000

    substance Bb name surprising mistake

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-print([x**2 for x in range(10)])
+print(sum(range(10)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
