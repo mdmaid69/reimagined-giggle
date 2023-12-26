b5c707b69236fcb9f0840384aa91b107daa971f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:52:10 +0000

    middle before hard ignore risk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
