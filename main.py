3d414b63dfcde7c8fdf6ad7b7d54fedfde065e81
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:23:06 +0000

    pick year increase responsible protest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print("Words:", len(text.split()))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
