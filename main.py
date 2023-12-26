2983d105501a64c0e4c5a36a2c30f733e0c16fa8
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:07:31 +0000

    foreign storm risk attempt there

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
 import array
-def convert_bytes_to_array(bytes, typecode):
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
