6c4ef6ed9bcced8facfcabf7d42e8fbbf4983a81
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:49:48 +0000

    noise contain than Itself expensive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
