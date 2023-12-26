2292e0df0f395cc770d71905a25b6c2ab88770bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:16:56 +0000

    performance him confidence turnoff welcome

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import array
 def write_array_to_file(filename, array):
         with open(filename, "wb") as f:
         array.tofile(f)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
