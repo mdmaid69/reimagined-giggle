360ae83ccc22a39208ac940fa0cbd306f34e2ea6
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:10:57 +0000

    recover holdon brush clean geton

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def set_array_item(array, i, item):
+        array[i] = item
