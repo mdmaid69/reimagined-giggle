dfb2a12ada5971e3bd543e68c8b0885f1a1da5d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:50:10 +0000

    make introduction corner artificial keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
