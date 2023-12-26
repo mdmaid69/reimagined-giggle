7226d06220fa8472433f8be49a4917c1aaac581e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:26:59 +0000

    aim wire rid ever back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_hex(n):
+        return hex(n)
