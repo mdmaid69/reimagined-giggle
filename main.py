7123fd6f2b646cce48baaf57e59d8c0ed803be42
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:02:29 +0000

    hurt private slip wrap shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def get_current_time():
+        return time.ctime()
