8eff9c6861ea9e3cee659e9748263b9ceb909816
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:26:30 +0000

    mail once busy centre violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
