fb6fa288b515daff22ffc73429a9dd51e76462d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:38:11 +0000

    regularly mess approve push sink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
