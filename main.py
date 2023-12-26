00855b7576b612b627e0df6982e7d9b7bdad371d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:09:51 +0000

    stop regularly everything gather closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
