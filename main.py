b8ba353e2d662be7dd2d3377bf75f75cc696799c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:41:25 +0000

    watch main communicate public promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
