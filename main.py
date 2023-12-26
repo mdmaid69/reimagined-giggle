d80329cd3e1892923ccbd90a0743347ccecf894a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:01 +0000

    programme stretch accordingto war prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
