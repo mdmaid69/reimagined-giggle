41b08a5aea393bda646f798f9eab67a93371abee
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:12:25 +0000

    odd narrow left course indeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def convert_to_octal(n):
-        return oct(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
