cf499948bd6de3766d9f6372d2cc7b7721ea4191
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:43:27 +0000

    invite little wear respect rule

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-import random
-print(random.randint(0, 100))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def square_number(x):
+        return x**2
