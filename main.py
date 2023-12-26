75666ff319996da69a79e7f1923d314609de4f9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:55:47 +0000

    one order next meaning open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_python_version():
+        return platform.python_version()
