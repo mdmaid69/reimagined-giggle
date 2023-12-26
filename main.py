2ba40e116a31f63f9566abbd87c0008eb65760dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:41:16 +0000

    police reasonable off skin tread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
