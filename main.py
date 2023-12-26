56cbe79817bac81569bd8cf8adfbc548d8989b43
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:52:14 +0000

    approval plain excited murder collection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
