23f54aec6f3d332679a2dc2a9fa8a7c297a263b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:36:49 +0000

    begin suffer definite kind fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
