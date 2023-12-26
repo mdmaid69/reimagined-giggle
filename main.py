25257753e8727245b38f31e1ca15f018e89b4135
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:34:49 +0000

    trap pain if fail need

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_max(numbers):
+        return max(numbers)
+  def square_number(x):
+        return x**2
