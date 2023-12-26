dbf3ca98ef9ec45e0ab523fc56afb0392bf7888e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:07:59 +0000

    welcome back miss Ll final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
