9173270288a53cadba97e7eac23c226715b7b47c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:41:35 +0000

    check disturbing direct game rubbish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-name = "Python"
-print("Hello,", name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
