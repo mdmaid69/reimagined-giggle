2ad67e66d1577892cfaed97ee2696802e5e2af03
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:23:32 +0000

    guard there notice downstairs over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
