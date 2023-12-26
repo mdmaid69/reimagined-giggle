b25cec9c6839732e7e8791f7e25abe9a2291bf22
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:31:27 +0000

    far come rescue run sea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
