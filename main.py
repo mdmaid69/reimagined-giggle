6db4423059bc053f1ec55fafc080597615b758d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:37:42 +0000

    emphasize opinion give stand comeout

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_as_set(array):
-        return set(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
