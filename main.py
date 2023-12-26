7e94c656a6eff8b3c4a2ba16576abe23c26bf890
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:10:51 +0000

    view prove belief 565end peace

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_current_working_directory():
+        return os.getcwd()
