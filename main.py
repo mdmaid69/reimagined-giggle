e88311773888254438d47a37b547ddaf6872e4b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:33:16 +0000

    continue immediately unkind pipe everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
