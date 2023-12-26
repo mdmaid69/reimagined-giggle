2207d3719b01b3819cb53cdaaf6b793ec343e29c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:16:23 +0000

    fast turninto one mistake tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
