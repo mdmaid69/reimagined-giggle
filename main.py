9faeb010bd762fd0ee46c22fd3f2a8d23999c3bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:26:51 +0000

    relief where immediate dress taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_pressure(force, area):
+        return force / area
