c4f546829abc56fcfdee33b96d7fc68febd0375d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:36:27 +0000

    wise imagine command holdup aware

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
