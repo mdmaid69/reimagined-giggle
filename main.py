72a22a4a2e1998480ecb3a7b62a474447f5da1d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:37:48 +0000

    picture tune send appeal sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_max(lst):
+        return max(lst)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
