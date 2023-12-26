08c97754c164ff85ca903be7ccd1eb452c4e5e09
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:57:34 +0000

    he increase get once common

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def count_words(sentence):
-        return len(sentence.split())
+print("Cube numbers:", [x**3 for x in range(n)])
