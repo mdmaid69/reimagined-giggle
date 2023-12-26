e91d778a80e2428e2e3288cd7667f4e05534146f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:55:51 +0000

    weigh mistake smile strong average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
