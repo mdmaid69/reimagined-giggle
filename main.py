4ad2458507881cb08f4a70fedb534d5f3c35b6ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:41:14 +0000

    I circle question mysterious bad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
