f7b7ab8cec6cbe2dfce37fa6a06a681b714f65c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:55:01 +0000

    wing various mind yours polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
