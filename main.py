12caad5d25dbabd1cefd132e37b0ee3421f56535
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:18:25 +0000

    bite ball birthday prize outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
