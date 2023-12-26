5cb1f7516819c444e25f60cf29b15db704442b7e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:38:15 +0000

    whichever warm receive since fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
