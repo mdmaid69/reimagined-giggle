df3ef1de9885df64b73bbc17453510242373dd93
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:25:05 +0000

    board wait tidy like mark

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
