855bf2a0d272f8af3c5bf05ad308eeabfd3141f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:00:25 +0000

    excuse 566end winter sister child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
