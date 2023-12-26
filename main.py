3e377b9e1edde834da31fd58bcf208890278c04c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:08:23 +0000

    face possibility broad game mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import sys
+print(sys.version)
+import getpass
+def get_username():
+        return getpass.getuser()
