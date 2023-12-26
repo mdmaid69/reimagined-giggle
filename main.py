8ea4da1f54794bcadd61155f278440dc5706a5c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:57:09 +0000

    firm beautiful violence happy break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
