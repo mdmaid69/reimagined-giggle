77d3c34d8630fdd724326a45bd9ebdc9d8211b22
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:26:38 +0000

    waste brush affect bottom interested

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def cube_number(x):
+        return x**3
+import os
+def remove_directory(path):
+        os.rmdir(path)
