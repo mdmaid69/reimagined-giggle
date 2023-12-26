3387a59ccf06851fbef07d2a766fd65828dc08c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:53:05 +0000

    classroom rank flood comeround tradition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def add_numbers(x, y):
+        return x + y
