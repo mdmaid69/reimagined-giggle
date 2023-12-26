3fb5234c43aa1a0e288b0cf4d7a6971ee527fdb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:30:14 +0000

    ahead Pp identify about emergency

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def remove_duplicates(lst):
-        return list(set(lst))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
