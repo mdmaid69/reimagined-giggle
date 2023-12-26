314c57501f838878a787b9415b75c24686fc489a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:14:11 +0000

    bin turn work 572enter blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
