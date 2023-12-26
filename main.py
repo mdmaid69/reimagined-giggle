7fe96e0427c212186093b960a0d374f378ef443d
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:13:58 +0000

    error damage male side care

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
