03f2b3dedf45f689a9dcd0a5a6381613289c4e64
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:58:28 +0000

    trust produce scare duty easy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
