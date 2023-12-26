dfafcea814cdfceaa9cd9a1bf23cb33756a83757
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:36:58 +0000

    payment runout Vv instruction priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
