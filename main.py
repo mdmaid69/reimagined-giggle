7c719f5098cac89285f6007b50b35ab7d3a29da9
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:59:28 +0000

    record anyone cheque tend passage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_index(array, item):
+        return array.index(item)
