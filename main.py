32b993dfb7ac72c79c20581948a97487c77f88e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:59:17 +0000

    view strange warn anywhere turnout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
