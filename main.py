56fdf11c7eb2431b1ede798131e8aff2326a40f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:34:07 +0000

    available luck surprise scream wait

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def find_min(lst):
-        return min(lst)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
