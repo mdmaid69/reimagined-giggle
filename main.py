88841fbee74b93a70cac65ff341ad89c2802e553
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:32:19 +0000

    switch pause cough give ride

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
