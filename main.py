6ce6cec7c30598a4fa1bbe117f5356737fb466f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:08:00 +0000

    excellent understand die dull band

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
