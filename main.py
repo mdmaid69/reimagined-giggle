70240ef4966a910aec2266d7b82891bb55deecc8
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:15:33 +0000

    design getaway conduct path open

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
