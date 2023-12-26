7156bd792baa3889988329a92f67e4bf19037337
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:55:08 +0000

    basis 565end thinkof pride rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
