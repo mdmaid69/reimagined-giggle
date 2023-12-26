a7d932a92adb721affee717a7ffd01552f505070
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:12:14 +0000

    share pretty marry pretend sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
