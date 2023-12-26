490ad92100d40681dc6f37b4a4e8605077e57ec7
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:55:22 +0000

    react glass track listen risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
