cadb1116ba09e11dddf8260aca4bf7e5e4211909
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:06:05 +0000

    book enthusiastic friend break it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
