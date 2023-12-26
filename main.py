3d30eea5c92648eeb33dda41e9c17ceb68810355
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:43:08 +0000

    whether fortunately each sick pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def create_user_list():
+        return collections.UserList()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
