7943aae4e1be3e2e6d50a7361b58d37317dc6cca
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:32:17 +0000

    conduct spot side chase thank

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
