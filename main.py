fb0ce2e0d9609951e03b761d4bbeb8b0fccd2282
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:05:43 +0000

    mood fight sight damage because

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def sort_numbers(numbers):
+        return sorted(numbers)
