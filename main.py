778c4fced6dbcd9db0afa9a22c17e1d8465d1921
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:08:27 +0000

    free remark light among physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import sys
+def exit_program():
+        sys.exit()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
