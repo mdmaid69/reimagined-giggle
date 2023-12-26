59c908f839a3477302e2088c4139a4e5cab02b1a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:56:31 +0000

    row body to poor ever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
