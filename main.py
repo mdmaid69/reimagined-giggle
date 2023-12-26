b60bf4e05acbdc4d2cac77927f302765b389f93c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:38:13 +0000

    wonder shout slightly unkind soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
