962fd9e2660e370d9e1d9fc64b6e08da75beb099
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:06:24 +0000

    address hurt name rather reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import time
+def get_current_time():
+        return time.time()
