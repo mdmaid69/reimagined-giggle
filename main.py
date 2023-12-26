d0947ded82991132cd089ada4e4c26c43c459435
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:53:18 +0000

    post generally ticket suckme no

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
