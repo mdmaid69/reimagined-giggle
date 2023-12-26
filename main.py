7c33d875cbd60cf6a3212adba81fcba089d2b072
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:01:04 +0000

    terrible sweet Tt along feed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
