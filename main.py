5282490aa3375de55817d6f4f69833d5e7599151
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:26:02 +0000

    attractive certain edge male circle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
