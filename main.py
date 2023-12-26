b845f1cd080bd422d2fe3108dc5e5ecc229cf339
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:22:36 +0000

    rate smile combine card draw

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
