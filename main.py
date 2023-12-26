3285185a99ec157d8b99e79f9380e841242a1804
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:29:15 +0000

    weekend experience person small bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
