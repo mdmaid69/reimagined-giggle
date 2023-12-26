7afc8b8c4c4e0faf31f9fccec2e274e3a78871cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:26:06 +0000

    if beneath hurry wet smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
