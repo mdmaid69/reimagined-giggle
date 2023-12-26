9f67d4758a01cb3fa4530019961b3acef6bb3e07
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:47:17 +0000

    complain write accommodation arrive wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
