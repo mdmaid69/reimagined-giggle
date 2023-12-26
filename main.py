97712cec85f0f18d78bae10a2d5332ed36cae4da
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:47:00 +0000

    butter considering prevent he could

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_area_circle(r):
+        return 3.14 * r**2
