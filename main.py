988ebeaa1d0a55005043f84c2b42587c72e357ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:30:23 +0000

    table disturb genuine hungry call

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
