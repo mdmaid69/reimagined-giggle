86f0b3b024f6d93751444623ffd73418fe32eb45
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:39:37 +0000

    education soft film between any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_min(numbers):
+        return min(numbers)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
