b60bd7ef446185019372603bb002468cfad1f271
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:34:53 +0000

    motion gate spend behind lunch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
