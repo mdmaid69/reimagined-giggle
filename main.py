31d4d0ad24f596b5f1e884c802e2ec32ab6064fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:53:52 +0000

    last what beautiful statement that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
