ecef184dd7c0d8927405899b47587c64133522b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:16:29 +0000

    properly case fast smile obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
