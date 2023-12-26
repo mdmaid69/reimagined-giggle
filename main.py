56b4785f3b6df4da2e2093bb07f05ccc086065ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:30:16 +0000

    apologizeapologise luck see fill frighten

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
