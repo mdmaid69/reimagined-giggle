d4b50b7ee26b81f73afd9648bc64b0a44c1dd570
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:03:15 +0000

    fight perfect defend correction turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_length(array):
+        return len(array)
+def count_elements(lst):
+        return len(lst)
