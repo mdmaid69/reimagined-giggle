4d4e3769d0367088890ef811eb08031d66654fb1
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:20:56 +0000

    sort take offence he correct

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import datetime
+def get_current_date():
+        return datetime.date.today()
