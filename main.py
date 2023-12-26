50a5eb682e2d4d7e96aad2a7e67b5bc1f3c5a5e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:41:58 +0000

    drink law lift snow characteristic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_odd(n):
-        return n % 2 != 0
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def find_max(lst):
+        return max(lst)
