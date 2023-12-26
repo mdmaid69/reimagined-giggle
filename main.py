145425fa326c3bc19dd931993b983b078e7aaf56
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:11 +0000

    crowded mark vanish area hit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
