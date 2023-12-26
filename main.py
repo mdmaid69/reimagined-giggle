b8d70771f15fa05c8d5417e881cf402b8d91ceaf
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:26:04 +0000

    either contact related speed immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-for i in range(10): print(i)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
