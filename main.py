743a5bac4d16cadee39695080a078db223dc8339
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:35:56 +0000

    argue class identify impressive state

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-print(time.time())
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
