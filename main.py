dec4388017e84e0ca41082a33cbf480a52e543be
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:35:25 +0000

    basis train sound dear run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
