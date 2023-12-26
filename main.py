62588bf25699b86ba9b417ee4f5d3e79a198cd9a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:23:21 +0000

    eager private important steal tooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
