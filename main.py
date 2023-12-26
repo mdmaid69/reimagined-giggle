d648db354fda0cb7bfc3f4d2f17be6a54e968913
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:05:58 +0000

    frequent like neck exaggerate deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
