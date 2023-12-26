cf882d495348d58c7e5c719f718b9b9718991c3f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:17:14 +0000

    boring busy language authority closely

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
