cc49ae132e43602156a1b06389e638f4bb850070
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:48:01 +0000

    near call sharp material very

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import collections
-def create_stack():
-        return collections.deque()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import datetime
+def get_current_date():
+        return datetime.date.today()
