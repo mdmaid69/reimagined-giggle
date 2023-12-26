d60543268e9effe66125c97e1e62abe46e4b7492
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:22:34 +0000

    new organization give guess in

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
