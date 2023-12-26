8fde2663faff204619b85f8b78a14cc455e30564
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:50:46 +0000

    pollution score hit surprise expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
