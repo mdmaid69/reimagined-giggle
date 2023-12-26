4319bec3c38d625a7f9f2eced753eeba61421a65
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:25:52 +0000

    thick contrast flat immediately sweep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
