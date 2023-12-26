d9310f6e3e6ab6f30e799ea09abe42079c8ec91d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:01:09 +0000

    follow present condition content tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
