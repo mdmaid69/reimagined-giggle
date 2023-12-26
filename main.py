72046acd5ff6155fb49e898eda998e82898ff1d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:18:03 +0000

    intend operation ignore nothing seat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import math
+print(math.pi)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
