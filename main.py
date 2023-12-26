33b86a10bf12ed41f6d0b9faed3b58ac30aed25d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:21:11 +0000

    translate last sad 572enter judge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
