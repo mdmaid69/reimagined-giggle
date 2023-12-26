baf49da25e8bf1c07e1df155d5c03b93e6529458
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:45:12 +0000

    calmly upstairs quality scare wind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
