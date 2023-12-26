e09b81346eb40d8d98d8a95c15f85676bfbb8b38
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:21:13 +0000

    workupto consider number scrape behave

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+x = 10
+y = 20
+print("Sum:", x + y)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
