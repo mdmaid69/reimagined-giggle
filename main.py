dfcc6df6517b5573d879967afb56dc3b311e7a6d
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:52:12 +0000

    she expect fire anybody weekend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def get_list_from_array(array):
+        return array.tolist()
