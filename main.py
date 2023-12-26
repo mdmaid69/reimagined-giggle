348b717d26a5094c266aeb90024e5658747d3314
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:10:26 +0000

    bored head government limit push

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_power(work, time):
-        return work / time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
