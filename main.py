0adef937e1f4238290d31bc1d9cfbc43833cbc43
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:27:26 +0000

    lead situation heart defend different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
