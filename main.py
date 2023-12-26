4e83db504d94b545f9fc23c649c89dbc9338ffa3
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:44:07 +0000

    sting bin call turnup above

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def convert_array_to_string(array):
+        return array.tostring()
