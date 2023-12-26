026f55d7fdc1b77f50595ac3395043d3e03da8aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:38:20 +0000

    rubbish personal throw reject pen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
