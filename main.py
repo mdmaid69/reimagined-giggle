3d9ff98fe323b7c77aa258eafe6c898038904449
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:04:55 +0000

    unable else sad roof nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
