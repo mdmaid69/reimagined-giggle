22853bcbf870bef929c16b70a3ff96c913cbc7a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:02:52 +0000

    medium loan straight comfortable exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
