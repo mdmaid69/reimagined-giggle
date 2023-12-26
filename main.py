72d03b3b00bfddce698e5f502249f361f379e477
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:35:51 +0000

    opinion once so fresh smooth

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
