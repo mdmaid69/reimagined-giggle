2b0abfce203192a9c513419fc593ad4663e2965c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:42:13 +0000

    entrance off praise calm available

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
