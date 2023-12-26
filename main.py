129d4c915bb2fc064b7af30e0d04972a0572c77e
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:21:54 +0000

    mind wheel forget matter turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_user_list():
+        return collections.UserList()
