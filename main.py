ed3a970aba5951dbbf2c8dfd54b00042e4e924d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:40:14 +0000

    doctor difficulty bread this else

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
