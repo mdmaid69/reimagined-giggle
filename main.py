b9e95ace7951b673bb16fe19b0264177cfab99b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:33:04 +0000

    tear flow sell frame immediate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
