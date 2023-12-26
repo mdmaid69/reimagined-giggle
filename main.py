f7df765da5ad9e1d16ae2f8cd6876aa672a16e93
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:53:55 +0000

    slice copy laugh trade type

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
