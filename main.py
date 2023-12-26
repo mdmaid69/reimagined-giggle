f87e4b39b629b152baac68528b1a7e5a1c389785
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:16:13 +0000

    allow object fight content condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
