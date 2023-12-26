94858af3a118e289c71af725a45dc2db802289d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:42:30 +0000

    outside my bit replace giveaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
