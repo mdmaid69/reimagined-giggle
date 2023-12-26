e99013a0ce4c2a676aa38d8036fea633c2efdbe1
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:42:21 +0000

    spill send win cry agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
