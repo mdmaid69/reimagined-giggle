7e7d2cca1cf85598164fdcd74ca4da4e827de369
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:02:23 +0000

    their snow wipe bury none

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
+  def is_odd(n):
+        return n % 2 != 0
