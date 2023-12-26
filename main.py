8dc35ffb71770218454abc8c557b758faac2bc77
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:50:46 +0000

    lesson pause towards pronounce brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
