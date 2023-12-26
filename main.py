15f7d6c1e9c8c84b4a588dd6332a3e0b45f34d61
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:12:09 +0000

    maybe trick dig plan surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
