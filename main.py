cada1a47e21145669f33c3942189d0973821c3b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:00:01 +0000

    close this instead treat light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        return sys.version
+import array
+def get_array_as_int(array):
+        return int(array[0])
