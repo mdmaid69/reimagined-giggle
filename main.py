fbb8bc5fa23ebe0607852198bb5c89a4cec60279
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:52:52 +0000

    bad total fill connect actual

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
