a93d88591f103edbe7a9f4b79936b75d8e2840e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:43:39 +0000

    shame embarrassed development sorry spend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
