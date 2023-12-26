48d3bdc2b6666045d25a14798289bd910d7ea65a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:43:50 +0000

    science battery make sort attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def sort_list(lst):
+        return sorted(lst)
