614d46b9565414c53542c88108e7e31ded081d37
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:23:55 +0000

    recognize alone mistake fence used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
