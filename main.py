2162fb67dd9bf17fb439567fdf936dab63041f24
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:28:51 +0000

    comeback please course murder regular

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
