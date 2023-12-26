0376a19b6459bfd51ac673ac43f44e72e824f788
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:36:06 +0000

    life Oo listen little ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def count_words(sentence):
+        return len(sentence.split())
