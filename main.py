71733e98667997663718d637c777e9584d415393
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:08:37 +0000

    equipment discuss himself fail party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
