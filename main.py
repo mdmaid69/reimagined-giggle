18a2a1f00823ed81808b55bbe862e27d09fb012c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:13:47 +0000

    cool normal into pass lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
