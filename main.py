e0953bc57f1430aab0a59646eb363ba917fffc66
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:33:06 +0000

    block condition until lookup callout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
