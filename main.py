6cac4dedab06718d94d4052203abdf4d14a67f87
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:58:27 +0000

    check top cause herself blame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
