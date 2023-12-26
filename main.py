de40f47b0030061ee41da2ddb69452b3f78bb3f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:01:01 +0000

    mistake head near bread arm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_acceleration(speed, time):
+        return speed / time
