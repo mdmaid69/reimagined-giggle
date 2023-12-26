177ee13c364ebafaab4327deaa6e3b9a9870bcb3
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:14:14 +0000

    serve rain frame mix reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
