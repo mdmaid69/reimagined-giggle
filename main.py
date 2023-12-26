461b9703a55b5855763aa0090059b25465b921ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:16:54 +0000

    month performance whistle right left

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def add_numbers(x, y):
+        return x + y
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
