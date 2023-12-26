8c14023eb8eb295697eed41d9f0ff8e24ad08821
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:24:46 +0000

    turnup smooth close read area

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
