2191e11955e6da5817c0fb00d36b0c619dbb10a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:05:39 +0000

    to shallow delight before building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_hex(n):
+        return hex(n)
