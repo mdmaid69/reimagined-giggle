f216fc6ff000bf9934a87f3bb6d5f4bc99c5ac1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:21:00 +0000

    mark message both possible apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
