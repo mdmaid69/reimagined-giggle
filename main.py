e67eb42d5ef47d637c7c9e77044b690195c4f9a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:30:04 +0000

    invitation owe goup possible sort

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
