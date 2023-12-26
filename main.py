5c3ad1c8d7916026d89ba612aeb0bfd62e2c5257
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:28:26 +0000

    kill cut write characteristic bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
