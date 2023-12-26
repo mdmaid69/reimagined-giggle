30769b11453fe0da48f06d604209d20efcd755c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:17:58 +0000

    remove pollution degree full comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
