c05a57a6a5dd2b8f15aefa01fd178bf66442bc65
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:09:15 +0000

    trick sister commit difference below

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
