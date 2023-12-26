949bcf2541d3ce617d95a0e4ee72773855a8fa48
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:50:58 +0000

    clue centimetre act precise spend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
