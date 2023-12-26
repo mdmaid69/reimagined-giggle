b3b889eac9373510e945ebfdff65b66a41ebb4b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:28:56 +0000

    sticky drop loss familiar give

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
