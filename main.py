5839dd9a128f50d36f2cfd53e882b9e7423f2dcb
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:23:13 +0000

    size unlucky dull later circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+import time
+def get_current_time():
+        return time.time()
