a320f939ffd40cb6ca7bc783e587156d84f5ba82
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:00:41 +0000

    underneath watch hungry occur deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
