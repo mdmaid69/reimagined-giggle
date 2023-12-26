bb71956102ba2c92d5201021c07cac432e3b1b57
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:16:34 +0000

    later grow further press guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def check_if_array_contains_item(array, item):
+        return item in array
