014e45654914d75377399952f4d669997c0f326c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:44:45 +0000

    male copy offer know land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
 import array
 def get_array_item(array, i):
         return array[i]
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
