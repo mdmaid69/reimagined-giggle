8fa08ad9dcff5f5cb930cef8ab974ae72e27680e
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:49:50 +0000

    word push definite basic become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
