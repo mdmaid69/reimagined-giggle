6b100ece06d5b2fd00a342bb3b0959c405365b4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:52:58 +0000

    light unusual draw reduce gradually

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
