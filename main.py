2217099b3c63bbaa4201d9badc4b48b38695d4a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:22:56 +0000

    case ride comment nature getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
