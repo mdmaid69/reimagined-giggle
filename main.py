376c280a9469878c7e45bbf2a8a71fef7c89ea2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:27:05 +0000

    getaway priority unfortunate judge share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
