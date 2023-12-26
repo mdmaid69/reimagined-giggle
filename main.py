d7eb75ec7713cf85b797c75d89612efa8312a017
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:51:08 +0000

    often shall careful Qq scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+print(re.match("h.*o", "hello world"))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
