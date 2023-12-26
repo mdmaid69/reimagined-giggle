d2297a388ad04ac5d12071e39f5bbdc6f39a07ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:24:06 +0000

    release anybody count breakout forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
