8d8e1b4ea65d3169bb24898e4698302ace22c450
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:06:03 +0000

    achievement still choose bring certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_circle(r):
+        return 3.14 * r**2
