0add91af2b1bf28310f6ab0ace7821236ea1440d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:57:10 +0000

    couple play wave goup everything

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
