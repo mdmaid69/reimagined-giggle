ffb6243b6827ba47c8c8fdda4851bf750736b98e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:09:23 +0000

    talkoutof slice congratulations suck dull

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
