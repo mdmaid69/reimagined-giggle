6b450ac9b8ae797213dc1a565bd2625d6ecaee28
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:56:59 +0000

    taste exist national row distant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
