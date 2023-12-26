96b4128c6cbd358843fc1bb3895dd30565ca68cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:11:06 +0000

    store mention challenge thorough search

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import tensorflow as tf
-print(tf.__version__)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
