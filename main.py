72a0a4421c0961b6252997840053631671d1cca3
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:02:02 +0000

    gather recommend terrible demand proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
