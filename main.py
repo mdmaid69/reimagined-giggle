ad9911e1d580077237be5a7577a605e336f090c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:58:06 +0000

    see wherever bank second strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_string():
+        return collections.UserString()
