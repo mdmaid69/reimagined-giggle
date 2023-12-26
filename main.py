fa264c5e9fc6d6222b32fc1ceeb3479263284bd1
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:21:21 +0000

    treatment of lunch game rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_array_to_bytes(array):
+        return array.tobytes()
