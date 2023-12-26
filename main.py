479ba7319237958bf949d4d813f522c8fc38779a
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:32:19 +0000

    colour handle down sweet each

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+from collections import Counter
+print(Counter("hello world"))
