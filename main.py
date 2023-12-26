c5d36ee886dfecd24e3155e71c2424737c7aa820
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:03:05 +0000

    decide beside concerning choose allow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
