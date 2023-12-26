266e4a70f1ac69f612f4abbd6b584c7e51986b4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:37:10 +0000

    objection kill most perhaps rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def multiply_numbers(x, y):
+        return x * y
