2f6fc592f9330d3fc4f7eefdaf2d265375d38df0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:36:17 +0000

    strange rank stage love breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def extend_array(array, iterable):
+        array.extend(iterable)
