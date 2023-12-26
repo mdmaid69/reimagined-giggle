60197d0e63158e19f7c7f74785f2639844af03de
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:49:31 +0000

    long brave beside card professional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
