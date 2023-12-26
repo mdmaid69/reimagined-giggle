12d1c10d18194001098ebc5f154320648d955a12
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:38:05 +0000

    cry disappointed turninto well comeout

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
