20a6ea41daa52f58e34a9557412b8848aab44169
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:00:30 +0000

    total party on which basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
