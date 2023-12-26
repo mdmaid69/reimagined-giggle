b4d83a15d926d25b6e10c7297bd0d7f52e9d62a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:38:49 +0000

    tired offer this stretch sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
