4fe5c97ea9bf7d532606dd6f721c6dfda9295bb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:04:47 +0000

    takeaway thanks label survive safety

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_density(mass, volume):
-        return mass / volume
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
