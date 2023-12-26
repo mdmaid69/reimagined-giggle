5fc6ff1f2be4d46e7ef5dba2e62ebaa31e5170b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:00:25 +0000

    spread properly scrape wise manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
