ee59bcb8d9854965f3546562aaf4cbef6368040e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:04:03 +0000

    gate lift warm season delight

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
