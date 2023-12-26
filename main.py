777572e551ac223aff0af35a6be229bc25fb549e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:11:44 +0000

    show band corner flat poor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
