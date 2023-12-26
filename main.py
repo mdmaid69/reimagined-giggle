89c8a56923a6bc2a8687e9faadbb547354f3438c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:41:48 +0000

    accidentally bread thankyou theirs pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_int(array):
+        return int(array[0])
