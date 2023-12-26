820f15e8c73a8d250a05360a42ee98488d7e74f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:49:00 +0000

    doubt their further stamp by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def convert_array_to_list(array):
+        return array.tolist()
