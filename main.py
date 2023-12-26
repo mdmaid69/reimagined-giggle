019d9185fcd248a4e5a976552b179d2262605d49
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:39:52 +0000

    out turnoff lucky argue own

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_min(numbers):
+        return min(numbers)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
