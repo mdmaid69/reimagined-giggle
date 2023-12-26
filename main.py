159ac001715e855f9556f8f3b7a30fcbcc0edeb9
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:02:08 +0000

    body sense will smell over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import collections
+def create_priority_queue():
+        return collections.deque()
