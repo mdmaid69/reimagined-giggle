8297d66a3b718f58ecf3aa8efc977191684f22b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:02:04 +0000

    act exciting disturbing scream behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+name = "Python"
+print("Hello,", name)
