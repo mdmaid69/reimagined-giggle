66c91a295a124b9e5da5adbd7b7a51084b545477
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:44:18 +0000

    computer meet worried may begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
