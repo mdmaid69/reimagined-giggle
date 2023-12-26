ebf55a0bbc40fbc60b587f1fc3a7af0453c16472
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:21:02 +0000

    scream mainly desperate method talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+print(random.randint(0, 100))
