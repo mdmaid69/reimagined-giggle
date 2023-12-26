9670049db6eb1bcc68fb7d77837d67e9cfb03dce
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:01:48 +0000

    collection cook chance already rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
