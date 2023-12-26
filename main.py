3479edf6e2be15ca90247356f37b46b91a813de0
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:10:00 +0000

    public 564encourage yesterday breath apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import array
 def get_array_from_string(string, typecode):
         a = array.array(typecode)
