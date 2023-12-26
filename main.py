66c02d5b2fb77dbe9da8a965a72e9bd7c5d33edf
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:27:05 +0000

    split fly hers basic far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def find_min(lst):
+        return min(lst)
