0becffdca55f80572d571032c9626c0460f92cb8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:42:50 +0000

    terrible Mm real tradition involve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
