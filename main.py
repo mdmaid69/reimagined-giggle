e956d04a30a3b11074ca53c7f4debeac53317052
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:15:08 +0000

    evening female face slowly rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_even(n):
-        return n % 2 == 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
