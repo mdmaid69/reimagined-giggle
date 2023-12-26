01ebd03a982f1c1308fb9a7143e8a399705cf4a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:20:17 +0000

    true announce activity record waste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
+def reverse_string(s):
+        return s[::-1]
