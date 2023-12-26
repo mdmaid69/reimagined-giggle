b6c9ac0923be9aa30f1542b0a3044db502071e7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:21:48 +0000

    march mass rest unhappy bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
