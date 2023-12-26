4de39179c2a66da0228dfa9968cf91ed0a7e23a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:37:45 +0000

    snow increase impressed leave teach

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
