30bf79876e050ec58059109b9bccbbd00498c2dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:02:50 +0000

    noone trap shelter roof introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
