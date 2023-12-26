737f46900d9e38ba95ead97abec5b7200f74e21b
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:04:23 +0000

    sit between explain offend bored

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
