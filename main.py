66ce225eb669dbd815a6d2a7887118f5ab532c1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:41:31 +0000

    speed machine reach getaway medium

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def clear_array(array):
+        array *= 0
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
