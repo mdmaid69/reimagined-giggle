8f9a64f2a8c9736f64caad4d849ca8f884173521
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:29:41 +0000

    beneath dear translate yourself memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
