2f2e84d62606a2e2dc2beb6fc0fd4e9bfa431af9
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:19:21 +0000

    attempt shall left degree connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
