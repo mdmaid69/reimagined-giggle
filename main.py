42629222f2e2df321c27e9603f13e47d01bc3d35
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:30:45 +0000

    during word unusual hit dish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
