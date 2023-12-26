135af3769db67aff14a1a3f162b2bcd028af3745
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:01:15 +0000

    pretty tell high but improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_cosine(x):
+        return math.cos(x)
