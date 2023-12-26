beb2ee9744085b66f1557dca64f350397572ef13
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:43:13 +0000

    scrape rob fierce bottom straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
