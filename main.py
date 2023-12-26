9a5d2e8a0dd08ee4c4ed9fe418815fdc0d802b7a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:06:36 +0000

    cost arm display sad lookaround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
