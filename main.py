c5d8060f0f450236fd60d8579a7fd9da87acda88
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:08:31 +0000

    cold delicate task forward thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
