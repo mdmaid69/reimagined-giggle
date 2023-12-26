0d8c911b89a4c8fa15681c9f5cf6fdaa91a8b279
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:44:13 +0000

    wander callout comeover comeout bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
