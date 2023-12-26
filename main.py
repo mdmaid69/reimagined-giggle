310acaeb2518f75044558bcc5e44a2e090b68a6a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:34:39 +0000

    weak previous mistake long union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def delete_file(file_name):
+        os.remove(file_name)
