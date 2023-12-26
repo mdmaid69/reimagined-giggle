8ad4e4e858055b9be155e214fc5a342d3161c0c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:38:53 +0000

    violent embarrassed region water difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_pressure(force, area):
+        return force / area
