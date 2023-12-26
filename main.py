ea5e729422f20e68b8345b2f61fcd6906d96f285
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:30:20 +0000

    target return estimate of far

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import heapq
 def push_to_heap(heap, item):
         heapq.heappush(heap, item)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
