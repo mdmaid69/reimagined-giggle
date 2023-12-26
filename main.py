12b81531a151df97094f964984e3b1a9c420a1bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:44:17 +0000

    used disappear prepare instruction top

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
