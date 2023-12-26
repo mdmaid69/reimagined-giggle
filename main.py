f39fb3ac0bdb4ba3d20e78049bb3ea6caca15737
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:20:12 +0000

    plan judge boat importance even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
