e85437f1133297fc22224980d31f36ceedd3d9d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:19:34 +0000

    hollow shine even open unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def split_path(path):
+        return os.path.split(path)
