8c348863bcfd3c5fd33ce9a94a9ba62cb079cf61
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:21:02 +0000

    welcome unfortunate clue exact happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
