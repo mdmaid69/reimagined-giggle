5e91520f1744ac0f663f1a3336d704dba2ee60d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:32:56 +0000

    steer uncertain claim cover bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
