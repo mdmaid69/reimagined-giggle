cf97d2ac23be3f7d83b4023d36297dd0ad86cc11
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:04 +0000

    571enough affect backwards reasonable ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
