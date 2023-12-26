5775fbf33f86d9a20c6d6a27881a33d8fc61829d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:12:06 +0000

    compare pause Ee sure seriously

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def divide_numbers(x, y):
+        return x / y
