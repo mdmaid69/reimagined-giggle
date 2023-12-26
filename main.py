451d724458950c604e93fb4a1466d5d83dc44945
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:24:41 +0000

    agree scream command feed turnaway

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
