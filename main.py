15d3b94a80b324ab36a7575e9c4630d07010bb34
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:22:31 +0000

    than police rather account this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
