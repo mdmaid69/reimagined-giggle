b8c1ea7ff1188d7afdf0da7f3b572703fccef945
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:38:44 +0000

    scare gas soak return demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
