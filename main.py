9948686e50d739a060f3bde7da1fa5cbef5afd45
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:11:10 +0000

    regular hot regional damp turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
