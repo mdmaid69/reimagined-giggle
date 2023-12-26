6a46a1dd9bc6361aabe234cc803714b60b94721f
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:28:29 +0000

    drink cinema feeling platform connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
