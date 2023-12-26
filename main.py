3713e86dac615ec49476283bd237675d2f8eb4f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:45:56 +0000

    interesting attention production none wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
