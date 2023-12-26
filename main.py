92c7cc2392ae83424f5634aa7289a7ad2ab8e4d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:29 +0000

    extremely direct better run judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+text = "Hello, world!"
+print("Words:", len(text.split()))
