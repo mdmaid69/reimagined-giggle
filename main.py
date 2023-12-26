5c01e62981afa9df5c4e8ce78e7d3bf2c5e604cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:45:28 +0000

    basic sink busy entry friend

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
