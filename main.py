77bec91795f2b8e05f647605cf561ceed3c54a3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:58:18 +0000

    last fall breakfast sign justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
