e08604cfd11fd4918fd7e2f21b8ed9325d349883
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:05:15 +0000

    past claim week stamp pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def get_bytes_from_array(array):
+        return array.tobytes()
