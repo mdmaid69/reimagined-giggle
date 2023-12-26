d4446fd98327ed8843b0ea224b213644b9cbee42
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:11:17 +0000

    beat pride sink interest stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
