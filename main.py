6706cd5b1a840e994f626f94011395cf58801201
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:38:33 +0000

    job mean race stop follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_area_rectangle(l, w):
+        return l * w
+import os
+def change_working_directory(path):
+        os.chdir(path)
