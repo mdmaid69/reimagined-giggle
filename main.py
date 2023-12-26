2552c3a3997f8139e1bd972892902d7999f356ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:38:23 +0000

    whistle violence bend fix load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
