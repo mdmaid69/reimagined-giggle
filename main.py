b279d90470aea2418bfac96db3fa396a206d3254
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:12:49 +0000

    under computer scrap material recovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import os
+def change_working_directory(path):
+        os.chdir(path)
