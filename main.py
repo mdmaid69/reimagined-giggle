8c7244510e53c5cefa0995b65f5e498e208e335d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:19:50 +0000

    hair we afterwards condition squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_odd(n):
+        return n % 2 != 0
