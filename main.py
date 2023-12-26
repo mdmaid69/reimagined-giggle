a1482602c4e40f91cebde536a2704beea77e1977
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:54:58 +0000

    practise reply test violence present

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
