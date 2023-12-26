87cb7f4522a31f946f5469f3d739343b7459b371
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:27:52 +0000

    passage reject autumn contact tip

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
