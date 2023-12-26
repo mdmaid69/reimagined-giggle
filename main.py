7db676ef5d17144ca8c3ae8ab6df685042124fd3
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:01:22 +0000

    defeat list confident surface also

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+for i in range(5):
+        print(i)
+def cube_number(x):
+        return x**3
