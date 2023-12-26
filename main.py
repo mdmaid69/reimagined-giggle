047a54267d4fc9f91eb39bb35a34267024b659d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:24:57 +0000

    rubbish fast uncertain present breath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
