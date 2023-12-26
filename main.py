4332e8036ea22fe70663ca8a6c40ae475b31027a
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:18:55 +0000

    gate bringback flood Ll risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
