93f91ffdb8e378cc1da2e7cb56f9b01c32212fa7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:43:26 +0000

    fast collection level agreement unite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import time
-def get_time_since_epoch():
-        return time.time()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
