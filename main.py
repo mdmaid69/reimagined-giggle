1e99d4ec9301de29cf9ef19101822e712f121783
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:34:51 +0000

    before flow comein strength second

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
