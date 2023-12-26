62d08d99a1c5f06236119819796f46440c62f85e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:31:54 +0000

    contrast damp period occasion clean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print(sum(range(10)))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
