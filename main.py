e2cdc26c43c5be553c4056dcf7e188c1c1d1ee7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:16:47 +0000

    minimum neck report pure comeout

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
