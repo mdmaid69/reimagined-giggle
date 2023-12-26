5f55134c5b18f0cc7cf71855c4c840fda915db53
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:02:00 +0000

    satisfied persuade defeat chat love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
