764619fda58352c4abaf68779b802087087fe2ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:15:21 +0000

    dull room recovery turnoff conversation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
