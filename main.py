bb1bd62b4b137a69e8ac92979968af59a027fd60
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:00:50 +0000

    declare cheek loose finger load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
