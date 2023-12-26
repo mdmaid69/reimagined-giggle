c1967a9703daab1528c6c37c7a8937829b5b7f7a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:19:43 +0000

    comment region nowhere doubt term

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
