cac3abc29d46326d6dc2f4e3071b9f12d459e23a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:28:20 +0000

    571enough opportunity break fun understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
