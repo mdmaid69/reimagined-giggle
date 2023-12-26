5bcce104d2c816f3b1e8fb3d84372108906f65c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:47:18 +0000

    nothing offend report see major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
