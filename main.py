482996aa881a7992ce3c0a83e9a31d7c510e2261
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:45:51 +0000

    ball bring create snow immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
