bf9c695ef40801162f28f51a72738fcc104a3dfc
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:38:21 +0000

    admire system summer meeting contact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
