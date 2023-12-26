2b01970e03a5ec239a91e37af50dfd9520e1628a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:24:43 +0000

    injury creature dare switch little

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
