881d471c8c063a8aeb4df34c23b5d34d930c6873
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:06:36 +0000

    win nigga detail area development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
