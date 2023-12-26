e41141ac4927d76e6195573bd4f08975d2c9b8fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:21:57 +0000

    enthusiastic team roll before previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def count_elements(lst):
+        return len(lst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
