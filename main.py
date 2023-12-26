8e82cc6212899320f42e88f26a0f5e482e931634
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:06:59 +0000

    straight step right play lower

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def cube_number(x):
-        return x**3
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
