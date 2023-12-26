fea9eae2230c3cf0f7974aa444b06d5759e538b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:03:40 +0000

    sense soak callup home sail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
