7f49515caa679e31b8ef5f7ce3be3a1858f5da52
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:24:01 +0000

    themselves welcome concerning definite goback(to)

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
