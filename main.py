149b30c244e3d05e0a354999d71a22876990da14
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:12:04 +0000

    promise possibly excuse sing roof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
