ff4a7df9929f543666b1585ec2bd9dc00667d38c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:18:44 +0000

    take position display fine typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
