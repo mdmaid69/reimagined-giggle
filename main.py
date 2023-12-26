5c9f3e2933f9edbcc107a834bcdb57d1f522cdf4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:24:35 +0000

    intelligent country ring interfere exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
