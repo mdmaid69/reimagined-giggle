9b1219384e0be7521f1c4bb2e1fda39b348e6881
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:05:37 +0000

    custom serve noisy rich win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
