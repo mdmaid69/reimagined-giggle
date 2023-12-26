abfed9c129f158da7fa2aace654f459269dab63e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:47:04 +0000

    past under patient mean object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_as_bool(array):
+        return bool(array)
