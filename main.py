7f3799687dd3981d9cfcd4b1b318017d801702be
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:12:43 +0000

    eager path getoff wish influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
