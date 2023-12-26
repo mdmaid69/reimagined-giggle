d014e412a921156d70d6d2a71d91c7ed9b09f61d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:06:18 +0000

    rob swim date long tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
