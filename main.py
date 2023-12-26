1714f99865c8cc89888fd665352c2ee62c998258
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:36:30 +0000

    direct old broken friendly wild

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
