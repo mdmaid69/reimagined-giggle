baa124315541df8e83baac504321602be9dc38f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:25:49 +0000

    trap healthy vanish instrument getaway

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
