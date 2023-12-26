663f916c8327a8af793acfb3c16fb2b1b522e99f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:45:48 +0000

    protest shadow impressive warn prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
