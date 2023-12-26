1abff6be660b78071cb34e27743442b48aaf98a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:51:53 +0000

    pinch unit sweet treat share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
