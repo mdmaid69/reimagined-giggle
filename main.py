eaed4a5bbdd3c0ca252022ee356cba5833201678
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:55:23 +0000

    open exchange hear custom hers

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-print(sum(range(10)))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
