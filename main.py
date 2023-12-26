e0504b4988010b12a5ca0669763b95eb7d56abc5
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:31:10 +0000

    organization breakinto free basically police

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
+def calculate_power(work, time):
+        return work / time
