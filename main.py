a277da6a1d9560dc6951e8b7beffb935df703d2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:40:57 +0000

    clearly deliberate peaceful demand plate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
