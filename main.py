8a9d8461207cde001c802f01e9984a41f0f1d061
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:08:15 +0000

    obvious pipe objection Vv our

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
