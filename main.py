4c90d7bb1a64b2e125f6cfae26a087159ef730f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:21:12 +0000

    promise then explanation tonight load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
