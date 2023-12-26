b36e54be4a76a7d31ee414361a4ae8c84122449d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:16:48 +0000

    improve far doubt takeout support

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def reverse_list(lst):
+        return lst[::-1]
