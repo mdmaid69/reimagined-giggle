c1ef8496fd4e4133a7bc7eb8b0cb67e4a146a8a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:19 +0000

    kill sweep collection then train

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
