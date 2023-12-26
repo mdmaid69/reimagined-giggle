04f7f0828d87981738f8fe517f6e315784563f10
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:10:08 +0000

    certainly mistake cruelty say sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
