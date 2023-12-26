91a79032469144c5b0f26e5fa3ad57e14e8d4a46
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:19:17 +0000

    cruel tip state chance wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
