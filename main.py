0d927da9ccebac5da306b466e2602c5e9c45dc4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:44:44 +0000

    address impossible risk fall oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
