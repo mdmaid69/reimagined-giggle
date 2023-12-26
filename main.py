70fa1c056f5b0930ac4c38a3cfefabccf4bcb999
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:33:54 +0000

    move impressed convinced cap slow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
