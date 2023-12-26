9d49fa780d79e5e81ab87d5e5f40bc95ff5ddac5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:35:34 +0000

    refuse more in and mark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
