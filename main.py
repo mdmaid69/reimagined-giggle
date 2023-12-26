858ac203ac7c77c8eecf50867089770d35ac989b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:03:20 +0000

    set club equipment odd open

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_volume(length, width, height):
+        return length * width * height
+def convert_to_octal(n):
+        return oct(n)
