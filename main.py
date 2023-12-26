220ab5032bed18e051624531ae5d8565bfa212cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:43:14 +0000

    start pattern direct transport other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
