81700db52c1a1ecc39116ea20c9b3483a0d60865
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:51:30 +0000

    matter look supply best then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
