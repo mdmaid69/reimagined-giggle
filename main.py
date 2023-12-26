7ec8dc58c8b1a34b6ec25759c5df23aad5bd6ffc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:48:44 +0000

    in post quantity plain direct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
