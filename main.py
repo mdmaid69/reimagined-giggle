8022eeb1f445b0c2a99c6923040ab74d9b69f837
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:29:20 +0000

    fear thorough cruelty dream swing

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
