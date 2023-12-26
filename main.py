32e2ae0b4edf070f1dbe4cccb223319598a841d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:09:12 +0000

    lost boring honest wide mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
