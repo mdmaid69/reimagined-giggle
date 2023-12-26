751ca420844b6af71b0fb6099d45b230762aa93e
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:21:16 +0000

    pleased tense hollow buy port

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
