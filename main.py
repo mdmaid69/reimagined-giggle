e2a3a7a488702148ad6055fa400f99eff53caa36
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:29:19 +0000

    worth improve off he obvious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
