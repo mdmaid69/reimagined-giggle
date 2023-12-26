b32f2ce2a627541ae4fd510c829681927e93213b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:20:13 +0000

    shop field pupil plate Tt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
