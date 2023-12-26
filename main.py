83e9cee8d09d30d2b525e851d1a8b0106a7b0ff8
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:51:51 +0000

    regular hear exit correct fence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_work(force, distance):
-        return force * distance
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
