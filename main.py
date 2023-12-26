1542077be045b50a9f56b60ecd86c43ccc057c93
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:48:24 +0000

    onto friendly treat spin less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
