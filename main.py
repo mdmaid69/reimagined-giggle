2abf542e1c3becde15d1c002ed5060300665beb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:16:03 +0000

    rule confused join workupto worse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_perpetuity(payment, rate):
+        return payment / rate
