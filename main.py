dad1e63fb9da18361f5fffdee62f5b81eed6dcb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:01:29 +0000

    scream concentration use holiday urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
