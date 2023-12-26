67bcb25b77faf2cd3723e8051f4b22c24fda28b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:40:45 +0000

    ring bring hurry pause wave

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
