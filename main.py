fadec44012ff54c9f9f27c80e307cebf6c22085c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:46:42 +0000

    by move everyone gradually remove

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
   def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
