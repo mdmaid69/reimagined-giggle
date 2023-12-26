893f964230dab26b4edc06b2abb6da0d94e7096f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:13:27 +0000

    through gas desperate live something

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_min(numbers):
+        return min(numbers)
