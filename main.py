4bb2bd563823fc01b26084c4b21b87cf7d34e391
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:54:09 +0000

    land full avoid introduce realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-x = 10
-y = 20
-print("Sum:", x + y)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
