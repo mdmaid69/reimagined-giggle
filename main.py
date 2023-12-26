f6e5116ab1a63218c5185c44596dfc27368b1f14
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:57:59 +0000

    this be until free contain

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
