53ec8f61c42381b73332affff06f23822cd2a06c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:57:27 +0000

    regional care survive what level

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
