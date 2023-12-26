d2e94b8a245d8fffefd95b99d07c40a1fd833995
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:32:30 +0000

    disturbing contact suggestion check secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
