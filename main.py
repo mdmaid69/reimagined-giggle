a3a7f61ad96e805fd9ac0d5c9e271f51c6dfc591
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:43:33 +0000

    crowd visit Oo opportunity underneath

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_max(lst):
+        return max(lst)
