6d1b8cc15975319c94ff63a9e845048ad3ced9a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:47:11 +0000

    swing major snow go hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
