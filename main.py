ab85599517f83efa3e695b70282407f21a9566b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:16:07 +0000

    comein goon embarrassing depend to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
