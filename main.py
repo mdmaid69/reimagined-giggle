bfc5281a393c3d93032bec943bb1ec9da20167ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:41:47 +0000

    the reply form wise error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
