9397dea4220189ec3f3084ddbfcef7d7cf6babf9
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:42:27 +0000

    on promise shame street demand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
