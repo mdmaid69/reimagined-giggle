5c55923b8dd90a2878e993728349ef6edf31fb4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:23:04 +0000

    react emphasis turnup spill friend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
