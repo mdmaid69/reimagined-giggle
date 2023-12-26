ba1c65119573b99e59c4e01b1b43fcbff68858ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:31:52 +0000

    record reserve anxious legal everything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def square_number(x):
+        return x**2
