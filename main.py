a3e392947ee499790b11d8766415dd29f4f02fb4
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:15:28 +0000

    568energy angry disgusting common basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
