c2b4f02c9a031f408713751d4b5e025664b252d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:37:51 +0000

    later ring step marry square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def square_number(x):
+        return x**2
+  import os
+  def get_current_directory():
+        return os.getcwd()
