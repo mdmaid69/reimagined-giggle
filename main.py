ed160d1b0dd6ea095b2b4d5ee578f5af427f7f1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:30:07 +0000

    strange joke few shop persuade

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def get_current_working_directory():
+        return os.getcwd()
