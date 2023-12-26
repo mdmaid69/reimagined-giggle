87f9d6674ac8c8222e05a83bee73ad1aef442f1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:03:15 +0000

    spread frame sky rid would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_acceleration(speed, time):
+        return speed / time
