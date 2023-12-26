3266cd27152b8c7c6d29e894b23f73e9017641eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:02:09 +0000

    back about risk hang suddenly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def square_number(x):
+        return x**2
+def remove_duplicates(lst):
+        return list(set(lst))
