0ed8f574a747b1e65a24b522c8adfc798414d6f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:43:07 +0000

    hard advantage such march already

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_work(force, distance):
+        return force * distance
