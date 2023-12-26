d86708db4b07560674164eaca3d0c56659c82ad4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:40:37 +0000

    purpose cry tape tragedy entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
