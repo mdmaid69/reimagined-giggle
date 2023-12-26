4fe1777fb5101bd0268a889ac649d6932058864a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:56:26 +0000

    survive control week convenient please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
