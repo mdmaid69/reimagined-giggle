99a0bb135a86526b60bec67a1f3f2458f8d5e051
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:48:23 +0000

    sentence born wait hurry production

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_force(mass, acceleration):
+        return mass * acceleration
