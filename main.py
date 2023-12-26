9c90fb66c63abde2cfa2316190091fd5d7d24c53
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:20:54 +0000

    lose excuse want than history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import time
+print(time.time())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
