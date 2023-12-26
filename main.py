5052aea22676a8921c44bbddf682db200c49adac
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:30:37 +0000

    lost settle phone discover beyond

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import sys
-  def get_python_version():
-        return sys.version
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
