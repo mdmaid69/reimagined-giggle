5aea66b0faafff4c7bfaf4d08ecc9a6fcb083bf2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:28:33 +0000

    pupil breathe official as confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
