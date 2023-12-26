f95f3ce8c76538f65e92d8dde870ab851f750c06
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:01:52 +0000

    sort cool Kk circumstance callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def count_elements(lst):
+        return len(lst)
