1f7690bfae95984f591ca396c59f06aab7cffd19
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:12:58 +0000

    previous discuss party remark education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_min(numbers):
-        return min(numbers)
+  def sort_list(lst):
+        return sorted(lst)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
