afc411b77c43a2326059427ed3dfb6b38f2f3b1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:59:38 +0000

    admit letter pause back boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
