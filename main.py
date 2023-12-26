02b5104c19f4f763c496c24dd5a00fa544786302
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:50:05 +0000

    careless cry fierce like relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
