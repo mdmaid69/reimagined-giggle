77c4510988c9169342932823e668d6fd94ed2dda
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:22:52 +0000

    here more precise light crisis

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
