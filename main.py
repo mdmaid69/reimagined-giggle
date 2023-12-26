17084bfd9baba8be621e588d7f58f9c1cfbb0f98
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:20:11 +0000

    really delicate luck draw draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def remove_duplicates(lst):
+        return list(set(lst))
