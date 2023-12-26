a4e2f72e6c1849346b27bb832324aa7849221668
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:11:33 +0000

    hour real permission polite getaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_work(force, distance):
-        return force * distance
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def square_number(x):
+        return x**2
