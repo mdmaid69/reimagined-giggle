af05e62854e893d0d280981b258f6af3bd808f60
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:43:19 +0000

    which temper recognize always hour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+print(math.pi)
+def convert_to_hex(n):
+        return hex(n)
