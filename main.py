fcd0b640add18f0faf58caad0c550aadd60d6ac6
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:05:37 +0000

    along hour word smooth someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
