711899771886e78f1ca7c6a9062005256e340b9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:35:52 +0000

    fence charge view wave fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
