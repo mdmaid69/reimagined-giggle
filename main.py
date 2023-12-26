8d53ac6980aa63e5a34bb2998e447c49d33193be
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:50:47 +0000

    borrow telephone noone public smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-print(math.pi)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
