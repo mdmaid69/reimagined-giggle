10cff78395f0c49e591d09c31a8240843c948163
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:53:53 +0000

    blame real cheap last shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
