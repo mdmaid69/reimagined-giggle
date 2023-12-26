35c78bd3bc62357d9ae81194d9a93cd3aa1644a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:57:29 +0000

    limit connected flat roof hers

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import sys
-def exit_program():
-        sys.exit()
+def get_array_as_bytes(array):
+        return bytes(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
