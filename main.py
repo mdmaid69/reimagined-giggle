ea7d8c1f8a683a75fd47d49e745d448361c30bac
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:59:11 +0000

    pronounce afraid offence except pen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
