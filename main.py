c5cfd598b583cf4b979beb296aceb537eecc624c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:39:47 +0000

    wrap confidence now consist giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
