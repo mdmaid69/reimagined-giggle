46314ef2051b9145c1273930133b25d367edf2ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:26:44 +0000

    lead separate education decide 572enter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
