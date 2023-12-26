4e5ac80d79abb4ec1cc2992751c8bec88a484e99
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:02:41 +0000

    about whistle test power bring

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_area_rectangle(l, w):
-        return l * w
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
