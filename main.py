fd9380edae41810d062648b22a2a7ee5cb22f134
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:26:04 +0000

    square lost forgive strength please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def square_number(x):
+        return x**2
