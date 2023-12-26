dfac0e8a5225a07a64a8cee61fe222333de8a4cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:31:01 +0000

    subject interview friend sometimes schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
