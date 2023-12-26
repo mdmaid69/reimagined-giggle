4d00b0575b80172edd895fe5a24b8b5108ab4efe
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:27:55 +0000

    choice 564encourage present result complain

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
