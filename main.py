e563a1230611855d1c98781b6eb15df413d91821
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:56:24 +0000

    telephone impression chase activity worry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
