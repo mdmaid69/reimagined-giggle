29b441f57285594f15119fbd3c35832720d09f4d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:36:04 +0000

    rescue takeover swallow side idea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
