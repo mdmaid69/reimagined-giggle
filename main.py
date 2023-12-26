081f4ac181aad3e1224039b08969c277ce3729b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:25:26 +0000

    warning finger snow case decision

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_max(lst):
+        return max(lst)
