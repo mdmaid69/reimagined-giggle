0cebb8b6bd2afcb27f2086405e82413ab429f3ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:18:03 +0000

    Vv during anyone call beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
