0d77a645be4b9cad4761baaf55e576f2917b4e8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:06:02 +0000

    build dress train feeling although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
