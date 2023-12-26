1e76f4a6e0b4bac0d8f5e72e4940a27dba82a686
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:08:22 +0000

    appear increase criticism study need

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
