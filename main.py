3802ebaf0461ac91d620bdcfc5bc527192183067
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:14:21 +0000

    private back 565end adjective within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
