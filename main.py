bdffb17311d9b29c11e1306a9df5e4461b32abb4
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:29:52 +0000

    size change immediately decision matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
