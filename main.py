da57b7d18badde99527e8465213dd7cdab6bd454
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:44:40 +0000

    reject please art ahead become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
