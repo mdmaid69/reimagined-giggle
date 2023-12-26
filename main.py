0f22dade73e458dff838499d55fc2a2704d99a98
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:24:43 +0000

    keepout anxious comment shrink pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
