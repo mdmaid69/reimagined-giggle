0b6a20fe5d1623770db0a2b6882bc4bfef58f8d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:00:38 +0000

    rubbish regret treatment swallow sink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
