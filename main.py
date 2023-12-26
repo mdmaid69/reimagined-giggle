029df913133e80024a3835cc9afd6ec012f09edd
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:13:56 +0000

    star best constantly music generous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+print(time.time())
