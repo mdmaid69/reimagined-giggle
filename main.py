eb7e5ede68aaa908b1b3d03b91956b20d482788a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:44:37 +0000

    constantly post constantly 564encourage right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
