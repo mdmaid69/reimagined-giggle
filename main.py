c011656ed7b0f0cd54c9c9d9ef5cd625d94a0b5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:54:40 +0000

    call use unkind smooth keepup

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
