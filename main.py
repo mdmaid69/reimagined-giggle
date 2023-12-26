f66db98c8208f7ee08f817cc6f6512c9e1e35869
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:11:50 +0000

    no hang its brain thick

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
