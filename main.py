f78ea556b03d5a8b05a3b17d1a12cadbc222f6e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:24:15 +0000

    spite impressed Uu fashion hide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import sys
+def exit_program():
+        sys.exit()
