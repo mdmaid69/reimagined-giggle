0758812681c6b6b61e8a89a500515228786abe4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:24:35 +0000

    love abroad report Gg keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
