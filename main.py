e88e37303ad6b6ab5631d99adef89204ea5eea09
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:30:58 +0000

    important Pp tidy treat blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
