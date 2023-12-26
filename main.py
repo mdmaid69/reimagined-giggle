156aa147f6097721571aab4277730e88a040cf8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:04:03 +0000

    we during patient muscle finish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
