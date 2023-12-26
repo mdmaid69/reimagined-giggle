20c29312ca4ddb28c6ef465d94a0462844b42aff
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:41:57 +0000

    identify wear after chance rest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
