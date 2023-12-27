4ac81e6858aea6633b056f79e5b72d893377aea8
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:37:08 +0000

    wet regularly accidentally string development

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
