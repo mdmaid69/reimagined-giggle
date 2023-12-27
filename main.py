8f7e252699860426f7963e5aaed3817a6ac81a86
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:00:42 +0000

    grateful pleasure ago Ww minimum

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
