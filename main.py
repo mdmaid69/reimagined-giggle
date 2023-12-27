fb27819f8e24ce05a41276076ad478a816ca3ba8
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:10:52 +0000

    makeinto platform square board result

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-for i in range(5):
-        print(i)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
