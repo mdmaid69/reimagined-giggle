d54337ecf1cdb0406ebf6d1278ba96721ccb9f50
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:45:24 +0000

    it blow match split next

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
