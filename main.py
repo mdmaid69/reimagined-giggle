c729aa79a847c4160e9277fc4f6e4c5963de7290
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:49:56 +0000

    regard claim modern trust backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bool(array):
+        return bool(array)
