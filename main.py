bd20adbfa7f582af56dc536f197fa63db4827622
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:34:50 +0000

    position building hand crazy also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
