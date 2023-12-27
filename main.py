ab130c7feb068d99c40af9c711640b57d300ad2a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:18:23 +0000

    callout cross hold weigh use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+from collections import Counter
+print(Counter("hello world"))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
