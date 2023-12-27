b11e33895f2eea429b866dd08c4bf092f0058044
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:51:51 +0000

    mess rate way idea blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
