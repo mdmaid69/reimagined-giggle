afb55edc0c8860f854755285484fba187c3895f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:18:31 +0000

    please worry extraordinary smell regular

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
