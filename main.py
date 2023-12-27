a02c9f1176750fbc395859bd093ac326f12555ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:23:02 +0000

    will regret badly expect same

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
