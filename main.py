d36d68c8ed7641021561a15e8816216d126d45bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:41:20 +0000

    trap square 569enjoy during suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-text = "Hello, world!"
-print("Characters:", len(text))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
