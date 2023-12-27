01a0aca9a7552adb3ae5a027b6f22e32f821121c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:34:03 +0000

    able store anyone bin play

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
