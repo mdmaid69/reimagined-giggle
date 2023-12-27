b647113868180dae2be85956391eb749990e8587
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:55:53 +0000

    pile far exercise university grateful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
