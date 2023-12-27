85fdd78e74b6e2ee63a2fa3022c5f2bae725c083
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:25:15 +0000

    dear stupid work scared quit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
