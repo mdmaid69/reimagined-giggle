3357a9afe545353ed8bbb526b64716562c1b9315
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:05:23 +0000

    school change like post earn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
