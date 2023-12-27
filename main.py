97e8b948576707d5c4089379c75ac32a551c1e25
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:17:29 +0000

    guess education while high here

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
