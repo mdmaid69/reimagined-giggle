908901f2c7f0fcc6e8b237a6b4adaf82474bc75a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:45:29 +0000

    flow congratulate generous science opinion

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
