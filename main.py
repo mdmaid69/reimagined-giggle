cdf7313f59b88f04245dbe242d3fdaa941b17ccb
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:37:50 +0000

    comein goaway public earn this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
