55c528a8756362fd751cb43a7d6313f05644a17f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:01:59 +0000

    worry turnaway space goback(to) that

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
