ec4a4d3cec7e94413b9a34d49697779869d41c2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:22:34 +0000

    part must keepfrom level comfort

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
