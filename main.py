337853161f61156cecd8964400afe1770ee06991
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:25:44 +0000

    taste off lie sister 565end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
