a81e186c1cc2579fd46f16c4c597b38d0db5c5da
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:39:29 +0000

    law fly brown correct turninto

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
