b951dc0d34ad4522e35abe607a6df8c4e0ff62cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:57:18 +0000

    introduction field Nn vote if

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def cube_number(x):
-        return x**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
