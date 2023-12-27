a4dfd6ecdf638e9f05e6821e847db0cd06942a2a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:00:11 +0000

    explain punishment set couple scare

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
