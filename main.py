ded9c11b106afb8104048f33de8172c8b96c97b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:26:26 +0000

    hollow machine listen run contents

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-from collections import Counter
-print(Counter("hello world"))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
