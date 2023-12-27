6d38fedf17fc1e9f1474cda74bd3dcf8583213da
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:24:26 +0000

    level clue prepare decision contents

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+name = "Python"
+print("Hello,", name)
