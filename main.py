5b3ffa68c0506ed7865d48dcd56734628e984989
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:48:19 +0000

    truth deserve deep time address

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def convert_to_octal(n):
-        return oct(n)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
