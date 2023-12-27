770c67b3a807f6923e3d06cd62829f57e3986585
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:23:32 +0000

    against preparation demand telephone speak

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def square_number(x):
-        return x**2
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
