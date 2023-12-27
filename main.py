24a42c520ae9869deea08a895b3c00875fba0955
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:49:32 +0000

    translate plenty send team through

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+print(sys.version)
