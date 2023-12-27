5c60f1df64768f01926e02ec0b4f7f8e2a6456a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:19:42 +0000

    date least general instead hate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
 def calculate_perpetuity(payment, rate):
         return payment / rate
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
