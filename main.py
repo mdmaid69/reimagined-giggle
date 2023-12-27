230262236ebbf2dd8c1e316f461d227f0c4e6370
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:38:45 +0000

    well strict this anywhere swing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def cube_number(x):
-        return x**3
-def calculate_speed(distance, time):
-        return distance / time
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
