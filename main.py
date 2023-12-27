b1256605ccf42cb662d93976e4f69a2b25b0aac1
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:57:35 +0000

    sorry road add forward introduction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
