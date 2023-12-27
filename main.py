4c2bcdeda4294e2210252317b93bd2cb31360d1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:10:36 +0000

    turnon pure influence whenever play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
