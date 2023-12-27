4e917bb09d091fb1025222fe2f14a7a141ba9176
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:41:48 +0000

    imagination want war admit stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import time
+print(time.time())
+import math
+def calculate_exponential(x):
+        return math.exp(x)
