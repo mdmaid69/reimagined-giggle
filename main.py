fe240f138716172787c0903074058d07cbf40583
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:19:29 +0000

    suddenly equal rest then cheap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
