39cdeb06a1ea96fa4408f3a3fdd4d96e7d2cce5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:05:59 +0000

    abroad stupid children convince bet

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
