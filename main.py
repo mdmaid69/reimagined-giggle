6b4f90976221abbf01631cfcf82983d071992751
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:37:17 +0000

    Vv compare free cheque embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def count_elements(lst):
+        return len(lst)
