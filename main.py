dde8444ebe3df9fdc0a59e891dce13f5a60058de
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:21:23 +0000

    doubt confusing half length common

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
