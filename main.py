332168c6c6dfff02fb65d96663049bd8ccf066dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:14:39 +0000

    understand light upwards welcome only

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
