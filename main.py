c0fd89a422f4d18048ca922c7341cc7b8293fb94
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:48:49 +0000

    choose appeal teach neither accidentally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+for i in range(5):
+        print(i)
