484925686b4bae9d0d6e7cd2dfa60f87c795ab19
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:11:23 +0000

    willing heavy crash twist attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
