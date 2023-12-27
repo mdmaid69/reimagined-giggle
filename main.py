4291ec80074c7d0faf4837960a3d91fc3031cf77
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:18:19 +0000

    frequent slight mark see treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
