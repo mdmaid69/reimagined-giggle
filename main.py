146040acdbfa5bfc53fb3be81165eb5ee208abb1
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:39:48 +0000

    jump minimum themselves giveback prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
