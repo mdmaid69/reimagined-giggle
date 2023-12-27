3831deaf94c39a8c52245b6b769871de8163568b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:46:20 +0000

    feed positive lookaround confidence degree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_power(work, time):
+        return work / time
+def remove_duplicates(lst):
+        return list(set(lst))
