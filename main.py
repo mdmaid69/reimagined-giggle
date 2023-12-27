7ddd39216fb37075382b4526d042d5e8a4547135
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:50:03 +0000

    position adjective on store achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
