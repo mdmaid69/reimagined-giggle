ce5f1404aa3c7244bc1a8bbf49eab0ad11bdf386
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:06:05 +0000

    shout excellent tough rub make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
