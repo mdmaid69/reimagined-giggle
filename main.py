25b0302b95c43c12cf3d114a97c0c1c5e5b987b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:14:46 +0000

    runover accident down cause that

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import collections
+def create_user_dict():
+        return collections.UserDict()
