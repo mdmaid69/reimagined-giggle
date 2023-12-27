3dbd0a265963c465cfed2159ef3fa69cf34ab158
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:56:23 +0000

    comeback swim professional impatient answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
