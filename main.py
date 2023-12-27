1136135ff044e60ba1e2f5683f05f57bf3d74653
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:07:40 +0000

    join room talkoutof cause object

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import collections
+def create_stack():
+        return collections.deque()
