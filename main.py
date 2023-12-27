1cc5046e636da995ca2ed90976428a6580ea590e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:55:42 +0000

    move ring tooth clothes ticket

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
