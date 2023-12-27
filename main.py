073c22ca5b72478cccf8a88639c3d34ab2a6a417
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:45:26 +0000

    no exciting night common fond

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
