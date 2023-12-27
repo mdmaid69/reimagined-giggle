fa544fdf7d764ebed0bfa7d927a1a26896e8aa2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:35:29 +0000

    yesterday their takeback chat telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
