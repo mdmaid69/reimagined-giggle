72ce88a9b32cc6d9c9fb6884e35d974a40a6eecd
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:59:37 +0000

    yet support crash interruption aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
