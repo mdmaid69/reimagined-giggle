e8aa38a7e695ce6694085c836d282d0ea01fff93
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:40:31 +0000

    stone workon sometimes pollution fit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
