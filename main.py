e1ecc046366d77c211c2f327a125a4ed237838af
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:24:55 +0000

    quantity put takeaway company workon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
