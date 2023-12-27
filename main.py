1d3cbcfb9a307c048e77606ce356722bd6a45196
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:58:06 +0000

    arrange absolutely already spin dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
