515db3721d96bc285b1904c202ebde8cd87cd412
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:52:40 +0000

    upstairs cough satisfactory dead often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_speed(distance, time):
+        return distance / time
