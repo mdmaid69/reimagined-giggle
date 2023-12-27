51c18d051f4ff691f42d1378b3e6f02c1fa12d2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:18:33 +0000

    fire type police bunch phone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
