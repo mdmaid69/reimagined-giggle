5b27413763fe9bd5795fffa34f0ac6cf86ea5122
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:43:10 +0000

    snow tape power steam address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
