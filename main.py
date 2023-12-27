f5077f8c1b894fdb7b91b62d76d04d84c7d4b338
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:50:30 +0000

    future artificial occur complicated strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
