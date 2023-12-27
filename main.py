af0e466c2257f0db1aeec434b9c08b3e3f337041
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:25:09 +0000

    unkind understand cap shade gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
