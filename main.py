f459b820207b345daa2e1e379528b83aaa756670
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:26:19 +0000

    left sort normal well question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import os
+print(os.getcwd())
