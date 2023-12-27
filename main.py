864f39b27282147dfdbde275cf44174fbe3ef0ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:46:05 +0000

    cut beat temperature way tough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
