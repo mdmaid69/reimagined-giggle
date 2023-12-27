7a651dc821e0c1bd1be1c54ba246ff69ef9045ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:34:58 +0000

    naturally promise direct drive view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
