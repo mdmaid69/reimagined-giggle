33b5529400b9604652b9a9bd39dff5937bdac972
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:30:46 +0000

    almost cold loan probably sort

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_area_circle(r):
+        return 3.14 * r**2
