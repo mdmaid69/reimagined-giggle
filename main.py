0f5c1f40b112be69bd06a037edef3c70bec9b6af
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:50:35 +0000

    smile put fault promise nonsense

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
