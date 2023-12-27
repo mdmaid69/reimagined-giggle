7fe9a0b7bfaff0f5816ba019655ea6fc6fff871f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:08:25 +0000

    while physical often tradition press

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import getpass
-def get_username():
-        return getpass.getuser()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_min(lst):
+        return min(lst)
