cd1921edfc8c26818d08062bc359ff0dff82e8c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:49 +0000

    picture rain female pupil pleasant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def add_numbers(x, y):
+        return x + y
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
