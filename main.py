f3e11be6aefe8e1a49a17055b1c0a987b0949fba
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:48:32 +0000

    bill far develop push succeed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
