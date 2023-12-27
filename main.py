5782143e2ec64ff67e14d12f5c91b2506b9cf178
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:47:38 +0000

    size disturb sticky vote influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
