b62ee2bf009850c6e4aa059dc22db390b52f67bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:30:30 +0000

    bin talk tape only content

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
