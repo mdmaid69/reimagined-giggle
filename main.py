594d155d90a3784d958a6005ad0e80d1055f2cf5
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:12:56 +0000

    onto season false Vv swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
