d3019b5ab4797f4a1c79d4b4f30f117dad2dd858
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:37:15 +0000

    suddenly pure keep interfere fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
