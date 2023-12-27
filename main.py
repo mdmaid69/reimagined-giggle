b176eaaeed539dfb62e5194c03dd1d304ee71d8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:34:55 +0000

    owe sure present introduce lost

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
