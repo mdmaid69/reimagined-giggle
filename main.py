f1addee0e5db24fd0e0e8584e1407bdc0f4afc6e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:34:13 +0000

    advice downstairs piece circumstance nerve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import numpy as np
+print(np.array([1, 2, 3]))
