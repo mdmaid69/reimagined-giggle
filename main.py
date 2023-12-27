a1a9a41f6d23e913ee84441244496b28ed54264e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:01:51 +0000

    so advantage objection poison next

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def is_odd(n):
-        return n % 2 != 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
