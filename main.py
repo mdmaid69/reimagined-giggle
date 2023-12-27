bffd9cbaeece6cda87fb6d51ad397da31dc5a63a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:28:54 +0000

    choose defend ready talkoutof main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
