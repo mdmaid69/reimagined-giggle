afb123b07d832cd8fe8a0d745a083414ad264d03
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:22:21 +0000

    case lie borrow off think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
