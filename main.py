c9a41d19a62c16eb0e819126072f614211c0301b
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:59:47 +0000

    mess record production heart safe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import datetime
+print(datetime.datetime.now())
