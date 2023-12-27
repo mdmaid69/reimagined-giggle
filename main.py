f1dc451d2bd1ee67aa4b56205f1114fecdf0db32
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:16:59 +0000

    Kk after challenge skin ticket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
