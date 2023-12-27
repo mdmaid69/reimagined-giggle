0f2dfbb3e5d442ce7887324acc99cedd51066b5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:04:50 +0000

    bad finger direction invitation deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_area_rectangle(l, w):
+        return l * w
