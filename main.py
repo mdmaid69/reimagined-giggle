635ecf70d57f9d7ed4809481ac05c0a777ae56bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:14:31 +0000

    tone fill afraid could somebody

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
