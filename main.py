2db3ee4285ac48546097e9ab63ecc26e0d4ed527
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:32:18 +0000

    little by dinner use road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import time
+def get_time_since_epoch():
+        return time.time()
