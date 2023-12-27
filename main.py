38cfe4246aa0f9cdc670e04184b7ca322c3ba16c
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:06:34 +0000

    another side somewhere trick giveaway

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import collections
+def create_user_dict():
+        return collections.UserDict()
