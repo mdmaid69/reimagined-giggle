0a118f5b470f8578c8b6afdcfeadb951a841bd3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:25:26 +0000

    back police Mm ago introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
