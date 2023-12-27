3c543b03180395a15fd7536f3261360505ae9e7c
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:18:23 +0000

    cry juice whistle recovery trade

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def fibonacci(n):
         while a < n:
         print(a, end=" ")
         a, b = b, a+b
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
