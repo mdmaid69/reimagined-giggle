81216ecfb5d6af50fcecbbc85adfbbb8e3b9ac5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:59:06 +0000

    replace ourselves ride building content

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def convert_to_octal(n):
+        return oct(n)
+def convert_to_binary(n):
+        return bin(n)
