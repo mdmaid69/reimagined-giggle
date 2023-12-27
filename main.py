44e3473d667c1d5f67af57a5b591ac273f13b7df
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:40:55 +0000

    trap unpleasant effort below swimming

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import re
+print(re.match("h.*o", "hello world"))
