fc4efa13c3af5411dbe5bb561fae3ea78c9dfae3
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:25:28 +0000

    important whoever pump wave satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
