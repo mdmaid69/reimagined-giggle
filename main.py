186493e04b4ea8e1487ce652e69bb3b975ba75d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:53:58 +0000

    wish splash interest intend loose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_as_bool(array):
+        return bool(array)
