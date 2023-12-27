52592caae3570b699abe2c8884841e69431a194d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:03:46 +0000

    difficulty depend couple soil support

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def is_odd(n):
+        return n % 2 != 0
+import array
+def convert_array_to_list(array):
+        return array.tolist()
