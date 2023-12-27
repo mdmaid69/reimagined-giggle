1a323f7fb1a519595a10f159f50ef2c73c12f626
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:54:22 +0000

    regular mark mark arm runinto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import collections
+def create_queue():
+        return collections.deque()
