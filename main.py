0096791e3e5574676da77c20d7bb21d1bdb7157e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:54:55 +0000

    couple mix Oo seat myself

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
