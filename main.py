3f476ab9bb125eb84f899f8e22f9050cefa03d00
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:02:24 +0000

    567enemy base physical to downstairs

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import platform
+def get_os_info():
+        return platform.uname()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
