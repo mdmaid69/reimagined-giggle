1699a49524193298333ae1c587a61dbef5985d25
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:20:44 +0000

    upwards too smell send strict

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def calculate_area_rectangle(l, w):
+        return l * w
