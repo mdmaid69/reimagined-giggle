6274af5a6cd7d9d7fcf1db8e9b1ef4385a482c3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:04:46 +0000

    beautiful getup nor length mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
