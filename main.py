dfd02c5371599e711761145a6a9501ae7a4fa057
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:19:43 +0000

    rather argument marry talk connect

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
