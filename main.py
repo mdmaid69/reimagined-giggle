3419f0034dacb847aaeb1c26b0e95f66653fb374
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:03:44 +0000

    deliberately experiment needle criticism with

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
