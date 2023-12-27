0176fbd6e7663479aa90ffdd983ec1ee2d9391e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:55:37 +0000

    judge male beat Uu detail

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def append_to_array(array, item):
+        array.append(item)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
