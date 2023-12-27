d2137a2917761ccca0112c7fd39125e9e6c4314e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:29:37 +0000

    off drive comefrom spring feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import platform
+def get_os_info():
+        return platform.uname()
