b24ccdd9a79602223122413c05d293c5fe37b143
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:23:47 +0000

    rush runaway relation shop mood

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
