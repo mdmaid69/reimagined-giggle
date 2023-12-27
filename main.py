b1fa99ceada6ad0df3d18c8119634941ab16320f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:54:43 +0000

    off peace indeed hear act

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def find_union(list1, list2):
+        return set(list1) | set(list2)
