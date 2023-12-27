723720ea096ff13db9501ab2c85ce36602d29d70
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:32:44 +0000

    round loose mention early left

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_length(array):
+        return len(array)
