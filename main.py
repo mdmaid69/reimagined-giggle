c85db9116b1c810e147e33671dd573435c1beff7
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:56:00 +0000

    hope pupil push guard beat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
