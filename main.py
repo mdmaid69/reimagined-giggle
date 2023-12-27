1fbcbb72277ccc007bb177885a87b3229429961c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:33:45 +0000

    dry satisfactory hello hold duty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_density(mass, volume):
+        return mass / volume
+def find_min(lst):
+        return min(lst)
