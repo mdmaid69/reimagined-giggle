da94e931eda7c27b5425a2fd5485b520a442c6a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:38:58 +0000

    climb difficulty squeeze lucky flavour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
