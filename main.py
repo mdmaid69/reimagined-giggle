1d73328798bc87ad85cd11924a2773c29502be3e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:45:43 +0000

    likely judge break least frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
