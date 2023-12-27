f3504d58616fd2a612f4f360ec318f44ba8734b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:31:32 +0000

    geton goup can beginning same

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
