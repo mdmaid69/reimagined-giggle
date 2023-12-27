7dbede9125fd2bbb013375024a8fa0ffd284dbae
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:09:10 +0000

    street still song description everything

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def greet(name):
-        print(f"Hello, {name}!")
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_int(array):
+        return int(array[0])
