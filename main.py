f88f2526cb692c49e40f1e266f74b730122a49e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:17:25 +0000

    cry spring copy true ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_current_directory():
+        return os.getcwd()
