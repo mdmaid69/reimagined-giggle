81f1404be1ea8a1d52e7480006c5c480f8cb5316
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:38:26 +0000

    thought workupto most hate calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pressure(force, area):
+        return force / area
