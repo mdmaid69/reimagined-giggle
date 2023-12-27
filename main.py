cbfe8d3b7b653bc6ac5bde48ac80817885874e84
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:21:01 +0000

    sore persuade feel tongue secret

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import time
-print(time.time())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+print("Hello, world!")
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
