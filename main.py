f279cbf21f76ac8bfe5d7860f2ae1b62cc9fd6c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:46:15 +0000

    emotion convincing real involve story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
