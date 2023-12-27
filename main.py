d6fa084d86b3628ad3f449e2bbbe4e939d564ce4
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:02:48 +0000

    mixture bill protection word guide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+print(re.match("h.*o", "hello world"))
+def find_min(numbers):
+        return min(numbers)
