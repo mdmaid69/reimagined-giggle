7486d584533e27c7cbbc5239ec684957718caa33
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:31:12 +0000

    change success word spill ear

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+print(sum(range(10)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
