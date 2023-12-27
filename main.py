73e7bcb6e9050b870c6bffa31e3715a47f903477
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:29:04 +0000

    tomorrow against now realize why

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
