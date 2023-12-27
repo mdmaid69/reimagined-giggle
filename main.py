35c4b6f4258215149ca28a9fe960263421693ee5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:14:02 +0000

    divide sink callback freeze race

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def convert_to_octal(n):
+        return oct(n)
