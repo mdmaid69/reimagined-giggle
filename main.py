30447bc7c572b1312c6ee328bbe56375e0a890ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:24:20 +0000

    each dance Yy weekend frequent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
