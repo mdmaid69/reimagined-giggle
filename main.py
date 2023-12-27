0e35def979a28aae7267a5ef67d519d5fd38a513
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:43:54 +0000

    round hello emergency hear their

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
