c3b4d9fcb086af879fa5f967ba3f978d483ea8d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:02:27 +0000

    but fortunately badly injury grow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
