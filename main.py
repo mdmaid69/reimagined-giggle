7e1b8aaf7128faf1b1f9c5ffc76be5bd8bef85f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:19:17 +0000

    realize careless loan concern pure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+from collections import Counter
+print(Counter("hello world"))
