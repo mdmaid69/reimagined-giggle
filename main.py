2f851d85b6f9c2a588c35a7fce44a496a97680fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:19:23 +0000

    innocent like wall soon broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_list():
+        return collections.UserList()
