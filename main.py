2eda1f4368cf909e70b0c437c9c0d14c5e67ac11
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:06:33 +0000

    ability culture kind naturally bottom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-for i in range(10): print(i)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
