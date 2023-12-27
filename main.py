5372c90795d935a5e2334ae1ce5042bc4de624e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:17:46 +0000

    score home firm snow respect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
