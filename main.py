df4b417c67889d12ded3709a4d60c38130d7625c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:21:16 +0000

    uniform blame world since Cc

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
