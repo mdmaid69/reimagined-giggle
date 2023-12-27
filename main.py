749cad3b88313daa5c98434735f6ad97a4d2951a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:34:36 +0000

    blame praise curious national contribute

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import sys
-def print_python_version():
-        print(sys.version)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
