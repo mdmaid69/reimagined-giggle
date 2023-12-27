501067f4c9ccb4b9849feff4dbb25de40367b701
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:08:00 +0000

    takeout your suit nervous geton

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import math
-print(math.pi)
