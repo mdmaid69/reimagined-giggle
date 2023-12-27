269c217f7fc541b4c7e6ff63e6f99c9cda1c3735
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:00:56 +0000

    beneath appeal yet near burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
