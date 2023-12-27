9ddec18984ab55d1c25a4473510dbd0da9eb0c2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:31:25 +0000

    damage confidence warn sort operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
