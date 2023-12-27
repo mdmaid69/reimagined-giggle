514a97798524024d7b5b6a5e74f18971f439bd68
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:29:12 +0000

    runover murder wipe action none

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
