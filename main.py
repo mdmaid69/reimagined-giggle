bf125b373d12c908cf45bcbfacb3ff25ce04c96d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:19:36 +0000

    stick favourite bag dinner figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
