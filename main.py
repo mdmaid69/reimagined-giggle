4402b5c06b983aee37b1df0b0d994d8920052b71
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:36:30 +0000

    rain to level swimming bridge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
