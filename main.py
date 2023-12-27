9c096f8ac8087f2faf5f4fb86c9c8913d6649602
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:32:59 +0000

    basic aim fit swing please

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_itemsize(array):
+        return array.itemsize
