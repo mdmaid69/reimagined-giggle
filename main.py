a1d80fed8da2c8d011161fc6bd4c2ba8925dd744
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:33:46 +0000

    shape most certain yours compete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def is_odd(n):
+        return n % 2 != 0
