ffbfd760caf07cca91962e07537cf4f5d34cbba2
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:46:12 +0000

    development young new transparent prefer

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
