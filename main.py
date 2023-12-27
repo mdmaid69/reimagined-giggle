2775b1f141e176366e2c6c6ff8eb757edbe8e733
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:08:07 +0000

    comeover many both bringup run

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def count_words(sentence):
+        return len(sentence.split())
