606cb172b5bf37cea869dd858dab7d9f16fef5b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:24:30 +0000

    Ee success famous goon case

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(lst):
-        return min(lst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
