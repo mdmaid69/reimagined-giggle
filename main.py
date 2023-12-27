728fd4c71bc13c1a9bd08e771ccb06b58e53b520
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:21:11 +0000

    march part responsible surface talkback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
