0a257b43624ead9896df5c01aa4b6b0fea8bf492
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:36:36 +0000

    comeround of lock run in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_stack():
+        return collections.deque()
