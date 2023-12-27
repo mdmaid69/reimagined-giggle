cb148ad22300561534b06e3bff163d3d7b35ac93
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:03:11 +0000

    urgent track centre finally film

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
