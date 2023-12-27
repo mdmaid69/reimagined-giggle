bb6056c7a4f651544d973697339021e20c01b8ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:18:21 +0000

    information capital certainly accept drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def find_max(numbers):
+        return max(numbers)
