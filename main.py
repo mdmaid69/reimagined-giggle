6dfcce328da78cff8247546651019b9b872a03ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:02:18 +0000

    rub threaten probably movement either

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
