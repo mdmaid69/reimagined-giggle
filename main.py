f6f9226a7b75475902ca805182a4364cd5bc784e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:06:42 +0000

    near whatever lunch room dangerous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
