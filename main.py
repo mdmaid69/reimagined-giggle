61b818b03478aa17d1758d5151974a479dc4f305
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:50:28 +0000

    pay light deep search shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
