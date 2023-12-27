fc232a8c90467abe333f0e8a933bb22d0934e619
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:05:37 +0000

    takeaway because suckme petrol slow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
