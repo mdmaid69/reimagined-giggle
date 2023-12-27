241fdea48b38c11ac8324ea6e8ebb754f4ac3e08
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:13:26 +0000

    hurt disturbing get pool cause

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
