a3960e634ea71389b50d77e10d545e2e9bfdb59f
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:48:26 +0000

    sort consist loss teach call

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def reverse_list(lst):
-        return lst[::-1]
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
