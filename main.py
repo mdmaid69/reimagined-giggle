ce34eb558b8dfdbfaa8ea691dad3e3c6eaeae414
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:50:01 +0000

    thirsty wire production narrow proper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
