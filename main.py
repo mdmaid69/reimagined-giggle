f40ce47d710edb6c1911e66314aff3f8a208735f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:59:21 +0000

    Cc morning free workout Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
