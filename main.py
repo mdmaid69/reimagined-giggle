22af3b24ce6b398eca52febd1d2071393a472e0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:42:13 +0000

    free basically preparation draw slightly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
