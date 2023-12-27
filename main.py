1ed0a99265c2ad8c21368fe8671b8a623b2b591f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:56:42 +0000

    turnaway new mistake regularly walk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
