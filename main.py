3696f13f73de615757b1219726828ded314004f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:51:14 +0000

    goon big ask almost quantity

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
