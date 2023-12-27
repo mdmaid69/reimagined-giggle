460c78cba3de5ad71fe577bd86fc9f843903450d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:13:28 +0000

    sting sink wind government shopping

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+print("Difference:", set(list1) - set(list2))
