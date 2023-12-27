25e9a2c10d8ef5595c0b16b67e9ed386155c6693
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:42:46 +0000

    difference answer wing trick another

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_binary(n):
-        return bin(n)
+import sys
+print(sys.version)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
