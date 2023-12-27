945c06254a51f26da26ddb2418343dbebe5672c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:59:51 +0000

    charge result port but study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
