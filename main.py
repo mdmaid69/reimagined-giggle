9ca55d3fcb2682d029076d296988ed6b00640ad3
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:15:50 +0000

    tense the dream guess combination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import re
+print(re.match("h.*o", "hello world"))
