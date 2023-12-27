c71a2a6f244bfc7cb203f74563047b21b5f03e1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:25:41 +0000

    getout success later work crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
