b4f01be96ad9e57d557e691af5b0550cfbece6bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:34:59 +0000

    comefrom succeed yesterday none front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
