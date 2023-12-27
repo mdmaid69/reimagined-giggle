dd38a428314eda3cc50ac86cfe91fde7c8a7b563
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:50:21 +0000

    will holdon history trip clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
