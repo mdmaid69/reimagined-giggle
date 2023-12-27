8fde35da1953b7389f669a10a59fee6dc21c25f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:02:05 +0000

    drive pronunciation responsible sit congratulations

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
