680ccad46b3dca3a8cacd499f36c77e1e190e189
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:17:32 +0000

    trick active swim wire that

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
