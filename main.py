49a91b5103a6831223729485bfd58da050a8cf64
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:54:58 +0000

    guess offer custom or become

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
