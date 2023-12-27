617fea8029b324321d800857188f85c41d015102
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:50:56 +0000

    sing either fault serious bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
