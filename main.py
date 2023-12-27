ab8c112b7c213623fe7789b07cab1ac44becec88
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:01:49 +0000

    bus sink field brave main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
