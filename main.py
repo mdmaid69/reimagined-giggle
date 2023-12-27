bada40463d05c883fb647b4e85b21e4e7d8fbde2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:54:33 +0000

    threaten colour sink smell relationship

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
