daa17c42abf6ac5e1ae624aed9e8ccd0cb3d0894
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:33:17 +0000

    pretty store appearance 567enemy touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
