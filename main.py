5396abea820d87630f722703e61a1e6cf934c803
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:09:23 +0000

    neat sore available command pleased

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
