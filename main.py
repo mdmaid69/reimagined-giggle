f9ca2c54878d8714d757c1b44bb95a51271c3f24
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:38:00 +0000

    proof people follow already hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
