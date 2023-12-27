330f64ffa55cbc7effe5ef09ef6baa21782a631e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:10:05 +0000

    destroy dig keepup report introduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
