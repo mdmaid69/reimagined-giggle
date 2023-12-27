e8095f558db2e5d99ce46fdc7c419eed7750206c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:28:59 +0000

    improvement tidy ring left my

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import tensorflow as tf
+print(tf.__version__)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
