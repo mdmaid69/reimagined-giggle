3f6655186eeed0dc468c9e5452015f1fe7f2e146
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:27:13 +0000

    nose related road us like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-print("Hello, world!")
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
