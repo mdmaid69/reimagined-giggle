eab63caad435837b6b6124bee89d896f3e1d8170
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:41:36 +0000

    middle television basis smooth shrink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def add_numbers(x, y):
+        return x + y
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
