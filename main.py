5a896a9af5b53bd2c39060e6cf1a80505e4ebba5
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:35:13 +0000

    comeoff left choice charge university

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
