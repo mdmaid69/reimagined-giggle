e912a0805f2526770be374584155c36079e710ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:47:44 +0000

    idea sort approve pull wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def reverse_list(lst):
-        return lst[::-1]
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
