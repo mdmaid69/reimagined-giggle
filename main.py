5f256bc83b79383b7c96545a766bb2f6f113c95f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:17:26 +0000

    summer left geton judge challenge

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
