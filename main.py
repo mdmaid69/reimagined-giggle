20ef6b1c204bafa42d6746d08b0c87971ce63c62
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:02:38 +0000

    comedown discovery team rough tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+text = "Hello, world!"
+print("Reversed:", text[::-1])
