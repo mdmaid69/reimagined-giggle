12e1f03e499681fa30cf0e04eda65a2c753a648f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:23:03 +0000

    contrast slide add beginning adjective

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
