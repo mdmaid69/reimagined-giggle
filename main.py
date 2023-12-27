f883fcab9410c0ccc26cfc9c3f2c618c75b21bc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:42:29 +0000

    fashion unpleasant mystery flavour listen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
