7a47b50735526bd230ded15705d50f4c3d0f7d2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:45:23 +0000

    tragedy before even circle whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
