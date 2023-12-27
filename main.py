2c9ef4545c22d664721f8f85a8d6d565dc3f6821
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:07:28 +0000

    like contribute probably may honest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_power(work, time):
-        return work / time
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_float(array):
+        return float(array[0])
