23a6bdb58614552c611ff43911b60deb6c08b8a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:33:33 +0000

    noone row also interest supply

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
