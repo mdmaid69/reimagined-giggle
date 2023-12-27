99db6c55809f0a3b0a7fa2fab9f379a584c94244
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:59:03 +0000

    smart shape goodbye spare giveup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
