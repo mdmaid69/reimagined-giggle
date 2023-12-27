ec8a8a535a133095cfce328714ac3f822f942d63
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:29:44 +0000

    name you add quiet luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_list(array):
+        return list(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
