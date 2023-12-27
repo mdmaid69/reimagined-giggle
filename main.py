82bf4c05e4157094a665069bd16af203fc45a2e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:55:02 +0000

    bury raw small catch serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
