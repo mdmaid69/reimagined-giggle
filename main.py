a387089c23cf8af3694ab188c04a5fdcccda15b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:28:58 +0000

    break legal crime destroy shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
