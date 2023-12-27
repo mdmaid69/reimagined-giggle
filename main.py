00475e6b9c4ba00c277ccee25ebbba058112528c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:01:23 +0000

    attitude scale development plastic comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
