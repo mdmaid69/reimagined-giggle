378e2447430b879943fd9fa3bce8e6bfff95b228
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:00:12 +0000

    young settle serve nothing search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+  def convert_to_hex(n):
+        return hex(n)
