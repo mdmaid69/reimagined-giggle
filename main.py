3755abe3a126f9727a4d078d1dc90d12a5b24d9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:30:03 +0000

    later when trust there base

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
