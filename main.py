b1a7915a0effdd72732b41f926ccc1b9f66f4a16
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:12:46 +0000

    rain second belief introduction speech

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_base_name(path):
+        return os.path.basename(path)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
