9488979c5e9efc00adfd9c685f282f1e506478f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:36:24 +0000

    tooth hate every tired condition

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
