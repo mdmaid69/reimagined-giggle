6d2fa247c2ebf7c1fe2905f0769b1de80cc91ae5
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:34:07 +0000

    tap call they hurt lucky

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+from collections import Counter
+print(Counter("hello world"))
