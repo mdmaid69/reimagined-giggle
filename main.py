c77dde69a302d24175069d2b67d1e523a48f915a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:35:53 +0000

    pupil life march shadow attempt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-for i in range(10): print(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
