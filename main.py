c5abd4c6af8ab42a8e9f239cf98ae4d95ff39ce0
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:52:33 +0000

    basic whoever occasion force rain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
