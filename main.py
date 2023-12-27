4c540da019b9f1356a480b77a90ede487f0202f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:58:18 +0000

    letter forget great sell pity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def convert_to_octal(n):
-        return oct(n)
+  def reverse_list(lst):
+        return lst[::-1]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
