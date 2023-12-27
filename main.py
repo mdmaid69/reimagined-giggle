99b216b3c76347bf4b944b819618b5f906b01c6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:34:14 +0000

    tune flow search public their

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_set(array):
+        return set(array)
