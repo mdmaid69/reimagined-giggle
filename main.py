d2ab132c0fbc34c7d618e8899618a1f03096fb5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:54:46 +0000

    accommodation most main after send

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
