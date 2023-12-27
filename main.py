0ed5a8e79fc20e1c9918c4b034a85c5e3ed570dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:53:55 +0000

    nose yes own approval way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
