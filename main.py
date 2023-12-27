229f51e44414660c1599f72a5bccb7539cd5e2cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:51:54 +0000

    whistle mystery cross city onto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_hex(n):
+        return hex(n)
