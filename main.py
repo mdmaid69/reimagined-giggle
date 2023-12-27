d958d36b3ef1a1a91dc30340a4f65d197c51783d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:53:37 +0000

    sensible clothes wealth sudden convincing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
