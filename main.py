530fbd914ae6801270571db54ec2536b737a6c31
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:30:18 +0000

    fly begin happen how replace

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
