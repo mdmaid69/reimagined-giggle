83dc032ce7fa00d9f1b26de7068b35becf49ccd4
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:36:47 +0000

    high beyond lead serve meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_index(array, item):
+        return array.index(item)
