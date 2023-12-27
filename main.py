7124c8bb3df7b350c64ed03efb126984af93aa9d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:30:11 +0000

    shrink score basically distance drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
