bf10e0fb0628d6cd01523c890d6dd26e1dfb4a3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:03:33 +0000

    swimming respect accidental project temper

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
