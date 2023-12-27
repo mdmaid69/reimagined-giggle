96e32d1f11d2657166502336dc89260f335e99cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:21:50 +0000

    shoulder suggestion sort look unhappy

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
