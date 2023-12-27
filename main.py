a2afd666c4b216a1cb1360b2cb9dead96478be8f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:24:40 +0000

    cause physical Pp foot luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
