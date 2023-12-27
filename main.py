a308be6897207f8f97e4436deb4c050de157da15
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:32:53 +0000

    female comefrom drawing guard first

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
