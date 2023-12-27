a976d74b21e51a8a3adf42ab5f308dbbfa4a8814
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:14:04 +0000

    appear lean onto separate typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_density(mass, volume):
+        return mass / volume
