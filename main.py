47cbf690dacbd7e210a3150a8c529c8524d8f0e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:16:42 +0000

    fall tomorrow so getoff force

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
