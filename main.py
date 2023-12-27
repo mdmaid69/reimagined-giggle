c33f055eec3a20918a61dba5e86f9baa6a4a50a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:54:37 +0000

    bring honest at right practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def count_elements(lst):
+        return len(lst)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
