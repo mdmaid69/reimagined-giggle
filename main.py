04041f8487c554215c42ac87aad90e9210a39a4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:15:01 +0000

    spend attitude identify wire rob

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
