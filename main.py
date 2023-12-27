d633a6b405b06891b984349cfbc10fa03c07704e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:23:43 +0000

    bin slip laugh balance keepup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+def exit_program():
+        sys.exit()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
