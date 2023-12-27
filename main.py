cfbb29fe2f588f97e65e18986d1a09a47f8ac330
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:51:59 +0000

    car with connected Dd full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
