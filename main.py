d34c59a1bbf574223b8d7c4685f03906e597588b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:15:47 +0000

    cheat cost afford plate room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
