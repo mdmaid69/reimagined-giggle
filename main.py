49a7db53dfb9ae4955b4c620d7a20b9bdce0d01b
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:27:56 +0000

    would bar achieve mistake bus

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def divide_numbers(x, y):
+        return x / y
