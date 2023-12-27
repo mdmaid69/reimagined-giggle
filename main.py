580a35d1feef64b8d3d6de52b9cbd22d054d49c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:17:35 +0000

    walk hold crazy interview guard

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
