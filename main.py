12abce2af36977fd2b1aa84b4252863cb22dfdb0
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:24:57 +0000

    style motion cold object neither

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def subtract_numbers(x, y):
+        return x - y
