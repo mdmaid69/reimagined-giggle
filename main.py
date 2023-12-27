432e2ca5f26174db7ec413752f43c8dc1db53149
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:42:45 +0000

    angry polite turn pen clearly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
