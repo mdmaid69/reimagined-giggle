057ef9bb382ce9f2482d5413bafc6348484cd4a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:37:03 +0000

    call left independent many Nn

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
