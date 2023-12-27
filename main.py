26c39856eee8d55f3b100c6051db941d85e7a5a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:44:43 +0000

    breakout message prepare view soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
