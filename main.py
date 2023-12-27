1997434f1eca6eb1bef2a2e701152c3381d9d0cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:03:22 +0000

    who spray confident wash within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def multiply_numbers(x, y):
+        return x * y
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
