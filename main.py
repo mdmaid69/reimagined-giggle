76931adf4261ab3f89b373c9fde248082e2f7e5b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:00:50 +0000

    admit birth yet recognize lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_counter():
+        return collections.Counter()
+import os
+def get_current_working_directory():
+        return os.getcwd()
