6600151d19e809a8b34a7d7bb7e60a8e62a13115
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:07:12 +0000

    fast coat worry steal along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_slice(array, i, j):
+        return array[i:j]
