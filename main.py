7ba363529408bc85e8516d3a2e4d0869db5074a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:49:16 +0000

    interview well message on wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def count_elements(lst):
-        return len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
