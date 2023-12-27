5684f1036759174b935cb19406a80d3d3ac765d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:30:58 +0000

    crisis woman fortunately flow succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
