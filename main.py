e7387afc0eccefa94ace592397dbf7bc6a4945c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:27:21 +0000

    black star instrument spray discuss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_user_list():
+        return collections.UserList()
+def convert_to_octal(n):
+        return oct(n)
