65e4de8b2588e6f92968c499c7ff88d70d9dcf16
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:10:31 +0000

    probably message ourselves dish risk

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def sort_list(lst):
-        return sorted(lst)
+def find_max(numbers):
+        return max(numbers)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
