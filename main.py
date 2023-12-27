66a2501c096af04eb2f8c6c23cf03614312d2edf
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:49:28 +0000

    start legal field pain time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
