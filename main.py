57fb59c4f13c8bd53b059c6e9e2f7922ee79fca4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:33:13 +0000

    trust circumstance runinto bin speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import datetime
+print(datetime.datetime.now())
