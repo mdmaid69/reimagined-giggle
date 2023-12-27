12682a4247687b2cc61578694c95d62925f58952
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:39:57 +0000

    study past plan satisfied clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
