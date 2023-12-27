f0d416c0cb0d93c2d2f2965596f4138a8a8d0a01
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:01:52 +0000

    deep cap present can black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
