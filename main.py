e73e4b8def0ff6490432ca8cbfc29455d7e43285
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:23:45 +0000

    only compare some home sink

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
