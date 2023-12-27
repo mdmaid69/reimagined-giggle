4d86fb96a1e21c00a2f93fb5fbd7c6604ee1b9f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:09:08 +0000

    wire beat total that properly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_density(mass, volume):
+        return mass / volume
