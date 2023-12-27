c73887a1eb7a9cdd33a6b604525df7ef999fa768
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:09:55 +0000

    possibility long standard black tomorrow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
