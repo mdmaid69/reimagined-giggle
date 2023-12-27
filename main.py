af1fb583bddd073a66b6b5c72887eeb4a7d8dfb2
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:14:22 +0000

    harm opportunity lie smile must

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import tensorflow as tf
+print(tf.__version__)
