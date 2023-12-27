e70a22c07559eec9f9449b62bb8613ccd18db1f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:34:52 +0000

    bin design object wide takeaway

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import sys
-print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
