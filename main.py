c59c1e92ecb642b1dc88d5c8bb5844fc82958426
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:02:07 +0000

    anybody custom true crash demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_string_from_array(array):
+        return array.tobytes()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
