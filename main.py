8ef7cf64ed2d43dbe5a11da9166d79ff54cde1fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:55:56 +0000

    since dry life previous correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def multiply_numbers(x, y):
+        return x * y
