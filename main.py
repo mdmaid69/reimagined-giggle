0eed1efd09d0ccd199914af0a1930ac361ddf39d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:43:07 +0000

    guide the receive sensitive slide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_queue():
+        return collections.deque()
+import time
+def get_current_time():
+        return time.time()
