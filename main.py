c36d16226abeb4cdd00d448d5f0617873ba1bd54
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:38:01 +0000

    cry offer shall warn dark

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_acceleration(speed, time):
+        return speed / time
