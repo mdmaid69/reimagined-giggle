fc058513be3854a342a4667da9e8794292bc45e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:25:04 +0000

    sort how relax order possession

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def reverse_array(array):
+        array.reverse()
