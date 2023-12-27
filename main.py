682b80bc902e4917b6f785b2e4c16e74dfb219b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:52:48 +0000

    sport keen offend obvious keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
