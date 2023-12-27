6f4396b524701e2dc10aaf66fcc50892a26bf7d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:35:04 +0000

    punishment substance quality talkinto talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
