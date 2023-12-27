97f8e0f3a963f74662242dfe2ffecefcb4f9a586
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:23:07 +0000

    obtain push bit target image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_density(mass, volume):
+        return mass / volume
