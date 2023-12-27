bcce9ff4bb60719f6832a717ea464459cc41aaa5
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:12:20 +0000

    dinner little common takeaway damage

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
