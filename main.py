0eb4ff3da8d9f7ff40c0528eb6160ba9ddcd0a86
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:25:32 +0000

    report Mm cough either hope

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
