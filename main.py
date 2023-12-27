f097a528d5d55b2065e851d1181b7e7d8acba7c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:54:00 +0000

    operate terrible steep figure uponon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
