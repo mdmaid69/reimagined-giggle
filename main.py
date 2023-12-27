28611f6075ee402b6ee190464e5d2a5f4f0687d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:08:10 +0000

    minority meal thorough success extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
