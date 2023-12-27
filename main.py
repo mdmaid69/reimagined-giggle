25cb5be70071c2042e3eafff5023910e95a3b5a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:02:46 +0000

    give comfortable mention hurry estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
