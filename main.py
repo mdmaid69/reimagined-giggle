db70988a8faadcf0b1cf767c6eedec8a8e64336b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:51:31 +0000

    physical form season formal ask

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
