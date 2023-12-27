ad91b73b1e98a3ebee2109378f9383b007a9df07
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:53:25 +0000

    reliable moment dream ours instrument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def reverse_list(lst):
+        return lst[::-1]
