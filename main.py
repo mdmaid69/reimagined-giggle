f3d7462949a9a22d663ffdab249df5bdde33fd69
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:37:41 +0000

    total hot force spin system

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-print(math.pi)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
