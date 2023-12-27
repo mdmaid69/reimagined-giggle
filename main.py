6298d0d464aca254453d9bbaafa30a49fe2b7be0
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:23:24 +0000

    scheme forget likely commit over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_error_function(x):
+        return math.erf(x)
