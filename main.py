bd47f6015a5289c7d270b8cad485a623a32169ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:13:59 +0000

    condition family month view slow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
