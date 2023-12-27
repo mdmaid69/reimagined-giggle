361b0009b722b5cf923838ae2ff32c1457306b13
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:24:30 +0000

    mean lookup none lucky movement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
