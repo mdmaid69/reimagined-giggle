91bd58b4128258c863df86ce16fe340aa3c29515
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:20:31 +0000

    outside ever Yy downstairs everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
