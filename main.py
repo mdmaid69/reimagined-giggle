baf2b7daa12f1b7e5e1a6dc8e6320dc36e8a02e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:12:49 +0000

    unlucky grand hers from our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
