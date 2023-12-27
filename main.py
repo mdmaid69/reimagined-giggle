64bbbc36f26ebb6e475af43f0288a7d92cee36de
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:29:52 +0000

    remark type connected Kk care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
