ecf8e3e09001634942e60bc9049e8f4c5f43fe68
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:08:02 +0000

    hello excellent better threaten wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
