6287a5d4f9bf0fb2c3b8a25483f5097deecd1009
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:56:33 +0000

    Oo serious university bored manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
