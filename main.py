80e6d0101bd3d8eb0200e39b1831a3aad479a3cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:31:49 +0000

    settle medium favourite meanwhile prevent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
