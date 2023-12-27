db32bc7158a72b5f3c04c24e661d5d081eecb18a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:40:45 +0000

    account about bath neat present

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
