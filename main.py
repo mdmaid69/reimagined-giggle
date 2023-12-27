1dda631a04c2bf285e72bd0ca0a9fe251ade0f16
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:10 +0000

    ourselves copy week lift cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
