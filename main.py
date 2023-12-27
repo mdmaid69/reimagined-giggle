2993586c09a6808d96a8393e2c13758f5a1fcf12
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:22:00 +0000

    disagree yet bend dick calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
