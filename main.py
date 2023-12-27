ca323c2dd6b32071cf4bb8ee832aa166f60fd58e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:25:52 +0000

    recover false splash buy somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
