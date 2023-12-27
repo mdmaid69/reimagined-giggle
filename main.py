d0dd0f8497d3b36d25e3df114377302cb7b9b878
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:50:17 +0000

    scheme switch recommend description nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
