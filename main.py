6847844699ca5913b6caab7127c718fb2443faec
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:54:41 +0000

    brush form progress equal criticism

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
