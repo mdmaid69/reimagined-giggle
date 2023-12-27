c5c2d3055994fd43193193083d898b381b2e4a75
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:42:08 +0000

    terrible live worried front spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_density(mass, volume):
+        return mass / volume
