fbaf2bf1c392195e795f40a34e5fd72871985b63
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:43:10 +0000

    remark inside frequent regional exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_exponential(x):
+        return math.exp(x)
