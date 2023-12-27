2e3e29a919756da78bd9098d44aaf60770891f0f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:10:34 +0000

    disappointed sing wonder nervous lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
