ea8d2a828c36fc656862a4ef802b6bc111abbcdb
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:04:02 +0000

    passage afford its point usually

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-print("Hello, world!")
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
