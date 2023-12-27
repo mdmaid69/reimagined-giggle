e002b8c6ce0e4c8ba5234a6e9bd7a07a134e9e04
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:36:41 +0000

    forever moral buy law matter

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
