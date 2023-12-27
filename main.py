0e57a577033c05570bf60b77f24a0c7f526c00ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:36:23 +0000

    failure ago lost equal such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
