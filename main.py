b52484a13812c319c492010141dfe0bb4581f463
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:40:05 +0000

    manner declare free content claim

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
