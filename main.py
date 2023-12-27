6d765bf2c8a8acbc0002c66de0bf14e64c1f8625
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:17:48 +0000

    protest blind wipe touch many

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
