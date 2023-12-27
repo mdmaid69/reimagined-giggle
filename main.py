601a6ef76e724b268d048821d9febca15d686f0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:46:14 +0000

    quit message lay butter watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_acceleration(speed, time):
+        return speed / time
