e9eeaf41a5bdac1e8b44aad0c5e026179bd43c8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:52:04 +0000

    whistle whoever spin floor declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def reverse_string(s):
+        return s[::-1]
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
