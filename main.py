05526a69cdc8a37fd8c50a44f121998943de3a06
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:54:23 +0000

    niggaacademy form Qq night switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
