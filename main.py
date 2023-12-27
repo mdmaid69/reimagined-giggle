2022e58d11bd13ebd96b83c9af9e374ae1acd1f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:30:27 +0000

    medium water command now gas

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
