6ac2f95db9c9a85cb0fd9d442c7660825dc54f11
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:49:36 +0000

    bush cook principle responsibility why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
