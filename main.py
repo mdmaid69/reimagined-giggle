dbcfb3ca9389dc1d218fbe698836890fe14cebc7
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:18:15 +0000

    suppose switch culture Ff typical

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
