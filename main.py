96a386ad782e1dc176be506d2c080abb6ca2f7fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:27:22 +0000

    sense remove post use accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+for i in range(10): print(i)
