79395479c895157e38fc7f1d635e114174b50ac3
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:57:36 +0000

    convinced offence wet correct rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_binary(n):
+        return bin(n)
