8d9147542c31b4aa6cd907b6e9f471dca457f54d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:18:08 +0000

    off everywhere much cruelty blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
