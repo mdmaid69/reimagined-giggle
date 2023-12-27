47fd87023021177b87ea0929b22162a816523df7
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:26:32 +0000

    variety introduction hole television there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
