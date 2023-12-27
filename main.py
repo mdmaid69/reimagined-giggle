63094e428c407c379c648c08fc8fa772279a590d
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:23:20 +0000

    already politics crash benefit bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_density(mass, volume):
+        return mass / volume
