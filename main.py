d622b0cc45c5a54a699e3b790b20f04be3620fae
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:14:33 +0000

    hate ready whatever shock ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import getpass
+def get_username():
+        return getpass.getuser()
