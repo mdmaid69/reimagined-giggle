1a5b167ac5df21c25da45740fcd9b7d478191560
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:32:30 +0000

    love bad near business time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
