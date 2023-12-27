0fb39ac7aa18e0b4443de488034b8468c291d9ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:57:41 +0000

    there each shoulder stiff wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def reverse_list(lst):
+        return lst[::-1]
