ab3166a52e655a912580d8b23f2524b8d9099a0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:25:13 +0000

    necessary cool software transparent show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_odd(n):
+        return n % 2 != 0
