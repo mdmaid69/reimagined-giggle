a5773a536e215516c4577f371ff29bd1e4f34b3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:03:40 +0000

    keep total breakin laugh dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
