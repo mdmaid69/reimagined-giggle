89b6cf69c43490e1cc7faa677e206b4f9b6d62ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:08:35 +0000

    cheque empty learn mine file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
