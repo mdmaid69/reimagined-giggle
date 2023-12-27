86551c9cd4d563970457be709c49feb092a6b77f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:22:27 +0000

    talkback hang whisper fit whichever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
