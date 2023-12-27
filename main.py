d6eef601f5a041fd801c11b9bb44a52829583659
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:32:28 +0000

    breakfast hardly less window fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def reverse_array(array):
+        array.reverse()
