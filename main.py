ab31bcbf0165d5de1c60d608b67fde49d07b6550
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:39:25 +0000

    square office influence tie say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import random
-print(random.randint(0, 100))
+def roll_die():
+        return random.randint(1, 6)
+  def remove_duplicates(lst):
+        return list(set(lst))
