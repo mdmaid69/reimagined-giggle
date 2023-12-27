18984c05d6631247faade10a643741dc19e22a20
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:06:08 +0000

    dance stop waste further obvious

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
