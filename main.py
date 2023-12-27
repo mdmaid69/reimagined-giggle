cfc74089269a7e914f1e41408159e40a1eebe9bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:03:15 +0000

    separate suffer innocent issue watch

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_square_root(x):
+        return math.sqrt(x)
