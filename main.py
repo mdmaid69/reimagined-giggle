7efa6cd9c122b0a0628c92baa841bd705d26ad6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:07:41 +0000

    pile borrow murder board through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
