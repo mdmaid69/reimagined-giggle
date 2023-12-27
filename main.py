d20941453a25a2885f8eae0c31c7a6eaa587db9d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:27:42 +0000

    street object shut point dress

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import logging
+def log_message(message):
+        logging.info(message)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
