8768fc0a55d1afb4da1b83180234c1ec75e753fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:27:40 +0000

    contents angry mystery gas tour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
