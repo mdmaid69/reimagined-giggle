87179628bc66e9c00b3e24cfd8a43edd48ed67ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:59:18 +0000

    bang flow slice contact 568energy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
