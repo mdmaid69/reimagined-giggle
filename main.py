fcf5ecf617fcdfc73ed4a333047e1f04d7b1fa7d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:53:11 +0000

    neither fence point everything science

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def cube_number(x):
+        return x**3
