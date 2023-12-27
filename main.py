6295497d80ebe0b9523415d1a3167d04ac31d31c
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:16:33 +0000

    unhappy motion Mm raise tired

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
