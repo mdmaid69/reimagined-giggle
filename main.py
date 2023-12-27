63ae4141fad5316227239bd6ff9f4a14ddb82879
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:04:04 +0000

    talk mean adjective remain while

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
