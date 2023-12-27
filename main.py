a4cd9972204a7f75015e8bf6ce35331966511894
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:52:40 +0000

    nice public draw lower strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
