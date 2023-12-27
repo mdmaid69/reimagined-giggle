f5525adbbeac5ae31d4fc1575153006b662e421a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:04:55 +0000

    least trousers final machine lead

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
