212c8e5e8fbd1f160fe7f9216226394282a725f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:16:22 +0000

    sense brother warm cheat weigh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_volume(length, width, height):
+        return length * width * height
+  def is_odd(n):
+        return n % 2 != 0
