eb5bedf6c8ca2119a89512e3ec9faca73b5ebd1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:13:29 +0000

    year room about pretty possibly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def add_numbers(x, y):
+        return x + y
+import sys
+def exit_program():
+        sys.exit()
