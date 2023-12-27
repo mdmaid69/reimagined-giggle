b703f928df55c9b49a081df4300238e4b6cb6f0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:19:10 +0000

    mark test firm crowd favourite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
