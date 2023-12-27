ee63fb68e31a78c7ca44a0dd5e91333001fc620b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:22:55 +0000

    sign again once taste complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
