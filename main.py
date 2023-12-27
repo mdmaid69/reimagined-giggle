92a50a5be3fa34efd3c7116dba3bfb4479856f84
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:06:46 +0000

    expect up punish sleep mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
