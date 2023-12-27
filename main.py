c6590f9842e6baf6909d94ac385b40cded9e04b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:25:46 +0000

    press increase stiff slice Ll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
