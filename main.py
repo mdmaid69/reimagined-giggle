dab9085cf2c2681a8e35cf1752367bbc80f67705
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:49:35 +0000

    dress comment hang absolutely spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
