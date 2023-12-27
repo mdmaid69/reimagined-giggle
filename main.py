2067e2b63686c609194cb5b38eb6f2c53b82a2db
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:55:13 +0000

    nurse ours loose past morning

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
