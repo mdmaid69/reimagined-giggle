ce428df9c380b1e902992f9a77af7dc0c6c6cfc6
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:57:33 +0000

    well immediately ball ring still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
