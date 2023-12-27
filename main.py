14bf8a4b640cdbc1eecc33aefc296b90099d5444
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:12:39 +0000

    support save sore blind turnoff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
