d55d8030ec12bcd13cde6f8cfb56fa179fb6827a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:05:20 +0000

    prize clear react blame exercise

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
