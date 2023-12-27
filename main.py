ea36f275abba81970b54780780df4b14384a1bbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:10:38 +0000

    punishment sticky occur urgent battery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
