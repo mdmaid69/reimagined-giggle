44b5f82272a4ee48c56d5ca8b232cd498f5f14c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:01:18 +0000

    name spring turninto extra track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
