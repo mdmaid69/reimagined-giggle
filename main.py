eccee4b902f259f1edf2fb5bc185c5f1326b9ee0
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:03:38 +0000

    refer so beat gift flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
