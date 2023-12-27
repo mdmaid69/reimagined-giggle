9d57cf136bc3c7e1adbd9473b4ad135f32daef97
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:06:02 +0000

    side kind reply rich advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
