e08410b69ca416bb2f1b8085a40ce3dfc83caf2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:15:28 +0000

    crowd of may watch disappointed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
