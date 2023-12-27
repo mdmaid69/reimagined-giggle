f804076432b84a71cd2b88d99e0447291b811e58
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:42:11 +0000

    man workout have news take

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
