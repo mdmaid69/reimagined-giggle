ee5b94be51e7b51a8542398057312464426028a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:28:35 +0000

    improve train sharp wake question

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import time
+def get_current_time():
+        return time.ctime()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
