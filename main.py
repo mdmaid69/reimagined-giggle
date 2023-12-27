0310373e422a922c13265f1342664eafe92cdfc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:25:54 +0000

    bringback refer bar clear radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
