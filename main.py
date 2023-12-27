8c98e8c46c7f26bb2d2135577237276b4dc8fce2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:56:17 +0000

    shop snow dry understand block

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
