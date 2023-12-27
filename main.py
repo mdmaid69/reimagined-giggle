b525f34f950757f5d932fed7dd67eff067e4adcb
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:10:46 +0000

    gradually strike rain insist survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
