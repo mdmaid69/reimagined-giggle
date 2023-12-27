51218c75ff5d726319ae6917164c9b60ef866328
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:21:43 +0000

    recover get know mood uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
