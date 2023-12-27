ac6ace550aa45f0815d0e919a5c097cb0e62a2e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:41:01 +0000

    question autumn holdon fix miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
