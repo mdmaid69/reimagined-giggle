89f7a88d72f68ce0210dbddeea4e6de9b5cf07a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:29:09 +0000

    desperate strike any there fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import sys
+def exit_program():
+        sys.exit()
