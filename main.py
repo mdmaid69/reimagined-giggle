c7b05e51fa0b553e49dba5abf57b2a989c517a41
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:29:28 +0000

    considering stay waste insist class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def is_odd(n):
+        return n % 2 != 0
