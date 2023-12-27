bc94d2fde447ab1d1260fab8fd7c9f5c0166f7bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:30:36 +0000

    receive firm reasonable evening home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
