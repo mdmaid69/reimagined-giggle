4d2d3f4ca6fb5b0c9fc21671553dbaf6837e7b4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:39:00 +0000

    word soft worth style model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_bool(array):
+        return bool(array)
