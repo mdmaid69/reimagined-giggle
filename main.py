ed4e7faec6117ce565c1452d0c072258fcaadf9e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:30:01 +0000

    dear pronounce happen he bound

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def sort_list(lst):
-        return sorted(lst)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
