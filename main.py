f04d91b75f8302dfa526a15346e3a7ac8d908447
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:30:22 +0000

    stay slow slip wish attempt

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
