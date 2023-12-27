e6f4899f60f4cf59178d2264937d2287151d463c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:27:16 +0000

    bright movement unfortunate movement sad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
