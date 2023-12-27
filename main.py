87a7407c478319fb9ea8b95bf07480595035a6cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:37:47 +0000

    replace admit recognize stiff part

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
