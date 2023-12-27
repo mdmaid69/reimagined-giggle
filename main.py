c6b6d664f5fb5cfece089f39aa16369d900b34a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:39:35 +0000

    stone vision closely statement hard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_work(force, distance):
+        return force * distance
