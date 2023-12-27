4456a55ecfa4e3b21e2dcb11019010ccb9657c22
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:30:05 +0000

    wise imagine fire afraid water

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
