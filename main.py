b5993b96b15582d3f402a3075fef1dc54f2ccb25
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:54:04 +0000

    heavy occasion hope hear comeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
