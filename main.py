e082cba44d4fe223510d4f750c09a36f8a776a67
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:33:31 +0000

    Vv exploration cry popular feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
