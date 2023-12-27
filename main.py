42c029da55a3412b21994fb6cda0029c153d5ffc
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:51:31 +0000

    aim male available space accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
