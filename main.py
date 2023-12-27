f2bfbce56f68e4acbc45b7fb6526ea8c2a332d57
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:14:30 +0000

    then pupil ground well runinto

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
