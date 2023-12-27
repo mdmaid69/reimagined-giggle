2eb10385648cb7bc8fb0030bb968e85ba7f11e81
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:48:31 +0000

    better trap base real promise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
