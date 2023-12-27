158ae4f5f13719a482a33e922820b8811b2b59bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:03:53 +0000

    mess weather angry tidy dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
