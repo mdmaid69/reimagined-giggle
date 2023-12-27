c9182ae667da5b09f6189774d9e5def211cdfc68
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:43:52 +0000

    frequent offer seriously deserve noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
