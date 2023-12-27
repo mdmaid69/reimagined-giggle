b258aacf8ea2b52aeb0db795213d2d5adfcb477c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:27:11 +0000

    goback(to) machine row comeback emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
