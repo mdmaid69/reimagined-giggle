6d0e2b15869bc9c16cd9b0b2e20572bbe4b1cd89
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:33:54 +0000

    physical force start blind concentration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_os_info():
+        return platform.uname()
