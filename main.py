738d8746b9f50d057987f89d1576facddcf3886c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:50:07 +0000

    pain remember nice short space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
