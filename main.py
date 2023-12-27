4c4047bd04dfad71d2b2a3e05f619bbc9eb9073a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:57:29 +0000

    holdout fold card war inside

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+def add_numbers(a, b):
+        return a + b
 import math
 def calculate_euclidean_distance(p, q):
         return math.dist(p, q)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
