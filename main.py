edf8815b4490071fe901fc4f45940001f465a62f
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:34:12 +0000

    responsible congratulate error figure race

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_acceleration(speed, time):
+        return speed / time
