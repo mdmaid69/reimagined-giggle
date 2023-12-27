6337443867d8a5a13697771e7ee9d052bf2bca9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:46:55 +0000

    juice threaten truth comefrom tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_density(mass, volume):
-        return mass / volume
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
