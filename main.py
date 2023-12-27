c06789c958e0aee18a90ec6da8c277a992e25e2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:22:51 +0000

    confident absolutely hope confidence tool

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import array
-def get_array_item(array, i):
-        return array[i]
-def greet(name):
-        print(f"Hello, {name}!")
+def set_array_item(array, i, item):
+        array[i] = item
