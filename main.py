f2fba52311229305a480e47759e62d38d5221365
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:53:02 +0000

    voice dream report science interest

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import os
-print(os.getcwd())
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
