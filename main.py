852e63d065e3db6d23edc1e18194181fc347c228
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:46:04 +0000

    knock production private meaning file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
