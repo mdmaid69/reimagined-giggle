2501a0d2dbb3d8fdcf96ead134a3112856d27a59
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:16:59 +0000

    film consult often walk comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-for i in range(5):
-        print(i)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
