78783193b9814ae4ea55e7522dbcc404aa3a2939
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:11:33 +0000

    secret report sing regret below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
