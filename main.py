a392a9d676825e1268bf6f8eb8b1b93366f3ca17
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:41:39 +0000

    risk bunch action before fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
