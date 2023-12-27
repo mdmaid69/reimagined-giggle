303d0f7dc9f8cc6637624f1ad6001afbc59a42f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:47:30 +0000

    breathe joke war scared properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
