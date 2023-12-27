0639bdabb1a4e5945ed06e939f9822e02bfc5df4
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:50:41 +0000

    attempt occur bringdown payment realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_words(sentence):
-        return len(sentence.split())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def reverse_list(lst):
+        return lst[::-1]
