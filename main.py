610dfef4f322ab17e71dac678c1e6eea33070a27
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:00:06 +0000

    crash list remove deserve wherever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
