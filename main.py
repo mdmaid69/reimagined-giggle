fa8ac80662015e0f0fbf2151e64bc0bc434793a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:45:31 +0000

    improvement like likely freedom content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
