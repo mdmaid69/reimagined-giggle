b7702f48fba038412f76083fab0f1abcf33a4fb2
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:43:46 +0000

    criticize care clue imagine behave

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def find_union(list1, list2):
+        return set(list1) | set(list2)
