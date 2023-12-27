d3703849598c4f6f85cf92e4248b59933ec99119
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:01:00 +0000

    vote naturally shop clean convince

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
