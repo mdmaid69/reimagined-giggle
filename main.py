013d122e35fc5480df64e62d5d826969d227d913
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:58:37 +0000

    branch blue save thinkover split

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
