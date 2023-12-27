a4e95df9dca5b7b6ddee7c7fcb9bb6a94ac9e2e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:41:18 +0000

    total accept mistake extraordinary contents

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
