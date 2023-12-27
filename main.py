443646db1fcac375f84083cc2da118afeb43b4e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:27:56 +0000

    brief mean giveback side permission

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_repr(array):
+        return repr(array)
