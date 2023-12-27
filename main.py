d470479d05740f43c8e14fabf1b4cd2bed3eab83
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:33:10 +0000

    like with square clear crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
