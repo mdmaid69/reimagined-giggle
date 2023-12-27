d0f4064e589dabb78493260c2a914a089281a846
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:43:19 +0000

    provide crash check breakup circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
