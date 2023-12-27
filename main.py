65117ab5464e1d7d1f04591b8fa7489d7929e62f
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:21:35 +0000

    show total scale successful speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
