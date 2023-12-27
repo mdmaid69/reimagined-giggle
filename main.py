bf7e4e385fa933baf6fa9f45237b4a10de51ada7
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:42:44 +0000

    finally crime stand instrument spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
