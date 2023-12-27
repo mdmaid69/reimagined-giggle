73ce56a1adf25aaa25a95057b1965e7e05756739
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:05:43 +0000

    worst anybody laugh nice soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
