55e0a13cfeff6eca95678f4ee4ba9e68ccd9c732
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:13:20 +0000

    field snow worried recent injury

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def find_max(lst):
-        return max(lst)
+def count_characters(sentence):
+        return len(sentence)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
