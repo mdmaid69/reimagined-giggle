855becfe36f72a894299ede1a1a6b7539887fc18
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:06:12 +0000

    generally slowly naturally view century

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
