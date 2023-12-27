ba33c5f2aa1366287715da1d974237156d33363c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:55:21 +0000

    computer firm nice hurt wheel

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def multiply_numbers(x, y):
+        return x * y
+def convert_to_hex(n):
+        return hex(n)
