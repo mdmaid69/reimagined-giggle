4e2d6177e8a621db48bdf7da7dcf70c573b99298
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:11:40 +0000

    friendly near treat admit force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_item(array, i):
+        return array[i]
+  def convert_to_hex(n):
+        return hex(n)
