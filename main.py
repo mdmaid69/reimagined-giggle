44697ddbf5f8bc098ebf2fecbd98bb262b10666a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:54:39 +0000

    afraid ago pile complain land

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def convert_to_hex(n):
-        return hex(n)
+  def add_numbers(x, y):
+        return x + y
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
