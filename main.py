1d894af36fcc1f15c03069376c3ad725eae4691f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:40:50 +0000

    guilty unite tired in discussion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def convert_to_octal(n):
-        return oct(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
