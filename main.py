9ab73020622d17a414e522f4bf03e8642628b7da
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:35:59 +0000

    uniform impossible receive backwards television

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_sign(x):
+        return math.copysign(1, x)
