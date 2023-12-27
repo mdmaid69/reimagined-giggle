1cf6e72e4d91fb3a829159f34fd5b62b37e30a19
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:49:36 +0000

    order deliberate sudden passenger worse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
