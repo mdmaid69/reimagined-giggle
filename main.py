1c4ef9cbb39e544477418730a3bc8533588364d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:02:03 +0000

    replace scream on behave protect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def reverse_list(lst):
+        return lst[::-1]
