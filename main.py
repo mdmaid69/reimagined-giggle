b3b9d0fbac696cc0a453a94b7ec7e4773cddafa0
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:42:23 +0000

    partner debt rush hard ready

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def square_number(x):
-        return x**2
-def calculate_power(work, time):
-        return work / time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
