623398a0719ded75692e1de032caa8fa7b246179
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:10:39 +0000

    half geton cough become clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
