729e21efde4af5eb19c384a3cf594a8396ae9cf7
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:05:16 +0000

    material foot section away inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
