d5449b416f93e69451ea6c405707a41338b9658d
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:29:20 +0000

    worse embarrassed thumb even body

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def convert_to_hex(n):
-        return hex(n)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
