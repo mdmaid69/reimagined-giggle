4a100d3491e728ac3cfa8fa11657ae7b7064a38c
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:36:11 +0000

    on beyond genuine reasonable reduce

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
 import array
-def get_array_as_set(array):
-        return set(array)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def get_array_itemsize(array):
+        return array.itemsize
