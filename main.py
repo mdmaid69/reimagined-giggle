ca715dead635d436eb113a87f9890ca6b6a438a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:42:48 +0000

    word minute exciting learn them

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
