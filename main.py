b7b2e5d518001fa538be7435827ebe8ac0465c21
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:05:07 +0000

    letter board broken unfair ashamed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
