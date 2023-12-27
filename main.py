d10d0f96a2a68138c07c430b56e68f2f385a137d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:19:28 +0000

    image hurry air clean exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def reverse_string(s):
-        return s[::-1]
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
