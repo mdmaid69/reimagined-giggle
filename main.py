e1c2f60369d2590cfde1acae997dae4c93291cfb
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:06:38 +0000

    turnaway worth lend shadow say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
