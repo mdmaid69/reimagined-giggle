989a2a29ce5729214e789f97622e4a2c6b5cf117
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:39:17 +0000

    fasten exhibition low union getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
