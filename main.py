901fd9cdaa516d8bfcc969ee8192c023dc086b46
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:26:24 +0000

    store breakin feel getaway fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_work(force, distance):
+        return force * distance
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
