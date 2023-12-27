0f2bb31aa1e30983548d5b1e7144e659ab072f8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:47:48 +0000

    contact argue stare fun value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
