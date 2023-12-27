e73cc652d297f9870ca4e0d1d06afd3be7cbe482
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:06:30 +0000

    fill tip since shelter inquire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
