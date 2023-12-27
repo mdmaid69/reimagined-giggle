bf696fc17126a7ae5e8d3c7238fba3a15d60338d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:13:37 +0000

    relation mistake seat hungry rather

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
