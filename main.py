4cd7077bff181e3d0d304e307c1b4c08dd02710a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:53:24 +0000

    ahead mark to picture edge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def square_number(x):
+        return x**2
