a2da5986e6439e51cfb92ae7e429b38a06e4f5e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:21:11 +0000

    shelter sick wild morning fast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def convert_to_octal(n):
-        return oct(n)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
