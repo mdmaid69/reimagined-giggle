97df008f0b8a9875fcef4a0c33621c0f5cf66317
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:21:28 +0000

    slight sail Oo half rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
