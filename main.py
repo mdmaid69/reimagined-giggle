954b5e01b66e8aaba414a3ce3c822bf7c631b07a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:11:55 +0000

    process service now produce section

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def calculate_area_rectangle(l, w):
+        return l * w
