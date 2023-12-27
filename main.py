89ecff798a280a6b90d3f6ae07b0297017a5dcfa
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:29:26 +0000

    so company hate price official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
