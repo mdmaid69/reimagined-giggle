7397ad7d9b57b83c708203690bd942d10f47b6cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:35:28 +0000

    meet cruel rather argue today

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
