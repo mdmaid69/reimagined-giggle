16ccdcaf88890e2e69b8a89860d68c257c74d94e
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:00:09 +0000

    serious order touch false cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
