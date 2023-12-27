ef4d3dac2c1ef88ba8ce666cc27f1029b881e340
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:55:09 +0000

    distance blue safe recently progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
