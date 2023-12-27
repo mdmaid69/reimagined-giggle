f37ad29aa47fa2a5f61211f53a3724af1e84c80b
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:58:43 +0000

    strange conversation climb near worry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
