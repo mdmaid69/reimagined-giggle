92897d615211b2d6939bfdae21c14c6e9a374c17
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:27:44 +0000

    who pride 572enter interview disgusting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def find_min(lst):
-        return min(lst)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
