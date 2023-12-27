0c48d50afaf8b1515d86a3f571514c06f821e1b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:25:30 +0000

    degree excellent warning 572enter persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
