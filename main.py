a8e32ece19ccaec1087e53f95b7a75a13cfbdeed
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:45:43 +0000

    favourite sail cross reject almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
