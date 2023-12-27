f1dcd170788ccbd63d875665a9d8548446333877
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:42:25 +0000

    contents behaviour visit passage alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def reverse_string(s):
-        return s[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
