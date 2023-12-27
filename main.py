6f9779f49978a5cc28a783faed014c707f8a5eb1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:49:57 +0000

    one demonstrate capital pretend music

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
