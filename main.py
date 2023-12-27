e05991a2aba365c6551076a22da1f3cb1f30736c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:18:45 +0000

    dance emphasis fail divide in

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
