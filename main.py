9e2f7097fbe0aa2ab26ebc7811f753e44c2464af
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:38:39 +0000

    dinner interview delicate feel accordingto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
