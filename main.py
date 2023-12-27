40b66a30280a20a09b57b4db2bf5a523121b438c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:26:50 +0000

    move equal opponent excited lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
