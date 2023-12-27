39e853405db3152af79053d4204e4bdc63e31126
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:32:25 +0000

    news bread face next bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_str(array):
-        return str(array)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
