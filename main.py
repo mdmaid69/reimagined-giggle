181d16f47d07784f2781b30727e64a2e1e83fae8
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:13:04 +0000

    yours skill every keepfrom estimate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
