9d514d911341297a56df4775a1918e74703b7ba6
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:22:55 +0000

    scheme home cheek secret tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
