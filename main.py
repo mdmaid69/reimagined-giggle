6ec07286aef1ac919fc1f025e84aef5ef7335be5
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:05:17 +0000

    price interested breakthrough instead workon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def square_number(x):
-        return x**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
