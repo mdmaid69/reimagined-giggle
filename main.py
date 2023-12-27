7fd0e5e38fd45a96241a8e95e4a950eab01eceb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:38:35 +0000

    Vv glad turnout lost thirsty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
