5162b62c79093fadea13e7dc4ae45d633f6868d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:41:15 +0000

    switch fierce Hh last human

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
