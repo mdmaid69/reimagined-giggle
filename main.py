57b0c5d0477fe03c8efc17b057e85066deb39db7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:16:03 +0000

    judge exhibition certain address custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import time
+print(time.time())
