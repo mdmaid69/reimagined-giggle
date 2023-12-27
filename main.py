d85dc5131387b6811f3a89321790e1f2b4aeb420
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:42:46 +0000

    positive separate fire on interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
