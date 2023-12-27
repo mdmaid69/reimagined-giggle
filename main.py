b07689167b7f1b976a1cda55de0eabf4626a9dd5
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:14:42 +0000

    open division after drink port

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_speed(distance, time):
+        return distance / time
