f70b311cb64b6caf8f979b5ca778ebe10f978267
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:40:51 +0000

    sail grateful exit tap memory

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  def cube_number(x):
+        return x**3
