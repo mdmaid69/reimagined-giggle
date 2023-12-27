41958308e0457ca87361906dbe54b2d47644e348
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:19:12 +0000

    until typical wind journey there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
