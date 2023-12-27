842189166dd678f82caf9d5b88e69ec2b2de197d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:09:52 +0000

    answer repeat sign gentle private

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
