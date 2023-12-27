089ab3e8ee4e54c39e5b0900a773f5b1803925b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:37:00 +0000

    Uu war round plate press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
