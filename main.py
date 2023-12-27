439730fadb4cf927daf2c87a0b89f3c251d771a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:09:46 +0000

    dig why tight off mention

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
