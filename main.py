5eca74dfc769847c78c2a8814479e22420df7d2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:45:31 +0000

    clear familiar boil run confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
