237fc3b6150841642787a3d758a2d54f7ac21ad6
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:34 +0000

    guess uniform getoff height contribute

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def square_number(x):
+        return x**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
