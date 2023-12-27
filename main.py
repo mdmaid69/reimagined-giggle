26485b364c15b697fd4123dfbf9766317839f652
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:01:03 +0000

    after cost dress perhaps compete

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
