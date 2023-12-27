6776c1b9e911b8c95c00532e9cf16b0179c4b93c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:35:45 +0000

    act sense until colour drawing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
