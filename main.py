7a2e516e6cc2b6eb4d8373729ada6a2c1030686d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:55:03 +0000

    giveout side ask thirsty dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
