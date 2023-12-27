5b4e2015a14a526ce3d4b1823244e5619fbab22f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:35:05 +0000

    better show judge freeze keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
