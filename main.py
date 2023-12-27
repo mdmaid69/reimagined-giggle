e5d36939f21a2fa2869233dfd0930ff1cc4e0b29
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:51:01 +0000

    pour last long sting wear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
