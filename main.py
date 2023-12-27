33810feff4b9c8ef5f4b77172c1aab744bc0fba6
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:18:31 +0000

    view cruelty roof post suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
