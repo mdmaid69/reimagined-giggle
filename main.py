a06a21bc97cbc66101a13e1a0af7b589dae7f168
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:55:31 +0000

    relationship usually ordinary gather lookafter

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
