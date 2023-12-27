a37733dc80aff7f479b5bb297a956e346009d41f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:05:20 +0000

    sweet add provide lean reject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
