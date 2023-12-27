ec32784fc5a0607520f3cb2a56c739c7f0f0fa23
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:25:49 +0000

    hide often proof side whatever

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
