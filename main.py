3760220b7593362f60f6c859f3eb38b0ebe639e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:04:43 +0000

    sister payment material dry evening

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
