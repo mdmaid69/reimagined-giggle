b86330a4a2962de789e1f3f37c20f127cc0a6a83
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:58:40 +0000

    wet high rely noise guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
