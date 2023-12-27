6d69a7ed640bb256dfbd9ce494200c366a23133a
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:51:59 +0000

    second measure beneath chin law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
