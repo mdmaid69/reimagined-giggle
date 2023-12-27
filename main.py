aba473553148808387dee6b085124345bc67bc51
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:29:37 +0000

    forward surprise tax criticism touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
