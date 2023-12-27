08ff74d61469cf5d15b7eff135a0d02106c51023
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:58:00 +0000

    breathe total smile money when

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
