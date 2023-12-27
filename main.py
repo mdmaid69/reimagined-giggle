38cd2e30b0595e355642cec233b7c49d1caed333
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:07:27 +0000

    storm ability Kk pretend breakup

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
