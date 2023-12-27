a8dcb3fddb05c160e337a881a7c169498f8509ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:20:34 +0000

    tired exchange Gg his around

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
