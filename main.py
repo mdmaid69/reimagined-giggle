f97527f9a679020a4ad04cbb147c73d0d673b613
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:01:28 +0000

    wild job impossible Bb you

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  def calculate_area_rectangle(l, w):
-        return l * w
