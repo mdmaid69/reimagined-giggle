ec73a47b4b7cf65d84f7492ef8d5bb54ab2d5cf0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:52:08 +0000

    habit drive star final string

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 import math
-print(math.pi)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
