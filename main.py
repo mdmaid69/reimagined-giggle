14630cfa0fb72db229dfb6b328e095a7bf12210c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:48:33 +0000

    please juice whisper peaceful never

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import platform
-def get_python_version():
-        return platform.python_version()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import time
+def get_current_time():
+        return time.time()
