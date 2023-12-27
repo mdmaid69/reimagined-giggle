19d5dbe883def2e2dfa245a60a98e06fb5cf9d43
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:40:33 +0000

    stop crazy instead would heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
