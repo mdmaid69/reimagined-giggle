6557012386b712a03e471dfbe94ac61ed902ba91
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:45:19 +0000

    equal hole father interest possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
