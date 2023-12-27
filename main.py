1426ef47c07833cda5347f19ae119f74bbb88f6e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:42:02 +0000

    day receive welcome so warn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
