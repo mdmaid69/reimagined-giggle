f9d602209d7c2bbdefc0105a0160ccad469c4ab1
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:26:32 +0000

    wealth match university trip pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
