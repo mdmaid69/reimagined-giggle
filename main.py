d2b810ff4f42cc78c2b3d22f3f5c9422c7c018f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:57:39 +0000

    unit pollution church getaway correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import os
+def remove_directory(path):
+        os.rmdir(path)
