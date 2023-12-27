0d2724811ca18dedbe2535b8c38fa54b1ca54bd6
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:27:44 +0000

    battery understand violence die in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
