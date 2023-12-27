5753267fe9dcb025b143d9606dd95a2bbfe5128b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:41:16 +0000

    possession difficult its pretty tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
