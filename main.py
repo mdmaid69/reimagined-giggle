ccd6e8fbc5047d072c9e31551a30dab89d0b03bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:28:56 +0000

    grand male form division lot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
+def find_min(lst):
+        return min(lst)
