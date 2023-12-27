37cddc1437607f0850ee6ae30922e559ca66b24e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:38:13 +0000

    past completely uncertain minority black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
