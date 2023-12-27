1da430ab9b95d06f77b6d1e07010fb5d7b9119df
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:45:35 +0000

    awake direct pay pretty unit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
