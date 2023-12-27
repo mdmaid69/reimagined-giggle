37abc897a46db47b76243fe3fd9defe9a1ad5332
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:16:44 +0000

    stupid war score split everyone

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
