8fb0c1ed5a619a354b1779ca6c51f2d199f9db23
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:55:45 +0000

    ought heavy ought hope common

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def is_palindrome(s):
+        return s == s[::-1]
