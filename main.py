237460fa66af7e2dd32a85b6b0d47a4d6183cd20
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:09:24 +0000

    everything consult love lookback(on) state

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
