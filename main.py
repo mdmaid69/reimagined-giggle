1acc700d249c0f756b03440c19cb1dd4c9f214b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:44:30 +0000

    change clean central finish uncertain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
