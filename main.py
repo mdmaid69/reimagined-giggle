70710420fc2e0a441fefdf471589816d7a5649c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:32:22 +0000

    full stay downstairs occasion obvious

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+print(sum(range(10)))
