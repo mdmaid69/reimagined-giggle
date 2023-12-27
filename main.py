5974aaa31d5703bce9fbe0f618179c4b239ab880
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:55:19 +0000

    influence slide now fall add

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
