1cdd9fe6e9e6f3ed1ff13ef59483b07997e8dde2
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:14:35 +0000

    how apologizeapologise odd damage promise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def subtract_numbers(x, y):
+        return x - y
+def convert_to_binary(n):
+        return bin(n)
