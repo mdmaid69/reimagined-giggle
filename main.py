b42a22d477e56f8cd3fdfcf020b00d340ed765b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:05:55 +0000

    mine classroom gooff meanwhile judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  def is_even(n):
+        return n % 2 == 0
