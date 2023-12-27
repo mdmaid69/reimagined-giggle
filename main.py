da1b9b6b61dd9e41c01d643691202fa84516c937
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:57:27 +0000

    sore pattern holdout thick someone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
