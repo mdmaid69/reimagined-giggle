954bd89fd159235b350fd3c676a9ee1d155c29b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:02:55 +0000

    usual makeinto ill name fair

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def is_odd(n):
+        return n % 2 != 0
