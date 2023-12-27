f3acb38bcf18db18d2b535047b3412d9281807ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:17:38 +0000

    bet early winter before comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
