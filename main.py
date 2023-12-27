0b5d361bad6f2b6fddd612f8ecdf16041e0c3de2
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:05:25 +0000

    sad satisfactory description hollow careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
