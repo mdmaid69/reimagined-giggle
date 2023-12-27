b7a3d08f7c57c2b605f775030004f3c2ae66d4ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:05:06 +0000

    sort excellent comein still apply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+name = "Python"
+print("Hello,", name)
+def find_max(lst):
+        return max(lst)
