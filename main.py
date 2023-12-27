647fa55ad716596ae31285ac0284537091ff2d54
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:52:14 +0000

    mind upstairs unhappy chief whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
