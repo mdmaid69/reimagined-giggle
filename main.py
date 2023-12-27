15a4376b905c3ab5aab41f0cfcc49500e0684263
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:22:41 +0000

    today tense exploration meal shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
