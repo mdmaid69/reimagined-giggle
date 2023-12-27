0f4aa4b6d033c27fd2a9435bcb423aa412925973
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:17:49 +0000

    spray myself visit getback square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
