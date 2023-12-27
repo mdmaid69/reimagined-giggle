8411b721b16aa646c61cc5cfea2ffc2659e59c98
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:12:47 +0000

    rise favour active purpose survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
