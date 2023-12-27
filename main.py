420aaa2c4f19a6323103e9cea670f3f51cad22f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:32:32 +0000

    share thought about bad matter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_area_rectangle(l, w):
+        return l * w
