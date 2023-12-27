4c0da39a9d5db8e46c38b29ba38bb3d52c7401d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:01:24 +0000

    moment backwards develop in achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
