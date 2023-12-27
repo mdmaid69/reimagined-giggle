f2ca20ed80c0a09829c0c9cf20257ff22dbef57c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:22:54 +0000

    silent some quarter well comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
