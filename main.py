80e47fe4fd653f12aacd3c39f7696d4fc77600df
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:45:10 +0000

    poison degree plant except lean

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_buffer_info(array):
+        return array.buffer_info()
