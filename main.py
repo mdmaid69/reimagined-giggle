edcd5aa94c64e90d443cedf97ac97dd6724dca61
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:28:31 +0000

    already next right apparently skirt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
