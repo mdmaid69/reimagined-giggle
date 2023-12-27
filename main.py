6dbce03838677878c756a12e76ed4c94e9fff4f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:04:57 +0000

    Oo opposite criticize up bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
