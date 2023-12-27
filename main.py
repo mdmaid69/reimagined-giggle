15f875392d25124d8320c5b21e84824a18571d46
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:14:47 +0000

    noise recognize brother right evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import tensorflow as tf
-print(tf.__version__)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
