be688d50000babc5c8533140e03b417fb417c12b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:52:20 +0000

    chat entrance flow later hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
