65a2c1a7924e420c2f5e050e266239b61ed2aa21
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:01:45 +0000

    post 571enough exit steal high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
