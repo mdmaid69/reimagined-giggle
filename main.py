d3663f97305a25f18089082c6a72627869863e44
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:04:52 +0000

    taste nonsense collection surprise feature

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-print([x**2 for x in range(10)])
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
