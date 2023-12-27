92523be1384dbe0bd3741a2bde6eeb389e5387c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:34:58 +0000

    set please uponon murder introduce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def subtract_numbers(x, y):
+        return x - y
