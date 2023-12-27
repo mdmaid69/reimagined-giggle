e318206c8509ecaaf4cbc10975610315558bd6ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:25:33 +0000

    nearly fall quality spread fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
