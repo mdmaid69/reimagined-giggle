aa50e31d6c17fd04d912e5af3c36320f2e325920
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:17:05 +0000

    so onto cook fit mistake

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def cube_number(x):
+        return x**3
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
