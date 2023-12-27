300a8940934e80392d11c76d2500cc1cb95f28ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:13:13 +0000

    apart count careful constantly star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def reverse_array(array):
+        array.reverse()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
