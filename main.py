94cb54490f21f3626e42cae143568e9cf20f782d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:25:28 +0000

    Kk defend hurry history far

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
