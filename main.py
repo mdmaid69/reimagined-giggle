0f4bceb2313c49e5ca3eafc3f1c7430179f6439d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:03:18 +0000

    deal price shut flat change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def square_number(x):
+        return x**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
