87834ecf0e573c3e24be95d8bedd0e56d0937041
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:19:34 +0000

    discussion list wealth suddenly importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
