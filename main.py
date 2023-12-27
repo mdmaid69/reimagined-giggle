a0d07aa4d1dc8a62fa09a0ef926337d278d38f84
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:12:22 +0000

    fill produce after skill thanks

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_min(lst):
-        return min(lst)
+def add_numbers(a, b):
+        return a + b
+import array
+def get_array_as_repr(array):
+        return repr(array)
