f0e2502e7d525902e595662d77fb5ac3563ac007
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:14:52 +0000

    illness shop anybody support confident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_max(lst):
-        return max(lst)
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
