ba312594da3f61306aace335b6e511afa2a8201f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:15:57 +0000

    bunch much record declare punish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
