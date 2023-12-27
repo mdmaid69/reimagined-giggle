bed10eb76a2d9ee8e40720c3607af31ca6f5e3ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:18:14 +0000

    winter colour top look punishment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_area_rectangle(l, w):
+        return l * w
