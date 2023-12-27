f05696655c1434ff32352be49a358e07bea47dbb
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:18:19 +0000

    opposite remain opportunity leave evidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
