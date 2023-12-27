937e9a6bb2b52802aa847c0272fa1799d91c1c5c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:04:12 +0000

    discussion shallow discuss correct admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
