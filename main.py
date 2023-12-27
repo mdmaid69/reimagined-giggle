8f096fdfe804d70b334c4346804f70c9710efcd1
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:55:08 +0000

    imagination improve say noisy none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
