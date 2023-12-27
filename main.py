e37229127cb5afc7ac2cb97851d6f570df5c74c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:41:16 +0000

    official Dd judge side grateful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
