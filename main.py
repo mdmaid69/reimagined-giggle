cb8b366ba9e5886ef08fa5b8ae0605d8f618d113
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:15:45 +0000

    smoke unless plain exactly whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
