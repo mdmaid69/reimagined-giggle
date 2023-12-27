5e3efe45c489f35989c556d92a765b5c8b55be3e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:49:45 +0000

    weather pour ahead Ii various

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import platform
+def get_os_info():
+        return platform.uname()
+import os
+def get_current_working_directory():
+        return os.getcwd()
