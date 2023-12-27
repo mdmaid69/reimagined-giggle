b5c5576b1cecc217332f419cec79bba76bfc4ca6
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:44:23 +0000

    concentration break teach identify target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
