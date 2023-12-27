d3a7ff9d3fb6f49dcb9ea0348bc8e63a828db539
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:47:35 +0000

    eye march around exam bringback

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_force(mass, acceleration):
+        return mass * acceleration
