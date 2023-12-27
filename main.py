38aad7a21201caa7e62cca61433496be5fad279a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:33:01 +0000

    relief little its ball year

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
