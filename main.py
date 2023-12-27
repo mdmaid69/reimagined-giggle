f005240450f2779c19bd2abc39e7e7d163bd33ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:09:03 +0000

    street word public comeout patient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+print(math.pi)
