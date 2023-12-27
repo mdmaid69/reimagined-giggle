b4ac1111421186758540c625d40eff89bc1ed6f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:32:00 +0000

    even positive fly ignore means

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
