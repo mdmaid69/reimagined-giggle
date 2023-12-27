71b2c0d8ee865f00ecc72a8774bd72cd314813fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:45:33 +0000

    turn poison regard study water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
