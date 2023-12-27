edd5cb815158364004bc7cb65fae82321cc0b2ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:25:06 +0000

    comedown criticism mark breakfast than

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
