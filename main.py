c501038e12b8450d0f36ce0aaeea832729e855e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:36:22 +0000

    radio also bad pair along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
