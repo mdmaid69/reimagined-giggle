0322e2582c98c57fb2b6c37d09ad6c69b2b8f67c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:46:14 +0000

    turnon passage protect swallow content

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def reverse_string(s):
+        return s[::-1]
