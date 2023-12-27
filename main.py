27eccf5f0e1261e8fddc5e1da16f711ca282cff1
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:20:33 +0000

    who nice sight pupil used

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
