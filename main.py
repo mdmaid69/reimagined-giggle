0b9fb5f26503828008b36c411bff566de35012d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:19:28 +0000

    invitation nerve pretty typical natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
