831a38cf2dae135bcf32b59d671b54240faa0627
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:21:24 +0000

    behind I occasional but maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
