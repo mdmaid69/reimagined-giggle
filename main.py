7531c223389d9eca71b3e712265164d02150d66b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:50:38 +0000

    maximum spin responsibility substance workon

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def multiply_numbers(x, y):
-        return x * y
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
