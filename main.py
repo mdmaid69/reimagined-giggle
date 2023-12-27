7ef92a6bfaeb314ce7da4ca62e52dbf5f3d81915
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:09:37 +0000

    walk wind lift understand crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_hex(n):
+        return hex(n)
