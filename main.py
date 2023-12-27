8943d60c1c0206f0fb608bfb9938e378ec663c4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:07:43 +0000

    region let strike bill maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
