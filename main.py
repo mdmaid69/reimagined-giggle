6ac8fb66c00dcb6a91ee4c9fe3a62abcc3d5f7f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:41:24 +0000

    question thought smell trap sting

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
