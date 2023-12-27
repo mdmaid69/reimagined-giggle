fa2b273f36c8625f2436e22b59e79b060302d91d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:50:49 +0000

    play form innocent course draw

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
