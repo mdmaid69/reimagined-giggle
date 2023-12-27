ff3722c2192bb86a8b4b19c7284fd4a482dbb85d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:15:24 +0000

    Ss culture strike pause personal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
