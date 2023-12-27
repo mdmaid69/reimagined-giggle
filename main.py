a55f28993e160493af1edb5af4bb3dd213b3a37e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:26:13 +0000

    fat recommend judge splash take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
