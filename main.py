83de952d17728391fde0929fc71dbf612a33446c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:24:43 +0000

    shock able everywhere plant well

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def generate_random_number(start, end):
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import random
+  def generate_random_number(start, end):
         return random.randint(start, end)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
