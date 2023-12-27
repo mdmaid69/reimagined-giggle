ddf4ce9deaff733b040d2e01843755d8a3ae1e37
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:49:38 +0000

    rob convenient fork ball power

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
