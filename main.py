730df338a1b936df6ae32ae3a9b352342427a361
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:42:24 +0000

    watch far shock joke direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-print(math.pi)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def add_numbers(a, b):
+        return a + b
