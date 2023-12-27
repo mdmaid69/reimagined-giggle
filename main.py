ad47738d59c785a113bba2ad46ec69a4cffe7172
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:21:41 +0000

    thanks whenever calm lot ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
