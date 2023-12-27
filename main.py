f439156b2f3ae2779619a6ccabcbce016ad29131
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:12:05 +0000

    tough trouble underneath disturb swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
