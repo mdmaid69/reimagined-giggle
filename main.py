c7942f833f86aaba11f695aacdbd0531ba0fccc3
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:18:33 +0000

    but pressure important opportunity test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def get_array_index(array, item):
+        return array.index(item)
