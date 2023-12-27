a893e70933b7d677595a731d60f572a379fa3565
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:11:45 +0000

    life vision suffer effort nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
