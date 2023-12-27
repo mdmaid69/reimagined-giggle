86403c85c3da924e17d4b564086b05541ada82bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:32:41 +0000

    attract yet group sure deliberate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
