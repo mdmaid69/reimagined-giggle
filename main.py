1d01c54f1c03294d0951183933c19c978e60b990
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:19:02 +0000

    avoid chance have course hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
