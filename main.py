e442b46b6be00754b42931b7dda99d9b86b5737b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:37:59 +0000

    on air pipe floor greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
