c5117bbbe6d2c64f753dd35166677bf86e436894
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:42:54 +0000

    disagree pass window delicate underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
