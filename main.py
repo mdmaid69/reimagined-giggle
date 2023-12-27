e49a8ab785a47dd6eb435ea8a9da22ac4af969ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:16:07 +0000

    giveout cold sort ill fortunate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
