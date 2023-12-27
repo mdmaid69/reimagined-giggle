0f77c9da7db094307607f5a16333e63651a9b286
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:55:31 +0000

    wise piece life proper cross

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def reverse_string(s):
-        return s[::-1]
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+x = 10
+y = 20
+print("Sum:", x + y)
