befb56068fb520a5758c11333ee760d4845a52d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:21:33 +0000

    attach large escape port edge

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
