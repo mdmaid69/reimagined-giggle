8b58727421bb05c41db7ad78f99160c12f71c016
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:17:37 +0000

    reaction convenient phone keep choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
