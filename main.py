6b50d33d45c62bd7613dd1c45b5caa46bf41bbdd
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:46:37 +0000

    pause wealth personal Nn turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
