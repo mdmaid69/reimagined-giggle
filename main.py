8a37f5fedb1f89cef2416c78f7706f446834563b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:26:51 +0000

    tonight division fierce opinion step

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_bool(array):
+        return bool(array)
