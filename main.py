6bf9b3dfa8001c13f4a2be0ee36929fdbdce6b14
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:40:14 +0000

    boring expensive count safe genuine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def cube_number(x):
+        return x**3
+def count_words(sentence):
+        return len(sentence.split())
