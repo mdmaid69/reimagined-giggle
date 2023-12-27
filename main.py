dd73b2d812efcad01230089460e2e7286aedae0f
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:33:01 +0000

    swim geton close anyone accordingto

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
