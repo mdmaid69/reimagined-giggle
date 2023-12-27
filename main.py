7eda592603b6f8c323efdc9f955c2acf5c31602f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:09:40 +0000

    talkinto on finally smell flight

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
