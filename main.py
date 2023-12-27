0779e49c9afcd51e9c0c3999106f192478a8f52d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:53:55 +0000

    content earth original clean lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
