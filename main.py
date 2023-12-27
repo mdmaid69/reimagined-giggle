009704b48ca3b1b4ac75a37a764eb65677912023
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:53:12 +0000

    tour bite out shine blue

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
