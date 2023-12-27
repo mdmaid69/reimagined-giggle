e347fca6112afedc46642d5030afc3b355dede6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:48:47 +0000

    file ball head fill last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
