8b96ce2ff351cbd4986019b22405450557aace29
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:10:14 +0000

    generally estimate convince throughout affect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
