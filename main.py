b99b5466267a8b6f0b9d1b36dab740ced0a4582e
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:51:07 +0000

    maintain 570enjoyable weigh whatever successful

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
