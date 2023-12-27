8092d3e70088af0f6003061f7ee038ca067401e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:47:25 +0000

    central off arrive underneath another

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import datetime
+print(datetime.datetime.now())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
