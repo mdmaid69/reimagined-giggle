fda006e7b43fc9d4f765458dce3d0ca3d0b3e30e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:18:16 +0000

    attempt takeaway telephone interview splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
