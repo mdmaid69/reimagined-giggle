436d3b4c18285ae22b5ca512fb6286793e83cbc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:39:02 +0000

    wish week affect sell issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
