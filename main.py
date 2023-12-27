de67b511eb3afdb516d5122428769795b57e4f5b
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:58:00 +0000

    minority surface slide fault well

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
