a96076ca9b58db91f408c6981b6d2d6243a15337
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:32:36 +0000

    still comfortable often completely temperature

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-for i in range(10): print(i)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
