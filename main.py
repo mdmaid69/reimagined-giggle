2bc6ac705b8679d7fdae43c30c5db1aacbc97f57
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:15:29 +0000

    one face trust interrupt part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
