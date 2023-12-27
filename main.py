ecdf01380f1fb38a7c29ee37e4a7992bc4a5f7b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:32:42 +0000

    destroy clue work marry creature

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
