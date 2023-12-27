e52a1430ee813b0639f2292ad5c14e2888282a7c
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:39:15 +0000

    happy welcome definite bridge Nn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
