ee7bdb7d7a126d51ec99e68a0322d3882d83621d
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:50:56 +0000

    comeup bite available me 568energy

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
