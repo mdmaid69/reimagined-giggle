526cd134a8e626fe7ced01cdb6fcc959819c82a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:43:48 +0000

    cheap 564encourage tense fortunately excuse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+text = "Hello, world!"
+print("Characters:", len(text))
+def reverse_list(lst):
+        return lst[::-1]
