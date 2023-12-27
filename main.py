4ecdfe389cece2ebedcae6b0631da397220d39ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:09:38 +0000

    round benefit story rush negative

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def convert_to_hex(n):
+        return hex(n)
