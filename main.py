244224cc060b2b60948b47edf3a703ec17ed9728
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:46:47 +0000

    read right trip workupto calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def convert_to_binary(n):
+        return bin(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
