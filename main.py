521ce69c3c5376e628f183dac880890ae5ccf6f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:37:22 +0000

    interested beginning winter profit cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
