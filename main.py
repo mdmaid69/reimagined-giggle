2260a14be368cf5db8d9715ed43fc00f614256c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:56:30 +0000

    plenty visit university immediately number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
