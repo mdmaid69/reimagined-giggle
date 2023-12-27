e51da200acc1abf07808ca862afc91a12861bdf5
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:16:16 +0000

    dry plastic poor promise fair

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
