24095c0720e45145b59ca123777ff982ead9824f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:15:33 +0000

    music nice declaration birth wide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def convert_to_octal(n):
-        return oct(n)
+import collections
+def create_queue():
+        return collections.deque()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
