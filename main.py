901ad568cff4f85dc48e26ffb0c66dbed94dc279
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:24:00 +0000

    if congratulate sharp female lock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
