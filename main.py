fb2cc79a81effebb05066764f1a61fee3a69cb16
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:28:34 +0000

    patience general inside exhibition holiday

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def sort_list(lst):
-        return sorted(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
