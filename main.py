8b3f42e7e5aeae700a3f6ea1964f7058c895135b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:17:22 +0000

    least gap say substance total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
