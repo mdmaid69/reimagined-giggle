178c5d1b1f83e5ff561a658062d5c70abe4c7574
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:12:14 +0000

    friend maximum connection profit Mm

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def find_unique_words(sentence):
+        return set(sentence.split())
