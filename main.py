ee29fac6bbd0f8c03f90ceafce5c8dfd082fcb05
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:23:04 +0000

    blue but getaway healthy base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
