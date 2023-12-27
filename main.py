af49c94fdcb0edf8890c5c702518d47959a34523
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:45:53 +0000

    nerve great angry its work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_string_from_array(array):
+        return array.tobytes()
