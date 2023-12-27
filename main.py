126bf6521f38e1dfef65aacfa67abe5664ba5490
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:04:08 +0000

    complain offer fear plastic almost

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
