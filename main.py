c4676375eaeafe5ae26293f22fac4a0b11dc91d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:00:38 +0000

    unusual tongue thanks argue make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
