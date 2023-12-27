e41a4efbbbf5ed59c328f9b7d4f79eaa7fd462cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:23:30 +0000

    wear art search fly hello

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
