d49c7cabce2a8cf3546e29923bf9488f6eebd947
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:52:01 +0000

    fence he ours plenty earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
