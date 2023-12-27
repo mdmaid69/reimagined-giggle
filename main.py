403477250442155bb3a62914422541ed373a9ac2
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:31:05 +0000

    owe walk row upwards feeling

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
