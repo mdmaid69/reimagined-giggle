84f009a5acefb19114a80ca3b7183187a32d33bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:24:19 +0000

    afternoon match excited accordingto quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_tuple(array):
+        return tuple(array)
