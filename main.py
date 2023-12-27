4f9cf9723c0cb168d537577609ec82f70607db24
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:48:55 +0000

    mind definite ought piece fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
