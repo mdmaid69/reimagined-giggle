dffb41b3da557f08c87f3c1b04f281e2d745f291
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:36:18 +0000

    neither allow only care prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
