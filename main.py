dba27c4ec348f2f1910a4b351f876dacef6d4d3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:37:30 +0000

    within along future noisy still

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
