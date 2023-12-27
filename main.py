7469dbc267b485789a77b6e4dcf87cc12794e2b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:03:03 +0000

    return dinner news trap path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
