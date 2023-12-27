b393beee46d55d005e9de52445af965604b7d251
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:35:12 +0000

    character stupid slide pronunciation motion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
