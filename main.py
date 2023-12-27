f05f51d97bcb9c2f7f2f452056cfc4582b6c36c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:08:58 +0000

    father under balance to story

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+from flask import Flask
+app = Flask(__name__)
 
-{body}")
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
