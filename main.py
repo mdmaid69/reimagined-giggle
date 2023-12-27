9a1ffcfc066b459f55af1474db4d3de7d8946c40
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:57:42 +0000

    turn law offer last interview

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def sort_list(lst):
-        return sorted(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
