25c451edd94c8fc694ca010042a7fa52af40058d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:36:40 +0000

    connect loss hungry undo form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
