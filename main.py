ec987cb6e32e94496d85daa374f49ebc8ef88a20
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:10:06 +0000

    through thought picture cover pause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
