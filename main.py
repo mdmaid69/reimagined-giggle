8aa3c5e1143561fe6e84a1564e0c59a99b8ccc3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:21:38 +0000

    me butter region Pp seem

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def square_number(x):
-        return x**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import random
+def roll_die():
+        return random.randint(1, 6)
