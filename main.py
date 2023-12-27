1b7a2c388b3831e711bbcd7b4e44d98853c133e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:31:02 +0000

    hard ought protect keen away

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
