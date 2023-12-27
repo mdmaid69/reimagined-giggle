c6bbac82fe1736ad92e1ef7b8571a918ee69fbf6
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:07:28 +0000

    neck hit rain road even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
