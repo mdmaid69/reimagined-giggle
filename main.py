f0195ec1dafe077f8ada803dd501b4fc90beda1c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:28:01 +0000

    passage early Ss bank he

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
