8ea84e87ebfe6a7af90c9e5ec7666c4cac3464d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:21:52 +0000

    workupto watch clear keepoff past

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
