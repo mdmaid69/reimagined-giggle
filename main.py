db5ab917b2762b6a7c4c78c5b188a46e8bb1df7f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:37:57 +0000

    repair use imagination autumn murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
