8512505b3d19d9977618bbc4208314b1bd20fce0
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:51:32 +0000

    shame underneath hurt water hand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
