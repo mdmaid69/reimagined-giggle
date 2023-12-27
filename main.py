dd4b167cd1eebd5493daaf13d0be0132d74a17e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:12:25 +0000

    characteristic wide dig escape opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
