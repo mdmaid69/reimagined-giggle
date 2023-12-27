0c0a608c31ac349a88f0045e51e5b65265936829
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:44:00 +0000

    compare lot hate large undo

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
