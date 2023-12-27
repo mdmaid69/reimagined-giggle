71234264077c3cdc21f6597a8c817032e9bf116f
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:31:18 +0000

    lower room address cut world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
