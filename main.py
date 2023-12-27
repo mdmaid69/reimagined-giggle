b537a32de63b47704c8091307090db7982a5700b
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:36:53 +0000

    necessary callat private head achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
