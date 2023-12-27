118247d5bdfdc8fc7fa313f6ba8d85076c0cf4bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:59:38 +0000

    exam mind angry ever kind

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import numpy as np
+print(np.array([1, 2, 3]))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
