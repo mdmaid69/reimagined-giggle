ec2f3382930e02255c72015e81255d65d09fe1b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:22:02 +0000

    vanish sweep word advice guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
