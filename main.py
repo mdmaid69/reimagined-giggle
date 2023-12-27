c456d847a1eb0b34d47470a1da0e9d6f8abce4dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:08:45 +0000

    ear hold simple no marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_string_from_array(array):
+        return array.tobytes()
