05c3342dab91360e1f1c55716326d915fe02ab32
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:43:28 +0000

    thinkover new violence tidy protect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def sort_numbers(numbers):
+        return sorted(numbers)
