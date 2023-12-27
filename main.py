2e4516c44a6943ca6879e4b3cf76087615d040e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:50:18 +0000

    lookup blame scream typical interruption

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
