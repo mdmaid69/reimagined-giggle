d926ce304d21d3d8b3e1859f7c0f4838714b1f72
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:48:31 +0000

    behaviour responsibility line finally argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
