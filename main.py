7818b9ade7b55e35c8c9132a7fe7bf4417d1ed67
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:24:24 +0000

    scale stand gap total picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_density(mass, volume):
+        return mass / volume
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
