6100e7b755572dc794d01e55932e680a94967697
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:35:24 +0000

    heat tear who result lend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_palindrome(s):
-        return s == s[::-1]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
