5130820c1304741470d44d5f7cbf3c6e7ca39347
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:09:04 +0000

    collection natural whatever string practice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def reverse_list(lst):
+        return lst[::-1]
