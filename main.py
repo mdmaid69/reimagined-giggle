39fc590528612d781340eaf1cb349c0fb6a47092
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:58:52 +0000

    aim shine gift level last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
