df5b5bad0afd483005b91ea6562bb029d839bbc4
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:12:58 +0000

    secret area impressed inside will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def sort_list(lst):
-        return sorted(lst)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
