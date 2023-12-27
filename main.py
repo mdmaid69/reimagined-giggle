c84b8e8dac0a7b9848adceed33be17bfbc25dc74
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:36:34 +0000

    team education fix trap holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
