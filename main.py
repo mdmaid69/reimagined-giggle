c776a4eeee873d8b78d5b935b4a11f6f8d23ed8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:27:17 +0000

    urgent impressive nearly nonsense afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
