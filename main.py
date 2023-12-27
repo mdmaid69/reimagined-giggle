bfcf6c8e6d5d7980c4d849b3501494cf07b3c64a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:34:00 +0000

    attempt offer organize sting conduct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
+def remove_duplicates(lst):
+        return list(set(lst))
