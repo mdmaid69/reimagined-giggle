75d1a64a5d0d6fd824052325b55f11a8666ea781
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:05:35 +0000

    minimum pity busy wander seriously

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_hex(n):
-        return hex(n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
