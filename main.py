4ac9b357281c2ebe68a43f2b7b94af5c8b969751
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:26:13 +0000

    switch shut sleep used opinion

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import json
+print(json.dumps({"name": "John", "age": 30}))
