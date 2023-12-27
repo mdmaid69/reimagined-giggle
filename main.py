43b52517f9f126e40f4ec12653b8ba341954d8ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:44:53 +0000

    spare join taste under feature

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import numpy as np
+print(np.array([1, 2, 3]))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
