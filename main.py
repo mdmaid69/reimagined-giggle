5a52ed31041a9e38e2b093e3de7d78d797c28d36
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:08:15 +0000

    heat thinkof affect deaf kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
