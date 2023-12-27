2a1599b063db868c8c617d429ae10d328fa5e740
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:34:37 +0000

    567enemy large plenty paper tap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
