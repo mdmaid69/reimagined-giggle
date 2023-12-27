99a56ca599b8adc763e38fad3464a9c9f13e10ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:06:25 +0000

    area attempt copy along take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
