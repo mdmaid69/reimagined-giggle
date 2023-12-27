1aa342531bb276d076f68dd60f69dcf556eade8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:22:54 +0000

    personal rarely passage callout rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
