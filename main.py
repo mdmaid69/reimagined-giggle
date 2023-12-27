9581a419548fce75eb3892c6c158b29812c76aec
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:24:01 +0000

    further means rely bound rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
