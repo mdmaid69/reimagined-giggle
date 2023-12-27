b09cdb83beed69e90467391fb37d81e49e497f72
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:56:07 +0000

    climb slice keep soon somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
