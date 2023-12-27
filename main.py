5a4d578cbe61077e578a30c5bd6021d889c4c5d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:31:25 +0000

    gas connected so other moral

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
