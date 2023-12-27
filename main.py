bdc924fc01af0d7a75c1033429fb08c6eae68150
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:19:55 +0000

    stop relax small left mistake

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
