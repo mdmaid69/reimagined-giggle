81f0c01a2f6f2bcae932e3893590fceedaaf7ad9
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:54:17 +0000

    relative mysterious office many determined

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import os
+def change_working_directory(path):
+        os.chdir(path)
