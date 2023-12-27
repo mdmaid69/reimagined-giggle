d3832ba1f20644db19432270e6ec6f0bb9d4f7f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:52:57 +0000

    plate under there school step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
