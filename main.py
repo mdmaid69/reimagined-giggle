cd64f78cd7d6214d2194afd84016fa191e19731d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:05:37 +0000

    protect clever cost save apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
