e6d97bab600ac9c37e5a66dcbb36bb1ba824dee3
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:26:41 +0000

    decision entrance distant able shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
