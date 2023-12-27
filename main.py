0fff793a387d125a306d7929f16ff0b92386a76f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:22:50 +0000

    jump rely bit lower object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
