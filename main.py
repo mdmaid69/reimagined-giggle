09294ca11b4e07812479ac315eddde5f50fc0b9e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:54:00 +0000

    solve as length move damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def cube_number(x):
-        return x**3
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
