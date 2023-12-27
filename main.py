2d19b952e007fb73a654eccd10ef1e3c0f09e7de
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:58:54 +0000

    creature height last fork tour

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
