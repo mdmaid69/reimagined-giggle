f5f391f6947ea750eefe7d37e96909f018608cf5
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:52:20 +0000

    substance ever choose rather language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
