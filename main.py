75754e69624fb525a6592cd0dd69c51efb13a459
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:51:30 +0000

    react rain born dead group

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
