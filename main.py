fb29c6a5f6992ee1ddc788d224dabdda4429bb91
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:48:29 +0000

    holdon left woman real plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
