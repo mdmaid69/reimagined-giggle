9e1cb401270cee63dcb545231df3f995cc3e91c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:20:49 +0000

    team settle zero hold anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
