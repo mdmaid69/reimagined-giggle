3dfa7653d5888112c4387ad654a23ffc68ab2032
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:04:55 +0000

    tragedy ilovedicks learn drop hang

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
