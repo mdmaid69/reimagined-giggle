0d8a53a6b8231cf23a6bb52bf391b62d614dee24
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:32:23 +0000

    important ring Cc culture field

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import time
-print(time.time())
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
