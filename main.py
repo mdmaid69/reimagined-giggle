014ff6877d2964aef04157c568aef048146cdc6a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:34:22 +0000

    live scale few judge 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
