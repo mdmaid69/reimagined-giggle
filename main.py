3dd846b1ef0737015be8bdfca25eb902ef6ff312
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:53:44 +0000

    rent certainly getout pleasure weather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
