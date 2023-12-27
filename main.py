0b15228721b7d8ff4e883562aedb3a6e57f2b3f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:15:24 +0000

    direction fill shut event himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
