1a068642c6dc30f851fe5c1648f78ee784b02fed
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:27:15 +0000

    row suggestion bye protest similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_string_from_array(array):
+        return array.tobytes()
