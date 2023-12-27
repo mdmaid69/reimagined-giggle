48bf5f6e1eef5761a5fe480d3f00cb44725eeaa7
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:16:04 +0000

    rob joke always many happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
