92db09019a37fce54f97e237862e8b4cef629171
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:51:20 +0000

    path public always bridge tool

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
