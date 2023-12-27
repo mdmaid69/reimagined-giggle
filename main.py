4302b8104d5b29562f50a2678468aa1ee47778ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:25:33 +0000

    who insist ability keepaway shout

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
