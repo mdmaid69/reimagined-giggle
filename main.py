37527cc97c55d8b5452b94e78ad54cec2fbb2574
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:50:34 +0000

    attach involve me juice swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
