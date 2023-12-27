88f6e3fa00050850271cc0a132b9f886269310dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:33:52 +0000

    class model hot environment expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_speed(distance, time):
+        return distance / time
