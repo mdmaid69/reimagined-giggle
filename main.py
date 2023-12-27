48493224809ac921f54ea7601504016e218bd648
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:48:43 +0000

    contrast plant slip many generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
