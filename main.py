bc4b7810b721d0c5a24e875ffc19152311abd4b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:27:37 +0000

    Nn direct across coat value

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
