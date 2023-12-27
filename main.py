a42979c01ea12484766ec256ed86ffa92391245d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:14:13 +0000

    before embarrassing turnon its aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import collections
+def create_queue():
+        return collections.deque()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
