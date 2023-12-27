cd36ef0acc92bca36bf6292f1e320f0c37dc3193
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:46:53 +0000

    amount believe trip loud breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
