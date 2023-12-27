5ab7099c4d1d134399e25a72c816aeb60218d67e
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:15:43 +0000

    guess line attitude nerve store

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def sort_list(lst):
+        return sorted(lst)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
