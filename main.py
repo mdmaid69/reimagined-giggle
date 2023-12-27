67bcca9378039799f874eec029995247299d343b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:37:35 +0000

    complete eat arm favourite welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def reverse_string(s):
-        return s[::-1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
