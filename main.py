139e229bf91253c8f1a80f85d4235a5cd25666dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:28:44 +0000

    plenty terrible complete distant degree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
