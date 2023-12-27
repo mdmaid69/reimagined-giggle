aa518c3e6e8b192f026652a46ab827acdd1a9b14
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:47:06 +0000

    179bite circle choose remark slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_index(array, item):
+        return array.index(item)
