c0eb53fe640bd4c9ff34a8631e498fbe15e13382
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:45:56 +0000

    taste air split but birthday

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def convert_to_binary(n):
+        return bin(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
