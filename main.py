81335dc14804a45f4b19f3764c5544272010b2f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:48:01 +0000

    lie wipe receive about important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def multiply_numbers(x, y):
-        return x * y
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
