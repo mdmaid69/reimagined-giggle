832d95f00f56169f7147d8c29ebf106ccf791b2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:02:09 +0000

    arm used married nerve display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
 print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
