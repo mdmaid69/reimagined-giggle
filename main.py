179e185caa23f757c44bc0e2f3dd1456df59304c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:57:44 +0000

    work cook already climb official

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
