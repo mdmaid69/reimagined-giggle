ac4cb0da7ad7fb50ca3ca8b5d1a19087ab33d915
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:38 +0000

    wish grow advise respect rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
