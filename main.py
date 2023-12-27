214d771f546dce613289a6f492ad95c4245bd110
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:38:40 +0000

    impression after persuade burn sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
