e17937743d4dcdba80579ed031d5d084b2d3091f
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:55:14 +0000

    autumn dress ours lead clearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
