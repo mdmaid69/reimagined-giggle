867d7ba136a5e0acffce1aa8b8f1d875893ee33f
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:31:20 +0000

    help solve practical deaf treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
