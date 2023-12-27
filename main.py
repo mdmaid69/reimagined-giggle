d275c147db21d8d0a0b1dd104aaed68888feec8f
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:47:36 +0000

    difficulty realize callout compare as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
