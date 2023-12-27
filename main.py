1a704826ec97e7c96956eccd4c12e89aca94efb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:35:43 +0000

    attempt crowd mother slip exactly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
