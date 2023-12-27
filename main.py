c76d0d79acbe29aa7a851eaa2c0d6bdc8320ed66
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:44:02 +0000

    permission used 179bite lower snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
