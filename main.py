414509fc5dd4a184e841aab58e6a233ffdfea134
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:04:34 +0000

    everybody neat environment free Vv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def append_to_array(array, item):
-        array.append(item)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
