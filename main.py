d0cee28ad97604e6a4629199366b54882fd15919
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:01:28 +0000

    gift willing joke appeal runout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def count_elements(lst):
+        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
