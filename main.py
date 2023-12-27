64dc864a8466a94e0ad329152992d7c35994e323
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:56:31 +0000

    protect break mean answer spray

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import numpy as np
-print(np.array([1, 2, 3]))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
