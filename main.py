86c9fd6ec031efbd425e2a50d63ec01bbd71a264
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:16:14 +0000

    choice attempt straight water congratulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_area_circle(r):
+        return 3.14 * r**2
