b2f0f738b1a248b80ee73637f41219c49928ad19
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:57:32 +0000

    arrange touch break turnup play

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
