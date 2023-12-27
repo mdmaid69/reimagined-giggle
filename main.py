bb6a4f1d63c605d25b89410032ea2bb0f9a9ccc2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:12:19 +0000

    thought tidy club star leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def find_max(lst):
         return max(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
