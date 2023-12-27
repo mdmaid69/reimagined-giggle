d0b92583f71b8d72001916434a69f067d596e29d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:06:53 +0000

    goback(to) fat divide authority appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
