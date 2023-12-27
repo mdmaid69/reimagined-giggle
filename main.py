c471a59ec2c78d9ca18b1962549d43b2995c0a26
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:40:54 +0000

    tip near still tall together

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def is_palindrome(s):
+        return s == s[::-1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
