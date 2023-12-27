9d6c3d2437d58da0da984f3e2dc22097aa13bb84
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:31:44 +0000

    important raise shall size subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def is_odd(n):
-        return n % 2 != 0
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
