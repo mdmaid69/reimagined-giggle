90db900c0afa5c4f5843f399537911b040c721e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:42:27 +0000

    through corner begin makeinto attach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import tensorflow as tf
+print(tf.__version__)
