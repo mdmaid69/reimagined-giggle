1b4cfacb643dd870214459cef69a53ed333d5587
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:42:22 +0000

    confused importance happen quickly suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def count_words(sentence):
+        return len(sentence.split())
+import sys
+def exit_program():
+        sys.exit()
