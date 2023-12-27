cf39a17b4f08bef77eed684821a3ba3eec77456d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:03:37 +0000

    among extremely schedule steer accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_binary(n):
+        return bin(n)
