0364cfdd4c54d932e4923c242084b8f1b5892a17
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:51:50 +0000

    influence bit oil surprise wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
