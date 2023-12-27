27fc3ddb1aa84d0909ef6789699624dc7c934e95
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:32:54 +0000

    reasonable hole please threaten machine

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_int(array):
+        return int(array[0])
