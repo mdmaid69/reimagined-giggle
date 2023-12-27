e76f3d9ee439e4863ed9444baf492ba6452d6566
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:01:10 +0000

    pretty yesterday cough capital stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
