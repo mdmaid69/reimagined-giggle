5e45e9ce6463f6bc78a107b2f48fcf8867efdb34
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:25:00 +0000

    character enthusiastic bath balance wake

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+name = "Python"
+print("Hello,", name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
