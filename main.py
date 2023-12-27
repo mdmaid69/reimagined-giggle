628c6254b16261ae345ed5315cbad533264e2d58
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:59:32 +0000

    mind treat bar idea threaten

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
