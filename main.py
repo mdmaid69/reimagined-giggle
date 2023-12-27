917ff19fe7bd984184c303d15bcb12c34e6befe4
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:41:20 +0000

    shoot disappointed argue explanation pot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
