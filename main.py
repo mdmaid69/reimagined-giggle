a8f8e4652b1d263e3e7a73859f0e754d104ccd57
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:04:05 +0000

    outside accidentally letter pleased air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
