8049b4577bf153553f9cb91c9ec4457d1c20f426
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:42:23 +0000

    fortunately money pride introduce object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import numpy as np
+print(np.array([1, 2, 3]))
