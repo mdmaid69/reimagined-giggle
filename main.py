db153fd3398200363669be82796f0cfe7ace4d44
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:03:39 +0000

    born clear rubbish roof hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
