fa9ca1b23dd9723b1e99e7782b5a43294a3d7720
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:01:41 +0000

    old turnup gather history capital

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
