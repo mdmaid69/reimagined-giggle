ffc2586b636bce9e61eae4edf0691df6b47d5a23
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:31:47 +0000

    natural cruel wet order there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
