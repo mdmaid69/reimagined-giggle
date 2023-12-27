867d09bc2a758e1426831e69330b0b167a24d7d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:01:39 +0000

    smile walk water steam anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
