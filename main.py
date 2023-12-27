04659405247f1c5f2471c9b96721145d8bb4c665
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:51:39 +0000

    Cc fill board contribute performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+def find_min(lst):
+        return min(lst)
