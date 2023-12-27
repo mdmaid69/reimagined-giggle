fb528b59eeb49b1992a92e48031bdd8552ce14fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:35:15 +0000

    hospital bus far bye touch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-name = "Python"
-print("Hello,", name)
+def calculate_power(work, time):
+        return work / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
