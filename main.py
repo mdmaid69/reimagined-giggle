f46fcb7381a9e6a4a320a08b677e3e7af24e1be3
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:44:18 +0000

    suggestion that second doubt hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
