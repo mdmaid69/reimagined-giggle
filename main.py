d96bda9d69c82670a7b5ee8baa0d50788dd9a533
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:13:31 +0000

    snow partner among dress round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
