3b3576425a57ea7c5f4e741d35f3d2a47e3df757
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:02:43 +0000

    play where study conduct bar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_octal(n):
-        return oct(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
