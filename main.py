4b1bad0caaf181f8c743b483e04ff9be2d6e95b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:28:42 +0000

    against born embarrassed hit thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
