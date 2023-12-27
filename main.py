cfc5eb1b3f0366bbdd65fd77a664d38e563a7243
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:35:56 +0000

    frame position yes complaint centimetre

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_item(array, i):
+        return array[i]
