7753da72873a93f1b8ddc4192d0bd9717fd1ae51
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:56:31 +0000

    dance change laugh shallow warm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
