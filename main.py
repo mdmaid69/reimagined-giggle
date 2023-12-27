164f176812c33e659eb8a5acbb04f6d4f7099f3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:58:52 +0000

    reserve season have clue twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
