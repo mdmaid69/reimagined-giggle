18cf701a868e5575e73491677caaff83b5357688
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:07:22 +0000

    inquire protect car see pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 import sys
 def exit_program():
         sys.exit()
