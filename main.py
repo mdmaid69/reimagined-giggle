56d6f9d237e26aa3cf51b7b2f979bcfe49cd11f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:59:12 +0000

    nurse love storm anybody race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
