e4d64dc426d8f9663b81ec08123196a2e309a11d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:15:12 +0000

    bringup spring board noise Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import getpass
+def get_username():
+        return getpass.getuser()
