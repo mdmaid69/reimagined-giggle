271b9c4620775e7dd73753bfbcd24fceee18cb9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:12:55 +0000

    unusual eager the wake talkinto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
