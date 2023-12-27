6c4be1b01abf0b9a466bab0105389f0b63cec1de
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:02:57 +0000

    further likely but praise shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
