401df40776ad6fe5b3b78363dea720da4784f022
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:50:09 +0000

    speech result move strict wild

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
