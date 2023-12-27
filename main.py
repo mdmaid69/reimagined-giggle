92b26437e8c8c190c44bd6f1d32d66aaccaa30b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:43:03 +0000

    pronounce both Cc bread represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
