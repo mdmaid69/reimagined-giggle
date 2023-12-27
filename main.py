f8ec1b14ab734bb05c4a27943e6440c79030fccc
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:25:32 +0000

    probably lookafter fear delicate generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
