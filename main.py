a631126da99875512db0ee2101de7f6ab2717dac
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:23:58 +0000

    matter workupto modern joke estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
