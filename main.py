a9db54a666cd09ee289dd1a47195514074694782
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:50:47 +0000

    curious main across many impressed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def count_elements(lst):
+        return len(lst)
+  def square_number(x):
+        return x**2
