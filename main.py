a6a18bdeea626cf8c154194fb77a693f94a5f323
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:00:06 +0000

    interested next reject needle invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_as_str(array):
+        return str(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
