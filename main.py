34511409c26f45f3e96c5d374a7bb9d95914b5d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:56:07 +0000

    in nature pity one out

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
