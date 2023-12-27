6296e3a46114d6314a4f09d39209c73a15713b36
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:19:56 +0000

    break answer loan occasional birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
