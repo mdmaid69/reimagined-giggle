3f412c2fc77a7fc59246368e3d2a9face36e3b87
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:17:41 +0000

    well exchange used only comeover

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-print(math.pi)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import numpy as np
+print(np.array([1, 2, 3]))
