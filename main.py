7c17037d503eb1d1e80a77954165b184b83ddfe0
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:09:19 +0000

    couple partner spare pen club

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def sort_list(lst):
+        return sorted(lst)
