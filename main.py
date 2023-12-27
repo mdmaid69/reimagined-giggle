df517decc26ed0a010eba507ded4baac73844bf3
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:30:16 +0000

    Cc memory scene nearly Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
