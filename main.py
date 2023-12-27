d0966b04858a2292db857943f3e9afed196a9c0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:35:13 +0000

    view cry raise remove punishment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
