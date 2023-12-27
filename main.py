098966a6618465780f6f8503c3df9fac3342c763
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:13:38 +0000

    means public rise leg sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
