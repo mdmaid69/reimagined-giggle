f4fce0fb24d357542340e87a6530711bf169e78a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:59:01 +0000

    concerning ring loss up period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
