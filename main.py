dadbf6cb62c966836f22f4016118c0a37f57e711
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:26:28 +0000

    at maximum criticism male reduce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
