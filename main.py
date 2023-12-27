dd74310a65035fbde8015c28b7eddb9a27ef6130
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:05:08 +0000

    telephone boring positive interruption help

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+for i in range(10): print(i)
