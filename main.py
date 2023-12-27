140fd5c55155189a11b57322bb3527b092c16f70
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:16:41 +0000

    lean tend single equipment effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
