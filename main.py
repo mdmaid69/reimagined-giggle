16fee737accb7318cd84bec7894033743fa9e839
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:58:12 +0000

    bite upwards clearly behaviour crowd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
