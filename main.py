451dadffaba7303a1657688d12a214ae2326409e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:26:25 +0000

    reply clearly centimetre advise attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
