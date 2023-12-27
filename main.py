f906002004c1bc520111ab1bef69661d122ebc58
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:53:28 +0000

    pure culture open this negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
