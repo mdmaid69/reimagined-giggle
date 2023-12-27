bdd0e4b593203318f996880c94a77a19f175eccd
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:25:12 +0000

    whatever attempt tense dark means

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
