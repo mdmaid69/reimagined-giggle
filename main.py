8fbe9a13fa27b73668e2b0c84216495176c0600b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:57:33 +0000

    risk uponon freedom tap workout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def clear_array(array):
+        array *= 0
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
