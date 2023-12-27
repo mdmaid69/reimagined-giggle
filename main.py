9d320d09b7182c9e962fc0aad792ffb105c31896
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:00:21 +0000

    little slice among but expect

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import time
+print(time.time())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
