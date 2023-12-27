501c9ec650274dfec06f25456efa7d045c21fed8
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:17:50 +0000

    weather drop funny present invite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import sys
+def exit_program():
+        sys.exit()
