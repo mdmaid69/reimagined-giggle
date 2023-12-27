4e6a9b1df53f5ba8167aa984034a57b3f3bb1d14
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:47:42 +0000

    whenever vision write computer eye

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def is_even(n):
+        return n % 2 == 0
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
