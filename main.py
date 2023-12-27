decd6bb884498c5c661ee4648b59759812fc7c36
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:28:53 +0000

    plenty someone far believe formal

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_directory():
+        return os.getcwd()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
