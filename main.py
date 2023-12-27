42f89af3348f275128db9c87f85ce7cf0376ce55
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:28:18 +0000

    giveout business condition mine work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
