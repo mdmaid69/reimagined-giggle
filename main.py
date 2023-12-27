5e22db0351f3e7413d45da9cae1a34449d82c06a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:54:34 +0000

    length stay seat bottom arm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_work(force, distance):
+        return force * distance
