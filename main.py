63bdaca9fec500c35d28d5ec5c3e109343c6b742
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:59:52 +0000

    union intelligent father pencil by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
