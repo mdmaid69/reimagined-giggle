0e00bec82aec6bb3edd078df848bc692abcc7494
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:09:45 +0000

    much agreement 569enjoy instead praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
