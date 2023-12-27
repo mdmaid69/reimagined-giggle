c73f398fbd535a2b6955cbc25dcfc1c7328528e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:56:09 +0000

    marry scale beginning brush extremely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+name = "Python"
+print("Hello,", name)
