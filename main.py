e30ca48e7bad9619322562edd23997befcba4d33
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:35:12 +0000

    speak call along ahead space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
