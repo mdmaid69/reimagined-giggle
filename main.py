4e85810c15a5bd68cb4b2bf97bb0b41f10903e7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:16:38 +0000

    earn start excuse hit dinner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def subtract_numbers(x, y):
+        return x - y
