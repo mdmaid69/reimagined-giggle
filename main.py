ada5e02ab36b019a6a9b89fe3f11e85154faefbc
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:42:13 +0000

    any hour along string clearly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
