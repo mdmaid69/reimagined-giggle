8c8f7575f19323398352e635e87146d2e0c039fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:03:06 +0000

    book bend same patience past

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
