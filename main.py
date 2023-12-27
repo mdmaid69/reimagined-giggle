edef82071e4d517e39f75e5d74bab41f31d59e47
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:26:19 +0000

    Ww breakdown vote spring bread

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import collections
-def create_priority_queue():
-        return collections.deque()
+def find_max(numbers):
+        return max(numbers)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
