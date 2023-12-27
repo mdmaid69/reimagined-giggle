2d8369ecb522888a8788af6b5601ef2778e37386
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:52:37 +0000

    bury report bad treat each

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
