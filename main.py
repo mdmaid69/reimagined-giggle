8f5f2469649e43b984bd4bd1830c692bf44e22f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:50:46 +0000

    comeround if complete related introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
