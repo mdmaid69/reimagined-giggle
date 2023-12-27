f6935404392e8ec796fb79238f44d034d38a6a01
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:54:43 +0000

    quarter picture show by breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_user_list():
+        return collections.UserList()
+def add_numbers(a, b):
+        return a + b
