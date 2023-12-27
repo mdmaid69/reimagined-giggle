1be7378334aac2a1eba1effd7e0e75c7725b0078
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:24:33 +0000

    comment watch 569enjoy disagree sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import getpass
+def get_username():
+        return getpass.getuser()
