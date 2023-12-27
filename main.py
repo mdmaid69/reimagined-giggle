1e23c614b5185e55b63c2cc9743ab1fc8e38b97b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:26:54 +0000

    slightly private early question actually

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
