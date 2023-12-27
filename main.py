4947f2c853235e4b18b6a4cefec567754606d104
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:44:15 +0000

    concern workupto sure improve crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import sys
-print(sys.version)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
