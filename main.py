096f6663b48813e600807f58f318b8863c141285
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:21:17 +0000

    affect convinced teach criticize workupto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
