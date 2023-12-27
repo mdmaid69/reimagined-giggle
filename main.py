552f367ba72e9968b41659bcb552921235abd029
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:35:50 +0000

    some full connect else responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
