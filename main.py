8543c1492efb2518363f34ab54a1cfb91b235a09
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:22:19 +0000

    along organization under deaf move

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+print("Square numbers:", [x**2 for x in range(n)])
