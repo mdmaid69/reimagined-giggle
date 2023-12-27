3537a26a6c50cf42ed92c31c08b4890f53020400
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:57:46 +0000

    afternoon second that left such

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
