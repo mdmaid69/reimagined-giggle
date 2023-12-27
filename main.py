eba4f09d69897fbd76d6471b07a322898934289a
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:10:06 +0000

    ever branch run fond bush

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
