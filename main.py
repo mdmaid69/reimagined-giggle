4c33570ab481a4ed07719c4fc682ac9859664451
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:21 +0000

    way they image base emphasize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
