a9ae4c2efd93cf9745778dba1a6a011f1bd0082e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:23:34 +0000

    consult fierce off shallow shadow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_buffer_info(array):
+        return array.buffer_info()
