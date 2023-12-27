dac1503896d503e103f006078063f3209b2581cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:23:55 +0000

    small improve hers properly intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
