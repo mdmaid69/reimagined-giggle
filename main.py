7d1bf5a5bea752b98069677e21cead390619667f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:23:45 +0000

    design goon profit giveup comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
