4b693f1a9bb8427a1c5d885c096bbe594e6a8564
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:35:01 +0000

    let interesting doubt spare below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import os
+print(os.getcwd())
