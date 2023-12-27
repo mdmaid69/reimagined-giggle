42309ee1d78885644edd55327715909f35244a6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:24:38 +0000

    earn foreign favourite field scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
