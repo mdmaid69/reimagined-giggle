9dbdb9e14d7f78d0f50a8d23471fdba25c2d2833
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:24:11 +0000

    cross meeting point suit play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
