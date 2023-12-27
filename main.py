8f98b47275106e74730956a7b9d8711986afa581
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:50:20 +0000

    regularly listenfor breakinto track create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
