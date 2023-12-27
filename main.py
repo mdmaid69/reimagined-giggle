66bf3a084a8c5d8ae0dbab01b183d1df37446e2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:47:34 +0000

    able towards clue squash official

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
