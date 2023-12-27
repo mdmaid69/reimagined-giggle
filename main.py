9b0842a0b4260152e3776c015f74389df1efdf20
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:19:39 +0000

    share regard bright solve free

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
