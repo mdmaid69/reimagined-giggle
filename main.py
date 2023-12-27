35ea7d6f50e0f501ff98a0798887d2df8ecdc3ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:44:33 +0000

    every familiar grow roof original

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
