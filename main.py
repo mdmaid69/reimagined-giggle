167c9f3e11ad82c394c52184b14bbb7fc831c385
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:57:04 +0000

    basis switch when onto sorry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_pressure(force, area):
+        return force / area
