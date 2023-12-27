20a3925b612aeb58f866bd6d1a8cbe7ec8dbc46e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:52:59 +0000

    smile concerned satisfied conduct convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
