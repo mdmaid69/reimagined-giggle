ebe548b89311554e900226151ad12f52ec89fa59
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:28:41 +0000

    unlucky mess recovery get crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
