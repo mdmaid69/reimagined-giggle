309cd4302d3223c5ca82bf24d43e2bf260dd18a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:23:34 +0000

    giveback admire uponon creature everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
