925795c6de722cd54de4bb7141c090c4cccc030b
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:44:46 +0000

    protest soft factory minimum joke

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def find_max(lst):
+        return max(lst)
+def is_even(n):
+        return n % 2 == 0
