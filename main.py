03af39aaefcfc9dc863a741546a90cd415f29cf7
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:58:04 +0000

    middle right extremely by hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
