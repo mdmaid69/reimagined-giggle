68a986316a5dcab226bf09039c9434bf8144522a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:25:16 +0000

    order basically boring move hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
