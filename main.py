5289b70e191b32e509905b7b13236aab442698b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:32:50 +0000

    doubt idea bang experience level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def set_array_item(array, i, item):
+        array[i] = item
