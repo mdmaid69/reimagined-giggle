e939e836dc7a7955c2b7d1a4970e2ff06c1b6bc9
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:06:14 +0000

    attitude brush medium already tip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
