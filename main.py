013066415f5877198b0dc718d6acc035552ba8dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:46:09 +0000

    OKorokay pleased female lose tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
