bc24c7639c881530a3db4715b8547ad7ca443d61
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:21:50 +0000

    forgive check skirt unkind best

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
