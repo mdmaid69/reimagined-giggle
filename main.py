1f6b4d3fec5756776dc92dfb16ea04cf0b0efbad
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:55:02 +0000

    he die remember equal smooth

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+for i in range(10): print(i)
