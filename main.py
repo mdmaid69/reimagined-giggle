d77f261dfd0dcbec33c9eae775d8bdf13c394b58
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:50:19 +0000

    or obtain alive different thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+print(math.pi)
+import array
+def get_array_as_repr(array):
+        return repr(array)
