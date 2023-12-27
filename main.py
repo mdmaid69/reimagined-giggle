efd925738d0a76462ef80ac56016f4af499b8176
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:43:04 +0000

    profit across sail term to

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
