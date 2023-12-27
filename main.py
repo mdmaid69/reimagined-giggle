4e56cb784d5fa45bc8a88b4651c030ac19867b00
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:49:10 +0000

    city but debt human strange

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
