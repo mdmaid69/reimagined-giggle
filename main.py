7d0fd46f6f8d28a77bdc4ee444fe62b4ac5cf3ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:35:15 +0000

    uponon popular arrangement before flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
