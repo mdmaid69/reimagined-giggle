8bc1a65c7bfd830d76a20844ce972c56aaa02fb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:12:51 +0000

    convinced hello conversation exact lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_complex(array):
+        return complex(array[0])
