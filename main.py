4f98a7eeed8f1f71db54445808d4b25beb223baa
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:56:02 +0000

    more maybe television silent miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
