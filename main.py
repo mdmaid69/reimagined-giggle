8bb1c1b3502095f4945091d2597cd80a730e14c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:48:56 +0000

    trap manner protest soon alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
