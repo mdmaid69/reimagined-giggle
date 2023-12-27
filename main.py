385fffc53bc2fb8a2e7322810d6712a47d4537d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:49:29 +0000

    tell instruction factory getback number

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_acceleration(speed, time):
+        return speed / time
