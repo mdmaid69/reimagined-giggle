c473872b9bbd7ef6183a44a830927ab1e49ee2a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:10:48 +0000

    children cum display perhaps read

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
