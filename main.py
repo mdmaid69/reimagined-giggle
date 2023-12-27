a0de525144dfdde188a879dee23d4b89363fb081
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:44:52 +0000

    thank immediately hardly rid attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
