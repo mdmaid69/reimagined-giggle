fc9ff93b0b13705bc88875dfbf4d045d5b8f2a1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:16:59 +0000

    mess break beneath window bright

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
