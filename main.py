54d75675bf0b3af0ad3ee951775a9b50d8788798
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:46:11 +0000

    clean whoever person approve 571enough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import collections
-def create_user_list():
-        return collections.UserList()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
