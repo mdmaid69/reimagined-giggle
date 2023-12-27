2dce9c3f52fb3f5d6190efa07f7f3cc3217cd020
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:31:48 +0000

    authority treatment song drawing please

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
