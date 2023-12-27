5b8d033121e2b539acd1c8c456fbb7f21362cd00
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:54:45 +0000

    talkoutof anywhere somewhere deep destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
