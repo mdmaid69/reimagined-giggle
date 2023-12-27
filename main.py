8a31b9fea9c3765b4b07db4cfb69d79ab7d7a878
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:41:49 +0000

    bunch in trust division cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
