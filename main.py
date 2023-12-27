e6d7a30ea85ea51c5ece3f7eebf000c18c157571
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:32:05 +0000

    light shadow perfect better legal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tensorflow as tf
+print(tf.__version__)
