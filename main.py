d0f2de978c591524efc4858ffdf7eab2bda449f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:55:37 +0000

    concern behind excuse stop runover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def remove_directory(path):
+        os.rmdir(path)
+  def square_number(x):
+        return x**2
