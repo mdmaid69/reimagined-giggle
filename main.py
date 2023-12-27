3a86bce57a23d0dac915f315ca517d848e866928
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:29:40 +0000

    turndown close remember against rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
