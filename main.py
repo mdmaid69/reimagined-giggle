5bdbb733cb1aab87bfc4dd6713d52ecc078d074f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:55 +0000

    nerve undo rarely city pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
