b7c1c3c11792c6eeb241983c9e21d8a0dabce17e
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:39:06 +0000

    after separate warm bar about

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
