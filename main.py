b96ca1848cb07f2836bff33eb187a3de85f99dce
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:18:07 +0000

    great load kind simple add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
