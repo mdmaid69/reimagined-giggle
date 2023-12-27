c29ac5410a5eadeafb530498c0516163ba6933b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:27:23 +0000

    turnoff upstairs worried deep attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-name = "Python"
-print("Hello,", name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
