783d6e29e75b944ff28965f1979adccc0f60cf71
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:31:54 +0000

    responsible row call watch narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
