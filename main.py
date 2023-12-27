d3b712badfce5256461c7e4967ddb100f9aa950f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:39:46 +0000

    poison bin freedom mother spray

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
