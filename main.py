d05b41dfecd180291e1aa4d218ee51b2a7e9d0c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:43:34 +0000

    card pile basically list drawing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
