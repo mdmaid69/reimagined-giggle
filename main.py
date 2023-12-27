9aeafdb712a1e69e9078c63b66d9af6ac71b315d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:59:27 +0000

    maybe rain firm stop lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
