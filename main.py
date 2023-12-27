caf3c69937dd046fb1f6665b7f745b04ad0d3e00
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:11:11 +0000

    prevent scream turnon dry threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import collections
+def create_counter():
+        return collections.Counter()
