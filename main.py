5861f2e7730a69589397ed3ef79348888b462fc3
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:47:27 +0000

    less sore fast pause foreign

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
