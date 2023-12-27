6ec3c44598b7a81626d30b94f48d9da989287e54
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:06:02 +0000

    right roof watch easy each

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def sort_list(lst):
-        return sorted(lst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
