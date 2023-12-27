5669c37cce314e6da9cb6f8666fcdd46157ed46a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:50:47 +0000

    floor start afternoon welcome rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
