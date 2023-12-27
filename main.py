6c9cbe99d59cb9c1912a3a8cafc3fd64d96b8014
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:04:46 +0000

    pay confidence bet long excited

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
