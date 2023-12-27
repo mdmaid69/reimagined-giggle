a63b88199a7b180482ee617bb6830bee6d75b6ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:10:45 +0000

    pain cheat test side accordingto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def is_odd(n):
+        return n % 2 != 0
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
