6712cba933c48ffb2ebafae35115d8a5d58f1525
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:32:08 +0000

    follow meet work together quantity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
