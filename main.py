a022059c725dae8b405c48e9bcefb3488a7eef42
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:27:39 +0000

    news matter discussion plain whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
