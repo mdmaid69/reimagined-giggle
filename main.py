d61cdcce36c4b49525e8111ff2bbe9aaded5bb70
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:00:52 +0000

    confusing Ll belong house quiet

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
