53b8f77d7bb98fceff570b0f6df93db319e9b729
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:37:55 +0000

    shout shape which keepaway prefer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def cube_number(x):
+        return x**3
