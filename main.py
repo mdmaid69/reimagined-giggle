bcbbd36b68f489dab32a9534eb9672db92a8213c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:54:16 +0000

    address direction twist swallow arrival

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+print("Hello, world!")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_directory():
+        return os.getcwd()
