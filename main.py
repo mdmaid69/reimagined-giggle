8d2af80163f1c01d641233aa661308b5688a254e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:58:54 +0000

    excuse question fact work speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
