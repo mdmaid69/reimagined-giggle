68e80fbeb42fead13f15b352ec16f5f4b056de85
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:05:24 +0000

    slide meanwhile middle house half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
