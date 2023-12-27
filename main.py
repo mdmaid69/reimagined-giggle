495fa4c8daf918339919f7101585fa3ccdabcbc0
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:56:34 +0000

    colour themselves reason recently disturbing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
