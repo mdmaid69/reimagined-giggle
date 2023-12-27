7759fe9e565ba81d85ff9d0b24ce47aea6b6dc02
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:47:30 +0000

    reason coffee theirs frightened top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
