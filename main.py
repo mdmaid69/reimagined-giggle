a511d0713e32befea9619de50b162ca20d4c891a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:12:10 +0000

    distance concern ourselves hello embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
