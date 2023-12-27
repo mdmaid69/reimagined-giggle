db690f1e9f753ce352a118fc329dad817b8bab5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:01:15 +0000

    backwards city dance forgive control

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+print("Hello, world!")
