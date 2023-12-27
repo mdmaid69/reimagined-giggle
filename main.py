5fc52488b62036acb755849106a79655756d495b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:04:12 +0000

    tomorrow lookup but sale other

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
