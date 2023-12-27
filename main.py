a9dbbad00c6a070ba7efc3f715e3bb4dd8c956ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:53:02 +0000

    sale tense future wear injury

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
