97cb31600702c5988e8d72209852a6f2e7609ff6
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:03:03 +0000

    among satisfactory build claim slide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
