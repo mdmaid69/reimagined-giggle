d973679fef3eb5a83e4e8a8f06dd5129b9e18739
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:07:46 +0000

    fact shout onto hate tone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
