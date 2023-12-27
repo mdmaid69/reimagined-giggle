7cce836384fe1a21c587a70cca81e6f9c23b4926
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:16:19 +0000

    pressure nor perhaps home trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
 import array
-def get_array_length(array):
-        return len(array)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def insert_into_array(array, i, item):
+        array.insert(i, item)
