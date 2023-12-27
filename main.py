969a292fd5535b98dafa1d787ca2de96474a7f5b
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:28:56 +0000

    hollow spread supply guess free

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
