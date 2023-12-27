0a4ea8c8d609869b59330f97c19b4771b6e8a489
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:24:18 +0000

    179bite best collect although know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
