ad30dbedb6b4a1f495eff0862ddfd802c4e4d456
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:55:26 +0000

    pattern follow claim arm effort

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
