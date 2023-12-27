7fa6f712c0a445962c5a91249f54227b7d5af858
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:10:26 +0000

    concentrate sound realize shopping dream

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def is_odd(n):
+        return n % 2 != 0
+def is_palindrome(s):
+        return s == s[::-1]
