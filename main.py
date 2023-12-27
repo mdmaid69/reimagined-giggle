8cadfababfacd503b78ebfabf207e92c91ddcba5
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:02:01 +0000

    suppose basically hope pin obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
 def check_if_array_does_not_contain_item(array, item):
         return item not in array
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
