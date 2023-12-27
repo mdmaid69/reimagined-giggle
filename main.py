292ef6b2eecc16305742fade135aa07969f5b683
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:13:35 +0000

    book interesting pupil actual ordinary

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_speed(distance, time):
+        return distance / time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
