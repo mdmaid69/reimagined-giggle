f90e2b7e6f121195708c2695a4c5326bb7e20862
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:00:14 +0000

    attention trick community fire breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
