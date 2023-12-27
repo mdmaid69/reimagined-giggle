dd937f64caf524baee607c11ea59b37f6e9badb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:34:14 +0000

    towards help black freedom afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
