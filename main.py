337f58d3eb6fbcbbbb311a12b432841d005f88da
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:22:41 +0000

    name negative manage person long

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+def get_current_time():
+        return time.ctime()
