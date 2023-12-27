d20b2af0910047f7df050b33b9e5c40256e95450
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:19:11 +0000

    sea surprised shut violent meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
