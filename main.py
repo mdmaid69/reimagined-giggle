281842e3e7c48082699cc833e1c89a136ab4bf75
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:33:40 +0000

    breakdown low bear unpleasant persuade

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
