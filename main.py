101841ac601846c2fb0bef5929072ad0251ef5e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:55:10 +0000

    disagree gift sad brush cheap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
