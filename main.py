bcbce2b3d0cfb74ab07ddf495bb5d8c5e966097e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:59:04 +0000

    warn none dance tour to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
