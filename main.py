341cf8f457169f8de2527cdef80516aed045795e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:58:38 +0000

    job shape cry connect band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
   def delete_directory(dir_name):
         os.rmdir(dir_name)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
