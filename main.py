f6f63850e976ae9faed6b7d2103f1a029b9cc9b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:23:38 +0000

    recommend abroad describe key since

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import sys
+print(sys.version)
