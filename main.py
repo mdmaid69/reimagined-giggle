0315253fc1ae1514584b1f7a0e82a935f78cf981
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:02:24 +0000

    ever behaviour fire head sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
