757a0172d81309b9dfede894676465b85cfe1db2
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:13 +0000

    song makeinto identify when warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
