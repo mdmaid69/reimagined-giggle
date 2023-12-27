25ab717f2b3f71997adeb1b18289007dbf829683
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:27:27 +0000

    table period match right relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def sort_list(lst):
-        return sorted(lst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
