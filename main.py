a00e1a2f27eefed4996b2d911c87accab31eed13
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:30:48 +0000

    properly complicated 566end violence front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
