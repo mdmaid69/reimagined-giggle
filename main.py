359dcea892bd61fe07dd7ff252d646938e21e686
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:28:00 +0000

    himself attempt Cc shut head

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
