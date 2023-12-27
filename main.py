754caef3b40c6f692dc8e293a16ed4ba629dbe71
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:07:27 +0000

    comment bin charge imagination design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
