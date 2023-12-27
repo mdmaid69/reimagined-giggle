ffac5cbd7014363de310159866da8a06e020da37
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:28:21 +0000

    willing would cheek wear black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
