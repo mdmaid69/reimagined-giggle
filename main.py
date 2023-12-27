771536f3db7fd7b47a5ab95a4b9a2651ae7d76d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:05:44 +0000

    combine stone murder emotion protest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
