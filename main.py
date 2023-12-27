4331dc280171c31df1544bd2fc3dab5a91211f0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:11:52 +0000

    usually brush keepaway importance clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
