fd53c82a83226ad1e4d0bf817964a1670ff97e48
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:20:06 +0000

    outside any father alive track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
