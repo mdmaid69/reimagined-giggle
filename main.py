9280dc5069cd693d113e23db1cbe020707bdb8bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:18:32 +0000

    wherever right tend impatient after

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
