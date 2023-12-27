7352451994ee19f21e36ba73c1f76924b6b49890
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:37:05 +0000

    scream every gradually price nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
