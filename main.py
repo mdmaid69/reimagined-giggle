3c27ac25190013f25fe43bb00328c61ba93b293b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:44:05 +0000

    in beneath cry damage original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
