0a85dd101fd039803360bf51a0d4899861209dd9
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:49:50 +0000

    identify bill prepare turnoff number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
