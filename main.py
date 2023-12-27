02c655660efbbdd0dc9deeaf0a529b6829c08fe8
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:00:49 +0000

    slide congratulations watch profit pull

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
