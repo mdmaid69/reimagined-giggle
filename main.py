d6a3c472236ef535e14f3d9c626033836624fdcd
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:49:48 +0000

    sweet uponon near form still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_directory_name(path):
+        return os.path.dirname(path)
