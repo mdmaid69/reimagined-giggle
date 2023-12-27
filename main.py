50ce1a4334193fbf88bdbde7c7bca43b0a830147
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:14:22 +0000

    tap defeat finish regularly minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
