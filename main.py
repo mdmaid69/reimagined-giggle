d34eb5c9c50657c62525e027906731b346500768
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:46:01 +0000

    soft mass thorough before bringforward

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
