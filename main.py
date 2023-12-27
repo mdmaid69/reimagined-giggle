50a2f05f8b2368fa6688406be0f7d38d64502e32
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:45:04 +0000

    poor secret before pride minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
