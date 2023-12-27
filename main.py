10b9eea79452d92e57e0ef84acf030af013cb523
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:47:54 +0000

    ring length capital disappear object

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-print("Hello, world!")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
