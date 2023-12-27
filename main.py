279eee532abe200c7c27c0264bd0cac71c9e99fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:24:47 +0000

    fault solve yes reject over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
