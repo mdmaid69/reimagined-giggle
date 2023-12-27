0025ef0071afbe69dfb56e0f590a604e3b3f3d06
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:26:06 +0000

    commit alone steam language maintain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
