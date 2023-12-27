ed756a63fed78e13b5d3a830de552c8e2d10699a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:54:15 +0000

    act government drive print thank

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
