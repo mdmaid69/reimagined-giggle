1c28334bd55edafa685a455a8bcdd9552f096adb
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:28:27 +0000

    part physical suppose deserve free

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
