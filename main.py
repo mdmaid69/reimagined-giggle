2579d7c022e6d4e994b8a1c551d6512867a7aeb1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:45:01 +0000

    passenger frequent behind answer heat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
