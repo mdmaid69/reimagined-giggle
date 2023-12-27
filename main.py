e87ebc7ed6e45422c0eaa68689d0e6e4548dd3cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:53:51 +0000

    regret clothes start attractive friendly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
