34a0f48e7c07949601db15a156bd2cc959e25cb2
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:17:05 +0000

    wish official runinto independent circle

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
