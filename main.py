749103ad5fb707a9a54139117838c1c6cd9740c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:02:34 +0000

    run scene undo pronunciation lookup

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
