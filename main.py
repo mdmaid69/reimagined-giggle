4387272b17a0f06fd42981def2bdd39491345067
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:54:43 +0000

    holdup frame test combination set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def convert_to_binary(n):
+        return bin(n)
