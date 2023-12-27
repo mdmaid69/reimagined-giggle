bc9ec0ccc080569f3c361ab0104409bfd1c1773d
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:24:54 +0000

    pipe sort her people considering

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
