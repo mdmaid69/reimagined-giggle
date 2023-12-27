18c663f175d64394c01273a031ae437854c84855
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:54:07 +0000

    upset attention great under opposite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def convert_to_hex(n):
+        return hex(n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
