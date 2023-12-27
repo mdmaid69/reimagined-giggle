18c40797b0dc65da046891de226867f9b550ce12
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:48:25 +0000

    buy as entrance her should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
