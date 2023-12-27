9654a12201b14963334cfb64dba7d471b1b0b2f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:26:40 +0000

    come success worst agreement terrible

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def add_numbers(x, y):
+        return x + y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
