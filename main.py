c49b2b42a50a9f9ac3d093ad11c741be6fc1ffb4
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:34:13 +0000

    consist weak rule break fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
