c298a21de7b1010091f49f64d12bf26f968386ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:22:22 +0000

    thumb trick conversation pair limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
