efb6dbac9985b3472a2ef76c5efb6db12e72918d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:22:32 +0000

    set only perfect love warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
