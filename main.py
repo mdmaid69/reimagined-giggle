0b1511210e00fb14cbc307ac899588cfff566edb
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:59:26 +0000

    unite escape emergency talk marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import sys
+def exit_program():
+        sys.exit()
