4f681ae696bd116a308d21e9882c9a95480ca19c
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:48:55 +0000

    complete turnout worth interest give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_item(array, i):
+        return array[i]
