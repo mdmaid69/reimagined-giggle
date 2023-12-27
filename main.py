479281e83c62514c754081946b82534507532aa3
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:47:42 +0000

    pure border use below direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
 import array
-def get_array_as_list(array):
-        return list(array)
+def append_to_array(array, item):
+        array.append(item)
