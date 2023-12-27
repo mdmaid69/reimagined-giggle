9dbb18ca6c12e8a3b51496c0c6451c295f93bf6e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:27:50 +0000

    lie example claim transport bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
