2ff0ec1cd0e1b894f3984db294507fbb234f749b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:30:03 +0000

    remove lend count comfort favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def greet(name):
+        print(f"Hello, {name}!")
