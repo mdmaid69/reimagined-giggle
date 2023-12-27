3cb6ae46180f79478e311830f0a3dd1275e95875
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:33:56 +0000

    difficulty progress flat rely check

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
