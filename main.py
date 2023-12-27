b4f2f58e1755ddafad810da9ca6d44ebe00e5db3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:39:08 +0000

    degree best money butter suckme

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import collections
+def create_user_list():
+        return collections.UserList()
