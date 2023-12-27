2b75062fbd08a577e0f869da06b9c71959e704f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:50:18 +0000

    naturally city study invitation high

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import datetime
+print(datetime.datetime.now())
