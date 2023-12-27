34d564cf6c94ac9bd6aa08201237ef0a66039584
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:25:20 +0000

    nurse wish heat check interest

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def clear_array(array):
-        array *= 0
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
