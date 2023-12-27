c5e9807fa1aac1b10239df84f8db93e362c07391
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:13:01 +0000

    suit off habit spite attitude

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
