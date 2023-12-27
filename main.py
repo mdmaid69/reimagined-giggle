3608d54ef88a916c2f6c5bc51f27d8a419b2b922
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:06:51 +0000

    movement owe Cc accommodation 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
