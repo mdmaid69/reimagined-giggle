ef530fa6adb60834a11c624c8a1ec0cbf9caafb1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:20:13 +0000

    ask slow brain hardly complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
