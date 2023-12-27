c2a1e1e5d6be7bc6433844d7c547f6da678837dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:40:31 +0000

    callat guess comment taste though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
