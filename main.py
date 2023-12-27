3fcf297d9c4a870681b0a95ba0c6c1822f6fe376
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:16:00 +0000

    tread twist picture constantly turnback

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
