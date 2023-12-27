5031f1e2e72354dcd9544d01aeb7d5b9ca939ec8
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:17:42 +0000

    art willing inform care best

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import random
+print(random.randint(0, 100))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
