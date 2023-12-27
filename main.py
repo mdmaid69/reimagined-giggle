624faffc24f2de4101c5cc1ddf8800e5084f78f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:14:52 +0000

    prepare nowhere far quit future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
 import math
 def calculate_error_function(x):
         return math.erf(x)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
