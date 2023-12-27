05aa1f3f0fdd6f2945fc8a9063a0309731590c47
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:34:48 +0000

    considering turnaway birthday change movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
