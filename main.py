0c62943ac251dbfab121a4f13e3ab07447fa35c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:47:32 +0000

    note scrape report piece like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_string(s):
+        return s[::-1]
