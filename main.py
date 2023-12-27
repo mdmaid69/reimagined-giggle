9e70b47c9918bbf51bc3d30fe8feb1c709400055
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:05:00 +0000

    think pencil realize get scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
