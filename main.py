c714392c94312bd273babb6149b1e42b63d06ae2
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:42:26 +0000

    remark receive stare scene guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def add_numbers(a, b):
-        return a + b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
