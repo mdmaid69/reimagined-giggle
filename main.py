5e7f36d5a4fb9793371be48a37f77dbdca431755
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:16:58 +0000

    ill yours apart tend content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_username():
+        return getpass.getuser()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
