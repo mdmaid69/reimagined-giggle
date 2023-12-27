7a5bed9a61af43dec5d34123de8b913d527ef8e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:40:57 +0000

    lie order turnout already Zz

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def greet(name):
-        print(f"Hello, {name}!")
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
