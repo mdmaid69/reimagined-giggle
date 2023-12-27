9a23e77ca003c14b08a2b4780c675fec3385ef8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:34:18 +0000

    tonight season remain left luck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def count_elements(lst):
-        return len(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
