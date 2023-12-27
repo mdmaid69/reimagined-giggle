ea2dfe63c46e531505d5cddb694e2710d54fa7cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:42:29 +0000

    notice fast mass urgent principle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
