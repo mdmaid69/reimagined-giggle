216ec90180968214717117cda806ba2eba1f4ad0
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:30:56 +0000

    report fork move fat lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def reverse_string(s):
-        return s[::-1]
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_string_from_array(array):
+        return array.tobytes()
