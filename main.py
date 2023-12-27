508d24bb3debe485bf99f57e372d762c9317133b
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:35:15 +0000

    please comeout apply bury country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_density(mass, volume):
-        return mass / volume
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
