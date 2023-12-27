1b6aa2324db55642281d895241796bbd16f16799
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:33:13 +0000

    touch bar uncomfortable independent need

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_list(array):
+        return list(array)
