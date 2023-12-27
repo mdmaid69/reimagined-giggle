e71f7f597ee8b192a3ca90077155e3f32c11852b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:36:45 +0000

    feel spring scheme trick secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
