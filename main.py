71991956d558c28c21ddebd16f32709173fb135b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:53:39 +0000

    slide far geton occur unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
