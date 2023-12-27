ff213711a779517ada4e8ef369fc8faedceb4af8
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:45:06 +0000

    I year cheque address communication

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
