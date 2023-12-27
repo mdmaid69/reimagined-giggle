cc9126e62f6ca3c14ef2d5ac970de2e92c733533
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:08:44 +0000

    mail everything workupto slide pocket

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_floor(x):
+        return math.floor(x)
 def calculate_force(mass, acceleration):
         return mass * acceleration
