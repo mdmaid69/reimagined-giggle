3f41113074a03500461d892e413c07856a4252aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:26:36 +0000

    separate develop important history top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
