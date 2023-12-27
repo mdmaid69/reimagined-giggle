1a11444056ce63227028d8935276f8a0f3a9f697
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:25:56 +0000

    ocean style pocket it cut

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
