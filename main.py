2712823d3c1cf83d1114e4e3c014a9e7b19763ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:08:51 +0000

    issue quit gooff weather battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
