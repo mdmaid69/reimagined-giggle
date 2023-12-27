367206359f89ae991019461f00022bf57419e3d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:37:10 +0000

    OKorokay home shut pity inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
