08626a644cb006859e62a9945969a7ea957ea6c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:17:27 +0000

    independent left mark rent home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def change_working_directory(path):
+        os.chdir(path)
