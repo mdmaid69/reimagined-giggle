16952b82bc076f9009a7fe399aa93adf531e2d5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:25:49 +0000

    chief Pp accident pretty share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def count_words(sentence):
+        return len(sentence.split())
