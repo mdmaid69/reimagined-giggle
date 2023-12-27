4951a08e5c7a43508f037c6c5861993ebbbcb779
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:58:07 +0000

    centimetre owe conversation sound minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
