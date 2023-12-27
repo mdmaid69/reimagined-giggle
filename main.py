295f60eae0570b7a77f20609b475179ab94570a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:41:24 +0000

    moral period runout squeeze nonsense

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
