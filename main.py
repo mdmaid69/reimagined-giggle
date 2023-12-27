1221e40f2e784ed7b68d7f1e9bd3f67718ba5262
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:06:00 +0000

    foreign return embarrassed basic we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import collections
+def create_counter():
+        return collections.Counter()
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
