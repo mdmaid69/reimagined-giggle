42af00f6f6b30ef26154ec3858842f0cb3c2d10b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:50:44 +0000

    sea slice later fact neither

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
