5cb1e47061d270a2632886b96e34519e081c9e00
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:29:01 +0000

    friend friend show fit warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
