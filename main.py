51787daf3de52c4b35e4f4edf47e6eea0f344238
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:16:19 +0000

    Bb mention accommodation mine watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
