c826b129a009e23973e0dd856149990908cf8b70
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:42:25 +0000

    account her burn bank nor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_power(work, time):
+        return work / time
