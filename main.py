e7ec093ce9610c960e396586de97dd2d3e55a5da
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:37:55 +0000

    apart field herself favourite comeup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_length(array):
+        return len(array)
