7e406fa971daccf03f9451f51db7dc938549aecd
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:33:15 +0000

    hard bringdown who comfortable breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def remove_duplicates(lst):
+        return list(set(lst))
