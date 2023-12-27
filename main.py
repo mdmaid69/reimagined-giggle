d0b16b9ebaad1ab1118dc6ae6f080eee17516517
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:46:50 +0000

    purpose side sure state any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
