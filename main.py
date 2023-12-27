4994b7fa3bdc5b35964b9b627a1d4e6ee7fde88e
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:17:09 +0000

    getaway last emphasis excuse stay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
