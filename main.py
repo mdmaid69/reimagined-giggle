ca6a2692497f66af7973bf7243988a384283e36c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:13:47 +0000

    past tradition spring account real

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
