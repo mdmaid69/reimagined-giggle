24551d546970a04a7ba621d8f6d77bfcf1dfee99
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:14:03 +0000

    personal reply until rain club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
