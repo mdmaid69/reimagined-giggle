5d3a8297cbf544a6377e76843ca2f26093f9159d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:37:02 +0000

    see lookaround defend trust downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def reverse_array(array):
+        array.reverse()
