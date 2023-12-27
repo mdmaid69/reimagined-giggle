f39fc33b8f29c5afd48beea874b92be0f8109927
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:26:52 +0000

    disgusting tidy zero every onto

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_index(array, item):
+        return array.index(item)
