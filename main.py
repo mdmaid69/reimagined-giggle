86b5954f7df6658e20ea11674e0cc282ccfca2d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:39:25 +0000

    ignore left information dance on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_hex(n):
+        return hex(n)
