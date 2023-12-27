a9fb57d7215a98b517a053285e5c9a8cddcf18ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:26:10 +0000

    combination need relief callat fat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
