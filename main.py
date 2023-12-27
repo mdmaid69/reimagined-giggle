35ab5bd78ac985db11d35e4370bc08803ab1b875
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:31:52 +0000

    suit show better conclusion precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
