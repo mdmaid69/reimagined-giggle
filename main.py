aa658eeda0363e631f3ae5dfbd5fff2017d9373b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:27:33 +0000

    record anywhere unlucky home top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
