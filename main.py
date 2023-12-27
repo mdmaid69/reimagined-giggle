4cf4c9e33dedc8f8e1c1be05107b05a33d9dad89
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:50:23 +0000

    protest try disapprove goup thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
