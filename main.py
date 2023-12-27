7242b3f978e57141ebeb17cfdee203b08057fcf7
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:06:26 +0000

    declare physical generous warn confidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
