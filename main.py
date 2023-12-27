1eeeeb4d419c158fd7babf210e4072f02cf8f895
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:56:22 +0000

    involve command exploration strong name

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_density(mass, volume):
-        return mass / volume
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
