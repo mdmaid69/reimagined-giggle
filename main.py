c0da7bf6b0fa8ee21b65027a2b8b43b718041881
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:19:04 +0000

    voice classroom purpose suit join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
