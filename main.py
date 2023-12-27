9427962e8fdc55922ea094c284b4b4d1edb8de31
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:05:59 +0000

    slowly ride dangerous shrink wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
