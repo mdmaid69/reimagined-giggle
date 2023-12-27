ff65bbcb2b97fd11400867b62f62babff51afcea
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:29:19 +0000

    knock game claim fill confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
