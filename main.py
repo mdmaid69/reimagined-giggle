d00219820f997a491595d788e5834b37112bbfbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:38:22 +0000

    because than big defeat full

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
