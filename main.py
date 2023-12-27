f97c2c32503326be748924dd016988ef84a6c078
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:34:26 +0000

    smile lean fork pump battery

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
