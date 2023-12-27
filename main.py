cc6093eefbb085465f166a15f1b4d9e974ada633
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:52:52 +0000

    turn substance scrape recognize study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
