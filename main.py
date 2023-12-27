fa3d63320b9cd0aa0ae380813b94e9a831018217
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:08:52 +0000

    workon wash they hungry suffer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_min(lst):
+        return min(lst)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
