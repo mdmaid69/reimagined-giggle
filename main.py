c3972c3bc9e642b7b6d97f5dadadcceadd6cee44
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:54:18 +0000

    waste willing confused impressed bear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
