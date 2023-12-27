06489ecaa752945c14cf9279bc2f0a329cc54869
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:55:31 +0000

    land chance advantage deliberate without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
 def get_array_length(array):
         return len(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
