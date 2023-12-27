410cfbfb431d9f1cfc3150a7c77619e6821e4494
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:26:23 +0000

    round demonstrate hang judge getout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import random
+print(random.randint(0, 100))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
