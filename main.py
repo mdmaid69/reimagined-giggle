a611660ec74424fb632479b98f19e795214213d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:41:33 +0000

    bend right callback begin on

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
