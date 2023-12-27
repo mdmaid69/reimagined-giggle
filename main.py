1c00103acb78a4fc41d1a0eea7ef41e80d02e0fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:05:15 +0000

    convincing control introduce love perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
