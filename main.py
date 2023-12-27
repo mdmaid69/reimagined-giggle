bf695987de31fee8a8de3875f9db1977766a9739
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:22:25 +0000

    break share passenger ignore century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def square_number(x):
+        return x**2
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
