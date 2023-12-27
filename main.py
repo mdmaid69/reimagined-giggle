c82b713563b75e1aeda645d237a9437d4543c1b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:24:17 +0000

    getaway fall Gg 570enjoyable Tt

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_speed(distance, time):
+        return distance / time
