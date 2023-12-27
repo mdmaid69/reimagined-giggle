06d948ee37911576db63014cb976af2debbe7454
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:32:02 +0000

    whole guard health drop exit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
