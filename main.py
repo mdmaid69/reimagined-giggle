1c6e92b4c1c5cbd6abaf6305af76b6485432c2e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:01:02 +0000

    boring exact remark dig really

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def add_numbers(x, y):
-        return x + y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
