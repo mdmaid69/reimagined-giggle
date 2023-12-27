c25f9ff592d37fb5719586e5a259b10bfac3ef55
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:08:37 +0000

    content record turn pause alive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+for i in range(10): print(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
