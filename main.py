2e2110ab04f7a2365ce55e785c8ed439add80091
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:15:54 +0000

    should appear love soak yourself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
