5bebc963c34370d4f89d4fde691616a1d82a3210
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:28:03 +0000

    so skill official decision noone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def convert_array_to_list(array):
+        return array.tolist()
