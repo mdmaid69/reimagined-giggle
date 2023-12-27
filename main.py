efb1a3444bbbfa84c678cc16dd4d517d6bd504d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:34:07 +0000

    secret please cover ability price

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_max(lst):
+        return max(lst)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
