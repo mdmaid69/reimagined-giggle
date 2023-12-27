949adfb3d5f70a5782cecd31791b6ff426007f76
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:55:06 +0000

    refer principle able everything recently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
