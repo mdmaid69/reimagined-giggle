16d8ef20320c886f649e1ead875577a16f0fb8ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:06:51 +0000

    sweep weak dance along likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_palindrome(s):
+        return s == s[::-1]
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
