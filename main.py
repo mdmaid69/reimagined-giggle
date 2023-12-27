4784b4cc0490f24957c2887103b879ec45cc6e84
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:15:47 +0000

    wait majority check address almost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
