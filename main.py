e07b11ad4234cb254b904d5fc09843e5ee7be0b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:56:33 +0000

    turndown aim shopping earth afterwards

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
