48e737abf3d75bef32bf0fa50f3ae82eb2c700e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:00:58 +0000

    busy feeling such band value

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
