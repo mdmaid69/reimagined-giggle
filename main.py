755bb3b8a68b9349647c5ba401d8768ad5e76f06
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:26:28 +0000

    one load rid clear group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        print(sys.version)
+  def reverse_list(lst):
+        return lst[::-1]
