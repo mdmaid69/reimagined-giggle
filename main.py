7b09b9e32b237af717c6b18d005d33c239c5b05f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:04:25 +0000

    certainly properly passage properly circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
