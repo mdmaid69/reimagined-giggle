e5177b1245dc5f2a5231196a13faf6fc272ee57e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:00:50 +0000

    suggestion month deserve contact hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
