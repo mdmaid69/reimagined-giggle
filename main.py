30c689f3dbfde409f6538c5228c9ced58342f937
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:37:30 +0000

    instrument future share negative possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
