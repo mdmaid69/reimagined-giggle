2503de453ba5bf77828910446ed4c17f714e2cb7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:58:22 +0000

    convenient expensive poison exchange trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
