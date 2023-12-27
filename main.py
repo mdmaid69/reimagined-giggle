4967df192aff73a56db306c1d0d262996a2a837d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:32:04 +0000

    difficult reserve dish sad holdon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def is_odd(n):
+        return n % 2 != 0
+def find_max(lst):
+        return max(lst)
