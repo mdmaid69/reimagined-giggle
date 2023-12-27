8e143e95236cb9a0d0ddf93bdebdb53f7b143a4d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:52:02 +0000

    strong explanation profit greeting anybody

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
