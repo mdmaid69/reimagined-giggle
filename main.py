962be1a1f606a1e5e2610174583d7ef854cf2396
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:42:54 +0000

    perfect list big slice movement

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import sys
-print(sys.version)
 import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+print(random.randint(0, 100))
+for i in range(10): print(i)
