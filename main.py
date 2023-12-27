76155c06a1df732d0699644f23cdc0a79fcb5b72
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:28:58 +0000

    brown frightened private workout organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
