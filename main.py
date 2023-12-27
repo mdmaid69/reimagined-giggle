140c3a24191249a2f542749e6ff5a827db200d0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:32:10 +0000

    through abroad independent correct memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
