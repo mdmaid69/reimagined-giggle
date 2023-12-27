8a84a4a8c291964c4b0ec66894cf299aa00748bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:21:45 +0000

    pinch age blame side pool

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
