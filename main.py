7bd91f2401e654b9c6a72f329fb6faabe1bc9dcb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:26:27 +0000

    size buy boring formal aim

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
