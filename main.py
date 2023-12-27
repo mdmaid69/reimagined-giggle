4f9f873ef7599e4a1c07cbe8aa0b8960c7edcb2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:47:51 +0000

    wash move prepare sad force

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def is_even(n):
-        return n % 2 == 0
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
