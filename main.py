9a5b3bfc35f9cd2fa7f1745114ce733dbc8f8fcb
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:12:53 +0000

    long favour art back plan

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
