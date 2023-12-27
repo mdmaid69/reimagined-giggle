3ad930087471f34581b82aee8731760496eefda2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:42 +0000

    rest sink cost consult autumn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
