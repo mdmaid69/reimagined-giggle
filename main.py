60745bf1eafb6e9feda0fe8ac746d8359ca92200
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:56:59 +0000

    fast exploration correction drawing 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
