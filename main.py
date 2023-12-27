fefe036c629aa76c754de9ce45cff364e21bd2c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:57:49 +0000

    praise dare politics offend thumb

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
