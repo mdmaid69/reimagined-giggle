143aba0bed73c2baf6a0a0c9fd52fa55eaa53edd
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:23:41 +0000

    search loose through opportunity relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
