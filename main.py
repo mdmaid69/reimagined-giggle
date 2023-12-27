9cd1a901d910dcb74b2ba5945a8121429ab84667
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:26:10 +0000

    fit stupid drive argue meeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
