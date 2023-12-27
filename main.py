a9d82bdf756310758d0788a25d2168811f88d350
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:48:08 +0000

    professional pretty practical contents rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
