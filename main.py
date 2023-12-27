4aafadc303d5664aa8a1dbd7fa10e081dde18336
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:22:35 +0000

    search loose problem print so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
