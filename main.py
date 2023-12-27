d373da33b2c8baf95cd12173635171fa078abfa2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:28:08 +0000

    set on gap blind watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
