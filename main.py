6565a0f07bbe3a8741cb510466d114b8919351d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:40:01 +0000

    hurry pretty feature mean medium

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
