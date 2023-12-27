4987ea6433ecad78f948bb0bb1e2d85c7cc5f2e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:21:10 +0000

    regularly brain flood measure touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def find_max(lst):
-        return max(lst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
