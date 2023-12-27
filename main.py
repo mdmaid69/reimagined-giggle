22ca9f2f6d69fc537e4c471e99b969ac0965d16f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:07:23 +0000

    pipe future hand half there

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
