df98b74b49c725a70b378526d048fc3488c26b16
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:04:34 +0000

    geton in else identify price

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
