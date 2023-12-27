6d791dfb5ddb20e053f604de8d2a90fec13fdfce
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:21:02 +0000

    swimming never rather lookback(on) official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
