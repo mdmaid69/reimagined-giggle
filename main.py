aab2d6f9cb99f8ee6684b3df68a8ba84cda5724e
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:42:51 +0000

    should leave unhappy relax actual

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
