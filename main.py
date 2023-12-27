532d80532fcdb44c4b70a4b38a0aabab2d469f78
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:45:51 +0000

    miss Uu have complaint dick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
