301e9c848c7c9cc42a744d170d7e010193711b62
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:04:26 +0000

    most slide area trick niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
