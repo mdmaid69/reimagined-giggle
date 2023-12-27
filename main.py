f489490da670bc6dc8a61f659029e3f44f7ae32d
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:29:18 +0000

    mess wake band thanks upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
