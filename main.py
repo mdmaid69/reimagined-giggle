2ea5d5d02a11144b0d13c412d4eed397831fe996
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:35:32 +0000

    bush shade concern ours comfort

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def remove_duplicates(lst):
+        return list(set(lst))
