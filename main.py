1fd4c96071cc0087f219583dba1b88a724c1242c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:04:27 +0000

    short crash wear try exhibition

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def count_elements(lst):
+        return len(lst)
