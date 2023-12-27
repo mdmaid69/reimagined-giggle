74486656d7d2e574353d58adcaf5ea94ee0dbf60
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:59:29 +0000

    not talk stamp dish by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
