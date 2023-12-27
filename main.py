0b59db1672c3dac84960714cae8339eacf62d729
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:52:51 +0000

    off bend position joke children

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print(sum(range(10)))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
