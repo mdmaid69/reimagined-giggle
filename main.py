6d2eb7bcb64add5793003ee1f0b426673e91fbb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:23:14 +0000

    order country port forward beginning

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
