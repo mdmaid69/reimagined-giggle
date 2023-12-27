f2a3ba715c19c3a78e08671df91ad5321a66f157
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:12:59 +0000

    every mean project through makeinto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-print("Hello, world!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
