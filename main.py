9fc179a4367094dac604a0e54619c45c9868461d
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:07:45 +0000

    keepoff break content short afternoon

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
