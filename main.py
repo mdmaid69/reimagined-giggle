d59e55ae938aa5375c5a2564b854c80258220c1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:10:54 +0000

    subject related speech secret left

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
