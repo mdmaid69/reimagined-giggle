10f455d6cd63876ef7ee5f1ff92b59b606546f42
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:10:46 +0000

    since sweep indeed course write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
