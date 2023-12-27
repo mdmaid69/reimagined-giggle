60c8b692ed5561bbeb4cc5cad13134ff6c21a1f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:37:15 +0000

    succeed bang tear exchange funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
