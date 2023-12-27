c0e14691816ef5a15a5ed4f4f3e268748e30f9a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:03:53 +0000

    down wide material why hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
