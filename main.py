04f9cf95505fe8be8443f15db00627865dfdb404
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:08:36 +0000

    comfort touch calloff unkind difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
