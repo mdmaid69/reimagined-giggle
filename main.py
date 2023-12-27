e8cba4756c65dd17581b2f24d884c7dce0a8159c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:47:30 +0000

    news interrupt letter character old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
