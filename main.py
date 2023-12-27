bc584e5b5c7f9f829bc600b483caff7b6b6f1a83
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:51:24 +0000

    this mark fold private since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
