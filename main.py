95842f10c9d8a900f0c269785c1a32ce70b7754a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:35:10 +0000

    sorry pile boil support independent

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+print(math.pi)
+for i in range(10): print(i)
