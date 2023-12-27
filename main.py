3a47e85898aba9eaa45ab7086dbe41f1bc723638
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:12:54 +0000

    later exaggerate remain lesson hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
