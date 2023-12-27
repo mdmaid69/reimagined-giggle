5f083e18a5af6867d62f950a4b2fcdbb5d991785
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:10:54 +0000

    whoever close increase precise high

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
