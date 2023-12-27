cbac834207e0ca89434b7d8ae0a7c6d8295f3423
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:31:04 +0000

    yesterday brain grateful disturb absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
