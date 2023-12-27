939587234c779c7ad7c663278ecf8abfaa23e28d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:56:06 +0000

    glass tell secret exchange steep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
