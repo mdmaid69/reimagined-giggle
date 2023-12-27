c6e7994462a1a2137fca90f7145f3476b03d0988
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:34:17 +0000

    practise famous drop plenty basis

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def convert_to_binary(n):
+        return bin(n)
