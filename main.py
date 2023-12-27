cc0bb907032f817c71ce7a2f555c24ef0975b9e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:09:21 +0000

    name regard damage definite mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
