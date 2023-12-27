1c7958f8ca463b21f134df4fc396f1390814f7bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:36:10 +0000

    interested me university discover ought

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def add_numbers(x, y):
-        return x + y
+  def count_elements(lst):
+        return len(lst)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
