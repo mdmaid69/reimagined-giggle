6a14864e50bdd15a8c45ccde45d0215b5cb47ba1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:03:56 +0000

    thumb she fresh control situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
