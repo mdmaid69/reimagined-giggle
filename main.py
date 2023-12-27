4f87e22fb4a03a5f91f5e36b6d91dfaa165cf446
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:16:28 +0000

    once rule deaf compete exploration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_power(work, time):
+        return work / time
