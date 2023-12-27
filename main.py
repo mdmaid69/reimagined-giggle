929e5580216b8bdf1fceb24aecbbf763a13eabe8
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:23:04 +0000

    badly terrible piece firm address

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
