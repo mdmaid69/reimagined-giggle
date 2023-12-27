6a55c8bef39b9ead865810e8013a5eef3fae67ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:53:42 +0000

    deserve when open either level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def sort_list(lst):
+        return sorted(lst)
