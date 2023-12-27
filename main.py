944cb7747f728c893c80f5cc30c973f137388520
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:41:18 +0000

    finally watch certain moon involve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
