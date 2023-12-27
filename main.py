6b53e4ac3e907e57c57c304cb950e645cc650e3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:34:11 +0000

    giveback one instead definite else

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def remove_duplicates(lst):
+        return list(set(lst))
