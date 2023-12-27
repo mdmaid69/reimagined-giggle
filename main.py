5b63752f5203d3331f49025967ebc5e9020c048a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:57:09 +0000

    off comeoff 569enjoy please anybody

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
