f8a956a85cb81bdb6ee97c6760313ff3650bda7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:02:08 +0000

    anything benefit study rest plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
