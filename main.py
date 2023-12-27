7cc8d103f6bb8954b66ade07066ff5be06dd4317
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:54:08 +0000

    she rise ring cruel sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
