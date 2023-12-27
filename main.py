39bbb42849db7afd0386dd478400dfb599743849
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:02:26 +0000

    move wake excited niggaacademy rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
