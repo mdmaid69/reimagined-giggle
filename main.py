adb33b6023a6d03d637c84080a1677c21bcc94fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:05:54 +0000

    bad process me play unkind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import sys
-  def get_python_version():
-        return sys.version
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import collections
+def create_user_list():
+        return collections.UserList()
