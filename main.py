257b1726ff327f17fc176d5908e866fa24742236
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:25:12 +0000

    choose talk pronounce return thumb

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_octal(n):
+        return oct(n)
