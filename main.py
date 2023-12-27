89986d84896fc58adf2c100f75b38cc7780553cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:35:46 +0000

    murder stir tragedy turnoff eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
