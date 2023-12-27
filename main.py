7608c6e3784aa6c1accc6b30fc92e41412aec94a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:53:07 +0000

    light draw remain deep store

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+print(math.pi)
