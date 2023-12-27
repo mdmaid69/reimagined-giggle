5664f6aa43c09cdfd06bf4f070e6f5ce041e7195
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:08:18 +0000

    however closed protection confused famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
