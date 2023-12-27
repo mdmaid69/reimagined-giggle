e9fd2db6610ad84355a1383641cabb7f245639f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:57:17 +0000

    myself everywhere hide please ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
