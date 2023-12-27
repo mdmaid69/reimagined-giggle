604ab402aa619659747ebd50dd4743f234c7448f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:01:00 +0000

    rely judge pretty sentence gentle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
