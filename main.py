dcc478142f09f1447bbd3402407495dc7b97ade7
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:42:15 +0000

    cruel film similar always miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
