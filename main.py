2d5ac438316b4af126a697dd2b6f7ee30f252933
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:03:18 +0000

    yet uponon face round once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
