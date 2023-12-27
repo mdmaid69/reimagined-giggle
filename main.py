9db884345c8fbce4e237619b0e1e3eb1dbbc3021
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:18:09 +0000

    relative unkind strange correct train

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
