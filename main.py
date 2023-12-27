e606ae799eb2cfe284e57d25267875406e54b546
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:34:26 +0000

    must fetch impression probably matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_area_rectangle(l, w):
+        return l * w
