2c10e2e113d4dd61c9b295706c4193602c56be2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:12:16 +0000

    paper mass do even result

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_speed(distance, time):
+        return distance / time
