f4349dcbd9fa80737f5d9992664b468e7b210606
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:20:55 +0000

    compete frame accident duty develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
