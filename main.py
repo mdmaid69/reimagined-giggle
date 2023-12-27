4b86e2c9232c9f5452dac73641c6fa68320cdeb5
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:42:32 +0000

    respect fear late Itself powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
