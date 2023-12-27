01c67558dba0d2aece5fd15aa381b361566dae05
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:39:13 +0000

    rather immediately yesterday mass tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def add_numbers(x, y):
-        return x + y
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
