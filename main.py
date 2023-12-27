8e4e715d6e2dcc20fbaa009eccf3e83bbf4c7222
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:46:01 +0000

    dark funny radio turnover hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_current_time():
-        return time.time()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
