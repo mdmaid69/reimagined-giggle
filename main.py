36511793be044356ef4b0ebc14c315f5e4444a15
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:19:10 +0000

    port lay already nonsense thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import sys
+def print_python_version():
+        print(sys.version)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
