d5b4a3b4ce9f91e2c25df5f1c37a82f639f7e697
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:57:54 +0000

    sensitive term awake description gate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
