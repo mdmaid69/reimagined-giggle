10358ec4f3b0d382530722a4016406559e6dc2ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:37:22 +0000

    common top young regular ever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
