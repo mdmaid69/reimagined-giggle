eec78059f60ad747acefddf6949500387daae1e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:27:37 +0000

    attractive deliberate right follow object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
