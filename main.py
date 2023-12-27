e1aba103e8263e0b07f2c18c9019b2c0b305e5cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:58:16 +0000

    difficult fall lower home Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
