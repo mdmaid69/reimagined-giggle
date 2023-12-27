be28392aa6aa78caea84079db6816665c7c0f4c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:45:33 +0000

    statement stick sometimes laugh tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
