88e1b70942a76a1fc9af82eed96085b9bbaaae0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:07:04 +0000

    quarter original excuse keepup harm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-for i in range(5):
-        print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
