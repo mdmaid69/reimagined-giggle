a2b372bbf602e5468b7580b1a5b96c9dbb456324
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:23:59 +0000

    deliberately regret pour there improve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
