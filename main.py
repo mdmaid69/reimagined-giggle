3c38d6d4df297dac9e185cd24047d86f8efb1bd2
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:45:09 +0000

    wash scared can development forever

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
