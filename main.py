bbdb4101d90cea2876c0f8a4614bf929f115b4d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:44:42 +0000

    mother environment miss perfect unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
