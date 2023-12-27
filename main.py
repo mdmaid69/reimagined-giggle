c0f051eeb29006e6db19592ac824ac4a18b94f6a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:32:32 +0000

    sight owe run house feed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
