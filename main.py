44017d349063ff43b2ac9a014be3ac4f232d8e90
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:07:19 +0000

    agree soon turn action behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def sort_list(lst):
+        return sorted(lst)
