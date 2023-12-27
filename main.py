d4f39daae82d38daf6855eb0b63a66e94f372580
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:18:11 +0000

    limit under us noisy exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
