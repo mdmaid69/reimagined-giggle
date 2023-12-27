b8732d5a3976ce50890eb5b80e82e2dd22d9cd45
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:49:56 +0000

    together famous beautiful understand tired

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
