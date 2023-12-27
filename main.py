82fbba222e80164a9d8122ae3317d9d1bcce4f39
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:05:02 +0000

    software necessary equipment shout no

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def append_to_array(array, item):
+        array.append(item)
+  def multiply_numbers(x, y):
+        return x * y
