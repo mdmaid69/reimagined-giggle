0bfc1d9a499e835bcc40c9c784b189796b854f54
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:08:22 +0000

    computer before send degree use

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
