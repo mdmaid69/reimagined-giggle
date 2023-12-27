f784677e12a363b385a72eae70a3b63ab51b4992
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:17:10 +0000

    description takeback low smooth repeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def multiply_numbers(x, y):
+        return x * y
