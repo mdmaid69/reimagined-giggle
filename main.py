80112c4d6e23b290f0a9e47dd473c686149fb417
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:34:47 +0000

    occasional trick grateful both repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
