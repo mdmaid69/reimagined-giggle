b9de45dbe7c5ffc608cbc6d6b55d8d2edf885ea7
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:31:16 +0000

    lesson damp estimate class month

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        print(sys.version)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
