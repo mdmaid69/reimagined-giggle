19340c8f536805a103f826ee9e91c24c381bc1c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:06:19 +0000

    way avoid image learn bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def convert_to_hex(n):
+        return hex(n)
