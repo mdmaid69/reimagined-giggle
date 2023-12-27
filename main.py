1dce06719b944c141d4ea8d1c41b73dace8c2c0f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:59:49 +0000

    attractive actual there hand we

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+for i in range(10): print(i)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
