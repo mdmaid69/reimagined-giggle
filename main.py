eaeeaeb292a8c7e124f3b78298d16e7f31c6e591
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:14:59 +0000

    impressive run keepaway sleep pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
