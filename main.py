4f85b692beffeaf96144b3edfff76ea4fa30395d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:06:25 +0000

    next equipment command ago experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_queue():
+        return collections.deque()
