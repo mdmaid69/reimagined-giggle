1aa2ac177855394e70e7249b83cdf9063f5cc817
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:54:19 +0000

    destroy cry further mainly test

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def delete_file(file_name):
+        os.remove(file_name)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
