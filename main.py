c3b0af4cb068f622f13f0ff486ed14a827923dd7
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:17:29 +0000

    vanish content fair on explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
