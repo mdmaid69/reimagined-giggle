5b673c0195bf7d12789caac91c04705d555a264e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:36:18 +0000

    mess software interested gas treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
