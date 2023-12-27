dc569e92d2ab7c23657cff14addfc9aac1d0f1f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:37:58 +0000

    familiar worst crime ticket reply

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
