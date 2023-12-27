5dcbfc0a469cdb6f96dbd8e9a68bcabcb580dbe9
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:42:59 +0000

    people somebody fear communicate congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
