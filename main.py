20339ab3cd6cf5126c29783a45ccc5b30a939dc2
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:07:26 +0000

    swallow mystery punish excited enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
