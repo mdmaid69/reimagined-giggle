0037b3ed4f13b576b61fca24c1397c0299d43460
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:35:46 +0000

    get 571enough vanish discovery so

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+x = 10
+y = 20
+print("Sum:", x + y)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
