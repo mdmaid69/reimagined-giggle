5b4228e772bde7c9abc5c539b89d49b1fed342d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:01:58 +0000

    might cut bound replace 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def convert_to_binary(n):
-        return bin(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
