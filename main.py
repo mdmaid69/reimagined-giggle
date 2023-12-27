bbffa2251123095026017058849f7e250c9a9830
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:19:37 +0000

    message play frightened relationship their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
