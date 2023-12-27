b4b828c67d505073fde261084b3cf6217b948a78
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:44:56 +0000

    comfort generous meeting object aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
