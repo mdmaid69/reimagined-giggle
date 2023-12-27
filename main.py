d4e19439e53d641174f8f7bacc094abf4bb140a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:28:29 +0000

    would keep week during pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
