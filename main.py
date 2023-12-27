8cec3a606e9edbf23b709d5047edc8bbb65f3432
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:49:06 +0000

    free window cause embarrassing simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
