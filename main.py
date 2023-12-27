0a7cd98f751fa54bb942fecc40665ec2abdc482e
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:05:13 +0000

    turnover steady risk snow help

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
-print(time.time())
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def count_elements(lst):
+        return len(lst)
