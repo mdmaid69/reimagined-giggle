c3ba586318ff120ee0f0b46fa4454205cdbe0582
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:47:53 +0000

    switch raise improve problem unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def reverse_array(array):
+        array.reverse()
