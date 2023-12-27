5d39b257a5147a519a9ee2972dec6a1191fddf09
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:00:20 +0000

    born turnout cross trousers long

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
