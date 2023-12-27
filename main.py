503ca310d4b92933053c94e9b2b5eda2b62a2cf2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:54:11 +0000

    by tired university main teach

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def convert_to_octal(n):
+        return oct(n)
+def count_characters(sentence):
+        return len(sentence)
