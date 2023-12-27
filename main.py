ae395fd0c535f8fba8f91f98bacf75a70ffb494f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:15:21 +0000

    real behind breakup interest would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
