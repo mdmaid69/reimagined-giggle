f95500dcc758b61d1cff8cfb9a68df8e46f431dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:18:00 +0000

    fresh study recognize gap damage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def reverse_array(array):
-        array.reverse()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
