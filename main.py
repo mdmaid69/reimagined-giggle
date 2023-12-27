f1f0fb037cc937acd22ed1d5eb82038efee6da7d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:07:04 +0000

    evening makeinto behave description best

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import sys
+print(sys.version)
