eb67c804bb4667c164228505e6707e90e89dea5a
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:02:18 +0000

    history natural light hope spend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
