e54d450ff15e03e12256367c90dc978c388aca09
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:40:59 +0000

    cope gather shine petrol round

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
