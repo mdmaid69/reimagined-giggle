88ff8a38cc09a497cb4adf40bcc2b4e5c2c94367
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:10:27 +0000

    unkind pleasant sink wander climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
