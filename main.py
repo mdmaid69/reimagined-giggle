cfcead17f483e1be2005946f5b57b4240dc708c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:25:51 +0000

    split until allow flood unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_item(array, i):
+        return array[i]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
