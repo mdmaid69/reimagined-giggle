bae39f715e46a9c9d06d6322fbcef1f87bc60fd5
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:05:12 +0000

    meanwhile indeed position concern sometimes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
