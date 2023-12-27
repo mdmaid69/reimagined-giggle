3479e97c0431fc8f42f9c4e6ca3901c56972f1c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:59:31 +0000

    breakin environment reduce blow part

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
