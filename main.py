3e2cfc983fb142317a424036a9799fee13ae31b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:18:53 +0000

    wave lean impressed herself unusual

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
