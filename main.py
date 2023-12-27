9e613b753439e2778486667e64e07b2fde77c9bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:14:27 +0000

    contrast Ii shock tongue around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_hex(n):
+        return hex(n)
