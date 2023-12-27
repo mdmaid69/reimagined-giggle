ef9b8a46004c847e53921cccd118158293aa5eb0
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:49:25 +0000

    occur level character rule trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import os
+print(os.getcwd())
