93afd1a8266b39d71b07d041919c206f6c44b51e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:05:40 +0000

    future agree safety workon provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
