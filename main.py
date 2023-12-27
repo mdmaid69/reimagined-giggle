1effbc40a435ec0c4cd26349c5ea288acdb5c69e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:20:14 +0000

    cook declaration creature wealth instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
