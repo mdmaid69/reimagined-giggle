6746c229475ed6d8711b19a029a8d0855a984f04
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:43:38 +0000

    gate arrive string serious most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
