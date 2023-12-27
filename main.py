9c907126dce21d230dafd8d2c434dd5de6116a5f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:10:50 +0000

    criticism completely image watch deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
