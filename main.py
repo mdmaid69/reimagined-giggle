f525ea8a656d4ce03e8859faad324dab90d20485
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:05:44 +0000

    brief dig cover suggestion tone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
