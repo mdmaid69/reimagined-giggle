c5feb368619e52dc5f5888f71d9ff7381a60b812
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:53:38 +0000

    use past bar show mouth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
