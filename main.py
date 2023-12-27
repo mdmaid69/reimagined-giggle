07bc7c376c917c90fa482e7bab1af89985071a41
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:06:48 +0000

    age minority closed bridge explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
