e7c963c66b6b93c5de8b133a465a6ed2a5a7e8db
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:43:40 +0000

    with slowly insist shame guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
