86b440a616435371af3aad42d8be1aa65c905af1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:44:42 +0000

    notice family degree mainly interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
