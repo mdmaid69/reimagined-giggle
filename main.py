1fe5fe0ab3070596b8e09a1ee2c54fa31fc99355
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:06:11 +0000

    considering bring depend organization use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
