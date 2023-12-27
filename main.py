ffd57309ed69379df89c50ca2225afdd188c9ec6
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:53:08 +0000

    public correction getaway lot arrival

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+print(math.pi)
