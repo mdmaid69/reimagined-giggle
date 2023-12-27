e28d85c2529f43a8e3cacb0bcc1f240dc326228d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:30:01 +0000

    mail breakout task experiment could

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
