74b4dffe2da8319b0a7a8f22fbd6c690e1c0d985
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:21:59 +0000

    fault race admit in afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_min(lst):
+        return min(lst)
+import sys
+def exit_program():
+        sys.exit()
