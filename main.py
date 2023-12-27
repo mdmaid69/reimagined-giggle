a9eed4526d0274855425ddd2ab279a370b8253cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:15:26 +0000

    keep high influence slide sense

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
