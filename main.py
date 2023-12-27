a67120d00a91434d42a8341c9b559765bfc8a9e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:20:18 +0000

    concern excited chase extremely argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
