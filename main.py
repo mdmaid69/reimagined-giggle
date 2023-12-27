8d2dbdd9ec76d67d91e6a8619d9228f4d9eb7cf5
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:46:58 +0000

    join charge recognize sleep cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
