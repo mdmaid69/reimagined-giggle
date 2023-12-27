fa038c5687bd7649093ace57d6671bb28c8e05d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:11:01 +0000

    crash brown mouth unlikely everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
