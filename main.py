ff58e1de7ffc876ef7c5138385abab525e7cd2b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:19:14 +0000

    sometimes window no where wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
