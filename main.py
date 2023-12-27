5c59cbf06749fbddf646700bf30820363c7ef894
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:30:40 +0000

    might criticism mix unpleasant love

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+print("Hello, world!")
