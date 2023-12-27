161ff05c603be31d71341113ea2d1eebc7fb7bfd
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:56:40 +0000

    ear Ii dangerous fence finger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
