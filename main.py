b4e4d8933c52f0862c269bc6008b952a6baa4c18
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:01:30 +0000

    pattern compete different above contents

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
