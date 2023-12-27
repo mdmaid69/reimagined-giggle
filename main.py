f78f4e377d77ea50c40cde93a4144b094461731b
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:40:55 +0000

    in 569enjoy nowhere face work

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
