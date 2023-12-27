c103b561fe93a1a37f58d403d881b3262cb97707
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:50:49 +0000

    arm test middle but goaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
