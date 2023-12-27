b82bc9d96aaf510a016bd2365248b81e616568c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:58:48 +0000

    complete support hurt tense disturb

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def square_number(x):
-        return x**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
