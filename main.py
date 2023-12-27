4096bccd3a1a20ecc99dd3fa9557e0e4f5a0fb81
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:40:18 +0000

    everybody perfect find dress balance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+for i in range(5):
+        print(i)
