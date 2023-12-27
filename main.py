01c2e24e5efde50cd1bbbb8a46e9df9ffbe66536
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:56:30 +0000

    home forgive wave embarrassed children

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
