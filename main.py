f12d531b66ac27af3a5b4a4cb13d63525b7b1a21
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:04:49 +0000

    world concentration refuse distance narrow

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
