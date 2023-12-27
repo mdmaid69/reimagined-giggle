e3e0fc1814af14dc73109d9592808f7ad9de072e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:38:02 +0000

    voice off possibility custom himself

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import collections
-def create_user_string():
-        return collections.UserString()
+def count_characters(sentence):
+        return len(sentence)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
