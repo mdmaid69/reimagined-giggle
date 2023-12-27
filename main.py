1cfd4b0f7af43baca0223a297c3d3efffc21832f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:12:41 +0000

    immediately rubbish report winter sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
