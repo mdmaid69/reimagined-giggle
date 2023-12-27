5172e43a0c236c5959d324a2931b8c76f5a617f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:08:34 +0000

    match available shoot low to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_error_function(x):
+        return math.erf(x)
+import collections
+def create_stack():
+        return collections.deque()
