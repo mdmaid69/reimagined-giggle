a73e784e817ca4deaf496041f563c7258bb5e17b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:46:46 +0000

    neat account software regularly body

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_future_value(principal, rate, time):
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
