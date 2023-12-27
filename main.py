a9d4a1dbe7ce121c07d874956b36e1ba04adf913
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:33:22 +0000

    Dd shut about transport Vv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def convert_to_octal(n):
-        return oct(n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
