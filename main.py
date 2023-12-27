c692046e40ffb2f9d9f100aae9bf6261fc6691c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:45:02 +0000

    question old thin become season

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
