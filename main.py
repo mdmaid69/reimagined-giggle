0aa80d61304dc6b9d0f823a2989840df4be9fd0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:07:20 +0000

    willing responsible none risk turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
