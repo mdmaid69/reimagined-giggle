56da4cf08f34b720339ae33144e0b1b7f2d25a27
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:12:39 +0000

    problem respect pleasant fall deliberate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+print([x**2 for x in range(10)])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
