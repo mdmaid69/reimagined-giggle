20a7a5b8c4fc7280238f136dfbbc27c7780dba40
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:03:41 +0000

    ticket form moment clear healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
