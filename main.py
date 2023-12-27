b05e6d58e652e1d9e10b4787db404c8f38118603
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:58:43 +0000

    where will use successful than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
