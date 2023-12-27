725d01c31ce3796989d74421a3c6ace0de8f83d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:36:52 +0000

    tread against scared sound fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        return sys.version
