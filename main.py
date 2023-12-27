05c115f40c744915cc1a74cfa679cc6d5ad2efb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:26:25 +0000

    shock spin operation study central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
