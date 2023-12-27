9fa86d329bd050e290b00b77945f10cc48f86de9
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:07:53 +0000

    capital rough heat damage cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
