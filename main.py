5452eb30755d21478aee70518b9c113972ccc3af
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:39:01 +0000

    murder scrape need every rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
