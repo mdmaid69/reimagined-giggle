7d91a82a24598e3b48fad68078857e085b27e57f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:13:15 +0000

    present once visit blue miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
+import getpass
+def get_username():
+        return getpass.getuser()
