fa2b72a4fe5501f2f84117330feb49b4b961d4e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:00:58 +0000

    communicate dare price part towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-print(os.getcwd())
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def multiply_numbers(x, y):
+        return x * y
