9abed104bf61ebd195200ca8a4979948b7ac93da
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:09:10 +0000

    how entrance settle mother spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
