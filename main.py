18760a66c9a1f44f118f9f1ba6e8b0c8be612e9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:21:07 +0000

    sort see imagination ready lookup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
