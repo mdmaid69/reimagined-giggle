cde7789e420acc7c5b97ed46e6b4a3265acccd08
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:29:30 +0000

    patience prove beat Ww begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
