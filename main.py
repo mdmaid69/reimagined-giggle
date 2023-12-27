bcf71aff6764dcf648ced53d292e280b4c70635d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:40:00 +0000

    exist hit include present any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
