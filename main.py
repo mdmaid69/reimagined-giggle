7d857818fa3a5500deb7abb63a1a4cc529aadf21
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:32:35 +0000

    partner love crisis trade money

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
