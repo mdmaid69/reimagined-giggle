5200a7fb543ace0706a76085ab9ecd727df40f02
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:21:06 +0000

    pretend deserve before handle slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
