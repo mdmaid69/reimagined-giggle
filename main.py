fcbf4cb3abbb5c5d7f77994f6779f357da3729f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:29:35 +0000

    pause market city petrol piece

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def clear_array(array):
+        array *= 0
