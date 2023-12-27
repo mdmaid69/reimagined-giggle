fffe1f5c1085aba6769b53632a0aa21384b90bea
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:51:22 +0000

    fortunately many soon accept family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
