79f229c6991ead6dce5aab4276fc6917fdf6cfaa
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:58:08 +0000

    must duty offer summer free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def subtract_numbers(x, y):
-        return x - y
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+print(math.pi)
