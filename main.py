74734d1b80e5aa49f799d5b94593a5602074521e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:51:06 +0000

    lock colour if medium married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_length(array):
-        return len(array)
+print("Union:", set(list1) | set(list2))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
