6180f5348fa1b0064339f4935447a627ca821e9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:50:44 +0000

    bringdown dangerous together no old

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
