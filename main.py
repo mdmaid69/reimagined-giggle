37dc2b973ec4979877c477844121de19a53cfa27
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:41:54 +0000

    steam die steep leg thanks

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
