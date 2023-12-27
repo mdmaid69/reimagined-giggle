8f5e6b2b92b2c1091360bae4ae4d588de1092554
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:10 +0000

    study you progress emphasis measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
