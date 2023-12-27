48efff9b4f9ed97cba284d144acf4c5f36f47494
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:45:49 +0000

    speak serious know provide hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
