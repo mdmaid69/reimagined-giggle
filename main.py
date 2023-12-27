34c95038439347d9a4c46f8e0cd6c63a0a45a4f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:49:11 +0000

    claim Zz creature ours pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
