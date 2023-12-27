fbb5128242abb8e65887902f9d5efb9c085f70fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:04:10 +0000

    flight piece mixture circumstance should

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
