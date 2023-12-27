49ca669c31cda4f18c0a2f4f0bd4234cf72eb9d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:19:05 +0000

    light example wealth unable view

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
