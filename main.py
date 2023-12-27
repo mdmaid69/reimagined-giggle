2afff2de7433c15ee56c7cbdd3aff9f609eb5e50
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:58:16 +0000

    bit comfortable let at progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
