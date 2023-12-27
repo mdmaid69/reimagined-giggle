8e7472f9c259ef5b07237cfca3138bcd42102732
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:24:56 +0000

    fault soon trip keepfrom mood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
