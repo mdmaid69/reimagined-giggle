0febc65286b12461c1f166cf05ba45c00be5f1bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:36:49 +0000

    drink once so opposite answer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
