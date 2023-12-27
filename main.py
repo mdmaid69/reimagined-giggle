4bbc0ca2501d28109836b80340e4500d0f02e4e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:01:09 +0000

    blind lookup usual use suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
