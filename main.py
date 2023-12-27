3635ca51d265dcdadfadf54ae281ef7aed392e66
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:10:36 +0000

    square slide bang related left

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
