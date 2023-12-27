2a0dfd40997c0e5ba997fae66ab2f43425f192bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:02:18 +0000

    soon hole should crash beside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
