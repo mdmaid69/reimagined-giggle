d5408362ecd030ab54a9aae689b5bcb80f27fcc1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:20:31 +0000

    shrink true spoil more approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
