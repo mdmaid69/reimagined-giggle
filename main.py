1641b5d280f140e51974eea0163be7766af04a3d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:23:49 +0000

    appeal stone live dance damage

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
