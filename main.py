eba93fc525c353aab66563431cde2e6d7c1f47c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:35:20 +0000

    under read answer beat church

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
