3c631b847c77fee7b10d44d5ee3fb937b4888eb7
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:03:57 +0000

    emotion later plenty problem back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
