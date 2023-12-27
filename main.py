58c4ca9756dc90e5607aef6011e14931f9e4ced5
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:42:49 +0000

    moon convince within shut ocean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
