d43a57806225ac8f9ba94ec8053cc069def84c79
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:40:09 +0000

    pay turnoff lower us the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
