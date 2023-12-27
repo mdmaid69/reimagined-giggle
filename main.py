b82ede5e06357e0957e617e5e7600c852b5a493b
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:24:29 +0000

    rob neck clean remark stranger

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_as_set(array):
-        return set(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
