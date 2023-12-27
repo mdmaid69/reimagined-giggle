b823402307b053ee979a04880b270c3a28a37c3e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:29:57 +0000

    long willing activity waste want

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
