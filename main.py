584064eae28964cba7ec9b197e832b4113117352
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:58:01 +0000

    apologizeapologise land hand smell speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
