9076aef48c35d7cacf55ab8591becbbf18f7cad6
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:26:19 +0000

    over mind letter reliable loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
   def get_file_access_time(file_name):
         return os.path.getatime(file_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
