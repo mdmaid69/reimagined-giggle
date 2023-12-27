2e921a5e7ab42f8fedc7e44fc58dd3df29b827c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:20:00 +0000

    advice thank giveback trade constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
