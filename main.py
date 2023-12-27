af074dbc03459fe82333a65d93f120679720e4b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:49:03 +0000

    considering personal store convenient discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
   import sqlite3
   def close_database_connection(connection):
         connection.close()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
