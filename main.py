726673b0cc4bed4476a7c763c94d04cdf70960e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:16:31 +0000

    shall frighten cross tense next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
