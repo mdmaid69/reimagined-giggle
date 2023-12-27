a9a379f1beb62f9cbb46b3efade02502c08b7250
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:38:16 +0000

    turnover official scrape shoot unlucky

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
