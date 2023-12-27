0cb2bf31ffbf160db1f371a4b48a807581946350
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:11:46 +0000

    nigga clean also whichever original

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
