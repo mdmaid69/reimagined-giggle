9aa0497a5e5e3b97045a9e813c1d480dd1e15850
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:56:21 +0000

    or object sick stand fit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
