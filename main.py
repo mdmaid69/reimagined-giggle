467bbb291d15be7e085ed7f19e513ef6e93b25f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:47:19 +0000

    takeoff afternoon past careful regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import multiprocessing
 def get_cpu_count():
         return multiprocessing.cpu_count()
