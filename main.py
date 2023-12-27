d6c2c78adf59bd37381385e6285f2787f2550589
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:09:50 +0000

    responsibility anywayanyhow stare I close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
