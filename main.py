a57c86392b74ea3cc67320d69276e20205c33824
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:58:37 +0000

    race spread practise on copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def split_path(path):
-        return os.path.split(path)
