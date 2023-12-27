8bcf3a1a39fa42b0dfdfbda94f233edd14c0cb8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:33:28 +0000

    set brush slice street age

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-print(os.getcwd())
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
