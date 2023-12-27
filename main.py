00f90bf8fec33e95c7f3aa2d63e75d731252b0df
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:28:27 +0000

    criticize possibly quickly bet last

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
