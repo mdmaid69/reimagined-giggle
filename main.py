0bee0df718854681facd44ab784228cc4ed5e8c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:44:54 +0000

    skin everyone tax interest buy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def reverse_string(s):
+        return s[::-1]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
