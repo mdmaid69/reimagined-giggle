a7707af295fe74ad13af32af1553d60ced686c1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:21:00 +0000

    afford low ready guess value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def multiply_numbers(x, y):
+        return x * y
