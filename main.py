bc53587471ec939cb8fdde4d487e04158dc8d1e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:54:50 +0000

    name breath Ww over attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_as_bool(array):
+        return bool(array)
