4633a76cecbd53c1156f09cc5781fe6ef4c3e91c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:00:09 +0000

    list thank now fill since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
