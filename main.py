832865403e874db1fe48c010ad0eae8b89e6adca
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:16:19 +0000

    rule near gentle really development

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
