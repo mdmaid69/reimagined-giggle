bd8fa9a05b8715e71548c7856e1577de1aa47e5b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:16:25 +0000

    mail surprised 179bite whom tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
