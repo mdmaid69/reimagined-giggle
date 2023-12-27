c0e2fb93e04c009d5741bfcca04c241925f50f9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:02:39 +0000

    whether commit pretty little stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
