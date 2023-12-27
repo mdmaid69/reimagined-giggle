293399c4beb15c4eb2a14f050a35a8b9950ded1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:49:34 +0000

    complaint shopping heart ashamed turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
