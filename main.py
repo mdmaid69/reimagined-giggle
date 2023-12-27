6f34510490339d68b0c454575515ee14e4079da2
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:16:10 +0000

    home funny fashion because space

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_speed(distance, time):
+        return distance / time
