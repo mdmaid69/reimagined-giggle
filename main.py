a64fcfb04bf99253104fab53a7483596a4296c89
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:28:25 +0000

    bringdown surface equal children straight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def convert_to_octal(n):
+        return oct(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
