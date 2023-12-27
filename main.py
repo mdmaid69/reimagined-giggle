6f986e4a628677d83737371d1b75ac11c70bd34a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:37:06 +0000

    store bar rescue so evening

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def cube_number(x):
-        return x**3
+  def add_numbers(x, y):
+        return x + y
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
