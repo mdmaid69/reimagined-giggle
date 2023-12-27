571e17d0c70e17183984e066ef089829adc01ec4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:53:20 +0000

    party interested far turnoff open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+text = "Hello, world!"
+print("Characters:", len(text))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
