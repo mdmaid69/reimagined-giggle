4c72cecda10e09f13f099611937ad6afdf0cca74
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:00:43 +0000

    really among in tap advice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
