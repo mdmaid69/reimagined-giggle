7fadf164eb0ea890b6d00effa5411d2c6d5178f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:02:30 +0000

    mess when probably sure still

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
