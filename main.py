5500552928f2cd8ade68284eba90d1d58b8c3e3d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:04:48 +0000

    like convinced company comein Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def reverse_array(array):
+        array.reverse()
