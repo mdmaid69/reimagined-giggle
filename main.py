b6b96efd089e6f8cad1f15197d6a3c7117a1d3c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:33:33 +0000

    frightened size drink control form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
