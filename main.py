154ed584cd7381e7c794bef065fb196bad0ace73
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:29:48 +0000

    lucky wall rare sea one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
-import array
-def set_array_item(array, i, item):
-        array[i] = item
