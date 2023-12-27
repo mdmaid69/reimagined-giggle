b1d3072ce5926470c819b567579a713ee8ce9868
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:06:57 +0000

    score keen rent be last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_power(work, time):
+        return work / time
+import time
+def get_current_time():
+        return time.time()
