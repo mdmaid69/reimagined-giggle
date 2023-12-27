82b9d8af3df71f7a2224cc5c9bb3edcdd2e2983c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:21:11 +0000

    turnback himself famous head generally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
