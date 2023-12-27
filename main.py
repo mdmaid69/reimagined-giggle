0cf3819638e7feed405599521f3f543fad26dc3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:31:09 +0000

    getout used side share sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
