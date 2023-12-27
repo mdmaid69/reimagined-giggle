19fb387366ee1d5ebf0b75ee6c79eaaf83148ef9
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:46:06 +0000

    action throw stand exact stranger

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def add_numbers(x, y):
-        return x + y
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_itemsize(array):
+        return array.itemsize
