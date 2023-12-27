68e585d18cabfd3f9a2b766cc4f19839e2136e54
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:20:42 +0000

    everybody process peace position turnup

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
