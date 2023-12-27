6a478f9a042c1983bd591e383edeeefbcf8f4ef7
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:35:49 +0000

    reply search lookafter rare finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
