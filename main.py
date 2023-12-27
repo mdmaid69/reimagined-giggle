8ebf67306f26ff578f28abfbe142bccca72cd6aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:40:06 +0000

    explanation admire history place never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import datetime
+print(datetime.datetime.now())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
