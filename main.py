66603f89f77708733c2a5d9e83ee6a733d74ff22
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:44:49 +0000

    this contain comeover office clean

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
