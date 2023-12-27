8c3dec8c5922a573b054133e1abbba57b9354dbb
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:16:07 +0000

    pull table expression block Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
