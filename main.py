4dfbc1452e0292e016b9f8854cbcfa1dc28727d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:09:57 +0000

    hope lonely battery hard computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
