b40a00cfd878a12258bcaf6058ae33650d8554d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:25:00 +0000

    meaning several match embarrassing simple

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
