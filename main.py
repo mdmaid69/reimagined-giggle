0dbcc8a24493e74080efeb062fb4d7857828a48d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:10:15 +0000

    result hit tool beautiful interest

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_user_list():
-        return collections.UserList()
+import platform
+def get_python_version():
+        return platform.python_version()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
