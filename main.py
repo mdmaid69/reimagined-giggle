a1ef886769c5f5ab3332ebb4f078e0008a898e80
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:53:20 +0000

    appearance colour thinkof splash taste

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+print(os.getcwd())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
