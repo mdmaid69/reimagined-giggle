7f1e4d6416765b72d78357f9a52515c5a6748823
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:52:24 +0000

    name recent never you normal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
