933965ca31487c4eaf3ce226116a13c922b1fe2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:05:58 +0000

    check method brown arrival shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
