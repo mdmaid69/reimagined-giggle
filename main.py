e0392ee5e657d4d6ffbd9c90ce2124e69eddb1c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:01:04 +0000

    tongue profit ill approval Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
