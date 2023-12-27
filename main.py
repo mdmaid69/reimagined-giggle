b75b12067270ebff4a5f488eb3b0afafa291e249
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:16:18 +0000

    insist depend takeaway forgive attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
