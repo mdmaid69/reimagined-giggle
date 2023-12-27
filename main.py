f8dc3d08494fd5b53b41669fc162115492dc7cc4
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:16:33 +0000

    once limit card keepfrom nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
