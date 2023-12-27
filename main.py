2b5e7f44e978763fa60b34f264bce8b405504f78
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:41:27 +0000

    cum year during row normal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
