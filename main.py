c3ff7840721f7e444d5e446c5206739b78beecb5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:20:08 +0000

    middle early scale shall ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
