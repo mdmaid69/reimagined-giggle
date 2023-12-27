a423bf2808f0423d17761830d94f3074a158d249
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:06:17 +0000

    hear practice relative whoever secret

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import time
+print(time.time())
