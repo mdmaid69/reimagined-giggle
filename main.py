05e826ac8ad3393cdb3578a87d81f30b6683d63d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:07:25 +0000

    estimate side cheat fly sure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
+import collections
+def create_stack():
+        return collections.deque()
