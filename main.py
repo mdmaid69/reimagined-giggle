de1c03719ae064721321dfd6a7173b82dbedae3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:51:56 +0000

    school middle pen fit stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
