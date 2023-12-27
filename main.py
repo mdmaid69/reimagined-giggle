35ef6c537661ff6ddcdeab2f0d3fdaa1c3d1e72c
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:21:39 +0000

    transparent one operation official pain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import numpy as np
+print(np.array([1, 2, 3]))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
