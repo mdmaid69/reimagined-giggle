b21e46ed2a7e74210fc1508b31bf470d0388f40a
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:09:17 +0000

    preparation thanks sense shut set

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
