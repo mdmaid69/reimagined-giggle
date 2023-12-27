e894600367d30fee7f936c18251570b1cba78635
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:01:57 +0000

    over connection swing forget complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
