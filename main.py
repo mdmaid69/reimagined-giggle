cfab1c2f14bef54b43c918fdb16550ccfde603dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:21:49 +0000

    sound under fasten art it

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
