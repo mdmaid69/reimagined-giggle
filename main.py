e8c2a6cee818f6ec1cd381e1b8806e3bbea0ca6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:17:40 +0000

    substance although marry pause treat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
