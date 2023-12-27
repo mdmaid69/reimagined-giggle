6e1b298ebc5ea19de14891516246a013e484fa29
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:43:47 +0000

    mail mind unusual climb minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
