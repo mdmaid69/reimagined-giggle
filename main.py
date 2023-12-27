8ba173f67fcb9929a47b0a9b386ccc03ebb017ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:48:24 +0000

    goaway so let tidy Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def convert_to_binary(n):
-        return bin(n)
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
