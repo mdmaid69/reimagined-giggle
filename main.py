6d78122c56376c8402aa3d16f286adce96531674
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:55:42 +0000

    guess reserve behave shop breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_area_rectangle(l, w):
+        return l * w
