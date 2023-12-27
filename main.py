bdd91e2a983f6defbf817ef5f7e7647daa625b1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:54:41 +0000

    land priority smooth half continue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import collections
+def create_priority_queue():
+        return collections.deque()
+import os
+def change_working_directory(path):
+        os.chdir(path)
