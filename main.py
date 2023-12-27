7fd50e44b36edd582577e8a38ce020d82f815ab3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:31:27 +0000

    breathe happy opposite allow demand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def remove_duplicates(lst):
+        return list(set(lst))
