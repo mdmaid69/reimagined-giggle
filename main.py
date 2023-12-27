0deb7b5944327a19db4f24f261f5e1f2081386fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:58:30 +0000

    for suit bill final satisfied

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_length(array):
+        return len(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
