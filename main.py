54bcad9d1e5cb0e53e1c6b6336bbbeaebffdbbf3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:58:44 +0000

    exhibition face project since sure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
