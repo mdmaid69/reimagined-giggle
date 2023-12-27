fdf6bed527fbd502d2f83e37054f74cab9fff362
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:16:01 +0000

    quantity whisper only dream ignore

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
