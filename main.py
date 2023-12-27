382e70987a330ac0eb5806c91d4e80bd2e73824b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:00:19 +0000

    completely over ground what air

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_pressure(force, area):
-        return force / area
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
