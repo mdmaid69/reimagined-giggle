a5df7418b03e533ef05a1fdb0cf8acba5aad76ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:50:18 +0000

    beat part accidental comeover month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
