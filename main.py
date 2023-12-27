9559c9a0db94f1567afcc78df361ebeadc9b836d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:03:46 +0000

    voice suit hospital know influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def reverse_string(s):
-        return s[::-1]
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
