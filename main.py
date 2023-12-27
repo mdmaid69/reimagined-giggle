f5e12d25526b37a11d58fab18265f2caaae5b347
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:41:34 +0000

    gas sting part remain provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
