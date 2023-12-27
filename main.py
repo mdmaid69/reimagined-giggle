674fc258ab2b11a81a581a18cc340ca6f73727ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:51:29 +0000

    interview anywhere lead train sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
