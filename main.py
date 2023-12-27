1f75d9d831565e0dbf389bc38fbc9efb626491b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:57:39 +0000

    by Ss poison experience second

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
