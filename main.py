37ab5db7b65fb6ed4cf8f26d6a7a922b3c80043a
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:40:16 +0000

    else before choose practical claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
