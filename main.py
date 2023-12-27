5c97a8666ee3eac9729eeb412cf8c68e22bff713
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:31:06 +0000

    wave shop independent present substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
