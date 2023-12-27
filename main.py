f83ac80dc01ed0caa4049a376a82398b8db65e05
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:51:02 +0000

    once embarrassed giveout able race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_speed(distance, time):
+        return distance / time
