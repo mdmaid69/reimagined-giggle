b13856b99e0fba460ccf0f679ac4a4137f3c72d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:32:44 +0000

    balance solve nervous except grand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
