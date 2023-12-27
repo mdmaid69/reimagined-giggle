d964db28a0b879355e02e34d96eee755b035a238
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:39:29 +0000

    field their joke rent influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
