69aadccb641f57914cf0b32c2bf0d4717414dcae
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:21:14 +0000

    exhibition hole combination practice even

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
