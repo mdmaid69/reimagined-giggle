b3bf72c1809e16ab9b337846fff8a2b2a165feab
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:52:26 +0000

    on feel deaf empty available

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
