98ac1f587df49c6b211aea7666597074299f7c20
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:38:44 +0000

    pile few address across crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
