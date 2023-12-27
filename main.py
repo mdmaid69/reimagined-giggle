fb2607234a014e0eafa9a3f6e67daf9a2d07dc8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:45:48 +0000

    stage support argue turnback cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
   def rename_file(old_name, new_name):
         os.rename(old_name, new_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
