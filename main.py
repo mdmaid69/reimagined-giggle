8fb90f7f4e55c69b0cfe80fe0fa7416acadd1b7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:17:13 +0000

    tragedy reply instrument water street

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_force(mass, acceleration):
+        return mass * acceleration
