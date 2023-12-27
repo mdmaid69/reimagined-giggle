3873de4d615712ec019d2eabc2b16a33e8ca912a
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:53:13 +0000

    light clearly worst count depend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_exponential(x):
+        return math.exp(x)
+def find_min(lst):
+        return min(lst)
