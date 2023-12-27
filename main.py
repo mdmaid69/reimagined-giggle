2ca593c6fe09bd8bca9efedd573c28c0edc8be40
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:37:49 +0000

    Ss benefit region group yes

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
