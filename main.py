33ad50dcbc4e5be7c4913d1123bfbf20f757a731
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:28:38 +0000

    cruel punishment term promise breakin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
