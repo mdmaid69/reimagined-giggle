3df828a4dd0f8339f43fe4eea6a42a28d76ce08a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:54:28 +0000

    measure find whisper cinema dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def add_numbers(x, y):
+        return x + y
