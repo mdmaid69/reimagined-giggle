b207f6fac8ad316466a88548fedf2c4172319033
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:28:44 +0000

    knock lend believe shade strong

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
