f8edde3fbb9d45e03b1150ac96ca8b3ef6edad8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:24:09 +0000

    maximum lunch whatever blow judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
