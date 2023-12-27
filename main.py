cac3e0c58ff34f0db9df5654190ac2cb127e7663
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:24:05 +0000

    press theirs early apply maximum

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def remove_duplicates(lst):
+        return list(set(lst))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
