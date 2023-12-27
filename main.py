0ab71d8d59f39beedbda96cc2608e3801e219811
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:31:35 +0000

    slide defend spoil doctor search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_array_typecode(array):
+        return array.typecode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
