249b9c74f6ec009f170ef72a3e9cc99870934b95
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:07:00 +0000

    sound follow this talkinto little

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_even(n):
+        return n % 2 == 0
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
