14ff6f1563c2c2918c1705486564a33d16f95479
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:17:11 +0000

    tight cut throw tear experiment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
