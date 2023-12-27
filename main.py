fdaf121386c713555a6cc1a9f09157b0cf986fae
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:16:25 +0000

    difficult record recognize disagree door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
