665e42405b57e44bb7bc9adcbff03db533d29a74
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:04:29 +0000

    pool Kk needle general treatment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import sys
+def exit_program():
+        sys.exit()
+import os
+def remove_directory(path):
+        os.rmdir(path)
