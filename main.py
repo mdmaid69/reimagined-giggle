e918bc2b4063fcacab042b7fb0581553de72db8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:23:46 +0000

    performance worth style guess complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
