674d399e6534729fdfbab2a350e6318ac5a2c8ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:17:27 +0000

    light ocean makeup hour unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def convert_to_octal(n):
+        return oct(n)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
