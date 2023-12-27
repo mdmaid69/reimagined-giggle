23faaa397e3edbd3f8764d29c80ec004049eae25
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:27:06 +0000

    rent comeround bus always sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
