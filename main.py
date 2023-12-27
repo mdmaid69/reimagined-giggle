097cf827081e67c449d62c91d9af2c21ead04144
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:33:44 +0000

    responsibility gap fight introduce disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
