a8720c70134d58b9712ac652945281018ca3e692
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:42:32 +0000

    unfortunate spare characteristic team look

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
