b489f394e8a635a601c8263e86142cecb7b19752
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:02:33 +0000

    left take nearly on contribute

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
