be50be8311f56cabef5ed7fa8e0de4ddf806ec12
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:18:44 +0000

    polite thin Ww trade hold

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+print(sum(range(10)))
