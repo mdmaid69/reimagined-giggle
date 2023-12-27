8b158a0b19a4bd4d9d4543c10d1203d2aaed91c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:50:13 +0000

    hurt surprising moment lay top

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
