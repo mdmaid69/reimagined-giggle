6e3b715854020ff8d16389fee8b2f456fff0e013
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:55:31 +0000

    circle only clue lookaround new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
