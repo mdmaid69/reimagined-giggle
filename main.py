fc4f807f19558396b37e8ab6ad0f0c4ff3fdcd9d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:36:05 +0000

    protect nearly active injury owe

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-def find_max(numbers):
-        return max(numbers)
+def convert_to_binary(n):
+        return bin(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
