a593fa15fd9f6d8181a8cab448c9fd64ded9594c
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:51:14 +0000

    exercise direct else door silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
