de1c00940181ebe13719aecf43c134ae0fffb71e
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:04:45 +0000

    autumn Jj workout work listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
