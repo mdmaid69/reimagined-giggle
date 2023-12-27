6dc3f6c671e7e2f7167b2c613da4d8e399600a8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:57:34 +0000

    modern discussion combine pump pull

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
