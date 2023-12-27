e9d54f7cb627dbc12dbfef08d50b96514040402b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:19:47 +0000

    weekend contribute first discover fat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
