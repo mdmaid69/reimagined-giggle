b5d4caf4e41f82d448b970247b946c135a971320
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:53:31 +0000

    brother few over pleasant flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import sys
+  def get_python_version():
+        return sys.version
