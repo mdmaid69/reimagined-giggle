43a46a47263f27983ebcaf763ed8b9e231961ec4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:31:23 +0000

    race healthy damp comein mention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
