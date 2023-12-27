1d4c1f969d61db60f58bc334df8de77497d27b28
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:56:49 +0000

    design goup common tape display

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+import collections
+def create_priority_queue():
+        return collections.deque()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
