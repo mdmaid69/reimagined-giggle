7dd9d6c9ecf833fa1eb328d5c82fa9a9f848d023
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:06:58 +0000

    opposite modern switch especially neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def multiply_numbers(x, y):
+        return x * y
