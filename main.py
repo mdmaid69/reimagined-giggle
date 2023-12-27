6bf202b9c5745251b7b444b70322447b0e77a7a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:03:49 +0000

    keen gas ready poor my

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
