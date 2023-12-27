b5aedfe8b88df217ac2662dc2790184330acb4cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:53:17 +0000

    keepoff realize quit unit breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def clear_array(array):
+        array *= 0
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
