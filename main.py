61019cafa3daa228231027c1356916c0dadf4459
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:46:41 +0000

    awake transport stare play clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def find_max(lst):
+        return max(lst)
