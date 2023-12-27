08ac43d765f8bbeb73f17fa9ec0d53653d3f0753
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:19:33 +0000

    hollow down collect determined callup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
