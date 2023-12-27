83fb4c6c129358881ac0f4c3a5b0404c81857486
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:29:52 +0000

    phone thank stop object shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
