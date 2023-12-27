6202f9a6466241538fa277e62bd941f2f010469b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:24:49 +0000

    hope famous suffer rather train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
