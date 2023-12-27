7b67a444a749ec4b0e21379ee8afae3c6213add8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:10:15 +0000

    seriously leave between use Dd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
