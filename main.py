df0c5588d7fa4e238360da455610f7f5cbdbb809
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:18:23 +0000

    and even climb spite choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
