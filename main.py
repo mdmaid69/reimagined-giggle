3782dbaee43d9f3b4823269f6595912468b68b58
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:09:41 +0000

    thinkthrough coffee university eye you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def change_working_directory(path):
+        os.chdir(path)
