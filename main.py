49e0d3d8fe9c0f113cfe5475cff69443764ff1b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:24:08 +0000

    turn damage drink talkinto another

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
