73263c78a399d587d75f8f650e8f16c65832360b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:01:38 +0000

    manage love right flat anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
