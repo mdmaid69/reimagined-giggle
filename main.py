a96203c35f2b0dfff9dad001eb051d0e94ed9e75
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:40:38 +0000

    open clear healthy lay week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
