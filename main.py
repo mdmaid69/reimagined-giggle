cfd49231b646926aa2c679c94981c00915308676
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:13:16 +0000

    realize finish difficulty who turnup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
