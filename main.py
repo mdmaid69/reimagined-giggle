9eafb67af24408f29ad848a896dddeb099c4e0b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:12:31 +0000

    now to cut poison cross

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-for i in range(5):
-        print(i)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def cube_number(x):
+        return x**3
