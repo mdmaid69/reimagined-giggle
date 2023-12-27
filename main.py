9afc41e306b193dd9b31c7e9d1658e92dc941cb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:14:18 +0000

    hardly majority work shine slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
