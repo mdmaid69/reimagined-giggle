6db5895ef1f53ef2f2a9317abe09a3eb4648d562
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:45:54 +0000

    pin on thank hard sorry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
