95ca1dccfe51d0108b0ce77427c9add5e1d38261
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:37:50 +0000

    slightly describe some occasion sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
