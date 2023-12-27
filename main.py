800299a92e0b85818a59c56d1fc8394c08d0aa1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:21:27 +0000

    pick total ilovedicks defend ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
