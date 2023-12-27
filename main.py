a434d803f5c1667bc7d9346459a9304bf36b6ea1
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:35:55 +0000

    confidence organize exhibition takeout nonsense

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
