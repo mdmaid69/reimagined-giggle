d8b442f979b72c272a27bb5a1445e450ff351368
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:48:22 +0000

    form legal oil fat whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def convert_to_binary(n):
+        return bin(n)
