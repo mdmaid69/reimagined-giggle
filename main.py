0a7d6314cb934f0ce739fd1a2145cdc73c1d5ffc
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:50:51 +0000

    union top instruction active flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_max(lst):
+        return max(lst)
