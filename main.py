98b344acf9d433a38c0f22fd8f6fc31ca0f1c1a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:04:28 +0000

    simple dish distance bread control

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
