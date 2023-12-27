b22f6aa218685707e7abfeeaceb3cfaa651abd30
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:26:17 +0000

    dick mad ought boring capital

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def reverse_list(lst):
+        return lst[::-1]
+def remove_duplicates(lst):
+        return list(set(lst))
