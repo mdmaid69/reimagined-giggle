47b288bb0d30191ae30bb066dd2151f1e827e691
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:56:25 +0000

    free lean generally date hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
