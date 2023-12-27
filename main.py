3a687a19b7168806abb23384f5b77e7a6a1b6a65
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:39:03 +0000

    fortunately do heat birthday place

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
