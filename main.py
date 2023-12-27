43670c3126acc92002ea277536a554590fad5d06
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:08:42 +0000

    choose show hit secret final

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
