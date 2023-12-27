01a40aa422a7c61271f9074e23c3977304e2ed5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:17:00 +0000

    strike probably ear yes worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
