4a30fd160756e38810cd5a10da31a8a916e7d39f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:17:05 +0000

    brain steal cool invite sharp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def divide_numbers(x, y):
-        return x / y
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
