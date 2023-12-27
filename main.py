78ead77efc16e0477d864933c7ab2de5b79c361e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:50:43 +0000

    OKorokay manage hope success become

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-print(time.time())
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
