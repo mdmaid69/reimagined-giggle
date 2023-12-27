9abf727559fca69e4bcb4504110dde71c587eb2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:24:10 +0000

    chief pour clean after arm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
