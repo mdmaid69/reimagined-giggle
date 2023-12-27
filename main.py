9c392105fe2d1fcddf2558b0966a254a0f28a4e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:39:41 +0000

    of innocent unusual unkind arrival

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
