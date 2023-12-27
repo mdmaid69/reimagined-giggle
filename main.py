05f73523c8b075a97fd17741203d90ec96e6cec7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:23:15 +0000

    callback much turnon goup pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def cube_number(x):
-        return x**3
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
