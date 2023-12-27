3334da7eee55710120ffda751c181a468576b519
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:49:15 +0000

    this eye protection mistake Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
