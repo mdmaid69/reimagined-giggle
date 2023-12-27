f99e4ebf6d5b2ecc39992d6355e51389cc02ce30
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:44:39 +0000

    that Ff 565end again issue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+print(os.getcwd())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
