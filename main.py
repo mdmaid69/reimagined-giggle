2ee8d520bec29cf046ca7fc779ec9a6af66530e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:21:18 +0000

    unlucky busy spray destroy lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
