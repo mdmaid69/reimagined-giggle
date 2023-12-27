cea77d07a047235fef91f7b295a2a6060b94e1a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:20:45 +0000

    operation price urgent basis whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
