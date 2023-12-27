fa96f5475faa7ce44ec33659413d72a4f54a5d74
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:44:37 +0000

    band smoke earth plenty goon

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
