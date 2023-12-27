70b0eb3ac0fae70e38a409a57bc6636b23ff0b6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:13:13 +0000

    dress feeling common could put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
