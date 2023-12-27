d2288964bdb36989de6583867d33f84e0e8dd369
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:09 +0000

    large everyone follow find wing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_list(array):
+        return list(array)
