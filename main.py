b659d2cb970387fb2b3389a0efd616cb82a8bbe8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:30:02 +0000

    whatever say connect workout commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
