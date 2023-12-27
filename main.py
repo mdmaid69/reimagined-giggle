6d81700f4c054edf5e8127c0a11c3bafdbc87739
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:12:51 +0000

    stamp state fork light estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
