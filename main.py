4b0fe339507fca112530075ca7c3c0f742e72da6
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:13:24 +0000

    contents event minor start cause

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
