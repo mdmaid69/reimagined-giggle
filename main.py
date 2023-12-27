5b99c6f8c4480e91d5b719c2529f9351cbd0fa14
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:10:44 +0000

    worth improvement interview injury physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
