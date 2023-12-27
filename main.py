18fb1699927528cfe88f15e2a321cdbdde12c741
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:53:41 +0000

    tidy recent jump score cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
