eb0f2447c8885f06ebc93416eba1fda802aafea7
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:46:53 +0000

    sometimes common ready dance afterwards

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def cube_number(x):
+        return x**3
