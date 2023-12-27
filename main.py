8a536cdbf2d76dd194ceb6828aea246237e1023c
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:30:54 +0000

    teach foot discovery rather 179bite

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_volume(length, width, height):
+        return length * width * height
