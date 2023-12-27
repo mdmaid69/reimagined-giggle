c0c73fad4f0d6919a9f8b2d88111cdead17d1004
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:26:07 +0000

    sharp apply feed as earn

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
