f390c8d92e35b1aaa9a473de3c15ff0de50be4a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:02:43 +0000

    flight check country trust recently

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
