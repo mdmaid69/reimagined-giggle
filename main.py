3adc22d80b89f4e4d553000eee3ac234de7f5a0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:56:01 +0000

    same fairly affect fresh lunch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
