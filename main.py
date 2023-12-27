2ef9c239c1c70ac248ac8710dbd1a259008578f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:17:55 +0000

    frame zero waste on pump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
