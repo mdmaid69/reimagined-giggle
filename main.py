c27f18225f3f26c593d6f78ef8e0a6194fed1b71
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:13:56 +0000

    communication recovery switch question exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
