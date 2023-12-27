581fefc8b0ae21cb0cdb6564afd3c50439bd1488
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:20:28 +0000

    complicated discover last about fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
