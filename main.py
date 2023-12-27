e1e3c5147f5b9e663b7affe336d0534463d20a01
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:21:42 +0000

    letter or supply keepaway order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
