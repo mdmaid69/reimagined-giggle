ceb7f0777115093adfe8987116323f7275cee113
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:14:43 +0000

    sing earn lie contents promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
