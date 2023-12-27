1309b50fb90fea13c8fd9784d08dcb3f13cebd29
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:28:30 +0000

    cap reserve aim under language

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
