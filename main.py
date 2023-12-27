253ff4144a1d0879245d28458d5a0cd150fca807
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:43:15 +0000

    mess swing him section mind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import platform
+def get_os_info():
+        return platform.uname()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
