5cad23cb03048e54f1f9cba159681006d3e4236c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:59:49 +0000

    meal attractive lock rubbish yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
