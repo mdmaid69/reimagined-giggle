3176d322239831d1cc9beab02fd39543fef69a66
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:08:16 +0000

    walk keep group store choose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_index(array, item):
+        return array.index(item)
