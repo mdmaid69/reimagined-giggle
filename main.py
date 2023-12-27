7d354ce1f2a5b966319e6502f15f046e4c250e53
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:19:08 +0000

    realize task apparently everywhere report

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-name = "Python"
-print("Hello,", name)
-def cube_number(x):
-        return x**3
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
