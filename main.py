345068c4a524dd28393c2a6a3aab834afdc07e60
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:27:30 +0000

    slightly depend purpose issue vanish

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
