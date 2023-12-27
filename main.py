59e7aa7463c350ce8e7f6b6715044630f31d4c72
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:43:23 +0000

    minor behind behave slip difficulty

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
