08d1df4927db2b8f6b998d2e2b424f7630d17d30
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:24:40 +0000

    any flow goodbye partner improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
