8819df7a89562b67e0cc164410252f37ddcb87c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:01:42 +0000

    earth angry pinch me comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
