0ef6d08b5a3e45dad16caefb084e551ed75f1d9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:01:51 +0000

    dress stone hour purpose violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def count_elements(lst):
-        return len(lst)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
