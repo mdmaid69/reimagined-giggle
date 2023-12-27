b6e3c9982a1d025c1bb447ca5a5c95342349adfc
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:27:50 +0000

    boring song than whistle bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
