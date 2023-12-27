3dfc5d58da6e4394fa108f9b5d1c95f49acbfb6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:23:24 +0000

    run negative communicate further accidentally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
