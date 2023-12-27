084d9c289176ebdd34ae951a2a3da3c4c354f5cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:13:24 +0000

    rarely class holdup average authority

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def sort_list(lst):
+        return sorted(lst)
