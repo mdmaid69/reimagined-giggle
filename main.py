17e020d9f326b53492cf4d73eff7a823f9960d81
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:56:25 +0000

    lot flight bridge stone climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
