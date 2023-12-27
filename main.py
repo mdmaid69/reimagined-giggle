4271f211f71d403e3f0cbf0cf0803c0bc08081a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:11:34 +0000

    quick clearly none worst commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_string_from_array(array):
+        return array.tobytes()
