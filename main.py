375b6a8e274f22277de4fb7c4c9e908dacdab593
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:26:29 +0000

    mail mean earn unhappy report

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import sys
-print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def subtract_numbers(x, y):
+        return x - y
