176274d3d6b00c2dd85a377a6ac7089cf455119c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:25:27 +0000

    soft pool scale future comment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
