d4fc98b844c222aa5105c2af7f8c92edc1a0e9ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:03:47 +0000

    heavy by stand half contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
