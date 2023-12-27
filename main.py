084bc165bc7ef9bcaedfa5b45e5f042545c35bc2
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:18:36 +0000

    workout choose high drink place

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
