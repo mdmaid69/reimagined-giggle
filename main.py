5a8246b8b15f0e0f4dc1f79d5a23a61ffbe9de75
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:36:51 +0000

    freeze fall comeround ignore shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
