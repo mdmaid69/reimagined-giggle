02ec8b572086dc0dbe1f4c24c3dcb4deea7b6257
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:04:37 +0000

    church tune control answer tour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
