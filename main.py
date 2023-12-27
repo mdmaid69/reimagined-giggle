28304791c71c90082b5c711213ab06bb4d740252
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:31:20 +0000

    history Nn interrupt introduction travel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def remove_directory(path):
+        os.rmdir(path)
