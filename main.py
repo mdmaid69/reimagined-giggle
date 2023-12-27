c22d1bd46f3f5ef90fa19d259a3f9ef9b5975cd8
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:04:45 +0000

    smile jump Hh opponent compete

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import array
+def reverse_array(array):
+        array.reverse()
