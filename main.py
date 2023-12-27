e55da9d61e9616bcfed02fec9e8d68838eba986c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:13:48 +0000

    share bunch argument hers piece

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+print(sum(range(10)))
