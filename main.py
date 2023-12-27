eca181f18df0ecfc8786f7fd76f9d52023454f87
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:06:11 +0000

    bill sick translate cheque slice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-print(sys.version)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
