9c1504df1b50a12cb37200922ef7c91f8ae2bacc
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:51:32 +0000

    very hard next Gg strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
