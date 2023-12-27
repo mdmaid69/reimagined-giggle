9f4f2e48e1ebd5adb576d81bf62b5adc494a6c5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:57:31 +0000

    fire except strange crash journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
