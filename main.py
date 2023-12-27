40bb5a18ec2c4d5ae72d6ba2461f405af95dd53f
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:41:26 +0000

    ignore immediate look have black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
