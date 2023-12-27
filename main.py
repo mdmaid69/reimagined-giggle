a6499fb8f48c50980c152b5f7ffe15b19ba739a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:09:46 +0000

    lookback(on) treat care mean what

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
