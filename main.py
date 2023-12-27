f65f3864446cd30ef36a3897b9b6df1dd1932a05
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:48:43 +0000

    both fierce juice maximum year

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def convert_to_octal(n):
+        return oct(n)
