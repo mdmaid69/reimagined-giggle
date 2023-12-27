e5e18a3d5b756765c5bae493f71b888bf36b87e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:19:24 +0000

    collection cross authority pin move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
