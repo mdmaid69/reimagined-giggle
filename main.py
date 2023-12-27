225d00bb9dc355b9e1fa6dda038be510a2d25f7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:24:08 +0000

    less life left rubbish file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
