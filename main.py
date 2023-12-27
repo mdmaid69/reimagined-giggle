668627fbe0faee57a15359137402f87d52cad7cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:25:45 +0000

    production evening detail bad card

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def is_odd(n):
-        return n % 2 != 0
+def get_array_as_int(array):
+        return int(array[0])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
