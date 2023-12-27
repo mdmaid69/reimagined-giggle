d58e1b10abba8bdba723544514a90abc064f9ad2
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:50:27 +0000

    ilovedicks callup contact wrong laugh

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_even(n):
+        return n % 2 == 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
