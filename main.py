c6a3b0531a63d237b6c4212e2e3b1b1c62430142
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:53:06 +0000

    introduce accidental his 565end bored

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
