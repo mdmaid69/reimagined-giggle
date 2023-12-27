a494a4274c46063097cc4e63b5ed3d4ef7753143
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:18:42 +0000

    mad holdon clue inform dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
