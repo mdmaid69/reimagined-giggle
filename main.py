016a1b70c38ead80bdd80e309fcd452507d7dc93
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:55:25 +0000

    mention complete voice claim serious

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
