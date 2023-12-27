afb771c639b429ab01634ca8b1d2ead0988ff98c
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:50:58 +0000

    keepup rest quick else towards

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
