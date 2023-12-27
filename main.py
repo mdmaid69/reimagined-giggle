1af11cf8b7b3f0dec881032584b122a56e86f493
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:48:51 +0000

    welcome necessary react quit flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_user_list():
+        return collections.UserList()
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_length(array):
+        return len(array)
