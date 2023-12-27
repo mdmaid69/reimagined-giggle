a60b22d634525a48c597f634652004079d6732f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:47:19 +0000

    twist nervous real bus defend

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+print(sum(range(10)))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
