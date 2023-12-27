9b0f2268589eade8a6f42766a5cc5ce947d0852a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:51:49 +0000

    up pride agree clean cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
