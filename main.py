3c9a3dc3121cf163dc9f60cc43c14ad065699f0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:43:55 +0000

    goback(to) comeround everybody claim stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
