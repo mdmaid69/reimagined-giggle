838d586f96ae190ab3abbf94d6e4409e87838ddf
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:24:07 +0000

    busy miss apparently exhibition freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
