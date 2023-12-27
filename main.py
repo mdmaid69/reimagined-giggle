3b2a408eae2177802cb40667a40ff11fb34d89cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:22:55 +0000

    567enemy read sorry press nonsense

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
