e857a9f8d863aefa40a9a4ffd505de6ddba7138d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:16:11 +0000

    taste while share roof spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
