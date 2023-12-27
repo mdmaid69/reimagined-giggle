0410cbcfec39facc141d11be75f001c940a62be2
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:33:01 +0000

    turn same runaway conversation off

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
