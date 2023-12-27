8f15268a171767a3653e472e04f89bdaa431d99d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:08:12 +0000

    pencil certainly little sister majority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
