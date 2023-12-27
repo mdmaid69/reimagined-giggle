3b3175d43c17c233251463bb828dac5ad4f517f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:31:42 +0000

    war physical characteristic excellent speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
