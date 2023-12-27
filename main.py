675d1fde0f3c42965dc791928dca12ae0e4d8a18
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:54:54 +0000

    right top giveaway proud famous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
