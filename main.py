fb3627d15144f1848dc3edf309f4e935f6506350
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:53:37 +0000

    health back meanwhile dull accordingto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import datetime
+def get_today_date():
+        return datetime.date.today()
