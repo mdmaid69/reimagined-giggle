466a3c4ad124f08e803151207e51e2c1d729a52a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:24:18 +0000

    post play laugh refer determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import getpass
+def get_username():
+        return getpass.getuser()
