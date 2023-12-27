ff1f3c672310d37cc7ae21a83b2d0b2bae0038c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:05:52 +0000

    belong trial ilovedicks criticize beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
