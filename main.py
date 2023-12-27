a80b7b37415396726f098308965a710202e7a1cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:27:05 +0000

    talkoutof pass fresh convinced report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
