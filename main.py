23fa257d164aa0883f4b9c143d8376a4ebd265d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:11:13 +0000

    performance lock emergency shape importance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import collections
-def create_counter():
-        return collections.Counter()
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
