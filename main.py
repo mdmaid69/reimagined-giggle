a375b60b20682cad6156532c29cff8b91e64b359
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:39:02 +0000

    how brush progress nerve confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
