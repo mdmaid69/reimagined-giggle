6e24df79870411f433dd9e4e0453c54ed5b1ac1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:25:39 +0000

    guess Itself free shadow until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tensorflow as tf
-print(tf.__version__)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
