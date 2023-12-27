425ffbedf520bfff42b7a372303b8cd1530b3e70
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:26:54 +0000

    some scene disturb against do

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
