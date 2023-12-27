ce6ec82a9fa41229f06df8c0da44141a3a22c46e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:26:55 +0000

    hear juice attach dress correct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def cube_number(x):
+        return x**3
