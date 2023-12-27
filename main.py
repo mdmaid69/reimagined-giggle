87366e390be483ac180eafdcb36a2dc7e9ac75cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:41:34 +0000

    care today language share frame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
