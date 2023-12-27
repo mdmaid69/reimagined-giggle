9433d473fdca79e0a001f656650ea1492e1e9425
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:19:23 +0000

    account right change miss record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import platform
+def get_python_version():
+        return platform.python_version()
+import os
+def change_working_directory(path):
+        os.chdir(path)
