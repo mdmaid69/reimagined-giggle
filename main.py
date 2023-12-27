ad137a11c28df58fc39c145a4c2b51b6caec0313
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:17:46 +0000

    note fond priority round difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-text = "Hello, world!"
-print("Characters:", len(text))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
