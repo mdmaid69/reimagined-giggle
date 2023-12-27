0e42b4c97558b703f8dc47f67f4540f90e8240f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:20:01 +0000

    region ride protest almost bread

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
