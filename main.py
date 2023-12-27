db431da6cf54cd656caf29fe76dac86737f7d6f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:53:20 +0000

    read help forget shelter getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
