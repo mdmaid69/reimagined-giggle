f7f5ede807c960f45abef0f29ab6ff45cbd3302a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:45:30 +0000

    favourite square close moon mean

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
