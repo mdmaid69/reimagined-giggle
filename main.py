78d9e4e91be83f39c13e89562197537ab0e3f326
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:05:19 +0000

    rare safe later extra he

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
