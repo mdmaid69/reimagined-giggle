b4dc32bb28359080908fe94a798edb440ba8bbf0
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:45:39 +0000

    me free whoever poison scrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def add_numbers(x, y):
-        return x + y
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
