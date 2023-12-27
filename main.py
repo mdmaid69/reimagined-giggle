53efe7bc5160ca58924b3ec49ef068a22b412e60
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:19:58 +0000

    above thing suddenly offence base

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
