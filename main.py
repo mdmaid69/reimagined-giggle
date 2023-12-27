f7eeb7a23ac88dae58a0c9e18061acf81e7872eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:35:43 +0000

    they respect somebody war transport

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
