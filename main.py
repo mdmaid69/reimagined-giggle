9ad2b4e3c4ec73e350fa85f4db5759432d544e51
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:54:03 +0000

    quick backwards exercise apologizeapologise walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
