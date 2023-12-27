f77491c082793d5908bdcdd812a635be73d3cb08
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:48:29 +0000

    effect hurt pattern search float

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-from collections import Counter
-print(Counter("hello world"))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
