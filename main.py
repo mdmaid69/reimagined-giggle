7b701b0acfce37d10f9a3cd20d8ea8b74dbec77f
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:01:12 +0000

    moment female persuade degree bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        return sys.version
+import sys
+def print_python_version():
+        print(sys.version)
