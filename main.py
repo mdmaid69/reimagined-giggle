aae0693345563d190a31b321c8b98e37daeb7dcd
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:54:40 +0000

    backwards drawing wish everyone weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_current_directory():
+        return os.getcwd()
