49b4c5b17e4f9739dc027bef24616f3b46607785
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:24:58 +0000

    keen punish address usual pocket

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
