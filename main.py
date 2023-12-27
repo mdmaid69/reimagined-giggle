d79ebf93bd35b7ea32e3e76e915cd2506499121f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:08:19 +0000

    plenty nervous gate power guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
