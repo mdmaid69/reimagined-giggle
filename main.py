bb50295aa57c3d3393f5e1882f735af3d1a78080
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:14:10 +0000

    only under hand 568energy tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import os
+def change_working_directory(path):
+        os.chdir(path)
