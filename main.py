2c037c964e582872dfcda93f61b1d25e5d9c5a0f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:27:04 +0000

    shout trick quantity generous advise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
