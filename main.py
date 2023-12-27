668972762cacbef706731c26d54fdb4d71229a95
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:21:26 +0000

    exchange divide sweet neither suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
