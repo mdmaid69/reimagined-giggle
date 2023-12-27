51d093af0072216649e9291447d536794c25f9fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:19:47 +0000

    defend pain backwards opposite today

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
