c233328ac26209c53cd6b56f4b5d6d632afa08da
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:26:14 +0000

    judge unlucky flow round exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def clear_array(array):
-        array *= 0
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
