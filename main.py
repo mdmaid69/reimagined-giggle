734a4a46abce8edc23366edbebba513357e919c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:55:33 +0000

    speed concentration contrast me sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
