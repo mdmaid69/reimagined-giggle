1c785acb9050f6e1c12a00dc2b46fac0773d2be9
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:14:25 +0000

    make fast low next wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
