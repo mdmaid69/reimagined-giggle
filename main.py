f2ee3f5e1d4ff281c51c0c65d2b140d43c365e65
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:44:37 +0000

    look discuss main cut intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
