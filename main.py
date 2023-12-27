a4623826b8cb94e45452d01312d7417c324c0493
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:09:08 +0000

    smile sign duty die blind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
