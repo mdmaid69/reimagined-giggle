3b3e058da623d6cf9a8a30dd3b91248c84c52b6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:18:25 +0000

    steam dry important breakdown thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
