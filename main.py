cabd7de2fe3fae015c009234c1543aba845cab66
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:19:31 +0000

    then juice confused also impression

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
