84628ec77ca8210b65834ee69165eb75d25258b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:44:58 +0000

    artificial job hide listen reply

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def cube_number(x):
-        return x**3
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def reverse_list(lst):
+        return lst[::-1]
