1881139aa016de1a70b8858d25593c82916ad053
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:36:25 +0000

    science argue rush pleased accidentally

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
