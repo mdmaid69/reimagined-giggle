4a432ee1d8d765bf4b51e61d60b45d8f3229e7ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:24:26 +0000

    row damage wall glad light

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def convert_to_octal(n):
-        return oct(n)
+def clear_array(array):
+        array *= 0
+print("Hello, world!")
