09f6d37fa4d1e8d5ac4ef818ab7ad5f931b3075e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:00:59 +0000

    sensible official swallow defend put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_working_directory():
+        return os.getcwd()
