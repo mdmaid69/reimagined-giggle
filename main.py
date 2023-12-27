11911f909253ba54df162cb8daf77a6b7b897b56
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:31:44 +0000

    shut stop print similar general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
