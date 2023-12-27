6e8fdef9605cf7f9874c76ef0c73965f3fafa940
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:44:10 +0000

    town strike pour coat slice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
