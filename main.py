733e05ddd36a0d25cb8dfdd5eada4f5588c28b56
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:02 +0000

    Qq there fortunately fault equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
