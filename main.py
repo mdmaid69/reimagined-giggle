7d63c6b32568aa3314151b6a5a9f47072ec5fd61
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:21:35 +0000

    swallow rest forward far dress

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
