1eb67ecf32ac03ea0bf8841bbe5651a2f525b0c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:35:38 +0000

    exit round worth book once

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
