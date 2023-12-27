6248ebf842f2d4ea07432bcaff53811ce208eac7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:05:11 +0000

    lookup consider law scream message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
