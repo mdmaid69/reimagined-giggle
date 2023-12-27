65ac45499dca99d18da0d70295c5a17ebfda533b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:39:40 +0000

    where number act artificial recognize

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
