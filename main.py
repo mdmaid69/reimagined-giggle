c84fe5dc44d010a94683a82d61f3010b75b21c11
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:40:59 +0000

    wake earn slowly crash getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_bessel_function_of_second_kind(n, x):
         return math.yn(n, x)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def convert_to_hex(n):
+        return hex(n)
