acbf48b9053ca647fd0d7392934407a13f117ca0
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:05:15 +0000

    emergency command godown confident sing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_bool(array):
+        return bool(array)
