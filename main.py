86b4a08c441c4afe2b64c822fa63d864a22e6964
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:32:40 +0000

    thorough reasonable different black thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def calculate_area_rectangle(l, w):
+        return l * w
