a091c90ed9dac83401ea03c12b678f8b0a8e8deb
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:25:20 +0000

    detail shout mix round calm

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
