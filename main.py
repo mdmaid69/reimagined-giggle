4cb074fe935e1c9c9eb6e0d9cf904c6142cbea5f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:49:08 +0000

    watch wind influence ahead nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
