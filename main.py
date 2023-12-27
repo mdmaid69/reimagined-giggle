3384fb60874a3c2b85e7f7a925150bb75f50ab13
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:18:45 +0000

    further march see surprise hospital

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+for i in range(10): print(i)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
