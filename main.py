2e55889c6471a80e58fe8955fe4aff3bd48f2ad5
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:25:48 +0000

    while centimetre pleasure purpose description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
