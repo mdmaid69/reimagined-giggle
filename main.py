45c63e70e5cb8269aaed841f27388dbd829c6628
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:33:58 +0000

    well neat apply listen interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
