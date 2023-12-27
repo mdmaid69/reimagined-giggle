f0bc6dfab3f5204edba008e0b97646bcef3e79ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:53:53 +0000

    commit great back hot switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
