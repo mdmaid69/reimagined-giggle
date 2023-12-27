7034e02ab3e6958e88e1777e37cc1e897e2b1f07
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:17:03 +0000

    stop be crowd judge letter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
