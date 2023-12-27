2dbe3525dde6752bee109de924c8d4234bb3ce06
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:15:36 +0000

    train conclusion score butter direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import getpass
 def get_password(prompt):
         return getpass.getpass(prompt)
