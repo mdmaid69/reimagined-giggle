4be6c97f10a4b18a9763901332486dcb1a3eb552
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:59:05 +0000

    exercise argument expensive continue with

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
