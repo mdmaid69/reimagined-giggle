7f583c9bd8f1b5927cdf33958edb57f6b7ee99b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:18:07 +0000

    shut content top appear collection

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-print(math.pi)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
