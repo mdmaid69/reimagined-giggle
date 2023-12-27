567d20051d6c70a08b095a0c5f8840228b850827
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:31:10 +0000

    description cheap remove first popular

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
