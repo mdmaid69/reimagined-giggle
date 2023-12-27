93510045eede2bbfd6e74c444fa93012c6789f71
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:34:46 +0000

    wrong understand pronounce dare people

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print("Hello, world!")
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
