a11921aeaaaacf00416a1125e387d3aa2cecd20b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:32:55 +0000

    confuse lesson capital probably teach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def is_even(n):
+        return n % 2 == 0
+def count_words(sentence):
+        return len(sentence.split())
