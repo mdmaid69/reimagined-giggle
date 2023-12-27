11da8e5430a3b53c86479aa37ace4d09241d8549
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:29:02 +0000

    distance protection stir hang rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
