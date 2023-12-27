352cdccfd5ca793e9c3d091b4b73b5c55806c372
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:55:21 +0000

    book pay anything odd conduct

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sys
+  def get_python_version():
+        return sys.version
