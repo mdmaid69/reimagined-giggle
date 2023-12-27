070cb035e53d9158dcb795bfc9d443052f4b99fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:57:21 +0000

    summer wind grow identify well

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
