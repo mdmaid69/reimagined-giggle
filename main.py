64838da995cb040fb3df9864bc3870f2bf818e39
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:13:23 +0000

    survive classroom sweet mess principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_directory():
+        return os.getcwd()
