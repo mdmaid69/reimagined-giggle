35df5e8eb0e2fb4baeaab0c0862d4d3639646983
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:51:34 +0000

    free sight bend unless unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def reverse_list(lst):
+        return lst[::-1]
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
