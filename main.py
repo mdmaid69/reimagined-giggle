40af8cfbaaa8a0a4dc239a14360c707473547d76
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:21:19 +0000

    rich team closed anxious bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
