e986c647bc5466bdd80fe2bcf989c66af2ac1528
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:31:15 +0000

    statement lead woman interested bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
