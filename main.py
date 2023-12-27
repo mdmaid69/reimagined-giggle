7c2e667e1529de498c79e850fdd23a54c9fe098f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:31:27 +0000

    least jump low protest make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_bytes_from_array(array):
+        return array.tobytes()
