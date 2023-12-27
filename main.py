c6be7a70b266af0d4abede2c16a261f259028336
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:59:11 +0000

    Ww even pair contribute replace

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
