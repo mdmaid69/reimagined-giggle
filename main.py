6e2d9c7aa9b756f83ee4abaa5fa5751cd38293ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:47:50 +0000

    complete dream arrange phone disturb

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import time
+def get_time_since_epoch():
+        return time.time()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
