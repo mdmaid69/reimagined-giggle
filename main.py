e2f404ce936c4b28a1e5cdec8c629b8ca46cb537
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:44:49 +0000

    sea nothing scared before moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
