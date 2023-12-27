4c9723e521f510e59a16bb10036f733872f8b11d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:53:42 +0000

    but race both pool bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
