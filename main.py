ddc82407e8138521ded24b3ab6fcd7f3bfb314af
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:05:47 +0000

    sing cut achievement blame act

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
