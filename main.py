2d5950dd7feab43149c248bfdfc9418dc5ded3f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:39:34 +0000

    bag cap lead Itself speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
