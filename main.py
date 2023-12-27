e0ab79c2e606e115e208798bdfa797959c6850c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:27:08 +0000

    sister main habit clear completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
