f34a6aae60207c971df8f87a9874d4acb65eb8b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:00:25 +0000

    Mm rest total science all

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
