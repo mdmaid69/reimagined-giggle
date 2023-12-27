22d99ff8ab3255f81ff6e4f263e4d3fb985405b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:58:11 +0000

    neat joke confusing steam right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
