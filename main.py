ebb21881f5ceba8eb8cccde8e83b859786bbdf02
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:53:58 +0000

    favour fast judge set explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
