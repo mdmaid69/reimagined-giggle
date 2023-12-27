ae883016a1f0ded014cf93c94c007be0bfd34a71
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:11:14 +0000

    bored keep cope obtain base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
