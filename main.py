8365ed9e54820bd49c18f3ae596c10b1ce7173ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:24:22 +0000

    write onto accidental bus cope

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def roll_die():
+        return random.randint(1, 6)
