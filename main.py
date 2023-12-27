d6c91d14277475b4aa1fd89784c57809cd515492
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:56:10 +0000

    quit keepoff science pleasant course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
