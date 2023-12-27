fec2ffb4e126f757af38ef64976066ea54620849
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:05:10 +0000

    cruel group thank conclusion everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_current_directory():
+        return os.getcwd()
