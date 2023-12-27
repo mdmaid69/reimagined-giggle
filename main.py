e63fdf7395ad4425bd8ffb0ebe3f6070cd5471d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:10:00 +0000

    Tt where train purpose talkinto

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
