f3d8a4aeebd1845649c54b4c05e916d1beb12e3d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:19:31 +0000

    road body expensive traffic invite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
