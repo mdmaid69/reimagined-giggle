ae12cd516943d5daef7c1cde4021a1f426beaabf
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:19:30 +0000

    cum spot settle person 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_current_directory():
+        return os.getcwd()
