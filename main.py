560b2ac8f3e32ba77a7e1c6300b5d7428874472e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:59:29 +0000

    violence common state persuade comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
