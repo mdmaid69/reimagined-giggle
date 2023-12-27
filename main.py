593011ded3c0c52f71f8157bab4badaf2fe1660f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:39:44 +0000

    pen pipe squash fetch path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
