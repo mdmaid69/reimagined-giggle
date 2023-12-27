45e0b07f6c171413f2147c706c81334513e52a56
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:28:46 +0000

    left further satisfactory minimum drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
