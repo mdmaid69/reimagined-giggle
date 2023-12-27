2bc3d15ced0da03fd1aecd6c89425401f974eef8
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:40:36 +0000

    adjective artificial dull tell themselves

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print(sum(range(10)))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
