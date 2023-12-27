bd9a6ba0e4d922aa52efdf66fa6f936367f04c4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:15:42 +0000

    behave mention slide bunch brave

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import time
-print(time.time())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
