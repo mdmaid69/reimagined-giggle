22ab2410dd9ed18f6a6b7e62ce689e97c9cd4d5f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:22:23 +0000

    record sometimes anywhere transparent under

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_length(array):
-        return len(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_speed(distance, time):
+        return distance / time
