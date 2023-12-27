46ebcf22dc9bf4f102e94fd3208890f3b2f5aa35
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:26:55 +0000

    ocean lead consist create relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
