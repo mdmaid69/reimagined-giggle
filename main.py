9bf338817615ca0dc5ca27cacdc29d231a88824b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:00:13 +0000

    tragedy occasional tip make coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def clear_array(array):
+        array *= 0
