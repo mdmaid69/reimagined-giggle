acd9464cb5389080d7536e77c4c702774843d497
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:58:32 +0000

    cum private trick dear taste

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
