8575dda44892b5f8a7cea5d9f4590d4556675867
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:04:25 +0000

    Itself hold about opinion test

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import collections
+def create_counter():
+        return collections.Counter()
