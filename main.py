2c2264a9aa9cb7a998492c3e6c5457cef7de0436
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:52:02 +0000

    press achieve us completely gas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
