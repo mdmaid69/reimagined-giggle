3326e99b4d2bb357e91f8ba55c133518d58957be
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:15:08 +0000

    single expensive swing thanks children

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
-print(os.getcwd())
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import time
+print(time.time())
+def sort_numbers(numbers):
+        return sorted(numbers)
