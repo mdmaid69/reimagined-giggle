e26259dedf446ca933027a60bcebc860f3134f8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:36:54 +0000

    suckme land practise character system

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
