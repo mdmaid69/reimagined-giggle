330b26a6219322d08ed678df8aa8dd7428bbd4b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:04:44 +0000

    congratulate handle wish plenty poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_list_from_array(array):
+        return array.tolist()
