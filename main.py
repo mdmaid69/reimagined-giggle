6787b83e9ae8a70674aaa0e9b9849d03fa117829
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:05:30 +0000

    interrupt squash deliberately sore wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
