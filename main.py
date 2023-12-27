8f99cdbdb42ce2e22c96342cd4a7c858f6cdf529
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:56:44 +0000

    minor poor suckme various congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_list_from_array(array):
+        return array.tolist()
