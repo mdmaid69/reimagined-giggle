0312700a15f41967a2af04dbef5e1e3a410f7ceb
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:06:43 +0000

    nearly shall forget steal degree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_area_triangle(b, h):
+def calculate_area_triangle(b, h):
         return 0.5 * b * h
+def is_even(n):
+        return n % 2 == 0
