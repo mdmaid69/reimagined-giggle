468acec8fbd0ee3935cf15d638d6ee70d50f7232
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:03:18 +0000

    breakfast sweet furniture turnover join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_octal(n):
-        return oct(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_list_from_array(array):
+        return array.tolist()
