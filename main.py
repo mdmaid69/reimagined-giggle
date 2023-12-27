113922dd0d11345625f9d59fc910353d0218d144
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:54:26 +0000

    illness disapprove may safe cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
