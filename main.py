f670845c29f8eb86610e6febbf97dfa2666445c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:54:47 +0000

    backwards scream successful sound against

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
