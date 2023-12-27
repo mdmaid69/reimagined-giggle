10698711dd370e619d9e1033ef32a8b51103286e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:57:11 +0000

    turnoff if hope quiet spin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def remove_duplicates(lst):
+        return list(set(lst))
