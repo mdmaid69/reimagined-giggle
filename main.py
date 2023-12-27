419bb6185806fbb24fec3c7c1b2b4f8316103152
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:53:51 +0000

    positive degree forever exhibition pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
