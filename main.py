02de3abd7e9f6c0f853616b555374f9befe18ceb
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:52:56 +0000

    have claim Nn ashamed quarter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
