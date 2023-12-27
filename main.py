7965e0e3e483c8104b24bd3ee7ef0725a3ebdf6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:50:50 +0000

    uniform whole disapprove relationship beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
