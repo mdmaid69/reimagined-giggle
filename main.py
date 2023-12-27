a3c8d33426e188a886f9019115255d1e1a0f5a09
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:21:09 +0000

    round type else pen weekend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def convert_to_hex(n):
+        return hex(n)
+def square_number(x):
+        return x**2
