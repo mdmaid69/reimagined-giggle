91f577a88425ba0fae83d4079ca77b9cbb52b38f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:40:01 +0000

    cough sound pleasure run complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
