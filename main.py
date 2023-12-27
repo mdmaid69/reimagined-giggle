e8c8ffb74eae474ad88ac945ad7e7e0f0bb2e58f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:40:09 +0000

    strike everywhere geton might comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
