eb465acccf1bb6b6f9291cb6d1d3bf0879744f14
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:48:59 +0000

    fact official whistle mean steady

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
