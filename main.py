1bb468dacd7ddb9c7899efc48a9e1369dc70b79c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:01:48 +0000

    reasonable force thinkof step broken

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
