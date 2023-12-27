ef4cb7206855cf66171590477055a513df19a2c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:50:35 +0000

    off unhappy discuss stand head

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
