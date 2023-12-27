aaeff16814e0c8e6e096b43f73f9ea63ffa2d7b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:25:16 +0000

    like appearance love exciting obtain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
