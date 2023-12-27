c6c0f37b3b1605a37af33ec9d8ab80adb321d1e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:23:11 +0000

    keepup trouble smile important coffee

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def sort_numbers(numbers):
+        return sorted(numbers)
