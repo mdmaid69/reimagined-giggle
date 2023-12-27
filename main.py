06671f44c3dc8904ad2eace3d3ccee03a7d65928
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:41:27 +0000

    giveout fine ashamed solve piece

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
