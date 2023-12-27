74df12d1837bd96ff23ed2c4739dd8b766bfa011
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:07:53 +0000

    Ii operate comment about sort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
