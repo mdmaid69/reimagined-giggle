4532e047ee312cff81d4a3f342d4b63015ec39db
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:05:14 +0000

    calm lookaround light escape evidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
