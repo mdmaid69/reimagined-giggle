2842138af82b36d0fabaab7205b6886d17319c88
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:28:09 +0000

    rubbish react insist hungry willing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
