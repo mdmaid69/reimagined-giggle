315b7f58f7f456c04436c213db362ffb3adeec71
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:36:49 +0000

    lead bread exchange this meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
