79e2863a613ff421f0f19049aa5c8cad05095813
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:19:39 +0000

    appearance convincing 570enjoyable tool reasonable

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-print([x**2 for x in range(10)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
