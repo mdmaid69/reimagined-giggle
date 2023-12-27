6be551b5e89fc78c787b8ad7aac7a5610ef85d36
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:53:09 +0000

    will lesson rate well regard

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_interest(principal, rate, time):
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
