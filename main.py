c3b2bba1cb6931945f68461a09ebf84325653ac4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:53:28 +0000

    such truth pattern sticky cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_acceleration(speed, time):
+        return speed / time
