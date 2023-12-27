46008e5788310570e9ef31845f358fd76f03b855
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:17:00 +0000

    guilty move mistake middle sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
