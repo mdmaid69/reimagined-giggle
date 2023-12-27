1ea30175d52e538be6993162683ea74f788b4b67
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:54:32 +0000

    bother appear likely last influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
