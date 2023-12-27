bf3edc0e171d2000ca76946c59d9a7db962e5b69
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:48:05 +0000

    pronunciation mainly argue break impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
