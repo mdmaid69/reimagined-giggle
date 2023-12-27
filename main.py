5faa7c727078cf09f9c1feee3fd862ada5fb747a
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:24:25 +0000

    somehow sure thing experiment official

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
