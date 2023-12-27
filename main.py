8b2d51b8d651a022f20cbd261c00222f3a77aa18
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:18 +0000

    beginning transport pour admit swim

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
