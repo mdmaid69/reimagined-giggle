b41c9186a1e358c254b33cd23f8c1f80a7059ea5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:14:22 +0000

    follow wrap consider loose exciting

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_float(array):
+        return float(array[0])
