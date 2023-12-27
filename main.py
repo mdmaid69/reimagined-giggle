636f3a7430e95f11e07d7724e72daa7b6a87addf
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:40:37 +0000

    hole entry among morning achieve

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
