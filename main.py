220c81c43d067357503cc3212921c1a43db77220
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:08:11 +0000

    sail centre surprising lesson speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
