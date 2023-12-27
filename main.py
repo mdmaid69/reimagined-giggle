5bdf78feba83b2c0c7a8b06b8252a4c93770290c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:16:07 +0000

    giveaway rid pleasant debt wing

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
