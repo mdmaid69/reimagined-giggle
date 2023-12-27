991fc77a3a265cfadca31c6589ecc8269a034dc1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:54 +0000

    giveaway sale range lead basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
