27cf4f88dcaaefc89a936d0a29673dee4688a682
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:18:32 +0000

    turndown touch recently scale apart

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
