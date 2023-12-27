ade7faba4edc843bbe093c66a7016a34c403f8dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:12:09 +0000

    bath turndown hole occasion yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import numpy as np
+print(np.array([1, 2, 3]))
