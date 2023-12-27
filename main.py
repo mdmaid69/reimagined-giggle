a6aaa2b9a5c231560bb5700b3d38b168a352b141
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:29:37 +0000

    friendly play buy comment talkoutof

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
