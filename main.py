346b7e5e2f4f39fdcfcf6469dc08facbfbc602ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:54:55 +0000

    compete soil use short production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
