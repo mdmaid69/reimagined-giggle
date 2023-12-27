e80153e1cdf795e95b6a8f0ede6ebdec50ef2457
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:09:15 +0000

    either butter wait however little

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
