551f1cdd69615669646a1682f170c3edfe2f1dc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:38:23 +0000

    build near whichever fear worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
