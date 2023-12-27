7316822f2a6730bea8e4c22d1416ba39f3c494a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:03:42 +0000

    goup pass sort popular quickly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_min(lst):
-        return min(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
