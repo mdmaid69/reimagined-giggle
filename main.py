69d33aaca03d323d2ed102663976d51b835be0b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:04:25 +0000

    pay always practise strength act

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
