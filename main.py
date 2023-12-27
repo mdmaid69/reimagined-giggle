cc3507db927747f569f9ab98fb6a19587bba6997
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:16:12 +0000

    calloff ahead join head switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_power(work, time):
-        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
