1b1403d65ecc8090cae0e6db1950d7dbc785e25c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:52:31 +0000

    previous wake colour stone dance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
