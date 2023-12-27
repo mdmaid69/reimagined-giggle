8b5e0a612b67bd80a76a1ed0a724af9cb9e52ed4
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:32:33 +0000

    squeeze dinner head from public

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def find_max(numbers):
+        return max(numbers)
