21bf46a7c7d31ca6839e76431d57fffafaa9cb7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:44:26 +0000

    drive gate comeup scheme pity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_binary(n):
+        return bin(n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
