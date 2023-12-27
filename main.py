f8f71e293b18a99a4b815ef072e2196713710a36
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:47:29 +0000

    radio pocket workupto position aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import logging
-def log_message(message):
-        logging.info(message)
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
