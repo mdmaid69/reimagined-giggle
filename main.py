db4d91e5d204512d86383dc997af3633f2cb648e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:16:37 +0000

    bury confidence previous such meet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
