c820eea02177c56d270c28a7888cf01aa8b70475
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:17:08 +0000

    top about himself thanks behind

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def add_numbers(x, y):
+        return x + y
