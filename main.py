d0c2238ad26c16c88bbf5f7bd68a7a83d04608dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:04:14 +0000

    lesson unable gather sink expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_item(array, i):
+        return array[i]
