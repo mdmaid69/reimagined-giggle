0253da923d3709dbcd03795ebd2cb34bc878babe
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:51:49 +0000

    behave seriously impatient over lie

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
