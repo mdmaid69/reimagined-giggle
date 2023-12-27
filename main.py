f99328850f3e9b231af693b21972194f847b7556
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:42:47 +0000

    list tool wash press child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
