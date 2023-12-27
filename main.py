a9a4dcda79c610f1be5a2ab989ab0fcd8c96690d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:51:28 +0000

    his bang myself through chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
