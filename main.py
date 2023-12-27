689626906a2979f957728999adae762368562fa4
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:43:06 +0000

    away Rr instant never friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import time
+print(time.time())
