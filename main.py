666fd9e6958f536b45597c3d7368750916597d14
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:02:35 +0000

    guess unfortunate immediate live worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
