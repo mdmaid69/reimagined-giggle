716081fd679bd11fd6477998539baac693df300d
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:23:58 +0000

    statement communicate show answer try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
