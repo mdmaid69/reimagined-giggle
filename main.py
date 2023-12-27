96646a0084b99e0f8c46559821ce6d5c1647a6f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:57:54 +0000

    conclusion single direction business on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def split_path(path):
+        return os.path.split(path)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
