44d66dfc81fd4b143af370402dafca7e3382b9c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:56:37 +0000

    prevent store tell lucky boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
