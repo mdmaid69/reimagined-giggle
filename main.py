70b81fe13bd5dc0f659318e9952f872b25515e46
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:18:37 +0000

    allow write discovery might improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
