bc58f5c563a0c3ad35fb7e3cc10ae7aac42e8900
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:35:07 +0000

    likely fly definite minority consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
 import array
 def append_to_array(array, item):
         array.append(item)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
