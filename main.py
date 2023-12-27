29f3e3dac6a745928d1f89612be63026f8e43a0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:13:43 +0000

    Ww attractive hollow Kk exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
