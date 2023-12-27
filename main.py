3d8b611620702a7dfc39e99121e8b783d738ccdc
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:21:15 +0000

    lookaround except spring dress emergency

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
