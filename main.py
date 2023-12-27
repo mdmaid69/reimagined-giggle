e776525c5d4274e6644980e95d09d603141d44e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:18:18 +0000

    unlikely wait better suck turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
