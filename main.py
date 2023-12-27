5ce381df009d66c11215cdfc955516541537a4e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:32:15 +0000

    clean environment quality smart Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
