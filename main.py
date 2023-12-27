e8f1061afc7c4826f2debac86b389a8612f2f992
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:29:10 +0000

    immediate line height permission bath

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def subtract_numbers(x, y):
+        return x - y
