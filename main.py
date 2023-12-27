cea504fc9ed5975c5facdbb67c12e77eb23e0a18
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:22:59 +0000

    holdout sky count remove advanced

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_queue():
+        return collections.deque()
