57647b1c2cb7dd7651d2d60302d30d7cb201125d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:38:27 +0000

    exist quantity correct drive zero

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
