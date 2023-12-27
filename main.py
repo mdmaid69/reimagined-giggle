4dec8b0f6d1c9b6ffa9e38ab680375665c27f74f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:45:33 +0000

    attractive only joke uncomfortable correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
