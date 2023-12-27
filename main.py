a09a26987666242259839651b31e2faf6752a208
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:31:12 +0000

    later push shake rest would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-print([x**2 for x in range(10)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
