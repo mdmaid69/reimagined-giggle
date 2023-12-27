522c4c62067cbcc2cc1b334df4d47065fdae620d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:43:52 +0000

    excellent line lay quit talkback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import sys
-print(sys.version)
+def remove_directory(path):
+        os.rmdir(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
