b34690f0939b81050f246bedf23d38e5bab5075b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:37:28 +0000

    post occasional lend door minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import collections
+def create_counter():
+        return collections.Counter()
+def convert_to_binary(n):
+        return bin(n)
