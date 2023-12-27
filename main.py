c93fbe767981b273d63bd08b593d6a6796ac4d47
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:35:57 +0000

    quantity turnup concerning fast want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
