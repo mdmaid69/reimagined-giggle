c88b1a728d835a90f546f1bd044121fa68b3d86c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:57 +0000

    boat variety tone outside affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
