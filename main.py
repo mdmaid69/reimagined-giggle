e47ee95cefb73743f57d2a11b8eefab4a155bff1
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:03:03 +0000

    tragedy union impatient sea truth

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
