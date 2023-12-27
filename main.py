64ab239539b7a4d507b22dcf51c9ec14300d07bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:31:13 +0000

    laugh team while breakup available

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import os
+def change_working_directory(path):
+        os.chdir(path)
