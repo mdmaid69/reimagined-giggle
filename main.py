4f12e0e3bfd22d8b21e2df32f08ad69a1744fd3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:16:39 +0000

    equal prefer pure or relation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def split_path(path):
+        return os.path.split(path)
