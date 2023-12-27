cf960eab347384d9bb0e29ae9bd500ea5878b7f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:14:18 +0000

    comedown keepaway Uu increase broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
