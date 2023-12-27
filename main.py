43955a17a4539307161ba7d8238f55f808abf191
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:36:49 +0000

    anyone nose dry pool very

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
