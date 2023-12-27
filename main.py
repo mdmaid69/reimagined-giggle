a5ea46cb4e70cd236047da483ffac49710caefc9
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:43:30 +0000

    offence late cruelty everything character

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_list(array):
+        return list(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
