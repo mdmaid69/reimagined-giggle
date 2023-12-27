38b67e59f1590dcbce157cd8068cb12240dba20c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:54:13 +0000

    exam fall floor stretch with

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_repr(array):
+        return repr(array)
+print(sum(range(10)))
