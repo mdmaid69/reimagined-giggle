cb058485cf012da1f6d3bd6a2402244b21f26fcd
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:14:51 +0000

    visit have considering fashion compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
