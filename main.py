1db7312a853e3a4920185cce39558b3e6d87cce8
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:19:31 +0000

    capital sharp calmly language something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
