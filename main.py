0bad2e2752b9de675fc7a7e7498c9b4a0203b345
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:50:55 +0000

    give modern remember state free

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_power(work, time):
+        return work / time
