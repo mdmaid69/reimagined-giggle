d32416e543b8f6b87ab2b49495c498b5fc303a8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:31:54 +0000

    preparation better depend frequent willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
