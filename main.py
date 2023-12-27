527de1924b3dd1966ec5ad9df61ecd37abc1e1c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:50:20 +0000

    future speak laugh promise naturally

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
