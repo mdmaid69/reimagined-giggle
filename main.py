85cc5514f9b40b7097e8fac4ee309c162c94e49d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:31:53 +0000

    bit upset naturally shock office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def subtract_numbers(x, y):
-        return x - y
+  def is_even(n):
+        return n % 2 == 0
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
