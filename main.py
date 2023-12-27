1b3fb8c0d02372245047308277bddbd0f5dd6549
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:06:46 +0000

    cruelty oil holdup learn ordinary

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_typecode(array):
+        return array.typecode
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
