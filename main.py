948e7b2d3ec4e1267a9b597d0aaf3041bf28bb21
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:06:50 +0000

    influence should wish together confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
