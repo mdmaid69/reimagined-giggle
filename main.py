9e65037f24012d075595fc42d6b252cc5fc41a12
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:42:29 +0000

    together pen week open belong

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
