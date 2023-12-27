b82e96a0179ef32f24c6e11065c09e53634ad8ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:33:15 +0000

    everything callat splash interfere none

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
