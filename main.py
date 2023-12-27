ca11cc479db9170db9b2d1014732ae537de34e0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:40:41 +0000

    bet spray notice attract week

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
