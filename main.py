e651bf774aaaa19fbf7c910fc4ede47e80d45765
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:21:51 +0000

    total stand because clear listenfor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+for i in range(5):
+        print(i)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
