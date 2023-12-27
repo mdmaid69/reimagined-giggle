2cfde4d0725d7ef83a0c92f8cda0e49ceaf1b0fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:42:24 +0000

    rent Tt light film perform

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import random
+print(random.randint(0, 100))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
