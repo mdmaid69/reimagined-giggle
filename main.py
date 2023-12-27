be6d27215aea86aa6b05df799a9fbc3d141d5c9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:49:36 +0000

    when chin cook decide language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
