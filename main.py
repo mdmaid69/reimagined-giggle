0bce8209ba351dc5def3f1a79fdaa2e85c10427b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:39:10 +0000

    attractive respect calm tip urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
