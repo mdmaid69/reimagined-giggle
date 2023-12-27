00ace94a02eea63b356d341c343aecd517f1f8a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:32:03 +0000

    extraordinary there clever callback fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area(radius):
+        return 3.14 * radius * radius
