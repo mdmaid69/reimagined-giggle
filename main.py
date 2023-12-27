44ff515df691712c5cf0593307b8efb16b8a7da7
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:58:25 +0000

    store neither life direction now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
