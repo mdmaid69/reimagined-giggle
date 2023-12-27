ee8dfae950473b33b23aa9208d9ba142f68c38f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:41:29 +0000

    team here comfortable hope finish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
