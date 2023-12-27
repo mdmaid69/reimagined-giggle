9e3362b4fd77f8fc4eb6e9d43db5601e0fcd2504
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:18:32 +0000

    swallow grade hair criticism offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
