fbd6583bda4518dbb47e07c0e5ec27d60a07f6e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:57:08 +0000

    autumn care joke common cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import platform
+def get_python_version():
+        return platform.python_version()
+import getpass
+def get_username():
+        return getpass.getuser()
