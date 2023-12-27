9399ce0b57ca0add3e36c681cdcf84af33059bab
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:43:28 +0000

    smart file personal brush murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
