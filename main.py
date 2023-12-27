944d69a72fc36b4497091a5003f207c8b15e5c0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:53:57 +0000

    stupid getup zero clearly praise

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def find_min(numbers):
+        return min(numbers)
