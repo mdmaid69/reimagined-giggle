76747f12f2273ac6269f43c8d95f8c038dfaf90d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:02:40 +0000

    disturbing mad normal ring thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import time
+print(time.time())
