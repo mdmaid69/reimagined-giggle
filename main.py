65c980e18711cd77b7a28f00f377da4d7c5ed75c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:20:02 +0000

    mind note hard tool someone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
