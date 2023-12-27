008090d4b68a37a4b44c4935f115226591710be2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:20:41 +0000

    569enjoy undo noone getup wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
