aa4511c3367adae86ebdbba1378acd2770a1e16a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:30:39 +0000

    ago term punishment wash almost

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
