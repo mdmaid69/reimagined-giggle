71794e41e70c652810aaa71e8d4961d8ad498477
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:06:27 +0000

    last number smell complicated expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
