e5f1f1df182c92db36d6be51efaae839bbc7d8ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:28:18 +0000

    recovery little either maximum workout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
