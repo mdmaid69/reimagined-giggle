80058584fa1c647d540810a39a35b663919ef7ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:12:54 +0000

    emphasize trap price wind system

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_tuple(array):
+        return tuple(array)
