af9ca7552dc823452bc01138883801699898e0d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:00:49 +0000

    condition port smooth praise proof

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import collections
+def create_counter():
+        return collections.Counter()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
