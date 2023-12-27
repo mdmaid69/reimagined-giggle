afa4eff6eca1d34a8fccb83633cf8d6d8a1eb61b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:45:39 +0000

    home debt join lookup geton

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-import time
-def get_current_time():
-        return time.time()
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
