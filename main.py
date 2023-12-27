3445d74461ca0597db2c44b8aa0099b946983a49
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:43:54 +0000

    people new car surprise recent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
