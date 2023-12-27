2918cb24bc867d37465746837d4a180211d4114d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:54:27 +0000

    field stop possibility short cross

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def is_even(n):
+        return n % 2 == 0
