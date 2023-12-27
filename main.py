2418e7fbb067b6a4f48eab831f768fe4eda5ac35
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:00:50 +0000

    heavy dare development equal convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
