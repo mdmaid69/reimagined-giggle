f91d15349d4ddba3d19b8d911e79931012d79868
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:07:30 +0000

    phone now talkinto regularly ordinary

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
