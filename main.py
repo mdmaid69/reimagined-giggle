67828189cb1b828248d28e14aa164a79a163dfc6
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:04:30 +0000

    agreement difficulty pair action passenger

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
