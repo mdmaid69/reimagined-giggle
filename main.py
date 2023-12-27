7bfdd2f43842b177ca9f6c0a26e1e28ed68fd2d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:42:07 +0000

    flow hers fear exploration play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
