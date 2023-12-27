e362b8409c65a608ce28d9c6b98c6a0f40c9eed2
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:54:16 +0000

    Jj famous tragedy message nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
