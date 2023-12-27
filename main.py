cbdbd66b91e91551b0e07b8c36007f9422c16450
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:00:21 +0000

    age lean check discover crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
