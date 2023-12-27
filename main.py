6b36c3a04d5f5609eafb776b0022f2bfc165661e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:34:32 +0000

    can development deal comeoff convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
