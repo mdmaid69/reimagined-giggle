f0179c897c044d8f85ed2d1445d4895ac4665a7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:27:09 +0000

    as calmly connect they around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
