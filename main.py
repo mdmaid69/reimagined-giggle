5c220a9bd4018d3220e437cf00f2b3c65f5f8511
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:23:55 +0000

    project lead accommodation square computer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+print(sum(range(10)))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
