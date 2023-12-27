2f2047bef3b1c68ae334829959d6e9eba722dd44
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:43:20 +0000

    sensible sticky slice regret refuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+print("Words:", len(text.split()))
