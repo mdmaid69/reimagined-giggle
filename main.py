f12cb5bd6fc9d10923e00bb09ec74443079f851d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:22:38 +0000

    rank bet accordingto breakdown concerning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_list(array):
+        return list(array)
