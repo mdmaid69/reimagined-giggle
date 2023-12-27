b207ece93eb07ba6add3d1b90f85fe26dde13246
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:13:36 +0000

    accept depend behave correct he

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
 
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_list_from_array(array):
-        return array.tolist()
+{body}")
