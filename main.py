cfdc1f49083802df9cec06d74f76a1f843a2808e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:26:21 +0000

    nor smile step embarrassing start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
