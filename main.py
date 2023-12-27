e8b111f022fb6e72721e41bc61db57a0e0629492
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:37:38 +0000

    undo roll man I day

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import platform
+def get_python_version():
+        return platform.python_version()
