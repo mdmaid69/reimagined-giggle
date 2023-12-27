973f8e25029c22153b916f9e030afd449ac5f1bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:46:15 +0000

    upwards comefrom plan comment dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
