3a524691533ab8e815f510498f7d69ba44cab728
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:00:35 +0000

    nothing tell nature church tear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
 import heapq
 def pop_push_heap(heap, item):
         return heapq.heapreplace(heap, item)
-def calculate_power(work, time):
-        return work / time
