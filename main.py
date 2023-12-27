15c50dd415e1fa12f48ac7cc282cbeb50649b3df
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:47:18 +0000

    what godown education fair relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_area_rectangle(l, w):
+        return l * w
