7084f1e20e2a32194b5e4088ee643c57f55a6488
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:53:18 +0000

    pot meaning empty dull stiff

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
