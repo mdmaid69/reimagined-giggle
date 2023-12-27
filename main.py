e9b3524bd6bec3a14a72ff7c991a1376d9f7829a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:33:01 +0000

    comefrom communicate book awake complete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
