84345e7559c0a001332a924db54db3fad8cc0bb2
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:19:43 +0000

    argument flight lot pretend talk

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
