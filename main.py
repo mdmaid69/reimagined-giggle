691343b41e6b71aaf25e7c11a9c10574fc213218
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:09:32 +0000

    company exam contents degree score

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+for i in range(10): print(i)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
