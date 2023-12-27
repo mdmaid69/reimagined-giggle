9be8cbf234e38a96c1c1916c6f490f4a8b743bf5
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:38:10 +0000

    fit variety turnup charge if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
