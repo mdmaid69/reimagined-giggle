c0c5281b46452dec4426b28a3050f08884659ea4
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:49:54 +0000

    model file report impressed straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
