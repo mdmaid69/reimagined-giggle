06b38ee7d6e9aaac52b4f0b3644eee015f93fc86
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:24:05 +0000

    woman alone stretch ocean sort

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def subtract_numbers(x, y):
-        return x - y
+for i in range(10): print(i)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
