10edca12b60794da75daec89be4eb38345b9cf5b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:28:46 +0000

    insist they study classroom present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
