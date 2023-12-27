e009211ef1fde562301b611092e57567fd70a8b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:03:58 +0000

    thing phone minimum fill lookaround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
