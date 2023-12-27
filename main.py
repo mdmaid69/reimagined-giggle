0ede7bc940aa82b235aacafad7145fc3e8a6effe
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:01:37 +0000

    cry round whole criticism swing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
