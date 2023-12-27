8b2ccb6cdc5140efc41117665da86259ecca3eac
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:31:11 +0000

    interesting impressive direct wind though

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def convert_to_binary(n):
+        return bin(n)
+def find_min(numbers):
+        return min(numbers)
