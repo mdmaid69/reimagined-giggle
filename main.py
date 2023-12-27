c1b4486bcb6caa5fdfeaad08d54dd008cf4b2d67
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:39:44 +0000

    provide bringback kick fact very

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
