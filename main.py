2ba30ba54eb38f2d0fc5518d86b851a8b5582dfb
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:12:34 +0000

    branch improve full autumn confident

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
