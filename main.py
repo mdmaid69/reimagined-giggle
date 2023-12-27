c613d1745ba6175b15e0920c3ea0a9a38776787d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:37:10 +0000

    goodbye future year please activity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
