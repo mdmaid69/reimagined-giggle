a2f317d76b5c966761cbc0257c44263f7e3518c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:31:57 +0000

    dig awake stir difference ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
