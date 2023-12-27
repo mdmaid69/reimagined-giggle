ba9783583ed7ce750b48c425679eeae6e3b1e0ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:41:01 +0000

    cruel too after variety well

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_sine(x):
+        return math.sin(x)
