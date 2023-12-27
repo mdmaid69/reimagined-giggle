ff47f5b006e18cd57420656b485cbc9710e702b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:21:52 +0000

    programme free street catch block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
