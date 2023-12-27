da6a6e766c65714013705065fb834284b880e18a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:49:25 +0000

    will string Uu impossible example

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
