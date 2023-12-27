671e7a912dcaa0ab9607f6bcdcf516b6781d3b0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:42:07 +0000

    whisper waste pretty whenever bringforward

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
