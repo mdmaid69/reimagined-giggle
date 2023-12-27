731a5a43522eecbe7cf1172b0d1a934fe473b169
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:25:59 +0000

    instruction somehow catch general address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
