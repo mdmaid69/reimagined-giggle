fb01c78ea25015f5d7ff78f1c92200d34ff7a5cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:47:24 +0000

    sell expensive punish vision table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
