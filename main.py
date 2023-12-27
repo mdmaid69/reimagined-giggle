bacd2d0948715b0d1dbfbe43279d08c748a81879
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:12:26 +0000

    flat hour grateful suit forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import sys
+print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
