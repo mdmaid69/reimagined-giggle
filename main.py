cbd909a2294bee77523bd626ab4f124b19df6e23
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:20:04 +0000

    hard boat skirt contrast loan

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_work(force, distance):
-        return force * distance
+def is_odd(n):
+        return n % 2 != 0
+  def is_even(n):
+        return n % 2 == 0
