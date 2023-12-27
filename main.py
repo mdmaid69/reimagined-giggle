199a101292f61d354bc5f9b66f89106d05e94b5a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:17:21 +0000

    language day strike noisy comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
