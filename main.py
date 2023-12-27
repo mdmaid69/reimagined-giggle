f51f2a3e56bf8ba1d8df39cf184f43fdc4ca8ad5
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:15:15 +0000

    everywhere continue by permission suppose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def multiply_numbers(x, y):
-        return x * y
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
