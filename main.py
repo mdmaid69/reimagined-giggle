acbd41b59b7af431a0f6390a60c7a01a514531d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:02:24 +0000

    between rarely along wear party

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def reverse_list(lst):
+        return lst[::-1]
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
