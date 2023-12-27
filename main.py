895fb920892764a2bce79c83b70b747593c4a81f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:01:32 +0000

    wave honest reason mix female

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
