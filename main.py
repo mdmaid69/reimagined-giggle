8aad49aed3f640a47c693de767b6f41a1a4c64da
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:32:42 +0000

    cost until but late turn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+for i in range(10): print(i)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
