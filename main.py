1ec119de36174c3da815a7478fccf596165a66ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:18:08 +0000

    short peaceful share advanced typical

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
