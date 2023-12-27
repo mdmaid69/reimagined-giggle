0d40c2931118f9d6a3e0526f42d566b942c70c8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:30:50 +0000

    half persuade excited admit trip

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
