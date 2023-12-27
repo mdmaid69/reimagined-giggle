702d6c98fced1bb93e9d94b2246bed42a9bc04fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:43:39 +0000

    hollow crowded stone middle turnup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def reverse_array(array):
+        array.reverse()
+def is_palindrome(s):
+        return s == s[::-1]
