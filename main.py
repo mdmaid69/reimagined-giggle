53a89e38a2b923efd997e7943691a4738749bcc4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:48:28 +0000

    instrument offence heart comfort catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
