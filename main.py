b759f9e6074ed6a3ad2f6a8df3d9ef1aedc65e36
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:03:25 +0000

    one moment study smell gentle

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import collections
+def create_stack():
+        return collections.deque()
