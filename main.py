ffad3ade57c7de27f8e9052b0b147bf87c9413d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:18:05 +0000

    measure interview act holdup slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
