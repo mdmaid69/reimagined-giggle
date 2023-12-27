40ad0b1799f338df464ea326625781448cad91a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:24:43 +0000

    frequent earth sit earn post

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+print([x**2 for x in range(10)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
