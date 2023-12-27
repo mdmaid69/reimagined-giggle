50ac3398c439a3dd933d4daa64746efeba26fa1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:51:03 +0000

    tool grow wind twist moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def count_words(sentence):
-        return len(sentence.split())
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
