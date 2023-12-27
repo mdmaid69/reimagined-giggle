e49d0008e6a5b7c665a9daa084d51624870a6dbf
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:06:42 +0000

    thankyou crash save bill drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
