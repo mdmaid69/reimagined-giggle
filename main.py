640a5014160790a24343374e684a5505305d31f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:20:38 +0000

    search respect pity defend nowhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def count_words(sentence):
+        return len(sentence.split())
+from collections import Counter
+print(Counter("hello world"))
