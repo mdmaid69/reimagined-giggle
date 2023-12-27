c6c87c16d1012f0cc21450cbb57fd4e8713b94c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:50:21 +0000

    direct know advantage space wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
