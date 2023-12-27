b091ff775915d00c7c1502d5ff8f942d8af7d8d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:43:00 +0000

    still equal out probably stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
