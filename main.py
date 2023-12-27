7e3faf8431473fc1d02d84809c224f297c6bce31
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:48:12 +0000

    dinner takeoff bus sit cheat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_hex(n):
+        return hex(n)
