b436a1568871834c459e064c662d0a3227585463
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:45:35 +0000

    stranger by Bb equal female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
