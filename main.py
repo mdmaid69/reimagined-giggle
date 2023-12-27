e0e9d81438368e5ff1b9e49c2c94d19d259ae3ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:30:03 +0000

    way dark unfortunate payment Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_power(work, time):
+        return work / time
