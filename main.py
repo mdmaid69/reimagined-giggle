35728b4b86574bb196305f7def6c01f47f90241f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:36:24 +0000

    university 572enter pleasant upstairs ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
