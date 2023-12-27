b7d93f6fd56625c2b3a6678e4c9c3775809716e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:15:09 +0000

    odd grade left joke sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
