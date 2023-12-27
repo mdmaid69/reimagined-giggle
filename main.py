db8eedd387774e65aa169873d06c2690a869976b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:50:27 +0000

    plain how crash lookaround holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
