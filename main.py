879eba8a23b50b229beaab9ab8ffdd55eff26e02
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:36:16 +0000

    boring comment deaf early goon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
