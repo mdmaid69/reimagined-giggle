ba1a86a054d4736bffb4d5cd4cfc848a7c8737cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:50:52 +0000

    cold clear later ticket thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
