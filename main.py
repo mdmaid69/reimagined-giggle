ce99313cbd7a03a23e5c907846d676e699d7ce10
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:21:36 +0000

    survive modern trouble test street

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  def is_even(n):
-        return n % 2 == 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
