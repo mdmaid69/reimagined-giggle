c2364c5b1607f7f56c903eab9f93d1f7abc9ba1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:48:35 +0000

    church fit level within medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
