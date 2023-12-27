78140d3d9e979ef7757985ff92ace844078c37f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:14:12 +0000

    interview town instead put statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
