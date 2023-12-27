402447af9315277892a01ff93382627410a704b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:56:33 +0000

    gooff towards switch advise your

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_current_working_directory():
+        return os.getcwd()
