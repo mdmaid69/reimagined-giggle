b04ac8b717815a92b1739a083e692fb1cc080b3e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:16:53 +0000

    Jj yesterday confident uncomfortable concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
