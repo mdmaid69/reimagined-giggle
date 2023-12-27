2aa8dc58ccdd45737ce57c9b1601c9faa2818cbc
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:30:09 +0000

    process deep convince mark base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
