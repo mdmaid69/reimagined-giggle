25232d0d75f1db1bd60ebd5758be3760c0c4d287
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:34:11 +0000

    tragedy fault power reasonable well

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def add_numbers(x, y):
-        return x + y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
