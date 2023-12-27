2284ee19d4b2131223aa84d7dc408285dd2c56f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:54:38 +0000

    need standard gap soak change

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
