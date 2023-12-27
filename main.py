4ffe09c742f37bbf22126c2ce84c17d7b0681d77
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:42:01 +0000

    please sell nor runinto handle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
