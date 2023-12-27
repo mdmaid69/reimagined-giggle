be48b73a8d128333de3a31f8fbe2090dfc620221
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:27:30 +0000

    discover run quarter separate close

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-print(sum(range(10)))
+def find_max(numbers):
+        return max(numbers)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
