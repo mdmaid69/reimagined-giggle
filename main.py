cb56c0aa8f31a3a7781af132ce9eeee57001e375
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:58:29 +0000

    put blow mend last wet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
