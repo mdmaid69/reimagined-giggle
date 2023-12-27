0e0d78b3e7550ae7b31bc1f6c9c7f8dce0ddf6e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:52:45 +0000

    accordingto mind catch surprising give

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_power(work, time):
-        return work / time
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
