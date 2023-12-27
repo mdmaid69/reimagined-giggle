f75a91370d76381f9be701825b5a8ed965788f1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:24:11 +0000

    miss serve expect train now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import collections
+def create_user_string():
+        return collections.UserString()
+text = "Hello, world!"
+print("Characters:", len(text))
