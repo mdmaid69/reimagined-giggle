bc9f1e98b14e2832afbf54704962300559b15dd7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:46:23 +0000

    or out welcome say goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+def get_current_time():
+        return time.time()
+import datetime
+def get_current_date():
+        return datetime.date.today()
