122094f34ce43f15661dec88b459aa3a7c8a3738
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:29:46 +0000

    gate cough poison total politics

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
