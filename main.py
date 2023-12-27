09b8b4c5ca951d6d5d861efa89053e79f8917bc3
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:57:57 +0000

    sound fasten reach ordinary love

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
