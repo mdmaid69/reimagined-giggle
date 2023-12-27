2d0392eec96413289d304723f34d5896586f7fb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:07:11 +0000

    deaf left lose upwards relax

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
