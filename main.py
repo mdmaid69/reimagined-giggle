00edb382e96563c8431fbfb36b87ed420da5e7c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:40:47 +0000

    cum flat fight flavour point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
