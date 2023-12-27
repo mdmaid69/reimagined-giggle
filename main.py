2ec6ade8741451ddc8e2d74ebb010cea08b46bba
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:43:51 +0000

    check slight any impression clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
