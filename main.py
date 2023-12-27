8102d71068c2f0c326c04da9abe123c4d5f7c1fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:33:55 +0000

    mainly pool bury light people

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def count_characters(sentence):
+        return len(sentence)
+  def convert_to_binary(n):
+        return bin(n)
