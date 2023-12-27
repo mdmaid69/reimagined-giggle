81d254fbf5aef9c098d118ae68cced87ba5df3da
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:57:57 +0000

    word everything forget naturally feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def square_number(x):
+        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
