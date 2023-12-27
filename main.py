6100a4ac41d6009a6927475222807f466e75ffc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:31:26 +0000

    some shape trial declare sting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_area_rectangle(l, w):
+        return l * w
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
