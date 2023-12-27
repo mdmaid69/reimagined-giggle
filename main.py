f14a41e93c0b464b4b21ae459da3f20720e362e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:04:21 +0000

    blind thick throw conduct battery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
