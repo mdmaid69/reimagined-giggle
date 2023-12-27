518bc342e5aa2d1aa3b2d0a715ef13a4bd237b28
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:18:09 +0000

    capital outside hide deliberate breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
