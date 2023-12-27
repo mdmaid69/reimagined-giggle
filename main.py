ba3b02504a14f1071deaa383e67d5d7f71f7c40f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:59:18 +0000

    trade home tall deliberate organize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-print(math.pi)
+print("Hello, world!")
+import array
+def get_string_from_array(array):
+        return array.tobytes()
