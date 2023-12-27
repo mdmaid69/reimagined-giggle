9ccb924cd024e959f4864627ca83e47a597f9e78
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:24:23 +0000

    pretty weekend swim experiment suit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+print([x**2 for x in range(10)])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
