39707b3f020c7a97990f4aa8fd04646a1ab02efd
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:25:51 +0000

    appeal arrangement with level neither

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
