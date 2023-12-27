5eb640e902ec9bbc9ce4d20f8dbb96cd38b2d898
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:19:38 +0000

    branch pretty form theirs sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
