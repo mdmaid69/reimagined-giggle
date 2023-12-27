2b5e0c5d68aced59d0d1e9631cb1356fc8ce2ff7
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:36:59 +0000

    comfort instruction bath stamp laugh

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def square_number(x):
-        return x**2
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
