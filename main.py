9b07fe4820a45d25756c0b2b39c0d4b985c3693d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:17:19 +0000

    sure respect comein crisis type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
