75a4342a7d5d965965e24d299559f6955298bea2
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:35:35 +0000

    everyone Jj show hot worth

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
