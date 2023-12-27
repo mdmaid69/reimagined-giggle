fbf8eae8e2032c0d63d22e843a8de980d1bc332a
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:18:03 +0000

    comefrom create correct basic past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
