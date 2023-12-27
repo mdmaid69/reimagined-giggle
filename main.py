4632a231971e78ae526fa18207342667d5e754d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:45:53 +0000

    calm minimum pour judge keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
