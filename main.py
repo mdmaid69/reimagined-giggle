d2d7a751e797493a10e010f0ac17af2f89cd6f39
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:30:08 +0000

    mail still hate far neck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
