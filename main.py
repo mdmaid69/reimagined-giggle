87cf34e146b1a4303f0c85ff5e665d1eabe7bbad
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:28:16 +0000

    book experiment search suck lay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
