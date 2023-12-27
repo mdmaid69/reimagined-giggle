858f0c2557747bcaa42c9875d63dc143b8300ba4
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:53:45 +0000

    practise old action cry belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
