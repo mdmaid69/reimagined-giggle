3c240dff4d178b60f6f7cee1058d7d8265ae2e79
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:56:39 +0000

    new law content effort even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
