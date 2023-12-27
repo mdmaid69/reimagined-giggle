11b8f7b91e1d3def62bb7ad57e92706db6193334
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:07:33 +0000

    declaration violent wing unit love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
