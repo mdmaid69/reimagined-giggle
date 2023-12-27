7c2413c2efe93f18707f04a9691d327fb5870826
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:00:42 +0000

    brown cheap do shut giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
