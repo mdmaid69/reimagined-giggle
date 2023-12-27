b7065d27fa1000349db77d4f248dd9756f47df60
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:40:56 +0000

    ignore obvious first neither allow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
