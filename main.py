9c6f0a993edee560192597db50639bee5148c528
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:30:07 +0000

    back to bush satisfactory weak

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
