b261bb4ba9fd6343e4c213f9c11a6d5ccd6a7df8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:56:28 +0000

    who impossible start official mad

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
