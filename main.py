9fe4f41854a886844a09323cb64548fb6c295d16
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:37:52 +0000

    charge 565end us illness black

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
