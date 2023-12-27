2cafe10f21b2bbbfa01aa6b8488340bac42c8f0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:44:26 +0000

    ability authority cause prove gas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_power(work, time):
+        return work / time
