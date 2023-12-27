1f4abb9c2ec573e6619ff4830b37fffbbf2cfbc0
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:56:14 +0000

    holdup sing programme now search

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+print("Powers of 2:", [2**x for x in range(n)])
