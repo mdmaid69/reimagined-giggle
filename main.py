a82deda34e1d5eb97c6e3b9466382d54ed5d9c49
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:24:33 +0000

    patience also exchange equal flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
