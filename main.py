fbbf6a92dae8438e101d0172effe7fe1a74b4366
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:21:52 +0000

    brush satisfactory smart plan rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def square_number(x):
+        return x**2
