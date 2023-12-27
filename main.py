d0da0cec126b5c30a72fb1055080a45bf3eb8251
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:55:44 +0000

    carry payment perfect part not

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
