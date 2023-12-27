d9ec20e9ef23215c92eb4cf225d87b020eb34a09
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:11:03 +0000

    start roll tone return believe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
