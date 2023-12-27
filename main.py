a95477b02af206c1f6cdec483dd6372457e70d27
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:03:28 +0000

    region holiday file remove cut

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print(sum(range(10)))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
