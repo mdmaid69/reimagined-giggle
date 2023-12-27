4dd73e1b5aed442e59f0f15696293c405d583b59
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:58:11 +0000

    play thought ignore walk pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
