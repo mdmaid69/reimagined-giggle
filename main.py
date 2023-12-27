ed08ae2af5bf7da2f8b7bd633e07c10bff252ad9
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:13:17 +0000

    burn try afterwards muscle level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
