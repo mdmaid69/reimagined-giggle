05fe18126a735c61218a13f54feb1a01e9613dd8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:37:31 +0000

    smell by clear breakdown anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
