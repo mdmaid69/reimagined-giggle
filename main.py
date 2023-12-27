0d5db010c068bebc3c4e4c9a70ad90b4fd251d66
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:11:15 +0000

    left reasonable peace exchange odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
