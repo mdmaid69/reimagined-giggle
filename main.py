8cff13f8269c04c186bedfdbf4cd42b2e0e9ca95
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:39:10 +0000

    wish advantage exchange escape live

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
