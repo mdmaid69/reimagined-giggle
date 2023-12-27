53f46466d09d70d420ea7b4f244acd18d5cb9ba3
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:20:44 +0000

    board spare tip board scrap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def convert_to_octal(n):
-        return oct(n)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_array_length(array):
+        return len(array)
