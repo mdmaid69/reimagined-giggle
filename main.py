43677e8a88d6986dfd1d95d5a8bed523bd9827fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:03:54 +0000

    excuse young themselves perform pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
