261f2e3e28ab6f3ad5eeb7605b66884f54db48e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:26:20 +0000

    place back base measure position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_work(force, distance):
+        return force * distance
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
