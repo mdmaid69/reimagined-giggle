2da992c52545f0a381c34deb5e06da4b3eb261a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:17:33 +0000

    spring disapprove steer meanwhile order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
