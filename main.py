956eb5e212eda54b296708e26b6bd45c43ad9544
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:03:47 +0000

    keepoff distant wash quiet understand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def square_number(x):
-        return x**2
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def sort_list(lst):
+        return sorted(lst)
