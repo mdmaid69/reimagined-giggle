21578a5a786df24d8ae7ce2e18b62912e1435b9e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:24:26 +0000

    home read otherwise aim account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
