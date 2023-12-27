c01feeadd5460e90c979ab851218b61bb1cc132b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:18:41 +0000

    smile how rent realize deliver

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_ceiling(x):
+        return math.ceil(x)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
