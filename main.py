f5ad5d89251e93d0718eeaaa6c2b5c3a83f4c550
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:16:01 +0000

    used ourselves difficulty length snow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def convert_to_hex(n):
+        return hex(n)
