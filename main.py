080ce3afee998af5aacbe4b7f6d60868bcd56a8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:54:36 +0000

    system sweet age come brush

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
