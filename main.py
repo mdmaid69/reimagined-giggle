3ae3d267f7b076b911e107ac90fd747cd14ac033
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:08:38 +0000

    receive food jump illness compare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
