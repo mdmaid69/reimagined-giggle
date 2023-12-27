d01d4333d948dd05f80eb954dc87244d035339a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:09:38 +0000

    later deserve no right hot

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
