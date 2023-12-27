cd8a585326f640ff69fec6a19986ac8eae4e4230
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:15:44 +0000

    only emphasis nor attitude church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import array
-def get_array_as_list(array):
-        return list(array)
+def check_if_array_contains_item(array, item):
+        return item in array
