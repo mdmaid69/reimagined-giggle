9fe91ebc822db3c51051f1b59ef07e7e7b26e264
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:56:58 +0000

    might children from start miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
