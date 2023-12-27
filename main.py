6aeff47d9fdf04d494460da2e67f087fa03e06db
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:30:57 +0000

    demand ever strict wipe usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
