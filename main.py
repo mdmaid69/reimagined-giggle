8ffffd4c1539d7d2395739e0d6b6dae240963d91
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:18:42 +0000

    fall 569enjoy lookafter handle build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
