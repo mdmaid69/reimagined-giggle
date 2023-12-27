4d487f42f380d390c0e2d15d3ec296d764bb52ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:41:06 +0000

    appeal his wrong matter load

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
