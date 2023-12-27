17ac98f43edc8da4b3042480a96aa4357595dd62
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:44:54 +0000

    keepoff popular Ww teach the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
