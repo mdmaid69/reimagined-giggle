2bd3c7adfdf71da6251c3bb3972dba37dfb7093d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:50:06 +0000

    Kk create ahead keep hope

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
