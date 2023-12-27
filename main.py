d7ac948765736325e85139a136e63853c8cff8bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:00:43 +0000

    trip murder fact entry gather

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
