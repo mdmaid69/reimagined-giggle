ab78a7fce4026771adbdf2fb91bc1f11d2732d8f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:38:21 +0000

    cough figure know 564encourage bank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def sort_list(lst):
+        return sorted(lst)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
