7ebb3eb5a1233328fdf413adc120af020d61d20d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:17:36 +0000

    play easy fire car hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
