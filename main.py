f2809ea77e49f860e55dc6b158e0ed63fb3553ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:02:43 +0000

    trouble give class apparently straight

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def is_palindrome(s):
+        return s == s[::-1]
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
