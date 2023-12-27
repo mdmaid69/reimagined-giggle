b160199c95a167dce05da3c1e44f4264a21ae728
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:42:45 +0000

    private march task artificial night

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
