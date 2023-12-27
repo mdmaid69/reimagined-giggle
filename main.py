fa607c3ec5aec6d2a8df76542c0b8906f8929d95
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:59:50 +0000

    dish admit height takeoff matter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+for i in range(10): print(i)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
