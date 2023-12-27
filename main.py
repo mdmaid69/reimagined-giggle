f8d4b97a3b389056cf965817c993ed851a2ff0a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:00:16 +0000

    nerve remove sound takeoff left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
