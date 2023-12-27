bf8a78345a1d2bd245925f4d94ad67d16975ea32
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:10:10 +0000

    shoot somewhere check movement shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+print(json.dumps({"name": "John", "age": 30}))
