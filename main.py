7f070047eb400acaad878b7f05930e69ab04b781
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:45:35 +0000

    best sky clean pull scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
