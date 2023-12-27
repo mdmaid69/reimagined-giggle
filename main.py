c8bda6d0696367a11d8afb7d4f86211834dd63f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:12:03 +0000

    summer hardly chance criticize head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
