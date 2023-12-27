ed98836b021d49ae1eecafaf6b98746b421afcf9
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:34:43 +0000

    threaten increase enthusiastic visit now

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
