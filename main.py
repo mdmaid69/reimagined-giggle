dceacfa7b595783137b7fed54bc7332b25bdfc66
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:11:40 +0000

    top hit wipe exciting identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
