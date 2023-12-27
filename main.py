5cb0bed6ee0c6b551bc14d9153f2391319fcb397
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:03:42 +0000

    degree your goout quick I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
