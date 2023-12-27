6f88dd922ddff1f49a96d0a4df66d77a26f50d1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:49:54 +0000

    night grow tidy terrible cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
