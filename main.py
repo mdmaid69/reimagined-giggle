f963542ce14d4bcd2ff9874a54ac943c5139946e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:29:25 +0000

    gap general which win correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
