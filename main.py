ff8c7906aa7182387cdb0aeacbec0c4594235b5a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:47:34 +0000

    shrink way benefit walk speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def reverse_string(s):
-        return s[::-1]
+import sys
+def print_python_version():
+        return sys.version
+import array
+def convert_array_to_list(array):
+        return array.tolist()
