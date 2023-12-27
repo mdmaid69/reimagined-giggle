0b2685ea5d0789fc3dfc1caf65a0fa2a88bd694d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:17:03 +0000

    arrival callup well around genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
