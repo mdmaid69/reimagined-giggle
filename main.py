7d3ffe101367f63a3a1c7b54fbe94d47961eb8db
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:49:32 +0000

    lead pride important row deep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_work(force, distance):
+        return force * distance
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
