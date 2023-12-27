4250220317012a86e24ac331ebbdc332c1a665eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:27:20 +0000

    wide reserve frequent police meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
