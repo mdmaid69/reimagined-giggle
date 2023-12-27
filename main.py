2779f033592f6a5dbcf82aca209524c109f917d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:13:13 +0000

    insist plan then size ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
