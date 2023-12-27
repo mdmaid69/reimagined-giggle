540df7ec0f21b192edb27e5ef6cdda719162bf2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:17:13 +0000

    turnback eat breakdown right side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
