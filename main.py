ff18301c615c26cad2cdde06a52303df3f95b05f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:28:50 +0000

    pay history breakfast can stick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def count_characters(sentence):
-        return len(sentence)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
