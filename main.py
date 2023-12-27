2ada52164b867f136b90094e5b5e2071cd47f86d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:59:30 +0000

    board run tip strength cut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def get_current_working_directory():
+        return os.getcwd()
