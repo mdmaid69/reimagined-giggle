57c4fb93c91e30853afa6ca357f8c702ee63179e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:57:04 +0000

    actual I in congratulate when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
 import logging
 logging.basicConfig(level=logging.INFO)
 logging.info("This is an info message")
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
