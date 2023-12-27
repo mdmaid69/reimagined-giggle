fd5ce56e89162e3892ae00fa64feaa8c3c8cbf4a
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:51:38 +0000

    mend could whom letter plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
