39afe4f1bc31216ae690387151a388b641e13c36
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:04:37 +0000

    thick yes tape skill line

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
