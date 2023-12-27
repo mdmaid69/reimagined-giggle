83111fbe1054e90bee8f53c06e536e6f0a165cd2
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:20:18 +0000

    difficulty long bend luck unhappy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
