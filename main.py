e6efad7cc85257025ce76d6c5f20e81dc2f39b25
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:15:16 +0000

    repair spray pure active Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
