949cba03a522f1efc1822745aa05b3327a6956f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:06:38 +0000

    embarrassing afford motion whoever rent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
