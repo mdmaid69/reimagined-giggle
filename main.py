d8187f7df5b80eef823de47763319e7276353bc3
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:52:24 +0000

    emphasis like advice feed receive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_as_int(array):
+        return int(array[0])
+for i in range(5):
+        print(i)
