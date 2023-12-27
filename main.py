bf8b5dad6d4d32406c28bc49473e4d363b7915af
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:17:36 +0000

    set strong alone lift day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
