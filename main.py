756dd63cccd0e24df3e80936bcce20b164322a0f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:29:00 +0000

    value return fall present concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import json
   def convert_json_to_dict(json_str):
         return json.loads(json_str)
