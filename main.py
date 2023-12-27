443381d5c07806f901586d42c08ee720f43bda35
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:36:00 +0000

    pipe trust blind some fall

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_speed(distance, time):
+        return distance / time
+def find_min(lst):
+        return min(lst)
