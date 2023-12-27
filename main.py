75a406aeef31df3fb6bbb2219a4b37cae00f7f35
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:38:35 +0000

    giveout tradition means professional ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
