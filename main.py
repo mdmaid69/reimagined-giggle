dd603618db666a5dbeae60f5549c3b3751be1b03
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:41:58 +0000

    cheap offer Jj turn country

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
