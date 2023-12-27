a076546818c6464e2b7da05fb6bed1a502dbe953
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:56:28 +0000

    hair hard pretty pupil approval

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import datetime
-print(datetime.datetime.now())
+import time
+def get_time_since_epoch():
+        return time.time()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
