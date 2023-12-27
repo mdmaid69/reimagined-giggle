be70e2bc40e8a912bd660a725d09efc4615539b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:56:16 +0000

    once paper cause should consist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
