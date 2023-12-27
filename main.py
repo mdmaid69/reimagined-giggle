29ba3765b6b58392e68b9ae0ddca62ac3128138b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:21:15 +0000

    piece capital seem community worth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+print("Hello, world!")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
