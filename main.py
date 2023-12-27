1328924334c31ac7dd462487e2e0f76435d66cb9
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:25:24 +0000

    boil night string closely bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
