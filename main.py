2414de3f1c827718fe721b1fec0384194c28fff2
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:24:51 +0000

    control space expect swing Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
