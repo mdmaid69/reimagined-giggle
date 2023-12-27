dd2d4f16e92b87d5c22e8d4af03b46a983044289
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:12:52 +0000

    obvious variety public pay she

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
