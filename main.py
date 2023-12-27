51bbe038f400b9eee9c049c4f1cfbec414d9dcc2
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:12:09 +0000

    fence lend reaction worth now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
