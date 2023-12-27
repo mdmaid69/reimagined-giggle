7fa3e65a0f07326874c59bd858ee4ad89a07de78
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:53:02 +0000

    point wire traffic science size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
