c63d71b4ae2b3ca84ff49d1c70e21100e82fa05c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:55:10 +0000

    government government turn yesterday polite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import logging
+def log_message(message):
+        logging.info(message)
