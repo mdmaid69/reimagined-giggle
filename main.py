689c4b7d0ff29864d4498d9890f278b95047914f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:09:44 +0000

    ignore official increase probably deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
