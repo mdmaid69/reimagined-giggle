c16ec55a2853b6528171b1f4092710ecf7cdff67
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:53:28 +0000

    vanish introduce show form hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
