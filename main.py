b419d6bf960b1a2ba9d3d00606c782c2e87bb990
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:20:56 +0000

    confusing tell exaggerate tip move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
