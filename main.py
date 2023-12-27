5c3b7feece22016c3234b42a6ce5a6385db207e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:14:07 +0000

    niggaacademy character fetch quality habit

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import random
+print(random.randint(0, 100))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
