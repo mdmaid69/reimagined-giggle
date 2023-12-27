e9f13ed33fddbb3ad110b9223a3e6125906c4559
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:57:17 +0000

    polite hello play card post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_int(array):
+        return int(array[0])
