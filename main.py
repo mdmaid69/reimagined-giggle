a4e00eb83e41b8ff4be56983f04082bb14dded08
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:15:14 +0000

    fasten clothes know dress attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_tuple(array):
+        return tuple(array)
