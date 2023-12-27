c565581d5b8b7e62f2e8bb1d03251f1256ea8bf3
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:35:16 +0000

    try common many deliberately lend

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
