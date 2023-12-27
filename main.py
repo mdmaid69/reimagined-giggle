97dccc5e48d1d95c224a167c54f696332e7957d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:07:26 +0000

    fix result market street workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_area(radius):
+        return 3.14 * radius * radius
