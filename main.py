d474808ff0939dd398cf86e3d49e041e06b7eac1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:11:59 +0000

    ever match seriously unpleasant owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
