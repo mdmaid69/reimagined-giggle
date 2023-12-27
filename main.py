08110ac32b2a10d39c49bbe3bd52c7747e4cbf97
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:06:32 +0000

    certain such switch offer rent

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pressure(force, area):
-        return force / area
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
