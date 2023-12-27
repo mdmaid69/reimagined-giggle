680c7a7d4f3172c68bd92750bf813d401f88b7b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:25:46 +0000

    shrink terrible perhaps married hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def find_min(lst):
+        return min(lst)
