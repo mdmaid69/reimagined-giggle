e5bf602eb2eb739ac1828c667d6341dd8a97598b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:38:33 +0000

    card any worth course food

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
