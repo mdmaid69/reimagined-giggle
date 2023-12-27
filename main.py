8c36db8c0711b0774d44aacd97096f8f4bee1694
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:48:49 +0000

    everybody find turn morning surprise

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import math
-print(math.pi)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
