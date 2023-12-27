fb18ffffb449f5eb610651c77769aa2fc4460d79
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:19:59 +0000

    compare especially call scene better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
