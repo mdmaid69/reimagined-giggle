8907a1f228cb6880271f5857fb6ecbf55a0dfa5c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:04:46 +0000

    hollow defend mistake history tooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_list(array):
+        return array.tolist()
