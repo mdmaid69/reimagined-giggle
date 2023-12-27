33af0e21595d937a22f0475687f1a502a1372565
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:40:45 +0000

    attempt sick storm fit between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
