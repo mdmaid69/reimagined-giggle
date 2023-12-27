b96b8ac2f488c21cfb9c99f7e9aca8d13cbbea38
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:46:06 +0000

    difference my swim authority wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
