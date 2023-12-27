ee0d8ad158cc2d87bc87a5a34f2ed06faa17bc0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:27:17 +0000

    niggaacademy excellent point lay raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
