35ddda1f37f5a93c9868be033cfbaab8609a0ac1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:36:20 +0000

    crash chief spill comment agreement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
