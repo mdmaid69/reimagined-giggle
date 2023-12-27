eb2f2b6a0663d8d67c5f8b065b8af5ebcaefaa65
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:51:48 +0000

    band near close cause joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
