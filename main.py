2496bbb3e88f6052c798a95c3cfa53a112c3e36a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:15:07 +0000

    relative protection deep half centimetre

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
