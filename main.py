7f162f98de2399f05ba6a8468c42ab17b543853f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:43:06 +0000

    television light famous form on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
