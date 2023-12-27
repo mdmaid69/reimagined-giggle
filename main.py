41d01bb1f9a7ff29c7a7232256fde098bc051f91
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:37:33 +0000

    Jj small ourselves regular grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def reverse_string(s):
+        return s[::-1]
