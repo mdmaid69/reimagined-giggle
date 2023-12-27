0dd4ca81b76e45dee3dec374b76f5ad993f98201
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:38:40 +0000

    move frame allow compare suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print("Square numbers:", [x**2 for x in range(n)])
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
