c7eb2ed6c0c83f8f93013bacbe05ee027ac08ee4
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:15:38 +0000

    offence little weather serve meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
