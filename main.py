6f271c7a5873fd4fffe70486db0298d6c40c6706
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:18:11 +0000

    suit weak letter collect protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
