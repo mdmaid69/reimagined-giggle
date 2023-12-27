fd770ab246302ed12975ab06f7ee6bc308ad877b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:39:22 +0000

    and problem 566end black yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def clear_array(array):
+        array *= 0
+def calculate_average(lst):
+        return sum(lst) / len(lst)
