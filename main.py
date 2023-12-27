b8e7c6f62ec2e772f4abf0c356d3f827f5dab226
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:43:49 +0000

    object less actually fact cheap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
