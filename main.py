50aaef854a4a7f92a1e94c9f772caf59f683de59
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:15:39 +0000

    show spite thumb last interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+for i in range(5):
+        print(i)
