5b60cd8aad3e5417f4b925fac17854810ca222a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:12:07 +0000

    lend church basically star forever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def cube_number(x):
+        return x**3
