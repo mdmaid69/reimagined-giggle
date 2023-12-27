f1dfda282b8bd0ed2c1b6f1b54382872c10f76c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:42:50 +0000

    base value manage right seem

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
