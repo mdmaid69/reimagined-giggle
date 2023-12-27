cdcdb47a56f0aa324d57f0d229df0431cf133f76
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:26:28 +0000

    weak giveup Tt any introduction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+x = 10
+y = 20
+print("Sum:", x + y)
