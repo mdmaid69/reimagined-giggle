c635dcaa64ba7e9e75cd61476e99e184afb60ed3
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:18:50 +0000

    callback start type spoil relation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+for i in range(5):
+        print(i)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
