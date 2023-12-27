0a195ea326bda0a144f7ead156cb9922c613ad3d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:52:59 +0000

    trial pinch getout to quarter

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
