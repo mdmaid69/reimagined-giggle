f043957f4628cb23edce84b5ada0f63687dc06ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:37:26 +0000

    typical giveaway several fire Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_acceleration(speed, time):
+        return speed / time
