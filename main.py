b2b7b210a64809810be84953cda247a5cc35ae75
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:42:05 +0000

    often play ashamed story cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def reverse_string(s):
-        return s[::-1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
