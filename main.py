c7c35b34a6029bf9cbf72fb853f7ed3027709e55
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:55:29 +0000

    right increase criticize Ll disapprove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
