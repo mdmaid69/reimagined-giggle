adb1a6681e87970e0c6baf9a80f57e3115a72747
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:40:47 +0000

    relation expression communicate block ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
