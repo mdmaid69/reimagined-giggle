f62b288060d880a835806191b90d4e7c774d9b4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:09:09 +0000

    giveback listen spill intend cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
