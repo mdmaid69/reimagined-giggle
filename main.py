9a4275e71f6ad632067cea21a62b3b9279b2c473
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:00:15 +0000

    before such tradition steam politics

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def convert_to_octal(n):
-        return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
