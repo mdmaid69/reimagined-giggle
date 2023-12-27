1284ac177847cebdb5983b711308da7fccbfbc52
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:21:58 +0000

    generally in along close else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
