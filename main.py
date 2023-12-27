55698349e69695ce353717c9c91514ba2c95cda9
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:12:21 +0000

    intend start include accidental fall

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def count_words(sentence):
+        return len(sentence.split())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
