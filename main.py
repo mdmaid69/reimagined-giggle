6f410eaf2bbb0c12e07a0d6139f57ea9a5ed02e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:24:23 +0000

    mess set check greeting market

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import collections
-def create_user_list():
-        return collections.UserList()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
