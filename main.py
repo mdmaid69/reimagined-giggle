96871d16fb875012e812a9f95def9fc9f88097c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:30:29 +0000

    rain matter must recognize mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
