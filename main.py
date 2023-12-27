3a042fa315ad3e1d3e59eb0f494d249d20e53fff
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:01:24 +0000

    field slowly satisfied speech represent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print("Sum:", sum(numbers))
