e5fabe576290b4e3a4f45fac8d37b967760e060a
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:45:50 +0000

    let school Pp home callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
