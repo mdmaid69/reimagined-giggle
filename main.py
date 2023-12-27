572527f065c9403c980f9e977a6c918102887afc
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:49:42 +0000

    convenient loan 571enough aim never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
