de2fc3337aa5844999e22dc6809b0eb1b278e3dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:49:38 +0000

    now comment squeeze physical hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def append_to_array(array, item):
+        array.append(item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
