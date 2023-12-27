54e537abd86d7229e4603a13cd7ce8b8429be1ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:13:41 +0000

    fire nervous clear contents disagree

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
