e59fb7d0cc2f20dcc08f729eb44930ce95cd3948
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:00:37 +0000

    sort care black bet whichever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
