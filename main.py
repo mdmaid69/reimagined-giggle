fc9c51f2a8fa493ca3a7d5aa5cd8703db875dcb5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:49:24 +0000

    radio tooth handle nor popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import platform
+def get_os_info():
+        return platform.uname()
