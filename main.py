42edd54ccabfc25f4282fb48e404a69708911a86
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:12:12 +0000

    confused meanwhile direct noone about

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
