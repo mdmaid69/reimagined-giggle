5de581b3eb7ff82332ef252a00f2a3c12de30cef
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:00:58 +0000

    terrible exploration mend influence lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
