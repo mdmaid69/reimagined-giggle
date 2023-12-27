84d8f8b250e8647fe3db9bd9f70a079570c88acb
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:15:04 +0000

    find somewhere cum admit date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
