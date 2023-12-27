8e7eae8b1a1989fc72a2a1dd213980aa08d99a59
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:14:55 +0000

    573entertainment much mess telephone coffee

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_volume(length, width, height):
+        return length * width * height
