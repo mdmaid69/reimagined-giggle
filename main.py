617ae613723fba92e88a98de59df3ae08f957658
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:16:26 +0000

    dick circumstance interesting underneath wish

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
