0dd92527d5229819dd5cf3f5e6a96adc23f674bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:10:13 +0000

    direction drop clever expensive pretty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
