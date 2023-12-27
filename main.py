da164ca0c02066c68a3b356e076815f3d1a321c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:33:35 +0000

    shock winter feel pencil fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
