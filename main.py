beda20d9602e70a645959b1d7770c9bdf3a5b791
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:15:16 +0000

    hollow generally worry urgent land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
