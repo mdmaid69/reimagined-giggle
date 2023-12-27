0c291db97cab9f6c7d0b5a9c6b92e9ee491b905d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:34:01 +0000

    address shoulder Gg inquire disturb

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
