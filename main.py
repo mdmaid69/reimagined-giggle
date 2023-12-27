568e592b9b58a5091f9ecc00b02bbb32ac792464
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:04:03 +0000

    advantage yes with Gg discuss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import os
+print(os.getcwd())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
