2262bb3bbcc148caca11a973a5dd8d8126b6221d
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:30:36 +0000

    oil question exist uponon exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
