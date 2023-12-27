c86f7aa481761b177ec6707211dc43ed2e6ac5a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:29:24 +0000

    odd memory thirsty excuse destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
