5164b3c5dc1686f7e96dc49a9eb0fe229cf0026a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:43:51 +0000

    happen attitude normal accept day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
