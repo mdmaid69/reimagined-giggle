998c8d44e0c9e9df4e1bc20cf9f09622867abc2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:03:57 +0000

    exploration takeover demonstrate Ff ear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import math
+def calculate_sine(x):
+        return math.sin(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
