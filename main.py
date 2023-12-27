9ae78651c8a4fe4a8df37e8204c254c614a0774e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:11:57 +0000

    me open quickly point proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
