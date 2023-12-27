a057483ffedc3b56b4f143c9164f1b344a101b36
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:07:55 +0000

    clear eye curious doubt chance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
