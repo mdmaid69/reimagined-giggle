2588cc296118ccf5472409868fdb75a8c2f99879
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:45:33 +0000

    interruption collection offer cost confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
