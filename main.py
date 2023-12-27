66f555b6b28bca5ccd05c80b014af78d31ed2a80
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:02:22 +0000

    above wherever determined attention schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_priority_queue():
-        return collections.deque()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import numpy as np
+print(np.array([1, 2, 3]))
