9c8da2a6a78c29ed992e55eb2801272957a1065b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:43:46 +0000

    consider near opportunity ability load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
