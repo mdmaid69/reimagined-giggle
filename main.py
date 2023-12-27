b9443690cdcde52a6b6e6e12639740eb5d1289ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:18:39 +0000

    stamp comeoff slight that centre

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
