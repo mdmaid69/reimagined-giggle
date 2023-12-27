deca44632bf3e91f76fbfd560b1eb1fdafef4ecc
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:27:10 +0000

    steady plate sign lift production

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def get_list_from_array(array):
+        return array.tolist()
