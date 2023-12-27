163cf9cd67bca64f96e2565e233c21570185e417
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:02:28 +0000

    wherever increase nice remove match

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import re
+print(re.match("h.*o", "hello world"))
