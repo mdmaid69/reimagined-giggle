4e3606d198f89abf996d5879ec6cbaa90a68f584
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:18:51 +0000

    towards none move respect wing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
