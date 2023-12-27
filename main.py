4f849d4c0aa7881610e71a370bef61fe883b1501
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:03:55 +0000

    music return tax without hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def append_to_array(array, item):
+        array.append(item)
