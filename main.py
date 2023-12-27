6675d92fb295409a3490c98b5d0fee767118fe42
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:36:02 +0000

    who contrast too doubt Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
