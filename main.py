7324ed96d8ea9c2d7ec66940c441b55a9e3d06b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:07:01 +0000

    die warning class personal busy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
+def is_palindrome(s):
+        return s == s[::-1]
