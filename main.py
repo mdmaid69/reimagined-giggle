add73916976f098993e2c8fa78f1dc4796369524
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:48:17 +0000

    occur cum hard level belong

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def convert_to_octal(n):
+        return oct(n)
