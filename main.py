5f0d02bc179b49e0085b52121c01aa8cc1f6cb1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:41:07 +0000

    border delight telephone approve coat

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
