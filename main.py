b1f796cc2b09870fa9fb476a09dd667b183f4bbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:49:17 +0000

    world wall previous back similar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
