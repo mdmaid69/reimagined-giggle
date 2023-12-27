6db47e65b26235a5eba3726fb532ccdf4f94dac1
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:17:45 +0000

    study force commit valuable light

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
