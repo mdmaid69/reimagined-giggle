3688b0164408c57bb568db281cb4f50070ea13a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:18:57 +0000

    turnon add weekend with lookback(on)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-for i in range(10): print(i)
