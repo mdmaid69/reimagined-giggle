97e9dfe145195d5155a7cd8d093769f9023f92cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:32:28 +0000

    accept pocket getaway sore when

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def count_elements(lst):
+        return len(lst)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
