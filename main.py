07802f6482eb2332b082f8e0fb4c9bae8ddc41a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:39:55 +0000

    move care whole team sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
