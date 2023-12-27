68b8721675a5525c4dac4b85e137283a299d5f50
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:55:17 +0000

    relative instruction himself surface react

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        return sys.version
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
