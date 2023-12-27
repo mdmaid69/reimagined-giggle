f6d4871de0d02f4e9b8a5bf2e70997b15651cd46
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:36:52 +0000

    getoff failure realize custom party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
