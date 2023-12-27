1b91ad1cc27cda9eb5067759d5eb43b5424188d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:50:28 +0000

    sleep side claim Tt pressure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import numpy as np
+print(np.array([1, 2, 3]))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
