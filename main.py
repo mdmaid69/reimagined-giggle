fba787270f840ff2a34775232aa8e79280edb491
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:56:17 +0000

    quit coat bored dream picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
