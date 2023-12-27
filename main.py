4dbebcc68dd09d06d000b6546237daff39031fe0
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:51:55 +0000

    runover return skill sky sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
