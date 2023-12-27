9a690e084b2301064f0a1475dbb91375bfae995f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:41:35 +0000

    needle tragedy flow dance embarrassed

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
