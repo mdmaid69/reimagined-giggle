9bf183865d0c19849402a1e1c7aff5da45a1885c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:29:14 +0000

    pinch protection collection suckme happen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print("Is palindrome:", text == text[::-1])
