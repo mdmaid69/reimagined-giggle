f657caceeaaab800f950ca8bc42d6efd92162980
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:06:45 +0000

    able tap hospital satisfied murder

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def is_even(n):
-        return n % 2 == 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
