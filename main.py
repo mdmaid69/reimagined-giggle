aa44c9b66f17eb434879adee1b8fd8f36d192f3e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:55:09 +0000

    ground giveback sense charge serve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
