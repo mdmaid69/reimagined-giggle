f371b5e84e340df836d6212e5dacaa72291a5986
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:54:09 +0000

    terrible completely pressure ticket relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
