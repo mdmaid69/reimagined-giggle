29845c366de14b89baaad37f7b6b09964b4f9922
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:08:22 +0000

    now excuse flood stamp development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 def calculate_acceleration(speed, time):
         return speed / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
