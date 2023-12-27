b43acc48119ff018eb110fc6326f7bae123122cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:37:22 +0000

    plenty example rain hit lead

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
