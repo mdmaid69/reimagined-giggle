a79a91deaf94487d4c45809733d2022573485d52
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:06:06 +0000

    like maximum squeeze country charge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def count_characters(sentence):
+        return len(sentence)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
