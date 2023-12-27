0af1417352636228dd3a5f538a1f803bd04b8ce6
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:56:19 +0000

    call rest 569enjoy considering handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
