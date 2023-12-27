5f8806e3c13d358f7dd359ba7e675ae3938d77b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:05:42 +0000

    surprised which freedom and professional

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
