92c8fa339a605f1fe99c836f10e2dce605e6bdc6
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:04:01 +0000

    list belief thinkover previous sea

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
