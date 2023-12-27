faa2d21f7a7ec465eacd43068560868f9d39b34d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:35:56 +0000

    whisper few excuse evening spoil

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
