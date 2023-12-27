7644a1a625856f04cad186ed722cbc94a21881f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:45:13 +0000

    walk insist keen talkinto well

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_max(lst):
-        return max(lst)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
