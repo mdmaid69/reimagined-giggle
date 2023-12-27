b193b4b0ba132d7a9fde832cc6706156168d3430
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:51:36 +0000

    manner present completely month burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
