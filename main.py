06ec5cd7cf3b194e6aa6cb969048afd79678bf00
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:17:50 +0000

    pencil arrive criticize sea comment

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sys
+def print_python_version():
+        return sys.version
