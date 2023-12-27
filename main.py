2962b4b7ea2023dea5ec4265d1e9c19c586d1a3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:03:33 +0000

    once bad event attach getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
