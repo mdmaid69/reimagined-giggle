315bb0ad2cec9b7c6310c2d2bf23c6d1f4c88713
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:57:20 +0000

    picture cry pass clearly payment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
