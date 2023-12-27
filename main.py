233568edb942e4596a2c683c5f1a161aef4d239c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:57:00 +0000

    gas shopping dream church breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
