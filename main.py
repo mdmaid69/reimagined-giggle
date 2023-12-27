5897970af20bab73aaa87916664d25708da78153
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:58:40 +0000

    authority chief memory pronunciation may

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
