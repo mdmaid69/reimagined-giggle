e9672b8262cc960977ba2c2dcedd47f33239d294
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:13:23 +0000

    tragedy urgent nor greeting satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
