7cca20a90b1e0c40f0861ca10d547a4f4d88d8b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:04:33 +0000

    idea spoil call fault national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_index(array, item):
+        return array.index(item)
