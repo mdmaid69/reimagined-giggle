c4f8a687477c170133eea832672e8c344526244b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:23:27 +0000

    comeoff trousers refer plenty creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
