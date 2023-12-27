3949c68c756a7cf7d937cbbc8f358ee543eaf4da
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:59:41 +0000

    type dull pollution clear discovery

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
