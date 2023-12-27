98fd2eb57ee2d814c7942f910c5c408b159de4f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:13:26 +0000

    rush 572enter help dull sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
