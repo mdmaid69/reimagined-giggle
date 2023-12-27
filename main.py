89d61fd179bbf0d292087a7a01f4528bc2ec56c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:45:40 +0000

    confidence office creature again city

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
