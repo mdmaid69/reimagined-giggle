c9c0a4c9bc63208eb6a8c339ef238577be43dc28
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:00:01 +0000

    weak dangerous go doubt money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
