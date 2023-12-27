0b79b51cf2c505e21e49740229fbba40e7022d91
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:54:04 +0000

    school risk same tool worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import collections
-def create_user_list():
-        return collections.UserList()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
