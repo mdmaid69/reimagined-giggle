58aecfa9668976a8ddf27a4ed7496a73071360b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:46:50 +0000

    solve cap needle walk should

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_pressure(force, area):
+        return force / area
