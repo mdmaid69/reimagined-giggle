fbff2d390913ded7524dbb0910121aff0071d686
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:37:16 +0000

    Oo gift busy party near

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import datetime
-def get_today_date():
-        return datetime.date.today()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
