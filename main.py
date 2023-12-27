d37eac006122eb5e659da7af59d59adab7127e8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:56:13 +0000

    real swing learn sudden come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
