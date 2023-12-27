55057d6d7924c4c03c58406e6b4120d130244bd5
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:54:18 +0000

    mad rob objection finally emergency

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_power(work, time):
+        return work / time
