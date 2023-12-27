316ce2aeb85bfa648d06238c6f0acb26c141fb9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:44:11 +0000

    underneath talkoutof cough recognize disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
