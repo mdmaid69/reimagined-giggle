ee99da219bae51c9053862fedd0faff185f1a8d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:30:04 +0000

    middle since cross major exercise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
