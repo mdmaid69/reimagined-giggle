0777df0b6afe20ff527f651987fe0b237f1bbb96
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:55:07 +0000

    load pull climb better store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+def log_message(message):
+        logging.info(message)
