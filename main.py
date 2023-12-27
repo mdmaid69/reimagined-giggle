1283538d1a3f1d8b590825a0b13e18bd49d1d0e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:16:11 +0000

    desperate anywayanyhow extremely recently number

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def multiply_numbers(x, y):
-        return x * y
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
