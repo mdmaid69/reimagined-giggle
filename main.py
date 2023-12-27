6e703c79f1ebbf95a4666732416df1ecc54190dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:32:49 +0000

    call fit thorough black character

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(5):
-        print(i)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
