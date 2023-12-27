350ed8c2ce816fe97b8c990f87aa5694c48ce331
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:15:38 +0000

    chin direction why fun occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
