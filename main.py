4826ec91c2766a67b855e1c5124943391d7696a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:01:58 +0000

    turnoff continue conclusion usual mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import getpass
+def get_username():
+        return getpass.getuser()
