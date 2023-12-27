74021d5e44a69382039b842739cadde6ac1b01f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:23:47 +0000

    head swim and emphasis keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
