1c4ef1c33dd3d247bbc7f251fe4ef12f84fd2d58
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:58:28 +0000

    plan concentration experiment object fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
