f00687aea953f1f089d056d4da989ada907ad350
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:54:02 +0000

    tired various wide instruction ring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def sort_list(lst):
+        return sorted(lst)
