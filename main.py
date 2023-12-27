0aa17f1f73200eccfb9b836f31674897492e8e5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:06:30 +0000

    beyond lesson demonstrate left distance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
