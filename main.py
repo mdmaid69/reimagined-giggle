1ac2f881708a67f4cedd5c7c021eed9c658f965b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:26:36 +0000

    appearance whenever necessary rub hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
