ed7b2423a9f751ec45117addc6be1536194f94ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:03:51 +0000

    turndown book so must bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_area(radius):
+        return 3.14 * radius * radius
