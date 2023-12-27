51f6a88089ec674227a9845ed4023e0dadb3f502
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:20:19 +0000

    callup fork hard stop performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
