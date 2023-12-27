f6f38ea2aef8da204123db27115c57e746ff774a
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:42:20 +0000

    talk convincing communication surface nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_array_as_list(array):
+        return list(array)
