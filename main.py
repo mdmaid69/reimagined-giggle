ea33e40c8a4b70135c0c38fd8be761386b3befcc
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:47:12 +0000

    brush fairly Vv appearance fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
