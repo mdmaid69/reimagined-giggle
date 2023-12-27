b0ed0444f5a5bd8635cb92093c1ce96d7ad46665
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:54:11 +0000

    life work demand past sight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def greet(name):
-        print(f"Hello, {name}!")
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
