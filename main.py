44ddc5b7c436cc9abbcca7bc064529c81dce7613
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:24:53 +0000

    might regret laugh obvious take

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
