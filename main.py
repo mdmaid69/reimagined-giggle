91ba89d5f3a9a4ef8ad406082123478ea00f1516
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:50:31 +0000

    human indeed information way important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
