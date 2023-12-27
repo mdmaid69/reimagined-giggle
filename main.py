74327d4d5a804615e6ba05645d853ff0263eef0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:01:25 +0000

    whistle sister sticky sure frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import tensorflow as tf
+print(tf.__version__)
