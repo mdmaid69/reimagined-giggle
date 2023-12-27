855958577c717bae694609a5238b8f04b2c7f468
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:03:30 +0000

    cost suck comedown step quantity

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
