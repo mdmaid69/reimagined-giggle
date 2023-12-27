c703fecb91325c76de429ba6154a86cab09f67ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:12:08 +0000

    prepare commit introduction private quality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
