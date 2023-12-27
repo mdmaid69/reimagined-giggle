632297d866e986580f8f1f7f28a43dfbccab576d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:56:42 +0000

    hard there tip die from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
