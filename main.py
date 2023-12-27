c18dd1fddadedc0bc6f63fd91ff6f7fc742fd29b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:10:01 +0000

    light comeback central pick trip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
