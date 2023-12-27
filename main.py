46dd88213f02d1e11e0a500d7b24e597dc7bb915
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:39:27 +0000

    fill everybody judge positive fat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
