da6087e78213e5e7f2e4b94ebd12412d4f296f81
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:00:48 +0000

    juice excited downstairs past habit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_work(force, distance):
+        return force * distance
