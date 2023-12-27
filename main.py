4c808fe8d27d3443cad923ac6686d85d60a15189
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:02:17 +0000

    book familiar compete duty clue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
