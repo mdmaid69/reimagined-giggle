0e5169e70a000258c93353d5dfe518417cce5cca
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:57:25 +0000

    reaction rely scrape such understand

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
