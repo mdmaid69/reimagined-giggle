8d89c86a2bfc98163b8ea6b047496e88f20bbf11
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:29:45 +0000

    fit badly shape bar Nn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def count_characters(sentence):
+        return len(sentence)
+def calculate_volume(length, width, height):
+        return length * width * height
