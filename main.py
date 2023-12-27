6cf5f1c18bee9dba730c99470859372ade55ae64
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:38:14 +0000

    exist Ii scared hardly precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import random
+def roll_die():
+        return random.randint(1, 6)
