d0157855f137126157351b5dfe36b95d5498d47a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:26:51 +0000

    once tread party equal disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
