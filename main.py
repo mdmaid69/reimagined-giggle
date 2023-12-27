6bcf9888bd9822e3f3ce4b8f4f8c1e720812fee0
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:14:16 +0000

    anywayanyhow friend fork reject perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_speed(distance, time):
-        return distance / time
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
