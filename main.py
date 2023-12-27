a4ee231720141d5654a289c3276aa61701ca0277
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:24:08 +0000

    trust door brush basic shopping

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def convert_to_octal(n):
+        return oct(n)
