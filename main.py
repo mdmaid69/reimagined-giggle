f3221207b4d3d628d1121e742c3219139e14a8d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:59:14 +0000

    realize earn introduction feed without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
