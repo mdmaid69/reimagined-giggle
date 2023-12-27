cfc9af0f9286db571e2d092d33f2dacd9f9d34be
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:25:17 +0000

    workon attention tax class release

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def count_elements(lst):
+        return len(lst)
