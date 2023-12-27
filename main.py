f9f51f7a997d5651dfc6f8034923be07e501b4e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:13:04 +0000

    blame achievement Hh 179bite eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
