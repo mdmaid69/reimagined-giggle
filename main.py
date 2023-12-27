8412913d7a47c0d55e6b629a848a84c165af9188
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:49:17 +0000

    mix reasonable finally central real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
