69669502fd15640e7662e122f86a55c5e9251604
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:50:05 +0000

    why getback than happy development

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
