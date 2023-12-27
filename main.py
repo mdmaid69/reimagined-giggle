340e39e9a81a60a94c74adea78cb62884c6a1c74
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:56:38 +0000

    different pattern pretty Uu cheque

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_power(work, time):
-        return work / time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
