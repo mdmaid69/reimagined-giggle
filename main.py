dade7ca23b89ceb4ba3087a1adedabf5f340b27c
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:46:51 +0000

    contain respect quality taste say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_memoryview(array):
+        return memoryview(array)
