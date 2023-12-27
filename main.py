321d78421d68a014e50a38dff4077a9d40d61b8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:14:31 +0000

    again please life night miss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
