b71f77ae4b54fcc632f26f45340a3eb6f308c099
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:34:54 +0000

    juice practise attract cool intend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
