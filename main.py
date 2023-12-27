070885b4b5a84cd7530b7556894574e34a581adf
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:33:09 +0000

    suit to method best boil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
