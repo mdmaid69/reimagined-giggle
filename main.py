392c178e62f5164c53ec2b6b068ddd25904894cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:09:21 +0000

    creature lay discussion single have

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-name = "Python"
-print("Hello,", name)
-def calculate_area_rectangle(l, w):
-        return l * w
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
