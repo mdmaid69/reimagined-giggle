52e2e02dab65d69a9fa6cafd1e1e72d00caa55c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:59:23 +0000

    treat meanwhile agreement determined attractive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
