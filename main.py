94af7b7e6cdd7bde16ff58f0df54233392ddd438
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:33:01 +0000

    guess appearance for doubt group

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
