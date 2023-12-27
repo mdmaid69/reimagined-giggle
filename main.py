5bf327c43b8649eca44f44d2bae01d94f376f6a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:12:01 +0000

    regional second satisfactory Zz control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
