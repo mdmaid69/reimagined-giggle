11d53c913dc7f81b4570c68a144e7cee597fd026
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:33:26 +0000

    healthy pour authority crime wave

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
