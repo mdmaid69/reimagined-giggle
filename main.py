a3b6b8528c2385c75ae11548fafe593d7ee6cf0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:27:04 +0000

    little swing hair command disturb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
