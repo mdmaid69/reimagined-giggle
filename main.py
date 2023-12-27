d5855aafb07e2bc105a9f755b853f7e8413eae64
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:41:10 +0000

    they scream grow star moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
