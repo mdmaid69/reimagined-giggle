4dd6a08a87e9585d20d7f087f61164c9d2ec7ee9
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:55:43 +0000

    anywhere spite like accidentally badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
