7164c0ca95a963d2ca19d7c2f811a75e9e6da48f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:01:25 +0000

    watch embarrassed obtain present bringdown

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
