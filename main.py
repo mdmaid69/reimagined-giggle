5c438d412a02ded0736177552c871aa83ea83ecf
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:27:34 +0000

    uncomfortable tour should hold address

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
