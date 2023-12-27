66acc39a97ae1f430b704585098fa4c5e0d8bfe0
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:28:03 +0000

    should hard hope speak scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
