00d97e686a4fe25b962337d804fce3193e58b116
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:34:51 +0000

    clear steady available we nonsense

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_float(array):
+        return float(array[0])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
