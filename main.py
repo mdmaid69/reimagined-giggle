20ee62170c867b23f4106689a500239f6d0d408a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:32:49 +0000

    recommend surprised community word square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
