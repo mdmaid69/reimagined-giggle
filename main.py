efb7f94820f5fa6a94045eed5df5829b83338394
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:15:54 +0000

    present recover maintain shop combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def append_to_array(array, item):
+        array.append(item)
+def count_elements(lst):
+        return len(lst)
