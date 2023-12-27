cb83158264371d7ffaad8b2d66166bba46013bfa
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:19:57 +0000

    workout face colour plant weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
