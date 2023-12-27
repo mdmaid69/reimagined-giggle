a695bf8eca907e78e4a08d7da20da1a4612939cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:01:21 +0000

    invitation Bb hers say tense

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_length(array):
+        return len(array)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
