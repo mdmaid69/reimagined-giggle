286d1b243dd931bbb2e48e30f807c8e8f9c7bc7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:51:12 +0000

    even shut pen each light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+print("Hello, world!")
