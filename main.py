9e4679b08e91ad510823bd859caf8ff49ef21dc7
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:08:49 +0000

    money schedule boil action reply

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
