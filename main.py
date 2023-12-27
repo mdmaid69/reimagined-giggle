28425eb9e116e8e6c83464eea6f28f9e59dee048
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:55:09 +0000

    closely permission another keen fail

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
