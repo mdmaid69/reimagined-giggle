a633c8d8ddda2913b92b2c72bfe71e575f259478
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:41:14 +0000

    nice hate vote introduce market

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+print(math.pi)
