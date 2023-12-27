122a098611f6f50c710f20cc1ca204be2490d827
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:35:04 +0000

    about pour confuse pretend think

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.time()
