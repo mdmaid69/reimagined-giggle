f23f8fab3f2a90434ca7309dc02e069d789855db
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:27:56 +0000

    bang hurry dress wait turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
