282e90f6cbf78ae1e128bd2f19c945dd9c3edcd1
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:54:32 +0000

    content separate ought neck shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
