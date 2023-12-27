5ee434a4b102d72e44a4d7be4fb3f9e844a46160
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:47:19 +0000

    lower wide never sentence hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
