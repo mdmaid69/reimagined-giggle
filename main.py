e07a502700b00f999dc7ca069434c7a966a2b640
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:06:00 +0000

    crime opposite sail near progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
