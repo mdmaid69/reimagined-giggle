551773400bee7436dba83e0190bad7cb7c97b781
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:47:46 +0000

    congratulate high breakout separate unit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
