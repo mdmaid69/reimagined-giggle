5f0efcba6bb9ae79edb8a2fbb08269400eb91393
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:12:29 +0000

    several worry complete interrupt fortunately

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def convert_to_binary(n):
+        return bin(n)
+def convert_to_octal(n):
+        return oct(n)
