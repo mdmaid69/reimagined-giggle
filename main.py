234c51112df440981adc8280a8a2b628901e4967
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:46:23 +0000

    rubbish cap listenfor party pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
