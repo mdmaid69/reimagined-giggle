09f135aa6fd1e49e8dfa776f21bf021e27ed67e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:27:02 +0000

    warn solve against idea performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import time
-def get_time_since_epoch():
-        return time.time()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
