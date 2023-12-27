0dc9a776068516a33aa71c5b3a693ad3c342126f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:23:13 +0000

    programme system bring ahead appearance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
