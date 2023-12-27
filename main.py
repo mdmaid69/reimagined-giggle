fb77340e52505191c71a820213104cd47110590a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:44:15 +0000

    relative turndown fit disturbing face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
