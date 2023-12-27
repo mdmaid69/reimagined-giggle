595c44c4f7209f94a8d7f53933d8092e952489c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:06:11 +0000

    name lost fetch experiment pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
