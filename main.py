aaa7e6fcb9765219d3a05be490383377f73ccc7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:17:04 +0000

    same experience entrance string boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
