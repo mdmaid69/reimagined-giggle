7c8526fd543aab8129499664efdfb9524b4da985
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:02:01 +0000

    book value field 564encourage warning

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def convert_to_hex(n):
+        return hex(n)
+for i in range(10): print(i)
