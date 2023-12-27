40814aa5824e3a91ebf835c1b38ab0c77ab68fbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:56:03 +0000

    drive range stamp length shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
