b3cc936a937665b6c8d1d7899b7ba61faca75a39
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:04:31 +0000

    blow trust awake straight calmly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
