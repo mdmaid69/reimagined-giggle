2c7d96bbdcfca943fb303955cc0941e69831e3e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:27:35 +0000

    manner guilty disappointed niggaacademy land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
