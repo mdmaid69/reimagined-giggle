8aa2cc54b021873ae3821ca931a2de8af08008a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:32:07 +0000

    male content practice above real

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+for i in range(10): print(i)
