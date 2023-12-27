f9f76fe390a5ec7e567cf1da3642d6cfe5fe84ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:25:19 +0000

    light judge visit field appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
