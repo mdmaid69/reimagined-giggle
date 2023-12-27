b42901bdf415187e95bd2edac915219c889a305a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:29:01 +0000

    behaviour affect still shout address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def is_odd(n):
+        return n % 2 != 0
+import time
+def get_time_since_epoch():
+        return time.time()
