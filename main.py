ea31492637e6df2163600c3b2b4069bccb2d3108
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:37:04 +0000

    runover discussion transparent scale minute

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
