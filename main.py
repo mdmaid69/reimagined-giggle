9dfe3bf14c76427a152bf42e2bc660dc2a129034
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:23:14 +0000

    peace downstairs determined solve introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_average(lst):
+        return sum(lst) / len(lst)
