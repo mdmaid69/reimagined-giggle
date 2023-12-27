159ceea864785306383984b77bcbb279e7e03714
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:00:00 +0000

    keepaway pressure crash split tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
