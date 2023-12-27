52fa18c942d8d9d341bb0f4dfe3dafa90ea13957
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:01:09 +0000

    hold usually laugh block belong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
