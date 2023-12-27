d8aba755321d45648228f2c493def02b0c746c81
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:27:52 +0000

    inside scared possible remark split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
