a00a221d12303a3d66deb11c07b82db5f0e783e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:24:49 +0000

    wide wrap space include anxious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
