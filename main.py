f78074d7a969d5bb101d9b77dea1ba1a00004f59
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:37:33 +0000

    but load person hour message

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_string(s):
+        return s[::-1]
