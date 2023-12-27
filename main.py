c9ab573f15a46d838f8dce2cb1c3a28d963dc278
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:59:25 +0000

    insist cut judge noise taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
