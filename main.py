0735124725484d4acccd397edd33228002eec615
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:53:49 +0000

    yours settle brave empty contain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
