03b0bfcd35a938dd0464f8559143717d5b71c17e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:30 +0000

    mystery laugh increase someone debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
