921b2838dc4986dcb4df5208034b89ca954ea788
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:31:50 +0000

    avoid bar consult towards ocean

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
-print(math.pi)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_factorial(n):
+        return math.factorial(n)
+  def multiply_numbers(x, y):
+        return x * y
