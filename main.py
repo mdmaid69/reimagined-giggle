dca0333c13a7cbf438b10b3f0b316fa34edfccd4
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:46:31 +0000

    if still happy that lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
