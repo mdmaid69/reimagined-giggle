25304079728c13a8a170511f91533b02a3e90cfd
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:21:06 +0000

    backwards fond check trap common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
