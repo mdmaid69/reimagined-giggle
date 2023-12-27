b204893d886164b04a0fbff3b78d543df6efe49f
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:24:24 +0000

    frighten fault thing closed speech

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def multiply_numbers(x, y):
+        return x * y
