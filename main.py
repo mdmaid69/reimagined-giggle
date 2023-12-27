bf5e5c0e9eed5f5cff10500f56c79a5eaf1f07eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:31:20 +0000

    live sign bus regard allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+def get_today_date():
+        return datetime.date.today()
