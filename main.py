721fbdd1c6b36c364c6e6453b2e6a1712b1c6cb9
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:16:23 +0000

    cheap sure city cheat declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
