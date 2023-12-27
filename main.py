ed151aa5077ef69e05bd79b6d5c3a3f758872309
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:27:36 +0000

    error image whistle even prefer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
