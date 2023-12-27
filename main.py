6fab0876ea5f36e7c6a6c432b3069955e225491e
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:48:41 +0000

    steer tip gate opposite sense

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
