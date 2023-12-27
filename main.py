80237e2959f9a22f0590e7f9dec73a9248885b16
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:35:55 +0000

    underneath mess guess reach determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
