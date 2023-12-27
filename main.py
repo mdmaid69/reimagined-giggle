541441ac6f33414dc0ed5d0918eb9b4b3057ca11
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:34:04 +0000

    recognize rest unable the partner

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
