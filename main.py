f2e86567a5b5ccddccdb4f9c5d023eaf431d2924
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:12:27 +0000

    under cold group considering underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import os
-print(os.getcwd())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perpetuity(payment, rate):
+        return payment / rate
