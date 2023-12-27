46dc316cd42d777363e422eec6e56f7ec1396cf7
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:25:02 +0000

    light inquire heavy force separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pressure(force, area):
-        return force / area
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
