8a0e6255d4751a6a1e3f77ad040287556577f21c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:55:58 +0000

    concern support closely proud consider

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
