92f43bf60992e5afad78ad4e5be6db81041d8e22
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:58:22 +0000

    sensitive opponent who breathe above

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
