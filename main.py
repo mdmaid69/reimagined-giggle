5e6ad12383a48e2bc2fefdc4d3cd041f37b94265
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:20:19 +0000

    laugh well wire justice day

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def split_path(path):
+        return os.path.split(path)
