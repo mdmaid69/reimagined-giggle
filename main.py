336825c36004a278caccab68cab73eac01d613cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:02:54 +0000

    disturb once who large scrap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
