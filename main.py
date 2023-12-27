0a38cefe77b36c3ad1d182810ad7a13e3587660c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:46:00 +0000

    national opposite vanish depend become

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
