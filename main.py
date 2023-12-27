e32cba5c75725d0372c1611ea43706582447e72b
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:45:02 +0000

    hide connect block brush introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
