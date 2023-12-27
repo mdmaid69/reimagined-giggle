3bfca108ffbb48b367c6149775720dea0b05f6f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:14:28 +0000

    flight left same free suck

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
