e28e460b89b2f6e33f5818f9003b80b71d1aeb08
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:18:24 +0000

    hang absolutely pen pay comedown

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
