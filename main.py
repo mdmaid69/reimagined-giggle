7ec52536a197bdc6406c9d5ce56477c936492c00
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:32:28 +0000

    fix calloff touch improve remain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def subtract_numbers(x, y):
-        return x - y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
