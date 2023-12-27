714b6afcfa2b21948eb6f6f1d56060eb70443c1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:24:49 +0000

    false pot excellent gap holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_min(numbers):
-        return min(numbers)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
