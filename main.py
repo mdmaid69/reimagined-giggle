2725f75d92af671016b20bd6809e02451f62c12c
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:02:42 +0000

    discuss complete behind home continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
