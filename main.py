31601ac4bbf90439fdd5028f041423a09dddfbdf
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:19:24 +0000

    again actually tonight strike talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def convert_to_hex(n):
+        return hex(n)
