a88056075f751a89cd66e23dd36777aac102dc28
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:07:07 +0000

    sign repeat progress naturally interview

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(lst):
-        return max(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
