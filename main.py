6bdf1e91fddf91f1f8493d77192a1d0d3ecd99ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:08:43 +0000

    classroom stiff schedule change Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
