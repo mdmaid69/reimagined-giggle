c925630ada984a49a991bfdce063e8ae3d46e998
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:55:15 +0000

    giveup avoid wing hold separate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_max(numbers):
+        return max(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
