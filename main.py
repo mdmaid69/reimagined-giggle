7d697c41a6407a20e2ab6cbef779db51ec4e673f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:19:28 +0000

    capital address bring improvement suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_power(work, time):
-        return work / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
