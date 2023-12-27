1be40a978a890eb0e33ff2af7a57560dbb0aa242
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:42:37 +0000

    wave central law can grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def find_min(lst):
+        return min(lst)
