13ee0921055eb3ef045d693e2c0d781e5c838b2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:44:57 +0000

    pause reliable lie try bear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
