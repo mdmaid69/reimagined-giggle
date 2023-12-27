305374a41830b4dca3757ee474165f2d82b7ef2a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:50:04 +0000

    direct programme platform service comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
