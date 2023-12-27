610259b897a5a23a91dabb6d3ee2dc8c1317a848
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:44:57 +0000

    570enjoyable talkinto match calloff programme

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
