5b287f7e2e4afc98471d338de5897ee571df9833
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:40:20 +0000

    negative my related well human

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-for i in range(5):
-        print(i)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
