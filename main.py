17aa3b263f74a91aaa477ca4109a8b4f7badc1bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:55:59 +0000

    mistake protection report chin emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
