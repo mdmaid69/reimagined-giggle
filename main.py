ad7595f34743fabe75e085ef0b9bcf94154e3d07
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:19:55 +0000

    pressure attach consist effect person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def clear_array(array):
+        array *= 0
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
