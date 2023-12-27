df8a864c613bdc5369b69125779f3c38e40687f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:44:25 +0000

    perfect guess guard cause float

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
