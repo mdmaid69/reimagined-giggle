0a8acf9d3ceab29660fbdf28871a70a76c49b256
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:18 +0000

    sick poison friendly exchange base

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
