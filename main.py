41dca2f59b1e9692bbac2a33375b144d9fd59dff
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:56:15 +0000

    damage comment fun head promise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
