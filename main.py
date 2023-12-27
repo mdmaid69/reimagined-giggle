c3d758908f82c2fd57e0b1f6b7bb43264f971a04
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:33:23 +0000

    tip hard friend speak suit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
