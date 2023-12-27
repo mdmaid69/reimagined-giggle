5ef867a08e8dad9d1393bad57132e19309abc52c
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:15:42 +0000

    throw doctor available break calm

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_pressure(force, area):
+        return force / area
