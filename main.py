5693ba1dacfb4cf8973cac6e2f70fe7f15d35b1c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:13:11 +0000

    inside side previous underneath band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
