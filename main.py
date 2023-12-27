9b15ddb6501a49f7b1ad8bcbdb4c13575a1540aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:08:34 +0000

    permission meal pronounce custom bottom

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
