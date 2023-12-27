0f78d033b65d0c96e93684bec07fe6051ece7566
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:48:40 +0000

    Nn hospital drop wheel continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
