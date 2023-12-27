14ecd3bbe214bcb4e76a136d0fdeb54163c70453
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:27:00 +0000

    class wheel hit home substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
