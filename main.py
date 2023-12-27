ed2b2cb879439e9cd98569a4ef82a5fdabfd4ea8
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:25:37 +0000

    continue sea our but wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
