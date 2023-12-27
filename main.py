3817f3faba24c78add89052fa6d59f1707745ace
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:17:36 +0000

    if narrow shoot use takeout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_pressure(force, area):
+        return force / area
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
