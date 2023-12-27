97ca4026001107ae5e9e3034ff9830ad9c23a7cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:09:36 +0000

    contain steer open no progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
