d52eba0b2b1cb89c3ba689fc60b3eb912c5d77be
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:07:58 +0000

    outside hot copy wonder level

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_power(work, time):
-        return work / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
