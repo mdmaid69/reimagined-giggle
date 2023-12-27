915310dd8ad69d72c32ae455d1a2e9553bfc74e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:36:22 +0000

    turnaway close touch in visit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
