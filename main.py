8a43044ee4c67abdc197661e1338202e6ee04864
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:05:49 +0000

    direct building someone Qq shame

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
