cf42f7bc2c2c6005189ae3a5cd5efa05648b527d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:11:35 +0000

    wide imagination talkback really pay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def cube_number(x):
-        return x**3
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
