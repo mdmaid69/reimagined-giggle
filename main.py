421bf06ca0e29ceb1dade0f0ead3fc73c2f2938f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:40:34 +0000

    march offend favour plan expensive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
