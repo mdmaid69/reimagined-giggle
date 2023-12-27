ea702ca9ce35c6c435c1d3d1669ce79ac62b0ef8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:35:34 +0000

    high hide face list Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
