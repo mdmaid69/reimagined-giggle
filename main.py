aab05b03c13f746030643616ce8849bb1b0328b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:38:08 +0000

    awake sing movement by classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
