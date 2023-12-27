52755a9ec97f6aa276be1fc4947367c3c6d885fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:26:30 +0000

    begin best could wrap straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
