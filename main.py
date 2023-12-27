bfa07a2e8687ad4b2a7b62d6dc3796a4dfc69de5
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:59:20 +0000

    basis period phone run along

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_max(lst):
-        return max(lst)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
