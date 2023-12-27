93e725d87cfd74c23500604ea7808b4e477b5785
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:45:50 +0000

    grateful scheme gas Hh old

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
