7f0ca49655bb31250f6a6228fc5d79d45ad50f6a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:30:16 +0000

    my goaway movement exam rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
