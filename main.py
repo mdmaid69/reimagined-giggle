89baa432c0d41149331b1a7a63f5e78c7225565d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:49:37 +0000

    plain persuade thin dry treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
