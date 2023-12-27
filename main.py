384273a780fd532b87036915b71c5f4fd0c0b226
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:02:45 +0000

    lay reach expensive band use

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def square_number(x):
+        return x**2
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
