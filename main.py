599b694e8e6b4be2e10e35d75600f967853444e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:31:25 +0000

    good squeeze shame eager immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def clear_array(array):
+        array *= 0
+import sys
+def exit_program():
+        sys.exit()
