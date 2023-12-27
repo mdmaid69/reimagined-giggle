b6163340ecdb7fc3221f347797319696c9d0098f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:40:39 +0000

    development market boil about issue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
