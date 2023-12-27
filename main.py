092a22eaf7348762884ae66fae46c782a5d54188
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:37:12 +0000

    maybe holdup weekend squeeze wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def subtract_numbers(x, y):
         return x - y
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
