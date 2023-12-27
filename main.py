c17a774647c64c83db1eb56677e5c08a69385f15
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:00:00 +0000

    home rain pattern phone succeed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
