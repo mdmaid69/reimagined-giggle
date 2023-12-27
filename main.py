4342eb83b44518affb793da8a35e6603fe1a4f80
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:15:49 +0000

    skill happen beginning travel nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
