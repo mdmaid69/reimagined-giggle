96a8f2435f4a3b718410bd8b4290e2734232b99c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:37:48 +0000

    unite that unlikely passenger habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
