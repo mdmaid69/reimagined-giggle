864e9baf7d2b3a9f61041d6283b993d1f9915bfc
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:04:42 +0000

    whistle taste early food complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
