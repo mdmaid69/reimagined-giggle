364daeeed762ad0ada87c5911a50abd9c96d49fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:55:35 +0000

    ignore mistake load constantly over

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def split_path(path):
+        return os.path.split(path)
