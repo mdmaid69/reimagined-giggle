b7e1086f01da34b6ff916185f9700ba18d6c9e50
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:21:51 +0000

    come breakout full Ww secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def square_number(x):
-        return x**2
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
