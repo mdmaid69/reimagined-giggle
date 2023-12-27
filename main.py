f23830c7600d1cb32b6d42db6fb2daeabb308f47
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:44:09 +0000

    stand acceptable breakfast train steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
