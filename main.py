5b609ab11616b6ae723773a28c4a5052e9c8d371
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:30:12 +0000

    factory various sky why voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def split_path(path):
+        return os.path.split(path)
