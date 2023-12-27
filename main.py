009e49d79b31aa03179358c766ddf06b9d367f22
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:25:17 +0000

    mess regular mad Jj trouble

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+print("Hello, world!")
