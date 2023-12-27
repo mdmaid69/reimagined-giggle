729a783bb13716e37d34c302333f0381c9014464
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:48:16 +0000

    talk contribute as it mix

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
