88c5cf0ebf0acfb912386e53f693e7dd8655b6ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:26:01 +0000

    post legal weak interested cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
