da0dd6ad0b12a6b9840520e7c45b2a1661fc6564
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:50:08 +0000

    quit worst pronounce the have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_current_directory():
+        return os.getcwd()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
