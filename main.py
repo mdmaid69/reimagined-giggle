46565e3000262aaae806acf63e2a0fab82b520f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:51:43 +0000

    contribute blind hurt develop private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
