e10726a3c2bcd98cb925580d98392abb388f1958
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:21:14 +0000

    him still next idea watch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
