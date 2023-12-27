1e33828a79ef0e9a9174c5ce7b2fd20b8c984e9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:11:32 +0000

    set stone pay pile art

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
