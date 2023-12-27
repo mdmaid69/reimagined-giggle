5d647f3587ba1db42a9057ffc0b340c23097a6f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:06:03 +0000

    string unkind thirsty single claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
