b76848bc7ab5c15b0cab9b5b2fd70689dcfd36ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:13:04 +0000

    question can fat unable left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
