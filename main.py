1176bc3a9a685d2f8a647fda5a670337e0b27637
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:25:38 +0000

    circle cheque actually us geton

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  def square_number(x):
+        return x**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
