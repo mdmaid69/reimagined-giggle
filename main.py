f4bcbe0846b76c8a7574dabe18b75d6f40ca4b0e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:30:28 +0000

    top hardly ready sharp yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
