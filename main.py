6eda3628cccc5b41ea810a9415ec726b3e865878
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:47:53 +0000

    frighten simple level branch age

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
