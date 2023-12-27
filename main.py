ea3e6d3a15c6534eff9fab92e35e7b671538dbe8
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:44:19 +0000

    climb offer border often weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
