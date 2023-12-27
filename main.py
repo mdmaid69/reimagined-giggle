8c061437de27777d98e249a53b25c71b5b8f2225
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:54:10 +0000

    plastic telephone there ever run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
