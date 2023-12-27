a141da0db3fdc2639ceb6796c444bb32ad0aa94e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:11:04 +0000

    friend race bring another set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-def is_odd(n):
-        return n % 2 != 0
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
