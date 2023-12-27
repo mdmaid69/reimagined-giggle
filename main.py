e02c3eb1b513c0645dec0149199fbbf6b367b941
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:25:16 +0000

    comeoff naturally wander feeling neat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import tensorflow as tf
+print(tf.__version__)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
