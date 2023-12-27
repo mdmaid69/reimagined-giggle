642cdc859fdd7ac62dfb63d1e77285f42001ac71
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:28:58 +0000

    maybe improve phone wind clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
