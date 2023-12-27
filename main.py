7f81577f2cb4961083c4cb4b5e006a4df5bdeb89
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:49:13 +0000

    slide broad over base sense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
