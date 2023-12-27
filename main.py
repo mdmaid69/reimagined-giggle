33324a968383444516a24801998fc2357ee5212f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:48:26 +0000

    swing upwards quiet information where

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_volume(length, width, height):
-        return length * width * height
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def get_array_index(array, item):
+        return array.index(item)
