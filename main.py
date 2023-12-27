0fb67f14b2eee9e0cc863e5461887415d12c60d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:03:14 +0000

    effect lookafter actually one environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
