28f7cc1bcac777ff4249c85ffcef956d49a19da2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:15:24 +0000

    straight clothes aim plant so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def sort_list(lst):
+        return sorted(lst)
