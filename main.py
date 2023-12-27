943e6a5e246f429ea4d521eaef09009324336f9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:03:12 +0000

    too over describe steal strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
