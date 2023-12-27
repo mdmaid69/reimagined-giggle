bbb4a500f218f4b4c69e3dc9de5b17fe98d0bc1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:51:47 +0000

    reply admit piece business book

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
