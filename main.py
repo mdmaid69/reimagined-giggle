c8486aa7e456632111bdfb2bc6a6aa0e158f73d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:07:23 +0000

    pressure drive dance interrupt notice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def convert_to_hex(n):
+        return hex(n)
