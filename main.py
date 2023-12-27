8b1491f656f93f07800f5fda178213fd1b3a5437
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:13:59 +0000

    guide may lose train moon

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_ceiling(x):
+        return math.ceil(x)
