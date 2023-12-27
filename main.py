3a673a69fcb603f6bc333c61517e7e9e0cbdbe73
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:57:52 +0000

    decide direct quiet sure pain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def clear_array(array):
+        array *= 0
