44604a43f93b2a72d4da2c9bf109485a567ef7f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:57:54 +0000

    bright float summer when blame

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
