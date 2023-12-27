c2e71e5cbb67148529863fb921bbbacd7d986c32
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:35:21 +0000

    none across clothes think board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-print([x**2 for x in range(10)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
