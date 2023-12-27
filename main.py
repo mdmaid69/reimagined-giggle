2e0073f9bf0f9349fc0fb39b08dec141a698972d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:31:59 +0000

    shut hour exploration consider please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
