8f7929c8511d0c3a7afe833aa1f75368fe80aff0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:17:50 +0000

    plan gentle fresh keen lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
