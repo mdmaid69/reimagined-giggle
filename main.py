1a60515c43b97803cec2d7dff84195b3d7d9873f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:39:04 +0000

    plant exploration related introduction single

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+for i in range(5):
+        print(i)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
