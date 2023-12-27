8ef29ee809d86273d158d99e859c9a6cf8a27c03
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:26:02 +0000

    bring difficult likely standard hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+print(time.time())
