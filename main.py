bf2cb6699246ce92619ae959fdde742361254318
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:36:01 +0000

    vote bye recognize wise dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
