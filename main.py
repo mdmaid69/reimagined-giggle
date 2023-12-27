d2af5df5ff2f511235619f3af8793c98076aeff0
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:31:28 +0000

    then feel subject fall division

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_current_directory():
+        return os.getcwd()
