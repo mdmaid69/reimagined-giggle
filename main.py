cb177fe90b16ff13d6d813798cd01103877ba449
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:50:11 +0000

    food join interview environment disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
