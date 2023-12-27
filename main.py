1ca41511b1b4384eab788d456ddd11a603310210
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:10:39 +0000

    last luck act bush part

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
