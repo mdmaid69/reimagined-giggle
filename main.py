d0915ed84ff2196ae03ffb97cf3e3ffd7ca3d824
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:55 +0000

    car old matter colour house

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import json
+print(json.dumps({"name": "John", "age": 30}))
