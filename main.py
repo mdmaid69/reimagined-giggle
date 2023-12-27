f13f6a518bb7474ddd89ddc85588b301f5aa3402
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:49:38 +0000

    exploration imagination show reply busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_list(array):
+        return list(array)
