55148b6af038ac62fc90b4716448b9340cf59d10
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:10:32 +0000

    use maximum rich generally pair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def remove_duplicates(lst):
+        return list(set(lst))
+def cube_number(x):
+        return x**3
