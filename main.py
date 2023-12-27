c9cff006a94875b960cb4a82ab2b68571d4c9bfb
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:37:43 +0000

    noise light share Dd Yy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+print(os.getcwd())
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
