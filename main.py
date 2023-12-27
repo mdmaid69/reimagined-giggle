fe268190a1283ea3fdcd9ad7b0f47ef048bc40d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:21:18 +0000

    relative getout mother travel lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+print(os.getcwd())
