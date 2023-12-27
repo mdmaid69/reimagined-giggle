952e3959c5e1fccc5f6ff9a0fb7b6adb3079c7a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:18:26 +0000

    turnback with nowhere next remove

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
