770fb4ee3606ee4fe95f727578a0bf22900bede7
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:14:05 +0000

    lower visit type cover out

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import json
+def convert_to_json(data):
+        return json.dumps(data)
