e2cee1bc2339b171af5f56d35d6fa329e51934b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:55:20 +0000

    favourite limit friendly difference fortunate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
