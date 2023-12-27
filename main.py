51db227ea8a6aab1133b675aca4ad243acb5f99f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:09:31 +0000

    accidental slide sell suppose display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
