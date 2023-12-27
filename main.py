b3a5b9df95eaec642e0bd504680997d6d1da45e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:18:52 +0000

    film nor represent available injury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+print(datetime.datetime.now())
