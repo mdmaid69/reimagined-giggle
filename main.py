ee8e9d286a6c7f74edb6489367cbc802f5fe4d40
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:01:17 +0000

    last ignore except murder complain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_counter():
+        return collections.Counter()
