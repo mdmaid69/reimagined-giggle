dea5c48993279c12c63f43f152d3a314dfbfca2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:01:33 +0000

    sensible unfortunate chance mind reach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
