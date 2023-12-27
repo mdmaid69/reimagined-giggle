391ed8e8e962a8d775a530c8e8c794a430902394
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:14:15 +0000

    complete sense safety shallow official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
