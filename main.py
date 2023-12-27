eb7f3edbeac91e32d0e26216c24316fc60b9d2c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:11:45 +0000

    family unable better divide shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_item(array, i):
+        return array[i]
