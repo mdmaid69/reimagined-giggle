dd1bf654cbfacceebf3817433aeb6f6437349b78
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:39:45 +0000

    drive something public introduction flood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def count_characters(sentence):
+        return len(sentence)
