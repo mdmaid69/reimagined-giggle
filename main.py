fd0afa1468c989d82211fde50e92134647c12b2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:24:07 +0000

    debt have next turnup everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_counter():
+        return collections.Counter()
