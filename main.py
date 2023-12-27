b123e721844aa7055bd66db05b043f319709c580
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:44:33 +0000

    your favour comefrom level mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
