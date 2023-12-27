66d61685083238f9b09c065d84d16f02ccb7ad77
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:33:42 +0000

    scream small quickly speech advantage

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
