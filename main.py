a13d99833e61bbf5bad7a5289fe29e43f09aa446
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:34:33 +0000

    whichever agree claim hurt 568energy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
