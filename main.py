a9fc90250f5c0a1f262b7e0bb5e5f3697977a82d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:57:08 +0000

    bringdown recently spare describe successful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
