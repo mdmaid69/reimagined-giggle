ea5f749c78caaab0982edca2fefe7dc4cbdfcf5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:39:22 +0000

    protest single their 571enough over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
