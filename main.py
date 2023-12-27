4e49a295fef1377fa5ee748ab793978c8f24cc08
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:06:04 +0000

    blind young regret special club

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
