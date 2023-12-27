f288d124a6520937acf2a0c047c6bfcb6462464c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:41:19 +0000

    awake then pressure welcome scene

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_set(array):
+        return set(array)
