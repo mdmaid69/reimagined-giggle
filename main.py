c1ea745d9f0f4c4791d318bd4d3cbf90e02fa0ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:19:51 +0000

    quickly choice cap clue figure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
