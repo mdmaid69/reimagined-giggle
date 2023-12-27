054c74efa58d328d57308bfe7dae0018c27fdc0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:11:57 +0000

    talkback mind roll damage bet

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
