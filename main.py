744f2cc78483b427d98ae12ec8245c5a7bb10eaf
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:40:23 +0000

    cheat turninto love getback demonstrate

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
