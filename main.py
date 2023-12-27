f9d81335b7228e90604f2c70a08b63f5dc3d6bab
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:04:19 +0000

    more pretend mistake that communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
