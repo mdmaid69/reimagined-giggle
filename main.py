03d5718fa9215301f39afbfe6c4ece7cab0a6a0f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:49:30 +0000

    communicate agreement stage frightened bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
