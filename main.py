f7918798b7a10b3b0f84c34610e8a9ad6fd02c15
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:02:26 +0000

    everyone face freeze across range

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
