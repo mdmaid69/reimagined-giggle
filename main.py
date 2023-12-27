a1a4df84be6890190c8572e2e2213ef9f2d9d782
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:50:41 +0000

    order ability convince boil her

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def add_numbers(x, y):
-        return x + y
+import time
+print(time.time())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
