b77e6512e8fcfd33808496dfdc4c90e48913e0c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:32:00 +0000

    reason part birth room art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
