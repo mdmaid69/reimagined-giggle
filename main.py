b81ed540e4a79be201ea8b0d925d86d1947584e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:38:01 +0000

    this post interrupt artificial lunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
