014706a94575007c1ee239b3a20bc475bcd7904c
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:58:39 +0000

    Vv benefit remain breakfast illness

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import os
-print(os.getcwd())
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
