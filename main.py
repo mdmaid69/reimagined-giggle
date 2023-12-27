28036f0d3fa026951aa4022ae744f3b154be1c98
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:52:37 +0000

    share pot imagine suggest young

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
