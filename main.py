0bb579b1227f1f883b58fbe8ee95d110fce08d9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:47:58 +0000

    yes capital shrink introduction side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
