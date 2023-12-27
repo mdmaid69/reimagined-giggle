ffea9fc7c2499bfc16cce9dd66d0b2f8183e857b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:18:53 +0000

    complicated open play children position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_length(array):
+        return len(array)
