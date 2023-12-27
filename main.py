a26b91a778f245c37e832b431205e3464ffc75c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:29:41 +0000

    everyone tell average match who

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_power(work, time):
+        return work / time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
