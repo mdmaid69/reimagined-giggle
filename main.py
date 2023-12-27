0d832213908b7b7e68dbe1888a3fa7b4e763814e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:51:39 +0000

    anxious question forward run correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_average(lst):
+        return sum(lst) / len(lst)
