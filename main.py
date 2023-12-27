042744bfb875a27f7844ea47c6707e3bed1e2288
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:07:15 +0000

    trust sensible uncomfortable rain show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
