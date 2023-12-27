ce574f2b99603eef1365fb97647ecb70697c4b43
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:18:19 +0000

    must immediately agreement break preparation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
