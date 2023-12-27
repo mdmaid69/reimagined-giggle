e4575bb4c3534b1007b32254633fbf8e5fba13b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:47:06 +0000

    music tip wire sweet tip

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print(os.getcwd())
+print(sum(range(10)))
