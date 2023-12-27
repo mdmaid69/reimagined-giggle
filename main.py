47be3a67d291e3bed40b9c34f3d839edfc95a9f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:27:46 +0000

    Bb error treat properly man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def convert_to_hex(n):
+        return hex(n)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
