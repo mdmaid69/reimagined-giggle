4aff81a85af01d99a29bdfea6e0b942c38e3c644
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:03:20 +0000

    laugh ring pause dream agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
