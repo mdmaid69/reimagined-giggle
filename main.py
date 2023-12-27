9a1eb5894aaffe15164012acc407a04388f4d7f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:23:09 +0000

    top from brush welcome Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
