5fc204352ed25a562456e61f7bf893cdf6b69ed7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:10:50 +0000

    dress surface ear pot empty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-print("Hello, world!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
