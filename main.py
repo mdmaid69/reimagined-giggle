42afa8ead9f0d741ca902ecfb9399f22a077149b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:35:18 +0000

    fairly be reason help loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
