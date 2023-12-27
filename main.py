29f21edb1d98386c4263663f1941bc3ea45e2863
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:21:12 +0000

    upstairs interested would fall do

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_item(array, i):
+        return array[i]
