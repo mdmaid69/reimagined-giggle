139fa89358cd8c9a03341fcc7d9c92fe2842e9bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:56:28 +0000

    interview inquire card second closed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
