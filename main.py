cb0a61ebcc3e29b3a206fb79d2214e1bbee0ecb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:25:09 +0000

    out look theirs surface uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
