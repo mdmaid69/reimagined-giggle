b0a8e9ac221edebed303757217015330f9178a23
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:02:20 +0000

    road noise concerned through scene

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
