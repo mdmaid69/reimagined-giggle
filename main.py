309a9da7fc676f99e1fb4a107111ba0a8558384f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:58:48 +0000

    service pride night off comfort

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
