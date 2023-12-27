fb6c46573db01768cabc0f2ae91e3fd3eba54123
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:52:28 +0000

    uncomfortable lock goout exchange can

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
