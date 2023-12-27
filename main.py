da337df309781a57aba81ef548b8fb79bd78737f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:58:55 +0000

    noise environment care track if

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
