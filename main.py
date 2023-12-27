9024c85adfc220f460dac8f8fe9ee2fb40f88e3d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:33:26 +0000

    after Ss error patience fasten

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
