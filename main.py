d1c0b248606ea209a33b5d2457c4beb748ef375e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:00:27 +0000

    professional rough both check pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_length(array):
+        return len(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
