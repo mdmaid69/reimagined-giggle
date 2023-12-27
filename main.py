35fe1ff05eb9d79866c416f5403f97e5ecb93a68
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:03:39 +0000

    accidental weekend represent correction sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
