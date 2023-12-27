9d7f1a507cc6af5ea68baab8048a5e32a0fe4a4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:01:05 +0000

    picture deliver realize wash prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
