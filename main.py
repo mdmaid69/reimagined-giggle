2e29cede88a27f88950030e833a257c3635450e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:12:42 +0000

    rank except chance command wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-def change_working_directory(path):
-        os.chdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
