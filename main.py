0e0dfdf9ef464f9a3f4c0633b47fb21af18bb881
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:26:31 +0000

    taste left meanwhile fast tie

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
