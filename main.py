46def07242caa0de46f91d1bf29441a9571ecb43
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:17:35 +0000

    weigh translate murder accommodation contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
