24291ca5cec5c1b0f2cf2e2086de384a53413503
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:21:06 +0000

    stupid complain line match interest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import tensorflow as tf
+print(tf.__version__)
