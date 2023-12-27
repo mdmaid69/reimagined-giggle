cabe6a56a52f6321e7a460542fe212bc2e161acb
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:36:53 +0000

    appearance complaint story there provide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
