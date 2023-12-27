a6813b4a656fc103525e76721f5d3450e3a82360
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:20:34 +0000

    pipe good otherwise curious dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
