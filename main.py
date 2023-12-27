84553b3f52c9c5cebce34fb822c8d943adc80790
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:20:25 +0000

    test brown brave breakthrough uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
