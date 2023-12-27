c6b1f503674a580f792ef991f6c56ac828eaa743
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:35:20 +0000

    wash wise swing friendly crisis

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
