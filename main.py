ff1d46a7eb102e2c8d7300d35fc0fc624298219e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:33:37 +0000

    add equipment worst slowly Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
