4003a5b8770866a44dce3c23f80d13ce17007bb5
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:54:07 +0000

    arrangement holdon run confuse giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
