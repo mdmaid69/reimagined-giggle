5489fa9a5a1216e7b3745aefc9e49e2966221c6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:48:48 +0000

    split thirsty recommend comeout Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
