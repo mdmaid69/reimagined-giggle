01c49ceb90bb03e3087e930ea4485250e631ecf2
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:09:51 +0000

    that paper exactly agree intend

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
