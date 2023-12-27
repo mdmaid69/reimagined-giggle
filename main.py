2891ffc499ff5fb74bf2f572992aceb6420d057f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:31:52 +0000

    judge centre music giveup polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
