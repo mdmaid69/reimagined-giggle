882f9237eb6006b2995242c8e097c2ccfd92a436
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:58:28 +0000

    punishment burst Mm affect mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
