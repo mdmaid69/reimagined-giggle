2b32ca22c61361e1a1cf998c4a089f65f8525b1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:29:42 +0000

    content slide scream trouble likely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
