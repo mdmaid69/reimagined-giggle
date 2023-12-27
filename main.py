ebd8f0a05feea23763a92fe562965e25822bc648
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:30:05 +0000

    tool dinner error realize character

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
