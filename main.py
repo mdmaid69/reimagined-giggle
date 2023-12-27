41a52e70bf112caaa3c1109b8c2cf681789388da
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:41:21 +0000

    slight listen exciting deliver gate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
