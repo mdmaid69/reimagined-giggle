7b9061b1177f05b4d120efb5dd04d01226fc4442
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:47:04 +0000

    thirsty possibly turndown fire turnup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
