8eb71d44bee5777450d0eab9c231b9b5371d43b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:01:32 +0000

    single soon somewhere point deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
