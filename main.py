9b1e9f1a941f8e5196f06556f486bec603eeb169
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:34:02 +0000

    adjective fail giveaway freeze should

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
