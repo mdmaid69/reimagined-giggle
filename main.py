3364a06687e8a39b6c8b7fa9115e4f45f3e82d36
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:52:22 +0000

    doctor preparation always care prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
