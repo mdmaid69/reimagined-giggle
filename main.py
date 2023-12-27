ce1f36dbf743ad4dc166affad3a301c63b6e995c
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:27:44 +0000

    tap tap geton breath occasion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_area_circle(r):
+        return 3.14 * r**2
