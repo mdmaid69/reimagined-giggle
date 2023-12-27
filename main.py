85363c6ca24e664dbf31f4fcecd2bec6af6b672b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:10:33 +0000

    float rough convincing gentle anxious

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+from collections import Counter
+print(Counter("hello world"))
