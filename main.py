c49d22e3bacce99f1f4b0558e8950b44d1edd6b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:44:40 +0000

    meanwhile goon confident beautiful copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
