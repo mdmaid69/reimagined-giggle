366ee3d4fce7afdb52e32575392347c5c03b8382
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:29:24 +0000

    system long tip pile best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
