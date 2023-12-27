439f724b56ce8055078461addffaa6b5b13764bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:34:43 +0000

    mine very medium accept strong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
