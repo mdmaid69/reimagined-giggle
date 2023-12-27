17ceefe5ba940f2ae30e829966b63121e3004de9
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:31:10 +0000

    over earn copy fetch exam

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+print([x**2 for x in range(10)])
