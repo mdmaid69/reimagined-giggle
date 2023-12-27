17fb2b37c04ea49cf3b570a64e3009d04e56d35b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:46:28 +0000

    children top ask positive calm

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
