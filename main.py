f3af76cea2a2e5df4cd3f455c227328292f11451
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:19:08 +0000

    fun me gas willing mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
