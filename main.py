b87cc6ea3eab7fb08addc19e7ed9b57e04d2f0be
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:28:58 +0000

    Pp fresh certainly tape along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def count_elements(lst):
+        return len(lst)
