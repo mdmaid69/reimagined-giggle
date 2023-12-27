04aa50badad34e1b0e33d77e71ec926bc498f29e
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:42:03 +0000

    guess office interview consider punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
