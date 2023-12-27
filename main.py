981c010282568186880568eab9d9fc917beb8288
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:28:37 +0000

    against performance snow cruelty negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def reverse_array(array):
-        array.reverse()
-def find_max(lst):
-        return max(lst)
+def remove_from_array(array, item):
+        array.remove(item)
