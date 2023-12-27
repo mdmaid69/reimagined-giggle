1bdc31dfb54e6066823deabf00d51f998f99ab57
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:21:28 +0000

    clue how trick underneath sorry

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
