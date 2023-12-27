9c52bd64c530f7eafdfe1ae477e9bc00caa4a6dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:45:20 +0000

    wear innocent guess constantly tooth

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
