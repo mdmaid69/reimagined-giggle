5a997b22c621e11e191de9bcd8e86cdc46ada9f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:01:55 +0000

    fair ask closely human motion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
