d88c4e90ad3476352a5c6c5ad65a6252c4418ba4
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:01:28 +0000

    generous shame comeup far base

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-print(sum(range(10)))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
