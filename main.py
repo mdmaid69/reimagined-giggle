be116eeadd431b77ba2f52dc07e204eb0cbad30a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:30:54 +0000

    custom tape guess narrow rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
