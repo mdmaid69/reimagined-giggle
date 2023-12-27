7c4ecf090348a64c20f53f715759e47c5e11598c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:54:27 +0000

    accordingto shop practical bad machine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
