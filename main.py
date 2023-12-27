607a103120d3cc05631984fe0dc32839d6c9a75c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:24:22 +0000

    taste force key approve mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
