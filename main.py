9b5e3b833d83175f2f0d685fa9d920477ac7ba4a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:53:59 +0000

    turnoff tongue rare Rr healthy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_item(array, i):
-        return array[i]
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
