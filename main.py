2ba68007bfc60f31c83f422fccfb0bc537c62057
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:17:10 +0000

    blow colour his both table

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
