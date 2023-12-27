98e589a476c67995137161359f8e02fa5a61be52
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:50:10 +0000

    home extraordinary centre scare government

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
