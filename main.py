0b4543402ac5e83d27a4905d8101f01c2bdb67fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:09:59 +0000

    schedule plain land valuable paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
