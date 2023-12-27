57f166f6a943f24405af1bb45ce360ca47f39561
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:58:52 +0000

    once sell lead regard advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
