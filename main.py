d092e9898106523f92e0c0e7e7992e513966cba7
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:57:49 +0000

    change brother will petrol dress

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_string_from_array(array):
+        return array.tobytes()
+import time
+def get_current_time():
+        return time.ctime()
