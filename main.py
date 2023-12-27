61c7a870bb918921e8eb428efe7b684dbee56fad
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:09:42 +0000

    solve common constantly frightened smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def clear_array(array):
+        array *= 0
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
