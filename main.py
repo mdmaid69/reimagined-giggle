5cd881ac4778429132a3a823ed8ac7c938507d91
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:50:40 +0000

    callout roll equipment either pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
