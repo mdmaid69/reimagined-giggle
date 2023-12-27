374d25c2dc16e4d6c3149c6ed73177cbaa48ff68
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:05:56 +0000

    have warm giveback unfair sorry

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_area(radius):
+        return 3.14 * radius * radius
