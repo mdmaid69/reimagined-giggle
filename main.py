b7739c87acaf8134c966276cddb63e965370cf7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:16:56 +0000

    announce handle church pocket comeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
