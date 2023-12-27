87419b3f4a0083cea4c077b9ad9ccaa8474be339
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:17:37 +0000

    shade bin report upset Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
