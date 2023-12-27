c6ca416f0554219446067e4850dfb27bd57ffb25
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:07:17 +0000

    cinema main card copy standard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
