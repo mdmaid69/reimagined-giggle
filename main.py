998859ee3299ee12ce2cf91c17fb616770bb72c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:10:34 +0000

    stick this cruelty loan division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import re
 def split_by_pattern(pattern, string):
         return re.split(pattern, string)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
