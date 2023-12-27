161e85929bab082268561ed53121f995ec6744b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:36:39 +0000

    mother wash cause fast dream

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+print([x**2 for x in range(10)])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
