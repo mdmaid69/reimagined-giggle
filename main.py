a55232f5c621169cdc95a46ab312484b93502404
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:43:39 +0000

    afraid equal house add finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def is_odd(n):
+        return n % 2 != 0
