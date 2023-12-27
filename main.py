275c3574ba75234015ec812bf8d9675b4131674f
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:27:01 +0000

    exchange mix face genuine eye

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import socket
+from flask import Flask
+app = Flask(__name__)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def count_characters(sentence):
+        return len(sentence)
