ad16a65059601ca788a168e935479568271d3df4
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:17:31 +0000

    cut waste firm vanish speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
