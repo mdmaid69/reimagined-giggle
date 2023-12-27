dbfe3b3759a3882853ce38e9ae13735804b9c260
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:44:00 +0000

    until lost basis use sing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
