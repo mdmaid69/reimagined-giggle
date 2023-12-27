d9707f313d65dece1b366fc4ae346c498b915c61
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:32:51 +0000

    exciting lose minor software direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
