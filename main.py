df50bf5a1b4823b5fdfb571d49a383f42a6c003c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:01:32 +0000

    cruelty dinner tired unlikely takeover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+def get_time_since_epoch():
+        return time.time()
