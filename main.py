fd14a5835fa89a030ae7da3a20c0c132bd289660
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:19:15 +0000

    wheel face watch unless add

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def subtract_numbers(x, y):
+        return x - y
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
