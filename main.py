4bd138ad773d11c61ec25ff7f11b306d14d1bdc6
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:29:19 +0000

    unless raise independent force who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
