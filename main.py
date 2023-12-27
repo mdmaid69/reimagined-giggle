ca539bd37925d6d762d66aa9a1061ee700d928f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:25:05 +0000

    conclusion moral start uncertain bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
