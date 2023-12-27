dbdf58d91a3f181b1af5696ba4679b553341a3b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:57:21 +0000

    shade shut to throughout defend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
