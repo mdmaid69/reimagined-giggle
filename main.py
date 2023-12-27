c646240cca7898fdcdaea4ee81c3ef372e4c7d39
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:47:54 +0000

    dare active raw compete list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
