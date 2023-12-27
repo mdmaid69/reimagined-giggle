0a1e4fafaf924f6e0922a3f846f1ef2bd319522f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:27:29 +0000

    callback absolutely workupto interesting choice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
