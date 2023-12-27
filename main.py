cf5244c1b10262273afe30bb9f767589af995850
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:25:39 +0000

    turnup Dd base stiff dish

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def add_numbers(x, y):
-        return x + y
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+text = "Hello, world!"
+print("Uppercase:", text.upper())
