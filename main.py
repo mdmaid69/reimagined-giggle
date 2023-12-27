28edf200ed00bf6f7e855c8026a15513867967d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:35:29 +0000

    home loss comein concerned love

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def is_odd(n):
+        return n % 2 != 0
