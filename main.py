45be656716cd2c6a23aba3dc09c64d69980a29b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:33:07 +0000

    waste knowledge nervous thin letter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+import math
+def calculate_floor(x):
+        return math.floor(x)
