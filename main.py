8efd9d5aa78b8b7d3b25be61c08f07ced84b1dbe
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:11:27 +0000

    pressure always patience plant surprising

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+name = "Python"
+print("Hello,", name)
