450dd6f15ecd6f6d608c004adbed5e24bab59f14
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:53:40 +0000

    value injury pleased radio mark

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
