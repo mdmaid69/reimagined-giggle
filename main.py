34207e0b65df0bd8663e7c1d3856a36753e6492e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:00:02 +0000

    fresh pronounce past behaviour relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
