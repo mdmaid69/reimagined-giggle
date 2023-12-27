6b163125e87c4eb298b5add3a308f04333057985
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:20:24 +0000

    introduction off love coffee handle

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
