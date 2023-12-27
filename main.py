447ef6806bc8622353e122ca7c7c7c06be7950a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:16:41 +0000

    extraordinary walk stage reply callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
