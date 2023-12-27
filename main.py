dfd6c7927928c30e3c1ea601cf9c34701fb790d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:33:11 +0000

    round shoulder bend fire high

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import random
-print(random.randint(0, 100))
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
