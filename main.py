9d84e3319dab04805e902bd95bf81c0af1e334ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:56:20 +0000

    responsibility many mother watch turnout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_density(mass, volume):
+        return mass / volume
