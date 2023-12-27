1a376a87fcad5c56573c2d76bf83ac4dd796cad2
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:04:13 +0000

    show break doubt famous exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
