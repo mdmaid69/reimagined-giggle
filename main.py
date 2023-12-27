58cc992f92e4c518a1d2d4b6e8c028284f8e23a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:02:08 +0000

    cost variety sudden hurt wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
