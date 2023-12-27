dca5c5a47ad7dee21aead67bf5044df2df13965f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:52:33 +0000

    line sport star attach effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import sys
+  def get_python_version():
+        return sys.version
