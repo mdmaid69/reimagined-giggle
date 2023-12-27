d7e06510856ad395be7a8ec05fbecac96b4c69c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:38:28 +0000

    regard loud pool attitude roof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_set(array):
+        return set(array)
