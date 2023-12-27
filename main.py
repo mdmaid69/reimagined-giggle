4670f1f4c1f4754214a2de2d1680871c3707566d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:24:50 +0000

    uncomfortable pride slow correction seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
