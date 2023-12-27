2c91ebf305d048b83b0b32a1bda6c2d6a6b9886d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:38:10 +0000

    avoid duty noone smell close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_counter():
+        return collections.Counter()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
