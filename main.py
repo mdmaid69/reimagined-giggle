f67ffd06f66c9636075b045f8aabbe14c74c0125
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:44:22 +0000

    because Tt turnon news be

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
