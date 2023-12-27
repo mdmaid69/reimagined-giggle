83618ff005a500e32490e90ec04fbb2d830c6cb0
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:47:13 +0000

    inquire escape runover try ago

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import datetime
-def get_today_date():
-        return datetime.date.today()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
