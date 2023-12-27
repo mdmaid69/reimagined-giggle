4eae8bbe4a5f649d03529e57c0360228f2f59e17
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:45:01 +0000

    study accommodation record priority town

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
