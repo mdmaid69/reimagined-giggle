75df08191b62398c89611189a90807329f95d9d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:43:42 +0000

    serious kick by Zz break

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
