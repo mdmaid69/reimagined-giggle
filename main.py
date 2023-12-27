58160c544e478071d715eba5540e828bd9374ba6
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:11:04 +0000

    party trust cover offer claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def subtract_numbers(x, y):
-        return x - y
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
