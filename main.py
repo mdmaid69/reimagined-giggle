d3be4674aa8cf47bbfedbe8f58bbc6f318fe552b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:06:47 +0000

    church congratulations tour reject life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
