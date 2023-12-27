a14cb55f16981e7c409c39b077930bc0d1c2f865
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:14:28 +0000

    home exaggerate offence try reserve

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
