bdf0c5ca379af613205013b24c22629a8b8243e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:19:10 +0000

    never public obvious control control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
