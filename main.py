ef05924acb5bfbd42de7dbd2a8a7c13771d15d48
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:51:53 +0000

    smell stir dry call say

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
