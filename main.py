51d600a72a31666c18d8907a5cec0b72024ca4c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:52:35 +0000

    machine cope raw calloff side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
