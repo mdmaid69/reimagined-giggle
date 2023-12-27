cb6c3ba768c8f455dc8d13b26634659d061b8cdc
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:51:16 +0000

    push angry lonely ourselves attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
