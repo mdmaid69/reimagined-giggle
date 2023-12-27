f6c131dfbc6a6433ea8d11836d014b3678411d6a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:09:04 +0000

    sharp takeaway tongue emphasize meal

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_power(work, time):
-        return work / time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
