7734b33a66ef4e2af957f0a9eb5ae3187038c697
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:33:04 +0000

    conduct dark bringback bother free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 def find_union(list1, list2):
         return set(list1) | set(list2)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
