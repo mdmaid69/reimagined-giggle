744e1de0df51270dede3aad6339341038ea91cec
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:37:56 +0000

    company substance pretty miss communication

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
