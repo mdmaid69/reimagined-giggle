0ff93f5f0dd1cb9510605d5d88f954cbf9433fc9
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:36:32 +0000

    decision your grow place classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
