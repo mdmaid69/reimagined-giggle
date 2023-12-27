c2aa7c7c3e8b761276b65fad07a2133dc25de122
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:51:01 +0000

    uncertain hot platform what involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
