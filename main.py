34cb03275ec6a4c52d3c4a3042ac460eb47692d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:09:07 +0000

    either word talkinto dangerous reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_current_working_directory():
+        return os.getcwd()
