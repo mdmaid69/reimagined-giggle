7524ecc79548ac95420a098fd06b358c46d13623
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:41:45 +0000

    lead reason hollow feature damp

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
