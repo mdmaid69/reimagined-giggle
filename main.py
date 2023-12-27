5077a2ce14bef8abc1881441241f0cc9951a641e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:14:14 +0000

    cut protest makeinto should pronunciation

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
