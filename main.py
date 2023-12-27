6645e9e88e6565966fa71f5f05f8018b61ce9139
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:22:47 +0000

    occasional hurry breakthrough flat prefer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_volume(length, width, height):
+        return length * width * height
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
