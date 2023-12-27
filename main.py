e830a607b35c5da60924671f9cc1742242cc56c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:55:55 +0000

    ilovedicks negative value wrong unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def cube_number(x):
+        return x**3
