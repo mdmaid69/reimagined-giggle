377d589f7f5d5b558639ac5d8a1888a42314d7b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:11:13 +0000

    exchange square big satisfactory warn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_list(array):
+        return list(array)
+def multiply_numbers(x, y):
+        return x * y
