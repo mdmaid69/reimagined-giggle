3b6352b65d86a1cf30780ca347fd20f8885ea0ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:25:05 +0000

    finish identify height smart treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
