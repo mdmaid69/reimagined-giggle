cf132d880a64861f8d68a8fcd7dccfd3ccf0a250
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:25:51 +0000

    waste slowly careless Ll mother

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
