575975ddca7cd891934b266f9b475532658a1664
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:12:41 +0000

    speed mood plastic way hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
