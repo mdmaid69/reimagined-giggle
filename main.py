db24f3bae6d9f60519e85e0f8974f2a60041ca55
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:02:18 +0000

    talkoutof crash Itself side basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
