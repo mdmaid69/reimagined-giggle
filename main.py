1da5e42b749c9dc7c8eb2c2d460f58fdb5a697c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:55:07 +0000

    Ss recognize while instant bringforward

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_cosine(x):
+        return math.cos(x)
