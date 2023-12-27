79522571704d60aa735f7b47d855935926fde984
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:20:12 +0000

    crime speech until comment education

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
