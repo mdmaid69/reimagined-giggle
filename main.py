091393ce363977121bbde182847fd9fa5f1b455f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:33:18 +0000

    send stick whoever for loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import os
+print(os.getcwd())
