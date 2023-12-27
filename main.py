b64cb01f4b79707485dc72439f1341aae2fe90e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:42 +0000

    grateful own difficulty join wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def divide_numbers(x, y):
+        return x / y
