39188e43d17ff6c13805919be439c95dbe498eec
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:03:02 +0000

    term late together suggestion question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_index(array, item):
+        return array.index(item)
