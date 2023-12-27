40e96af9b29a813124dc1710aa3ad28a3374bff4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:01:01 +0000

    ordinary drop else false book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
