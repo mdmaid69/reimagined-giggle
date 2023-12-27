13ea34c7ff028d6c4e265ec65c35aba1a99a91bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:59:31 +0000

    blue talkback around calm no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def multiply_numbers(x, y):
-        return x * y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
