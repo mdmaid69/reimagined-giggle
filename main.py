2ee39212e3cd0f16de277f17102447ae83378e22
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:35:33 +0000

    behind content fire takeback Bb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
