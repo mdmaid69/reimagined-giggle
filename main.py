46079580f8d97ff59898c02ca76c8018178fe354
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:45:47 +0000

    shadow fence stone fork apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
