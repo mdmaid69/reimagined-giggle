0bebc459b6ec7365c67fd38b85a6291912a7735b
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:13:53 +0000

    pinch task frightened Ff Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_as_list(array):
+        return list(array)
+def remove_duplicates(lst):
+        return list(set(lst))
