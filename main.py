fd203054acd923e2856c335f13731b6f3fda5aea
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:26:40 +0000

    exchange lunch brave face show

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def reverse_list(lst):
-        return lst[::-1]
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
