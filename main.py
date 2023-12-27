c0242c0654501ce180b99653cef61f10b67edc1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:29:51 +0000

    lock change listen female take

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
