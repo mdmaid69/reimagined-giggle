f33db06bdee216d5b308e9c8516c48a7a746205d
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:16:39 +0000

    responsible steal alone this positive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
