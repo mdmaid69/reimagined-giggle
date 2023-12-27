f1cca082cc793051b6392ae7e51b50d47d4c7027
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:50:51 +0000

    connection turnback join bringback suggestion

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import platform
+def get_os_info():
+        return platform.uname()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
