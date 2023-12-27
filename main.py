d7b80f3cbe0b83c86d3fa4f3847daac31fdce53a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:30:04 +0000

    example rich possibly arrangement sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
