04ed342430cf338bf1f2c20fbdb769906974463f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:35:16 +0000

    proof remove share ride takeover

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 print("Hello, world!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
