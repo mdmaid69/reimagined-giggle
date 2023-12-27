3ba8f2bad7c949dd36a7c84f2d2062c2f98fc76a
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:48:33 +0000

    entry challenge brave praise possibility

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
