eb617014e2dde8f84b3a8b595469a8597f3ee9ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:17:24 +0000

    cut please yourself walk Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
