204149204a5bc213734d8562ba8359e839e4fa38
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:02:32 +0000

    same comeup explain outside apparently

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def get_array_as_int(array):
+        return int(array[0])
