c0854b591b09816ff96c7d929d1d0f8f8ac8564d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:44:39 +0000

    shock like cinema say community

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import time
-print(time.time())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
