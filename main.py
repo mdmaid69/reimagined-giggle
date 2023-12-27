f2a62316807a812f796bb6b84016226a69d4320d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:08:33 +0000

    tap bunch steep appear divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_list(array):
+        return list(array)
