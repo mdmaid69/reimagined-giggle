f58dc71419511a6f2867d0a51747e18b02c35548
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:50:08 +0000

    earn bring respect pause project

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
