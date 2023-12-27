b2d8fa71532e79141d628498bf24dd261e1c20f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:08:56 +0000

    music material feeling offence Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
