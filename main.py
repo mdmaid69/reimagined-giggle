97d5f2feb01e2133b7ed9bd312d3d2f346968140
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:21:46 +0000

    stranger from address turnback information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
