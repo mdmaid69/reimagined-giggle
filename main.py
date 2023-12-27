9e089b55857e404ef68c1537e3084807f2a99e76
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:17:52 +0000

    clean boil rubbish closed drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
