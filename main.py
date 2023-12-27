7757a402978f131f59ae99875cdcbc5b94017c97
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:57:54 +0000

    speak neither tradition whisper stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
