951a20b47fe2d57afeef11ec4550b9e7c347b3d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:58:09 +0000

    trial woman experiment left nature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print([x**2 for x in range(10)])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
