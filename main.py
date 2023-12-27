e0212be474b1adebae06b1fefcc72b0be36e585e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:51:43 +0000

    dark slowly bin possession from

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
