f4e2fd119df9a7e519fe0db925d3246645b72b99
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:20:39 +0000

    cum correct book branch accommodation

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
