659c4c976c8c7cf6babad6bb87caec887eb1855f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:45:36 +0000

    invite deep sink rest problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
