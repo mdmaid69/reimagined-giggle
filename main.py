f74c388eee84fd5ff0158ae6a99474a836b4f148
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:49:20 +0000

    frightened glass nowhere through accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
