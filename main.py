53b542576366c69d776ecf48946f8016acd21906
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:11:31 +0000

    border form settle star demand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-print(datetime.datetime.now())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
