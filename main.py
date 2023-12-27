5097b5535edb85db0fca0372d30160c373b71f7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:32:57 +0000

    bottom begin imagine school address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
