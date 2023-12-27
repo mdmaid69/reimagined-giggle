484e773c4c30be2f4bcab437d2365d9af2016228
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:23:02 +0000

    guess always sore skill judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
