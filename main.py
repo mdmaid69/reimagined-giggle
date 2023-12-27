2fe289f287d7bfc091654817f5c46f23a6fed251
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:39:30 +0000

    appeal impressive ignore face generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
