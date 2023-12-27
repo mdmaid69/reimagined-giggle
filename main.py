d3603f57b55dba98d2e78a64ebb1f336e647fde6
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:13:44 +0000

    somehow manage clear that giveaway

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+print(sys.version)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
