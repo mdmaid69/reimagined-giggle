d6133b48f797bea02667df2d3b314e3bb90cf0ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:44:40 +0000

    wheel sister goup crowd guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_ceiling(x):
         return math.ceil(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
