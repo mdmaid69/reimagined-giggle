09119a29ce8cf24055b2aa7a3c566a969a6cfc07
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:16:22 +0000

    slightly wash something slow well

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
