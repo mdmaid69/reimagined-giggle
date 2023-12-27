8bc7e17a307a52f8ddffc64816e15f075a708026
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:25:06 +0000

    deliver another official mess clearly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
