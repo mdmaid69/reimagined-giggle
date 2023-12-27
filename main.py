e4ae0af32367b281e7896fc54820e8b6bef83e6e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:20:36 +0000

    translate say opportunity whatever fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
