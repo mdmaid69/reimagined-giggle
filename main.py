6c8981aee591e49f1dcf5b890c0c1fc937c818fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:15:52 +0000

    he instant interested any anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
