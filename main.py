19108b6401bab6be355d59e36619f63b00239c86
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:41:57 +0000

    breakthrough bottom criticism top pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def count_elements(lst):
+        return len(lst)
