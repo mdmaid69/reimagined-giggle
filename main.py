6bc4b5e2bc954059858bae4c7e9a19ca5b39219e
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:16:41 +0000

    factory loan shelter introduce purpose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def set_array_item(array, i, item):
+        array[i] = item
