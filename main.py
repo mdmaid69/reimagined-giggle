796712786cd71c513a883d77e477935c1b08fd31
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:28:57 +0000

    waste talk term thin concern

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
