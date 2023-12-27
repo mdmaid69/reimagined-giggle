b1c54409c8ab4b5865bd77f9ee7d23ad4d0b0e34
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:30:23 +0000

    comeout hide risk related public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_base_name(path):
+        return os.path.basename(path)
