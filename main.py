8e8f59b99d5cf15c78ff9efad9f794457921260b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:30:21 +0000

    declaration clear straight complicated object

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
