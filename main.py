1334721ec4b994eba251a5ca17d463da99b16427
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:52:50 +0000

    birthday cruel repeat friend legal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
