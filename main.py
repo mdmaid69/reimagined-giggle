aa6d40b0dd341f39df76d72d615ffbcecd39df1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:56:57 +0000

    imagine field wealth matter slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
