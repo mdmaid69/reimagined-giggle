71c48ec148557b53eaa02b525b5ea067b9c4a392
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:56:47 +0000

    by butter community rich wire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import os
-print(os.getcwd())
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
