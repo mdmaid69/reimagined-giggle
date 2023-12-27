32517695b86621e15f99573ad6989308ae6d47c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:31:46 +0000

    claim brief narrow edge prize

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+i = 0
+while i < 5:
+        print(i)
+        i += 1
