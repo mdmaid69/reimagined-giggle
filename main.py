31d5f41669de9426a5b3eed223dfb3f6f1029d92
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:03:01 +0000

    actual somehow getback only down

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def multiply_numbers(x, y):
+        return x * y
