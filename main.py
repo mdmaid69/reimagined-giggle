fee0f35a70201bd10cbb65865bd96728ab93b8ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:24:30 +0000

    journey breakthrough sorry steer match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
