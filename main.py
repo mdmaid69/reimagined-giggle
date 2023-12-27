ebaebd33d568ca85a6cf0bec2f4921bef0ef9c18
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:11:38 +0000

    foreign Oo you choice throughout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
