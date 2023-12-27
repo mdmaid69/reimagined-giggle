23d7bbbfc915bac3415f6d655744a9150009af73
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:55:19 +0000

    advanced 572enter share getaway search

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def subtract_numbers(x, y):
+        return x - y
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
