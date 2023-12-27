fb47f7b1bdbff4249daa2bde0c40d44480a3d94a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:44:08 +0000

    possession uniform mind brother nerve

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
