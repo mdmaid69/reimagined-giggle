21cd562b26073b6026a27460fcecee95b1d20dc2
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:41:12 +0000

    minority deliver hot concerned well

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def is_odd(n):
+        return n % 2 != 0
