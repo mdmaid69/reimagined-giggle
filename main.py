c3f4b277d80a474631c7dff739ad7995a582d4db
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:40:22 +0000

    story unable regular forgive perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
