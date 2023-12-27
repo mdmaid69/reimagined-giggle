4f4669bd3251eea4ad8bba2024defbc6ad17a55f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:57:12 +0000

    also lookafter talk sometimes begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
