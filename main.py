9bd0b208f72168bd94209a999228432aa13af745
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:52:52 +0000

    weigh quality limit question key

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
