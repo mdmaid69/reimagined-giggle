73a05a450ee9544e3da25e889c5b3a197e0fbb1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:42:26 +0000

    fall friend temperature any criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
