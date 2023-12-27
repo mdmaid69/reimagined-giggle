e4976e020ad18e750e9d629ae5a14f3ad551b3cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:52:03 +0000

    narrow both string already arrange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
