cc517e07e9b8581f7c0a8c636d5108bdaa5c8605
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:13:15 +0000

    pull perhaps ring recommend chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
