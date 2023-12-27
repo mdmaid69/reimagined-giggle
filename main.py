9ac63788c41e0f5b93eaf789122d0e20eec75ff9
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:02:19 +0000

    compare big centimetre supply various

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+print("Hello, world!")
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
