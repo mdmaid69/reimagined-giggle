af434d275e814f8225b54b6f89fce04192bd993c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:30 +0000

    confusing food turnup march handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
