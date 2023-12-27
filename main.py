28ce07bebc4cbf0b5c48b1e1e6b23d5247abee66
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:41:51 +0000

    check will contrast hide forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
