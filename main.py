c29335f7d12595c84bf0a678b0ae95c79659ba37
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:53:15 +0000

    today length cut poison whistle

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def sort_list(lst):
+        return sorted(lst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
