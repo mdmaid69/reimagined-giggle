eba5c6143544b6a320b03c7a65d06df874fed2ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:42:40 +0000

    block prove waste top suit

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
