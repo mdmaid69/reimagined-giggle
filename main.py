d51ba1c117e98ef7e960f980cb073441eca5f7c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:34:09 +0000

    fast instead knowledge tall together

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def square_number(x):
-        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
