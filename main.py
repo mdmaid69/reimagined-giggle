7e888006a32f39b265c7260838fa6b327b96e457
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:59:25 +0000

    under patience home popular absolutely

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def cube_number(x):
+        return x**3
+import collections
+def create_user_list():
+        return collections.UserList()
