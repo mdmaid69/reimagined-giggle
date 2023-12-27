41bbc0959226794c10521cf1c284c28bed810aaa
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:51:28 +0000

    tragedy sale anyone listen sense

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
