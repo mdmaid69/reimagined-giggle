490715ef4b140310fb77c991803977f9ecbe30fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:08:31 +0000

    uncomfortable behaviour admit emergency tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
