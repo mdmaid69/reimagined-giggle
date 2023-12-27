553389cdb0573e685f04b9f1deae5d5ed3cf199a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:02:21 +0000

    soft later field join approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_bytes(array):
+        return bytes(array)
