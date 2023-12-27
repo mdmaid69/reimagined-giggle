cadc14190500566eb5c85ba7909401f2ce0a5e22
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:21:27 +0000

    tone door determined regret healthy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
