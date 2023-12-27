d3dc776a52ff60898788fe80bf6ddc69cc3159fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:36:53 +0000

    legal bright apparently small Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
