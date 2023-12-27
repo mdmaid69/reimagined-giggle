f739b864a41ae346904802956ec78019340abb20
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:06:14 +0000

    born cut mean go Ee

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def convert_to_binary(n):
+        return bin(n)
