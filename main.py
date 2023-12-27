60b925e820a4c8a1023a8ba0110ecdb7d42d50cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:54:00 +0000

    hole brown pause destroy active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
