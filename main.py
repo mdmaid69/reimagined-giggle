bdc44d7b7e9ef60c62a3900acb9db2bcbb65d9ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:27:24 +0000

    quit usual sign throw nervous

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-for i in range(10): print(i)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
