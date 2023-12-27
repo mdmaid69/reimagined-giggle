66846176647a85cc0c9daba902eddd8fc7e1d0ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:42:25 +0000

    steer turnon course run complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
