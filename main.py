697c18c65680e1fb60c4ecf11c5f27cbb8a3d455
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:07:43 +0000

    equipment stamp contact battery best

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
