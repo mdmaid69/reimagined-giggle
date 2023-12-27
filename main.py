bdf0c8dbfe823789f91531425087057a89c81873
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:08:07 +0000

    in building complain during ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
