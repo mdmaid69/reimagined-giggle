06bdf4a287b0614b954a584e34df9c8efa1220dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:01:08 +0000

    search trip offend throw uniform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
