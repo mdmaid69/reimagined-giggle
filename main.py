657a30e51f4221cab998cad2949eff414ff371a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:30:23 +0000

    challenge and part mine achievement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
