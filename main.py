fc32c24f3a1d63bda85fc91c9d328757c184eaf1
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:46:48 +0000

    sail development regional available him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
