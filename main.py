d691e8e4ba79ef4f8ed11d984ddf0f63559bcd67
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:39:07 +0000

    disgusting splash grade definite evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
