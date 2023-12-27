f98c0acf76ca52c313111d012e0944f1b0b6ed9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:36:03 +0000

    club record successful relative decision

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_min(lst):
-        return min(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
