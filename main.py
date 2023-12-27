900e43661a5e7406fc530b1b7148f3df82104467
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:40:49 +0000

    practical sudden music design write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
