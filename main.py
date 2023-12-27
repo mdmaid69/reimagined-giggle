490f5cd4d4871e2f734078085c9022f904fb2cbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:38:25 +0000

    fault kind than flat suggest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
