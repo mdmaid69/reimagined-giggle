f15d00222997f6f84bc1e72445e2dae56867cac9
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:47:42 +0000

    impressed increase onto respect effect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
