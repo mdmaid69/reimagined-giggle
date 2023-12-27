8ed87aabf930c81e8677eee153759dfbb8438563
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:01:11 +0000

    tidy tomorrow few offence offence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
