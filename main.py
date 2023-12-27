e3827e1cb17c3c30b7bb0e1e9793ef568e33b635
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:59:51 +0000

    month swimming try back close

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
