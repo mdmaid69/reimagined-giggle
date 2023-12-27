27074a2e6cb1182ef26fac665076cfb654f9dc90
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:52:28 +0000

    considering mine total lookafter attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
