b37771ad011a897efae3f80d91157882969a89bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:21:17 +0000

    anywhere amount owe roll damp

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
