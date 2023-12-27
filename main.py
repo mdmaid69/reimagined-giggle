2a07fa7d9a0c20461bec4b7df6c5be66704caed5
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:45:54 +0000

    round fire takeoff at Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
