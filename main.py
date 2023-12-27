ca8f28db1fc2f76890d38e05aac5b9aba5ae0fbe
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:17:44 +0000

    connection whichever result represent approve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_to_binary(n):
+        return bin(n)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
