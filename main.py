b1c7f1445b793970118a8aae62a308b98ebf5cdc
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:40:08 +0000

    create lonely remain snow as

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def roll_die():
+        return random.randint(1, 6)
