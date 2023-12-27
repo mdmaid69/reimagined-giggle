86f69d7ab8c9652fae35aab4579d84377456c956
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:40:34 +0000

    remove turnon neither comein loss

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+print(sum(range(10)))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
