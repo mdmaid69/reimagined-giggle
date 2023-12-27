90373c2a2a055684f8f0eb86e7bc93f3f136c302
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:05:02 +0000

    original frightened machine born help

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_tuple(array):
+        return tuple(array)
