5d13e1e3dae0ecfcc629684c574418c357a26884
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:40:16 +0000

    any space goup before represent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
