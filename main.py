bba1f805f90b65cf5068cd388c6757fc4e83ae45
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:38:27 +0000

    damp content subject keepout no

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
