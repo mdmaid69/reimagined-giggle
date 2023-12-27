37153e2753bc5b175aa9b64360a9c4e8660ac884
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:39:27 +0000

    may produce across keen pattern

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def remove_duplicates(lst):
-        return list(set(lst))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
