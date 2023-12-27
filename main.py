220166ac9815e95fcb372bbde98a935403733d91
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:40:30 +0000

    smart operation memory spring rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
