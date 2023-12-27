84e576e8bf75219e2203a7ca7437207827c9fc45
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:29:50 +0000

    punishment behind on Ii please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
