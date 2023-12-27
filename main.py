772d68866fbbf79364834145924138b937e612e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:57:59 +0000

    help you careful month tradition

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def reverse_array(array):
+        array.reverse()
