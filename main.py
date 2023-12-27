ec19a06036e26ba081f50ee5c92400c47bf647f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:15:00 +0000

    respect left turnoff sense 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
