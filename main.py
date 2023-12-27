4ee9b3d8dd75005c8f4f4ce86a4e0f64e4d5069a
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:59:56 +0000

    apart slow concentration stage fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
