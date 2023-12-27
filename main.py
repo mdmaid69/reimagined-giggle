640b8b6bb709c7b40dc881db4c3aef3ef39e4e78
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:33:14 +0000

    soil touch ear cheat operation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
