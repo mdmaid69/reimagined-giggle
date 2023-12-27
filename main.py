d1e11df799884f95d3f3ad0820e46fd4ba710e85
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:23:16 +0000

    mass Gg there fat surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_speed(distance, time):
+        return distance / time
