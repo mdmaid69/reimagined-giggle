f97199adf249aaab56b53d4b162dbe4e9cc313d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:57:48 +0000

    wall lost shop supply left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
