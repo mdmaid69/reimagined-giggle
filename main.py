f072829f4e0aebecd5cd61e57ed5f4c1b987baf9
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:48:24 +0000

    contain through steady sensible getup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def reverse_list(lst):
+        return lst[::-1]
