23ea013930346b0992130103bf94c54bca1d56f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:58:01 +0000

    account behind comeout face bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_circle(r):
+        return 3.14 * r**2
