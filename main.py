31ae34565dd320b52e91fc7bc3c624b282a21e5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:53:58 +0000

    sound safety commit pot news

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def cube_number(x):
+        return x**3
