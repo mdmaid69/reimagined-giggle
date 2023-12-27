10ec1f3d456fec25e79258aca3e282726e117635
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:49:52 +0000

    swing limit valuable nerve instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def delete_file(file_name):
+        os.remove(file_name)
