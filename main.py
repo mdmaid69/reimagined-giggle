7817397791ceff9e1d55a894a2d6e11be22a7373
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:13:14 +0000

    history birthday test takeoff rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
