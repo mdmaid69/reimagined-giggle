245d48a37bb26ac77457d0f5c3c9891daa38ba56
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:39:32 +0000

    thinkof married thank original never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
