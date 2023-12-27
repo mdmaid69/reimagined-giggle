7cca07730c8abf4244d092a0fcfca563747e0222
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:51:11 +0000

    admit condition over nor zero

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
