cd7d3551bbcfbb3405849ebd24fffbaa6d73bfc2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:06:36 +0000

    into rest tonight giveout better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
