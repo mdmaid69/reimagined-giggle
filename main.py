f9004946f71d4329aeea10e4515e2c8e362c349f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:18:43 +0000

    Tt constantly appear dance identify

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
