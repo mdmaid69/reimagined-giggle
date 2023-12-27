ab274b75691ca78b26a1ee8e78a2b737fb805825
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:29:47 +0000

    watch black agreement earn possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
