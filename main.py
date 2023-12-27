c4af51615c5a54b0e6aa0655f2f069e986753141
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:08:15 +0000

    storm blow by feel wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
