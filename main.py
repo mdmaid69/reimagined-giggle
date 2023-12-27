fb9a2df89e1537be2de2e111c65098f31fe00ec4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:43:57 +0000

    release vote whole entrance callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
