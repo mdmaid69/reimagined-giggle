2eb9590cfc7694a3db80462eed0f231220e6e9bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:06:47 +0000

    convincing anxious bit announce realize

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
