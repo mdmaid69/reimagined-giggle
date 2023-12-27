7d6dd71a634461ddc7aa2b1fa1f996eafca0348c
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:43:47 +0000

    ready borrow handle destroy business

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-for i in range(10): print(i)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+print("Hello, world!")
