40114fef7e662f19b6e4c717fd8a164f923984e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:50:03 +0000

    wave smell of Jj real

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
