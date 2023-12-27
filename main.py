7f3dbe1fa644461fd3df451c6da72234714ef015
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:58:29 +0000

    his makeinto giveaway rather her

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_index(array, item):
+        return array.index(item)
