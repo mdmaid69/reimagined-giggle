8d349afa059f3a2cadaec2c7489a1d6ca866d116
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:55:08 +0000

    consult difference environment Zz prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
