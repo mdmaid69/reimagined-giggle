7abfedf324a755058fe81bbb560f54a92a534e08
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:01:07 +0000

    try embarrassing start attitude this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
