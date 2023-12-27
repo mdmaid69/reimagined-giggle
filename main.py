1a940519a5ccc5719702f63b48c098ebdd6371cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:53:46 +0000

    none opposite ring me matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def count_elements(lst):
+        return len(lst)
