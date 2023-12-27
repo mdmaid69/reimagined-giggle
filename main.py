a0e3d01ea134c1782908ab331aad6d5445166855
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:46:04 +0000

    tired news seem print kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
