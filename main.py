e1bc0a4e7c952f31a3ad6c7a1e157f663d460f96
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:42:58 +0000

    change talk usually big by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
