586ddcd285a86c1eccdc996a1bb1f75b1edadfbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:58:35 +0000

    plenty remain eager note jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
