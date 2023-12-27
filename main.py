006712962d0480a3befb5d9188d5e94adb8e2803
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:02:36 +0000

    kill spot otherwise available shop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_typecode(array):
+        return array.typecode
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
