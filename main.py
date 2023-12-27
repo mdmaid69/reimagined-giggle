6d4b714f21893921b40640431883e4628eac6455
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:49:37 +0000

    split rely long objection embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
