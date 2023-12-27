c09eb76dff934d894efcf968af3f4c47900efb0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:59:07 +0000

    travel sweet company clothes cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import re
+print(re.match("h.*o", "hello world"))
