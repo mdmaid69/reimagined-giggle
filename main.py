a3790a28dfef12fe663fa22949f6cf16aee43345
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:06:50 +0000

    precise similar war contrast popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+text = "Hello, world!"
+print("Words:", len(text.split()))
