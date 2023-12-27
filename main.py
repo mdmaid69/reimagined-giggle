5aa0a7bf0fc33a9548f07043438add9ae30ef1bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:47:18 +0000

    material slowly factory country final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def cube_number(x):
+        return x**3
