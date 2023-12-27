c6bcec4039a0fb2cf53ac025d5e593412f6b6a0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:42:42 +0000

    drive pair bill brain wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
