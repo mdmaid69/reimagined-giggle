af5fa4588e7986b58cae2d7a0e6745bbf97463f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:07:12 +0000

    trial few boat scared use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
