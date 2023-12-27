91fa03af4c2ad2110466de0f21c483df5b84bef1
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:29:07 +0000

    return to innocent I increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
