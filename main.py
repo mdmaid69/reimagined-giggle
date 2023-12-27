13a85b2882754143b00c912247c8aa042ed60b72
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:27:24 +0000

    minority pupil now exactly need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
