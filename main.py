86d02705cda13355e7d3ed5bf3f9cde441b37e2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:09:59 +0000

    female work react apparently politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
