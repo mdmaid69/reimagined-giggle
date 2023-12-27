5be8d8555f145414d154ab9a936af59ce2d52c1c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:11:03 +0000

    feeling smart wash reject if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
