b8b52dad3603e0630b31b423f1c54ab4b1216648
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:22:32 +0000

    muscle uponon young sister runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
