a79262b37ce318f65f0562338d2eab80400463f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:45:25 +0000

    approve comeover colour explanation sticky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
