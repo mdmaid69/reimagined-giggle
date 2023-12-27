c82f25af6c09734ad783ca1252d287d7c6b5b196
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:35:46 +0000

    fly 565end argue care substance

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
