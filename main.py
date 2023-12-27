4b8c53863155a6e31cde3fe3f75db5bf0b1094a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:50:39 +0000

    bag ocean remark keepout charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
