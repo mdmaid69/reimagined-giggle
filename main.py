fb1ac376b4ec38d9bfdb4086d460a398956ef8df
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:27:30 +0000

    record measure rain sing work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
