a90be090e9d448cead81bfd6dbe36ff890fe8f7f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:49:59 +0000

    sister Nn lucky rush left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
