f6f1a38e12649a841d50aa168c0b101ba99bdaac
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:39:06 +0000

    lead admit warning new operation

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def is_odd(n):
-        return n % 2 != 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def remove_directory(path):
+        os.rmdir(path)
