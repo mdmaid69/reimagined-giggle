6daf03656f9ed494a32fecdabbe96cd7311bee20
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:14:37 +0000

    whom muscle must low seat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
