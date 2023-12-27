2efb8dbe9315f0b2b6bf345c5837d59d0d81238d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:47:41 +0000

    scrap brain fear fond situation

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+for i in range(10): print(i)
