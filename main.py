a3e488e883540d7916bcc8b0249b89470d6cfffc
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:54:20 +0000

    music weekend here place harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
