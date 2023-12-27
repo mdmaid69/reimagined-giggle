121f2c820f5098c8b35b7965ccc240c03b1f6e30
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:37:45 +0000

    single better what trust scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
