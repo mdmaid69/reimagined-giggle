6b1376dd77d8aaefe14c5fb351c75392faea4e54
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:48:20 +0000

    hardly tense reply really sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
