119d9939e9b9f93f9e95301b59338a1104f4674e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:08:44 +0000

    careful takeout stiff constantly trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
