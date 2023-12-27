7819f6e2befc68b625db4e3556c40e4e61347a60
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:53:46 +0000

    deliver stupid wide flight pump

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
