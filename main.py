b33f3578bcd10b3695b2c0f3e883bedecddf00c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:37:31 +0000

    mad sorry circumstance artificial Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
