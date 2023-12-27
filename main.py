d42ea03ce3a51b29ceb001f3587916180761d984
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:09:06 +0000

    soon trust performance scare twist

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def find_max(lst):
+        return max(lst)
