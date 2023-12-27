ff014d18a8846efd078b41bd7ca1fb38d57ee835
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:30:19 +0000

    final confuse interesting check powerful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+print(sum(range(10)))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
