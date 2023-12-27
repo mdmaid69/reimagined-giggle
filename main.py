a9910b4ae94e5c89837a1442f230b6127ea4bd41
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:48:26 +0000

    comfortable complain towards fresh flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def multiply_numbers(x, y):
+        return x * y
