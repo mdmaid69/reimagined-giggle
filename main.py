ca89eb963f816489b234f511b4bca3e0fe1930b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:05:54 +0000

    false fear earth programme add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_list(array):
+        return list(array)
