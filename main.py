ac508d66cc341b32b59f7611ed18a0acb2496955
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:48:24 +0000

    improvement order aim before land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
