d16d77900c58bd2f72ac5d8f6bac86e07f4d03a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:14:32 +0000

    whenever land night necessary sentence

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def greet(name):
+        print(f"Hello, {name}!")
