65ae7adad649251a4e93aa371254433a6da97d63
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:34:40 +0000

    pupil passenger about mark could

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
