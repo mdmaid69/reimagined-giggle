4223a1d38b725ca77dc289b39e1978b9824bf952
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:52:09 +0000

    nothing constantly translate unable badly

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
