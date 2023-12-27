5c644c26bc99f1a02a0a7e4b32dd9fe6729510ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:35:17 +0000

    song ignore connected rest owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
