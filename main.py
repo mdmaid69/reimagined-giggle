ff71a08fdc9878da7418f0344881773f5f9d9e88
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:39:55 +0000

    sport tough comeback inside threaten

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
