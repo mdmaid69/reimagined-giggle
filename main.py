2f6e23d61be0c5f9a74b42f050f286a82d302d2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:20:00 +0000

    become all sleep spot hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
