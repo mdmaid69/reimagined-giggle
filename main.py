28ca5075dffedb9e3aff0d8949ad70e8e3bd32bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:15:35 +0000

    they pin sink preparation when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
