d369b592e82d470671ac8e0036eb859515d7a3e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:54:30 +0000

    this less movement whether Hh

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
