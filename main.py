a06f4d0669727a8ddb98f721b5261e8611e03a59
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:59:22 +0000

    press schedule complete miss achievement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
