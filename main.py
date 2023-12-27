01bfd93dfa5ee484dc626deb65f123d699932d03
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:47:03 +0000

    flavour butter acceptable little statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
