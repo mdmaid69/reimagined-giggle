a5146388419d0c2c6c70cbd6e0fd686b0f88e8be
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:58:20 +0000

    sport speak children empty worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
