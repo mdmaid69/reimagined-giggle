1072e01dcb255cd802c796b75ae29f50e4ecc198
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:42:51 +0000

    clear for quit failure practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  def count_elements(lst):
+        return len(lst)
