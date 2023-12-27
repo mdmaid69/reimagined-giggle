3d418cc96a7614c1524b5c1b2600bd3806a388b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:17:18 +0000

    border disappear majority cry public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
