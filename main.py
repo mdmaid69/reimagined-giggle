cbb6349a46583075c29879c1e146bffa52097f01
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:18:59 +0000

    wear exchange complete give callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import random
+print(random.randint(0, 100))
