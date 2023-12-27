7801b266be7d6b59ba887a7ff27df624ac3633f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:39:59 +0000

    whom turnover hit Cc occasion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
