50e4ff50a528a68355222572d35e22a760db846f
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:02:52 +0000

    distant generous exact street succeed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
