5cb957420e1df115fa8683951cbb33f0db607f54
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:05:33 +0000

    move copy march criticism different

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_hex(n):
+        return hex(n)
