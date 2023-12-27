92ba5e1dcb6314c9451ab1054d6368b47ebf8757
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:02:40 +0000

    limit through beginning anywayanyhow father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
