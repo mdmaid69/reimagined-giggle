140bb0159067d5ae476238e3e91fd65537bfb941
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:17:35 +0000

    stiff within sticky nose crowd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
