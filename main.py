dfd3541b138d56ca46fcdbe6b5da0ad2a40fab8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:43:35 +0000

    street talk Vv gap there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_user_string():
+        return collections.UserString()
