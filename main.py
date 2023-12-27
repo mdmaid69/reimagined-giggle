7c6c71afec207e0728c4c3c38602434583d5b805
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:37:13 +0000

    direction whoever sort as but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
