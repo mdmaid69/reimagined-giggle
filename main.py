635cdfe852a0d79b1e84a6e94a2d25870097247f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:18:14 +0000

    pencil shoulder head whole punish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
