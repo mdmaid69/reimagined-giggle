b5900fb2a067398b9e9923f32fca351ec0a008e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:27:52 +0000

    welcome surface together low invite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
