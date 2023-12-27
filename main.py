622440f5e2f8a97f141e1dcab3aef955fa29c4e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:11:58 +0000

    pinch chance forget either trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
