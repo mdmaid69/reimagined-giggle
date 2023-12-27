b52978dcfe7eb33f7fc512e4806c3ea917264aa3
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:29:34 +0000

    left tip crowded scare date

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def append_to_array(array, item):
+        array.append(item)
