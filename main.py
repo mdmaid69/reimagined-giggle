347467f58ca42d3916ed5b13a82a709dd9ee2e88
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:00:47 +0000

    swing promise survive chief journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
