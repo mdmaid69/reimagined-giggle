56002b9459fdf5f5e0ef918543c6071ac67b5486
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:28:48 +0000

    could control impressed of meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_max(lst):
+        return max(lst)
+  import sys
+  def get_python_version():
+        return sys.version
