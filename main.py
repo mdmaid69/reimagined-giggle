621f0b5a44db45d38875e6948a11bc03b69614c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:46:11 +0000

    point change slightly do shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
