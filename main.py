1a7de6e14cd49658651b795fff06c46215cd4354
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:05:11 +0000

    indeed content read uncomfortable underneath

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
