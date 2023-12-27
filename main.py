9dc88895b2b1bf4ef64a9cf3b4a79672b84c7fd2
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:40:19 +0000

    system Ii explanation sign pencil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
