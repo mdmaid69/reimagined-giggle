3ee8aa4ba658b8c8093cf426bd5fce88ceec1895
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:29:17 +0000

    runaway walk the defeat stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
