7ddf8c47ab0623f1b8a55c8382fd1fb0f5170a84
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:09:40 +0000

    spin basic shut extra advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import collections
+def create_stack():
+        return collections.deque()
