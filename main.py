4b889ffaf8d2111b10fb6f124ec961d4d128903a
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:16:40 +0000

    town too difference on all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_as_repr(array):
+        return repr(array)
