0b691ad206aaad59e0bd7f8d56f0ab291cf8a19e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:06:39 +0000

    search protection exist different answer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_typecode(array):
+        return array.typecode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
