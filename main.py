c79ad1733499ad5d5b9d76435b0c6faf84b7c308
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:56:55 +0000

    march grand drawing tend violence

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
