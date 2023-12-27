dc80de78729ecbb0a08b0259f9499c57edae2a14
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:20:42 +0000

    belief tip book bend niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
