f4461cd37d0ebb41f393b415ab6e31365f4436d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:07:31 +0000

    over skin reliable gate whom

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def find_min(lst):
+        return min(lst)
