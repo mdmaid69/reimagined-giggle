698cc4a2b94aea62323b95a591c1508d31e9df4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:35:42 +0000

    go environment rid nor sing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
