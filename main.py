75becfa6887ca422cc27719f0f270e71102e92ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:01:51 +0000

    many level tune moral explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
