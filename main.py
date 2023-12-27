79474da9c4a2d4c37a44df110d5d9a0012ff7ee8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:31:42 +0000

    water strike patience everything Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
