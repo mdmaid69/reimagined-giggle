6864ca898422a698097f36ba76d730be151d38c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:56:56 +0000

    arrive thumb sister praise 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
