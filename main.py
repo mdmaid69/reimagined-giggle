2a81901aae95f635d8f46d1a2b464738ede94d81
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:05:12 +0000

    pollution close communication bit along

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
