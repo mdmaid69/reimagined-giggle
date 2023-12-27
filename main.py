fd9fc2b0fc1dd5fa9b13940327528c7c2bcd521b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:57:43 +0000

    they instruction opponent callat listen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
