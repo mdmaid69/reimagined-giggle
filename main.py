46e6e11011b54aa677824343d189755bec357f42
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:27:56 +0000

    perform wing announce suit thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def sort_list(lst):
-        return sorted(lst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
