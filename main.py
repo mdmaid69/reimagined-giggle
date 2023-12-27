03f7815efae93584613f1fb3b0be762c982b88c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:20:08 +0000

    learn run low criticism embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
