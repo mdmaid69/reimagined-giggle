5720727f4526e9a2b3b3ec878daf26a54075cd97
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:23:08 +0000

    finally store factory talkoutof late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
