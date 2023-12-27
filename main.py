6d9ef33d8f1cf6df82be6fceb00bbde2eea7e8b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:45:20 +0000

    violent sensitive meal tear demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
