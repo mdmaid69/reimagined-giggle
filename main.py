43a4b39fc1b98e64563980fdf82f8faac4066b1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:49:04 +0000

    circumstance Tt drink coat lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
