f24ff5dfded1dbef03d419f23e08ff57ae04d8a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:40:41 +0000

    reaction average dick whom anxious

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def subtract_numbers(x, y):
+        return x - y
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
