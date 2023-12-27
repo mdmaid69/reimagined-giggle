c7fd37c710c8ce7391122a29fffc8133bd1aa005
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:45:42 +0000

    through murder thin turnout free

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_odd(n):
+        return n % 2 != 0
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
