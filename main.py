065c0be9a7ec2cd39793edd7179abda46816b627
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:13:51 +0000

    physical bringup television aim total

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
