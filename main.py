51bb193b71fbb9d19257067cf8f7ad33b43a6365
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:24:05 +0000

    settle emphasize wind aim surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
