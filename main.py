377430f73031dd124f1961975a572c36c27fee4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:24:37 +0000

    holdon question make fierce cook

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_density(mass, volume):
+        return mass / volume
