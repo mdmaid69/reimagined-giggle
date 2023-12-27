3559323905cc37f36c9514a389b27a564f74f9cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:07:21 +0000

    below solve start warm have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
