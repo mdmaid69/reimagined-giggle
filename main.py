935d0695f816636d1c45ffcdddc572ae038b2215
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:57:19 +0000

    Cc progress equal shadow reaction

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
