9d90106b1feb4185a86696f8caac43e39df71d0e
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:41:02 +0000

    blow thinkover before finger science

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
