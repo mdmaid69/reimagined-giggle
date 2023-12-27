5680268cac93aa060087d7a539f423ec91dd5915
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:43:43 +0000

    beautiful considering respect comeover perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def convert_array_to_string(array):
+        return array.tostring()
