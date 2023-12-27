0d7afe75ebc73750a8cadf4956f7f977eadea979
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:41:38 +0000

    thought of importance guess many

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def multiply_numbers(x, y):
-        return x * y
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def count_words(sentence):
+        return len(sentence.split())
