745512cdc34dc2e1a1dd3811ee9b048a49d9296b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:17:13 +0000

    protect main juice wrap hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
