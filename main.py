b0296a8473e79888b681538b76fac219d8b0155e
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:23:14 +0000

    backwards hang own risk suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
