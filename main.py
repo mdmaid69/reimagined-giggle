927fbb98218ea0af7dab7a738a33ac143505b49f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:14:31 +0000

    dull confuse sleep worth willing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
+import math
+print(math.pi)
