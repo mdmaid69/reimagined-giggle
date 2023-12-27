96e05306fcdb003cab57d1aea174260e5ecd3ac5
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:56:58 +0000

    weekend later crazy still lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
