4e15fa75949f0613ca259a446a888b7a37e4ba7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:48:13 +0000

    meal matter decision drive busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
