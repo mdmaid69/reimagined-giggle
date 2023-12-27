4917f1a22532909f0254bcf3de4d6e4fbeec4859
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:56:29 +0000

    open development dish oil office

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
