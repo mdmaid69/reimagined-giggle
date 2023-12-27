1bbb455d2da92179f39f3e5569ff2c8a7d283616
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:49:23 +0000

    powerful prepare takeover sense succeed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
