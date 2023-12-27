9e275d82fbfa1ad25697ff5422c0adb110db6401
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:40:49 +0000

    our callat chase list main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
