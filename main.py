d51353c9799b7913b3318532988ce09d474599b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:22:29 +0000

    deliver turnaway total physical keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
