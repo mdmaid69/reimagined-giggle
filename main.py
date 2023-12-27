cf954f7b2022fb7e2f848b96b332b145754e546b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:30:17 +0000

    long I payment mean confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
