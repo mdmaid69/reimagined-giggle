20e722b5e092976b51901e8f7f0519b3f209e046
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:34:48 +0000

    relative prepare settle minority old

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def square_number(x):
+        return x**2
