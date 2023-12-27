71c357f948479b919bdb416059318c5cc5164934
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:05:45 +0000

    interfere refuse over relationship perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
