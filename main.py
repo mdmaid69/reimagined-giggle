608def4ca15d2ae7b0598be5cce3eb6211881466
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:13:35 +0000

    radio main emphasis but tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
