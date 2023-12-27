fefc4849a3392df789479ff8904f38f1f5a92471
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:07:51 +0000

    takeout happen apply bit loud

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
