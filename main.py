6a094f6428e4b888a55b396390e30f249e6f0111
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:28:34 +0000

    thinkover then push front force

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+print("Hello, world!")
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
