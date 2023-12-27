0ac7a945336fbaf24a8e3ba7a145b8e88f9b8e37
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:36:40 +0000

    this thorough remove odd model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
