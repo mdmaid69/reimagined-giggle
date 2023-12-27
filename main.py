ef3b84a794c6eb43321f44ee32dad8a69baa8227
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:52:00 +0000

    thinkof tap continue fight prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import sys
+print(sys.version)
