6dc17548135ecdce1263fa1b7b8e15def5f1d69a
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:26:29 +0000

    history black also replace lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
