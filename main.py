6a69525cd50ae0cf5d03e9447c9e7917aff12929
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:52:31 +0000

    compare away dig arrival accordingto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import os
+print(os.getcwd())
