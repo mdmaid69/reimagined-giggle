dbddc9788a15f3ad7787b9981a7495d515f74628
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:55:28 +0000

    tone shop gather love respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_length(array):
+        return len(array)
