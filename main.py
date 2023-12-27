ad228eb24b82c496040c9840a060aefcd5662f88
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:54:31 +0000

    serve over bin exhibition wheel

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def add_numbers(x, y):
-        return x + y
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
