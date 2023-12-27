a54c28510e96100d65476915f0ae006eb73596d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:30:34 +0000

    century off develop judge wise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
