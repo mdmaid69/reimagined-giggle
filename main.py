1a3ce6d94d1c006581beab39b1ce568fbb7307b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:46:50 +0000

    571enough case best ride congratulate

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
