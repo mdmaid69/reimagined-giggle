5d09d91a9b3598ae61cc1b3b972a11b0d8cd1138
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:36:41 +0000

    valuable cheek takeback over respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
