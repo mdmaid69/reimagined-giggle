06619f89240fc8005e58b61bbadf303f8d90d84d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:40:32 +0000

    development round group luck length

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
