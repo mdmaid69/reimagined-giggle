8db2c4cd885012531f56fc4ae6e03d8ae7f83eb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:59:49 +0000

    operate steady kind programme message

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def find_min(lst):
-        return min(lst)
+print("Powers of 2:", [2**x for x in range(n)])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
