0cbe74e3c0b3b6609038752501072553d8de2973
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:12:18 +0000

    business owe cope visit although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
