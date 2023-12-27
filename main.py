dbd5dee805454bec72e6ce8e316227b2f4df6f97
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:10:13 +0000

    print equipment idea increase first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_work(force, distance):
+        return force * distance
