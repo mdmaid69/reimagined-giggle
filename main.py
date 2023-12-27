27fedbbfda76e9ef755dc1ed3e033217acc3d8f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:13:48 +0000

    increase push excuse smell harm

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
