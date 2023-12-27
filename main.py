287a456b0e01d858e3e20f9d212947d6b1c67e36
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:37:13 +0000

    emotion miss closely basically while

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def sort_numbers(numbers):
+        return sorted(numbers)
