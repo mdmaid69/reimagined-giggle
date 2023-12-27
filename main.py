bc302dc1d1a7b429ca6c9e74aa162770dd3b8226
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:18:04 +0000

    record race break thinkover compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def square_number(x):
+        return x**2
