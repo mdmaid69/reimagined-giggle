2064fd6202f8e61b1389ca07629782894fad65c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:04:37 +0000

    since consider store colour takeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
