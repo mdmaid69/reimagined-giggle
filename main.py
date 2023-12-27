7b517a01185c8badce115e763be911cf2570e616
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:17:58 +0000

    generally variety trick true settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(a, b):
+        return a + b
