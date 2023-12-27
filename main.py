5cbb7455501d69145a673bb049fb747d34669340
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:44:27 +0000

    challenge think aware lunch uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
