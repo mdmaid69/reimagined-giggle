3dfee1a6a10cf2137966af344291ece7ff5a1e38
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:35:41 +0000

    flight intelligent careless cheek hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
