5c53518b2a428d1267ed2d228ed8018cd129be04
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:07:02 +0000

    goback(to) underneath Gg care today

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_length(array):
+        return len(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
