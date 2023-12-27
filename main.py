f73281a037e7e50966cc9f0f6b02d066d4ea87a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:46:20 +0000

    unpleasant goback(to) shut admire reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
