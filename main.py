61a48229c40ab7a3c4b2e31089bfa8589869b53f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:00:15 +0000

    old success kill miss through

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
