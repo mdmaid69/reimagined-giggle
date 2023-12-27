1faf44b37f392cfd8e13bd043a3199e7d1b8ff13
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:43:13 +0000

    butter bit especially present term

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
