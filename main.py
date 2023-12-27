1fcc154670120d9471632b557620c4ff461e9d16
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:07:57 +0000

    operation usual rank handle university

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print([x**2 for x in range(10)])
