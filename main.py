4e7873f8f9e3c3c180374ee6a5ef20b0cb3d87f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:05:58 +0000

    punish comefrom phone place quickly

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import collections
+def create_priority_queue():
+        return collections.deque()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
