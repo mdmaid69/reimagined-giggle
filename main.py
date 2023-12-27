a7e2c94b66ccdda6c84901a689a227ef259d4b64
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:20:09 +0000

    hide capital owe young contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
