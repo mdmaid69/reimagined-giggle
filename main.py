688c436e0bcba975c6137ec59f18395b0fc1a09e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:46:13 +0000

    use embarrassed estimate ahead myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import tensorflow as tf
+print(tf.__version__)
