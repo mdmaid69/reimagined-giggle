d74fe8183352b23e0c2171a4e561a471bf7bd828
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:28:38 +0000

    floor claim failure laugh unite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+print([x**2 for x in range(10)])
