14e90a2f437475a3f11287e912f31b39b00a0389
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:21:36 +0000

    goon within tomorrow be mess

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
