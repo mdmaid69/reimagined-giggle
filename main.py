a48b8a6cac09e59a9ccdcd9c59234ae8a569ed9e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:43:33 +0000

    Ee load for far decide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
