a40e7138374f556bf8f456e48a3c8bfbea682671
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:14:16 +0000

    immediately afford expression dress mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
