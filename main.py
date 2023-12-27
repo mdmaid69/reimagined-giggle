95a5b424e4fe972d01857c5a2f1f0ebdfaa66da6
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:19:37 +0000

    pressure side keep back anywayanyhow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def convert_to_octal(n):
-        return oct(n)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def sort_list(lst):
+        return sorted(lst)
