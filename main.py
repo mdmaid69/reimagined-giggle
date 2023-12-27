6a3027f50b6203499791abf89f8ee2b8614718f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:53:14 +0000

    consult stupid frame as dance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+print(math.pi)
+  def multiply_numbers(x, y):
+        return x * y
