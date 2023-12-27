6d291bb0890a1f90fc8f5e9ce6bfd983351162c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:36:55 +0000

    thankyou burn reliable cause shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
