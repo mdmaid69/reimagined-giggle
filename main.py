31b40a0e00de63f9c649f6718dcf234bc500049e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:54:18 +0000

    print operation then boat worse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import getpass
+def get_username():
+        return getpass.getuser()
+def is_odd(n):
+        return n % 2 != 0
