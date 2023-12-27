74227a0a79edaa39ac13aa0e86ed30468f07fe1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:16:03 +0000

    worried principle speak pipe joke

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def convert_to_hex(n):
-        return hex(n)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
