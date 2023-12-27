a348ec5529671f8a0b4361ad257196be251ef7f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:21:31 +0000

    usually sink odd force shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_density(mass, volume):
+        return mass / volume
