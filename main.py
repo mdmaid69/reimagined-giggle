18aa5e1150f1657405ef457a25a635d126fd58e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:40:10 +0000

    spite depend right reply everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
