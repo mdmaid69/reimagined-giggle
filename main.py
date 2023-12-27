685499bfef97d42a15bed1d8691ec8720b2d3707
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:24:51 +0000

    pay quiet lunch goup continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
