e2fa761b60be2b9745dce1d937441efcf685d916
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:36:13 +0000

    rise reasonable protest hollow completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+print(os.getcwd())
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
