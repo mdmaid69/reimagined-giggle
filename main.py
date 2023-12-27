ac96eaf98101fd24e0cdf5abd76541bd36f2e108
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:51:47 +0000

    remain little criticism especially nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
