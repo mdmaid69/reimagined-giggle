606347bed074cab5a981aaa8d4253023004f5afe
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:45:05 +0000

    565end brush reply like thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
