9afe36bb7be1110a09b89a024cf4c9af336fae03
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:17:04 +0000

    mean to file before report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
