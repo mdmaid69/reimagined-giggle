0d2d178e4fb21e081d838c7cd9be99eb933fdc65
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:33:07 +0000

    sting afterwards over can impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
