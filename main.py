bd414610791c3927ea847ec0162843a930088e71
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:45:29 +0000

    walk rescue store silent have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
