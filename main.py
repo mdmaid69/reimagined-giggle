0f540e1e6b6aa2b70e64270acec4182d0b12ef8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:56:54 +0000

    record tune match action harm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
