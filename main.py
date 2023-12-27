43a7c3aff237ce9cfab2fb9f365060a2de99415f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:34:05 +0000

    command above callat hair exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
