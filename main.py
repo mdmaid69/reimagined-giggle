37ec7616e3cc28a0d9b8c8727c77c9e0e66cb165
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:14:20 +0000

    majority pin married connected innocent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
