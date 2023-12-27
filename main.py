5d5e609cd0801dee6f61c219634ba9b903a38d65
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:49:45 +0000

    flat week breathe him stop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
