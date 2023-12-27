bb64c211a8cb492930a56f4623b308879b36655d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:43:25 +0000

    already hand badly tidy manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_pressure(force, area):
+        return force / area
