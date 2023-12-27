c3d63e68a5719f3e1dc0e1e9638ce95b68fb916f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:56:44 +0000

    hair underneath relationship either little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
