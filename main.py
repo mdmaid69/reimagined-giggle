c73ab19a762fc8e624348bb4556c440ae41ef07d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:56:16 +0000

    voice Zz trip slide offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
