74ab14f39950ab92614ce798d756193d95d1c02c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:19:08 +0000

    question year path speech sentence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_area(radius):
+        return 3.14 * radius * radius
