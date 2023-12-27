39cf3b3cc8e5aa6e33179dc8cc44e765eab893de
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:55:10 +0000

    loss shade heavy clear confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_volume(length, width, height):
+        return length * width * height
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
