9be286167fce8eb6fabcc372195ea6d1d91b176c
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:16:15 +0000

    tense disappear lower collect grade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_work(force, distance):
-        return force * distance
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_sine(x):
+        return math.sin(x)
