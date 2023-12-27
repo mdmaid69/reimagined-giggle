e9ec93be102c1baf9d9414f3289cae94dc24add3
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:53:05 +0000

    side law legal wind influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
