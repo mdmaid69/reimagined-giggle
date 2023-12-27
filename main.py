e990e4ca3e08b2927bc086b127e5bafc1e999d37
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:30:47 +0000

    stir star trip generally town

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def square_number(x):
+        return x**2
