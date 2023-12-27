ed8cdbd20dc2f5f906d72fbfaf17b52b739ed551
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:38:33 +0000

    border rush from father until

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def add_numbers(x, y):
-        return x + y
+import time
+def get_current_time():
+        return time.ctime()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
