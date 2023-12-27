67dc8eae1b8bf961c9588be66a01810541a98b24
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:31:08 +0000

    game I purpose national past

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
