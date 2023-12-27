ad1f9e6ddcc13d2aed02f54dc83ad1bcd45b5393
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:08:35 +0000

    wheel expensive police definite famous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
