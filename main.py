ba61ae9305afa9a3654de34770f9b1722fb27f11
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:44:23 +0000

    beyond meal none bright part

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
