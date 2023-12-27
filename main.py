72e15ff05f2562c760b0f0001e980f9f5410a219
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:09:57 +0000

    Ee dead once talkinto near

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
