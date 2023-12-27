88d1c0d3058562adbce78cf323590f9510871fd6
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:54:30 +0000

    send careless refer condition alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
