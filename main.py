ad1d6778cf0d55c7c2c1ed70a7e2a0d2fc8c5524
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:03:48 +0000

    dress warning contents shall normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
