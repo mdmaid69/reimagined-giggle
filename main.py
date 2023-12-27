1ed81c48bb583fa94afd9cd641cc58c9756ccf2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:17:54 +0000

    but receive sometimes accidentally sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
