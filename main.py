7cf765ec0379c5d70653edc78154545c6093faaf
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:54:03 +0000

    such cook pause home sister

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
