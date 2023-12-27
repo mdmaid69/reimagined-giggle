50c5c597fdde8e76387051b716f20c5b2a35684d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:28:43 +0000

    description I workupto corner regional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
