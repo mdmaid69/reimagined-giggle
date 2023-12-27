40c6b1f2d59f712e5b58ea954237a6461634ec01
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:36:53 +0000

    week aim performance maintain market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_odd(n):
+        return n % 2 != 0
