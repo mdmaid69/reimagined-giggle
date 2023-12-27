6fea689f6e8fbc257035a4d996bf6b42f5cb68b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:57:52 +0000

    remember force culture coat tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
