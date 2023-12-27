fb09eba0411a40930c572bf8a77dd72d5af80b36
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:44:57 +0000

    while 572enter uncomfortable store room

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
