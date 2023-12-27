fe8b1f31156a74a6c6fe47edddba6ed4fafe052c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:42:29 +0000

    frightened attitude perfect strange left

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_work(force, distance):
+        return force * distance
