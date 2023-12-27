7818ccff48fac38864bc682a4c429a3f190fad41
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:39:17 +0000

    duty among set respect fill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-print(sys.version)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
