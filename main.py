538d3e050541671001ed22cd6d2647d0fa4ea4b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:02:08 +0000

    issue warning minor go bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def count_elements(lst):
+        return len(lst)
