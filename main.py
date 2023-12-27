69a630a9b4f52d996e5433b5218f8bc96f2380da
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:23:03 +0000

    fresh off rate scheme influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
