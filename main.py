c8ba5c95399da7ec116533c648c3db4f7bf452e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:04:42 +0000

    sport compete detail Hh variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
