4ed11ad10c74070684ce7307a1cbe9b0f2b9c065
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:38:22 +0000

    fall gentle extra hard public

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def split_path(path):
+        return os.path.split(path)
