4c7862778213c336d16aee554278889b86622a07
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:20:57 +0000

    long telephone cap pattern defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_even(n):
+        return n % 2 == 0
