c97e4d968573dbf11621250bbb5f155bf9cbf17b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:32:38 +0000

    chance pinch home once part

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
