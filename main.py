5221bf496b0f61f9bb35cc50898ca5976f487fb1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:35:59 +0000

    dress together work uncertain own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def square_number(x):
+        return x**2
