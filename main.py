13c84a6bb269a34e79da2045a118a0611d3d9350
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:20:48 +0000

    comedown centre serve cruelty trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
