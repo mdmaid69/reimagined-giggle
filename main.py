94d0c67a768c4cd3fdb7f0dacf25bf9672b9208b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:42:34 +0000

    cry expression mystery afterwards definite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_bytes_from_array(array):
+        return array.tobytes()
