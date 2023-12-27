83a772cde2085727b8a6bf0e65cdb037b135aa85
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:02:42 +0000

    giveaway discussion temperature cause extremely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
