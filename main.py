eb23f7786e4dd70d0774a16e37e3127d11e814b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:36:55 +0000

    sing conversation refer lock kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
