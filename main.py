071cbef10e410d543f730489c7ceda77af6617a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:29:36 +0000

    present film bend drop against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
