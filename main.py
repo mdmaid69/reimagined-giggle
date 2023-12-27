7dc7c8ef875312c38faffcdd6fb4be2ed394dec6
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:10:04 +0000

    address time know drawing soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
