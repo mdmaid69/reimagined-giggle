4038cab7d1b46d67814d2cb246e18ea5c0dfc9fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:22:37 +0000

    sing love little should private

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
