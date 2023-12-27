dd5eb1655b7f8f31ce3de054528f295abb8cae7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:24:53 +0000

    band decision afternoon ill tax

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
