8b9ee1553a62ebcfd142f93fe8c42721cd33028c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:39:22 +0000

    yours translate tense better paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
