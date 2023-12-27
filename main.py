10bd12af47c1b4833bc75aedff34b6747ae2ccf6
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:31:44 +0000

    push burn meanwhile tread secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
