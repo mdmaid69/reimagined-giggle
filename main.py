9fd6bb17b2e033387b61d98035f82d9349ed5113
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:05:15 +0000

    giveup classroom free embarrassed survive

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def square_number(x):
+        return x**2
