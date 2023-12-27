f86f654d3c5acda41c606f77356d4f6b900636fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:11:43 +0000

    giveback point thick contrast ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_list_to_array(list, typecode):
+def get_array_from_list(list, typecode):
         return array.array(typecode, list)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_hex(n):
+        return hex(n)
