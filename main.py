eadf522bab874b5c35f3367fcdcba1f24d15f4ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:29:11 +0000

    gas takeback claim sign getback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
