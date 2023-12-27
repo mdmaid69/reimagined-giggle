cf84f34f0baa44358435ad99fd10ceba8a76084a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:05:54 +0000

    funny have result extraordinary home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
