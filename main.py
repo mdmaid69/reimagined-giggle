3f0150ea4ae9c480d78542fd9e7f987226ff66a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:16:35 +0000

    emphasize lunch object both pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def find_max(lst):
+        return max(lst)
