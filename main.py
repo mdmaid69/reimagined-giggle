50b6ce632e639b098b0b946f84493e43671fcebc
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:54:36 +0000

    firm spring join bit trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
