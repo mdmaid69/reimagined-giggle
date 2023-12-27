38309466afec03ba916f0bd5e30255a2e125b67a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:42 +0000

    too sit follow motion shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
