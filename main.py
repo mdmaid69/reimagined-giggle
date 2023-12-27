33975edccac6159950980815be47e15da8f98a23
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:08:23 +0000

    Hh crime thinkthrough short write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
