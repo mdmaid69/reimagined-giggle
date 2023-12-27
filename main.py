2d446d82ec6b5765cb87faf35e9cc5e6ff32e72f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:48:42 +0000

    geton person much hole habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
