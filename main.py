c1f19ccacdfcc561ec33be7587c8402ac8149669
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:21:07 +0000

    mother trap ourselves its fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
