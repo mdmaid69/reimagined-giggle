6c2b17e7397ab641e93d0efeaf8716cb693c9568
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:33:42 +0000

    slowly operation 572enter sink group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
