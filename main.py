c14a0d9850321d43a2f222f8653e791387f89faa
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:47:18 +0000

    dress dream thought relative sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
