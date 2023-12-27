2fe0e55427bcf4143ad951ad38a35980f373cd56
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:06:33 +0000

    pretty long tongue worse keen

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
