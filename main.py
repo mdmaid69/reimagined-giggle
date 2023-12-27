aa1dec2103ac9b4f8173f05f722760b57f5ecd78
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:32:42 +0000

    pump estimate back quantity soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_speed(distance, time):
+        return distance / time
