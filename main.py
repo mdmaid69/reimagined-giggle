fc40695186bae2b99028cd47120e91b229da8117
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:49:26 +0000

    win operation depend guess politics

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
