95c7614e1df9d0a77454bf9d770bfd36a787a595
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:29:28 +0000

    frame train score curious evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
