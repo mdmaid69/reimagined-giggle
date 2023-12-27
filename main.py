da8b819ec35546b4ffb1ab02d07eda467916a47a
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:22:24 +0000

    admit family keepoff you book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
