628b0abb734db7b9a622824697ec6b4405a1ba9d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:41:30 +0000

    face wave confused them accidental

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def find_min(lst):
+        return min(lst)
+def find_max(numbers):
+        return max(numbers)
