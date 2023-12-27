8838cc9bcbd1d7afb6943c90bdf2cd7086d26c7f
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:34:52 +0000

    each decide might sell upset

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
