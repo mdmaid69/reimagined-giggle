a9b0f43cfb5484698240338100586981fa39d237
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:26:24 +0000

    last comeup eager dish tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
