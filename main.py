23181ae925d83feda293b9832ec5ead4c24856b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:27:01 +0000

    within chase myself sign comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
