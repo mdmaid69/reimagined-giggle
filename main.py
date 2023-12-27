8452015156a2a8dc648c7064566e5a8d0704629e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:28:07 +0000

    ask spray fast straight slow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
