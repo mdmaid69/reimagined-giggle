188d31094f83cdffa66ab4bfa91aecdca5bcb6a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:39:06 +0000

    instruction innocent middle approval wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import sys
+def exit_program():
+        sys.exit()
