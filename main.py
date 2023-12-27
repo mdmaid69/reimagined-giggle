f1a8a38a1dd93f8fe005485f7c1b3918f849fce8
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:45:22 +0000

    eat over crash miss live

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_unique_words(sentence):
+        return set(sentence.split())
