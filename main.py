9b9a297ad9994e5d632333e080b2d90617defdc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:47:21 +0000

    frighten sound main bag window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
