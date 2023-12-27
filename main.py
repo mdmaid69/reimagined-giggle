77e05097c33625f64f465e1cae3ea95a1467f72a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:33:02 +0000

    information question makeinto forgive nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
