d9a5fb6e083436898e2a795c40a0d8d2f9c118a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:34:27 +0000

    steady play sister only splash

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+for i in range(10): print(i)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
