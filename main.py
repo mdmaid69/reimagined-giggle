013e47b98ee9b0e327e8149f7387749899ba6099
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:23:37 +0000

    abroad design afterwards like unfortunate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
