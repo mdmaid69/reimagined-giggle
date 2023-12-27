2f8ab4099cedffe2edae74723f9f039f0d92f61d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:55:13 +0000

    until pollution wheel he ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
