cb5f371ffe0d0f14a0c4b148f7b7953aba645289
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:46:58 +0000

    successful aim forward please regularly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+for i in range(5):
+        print(i)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
