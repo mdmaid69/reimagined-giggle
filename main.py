221d172382f353ac4bb4612b517b0ac96464fd66
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:47:27 +0000

    touch height situation hot almost

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
