8cc90ff901acf861e267f2ed993f329696b9215f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:12:06 +0000

    tell disturb upwards cheque frighten

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
