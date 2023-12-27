8277bfbbb6ce15e88adc09e49b73d2924f375e52
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:08:06 +0000

    schedule immediate view give discovery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
