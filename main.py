4b8615ed7acdf1d9966f423c8b604e657d2899a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:57:26 +0000

    mark separate basis teach project

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
