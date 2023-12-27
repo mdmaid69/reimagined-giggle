1a6b2fbf549858fa83c2c66bb11cf3c9aacca09f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:03:23 +0000

    hurt connected perfect bill same

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
