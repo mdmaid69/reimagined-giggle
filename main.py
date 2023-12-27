f957dd53de1e40255fef05eb3a7af8ee48d12eae
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:42:19 +0000

    keen press broken divide accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
