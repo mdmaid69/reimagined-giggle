4bf2ff6ef18f36ed39e142b181b3e33b88953ce5
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:58:01 +0000

    truth statement impatient thinkover same

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
