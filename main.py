bc8b1fa5fb39c21dc1aa40515ed92ab1a7e504af
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:56:39 +0000

    suppose sensible late create side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
