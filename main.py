8b18f84a27974a077131be1c7c8ad9d5d9f0ef8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:19:50 +0000

    noone wind cruelty sick wind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def is_odd(n):
-        return n % 2 != 0
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
