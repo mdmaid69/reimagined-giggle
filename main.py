deb34af01a4b2fa09b30d65c19684c1b8fa65e7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:46:42 +0000

    life loss turnon responsible deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def sort_list(lst):
+        return sorted(lst)
