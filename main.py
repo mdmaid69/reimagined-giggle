1f46990505effcc49f6892409ef16fe935feb877
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:41:55 +0000

    key share study set snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_exponential(x):
+        return math.exp(x)
