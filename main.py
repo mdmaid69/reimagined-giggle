6f578590a98fd9553fbaa6546533933de8339ee0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:21:47 +0000

    either exhibition job sweet progress

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def get_array_as_float(array):
-        return float(array[0])
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
