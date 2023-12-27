b5960b2666d57825475c053d30b62c7565f93a53
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:31:19 +0000

    desperate line lost sink on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
