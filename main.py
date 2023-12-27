cd801e235bf41807972c863d157f252b7588f200
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:26:58 +0000

    university sign finish love after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-print([x**2 for x in range(10)])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
