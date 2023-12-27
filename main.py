ea9bb6eb09ffe0d5e99997311a2953a2ac7ce988
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:56:02 +0000

    Gg above upset abroad leg

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
