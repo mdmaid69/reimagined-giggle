96503c285b2423991fdf2132efecd93adde459e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:15:33 +0000

    hear ground please leave look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
