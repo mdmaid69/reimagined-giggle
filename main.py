5bb7aac347568efe1af35a995cbe26ddedd3898e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:09:52 +0000

    swallow image disagree on pool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
