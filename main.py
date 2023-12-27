cf5cf53e76aa74c97a18595a22896140c21d2ae5
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:17:46 +0000

    heart accordingto destroy phone eager

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def convert_to_binary(n):
-        return bin(n)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
