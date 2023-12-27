20be5d1c10bc2bcad6925c49bddc9e57341b9a38
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:37:08 +0000

    string himself blame range push

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
