6013443daef4d4bfc8c42c3ace7974bb30bbb487
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:45:01 +0000

    space crazy bunch shopping believe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
