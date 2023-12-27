a28c914bc4e1d8b53ad74e035cb3b8326ffe46ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:30:09 +0000

    your needle book bored careless

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def square_number(x):
+        return x**2
