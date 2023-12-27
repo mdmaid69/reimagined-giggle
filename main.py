d1413188e44d440e43f1980013d4c32fa9ee8edf
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:32:05 +0000

    world print another circle morning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
