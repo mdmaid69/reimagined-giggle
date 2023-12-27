cab2634f54e93bd1f6f9d45eb7ff8395bccf8c93
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:07:31 +0000

    such stamp except talk equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
