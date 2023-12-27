d74bec46f18e1a22d3cd7036404fdf32a78d37bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:13:22 +0000

    mysterious process succeed somehow repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
