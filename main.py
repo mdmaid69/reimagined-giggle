6d10fa1014f62fa255e993fecf80fad4cf14905c
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:42:29 +0000

    geton Ww common blind regularly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-print(math.pi)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def count_elements(lst):
+        return len(lst)
