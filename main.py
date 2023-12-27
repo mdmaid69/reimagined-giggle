edb131706c2ef1ea639cb079045d6064c9badbde
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:42:29 +0000

    pronunciation type interesting wonder bill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
