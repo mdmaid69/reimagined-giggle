4843b10feaa07550259ecc1ea63f0695d8e9e6fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:33:38 +0000

    petrol manner vision offer appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
