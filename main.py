5dba1eb9ff218284529cd44fbee4d2c7d9ef5606
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:11:23 +0000

    film put base sound behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
