f4db0ff28dfc35c4cd47d663dd126ae934c89f21
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:32:06 +0000

    waste imagination similar turn rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_list(lst):
+        return lst[::-1]
