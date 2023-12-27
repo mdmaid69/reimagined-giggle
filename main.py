170ae5a14c1a5a43e61cb0e0ad0444eb378bc749
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:36:09 +0000

    interfere demand spring good emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
