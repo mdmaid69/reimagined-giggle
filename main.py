119429a9113f1e8dc0c1a91edbeb6c999c466a7f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:39:43 +0000

    convenient scene stamp judge makeup

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
