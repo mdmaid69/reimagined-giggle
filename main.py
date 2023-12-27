9e35691516bf971de750133d8b9edeff495a156b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:04:44 +0000

    scare hope welcome instead warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
