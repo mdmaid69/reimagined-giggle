e35d8ed3df22470177206e9cebd6a8a69dd17187
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:43:15 +0000

    cold rely trip matter pinch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
