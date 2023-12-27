8d2a7c47488ea2f9058f77cd361e9ff1510094b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:13:34 +0000

    pile consist heavy care odd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
