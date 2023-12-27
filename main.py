cb1bf60413b545593349304b1e22c5ac586bd2f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:54:04 +0000

    attempt fashion odd lose stage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
