226ddb13b136a8b86d174dbf0d71df8816f1f168
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:55:28 +0000

    point respect label pin brown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
