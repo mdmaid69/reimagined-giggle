0fb095e92725d5bbd0676a0d151220185ec59fef
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:35:05 +0000

    force suddenly definite reason comedown

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+name = "Python"
+print("Hello,", name)
+import array
+def append_to_array(array, item):
+        array.append(item)
