67747dd79bede8e28f87868d5c6b6f9a7e8216a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:25:54 +0000

    fall treat uncomfortable thank image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
