e34b2fefd2e93536c2123730439ffc5417da170b
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:39:00 +0000

    waste name patience comeround birthday

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
