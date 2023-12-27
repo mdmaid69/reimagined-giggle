6909afbe94a5994a3a52cdbdfb8d015e3c1a5e1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:56:23 +0000

    from throughout command meal cheek

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
