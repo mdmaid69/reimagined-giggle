baad633b51262e242ccc0138a17df5265d3ada62
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:16:47 +0000

    neat use taste around ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def find_max(numbers):
+        return max(numbers)
+import array
+def reverse_array(array):
+        array.reverse()
