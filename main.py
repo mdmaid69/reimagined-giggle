acfd16c28077342d2c00422a03232fbd0f852906
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:21:17 +0000

    niggaacademy nearly edge same with

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_volume(length, width, height):
-        return length * width * height
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
