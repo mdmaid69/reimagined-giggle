18095eaa2bf38b1bd73478f4044361c07f91e9a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:01:52 +0000

    door second post usually cross

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def square_number(x):
-        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
