1a8d1e198958fb7d8d1a0e3232de95b47619fbba
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:27:57 +0000

    and anyone hand office ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_arc_cosine(x):
+        return math.acos(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
