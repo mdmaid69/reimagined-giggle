c6172b0eb304f4522b1f62a3effd03ff526b2dd3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:04:40 +0000

    speak obtain once his port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
