e6f00b19c6d1fa3dd8f99a026306fb496cf213eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:43:28 +0000

    someone half remember truth spend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def clear_array(array):
+        array *= 0
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
