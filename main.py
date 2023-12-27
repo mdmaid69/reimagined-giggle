8874d2bd709656d3464881f4de5f594b893547da
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:39:26 +0000

    effort regular minute cruelty more

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
