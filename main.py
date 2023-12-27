f9ada9241bec9533a56cf3e1bf18df7ca98dfc01
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:35:09 +0000

    relation flow complete progress find

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_density(mass, volume):
+        return mass / volume
