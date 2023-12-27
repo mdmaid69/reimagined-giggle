5340f842a7862e8f6caebe6d6204e72b21264109
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:17:38 +0000

    sort basic bright unfortunate least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
