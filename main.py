542666c8ac1faae899349fd269ea661f2951a800
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:55:14 +0000

    performance common belief switch scared

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
