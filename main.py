687d5820d699d5a00f0b7543fd4d5f1b855314a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:09:00 +0000

    by broken will organization birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
