1b5717a8ee6bee75f794be0987596b34b137e70c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:03:41 +0000

    never around contents impressive travel

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
