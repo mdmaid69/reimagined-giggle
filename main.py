5fcb38d1f66c1b9e191f555dc5b4b001b9f58c21
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:54:30 +0000

    count travel number lend sick

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_power(work, time):
-        return work / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
