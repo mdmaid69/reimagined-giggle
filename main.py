f833c6f96c7359a305e5d61e5a7a4f0bd4fd6098
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:52:25 +0000

    persuade ahead there famous convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
