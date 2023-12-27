0d0302958be8abcd4c6e291a1d435b9ed39ed6c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:19:35 +0000

    goodbye straight clean spray scheme

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+print(os.getcwd())
