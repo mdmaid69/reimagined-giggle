759122f1e7d50f72aefc7e280529442dd5954f15
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:03:38 +0000

    book what legal piece left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
