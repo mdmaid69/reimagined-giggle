1ce3e8146c94ce90de6e28157cd4c639d1e868e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:18:09 +0000

    miss room satisfied equal work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
