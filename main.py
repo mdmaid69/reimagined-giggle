1d7563850daf4c09c9c94a4e53a035aaac8dfc12
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:58:47 +0000

    turn comfort pupil over tell

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+print("Hello, world!")
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
