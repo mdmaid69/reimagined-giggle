c7756ed13fb2cefb7429cd0f61096ff7d28b74b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:05:43 +0000

    child ground unusual care safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
