3f1bb097c4e1689a52471053d8c23ccfdca55abd
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:32:44 +0000

    often Ll patient consider any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
