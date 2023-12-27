3f7a757cfbf0d5beccc94c095373c38ee8d06e8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:11:42 +0000

    on violent pick godown level

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
