ac9ec2f6b5dcffa33ed884182d9d7e43b2e86573
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:33:33 +0000

    rather matter passenger food comein

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
