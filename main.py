2f283163c69446b172973edacd6bd4df2300d49a
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:52:00 +0000

    miss account free sorry wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
