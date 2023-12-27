5c8137d946b521212c65f40c60dc76b713a25e40
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:30:47 +0000

    getaway push away absolutely onto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
