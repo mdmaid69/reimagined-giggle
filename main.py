9cc976561c65abd856bef37acba6c820eb5a7492
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:56:17 +0000

    actual field goback(to) keepfrom poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
