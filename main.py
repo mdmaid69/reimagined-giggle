11eb3497736aa0a9dd36729e24bb8c7b67a66c03
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:53:13 +0000

    connected talk relative hear process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def count_characters(sentence):
-        return len(sentence)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
