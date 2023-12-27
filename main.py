8820760ed936fe38f493d3acee85229e1f4e53bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:28:14 +0000

    rare score snow rain threaten

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def square_number(x):
-        return x**2
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
