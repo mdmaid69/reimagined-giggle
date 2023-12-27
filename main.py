0b8dd167a491b2d00319e4beb54f5dbee0374f40
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:45:41 +0000

    abroad hang seem area how

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
