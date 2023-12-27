9b656947a26c759ed72246c89a15c690b0f670bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:19:13 +0000

    shade watch around price friendly

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
