afa6c78a6fd1c744b439f7aba1688dea5e304e3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:29:50 +0000

    year platform shall important coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
