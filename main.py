3eedaa415d165abed082f04509793d9deeb8c7e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:42:55 +0000

    constantly underneath lookafter giveup swimming

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def split_path(path):
+        return os.path.split(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
