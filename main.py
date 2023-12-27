2014a0b2e5229da9aa80ed2801921767efb6958f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:48:30 +0000

    between no down power dig

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
