373babc2a774e2f80a5e259d3f0a8604d34d2136
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:57:48 +0000

    sudden catch exercise belong hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_base_name(path):
+        return os.path.basename(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
