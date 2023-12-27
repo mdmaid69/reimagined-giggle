e36b2fc9b4cb6b08868572d82c2e1473081c8669
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:14:08 +0000

    this foot frighten awake prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
