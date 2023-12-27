234af4a513c4db024787a78fe188ebdec38afc8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:08:20 +0000

    name quantity runaway pull left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
