d0296f293425a0580aca67b3e046224867d6787d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:04:17 +0000

    goup while tooth birthday platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
