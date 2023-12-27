10fa0301f73404033570e1b5f8389b8c14471f3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:21:41 +0000

    characteristic eager cry basis deal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
