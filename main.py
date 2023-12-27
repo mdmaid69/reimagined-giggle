2dd7a8be927d6f48cd4d5f9cb02e68520830d925
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:37:50 +0000

    when quarter callat long shop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
