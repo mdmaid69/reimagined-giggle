00f9993c4ea2f9379406df6cd36e8ccb114a7cb5
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:39:45 +0000

    carry soon collection sing half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
