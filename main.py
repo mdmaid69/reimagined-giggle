477f398a3f4878d5902a0007d067def0f040d22f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:54:14 +0000

    different drink straight afraid partner

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
