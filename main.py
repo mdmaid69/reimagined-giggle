04657253e5dadc970d70b45352a5a80944c2f314
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:16:01 +0000

    tough about evening stupid lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
