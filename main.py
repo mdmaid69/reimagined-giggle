f215e1bcd57c9bb8c1b3f4992efef3f1651cd2df
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:55:58 +0000

    fault vision confident coat legal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
