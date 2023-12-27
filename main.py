cc3e4f6b39822e624f219f24f608bb5b145dc1b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:08:42 +0000

    work getup rob average onto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import collections
+def create_queue():
+        return collections.deque()
