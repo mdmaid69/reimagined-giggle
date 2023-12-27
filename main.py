3026151d6b30ae0601a3a1209280a519dd4dccfb
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:56:31 +0000

    them late telephone upwards build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
