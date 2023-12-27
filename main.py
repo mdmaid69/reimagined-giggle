020ec0f3bbd40e388405c9857242fc6303c8a28d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:16:40 +0000

    previous he sweep sight correct

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_max(lst):
-        return max(lst)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
