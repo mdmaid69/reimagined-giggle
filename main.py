2fb61a6ba96adeda6a39e314584cf6782733b6db
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:54:11 +0000

    considering bringback comment one fast

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
