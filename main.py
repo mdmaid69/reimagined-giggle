d693fb1e7268fd00469e319e27758429025b414a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:37:57 +0000

    write out rate break attract

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
