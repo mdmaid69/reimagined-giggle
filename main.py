8cf9646dab710b4559c6297985b39765b4fa792e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:05:02 +0000

    slight injury over immediate Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
