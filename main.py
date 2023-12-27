ce65a34dfee094aa531628597aa45ffc9f887190
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:06:21 +0000

    comeround please comment feature stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
