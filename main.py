5ae34bbc3f335efb068d99e802adced4bfb2626a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:52:02 +0000

    heavy forget slowly concentrate in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
