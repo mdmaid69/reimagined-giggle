2049f860f4eed81f6a53d057e9e20c57c85a0c4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:09:51 +0000

    branch lock gate them Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
