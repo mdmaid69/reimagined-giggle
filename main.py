537e73a19fba8d77bb984a104695cc8724f5019d
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:37:14 +0000

    catch comfortable cross giveup poor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
