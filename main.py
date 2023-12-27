5a8d0840c8c9fdd08228e9a217c36f91cb3420ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:45:59 +0000

    miss nice attract drive wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
