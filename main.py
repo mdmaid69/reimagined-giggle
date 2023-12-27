8db965162b172258186bce608d76df469656b5f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:11:51 +0000

    Yy you underneath why content

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def multiply_numbers(x, y):
-        return x * y
+def count_characters(sentence):
+        return len(sentence)
+def subtract_numbers(x, y):
+        return x - y
