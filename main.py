c861a8653d4d0d52b50298f9848a1a86ac23cb79
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:54:58 +0000

    instant perform community because excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
