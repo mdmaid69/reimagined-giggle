ca8b7dd30bb00f68732f380d58000711638b0fb5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:59:32 +0000

    hurry introduction weekend true consult

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+for i in range(10): print(i)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
