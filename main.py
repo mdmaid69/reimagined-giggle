f61c68cd35e5c4b108fff3777506378fd417b31f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:00:39 +0000

    physical copy attempt lost OKorokay

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
