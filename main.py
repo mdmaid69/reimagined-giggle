ed669d8173a338587c23ac2bc90cde9538ea3680
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:02:52 +0000

    turndown tend interview related already

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_binary(n):
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def convert_to_binary(n):
         return bin(n)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
