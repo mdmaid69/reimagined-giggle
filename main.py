ed1090a2037448550d914fd1f7d9b08551b6e448
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:37:03 +0000

    lookback(on) throw feed holdup sharp

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import collections
+def create_priority_queue():
+        return collections.deque()
+import sys
+def exit_program():
+        sys.exit()
