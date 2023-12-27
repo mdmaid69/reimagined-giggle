bb4381b45b157cfc9e17aef6f3511d7fd2da3e9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:22:19 +0000

    vanish respect tip opinion Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_as_repr(array):
+        return repr(array)
