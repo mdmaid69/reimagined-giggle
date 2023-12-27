16e8f9e30db460a2d246f1ef7f90e1fa83317e6a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:53:05 +0000

    control switch disagree mark behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
