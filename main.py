e8e2b5c72bde32a7449b7d3520b08e7aeacae94c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:04:31 +0000

    deserve tired tip minor this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
