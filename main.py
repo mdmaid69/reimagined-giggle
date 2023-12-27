95206c7485131df4bc70e00f0cd4070f89807f4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:00:36 +0000

    proof lookafter detail concern act

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
