cc9bbd007de6ff005ae7e29acb894ff1e228a52b
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:40:58 +0000

    impression offer cause mad stamp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_tangent(x):
+        return math.tan(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
