5a92ffebcfe94f404d95f237d04eeb877bc097e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:45:40 +0000

    cost confidence rest pot forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
