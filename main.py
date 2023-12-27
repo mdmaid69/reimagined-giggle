54001c1be6e4c55649f141bd0b52291ce9ff8f50
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:25:26 +0000

    bag club again without calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
