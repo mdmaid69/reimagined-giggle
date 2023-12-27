55b38c7ad1752ccb5a58b9b19447c146f3b83b26
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:13:16 +0000

    turnon recovery friend complain exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
