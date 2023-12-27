b0bf77b837699f4d973660258080dafdea2f938b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:29:00 +0000

    makeinto takeover makeinto go late

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_acceleration(speed, time):
+        return speed / time
