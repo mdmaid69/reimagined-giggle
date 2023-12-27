b13554cc07e4e0eff235fa7c7959bef5a48dbebd
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:51:14 +0000

    achieve still Tt comeround quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_pressure(force, area):
+        return force / area
