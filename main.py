da12dd90ac416bb64a42b5731df9bc88c4cde851
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:59:00 +0000

    dinner cut voice concern twist

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def convert_to_octal(n):
+        return oct(n)
