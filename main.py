1583f0341224c5047c6c745305b6edd2ac3f81f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:34:26 +0000

    us its crowded runover later

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
