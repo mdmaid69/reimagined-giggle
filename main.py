165db8510fc2ed6c13cfe75de7bca1e8f893a9d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:04:48 +0000

    feel please may hide right

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
