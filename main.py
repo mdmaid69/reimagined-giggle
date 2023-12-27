e379ede84d2890bf21712fce75d4ef147080dea1
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:56:53 +0000

    progress describe needle behind slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
