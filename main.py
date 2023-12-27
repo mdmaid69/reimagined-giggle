946ff0efc2db3c366de4aae4124eb138ba85e119
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:18:54 +0000

    Mm pronounce poor culture agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def clear_array(array):
-        array *= 0
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
