5ac7bb0d94c1292074668022e192198745c09197
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:02:19 +0000

    tonight hard event Ff extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
