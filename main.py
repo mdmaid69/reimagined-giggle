2cca0268591d047e1f660bdf1e06ea9c64fd149d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:16:23 +0000

    birth reduction right ashamed owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
