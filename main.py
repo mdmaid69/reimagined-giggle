88894f1e16a531ff86aa4fba6b5e36a6bcc1ad76
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:41:33 +0000

    read police satisfactory nor dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
+import array
+def get_array_as_bool(array):
+        return bool(array)
