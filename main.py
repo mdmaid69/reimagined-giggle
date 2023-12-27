b6da02f60ead448d31fc0f841d38fc2024d43df8
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:26:02 +0000

    us some ocean attitude sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
