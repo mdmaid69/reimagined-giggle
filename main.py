dc7a896e5d37bc6b84cc9483cc3412ea9870c2cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:30:22 +0000

    contact importance mood bus head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
