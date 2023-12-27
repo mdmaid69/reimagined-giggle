9931df3d259475d72ca1c00e688cd827a74ca631
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:54:10 +0000

    left workout shop possibly morning

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
