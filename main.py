97cd91984d19de862f68cffcec4c180732fd9ba9
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:45:36 +0000

    geton pause produce load fun

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
