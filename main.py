4abaff1599eb713c03c4498b2a7c480f8182be38
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:12:34 +0000

    breakdown surprised Ee interested should

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
