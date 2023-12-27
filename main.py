1f06091bd1b943e22a68b3d3fa608ba5fa04e2e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:59:35 +0000

    negative reply mark where operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
