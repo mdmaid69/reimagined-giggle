e9e70cd354647fb466866fa16b1fb3789db670b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:15:26 +0000

    undo unlucky once strong favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def sort_list(lst):
-        return sorted(lst)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
