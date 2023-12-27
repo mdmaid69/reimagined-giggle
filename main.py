027741a23485fbd59c6be3cd796db2e1b6b53ed8
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:59:03 +0000

    card subject fortunately message defend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
