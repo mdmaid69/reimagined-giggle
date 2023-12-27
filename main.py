0cedba80b4fc7be1f5b3fb06449e4f61b147f4c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:36:27 +0000

    picture nearly excuse afterwards message

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def convert_to_binary(n):
-        return bin(n)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_str(array):
+        return str(array)
