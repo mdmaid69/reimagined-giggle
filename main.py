20c2ea9585ad75fb620d4bec2dbf52ccad243857
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:52:33 +0000

    ashamed weekend doctor work fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
