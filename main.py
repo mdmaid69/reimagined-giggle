e805eeb0be524a74394f17e60c092c854fbd16da
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:51:44 +0000

    journey understand persuade complain few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
