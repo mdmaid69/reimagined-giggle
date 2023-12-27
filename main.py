be31df915009ababcfb77af1f9e4ccd73e478816
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:35:29 +0000

    neck mystery needle over test

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
