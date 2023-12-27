e38be33313421cad5cf869354704c7094528e237
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:43:29 +0000

    bringup shelter frightened match cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
