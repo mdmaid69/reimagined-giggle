6c1f8165b20c8c5d214c1a1ce78cfd5e111a3303
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:34:30 +0000

    feel stone 565end apart nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_time_since_epoch():
+        return time.time()
