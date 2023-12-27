762491f451082c94146c9a4d247ab0c532eab874
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:46:17 +0000

    contact tidy shock scare equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
