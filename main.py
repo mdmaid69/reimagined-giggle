f4887ceb9068d3fc686528a71ff4b72e49c50699
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:05:23 +0000

    partner admit getup pool they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
