b4940ef7465b8218a035ecce01375abefb9a24af
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:33:00 +0000

    pay excellent impossible Cc his

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
