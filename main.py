f46ceb53a66dae8725fe0798215f4927614db65d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:45:15 +0000

    loan personal to often shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
