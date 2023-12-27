154086b0e39c4659fd0a97d090bad976049d9a5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:33:26 +0000

    worry pool tidy separate organize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
