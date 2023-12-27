bf2a5a861c8d09aae74a3e2e117382609589546a
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:36:43 +0000

    share comeback sink equal every

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def is_even(n):
+        return n % 2 == 0
