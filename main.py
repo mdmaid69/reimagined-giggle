d2015c10123d4a5e8714f30c37adb56698b99dcd
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:20:00 +0000

    knowledge laugh breath wake form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
