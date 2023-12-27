d17d301dde9cba0db2a73199741486cad64eb84a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:47:35 +0000

    refer clothes unlikely call put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
