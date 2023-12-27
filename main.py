1fee071c50f77fe2d4418f44706c9c38a95a2444
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:50:24 +0000

    situation wrap recently minority sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
