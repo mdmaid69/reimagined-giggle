f65a45edd6d1019911836ad07d055a7cc34f020c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:14:24 +0000

    makeinto whole dress Nn remove

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
