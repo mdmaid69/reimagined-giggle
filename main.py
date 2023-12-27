3f60a581b8e093850ba3eaee5cb5f1b34f01edbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:53:43 +0000

    ilovedicks education pinch foot commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_binary(n):
+        return bin(n)
+import platform
+def get_python_version():
+        return platform.python_version()
