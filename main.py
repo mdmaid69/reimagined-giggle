45d897470d6bace85e14796d9c851f29c4c88fc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:29:30 +0000

    Mm stay exact neither then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
