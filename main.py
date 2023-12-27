fd9da2a680cebe9b69a86ccba38516f71f6ad538
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:16:31 +0000

    street duty already fear prefer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
