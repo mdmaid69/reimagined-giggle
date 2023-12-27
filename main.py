fadcfe96b7d1e9814d62308d68b6b0a0f81d280f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:53:08 +0000

    towards rise straight passage pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_palindrome(s):
+        return s == s[::-1]
