44b2ee6a9a4108c919ae5e6b6bc1ecf8415e1d7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:53:18 +0000

    talk way learn difficult equal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
