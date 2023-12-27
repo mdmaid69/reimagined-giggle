458163dca3365c5a980a8110fbcc134f2769e7ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:21:26 +0000

    callup worst comeback brush decide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
