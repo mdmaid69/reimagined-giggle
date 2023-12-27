c07923bd25139a717b67ca28707e09a71182a875
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:37:40 +0000

    ride sentence meeting pressure still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
