42f15477e6eed0484451f914f2dc041131da7edd
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:02:33 +0000

    earth genuine family beautiful music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def create_counter():
+        return collections.Counter()
