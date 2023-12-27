d024e5203792f9b8c4db97241a981b1a229b4a1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:50:17 +0000

    turnout turninto pause sink sensitive

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
