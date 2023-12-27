81c0624c3dc11cd947133c07ca99a9618861b140
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:41:45 +0000

    prize matter reasonable clear powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_area(radius):
+        return 3.14 * radius * radius
