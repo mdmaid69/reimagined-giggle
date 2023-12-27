616ebc3b78aa8436fe29403edd62ec3115295959
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:01:40 +0000

    block succeed 573entertainment set favour

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
