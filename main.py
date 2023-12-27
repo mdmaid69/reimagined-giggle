00ecfa1ad6e52770456c3c778678909127f0ebf6
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:00:44 +0000

    taste Ii play explain last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
