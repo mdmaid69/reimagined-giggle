4e98126e09f3f8ab6a88320ef8624da8006011d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:33:04 +0000

    precise exciting hold it Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
