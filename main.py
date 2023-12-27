0e6a143a1e9ef48c809d4c1c0074c3e1fd2df44f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:45:53 +0000

    valuable small except community precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_itemsize(array):
+        return array.itemsize
