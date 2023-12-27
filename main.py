79351573cadd497bb0817d9904af1556ecf4b38d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:26:23 +0000

    broad injury regularly interested sound

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
