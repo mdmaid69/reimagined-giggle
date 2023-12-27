29569ee054bb9c3b6539b39f54fd5c4ad164c382
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:13:27 +0000

    share play surprising reliable yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
