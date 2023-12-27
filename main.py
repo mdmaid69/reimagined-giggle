873e7cac59cc095bfd9ff6cef3dfed3df28465dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:27:06 +0000

    cross so cook clean sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
