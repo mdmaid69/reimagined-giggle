7cd04f3941b238cd4979103e136f021396353713
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:18:35 +0000

    it winter glad dance basis

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def clear_array(array):
+        array *= 0
