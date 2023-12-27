0ed78451232328720d60e1866e5fbaef3e180a53
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:14:52 +0000

    challenge march forward connected past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def add_numbers(x, y):
-        return x + y
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def is_odd(n):
+        return n % 2 != 0
