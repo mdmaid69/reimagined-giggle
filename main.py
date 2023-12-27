f05f0d6467c6aca94d646b43c5eb9617216eec3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:14:31 +0000

    climb occasion lesson and message

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
