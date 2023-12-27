1fd61b8c3bdfcf5eaa100f949a82950582b65010
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:03:11 +0000

    speed shadow each file near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
