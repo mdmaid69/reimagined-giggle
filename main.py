d3c8cfade3481cc512863be5699ddf2bfe721473
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:48:22 +0000

    punish concern shade snow getoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def convert_to_binary(n):
+        return bin(n)
+def find_max(numbers):
+        return max(numbers)
