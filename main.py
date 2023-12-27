4336f0b9408508ba9143f8ad6ff1e0bb25d18716
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:46:02 +0000

    method know second sort project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
