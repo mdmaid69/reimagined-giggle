8db68847fdf7273d3c4b34c2edcc93c6b6c7d444
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:53:13 +0000

    behaviour yet punish call book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
