f793e4d76ef6898e676a27a7c4179e9cdaf31ee8
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:53:14 +0000

    pain little welcome workon study

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
