2180f4c65b2031c508aaeaf944a59e99ade1a355
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:58:08 +0000

    contact single contain food carry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def is_even(n):
+        return n % 2 == 0
