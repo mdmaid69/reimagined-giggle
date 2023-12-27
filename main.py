0d8fc57b198297630e723c2a9e15dceaace0590c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:09:00 +0000

    plan takeaway tip improve sister

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def count_elements(lst):
+        return len(lst)
