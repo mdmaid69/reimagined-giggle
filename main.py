daa076c3c1afc82241d0955c955f2f400379e284
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:20:27 +0000

    uncertain angry shop work doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import re
-print(re.match("h.*o", "hello world"))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_length(array):
+        return len(array)
