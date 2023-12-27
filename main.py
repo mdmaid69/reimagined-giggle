c9b102320f783be4e2f4315c2ed9fc6c9eb10b6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:30:02 +0000

    meet trap purpose beautiful male

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
