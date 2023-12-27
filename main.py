1db4e05eceb18b061737246ae07e1d4d4765152a
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:42:21 +0000

    black grade smooth blue find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
