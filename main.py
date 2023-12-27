a3da75cfc64ff4ff8b8773c73786968d9aaa8b86
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:51:15 +0000

    stick goback(to) goout fail swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
