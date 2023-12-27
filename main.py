e8dfd1286bf93dd03174577a57b6c0a73ea87644
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:02:40 +0000

    send feel object guard connect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def convert_to_octal(n):
-        return oct(n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+print(math.pi)
