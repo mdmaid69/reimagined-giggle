e68c8cd38c088d953b0ffbf4064670aea8136f5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:04:21 +0000

    remain suddenly realize hear shock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import collections
+def create_stack():
+        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
