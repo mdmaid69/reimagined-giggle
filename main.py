dac26c30bfa58d7601dd5f9e264379e1b9108922
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:02:20 +0000

    find firm either persuade work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_priority_queue():
+        return collections.deque()
