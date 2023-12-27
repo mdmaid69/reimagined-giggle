e5854b49e8e6bafef4bf1db80ced312597755985
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:51:35 +0000

    further health rubbish earth national

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
