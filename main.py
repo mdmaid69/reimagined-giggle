3b63106e453b11f4f5668b549c561e782f02360f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:44:25 +0000

    comment wave react next accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bool(array):
+        return bool(array)
