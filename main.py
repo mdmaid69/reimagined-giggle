942c674cca5fd4ebd550279f4e8a6223537c2dc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:36:07 +0000

    pupil work passenger news old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
