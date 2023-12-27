ebc898d8d882c3a2052e3fcaa05ee60e9d4ead1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:49:40 +0000

    regional wake extra ocean measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
