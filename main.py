fdbf5b60ecf63359947af606a17b44641ee4320e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:03:00 +0000

    grow round under how flow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def sort_list(lst):
+        return sorted(lst)
