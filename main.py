933d6141cf8925e1cba45ab40ab7fa133a7429eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:52:07 +0000

    stage air fault show he

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
