c7547d2a042c3fb85866e8970fe9b526d40de7c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:07:22 +0000

    without send maybe state transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
