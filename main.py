0fd183dc5a1a364465c29b60a9d6ac95992c8b0e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:23:32 +0000

    comfortable study method recent voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
