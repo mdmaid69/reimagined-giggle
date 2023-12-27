27068acb450052d0423577295634550a76a8316c
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:47:43 +0000

    next comefrom runaway roof late

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
