e03174eead59d7f72755b49ccaca2ed6fd7954ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:27:30 +0000

    more ordinary his Kk telephone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
