11b996d6576ff96c21632bdb37a1290d7e91b1ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:21:24 +0000

    part expensive division along stage

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
