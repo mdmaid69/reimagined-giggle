529e458ba9d6a30ccfde1e7fe4ca31d0c4b387ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:03:58 +0000

    charge language silent square usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
