614dd4a885ee4dbb42cbafd89311d4e11f426da9
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:04:34 +0000

    record limit tonight expression thorough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-name = "Python"
-print("Hello,", name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
