c41e789958ca29a38bc11e95fa17a4d7ee31351a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:34:26 +0000

    occasion clever common whatever freeze

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_work(force, distance):
+        return force * distance
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
