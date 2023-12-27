b953c4e4c701a7d1d566d486477f39761139ca47
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:51:01 +0000

    concentrate workupto waste makeinto nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_min(lst):
+        return min(lst)
