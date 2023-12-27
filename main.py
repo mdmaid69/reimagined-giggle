d6016c29d177649b992e01fcd4c6fba42ae64427
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:22:09 +0000

    trick both part slowly between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
