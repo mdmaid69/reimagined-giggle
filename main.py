83cbeeabe6406352e3ea149e08ed9c05df1101a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:36:45 +0000

    break twist safe wake emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
