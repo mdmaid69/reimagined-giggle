49fb46a89227953a7746cbe87d8041b33a630d0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:14:54 +0000

    talk brown deep doctor repeat

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
