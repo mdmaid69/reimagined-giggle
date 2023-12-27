89bfdd698db8f1e229baa94497fe1d30541df1b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:16:57 +0000

    finally print wish glad regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
