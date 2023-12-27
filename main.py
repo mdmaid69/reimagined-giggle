c2741f5e11193ea4cc7f4571e952e3aa6b2dd90d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:37:11 +0000

    guilty bored appeal black her

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
