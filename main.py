fba44a219d274716657e528876144d4222770108
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:15:53 +0000

    nothing sign importance occur activity

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_pressure(force, area):
-        return force / area
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+print([x**2 for x in range(10)])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
