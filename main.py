7a335b4028ce3fd26777fcad0c5b35431225bfda
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:01:22 +0000

    fix Gg reply wash health

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
