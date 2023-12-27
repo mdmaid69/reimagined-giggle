5a8054259b769cb674e7b351b3ff8311b07694d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:08:11 +0000

    own may congratulations area project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
