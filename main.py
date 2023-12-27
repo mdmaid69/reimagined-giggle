2569ad5d88d60bd241972f274c547ab36334105d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:14:21 +0000

    turn name lift bad give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
