d8044003d7356f3fc231320c56c1f9cfe1e6590d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:06:23 +0000

    last flood where keepout serious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import os
-print(os.getcwd())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
