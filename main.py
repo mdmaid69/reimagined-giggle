02d035eefd478be745738412b41002dd16c86e6b
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:57:07 +0000

    roll wherever office harm hollow

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
