f1bed07d25a7a5e29a440b301da60ca783259bfe
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:20:33 +0000

    happy front clear cost do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_bool(array):
+        return bool(array)
