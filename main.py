4c5100f6c7e10eb4da1fd33cd3a1f1eea4bdfdd4
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:39:20 +0000

    upwards under task lesson against

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
