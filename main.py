aa08869ebd1d99dcb15c7de0d2b8daa4bdd31755
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:24:47 +0000

    only succeed free disturb tidy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
