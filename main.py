052879463a6d2aa0e819c56b1f6685ac4d3c3d15
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:33:29 +0000

    rescue talkinto sign small remain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import time
+print(time.time())
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def check_if_array_contains_item(array, item):
+        return item in array
