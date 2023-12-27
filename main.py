fd12002dd82d0967cc9848ef9bcb45169df412e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:09:30 +0000

    ticket certain write beginning plate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
