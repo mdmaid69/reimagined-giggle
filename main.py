d011a02241788d7196a73efb008777ce2f43c44e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:22:26 +0000

    make generous introduce steam compete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import numpy as np
+print(np.array([1, 2, 3]))
+  def count_elements(lst):
+        return len(lst)
