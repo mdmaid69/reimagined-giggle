4e13c0bca96f43ccbd6c76ddff8e3e637e02eecc
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:30:43 +0000

    happy recently travel physical hole

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
