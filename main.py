6f69489d22f3061cc78af1671303a2d118feadb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:57:13 +0000

    section sister ride pattern brown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
