48acba682d3cf98e806f9c2f3c60f2ad0921fd04
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:42:36 +0000

    married fortunate here join reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
