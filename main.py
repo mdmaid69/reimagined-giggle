913fa13c64a2e56eab5fb4ccc93e0cf8167b7b80
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:12:52 +0000

    time they through but goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def print_python_version():
-        print(sys.version)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
