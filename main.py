bc4c1bf793b69988d8638466491c0ef6ef6486ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:26:17 +0000

    notice entrance smell waste during

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_str(array):
+        return str(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
