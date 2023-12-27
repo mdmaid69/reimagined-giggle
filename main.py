8dac60ebadbe4c4a86e90c7ef58fea1f1d963868
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:23:13 +0000

    unite plate hit many song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def convert_to_hex(n):
+        return hex(n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
