297edaa6f441baa5ead9f825018aba1a4953dc4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:12:44 +0000

    software new love fight throughout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
