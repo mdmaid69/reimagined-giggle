d8949aa8f788d8a56ada52f017e46f269df89dcf
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:34:58 +0000

    remove many city care detail

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
