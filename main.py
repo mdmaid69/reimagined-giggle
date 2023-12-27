730c24ac70923a5a7f29a97b69fdc59dd1dd5d31
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:03:35 +0000

    nearly pile scale amount book

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
