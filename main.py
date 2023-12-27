737317b7b89301a23a4e89951c4c7c41ddb36f0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:00:41 +0000

    impressed contact still fall turnback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
