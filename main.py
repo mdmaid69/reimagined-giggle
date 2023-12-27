b8b0d428efa9a0ccaa942c41830e308393ec450d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:36:11 +0000

    violence last upstairs tomorrow search

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
