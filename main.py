f1024c08e998291a5fc3004fffb285570cfe1890
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:02:25 +0000

    blow paper amount further remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
