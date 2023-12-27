1a0f569aea86758be07bf504d3f735aa019f8548
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:39:45 +0000

    country shout tough first shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
