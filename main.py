b318ab5aa5bad90bef9f50a48b00dcadc15a2af2
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:16:09 +0000

    already his head dig practise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
