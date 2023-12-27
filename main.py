ecf467c5a273b809f27b412cd2c67ec8342f1898
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:07:11 +0000

    variety 564encourage peaceful content emphasize

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
