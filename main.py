f6b7c331de61d4cfadb30c7fed029c68429f023e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:57:22 +0000

    feeling we happy goback(to) send

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
