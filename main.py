6c739263adc6ff9fcbff1a17a89c13c6b53b11fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:21:13 +0000

    picture introduction spare pot mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def divide_numbers(x, y):
-        return x / y
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
