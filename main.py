38e911182ac5c2eac1a3797d0c7a10c1e420ad92
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:11:09 +0000

    awake nothing at estimate feeling

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
