cdc428686296dd494b31bfe444f9a922037529f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:23:26 +0000

    outside inquire temperature order regular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-name = "Python"
-print("Hello,", name)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
