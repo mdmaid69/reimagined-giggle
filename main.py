26ea4a35ec804ce6f1562fe56d53a9d920243d03
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:46:43 +0000

    human shine fail promise long

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def find_max(lst):
-        return max(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
