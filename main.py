5944632c028fff06502e4c8d208d6e9c35719250
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:35:40 +0000

    bored serious consist quality stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
