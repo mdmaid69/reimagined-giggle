316209be0dcb95f6f183f18a4a5e5dd0f2d94fee
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:02:38 +0000

    any interview interview some aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
