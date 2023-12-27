49e04e065037ea71cbcaaf1c0167e410f0bd9c94
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:17:48 +0000

    acceptable preparation dead boil active

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+print([x**2 for x in range(10)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
