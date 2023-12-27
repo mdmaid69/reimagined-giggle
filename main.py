2da485f5efbaa116550e75c3d2a9afd007e5fb16
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:53:04 +0000

    hardly either record activity emphasize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
