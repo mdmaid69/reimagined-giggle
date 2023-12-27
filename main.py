f9d8097ebe388660c480504b6c4eaf4e8c50001d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:29:59 +0000

    performance common steal tight thumb

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
