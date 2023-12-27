927ed4403764535d0127c24240009d85fd99d8e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:20:03 +0000

    confidence shoulder actual drawing repair

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
+  def calculate_perimeter_triangle(a, b, c):
         return a + b + c
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
