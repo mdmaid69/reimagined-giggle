b1dc821eca33842e20006003437ce8579ba67d31
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:29:24 +0000

    goback(to) dangerous believe really marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
