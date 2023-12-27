b09b0f9193eafdebb888316916876942684b17f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:49:40 +0000

    progress like protection will peaceful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import os
-print(os.getcwd())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
