e2c5e522c2dac43a8832576a07c479e62a9c946d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:31:52 +0000

    hello personal tomorrow clean move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_ceiling(x):
+        return math.ceil(x)
