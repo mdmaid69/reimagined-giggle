94eab69365be0f3fabc186b02ca8bcccf817b1ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:08:19 +0000

    occur onto backwards free sense

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print(sum(range(10)))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
