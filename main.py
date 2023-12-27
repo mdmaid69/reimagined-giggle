404c86922c2d09a39912b60a511001af02bbc908
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:57:38 +0000

    load Itself weather beginning flight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
