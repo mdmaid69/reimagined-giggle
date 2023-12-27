e059f0ac879e838cc12ad80549d56d8ddd78acb7
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:54:12 +0000

    interest familiar pollution opportunity new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
