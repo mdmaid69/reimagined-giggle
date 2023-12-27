98150529924c2403736d9c2d9e9292c7e59deec3
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:24:43 +0000

    set suggest roof however build

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-print(math.pi)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_length(array):
+        return len(array)
