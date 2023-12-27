8b5e60bfa8a80e89600cc623a122d444de6a2284
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:59:41 +0000

    row bottom possession gas illness

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
