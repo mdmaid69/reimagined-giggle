72f3b93095ec8aca7c363d301620bd17f2af3b9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:58:33 +0000

    correction lower lookup independent fold

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
