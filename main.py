0558a9ad9d939b72e49938c4b73dee368874f7aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:45:42 +0000

    education custom belief damage refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
