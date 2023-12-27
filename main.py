67006c496f20816913a9f716d2ebfd3b6e789456
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:47:30 +0000

    influence gather tragedy scream raise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
