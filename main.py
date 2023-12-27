ca968bc8887db46d66bd8482c36a7770d412a34c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:30:07 +0000

    male measure imagine practice character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
