ee8b09ac0729b58e36e9628ab1b58776aead992b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:00:19 +0000

    against slide quickly one watch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import re
+print(re.match("h.*o", "hello world"))
