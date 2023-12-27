54c0603f029938a48e6af27205d9a6adcf1f42ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:41:33 +0000

    edge worst neither block wait

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
