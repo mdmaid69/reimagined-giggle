eccea256f2186958f19491422e9325d3472a7efd
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:25:24 +0000

    load gentle past compare appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
