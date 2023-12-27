f59bb78dee4fb491d7bc3bb91c096fa7cbd83a72
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:55:38 +0000

    we study thing lead shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
