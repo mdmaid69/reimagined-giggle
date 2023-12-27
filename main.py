4433b01e8d6d274e7915b97616106f1974f7a457
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:17:52 +0000

    order flood hospital task pencil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
