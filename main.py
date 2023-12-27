de5b304613579fc6f331271722c57160911ffcbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:31:09 +0000

    anxious what shout alive sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
