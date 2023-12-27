4f8d2fe2fc52323647c60a11b45b4ab9fdc02911
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:41:16 +0000

    never smell blow ignore quality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
