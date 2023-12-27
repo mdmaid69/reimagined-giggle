457d2c6ac1b8ff54249e1447c849adb5c37739b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:07:10 +0000

    regularly food doubt silent reliable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
