f2f25cc95e1ab87c50e504138df2f89f625b811d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:42:56 +0000

    woman sense onto thanks base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
