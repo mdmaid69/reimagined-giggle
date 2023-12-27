c0b24de6d87a603e3dc22e5db0158c35923c32ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:21:46 +0000

    call ashamed punish fit claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_permutations(n, k):
+        return math.perm(n, k)
