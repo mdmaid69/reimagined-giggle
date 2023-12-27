1b848da4e926e9cd66882f3901d1d1b988cf3805
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:32:11 +0000

    outside damage lot scream shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_repr(array):
+        return repr(array)
