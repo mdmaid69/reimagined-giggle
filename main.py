35a1d82e3e262bcd159a3e23f41d5aa4349c43e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:46:07 +0000

    give ignore maybe see stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
