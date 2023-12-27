2e0d256fb553f8d876958233ddc67485149f37b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:09:11 +0000

    pleasure several badly music nonsense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_index(array, item):
+        return array.index(item)
