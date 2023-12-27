b533422e04d08b763e0641e61b5af21302992b6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:12:01 +0000

    in prepare sharp jump official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_speed(distance, time):
+        return distance / time
