380c97ff26eee075a30afe93732e7f849549be2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:31:21 +0000

    steady nor office able work

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import collections
+def create_counter():
+        return collections.Counter()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
