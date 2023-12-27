1811e795c12cff5ded85b9904ac9026b525fa11f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:34:19 +0000

    hurry person once plastic basically

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def add_numbers(x, y):
-        return x + y
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def sort_list(lst):
+        return sorted(lst)
