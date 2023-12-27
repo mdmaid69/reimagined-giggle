9c3c6ffc75ca20a8457a97dcc7f7db93e9517d8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:19:13 +0000

    both middle worth guilty aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
