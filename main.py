0bbd636de1abe844a6fc4faebb591ac8007b2333
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:45:45 +0000

    bury equal perhaps passenger depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
