cd5fa9f9b6fb708390782505880c687be0b87451
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:30:15 +0000

    fasten experience suggestion afternoon swing

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-for i in range(10): print(i)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
