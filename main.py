afa635f37b4e8e762b48da73f9a041ab256a8d91
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:37:56 +0000

    minority pour term take likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
