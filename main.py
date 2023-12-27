f42d1328016c2efc98ff903f8f1c3b9dc6d322ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:24:04 +0000

    base door wherever confused fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
