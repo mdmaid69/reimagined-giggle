bcf64f9cbf911d24376b53b44cfe81120a1e919f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:37:05 +0000

    offence themselves break as ashamed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
