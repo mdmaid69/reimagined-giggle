3991ed8d991ae6b6b9284f17170710515e0d2c6b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:07:22 +0000

    afterwards sort you set praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
