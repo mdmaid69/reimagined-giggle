172a00534982a681c65dd0f19bf4074473367793
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:05:26 +0000

    quantity shall or television heavy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
