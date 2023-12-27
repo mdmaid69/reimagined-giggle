376d80a27e013db05e74f9158c229d323a236727
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:02:30 +0000

    suggestion condition divide narrow solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
