184b0582e9d07ba57ab86220decd7837351e0e8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:48:48 +0000

    record clothes pleasant correct spend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
