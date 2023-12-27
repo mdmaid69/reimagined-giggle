8f2bd573b4f7170f68e322e7d8e3fbb23616dd3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:44:43 +0000

    song shake who bread frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
