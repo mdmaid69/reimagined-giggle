a34a1ee7867ecf5adb4cf6ccbe2143664bf6e2ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:24:45 +0000

    satisfied split ever include level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def set_array_item(array, i, item):
+        array[i] = item
