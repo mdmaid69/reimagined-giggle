882da5be6492e02b355c1411bbf23e521928a67e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:46:58 +0000

    relation mouth beat boat pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
