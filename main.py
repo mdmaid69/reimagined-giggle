23009023d8dd0076a28a192b5f912a6771956501
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:30:53 +0000

    disapprove underneath wake careless human

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
