ab1ed035ff093a5ad926a1192bd350f1484cccd6
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:57:46 +0000

    practical present copy cheek leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area(radius):
+        return 3.14 * radius * radius
