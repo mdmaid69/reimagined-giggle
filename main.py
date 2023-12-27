98c6bb08c035593ae84c6163c085a121f1c204d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:51:34 +0000

    visit check use scale schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
