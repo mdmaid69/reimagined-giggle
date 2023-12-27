0551b5d2e65ff7ffc49eda5037111965925e9d3e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:43:21 +0000

    way agree period remark convincing

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
