e91acfd4420d580a251394e01b57c89c17218e6b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:18:29 +0000

    proud plan declare splash care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
