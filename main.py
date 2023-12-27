82d7c9df99fe6db519e130674d1252e2c262e368
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:54:51 +0000

    congratulate commit minimum command autumn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
