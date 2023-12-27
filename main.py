b7e22e2004b1fa980a02af13208ff953ddad53bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:44:56 +0000

    steer female keepoff family take

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
