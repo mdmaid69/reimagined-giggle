5f99cc8cc7a8aab91cc4a34bb624ba0cdea647dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:20:20 +0000

    what wise glass chance bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
