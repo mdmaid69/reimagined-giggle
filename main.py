6d84c99adfd736918e377fdf9f2cd3878f7cebc4
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:42:23 +0000

    fear sentence activity sale makeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_string(s):
+        return s[::-1]
