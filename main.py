4ca12511449ce61f76813530d22312ce1f4d1471
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:24:19 +0000

    city moment dress record solve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def is_palindrome(s):
+        return s == s[::-1]
