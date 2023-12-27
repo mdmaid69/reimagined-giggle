ee3c1f9c8699efb9bf63bb2c7cf7f55a5cedab36
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:52:25 +0000

    mysterious agreement hope skin tape

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+print("Hello, world!")
