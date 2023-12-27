0ceac3297c84515512c89868fa1d166e0e680f23
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:16:04 +0000

    necessary criticism company explain schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
