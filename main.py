be543ecb3896f7b0499c99ead0aae46ef2848bb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:45:32 +0000

    birthday tread present answer wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_date():
+        return datetime.date.today()
