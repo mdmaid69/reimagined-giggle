ba761aa8a62cdbe419a7cd4dafe44fadfc5beffd
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:47:55 +0000

    collection country time whistle consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
