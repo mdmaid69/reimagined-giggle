e89330da29acdad897ed2408822e4535859f6119
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:46:27 +0000

    wing check mix snow either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_typecode(array):
+        return array.typecode
