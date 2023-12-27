84ae714afb77a69056d55fce90af196041d2ccc0
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:15:41 +0000

    steady cost hard other obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
