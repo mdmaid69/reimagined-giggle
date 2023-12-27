6e99bed8728b760669cce9ad5161b517cf892574
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:12:20 +0000

    cover you blame far upset

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
