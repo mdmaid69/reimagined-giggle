9c53e450a109ddd807e801e879f4546cfe8d6670
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:36:35 +0000

    born serve practical height like

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def cube_number(x):
-        return x**3
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
