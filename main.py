a0c641e1faf456790247886211b4308598c4645f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:40:26 +0000

    sure length breathe path address

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
