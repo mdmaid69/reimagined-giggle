1ebfbbca9a0ae4b80cfc9ff816070a25fecc7631
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:17:00 +0000

    rent upwards remain insist attitude

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
