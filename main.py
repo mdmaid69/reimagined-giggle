6232873744f141a4e697bcc3f709bb39bafc4cc3
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:21:37 +0000

    rule room hate maintain violent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_acceleration(speed, time):
+        return speed / time
