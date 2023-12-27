5e91d4ecd0066534254bf99bbc3944df6535d252
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:28:14 +0000

    belief us until keen central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_typecode(array):
+        return array.typecode
