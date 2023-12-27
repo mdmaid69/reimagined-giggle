ea4a6de5332a1e80e551933c5ae751b9d1054057
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:02:05 +0000

    accept wave innocent me famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_work(force, distance):
+        return force * distance
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
