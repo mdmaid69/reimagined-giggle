76ab3847a5b24d5c9c91e30d14b184fa7c055c7f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:46:29 +0000

    challenge past guess help cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
