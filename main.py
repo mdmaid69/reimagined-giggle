537007182d61910d97fa2b0d2c6caf842f882be0
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:33:24 +0000

    unfortunate everything lesson key black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
