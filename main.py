cb63141f04510c267314df546b488d1c7cb93815
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:46:57 +0000

    suddenly pain about somewhere who

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import re
   def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
