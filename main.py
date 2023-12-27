3f5ee21eaf8ae2e69d9fd9e02589dd1113c40bc0
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:56:08 +0000

    feel Rr takeaway hide piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
