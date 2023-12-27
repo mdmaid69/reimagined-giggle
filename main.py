c0b729f23829358b34089a850a28889d57ad071e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:22:14 +0000

    good nose vanish prefer maximum

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
