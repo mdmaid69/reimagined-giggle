fa124dda76cfc6c2722507c0963ec61ab30c13e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:01:50 +0000

    open day prize knock rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
