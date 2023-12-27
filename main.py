6e9d50a36d00a65d3d1b748910c17933ee9d07a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:47:45 +0000

    interview danger instead should present

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
