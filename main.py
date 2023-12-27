966c235d552e4edc15200283d51c23849e1116e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:49:21 +0000

    myself second fly risk comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
