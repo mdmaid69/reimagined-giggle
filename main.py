839d19b71cd38ed3f14be0b554323113d84362db
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:47:58 +0000

    depend lunch balance safety create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
