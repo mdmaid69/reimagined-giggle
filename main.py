5a6eafe007c6c23e669c41ed4b929ad4aa07e5fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:49:57 +0000

    actual estimate smell remark guilty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_current_directory():
+        return os.getcwd()
