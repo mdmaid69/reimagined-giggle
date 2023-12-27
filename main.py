525aa0dfcdc331b4bdd8801698804f8348c38528
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:32:03 +0000

    advice cover stamp funny immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
