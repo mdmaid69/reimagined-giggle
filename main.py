da463313af89f18a0180adb303e7be8a0cc4de78
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:09:14 +0000

    explanation ring skill previous trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
