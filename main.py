8eb952432fa0a8bbc59b947f4bfece0fba10b9da
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:51:31 +0000

    find favourite kick warm regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import sys
-def print_python_version():
-        print(sys.version)
+  def convert_to_octal(n):
+        return oct(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
