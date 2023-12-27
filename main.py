4583ac4962f7ec647ba84bff8c2e1a9031d168cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:38:30 +0000

    neither lot government bunch also

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def calculate_area_rectangle(l, w):
+        return l * w
