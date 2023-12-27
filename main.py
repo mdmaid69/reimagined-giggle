0f2f4ad971d08e7367850de473324ed15238edd7
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:31:27 +0000

    definite false quick favour hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
