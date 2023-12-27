23bcc97281a68a7a32b22c056c0a73e48991bd6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:34:17 +0000

    store march print 571enough connected

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import logging
-def log_message(message):
-        logging.info(message)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
