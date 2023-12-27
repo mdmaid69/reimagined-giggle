a2bad9c207d6fa19c4b5bf60d9f4b196c7414dbf
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:29:53 +0000

    violent lean wall out snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
