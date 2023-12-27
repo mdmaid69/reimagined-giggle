11afd33311de8186d82164c0b59463c6f0637f1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:30:56 +0000

    allow copy success compete mix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
