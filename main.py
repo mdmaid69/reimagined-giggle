222021014c7de255d81a3c016b31ee46c9771102
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:33:23 +0000

    hospital still right face not

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def is_even(n):
+        return n % 2 == 0
