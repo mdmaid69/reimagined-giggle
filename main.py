154b2ed87cc450b3489c565672c940ca3548961b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:37:30 +0000

    calmly interview fast head all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
