6da7108bf298b29510f624ded866c0e0e20145ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:30:11 +0000

    tap neat well ground worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_ceiling(x):
+        return math.ceil(x)
