f1f265c5a964b873009906d9e7f2f9161f974a86
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:46:23 +0000

    ago swing lot boat section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
