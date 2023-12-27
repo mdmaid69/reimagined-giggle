b1de09a34174c8c7b07211bd15b0d7599b06a10f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:34:22 +0000

    cruelty patient small recovery label

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
