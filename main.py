87f191fdba9109692425aec5a550a78d773a4b44
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:48:31 +0000

    match spin lesson hurt warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
