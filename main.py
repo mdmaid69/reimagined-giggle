717b3e0010798c23c2173660af8965125c14b54f
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:54:34 +0000

    careful waste side platform sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
