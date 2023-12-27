91136ccaf517f15ec24ff8386e0246833d14782d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:11:13 +0000

    high mother warning drive congratulations

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
