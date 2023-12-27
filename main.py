84d34a0a30fe53d1fc8a76abfa2848c757968242
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:40:28 +0000

    perfect term suppose so attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
