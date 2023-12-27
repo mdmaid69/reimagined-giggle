7005c7d47970446d7c4107c7a5051ae7b7a54137
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:23:48 +0000

    exchange yet any sensitive exactly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
