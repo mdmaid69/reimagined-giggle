ad6f49d28a2014504822a0715dcaf72ef76ec2de
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:07:42 +0000

    gather talk friendly quiet familiar

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
