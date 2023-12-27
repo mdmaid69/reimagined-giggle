524b27b0cfd6595f6c5b55df9ee9e90aa354a158
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:23:14 +0000

    duty manage seem Ii that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
