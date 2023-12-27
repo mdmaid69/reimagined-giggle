d5cad7cab9d4e7826815b9482c817996486c0d3e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:24:05 +0000

    relation handle control nearly I

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def count_characters(sentence):
+        return len(sentence)
