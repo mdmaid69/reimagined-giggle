a3eca2742bf3f6e5ad439e0ef9b51476ed6da444
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:53:46 +0000

    rid check moment sorry lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_work(force, distance):
-        return force * distance
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
