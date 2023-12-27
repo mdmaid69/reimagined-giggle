188df199c0434e102494a74d4a8d3ee90b87f224
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:08:06 +0000

    sport sit goout little lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
