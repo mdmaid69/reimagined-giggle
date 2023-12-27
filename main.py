7714f7b988c8538e695143c80d9328a2beed9167
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:03:48 +0000

    mend information capital part impatient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+for i in range(5):
+        print(i)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
