d5dfd00086c5832bc52ac5f6ed7db43583e45a65
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:06:10 +0000

    position down design wish problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import time
-def get_current_time():
-        return time.time()
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
