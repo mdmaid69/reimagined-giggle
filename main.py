f20f4ae61944823b33083aadb3e5c522e6ce60a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:45:34 +0000

    absolutely lift feature time release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_complex(array):
+        return complex(array[0])
