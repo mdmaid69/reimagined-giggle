3daf5e33d4f83ce25fc7beae1a3977c8e80e55ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:21:02 +0000

    holdup union but throw conclusion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
