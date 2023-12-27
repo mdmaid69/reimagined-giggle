2c11fe2af660ee3a92723a0c2f89b1a5bc894ebb
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:08:21 +0000

    tooth would through well freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def is_odd(n):
+        return n % 2 != 0
