5df7fecf79df14caa8f79dbd76e2cd3833c9ce8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:59:24 +0000

    number tell pool now someone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_list(array):
-        return list(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
