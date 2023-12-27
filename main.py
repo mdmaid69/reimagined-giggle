5774493e4ab8daf44b91dfd6d0f609d84c6bac1c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:34:04 +0000

    giveback computer tongue platform tense

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+def get_current_working_directory():
+        return os.getcwd()
