a3f79d320ef82594c304adc47af675ea472693b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:06:58 +0000

    anybody style reason better whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_octal(n):
-        return oct(n)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
