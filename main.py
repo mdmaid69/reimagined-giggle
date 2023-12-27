c891472151cc6118dbe699f92e5613f7a6bd4167
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:20:49 +0000

    switch mad time close crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_force(mass, acceleration):
+        return mass * acceleration
