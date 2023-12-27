978f16ceb58f32f77f53b98561a4d02a07a93cf2
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:13:54 +0000

    month convince short press means

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
