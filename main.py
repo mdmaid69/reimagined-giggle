0a573af0ec396f73591aa7d0795ee957fa210458
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:31:55 +0000

    certain team afford justice disapprove

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_max(numbers):
-        return max(numbers)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
