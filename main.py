c0d78fdc21e5e4d4a3663f3f4ad8a72de470b3a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:41:31 +0000

    lucky minor fit uncertain scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
