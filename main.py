2b6e9a3d474662e07420bdfb99111ff945c1fe35
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:09:02 +0000

    extremely last label space regular

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
