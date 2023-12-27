2d757f957ec27ae72712f2ddb43cd0a167fce2c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:05:39 +0000

    lend hope invite right follow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
 def get_array_item_count(array, item):
         return array.count(item)
-  def cube_number(x):
-        return x**3
+def calculate_perpetuity(payment, rate):
+        return payment / rate
