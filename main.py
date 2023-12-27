f1c78c8e98fcc3824f617d4a5def787a04b9f209
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:07:51 +0000

    around view want impressive occasional

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
