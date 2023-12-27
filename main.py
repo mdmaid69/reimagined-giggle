d1c3bb8cb50f42a48f97251c23773e55d1e2c08f
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:11:16 +0000

    minute fasten satisfactory world sore

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
