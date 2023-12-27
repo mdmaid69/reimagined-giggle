17381b74981f707eaa14d67edb6b6ad129f6094a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:38:48 +0000

    object far perform taste pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
