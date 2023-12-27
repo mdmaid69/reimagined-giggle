2e60bb0811bffbe60a1876f0a7954f835f6e06f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:00:41 +0000

    note row regularly return central

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_directory():
-        return os.getcwd()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+print(math.pi)
