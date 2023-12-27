6fa94406237ac80d9aa5da170a5c71e0f8609b30
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:23:53 +0000

    ball second intend woman air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
 import math
 def calculate_combinations(n, k):
         return math.comb(n, k)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
