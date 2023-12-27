dfb8e22accf7cfc4edff6b6b6b644c23d658b945
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:22:04 +0000

    regularly discuss statement weak comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
