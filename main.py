c88ccc5de485f238fef635dff862fc1d2f190e50
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:31:45 +0000

    road touch regret knowledge short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-x = 10
-y = 20
-print("Sum:", x + y)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
