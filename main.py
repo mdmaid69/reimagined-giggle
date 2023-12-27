0e801d727a6f8278468203ae69b8f28a3fdee62e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:40:04 +0000

    right physical convince quick interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
