94920f0e17cc49127cb320845d76d49f08e85e22
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:02:26 +0000

    familiar comeover comment culture party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_base_name(path):
+        return os.path.basename(path)
