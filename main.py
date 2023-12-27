7dcaab3a0367fe303bf3a9929bae83ab8b9186cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:37:28 +0000

    wander come inside previous smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
