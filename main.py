af592dcdf880e5ef061cafcc632498090dfb38b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:13:34 +0000

    method concentration tough like straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
