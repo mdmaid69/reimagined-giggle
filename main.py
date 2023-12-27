631087bf8ae513ef5b5adc3df36981e5d4b738fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:45:53 +0000

    drop patient relation will always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
