357387bea1442b0919d5bbe3ea4421084bb71c80
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:12:44 +0000

    ticket know pay paper shoulder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
