88a863cd21334ab167669c6cc3612ed560974c4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:11:27 +0000

    boat attempt quit morning rub

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import array
-def clear_array(array):
-        array *= 0
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(10): print(i)
