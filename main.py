f485fca322b03a4f40995e1486a51b1f9c1cdef0
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:15:09 +0000

    under sign practise used design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
