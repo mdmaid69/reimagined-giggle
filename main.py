4801ca5fde4679bd69919f07ad32d09b3101fe85
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:04:18 +0000

    somewhere so file unfortunate false

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def convert_to_binary(n):
+        return bin(n)
