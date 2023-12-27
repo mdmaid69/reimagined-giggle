4079b4ed15ad59585a66b1e35d335ec911976b2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:00 +0000

    police characteristic emotion fold dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
