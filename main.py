d099890a991679eb9d4de6d77507bd196dfe04c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:45:00 +0000

    them familiar clearly car trip

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_acceleration(speed, time):
+        return speed / time
