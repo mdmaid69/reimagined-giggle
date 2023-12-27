038567ef37d437cd2e9f6f143c0a1046665a92d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:18:36 +0000

    estimate sink set reasonable so

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+print("Hello, world!")
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
