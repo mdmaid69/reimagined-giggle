3bcf30dea27ff0a0f327b088a4321296dd3eb97f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:22:00 +0000

    since taste term business needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
