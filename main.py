0e9976a95e66dbe3e5281e15564bf95c7735fda3
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:20:15 +0000

    regret empty glass hide beneath

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
