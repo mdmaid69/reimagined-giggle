897c457b8ba230819fd9ab586f3cee5b1f99465b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:42:01 +0000

    demonstrate smell wind immediately class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_binary(n):
-        return bin(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
