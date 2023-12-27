e8c9b0b7d676b11894d85f143309df8504d68ec1
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:31:38 +0000

    object tonight opposite project listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
