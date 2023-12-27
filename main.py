9696cd71b9b14a087de838952cd0d6fce09c3559
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:51:35 +0000

    unite runinto unpleasant us imagine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
