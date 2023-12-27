8f4104533c2e35d59dc68be9906bcc5c94bccf85
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:28 +0000

    consult delight against through better

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
