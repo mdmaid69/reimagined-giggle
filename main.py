09d10f570cc451726e8dabc0301cd9d390c9785d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:30:07 +0000

    Pp understand aim add owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
