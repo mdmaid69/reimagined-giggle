a2e8e57eaca30a3e89daceaa0951d50850d6dc79
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:08:02 +0000

    swimming spread manner temperature sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_item(array, i):
-        return array[i]
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
