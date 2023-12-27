b85649b5329f30a9c490a2dc0d13f8bb4ca87c27
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:25:08 +0000

    wave workon ilovedicks protection pupil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_palindrome(s):
+        return s == s[::-1]
