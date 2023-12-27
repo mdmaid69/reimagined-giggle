c053a0305a80182cb5b2038fef100e7dc07ac18a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:25:35 +0000

    imagination play strange trap wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
