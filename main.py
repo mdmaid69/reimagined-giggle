e0a152dc1fdfdb10d0ec9e191458d9269fd849de
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:55:02 +0000

    accept difficult cinema mass right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
