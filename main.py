71c2ea90553188dbf4fc944fd3a48cbc581f2e10
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:15:31 +0000

    act unable calm keepfrom damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
