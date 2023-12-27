5b2f167dfc49352075c7dabf1293e69fbcc104c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:40:29 +0000

    crisis child extremely deliberate stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
