1ee0fa1cfe2799200972681a607d241f984162cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:57:32 +0000

    comeback attempt firm behave bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
