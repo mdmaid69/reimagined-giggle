7e077bbc1e85177ce01532758cbfde200cdc25fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:03:35 +0000

    follow bend to under harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
