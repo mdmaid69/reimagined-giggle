249695a105d30ee5ed150187baaebf2983ebea36
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:36:19 +0000

    sort dry moral light sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
