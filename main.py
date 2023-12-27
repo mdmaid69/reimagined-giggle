ca522c1cd29a37d428be72047d12a70edebce49b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:30:51 +0000

    brain insist major occur acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_list_from_array(array):
+        return array.tolist()
