319cdd78543509c2e20f864daab9ff79862624bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:14:53 +0000

    stir possible check try seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
