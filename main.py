ff3b1c21117068a1a55e5aa841d825541b6659e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:12:43 +0000

    surface little mark sentence undo

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def remove_duplicates(lst):
+        return list(set(lst))
