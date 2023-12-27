7f22a62e34772d4d57e577b64be9dafb02bb89b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:48:05 +0000

    top square sport recommend compete

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
