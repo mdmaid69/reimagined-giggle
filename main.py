f5821109cdeddeec17c7b9f95de4829e2c431452
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:00:05 +0000

    persuade at speed cry wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
