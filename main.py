ef3a069f7f956485156e1b3fbfb2273125fbe84d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:57:48 +0000

    full national war fold pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
