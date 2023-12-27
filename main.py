be24dfcdde0524a8f4cff32af6d0d75b4f322ee1
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:01:22 +0000

    trial everyone future minor kill

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_as_str(array):
-        return str(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
