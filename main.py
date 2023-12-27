f55499f471dbe5581e8dafae8fc2496160f8a1bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:37:36 +0000

    control politics there happy container

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
