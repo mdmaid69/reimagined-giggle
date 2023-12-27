1734604fb162b6d39b93da0d146c86c6a23a8155
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:10:33 +0000

    tomorrow getoff step drive tongue

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_pressure(force, area):
+        return force / area
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
