b3a32b0a5b85a639a562d571cdd9749dc338ee37
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:15:33 +0000

    then practical study fence himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
