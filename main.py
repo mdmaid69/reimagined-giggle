56017e24c05b0d79887e024e023a31387955daba
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:35:49 +0000

    middle never familiar state move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def append_to_array(array, item):
+        array.append(item)
