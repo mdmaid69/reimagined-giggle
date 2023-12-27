ce3d01222b12a5d5ada2b4c64d27bd9fa1aa0d08
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:18:16 +0000

    little disappear she period 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
