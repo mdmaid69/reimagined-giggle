3c327cabf35da4f0968776687f5ebe46c9aa401b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:39:35 +0000

    bringdown age check behaviour afraid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def reverse_list(lst):
+        return lst[::-1]
+import random
+print(random.randint(0, 100))
