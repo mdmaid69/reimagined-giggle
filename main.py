7d873d33ee286f220993d9219ded9c73dd3644ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:18:09 +0000

    mistake loan hold trick learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
