620abfb508ecdd8aa441f43ce83fb2f8c0c5b8ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:08:22 +0000

    plenty strict telephone cross while

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
