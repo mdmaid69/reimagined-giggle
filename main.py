0175f075c0c525fe8c0cc158713cc7eb18969fe5
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:59:37 +0000

    properly yesterday violent measure what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
