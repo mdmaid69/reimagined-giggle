74247fbd642b5af2f5547edbb4757742bb47cdb9
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:06:10 +0000

    mixture fortunately spill dull clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_octal(n):
+        return oct(n)
