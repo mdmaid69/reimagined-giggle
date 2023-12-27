e0334fe219e82128549fada29638be01a9f5e394
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:33:57 +0000

    post judge we embarrassing morning

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
