77579c102538c802602656e65fb23f2627eb0731
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:14:06 +0000

    talk favourite need aim cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
