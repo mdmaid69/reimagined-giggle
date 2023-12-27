a9535007742c3d8aee08ece54626ca74109b1e37
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:01:36 +0000

    line shock react use rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_current_directory():
+        return os.getcwd()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
