2090fa163cd7fa136407aafa608277a0bdf27834
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:13:51 +0000

    mail dry there taste never

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_as_bool(array):
+        return bool(array)
