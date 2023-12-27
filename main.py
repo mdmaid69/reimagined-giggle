7f5a08b81e886e04071e77704f570be9c8be2e90
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:07:55 +0000

    backwards live classroom backwards study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.ctime()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
