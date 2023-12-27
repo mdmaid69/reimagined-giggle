fe6a2ec143b2ae232c9f94bbe59a53a22d6fa358
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:12:55 +0000

    thorough recognize stamp angry swim

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
