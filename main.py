c2026ab74b69f27bdbb1cd361b4351f8dad6b010
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:24:50 +0000

    bridge squeeze whisper again calmly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_length(array):
-        return len(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
