ca1c9148a4cf2616f8e312a939ad55211cc362ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:57:21 +0000

    port yesterday turn burn the

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
