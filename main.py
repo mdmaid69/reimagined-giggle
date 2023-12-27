ee1f6ba0f7289ac25fbd5d137da3549269b94687
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:58:46 +0000

    physical seat mad convince second

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
