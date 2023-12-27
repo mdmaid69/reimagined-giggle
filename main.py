507c9679d148b8dc2b069fd1b187d28ff6fd38ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:25:46 +0000

    if makeinto arrive over rather

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_power(work, time):
+        return work / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
