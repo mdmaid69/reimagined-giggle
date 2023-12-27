ee0c0d1542c7e040050de6a14135d37d8ec20036
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:19:29 +0000

    be hit tend excited cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
