a8b0132228193fbf7f96fa458935febf98e5a093
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:40:30 +0000

    will ashamed normal place admire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def square_number(x):
+        return x**2
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
