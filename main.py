895518b7fb0fe11c954c9b60ade30f60a3bfd605
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:16:05 +0000

    rubbish keep difficult woman regularly

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import time
-print(time.time())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
