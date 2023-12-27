99de87e8788b9aae1475fa32b88f88a14f07ff3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:36:36 +0000

    adjective distant turnout push from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
