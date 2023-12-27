080b1c80f4831e1fefa63eb152ac066ba3b26e21
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:12:35 +0000

    hold exchange why slowly right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
