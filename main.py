4d48f5532c3abfbde58cde785019e12a8d9d8349
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:42:08 +0000

    connect mouth list shrink bunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
