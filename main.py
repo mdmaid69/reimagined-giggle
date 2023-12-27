990db56e7b9884d391789bc9fc5c876b7c626202
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:06:49 +0000

    like later commit someone slip

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
