653c96053912b425d5499842d06b9f11acb6dd31
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:18:00 +0000

    before home ability underneath question

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
