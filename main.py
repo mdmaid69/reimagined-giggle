12428d44e4d830ab843b21bfc7ab8608f50c00f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:31:50 +0000

    comefrom any keep lead skin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def count_elements(lst):
+        return len(lst)
