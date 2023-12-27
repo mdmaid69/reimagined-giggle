83422ddf28b1399b304a82106745c2315624472b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:31:06 +0000

    giveaway watch because squeeze uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def count_words(sentence):
+        return len(sentence.split())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
