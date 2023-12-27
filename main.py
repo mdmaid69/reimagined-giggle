59bed623a4abf29d104b16d51865d83bc01fabc9
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:56:56 +0000

    almost trial high nearly upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
