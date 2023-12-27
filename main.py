09c67ab16ecb99d300e6a9e2f1e8c6380d67cb2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:59:37 +0000

    fear commit dance bury inform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+print([x**2 for x in range(10)])
