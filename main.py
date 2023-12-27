af55ec747571b733a956a7fdbf6af350ecd4eceb
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:55:15 +0000

    prove any ball common pollution

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
