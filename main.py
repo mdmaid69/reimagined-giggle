2036b52cf920e453af44d66477b79e9b92a4b9a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:44:26 +0000

    hollow fill communicate nothing upset

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def is_palindrome(s):
+        return s == s[::-1]
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
