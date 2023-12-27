8468fa6b0b14ba68df9e2dc21b74ece1571670dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:56:56 +0000

    physical start soak circle zero

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
