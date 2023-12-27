7bae8e0c3705a9e1685c4e008fecd6b536e944ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:46:34 +0000

    cheek plenty fold separate open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
