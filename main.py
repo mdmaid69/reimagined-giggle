00011e4d592ee39aac8497a926f32410c913a530
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:10:47 +0000

    contrast towards tomorrow greeting wave

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+print([x**2 for x in range(10)])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
