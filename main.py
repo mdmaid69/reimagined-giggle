f214286c051ef149a7b9ecb063a2e974d35ca838
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:00:39 +0000

    both pick opposite window business

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
