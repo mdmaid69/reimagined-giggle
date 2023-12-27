25c5988e6da5ff33fe15b829308717e6f73e8cfd
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:08:58 +0000

    consist show fair normal successful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def cube_number(x):
-        return x**3
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
