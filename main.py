abd00bdf1db504ce111093e3082ad71bd17fd7a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:08:13 +0000

    degree report suit follow terrible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
