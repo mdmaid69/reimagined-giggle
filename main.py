264c2376b6a5f88268cfd5bcd09021a98c01d6ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:58:24 +0000

    union broad sure fix shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
