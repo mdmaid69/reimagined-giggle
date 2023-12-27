aa5c6f2fa671239cb7a53f21b8e0dffd61080ce9
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:49:04 +0000

    praise usually plan hard concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
