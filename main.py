ebd9fa706842a35b1f417b9f64e8433393e3ae8f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:55:00 +0000

    instant head Kk programme attitude

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+for i in range(10): print(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
