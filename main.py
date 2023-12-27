794e06f2b3bbff522be708535444aebc33a38d68
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:21:41 +0000

    cover only too survive interesting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
