09033efa3add08f9e3ddeaeb1b50bcd0125c9bf3
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:05:55 +0000

    let continue someone divide length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def greet(name):
+        print(f"Hello, {name}!")
