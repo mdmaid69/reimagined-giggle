cfcfc97178cd48d5bfa122c2681c14890c29035f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:05:52 +0000

    alive test completely clear need

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
