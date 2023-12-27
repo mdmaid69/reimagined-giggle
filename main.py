fb2cdbd5a93825d154d71a3819b104cd43f07e58
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:17:34 +0000

    fork coffee cross risk branch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
