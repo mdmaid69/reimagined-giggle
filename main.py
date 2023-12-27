477e1c80dfc6211701a0bad99d9f9ac0605b1f59
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:05:25 +0000

    away push train Pp frequent

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+print(sum(range(10)))
