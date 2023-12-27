4175d3874d70d0a553a278f6e94566bb49549843
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:19:19 +0000

    luck impression belief much environment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-from collections import Counter
-print(Counter("hello world"))
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
