7f29691a8ea37d9877fdff34ee9969a8d70da72d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:35:25 +0000

    guard whisper gate trip minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def multiply_numbers(x, y):
-        return x * y
+import time
+def get_current_time():
+        return time.time()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
