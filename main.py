d6f9aeac2f240fdcb3a7a2fae39dd760a0e7e0f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:56:19 +0000

    law ours sister sister quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import array
-def get_array_as_set(array):
-        return set(array)
-  def square_number(x):
-        return x**2
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
