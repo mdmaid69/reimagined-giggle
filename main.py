cd2bb75ac450c4ef8389bd5df89fdadafea7692c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:09:52 +0000

    corner murder world attach introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_float(array):
+        return float(array[0])
