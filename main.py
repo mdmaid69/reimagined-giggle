52096522dedd9f5dba250590026855f977a5a28f
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:20:31 +0000

    pocket minimum to meaning might

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
