3ae0dda6053955d3bff0a66b234bc5265ab79505
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:47:53 +0000

    hard talkinto work floor cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def create_user_string():
+        return collections.UserString()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
