aeadda035ed80d7c7b296fb514529aad4ca47a05
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:00:48 +0000

    future maybe convenient unpleasant fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
