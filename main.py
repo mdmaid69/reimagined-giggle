31b77e3737879f35c14b50725d44ea4ad0f7f248
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:50:18 +0000

    zero store loud injury instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_typecode(array):
+        return array.typecode
