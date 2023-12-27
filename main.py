a3bdd4eb0ca1d07a8bde4100dd6e5442bb236a80
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:34:38 +0000

    bunch pleasure ever turnback secret

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_string():
-        return collections.UserString()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
