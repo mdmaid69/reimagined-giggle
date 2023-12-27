dc748203521bab55afa5430ba5d0f907835b2d15
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:13:57 +0000

    air game copy careless child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def get_array_item(array, i):
+        return array[i]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
