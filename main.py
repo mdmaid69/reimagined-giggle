f1168522dcab5f6e185e533471a31f2875ff50fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:08 +0000

    rough mouth trial pull tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
