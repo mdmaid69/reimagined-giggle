aa81a1795ae4f99e285fb933682b67bec03d4257
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:33:24 +0000

    zero wing spring soak getoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
