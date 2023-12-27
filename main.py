a1d80a58031ad67fe51e9e20d10ff9294349b683
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:03:13 +0000

    in fill vanish before ride

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
