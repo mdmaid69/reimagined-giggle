9587770599432e50d3797f6a0e7379728ab048d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:17:48 +0000

    single cold damage brown should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def clear_array(array):
+        array *= 0
+def greet(name):
+        print(f"Hello, {name}!")
