e23df02c15ac0ef5897a8984a18fbcfd87a2594e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:24:13 +0000

    after sport feeling wide towards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def convert_to_binary(n):
+        return bin(n)
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
