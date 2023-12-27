608345243d4af89dcdb98351c4125cb763ff85c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:50:34 +0000

    both keepaway important relative keen

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
