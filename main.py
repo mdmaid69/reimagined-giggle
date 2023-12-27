e4d04a7cf018ef342117a43b4ef040cea920be6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:01:02 +0000

    disapprove stage discover ought cool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
