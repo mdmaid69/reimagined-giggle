553e21c3adc1e90f8ae364c31d3905d6b5125942
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:27:39 +0000

    anybody store in trade wide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+name = "Python"
+print("Hello,", name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
