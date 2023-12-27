b087595ce790046149030c1f69d066262a9485ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:10:02 +0000

    behaviour 570enjoyable squeeze attempt left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def divide_numbers(x, y):
+        return x / y
