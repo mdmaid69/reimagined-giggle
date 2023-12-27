85d0fbb0471bbb3480db572a3d1c4f9557cda47a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:52:22 +0000

    hello whichever pronounce slightly stupid

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
