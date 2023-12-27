b8bb5f1b58a85203375b5ede2839b7c8b87a5f87
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:20:03 +0000

    name average rough runover welcome

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
