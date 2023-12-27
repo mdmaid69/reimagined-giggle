e23302371fc800a5aed0abb9eb8a2c2447d612df
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:45:02 +0000

    age transport somehow whom quick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
