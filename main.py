41a2a088885b4b753f9f49c6755045d170468b67
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:58:23 +0000

    oil dress whichever usually perhaps

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import datetime
-print(datetime.datetime.now())
+def convert_to_binary(n):
+        return bin(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
