ff8df24ef599edf0f53e3c0fbc1e6954ee6de053
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:41:48 +0000

    delicate against happy proper write

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
