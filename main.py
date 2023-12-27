18f799afcaa786767a8bea625ff54967877f9367
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:00:24 +0000

    share reduce comeout come consult

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def add_numbers(x, y):
-        return x + y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_unique_words(sentence):
+        return set(sentence.split())
