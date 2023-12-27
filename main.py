9f4ec0f1e9d1375893fbb7d912985fd7cf4d9698
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:30:09 +0000

    yours interview talk extra combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
