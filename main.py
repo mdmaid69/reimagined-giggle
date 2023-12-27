dd171b1c1afb1023002e5e1a638a61da3bea8816
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:27:33 +0000

    person coat book favour passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import os
-print(os.getcwd())
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
