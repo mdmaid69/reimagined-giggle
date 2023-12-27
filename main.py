32c304b0005af7da6f1a1557353522670a07c89f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:52:18 +0000

    pollution sometimes Ee relation right

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
