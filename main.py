649c3ab7da10ea34e7180168433522ec62a35b40
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:50:01 +0000

    judge impressed relationship human score

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
