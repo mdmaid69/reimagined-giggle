9d24c1f37db077921855978f525d0b7da84e4bad
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:03:32 +0000

    child convenient right indeed bread

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_acceleration(speed, time):
+        return speed / time
