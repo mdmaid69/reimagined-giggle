afb3ca63746041c6adf429c63944aa6bb4cd7fb0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:28:45 +0000

    could behind capital proper into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import os
-def remove_directory(path):
-        os.rmdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
