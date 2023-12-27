6696a2e1c88f973a861e4f6d5ca481815a3a948f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:48:54 +0000

    shock steep keep 571enough post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
