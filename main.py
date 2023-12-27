ce36c4cef4dd9473b4386b3fc50989334560e6af
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:31:55 +0000

    pretty spot stamp proof judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
