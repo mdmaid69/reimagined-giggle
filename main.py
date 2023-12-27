cbbc99fece5831c995c4070fca273bb12b14f230
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:11:32 +0000

    thumb success poor buy runaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
