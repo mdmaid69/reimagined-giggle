4dbbffa5519420c4658d0c9a34315b22fb9c46f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:22:52 +0000

    takeoff pump plenty peaceful private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
