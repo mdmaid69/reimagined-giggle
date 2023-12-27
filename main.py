02dc619863d14b8e981f82e16f8f84e570eca1b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:44:57 +0000

    pick relief fire immediately deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
