6786c01cc4739f3822accc0c7a58a5579adb0228
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:39:01 +0000

    exaggerate myself natural family target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
