6c06951ad2bb19e789ea666f37e8c916f40677b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:42:13 +0000

    runinto bag disturbing minor anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
