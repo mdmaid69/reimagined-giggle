026c9ff45f95e0cea6d16f6539d76c554f135a86
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:22:10 +0000

    lucky bother pity we friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
