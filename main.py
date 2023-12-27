40aeb249a308aa321ad6c15e9f97263ca682786b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:53:39 +0000

    foot apparently judge phone shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
