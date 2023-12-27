924e1d8e1e0c3a668018b9a3626ce804a44a3628
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:08:27 +0000

    such Gg natural recover woman

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import sys
+  def get_python_version():
+        return sys.version
