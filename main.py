1cf95e73f9ea92ed871712fcdd3330c6399392a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:47:06 +0000

    obvious wash Yy hurt makeinto

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
