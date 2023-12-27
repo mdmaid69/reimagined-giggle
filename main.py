e85c2e539973d9b348f21fd049b160e64a109482
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:07:17 +0000

    aim fierce friendly moral base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
