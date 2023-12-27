1c34c38426d8ee30e6489dddb6aa577629414afd
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:47:12 +0000

    crowd rest sure mine perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
