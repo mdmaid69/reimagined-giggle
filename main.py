f5e0ba2865f3e4c13ae694f14d3ab94a1e17ea5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:56:25 +0000

    mark plain he wind shake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import sys
-print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
