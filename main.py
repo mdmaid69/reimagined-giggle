8854f43c9461b23bf61675daf46101b7e6a387ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:33:30 +0000

    breakout minimum morning tune rent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
