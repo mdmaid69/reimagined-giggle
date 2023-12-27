4e3968778f036535d6af83c390f2f1bd83b84940
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:37:06 +0000

    sail scheme company glad laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
