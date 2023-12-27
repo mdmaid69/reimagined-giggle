4794dcb24b690fe57d363531dfcfd3a108ec07c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:32:13 +0000

    lay stiff forget cause call

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
