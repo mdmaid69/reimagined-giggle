74f5576f91eb001e7b89581c267dd9fc8d3dd619
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:14:16 +0000

    delight anywayanyhow yourself field difficulty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_area_rectangle(l, w):
-        return l * w
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
