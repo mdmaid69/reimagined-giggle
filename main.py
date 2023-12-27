e221111aa3a81832d31fd37f257af4a92dc165b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:01:06 +0000

    punishment pinch floor above prepare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
