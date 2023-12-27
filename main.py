cbf88c96d193e5dddaed5b199cfbfc91411f7b32
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:30:54 +0000

    reject nature pause hers will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_octal(n):
+        return oct(n)
