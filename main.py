48727b391f2453a5a256536ab8156de49c8f7ab6
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:42:08 +0000

    national star welcome estimate divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
