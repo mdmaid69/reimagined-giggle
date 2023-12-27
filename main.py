d16b12920969cf925a0f2fbbe184f0d72fe0b9ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:11:27 +0000

    afford giveup club takeback substance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
