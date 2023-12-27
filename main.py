eee1d4f009618584eaea8a1389d1ce8416696664
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:43:29 +0000

    language laugh state now its

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_array_to_string(array):
+        return array.tostring()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
