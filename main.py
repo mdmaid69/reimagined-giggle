5a9c7422b29099136a346d2346ccb5ce96648bdd
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:45:07 +0000

    tradition burst nothing tool length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
