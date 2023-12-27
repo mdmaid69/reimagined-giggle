ab748c275858f5219a78d2dd7cf7b5d0f56a3b7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:01:43 +0000

    hair drive hurry soak mix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def convert_to_octal(n):
+        return oct(n)
+  def subtract_numbers(x, y):
+        return x - y
