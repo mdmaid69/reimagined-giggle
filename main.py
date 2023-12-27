3d28e843d4f44c77e2f52fce0886d5616d6fbbbe
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:41:47 +0000

    safety theirs comeround giveout holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_itemsize(array):
+        return array.itemsize
