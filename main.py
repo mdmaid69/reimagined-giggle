f17b2cac1c74a9b959e0b2137bff18825e2fee9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:59:02 +0000

    either cut peace field to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
