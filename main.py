caccb50eb6b72ac8e600143996d34c701ea4f533
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:34:10 +0000

    lay full range simple pride

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
