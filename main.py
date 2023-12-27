71e7b3c16b3d7c29df214a9417259f9378f71d93
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:59:45 +0000

    build worry hit treat stage

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
