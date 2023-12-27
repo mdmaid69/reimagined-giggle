c72d269160c1d46a878115695cb95d635b92ba36
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:43:09 +0000

    clearly medium either that possible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
