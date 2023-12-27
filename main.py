8f378aee26e1917dc37d8f89be542ad83c3283fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:07:23 +0000

    memory else male rule goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
