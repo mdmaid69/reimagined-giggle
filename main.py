5efa2adefca8dbce55f36eded4a67f655b578ab2
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:18:31 +0000

    emphasis with estimate major squash

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
