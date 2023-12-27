f2c050a6685c15256e94de99e0ca8be861578722
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:34:14 +0000

    about breakup zero light among

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
