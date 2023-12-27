f87dec3f52e5eadc337b58e21bf7571b70f9820c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:00:34 +0000

    win maybe above regret warning

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
