df0c9558575e71a95f655a1871bbc172fb4d9d7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:56:36 +0000

    report respect home since promise

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
