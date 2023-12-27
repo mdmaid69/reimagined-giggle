0e2bbcbc1b9a30fc40867ce1001392c4af5c2eb7
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:33:22 +0000

    address sense Yy offer bringdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import collections
+def create_counter():
+        return collections.Counter()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
