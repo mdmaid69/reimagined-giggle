2526c131a8bb05b919edfe332b87d57888a2b487
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:00:27 +0000

    strict coat tired church number

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
