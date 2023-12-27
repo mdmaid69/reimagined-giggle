0a12cc72f7d6af23786853c9fcb8c477dabd7468
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:47:01 +0000

    definite real force bottom look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
