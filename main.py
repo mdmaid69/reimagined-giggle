3d8329b89bb16074c723821dd033731db6a7f10c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:40:43 +0000

    city sort interesting play congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_typecode(array):
+        return array.typecode
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
