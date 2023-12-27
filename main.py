b28a3395ff1812e95df348112b929998b5ba5173
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:11:41 +0000

    perform sore out question cheap

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def multiply_numbers(x, y):
+        return x * y
