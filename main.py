28f19863e5f6e23ecae46e6fedf346be4f2c15c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:21:30 +0000

    wing replace design 572enter excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
