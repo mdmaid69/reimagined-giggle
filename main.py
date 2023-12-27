94753901d9600e52e9490ea63c861cfa0f4b7deb
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:29:27 +0000

    from advice dream because young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_pressure(force, area):
+        return force / area
