a63de46b05462a2a0fa1a605d977c3b3ce4fd5ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:46:31 +0000

    general half visit step possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
