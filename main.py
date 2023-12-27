2684c7f068d1df8ac31e91eefe29fedc1effb4c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:33:08 +0000

    notice takeoff shock collection table

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
