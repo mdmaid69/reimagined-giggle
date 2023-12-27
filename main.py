adcedbb530ae76d8eff015ea985b7d0f9c24281b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:43:40 +0000

    persuade comeround free apply noisy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
