a346f5f6e28d2f0fadb17f24854ef4103fdea2b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:38:20 +0000

    study stand lot sink sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
