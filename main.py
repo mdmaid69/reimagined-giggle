35f04e938a5e5abd9c339dfbee891c0a4b8077f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:11:05 +0000

    meeting luck suddenly brown pupil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
