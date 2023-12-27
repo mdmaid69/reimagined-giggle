dadfb96003257b95c0a83aa880a4d8287bf10ae4
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:41:48 +0000

    radio quality lookup protection night

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def divide_numbers(x, y):
-        return x / y
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
