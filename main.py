6870667c0f3666353706be79dfe5becb7279c261
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:25:38 +0000

    dry advice scrape like mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def subtract_numbers(x, y):
+        return x - y
