828c65e17ecb87bd3a543a8417155aaecf21d336
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:20:30 +0000

    acceptable himself guess themselves yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_string():
-        return collections.UserString()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_length(array):
+        return len(array)
