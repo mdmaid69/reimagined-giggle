2c9a5ca818c5cd43d92dc4dd9b6d74ed92499d35
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:21:48 +0000

    off hurry gift command think

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
