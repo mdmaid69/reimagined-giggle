db736f87111a8d86a19dc7be79026abad814d751
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:25 +0000

    instrument cook shut mind priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
