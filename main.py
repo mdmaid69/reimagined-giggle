e54a3cba0fb8b29eaa6e0e2f9f32e81a67e72b52
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:59:50 +0000

    control never glass entrance payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_item(array, i):
+        return array[i]
