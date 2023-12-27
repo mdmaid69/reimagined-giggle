69c15359a9362eabf3117756c6e0dc9504f7c27c
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:18:49 +0000

    whenever tell government past something

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_area_rectangle(l, w):
+        return l * w
