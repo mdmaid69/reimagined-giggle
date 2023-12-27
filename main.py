9dbb407b36061717b02f9571fee60b5e9196ced7
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:27:37 +0000

    through water once later walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
