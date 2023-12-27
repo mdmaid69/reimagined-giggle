3bbc02a83c2e233521d05b4cd7d2e9acfd5a2b94
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:38:45 +0000

    fasten can trade soak tough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
