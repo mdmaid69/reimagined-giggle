ab696186dd770bee845c68d8751012e0df65cc14
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:56:50 +0000

    fierce knock hurry manage introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
