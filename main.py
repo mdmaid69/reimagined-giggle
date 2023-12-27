595cebf58b01799bb0fae0a17d3beb59e407136e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:21:56 +0000

    bin distant unpleasant need well

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
