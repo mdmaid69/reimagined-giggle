cdaa2aeea55cab1a36b885d7cfa2ed7883e689d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:32:59 +0000

    yet team step bad suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        print(sys.version)
