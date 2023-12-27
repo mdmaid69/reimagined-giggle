df143f1fdaf2d42196115d5ec5264f88edcd28c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:22:13 +0000

    knowledge up consist off command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def subtract_numbers(x, y):
+        return x - y
