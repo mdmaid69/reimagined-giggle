3738dc7995217f6695c403a907e45aa7c5959758
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:10:50 +0000

    early hollow finally bread sweet

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print(sum(range(10)))
