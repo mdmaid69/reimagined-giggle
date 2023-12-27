fa24f27fb0a7f21766bb75a773e5ccba7ffb6300
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:10:13 +0000

    repair sad during warm start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
