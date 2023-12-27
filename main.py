44e7316ea2469bb4b0be1381659147e9394c6231
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:52:21 +0000

    rent home performance miss tap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
