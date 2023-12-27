22996cdc2295d2bd7e4a01308bdad2cd18d03f3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:03:43 +0000

    strike sleep get ring judge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
