4bf03b4178bb2bebe618222893cb377743fff179
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:41:11 +0000

    invite classroom generous entrance divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
