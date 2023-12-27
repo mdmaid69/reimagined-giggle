6f9c91fcebc0319a730e6135fb1c0fefbcfaee7c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:08:25 +0000

    appeal fit snow float active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
