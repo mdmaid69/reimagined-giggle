e45e9544061473b4e68a2c098dc0d9bb4a0b6af0
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:35:06 +0000

    carry sign either doctor shall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def convert_to_hex(n):
+        return hex(n)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
