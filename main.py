8c39f15d25304d60e42360b69578407881013549
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:03:05 +0000

    damage complicated nearly name develop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_current_directory():
+        return os.getcwd()
