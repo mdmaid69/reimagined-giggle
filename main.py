d78a4942f923347a30aa9963b3a8a13a4862fc78
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:23:26 +0000

    plenty fasten long history everywhere

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
