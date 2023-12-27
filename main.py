aa3d6022accae99c0dcf7cc78a39300b9823ac0e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:45:31 +0000

    deliberate experience without accidental foot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def add_numbers(x, y):
+        return x + y
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
