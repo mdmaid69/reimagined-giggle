2902378334d189e2d8e4178f3a756d1eac9d002d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:06:40 +0000

    line clearly should strike that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
