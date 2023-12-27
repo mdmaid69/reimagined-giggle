69c4f5f231e817af6642d206b5dc0fff8bfec97e
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:26:31 +0000

    quickly precise about single depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
