eaa95ad1ea38bae43811929e1270724d753c136c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:10:33 +0000

    at eager artificial value strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
