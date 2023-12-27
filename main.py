492c301ff0f95509afc9dbc0223a6cb62e513861
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:22:23 +0000

    nor name disturbing ocean tear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
