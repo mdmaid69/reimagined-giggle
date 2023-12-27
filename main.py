893968f8731baad799519b718be13fbd70457b56
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:34:15 +0000

    excellent cover plant own proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
