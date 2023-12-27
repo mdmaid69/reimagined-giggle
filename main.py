ffc48e8b66f178848ced5212895352ae037777b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:32:56 +0000

    one view tell variety deep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
