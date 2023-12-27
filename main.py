afa0da06d279b3d7ba71104e819351b031b8e0f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:30:03 +0000

    fence preparation for splash borrow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def find_unique_words(sentence):
-        return set(sentence.split())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
