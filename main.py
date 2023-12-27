e4cd864f50b94aca64faa2f786afa1dea59e45cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:50:47 +0000

    woman people estimate consist community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
