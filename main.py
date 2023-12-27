328dfb5357249b5acdb9e7a535b5f446507dba2a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:18:55 +0000

    interview strong downstairs less divide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
