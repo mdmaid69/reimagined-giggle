f2b89a127449a35d5f1d16d81fec1f48e900c4cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:03:40 +0000

    scared comfortable vote door move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
