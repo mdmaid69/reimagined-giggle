ef6327fc33d34520a363c2604d186f1f647f6482
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:41:34 +0000

    exactly day extra release song

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+x = 10
+y = 20
+print("Sum:", x + y)
