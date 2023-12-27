cd7ab26e7fd0301be3fadbc97ef7529f87ba3920
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:37:51 +0000

    deaf large say wrong far

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
