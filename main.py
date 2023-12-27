0607b75284e748137e20b86cd9801979e276d670
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:12:59 +0000

    rare seriously physical object block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
