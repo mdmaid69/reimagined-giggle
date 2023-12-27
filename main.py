c5a1f436754ff1da5192a18f2e80f1ceeacd95b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:03:45 +0000

    combine use normal impatient software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def square_number(x):
+        return x**2
