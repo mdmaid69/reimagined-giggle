108c13c7abacabff64b4cdcda7f93221da421bed
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:03:16 +0000

    brush fight over upstairs smoke

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
