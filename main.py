2c097234cf965a5d6cb180786a51128861a0e029
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:18:06 +0000

    zero probably no promise spill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def remove_directory(path):
+        os.rmdir(path)
