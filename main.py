ffbd75783918151ec213801d89292a8856df3862
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:01:17 +0000

    quantity public mind translate prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
