6af595fc3606322db62a418f3844190f05235f5f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:17:51 +0000

    constantly nearly exchange seem him

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
