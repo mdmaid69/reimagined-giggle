c83cd1365dcb6a1a0c34078021ebf32e0ac6d3f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:46:22 +0000

    together over violent holdon protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def exit_program():
-        sys.exit()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
