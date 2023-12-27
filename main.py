cac66b4242edd5bb350e36f4de1060cc60c16674
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:53:24 +0000

    unite run occur plenty some

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
