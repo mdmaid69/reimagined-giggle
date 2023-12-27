7dd46afb6e3db6760af670f91a0d8939b7d83125
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:38:22 +0000

    along bet mess professional near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import sys
-def exit_program():
-        sys.exit()
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
