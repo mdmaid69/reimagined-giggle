73dda1d41b86ecda4965421f2d3d58246561057f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:01:18 +0000

    trust Ll backwards soil purpose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def reverse_list(lst):
-        return lst[::-1]
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
