95dc77dfdf89de5beb9130dc4eed12316b979299
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:18:01 +0000

    press child pronunciation minority keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
