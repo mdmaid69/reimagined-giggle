ab1f637374826cf81fa28561ed52c63bd0e7aa1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:25:27 +0000

    naturally card empty grow book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
