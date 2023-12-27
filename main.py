0d8626bb8dab60861e03426ceb0e056834f19b1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:37:08 +0000

    action discussion on ready satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
