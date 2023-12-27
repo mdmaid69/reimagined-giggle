ad071ec24b5754d59a808423432696637b30621f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:37:14 +0000

    minimum spend lead ring related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
