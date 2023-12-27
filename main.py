f469450875b6c7ba965755f231c2d66ff3ef8b8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:38:53 +0000

    pain wind Pp spray pride

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import tensorflow as tf
-print(tf.__version__)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
