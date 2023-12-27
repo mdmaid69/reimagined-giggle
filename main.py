330c8da9f7646966af803459a2a71ea8de3d0546
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:15:44 +0000

    ask tooth bus feature left

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
