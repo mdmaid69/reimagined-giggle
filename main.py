2a3e3fe7bd3a93a78864a4d7f2c397e2d43bb897
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:33:28 +0000

    get guide plenty upset embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+print(os.getcwd())
+import tensorflow as tf
+print(tf.__version__)
