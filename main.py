9815d90e0d2830557f776fab23a63744ee81d2e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:54:58 +0000

    pinch rid attractive challenge getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def convert_to_json(data):
+        return json.dumps(data)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
