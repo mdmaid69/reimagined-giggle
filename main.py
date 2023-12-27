a9e987d89a78f19136b3d94524e0a54a433442f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:28:18 +0000

    instant gradually rescue sail callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
