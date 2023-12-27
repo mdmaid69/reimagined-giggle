92fd3d83bbded63bcdb4c7ef732759363099a44c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:15:26 +0000

    consult attach during connect follow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def square_number(x):
+        return x**2
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
