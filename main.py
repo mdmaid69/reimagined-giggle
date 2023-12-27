16d0977e80ae80abdfcddd6c343a9e036d247d1c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:26:27 +0000

    produce close innocent duty dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
