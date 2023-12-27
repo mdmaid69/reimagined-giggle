ec59e51e0c9121de38ab01b46115f889f2d55d77
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:44:25 +0000

    unfortunate light tonight remark punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
