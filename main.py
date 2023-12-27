548ed2bd60463f9e5af0188bf451d8bf03b951ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:08:36 +0000

    arm control matter interruption off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
