3c6bf33e5c48047fa80dc9d4078c1c03caf1b150
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:26:17 +0000

    pump geton safety flight remember

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
