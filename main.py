37bdf4a3e5a90408ed39d06030744346d05ed98e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:24:39 +0000

    improve beat coat because increase

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print(sum(range(10)))
