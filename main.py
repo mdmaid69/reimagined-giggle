50779ab75a01230629087d9f52ee66761547694e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:35:48 +0000

    square sail the actual urgent

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
