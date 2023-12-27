df8d872cdbb822361ca50c8d1e007313e6cff833
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:35:19 +0000

    goout but reach height course

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def add_numbers(a, b):
+        return a + b
+import re
+print(re.match("h.*o", "hello world"))
