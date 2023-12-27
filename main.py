f8d8929786b296ec1d8b112730039cc6b9c3017f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:43:41 +0000

    mad keepout curious not on

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
