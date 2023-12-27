9dd2d1f7e747bdb27fddd7218500d2a91b9ba5fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:34:05 +0000

    continue emotion thumb try university

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
