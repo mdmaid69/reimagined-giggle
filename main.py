5179f46b33e652d7eda912b4411bd67c1efb6c68
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:07:55 +0000

    completely pick top hope general

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
