5b2d0008dd931fddaf098ece81639ee1b80c33d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:40 +0000

    through hold month persuade getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
