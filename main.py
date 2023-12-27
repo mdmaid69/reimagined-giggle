5543f322ea3f6f3af7dcba57d340c234380ebbb1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:01:11 +0000

    completely murder estimate could remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
