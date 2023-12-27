5ddca75a4b5a8a9071d55e75d54d6d69024acb13
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:23:38 +0000

    present affect sensible yes excellent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
