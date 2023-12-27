faee3471c561c75461c98ead1a2f9a78a7e3973d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:46:21 +0000

    hurry someone necessary offence creature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def greet(name):
-        print(f"Hello, {name}!")
+x = 10
+y = 20
+print("Sum:", x + y)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
