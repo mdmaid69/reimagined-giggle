61b3834ae3a981a0e8b041de8531f8bf05c8b15f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:07:17 +0000

    their persuade strike store hear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print("Hello, world!")
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import numpy as np
+print(np.array([1, 2, 3]))
