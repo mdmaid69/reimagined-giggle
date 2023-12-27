9672bde3baedf357d19dcd7d9278cf83e1bc8a5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:01:14 +0000

    on left strict environment rough

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
