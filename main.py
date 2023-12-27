b29ee8e2d5bb1cdf7f8a180f531eb0b19d2afaf5
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:42:21 +0000

    dry finish neat workon spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
