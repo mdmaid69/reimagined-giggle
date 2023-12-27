88c39971ad886238f99fe3b4eceb5582b71f7a89
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:18:51 +0000

    path pin will accordingto ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def clear_array(array):
+        array *= 0
