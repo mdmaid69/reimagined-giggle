d0601e8c63a10cbb10d61a9bd9d41df3a5d40430
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:01:14 +0000

    hang regular thing cheek responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
