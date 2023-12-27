e88ce9070092b1da2b3f7e42b3617d49e0701832
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:36:30 +0000

    tour bar present rate cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
