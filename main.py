281289c4b66cf55395197ca3c00233c2974331ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:15:10 +0000

    attention block message block business

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
