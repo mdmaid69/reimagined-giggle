ece3cd4aae57b02283ea9953e2b1582f394394d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:43:17 +0000

    gooff young last forget search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
