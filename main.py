e43423b130446a0548e9bb7c704f508e47b30e79
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:59:17 +0000

    splash bright geton important base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
