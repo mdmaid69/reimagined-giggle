dfeb11cec4a72fc5acddf78a303a15d13d928e92
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:16:05 +0000

    opinion dream comfortable characteristic poison

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
