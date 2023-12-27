b5d8f9e297d421a78be77ace61272001d5cb482b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:17:54 +0000

    unlikely why adjective lucky disagree

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
