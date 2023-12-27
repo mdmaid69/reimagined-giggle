0ec1cb071ab2ee34e15e1349af8a69eb17b8c6ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:21:02 +0000

    announce image Oo boring spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
