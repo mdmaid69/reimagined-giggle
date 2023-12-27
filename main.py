87ecabeee040ecf532a5278274ba5c1050bd9e1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:18:45 +0000

    wall butter completely study peaceful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(5):
-        print(i)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
