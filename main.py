15db99237ba4b21cffd64d3c90239f4eebd30dad
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:14:56 +0000

    hang bank disturbing pain adjective

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def sort_list(lst):
-        return sorted(lst)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
