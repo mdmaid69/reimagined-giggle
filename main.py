5fbce1c3a673e15dc797fec0e0c583c54f43b85c
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:15:13 +0000

    project seriously make operation muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
