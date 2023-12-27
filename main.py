b0adc5f8bf6bff1976dd20ba3c63c4b974797aae
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:54:00 +0000

    trade print purpose happy invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
