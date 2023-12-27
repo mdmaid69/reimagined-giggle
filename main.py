9f556c539b1dc45991073820cc2cd9f6770a72e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:06:55 +0000

    compare measure boat sign action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_today_date():
+        return datetime.date.today()
