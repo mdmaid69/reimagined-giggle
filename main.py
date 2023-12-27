e805efc9022d0e572d974e40eb9f88c8afc9a310
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:25:01 +0000

    mine approval pay care compete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def is_even(n):
-        return n % 2 == 0
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
