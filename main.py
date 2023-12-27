3dd7f52a8ad78cc17d7ff823293416fd7031bb45
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:04:52 +0000

    season birth rely deserve holdout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import time
-print(time.time())
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def reverse_list(lst):
+        return lst[::-1]
