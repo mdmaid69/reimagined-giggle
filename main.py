b5fb8d5cec6dfa4f7663f0bc6299767bf39e83b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:37:39 +0000

    correct comeout splash think label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def cube_number(x):
+        return x**3
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
