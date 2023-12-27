0c5ff41c7b57564b59d0a0dff82a92426705b201
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:52:13 +0000

    ring press plain temperature definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
