96f0aefc36b0f77598cb4b7875efcebcc9c787a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:20:06 +0000

    prevent action nigga wide mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
