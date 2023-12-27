dcf237fcce5d0bd896bf88bd4589b37ff7828ede
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:16:37 +0000

    rent early any window beginning

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+print("Hello, world!")
