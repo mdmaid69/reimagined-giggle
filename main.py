d9285c9d56501b12762d688539be9b19cc0a50d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:01:15 +0000

    straight impressive familiar prepare interested

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print("Hello, world!")
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
