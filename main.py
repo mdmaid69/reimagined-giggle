7aecf6b1f31e53c392f612c7791f1761ba573190
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:38:37 +0000

    keen Itself begin print basis

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
