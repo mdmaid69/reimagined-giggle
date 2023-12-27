f29a311ba2fda2b736bf5c5175e4e1a82033d85b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:33:14 +0000

    cruel while suit healthy ordinary

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def multiply_numbers(x, y):
+        return x * y
