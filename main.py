87c74f1c0eca45764d2a49f0a8b5ebfda19ae1dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:52:07 +0000

    respect kill second typical neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-print(datetime.datetime.now())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
