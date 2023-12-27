9f24f80b73c61a17eb58491cf134725b4703158d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:51:05 +0000

    wipe union world comeover value

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
