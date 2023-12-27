6c7111e111fc0ef9d8b89bc0d92473a647ed826b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:37:16 +0000

    think trick advice nice second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_index(array, item):
+        return array.index(item)
