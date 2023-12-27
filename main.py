3f6cf511748854310066743faf73ff5a36493967
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:04:13 +0000

    worried undo complete strike Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def convert_to_octal(n):
+        return oct(n)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
