0012be458a7d19cc134d42c6c1c34c3917873172
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:31:59 +0000

    play than mistake comefrom dull

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
