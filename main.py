d39270a8ffe66ee7e94c5eaf74fdaa5472451e70
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:53:41 +0000

    chance very mass everything tell

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import random
+print(random.randint(0, 100))
