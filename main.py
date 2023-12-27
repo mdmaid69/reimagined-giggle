fd335fca510fb806d781bb36b615cfb6fb8caa1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:12:28 +0000

    upwards fork stop length collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
