6ba6c78eb431ae03b6db2cfe6936df73b2329170
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:15:21 +0000

    extraordinary memory join juice weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
