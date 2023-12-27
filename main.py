5cb72c6ca9aedaa38a2061edf7cd2afefeb6d660
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:38:48 +0000

    light answer takeback terrible mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
