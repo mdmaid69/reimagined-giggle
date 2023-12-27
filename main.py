8bd06bb7bdea806e31c498051ed0149498e2ea0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:06:37 +0000

    jump begin stick scare past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
