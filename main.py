f07149ce65139cc2d52fc17ac23efe281df3dcbb
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:50:00 +0000

    content opinion fix direct meal

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def remove_duplicates(lst):
+        return list(set(lst))
