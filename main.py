3dd3f1c4bc5ee61a2b7ac130c4f8ceb53911957f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:50:53 +0000

    scrap square but each shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_as_str(array):
+        return str(array)
 import http.client
 conn = http.client.HTTPSConnection("www.python.org")
 conn.request("GET", "/")
