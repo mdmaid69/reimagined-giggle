5eab8425b60f3e92ecf6abc3b8708afbe8127d92
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:57:04 +0000

    mass strong copy believe punish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
