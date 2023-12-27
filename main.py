55aeb19a4e483b3f0fa5a5c4b55ccbb5dd1b098c
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:27:25 +0000

    manage thought support twist win

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def convert_to_octal(n):
+        return oct(n)
