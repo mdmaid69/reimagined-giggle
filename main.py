adb442aee3961b1ed5561d279b1db9e1a0927195
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:52:51 +0000

    national avoid probably frighten really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
