b388f3af715dddd85e5b3b4d04af7dd53e2daad1
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:57:55 +0000

    inquire away glad consist geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
