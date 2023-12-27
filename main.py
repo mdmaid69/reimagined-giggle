ad5c2c692022401216fd60ca002c345712770ea0
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:06:07 +0000

    better motion new scheme favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_float(array):
+        return float(array[0])
