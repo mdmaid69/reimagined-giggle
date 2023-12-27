5c5ef97cbcd53fc7b8c75c1191cc85bdd896a660
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:22:44 +0000

    fork order necessary takeover bush

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-for i in range(10): print(i)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_floor(x):
+        return math.floor(x)
