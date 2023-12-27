a3e9eaf2338c9d4cc56593a8abc4050d01cdd3a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:37:14 +0000

    various delight command workon immediately

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def multiply_numbers(x, y):
-        return x * y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
