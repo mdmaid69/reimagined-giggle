7c3b7c37e5cb9e435c6727e3106fa1465ffd7048
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:28:54 +0000

    trade shelter generous smell quarter

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
