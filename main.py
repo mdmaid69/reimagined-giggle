03e508f7ba26f18e2aa222977513981604243064
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:03:07 +0000

    television organization precise major left

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
