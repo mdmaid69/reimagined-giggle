caa753a35077476c1534abdf70cdb004da658480
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:35:57 +0000

    bright never reduction hear block

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
