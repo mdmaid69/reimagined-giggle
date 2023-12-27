8f79e29d8b632c50b48e276fdbb0fccaa30e8125
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:41:12 +0000

    road only use extra yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
