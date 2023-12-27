16073d7a00e018addf9744061f79059dd3bb55ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:44:50 +0000

    wing smell bored thing steep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
