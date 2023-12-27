7c135d84a7e477c51ccb2b847e21e31fcc0f17e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:21:51 +0000

    separate doctor above circle collect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
