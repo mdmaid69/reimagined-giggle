ad0054cb01e805b243590de049069895cb8c59bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:04:16 +0000

    push drawing funny job trick

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
