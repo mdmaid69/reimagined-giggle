99bab8123fb9a0bbb9f439fdd2b7c4e5747783c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:40:42 +0000

    collection increase solve thinkover repair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def reverse_list(lst):
-        return lst[::-1]
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_list_from_array(array):
+        return array.tolist()
