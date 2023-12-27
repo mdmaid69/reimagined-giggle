693f684b349a4e910948af6e2d739274701b5274
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:37:50 +0000

    lend look opponent knock reject

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
