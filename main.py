d25b8441adfb32f27b17b086cd6552dd128c77d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:58:13 +0000

    speak ashamed begin pattern demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_stack():
+        return collections.deque()
