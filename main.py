5da30ff347d73181fa673889dd02c72ab2d1bb0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:14:43 +0000

    match block discuss too lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_area_rectangle(l, w):
-        return l * w
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
