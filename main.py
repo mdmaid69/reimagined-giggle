ad68333899bbd51786752a4ddec1a8042227bf93
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:51:15 +0000

    connected mystery Ll somebody exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def is_odd(n):
+        return n % 2 != 0
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
