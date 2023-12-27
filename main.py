bac54b7d2c56434afd2dd7b77f97327cd15c1c18
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:01:34 +0000

    of tense search recent this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_error_function(x):
+        return math.erf(x)
