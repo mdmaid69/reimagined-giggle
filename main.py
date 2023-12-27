420bdcecd2da33f85ac420555a21f6030118167b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:21:17 +0000

    important shall tragedy wide different

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
