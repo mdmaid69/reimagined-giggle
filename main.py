833618bc620bee65e160d17cbf226e605b91b628
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:39:41 +0000

    flow ring tend concerned wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
