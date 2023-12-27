004d9f29a482436e34882b2996c86628093cd78c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:07:20 +0000

    identify side independent spite opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
