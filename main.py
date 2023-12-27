49b5debc1616f4c9a3836d0cfb3966b123c3f97b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:07:47 +0000

    introduce please left furniture city

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
