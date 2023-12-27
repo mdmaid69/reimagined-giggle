5c1db13760038365f3207dbe7743ed3f0857fe5f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:13:20 +0000

    murder afraid frightened callout spoil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
