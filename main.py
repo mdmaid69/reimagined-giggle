427415259393a05c718bd7a29659baa602908056
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:49:41 +0000

    waste impressive shoot workout nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
