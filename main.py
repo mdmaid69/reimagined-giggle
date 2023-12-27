bf28664015b7b53e9ccfaed14e05d65d1ccc6588
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:27:44 +0000

    hand mouth interesting smooth matter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def square_number(x):
+        return x**2
