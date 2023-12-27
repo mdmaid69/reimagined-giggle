7077260d9000110764b501bc03c70f0a79cd0b09
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:01:21 +0000

    half share punish past clean

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
