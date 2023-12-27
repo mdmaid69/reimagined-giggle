489d2bdc7c70cede7f989bdb96f81c9217a735ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:16:33 +0000

    too familiar mother fail Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_current_directory():
+        return os.getcwd()
