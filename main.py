3fdea6d1d5159b3a9e26216ab756e5475cccc81d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:33:19 +0000

    business book clean invite independent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def remove_duplicates(lst):
+        return list(set(lst))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
