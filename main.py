0fb1602d71b6e936d6fa215b13905a2be5db74ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:57:19 +0000

    unpleasant attempt cook forgive relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
