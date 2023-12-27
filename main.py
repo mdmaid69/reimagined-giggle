af3fee1c2cb8434d4a96d0f2b45f6a1a0534e8d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:50:52 +0000

    modern war business woman effect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
