faba98e7bf15124c834dbd9ed5db5db156a86132
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:40:08 +0000

    instant immediately describe past pin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
