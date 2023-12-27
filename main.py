1cf50bc437b9eec43e0702c024ede06945dc2758
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:09:38 +0000

    lose escape complain bin muscle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_repr(array):
+        return repr(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
