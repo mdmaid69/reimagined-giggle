30759a368825c10c4c14bf8cc39bf596d363fd2a
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:39:47 +0000

    rise return brown subject class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_current_directory():
+        return os.getcwd()
