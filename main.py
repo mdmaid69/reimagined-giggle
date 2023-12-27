597533cc397c0cfda2f0773c9ba3223cd778ce0f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:12:30 +0000

    completely little harm furniture badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
