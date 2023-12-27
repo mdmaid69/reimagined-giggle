fbaf3a413a89f66398c4984058af30e1ea47edc0
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:50:57 +0000

    write no with other 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def reverse_array(array):
-        array.reverse()
+def append_to_array(array, item):
+        array.append(item)
