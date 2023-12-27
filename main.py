ea96478b37590c90f3ed94a830a837ebc18a19dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:55:26 +0000

    mixture search polite laugh cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
