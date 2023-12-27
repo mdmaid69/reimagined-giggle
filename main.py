04246507944ae3bc35a2db4a722e9217abd9ba8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:12:40 +0000

    breakthrough expression lookafter separate bye

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def multiply_numbers(x, y):
-        return x * y
+  def convert_to_binary(n):
+        return bin(n)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
