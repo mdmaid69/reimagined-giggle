225a094be1bb83a487245c9a52019f65cc9ee867
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:47:53 +0000

    design present score brush delicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
