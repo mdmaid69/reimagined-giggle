c3fa57d7b8c245fab26706837204c917e5275af7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:57:38 +0000

    frame which with distant offer

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
