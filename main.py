01869f7da24861b13fcdf3fc00e8a8628844a034
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:21:19 +0000

    impatient limit complete base proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
