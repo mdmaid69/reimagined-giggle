4b6ef998655ba504d9a625a6b4bf45774ece0680
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:12:24 +0000

    responsible strict month disappointed me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
