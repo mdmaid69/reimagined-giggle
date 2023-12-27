eda9031903515184dc140a5c47f7ad5b98fe8010
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:16:46 +0000

    successful pass promise interrupt open

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_set(array):
+        return set(array)
