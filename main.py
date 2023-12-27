83e7b4d2a3435821cf1b85043476ee8416a54e8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:07:31 +0000

    weekend Ff raw smile friendly

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
