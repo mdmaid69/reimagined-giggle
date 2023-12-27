09fbeadf7d372ed421a074964f4873ef8a91678b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:58:20 +0000

    swim left talkoutof narrow payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
