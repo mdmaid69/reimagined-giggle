142fc3466aedd7a0042d3998420712aec7c5b847
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:33:23 +0000

    any song together comein mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
+import datetime
+def get_today_date():
+        return datetime.date.today()
