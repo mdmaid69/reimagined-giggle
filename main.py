9347c476d08b5c55437c07c0d7a12f95571d9d35
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:43:43 +0000

    towards couple regular promise buy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def clear_array(array):
+        array *= 0
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
