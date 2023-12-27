44e9489a368b97a3b2d07a254caed727bc278c03
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:26:54 +0000

    straight class thorough unlikely neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
