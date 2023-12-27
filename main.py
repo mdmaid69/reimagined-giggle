f8f6cfaa202d1542c641a04c6d728b0c440553f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:21:31 +0000

    rain sink nose deep upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
