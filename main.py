962c773c0a757d049d8a4dd81427241160ed4b6b
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:20:00 +0000

    precise rush private ring exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
