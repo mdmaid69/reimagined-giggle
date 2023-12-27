4f2394775b931c9c5e520bedb4694f1fc823efef
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:06:09 +0000

    hospital uncertain rarely always memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
