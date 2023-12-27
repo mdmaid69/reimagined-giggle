c0885d625213bbc131e90443c39c082cae636c4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:30:18 +0000

    maximum clean blue runout bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
