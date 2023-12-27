d611d9444ab605b6dc4b58aa071a077795e7b929
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:44:35 +0000

    hand inform comment conduct little

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
