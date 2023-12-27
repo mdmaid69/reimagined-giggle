b575b3bb6a96515358e3acfa8cbd9eab3412798d
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:42:10 +0000

    name imagine profit from store

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def convert_to_hex(n):
+        return hex(n)
