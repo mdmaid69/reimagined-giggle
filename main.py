dd4c7226db1f7e25d802b2871b8e4867bd788cae
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:00:47 +0000

    doubt dark room season recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
