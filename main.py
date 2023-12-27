cd5bdc92d775d53f7ee136909aba34f26f3fc4b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:04:59 +0000

    after throw forever my emotion

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
