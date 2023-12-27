26b6f006efce8a379c6b4df9e643860f8cec0022
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:29:06 +0000

    middle maybe habit game unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_error_function(x):
+        return math.erf(x)
