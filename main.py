18d0c1727e3dcf99bd0ea362e8e97fcba22711f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:47:23 +0000

    production rescue friendly underneath attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
