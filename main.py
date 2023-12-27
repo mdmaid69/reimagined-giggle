7ae07ba808be9c8d857472927c301516f85d9ee1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:37:26 +0000

    Kk lock wonder they stamp

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
