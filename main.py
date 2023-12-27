f4181dc2f60861397b6ec2f0635d2463cf9415bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:05:09 +0000

    Nn bury look production evening

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
