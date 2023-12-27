daf81300d3689db02e0e917812052e3c6ae50dd7
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:04:12 +0000

    bright arrangement meet pencil activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
