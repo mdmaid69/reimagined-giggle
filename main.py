ff57c95e85fa2b2bf7df1a8fc306c3fa9c9b06b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:02:23 +0000

    share phone block display makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def greet(name):
+        print(f"Hello, {name}!")
