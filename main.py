29ad0b446328f35829438b1d2600dbf89af8d6da
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:46:49 +0000

    squash star holdup cry relationship

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def reverse_list(lst):
+        return lst[::-1]
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
