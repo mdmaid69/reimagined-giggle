bd92f43a9d6a3940b853fd191f599360a870c18e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:10:48 +0000

    release top meet wide understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
