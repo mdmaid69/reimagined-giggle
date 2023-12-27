b202b15d616776f8e9a93c304a93bcc784a84ad3
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:18:39 +0000

    operation course tomorrow comefrom just

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_min(lst):
+        return min(lst)
