53b870c0c117ebac3841dfea5a8bfd4476d0913a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:11:45 +0000

    couple climb pot very well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
