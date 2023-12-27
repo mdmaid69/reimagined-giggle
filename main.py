b72ffe5acb11a819235c46a8725fc0b6078b68f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:32:35 +0000

    announce press class ill where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
