71e381b306fff81fa5c52503baafb539c1982d63
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:26:12 +0000

    rarely week tip reach open

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
