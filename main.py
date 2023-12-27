61a0e8424b36eb0284bb67a21b8c4e9445a20da1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:18:31 +0000

    meanwhile project announce frame petrol

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def subtract_numbers(x, y):
-        return x - y
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
