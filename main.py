792b409ab7a7b6ea84cfcefdac7b8cf07fcfec1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:58:45 +0000

    test exchange exit be Dd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
