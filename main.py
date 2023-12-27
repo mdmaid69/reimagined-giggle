0026f598b155fd4a6226996c47eae82e2a8b7891
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:32:49 +0000

    ought hope pinch bright degree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
