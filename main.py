1a33c559dc7ea5ea395f586a66a3967e9fb06464
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:38:31 +0000

    circle be ill correct suddenly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-from collections import Counter
-print(Counter("hello world"))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def add_numbers(x, y):
+        return x + y
