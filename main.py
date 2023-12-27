ed012044289f8ab075c536bbf63b1dd188a0e113
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:11:34 +0000

    copy method favour everyone allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def subtract_numbers(x, y):
-        return x - y
+  def add_numbers(x, y):
+        return x + y
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
