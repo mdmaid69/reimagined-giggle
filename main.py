bff5aa7ac5fe4c6c57027b70693fb779a372b4ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:37:34 +0000

    by blame night quiet pity

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
