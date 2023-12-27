22eba484ced7b2426625a95ea42af98434f46e15
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:26:35 +0000

    anywayanyhow else 569enjoy careful unable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_repr(array):
+        return repr(array)
