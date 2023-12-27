7d9c323425008550d06edaa0b89e0ab9b80526df
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:38:08 +0000

    do protect plain poison state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
