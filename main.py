b79e1864f89a155de86eb18d334c624a99375ded
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:31:55 +0000

    importance score before forget confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
