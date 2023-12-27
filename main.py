0856ec1be6a6c325d077e21b9bf1752e3cb06091
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:21:54 +0000

    announce path fasten object period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
