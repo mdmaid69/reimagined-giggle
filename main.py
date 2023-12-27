4ac7e543e17f8775c1801ef1c82cae59bac0fb2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:23:56 +0000

    scared weather swimming news firm

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def remove_duplicates(lst):
+        return list(set(lst))
