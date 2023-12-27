24e73ddc86e1c9e4ec7bf04cf73aaf4935d2ddca
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:07:01 +0000

    occasion broken everywhere bus thinkof

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_max(numbers):
+        return max(numbers)
+import array
+def append_to_array(array, item):
+        array.append(item)
