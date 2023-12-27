5e766200635692d0ff8aa713eff019312a8ee72e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:20:35 +0000

    opposite miss comeover 567enemy comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_item(array, i):
+        return array[i]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
