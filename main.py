7302e52bf79af870033e3bc57f6eab109dc87467
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:16:24 +0000

    reserve great here serve confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
