ebb4a4122c8193f3e4bc8e2dae8fa39fc069b7a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:34:27 +0000

    bored tape deal rarely neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
