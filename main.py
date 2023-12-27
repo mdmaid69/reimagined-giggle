6df324600149ed7fc0c767146e53b9c248f48044
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:57:11 +0000

    rush theirs 571enough perfect in

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
