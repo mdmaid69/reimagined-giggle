219a832358957b38487c8f5903b34484e56c8da6
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:31:26 +0000

    port habit smell today principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
