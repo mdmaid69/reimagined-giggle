18fb777d9953c5d704fcef472a5267b8568377f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:26:29 +0000

    tap nice ashamed snow afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
