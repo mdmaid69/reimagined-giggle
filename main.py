e5d2a361651cbde8076b13143d340ef3668b79f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:57:41 +0000

    some protection stupid bite lead

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def is_palindrome(s):
+        return s == s[::-1]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
