1f3f4af97ed6b9b4e33ddeec6f442af91f6e37e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:34:30 +0000

    key service accept before final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
