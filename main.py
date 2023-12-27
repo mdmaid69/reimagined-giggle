e7637717b43cbfa911f97b39741c0af8a026e8ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:40:40 +0000

    precise exercise television limit little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
