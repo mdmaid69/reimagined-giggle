e5625f723f6b2b3ef846f52832938201a37bfab0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:54:26 +0000

    niggaacademy correct hard dinner clue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_repr(array):
+        return repr(array)
