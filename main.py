24c66d5a148ca33013cbe779d14964bba6294a2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:33:37 +0000

    over surprising depend free forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
