f8f0f8532ca0b0ae755e5d48f2d26139f9ab92e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:35:43 +0000

    wait 567enemy many needle late

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
