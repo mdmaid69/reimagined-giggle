187c81e6bd561b11604a10a5695e2c7ed4f4c0e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:41:31 +0000

    dare 569enjoy healthy trip cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
