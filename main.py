53ede7d17e48dd8b46d3da94b434b461dbe9ab71
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:22:15 +0000

    score gentle new contact control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
