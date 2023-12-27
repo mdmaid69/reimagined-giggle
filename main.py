1781a465980df84e12e171dbf94f350dd2862bd2
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:33:37 +0000

    purpose people problem reaction compete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+for i in range(10): print(i)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
