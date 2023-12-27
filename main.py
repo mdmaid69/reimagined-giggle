5f91f8748c9eb4966320d5c29ff3d8441b01fb7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:51:52 +0000

    space better turnback steam target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
