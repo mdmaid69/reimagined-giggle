e23d40d9d649acbd623123ea002f454219e6fdde
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:51:12 +0000

    duty kill manner wheel pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
+import logging
+def log_message(message):
+        logging.info(message)
