a984f7991ef5710d9dd81140a7bfa29bf0229980
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:53:11 +0000

    talkback dare wash notice kill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
