8aae82401acc37c495e88eff7a2c3a8cd112f21c
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:59:33 +0000

    warn carry just trade still

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def add_numbers(x, y):
+        return x + y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
