66073bcd54f7647b0247d008fe9838cd5339ffa4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:38:55 +0000

    nose while original spite storm

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
