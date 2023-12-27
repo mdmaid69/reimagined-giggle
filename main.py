515498e9afaa1c53db45797011802cb19849f25a
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:10:17 +0000

    occasion seat state wash towards

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def remove_duplicates(lst):
+        return list(set(lst))
