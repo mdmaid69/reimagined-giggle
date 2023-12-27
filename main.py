eb77b9eaf88acfa52035512251e0bc142a668995
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:45:44 +0000

    about stretch pass around party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
