e7f1240893c7a050129c47e4d7ba2645a06337b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:34:33 +0000

    undo to suck importance question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
