a00e7eebb07c7157f91fb02554298a781561f5d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:43:59 +0000

    official sad attention drink attract

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
