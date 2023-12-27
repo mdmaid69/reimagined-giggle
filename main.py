ced638ddaab732529a1eec9371f54b8e2babdd2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:04:35 +0000

    satisfactory several ready square feeling

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
