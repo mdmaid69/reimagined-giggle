54f661f16dc64e4ba8cccc8c09a6ae2917b7bd65
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:54:34 +0000

    shop suit spring ashamed deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_min(lst):
+        return min(lst)
