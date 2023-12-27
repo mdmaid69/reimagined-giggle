9366ab7b11fe70cde47ee19537c1737cd17dd0f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:02:15 +0000

    muscle bang normal stand claim

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
