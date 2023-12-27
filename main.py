473baf3b8a9e196aaeba2f59922a9ab2242583e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:14:32 +0000

    live special official spoil murder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_int(array):
+        return int(array[0])
