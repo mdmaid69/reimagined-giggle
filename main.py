5152465b87039b08e5e37879b861a21ce11ca9e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:13:19 +0000

    comeover conduct combine down meet

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def count_elements(lst):
+        return len(lst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
