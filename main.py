3e4e270735cf697878ad95f274371574e9654148
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:39:46 +0000

    thin accidentally sink silent terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def print_python_version():
-        return sys.version
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
