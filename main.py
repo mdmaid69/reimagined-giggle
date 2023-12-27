278e374ac60761834b83eedfa100351b3c9b673c
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:26:08 +0000

    roof laugh easy correct tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
