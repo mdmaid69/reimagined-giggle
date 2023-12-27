c3c79ad0fdfd63ce6b46c8a86495c30b1fa08a9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:54:10 +0000

    question police shame ear provide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print("Hello, world!")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
