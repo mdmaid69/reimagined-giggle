4fb3d6b03d0393f8c28b4f6b7df772d1c3343c4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:43:37 +0000

    obvious land train rarely although

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
