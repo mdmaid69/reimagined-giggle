c18392ed1646fbc3ca2d4dd256844dadf4e46643
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:27:42 +0000

    run prize bored loan project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
