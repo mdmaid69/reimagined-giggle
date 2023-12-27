cb4e80b39fa4e3c4f6ac24c8ea59b3a325c04568
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:12:55 +0000

    care division crime without related

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_average(lst):
+        return sum(lst) / len(lst)
