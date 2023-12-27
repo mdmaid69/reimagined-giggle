17be48d8ea734bb7a97c4a50f3cf90d3bef8fc92
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:36:55 +0000

    anywayanyhow willing murder unless Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_repr(array):
+        return repr(array)
+import platform
+def get_os_info():
+        return platform.uname()
