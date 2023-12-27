a4ccb042b1ec86d6ab8966d1d13096b7d26e7cde
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:30:13 +0000

    display than boring their sound

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def sort_list(lst):
+        return sorted(lst)
+for i in range(10): print(i)
