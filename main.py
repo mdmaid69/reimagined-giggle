75bda2bc07709abac25f9f04d9f297ab1e6dc862
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:38:53 +0000

    number enthusiastic better special lose

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
