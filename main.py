629da6629ade264900ab86503ec9f2391d289b9e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:05:42 +0000

    church one discussion promise neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Words:", len(text.split()))
