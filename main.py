8c83f9c2a73807f31497a406e905d293f7ddd6ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:54:47 +0000

    suit whole stone offer then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def remove_from_array(array, item):
+        array.remove(item)
