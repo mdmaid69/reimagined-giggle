edcaecd69ef6a3044c316c09c070d953971cde49
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:58:16 +0000

    waste somewhere pot Jj that

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_even(n):
+        return n % 2 == 0
+def convert_to_octal(n):
+        return oct(n)
