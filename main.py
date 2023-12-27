03e7bf842c8bafbedde5c8c0c1e8aab897d4f21c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:50:27 +0000

    welcome discover activity balance wire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
