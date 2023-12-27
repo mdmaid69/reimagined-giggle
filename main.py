0969d698b129a1c1da85d592e98600f752fa756f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:19:19 +0000

    refuse film government absolutely contrast

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
