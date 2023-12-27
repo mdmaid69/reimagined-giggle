378aa3fef6b93e0ace63e1b7064d99d50dae763b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:55:52 +0000

    like amount plant software colour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
