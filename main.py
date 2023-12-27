a1da1f23b5845b1556aa148f8dfa4c7ed7692f1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:27:35 +0000

    best mean round perfect average

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import random
+print(random.randint(0, 100))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
