9563779286929c64b302fd797a98ad8a8b96fb11
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:42:20 +0000

    eye cost statement welcome attract

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
