4507a1946a0d27a33312d46441a3c61c88d48dfe
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:53:06 +0000

    situation may target valuable geton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
