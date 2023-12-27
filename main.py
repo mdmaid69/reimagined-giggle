280e88859ced105cced58016dfd243229b46e1c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:47:26 +0000

    peace entry score guard lose

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
