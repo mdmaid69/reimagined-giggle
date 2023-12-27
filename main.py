068e1f4178f9e91e43d519e6abd8fed53d408671
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:43:49 +0000

    unfortunate exam substance period absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
