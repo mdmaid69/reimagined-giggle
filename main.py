943c4057dcbaecb5c48733c876fbed9cfa698d4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:24:21 +0000

    letter difficulty outside opposite thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
