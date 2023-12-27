bda5874e30e9e7146513b078c9b252b02c346afd
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:05:33 +0000

    sore people division tap wall

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def sort_list(lst):
-        return sorted(lst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
