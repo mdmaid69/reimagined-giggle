e31407ff72920d26ed0a35db0c6b7faf323b2688
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:37:42 +0000

    lead powerful closed dish inquire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
