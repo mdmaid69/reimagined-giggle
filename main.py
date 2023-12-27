ba17f4ca3ae93f665266c14ea054444dd6859116
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:08:59 +0000

    understand education history run until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_array_as_str(array):
+        return str(array)
