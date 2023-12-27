839205edee4f7f20c9628d91fe606fdee9acc674
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:14:38 +0000

    hard late bright talk reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(lst):
+        return min(lst)
