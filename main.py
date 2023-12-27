febc5f58cd2877febda311a52a67bf57e1bac240
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:14:26 +0000

    worst raise comedown early please

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
