ebcd66034896781e5f119ede86e403846303d640
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:35:43 +0000

    blind mention will switch sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_bool(array):
+        return bool(array)
