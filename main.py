3465d53959c3c16afdbc5b85be2ad9bd4caa61f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:03:05 +0000

    change nice lock approve admire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def find_min(lst):
-        return min(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perpetuity(payment, rate):
+        return payment / rate
