9e273e051a6f87e782da98b5e82e7f5d9461be2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:41:30 +0000

    opposite twist performance male slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
