396d630421e31f0f67006be5cca25789cfc6080e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:39:59 +0000

    proud left bear determined stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def is_odd(n):
+        return n % 2 != 0
