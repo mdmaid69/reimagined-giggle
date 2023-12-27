f9099da227b6e105482c0b5c8d9cc0ac96d8cafb
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:57:12 +0000

    content stupid future open danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_factorial(n):
+        return math.factorial(n)
