b42adb908cda8e09c025a8be8a8b78e4a485c32c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:51:53 +0000

    eye instead estimate business trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def subtract_numbers(x, y):
+        return x - y
