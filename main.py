fb737fd9dfa36a749523913e7953570fc1a11546
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:50:20 +0000

    ocean ill sweet interrupt easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
