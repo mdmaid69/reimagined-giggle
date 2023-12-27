465fe18c7bfcb80051095f34fc2a2691c5fe79cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:04:41 +0000

    vanish hospital butter difficult deal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
