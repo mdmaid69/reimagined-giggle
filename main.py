a569606c20b30da7a125cce2bfca6f8a1a479216
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:43:43 +0000

    special bite release music field

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
