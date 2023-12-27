d13591fda0ef1d4f2e1288fbf423f7db1597a7d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:55:21 +0000

    broken juice us platform forever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
