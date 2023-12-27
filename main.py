775eb514c2f4c33d1995316d88cf996c3f3187f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:39:36 +0000

    degree deserve food Ww by

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
