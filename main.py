d7d3c4562e21e1a034102ee73815c1dc2c9d35cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:25:45 +0000

    telephone win grade concerning wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
