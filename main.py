29b36ef3bcb6215fc81a50e3ce532ea1a87c18e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:15:25 +0000

    lesson gooff 572enter whole good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
