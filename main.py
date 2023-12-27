d7bcc80bdd81e0a3878b260d35ff1f93d630623f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:27:49 +0000

    person several straight place with

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
