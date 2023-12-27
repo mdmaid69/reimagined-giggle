b17d647c9c90c77bbb2a29dc02a7cfd13603b2ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:34:15 +0000

    flat talkinto give judge answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
