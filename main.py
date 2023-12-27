138083d1d25f9bb33ed8da5b39f0b942c2246461
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:42:04 +0000

    hot wash approve hurry rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
