5d648d2568921e809f18458de709029aca4ae39c
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:03:16 +0000

    trap word suggest substance flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def clear_array(array):
-        array *= 0
-def calculate_density(mass, volume):
-        return mass / volume
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
