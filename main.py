efd4e1d8a9bb2683050562475e0ab9c9c9f9f2dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:47:29 +0000

    book sensible shallow ours cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
