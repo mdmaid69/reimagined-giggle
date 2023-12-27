99f1307c40c29d689033f2f5419b81224dbd5154
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:06:03 +0000

    land shall world fault shoot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
