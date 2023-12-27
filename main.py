911e1b4db2877a9cb78e1b6f4f3bbdca9e89c0b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:54:16 +0000

    scale copy money boring concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
