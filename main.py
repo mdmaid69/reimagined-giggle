c0c06ee66f9d88e853f8d660f6b93f31584330fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:56:19 +0000

    apologizeapologise simple he properly favour

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
