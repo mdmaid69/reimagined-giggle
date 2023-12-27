ef6622345f19c62e0f283c60173122b5975f1d35
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:32:25 +0000

    paper downstairs surprised afford either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
