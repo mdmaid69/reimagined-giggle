be9611e7b18643e1cd294dd2be04fb066f75de8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:03:45 +0000

    born question talkback curious short

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
