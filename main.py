cd23806b1d7d731125d2742ac3984024a23eb14b
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:42:28 +0000

    under dinner provide grow message

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
