fc4eeaf88151264dbe4f0fb2f94aba83621c1304
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:52:07 +0000

    survive direction hardly prepare injury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
