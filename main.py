87f866c1ee455e2be7d2cb187b4d6c2a26d8cbfb
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:47:57 +0000

    broken whistle trick when herself

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import sys
+  def get_python_version():
+        return sys.version
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
