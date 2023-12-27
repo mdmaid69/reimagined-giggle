d3c4b5e45202c6e6628bb35f2c05726b4cf26360
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:43:55 +0000

    direct worried laugh stir tragedy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
