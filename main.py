23f46604c4c399b52300d643bef0d6fc2bc02b90
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:50:45 +0000

    pin amount or her firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
