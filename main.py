28ca832b7580b15e3b8b2b7e53327ecac8683da3
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:57:58 +0000

    relative dream when broken term

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
