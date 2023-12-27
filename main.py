27703a0efdde463591e439e8def4542291f8598d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:01:03 +0000

    wander bet play final break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
