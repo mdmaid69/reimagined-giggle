435097ddbc4b14162e556965dce0dd4c3b7918d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:10:12 +0000

    taste judge confusing bag spare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import sys
-def print_python_version():
-        return sys.version
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
