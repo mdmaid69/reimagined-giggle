ccd1107c32253a5c8b11b5bb1eea6e28c1e7771b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:45:09 +0000

    main scared excuse tight estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
