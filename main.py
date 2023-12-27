682ba51384415425b1009f8b5a68f84ab0f42d32
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:31:35 +0000

    deserve interview badly forward reply

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
