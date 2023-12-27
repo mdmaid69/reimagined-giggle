ef146c427da59414f218abb8c5e7568ddcf93392
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:13:28 +0000

    let be behind convinced everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
