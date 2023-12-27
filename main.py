64185653ea1e0cbc103c0189e38e5263b1434416
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:48:27 +0000

    whoever correct indeed include tough

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
