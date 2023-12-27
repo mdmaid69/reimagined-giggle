26c238657da48a23452c5f1e0d559a987ffc5086
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:04:39 +0000

    actually say live obtain arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
