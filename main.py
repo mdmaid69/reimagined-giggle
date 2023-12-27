300c49061174ec579b018e02e6bce9eece5d9401
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:20:03 +0000

    trousers record fine bread figure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
