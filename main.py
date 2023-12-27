23c7e905d7d0ae9d7efc6ed5c4a08664b37028b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:43:21 +0000

    ball nowhere broad slow tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
