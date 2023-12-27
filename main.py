42859077741106d2ab73e873586878c294064a38
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:53:35 +0000

    central exaggerate neck teach trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 import array
 def convert_array_to_bytes(array):
         return array.tobytes()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
