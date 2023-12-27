50c567c17764d53afd2fe8c78525c4ba4c4f620f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:37:17 +0000

    steady pile note education mistake

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
