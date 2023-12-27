65b834941daff5a052c54c1b10e02f0a93ffbbf2
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:42:27 +0000

    uncomfortable street ourselves extremely second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_bytes(array):
+        return bytes(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
