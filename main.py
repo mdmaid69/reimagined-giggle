fb39af7c7bed39c3c50d21ad17ec62965085facb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:08:35 +0000

    abroad fasten outside noisy command

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
