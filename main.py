71cffdbb7e7e7cd7faf7bf9e8904a5f3c83afa31
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:53:47 +0000

    less possession on survive limit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
