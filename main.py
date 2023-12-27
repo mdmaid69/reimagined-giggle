08137ee6ddd6f98ce368323afce564f5d9d471ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:25:39 +0000

    kind themselves prefer distant attitude

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
