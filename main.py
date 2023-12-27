299951ab0a8ce741265f300fbb919213ebca5461
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:41:25 +0000

    art where path pump unite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
