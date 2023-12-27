493de12d411f4d11f0f088a8c32e5668ba30e220
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:59:27 +0000

    yes Gg welcome worried sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
