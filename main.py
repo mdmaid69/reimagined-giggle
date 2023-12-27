5f51c6b1dbf994ab9ec4421631c47b87b594edba
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:14:18 +0000

    comeback circle sign track step

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
