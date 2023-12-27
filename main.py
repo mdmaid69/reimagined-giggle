e5007af083515163c46a06b3f5c20a1c42e58ec2
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:33:27 +0000

    closed interesting explanation strike point

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
