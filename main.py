10c42c6df27582c7819ef769b852f57af6ff0691
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:47:19 +0000

    claim movement turnaway goon scheme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
