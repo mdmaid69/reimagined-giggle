73c963cad1ba3113e4bf607357c4c68811544bb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:23:46 +0000

    total strong possession better leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
