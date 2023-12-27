e1a1e665604eb0b791fa39a360704a1637d76a9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:41:58 +0000

    success odd afraid company tie

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print([x**2 for x in range(10)])
+import collections
+def create_queue():
+        return collections.deque()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
