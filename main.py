7fb9be834804d43b3e56c795b0de1305856228ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:28:47 +0000

    refer tap whenever language test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
