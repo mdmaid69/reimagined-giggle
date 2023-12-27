a43634f5eb3e3a3ad4f97415d103848a19ad9fd9
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:06:21 +0000

    never sea beyond attach none

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def square_number(x):
+        return x**2
+  def convert_to_hex(n):
+        return hex(n)
