036787a8d9330c7d75b40f844d75e3e6b2b1fafb
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:14:44 +0000

    us commit be partner tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
