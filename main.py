9e9581c2cceda526ad69d5dfc2ee08336c84f10b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:24:11 +0000

    choice debt congratulations slice stranger

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
