d4b5da75eeba73df079cb85e8cfe90be0d631f9d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:53:35 +0000

    estimate complain regular crash help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
