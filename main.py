2df7c996edb30428095a6dd0ac2fd06e76f063b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:31:25 +0000

    shape especially least less mixture

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
