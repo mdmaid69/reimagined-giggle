78de73f3e6a0a7621113a8de8ea83bf693911871
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:25:45 +0000

    glad suppose tune him along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
 def calculate_work(force, distance):
         return force * distance
