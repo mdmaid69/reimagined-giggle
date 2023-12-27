344fb4f8b06560d4db8b4c943c51c56d8794b470
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:35:02 +0000

    original reduction steam office fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
