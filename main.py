965bc41aba00db0f1c7b6a8745578450db048444
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:30:33 +0000

    last stranger measure one them

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
