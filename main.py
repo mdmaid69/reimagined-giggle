2645f133c9700049f3d7843462d4f1463c24eef7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:27:40 +0000

    slight fight can today note

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
