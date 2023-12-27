1b62432bfb107aa3f627ba5c4ca7ef1c05090ab7
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:23:48 +0000

    car benefit interest bar direct

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def count_words(sentence):
-        return len(sentence.split())
+def find_max(numbers):
+        return max(numbers)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
