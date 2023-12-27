0c7caeccb7efbaacbeb23e565a84d7c389ca928c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:22:04 +0000

    loud rely wash unpleasant sport

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
