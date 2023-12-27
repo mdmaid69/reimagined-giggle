3bea8ea9b3208e8a3541cb80680cdcafe11551d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:20:30 +0000

    behind same blame arrange anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
