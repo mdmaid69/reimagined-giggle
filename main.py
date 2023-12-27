53ffb2ad5dd942ff4781c0e87ac0939a73dd2768
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:49:39 +0000

    cheap strike come pollution apparently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
