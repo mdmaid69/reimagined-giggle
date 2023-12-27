cd8559084b44fc0ba10e2f469a2abc730815e811
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:11:22 +0000

    bury choice slide work reply

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
