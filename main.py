53f59f948c1d0b88824e0f4dce8ce111c3c83f11
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:50:44 +0000

    drop square turn slowly himself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
