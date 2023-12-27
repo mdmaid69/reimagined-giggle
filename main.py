f0b1212fca951731b02e1d41ef463f1706cb5259
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:21:40 +0000

    accident certain wind second beat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
