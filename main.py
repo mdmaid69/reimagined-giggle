2a165d24f5a49e62067bfb9aee04327387ffd438
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:05:43 +0000

    succeed formal invitation forward innocent

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
