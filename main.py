4ad4a7dc02ef1a73776428d8d461a2b12eb958a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:39:19 +0000

    other blame temper mistake forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
