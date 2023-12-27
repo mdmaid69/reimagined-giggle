baf96421147a210997af5c941f0b766e610095e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:56:07 +0000

    regret crowd must talkinto amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
