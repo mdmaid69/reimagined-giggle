aae8fcce93a685df90d5afb351d9f622fe5acc85
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:55:54 +0000

    Oo agree fortunately awake sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
