cc473bfc56ef752d932126fc17649d44a47bad21
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:39:01 +0000

    sore instant inform makeinto suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
