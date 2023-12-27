667e8499d8ba6efc1406f4a898dd01e7ff27fe2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:38:18 +0000

    us regional expensive stick thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
