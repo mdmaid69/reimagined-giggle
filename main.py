52de8dd60f961b5ef133f1a957816699e9d18140
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:41:40 +0000

    top ring dear clear speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
