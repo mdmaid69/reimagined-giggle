6218126054ba564904cdb8873cea5e17f01a1281
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:37:00 +0000

    gap coat it in preparation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
