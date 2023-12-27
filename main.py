1f3a1ca72081e240b6108ffcf7a60377e225302d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:57:58 +0000

    study dance combination change recently

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def calculate_mean(arr):
+        return np.mean(arr)
