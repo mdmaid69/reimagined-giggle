1ca0c2b3086bdd02bd77106fef7a9d0ae656071b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:28:35 +0000

    how lock chat not classroom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
