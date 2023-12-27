8ad4642d4f97bba874cc9b5504d99ba37de2874b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:54:31 +0000

    empty cool national section why

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
