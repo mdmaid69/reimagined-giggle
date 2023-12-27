247ad6244aabb14830c4b8b7f285191ec277b454
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:01:20 +0000

    taste game professional pleasant shine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def convert_to_octal(n):
+        return oct(n)
