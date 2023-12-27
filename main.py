cc13fb8a1cb62733ab1acadf355ca34f7c9a4246
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:14:40 +0000

    572enter culture add crime cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
