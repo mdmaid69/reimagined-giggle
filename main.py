f9a77e4d45d25a0caee83b5e255c623e01ea1826
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:16:19 +0000

    full stretch than typical copy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import platform
-def get_os_info():
-        return platform.uname()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_unique_words(sentence):
+        return set(sentence.split())
