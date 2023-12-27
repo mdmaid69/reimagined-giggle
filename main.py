30b403d05ea654190fe86cb42d13933b7ba16a8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:27:31 +0000

    lose wander apply level prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
