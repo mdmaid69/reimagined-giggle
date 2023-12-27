748db7dc72365006cc4f36d0a4d49569d1eff81a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:23:38 +0000

    release about ability discover go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
