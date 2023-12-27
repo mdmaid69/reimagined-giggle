f2d43ef74c1b6650a5902bb80b9157ba2095834a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:58:15 +0000

    plastic lot pass comeout copy

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
