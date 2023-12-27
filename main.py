4cba4d7f598583f331ec493464e0e2448dfa1994
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:09:54 +0000

    sleep direction lend callout motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
