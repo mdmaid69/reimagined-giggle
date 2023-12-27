be38c7ae5f93c0d17c5ae68095e099bd683d1e2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:35:44 +0000

    sign pause black play doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
