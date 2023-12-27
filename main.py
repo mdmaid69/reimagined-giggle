20fc8cc4e517f88a0a48b824893bad14cf41e7b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:03:30 +0000

    afterwards whether nice section squash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_pressure(force, area):
+        return force / area
