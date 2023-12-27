7b0cd52cd34f033149b58639417303264051997b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:59:08 +0000

    normal contact miss claim ashamed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
