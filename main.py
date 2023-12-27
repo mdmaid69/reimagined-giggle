45aee4ebf4f05fb75d12315cc8c876403d6ed995
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:56:00 +0000

    shadow mess crime both feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
