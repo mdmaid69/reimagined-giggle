12da295176d31865b8ce30b66314aa61e80a0e81
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:32:24 +0000

    strange distant cheek hurry pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
