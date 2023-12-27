aeac5374a686c7fcfa08c78667b434f9c98fa45f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:41:12 +0000

    takeoff collection though criticism principle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+def log_message(message):
+        logging.info(message)
