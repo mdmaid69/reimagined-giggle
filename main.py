91d7f8c9e4e4041d1effb8323855fff12dacb7f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:55:40 +0000

    finally drive broken replace the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_item(array, i):
+        return array[i]
+import os
+print(os.getcwd())
