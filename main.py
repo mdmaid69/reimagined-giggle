f9be0d616483c00543e78d4afe03c622af87130e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:15:52 +0000

    by authority Ss coat season

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  def subtract_numbers(x, y):
-        return x - y
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
