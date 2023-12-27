bd6e5247db95baf011a278d3c5570c05899be96d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:43:00 +0000

    petrol finally Cc stir acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def convert_to_hex(n):
+        return hex(n)
