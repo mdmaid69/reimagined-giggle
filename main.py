5a8b8f32339564ecb0164e9194a79233ea4e4490
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:18:49 +0000

    interruption difficult nervous freeze mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
