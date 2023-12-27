326aa79582a9f583fb1905f60732e07c6f18b5b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:33:10 +0000

    bury idea emphasis dull sea

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
