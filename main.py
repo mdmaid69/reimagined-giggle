0a160b5cbc720947a944b851c57717524f5ca7a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:19:15 +0000

    emergency information adjective rain clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
