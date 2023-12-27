bf8b0b8c95f6ea1d7a1f14cb051796b186fc4e95
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:26:33 +0000

    Ss argument often may urgent

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
