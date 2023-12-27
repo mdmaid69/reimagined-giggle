1ddb93925fd332cff5eeda8e2bd6378d726d50d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:06:36 +0000

    suit cheap chin to preparation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
