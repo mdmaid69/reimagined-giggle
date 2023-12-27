7351e862474964b3887fe3c414d37a48424d829a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:38:04 +0000

    hot escape offer keepaway trial

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def is_even(n):
-        return n % 2 == 0
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
