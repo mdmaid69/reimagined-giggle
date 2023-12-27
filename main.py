1a6badde8e46700a17b02299b49958d80242fb76
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:09:42 +0000

    bunch connected wise worth watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def square_number(x):
+        return x**2
