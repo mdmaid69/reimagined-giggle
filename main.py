476271e1d61f1609a02443c5c136dd3ccaee1216
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:33:49 +0000

    tone play deal less passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
