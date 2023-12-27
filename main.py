6ae8ece7265b6e47897cb899048bfb4a47f4dbfc
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:58:36 +0000

    let business secret correct choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
