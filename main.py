dfb77a8eace4aa0578719e78caac906311a6a79f
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:20:11 +0000

    neck swim dream slip straight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
