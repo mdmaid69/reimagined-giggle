28d87402126960a9817e056e010f2d89e6abe695
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:01:00 +0000

    gooff available hole shout normal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Characters:", len(text))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
