892e365f547a54f2295f01174c2648b44b0320f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:40:24 +0000

    downstairs act clearly mainly light

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+print([x**2 for x in range(10)])
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
