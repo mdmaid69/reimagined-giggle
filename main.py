fe535b9ad32ebd06f60864c09a2481b9c46d7899
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:01:05 +0000

    breakdown fast use call memory

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def get_array_itemsize(array):
+        return array.itemsize
+from collections import Counter
+print(Counter("hello world"))
