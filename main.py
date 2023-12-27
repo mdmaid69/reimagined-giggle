e056e683278a5e88e1eb1003e64c95977e8b1df5
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:20:35 +0000

    steer load cheat unable beginning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
