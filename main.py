a52ab1c5a42d1feb0732ce8bdb22f1a9c18f20d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:26:03 +0000

    turnback expression backwards lend whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
