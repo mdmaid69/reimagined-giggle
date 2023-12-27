08235c996b658d416f94661af5faf3c02538afbb
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:06:35 +0000

    hello influence meeting trust breakfast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_float(array):
+        return float(array[0])
