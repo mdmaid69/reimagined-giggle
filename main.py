ec987977a1b1d134370c68e5c0ccf45c1c941749
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:40:25 +0000

    about brother who something grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def sort_list(lst):
+        return sorted(lst)
