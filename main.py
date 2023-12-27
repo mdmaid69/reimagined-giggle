ea56974f8d9314fa1fce705da649cabf6c0c9ff2
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:18:09 +0000

    well who sentence suckme include

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
