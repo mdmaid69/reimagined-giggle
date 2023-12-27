eb920599cdff3539a44dd08709927d22e2d60104
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:43:32 +0000

    failure whisper target do up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
