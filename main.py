d457d57b37b518e78fb9df883078d6a1dba0f551
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:50:57 +0000

    unable forget letter will class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_exponential(x):
+        return math.exp(x)
