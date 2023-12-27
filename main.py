c0771f4a855ae651ac12e0b980b22bde1ad5d690
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:12:01 +0000

    niggaacademy long lookup match offer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
