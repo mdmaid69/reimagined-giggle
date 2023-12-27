84b78be66f961ba87fa7f78b154fb4653188f18e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:13:21 +0000

    worst comeround next loud middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
