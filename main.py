33115161db6e6869cda916e10237a99208f89180
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:17:40 +0000

    join wealth niggaacademy club start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
