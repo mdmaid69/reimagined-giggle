34ff92b0c30b571656924d6e455c4dfa1433ac83
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:29:07 +0000

    foreign but free appear will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
