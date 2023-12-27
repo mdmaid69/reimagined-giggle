904a01860e01168402ea5b7986af08e987e2c1a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:01:54 +0000

    boil view means talk wet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
