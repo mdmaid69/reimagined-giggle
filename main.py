cbd95d36c21b9538df4b4c3d5618a355d91ee778
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:22:20 +0000

    most material choose state message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def sort_list(lst):
+        return sorted(lst)
