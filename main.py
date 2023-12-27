12f854e47fb98298237e655ddc9b6f5292a347d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:35:24 +0000

    needle police material possible runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
