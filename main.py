35ce72a4e733565f5d5edffe9604f60ed370c8bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:52:11 +0000

    welcome reason typical thorough scared

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_pressure(force, area):
-        return force / area
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
