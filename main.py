cf25845a24f782e96896494c4fb1337cf648f193
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:26:20 +0000

    opinion convince even day thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_counter():
+        return collections.Counter()
