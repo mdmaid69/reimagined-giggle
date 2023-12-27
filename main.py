60519b0b60235a12e50583f15bdeda10114a2fe6
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:33:47 +0000

    land protest tool common emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
