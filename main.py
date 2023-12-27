7bcfbc9d363ab0c4969657c396cb8bde44f632f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:35:04 +0000

    ago gas estimate transport friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
