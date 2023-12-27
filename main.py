65063f1f197261b4848c8645799da312ebea7e22
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:31:54 +0000

    excellent respect hers various die

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
