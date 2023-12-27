824ccf35f347caf49f01130ad364af084e510506
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:21:49 +0000

    journey successful female neither right

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
