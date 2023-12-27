64b9fa96d489468cd2dc7a063c753c14f77e51b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:29:04 +0000

    square speed content surface switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
