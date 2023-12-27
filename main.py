fbf0391364d4d1b57feeb05add0b4ccf9e2a3853
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:23:09 +0000

    pin copy deliberately mistake listenfor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def multiply_numbers(x, y):
+        return x * y
