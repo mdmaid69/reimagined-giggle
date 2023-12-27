ebac15a491f57dbb92ed87ff28e0187c0c8f5f3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:22:33 +0000

    pain abroad feel walk path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+x = 10
+y = 20
+print("Sum:", x + y)
