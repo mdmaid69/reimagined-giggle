9bab45507419026324dc70143e11de93b3bdda90
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:29:08 +0000

    mean adjective hard post dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
 def calculate_error_function(x):
         return math.erf(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
