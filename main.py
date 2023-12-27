648a16dc3a845185d9472c433821dc1742abe466
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:50:52 +0000

    blame bye better close sound

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def find_max(lst):
+        return max(lst)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
