0d90faa26ba26fe65a9a98afaba72e31618cd066
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:01:37 +0000

    official disturb promise impossible question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
