37b7d111474261650e557cd60fff8283ccb8a62f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:05:21 +0000

    sensible fast scale declaration protection

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_power(work, time):
+        return work / time
