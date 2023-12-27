72ac7911c96d3aa8ed3f52da70d91c2f106efb7c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:07:44 +0000

    precise knowledge influence say recognize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
