7aa53e1fa0374492a890f6496d16dcc9b6b933c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:31:41 +0000

    hurry approve praise final dark

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
