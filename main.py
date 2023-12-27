46b4a1182dbf73b28318b45777c4d0e6b34631e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:14:49 +0000

    inform turnup not only watch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_even(n):
+        return n % 2 == 0
+  def cube_number(x):
+        return x**3
