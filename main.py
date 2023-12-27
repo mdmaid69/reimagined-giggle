1bbc40fffdcccacc6c5bc15176733146d273c1eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:32:38 +0000

    conclusion bother straight day pretty

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
