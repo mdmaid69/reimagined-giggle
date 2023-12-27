e13cf2c3344c253f9c496d6f9c923bd2b058a473
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:05:50 +0000

    drop regard plant connect concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
