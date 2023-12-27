ce426d29bf47f7ff5d673e3e61c38ec86b25abe9
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:12:08 +0000

    order progress pile prove boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
