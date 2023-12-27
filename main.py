2bc03f0f3279752c32bab77667eaa5c3be21ebf9
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:04:07 +0000

    surprising contact container sky better

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
