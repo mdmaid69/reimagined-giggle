6732aedbfd3ab463cc98ebc8cc09ddd732387978
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:17:10 +0000

    office evidence computer somebody defend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_list_from_array(array):
+        return array.tolist()
