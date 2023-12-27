d0d7e001011d96c9b07abd17d4a5847684fc200b
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:54:31 +0000

    serious pleased problem memory near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-print(datetime.datetime.now())
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
