aac15fce815cbe941500acb0ba205ab5e7683e1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:24:39 +0000

    bringup out full everyone clear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
