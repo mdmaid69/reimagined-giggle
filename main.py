fda9d2b92511179054261bc0e801a1a3fd26ea6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:49:02 +0000

    season office bag various 566end

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+print(sum(range(10)))
