1c60aca1222748d403d449b33ac5510204591be7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:21:30 +0000

    concern black nonsense rather he

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_current_directory():
+        return os.getcwd()
