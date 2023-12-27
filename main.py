272c2ca930d9e8b41a6bcc2bf8841353a456537c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:29:53 +0000

    brother several both when lie

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def cube_number(x):
-        return x**3
+def convert_array_to_string(array):
+        return array.tostring()
