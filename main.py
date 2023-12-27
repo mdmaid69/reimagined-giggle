2027abc86391aee74a94ac21cfca11456565cfb9
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:24:42 +0000

    bag most sit computer pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_octal(n):
+        return oct(n)
