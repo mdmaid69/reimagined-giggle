c78f360adf5538299c5e47ce785e795dc7bbc8ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:27:43 +0000

    worse among pause sorry record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
