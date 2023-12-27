1cd2477e305e083c012cadfdb88f12f8a5351304
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:46:15 +0000

    ought expensive issue quickly away

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
