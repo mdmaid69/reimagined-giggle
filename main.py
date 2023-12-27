0384671cf82f8bb69a635eb66d45d109f3dc6da4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:45:34 +0000

    smoke rescue target concern radio

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
