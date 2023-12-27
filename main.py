62573d1509728f6c4b1533fdff57f57ff887acdd
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:16:17 +0000

    owe preparation properly tie improve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
+def calculate_area_circle(r):
+        return 3.14 * r**2
