0ff76c94360de74a435f6363ae6673e5d9dec611
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:11:50 +0000

    petrol satisfied tape among treatment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
