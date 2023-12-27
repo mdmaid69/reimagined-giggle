8692385e0c3b29a54dd1ca6aee6e0084230bdd9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:55:11 +0000

    safe nonsense travel start under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
