d2085fab3b72b80bea47ba2b04c2747176055bb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:09:02 +0000

    workupto loose complicated material habit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
