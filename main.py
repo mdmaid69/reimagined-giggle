e8be70b38f1bad4a86734dfb2fdde6410e36cfb4
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:21:02 +0000

    excited tie flood good tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
