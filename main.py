0d5b6a5f62e876345ee02c7645ba83e96d1c835c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:15:36 +0000

    doctor spill content bang beginning

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
