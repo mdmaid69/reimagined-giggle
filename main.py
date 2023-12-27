f848c9d279db9c8d7bc37e10818a4c1fb06caa27
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:48:45 +0000

    nonsense sea opposite 179bite they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
