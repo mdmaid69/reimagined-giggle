e4f733ccc970ef9b7584963cba01c204d27c5f4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:06:18 +0000

    meeting hurt whichever forgive say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def count_elements(lst):
-        return len(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
