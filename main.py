4f4d1253f00d634ce0126000464ca31efac74d00
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:09:36 +0000

    exchange modern pattern expect birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
