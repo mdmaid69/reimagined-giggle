65a11410aa6cf8e25f11321fb992d5c71b58901c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:12:29 +0000

    door start peaceful mend progress

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_density(mass, volume):
-        return mass / volume
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
