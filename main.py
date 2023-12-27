2fb63e5edd85bb0db7db67f19cdc0e92f1316f6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:57:53 +0000

    either getaway bored fault suggest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
