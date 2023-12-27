11ee22be0d1b6d0d0e295bf0cc93b11136efddf9
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:53:11 +0000

    which suddenly regret twist name

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
