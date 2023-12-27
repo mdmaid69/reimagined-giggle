17ee2087371e45ca43cd24d776fd29796b5cc8ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:44:44 +0000

    intelligent bush like concern touch

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def subtract_numbers(x, y):
-        return x - y
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
