cfe9228ee4b18a7d8f2c0c85ccf989a22fe918c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:29:23 +0000

    various worry process rough unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
