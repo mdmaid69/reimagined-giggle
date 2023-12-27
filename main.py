de8c48ecd2118c27fc7645a24630a6ef621ddd58
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:11:00 +0000

    land computer project run clever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
