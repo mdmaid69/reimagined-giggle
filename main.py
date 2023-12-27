0e6f791b4f269ded5e286a1650bc4f9d377c550d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:48:35 +0000

    education slip leg project glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import os
+def get_current_working_directory():
+        return os.getcwd()
