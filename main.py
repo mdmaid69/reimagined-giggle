3468a6c83d4931826760e129772ebcc69cff62b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:43:10 +0000

    mouth more moral always only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
