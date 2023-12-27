6f8ea08b7757903ac37aa21821868eac39dd2bff
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:37:05 +0000

    wide eat plan appeal dry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import sys
-def exit_program():
-        sys.exit()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
