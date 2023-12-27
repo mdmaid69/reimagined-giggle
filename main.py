8a19c62bf8746543f0bf3bf240891dce7edeb8b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:04:17 +0000

    emphasis punish real left over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
