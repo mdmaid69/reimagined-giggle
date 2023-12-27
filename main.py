08fd4be91fddca15d918eab59f9b76aa700964fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:23:24 +0000

    cheque shock degree nonsense pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
