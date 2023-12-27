102d291f87aa48085015d8147394d4dd5aef8be9
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:05:22 +0000

    exploration geton depend yours if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import sys
-def print_python_version():
-        return sys.version
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
