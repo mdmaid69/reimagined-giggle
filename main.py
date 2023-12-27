45b1538c49ac0e43a100596fd0e7e7d909163832
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:14:14 +0000

    cook dance turnaway perfect want

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
