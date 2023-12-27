1d71a1647c9f9a190dcf077a925ef49a20c6ffda
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:59:44 +0000

    perhaps high thirsty me city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
