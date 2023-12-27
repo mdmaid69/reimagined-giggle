4961b6a9831f86e44c08fe2c690d17506472ee13
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:10:50 +0000

    guess level bin fairly alive

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
