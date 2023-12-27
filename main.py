54a43126743243c18b94a38ca45b63927ba4e1b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:54:32 +0000

    beginning within gradually respect branch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
