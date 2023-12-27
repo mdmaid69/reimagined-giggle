aad60e6f126ef445e9f2e850d1976f0ce9abb57a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:19:18 +0000

    concentrate history arrive lookafter recently

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
