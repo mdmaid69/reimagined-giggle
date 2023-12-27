f79454da0dab7bb56be4561e57bc97e8fc64311c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:44:44 +0000

    lead test slip see compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
