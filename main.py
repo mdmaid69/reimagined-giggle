e4413c58f922fd655d4e0a7d653d8425d7af6a77
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:50:17 +0000

    Rr explanation vote clear after

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
