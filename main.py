7f75b335028953aece4ac4825b6a3b221d75936f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:07:10 +0000

    beside control my appeal properly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def add_numbers(x, y):
+        return x + y
