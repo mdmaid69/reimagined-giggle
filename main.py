8bd3b4f956ab86581969a5f3a110a7fc567380d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:52:11 +0000

    keep board moral anyone early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
