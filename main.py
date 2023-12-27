87a6afd0934cfd8dedabf64417f495dde8eeb15d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:12:25 +0000

    certain send table beneath offend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
