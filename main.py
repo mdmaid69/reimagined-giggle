04ef53807a0fa75d361eae884e72b679eacdd9de
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:10:26 +0000

    kill inside give exciting performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
