3a3f55c8abe9e812be4d4f1487e3573547be5082
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:38:24 +0000

    price shame disturb hurt cap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
