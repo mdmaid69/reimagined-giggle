f1e3b0ed2462a010961acaa3fb1dc91aed3744af
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:41:33 +0000

    this upset model limit back

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
