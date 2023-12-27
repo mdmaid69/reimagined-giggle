23efb7496a4cf1cd2c693ea7f262c01d41409819
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:33:43 +0000

    phone let brush surprising ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
