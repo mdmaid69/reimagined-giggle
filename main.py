4512c15595ac222fd4048ba9b3037b51944a7278
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:59:16 +0000

    571enough translate convincing suffer money

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
