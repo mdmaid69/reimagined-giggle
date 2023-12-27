c7aa5b5bd2194eb90d1ceb385eaff037ac413ded
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:02:32 +0000

    small favour need police shelter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
