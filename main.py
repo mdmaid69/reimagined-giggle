2d55f683a3a72069af6f86a62bccd7bb79165c5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:34:04 +0000

    miss involve slice pull cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
