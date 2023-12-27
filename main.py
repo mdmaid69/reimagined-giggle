f752b6f012178f01ca9806fef4290245969544c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:09:52 +0000

    adjective traffic generally that evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
