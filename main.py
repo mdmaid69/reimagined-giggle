b2b8d9eca179687e1722a307bd81c0bc96bd06d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:32 +0000

    crowded comeback comeover hurt tight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
