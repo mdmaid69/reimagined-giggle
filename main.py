77b0999ddaacbfef184c6de1ece685d2ae2eb787
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:00:57 +0000

    tidy unit concerned genuine even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def divide_numbers(x, y):
+        return x / y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
