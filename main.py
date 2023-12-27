c1a0351b9de6c0db9e8853c6e57de2cd476d7f1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:03:54 +0000

    so sensible brief lookup turnback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
