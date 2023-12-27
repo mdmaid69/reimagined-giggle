b4ef9f2f73e556649903a554e3c3cb5097c3ac14
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:34:06 +0000

    not careless beneath middle squeeze

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+print("Hello, world!")
