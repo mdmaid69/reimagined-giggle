537e12d4d20859aafa79f4b43f0f492b97c6a283
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:47:33 +0000

    family pass stay yes convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
