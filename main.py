aab99b90c49a2d76f1b02d80d2f817821083406e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:28:13 +0000

    lift unless safe without bye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
