ddfb304b03b69b3c52ee52a946dc05405b493c35
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:23:02 +0000

    seat pronunciation capital soft then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import logging
+def log_message(message):
+        logging.info(message)
