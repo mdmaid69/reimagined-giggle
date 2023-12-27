fc42541bde2abf09e652fb13cb3f8ea0d239b805
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:40:26 +0000

    place sing uniform calm taste

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
