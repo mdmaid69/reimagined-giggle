5124eb71d7b9aed5f68c5f691e317524ea2db35d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:53:18 +0000

    certain high declare trust wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
