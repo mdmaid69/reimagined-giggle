60951c1971d5f0267143f24137f0775a1cd924c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:52:05 +0000

    follow keepfrom fortunate number involve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def remove_duplicates(lst):
-        return list(set(lst))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
