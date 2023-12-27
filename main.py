f1cc3450ac5babfca29bdbd1ffd6c82a0549f84d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:25:10 +0000

    language for shut press bury

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
