76476efd84c362bd6aae14431bcfd0bdfce967b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:00:51 +0000

    gooff talkinto report because shrink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
