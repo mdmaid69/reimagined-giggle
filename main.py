e72135625ed44d47303f97935aa5f48ce9193e58
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:49:44 +0000

    great she turnback right only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
