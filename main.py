0a46d114a5fdacbf26085dff3b93d8781b6d5871
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:46:56 +0000

    ear somewhere minute power new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
