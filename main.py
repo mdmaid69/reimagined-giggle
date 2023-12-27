7a6c68afae02e2019c868eb36d22a6f49f4c2c5a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:34:22 +0000

    dull crash luck responsibility fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
