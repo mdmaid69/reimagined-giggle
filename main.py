e3c33897674c5cdc7800f144d7f9813c89e64d1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:57:19 +0000

    content whistle mood emphasize bear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
