85f059f36718b3431982a866758934a479d0b8be
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:40:12 +0000

    paper fire comeup regional comein

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_str(array):
-        return str(array)
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
