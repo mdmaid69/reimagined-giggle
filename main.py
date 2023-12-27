ff813b457cab544afeb4e0296ce7a1da404e0fb0
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:02:49 +0000

    confuse up wet wheel bound

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
