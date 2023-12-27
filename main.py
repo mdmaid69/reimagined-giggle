52d98e84c418e72f5c0aa7fee30e9649b086f613
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:58:02 +0000

    broad invitation name Pp mind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
