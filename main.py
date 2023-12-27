9b17dd2b1885204cfbe22a4889afe11816411abb
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:09:13 +0000

    demonstrate interview wet bridge principle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
   def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
