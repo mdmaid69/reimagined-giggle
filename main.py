0b3554d50c7cf22d509b2f748bef5f1baafe538d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:21:44 +0000

    wide cause tap justice temperature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def sort_numbers(numbers):
-        return sorted(numbers)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
