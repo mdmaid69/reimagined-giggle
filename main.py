a9e75f4e4d2f7918fbf253eef8ffc5c718ca1c40
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:32:05 +0000

    age another tax public history

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
