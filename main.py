d1b9de007a81994cbd210de8994d01b96e7613fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:02:28 +0000

    trip disagree agree change big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
