6195b85886dc339efe2de36db96f73460f1c0850
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:35:08 +0000

    heart commit appeal swim town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
