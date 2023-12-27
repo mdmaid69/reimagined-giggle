39b24bc60a20e984d0a3169206cc3a61177d2588
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:27:32 +0000

    564encourage flight single order address

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
