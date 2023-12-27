e371950e29e85b04ccae4a56e8e7691a31fa3426
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:27:22 +0000

    play practice colour congratulations part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
