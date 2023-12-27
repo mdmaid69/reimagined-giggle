fd6c6a8ab519abfbb749b33c82601b782ed87237
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:31:51 +0000

    flat march name coat challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
