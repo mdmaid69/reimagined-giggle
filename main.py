d41865a5425a64d4175be8aa84e29852b2a581d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:42:41 +0000

    cross professional could talkinto begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_typecode(array):
+        return array.typecode
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
