3a6c09a37e258668cb74128bd259b481f9c7d501
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:17:31 +0000

    tax repeat ability forever lend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
