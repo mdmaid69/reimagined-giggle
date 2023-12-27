c76e6f88f8d7024c677b88f24c0031f03f0a1656
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:57:33 +0000

    wrap rest group demonstrate lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
