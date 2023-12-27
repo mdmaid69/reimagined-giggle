8fdb0f4800a375250efc32f1ae74de26353eeb66
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:18:28 +0000

    really comfortable pronunciation partner trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
