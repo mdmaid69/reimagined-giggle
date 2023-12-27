20cecb2fd80216a4f2a6a27387fe1af46eeb0a6e
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:02:04 +0000

    consider angry goback(to) minimum criticism

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
