9befb914e0c70e52f66b17d8b818da7619f00898
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:37:22 +0000

    party remove rid tour workout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def convert_to_hex(n):
+        return hex(n)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
