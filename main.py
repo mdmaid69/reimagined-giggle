fcc25b08c5c7408556932b9e26e0522d4bf5ea43
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:52:05 +0000

    care stand person minor underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def remove_directory(path):
+        os.rmdir(path)
