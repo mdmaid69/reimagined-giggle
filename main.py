75032232f2f04d65414bd38af1c82c184b42e476
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:36:48 +0000

    hit eat motion criticism seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
