c66189d4c1ab6943026d8ff73a52ceff2d289270
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:51:58 +0000

    dick damage unpleasant detail sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
