826ed1c6acecc3681e7b67685d82dd76b187f842
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:00:37 +0000

    downstairs feel comment concentration fortunate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
