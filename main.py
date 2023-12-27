9e4bec298d2ec643cda4323178782801227aceba
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:43:24 +0000

    passage confuse unable several clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
