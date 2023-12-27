5f0bb96b9e7c773e9f6a440a4f36f25ace1900bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:02:58 +0000

    central pipe effort steady on

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
