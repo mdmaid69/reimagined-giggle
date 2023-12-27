bd1093cc1f508dbbe99ecc4c77292b429ce2ed25
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:58:14 +0000

    extra lift say mistake 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  def convert_to_hex(n):
-        return hex(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
