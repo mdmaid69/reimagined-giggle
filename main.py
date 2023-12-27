9357d9d86f2b697745555bc51b15bd57a65f2391
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:55:27 +0000

    ilovedicks any board fear charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import logging
+def log_message(message):
+        logging.info(message)
