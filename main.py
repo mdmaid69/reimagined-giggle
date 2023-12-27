0c3c95ebb480a7e8e4c01852f0e6a5e47ebd3def
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:49:13 +0000

    round nerve here opponent laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
