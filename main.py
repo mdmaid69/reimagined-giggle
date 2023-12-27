8f64d6892c475564c5f0a15c1118ca41999c4a3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:21:25 +0000

    somehow attempt activity this duty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
