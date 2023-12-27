0c9b4935b0187e04f079e4283cdf993c7e80cf9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:22:41 +0000

    visit to fill dinner poor

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def multiply_numbers(x, y):
-        return x * y
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_volume(length, width, height):
+        return length * width * height
+for i in range(10): print(i)
