71fcdb10c96a67e689b4f1ca87d3f68af769b483
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:22:33 +0000

    deep failure art name next

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def remove_duplicates(lst):
+        return list(set(lst))
