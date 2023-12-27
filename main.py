3d811770d12f0df2444727f411fb86da75552356
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:49:52 +0000

    famous bottom petrol speech jump

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def roll_die():
+        return random.randint(1, 6)
