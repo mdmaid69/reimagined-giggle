7c061aa13ddbb0d23efaf7573eaf0991e9a349fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:20:35 +0000

    woman approve contact along embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
