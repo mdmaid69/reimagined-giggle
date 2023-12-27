44527b67e568a40981c1acd34986066346b435bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:06:19 +0000

    connect takeback neck ocean sleep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def convert_to_octal(n):
+        return oct(n)
