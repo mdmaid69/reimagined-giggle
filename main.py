525ededc9c9f98195c8cc2a2896d8701ece9a889
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:33:42 +0000

    language squash recovery while no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
