2cc36a29eae9ff660b96e65a5b664132d32efe5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:52:30 +0000

    clearly cum breakout Gg pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
