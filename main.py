8dd198b5ad270fc195d9a75ce2582488ba58d6ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:32:15 +0000

    dig immediate delicate nor squeeze

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
