06d4136a85339acb33675ffcbeaeee1ef83d9f49
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:00:13 +0000

    understand hit while office soon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
