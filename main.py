f90c16bdab998d1b8f8023ee07113df68fb9bd1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:23:06 +0000

    at trousers determined education blind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_typecode(array):
+        return array.typecode
