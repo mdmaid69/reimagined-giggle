d00a271bd23f7f7b213cb3c4dffdf474ba66f7da
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:40:29 +0000

    limit connected attempt even must

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def multiply_numbers(x, y):
-        return x * y
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
