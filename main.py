8390514c1a5c609483d90a13ac1e84a828f335d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:30:34 +0000

    rush mass band unfair repair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
