6395b0f570fe1b3f98feb71578b903f523419c1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:45:14 +0000

    copy advice achieve come workout

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
