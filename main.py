79afd89dd9f900c39a744ee91340d98412a273f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:07:13 +0000

    careless reasonable common comefrom fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
