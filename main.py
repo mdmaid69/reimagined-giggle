80bb001fa4929057637142751d6db433fd88e4bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:28:45 +0000

    another perhaps decision positive flat

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_work(force, distance):
-        return force * distance
+print([x**2 for x in range(10)])
+def calculate_acceleration(speed, time):
+        return speed / time
