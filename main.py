fe75152b2d0183286c84c16642b9ccd02e4c6820
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:59:22 +0000

    design similar attractive day position

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
