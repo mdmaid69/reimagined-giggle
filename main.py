97d4813fc6d6bfac58848de1f21ed70b512130e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:42:38 +0000

    meeting paper commit glad late

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
