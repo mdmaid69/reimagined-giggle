fd4c9830d687716dbe66a92a29654c936f145b65
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:46:12 +0000

    steal lunch father identify far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def find_unique_words(sentence):
+        return set(sentence.split())
