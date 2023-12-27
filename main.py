08dd79a8dd23d025fe5755809f33e034f983e523
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:21:24 +0000

    swim careful bright polite born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
