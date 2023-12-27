f7cd1b15c1584b2fd00a2cc0135eba213e1b8794
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:00:56 +0000

    later mine build pull like

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def reverse_list(lst):
+        return lst[::-1]
