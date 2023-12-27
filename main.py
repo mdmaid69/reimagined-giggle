bfc654eac0416086e2e38ca743d2a6e69d395204
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:20:37 +0000

    square community ill fine regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
