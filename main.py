73bce236c93b9be2bf9fa8689a9d3903a042bdfe
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:27:12 +0000

    correct excuse keepout himself cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
