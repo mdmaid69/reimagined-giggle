d6b12a1e1ea11d594c2cbf352180a358cce02259
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:22:53 +0000

    gap day hide yourself arrive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
