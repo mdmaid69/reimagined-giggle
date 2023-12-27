e2a989fc9b954c01cd9dd5718f10c2c80a5d5a74
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:00:54 +0000

    front phone certain attach bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
