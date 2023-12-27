3aff52ecda8ef47329529cb93a3fabe2426b2174
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:58:07 +0000

    offer trust even just carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
