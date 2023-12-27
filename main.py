dea8eedbdacf33ca2c7986e39ec419c468880fc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:56:49 +0000

    in dare bang little area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
