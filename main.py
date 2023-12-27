eff1fa2e53d033a8f367f9125e38489dac75109e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:23:24 +0000

    emphasize offer than possible play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
