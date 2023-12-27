772fc5415c19135ad3a1b18be6363df2e90fa546
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:23:29 +0000

    tend check pretty exchange exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def calculate_area_rectangle(l, w):
+        return l * w
