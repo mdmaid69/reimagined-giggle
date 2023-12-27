50a4086ccf8e6aef0ec9f5588f7858924d569ce6
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:16:38 +0000

    dick explain affect fast tradition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
