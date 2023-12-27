6404fccda3404b4fc8682cbe12585fc45980b063
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:21:14 +0000

    hair dig high opinion poison

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import collections
-def create_counter():
-        return collections.Counter()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
