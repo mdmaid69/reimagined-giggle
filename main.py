5c3e330816ab0d209ad3446cd41c0cf02209232d
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:04:06 +0000

    sell detail steal shock value

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
