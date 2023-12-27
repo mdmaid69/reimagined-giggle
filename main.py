e89c71ae6ec5454121fb7825fdebdd3fc8943ecb
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:46:59 +0000

    relative surprise under wear danger

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
