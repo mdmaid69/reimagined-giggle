791a6ef72431e75712a231c8f9973cbe1c39d7e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:50:31 +0000

    count number translate cool getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
