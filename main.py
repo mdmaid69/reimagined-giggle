91f4a09353cfc0f0bf47ec7426cbbbfd3da25386
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:54:51 +0000

    mad vote rich think search

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
