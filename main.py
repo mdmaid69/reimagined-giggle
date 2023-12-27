9ce9ab23203cccf9f83fc686746ec4c9d0cb2482
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:21:55 +0000

    bread law mass platform mistake

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def square_number(x):
-        return x**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_area_circle(r):
+        return 3.14 * r**2
