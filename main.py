894ef0b6a4556aaed886039098339f11689b61df
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:55:29 +0000

    snow comeover grand scream before

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def add_numbers(x, y):
-        return x + y
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
