a198d5c5406ee22626cd3772cf2adb25e2b3e841
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:56:39 +0000

    port travel 566end wash content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
