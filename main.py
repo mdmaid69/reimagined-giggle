e6b8254b04a115d043bfd8be7ecb9f6f106e0db9
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:13:25 +0000

    right easy ago sometimes chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
