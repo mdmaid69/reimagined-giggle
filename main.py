f5800ec75b51014a45179ba4fd8ec4de9a28b3ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:25:42 +0000

    positive advantage judge as unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
