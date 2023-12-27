9a25100b19aaef6f9468cfb65ca649bcd1bcb9fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:57:44 +0000

    active throw run keepup bound

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_prime(n):
+def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
+                if n % i == 0:
+                return False
         return True
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_sine(x):
+        return math.sin(x)
