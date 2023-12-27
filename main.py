c95141ecd94b530145fde052fec4095af3fcd3ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:48:03 +0000

    hurt bet pump callback now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-print(time.time())
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
