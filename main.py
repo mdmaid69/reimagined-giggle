d3bf9ed2a75f44641f63ca077b2a2d821801defd
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:35:06 +0000

    pattern influence handle protection negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
