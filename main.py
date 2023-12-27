12e1b6ef684b8378ff08f0e481476da8bb0e5bb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:31:09 +0000

    fashion negative noise make year

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def get_array_typecode(array):
+        return array.typecode
