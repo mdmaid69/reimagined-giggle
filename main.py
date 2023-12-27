151d81b5b6d40810d71f8271f0acc5493712423d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:58:11 +0000

    secret waste lunch old medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
