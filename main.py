369e2a9cbb51ef6c6695521d27a6043215ad2284
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:35:54 +0000

    left turnout male progress emphasize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+print([x**2 for x in range(10)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
