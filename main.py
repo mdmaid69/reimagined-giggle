2c59a3d90d6d8fb52d755c31a7f038110fd496a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:29:00 +0000

    release connected makeinto muscle private

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
