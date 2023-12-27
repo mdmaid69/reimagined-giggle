2ba0e85bc60550663e8cb69748b4709b8b1c5a2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:06:36 +0000

    outside nice pool upwards interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
