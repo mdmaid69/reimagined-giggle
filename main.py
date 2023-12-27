9388e71287324b454c6428b023ddf9153e0a1df0
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:32:17 +0000

    public gift ignore pronunciation birth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
