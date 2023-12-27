3b6c804dcadd3f62790f745e4cd2058f66462dd7
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:16:47 +0000

    error official ear meeting life

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
