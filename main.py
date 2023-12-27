1bb09865462927036d7ad8e1bde2114cb614fa8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:04:29 +0000

    wash peace influence excuse lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
