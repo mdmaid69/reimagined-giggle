875cfd43814f083c9c0cce01be2bc9d56421e9b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:40:21 +0000

    runinto plenty fall call Ss

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def square_number(x):
+        return x**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
