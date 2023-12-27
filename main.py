2d1da1152552b097f6464fd04c015bd1feeeb146
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:57:55 +0000

    car cut bite zero like

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
