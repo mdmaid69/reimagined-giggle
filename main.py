4430563b2995780e9e13be51174a966780d2122b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:31:42 +0000

    giveout shrink contribute tax season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import os
+def get_current_working_directory():
+        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
