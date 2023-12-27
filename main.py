1249f438cef601b84099b192a4a0e1a845a15bb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:02:32 +0000

    truth conclusion calloff defend minimum

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_str(array):
+        return str(array)
