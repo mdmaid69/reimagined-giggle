54eee0c2d1232229873d267b12f96f2d1bc9a545
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:09:34 +0000

    tie final try wear hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
