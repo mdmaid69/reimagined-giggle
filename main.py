6812cdafdb1e829e614acfa0d4116b9aa7fe168e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:02:02 +0000

    depend compare fall obtain custom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
