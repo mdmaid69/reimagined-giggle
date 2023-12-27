6f1947728e60fb7cc72c5538b5e3d8c1197172c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:25:11 +0000

    573entertainment operation deliberate tool ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
