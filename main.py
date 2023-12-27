64a5f5ea137f95d7073d6df11a3ff272885758f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:10:33 +0000

    urgent tear announce equal all

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
