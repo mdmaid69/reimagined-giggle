f89c8d0e0e53693e4bb1ee59d6570ca889588a85
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:10:08 +0000

    stamp wheel floor it silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
