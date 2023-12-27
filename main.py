e7726d009323e9b4b64c1ab4fb7c39628e628dfa
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:25:24 +0000

    post future dig upstairs private

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
