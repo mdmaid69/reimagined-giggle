a05e1374c61a75bb8b80e0ee09fe57f892d1d07f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:34:37 +0000

    left apply chief since would

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
