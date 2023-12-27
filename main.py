d1d83e9c0ca43005bfa134cd87649deb9295e17d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:29:43 +0000

    pot journey drop month fact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import sys
-print(sys.version)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
