cbef9764e4a9fea96eaa24b439a864a2584ddae7
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:42:13 +0000

    body closed connect pile fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
