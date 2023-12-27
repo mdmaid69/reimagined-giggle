18760baf0d29f3b1b9e5463be7315e0b8a3f6a1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:22:04 +0000

    murder smart stamp that somehow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
