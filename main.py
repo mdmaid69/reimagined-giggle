ddc30febbae5827343381d3ecfc5c2763293a29c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:19:37 +0000

    against service call inform show

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_unique_words(sentence):
+        return set(sentence.split())
+print([x**2 for x in range(10)])
