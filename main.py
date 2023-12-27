91102fbd4837f089b101a814494b63b9b922f63b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:02:24 +0000

    onto vote tour skirt butter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def add_numbers(x, y):
+        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
