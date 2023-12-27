ab434482d27a6f7d033d3abafb444c22df4f2a92
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:15:57 +0000

    tear arrange all limit dangerous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
