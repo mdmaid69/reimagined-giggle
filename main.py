c80e50a2e942307e42d7466b6d4debda9447f8d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:38:44 +0000

    travel later again bury talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+def get_current_working_directory():
+        return os.getcwd()
