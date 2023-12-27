4aa548795a82d4832d68bc5b2a6d27899e451a2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:22:07 +0000

    reduce create breakup breakinto agree

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
