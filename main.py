b2bd4b7daba47d06496010bffdb1063afe2b823b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:09:28 +0000

    bus aim once kick slowly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
