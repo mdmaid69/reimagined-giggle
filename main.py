0f98a4c002c1ff66a314a64f276dfe622a10e2ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:22:33 +0000

    describe cost deal fault interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def cube_number(x):
+        return x**3
