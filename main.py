958eb979305e99ff22ee1a4c45f3739a0bd5e832
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:25:07 +0000

    odd Gg nice activity winter

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
