4d77a9c714232439fd00f8b2a1030b6a8ececf15
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:42:42 +0000

    by grade square mean attitude

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+print("Sum:", sum(numbers))
+import platform
+def get_python_version():
+        return platform.python_version()
