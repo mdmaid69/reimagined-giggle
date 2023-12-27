791354c98d5fa3ef755acadafabaf93bce569a99
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:41:22 +0000

    press produce custom wrap over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
