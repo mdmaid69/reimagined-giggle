b7603560ea4d49bbf5df24a55710f76f748762db
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:10:49 +0000

    your shout average amount save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def convert_to_binary(n):
+        return bin(n)
