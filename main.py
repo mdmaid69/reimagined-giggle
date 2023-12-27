60eb3819395ca23b1386e16406013831419a4dbe
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:42:53 +0000

    account it comeback old correct

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-name = "Python"
-print("Hello,", name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
