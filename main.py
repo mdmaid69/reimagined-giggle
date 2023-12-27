14333f23d7751840c83b85c03bd7beaf8ebbf4d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:45:02 +0000

    goup ball full rain reduce

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
