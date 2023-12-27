877ff217f86a64bbc04da30c7795901d06ed190d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:21:20 +0000

    identify example limit reply door

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
