6822f76d6d35f060ac372fa0d5126a9d2aa395da
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:43:30 +0000

    twist or welcome mad finally

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def convert_to_hex(n):
+        return hex(n)
