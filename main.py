eae6f7ca80d05f48a1fedc19c187e29ce00a5194
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:04:01 +0000

    wall sky march mystery wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
