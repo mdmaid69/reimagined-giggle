9ebcf4b4aeb2c1c6cb507436afe9b1618a4842ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:08:36 +0000

    march mean concentration drop company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_list(array):
+        return list(array)
