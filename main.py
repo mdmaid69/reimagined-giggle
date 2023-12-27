289531844464ab73d864ffaa0985d1d3d1214228
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:15:20 +0000

    reach phone suit comeround welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_unique_words(sentence):
+        return set(sentence.split())
