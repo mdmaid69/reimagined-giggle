6dc7e17b27c91daa678cb06971b08d1969c9f22b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:29:03 +0000

    ago come workon spare neat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
