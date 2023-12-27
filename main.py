89aa88217fdb6007009a2c95fa7f3002e1262763
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:09:20 +0000

    worth find callback knock note

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import platform
+def get_os_info():
+        return platform.uname()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
