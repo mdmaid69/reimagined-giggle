8f598e44cd164ef79f4aac9da966f4ca3bb1e95a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:09:27 +0000

    grateful Cc peaceful roof attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
