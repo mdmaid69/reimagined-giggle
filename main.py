142a874c1e9048210bb567b800b0f18457a5948d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:48:52 +0000

    story wide twist set possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-for i in range(5):
-        print(i)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
