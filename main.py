79781a42d54381ec99de627b28d734c06136a386
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:17:58 +0000

    man exciting influence luck hard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def subtract_numbers(x, y):
+        return x - y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
