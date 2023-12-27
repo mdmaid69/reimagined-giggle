688467fed6172425ff827bb773ed41c5078c7267
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:32:53 +0000

    answer correct minimum direction please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
