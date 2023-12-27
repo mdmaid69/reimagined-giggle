8edcac751a0116c192aed41792a1f4f33cf0ce29
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:35:39 +0000

    door wipe cope design skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
