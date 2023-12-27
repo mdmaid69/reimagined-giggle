86cfd2c6ef1c9693ff78c9ecfd08f5ac98ec33b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:05:17 +0000

    suffer whoever always year border

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
