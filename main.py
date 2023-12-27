57ece0115d2b0757d8428564be26d1d3e2f9f05b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:56:52 +0000

    practise related separate why match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def find_min(lst):
-        return min(lst)
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
