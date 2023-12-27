236a25c489d21ba5b10e5a68e2e33d914636fef0
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:59:02 +0000

    temper afford callout cry attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
