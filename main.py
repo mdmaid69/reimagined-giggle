06be28a2e1f761b2e33e05102066e3b275487009
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:24:40 +0000

    stupid pleasant ear kind difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
