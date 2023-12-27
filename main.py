c86c549a5d8b88ec80aa45bae9bee66399aa8f57
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:55:50 +0000

    past love from trip convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
