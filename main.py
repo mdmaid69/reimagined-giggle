46ed4fb9021ebf04d5658bd4447cde8a87948fc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:38:31 +0000

    call word such measure human

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
