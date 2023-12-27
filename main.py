116d8aeab80d8b01e175d977b6f1490eca73bb98
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:24:41 +0000

    person permission feature worth along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
