6d1f599a37ab59feca6be88d4a18855c15a7575c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:22:58 +0000

    married improve hurt pass patience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
