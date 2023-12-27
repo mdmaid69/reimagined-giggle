ddb1046349636967f402ad80b8389f9739f15416
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:01:03 +0000

    main cost cross touch minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
