c0693379b17907d658ac9b23b7da7f14480b2287
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:52:16 +0000

    kill love sail responsibility must

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
