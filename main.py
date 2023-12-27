abd3d455759ba5750d4f841abccdc9189a35de3e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:01:13 +0000

    computer building rubbish opinion Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-name = "Python"
-print("Hello,", name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
