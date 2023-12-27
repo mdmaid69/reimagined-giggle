02d5b21c3308c31037bc22330adca400895116ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:53:26 +0000

    fire kind active thumb card

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
