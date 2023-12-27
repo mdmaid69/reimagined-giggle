de0dbe224984f6a66319840b65780104f025ec41
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:14:59 +0000

    group road answer want failure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_acceleration(speed, time):
+        return speed / time
