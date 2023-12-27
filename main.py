6480d1dc1dcf1c16e7ca6335729eb63f28987384
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:29:32 +0000

    busy positive match previous return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
