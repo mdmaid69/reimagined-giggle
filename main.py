d8c25f95a12a75809e138fd063e5718e636e75ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:01:57 +0000

    class history from load scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
