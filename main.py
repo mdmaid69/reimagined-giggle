3a36894f8a66dca5b7e37b3971533b26706289ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:50:43 +0000

    shout less community successful takeout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def count_characters(sentence):
+        return len(sentence)
