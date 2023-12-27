3727d8238b7d02a09aa6b89342e533f58d988672
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:34:48 +0000

    manner serve production holdout urgent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
