14aee874920ce8a3cf39b7bb3dbb52439b4f12f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:10:10 +0000

    sign organization now age score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
