b9ac681c5a8c89eca483ab11cd509443190a87ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:15:46 +0000

    breath chat lift flavour complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
