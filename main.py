18f75e89ba09a68e02b37e484b9dcae48c51a83d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:16:56 +0000

    health thing group lose heart

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
