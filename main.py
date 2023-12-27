a8bb9635e1a0be01f2fc44238b98fb30968b58f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:09:40 +0000

    wire mind study move instrument

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
