a49241c1ef4126a7df3041f8950f86614dfea99f
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:14:14 +0000

    direction with generous show such

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
