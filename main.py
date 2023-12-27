1c2e88abe845c18cf88f9287c4224a5a9f6ef6aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:26:24 +0000

    sure kill shop unfair wild

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
