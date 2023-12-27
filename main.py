431555c3dadf65dbf31ae2453bca7fbb9cb9fe4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:28:31 +0000

    part standard bother hair however

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
