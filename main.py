6c0e62f190bd93d8f36b00e32b3b641dbeaa62f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:57:24 +0000

    exchange Vv slight move pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
