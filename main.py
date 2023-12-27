b2caeb844c867b84cfef054350d834b1b37b9b38
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:58:13 +0000

    cheek standard means discussion takeback

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
