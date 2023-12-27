4acc94f8ea4633920b7287ca19bda37cbde2f7f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:53:35 +0000

    still improvement matter building eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
