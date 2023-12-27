1086e417f485dfa09dbe6871f194d585cf698426
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:05:55 +0000

    one fasten 573entertainment approve pin

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def count_elements(lst):
-        return len(lst)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
