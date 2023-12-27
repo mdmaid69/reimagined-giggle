6f2022fbb5ea875f92b9cc38bd46badd87f1fcc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:02:12 +0000

    challenge concentration guess female inside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
