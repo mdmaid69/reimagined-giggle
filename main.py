b962bb945b1800b5eb561aa88e5fe528378c2697
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:39:56 +0000

    state chase trip sit ever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
