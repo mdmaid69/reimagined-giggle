f92b546f23e105e8c5a8fa36315f67729ed74cba
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:58:42 +0000

    between offence unhappy fault collection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
