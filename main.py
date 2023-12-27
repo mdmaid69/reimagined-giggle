d8db290f7851c2f7c5130e6427368bd76390e518
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:29:00 +0000

    meet market acceptable bye up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_list_from_array(array):
+        return array.tolist()
