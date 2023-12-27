25696769f6354e6b2e3443930debfcfca4cb5f06
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:27:03 +0000

    confused balance check doctor since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
