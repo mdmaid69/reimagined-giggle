3be09ef56a3b7f4d3d2223c0de351e5c8265c646
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:19:23 +0000

    unhappy deal practise realize search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
