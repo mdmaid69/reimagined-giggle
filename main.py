b4c479c04cba5203ddba5ef52afbd3b567ef903f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:46:42 +0000

    lost recover serious hold chase

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
