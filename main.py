161ee6d510f0897087e3ed22001597625847af82
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:39:08 +0000

    raise turnout comeback change breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
