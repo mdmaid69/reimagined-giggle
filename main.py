86b21a6850ea60d27588ae5df537c7753da060a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:30:58 +0000

    pump it impressive stupid union

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
