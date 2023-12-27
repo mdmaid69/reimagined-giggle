b11f610c4ecf730585801b6e49ab69ab47a04b94
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:19:46 +0000

    whether autumn left sit answer

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
