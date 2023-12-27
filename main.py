0d060ee89aedb8ea4b59562d67a2014a429e6c90
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:44:20 +0000

    will burst begin world both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
