04ddc5e7d0442887d740b7a7e736dc14fe9a23ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:19:47 +0000

    wherever properly whistle equal rush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def is_palindrome(s):
-        return s == s[::-1]
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
