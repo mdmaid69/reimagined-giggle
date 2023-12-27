c3a1a16ec083950887eceb4ef09b344143405f20
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:57:02 +0000

    understand listenfor why split unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
