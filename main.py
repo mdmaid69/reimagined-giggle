c8e0fdd468ec14255d61064690d958ef8fac3f42
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:56:27 +0000

    present ought boring situation spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
