103a7fec4db2ea53551d3c5deeccb9801902abc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:54:15 +0000

    accident pleased expensive cover right

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
