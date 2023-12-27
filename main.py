035ce9459b44ac34ea4d91235685df18c04a388d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:08:17 +0000

    emphasize either turninto unfortunate sea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_even(n):
+        return n % 2 == 0
