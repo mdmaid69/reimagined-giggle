2327fe5a7c9396b6062b94ccac0992da9abb8b62
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:28:51 +0000

    bring mend birthday wave receive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
