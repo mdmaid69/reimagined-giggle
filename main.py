a4e40a52d8188474aa682a699318de0d6ed20fd4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:53:41 +0000

    ours that takeout score even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
