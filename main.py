80b7dbb7f420f843496cc9be2f3b805eb2cf07ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:31:01 +0000

    takeoff refuse wing back present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
