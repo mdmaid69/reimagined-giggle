e553a7fef126fa048cca11c23595beaae7240ca3
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:23:50 +0000

    capital profit empty vanish watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def subtract_numbers(x, y):
+        return x - y
