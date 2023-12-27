29c1d1645aef7286d1f2c430f2afba8c9a354106
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:54:51 +0000

    invite fly afraid unfortunate after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
