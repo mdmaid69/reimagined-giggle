35581a905397cb7b449831279e6a91e8d6af6cc1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:57:59 +0000

    pleased switch crowded pile breath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
