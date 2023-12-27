086ff1ac3e00ed62e7b4a642be65b54e6d6e25b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:19:03 +0000

    normal characteristic frightened trip form

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
