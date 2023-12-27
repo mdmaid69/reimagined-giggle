df7c6cbfe33b3203dcb6f9a1f060f107638da7cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:47:15 +0000

    its temper street father arm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
