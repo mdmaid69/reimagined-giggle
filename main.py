e23ca4c2d864892959b91f077547d6b63ae46c0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:56:35 +0000

    zero strike bring defend Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
