d42d5e48d66b279107723a60500710deb50c6f91
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:21:29 +0000

    ground sleep dream bush name

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+print("Squared:", [n**2 for n in numbers])
