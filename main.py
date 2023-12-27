60e78b2c3fb36836c1475a5e9650e0b02a5eb324
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:53:39 +0000

    accommodation someone country wave ought

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
