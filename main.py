717dc43ebd2bd1cb7ae62c535997f86f8c3b0291
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:44:44 +0000

    Oo Pp pretend weigh laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
