77b46633b00e79102d6bf1784a44082fa39854c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:51:05 +0000

    evidence hurry ocean bite phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def sort_list(lst):
+        return sorted(lst)
