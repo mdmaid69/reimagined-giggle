08637b3e82a95f08329463caa03098db7eb07c03
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:34:14 +0000

    area lend offence swim anywhere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def count_characters(sentence):
+        return len(sentence)
