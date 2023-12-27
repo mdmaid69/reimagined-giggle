bd87f549c6032fc05ab15ecf90b5d3ae2a5753a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:37:41 +0000

    escape habit sing pause light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
