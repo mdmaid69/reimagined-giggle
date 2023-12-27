d96dc910e5410c6e34d0aba7fdd6faea7f7815fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:13:14 +0000

    sleep lower possible season another

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
