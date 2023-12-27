17418dc70336b0b121ecaf505f1cd65fb2eb94c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:22:28 +0000

    room pleasure sweet connection few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
