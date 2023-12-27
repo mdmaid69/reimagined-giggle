6e3ca28954e459e78f5629df2d0958cd2be1d2ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:48:19 +0000

    kind issue immediately create pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
