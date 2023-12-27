54133898fd11bf645043d0e597be7d31dfb5530c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:51:10 +0000

    seriously nowhere charge edge weak

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
