228f2bb97ae1fb06aede4937b642713d26b977fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:06:29 +0000

    table interrupt day connect bag

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sys
-print(sys.version)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
