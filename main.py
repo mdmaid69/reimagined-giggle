a767c3f91e78297f8db4f30db68f210027a29fbc
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:46:38 +0000

    lead at realize history middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
