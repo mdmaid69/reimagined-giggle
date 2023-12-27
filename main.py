bb4ea2576d56e4d57980dedc329bc7729056b4f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:51:16 +0000

    sink divide stay style meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
