1f61848f1132710f146545b89461ba07185ef823
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:53:06 +0000

    sport noise spread turnaway out

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_min(numbers):
-        return min(numbers)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def is_odd(n):
+        return n % 2 != 0
