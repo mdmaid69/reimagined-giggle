d7fbd743cf6b5762ad12ec419b28662bcabfdeed
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:25:55 +0000

    culture bottom side carry group

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
