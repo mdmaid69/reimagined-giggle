6e448d5313a3cb532ceb6dc037fc82649885321e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:31:48 +0000

    wash moon under after quiet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
