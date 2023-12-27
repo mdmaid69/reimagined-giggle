84136ae86b8f5b56bf29765d2e3f942783ebaa7d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:16:03 +0000

    characteristic idea spot emphasize extremely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
