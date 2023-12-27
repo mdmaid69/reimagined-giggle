1745cee6d375062006b6f8cc6facd87d4f983bd2
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:33:45 +0000

    person limit declare hello eat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+print([x**2 for x in range(10)])
