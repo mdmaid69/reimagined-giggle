0d58845c699875b4e42eb1c4749221ee438d7b44
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:11:23 +0000

    authority point content hot benefit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
