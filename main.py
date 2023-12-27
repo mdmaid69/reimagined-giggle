7d99793467eb8f0cac5872bc450a501ac4295a82
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:38:45 +0000

    minimum pause easy catch clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
