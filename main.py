956685fd45eca95e8fa1640c0674d110a74f28bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:05:28 +0000

    rain unite appearance muscle uponon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_set(array):
+        return set(array)
