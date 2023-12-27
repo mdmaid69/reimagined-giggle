9ce46367895f801899bc833ffd51b5e1ec777502
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:15:48 +0000

    turnoff drink telephone wake object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import sys
+def exit_program():
+        sys.exit()
