4e1b5e1eb142621971a690abf3430261a45ffe86
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:54:10 +0000

    please bush protect commit edge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
