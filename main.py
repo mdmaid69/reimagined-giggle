1a9a689458cd0e776143675ed4d52a0f71b82385
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:07:39 +0000

    promise tip common basis here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
