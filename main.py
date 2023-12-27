43c5aebd1f418e2143c98f4c7f6a1d027315134d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:21:19 +0000

    bar expression storm complaint day

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import sys
+def exit_program():
+        sys.exit()
