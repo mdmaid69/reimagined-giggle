41cce2db37bf66ff45606c1c5461a00e0f71b9a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:28:55 +0000

    house song object recent workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
