39e68f563a2e1aec7bf788c4c3f356475b9581b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:35:57 +0000

    high affect heat book sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
