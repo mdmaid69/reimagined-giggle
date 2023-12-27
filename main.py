904c0caa5dca256d9eb0cc8b94439657a58c334b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:31:32 +0000

    certain together step embarrassing unfair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_perimeter_triangle(a, b, c):
         return a + b + c
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
