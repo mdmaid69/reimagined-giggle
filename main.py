5c4712e87178da1bc069a897e716d2c06fc350ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:36:46 +0000

    disturb keepup slice exact quickly

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def remove_from_array(array, item):
+        array.remove(item)
