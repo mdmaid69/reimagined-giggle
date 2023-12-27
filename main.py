ba69758ed1f32669355ab5b7db66372e2843a8a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:49:12 +0000

    downstairs turn eye accidentally government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
