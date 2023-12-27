91aca96f0bd614232ec7b6e1ac5fef5760cbc221
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:12:32 +0000

    treat fierce year worst minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
