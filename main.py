0bb17b81650e02fb6facd6b807aec5e62a58bcf9
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:24:05 +0000

    exercise tone actually call exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
