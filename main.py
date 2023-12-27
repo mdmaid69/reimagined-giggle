94bdffac6a153de576375e731117e0d69aa588ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:31:03 +0000

    ilovedicks system watch bread importance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_list_from_array(array):
+        return array.tolist()
