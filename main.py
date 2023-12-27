edda6489a38c8b20722fd07bc26686a66407ec71
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:45:12 +0000

    regular complain offer summer organization

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_speed(distance, time):
+        return distance / time
