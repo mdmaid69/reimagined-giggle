20fc9179e3b45e26c7b32a395c9ab2415ab51649
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:43:49 +0000

    ashamed home climb positive Mm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def count_elements(lst):
-        return len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
