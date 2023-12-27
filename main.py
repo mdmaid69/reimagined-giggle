ce2d82604c382298d852ffb19b0f76bbfeaa39eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:52:39 +0000

    declare interrupt build connection tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
