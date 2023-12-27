59d0cfb3762d4e802b492610da4b556f03fecb0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:00:34 +0000

    tap powerful trousers secret sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
