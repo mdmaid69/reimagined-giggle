aaada8c72f88e99bcd0bb55b790b182d325ffe56
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:58:37 +0000

    neither violent work show estimate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import platform
+def get_python_version():
+        return platform.python_version()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
