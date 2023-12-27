50a0d1613e6628524157093698432ec0b7209de9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:21:52 +0000

    standard present kind Ee centimetre

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
