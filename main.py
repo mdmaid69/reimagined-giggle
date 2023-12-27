2af157ae6ff5400bfca42004300d1f0e56196496
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:59:52 +0000

    search majority cross later relief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
