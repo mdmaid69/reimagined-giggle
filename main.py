c8f49d9fdb26bc89759685a7cc4c10b9b6231fba
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:35:36 +0000

    about plan out thought line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
