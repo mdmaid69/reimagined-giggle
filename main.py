8601bbbd16949f7d57c0a161aa0b101f9c8589b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:23:38 +0000

    acceptable issue keepfrom goup wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
