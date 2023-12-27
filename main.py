78c32b4814b4889d44acdbaaa2ba7cec6e146a84
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:36:43 +0000

    wet steep blow expression learn

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
