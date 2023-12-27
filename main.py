d0f05e35f6b72ffae39b800773e0416df9a66aa4
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:24:37 +0000

    giveout star for sort protect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
