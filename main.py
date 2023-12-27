11574f9c664e2249a44cc547b767d7af61fef586
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:30:28 +0000

    comment people consist calm deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def count_words(sentence):
+        return len(sentence.split())
