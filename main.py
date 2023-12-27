fc7d93d3f17dbd7827087c6918c749073c7787dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:14:33 +0000

    getaway 573entertainment slightly everything disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
