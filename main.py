ba230d7d4b74f914577eda520b5e646950daa324
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:34:22 +0000

    practical some recent whistle completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
