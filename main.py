58644ae09ad20cff5970eb720b6f699cb781fde5
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:48:50 +0000

    hand name nature gooff burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
