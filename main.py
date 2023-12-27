78e21d88dc7ec387bfb78a0d285ce4a5e39c7d64
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:30:01 +0000

    fast ever curious feeling whenever

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_str(array):
+        return str(array)
