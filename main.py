5cea8683c9cc2e710fd8f55a6472f0b1c8fe14b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:33:17 +0000

    flight breakdown skirt pollution limit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
