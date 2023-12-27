3e83a9eb8819e513fd37afb32436ce841ce73d9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:35:23 +0000

    home genuine gas most desperate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
