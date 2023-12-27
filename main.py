8f84ec533feaac842eca5f374c62a8d5ee534e60
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:42:10 +0000

    clue strong explanation majority relative

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-for i in range(5):
-        print(i)
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
