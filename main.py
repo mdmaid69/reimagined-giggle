b2751139af12e0535a85bfb462f563d041410ba2
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:27:48 +0000

    still shine exact trousers survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
