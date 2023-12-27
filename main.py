647ff7a7b5e3bb96b47fbdeca7583502c57fbc24
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:42:52 +0000

    lost introduction community Vv read

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
