5ff6bf5e6e7aa32298bc3d00600e7c5ef8662206
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:04:57 +0000

    under like slow rush stamp

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+from collections import Counter
+print(Counter("hello world"))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
