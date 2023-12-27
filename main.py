3b5b7f542d2466e610a145cff8e1fd05bd24404f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:14:21 +0000

    obtain squash tense prize finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
