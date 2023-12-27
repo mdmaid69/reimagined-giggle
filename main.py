612cb023f09a1ec5bcaac9b3661339ea1f40c758
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:33:51 +0000

    finally depend than carry straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def cube_number(x):
+        return x**3
+import sys
+def exit_program():
+        sys.exit()
