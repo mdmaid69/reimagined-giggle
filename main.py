69814108f463f4cc926b531074ec3ae3b0d630cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:17:09 +0000

    sharp save once gather busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
