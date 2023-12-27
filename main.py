a05f47b9cfebe93b2576b15a97a6ae524de77d6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:58:42 +0000

    thinkover traffic afraid powerful effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
