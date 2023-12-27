635b35456fbe069b9d5e52b49144309a9ea27180
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:27:09 +0000

    strict gentle disturb blue line

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
