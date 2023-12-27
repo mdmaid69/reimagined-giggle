be10b2e48db5d54a8893a0d2f5688520f4616c0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:27:52 +0000

    Jj language bet record ball

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
