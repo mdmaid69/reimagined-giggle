4040c20772dfba6c3d63e3287ff46160adb489a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:00:08 +0000

    rescue arm time once familiar

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import platform
+def get_python_version():
+        return platform.python_version()
