e0b7ca8e8d5cd5b9652cbe73933e216900e4bbec
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:11:10 +0000

    crime demonstrate fairly last rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_pressure(force, area):
-        return force / area
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
