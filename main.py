856b1191bfa24d3ba8bcb3cb8dccfcd6d753fce3
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:37:31 +0000

    it understand Jj nose discovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
