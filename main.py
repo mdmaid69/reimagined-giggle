0beaf9ba9e24b48d30dabf98ff86b55ea13d3cb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:01:55 +0000

    recover which figure stranger solve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
