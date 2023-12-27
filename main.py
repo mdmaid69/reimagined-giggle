c0f9474e29c88dd9703a4ca38814734103f8a01c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:52:16 +0000

    operate sky laugh effort gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
