6f2e5cba8f7e70abc4b8b7149fb15aef31cfc761
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:46:39 +0000

    remark stand whistle name piece

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
