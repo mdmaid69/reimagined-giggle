d1285c35115a9b74a7ca0e57cbfe8791ad26d437
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:18:31 +0000

    mail mind food major offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
