ec0fc1b65c0c5952575bd25228a390dbedba8bf8
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:53:48 +0000

    rain quick favourite unfortunate past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_int(array):
+        return int(array[0])
