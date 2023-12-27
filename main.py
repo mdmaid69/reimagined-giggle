75d908597d557129c1b3539fd170cc1fdae39c1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:30:28 +0000

    principle edge characteristic smart grow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def find_max(numbers):
-        return max(numbers)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
