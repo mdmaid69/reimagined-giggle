d509c4292daa4802dbbbc80c3db11896ac9192b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:28:24 +0000

    succeed position break usually send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def extend_array(array, iterable):
+        array.extend(iterable)
