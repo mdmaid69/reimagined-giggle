a8251ce18ef76f7b3fa1bccebe598e91c852e9f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:00:09 +0000

    tie from big hurry dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
