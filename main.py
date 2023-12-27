3db2879ce133c5a8df09300e8596ec27d7976136
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:21:13 +0000

    always section air slowly telephone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_current_directory():
+        return os.getcwd()
+print(sum(range(10)))
