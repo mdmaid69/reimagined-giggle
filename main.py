3721dd6b68edf3c0b203a61addabd37cb4739e02
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:46:30 +0000

    also means already television relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
