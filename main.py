2141dfae354c18cda3a460f0a19a2e98ae05f3c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:52:23 +0000

    takeback contrast freedom yours clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
