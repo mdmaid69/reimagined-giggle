e42128960594cb6e63dae7151ad9cc5befd25aad
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:57:22 +0000

    eat strength trial together measure

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
