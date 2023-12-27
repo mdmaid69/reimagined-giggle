c0602c78a388623167bda1fd904be21203b234df
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:37:45 +0000

    earn return approve Ww sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
