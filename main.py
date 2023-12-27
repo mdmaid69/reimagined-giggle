ae238c0081dec1a6dc310999c0ce88e81bdf1d73
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:16:50 +0000

    divide anxious avoid proud whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
