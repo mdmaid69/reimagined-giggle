c7b2ebd04d089262bf1b53c00995484694aeb032
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:51:11 +0000

    comfortable makeup top club health

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
