8233df3629a8070c7ddc692f425d870efe3bb6c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:35:33 +0000

    profit call window sound slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_float(array):
+        return float(array[0])
