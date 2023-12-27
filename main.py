6fa9754258079079acb970b46c0bd8b5425cff16
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:08:24 +0000

    gift hour Rr appear turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+print(math.pi)
