0aa95868800d8446e34282364d6bba0c4d5e2a76
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:28:37 +0000

    temperature complicated bottom after twist

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def subtract_numbers(x, y):
+        return x - y
