c985e6dfdddd028407e529ed7037466f93ea3231
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:54:50 +0000

    guess fresh Uu improve complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import os
+def remove_directory(path):
+        os.rmdir(path)
