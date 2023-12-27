59914247985fe548d0672a31b68524dc54ff24c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:45:52 +0000

    none cause defeat little any

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
