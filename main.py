f222a0a481e2ccf4450fd38253fa88b25ab4219e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:11:22 +0000

    until Ll look rank lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_frozenset(array):
+        return frozenset(array)
