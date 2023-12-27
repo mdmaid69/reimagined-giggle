6ecf87f28f30dc002c9eef9f13a339595c50c69f
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:16:16 +0000

    floor safe without him roll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
