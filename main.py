f8325db3ad8b2cab05ef1ac650234c3e37d0abfd
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:29:50 +0000

    bury basic interview happy rather

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
