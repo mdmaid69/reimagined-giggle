bf92602cfd0288529bca38cc313b6a0238eaf935
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:32:02 +0000

    short actual declaration majority stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def cube_number(x):
-        return x**3
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def square_number(x):
+        return x**2
