23ce933837403cbe21c1615e0b82a079596a8ca0
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:27:48 +0000

    lucky say strict trousers perform

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import os
+def change_working_directory(path):
+        os.chdir(path)
