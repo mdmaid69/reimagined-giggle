6017d06d2c31c54c23c1c518574fd8321ca23c8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:19:09 +0000

    slight injury bottom make same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import os
+print(os.getcwd())
