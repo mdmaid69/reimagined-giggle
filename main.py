1e3cca113a96ac8246ff40b9c4a00c82f7d0f7cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:36:58 +0000

    blind pretty direct rush murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def convert_to_hex(n):
-        return hex(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
