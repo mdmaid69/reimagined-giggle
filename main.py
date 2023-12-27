188c58aff5011cecc42e8b58b3332952a71aaf84
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:34:12 +0000

    tense worth team both instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_directory_name(path):
+        return os.path.dirname(path)
