0996525f05dc6471e4ba44dc6b119b12d9f2180b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:50:44 +0000

    reduce most over woman spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
