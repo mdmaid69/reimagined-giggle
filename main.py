3f32d5436e39c88c4fba829293beb8dd65777a3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:21:36 +0000

    joke giveback fire project contents

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
