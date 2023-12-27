b6750615dfeeccca02b117ce3e44af2205b2f0d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:43:40 +0000

    minute which well study explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import platform
+def get_python_version():
+        return platform.python_version()
