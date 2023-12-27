dc375887aa563dc549544393cd7a0d526f79166d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:21:34 +0000

    exist beneath murder thick squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
