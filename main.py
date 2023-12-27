2c64b4e689e8ee7658fdb14421e7f416be7ad7a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:56:03 +0000

    insist flight pressure respect far

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
