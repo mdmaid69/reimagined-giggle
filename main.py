1b11bb8f258437a76cd7c23bc9901a4d26a25e2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:56:00 +0000

    clear lesson pipe offend change

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_power(work, time):
-        return work / time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
