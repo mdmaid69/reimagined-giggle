ccaf0d4840e3f0cd0650034efd172aa6b72a909f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:30:28 +0000

    calm surprised either event unless

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
