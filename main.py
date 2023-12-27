7fdcfad3363aa326d640c2a7c272513316acf798
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:36:47 +0000

    plate swallow afterwards rough stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
