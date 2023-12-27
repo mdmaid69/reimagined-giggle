3396aca87350e6788272db2742bd1069f034fc49
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:11:27 +0000

    bath serve love affect exact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
