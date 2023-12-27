d413fd90ee290d6f89c56387496ffdf4188414b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:20:46 +0000

    match occur computer makeup rarely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
