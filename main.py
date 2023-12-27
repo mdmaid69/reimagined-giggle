385b7aa26dacfd15aecb4489d85beca5ecee15e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:11:14 +0000

    rule shall share degree confident

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
