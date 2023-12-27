3d2034ec3a97488341f92a0839466812e89bc776
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:54:57 +0000

    gradually since door communicate new

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import logging
+def log_message(message):
+        logging.info(message)
