ce994ba90d8a3f6b12406d13dc82bb4bbf9b81c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:51:04 +0000

    tall traffic firm profit heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
