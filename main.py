f305f6b248823ec6f32637ce9a0b766487cd4eae
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:01:01 +0000

    print count no noone instead

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
