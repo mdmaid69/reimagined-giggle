1a36e6bc673bb4218f2e0d55b23fd89c57ff8dca
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:17:43 +0000

    service large belong scrape ours

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
