7628f2bc0b1c322cf85503591c1e97dbfe608282
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:15:07 +0000

    defend injury service along in

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
