81f33edcbefddf964fd4b9f98abbfc56b92701d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:36:07 +0000

    emotion shout turninto correction land

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
