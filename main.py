d92a8aec80a85adef2bb8d78407734e19aeab810
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:09:43 +0000

    programme load high service ticket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
