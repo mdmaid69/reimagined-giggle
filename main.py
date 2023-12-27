d01ddf5e7f8511ea6e2c2f012228f6336d605bca
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:54:06 +0000

    thanks though getaway could equal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def remove_duplicates(lst):
+        return list(set(lst))
