292084f0f54a12d087dd00f943213986712a1dd3
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:44:16 +0000

    related cry properly weak Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
