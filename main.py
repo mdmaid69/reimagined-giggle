32ec9cf74e00a636ca0b71aeab2ff8ee78238246
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:37:18 +0000

    anywayanyhow success flavour downstairs usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
