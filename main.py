4a16c353458c04140f0eee27821dea3ebc8580a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:46:22 +0000

    seat unusual sister luck people

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
