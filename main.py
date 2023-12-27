8f5f59be0e24ea47d972030c0ce45fd4e2194d61
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:38:47 +0000

    relax total sister foreign town

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
