8a83d67afe96f85a31dfa1e4986fb10555b1babd
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:07:13 +0000

    upwards issue little principle freedom

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def is_palindrome(s):
+        return s == s[::-1]
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
