c70b63dcd0b9114946a6d7cc2bc25e5f751cd5e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:54:07 +0000

    holiday risk mail empty run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
