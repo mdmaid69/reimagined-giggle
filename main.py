476dc5c3bcb2c1643a4c2f15cedb228370dc8ce5
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:38:39 +0000

    kill relationship search rest bring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+for i in range(10): print(i)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
