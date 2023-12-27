ad65cd35c8affe19fd3f949b16113c9f00046e79
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:24:57 +0000

    keepaway giveout risk blow spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import datetime
+print(datetime.datetime.now())
