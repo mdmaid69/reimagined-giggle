5d959b63db9647b921f83349dd7d9283992caf5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:12:28 +0000

    turnup Ee busy face whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
