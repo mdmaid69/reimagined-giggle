c5aa0edb152d4ac2483f0925b9884f7d2f1a194b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:42:15 +0000

    past exercise smooth broad second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
