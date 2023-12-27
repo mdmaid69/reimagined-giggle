26513c5d2c1cc1900ccf3be42d972201e6634001
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:38:00 +0000

    plate apart knowledge comeoff burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
