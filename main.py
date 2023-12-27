e905b36da7aeae16b69eb3deae3114539764e86f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:31:55 +0000

    into right tonight patient bend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
