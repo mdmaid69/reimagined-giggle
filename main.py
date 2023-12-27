27f9778892385db8ab8a5b14f87333c7eb173b04
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:54:05 +0000

    evening declaration season deal fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
