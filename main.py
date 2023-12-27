0d4814c2253d2a046c40beb21e1667d6a6c2655b
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:45:18 +0000

    Rr pull punishment person neither

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
