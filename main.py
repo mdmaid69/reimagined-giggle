e1b3c68eb56c7942efc7988eb9c459516d96c4a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:22:10 +0000

    avoid reaction all claim move

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print("Hello, world!")
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
