b8f0ef31b9aecedd0d3abfb7e9c32b9cb2eb9a16
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:33:31 +0000

    pocket admire whisper evening soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+x = 10
+y = 20
+print("Sum:", x + y)
