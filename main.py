588ca29564097051f0d4d02d64dd739373ad153c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:58:42 +0000

    edge the sense realize pollution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
