eba406927e27cc44cb50501d8c68a28670ddd850
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:49:30 +0000

    within character evidence smoke towards

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
