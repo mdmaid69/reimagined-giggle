2e239e434e5fc746226e0af4b62d8a6e541ae1cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:10:28 +0000

    innocent blame cook must rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
