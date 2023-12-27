66679e0a1097a09b14e1408f57315dde8d1af411
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:34:00 +0000

    pressure model away talk science

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def is_even(n):
-        return n % 2 == 0
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_speed(distance, time):
+        return distance / time
