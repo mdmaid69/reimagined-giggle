e3b17bfca1c936352aba8adc5f37e98c3ed8d2df
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:32:48 +0000

    present floor ever 565end pretty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
