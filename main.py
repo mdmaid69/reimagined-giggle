e260f2d0f086cef0f7675912a9fdb7d23fbc41e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:56:22 +0000

    breakup give maximum Pp someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  def square_number(x):
+        return x**2
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def iterate_over_array(array):
+        for item in array:
+        print(item)
