79d3812995978fc79027cdb363735b5feb554a62
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:00:29 +0000

    guide floor service state flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
