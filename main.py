3aac037bced754ea067731daa69c4f40731eeca0
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:42:53 +0000

    breakinto lucky turnon bright base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def reverse_list(lst):
+        return lst[::-1]
