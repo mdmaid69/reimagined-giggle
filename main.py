fa55274fba59b6efee5ec38c49ba0771508dfb80
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:13:55 +0000

    artificial answer content danger confuse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
