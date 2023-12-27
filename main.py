6ba2d8514e20c6eeb96e83251b8a84e93c34aa45
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:15:30 +0000

    why attractive it whistle obtain

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
+  def cube_number(x):
+        return x**3
