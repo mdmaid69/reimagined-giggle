9dc3eb9160a2901d22442c744ff229810829dcb9
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:22:16 +0000

    key responsibility satisfactory idea sharp

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
