e647fb5dfb54baceee188b5c86ea6a0657a02f62
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:59:42 +0000

    beat period serious interrupt convincing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def is_odd(n):
+        return n % 2 != 0
