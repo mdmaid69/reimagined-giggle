924ed9f7b5dc62151359f0f503c9e345af1656ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:39:48 +0000

    nearly means risk react measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        return sys.version
