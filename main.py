621ffb3a611a0d71949fc8d129090e093c000b07
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:53:53 +0000

    book Ss whoever obtain creature

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def convert_to_octal(n):
+        return oct(n)
