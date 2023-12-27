d3a222a7d7574ded05d17c7c9b92dcbdb14e62af
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:46:30 +0000

    subject blue public class with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-x = 10
-y = 20
-print("Sum:", x + y)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
