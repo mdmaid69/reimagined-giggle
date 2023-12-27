59e0541bc76392f03006a9ce04febe139131ca9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:53:32 +0000

    meet lookaround song yours cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
