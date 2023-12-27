2e01d12a3477abb7fcaf4dedbe6c56ff3850fce3
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:10:02 +0000

    sink practical land that undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
