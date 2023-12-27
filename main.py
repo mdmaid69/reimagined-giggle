e2852fe86bba6584ebaef71e58059c063df4c6dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:07:22 +0000

    pleased neat admire group on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
