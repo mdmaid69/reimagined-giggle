695fca784a13db5f5b13d76c7123f0920f96a903
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:38:17 +0000

    anywayanyhow pass step lay involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_directory_name(path):
+        return os.path.dirname(path)
