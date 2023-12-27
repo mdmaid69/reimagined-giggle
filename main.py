1637f1d02a3de0d0f142e72ab464080685cb6355
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:56:56 +0000

    plain tip embarrassing include strong

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def convert_to_binary(n):
+        return bin(n)
