7211e78f41efb1be666ab5f8ca7ad8b0c35d181e
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:51:22 +0000

    bang its no order modern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
