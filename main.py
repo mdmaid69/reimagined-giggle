3a1debbb52780f8101d293ccd799169871934b05
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:29:28 +0000

    fun easy now delight keepout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
