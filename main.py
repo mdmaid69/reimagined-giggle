a44a50b9920d8b3272e189a979215ac1f16f7fb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:56:17 +0000

    to opposite manage software clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
