6211d8701522c8bd2303987504cb466747b57b41
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:52:00 +0000

    round expect free police familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
