73544941d6c81cab3d9a48054b805260bb782b05
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:04:14 +0000

    least smooth stop night accident

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
