cfa8f4785d6194130ed325b3ecbd6748ebf905b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:04:00 +0000

    body pinch everyone book theirs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
