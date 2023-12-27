36d80fcbe926e25f9b76adfe2cebb7e81d36147e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:53:32 +0000

    declaration pleasure fierce face tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def reverse_list(lst):
+        return lst[::-1]
