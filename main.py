6441ebe4c69d4e9cd5f2f1f2016274e7dd97b990
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:07:39 +0000

    beneath scrap holdon previous away

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
+import sys
+def print_python_version():
+        print(sys.version)
