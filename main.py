d72ee0b863717da4d47bc1c46a4629d98de2a095
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:08:47 +0000

    fly punishment over run empty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def is_odd(n):
+        return n % 2 != 0
+import collections
+def create_user_dict():
+        return collections.UserDict()
