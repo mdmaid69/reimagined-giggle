9aee5497a67ae8879f67a59482f8cc40b8a882c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:59:42 +0000

    trade battery fast long goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
