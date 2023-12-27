dc0b42b54614da3ae0e213f6acd0a184b0678f5b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:12:00 +0000

    degree false hurt dream sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
