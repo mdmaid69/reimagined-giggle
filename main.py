a430cc27745879d672696de650fecc4f2c0100a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:45:46 +0000

    at weak thought jump quickly

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
