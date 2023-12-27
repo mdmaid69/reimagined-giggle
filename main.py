4ae0884a8858ec54740cfb59c3a8a207bc644567
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:49:50 +0000

    send dream exam dry second

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
