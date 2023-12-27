82d28d8cd62f087a6c54568d37ef5aa6e411243b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:25:08 +0000

    relax arrive to ear modern

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
