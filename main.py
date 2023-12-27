17a3892473f4103d7cae720dbc7d77238ba1d76d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:59:52 +0000

    man indeed smoke top relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def cube_number(x):
+        return x**3
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
