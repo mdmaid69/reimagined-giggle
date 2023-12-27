072773062f5fba245e247d6d0e4ad2863fbc5b08
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:47:51 +0000

    battery step recover model relationship

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
