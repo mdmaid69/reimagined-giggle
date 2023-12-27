19679d74c28a48d7ac49ee8786503078705ead47
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:04:19 +0000

    entrance pattern approve building project

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import logging
+def log_message(message):
+        logging.info(message)
