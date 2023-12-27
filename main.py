b668bbdf24052e42babba3958f18945ea8189275
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:21:49 +0000

    downstairs chief definite Pp film

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def square_number(x):
+        return x**2
