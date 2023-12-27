163bcc6dc3638a9c0105dca0f1e6e420928b2e7d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:27:31 +0000

    artificial coffee minimum cap conclusion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def divide_numbers(x, y):
-        return x / y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import os
+print(os.getcwd())
