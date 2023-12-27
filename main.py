5f1d1f6831627db7997f41f2bf9b6823670b63c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:24:26 +0000

    feed steer notice sense behaviour

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
