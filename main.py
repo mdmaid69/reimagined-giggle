7ca39ff512fd3bffdb1fda8edbebb081f65239ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:46:33 +0000

    able warm letter no patient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
