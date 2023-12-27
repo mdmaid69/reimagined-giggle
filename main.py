b23d55af2ed4ec8e66a5305c915395dca2d73f92
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:25:26 +0000

    soak here takeaway mend search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
