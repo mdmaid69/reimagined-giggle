c4d100c90a5893f899d806594c7836c275edd020
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:58:20 +0000

    appeal work shall reply visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
