ad2139ab6d8e071e589360bfe5d183ac771a452e
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:32:18 +0000

    unlucky priority smell attention charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import time
+def get_current_time():
+        return time.time()
