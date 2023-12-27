8b1a799f34937523a472dede8e6a41882e040e32
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:43:19 +0000

    definite introduction stretch lookaround store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
