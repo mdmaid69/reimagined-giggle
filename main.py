265d62119ac5eef10bfeaafe17c1cc3d71ed4db6
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:43:42 +0000

    near arrangement not beat pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
