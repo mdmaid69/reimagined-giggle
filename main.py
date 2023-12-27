ca424003b06627c6ef4f2704626f8a8cb2cdcc26
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:12:30 +0000

    disturb weigh it protest remark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
