86a968be4a52d99a3a5d4d35be2fd5a45247dbfa
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:57:29 +0000

    pleasant stone transport month really

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import json
+def convert_to_json(data):
+        return json.dumps(data)
