a58447ad6de4328cd2aec07993b8f70aac9c3b1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:00:57 +0000

    as fine chief convince this

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_ceiling(x):
+        return math.ceil(x)
