38fe4a9b1142617e6e7d9bec9607d1ebb9e7c0a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:39:05 +0000

    beneath promise desperate politics inform

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-from collections import Counter
-print(Counter("hello world"))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
