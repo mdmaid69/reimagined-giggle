ecd38554f2015bc9b554ab8ad25c7fd3321fc4b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:22:24 +0000

    improvement declare pleasure stop connected

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
