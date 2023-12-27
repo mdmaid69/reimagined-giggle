979fc0c6a8526a3f72b4a5c7bb54a29b75dfbe18
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:21:08 +0000

    declare crowded comfort test regional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def find_max(lst):
+        return max(lst)
