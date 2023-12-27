c578aeefe961785708d6ffab6edcd8b58d60a3bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:40:33 +0000

    defeat bad disturb official confuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_length(array):
+        return len(array)
