70eff030900cf1a48896ef1642ca5d16f607c3ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:16:28 +0000

    depend foot new hit firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import getpass
+def get_username():
+        return getpass.getuser()
