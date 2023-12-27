3999a78f87bbdcc651ecf7b4a40cab8f88925638
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:51:03 +0000

    subject mail poison open traffic

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
