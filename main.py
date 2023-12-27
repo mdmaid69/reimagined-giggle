743d9027d2d953b9edbb35564cc0483dbfed7fb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:20:52 +0000

    medium opinion table trick serious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def reverse_list(lst):
-        return lst[::-1]
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
