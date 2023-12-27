7e3c93ce82a96114cedf66006b4f118b8936396f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:17:17 +0000

    protection turninto shoulder ready that

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def divide_numbers(x, y):
+        return x / y
