6f132871ab7e7a5e381a95ddc90a757c9e2cd800
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:59:29 +0000

    support goon busy brain way

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_rectangle(l, w):
-        return l * w
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
