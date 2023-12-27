810c611ac54dcb675133a26df9eec934f862d371
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:09:03 +0000

    task patience language tooth building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_stack():
+        return collections.deque()
