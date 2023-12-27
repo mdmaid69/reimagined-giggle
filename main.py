da8d26a31cc5f513d395414187bb8a17911a07df
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:47:17 +0000

    may towards above thinkof noise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
