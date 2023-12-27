fc5f1766e97bfb6283ac0ac95ccb823246c5c5f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:40:09 +0000

    permission confused goout answer air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
