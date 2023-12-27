f29ac7d6f1aedce70e24b45b5afd9bf0b6ed713f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:01:27 +0000

    winter wash load male refuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
