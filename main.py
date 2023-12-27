5cd9f2240e27ce2ab947f44a77cb77001a0cf688
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:31:05 +0000

    themselves regret introduction lean like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
