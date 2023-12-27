59755a92a53d889e06bdf0f739f83b32c70cde8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:34:33 +0000

    complete change lock correct then

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-print(random.randint(0, 100))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
