1a207842f6449102ab1b929a04f1be9c6b17b0d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:03:18 +0000

    catch sleep thorough lookaround band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
