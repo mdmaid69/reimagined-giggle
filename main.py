b1cf766a35f882b4763c9f49c701cc08ff327ce9
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:24:39 +0000

    excellent blue ago pool various

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_set(array):
+        return set(array)
