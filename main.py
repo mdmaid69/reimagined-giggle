59b7305cee4ce85c7c5da411f42e011f77fb8501
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:31:00 +0000

    hungry lost hour beneath always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
