f68eb0cc02cfe05b75a0e235bf96b2bd5695b7fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:21:16 +0000

    rescue breakin stir example thick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
