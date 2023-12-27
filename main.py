d21b7256e54ca39ac533b9ebeca38f3cb52d2d7d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:58:05 +0000

    discovery sit cry exciting together

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-print([x**2 for x in range(10)])
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
