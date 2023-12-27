0cedf3bf014da2e18d976ca9072b650f446efdae
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:37:02 +0000

    dear advanced refuse somebody base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
