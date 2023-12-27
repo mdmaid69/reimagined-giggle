c1f5443321f1ec55667c249846d06732f9e21683
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:35:12 +0000

    them mess left light between

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import math
-print(math.pi)
-for i in range(10): print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
