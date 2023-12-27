e16088647b3aab2ccc425db5543cea82fc940fab
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:56:44 +0000

    tense permission into win possibly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
