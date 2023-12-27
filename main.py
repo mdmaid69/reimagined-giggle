cc41c83aae8c834fbc1739acbba89f00b30bfbaa
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:01:39 +0000

    home course weekend bottom defend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
