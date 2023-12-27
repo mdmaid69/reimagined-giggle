8f5b74be6846902b164f3ca54feccd963ad970b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:04:55 +0000

    better workon weekend sure nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
