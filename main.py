7903dd1e7f82e484471194d8b94d72b3adb5c06f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:47:31 +0000

    somebody squeeze spin issue safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
