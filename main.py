e631e7dbbeb625207b1a9073e56514a5bd7761c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:14:33 +0000

    breakinto opinion regional busy objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
