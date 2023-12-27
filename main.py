cff0a37808639269a589aafbddc21b9fee6f27e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:33:52 +0000

    rush borrow conversation correct odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def close_database_connection(connection):
+        connection.close()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
