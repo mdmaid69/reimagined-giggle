58d75085cd978a96d5a51eaffd969057411fb601
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:03:44 +0000

    deserve even organize central minor

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def reverse_string(s):
+        return s[::-1]
