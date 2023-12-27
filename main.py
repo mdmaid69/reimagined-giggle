ff120da16be3aa3489e43c4effeea352d716948e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:43:01 +0000

    allow because fresh sea cheat

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def is_odd(n):
+        return n % 2 != 0
+def find_min(lst):
+        return min(lst)
