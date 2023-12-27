91bf8cf8d1bace0d40b299c15c405d742b6e31f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:52:08 +0000

    shrink follow turninto something cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
