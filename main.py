3416c19056bebd9c6fb32e9d70deb75bc7fa2171
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:01:21 +0000

    benefit somehow raise smell cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
