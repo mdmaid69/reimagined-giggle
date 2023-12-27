bcc9071d700cb9b5de95eaf587e9444a5c95818f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:39:53 +0000

    572enter face quantity advanced ride

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
