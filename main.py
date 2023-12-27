e93347541273055029520e56dcd1b4113ea39531
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:58:21 +0000

    size splash describe spray entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
