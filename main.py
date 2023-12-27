b7f6bb851bad6d0cc3bdcd67fc2dcdc44ceb60b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:23:17 +0000

    cap train course bang contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
