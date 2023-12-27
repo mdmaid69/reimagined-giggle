3bb0ada8661b7a3d90ab8c75f1e84c462ee9b1da
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:10:49 +0000

    of loan after nowhere material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
