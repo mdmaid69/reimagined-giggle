a69f2a00f0b20f1f22651f5416e02a24d81dc913
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:33:14 +0000

    attention modern contact duty unkind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
