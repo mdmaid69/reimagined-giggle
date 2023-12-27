040c14e1fa3debaa8cf78e8a851308daa93cc616
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:45:29 +0000

    under remember public such marry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_exponential(x):
+        return math.exp(x)
