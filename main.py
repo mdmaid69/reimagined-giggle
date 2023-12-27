4360a3506fde03eab32f4397fc9bed72012cc4c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:45:44 +0000

    pretty solve piece spare room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
