78e5ff3eb421683030778181d98ddf72fd834372
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:23:40 +0000

    figure problem I confused replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
