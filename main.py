a22d5d864c7af36e4a858b296ae5cbb44a017962
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:15:50 +0000

    phone ours brown declare earn

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
