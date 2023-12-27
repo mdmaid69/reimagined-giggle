c6af8aabf4bb802ce271ab69ce73df6fe5ed68f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:17:16 +0000

    coat honest house view various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
