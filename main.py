6eeb84c52857057ad46519f3281fadec9cea8d29
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:33:03 +0000

    conversation entry also copy interfere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
