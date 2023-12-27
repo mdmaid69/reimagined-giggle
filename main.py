30bd7632abcc675db6564ad80622b9a4b80e921f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:59:02 +0000

    breakout terrible pour fail still

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def convert_to_binary(n):
+        return bin(n)
+def is_odd(n):
+        return n % 2 != 0
