370e0fb7e6ffb77fa47c09a0a5a66237909043ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:07:22 +0000

    getout breakthrough national cause figure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-print(math.pi)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
