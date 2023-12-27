2efa5e57c6faef218b6c1de6011487ea7de7fbf2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:48:53 +0000

    Ee perfect city wash survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
