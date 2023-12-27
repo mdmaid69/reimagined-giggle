92fb0b11ab1f4b8e40600f93b8e24915e1f747cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:42:07 +0000

    tap visit occur tour foreign

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
