8e318f3f92e94eb7ff734c99305c157bd30a6eab
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:03:24 +0000

    black sea suit true hear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
