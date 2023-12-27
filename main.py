9c53215f250901a0d2d1b9863e002a83d7482449
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:09:02 +0000

    instead reduction provide urgent confident

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+x = 10
+y = 20
+print("Sum:", x + y)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
