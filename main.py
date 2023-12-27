59e7c1d558117a9b541320f69e756af5de4d127f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:30:48 +0000

    name little thank equipment image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
