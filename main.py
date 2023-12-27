411cd7e838ae6313e9668ec557a140c5de3067e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:54:04 +0000

    terrible defend goodbye comein slightly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
