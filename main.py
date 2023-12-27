2cc1b2384af25f05c336934dddc2ac9083963a93
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:37:53 +0000

    comein part steep children whether

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
