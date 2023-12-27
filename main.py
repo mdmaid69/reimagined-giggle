6eae3800b1985ab2130ec238bcc813d8c0bea04f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:49:40 +0000

    Ee left copy hope war

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-print(math.pi)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
