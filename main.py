eb6dab98720e2a49b87a9b95cfa4c06ae6547beb
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:53:35 +0000

    total shall better yet last

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
