9be01f7ab3bb847dd9c7c10ea481579493e5fc03
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:37:41 +0000

    question suck suit pencil sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
