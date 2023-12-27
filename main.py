210e95f9e960fab3e8863a406a88223544c1fbef
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:47:50 +0000

    it across himself soak because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
