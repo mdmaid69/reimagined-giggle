4460c71b798913ca44c26a1f93a7e286de9c9125
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:14:36 +0000

    reach that several lift write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def get_array_index(array, item):
+        return array.index(item)
