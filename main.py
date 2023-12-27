bdfcd2323aa47dfdec5e63baf31887ed05aa80b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:15:46 +0000

    estimate ever answer hide keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
