70598cd29f5b883fbfb70f749a683ee939338c9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:34 +0000

    listen strong might realize curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
