e8b232d528c461ac8849fc8f6c812e001ff945cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:46:24 +0000

    minimum everything now out ring

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def square_number(x):
-        return x**2
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
