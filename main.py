aa2065c58b5cb4e91dd1036ae0ab8a75518db5ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:13:30 +0000

    house pen rob breakup length

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
