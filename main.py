f393909284946d46fbb4b960895ecd5f6d868d05
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:29:35 +0000

    upset chat impression I accident

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_typecode(array):
+        return array.typecode
