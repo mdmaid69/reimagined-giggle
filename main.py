6f334f397b19d11bb9c33e6fb20ce0041c0172a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:02:05 +0000

    hide story opposite colour bin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def cube_number(x):
-        return x**3
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
