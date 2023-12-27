97c03b4942def3d8382bbf429834434f8f7bf8e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:05:14 +0000

    copy charge whether fasten minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_typecode(array):
+        return array.typecode
