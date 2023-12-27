3f577915e53af1c0f2bf357287f5076397e21692
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:27:47 +0000

    below gap block disturbing fill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
