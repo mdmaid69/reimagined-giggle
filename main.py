473a52d4aecb678a05fef70888cc048f9badb720
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:51:50 +0000

    dry weather bet cough powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import numpy as np
+print(np.array([1, 2, 3]))
