150098449f3a752474716a339a572cc12cdf7c33
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:41:13 +0000

    share swimming allow play greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
