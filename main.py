1531b85c7a534913f914156e442527cafef2fb81
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:54:31 +0000

    whole thanks rule think unable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import re
+print(re.match("h.*o", "hello world"))
