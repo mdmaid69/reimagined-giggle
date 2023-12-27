a2b1f8e5f1a0589057bfcf1e83e9dcb993ddde37
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:33:36 +0000

    even money speech joke design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def find_max(lst):
-        return max(lst)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
