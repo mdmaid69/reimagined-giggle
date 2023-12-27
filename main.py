68530d29ba10bf492c353e7667bb93f809a0df9d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:50:33 +0000

    certain wide blind firm little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
