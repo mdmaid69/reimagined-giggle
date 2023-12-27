64e40c6e7c123927a198299e7df21e61d66dec40
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:41:57 +0000

    equipment quiet block water exploration

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
