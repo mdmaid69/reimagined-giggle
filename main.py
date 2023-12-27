4367dad22614918b22d433d01b77c6a75f17f6e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:25:18 +0000

    lean record able set trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def find_max(lst):
-        return max(lst)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
