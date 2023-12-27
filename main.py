ed3f36016b66acd7e13281bfea7309dab04d12e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:56:49 +0000

    project opportunity listen edge forget

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
