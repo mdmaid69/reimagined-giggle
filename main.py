d29841e29e0be3c64a6cf6bd84df3cf4f7408f7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:13:16 +0000

    report generally turndown early exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
