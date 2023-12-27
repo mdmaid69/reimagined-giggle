0d62c5af1a45c54703f132115caa4ab6921a9bbb
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:39:27 +0000

    beneath string pipe cope there

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
