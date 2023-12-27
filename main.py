ba677aac35e8242005de1044d8d682f536cc5fac
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:12:22 +0000

    final sound state typical compare

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
