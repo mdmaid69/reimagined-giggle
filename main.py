68592376a1e7c6dcf18d20f342d3cdb669339dc0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:11:59 +0000

    bit night desperate home bury

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
