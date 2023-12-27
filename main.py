d3e294eebdb9237bd8bdda0378161b07d768f5d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:52:54 +0000

    still winter hand every suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import numpy as np
+print(np.array([1, 2, 3]))
