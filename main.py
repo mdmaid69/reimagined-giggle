2514c987af4ce364ae75427caf4808cdb97f2fd2
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:46:00 +0000

    bank harm innocent everyone stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
+def is_odd(n):
+        return n % 2 != 0
