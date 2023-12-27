31854ad8f671069ec4840f4efe65af7acde89f58
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:36:11 +0000

    me office typical turn meal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
+import os
+def change_working_directory(path):
+        os.chdir(path)
