e17e19cefe0e6d84cab17fa6424bc0840364542d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:19:57 +0000

    rub repeat turnback get programme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
