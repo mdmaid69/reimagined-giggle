4fc21f71a08322eb2dd4e9ad05edfd626b65fda2
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:05:20 +0000

    sell confidence shape apparently knowledge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
