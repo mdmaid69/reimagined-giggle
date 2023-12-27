fd541780840ede4ada5366a62a16d828afdd48c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:58:10 +0000

    matter lean air punishment length

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def divide_numbers(x, y):
-        return x / y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
