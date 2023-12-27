cc2521688fd96f810f89bb9a9efd1dc1c5be18ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:45:06 +0000

    yesterday bringforward typical remember eat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_typecode(array):
+        return array.typecode
