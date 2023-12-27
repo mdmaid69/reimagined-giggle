2455681847d58800969ffa552e3a771c004cf7d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:59:32 +0000

    match wake together take rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def change_working_directory(path):
+        os.chdir(path)
