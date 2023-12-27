9e62d68e014c3db379888ca1035237f2f242de84
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:11:25 +0000

    surprising normal charge performance extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
