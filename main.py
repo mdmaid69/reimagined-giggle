99a833c5f07a33bc89a406a5d055d2de177d5ee2
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:53:56 +0000

    makeinto comefrom leave indeed watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
