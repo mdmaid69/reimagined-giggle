7e2467b89bc0448da3bf1860d75849005fceae26
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:29:35 +0000

    style some circle pronounce thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import sys
+def exit_program():
+        sys.exit()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
