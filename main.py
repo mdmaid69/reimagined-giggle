4cec1f3cd511582666c2725250fd9a7e8f8808a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:22:41 +0000

    improve unless tool rest in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
