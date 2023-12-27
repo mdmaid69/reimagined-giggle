4ae72f600dafe02a2ed127d71174b504cd70f3be
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:05:17 +0000

    organization shame equal fast house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
