3a0f4b07231d10d5fea3993184a646bcdf5d398d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:34:31 +0000

    little even method yourself part

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
