8d37fcd2e865a33830efcfab6ae0a856bee257cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:23:51 +0000

    record perfect too on supply

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
