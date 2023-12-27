df34655a036dc77811b90c2ad67d01fa1c9424db
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:19:06 +0000

    sick come wish crowded very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
