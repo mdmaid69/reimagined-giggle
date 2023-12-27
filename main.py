14779b7d7eb5dcfb53909153671649a999af4266
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:13:19 +0000

    announce colour breakout possibly improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
