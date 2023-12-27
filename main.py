8be12746b9ffa8b1ee13e274095b2512d9f8e59a
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:24:20 +0000

    really store excuse pool card

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
