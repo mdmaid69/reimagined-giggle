e6243ab6a9101dc2a814e8ccf7d37258332a7967
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:55:33 +0000

    stage female lock hurry in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
