a1dcdae2f823f46a5ebb2bc4304e0feeb5277923
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:28:18 +0000

    unable unlikely clean flood fasten

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
