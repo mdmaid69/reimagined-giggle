3bfc60b6777455e7eaa456014698833efd4539e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:06:45 +0000

    joke official angry recent wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_pressure(force, area):
+        return force / area
