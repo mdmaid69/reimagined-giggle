71d36da56ed5d0a17c4d434435cefb2b56eeef96
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:06:51 +0000

    stop month information gate collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
