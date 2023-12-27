d3bbe756a3db710d2063b77b0b381808ab223a04
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:37:37 +0000

    try gather community task reduce

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def find_min(lst):
+        return min(lst)
