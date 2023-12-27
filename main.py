ceef2ba0260b0138ab2b8b69e07f33f90986c03c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:43:35 +0000

    book set always area after

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
