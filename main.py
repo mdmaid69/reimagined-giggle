dedb7bc419aeb576e2ecabb420a02b7e961332dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:36:28 +0000

    anybody thankyou community tidy bringback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
