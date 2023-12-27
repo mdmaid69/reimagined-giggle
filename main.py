65c45c5f1343a24f2d8c51dbde0fa538fb3360c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:50:12 +0000

    disagree weigh platform back strict

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
