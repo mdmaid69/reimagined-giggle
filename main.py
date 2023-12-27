d5b90ac46a936f1177bc0f0fbe931ee8e3a87870
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:07:02 +0000

    offend force shall price subject

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print("Hello, world!")
-  def is_even(n):
-        return n % 2 == 0
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
