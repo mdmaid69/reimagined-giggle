4ffc547c4bc0d176d7cdbfc1f284e9fdc3428751
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:10:28 +0000

    town advantage force one past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
