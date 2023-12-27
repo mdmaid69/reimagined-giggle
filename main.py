e1afcc4414c3060ec4fc27f34661a5ddb29f14f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:08:47 +0000

    straight lookaround steep base last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def convert_to_hex(n):
+        return hex(n)
