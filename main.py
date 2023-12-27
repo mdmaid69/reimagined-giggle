fc357479495aa2526ff32d7f8b6049f875951ebd
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:03:28 +0000

    nothing damage flavour takeover explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
