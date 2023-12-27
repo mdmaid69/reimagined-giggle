aae63d4a63d721e1a452edd15568aa875f9d359e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:48:02 +0000

    rather dish party patience vanish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
