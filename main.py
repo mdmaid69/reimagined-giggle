a94c2c7d3dc2c0c4c6faae4859fe9fc3ecacd736
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:19:09 +0000

    progress thinkover properly write within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import collections
-def create_priority_queue():
-        return collections.deque()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
