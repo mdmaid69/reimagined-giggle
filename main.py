58f6e70472b8a1c1c169b6600f4060a6b3c4dde3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:25:39 +0000

    study relative anxious ashamed pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
