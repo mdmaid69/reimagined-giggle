16e5638452b756b902dd8d8becafc28f3bbd9508
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:52:51 +0000

    accordingto foreign plenty explain train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
