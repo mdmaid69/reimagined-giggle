bba7d24a43eb1a19404fd35545cd3ca3e8eced94
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:11:15 +0000

    unkind remove case emphasize command

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
