c29a7fb61c3b754d03c6df244dd9b021dd2bc62b
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:58:41 +0000

    head Ff pupil belief attention

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
