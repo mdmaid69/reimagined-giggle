e4d67f95b468b4ffce3ba7ff97ca34d4ead9d8f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:32:33 +0000

    pencil shape advice Pp attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
