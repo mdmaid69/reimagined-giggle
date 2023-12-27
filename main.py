eccb16b3b6a548f16cb4ef76e9e3d49e6cd59bd4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:31:13 +0000

    sort police not fire aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
