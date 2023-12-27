6415d0a62029138583e6e5399e8bb98f0d52aa7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:20:15 +0000

    bread line scheme getaway no

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
