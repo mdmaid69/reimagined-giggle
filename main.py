8b6ffb83e5bf2f6995a8cc7ed3abef98a0ee78e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:37:24 +0000

    thirsty fasten lunch wherever correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def square_number(x):
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def square_number(x):
         return x**2
