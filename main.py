62ef613a7ee9ddac008af48da0874be34cb0ab41
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:20:41 +0000

    obvious dance prize ticket comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
