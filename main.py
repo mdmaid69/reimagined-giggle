000d353d2c96d0a56df0fbb4813a8d6d5a5e110f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:07:00 +0000

    store treat ear direct field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_repr(array):
+        return repr(array)
