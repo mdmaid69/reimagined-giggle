8d7cbcbf4f47cf890c488a7a91ecf2e0c2800274
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:12:31 +0000

    congratulate rub appeal equipment project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
