2a56412da3978c47780782fcba81f93ff489d641
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:12:38 +0000

    care bath important either party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
