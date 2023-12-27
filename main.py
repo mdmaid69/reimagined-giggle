339cc9d7cf71c95f0d0b731a46c021784008cbb7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:01:55 +0000

    slide cost rank tap bag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
 def get_array_from_list(list, typecode):
         return array.array(typecode, list)
