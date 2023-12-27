a06933c8713729c0eac496eef642c94e47dcaebd
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:26:31 +0000

    trick see win high open

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def add_numbers(x, y):
+        return x + y
