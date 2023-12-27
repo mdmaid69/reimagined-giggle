07d68b84dc1c7dd4277e00caf4ee4fc0ba82dea5
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:27:31 +0000

    exhibition describe poor throw bad

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
