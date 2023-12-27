d72f5bd933f19db3248afbd878446bdc860b92f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:36:44 +0000

    in worse imagination have crazy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
