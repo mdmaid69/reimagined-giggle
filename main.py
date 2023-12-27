968c6f9bf4995d475fda2376c69405370b9253a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:05:28 +0000

    phone circle except careful rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_cosine(x):
+        return math.cos(x)
