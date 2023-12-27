ae30dab64b53ebc4708d01f0be450f7c4943780b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:59:26 +0000

    small bored big lunch father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
