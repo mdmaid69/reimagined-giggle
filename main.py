efd7d22ba10ed34892204bda2b765383f08c3361
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:03:11 +0000

    getback note to artificial scene

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
