0109f497369a3c96d065ea186f49c443c3f89349
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:45:43 +0000

    account class appearance table fairly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_directory_name(path):
+        return os.path.dirname(path)
