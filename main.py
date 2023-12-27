212e3b03cde8e21358ca29837123261aba3c20eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:47:25 +0000

    possibly soil guide stamp otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
