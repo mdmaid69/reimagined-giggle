41c6f72e681ae96b0eee196c67411d746b51fb0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:51:26 +0000

    high love disappear scare physical

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-print(datetime.datetime.now())
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
