52370fa0b6b3b72c8820f34fad60215b71602318
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:10:58 +0000

    judge prefer party commit store

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def get_list_from_array(array):
+        return array.tolist()
