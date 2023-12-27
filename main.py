c03b062bad1ca240370b617510c963b10f60fee3
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:21:30 +0000

    over pool past pour we

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
