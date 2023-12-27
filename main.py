970f59d93d71450227daa412d86a3115c535b6ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:07:45 +0000

    Cc impatient peaceful father spray

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
