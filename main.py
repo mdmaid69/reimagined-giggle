d5683441d0056bbf3044cd7830f6a529c4d70652
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:51:02 +0000

    mess tip now Bb tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
