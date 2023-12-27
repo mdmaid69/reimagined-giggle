bceba1e60a4d6500182297f4de4155d172001017
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:24:57 +0000

    fall willing show set Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
