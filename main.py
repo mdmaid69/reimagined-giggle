fccafd24b9f95083da4254832757417b2f71dc53
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:32:58 +0000

    card switch course replace sure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
