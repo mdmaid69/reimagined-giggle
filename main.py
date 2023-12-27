965864f9154fdac939e166aedd92dbc97747cffa
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:01:14 +0000

    wipe frighten race afford surface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
