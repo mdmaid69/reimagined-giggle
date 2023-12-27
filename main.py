046d02eed8897a2ad0bdad372b16ee195b4fc7ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:04:09 +0000

    run debt service defeat dig

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+print(math.pi)
