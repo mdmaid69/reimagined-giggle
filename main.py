2a7dc222a73a65fe0f770a85cef2ab8e880b36d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:03:35 +0000

    unable up moon dance complaint

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
