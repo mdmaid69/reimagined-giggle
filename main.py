ba40dfb62885a821055f75d8efa6af82037d0558
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:56:53 +0000

    chin gradually goout consist sweet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def find_max(numbers):
+        return max(numbers)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
