5f099ccac7052ab08eb352d8020385cac7572f44
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:36:32 +0000

    runover remove bright interrupt comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
