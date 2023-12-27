8a9cf5017e3104ebc64231053045ec4fb3e2231d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:48:41 +0000

    snow vote cruel sure plate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
