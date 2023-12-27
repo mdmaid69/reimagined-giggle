5ed921c7afa873d1cb8c5a96af0a97cd51b1d7a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:29:01 +0000

    very pipe react release mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
