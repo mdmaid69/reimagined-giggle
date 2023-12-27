1544afe061bfde2bb1257e86620c22e5873bd522
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:06:03 +0000

    interruption right sure gate exam

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
