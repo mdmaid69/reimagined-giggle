0cfeec4907db3302bd4420107ea1155d62db5ba9
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:49:57 +0000

    praise giveback different flow manner

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import numpy as np
+print(np.array([1, 2, 3]))
