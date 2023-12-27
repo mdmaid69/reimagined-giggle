25809e47446f5290e50d3c6c489f09d606b35b13
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:24:14 +0000

    the challenge listen intend interest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
