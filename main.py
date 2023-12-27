e12c043f1d8dd81a4a22cfcdc8f624f978c84776
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:11:28 +0000

    far name phone love pretty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import time
-print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
