0b99c7c9aac6cf96a0cc3d26f419d5cc5cd907da
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:55:29 +0000

    soon school next type complete

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_acceleration(speed, time):
+        return speed / time
