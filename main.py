44fecfc306ca63ebdb2818ba2c1a0f6df96551e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:08:25 +0000

    breakin dream fact close mouth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
