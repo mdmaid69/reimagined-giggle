e52e1242736c5fd034fbb657de90374093ee1a43
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:11:37 +0000

    past majority tense grateful bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
