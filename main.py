4be94df4908e96fc62c04a2e416281d07a48ee5c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:38:38 +0000

    ring clue inquire view cause

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def get_array_as_bytes(array):
+        return bytes(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
