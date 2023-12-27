644f952c181da496adc432a9b4fb35ffcd23bfbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:07:43 +0000

    owe instruction prefer one second

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_combinations(n, k):
+        return math.comb(n, k)
