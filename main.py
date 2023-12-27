0cea07437dc6bdb6a27f8e552f357976fb677f22
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:28:15 +0000

    everywhere delight performance work inside

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
