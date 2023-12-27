88a80f96ea3603f6457aa134e3873b8b0f3e098f
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:35:46 +0000

    impression fit recover ashamed move

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
