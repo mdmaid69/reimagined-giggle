4c828c1d1a4cb16590e482353f6ba16445aab190
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:06:07 +0000

    mine maintain watch either whistle

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello, world!")
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
