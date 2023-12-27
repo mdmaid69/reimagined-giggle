da983663aeb2d41bbaa9db77575c5b03cd609708
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:56:18 +0000

    judge protect level impressed related

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_area_rectangle(l, w):
+        return l * w
