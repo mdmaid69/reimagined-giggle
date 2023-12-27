f76d2339b83d7ab48086cb1166ddc589264d4278
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:45:20 +0000

    push square central bunch Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import sys
-def exit_program():
-        sys.exit()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
