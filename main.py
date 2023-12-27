ee73337d93279dbf11d7b02cf86d73f49390e51e
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:52:14 +0000

    aware release when before nervous

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
