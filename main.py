23113115e18b7903bc7ee5381a9e88ac8ab58cf1
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:12:17 +0000

    large production react carry cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
