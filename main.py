04e8892ea268dfa80cdb60e0109e9b470740cb9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:00:34 +0000

    gap ocean prize steady tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
