a8294369d1192277d7270dd1b339381459a2ae93
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:35:07 +0000

    child past noone funny plenty

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
