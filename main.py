b69e6631c63c9b5d44bd67bf2cfd0915fd880256
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:00:25 +0000

    work path honest straight air

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
