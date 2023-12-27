7d9ccf0cb55d0a6eca52ead30cf11f5f0da8a73f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:06:02 +0000

    good black free fortunately style

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-for i in range(10): print(i)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
