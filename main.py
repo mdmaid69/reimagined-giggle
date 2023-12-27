070b012b6e444cb2bed16565d54fa2870372b431
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:00:48 +0000

    say connected figure phone imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
