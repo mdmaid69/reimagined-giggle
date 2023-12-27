808f6fc8f00c91e5836aabd36ef7714296b360d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:44:05 +0000

    come laugh attention day some

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
