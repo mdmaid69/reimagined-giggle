5cadd121b3cb77cff27d0fd409bd5db8f57b81ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:16:20 +0000

    attempt bet different sure set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
