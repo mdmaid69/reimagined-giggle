b3e0535c6a72c4e834572451b8a89e21bceb4861
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:17:54 +0000

    worst holdon ahead mine study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_circle_area(radius):
+        return math.pi * radius**2
