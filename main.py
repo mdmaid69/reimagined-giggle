2ec08d63500b66a51c8753a6cf2848f8b9ea2a02
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:13:35 +0000

    form acceptable sort genuine oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
