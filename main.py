d69cd84e014e26186d10c08442e1aeabb4757bc6
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:20:33 +0000

    content criticism minimum stamp sky

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-def print_python_version():
-        return sys.version
+def find_min(lst):
+        return min(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
