0bcc3cbb3c6b06b74d2c1b81e7b22fc4f5896aa2
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:31:00 +0000

    tip drop underneath zero hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
