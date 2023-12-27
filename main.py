9d9dd39b4075d196e73953b57817de96e694cf3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:33:26 +0000

    ours freedom inform meaning warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def count_characters(sentence):
+        return len(sentence)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
