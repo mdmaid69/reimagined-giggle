8681aa77ee972b273355a5f20872260fbf3887a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:01:50 +0000

    control myself first forever regret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def reverse_array(array):
+        array.reverse()
