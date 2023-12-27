8972741d8be703f0159f5a8e77e3f4f781c5717d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:50:34 +0000

    save strike nerve complaint I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
