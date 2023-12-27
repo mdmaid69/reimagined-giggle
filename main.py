b1189633c41b94a2d221bd79c4b4250f570c5d2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:51:35 +0000

    conversation government section creature pity

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
