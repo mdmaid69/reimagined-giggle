1b193373d1ae7efda10a1037b6ec1ee22313f10c
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:35:08 +0000

    pity condition realize permission few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
