47e659a6de5ad86eef3caa127602752c951633f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:16:07 +0000

    expression sea scrape nurse and

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
