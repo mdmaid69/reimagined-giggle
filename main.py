20a558907cee116cd662349af182fab843cb83d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:04:51 +0000

    treat upwards keepup ordinary splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_tangent(x):
+        return math.tan(x)
