3ec1cf4f372f073fe0139f40cd7a6cf2ad0b409e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:52:47 +0000

    willing proper 572enter add exploration

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
