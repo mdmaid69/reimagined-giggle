dc07c8e290213cc9dec9647130ac8393f39a2d30
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:43:47 +0000

    fierce sick average relation afternoon

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
