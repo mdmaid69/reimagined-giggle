8faa9b2f2e7269073e123f088eb1eb3becb2b335
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:52:55 +0000

    possession Itself dead sweet voice

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
