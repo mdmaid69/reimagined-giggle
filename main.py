e671884d773cfa11aa66fb43932d0abae163ecd3
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:29:54 +0000

    whom meeting switch pull loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
