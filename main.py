63777c33e0c4c750a8a5f4038709c83f8b083f2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:31:15 +0000

    talk lose influence relation passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
