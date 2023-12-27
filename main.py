6e08b5cbfccc54f78f581aa6ba306ee929c31bc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:45:19 +0000

    excuse so schedule divide instruction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
