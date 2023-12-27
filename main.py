b241ae57c949005e314fb813834e68b2b280b642
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:04:11 +0000

    make surprised proud sort convince

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
