e21906266dad10787938464ea8793b49c9583312
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:42:44 +0000

    doubt petrol rest fix song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def count_elements(lst):
+        return len(lst)
