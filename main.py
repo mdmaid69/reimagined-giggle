ff5c896bfd7cdb01c31abef7b218ad8114e3c5e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:13:41 +0000

    principle female learn us upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_even(n):
-        return n % 2 == 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
