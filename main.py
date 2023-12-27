bb07c5ab2fbd633ec4db651ca5cffb044347966a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:03:50 +0000

    past smile little man matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
