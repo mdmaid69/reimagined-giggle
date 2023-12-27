1e88717b2c5c062b458d86213738950fe3b19073
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:47:24 +0000

    pool maintain various shoot aware

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def subtract_numbers(x, y):
-        return x - y
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
