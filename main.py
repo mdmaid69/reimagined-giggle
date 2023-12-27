53a8a6894593f09a93ecab17d533babdc51bffb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:19:03 +0000

    cut custom image disappear bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
