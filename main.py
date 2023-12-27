3bea9d7113fff1324841ad60d25495a3b22fd97a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:59:11 +0000

    charge complete ear increase draw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
