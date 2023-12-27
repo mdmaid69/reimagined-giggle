74595a9db8d160b8fdfbb5a4b352f190d8d5e513
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:31:16 +0000

    gradually scrap declaration past refer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
