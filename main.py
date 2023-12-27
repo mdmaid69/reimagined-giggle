aaafe0fb1bf15ff8cc5b347815d13ea4c5729b8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:00:42 +0000

    although suggestion name passenger lunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import sys
-def exit_program():
-        sys.exit()
+def calculate_work(force, distance):
+        return force * distance
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
