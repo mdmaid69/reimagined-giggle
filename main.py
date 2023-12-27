8af78441df9d2bd11606aaf3cd4eae97ced9b50e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:25:44 +0000

    choose surface tax hand peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
