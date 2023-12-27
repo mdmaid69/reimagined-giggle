ecf9f41b7d358bbae215e9370bb6197c4d065a2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:52:17 +0000

    now machine extra ability relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def reverse_list(lst):
+        return lst[::-1]
