1c6dee0b59ec7300a80f21f48f78742be397d64e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:07:34 +0000

    pin confident beautiful serve insist

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_speed(distance, time):
+        return distance / time
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
