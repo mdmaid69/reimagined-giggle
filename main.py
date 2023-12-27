70162c7c30d9d5b31f778fdede986f483a9ac1d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:21:39 +0000

    prove blind fly extra exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
