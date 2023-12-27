7c18d7310076a9879fabde521f08b5a8bcd588ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:40:33 +0000

    holdout trap dinner heat tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
