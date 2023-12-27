a4190907315c8dfb8e3763c2bd24313b9ebb4634
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:19:19 +0000

    sticky many begin trip peaceful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def reverse_list(lst):
+        return lst[::-1]
