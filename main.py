f9380b8b754ae8d82ca123cb5e084782775f5d69
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:48:57 +0000

    agreement left subject backwards plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
