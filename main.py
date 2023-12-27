9dbfd67888487ce60d78b96fa5bef633e828c6dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:54:31 +0000

    private interfere bright communication life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
 import array
-def reverse_array(array):
-        array.reverse()
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
