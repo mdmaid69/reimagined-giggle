806ebafe8ead696bd2c8625ce9c919607601d7d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:14:20 +0000

    star Yy takeback wake drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
