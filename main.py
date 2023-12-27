eea0464f561acf210bed69bdeb9f8a01db9fe59d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:23:53 +0000

    steep wash imagine term light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def square_number(x):
+        return x**2
