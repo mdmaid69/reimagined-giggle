6cfe7008f837cf7fe6188816807a5a51614845ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:11:04 +0000

    delight takeover begin win part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def add_numbers(x, y):
-        return x + y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
