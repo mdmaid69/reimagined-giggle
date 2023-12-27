7db44d107046740233b986e3131116d9323e69c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:33:39 +0000

    worst difference connected sleep congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-print("Hello, world!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
