619988406db9857e172bb43afc1b9fc8eecc5efb
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:01:32 +0000

    real bin whole sure disgusting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
