a057ed3ee89f2de5217bd849129d3ca4078ef58c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:06:14 +0000

    callup dry turn open crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
