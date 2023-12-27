10c49008eab3506f8ab3f73cc7f718d7315fd945
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:19:01 +0000

    dance street break joke control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
