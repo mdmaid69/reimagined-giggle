057e5245c77dd3457e3fb9fa0d366f36dc39b33f
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:19:57 +0000

    height interrupt we naturally believe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
