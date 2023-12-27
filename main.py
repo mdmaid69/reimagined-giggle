564e33dc590c6fce0aad255d2cf9df5a63499083
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:58:56 +0000

    afford meeting rush moment colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
