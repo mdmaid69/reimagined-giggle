69c602379a95cc7957d3dd1eb61090e827aafa90
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:02:59 +0000

    desperate cool hungry turnon air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+print(random.randint(0, 100))
