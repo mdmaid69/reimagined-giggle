a340aa1dde4639ecef08bc4225a0f02f06ce6efe
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:31:42 +0000

    pronunciation mend mark drawing wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
