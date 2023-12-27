943a230cb72992619848357335c939744e778df5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:09:03 +0000

    pair in level anybody keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
