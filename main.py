65a184d014235c6a2d488294bfbb154a26b5a4e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:41:52 +0000

    where create regional around than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def square_number(x):
+        return x**2
