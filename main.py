c76a680ab31ab3146a2663fd0b38607c00d124b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:04:05 +0000

    bringup steam impossible real sort

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
-print(random.randint(0, 100))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+for i in range(10): print(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
