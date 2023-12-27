91676294817f700253552c28a50ac0f7f818e981
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:39:08 +0000

    soon disappear do clean exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
