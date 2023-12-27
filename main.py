50518ba8e953537581540d17f645d3aa9fc02b79
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:38:50 +0000

    leg callback anything call leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
