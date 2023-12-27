27613967db16e9d10b88558422f8c371f24d6d74
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:42:20 +0000

    visit afterwards forgive either abroad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-print(sum(range(10)))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
