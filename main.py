aceb35cd04380beb2a3157ec7a7fd365b1668168
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:29:54 +0000

    introduction report suddenly touch coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
