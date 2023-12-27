a8c8d4b75d29ec3daf6b713148b745c3b92a846f
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:08:22 +0000

    workupto handle service thank deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def count_elements(lst):
-        return len(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
