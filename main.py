22a588fe74bbfd8cd2885530e24d248f7d2b7673
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:03:53 +0000

    hour carry plan somewhere nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
