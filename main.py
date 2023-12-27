343a4fcaed443aa51a48d64e2e7447f079455b38
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:56:18 +0000

    late prove wheel authority interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import sys
+def print_python_version():
+        print(sys.version)
+import sys
+def exit_program():
+        sys.exit()
