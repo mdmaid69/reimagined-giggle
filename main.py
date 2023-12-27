4bd1943023e8ca97e006d7848acb4610feff8f3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:47:05 +0000

    wind create adjective stiff normal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_odd(n):
-        return n % 2 != 0
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
