5f97277c4f7b08c4972cd467cc80ada279409ad0
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:00:33 +0000

    second birth us its habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
