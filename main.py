e46f48fcd28d9daa126cf696867c2c97c5b604af
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:35:52 +0000

    solve follow direct think sticky

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
