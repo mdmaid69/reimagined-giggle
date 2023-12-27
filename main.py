5f8b3b93cde9a3bcb3ebfc329a31f8c6b4927110
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:36:35 +0000

    murder sticky crowded emergency afraid

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def convert_to_octal(n):
+        return oct(n)
