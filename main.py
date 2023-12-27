52904d23ce0b6a81cb5c3c2d0631941978d4e900
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:15:19 +0000

    disapprove set or radio giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_density(mass, volume):
+        return mass / volume
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_bytes_from_array(array):
+        return array.tobytes()
