eb22e1226a12f209ce78b0b81b88255c3dc263e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:59:21 +0000

    available division modern organize disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
