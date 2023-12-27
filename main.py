bb85da981cd6dbcd01d3a61e190b98049367252c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:01:03 +0000

    operation deal bus obtain yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
