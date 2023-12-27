c623fede2c504837afc148c4aa78eb6aea8bdb9e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:57:00 +0000

    story your bored tap fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import time
+def get_time_since_epoch():
+        return time.time()
+import array
+def get_array_as_repr(array):
+        return repr(array)
