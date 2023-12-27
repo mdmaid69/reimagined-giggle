ec117b5177449827dbfe765af00b76ba5b2a13e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:51:35 +0000

    since shake trip fat further

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
