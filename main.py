a1ae6c7f260f525133f7c71870693e14555e5db4
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:16:16 +0000

    main hold for sort squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
