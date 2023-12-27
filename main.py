8fa4f1ac681544030bebb78804a32cfc5e6a3241
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:34:28 +0000

    mail discussion bang announce name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
