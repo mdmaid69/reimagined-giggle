bd2c643c432a5c1c824af17b7f853523ab85e85c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:00:15 +0000

    drink damp talk adjective pure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
