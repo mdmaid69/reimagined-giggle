424e977e3583139c50df2e14f3ff494c70d626d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:07:27 +0000

    brush criticize slip tie change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
