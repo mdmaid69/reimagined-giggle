6faa70171195fd72269fd36eb39b637148e10527
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:52:13 +0000

    hit satisfied door contact many

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import random
+print(random.randint(0, 100))
