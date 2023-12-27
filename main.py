1280c2d736ffb8b4b687a707249d6e08e982098f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:10:20 +0000

    that glad whatever gradually comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
