cc56bea483b20d912f4facfe3172c8ee291db68c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:03:52 +0000

    secret independent Yy front produce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_min(numbers):
+        return min(numbers)
