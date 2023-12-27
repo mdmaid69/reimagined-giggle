c3e7d64a335a8ad1203d6d5d760eac485b0e9add
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:42:33 +0000

    treat match shade high Mm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
