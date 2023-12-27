53320ef4b9f91eb3260edbc1b451ea71885b1588
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:24:53 +0000

    ashamed build cross fasten complicated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def sort_list(lst):
-        return sorted(lst)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
