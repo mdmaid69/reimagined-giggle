2f81c8bd86980b08dfe43a1627cfe09091012f74
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:51:40 +0000

    delicate interview black surprise 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
