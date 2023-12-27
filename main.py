a68347ba09c8e0f5e8bc3c0820b971726d2ab53a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:38:05 +0000

    tour pause hour people opinion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
