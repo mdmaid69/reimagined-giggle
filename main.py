4b7c9c06e7fa1d78e246aea5b7d6601a00304dee
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:09:42 +0000

    discovery clean share dinner trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
