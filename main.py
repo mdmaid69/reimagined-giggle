5c568ee5dd47baba0ff50790d8f258c9f782e9d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:25:11 +0000

    computer finish slice care declare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
