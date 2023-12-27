d0bed41a7ef30e3a3c1ac44ef2f840bd17812fff
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:48:14 +0000

    release behave centre would original

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
