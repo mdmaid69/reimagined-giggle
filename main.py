f3fd457772a8fe10e8ba47f06ec2dc5aea547921
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:43:23 +0000

    scheme and police mistake occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
