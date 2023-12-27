86e9d5448ba354c0fc482ea2f5325ca12b465c44
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:44:16 +0000

    distance thank cover perfect comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
