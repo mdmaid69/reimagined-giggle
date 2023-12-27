d8d8286518b2466d75d70c20902f4388f2d75e45
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:20:01 +0000

    reply right exciting rough suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_area_circle(r):
+        return 3.14 * r**2
