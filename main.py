9de99efd8c80eccefbde3d4e2919fb0b6264d35e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:52:54 +0000

    human far Ff may activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
