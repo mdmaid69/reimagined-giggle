58da27eaad14aee6dbb3086452d315c6c611a998
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:10:55 +0000

    visit wrap understand winter such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def find_min(lst):
-        return min(lst)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
