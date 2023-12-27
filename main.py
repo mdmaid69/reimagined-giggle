11619c0d82aa6c3e9338e23a2b23c190305c6dcb
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:10:11 +0000

    wind reason method fierce partner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
