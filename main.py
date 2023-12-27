43f957d579ae35c1101b33ba000299549fca3cf5
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:04:30 +0000

    difficulty design concentration whisper plenty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
