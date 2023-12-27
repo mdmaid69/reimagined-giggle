d86b2be35258cb997141befa55d06e92a039cbf5
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:39:13 +0000

    height spray touch unable clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
