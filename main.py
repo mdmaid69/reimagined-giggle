3bee09e842337cf0544a08202bb7a8b4a57462a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:52:44 +0000

    float ought dish must key

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
