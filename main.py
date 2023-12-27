6899155fb50f37082b2af3594d33b0bddb46afe0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:27:13 +0000

    sharp turn finger smooth everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import os
+def get_current_working_directory():
+        return os.getcwd()
