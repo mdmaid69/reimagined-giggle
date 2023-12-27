32714e4f2b8ac3a65ba6d6b440f9dcd9320ba7e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:15:43 +0000

    deaf noisy interruption fasten acceptable

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
