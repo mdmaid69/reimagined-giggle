ea3b0f2cb0e7064ca2b564899de73f762524d768
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:24:12 +0000

    march gooff Jj activity spot

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_min(lst):
-        return min(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
