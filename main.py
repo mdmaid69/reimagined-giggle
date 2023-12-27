7cd9b360c4c4388d528857c45b4265dde673ba73
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:34:45 +0000

    drive rate damage sure brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
