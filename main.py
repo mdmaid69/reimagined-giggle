abc63d570d3bde037d6e0c09421864a652c140e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:46:17 +0000

    no discussion connection difference talkinto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
