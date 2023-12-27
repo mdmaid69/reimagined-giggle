e6f40ea907cf971f3796dd955f8033f1fd69d75a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:03:46 +0000

    worst sentence season travel mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
