981750e7c4a3b4981b5062e571e0b54bbb608460
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:02:15 +0000

    afterwards report giveup fit book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_set(array):
+        return set(array)
+text = "Hello, world!"
+print("Characters:", len(text))
