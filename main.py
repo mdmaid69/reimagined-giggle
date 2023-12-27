a0cfb2bd6cf1fbbae6f9530b36bf18aa0cd6033c
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:48:34 +0000

    point situation tomorrow still label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+print(datetime.datetime.now())
