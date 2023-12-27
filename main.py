f3274b542fada0ef0eccffcbdec346be5c3d71cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:11:29 +0000

    part word waste situation fact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import numpy as np
+print(np.array([1, 2, 3]))
+def divide_numbers(x, y):
+        return x / y
