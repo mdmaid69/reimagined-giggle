9bf341c077f5127dee285a5ebf6263b823de58aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:14:16 +0000

    out that exciting convincing earn

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+for i in range(5):
+        print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
