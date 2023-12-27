16c3988eda64e6f7c89afe01dc5b69c826b4217f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:36:55 +0000

    break claim true spot main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_today_date():
+        return datetime.date.today()
