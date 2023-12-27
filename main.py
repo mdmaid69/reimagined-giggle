c3dfdd2311fc01d977751a0ccf3fc126eb071627
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:07:46 +0000

    cheque among organize pattern guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
