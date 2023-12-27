16daa72f36771c1a7c6ce1f18dc01fe6473de159
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:19:45 +0000

    silent cross address more abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
