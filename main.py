dec97fa01f9da995c563dc85791f6f0a6d09d091
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:05:56 +0000

    speak border file place beautiful

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
