44fd24fb74541307dc63e6bbb849757b40659d1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:59:04 +0000

    tool about content course calm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_user_list():
+        return collections.UserList()
