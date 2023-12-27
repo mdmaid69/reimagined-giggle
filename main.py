1466ab5ae5eda950efce78b2e804d1082377a78e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:13:45 +0000

    culture bar solve lower runinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_as_float(array):
+        return float(array[0])
