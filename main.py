6b32be6824409d96890003e9f8a04a88ec5f30a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:38:41 +0000

    cause OKorokay choice still official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_index(array, item):
+        return array.index(item)
