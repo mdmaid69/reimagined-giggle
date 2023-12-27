37970ad4c86abc9632b786301f3275e280b7d035
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:37:32 +0000

    yourself cry responsibility entry scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
