214c1accf4b27e9d6d66c24073762bf5eaa5ee07
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:39:32 +0000

    along slight trip sell search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
