13898a6cd4c38b1a78fe91c085101322c7340068
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:04:53 +0000

    complaint spoil popular cry attitude

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
