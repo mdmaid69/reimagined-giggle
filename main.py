327c6490ece5a0f55a5cb9fe850bf791ac337975
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:05:50 +0000

    hungry care here best direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_repr(array):
+        return repr(array)
