1d9b7a3c051299641d41b7a1587c53db4fe1bdcf
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:49:03 +0000

    memory half behaviour live company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_hex(n):
+        return hex(n)
