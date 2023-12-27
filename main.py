fb9f4b2943ffdadd15c24bd5822af47f6a09d935
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:41:47 +0000

    arm drawing hard path knowledge

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
