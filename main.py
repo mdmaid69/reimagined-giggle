febb5834b1475c4c1a8ad683549f38f23dbfcb1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:07:25 +0000

    wander have comefrom tomorrow clearly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
