39af424f164522695040986198ad3954d69c62f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:09:16 +0000

    mention reply go stick relief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def add_numbers(x, y):
+        return x + y
