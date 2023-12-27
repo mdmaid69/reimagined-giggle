30ece0544e4402f8c2242c053319c3bd9b8de02d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:18:35 +0000

    firm we culture defend rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
