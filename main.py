c096c18323531bee8718c79f4c702899eec55c61
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:28:17 +0000

    offend left something flat organization

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+print("Union:", set(list1) | set(list2))
