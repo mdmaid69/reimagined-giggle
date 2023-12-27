f280c17b6d784d1e801882965644a4a8cef729ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:53:02 +0000

    illness often frightened work Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
