7eaeb9bc838ab34628ee77bf95bf9445d1b4d5e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:26:52 +0000

    still anyone borrow afterwards comeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
+def count_elements(lst):
+        return len(lst)
