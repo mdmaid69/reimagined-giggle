852840242f188ad08e793b729d6dbb203603794d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:14:02 +0000

    actual crash turndown start secret

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def square_number(x):
-        return x**2
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def multiply_numbers(x, y):
+        return x * y
