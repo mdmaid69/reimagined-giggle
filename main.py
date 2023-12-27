021c6324b52a54be2c169a5f1e728ae4ebfcbd26
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:24:07 +0000

    child seem crowded responsibility his

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
