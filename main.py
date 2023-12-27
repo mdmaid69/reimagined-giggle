227e2354636d59933594c066000fe04f9e235596
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:04:33 +0000

    everywhere shadow swim maximum sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
