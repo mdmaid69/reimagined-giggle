3fa24900aee024ffaa4e3f7c8c42838b12207942
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:01:29 +0000

    long responsible leg shoulder capital

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
