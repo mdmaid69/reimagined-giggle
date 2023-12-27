5472e02a90eeca5d67388e8d7091711a5d0833c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:35:42 +0000

    proud Nn trust yours sit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
