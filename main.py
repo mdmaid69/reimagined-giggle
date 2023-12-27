87049d6a9e725c460a21ec78d177479a409fa9f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:07:28 +0000

    tired pretty mad beat snow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
