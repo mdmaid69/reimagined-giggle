f48951fe502ec405661e1b83c93e77e3368b1ed5
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:59:26 +0000

    certainly 569enjoy software realize height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
