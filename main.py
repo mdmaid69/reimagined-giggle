27ef20bc1fb3d0788d84799c08ef139c0002310a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:22:09 +0000

    big big shape manner tour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import numpy as np
+print(np.array([1, 2, 3]))
