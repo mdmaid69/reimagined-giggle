263d48a01837dc16b7815d824438613a15c577c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:58:56 +0000

    crowd experience strong port hand

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
