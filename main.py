33063398fbe238b19ca6352c889dee5bcdba4457
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:11:31 +0000

    patient even change possibility their

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def divide_numbers(x, y):
-        return x / y
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
