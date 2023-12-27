dc39e6c2f1fb45213eb823d3b9c7c1d6a1a08fbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:09:25 +0000

    expect problem fresh sick shade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
