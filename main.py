5f0c36082d82bcfff8648d7d9379059b5711b452
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:34:41 +0000

    stop information wide practice lookaround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def subtract_numbers(x, y):
-        return x - y
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
