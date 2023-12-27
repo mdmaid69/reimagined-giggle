137bdb71cd0fdecb3d8a7855183b06391b3debda
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:43:19 +0000

    accidentally put picture memory skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_typecode(array):
+        return array.typecode
+import sys
+def exit_program():
+        sys.exit()
