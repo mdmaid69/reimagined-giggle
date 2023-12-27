1ddb1655924af8f23bf453266e35564fbb13d33c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:03:52 +0000

    rush how average draw suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
