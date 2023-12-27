3efe491d3fd7ed80d444a18e8536f58a9955a235
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:23:28 +0000

    strike back Jj free lower

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_density(mass, volume):
+        return mass / volume
