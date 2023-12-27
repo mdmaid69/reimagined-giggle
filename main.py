169f93627efd22e13a69d001591b391b2f8b7917
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:15:04 +0000

    everywhere to high deep comment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
