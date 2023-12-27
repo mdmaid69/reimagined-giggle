f1ca031dc088402b11ed791a929ec6cc22677fb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:55:18 +0000

    cool rid generous year rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def convert_to_hex(n):
+        return hex(n)
