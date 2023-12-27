e96288448c5ebc6902a397dc6dcaac322243f456
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:12:53 +0000

    rely tell normal emphasis know

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import os
+def change_working_directory(path):
+        os.chdir(path)
