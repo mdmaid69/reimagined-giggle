05cf38efb0ec69fe8f6fd2f10946e2e03735feed
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:28:24 +0000

    disgusting Cc steal exchange gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
