e22364e3abc827bbc75406dac0d785a51b62f060
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:33:50 +0000

    common yourself expression tonight born

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
