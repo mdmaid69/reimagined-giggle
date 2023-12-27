b97b6b7ce042e48ea50dae7d1d8e4c079df81462
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:01:47 +0000

    wave equal acceptable spread practise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
