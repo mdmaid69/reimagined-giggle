606fe348fab7fefc6598be1b5ec6e6c51b307a2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:47:56 +0000

    probably store play accident habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_int(array):
-        return int(array[0])
+x = 10
+y = 20
+print("Sum:", x + y)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
