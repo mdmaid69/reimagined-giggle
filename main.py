8c2da979db5455ab369beafd3e7cd398913b0915
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:44:35 +0000

    opinion spring firm large splash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
