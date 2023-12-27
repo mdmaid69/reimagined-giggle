4601fbe56a2a1e0c50abedc07b3fb2ad81075981
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:16:45 +0000

    kick true undo short go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_list_from_array(array):
+        return array.tolist()
