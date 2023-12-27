5dbbd4a2be14dc236723b2a150bd31dd03f7f7c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:49:31 +0000

    get wire strange physical if

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_as_set(array):
+        return set(array)
