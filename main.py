39eb7598a606d8099cc492f5065dda2d51a8d74b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:30:57 +0000

    lookaround along foot I deaf

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
