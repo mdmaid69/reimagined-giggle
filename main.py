b452f298f75018154c6da15816e830f0a7b2d774
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:30:37 +0000

    hard heavy term every Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
