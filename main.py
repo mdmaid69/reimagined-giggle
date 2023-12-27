32080d125aaa5bad1bd05e2b25926dd4e36db62b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:38:29 +0000

    eye cook same embarrassing plan

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
