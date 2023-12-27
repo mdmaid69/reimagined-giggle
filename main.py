5e750cdece3f9ce51f6da3e84cb7101eb45c9a56
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:23:17 +0000

    generally threaten family his will

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-print("Hello, world!")
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
