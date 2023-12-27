05b84dd6191c9e0cf2f7174a3e2cff8553df2412
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:45:57 +0000

    contrast beyond very young print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
