1150f1f1047788051f8f6bcb153051cbb5817da4
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:17:12 +0000

    exchange expression mean thing shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def subtract_numbers(x, y):
+        return x - y
