19343938d6892ef6f222bb43739b82eab4b363fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:05:56 +0000

    blow tap ago tomorrow start

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
