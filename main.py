3fe1375c71bf1466bd00143096eee9f1dcfd3526
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:11:23 +0000

    scheme top trial paper port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
