b71e17d656242d5339a12c4c866c12ab77a2c8a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:03:46 +0000

    ever crowded speech forgive account

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
