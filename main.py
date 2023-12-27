7528fad0c711d3a75387a7721262d6483d1aadcc
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:16:32 +0000

    sing attention pretty calm 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
