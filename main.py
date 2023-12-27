def6ab08929ca2086a17687763d2b1ba4c9da795
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:22:49 +0000

    another consult explanation distance comeround

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
