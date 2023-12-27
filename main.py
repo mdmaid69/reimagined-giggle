3810e2504f05edb9da00ad61f70ac1a2dfa23ebd
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:27:57 +0000

    floor public precise classroom makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
