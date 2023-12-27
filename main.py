132dfe7f5548fddf6180f9644dee8db70e4d50ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:58:20 +0000

    finger mine short choose whatever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
