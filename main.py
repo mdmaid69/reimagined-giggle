0e703e46957e63756a373cd910ff7a1003b8982b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:38:40 +0000

    command excuse stop land stop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
