1916ece378ee5539daf5e21a7b9079c7d8005983
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:52:06 +0000

    waste nervous turninto warning mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def count_elements(lst):
-        return len(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
