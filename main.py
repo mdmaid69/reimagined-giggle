0561794dcf1a54ebebc9d21b9d6e005d9ed87b92
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:17:39 +0000

    have refer contribute food we

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+print(math.pi)
