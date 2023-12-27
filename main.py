ad9f07164195b4a181287f688f3e971541e290ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:22:08 +0000

    talk always mad clear fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
