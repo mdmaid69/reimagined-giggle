8634e7661b1bb8583fbeda28405a476786f70952
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:38:49 +0000

    discovery perfect personal tidy tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def add_numbers(x, y):
+        return x + y
