c6e5ad29f460bca0fa4c4b145e202cd82b81ff8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:49:46 +0000

    thinkof shop powerful direct regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import collections
+def create_user_string():
+        return collections.UserString()
