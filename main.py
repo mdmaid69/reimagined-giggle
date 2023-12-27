96bd68f47e0bd0ad49fa954a7df53d0ae2b3cb6e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:18:42 +0000

    leave measure estimate summer correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import math
 def calculate_circle_circumference(radius):
         return 2 * math.pi * radius
