6914e6aacf8d8caccc2ad9e05d45c8130b8435cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:37:49 +0000

    equal everything pile hurry hear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def multiply_numbers(x, y):
-        return x * y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def is_even(n):
+        return n % 2 == 0
