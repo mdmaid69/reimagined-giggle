c263a51bf51f01b704a15d5df5f4cee9837e0829
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:08:17 +0000

    mad wave follow side sound

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
