102cc7a27f0cc36af17fd0e477b5176208aaa060
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:11:27 +0000

    now like over guard frequent

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-print("Hello, world!")
+import array
+def get_array_as_set(array):
+        return set(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
