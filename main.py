6cdf0d1ff58f03c0c5a65c0831ffff23b2d91a66
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:18:25 +0000

    thankyou town connect 179bite smile

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
