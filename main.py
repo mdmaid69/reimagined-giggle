e58656b6b3884fefcd58fe2264e6eca90cdf6e2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:15:01 +0000

    point holiday unless firm straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import collections
-def create_counter():
-        return collections.Counter()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
