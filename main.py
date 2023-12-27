2931aa7bb5f0a0629bd0c2bbb9995f9f1a38a51e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:16:25 +0000

    mix through equipment change announce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
