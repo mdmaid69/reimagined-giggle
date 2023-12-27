4584ceb919ebdd9fef45864c2f37c973befa4e09
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:10:51 +0000

    suit rate private street tough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
