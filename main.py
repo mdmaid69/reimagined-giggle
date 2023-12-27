51545fa0a3f69332ce36c03e54a2fdb80b9352f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:15:08 +0000

    such look nervous increase glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
