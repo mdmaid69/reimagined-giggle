fc7fd28f06d2627f2b04e451de2f994f8b6e0cba
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:10:06 +0000

    moon regional grade welcome embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
