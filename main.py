f82ec14d3af1aba063fcb83d7cc2e90c1937b57b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:38:27 +0000

    dangerous bread person feeling deal

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import getpass
+def get_username():
+        return getpass.getuser()
