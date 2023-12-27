d6a58d8dbde010e933b45aa3e1d67bd405dcbbfe
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:07:46 +0000

    circumstance goon slight sky comeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
