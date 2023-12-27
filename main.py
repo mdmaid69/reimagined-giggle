1771fe20de5359933ffc4271c351814ee1b58637
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:39:46 +0000

    benefit recently hit pipe relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_list(array):
+        return list(array)
