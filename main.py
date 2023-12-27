af809d897032d317a6483d77006818a7fd45d057
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:29:14 +0000

    lookup whistle subject handle active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
