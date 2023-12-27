4247a293edbf1b21c7fdd8608d4a868e5386fb59
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:40:00 +0000

    thinkthrough conclusion colour effort pleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
