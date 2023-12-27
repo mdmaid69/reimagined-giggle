2173ff568ef2b59a784a26fce8a9ba3d93819a67
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:54:28 +0000

    connection opposite hate hold rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
