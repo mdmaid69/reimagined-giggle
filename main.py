7082c3ba313ae881dca5a8cc02969b5af9a8fd56
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:38:35 +0000

    tax too before outside reason

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def cube_number(x):
-        return x**3
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
