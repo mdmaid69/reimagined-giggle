f990038e6ed04a551a7ead3a5bd546b47a4907af
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:53:04 +0000

    bag discuss shelter strength curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
