2daf5eacfb0f7170169d6b428fff63f014f6dad4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:57:36 +0000

    past round juice mine holdon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
