b56b0927eaba0ddbecc1d473ec8cad2059f30239
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:46:34 +0000

    fast among but argue callup

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
