2920ab83c4bb62e1e4fa485a2fd6c27cf3d28787
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:43:51 +0000

    spill challenge almost excited well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_list_from_array(array):
+        return array.tolist()
