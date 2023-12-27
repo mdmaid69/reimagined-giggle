532e2e1a5fda4ba5986347393b075b05afaab476
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:42:53 +0000

    move interruption empty how jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
