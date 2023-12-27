838dad49e528a61be047d7df8a61cb70c293bce2
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:16:46 +0000

    same disagree card reserve more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+print(math.pi)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
