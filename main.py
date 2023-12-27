8caa1a1ce44aeef40d6a2ab679dee23a7486de0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:54:51 +0000

    Ww badly nearly nature careful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
