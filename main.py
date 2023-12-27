78eea6de35ac6d92896ac78ac6c478fcf9eb560f
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:28:56 +0000

    firm depend impatient air stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
