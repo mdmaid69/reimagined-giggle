34b8cf49f7e187cdd4d1fec7d0ebaea7011572b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:14:10 +0000

    base study message emotion immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
