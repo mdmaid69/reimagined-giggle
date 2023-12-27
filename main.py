2a0a367c69852eebd2e030d067ecfd2cb1671066
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:20:06 +0000

    regular platform question theirs turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
