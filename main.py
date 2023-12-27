4e7694c0fa4647c3f9697832d4312fbad59dc28a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:30:17 +0000

    land form prepare instruction interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
