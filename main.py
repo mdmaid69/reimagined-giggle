96aa2c8813527bce5a391cd5c2558a1d7e5bf1fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:59:46 +0000

    fat hurt tight front different

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def remove_duplicates(lst):
+        return list(set(lst))
