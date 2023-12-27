03f27b2574f11d93f45cfb731c822a0631c785a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:52:59 +0000

    backwards chin reliable war quiet

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
