db36666d1bed562bc6bc2e1b5dd79764e111acac
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:39:06 +0000

    first tip bound offer bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
