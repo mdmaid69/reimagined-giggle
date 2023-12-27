be951c0df0eee8a47f60eb1459c0a2367e5b97bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:21:51 +0000

    breakinto university Gg let but

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
