54c57b21d95aac871774fedc0cc9f40046a90184
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:25:51 +0000

    convince hear sort any building

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
