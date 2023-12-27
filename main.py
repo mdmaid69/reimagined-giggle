ecf10e3cead0c44d1c2badab91dd67b7779b264b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:44:44 +0000

    prepare meet container seat broken

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import time
+print(time.time())
