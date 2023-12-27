8d1973ebe1304fa0ea94c281ec2d5300c70c1f8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:09:02 +0000

    cross temperature eye bottom neat

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
