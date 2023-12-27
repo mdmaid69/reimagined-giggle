f15ce90f23ced73ac0d006c4d99b55cf8acce38d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:54:01 +0000

    after worse birth spot brief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def count_words(sentence):
+        return len(sentence.split())
