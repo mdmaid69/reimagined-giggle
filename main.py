845f5e00de708d584d7eb0f005084df0aadf1021
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:20:52 +0000

    few scene mine callout cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_quick_ratio(current_assets, inventory, current_liabilities):
         return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_list(array):
-        return list(array)
+import datetime
+def get_current_date():
+        return datetime.date.today()
