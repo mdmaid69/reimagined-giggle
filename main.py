144ffda3fd2a0cec67cfc584714f011d69d4d383
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:53:56 +0000

    phone shine plenty false before

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+print(math.pi)
