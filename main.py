5bc2e8ca7bc10e08f59b1b492ef98f5fb6d46c08
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:31:47 +0000

    not stamp fold yesterday obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
