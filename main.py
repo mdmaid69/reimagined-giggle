f91ba83b084f13d8c4c148ddae5371dc7eae47df
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:11:30 +0000

    innocent before who part bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
