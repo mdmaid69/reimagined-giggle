c2bae136aabf64f0c314f0e3ff7c2dfd5259a93f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:59:20 +0000

    slide like arrange only relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
