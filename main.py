973854ebdf14ecf49965a6d6984384bee4387344
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:21:33 +0000

    work gooff suddenly you force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
