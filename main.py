e823914b267a4cf721e5b714680efbd3e7810106
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:44:46 +0000

    ought goup pronunciation cost keepoff

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import time
+print(time.time())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
