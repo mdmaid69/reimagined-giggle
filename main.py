fb69dc16b176c99e4f8a99ce91ec1766f59eca49
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:23:57 +0000

    thumb after instant far tragedy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
