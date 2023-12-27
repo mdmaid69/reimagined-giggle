5e9e2ce8b95fd042ae0f0834c9082fd732e52539
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:55:48 +0000

    bring nigga identify relax boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
