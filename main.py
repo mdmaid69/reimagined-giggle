da111b6cfe8b31b00caba751fc892d7accc87a8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:17:14 +0000

    either rise goon before bring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
