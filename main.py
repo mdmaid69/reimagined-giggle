5a5790077bc00eeb25c72cad506b974a31d7be9d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:49:14 +0000

    combination basically miss what factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
