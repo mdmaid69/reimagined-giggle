85839905daebe731602250721c5193ce1c86c2f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:56:46 +0000

    famous needle face operate earth

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
