31ce28b4709e17e34b52db75a67cda97c4af1a76
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:00:25 +0000

    mainly and copy speed right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
