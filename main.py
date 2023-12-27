b2bca57a87efa5032240c39d01a264dd902ce65a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:34:58 +0000

    often thankyou dish fix autumn

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_odd(n):
-        return n % 2 != 0
+def greet(name):
+        print(f"Hello, {name}!")
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
