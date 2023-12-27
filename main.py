8b5af6f206a0edf37fae46022f6f0b895109463d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:19:25 +0000

    turnback niggaacademy twist air make

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
