3c1308716d5a61419a6f49c674cd53e7105a3cb4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:13:59 +0000

    blow record fetch build support

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
