8041afaf8a9feacfcfa753905f6aadeb913e1acf
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:09:52 +0000

    feature cough happy certain tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
