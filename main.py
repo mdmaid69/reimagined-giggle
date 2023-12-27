bc944fb2e2849ea56501f2e5f13716177238d541
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:26:35 +0000

    talk walk possession cinema like

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
