5638c2b647123a55e1f4d2049fde8f2cf3ef38c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:59:00 +0000

    explanation finger loud company interfere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def count_elements(lst):
+        return len(lst)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
