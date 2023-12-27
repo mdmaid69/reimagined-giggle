8d767b0129dd9ecd064f6d847e2b2355aa5aabd2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:19:23 +0000

    sure design not neither Gg

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def remove_duplicates(lst):
+        return list(set(lst))
