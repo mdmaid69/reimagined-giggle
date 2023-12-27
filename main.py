e29311c0b940646ebd01997c9078055f9028c2e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:13:50 +0000

    plate politics picture satisfied unfortunate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-print(sum(range(10)))
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
