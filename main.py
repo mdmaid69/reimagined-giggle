7f42b4c7f707c19394d82847447f507eaebd3be4
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:06 +0000

    today fierce frightened supply instruction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
