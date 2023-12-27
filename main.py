96ffaab8a1fd3c7c3555bfe2f4b0faaa794748b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:24:28 +0000

    tool lend burn correct payment

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
