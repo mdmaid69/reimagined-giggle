09d5f2d440b5a376b8cf4c85590a868f023fa89e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:23:13 +0000

    lean only part back 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_item(array, i):
+        return array[i]
