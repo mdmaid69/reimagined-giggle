0500cc86533d0229cc2c4f602968e2995429ee69
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:03:26 +0000

    surprise image near business to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_working_directory():
+        return os.getcwd()
