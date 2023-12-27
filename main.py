8ab55e38022dbefcf4cfa6cbb9802cfcb5e4bf4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:08:50 +0000

    petrol language operation include inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def convert_to_hex(n):
-        return hex(n)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
