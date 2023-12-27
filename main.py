a62a0442b5d860fb41b627dcfe615834dff993c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:53:45 +0000

    wherever eager pour criticize over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
