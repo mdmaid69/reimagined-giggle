ab172c1c99104b2cb85467641a52cfc99698c277
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:27:08 +0000

    claim tidy insist warning not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
