4750dd33c7fd81834765d69288a891f0df33e1d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:18:48 +0000

    enthusiastic whistle decision tongue question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def divide_numbers(x, y):
         return x / y
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_str(array):
+        return str(array)
