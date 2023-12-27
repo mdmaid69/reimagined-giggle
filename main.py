ccd8f96573aee13066016cb58c80b374ac0c4710
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:57:54 +0000

    own friend dry frame clear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
