815fea3e421a6c26ed0a81c456b05ee37f1ee869
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:39:42 +0000

    dream equipment inform any fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
