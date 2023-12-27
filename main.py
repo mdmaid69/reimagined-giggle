20bddd23928fca55284773c3b3768d3a3489f279
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:06:23 +0000

    help sign relief theirs point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
