6cf11d9cbd1206d42e46305596ad182d7d8e31c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:47:26 +0000

    protection compete murder thought public

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+print(os.getcwd())
