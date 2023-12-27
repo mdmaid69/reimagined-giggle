d695b71009e05b9b79ddf903fcfcce5f58a2a665
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:39:25 +0000

    necessary move sign switch thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
