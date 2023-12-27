0adc31fcdabdeeb9aec4496274e44bf24e730d90
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:48:59 +0000

    lookaround concentrate learn belong improvement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import random
+print(random.randint(0, 100))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
