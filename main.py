d22cfb3e824e75b564643d3e8f57e3aac208bae3
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:58:25 +0000

    pity start turnback shoot usual

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
