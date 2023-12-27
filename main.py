b0bb8bd1a3e545d3ab6d1844e45fb5426187ebfb
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:32:31 +0000

    hospital damp balance combine education

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
