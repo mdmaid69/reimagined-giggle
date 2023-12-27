6624c7edb697fea5d980bc5b4e79bba0b3a78d50
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:17:30 +0000

    good bright mention substance handle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+def get_current_date():
+        return datetime.date.today()
