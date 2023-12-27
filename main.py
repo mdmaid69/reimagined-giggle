d40f6b57f90aa317c4bb9d63fabb0832e92e67d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:42:00 +0000

    excuse tough increase type change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
