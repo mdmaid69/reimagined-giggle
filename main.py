497896f3116c5de63948d132d177b0436cba9f69
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:52:54 +0000

    disappear people goback(to) grade vote

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_exponential(x):
+        return math.exp(x)
