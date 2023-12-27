4b647adfb0d2649ddcd3fc5e697295d0808b4366
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:30:36 +0000

    ought forward cruelty at shape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print("Hello, world!")
