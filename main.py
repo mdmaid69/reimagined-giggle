8e9d2075246a0d47b60e130fa69cdf0acdc9e9e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:23:42 +0000

    someone no catch challenge swimming

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
