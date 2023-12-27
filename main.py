d4e5379ec99530d42e354f963968e38a5a1f7329
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:36:49 +0000

    marry repair suggestion contact turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
