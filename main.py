f5d487204b39733d1de186dc9d88e729358c7bf1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:11:39 +0000

    wander sink breakdown pride comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-for i in range(5):
-        print(i)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
