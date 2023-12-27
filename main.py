106fbf65220bfbd76355ef1a891ef47513d64ca3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:46:27 +0000

    bit valuable bunch reserve reserve

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
