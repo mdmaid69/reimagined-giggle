958c00f6c5ba72eb2504d2411bc1d7d38dccb325
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:42:56 +0000

    mainly human turndown shut play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
