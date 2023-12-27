80c292f07109fe917250f3fb3319b31926614e27
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:24:40 +0000

    Ii tax increase age control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
