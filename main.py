e6f0d9b901d200d333575e99678c82da664d75bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:55:40 +0000

    few none pleased settle trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_current_directory():
+        return os.getcwd()
