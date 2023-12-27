e3c43d7151e01a5876b9e7e0fbdaf553218ed501
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:55:50 +0000

    effort contact thanks suggest unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_arc_sine(x):
+        return math.asin(x)
