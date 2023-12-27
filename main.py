fdd26459f49c9e9a92fef27a8521e8bc4ed4f39c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:17:16 +0000

    charge modern top pretty hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
