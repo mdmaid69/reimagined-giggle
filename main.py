af26ba87ae4f8c4de3734f8e8a751ca0f92f39b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:44:22 +0000

    long you other attract stop

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def add_numbers(x, y):
+        return x + y
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
