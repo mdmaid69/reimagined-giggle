2fc07b2f5b5bd2e5d62cc08206616ae4bab9ac65
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:38:49 +0000

    carry lead fly relationship bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
