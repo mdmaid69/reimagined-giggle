15066a39ccf2c7e5e8b2e3ff8b2ed6d143688f9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:24:55 +0000

    gift wall Yy sing please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
