6341983328be863a36c0e599c1f5f62b368f19c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:14:22 +0000

    damage dull traffic walk height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
