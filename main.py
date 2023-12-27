b646de66d77312f269911b49df6e15f45352f1f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:40:20 +0000

    reliable wealth manner fierce Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
