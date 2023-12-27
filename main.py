62fc1f0079b0052668ef3adf7acdbfbf59dd00c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:16:44 +0000

    everyone compete reduce last pay

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_length(array):
+        return len(array)
