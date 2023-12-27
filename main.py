29429acdbcda5ac28fcc1b25713de3ea33026e4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:45:00 +0000

    waste agreement branch become shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_base_name(path):
+        return os.path.basename(path)
