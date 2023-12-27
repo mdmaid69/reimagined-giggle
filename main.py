198b08b14de0de08f055815e73c20267a939b0f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:41:40 +0000

    reaction form brave yet protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
