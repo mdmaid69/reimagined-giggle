4789b15da5b55bf1a68731b114583bfc76c719e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:02:14 +0000

    of name comefrom frequent extraordinary

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def multiply_numbers(x, y):
+        return x * y
