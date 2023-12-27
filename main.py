9a4f84c10831999172df4a1457b3b1ed02d10635
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:51:49 +0000

    warning typical worried crowded argument

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_work(force, distance):
+        return force * distance
+import time
+def get_current_time():
+        return time.time()
