a0d97edf7561e316889d75e61acab98d7484b2ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:48:25 +0000

    heavy lower wall confused hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import sys
+def exit_program():
+        sys.exit()
