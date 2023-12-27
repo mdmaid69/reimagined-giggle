84756f1b471618e8678f3e53318e64541af1fc5b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:28:16 +0000

    music your among model expression

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_typecode(array):
-        return array.typecode
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
