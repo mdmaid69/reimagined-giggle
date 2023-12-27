70594c5e11eceaaebaf3f5ecedc04f991b7febcd
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:36:19 +0000

    without any example scream pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_speed(distance, time):
+        return distance / time
