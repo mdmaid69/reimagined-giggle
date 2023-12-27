333c47881f9a3247aee9eb5091f22b1f2ad8142f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:07:04 +0000

    situation left car acceptable kind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_repr(array):
+        return repr(array)
