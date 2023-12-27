b96c833896458bc8b26a43782269f6bc105c9210
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:23:59 +0000

    temper effect record also suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
