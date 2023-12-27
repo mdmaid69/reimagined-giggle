d5154966758bbbb84312784be46e8983e6a0e796
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:23:15 +0000

    passenger poison lesson port operate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_array_slice(array, i, j):
+        return array[i:j]
