ba7d86dba1337cc72e71471136f979856ee1c0c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:12:51 +0000

    spring thinkof but inside scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_pressure(force, area):
-        return force / area
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
