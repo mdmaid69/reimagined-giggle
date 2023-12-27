4a51d3415f6cfd6cec00e46aa9906359fda68be4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:24:01 +0000

    no confident kind over wind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_list_from_array(array):
+        return array.tolist()
