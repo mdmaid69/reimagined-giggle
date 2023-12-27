b8ec830cab9c6d2a5c0b307d26c6ba143126c7ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:36:43 +0000

    sore pull brother slow distance

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import collections
+def create_priority_queue():
+        return collections.deque()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
