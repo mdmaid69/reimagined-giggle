3a6ca742fabe3ac63f146a7abb1b72c86bc6be7d
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:52:41 +0000

    visit buy arrival it scene

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_power(work, time):
-        return work / time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
