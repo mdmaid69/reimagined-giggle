1dd4237721ba828dad34491f4cd9d87d426fe15b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:13:35 +0000

    fasten file shut tend lay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
