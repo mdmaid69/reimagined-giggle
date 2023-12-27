488c11f983cdddfe935372e642911752aec088c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:07:42 +0000

    will identify compete grateful refuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
