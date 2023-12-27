cdc6efeb95991b5f56ef8db2ab8e68839efc2279
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:39:02 +0000

    person size steal deep plan

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-print([x**2 for x in range(10)])
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
