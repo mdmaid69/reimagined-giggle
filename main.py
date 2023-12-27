3754c614f22698a846a1ea250a02d09d2db38e8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:56:13 +0000

    friendly go develop tie gate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
