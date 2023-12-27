dba1d3fa7157a899e04df24b4cf6dcf0789e9faf
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:42:25 +0000

    mark action fall total want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import sys
+  def get_python_version():
+        return sys.version
