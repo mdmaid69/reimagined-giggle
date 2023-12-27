cf0e92c6834f51870dfaf453d9ff103025205e22
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:58:38 +0000

    swimming minimum score imagination figure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
