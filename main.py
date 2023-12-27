c936fdd7035a4a3690c7ce431ab117e5401f9cf3
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:41:15 +0000

    birth create now like amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
