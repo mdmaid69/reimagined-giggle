eafdaeb0546c90ddea018ca2d919ff24706287f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:18:36 +0000

    convenient rush let impatient pin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
