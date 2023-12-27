d220f488fa0d28c963ca14ce93500d5bac2b1e3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:23:04 +0000

    share distant couple crazy modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
