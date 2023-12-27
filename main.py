0b30d9aa346c6f26a2e8cc4c5e29f4d9aa55b196
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:10:40 +0000

    order prepare free programme measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
