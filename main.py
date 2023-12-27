ca39eb39844d674fb77bd59e736b2603bb8bf6f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:22:31 +0000

    pain fortunately convinced make you

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
