e64871cfc86a360c1487d1f340dc704fc61b22fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:01:57 +0000

    computer create instant ear appear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def sort_numbers(numbers):
+        return sorted(numbers)
