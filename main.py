c87fc619d3c94777cf05a0e7456d55c63ffe9594
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:16:08 +0000

    exaggerate explain smell sign worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
