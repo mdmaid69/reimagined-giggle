bb813f905e74f4970e6a1f8b255e29942b7d870d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:49:21 +0000

    rubbish during fight bringforward burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
