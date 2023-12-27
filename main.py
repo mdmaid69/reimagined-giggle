0ba6d196a29943f7af6b3bd3cfb9dbb78301356a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:17:54 +0000

    knowledge last change piece secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import sys
+def exit_program():
+        sys.exit()
