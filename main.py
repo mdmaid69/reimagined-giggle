83180d264f3ee2b3bfcdde44da63b385d1113f0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:53:06 +0000

    when rent increase division exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
