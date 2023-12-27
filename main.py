e1ddd07bf346de2d648b5cc225772fcb1c0728de
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:14:00 +0000

    announce quarter official regret rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_list(array):
+        return list(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
