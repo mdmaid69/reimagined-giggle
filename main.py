333b99e434521b1162ff2add17ba9711f07be035
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:38:41 +0000

    name water sticky try rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
