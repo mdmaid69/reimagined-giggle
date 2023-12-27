67f808aa0ccc87de91a9a9161f1dec2e043f6de8
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:58:07 +0000

    deliver tense once abroad acceptable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+print(os.getcwd())
+for i in range(5):
+        print(i)
