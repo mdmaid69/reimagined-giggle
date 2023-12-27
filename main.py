2a08d47f898ad1e1b078bb79af634384ba56a528
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:25:21 +0000

    change runaway getaway grateful tour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_acceleration(speed, time):
         return speed / time
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
