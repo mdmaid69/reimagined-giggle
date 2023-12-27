3e0ca5725e071e0f3b420043583e2fcd99aa2853
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:18:20 +0000

    properly silent image quarter underneath

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
