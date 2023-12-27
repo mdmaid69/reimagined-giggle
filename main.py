58f49418ce1da17e6a3fc49b76c4912441fba908
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:07:44 +0000

    rent steer estimate law fair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def remove_duplicates(lst):
+        return list(set(lst))
