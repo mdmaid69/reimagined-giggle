91a8de1c14c1eadde9ab497a59cc1d478240d085
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:11:29 +0000

    breakthrough storm breakfast extremely free

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-name = "Python"
-print("Hello,", name)
+import math
+print(math.pi)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
