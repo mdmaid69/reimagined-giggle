a56ba948893f555268a09495cd63375e5d6563e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:55:10 +0000

    man justice holdout OKorokay fill

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_length(array):
-        return len(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
