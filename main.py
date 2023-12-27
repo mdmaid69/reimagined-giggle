e1a659b34b3fec4767301910e297fcb64403d705
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:03:46 +0000

    secret gooff print doctor try

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_error_function(x):
+        return math.erf(x)
