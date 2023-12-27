73e77f287990e1a30d4aae645186e1bdbdad973e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:27:52 +0000

    coat connected undo hand possibility

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
