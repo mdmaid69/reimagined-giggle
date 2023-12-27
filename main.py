37ff61d3f86c906565fcb56ed358ad66e701713c
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:51:27 +0000

    confuse worse come brother usually

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+print("Hello, world!")
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
