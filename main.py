e685311e56f9829a9e1525d6111654cf7f4e835e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:53:16 +0000

    pick speak politics bar poison

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
