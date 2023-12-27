7bb16316c9f0415562b2f0985478b633f459bacb
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:45:29 +0000

    damp record experience loan hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
