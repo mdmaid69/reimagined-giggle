285d8c6245e3e4e4c36cd5dc4e336b9aaadd16b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:47:09 +0000

    before you pride involve nonsense

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
