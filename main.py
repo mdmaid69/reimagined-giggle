88af3015eaeea393687477a5c73009a158bc2ba9
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:05:02 +0000

    distant copy matter uponon woman

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
