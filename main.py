50b1f5dc916afa8943493dce5f3a8c9c3ef66ee3
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:07:20 +0000

    mind march drink fairly add

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
+import math
+print(math.pi)
