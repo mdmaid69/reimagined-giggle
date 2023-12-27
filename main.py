6c21318cd1c939d74e748f18719cae449883bcf7
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:10:58 +0000

    Tt stick fair cap spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
+import datetime
+def get_today_date():
+        return datetime.date.today()
