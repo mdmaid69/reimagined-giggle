292ce90cd337c301d8c649c471164271b1f44d91
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:43:06 +0000

    reply giveout strict morning physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
