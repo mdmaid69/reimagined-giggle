ae051dfa6fbc001630647c6b30aea21409221ff8
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:13:46 +0000

    usually marry remove pick shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
