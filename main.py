b34ae5b5213e1822d0106b3ef3ece824bdaff761
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:39:42 +0000

    guard belong best purpose pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
