5e662503a3985f13de53750e27bdb89c71cc7089
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:57:46 +0000

    century collect ball crash tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
