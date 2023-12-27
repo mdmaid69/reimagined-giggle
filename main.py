6d875c5171291f157bea08701ef3e5e53029040b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:57:37 +0000

    something noisy goaway getoff cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
