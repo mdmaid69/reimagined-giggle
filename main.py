dd7b456a709e01cb3d4320a199a020d15d75b269
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:10:52 +0000

    wind that split guess general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
