750ad2ad7651e92f66209b82a3888e53cd70469e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:50:19 +0000

    generous fairly pride able usual

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
