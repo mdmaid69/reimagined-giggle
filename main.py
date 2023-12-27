b30dc30016821669830f6d671fb65e19020b5e9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:58:22 +0000

    search know must consult race

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_max(lst):
+        return max(lst)
