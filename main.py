f657c2a13031ff34d0200c96c046ac2e85325e85
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:43:15 +0000

    attempt its opinion group brave

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_speed(distance, time):
+        return distance / time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
