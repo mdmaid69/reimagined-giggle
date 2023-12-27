8940ed6ec9a305b7b249d1ecac08c6fa4ccc1221
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:57:22 +0000

    correction exist pollution full loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
