7198a3495aa5d2c78aed739fe727875a00e49954
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:25:57 +0000

    problem tomorrow game meaning last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
