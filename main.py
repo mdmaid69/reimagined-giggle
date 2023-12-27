b15cda3d0e47f7dd533139963c2b24dcce87202e
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:15:57 +0000

    behave previous break really cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
