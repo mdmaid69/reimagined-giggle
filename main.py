993f668d304ef436bdd8eadc582b48bb6413b826
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:11:03 +0000

    pretty majority unfair splash flat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+for i in range(10): print(i)
