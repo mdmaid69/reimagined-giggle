d63ba6e98f4c9cf317e889e2ddcb238e6ff1c8b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:57:18 +0000

    murder blame now wheel crazy

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
