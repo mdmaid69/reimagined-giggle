c550f5446df2fb208a06fb0476cbb82308b5e5b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:21:24 +0000

    operate mean wave willing they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
