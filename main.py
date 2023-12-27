f7592e3d060722f31baf51e084ef43ebd484dd6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:13:22 +0000

    tell remark him circle rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
