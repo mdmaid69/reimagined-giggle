1f8beb73508568b2c63ccf909ac9fac4d9639729
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:11:57 +0000

    broken head shelter smile while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
