b1feda7568e17cfb6fef2f79fb1fd6cdadd56874
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:35:32 +0000

    difficult moon last suggestion available

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_power(work, time):
-        return work / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
