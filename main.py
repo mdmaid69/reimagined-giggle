3241b08c97a4637e147c29d1f03c7931f4f3f58e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:56:15 +0000

    never fit damage team accordingto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_speed(distance, time):
-        return distance / time
+def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
