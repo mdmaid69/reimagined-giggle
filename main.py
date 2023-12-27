3ef6c2502f25226e22e6a1ffb98c23c2e3107f8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:21:36 +0000

    square interview butter tune complicated

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def reverse_list(lst):
+        return lst[::-1]
+def square_number(x):
+        return x**2
