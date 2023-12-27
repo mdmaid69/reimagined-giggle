38e25a3be35fc130e0d341e24d85e90358a61011
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:05:56 +0000

    terrible laugh ride view announce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
