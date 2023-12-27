290253938a4714ab456c08e170cc093bce11984a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:24:42 +0000

    crisis neat alive underneath search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_item_count(array, item):
+        return array.count(item)
