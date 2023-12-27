32a6f08c3066a48167bdd7bf13ceb18fee5ffe55
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:25:18 +0000

    satisfactory gooff estimate which surface

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-print([x**2 for x in range(10)])
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
