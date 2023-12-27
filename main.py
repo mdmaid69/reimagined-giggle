1c4138c19bb31a485a78b1728bcbdb808ae9d18a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:41:34 +0000

    drive warn agreement form second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
