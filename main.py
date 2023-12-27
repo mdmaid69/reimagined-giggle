26a895d197f0f3ab68d28d972e21872e43edccbb
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:49:38 +0000

    communication damage behave point butter

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_exponential(x):
+        return math.exp(x)
