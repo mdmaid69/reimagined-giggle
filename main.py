ef0c529737ba9561abbb40b320bf4176b524d273
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:41:00 +0000

    independent underneath comeback direct ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
