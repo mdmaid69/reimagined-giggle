efe876516360f79e7879e0195d012d78bf56e7a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:26:12 +0000

    reasonable offer trap send surprising

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def clear_array(array):
+        array *= 0
