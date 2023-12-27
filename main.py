a7bf70fd52941c2b47f61f79d8cf49fd29d64df8
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:11:33 +0000

    suppose early still hang tonight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
