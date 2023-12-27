a13f7e4f452a9d37b119ab5d9ded0795e95ca0bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:33:51 +0000

    pot roll hand dream hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
