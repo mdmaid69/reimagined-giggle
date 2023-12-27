fa2c34548a230ea310f16f5660527265c1a8b986
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:56:05 +0000

    unhappy temper quick schedule after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
