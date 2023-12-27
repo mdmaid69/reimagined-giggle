99f7e157878c15d3b09aad1a2c20261992c59c5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:59:03 +0000

    miss draw why do limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
