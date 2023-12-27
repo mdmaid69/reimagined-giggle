0ad4076f0203e02fd6124d407451c50d94e934e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:01:01 +0000

    rent exaggerate practice education development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
