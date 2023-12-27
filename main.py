f288dc0b879f25714599426a7898cf3e2fa479ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:34:53 +0000

    meaning problem town rely surprised

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def find_max(numbers):
-        return max(numbers)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
