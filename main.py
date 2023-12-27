b2b31198ed29d007ff78ef5dbcf7e4819fc5d8f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:18:48 +0000

    Cc block lonely reaction fine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
