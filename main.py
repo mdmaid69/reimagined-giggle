eb83801ea74d15b784655b7fbfd2be56916a3a2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:30:02 +0000

    call workon watch prove nothing

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-for i in range(10): print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
