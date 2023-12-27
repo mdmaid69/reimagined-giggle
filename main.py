80491c0d123d8175bb26a80ce1317cf42c2ebf8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:21:34 +0000

    hold study disapprove runout bunch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
