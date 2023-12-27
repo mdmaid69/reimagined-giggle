b6fab879fd4c0a9e4d097e9124b88711821e7c99
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:50:22 +0000

    deaf name pity accident flat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_string_from_array(array):
+def convert_array_to_bytes(array):
         return array.tobytes()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
