b39928ca595c7568c88c100b150a7974aab171b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:47:58 +0000

    expression excited snow copy rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
