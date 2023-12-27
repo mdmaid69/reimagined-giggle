ea4ae71a6eda65b5562c00762a0a53a6d6d65169
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:48:09 +0000

    clearly film less swimming young

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
