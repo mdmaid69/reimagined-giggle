1c572aab59a24a310cc2b64e12baac604d872861
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:52:47 +0000

    please meal strict section fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
