e0a1873d60ca8861513e8f413da6289828981ea1
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:05:08 +0000

    next tall meet calmly risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(a, b):
+        return a + b
