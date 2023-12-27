498cc96ae70bf29adde439f38997f34b4aba4aed
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:36:22 +0000

    break temper move fast fall

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-print(math.pi)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def multiply_numbers(x, y):
+        return x * y
