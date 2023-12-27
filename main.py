d5c312ee109bd076d6cbde12ef5279ee4fb1adcc
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:58:44 +0000

    advanced preparation juice instant over

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
