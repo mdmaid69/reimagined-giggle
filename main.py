88f566821bf2998dfb07f72206e58f86bff2ad92
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:04:14 +0000

    simple tomorrow chief or turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
