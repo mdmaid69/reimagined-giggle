8ee12481dc3891c7ad81da1da0f8378515d0ad40
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:51:34 +0000

    afford behaviour reserve air wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
