245b4aab6a800aab3e67db428d6b542a9f905659
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:46:22 +0000

    fast bar keepout confidence field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
