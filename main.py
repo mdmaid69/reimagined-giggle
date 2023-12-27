665a7ab092bb159b9d2a2bb4f923094809b46855
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:32:19 +0000

    keepout bush statement fight Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
