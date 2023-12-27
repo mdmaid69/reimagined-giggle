8b48f3d4485afa7a356166cc4bf7171735cfb6af
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:34:27 +0000

    geton throw bet breathe statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+  def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
