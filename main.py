f51d0e49980a2d5a5ec0bc2dd0ca5bea8651a3bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:15:52 +0000

    inside edge demand question art

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-print(sum(range(10)))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_list(array):
+        return array.tolist()
