ccfd7bc194f114c7aed91cda33b85939fa5f2625
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:43:53 +0000

    ashamed always motion opportunity behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
