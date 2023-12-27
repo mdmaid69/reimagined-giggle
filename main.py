1a6e126906a74159a6932e4e35f2102477aea33d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:31:33 +0000

    spring evening determined story sort

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
