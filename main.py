df61fdd13f7b2e24c3c122880ce65f03659655a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:42:47 +0000

    race fear partner happen form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import sys
-print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
