9db2f963fe5013a19c820cfef79dc14b4fdcdf50
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:02:48 +0000

    develop set indeed 571enough build

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_area_circle(r):
+        return 3.14 * r**2
