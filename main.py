d1c49fb366c891213e738164015308ac0a4a2d7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:58:49 +0000

    treat record flow press welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
