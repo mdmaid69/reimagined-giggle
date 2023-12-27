29a8cf625e4dc3224ab2c901fbbfd5e7e8acedfe
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:49:20 +0000

    common float spite fall comment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def convert_to_hex(n):
+        return hex(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
