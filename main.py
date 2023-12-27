2f8220e48555527f84347faf3b9cb205a8cd0780
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:52:47 +0000

    collection when rent teach great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
