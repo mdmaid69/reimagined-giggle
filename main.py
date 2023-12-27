0adb7a6953f10dbf03999e5f847fafcc4fa64726
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:47:03 +0000

    tidy aim run shame lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
