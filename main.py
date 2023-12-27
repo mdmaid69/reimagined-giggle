d0c72b89dbedcde14728fc7d1b4dcad105a36ecd
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:11:25 +0000

    angry pour across main rob

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
