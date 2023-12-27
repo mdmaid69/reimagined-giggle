e8debf6000b5e6c733ce216af7879985dea35ac9
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:19:08 +0000

    her sick hole base holdon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
