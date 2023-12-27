74c7cc995939b77fb7a2850d9fa0d8db70b6e24e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:15:44 +0000

    professional possession admit low phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_to_octal(n):
+        return oct(n)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
