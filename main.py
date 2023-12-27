aee285034c238c4aa409df33345a723b29754413
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:12:10 +0000

    though please custom instead workupto

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def is_palindrome(s):
+        return s == s[::-1]
