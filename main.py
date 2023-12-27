3ed7ed439bc96b6d777aac67ed6485dcdbe9bf1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:53:24 +0000

    earn direct separate consider human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
