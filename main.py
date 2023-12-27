3a4897ee707c017d4e34164ffd92a8d89ce3643a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:07:13 +0000

    plan in bend male describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
