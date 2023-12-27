285d1c3d4a77efece7adafb28d78465b16ffb42e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:16:52 +0000

    answer because interview even find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
