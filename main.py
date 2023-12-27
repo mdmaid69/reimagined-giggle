ef9597e24a27bca193aab48a16e73047d6b81f9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:57:31 +0000

    supply phone light advice level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_density(mass, volume):
-        return mass / volume
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
