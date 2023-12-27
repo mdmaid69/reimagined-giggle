4fd886ac39c992972dafa1e541cc0f40042958e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:24:02 +0000

    lesson squeeze spot otherwise form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def exit_program():
+        sys.exit()
