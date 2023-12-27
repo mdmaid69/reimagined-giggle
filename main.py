dda648f9b58f0b5d98516d7acad001bd88303976
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:51:00 +0000

    day OKorokay instruction hardly thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
