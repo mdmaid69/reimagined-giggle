19caeef7b5cf68cd1fc9fe313eb706e23a234b98
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:03:46 +0000

    breakup business nothing schedule feed

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
