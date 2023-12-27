5a90c20811f43152d292c3bc87d703ac7527814d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:06:05 +0000

    building typical mind common environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import os
+print(os.getcwd())
+import random
+def roll_die():
+        return random.randint(1, 6)
