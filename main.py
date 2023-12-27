86e458739f93e3a8ac3c4f75731be0e70d625a63
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:08:46 +0000

    duty lose petrol breakout surface

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def count_elements(lst):
+        return len(lst)
