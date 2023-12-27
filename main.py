7ef50097f6b560968d34aea6002b502783139d8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:38:15 +0000

    able through image fold block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
+def convert_to_hex(n):
+        return hex(n)
