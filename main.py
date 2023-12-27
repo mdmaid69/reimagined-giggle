81cb5f09f46396fd940faa8211a129bb2a608298
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:06:48 +0000

    worst first measure nature enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_length(array):
-        return len(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
