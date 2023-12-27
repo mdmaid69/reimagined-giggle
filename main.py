6a6219cac4c14b9f49133f502fc7e8509527aef3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:17:34 +0000

    dress life one work profit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
