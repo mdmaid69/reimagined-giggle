38a7158f57e3f248e6463af49377b90ba7222be9
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:12:58 +0000

    through sudden wander emphasize guide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-def convert_to_hex(n):
-        return hex(n)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
