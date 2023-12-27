1e67f229d5e1bb876818d8050b3a11deaa50c5f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:57:29 +0000

    so interrupt sweep probably exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
+def calculate_density(mass, volume):
+        return mass / volume
