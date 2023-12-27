428cf793fc56ccd38510aed4654aa4f0071fbcfb
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:33:27 +0000

    first concentrate creature lock fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
