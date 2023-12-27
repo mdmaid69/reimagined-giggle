6c81cf0f9c5c1a295e77131cfca0141d2c7a6bf6
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:45:01 +0000

    lunch fact office callout plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
