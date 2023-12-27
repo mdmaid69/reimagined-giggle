19d76c1db905008e062503b63aeb02b81c34a919
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:33:40 +0000

    hot more sound clearly challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
