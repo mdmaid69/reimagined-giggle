ce43134e7ff51d1c3c82f387f1a79657d8b91325
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:43:55 +0000

    stop natural rub correct dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
