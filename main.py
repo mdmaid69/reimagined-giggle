866de99063170088d508c07e6d7544c54907c178
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:53:03 +0000

    connection surprised pretend move anywhere

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
