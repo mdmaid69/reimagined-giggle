cea876fbb0d0f01926147f1bd04cea50570d42a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:04:53 +0000

    hit punish such police get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import sys
-print(sys.version)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
