d9d385227fd916ea2929563a810130bc7f2df720
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:52:45 +0000

    apologizeapologise mess willing which out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
