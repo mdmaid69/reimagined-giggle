2a8b5bbd0fc2723f179edeaf245e8cf189de7c04
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:20:53 +0000

    rent politics concentration computer grow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
