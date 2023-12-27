c8a9dc292aefc7f7d88f2b2e684341ceb82fad81
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:20:25 +0000

    warning breakthrough wander talkoutof market

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def remove_directory(path):
+        os.rmdir(path)
