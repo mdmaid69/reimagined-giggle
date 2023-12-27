0367d0068a699d19952324b78fae5da04d433bee
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:54:33 +0000

    peaceful accordingto bread tidy sea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_working_directory():
+        return os.getcwd()
+def sort_numbers(numbers):
+        return sorted(numbers)
