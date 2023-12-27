8d8efced7c7ba0aa88c10a7aa6db5276a6173f3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:34:22 +0000

    bend climb Pp sense communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_str(array):
+        return str(array)
