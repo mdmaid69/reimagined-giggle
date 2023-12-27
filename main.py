04b7566746029ffaa885d54f54d84ae9f19b4405
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:06:21 +0000

    comeup conclusion thankyou hello sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
