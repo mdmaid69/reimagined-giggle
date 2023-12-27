c8385ad9ec0cac86b5b9db933a9417f9e0650644
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:12:52 +0000

    fold few one scared float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
