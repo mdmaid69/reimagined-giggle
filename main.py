6c1a229d450b29ef8103acf9b54bbfea3caab6e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:09:48 +0000

    if contain then unkind regularly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def add_numbers(x, y):
-        return x + y
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
