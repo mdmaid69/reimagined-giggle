fa712c3aca7a93ef3eb6a28acce21779e3cec200
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:48:37 +0000

    tone treat disappointed unhappy carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
