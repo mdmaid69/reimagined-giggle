23482073d617f74b445ce4a0ec2beda93292a0cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:23:00 +0000

    but comment scare obtain pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
