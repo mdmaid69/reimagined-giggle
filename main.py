2a4bc8662aa7054f9714743e88117efcddace08b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:44:38 +0000

    outside forgive Rr us successful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_binary(n):
-        return bin(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
