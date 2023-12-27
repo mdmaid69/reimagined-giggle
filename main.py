98cf1244920a90c54568e23621863334cdfa3dff
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:35:56 +0000

    mention advise mood container pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
