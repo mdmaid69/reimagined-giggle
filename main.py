972cd8682fedb2d5ebd4bfa086c676e3f85093d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:44:23 +0000

    part turndown present few shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
