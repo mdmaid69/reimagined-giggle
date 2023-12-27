1e90af785941d3946ddfc64a5b5d4f04fd0b43ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:28:19 +0000

    square never view address thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def is_odd(n):
+        return n % 2 != 0
