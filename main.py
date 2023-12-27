ddc8530bf66de394126834b5d00a9b441af63e27
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:10:01 +0000

    trouble sensible warning further vanish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
