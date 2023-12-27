9cc3c4ff4c4f21300ac99995077d75c6652bd014
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:17:46 +0000

    imagination feel try surprising will

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
