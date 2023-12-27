40751c977ea1db0abad55edb6cffdda25175a259
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:28:05 +0000

    pattern contact great swimming music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+print(os.getcwd())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
