26572fe1ac88681d4b7a093562acb465538505c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:30:01 +0000

    address emphasis treatment goback(to) good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_work(force, distance):
+        return force * distance
