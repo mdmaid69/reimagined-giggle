c16dfe32e152972543085407c451727d54b425ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:23:03 +0000

    treat crazy error sky yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def is_odd(n):
-        return n % 2 != 0
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
