eaedaea77c17604c4e295ce46b75ffd553f37927
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:11:58 +0000

    belief only extraordinary always will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
