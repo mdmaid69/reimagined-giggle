f0cd028412dc75c7fc4102af12bbfc19a85b108b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:34:49 +0000

    free estimate tidy arrangement taste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
