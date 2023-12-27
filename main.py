7c83a33b4359ad0027fe2d4f195a5eb6d6afb44a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:31:16 +0000

    should future breakfast shine month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
