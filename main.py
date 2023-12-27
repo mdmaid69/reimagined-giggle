54aa8ac926032265bcc6f9297703a978ceb1d76d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:17:10 +0000

    similar popular trust slight goup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
