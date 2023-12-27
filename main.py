e3a8b9cb5750b3cceeb591f3418b3b39c0cd11d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:43:30 +0000

    turnoff top successful zero wish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
