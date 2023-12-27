59ff817a43b8932554515c115933845402744c7c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:46:22 +0000

    tired copy hold suit excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
