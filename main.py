5a3ab0b9d79be2b65a5e60621961b1b895ae9ab3
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:40:52 +0000

    keepfrom gas effort ask ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
