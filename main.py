dabd259e90489a9fb4d0b7e9dfea4b09fb3d9bd3
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:46:08 +0000

    hand afraid rub square certainly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
