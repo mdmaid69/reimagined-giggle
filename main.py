6bb7938c641346c6bdef5109f1793d3ad0dc8e0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:58:46 +0000

    scream memory bringback shout congratulations

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def square_number(x):
+        return x**2
