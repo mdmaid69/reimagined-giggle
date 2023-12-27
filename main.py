af907d5a3478ff7b8235f8a14944e7022defbfe2
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:32:19 +0000

    which delicate second neither teach

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def count_characters(sentence):
+        return len(sentence)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
