3def915477867dd35eb9dddb882120d3f1c6dd37
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:52:15 +0000

    disappear injury takeback force exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
