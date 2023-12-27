be15af7095c8d80ea4d9ce5b41e1a2b332ca6f29
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:54:18 +0000

    somebody piece either else hungry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
