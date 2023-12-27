e03582bace30fdd6e55016587fb973d440b38fb2
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:55:02 +0000

    now look top contact active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
