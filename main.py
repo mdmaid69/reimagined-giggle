baee609dc827504f39df16f966eb1fae2b5b032a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:35:13 +0000

    autumn hardly I soil dig

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
