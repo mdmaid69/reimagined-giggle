f99fafde5fd534c8c1541129d46a461a48a085fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:46:32 +0000

    account tip passenger slice exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
