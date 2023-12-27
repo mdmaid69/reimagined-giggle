a0e88dcd7352cee2e29506072d20820b6cfee61f
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:42:59 +0000

    increase travel dry last promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
