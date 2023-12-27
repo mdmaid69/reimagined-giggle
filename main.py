d7b869030ef6c1c75a63acac470b64628cc0d3b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:11:12 +0000

    defeat allow face demonstrate quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
