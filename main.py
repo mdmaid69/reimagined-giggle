dbe182fb481750cd4870097bfbbe3772923b23a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:46:01 +0000

    especially trousers so purpose care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+print([x**2 for x in range(10)])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
