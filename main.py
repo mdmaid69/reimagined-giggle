bf61ac05bd9552c0fb4d0990f016816530dd3c97
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:26:19 +0000

    fashion straight about defeat square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
