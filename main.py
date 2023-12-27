27604384d83c89ba07f5b49e452ca3a4309c015e
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:25:50 +0000

    total comeup sensible meeting set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
