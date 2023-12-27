3c0732b85529b3ccfd7f0c6ba6aa2cb769e3a522
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:04:56 +0000

    prize emergency object cough closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_tuple(array):
+        return tuple(array)
+text = "Hello, world!"
+print("Characters:", len(text))
