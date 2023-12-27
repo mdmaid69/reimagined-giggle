21f0f1363ea732e75830920551c5998447545f9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:05:12 +0000

    comeback effect Qq problem broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.ctime()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
