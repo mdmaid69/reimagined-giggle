61008129c475d2d8c3c791636726fcddbd106538
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:23:46 +0000

    test himself crash church comeback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
