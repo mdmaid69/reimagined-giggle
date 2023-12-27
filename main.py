983362744ab006840ea1db4f17ade1a8264284d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:00:49 +0000

    ignore grand scream present upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
