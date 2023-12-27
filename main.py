b7797023ed02b86a26fbdb12f807c49a78a74769
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:55:42 +0000

    them head proof but change

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
