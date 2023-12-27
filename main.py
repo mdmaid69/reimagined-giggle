e796502702cf2aa2e09ed4355cee9ea44ed5efdb
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:20:41 +0000

    remove lonely blame upset set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
