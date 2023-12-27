6633a36e6b835bbd52539a88d27a6d4958d5ec23
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:00:29 +0000

    workupto off lock him learn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def divide_numbers(x, y):
+        return x / y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
