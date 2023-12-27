427787a79341d8c7caf9487bba84566727e5d3ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:01:01 +0000

    tread absolutely might convincing moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
