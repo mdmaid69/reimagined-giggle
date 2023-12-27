f1dc9ead7a1a57ba7c8abc17209b675b30360ff7
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:12:35 +0000

    will left control afraid crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
