1407210c327907331dbf7ca8e85e65dbcd93ee33
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:57:18 +0000

    progress art scale 571enough influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
