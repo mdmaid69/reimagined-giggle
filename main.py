f46a595b955ab80f748e2eafc30370266da322f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:50:58 +0000

    service might workupto snow unfair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
