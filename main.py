0c1c46f5817ebcd09e6c9a13486f317dbd1201ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:04:16 +0000

    difference regularly wrong wish fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def convert_array_to_bytes(array):
+        return array.tobytes()
