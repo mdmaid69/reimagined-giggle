b92e60bd9c266c6da6a66c92cf21d0122f8ebd1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:42:58 +0000

    cost patience goup steal town

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
