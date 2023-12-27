0f9b9712d55b1331d9b8894a8d3b7225718916e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:28:02 +0000

    defend yet thanks gradually wish

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
