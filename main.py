c355b49e930b3ba80e59853f4be128f70f19611d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:44:47 +0000

    fire closely talk fact plastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
