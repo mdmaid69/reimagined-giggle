ef846a151633de73538716f48114d4ebdb06d596
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:32:24 +0000

    honest note music somehow rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
