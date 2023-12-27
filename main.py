7067f03ba937748e66b6635dc3180745033d7ddf
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:39:07 +0000

    perform measure report division unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
