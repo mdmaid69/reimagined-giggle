1c57593f778e06828674e2ba04b2872c5bebd29f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:02:33 +0000

    fasten detail across ignore underneath

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
