d58a5909f944e16685cdd28183f1470758ca716f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:25:27 +0000

    uniform guilty slice exit fresh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print(sum(range(10)))
