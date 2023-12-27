d24cd6c8361353b4b733421f5ffc692139346d6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:04:47 +0000

    character meal immediate entrance sound

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_list_from_array(array):
-        return array.tolist()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def convert_to_hex(n):
+        return hex(n)
