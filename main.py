f9fc3cc77c016f029d8872fe8e8b278577433166
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:26:02 +0000

    more stretch only similar with

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
