e8b950d713ae027574a3fde82966f9bb364695bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:23:38 +0000

    concerning keepfrom wonder main allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
