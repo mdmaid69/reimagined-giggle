968d04584a0040d6ef7e0566df584a8636487c88
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:42:30 +0000

    case how trial shame disgusting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
