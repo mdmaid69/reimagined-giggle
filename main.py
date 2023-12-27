d62df6828944f8e0be3f687e85e28b8ef8cf4e35
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:33:20 +0000

    back lunch accidentally serious distant

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def cube_number(x):
+        return x**3
