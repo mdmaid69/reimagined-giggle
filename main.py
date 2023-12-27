4a19994f57c0497414cfc0032a59e9e3a87f0878
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:52:32 +0000

    behave temper impossible rich crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
