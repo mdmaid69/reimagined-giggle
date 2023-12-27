b53c162e2ffec9d403974d82c01120959ed65ec3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:00:28 +0000

    flow central especially case getoff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
