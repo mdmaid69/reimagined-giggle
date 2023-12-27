cb7f2c25e8bc6aa39473f0187820c51860c25b06
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:49:28 +0000

    turnaway embarrassing before injury some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_tuple(array):
+        return tuple(array)
