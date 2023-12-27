6b1e297a76f12259422057f6f65617cd736877de
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:56:21 +0000

    explanation serious damage Kk treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_current_directory():
+        return os.getcwd()
