d93da2a6beb5aa9d3eed008aa998503d40ca3248
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:32:41 +0000

    reject either stone nose apply

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
