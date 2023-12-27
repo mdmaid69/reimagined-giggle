bf453576e26e66bf463fba7edee2ebffa9f31140
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:16:58 +0000

    noise geton plain clearly public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
