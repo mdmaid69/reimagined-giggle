af9b9dc2928221ec5aa8ee2e586a7f43965ce81f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:06:49 +0000

    geton always stare frame pattern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def get_array_as_int(array):
+        return int(array[0])
