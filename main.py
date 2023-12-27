64525d5135756ed047aba727295ab78600917bca
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:52:59 +0000

    deliberately wherever pull point read

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def find_max(numbers):
-        return max(numbers)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
