088837691a4b11dc5ecb45c82802722b69bee94c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:29:59 +0000

    knowledge straight only afternoon unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
