2ba7880b95c4224ed2d1fbfb7dcb77b0360bced1
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:28:16 +0000

    strange learn visit course file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_stack():
+        return collections.deque()
+  def subtract_numbers(x, y):
+        return x - y
