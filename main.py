bdf5913dc8680caed7cea2e79e0bfe3ee313c1d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:57:10 +0000

    convinced run bush laugh study

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+print([x**2 for x in range(10)])
