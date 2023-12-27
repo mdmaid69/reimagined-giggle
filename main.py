ec533658685077e0a1c9d66e2fdba72f967a22e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:15:40 +0000

    exploration downstairs quarter grand bored

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
