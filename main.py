de296e63a0b2cc753609ac3d2165f7ea7d6983a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:18:27 +0000

    obvious rate hard increase confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
