9bd60da2b7a5a1e02d7c8addb1955a7159c7c146
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:48:24 +0000

    politics see into think enthusiastic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
