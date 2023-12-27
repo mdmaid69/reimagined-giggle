55cf16f34787c2af2219ea76e9fadf84c07454e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:29:49 +0000

    centimetre letter dull wash strict

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
