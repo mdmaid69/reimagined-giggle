5ea6a25489ae51cafcbade724b44ba8d09de0b97
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:17:41 +0000

    card style if holiday afterwards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def convert_to_binary(n):
+        return bin(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
