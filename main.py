5543f4533b7ef4ce544f707e79e05af2123778c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:51:57 +0000

    pull suddenly wind noone extremely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
