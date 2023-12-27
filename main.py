d7587db9d8701b278a38c274ab11e0378c28487e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:03:37 +0000

    failure turn worried deserve doubt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import sys
+def exit_program():
+        sys.exit()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
