3392240e9c012fd3eb46bc0f6a048c63d7d03949
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:20:23 +0000

    dinner only way through reach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_volume(length, width, height):
-        return length * width * height
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_list(array):
+        return list(array)
