1a346f53378b4027bf858b2b30403a3cd882b186
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:07:10 +0000

    teach frame little communicate greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
