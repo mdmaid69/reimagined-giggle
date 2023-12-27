5713ad563db23225fc43042ee4a49a3038f77278
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:36:36 +0000

    preparation issue act accidental proper

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
