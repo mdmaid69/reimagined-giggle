08fb0cae4525d8bb8ae86a6048d56b75bd4eb3eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:04:12 +0000

    along house on wherever rest

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
