eab4ad1c4c3d12c226585e1f5d5478927ae4e9b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:27:57 +0000

    bored habit star tense fair

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
