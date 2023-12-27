08e517804903992bd4cc64995f61451c75e08366
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:16:15 +0000

    takeout Vv catch pleasant agree

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import sys
+  def get_python_version():
+        return sys.version
+import collections
+def create_user_list():
+        return collections.UserList()
