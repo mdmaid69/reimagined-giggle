c106b272d65f5588bdab2cefcd4b4b8bfb5ba836
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:25:28 +0000

    command sentence chief before ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_characters(sentence):
+        return len(sentence)
