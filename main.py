7cf9dede1d59ddbda4348d060da5495cd3f21b77
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:09:46 +0000

    control person runinto workout search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def create_counter():
+        return collections.Counter()
