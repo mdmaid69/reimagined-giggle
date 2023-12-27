2ceb9a8caee49a779b03d36d5ba595e794a7bb83
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:52:49 +0000

    react lonely common bad accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def reverse_array(array):
+        array.reverse()
