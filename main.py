2f72406fceccb11362d2c526c9105be116e27496
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:54:15 +0000

    experiment secret before unfair worth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-print(sum(range(10)))
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
