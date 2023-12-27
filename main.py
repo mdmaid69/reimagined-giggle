a093984c9954975579e705558d6a629ce027ee0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:29:10 +0000

    interrupt many press stiff confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
