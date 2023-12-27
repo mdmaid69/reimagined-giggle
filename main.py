ce58b7afafadf174cbe63b3c976c3d198b9efc38
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:34:14 +0000

    gooff dead relax keepfrom minority

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
