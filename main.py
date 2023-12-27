6865949026dc2c882509a3a8764228a2a3ccc51f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:59:53 +0000

    ago sound classroom uncomfortable every

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
