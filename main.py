70a91a02073bb56987991080db798ad26bff3574
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:36:35 +0000

    walk bill food hurt breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
