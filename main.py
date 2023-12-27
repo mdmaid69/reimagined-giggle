587b10df5d658948db318fd4fa84c840caafdfc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:40:56 +0000

    suckme throughout centimetre house hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
