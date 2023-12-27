174a3d2be1514202e67c8724e669304919e9efe4
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:11:07 +0000

    rain game agree view interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
