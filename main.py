fd28a0bcbc66201ab57a6af8ee52a47cd753c338
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:27:48 +0000

    apply full past any give

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_int(array):
+        return int(array[0])
