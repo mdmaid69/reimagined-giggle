4d1c6e8e95056011a2915ed4e5bb3fec1a6c54d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:31:50 +0000

    holdup best Ff lead block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def count_characters(sentence):
+        return len(sentence)
