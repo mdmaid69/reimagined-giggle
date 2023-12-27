64c046b020fc81de67bf9acebf396ba99fc36e19
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:21:00 +0000

    forget innocent chin business carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
