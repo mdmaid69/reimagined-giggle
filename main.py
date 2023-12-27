5bec6363db2c406e638daec2056a887bc8f952ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:24:04 +0000

    create their field capital prepare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
