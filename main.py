23453d35c884a8f68f04f742feea7089db9db308
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:42:52 +0000

    weekend cool smart suit fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
