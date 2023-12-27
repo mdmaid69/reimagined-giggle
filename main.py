1e6b219da4cef312556193f44ae69e145386bfea
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:49:09 +0000

    base put but goup twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
