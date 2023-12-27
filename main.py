9cc797bdfbc4f3d6e334c6b9fcb5999153ca00fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:53:16 +0000

    workupto error impressed gate related

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+for i in range(10): print(i)
