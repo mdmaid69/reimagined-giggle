b54ab7fb069659b3f9190c47295b28fce6d2d263
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:31:20 +0000

    happen serious top since everything

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
