8d005d24a12317abdac5c0d48e2fd81a08155b6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:42:33 +0000

    trade low somehow by middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def add_numbers(a, b):
+        return a + b
