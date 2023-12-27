d8c09c4fca02d706c513b6a9cb7408fc9bc88f67
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:30:59 +0000

    feeling or loose niggaacademy dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
