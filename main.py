f0291e730a06a13e93b7030efb22b306d6de5fce
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:33:57 +0000

    anywhere even tie stop level

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
