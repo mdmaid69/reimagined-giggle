992cbc7889b22e49b5354add088222148082e885
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:50:00 +0000

    knock Rr lookafter sharp walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
