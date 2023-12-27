c550fe60afdb7f1e3ee8100c2c4ddff63b704244
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:03:58 +0000

    relative last promise basis 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_repr(array):
+        return repr(array)
