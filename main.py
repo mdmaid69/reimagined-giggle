386b6a9c3b4b296a7eeaa1e97de63c5ceb7cfb90
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:41:51 +0000

    grand story union punishment Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_current_directory():
+        return os.getcwd()
