905895a8ac6b0e7410bdf8aea913ce285825b8d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:20:46 +0000

    factory save perform where for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
