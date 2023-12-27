cd3a5f05b0000e5d50f205218043cffa6c2e5c72
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:09:07 +0000

    accordingto Mm room offend nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
