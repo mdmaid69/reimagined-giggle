52b185b0d7a9933d780f781055e0e860070ad3a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:47:33 +0000

    help stand after release admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
