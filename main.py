379a30680042e8249f1f42c90eff5a49ece8913d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:11:35 +0000

    government trust regional blue frightened

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-print(sys.version)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_list(array):
+        return list(array)
