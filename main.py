a4a9f67e50476821d1297421523ddcffa431b198
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:05:36 +0000

    light put holdup chief tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
