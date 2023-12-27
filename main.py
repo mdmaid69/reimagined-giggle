6df852a5283fddebe7e80c95a422f83494f11c96
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:21:23 +0000

    rent system properly bank whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
