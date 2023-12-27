e1ab53714ac32b151d8f144544d5aded15013bf4
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:50:29 +0000

    mad area confident goodbye exploration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_power(work, time):
-        return work / time
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
