1db815d6c1c7e62d7a29029fbad5bc5d98479266
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:37:20 +0000

    question sleep win tape advantage

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
