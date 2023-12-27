6c4b20486be5cb3e3f88dd4c698800caef684d64
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:22:11 +0000

    live order situation anywayanyhow eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def subtract_numbers(x, y):
+        return x - y
+import os
+def get_current_working_directory():
+        return os.getcwd()
