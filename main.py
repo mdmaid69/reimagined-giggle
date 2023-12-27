eb19d1f473a7de53b795a164a92bb91a6d726c52
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:20:37 +0000

    so behave flat very brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+import os
+print(os.getcwd())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
