0b1211291d5461a89af8e8bad52cc7903a17a11b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:56:09 +0000

    arm anybody breakout ignore choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
