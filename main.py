990cd4f3394adcafc602de49998d50c066010bb2
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:06:54 +0000

    wherever stage cinema accidentally most

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
