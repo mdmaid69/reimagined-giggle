ddff9f583448f52d587990225d7c87182ecb4c77
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:15:53 +0000

    affect form face cause hope

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
