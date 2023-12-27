7fa86371d79d79e0deff3d2aa34643cea942e582
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:34:46 +0000

    what actual shock usual stare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
