9c381ee74159135608beb30068b2ac20cb57946c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:41:16 +0000

    gooff little like can priority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import numpy as np
   def calculate_median(arr):
         return np.median(arr)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
