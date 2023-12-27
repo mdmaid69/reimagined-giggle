175d846ffeec202384a9cdf64cb3d801d1f89e82
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:56:25 +0000

    into operate male amount waste

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
