c18bc55723237b8e18160adfc3b63a71f5a0b8c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:06:28 +0000

    birth reply act runout picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
