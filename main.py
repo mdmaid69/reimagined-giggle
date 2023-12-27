b1f61e2cc0a0394144777f8d8d058cb1f4f6f630
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:15:42 +0000

    neat keepoff disappointed direction serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
