11bc55719dcf36603fad5b081dc086f577f5febb
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:58:05 +0000

    string suppose trick unfair responsible

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def roll_die():
-        return random.randint(1, 6)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
