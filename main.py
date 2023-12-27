e54fba92808dddbb829c6d0479cdc571ac8a08b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:44:10 +0000

    new suggest warning influence key

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_pressure(force, area):
+        return force / area
