c1a5e2b4274b53e33961642f5e2a675548e636a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:47:48 +0000

    evening tell here side lend

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
