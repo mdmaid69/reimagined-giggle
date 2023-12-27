60cb7c1a095fef0ce5798b4798800c28b4eb27f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:19:18 +0000

    film regard best mix refuse

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
