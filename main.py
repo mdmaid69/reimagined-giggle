44e7a6e4567c7f179093c3c8e3a0c1a669715d58
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:21:25 +0000

    die blow where instrument club

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
