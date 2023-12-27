6ca2ce2bd5de3e268ea26418ad81fa7bcdf668a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:43:26 +0000

    wind excellent dear probably guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
