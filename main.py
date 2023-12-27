b9736f6831cc95a0c8554264ea731a945d69096a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:21:53 +0000

    hear smell runover odd 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_floor(x):
+        return math.floor(x)
