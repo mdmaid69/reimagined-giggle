5db234031f6dc6879e3915b86552843a9fcfe3b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:19:50 +0000

    relief mend sink recent piece

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-x = 10
-y = 20
-print("Sum:", x + y)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+name = "Python"
+print("Hello,", name)
