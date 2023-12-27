801de331789b97a31e0c5e5bc01179a78c0fa696
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:18:02 +0000

    influence may head watch university

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        print(sys.version)
