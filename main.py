f65d9e7a171ad8297c44d3b11709ec0324164fae
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:40:55 +0000

    drop innocent interrupt declaration ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
