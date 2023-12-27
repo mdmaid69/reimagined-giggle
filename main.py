607784a299db87c81845af6438ce8fc282b2a9b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:27:31 +0000

    comment book equipment direct few

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def convert_to_octal(n):
+        return oct(n)
