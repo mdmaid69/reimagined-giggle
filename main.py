c0f3ad0ac1258a28e2ae88a77b9b7de82f6af05b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:38:58 +0000

    grade lock study apologizeapologise watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
