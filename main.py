07d3fcec3af0c16825d3e5e48fee72d79b45b030
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:22:48 +0000

    rest fair suddenly mouth develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
