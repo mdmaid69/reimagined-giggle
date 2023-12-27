294bc92aeed017568ed24eb380343ed3fcbda4aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:56:34 +0000

    of under sing off road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
