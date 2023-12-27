c0cf71a5f36b9addf77659b749a67252d635661c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:57:35 +0000

    result food transparent bath pleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def is_odd(n):
-        return n % 2 != 0
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
