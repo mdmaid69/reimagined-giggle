1dfdc1fe7e593278369906f33ac84ba18ba62af9
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:17:36 +0000

    pot stick cough process appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
