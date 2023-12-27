0518864ef29422727b30c699b8b3e807c3d059b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:55:04 +0000

    trick standard sink opinion flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
