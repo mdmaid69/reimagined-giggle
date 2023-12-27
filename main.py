9f4becc0768138893ed0083066c5ddc7469cdaa9
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:17:58 +0000

    all door lean impossible closely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
