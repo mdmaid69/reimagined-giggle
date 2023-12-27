5a75bf237b0d0ca9bd4b8736f3654fad0a963600
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:36:21 +0000

    ignore block further maximum steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
