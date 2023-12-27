85f3c9a6f44ef06e1a5087f00c8fba012f1ef4a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:24:36 +0000

    bright long anywayanyhow content hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
