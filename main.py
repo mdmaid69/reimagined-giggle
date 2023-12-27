f855791f67e3226ab6e8d0c50e2042e1b32b777b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:09:20 +0000

    frighten extraordinary present eye tomorrow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
