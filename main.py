3008eb75fcad6afd571729d6a5afb7b5958f8ff4
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:29:32 +0000

    store your holdout radio community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
