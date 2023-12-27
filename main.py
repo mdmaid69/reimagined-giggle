60ec7ff7320895c2c737cbbf3184abc0698d9f8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:18:46 +0000

    to freeze turn nature touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
