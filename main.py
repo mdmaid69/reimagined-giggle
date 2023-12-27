69823c59f37117221d0d3df793986d7565fa8edb
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:56:21 +0000

    range team bridge agreement aware

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
