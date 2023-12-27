8aa248dc14eef9a47d7d8847984ed89f3495c5f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:05:54 +0000

    across question ourselves force rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
