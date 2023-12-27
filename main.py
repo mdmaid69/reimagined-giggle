fe39d31d43663b5093baa96487f838b8d3097b53
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:21:42 +0000

    whatever he dangerous cross coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def sort_numbers(numbers):
+        return sorted(numbers)
