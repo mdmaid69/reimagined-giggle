3891eff6ba5c854df4d6b4ebc5f97b52fef56413
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:26:58 +0000

    translate improvement acceptable total note

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
