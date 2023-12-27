caf067dd82d1cb0f5a48bfbfdbb302bd1ce838af
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:18:46 +0000

    approve love ticket play way

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-print(sum(range(10)))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
