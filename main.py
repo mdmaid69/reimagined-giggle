63863812112af4413cfe57b06af7e13caff9d4cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:52:26 +0000

    getup ride tip valuable steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
