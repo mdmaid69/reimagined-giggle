e9e9b907eeaffcfa7448dbffcb02fb5914811b2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:16:09 +0000

    section rent bound blow free

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
