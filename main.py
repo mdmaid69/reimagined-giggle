241f0341a6e0ddf50d98c6d3133a3fd3081aeebd
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:17:20 +0000

    callat certain Uu regard hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
