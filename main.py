62d726356d2de21251e27cddb729fe6b52d20057
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:44:40 +0000

    regret big keepaway hurry noisy

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
