4b48582cd7fc589ba055f62be04acd290cb9b245
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:49:27 +0000

    loud afraid black check accordingto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def sort_list(lst):
+        return sorted(lst)
+def find_max(numbers):
+        return max(numbers)
