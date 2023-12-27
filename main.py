d9bcb82a2ca2720fc9532aecf85d6db7e5a2e54e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:35:29 +0000

    concentrate basis keep estimate blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import platform
-def get_python_version():
-        return platform.python_version()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_speed(distance, time):
+        return distance / time
