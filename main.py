1395396f379eaa10622815f4dbab822fc4258cce
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:36:47 +0000

    born protest major suggestion judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
