b617d891a02bd159574745441f2218bc5a6f8a62
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:19:33 +0000

    borrow major under wish well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
