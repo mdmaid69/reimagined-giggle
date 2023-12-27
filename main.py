e3e979ec5aee9c07da1d5427ae669b6755f7a927
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:32:19 +0000

    street pride raw comfort record

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_acceleration(speed, time):
-        return speed / time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
