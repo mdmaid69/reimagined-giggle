a51fb777bd0eff25467c36872e5590a77e3e3fbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:06:14 +0000

    present getback account chat every

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
