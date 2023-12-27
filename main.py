1a3d7d26ece827ea772719e9fc31c998a2310f28
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:30:39 +0000

    now wander importance border anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_str(array):
+        return str(array)
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def convert_to_json(data):
+        return json.dumps(data)
