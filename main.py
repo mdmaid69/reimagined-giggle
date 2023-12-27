7a03b89763236c0de471b14ef5c618144970e6ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:25:57 +0000

    error bringforward label quarter order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
