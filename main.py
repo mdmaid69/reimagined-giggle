0649cdcc41de3b1ece4d1b168246068b5e93f54d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:21:12 +0000

    idea sentence undo immediate over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_pressure(force, area):
+        return force / area
