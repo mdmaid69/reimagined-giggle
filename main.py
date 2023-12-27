37e7a25c5739c40c3a1ea76a541ce7754742dd2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:58:35 +0000

    note develop anywhere trap 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
