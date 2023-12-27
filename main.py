4a8d9c15fcc6d4816b853097f46d51193100d03b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:51:43 +0000

    chat scrape turndown sale tight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
