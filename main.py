8704ee51039716c2464e53e42acd11d4707baf7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:47:25 +0000

    block free table exaggerate appeal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import numpy as np
-print(np.array([1, 2, 3]))
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
