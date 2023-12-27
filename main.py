4293cd9e26a74b45415f434f60fac5893ed87a56
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:37:18 +0000

    contrast convince first man middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
