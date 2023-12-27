bf43fff27863d6bef8f92948715ffc58e04cbf3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:29:19 +0000

    amount middle positive intelligent lookup

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def convert_to_binary(n):
+        return bin(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
