b3b273bf4d4e3610a7ddb3d78a8c791f58d8b862
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:05:40 +0000

    once true bunch all delicate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
