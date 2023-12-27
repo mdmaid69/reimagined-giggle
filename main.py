2ea85d3e6a279b25cbb67ae5ada48d0f61f921ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:28:20 +0000

    related shall important opponent badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def is_odd(n):
+        return n % 2 != 0
