9c7367ed7e593e3b1367325ef9ae9411be291e73
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:13:47 +0000

    habit impossible accidental traffic basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
