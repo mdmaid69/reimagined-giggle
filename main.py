f5da4318cd775853a80efde613629551c650d18a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:54:15 +0000

    regular health anything rush not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
