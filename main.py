723927468e98429b216938ed0d86f4e12fae5437
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:36:37 +0000

    satisfied convinced dress form us

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
