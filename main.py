e72eff6c933a179beb52d539d2b57b429c385926
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:24:24 +0000

    aim school secret through train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
