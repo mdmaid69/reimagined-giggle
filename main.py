990e8ae6fb68b91bf452146eaff0b91b48e6f107
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:32:50 +0000

    relationship set recent bin club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
