ff9c526fd0183ea30aa854ada2847a3795da7a18
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:31:04 +0000

    correction form fire blind bend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
