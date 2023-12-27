b8c0dc9d25be9e69f4e8607cfb97e621ec2f04aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:57:52 +0000

    light fire show sort own

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
