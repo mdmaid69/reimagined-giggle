b3d2da531644e2c2e2b201b36679561bf3a5f63c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:08:32 +0000

    birthday none comeback shop sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
