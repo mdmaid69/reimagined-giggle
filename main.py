4c010b4a64b3695655ba12fdbe0ca986ccb15877
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:17:16 +0000

    material state cross remove somebody

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_array_as_int(array):
+        return int(array[0])
