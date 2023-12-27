72a24a6d67a85defc6e40ddf829a45ebdf5c94a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:40:05 +0000

    direct various statement store available

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
