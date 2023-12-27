0a68d164f1e00bffafb4f64cc36a87317e1b1bc4
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:21:06 +0000

    interview hollow repair play complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
