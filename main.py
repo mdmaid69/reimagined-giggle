b1a9caf3144eb065a0eff9fa1a88bec8d725a1ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:05:24 +0000

    ground process degree risk eye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_unique_words(sentence):
+        return set(sentence.split())
