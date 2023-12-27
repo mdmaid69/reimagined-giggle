ce34858ea55ad863463efbd04f795feb38860d80
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:20:33 +0000

    design ring bill ago brief

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  def is_odd(n):
+        return n % 2 != 0
