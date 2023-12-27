12f110d98fee3c0df750a30f655d93693036a5ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:17:22 +0000

    scream frame past remark have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_index(array, item):
+        return array.index(item)
