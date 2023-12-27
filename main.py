d136d37fc578eb1918fe549f3c50af0204963861
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:40:45 +0000

    gooff bringdown comment female standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
