85f1200340a8be7e8d7508dfe18dc30a3e3bfd9e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:35:42 +0000

    favourite off Hh demand impressive

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
