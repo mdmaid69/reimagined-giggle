1197f3b07dfe9d18c9086c6b33a7ca789d2e32ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:13:32 +0000

    collection memory glass uncomfortable recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
