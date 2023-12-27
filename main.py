46229c5e6e831eb7b0f7656063b6219e4d0ec418
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:34:10 +0000

    avoid compare independent appeal importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
