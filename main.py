c3cd31338a8ec30001495e2bd1260591e213ef9d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:44:59 +0000

    programme gift error sting most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import platform
+def get_os_info():
+        return platform.uname()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
