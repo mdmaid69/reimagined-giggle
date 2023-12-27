55b90ae5ae833b42d9a812681ca197822cf66bc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:03:05 +0000

    fly kick like kick ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
