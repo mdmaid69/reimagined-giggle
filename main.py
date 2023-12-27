360c2e181b83a358734aa43cdc2a7fee87b8cfb9
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:59:46 +0000

    Itself apologizeapologise coffee teach may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
