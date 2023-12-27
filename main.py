6d650a944bd2c9d8aee821125fc6a1173b00ed17
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:19:44 +0000

    steer art mess tip delicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
