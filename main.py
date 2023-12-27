2edbad033c093759456f3b0fe7b42038055f28c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:53:12 +0000

    them fly keepfrom gather gooff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-for i in range(5):
-        print(i)
+import time
+print(time.time())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
