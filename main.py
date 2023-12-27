46d494a74b805634be1c5893e14fd514a845e35e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:14:44 +0000

    forget suppose disappointed home minimum

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def count_elements(lst):
+        return len(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
