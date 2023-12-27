b9443b3a65904c90754bb4aa633e39f0e39b3f61
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:00:58 +0000

    declaration everything level like shop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_acceleration(speed, time):
+        return speed / time
