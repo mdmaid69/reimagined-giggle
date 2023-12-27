0f07f57c14d6f3c9a4402502b132b16bb4dd112e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:48:07 +0000

    theirs myself drop hour evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
