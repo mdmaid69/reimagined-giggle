31ecdefe607604c168366d7b181bc8bb68fe5103
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:58:09 +0000

    subject loan entry succeed but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import sys
+def print_python_version():
+        print(sys.version)
