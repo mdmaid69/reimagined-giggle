a448d1d0f04ae099bed6564365a04a0d4a6cd9be
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:37:52 +0000

    rush trap limit swallow press

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
