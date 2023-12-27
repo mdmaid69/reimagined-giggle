036cb876081a53571e88307fce11cd87a2bee0bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:35:38 +0000

    breakup think tune compare absolutely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
