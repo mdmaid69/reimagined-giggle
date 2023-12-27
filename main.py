7b32e7169349348fc8109838a82463127253015e
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:51:31 +0000

    wash hard turnup party kick

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from collections import Counter
-print(Counter("hello world"))
+  def get_current_directory():
+        return os.getcwd()
