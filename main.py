989af8143cd739a185ed831b985fda8ce25f33ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:06:40 +0000

    control search thorough valuable send

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+print("Hello, world!")
