b5a81f44884e0a3447e888de9371695a5c9f80ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:34:55 +0000

    store Ss correct remove smile

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
