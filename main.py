758ffd36f0fe042c73236031156433cc894a7d66
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:37:14 +0000

    season lonely bringback bite opponent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
