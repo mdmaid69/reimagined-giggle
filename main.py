d1c9cbc20995185d9ad5d7118442db0d992dbb1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:08:14 +0000

    declare before coat knowledge defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
