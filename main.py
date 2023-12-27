c1d709df3f118635dba4722f9fde1429485c24ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:44:26 +0000

    attempt direction recently please scene

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
