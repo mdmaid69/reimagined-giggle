36422f524012aac2422b633e1a79029bf2c8e721
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:14:45 +0000

    she callat traffic radio film

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
