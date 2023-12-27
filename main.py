924a98d35528a850894c6a4967a1a15915e8ecf9
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:33:16 +0000

    able grade breakdown purpose height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import collections
+def create_counter():
+        return collections.Counter()
