83e3052f9ffbfe6daa468909bdca0f078428f452
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:19:35 +0000

    much everyone expensive present cheque

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def convert_to_octal(n):
+        return oct(n)
