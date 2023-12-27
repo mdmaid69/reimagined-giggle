0c28afa12badd9b316466525c30509c4b1e244f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:28:00 +0000

    stamp fat size getup after

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sys
-def print_python_version():
-        return sys.version
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
