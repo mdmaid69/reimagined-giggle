abaa23f4c171b5f98bde491da329ebc572731b25
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:51:27 +0000

    exist match deaf suffer round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
