0981b38b154375f9a7fdaa16fb20f048c277ed87
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:23:43 +0000

    mysterious body care owe add

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_power(work, time):
+        return work / time
