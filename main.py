aa1b475cc285506831be3846ff59a32c2c7a1e2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:34:25 +0000

    succeed little impatient appear temper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
