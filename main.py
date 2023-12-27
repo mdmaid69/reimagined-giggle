442c057e03dff6df0b63d2d9f57f4d9a651e8ba2
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:42:27 +0000

    although cheat breathe real example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
