f457cb7ced48ba241dc5497e9b92a1c3f6caea5c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:59:30 +0000

    complaint burst by steal crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
