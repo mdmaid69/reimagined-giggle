258ef2d8551bd07ecd24e78fb7324353f98a3f6e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:52:20 +0000

    suddenly wish positive Dd dance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
