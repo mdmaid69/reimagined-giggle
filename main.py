c1d0f044cc36cbd8a4b9e9fd9c8e77b1670c8716
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:35:43 +0000

    everyone hand attempt later regularly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-import random
-def roll_die():
-        return random.randint(1, 6)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
