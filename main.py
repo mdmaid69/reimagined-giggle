f1896839a82437f4f674f6c35e2ab41601da8ffe
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:55:19 +0000

    uncertain himself basic call combination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
