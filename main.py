0629a90e236b78bb9524ae9f370bbaaf04761913
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:39:56 +0000

    set butter fashion glad equipment

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
