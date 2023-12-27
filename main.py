41006000eb87c0baa96210fc2ca8a5dab7d7eaaf
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:51:30 +0000

    confident face evening hit wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_set(array):
+        return set(array)
