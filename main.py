cbcceb7ac33739e28051a4cc36b1f497d5bca672
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:04:04 +0000

    towards strength turnup criticize considering

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
