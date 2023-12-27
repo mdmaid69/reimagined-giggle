2ec812977c4f38ae11e68cf4b2ab2913bd7e0a32
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:19:53 +0000

    present friendly receive pattern travel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-name = "Python"
-print("Hello,", name)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
