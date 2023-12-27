00162ab84161b068a700ddc77e591430a58c9a79
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:42:17 +0000

    beyond best why teach serve

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def find_min(numbers):
+        return min(numbers)
