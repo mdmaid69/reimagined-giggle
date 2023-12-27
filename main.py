cc67a116a82198b8043c5d0ea2d1d6e409790018
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:12:45 +0000

    attempt gooff brush explain 569enjoy

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
