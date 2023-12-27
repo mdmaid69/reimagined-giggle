e9736bb6f637e62fef97b2016639191c8bc8068e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:30:22 +0000

    within pump fairly path right

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print([x**2 for x in range(10)])
-def find_max(numbers):
-        return max(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
