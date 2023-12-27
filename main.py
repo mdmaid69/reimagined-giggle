c7a74c4bfb698a79d4d34d1e6f81fddf9a9a4b4d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:43:23 +0000

    short Gg before mean lower

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_tangent(x):
+        return math.tan(x)
