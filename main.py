032794b76f6875883d0a1e0d7744112607253aff
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:23:17 +0000

    comfort complain set opponent also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
