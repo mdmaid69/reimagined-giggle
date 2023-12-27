93f49809f07a0948c6423aeddc35cb85e9ff96da
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:29:34 +0000

    anybody invitation brother thinkover show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def count_characters(sentence):
+        return len(sentence)
