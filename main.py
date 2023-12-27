ff0edf9c29cd905a62c1afd16f8650d53c9fda6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:03:29 +0000

    past makeup generous replace Gg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
