88614b5b9c6f11c6804cc99a0d7d1592e40f4df8
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:59:39 +0000

    responsibility shout settle disappointed healthy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def square_number(x):
-        return x**2
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_min(lst):
+        return min(lst)
