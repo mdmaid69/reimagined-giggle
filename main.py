a7954c54704127ee5521c65c152eed9c57a53da7
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:50:38 +0000

    discussion write content whoever Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
