6a96534c697ec25b6ea223f64b7a5cdedf1c34cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:59:31 +0000

    no passenger which famous usual

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def count_characters(sentence):
+        return len(sentence)
+def calculate_power(work, time):
+        return work / time
