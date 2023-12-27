4f13ef973be5b1f902b055eac073476f6649052f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:01:12 +0000

    answer otherwise chance later container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
