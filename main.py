3ac57634fb03e4dae8ab7fa39480b7b64dac585b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:50:17 +0000

    birth message fear noise sick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def reverse_string(s):
+        return s[::-1]
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
