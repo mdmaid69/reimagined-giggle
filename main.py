ef1d8db10ea5fbbd82b4401be386e7df8939e68c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:08:27 +0000

    knowledge exaggerate respect learn lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
