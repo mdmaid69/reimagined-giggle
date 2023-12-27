3040c90339429704e35b1183cf66f8e1f18e5934
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:16:52 +0000

    front recent divide tape disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import sys
-def print_python_version():
-        return sys.version
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
