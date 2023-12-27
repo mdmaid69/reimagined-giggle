a8593f05527c52ae361d37e66a3ced4d967224e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:20:27 +0000

    only square approve attach police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
