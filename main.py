6dd0429876e765c2970a54eb3559fd64bac12796
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:50:17 +0000

    conversation tear test suddenly future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
