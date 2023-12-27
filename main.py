7941378e154e33208ef5073e1dde65ef30725209
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:47:43 +0000

    wait strength generally practical key

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
