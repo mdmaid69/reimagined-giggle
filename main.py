d23fda3e2444969d0393cf46b178e716d754cf8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:28:59 +0000

    human extraordinary care inform treatment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
