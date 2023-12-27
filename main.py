00f166e5f1c8d9ee09c094ad45e81cb05acd154a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:41:57 +0000

    explanation lookup regional why temper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
