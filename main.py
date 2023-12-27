c1d99f211cbbf8859f782c414c6020279b730ba0
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:20:14 +0000

    question another person argument crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_hex(n):
-        return hex(n)
+  def split_path(path):
+        return os.path.split(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
