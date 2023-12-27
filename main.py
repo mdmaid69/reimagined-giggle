ec22e07bc93db1d48b3c83c99a94ced5649208cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:59:37 +0000

    damage carry keepout Uu afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_int(array):
+        return int(array[0])
