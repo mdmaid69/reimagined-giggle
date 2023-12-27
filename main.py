7864fbd9529be40d23afbc2511c566b043537d37
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:10:59 +0000

    risk new worth transparent shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
