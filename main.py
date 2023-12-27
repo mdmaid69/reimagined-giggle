64647bde3cc2b6af079b78fac2df8022121faf31
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:23:46 +0000

    vanish square trip suppose test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
