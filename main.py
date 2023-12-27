be885006eb4854e9f25880e09960bab9bace2660
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:38:21 +0000

    raw nose settle shrink part

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
