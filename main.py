6eaa129f518e99977d76a95e22b079c35187aacb
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:34:10 +0000

    clearly minute sign hair issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def count_elements(lst):
+        return len(lst)
