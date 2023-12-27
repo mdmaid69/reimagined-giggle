43c36be24b0cdd78abde0d9125f237da7b8f653e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:35:24 +0000

    more scare unusual relationship spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import re
+print(re.match("h.*o", "hello world"))
