e98daecc69eec285a454976be17d3b1b4be018de
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:15:04 +0000

    general crash runaway forgive better

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
