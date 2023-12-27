aa5d4e2814566b9f96135ba9607bdb926a800166
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:14:07 +0000

    onto this sale rank independent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import random
+def roll_die():
+        return random.randint(1, 6)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
