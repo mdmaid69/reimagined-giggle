8f9a9d7f97a6e6880cd407843d417b7b0b0877ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:23:52 +0000

    recovery story connection whoever regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
