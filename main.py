d7d90e19831799981267b6486ca6e76e19a40265
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:11:14 +0000

    sit judge us safety load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
