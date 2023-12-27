c205ecbac0b7062ecaaf2be760b89f011e778ebf
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:20:48 +0000

    reserve event healthy aware cinema

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
