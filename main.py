98ab8dd62037d8a28a23475b95c7b01e40f995ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:33:16 +0000

    song that popular exhibition control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
