b8d3e1aea513bb17042e2d81e2e42b9ab43c1ce6
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:39:09 +0000

    cum calmly unkind advise rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
