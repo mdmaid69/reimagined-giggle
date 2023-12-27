01b26e1e068d53dc84b1f100fedc78ac4f664659
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:43:59 +0000

    help certain win innocent event

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_perimeter_rectangle(l, w):
+def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
