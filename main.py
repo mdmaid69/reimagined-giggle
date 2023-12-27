caa801e090ae442f4442e432b37d4323f7814796
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:29:26 +0000

    either combination spin announce nurse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import sys
+print(sys.version)
