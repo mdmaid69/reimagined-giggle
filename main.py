279c8c5784cf2517fd849e93c66e22307dd17b99
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:09:13 +0000

    sea neither reasonable introduction lose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
