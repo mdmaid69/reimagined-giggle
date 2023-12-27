ddf907e0485ac24f19c003b427a9c3edef18dd67
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:49:00 +0000

    receive ever pump reduce return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
