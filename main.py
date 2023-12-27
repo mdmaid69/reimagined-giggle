a2680bd8ad05ce26099fb49b30f0d21f0c141ce1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:49:31 +0000

    because pile appearance line reply

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_acceleration(speed, time):
-        return speed / time
+import sys
+def exit_program():
+        sys.exit()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
