01833ae8ab0420c2ff911cf8ae326a57af49464b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:39:33 +0000

    shop rest lunch past discuss

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def get_array_itemsize(array):
+        return array.itemsize
