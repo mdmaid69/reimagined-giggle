7ec6e58fd24d5ce709ba0e58376b990cf7f1edf3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:00:33 +0000

    sport freedom low of buy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
