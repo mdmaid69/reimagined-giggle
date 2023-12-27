f758febcda7ed1b74c191ac8decfff9db0e4f396
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:35:33 +0000

    cover next profit quarter goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
