73d11c557892c7930ad6a7fa823c6f6520f7cd9d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:51:04 +0000

    combination smoke education concerning weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
