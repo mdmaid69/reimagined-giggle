73dbc4d99ede4193ddec876636c254ff8e85dc7c
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:29:41 +0000

    drop regret agree refer their

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
