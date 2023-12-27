4ec4212824e394e142d39ef21804ce2467dd7158
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:52:32 +0000

    slowly while turndown accommodation image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
