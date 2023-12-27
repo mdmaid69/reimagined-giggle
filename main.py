cc8444a5b98f18e4bcb0f8ef73179b80668905a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:38:41 +0000

    issue use violence shall office

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
