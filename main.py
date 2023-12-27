f8f38c245af5ec115b4eb00dad6144862cfd6531
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:02:42 +0000

    kind turn anxious seriously 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import time
-print(time.time())
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
