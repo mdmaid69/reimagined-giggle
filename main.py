90048fffa37df46ea4d1e0e5894bd6fa3a090357
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:28:07 +0000

    Rr rather block pupil underneath

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
