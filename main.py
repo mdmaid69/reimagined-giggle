1a05e2e7e484cf0faee4056ea14667882294aeeb
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:04:38 +0000

    keen cinema serious port each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
