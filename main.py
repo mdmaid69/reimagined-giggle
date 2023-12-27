7cdfde26f91e1e34c2ffeecec05c753b703eea97
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:20:58 +0000

    bath yesterday worst way disturb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def convert_to_octal(n):
+        return oct(n)
