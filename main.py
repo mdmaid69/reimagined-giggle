84fc42972bade9d950fb0b7caf4985967cf527e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:58:18 +0000

    embarrassing government lost transparent contrast

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import logging
+def log_message(message):
+        logging.info(message)
