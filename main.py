ba574bd85dfc52abc63c74944a90d3c0d4df6808
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:15:53 +0000

    joke touch belief centre gas

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
