6b580e23f8152ebc76504e179ce2b834c9e22d3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:52:12 +0000

    part woman star train nowhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
