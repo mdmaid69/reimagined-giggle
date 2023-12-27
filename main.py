d3ffe4bf70e6d9bcae30f7970af6abfadc38626a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:30:58 +0000

    form entry large teach aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
