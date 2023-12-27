a36da7c9fabb5bc4323d42f3d368849309ef94eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:49:55 +0000

    belief tongue slow key mark

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
