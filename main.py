2a6940dc82121f5b894e0f47df0f840b4861dfc4
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:13:49 +0000

    themselves bunch fit react unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
