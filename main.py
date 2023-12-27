4716b3242cbfaf11ac195976129cd11cc65eab57
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:33:45 +0000

    willing excellent foot loan moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
