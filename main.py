a128950780c72fe68eb37499800363afb6722586
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:22:46 +0000

    patience holiday follow pride generous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import sys
-print(sys.version)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
