b93462b4aed00b46c57b84d278a292046f26ea3d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:41:57 +0000

    eat young vanish float go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def get_time_since_epoch():
+        return time.time()
