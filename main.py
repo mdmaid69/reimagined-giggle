b3ebfcd0cdbe01a0df26205e66ec62da43fe759e
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:00:51 +0000

    favourite range wash backwards turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def multiply_numbers(x, y):
+        return x * y
