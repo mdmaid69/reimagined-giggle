100b3955de118feb21645a74bc4ce0251f5aa02f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:27:13 +0000

    yours hope appearance exact mention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
