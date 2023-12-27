ce1e1e979abea6c286ed0037c337f84a374133fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:00:08 +0000

    even second speak hate meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+for i in range(5):
+        print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
