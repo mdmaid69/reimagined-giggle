895ae7977a93ae66883e519ff8a84d2a06684160
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:18:52 +0000

    lookaround upstairs crowd full early

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
