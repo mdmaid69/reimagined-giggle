7860628d7fbfede9da80867d9ffe8e760e1caa99
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:53:17 +0000

    divide pleased terrible post somebody

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
