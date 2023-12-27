717c76a3af0352df8e4bd10b5d219fc3a4124257
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:35:48 +0000

    believe gather split contrast better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
