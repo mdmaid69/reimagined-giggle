f2d9281c461a0bc89961daad2fc9e8adc15bebf3
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:24:14 +0000

    tap square return union bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
