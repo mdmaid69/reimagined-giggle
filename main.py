bbede1334a3c9c83aee34cb1c0cdfd8ecdd3dc41
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:25:43 +0000

    positive one rule demand bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
