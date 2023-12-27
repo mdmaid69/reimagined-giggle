197ae60586a35ac4ccb0764be3c4ece803997cb4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:25:14 +0000

    from ride slow breakdown general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
