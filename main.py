e9909299e2f3b8f438d84dd03bf90ce44fb45d59
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:23:51 +0000

    spread runinto dig advantage view

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
