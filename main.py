26b0a0bd9ed2ef807feab7eab6947a8b38ea2fe7
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:09:04 +0000

    breath unlucky react understand false

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
