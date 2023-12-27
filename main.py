044e37e1628fac211ce009d09ba86a498beab3b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:00:29 +0000

    female bringback excited 568energy crowd

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
