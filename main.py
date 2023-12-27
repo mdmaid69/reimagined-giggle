071a4a7926f468956c73faffa377d5c840b2641d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:27:37 +0000

    connected owe cut tomorrow tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
