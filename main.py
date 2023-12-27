d9d43e3ce6829fd729e696d8f729afeba74fb1f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:32:05 +0000

    floor lucky complete hot beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
