df1f95d0c9aef2f33b38bf416094c2f2b87a00c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:46:02 +0000

    move inquire apologizeapologise smell day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
