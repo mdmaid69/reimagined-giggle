f4d98b109fe9e3590c1f02691cfb48dc9c518619
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:19:27 +0000

    fire bite long gentle later

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-print(sum(range(10)))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import sys
+def exit_program():
+        sys.exit()
