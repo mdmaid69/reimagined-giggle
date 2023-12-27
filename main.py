34301118fda2a9cfdc1e5c18870c8b5e5bb83153
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:33:38 +0000

    warning emphasize blind chin instead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
