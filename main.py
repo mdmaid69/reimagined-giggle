70ead367a35a19087cbdb3a311846f0bb092fb03
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:37:24 +0000

    laugh Kk defend food exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_speed(distance, time):
+        return distance / time
