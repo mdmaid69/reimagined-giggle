a9a45c721954cc3b9647f2419352df71b6fa215d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:11:20 +0000

    lean motion home suit interview

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
