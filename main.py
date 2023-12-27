409382ba20ac19fcd174fdbb44baa514a05eb44d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:48:07 +0000

    place difficulty safety alive conclusion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
