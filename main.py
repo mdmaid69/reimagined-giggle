56c01937b7562e65be3f31cc57e1ebe65ecea9cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:21:01 +0000

    bend occasional while anybody pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
