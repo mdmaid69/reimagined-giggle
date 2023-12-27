f68e3c2215f2a831329bb742cdb0bcdac382c4f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:50:15 +0000

    accommodation combine kind guess hold

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
