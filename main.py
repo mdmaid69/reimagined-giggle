e30fbfec14630f80d540413a94c4d4fce3a33e1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:31:55 +0000

    personal side deep price cap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
