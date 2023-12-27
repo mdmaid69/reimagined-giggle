fced24acea0df5fabf9882a3511adaca1b5b771c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:44:56 +0000

    side appear add crisis callat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def cube_number(x):
+        return x**3
 def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
