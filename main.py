839f3071ecdbc26225af8b921b305ac9385e0499
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:32:44 +0000

    wait probably block allow repair

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def count_words(sentence):
+        return len(sentence.split())
