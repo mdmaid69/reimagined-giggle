f8a7904b845652b5f44dcee8f047c06c10e71a22
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:43:05 +0000

    rob arm 572enter far common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import sys
+print(sys.version)
