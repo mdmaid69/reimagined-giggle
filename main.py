e2de5f3e527faa95ced05861d64918670f21ec0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:39:42 +0000

    bye few soft minute sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import platform
+def get_python_version():
+        return platform.python_version()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
