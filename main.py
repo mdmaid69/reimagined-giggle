39c60ddebc266e5bb17b0ad43ebd113f9aecfec3
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:52:24 +0000

    later turnup regret track fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
