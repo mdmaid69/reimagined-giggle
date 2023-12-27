c18540d303c90c4808595a432fd23785f9f3ef51
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:52:53 +0000

    sound offer summer own dance

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
