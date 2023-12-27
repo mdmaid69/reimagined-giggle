d2e27911090a2f6e93b1072b78b10cf74b1fdbd5
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:02:17 +0000

    accident stone count reserve calloff

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-print("Hello, world!")
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
