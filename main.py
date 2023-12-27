eba3a5e0e25dae854a462b35f8eecf02f74e1dd4
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:30:32 +0000

    voice be least business whenever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print(sum(range(10)))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
