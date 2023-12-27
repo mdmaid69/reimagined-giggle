1c8b30fd183db1475dce077eef759263ba9d59fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:53:47 +0000

    tidy constantly disgusting voice like

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def find_max(numbers):
+        return max(numbers)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
