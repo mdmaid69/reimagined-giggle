b697f48604ba8cb70519127748e6eb98eae11f0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:05:14 +0000

    boring repair not free then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
