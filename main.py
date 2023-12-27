eedebd2ff1a70ebc792d8be7390da768b2c34b66
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:59:58 +0000

    takeback stare temper risk few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
