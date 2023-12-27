93a3db4334916e3ed5e9d795f940ee58b4506d82
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:19:46 +0000

    thinkthrough damp section face spray

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
