8ffe7ea2fcbf1ea639d04a37a34516542178529c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:48:36 +0000

    list popular square proper board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
