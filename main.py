6801d6f1599e90290a50b5656ee65cbd2c4c3193
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:58:11 +0000

    whole plan little concerning Ii

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
