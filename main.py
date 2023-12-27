841420d8ef67a8a0ca8de2024e1b9a6667ac2a0f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:46:29 +0000

    now wonder smart mistake plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
