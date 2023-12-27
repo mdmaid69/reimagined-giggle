db7119f1a03287b8428b72d0803279ef5a8003ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:55:14 +0000

    over cut certain fasten principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def clear_array(array):
+        array *= 0
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
