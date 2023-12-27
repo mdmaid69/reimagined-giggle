d47ba045f36878b2265278ae266a9fee492e798b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:36:14 +0000

    suddenly trade tip steer university

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
