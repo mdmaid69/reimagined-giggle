f53eec49f0a7e60170623246bd5ab2ff325052e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:39:25 +0000

    shade reduce save relationship change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
