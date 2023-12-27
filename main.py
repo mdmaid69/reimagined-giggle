3331fbad2a1c13adeb192ad3b7c298c9f9953882
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:30:29 +0000

    visit set comefrom house bury

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
+import tensorflow as tf
+print(tf.__version__)
