5793d2b6814086301a8164504bd798d49982bb4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:21:29 +0000

    steady so well else record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
