c3b502758b59c7f15665cac215982500b5f96fc3
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:33:21 +0000

    throughout holdout test opposite cut

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_string_from_array(array):
+        return array.tobytes()
