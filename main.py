993501c39149ac1ecd34c26ee68c5be8c936f00e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:55:53 +0000

    afraid brother everything very hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
