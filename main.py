09c1c978c7f4431f05ee8daab7c920e5dc5b2fd5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:20:35 +0000

    shout scale issue speak ear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
