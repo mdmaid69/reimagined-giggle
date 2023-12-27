8b5d6164ae8e601b937535cd0d493b09f8306577
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:39:50 +0000

    guess swallow show ago watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
