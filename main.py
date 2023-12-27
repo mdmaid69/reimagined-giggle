369c697d0ba4a1ffb74da9065dce7168693202d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:40:00 +0000

    door shop pair study prepare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import random
+print(random.randint(0, 100))
+def subtract_numbers(x, y):
+        return x - y
