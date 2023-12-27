5323e05b4b111ac73bfcdea46971c8c972539a25
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:36:05 +0000

    use uponon length skill big

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
