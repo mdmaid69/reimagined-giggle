356ba6b621d17a009be3875dd67d058eea187292
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:13:09 +0000

    office spread account sure paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+from collections import Counter
+print(Counter("hello world"))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
