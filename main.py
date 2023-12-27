90a733c9951cea9edba2825cc2e779fee1196407
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:25:13 +0000

    journey freeze clothes arrival peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def reverse_array(array):
+        array.reverse()
