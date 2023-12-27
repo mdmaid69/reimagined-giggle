1f933411dff2d805137806f5a88510c25e9d3b73
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:02:19 +0000

    work Itself say process under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
