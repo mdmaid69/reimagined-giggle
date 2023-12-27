ca516242f1a07027949bb00653009030179b3dd6
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:18:03 +0000

    path rid pronounce treat holdon

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
