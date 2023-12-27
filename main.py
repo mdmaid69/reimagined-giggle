202545dfe2442dfeff3b1dd0eba2a6e8fba9ad04
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:51:07 +0000

    whisper act duty confident pleasant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
