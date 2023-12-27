3bf8b12f71ea1b3dcf8f0bebc627d24c0c05c7f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:01:12 +0000

    go neck visit worry bringup

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import json
+def convert_to_json(data):
+        return json.dumps(data)
