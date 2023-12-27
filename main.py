558bcd28344752669818f65a8d6ac965ebdd36e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:16:45 +0000

    whoever promise loud arrival obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
