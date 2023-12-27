de1bf185b9cfb786e7960452d39938c88a35e1ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:43:05 +0000

    pass good evening share tear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
