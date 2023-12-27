f0fdec62164cbc7604bba06e9dae1ba2d637fcae
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:59:44 +0000

    live pause floor stamp beat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
+def sort_numbers(numbers):
+        return sorted(numbers)
