ed362e27e36c9be526e8b90732d38cc828c9a93a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:19:30 +0000

    fork disappointed guilty gate computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
