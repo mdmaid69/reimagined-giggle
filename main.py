088f3ff38860b62b6f924a1a56f9943018df7cfe
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:59:34 +0000

    along win sport Uu worry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
