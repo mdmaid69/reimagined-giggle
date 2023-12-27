6f55f8e1a2cfe38721576567e9ec0de7dee57973
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:22:54 +0000

    shine remark apologizeapologise evidence mend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
