b581c15c38e7657417e58b7dc45b7c159f21e6f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:08:20 +0000

    sticky positive this bet joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def clear_array(array):
+        array *= 0
