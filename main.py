c931ee8e3fdab73be4960c6783c6d676a4fc08d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:41:20 +0000

    neat worried wipe may field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
