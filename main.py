ce041d13e7c80fe0aa7b72b337c7b685de73ed08
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:39:37 +0000

    dress goodbye breakfast anything use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_even(n):
+        return n % 2 == 0
