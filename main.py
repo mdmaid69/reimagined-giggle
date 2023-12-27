e66b6ba95ac5a2628562ae20a654931fe5640732
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:53:26 +0000

    lay deep fold challenge listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
