1aa35fc199b35d6f86c9e586f0571accdaf3b324
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:15:11 +0000

    comeup learn Tt possession clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
