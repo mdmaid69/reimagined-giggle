6f5b98ba27fd5fab37b650c1d73a622380ec0e29
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:00:06 +0000

    convenient practice rise declaration kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
