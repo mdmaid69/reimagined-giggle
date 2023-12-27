f9235ea441ff1c7bbfad6d8ab86db9daf386353b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:52:19 +0000

    science type stage land present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
