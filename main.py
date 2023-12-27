0fe659c758be102ea7f4ef2f109dcd71cdadb163
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:39:30 +0000

    herself post remember with Dd

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import sys
+print(sys.version)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
