1d6df939e2925fc054a58c74ef67c1dd6d456c90
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:13:07 +0000

    them new bag bread fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import re
-print(re.match("h.*o", "hello world"))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_density(mass, volume):
+        return mass / volume
