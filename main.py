01807d880662e230353718cb53bd9daa947c745a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:33:34 +0000

    remember thirsty complete chin science

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
