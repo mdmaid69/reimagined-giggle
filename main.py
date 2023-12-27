4ff923813191df0ef18648756296b4a71823a51c
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:23:17 +0000

    reach tidy replace divide stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_float(array):
+        return float(array[0])
