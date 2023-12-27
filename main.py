c3f1a0eda7184732d96901ca892c464a4f0045bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:29:11 +0000

    discovery active means supply often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import platform
+def get_python_version():
+        return platform.python_version()
