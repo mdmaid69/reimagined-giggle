37b89e13c805a3b384195b6b35fc3d3bf475c65c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:58:10 +0000

    tidy after pride completely height

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def is_even(n):
-        return n % 2 == 0
+import array
+def clear_array(array):
+        array *= 0
+def calculate_force(mass, acceleration):
+        return mass * acceleration
