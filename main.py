d1e0aaf52ada687a5fc5e5b6b02068828c4df738
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:45:56 +0000

    man run worst trouble across

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
