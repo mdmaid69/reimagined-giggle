6d62eac04e91106d02332b94e7a6922292de84cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:49:56 +0000

    who patience food recognize recovery

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+from collections import Counter
+print(Counter("hello world"))
