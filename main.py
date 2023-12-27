93e3bf9ecee3fae593de096f59569c568981c497
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:27:16 +0000

    base secret variety crowd immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
