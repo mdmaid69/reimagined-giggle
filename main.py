6d8903f38a146a580c5b37cd891f54545b966085
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:24:26 +0000

    practice trade small include amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def find_max(lst):
+        return max(lst)
