11b0332cd91168835cdf1c2bce6e244cbfba6fbc
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:38:01 +0000

    fair regard blind exam call

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
