4176a3227609ac6838f8d89894d8b0768f8eaf12
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:39:18 +0000

    fault breakfast advise strike today

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_min(numbers):
+        return min(numbers)
