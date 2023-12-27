7c07888e8b4458390985f18147312a96b3fcc3c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:31:38 +0000

    motion set card bury every

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
