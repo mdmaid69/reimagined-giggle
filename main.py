13af8cac4e1c6d849e5f8c49b3831fb62891ccb5
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:32:46 +0000

    leave bill forget back direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+x = 10
+y = 20
+print("Sum:", x + y)
