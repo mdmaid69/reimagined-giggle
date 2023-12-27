6543eae3f1f6c73299cb43fb9a979a9db5719d6e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:54:53 +0000

    know uniform always dull spoil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
