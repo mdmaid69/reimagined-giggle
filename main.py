8c791be7f3240fa8e0f95a3274fb68bf0c359169
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:06:59 +0000

    tight confidence contact shade healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def find_min(lst):
+        return min(lst)
