94902b9d8eaa6dbee8ad0a1e7407eaee7bbdad91
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:56:24 +0000

    towards story shake use tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
