67d8b0ff21b52ee950ce4338686e2c4c7894db53
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:02:39 +0000

    wise demonstrate available steep justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def reverse_array(array):
+        array.reverse()
