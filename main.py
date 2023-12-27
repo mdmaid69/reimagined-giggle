57c49e8515e26b313c01342d3a81406336395a17
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:49:19 +0000

    authority form regret gather brain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def sort_list(lst):
-        return sorted(lst)
+  def square_number(x):
+        return x**2
+def is_palindrome(s):
+        return s == s[::-1]
