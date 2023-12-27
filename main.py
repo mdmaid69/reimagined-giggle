43b32ab318e9f754b285128df28e1a34a8d76442
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:14:25 +0000

    stare fashion advise extraordinary style

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
