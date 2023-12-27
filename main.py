096554cf92c60111bcc47d66e2a172f65ae1a303
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:44:01 +0000

    think chase disturb emphasis ball

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
