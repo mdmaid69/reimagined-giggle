64cba868f671da3e2073336db5517c10f8f06ac1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:09:22 +0000

    clean mind for although last

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
