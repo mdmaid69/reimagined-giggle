8720f8bb2cad82b8bc5df2d30a9af6c32f90c8d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:00:00 +0000

    bridge convince character hour silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
