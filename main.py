d87674f00946540d90bbd1ab8dc8cd3ecf6317e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:24:39 +0000

    foreign bang government much famous

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+print(sum(range(10)))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
