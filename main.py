265fcc4c99e04b176b6230d6b551051a716ead7c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:38:21 +0000

    gap hide wash early practise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
