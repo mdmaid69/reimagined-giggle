e2741fda618cc85f032506aa06a05b82e7ecb7cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:37:53 +0000

    pull effort earth operation frighten

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def convert_to_hex(n):
+        return hex(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
