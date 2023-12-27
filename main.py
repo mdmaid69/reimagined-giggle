acfe22070ded10d47fe49027a1f8a0c0a00c34b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:27:50 +0000

    eager poor heart scared underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
