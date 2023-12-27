26787ed765c8e9c6900b7d43373fcff50ed41c55
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:08:13 +0000

    stop callup Qq practice describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
