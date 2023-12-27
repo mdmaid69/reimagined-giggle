2782e0cb8a544abb15df70acca0220d1b1c26475
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:18:04 +0000

    trip gooff clean pin serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def add_numbers(x, y):
+        return x + y
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
