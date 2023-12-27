359d8031ff1a26731167e18e2ce1f23c0e6a2219
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:30:14 +0000

    because schedule permission man murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
