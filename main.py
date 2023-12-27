24d00865a8747f8eb04108f2b08eaa864e408842
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:49:33 +0000

    will except forever either trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import tensorflow as tf
-print(tf.__version__)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
