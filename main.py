1c96e182136a94fa47675046db84a70b0102d573
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:30:43 +0000

    nervous worry worth the hole

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_work(force, distance):
+        return force * distance
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
