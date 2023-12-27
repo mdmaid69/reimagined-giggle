52aad738a6ab8decf3a04743306678868054a943
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:14:07 +0000

    burst responsible trap wish tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
