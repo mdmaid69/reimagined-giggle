34c096bac74957bd4e80f14944572a14a256911d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:30:09 +0000

    dinner agree top range note

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
