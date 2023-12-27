07bd6fb58769feaf2c6453cbd688ee45774b37ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:53:21 +0000

    tap pronounce proper clear top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
