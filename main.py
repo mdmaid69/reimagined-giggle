781736dc840dc2fe030344b373ed3c4452c55ffb
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:16:21 +0000

    left because flat regret mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
