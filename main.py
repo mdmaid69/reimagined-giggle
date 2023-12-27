ba76982ce16a6399a321edaec345f4ec6a6d715e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:06:38 +0000

    amount justice average compare sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
