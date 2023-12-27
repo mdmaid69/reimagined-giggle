2ef3797c4e5c0895d9d43b353e5e4be9d8aff2a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:08:59 +0000

    receive from autumn congratulations makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
