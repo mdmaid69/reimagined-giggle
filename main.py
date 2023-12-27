f6207d0d6291e242c8185cc193c4dfe1a2990de0
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:50:46 +0000

    thank search comedown nurse prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
