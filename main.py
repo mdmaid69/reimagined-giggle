043ab07b6caf5ae0e1100fa847b16030290ccfb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:00:04 +0000

    high hurt centimetre amount command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
