9b8254f4bb9cc0ed6b2702db8bf5ca5f96a645e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:56:53 +0000

    negative dream strike tragedy Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
