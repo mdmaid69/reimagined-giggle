47a38f7d34e75654dd52e775418a51a5197aa9f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:50:21 +0000

    clue Ee must certainly variety

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-from collections import Counter
-print(Counter("hello world"))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
