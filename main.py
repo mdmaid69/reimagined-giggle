eae3316535253d46c3c926e0a18c6946b41c5bd8
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:05:13 +0000

    freedom uncomfortable quick divide concentration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_current_directory():
+        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
