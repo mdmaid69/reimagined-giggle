f14bf62ca78afb3c24e41b7b89188fea0db21448
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:41:21 +0000

    week union familiar file spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
 import array
-def append_to_array(array, item):
-        array.append(item)
-import logging
-def log_message(message):
-        logging.info(message)
+def set_array_item(array, i, item):
+        array[i] = item
