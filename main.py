a3277a74064e10c5e387ea3d0e6c978f723c42ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:50:39 +0000

    near taste notice worth score

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def convert_to_binary(n):
+        return bin(n)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
