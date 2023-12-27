cbec5cc3da377298a793f605e2ce64fe155cd758
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:10:06 +0000

    somewhere argument male hurry opposite

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def get_array_length(array):
+        return len(array)
