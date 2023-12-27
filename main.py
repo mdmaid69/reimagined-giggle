2c1595b103e4b850f2ea7a3b5fb2294316f05a4d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:07:25 +0000

    wear suit hope goaway repair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(10): print(i)
