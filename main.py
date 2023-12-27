ba12066084ef611999e99da513f74af7aceaf012
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:52:31 +0000

    meal order plenty dark level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def square_number(x):
+        return x**2
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
