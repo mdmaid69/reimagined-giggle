c71cef54d03a598a8782d289735be4d70d16dd2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:25:18 +0000

    circumstance noise force unable begin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
