1d28f05c92a43e239f6db141d7154fb9b42548fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:49:46 +0000

    major weigh not much clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import collections
+def create_stack():
+        return collections.deque()
