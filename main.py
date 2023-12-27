7efc1ac55089d2e6a8d8a0ef50cafce1ca7cf3bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:41:33 +0000

    cruel picture petrol region clue

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
