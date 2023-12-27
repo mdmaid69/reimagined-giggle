2c202cd1c4d4a28b5df31eaa0a2aaf4f4bfee374
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:20:48 +0000

    total might above popular rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
