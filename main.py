6c8cc5a4f2b79b7554394c16e92e0be39df1eb81
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:28:35 +0000

    drive takeaway scheme rely key

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
