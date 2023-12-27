b6e8a8f9daf983ed6e482d2f5d3df8d30f7b2041
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:17:53 +0000

    several interview measure uniform speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
