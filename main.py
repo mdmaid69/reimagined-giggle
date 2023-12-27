fb8902ac5db92a1542b936c14e8bffac205ce6a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:56:54 +0000

    weekend properly food previous lookaround

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
