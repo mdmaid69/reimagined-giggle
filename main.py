897bf818ea19e21d9009bb7fc8f3d2336fe209e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:04:57 +0000

    reason arrival sticky remain news

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
