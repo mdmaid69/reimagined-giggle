0f23786516116e352eef64cc06fc23682f96c935
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:18:46 +0000

    harm frighten sensitive show leave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
+  def square_number(x):
+        return x**2
