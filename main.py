8d1e736ec45cb01a74156a24f3f74357275ffa67
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:37:45 +0000

    switch scream workout time pupil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+print(time.time())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
