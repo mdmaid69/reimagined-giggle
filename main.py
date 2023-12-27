4279436c091da2e78be95de7543265df6c897c4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:32:55 +0000

    impressive will OKorokay visit belief

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
