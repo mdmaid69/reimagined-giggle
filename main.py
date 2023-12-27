34f06ee0ef25d8469cc4ac170ae1bb1de529f810
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:50:16 +0000

    slightly dead Oo male direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
