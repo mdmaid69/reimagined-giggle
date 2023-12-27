51ca46c95bfeb680b8fb3a8c4fd3bb7df4b2d712
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:04:41 +0000

    Rr increase scream step quiet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
