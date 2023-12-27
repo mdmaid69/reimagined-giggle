743cb188edc3b815a7d5a27fe744c29dd590ac24
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:10:47 +0000

    temperature television powerful project favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
