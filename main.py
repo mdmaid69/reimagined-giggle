10f6d7e7b73ea9a9ea30dfb03722e61176a7f124
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:31:27 +0000

    play completely careful accidentally congratulations

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_speed(distance, time):
-        return distance / time
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
