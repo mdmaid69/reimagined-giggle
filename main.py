146289798a27f7955314f5097f7d0b8519d756a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:33:25 +0000

    return many teach Kk may

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def is_even(n):
-        return n % 2 == 0
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
