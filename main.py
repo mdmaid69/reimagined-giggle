a5e5731256dd2f4b02c151822c0cc9501c6c0121
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:38:37 +0000

    roll start properly especially everybody

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
