b40ca52ab7f6e024f808774fad2d000cac65caa4
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:11:58 +0000

    break office pretty thinkover discovery

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
