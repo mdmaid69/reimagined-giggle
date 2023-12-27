ccd519d896fdbc55ce9332ccec0a1e09a5b53e42
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:49:27 +0000

    doubt watch action even from

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
