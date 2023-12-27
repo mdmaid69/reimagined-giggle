0f75a93b3383813cea0e37cd78ed2a1850610eeb
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:40:39 +0000

    worry Mm break advice indeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def get_array_item(array, i):
+        return array[i]
