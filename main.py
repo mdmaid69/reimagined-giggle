84f1b73225556080adbdf95665ac4b3d8de4c471
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:34:13 +0000

    public deal 566end hurry hair

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
