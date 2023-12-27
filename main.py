5f4aa6fe76a0570239db713273c6cb28fd88c01a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:57:08 +0000

    suggestion work general move Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def subtract_numbers(x, y):
+        return x - y
