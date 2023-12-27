52f62430862a44f284df4e8884f21bd9a718f75d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:19:45 +0000

    him bang sleep seriously table

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_palindrome(s):
+        return s == s[::-1]
