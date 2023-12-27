3a2947f189f707060f58441648e8c2cbec749c1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:01:20 +0000

    land sudden ago spend nor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
