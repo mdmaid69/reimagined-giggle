be4b67fb140b5cf1bc8860da12fb8f77c900f18d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:57:53 +0000

    case cum turninto nice recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
