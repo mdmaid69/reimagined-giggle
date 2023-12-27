c6a2accb62c9524cdc7862d7cc22b735e65fd5f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:12:56 +0000

    organization basis alive breakdown memory

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 def convert_to_hex(n):
         return hex(n)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
