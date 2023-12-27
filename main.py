dff23ef1d4d669de79271d5e3948c06108b065eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:11:42 +0000

    connection cause partner popular no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
