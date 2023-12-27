3fd736fb0a0d558e6f2e89ffb46e889a5b79228d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:14:33 +0000

    sight defend classroom operate special

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def split_path(path):
+        return os.path.split(path)
