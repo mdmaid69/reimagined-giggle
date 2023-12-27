a8a52817ac3290722c60a01f20eafcea36d5cd51
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:43:04 +0000

    bottom disturbing reduction sister match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
