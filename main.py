78719525fe11951e24cc396ac3a12341a5b16c3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:23:50 +0000

    congratulate as allow comfortable performance

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
