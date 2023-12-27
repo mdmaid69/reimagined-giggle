175f4b23247c954fdb82763b3f6724abd6d82199
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:10:59 +0000

    off provide nowhere bored fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
