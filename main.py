4b9ca2d23b23e188675ef8bfe08ca084fb16e4a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:18:44 +0000

    sound ought duty comment study

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
