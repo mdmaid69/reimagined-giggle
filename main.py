8f6ab2de83a55d5f0b047f22495a3616eb5c6e54
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:52:19 +0000

    organize get earth afternoon hand

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+print([x**2 for x in range(10)])
+x = 10
+y = 20
+print("Sum:", x + y)
