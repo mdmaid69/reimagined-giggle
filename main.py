3e737f90571315c1471a8935504561cb07691639
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:18:49 +0000

    rain basis reduce on position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def find_min(numbers):
+        return min(numbers)
