8024429e58aac6b77579084040c8b1804daeeae8
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:15:58 +0000

    date system except favour cool

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_set(array):
-        return set(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
