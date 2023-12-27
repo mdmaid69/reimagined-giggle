a1ddf1b3978e4cf1f0bf5e4500b6e13f9b50aa0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:23:01 +0000

    command car balance destroy crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
