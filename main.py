21da1f37c87c16a4c975c62888a7d21f495ee11f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:01:38 +0000

    profit near actual depend clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def sort_numbers(numbers):
+        return sorted(numbers)
