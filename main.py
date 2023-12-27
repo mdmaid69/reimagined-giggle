70644687d313b48e76695d570011ecc833735a3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:23:35 +0000

    wipe rent contrast owe bye

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
