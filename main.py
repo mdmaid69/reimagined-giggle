2853cdf5d8d388e7637a41eaf139e58b17fa5dd3
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:57:08 +0000

    keepup standard still throw secret

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
