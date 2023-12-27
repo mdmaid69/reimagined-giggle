ac66b8415309e43705cfa637ce121659be95da86
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:00:56 +0000

    workon later warning seriously turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
