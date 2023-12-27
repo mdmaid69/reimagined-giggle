b8c69a125f60467ac8f388a31c66bb906403335d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:47:40 +0000

    close food peace second future

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
