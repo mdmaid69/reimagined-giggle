2fa4eb50377aa3e2f01209ffdaf237fff3372209
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:09:21 +0000

    secret interested exploration solve breakup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def sort_list(lst):
-        return sorted(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
