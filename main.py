0f492db7248ff25eb5082eff3296adaacf7aa1d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:29:56 +0000

    obtain organization gate fire remember

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
