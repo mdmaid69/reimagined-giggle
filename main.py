2736969c04c7ad539eab2af9ff6a00b336e417f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:46:24 +0000

    personal call worry smell condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
