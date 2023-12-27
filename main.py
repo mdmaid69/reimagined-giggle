b6c1483ca554fc09eb23b88cfb7993792fd669cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:39:23 +0000

    discuss turnon right busy use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
