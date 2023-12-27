dbd0a1f690f88338fc0a5b2a078e161a3fe64fea
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:10:11 +0000

    length obtain confidence phone crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
