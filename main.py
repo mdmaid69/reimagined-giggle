45664aef780b542901f5734c3d190a4f1c8dc532
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:31:38 +0000

    tend judge experience fashion easy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
