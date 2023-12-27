e194653815c5800081163d690cf0b2ccc06e197e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:56:44 +0000

    protection gate lie tight pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
