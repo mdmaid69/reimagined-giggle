768cb563f5b2bb9d9dfd699d9cc509ffbe7262a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:07:48 +0000

    drop patience combination tip burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+name = "Python"
+print("Hello,", name)
