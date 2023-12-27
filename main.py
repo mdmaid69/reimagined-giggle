8f3da6d92ab2b1a9ba74f520a7f840055f351328
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:59:27 +0000

    soil too instrument cry turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
