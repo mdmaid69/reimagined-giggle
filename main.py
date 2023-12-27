306bc0fda59cbfb84430b3aaf013c217784bb983
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:37:54 +0000

    birthday total change correct stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
