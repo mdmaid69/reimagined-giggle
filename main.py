36496249f0169b63f18d7955290cbe60e17dcb36
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:34:25 +0000

    care land standard early physical

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_rectangle(l, w):
-        return l * w
+  def split_path(path):
+        return os.path.split(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
