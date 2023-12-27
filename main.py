fbe1b2b6e776760bdcdec607b184d8e8d3f3d407
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:33:46 +0000

    surprising above ticket male yet

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def square_number(x):
+        return x**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
