cb1e654a1aad06571dcb648dfb62cc22552387dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:03:01 +0000

    flat car urgent risk square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import getpass
+def get_username():
+        return getpass.getuser()
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_length(array):
+        return len(array)
