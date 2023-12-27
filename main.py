6d8aee26332be1cee69a8551038beec3023f12a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:24:47 +0000

    imagination edge geton pleasure match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+def get_time_since_epoch():
+        return time.time()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
