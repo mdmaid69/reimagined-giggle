aef8d5a50f87aa3d4e64758ca475fed339c934d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:05:10 +0000

    Mm flood stamp turndown well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(a, b):
-        return a + b
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
