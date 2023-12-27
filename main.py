4a0dc389292d43343e03ddefcfd4efa390e38897
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:28:56 +0000

    again may free spread funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def add_numbers(a, b):
+        return a + b
