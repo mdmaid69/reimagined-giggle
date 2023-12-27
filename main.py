7787c1a35e2020c87deb067e95f00dab43a92a33
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:51:34 +0000

    period range drink roof care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import datetime
+def get_today_date():
+        return datetime.date.today()
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_bytes_from_array(array):
+        return array.tobytes()
