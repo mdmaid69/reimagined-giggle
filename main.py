2861589174e9c3d307633115f06289823892359d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:59:20 +0000

    worth risk sick surprising complain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
