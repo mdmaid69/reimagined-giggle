e4c1764f1254559fa31fff4968f741b0db5be3bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:17:47 +0000

    correct few complaint square further

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def add_numbers(a, b):
+        return a + b
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
