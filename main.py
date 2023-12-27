30dc34933b1e9fe8386fed273403966a7f126a90
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:14:25 +0000

    ride careless famous rough fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
