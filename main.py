9b39f126eb75e1719b6fac1994537ba2b6b6cd01
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:08:59 +0000

    makeup blue store play any

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def convert_to_binary(n):
+        return bin(n)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
