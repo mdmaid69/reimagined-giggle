a4b44ceb81caa1f14b23913556169c457d7f3ac6
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:44:18 +0000

    exam sound spill rush test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
