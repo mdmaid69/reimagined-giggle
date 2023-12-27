4675c0591fb13873a5d7453e5a2fe2900127be12
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:52:57 +0000

    history convince contact mean cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def cube_number(x):
-        return x**3
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_power(work, time):
+        return work / time
