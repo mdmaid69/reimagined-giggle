88afd202a797c520e53426bbdfe5c935f17f0d34
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:26:52 +0000

    shop force way rarely description

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_max(lst):
+        return max(lst)
