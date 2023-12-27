43716638fc8ee408802a243871ba1732945a4889
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:43:45 +0000

    soft 564encourage reach Dd accidentally

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def is_odd(n):
+        return n % 2 != 0
