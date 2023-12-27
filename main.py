f1ed529fb2708c7c7a0ca74ae6268e3c325edf61
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:30:16 +0000

    sure march bang remain maximum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
