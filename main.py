066fe7490c722d59425855e9c43017328dd2be80
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:29:18 +0000

    pollution quarter message cross cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_base_name(path):
+        return os.path.basename(path)
+for i in range(5):
+        print(i)
