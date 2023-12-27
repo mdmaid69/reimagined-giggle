bda3b9bc24fed818871126be67ddc3970721c569
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:44:54 +0000

    forgive fairly breakup tired will

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
