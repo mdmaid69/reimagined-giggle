60f8b2cbaeb082c39eeda8aa4552ececb74818af
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:08:38 +0000

    reasonable arrive remove spray tone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
