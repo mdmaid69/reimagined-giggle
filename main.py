1e5ac4cd86e44c9cc2b8efd6599497b1aac91752
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:57 +0000

    turninto win Ww sensitive call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
