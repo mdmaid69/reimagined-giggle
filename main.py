7663234dfed7b765d6acd0e7f91a964ccfc46a62
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:51:57 +0000

    rate provide all front either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
