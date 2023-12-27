6dd63ba2f972fabc1c782845b0f329fa9e20db86
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:20:45 +0000

    piece praise crash scheme connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
