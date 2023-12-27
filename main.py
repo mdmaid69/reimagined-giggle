c5c31f886cc179957f437048f50c61cb2209c481
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:35:40 +0000

    wave unit odd unable spring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
