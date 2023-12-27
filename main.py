5ed7121542b866b56420d5049eb529d2e787d400
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:04:19 +0000

    waste strong stupid can passage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
