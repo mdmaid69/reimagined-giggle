0ffc397fa2d086f0e115037ffa5cd414a5cb9cbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:23:38 +0000

    keepfrom abroad influence nearly shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
