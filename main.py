32f4a635af12735e136e26093da08de957df4f97
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:23:38 +0000

    difference perfect people view not

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
