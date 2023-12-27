0f8318385a805cffd61ca1afe5bb5cdf336f8b8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:25:20 +0000

    uniform work damage luck fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
