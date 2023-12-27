a245ab759389e2604a41f000d423de040f5a205c
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:41:56 +0000

    yours provide comeoff skirt taste

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
