3a9f0d26218fb3762ff97f44117d445c2d0abe1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:29:19 +0000

    race somehow have practise style

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
