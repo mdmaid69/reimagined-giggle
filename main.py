6d247d1876ddf45bae2d0efd3b489614bd7a7e83
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:59:47 +0000

    exit game 564encourage authority meal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
