2fb7a23a89e1d8398d6ff357bfac2eb31935f321
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:39:27 +0000

    recommend chin prefer snow soil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
