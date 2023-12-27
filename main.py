00f1f09169b1878c028b0d1b7c3a6bed6193a20e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:19:43 +0000

    peace mine hardly marry worst

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def get_array_as_memoryview(array):
+        return memoryview(array)
