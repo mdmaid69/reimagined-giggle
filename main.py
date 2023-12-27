94f590889b381777f8e30a8faaa219619349accc
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:16:42 +0000

    shade intend second scrap crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
