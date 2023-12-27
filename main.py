90cdf858aa29b88f173eaab04dea2fd08866c042
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:40:16 +0000

    thinkthrough trick choice comfortable little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
