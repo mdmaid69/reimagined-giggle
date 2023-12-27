5aa8625fa60df1f77462f612943855bea2066b2a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:11:44 +0000

    buy dream challenge power lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
