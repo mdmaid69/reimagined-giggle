08f0622b3adbbc15a39f6ee0b1bce07a568ee99d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:06:16 +0000

    type boil guilty comeup error

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
