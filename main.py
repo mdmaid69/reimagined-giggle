14f2dba8549f169e944cc7e2efc273cdae1b887b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:24:41 +0000

    man tradition slowly badly wind

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-print(sum(range(10)))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
