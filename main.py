ec51b5a66271b0542eae5335c8ed5a131b4958e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:33:51 +0000

    bury excited secret command culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
+for i in range(5):
+        print(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
