e74430b978961348f3d5a2b1226474995c3cd1a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:49:18 +0000

    collection well turnout direct hold

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import array
-def get_array_item(array, i):
-        return array[i]
