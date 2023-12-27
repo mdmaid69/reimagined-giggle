a69f61379a535f396d7d4b4db35be46174224220
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:43:10 +0000

    wide major share comefrom busy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
