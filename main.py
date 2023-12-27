0113435ec1471ce6349121cc0e73dfc9157129c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:26:19 +0000

    hole goaway move what book

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+n = 10
+a, b = 0, 1
+while a < n:
         print(a, end=" ")
         a, b = b, a+b
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
