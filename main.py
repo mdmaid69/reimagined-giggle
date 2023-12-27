dad856ee8271342bd7ac29e2f83ac44ab464f02a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:50:26 +0000

    spring pride equipment rush Uu

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def add_numbers(a, b):
-        return a + b
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_power(work, time):
+        return work / time
