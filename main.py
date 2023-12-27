217e711ecc827350b43d8fc2babe6cd6457fc6dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:22:48 +0000

    authority burn ours fine correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_pressure(force, area):
+        return force / area
