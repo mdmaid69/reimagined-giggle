38800bbecb4d8cac75cbbcc9ec5411ac41cf1d4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:43:52 +0000

    reply prepare bet one since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def remove_directory(path):
+        os.rmdir(path)
