cc48bf7278168207a44495dd534f223e26602e15
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:17:19 +0000

    sail concerned objection nigga free

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
