be02802bbb8fa36fca5e1b47b407f4c34427c636
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:06:07 +0000

    downstairs general own fall embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
