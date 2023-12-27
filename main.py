8cbd9a64df97b082978accc468c1113714534538
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:37:37 +0000

    tool interrupt nearly Ii thing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def square_number(x):
+        return x**2
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
