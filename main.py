3dfa3c9038248a1bc0bb84c2a469edba742bf236
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:45:29 +0000

    back flow right hello beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
