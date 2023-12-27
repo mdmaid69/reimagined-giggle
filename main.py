4d44b545aed0dee2336ebcf5e9336f40e2a6d6ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:09:19 +0000

    offence 179bite second beautiful bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
