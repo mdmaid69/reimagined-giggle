b81a9f561517118f072bac8ea53891fdb48efdf4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:52:20 +0000

    busy effort whom truth anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
