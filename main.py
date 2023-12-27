3722779e3d69c6d249efb51f63a221f31c06438a
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:54:35 +0000

    should doubt fact scale remark

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
