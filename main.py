e5bfaa13fe7da841de837303d10f65a40f0493d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:35:36 +0000

    seriously sort approval compare themselves

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_string():
+        return collections.UserString()
