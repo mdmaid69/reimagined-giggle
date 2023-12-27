04ff187b184586876fa0b7624d8a4f2f10f462b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:22:05 +0000

    transport bury attach breakdown ticket

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
