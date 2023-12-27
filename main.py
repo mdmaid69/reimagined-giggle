e3bc08598ae3be7120b54678aca7b7bdd8678b79
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:26:56 +0000

    down hollow study protection position

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
