0c9230bbdd9fbf37586cd451895bf45379bc0203
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:41:29 +0000

    meanwhile keepfrom doubt jump address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
