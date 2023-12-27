abc9533998a854cc421f2a895d33ecbc7d2377cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:07:29 +0000

    than peace lookback(on) second not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
