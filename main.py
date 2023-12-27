9b6089776671249e8d3c4e1d078ee1809d93fa74
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:08:35 +0000

    concern mine soon basically cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
