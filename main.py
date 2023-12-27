929a6db6bb396f101112240714e8464e4df3498a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:23:01 +0000

    length happen pleasant freedom conversation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
