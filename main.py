6f1e06e89b018a80f26d134083ba5eeeb43681b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:03:57 +0000

    understand slip truth put shallow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
