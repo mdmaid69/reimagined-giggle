14ee946610e9062d02b42eef94fa2788a3751db7
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:56:54 +0000

    show everyone slice worried perhaps

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+for i in range(10): print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
