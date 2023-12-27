8334571ab1f5e9736d95688327f8dd4e42ea1ced
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:46:39 +0000

    sail lead conversation proud house

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
