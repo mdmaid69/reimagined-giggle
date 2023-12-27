253c3aa3ba4c7878c3eb86c2a64d5df82093e102
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:52:04 +0000

    last only language interview majority

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_repr(array):
+        return repr(array)
