586d1ca348e9db743dc41ef9785850ee8956644f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:44:34 +0000

    obtain most heavy bridge turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
