cd14a805da97be0e3a0d6debc3ce3f22202ae72a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:52:01 +0000

    train patient print clue spare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def square_number(x):
-        return x**2
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
