67933991992666bee191ab9a3f5c22a7178f5521
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:58:09 +0000

    rescue space soil like person

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
