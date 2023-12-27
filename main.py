94f5f70f5c04573ba2cc13656bf77d04d38b9e8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:26 +0000

    position discuss score honest nature

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_area_rectangle(l, w):
+        return l * w
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
