a9da94bf9382b07d94355676608238edfaf2e281
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:10:22 +0000

    completely breakup our refuse tone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_circle(r):
+        return 3.14 * r**2
