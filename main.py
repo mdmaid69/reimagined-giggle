a5e08dd7df32089c629c8933ac2699a70960977d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:56:38 +0000

    date instead intelligent weather complete

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
