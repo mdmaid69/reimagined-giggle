15f0771b14bb79bd8e24b2882e1ce6aa95092206
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:07:18 +0000

    declare window sting influence guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
   def change_current_working_directory(dir_name):
         os.chdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
