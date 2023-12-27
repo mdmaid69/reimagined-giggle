b63606db5d2611d3c2177f838bb22ce1864db2ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:38:12 +0000

    worth stupid comeout mine light

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import getpass
-def get_username():
-        return getpass.getuser()
+def sort_numbers(numbers):
+        return sorted(numbers)
+def remove_duplicates(lst):
+        return list(set(lst))
