a741e9321fad0176c2ce2bdb344e55971a08246c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:45:05 +0000

    error limit if language dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
