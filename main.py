f255e419c5925014dc7e1ea415823a4b01713f0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:31:56 +0000

    read name Dd runinto scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
