384c25f7aa42a75648ad043e8cd1bd08856596dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:25:26 +0000

    side television name lower relationship

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import numpy as np
+print(np.array([1, 2, 3]))
