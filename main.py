32f732f63535a1b465f94cd99932495c88ba49dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:25:23 +0000

    spot some fall goback(to) statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
