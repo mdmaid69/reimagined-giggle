4fb1c7fbdade40a6665b4873f7fccbc27e234f77
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:27:07 +0000

    best height pain like swing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
