da39b6c8cd9b71442db3f579814bd37614126808
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:40:35 +0000

    admire destroy smooth importance like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
