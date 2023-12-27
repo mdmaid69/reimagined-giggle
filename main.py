cf1fd65cc605aa8f5fec36f4d84b6ec593cce22e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:47:55 +0000

    floor impressed payment than flat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
