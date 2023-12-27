654bf606be04b10af133e7402cd2b7f3a4068f0f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:19:43 +0000

    music Dd geton should seriously

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  def add_numbers(x, y):
+        return x + y
