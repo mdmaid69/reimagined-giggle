599d7fd15c373e8c8b895edbc7898f5e4bcd59e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:25:57 +0000

    improve burst gradually aim instant

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
