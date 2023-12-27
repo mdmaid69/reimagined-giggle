fe3dc8a6775504883a6f7549f8685847c114ee0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:41:10 +0000

    then badly attention weak miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_density(mass, volume):
+        return mass / volume
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
