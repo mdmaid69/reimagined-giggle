6ebbc2c4dbc6b5527625bb6a7de46a0708ff49a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:25:10 +0000

    sell proper shock top takeover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
