88960cc92326c0b2afbfd4d5b0fff8f4a8620853
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:25:35 +0000

    drive receive behaviour start strict

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
