b208a988488b899c9a5f4865b863d674737b3512
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:33:13 +0000

    bored film content slow bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
