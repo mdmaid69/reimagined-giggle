2af4aef0fcd0dab6c7692192705e3bdd57039fba
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:36:48 +0000

    season battery first brush vanish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import time
+print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
