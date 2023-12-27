c3c1f02fd60a0ec92f7f38598c4d3b600b3541b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:54:51 +0000

    design defeat past thumb confusing

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
