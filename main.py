1069441356abcbda8e2cf719f8f224f275abf2d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:46:57 +0000

    comfortable swim acceptable light handle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-for i in range(10): print(i)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
