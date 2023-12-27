ed229650d3177f0ffec45e4208b1b35b2bf7e267
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:23:00 +0000

    determined getback about acceptable cross

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
