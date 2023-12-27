2e21bbe20f36f89da0272b916b397160389e6366
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:54:19 +0000

    fast forever brain dull next

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
