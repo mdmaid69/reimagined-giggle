e7bea067777bee3664d18a3892ac0fdb2d092304
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:42:02 +0000

    thumb steer sometimes learn soil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def cube_number(x):
-        return x**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
