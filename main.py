37b48c6cb4364ffb48a49f05fa903e18a8ac9a22
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:44:27 +0000

    finish he less pass rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_length(array):
+        return len(array)
