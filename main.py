4e67f14a3eaf51bdb4cd598b2263dbc583ec0967
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:46:08 +0000

    book clean stand no turnback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
