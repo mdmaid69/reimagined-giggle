f56e3c6783074588d6394901429cc78215640f1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:33:38 +0000

    appearance giveup concerning ring refer

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
