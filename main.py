0dfdd345082c8d1dd0f12d88af7b2abf43ded232
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:51:49 +0000

    mark keepout fasten soon shade

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+print([x**2 for x in range(10)])
