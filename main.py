2d9b0ec0f4153d93245aaa04e29c8f42dcb9b50d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:21:10 +0000

    except home belong especially tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
