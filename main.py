78e61996e1d8f1f7046c5255d600e65c5e15f43b
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:08:33 +0000

    apart discovery necessary moral tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
