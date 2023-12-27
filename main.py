9018061bc5c6d2c76985872ea74d2846e5864a61
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:06:25 +0000

    mine kind offer narrow operation

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def square_number(x):
-        return x**2
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
