a7b5b7430c43583f70a43507bfc5db191a64935c
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:25:07 +0000

    distant fire careless cry thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
