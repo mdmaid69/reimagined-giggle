eeb3755bed633ffe8a88a9c2f743d59b947fd61d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:50:32 +0000

    car shock through suckme breakup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
