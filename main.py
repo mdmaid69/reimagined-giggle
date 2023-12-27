00f7bec473fb3e102bcb7bf5f032079fa6c88362
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:13:22 +0000

    play confuse plate nose terrible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
