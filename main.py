5f9e0cfd11e1a04c061ac1d40551163df1216a99
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:51:30 +0000

    centimetre acceptable table warm taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
