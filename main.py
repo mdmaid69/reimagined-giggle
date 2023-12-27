dd9c8de0337d1000942df30468f71e2835b1ad7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:07:25 +0000

    separate compete slide crowded fix

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
