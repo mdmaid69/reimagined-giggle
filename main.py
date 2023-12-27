b71aa9cfe973acc126a243fe7c66f7d247af921c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:52:43 +0000

    arrive respect lookafter no fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_time_since_epoch():
+        return time.time()
