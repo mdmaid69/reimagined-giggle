ffc2d4d15b8118cd3dba35fb9ade5408af6d9e0f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:05:01 +0000

    belief strange fight reaction class

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
