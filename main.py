a039b201b2fc08612832b2ef16411e710eaeb5b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:48:55 +0000

    only strict nose floor roof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
