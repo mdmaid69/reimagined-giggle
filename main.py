d8e8a9021ddffac2f700a4f337fbe3cfcbb4ccab
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:19:38 +0000

    comeout generous so charge weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
