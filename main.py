e7c98a9de8797a665f1600284e19d129d4a376d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:06:19 +0000

    excuse comefrom travel pronunciation fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_array_as_str(array):
-        return str(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
