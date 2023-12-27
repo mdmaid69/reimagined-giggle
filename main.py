39b9fc54a5f6cbcb60dc24c91a72b36b15e0cfcd
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:12:35 +0000

    quick debt actual argue rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_directory():
+        return os.getcwd()
