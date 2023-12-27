9742ead6462e690ffa18f02e3253c9e1326be7e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:09:46 +0000

    regional head mess steer appeal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def subtract_numbers(x, y):
+        return x - y
+  def convert_to_octal(n):
+        return oct(n)
