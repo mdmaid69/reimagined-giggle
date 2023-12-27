08722582c7a89ebb9ecf3a7aaa7a806c5340ee44
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:01:32 +0000

    true whole order know strike

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
