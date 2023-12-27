ad6f2d5f6a0045e4779693f69a8a170789ee9751
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:31:50 +0000

    vision arrival sleep Jj thick

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print(sum(range(10)))
