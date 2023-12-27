4a0d502963600020c04406810a169ef5708179f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:44:45 +0000

    because learn sort rich stick

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_area_circle(r):
+        return 3.14 * r**2
+print(sum(range(10)))
