05b3dea4b190e875a7d03205bd816b01f0102b76
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:43:13 +0000

    admit slice move least form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
