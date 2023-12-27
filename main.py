9f30cf596cd079ac54618c3f9c2388072eb1fa4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:08:32 +0000

    limit sing also mark sign

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sys
+def print_python_version():
+        return sys.version
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
