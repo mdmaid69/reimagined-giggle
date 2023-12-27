19f4541b886a3dd87dc78ce02b1e81d2437e0214
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:21:27 +0000

    risk brave opportunity ground perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
