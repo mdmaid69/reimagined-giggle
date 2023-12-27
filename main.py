2b4f4051ebcd88b387de4fde00ef29fc0c617949
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:33:36 +0000

    history person lead create Nn

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
