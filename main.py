ff472103868c00811001fa51b4f6e151142122b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:32:19 +0000

    pick trick maximum tax inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
