43f2b8823bbfafc02d709f16efe0d05ed5f5a3fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:35:18 +0000

    comeover cry push short tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
