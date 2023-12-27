93c9f02bdae1b6515bacf57f4e2005c6a5ea8cb5
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:07:44 +0000

    tip deep copy race cheek

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 def calculate_quick_ratio(current_assets, inventory, current_liabilities):
         return (current_assets - inventory) / current_liabilities
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
