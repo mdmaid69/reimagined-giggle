de5fea5cb3d59816b73789df54e9cc5bc4b2a887
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:59:29 +0000

    advise few unkind here left

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def remove_duplicates(lst):
+        return list(set(lst))
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
