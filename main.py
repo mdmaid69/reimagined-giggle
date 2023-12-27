93b455d6145bfab08318687390364ca2cf578513
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:21:54 +0000

    right correct damage unit cost

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import sys
-def exit_program():
-        sys.exit()
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
