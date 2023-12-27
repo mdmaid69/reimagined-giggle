4c60c405cb9148a6d1286280d31c01e981081d41
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:50:10 +0000

    public bored from smoke quit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def get_array_as_list(array):
-        return list(array)
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_as_set(array):
+        return set(array)
