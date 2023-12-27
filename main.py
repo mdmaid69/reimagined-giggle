37cc8d22bd7d5a402fb96b8e484430debb690ca0
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:28:15 +0000

    bringback embarrassed among criticism tear

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
