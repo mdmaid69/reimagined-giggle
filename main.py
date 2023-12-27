818ee3cf10a0e9812a8404e9a8c8c5ad4323b241
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:16:07 +0000

    heat age shoot backwards study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def is_palindrome(s):
+        return s == s[::-1]
