16643520160b5b78214d1252020370b552812a6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:22:14 +0000

    forget strange pool offend second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
