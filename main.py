65ffaf184043ccadbbc004b66caf47e044b8f9d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:21 +0000

    unable hole feature science clearly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
