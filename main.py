90ae1913ae1e34b59ca85d7e24657e4bcacd9a85
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:57:23 +0000

    exaggerate himself apologizeapologise thank listenfor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
