71f41b841d676d2542e470ef3ee224e24159eebf
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:38:59 +0000

    comeup honest lose payment every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
