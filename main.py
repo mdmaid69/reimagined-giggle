8791dab2518cdbd629cf56a2d6b4bae39920e694
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:33:39 +0000

    information win raw slip matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
