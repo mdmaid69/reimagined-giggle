64328068188f49ed20aae27e53e1432a8fa1a3f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:19:34 +0000

    Pp pencil fear must plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
