517e963db3b830c8af191ab71590c181fd978bb1
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:11:48 +0000

    bye freeze not handle teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
