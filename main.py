3c0f2a8d3a55b59b43fb583db023b2b4466ee702
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:27:23 +0000

    bored shout passenger announce impression

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def convert_to_binary(n):
+        return bin(n)
