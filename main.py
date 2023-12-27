08fc472d459de958384ea303b367ea3c9f684820
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:15:23 +0000

    pride survive gap makeinto sea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def find_unique_words(sentence):
-        return set(sentence.split())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_pressure(force, area):
+        return force / area
