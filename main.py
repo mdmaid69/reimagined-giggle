8454981b0b35b87692c3e311d8d7c0b94c2dac7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:13:35 +0000

    lookaround calmly key fly snow

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
