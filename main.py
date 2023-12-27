e1c03ed4e4da7319e9bc3ecef33760cce1a5a536
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:34:24 +0000

    explanation occasion phone slight fun

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_str(array):
-        return str(array)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
