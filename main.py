15715078cb818c92e164991b8affed4945037bc7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:22:09 +0000

    repair play should anxious quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
