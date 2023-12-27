fb10d8ff5ff9bb85e3e20b765bdd99744ba883a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:09:44 +0000

    seat cold bush considering welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def subtract_numbers(x, y):
+        return x - y
