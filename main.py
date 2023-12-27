c1867131280cd6acd00ff63480d0a9733d22b44f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:17:07 +0000

    arrival treat crash gooff in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def remove_duplicates(lst):
+        return list(set(lst))
