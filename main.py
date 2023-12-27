2ff99d248f99045e5a07f85e61837df5cc34c4e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:23:19 +0000

    gate have laugh bus safety

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
