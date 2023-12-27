ec184e27ba32fab8011c34cd63024f1dff4f3ecb
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:01:30 +0000

    everybody best someone area callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def find_max(numbers):
+        return max(numbers)
