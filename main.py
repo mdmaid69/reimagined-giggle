56401e82148e9c57ae565a4e7456c34b5dac4139
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:13:07 +0000

    turnback fasten holiday thinkthrough change

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
