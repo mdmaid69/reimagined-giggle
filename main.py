f8b583f24497c315cc4cd93958e7e0d152515564
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:32:41 +0000

    oil anxious add switch ocean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def cube_number(x):
-        return x**3
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
