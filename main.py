d6fd9a66ae410f5cbd26b96e8747555d6418e54a
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:36:37 +0000

    relax protest rain education wild

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def append_to_array(array, item):
+        array.append(item)
