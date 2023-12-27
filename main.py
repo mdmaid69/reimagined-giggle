e132e50faf70ce8f2576f8cc3988b7d7e628d6d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:46:41 +0000

    pause become what board fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
