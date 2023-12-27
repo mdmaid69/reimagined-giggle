f80088d737c4e4246a1ae58dce8564f1d16915ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:21:00 +0000

    convince excuse production shopping scrap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def reverse_list(lst):
+        return lst[::-1]
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
