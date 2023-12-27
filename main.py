95466ff18029db5f913270dcd15ff6e074345ae5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:21:40 +0000

    shop while introduction debt break

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
