23b2e5b190cbbce763c7303160929b2ffa6de7ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:46:12 +0000

    agreement ride unable far runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
