6f5b6d6a18c55232459df55a3aa78887c5ea0839
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:30:59 +0000

    sound boat Rr foreign clue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
