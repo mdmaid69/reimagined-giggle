08c8f653846850774dcc53b55d628fd5eedf629c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:45:20 +0000

    honest kind emphasis possibly serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
