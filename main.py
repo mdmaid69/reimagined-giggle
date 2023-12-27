71557876076056e3d80f35e38172ed0e3a67e7a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:56:11 +0000

    at section disappointed manage takeaway

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
