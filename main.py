4fb4d71de13d0153e2588d924c5dcd85ffb30b3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:29:15 +0000

    protection post just nor influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
