c6b40916d4686f1b0091736d4f466c30fb5b81f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:38:18 +0000

    draw tap estimate calloff waste

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+print(math.pi)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
