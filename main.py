55a0eecaf32b60e5d4895fb51c3997019dc3db8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:18:42 +0000

    possibly practical upstairs smile outside

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import sys
-def print_python_version():
-        print(sys.version)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
