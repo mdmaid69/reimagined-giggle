0428c250df503e74b9a5a67be68d0772d79973a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:54:10 +0000

    no escape mine argue task

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def sort_list(lst):
-        return sorted(lst)
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
