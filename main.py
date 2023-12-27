bfe3fa285a3ac8ad55610916b72dfe755e2fcd0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:50:42 +0000

    key breakthrough patient thanks sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
