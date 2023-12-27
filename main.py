c66f1d4244ccf756bb49b8528ed99d23b77f2ffa
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:16:59 +0000

    politics opinion bridge cry nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+print(time.time())
