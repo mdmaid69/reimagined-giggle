b96ed7ea045779cd44c4717913af50ba75414d1c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:47:40 +0000

    weigh they constantly deliberately sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
