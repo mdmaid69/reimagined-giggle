4d2033a2ca7330f6591a9c78f5d4d2d0ed121c4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:22:56 +0000

    tidy rob either harm treatment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
