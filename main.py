c4d10697aece46de5bf8f153669c909b58a4c9e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:29:26 +0000

    base switch neat operation this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
