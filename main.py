23f731063f568dacdac2b83cf1c3152c31d33a4d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:32:45 +0000

    limit storm return distance while

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
