b034f58f7e627576abf18c66e571970d2a709304
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:52:27 +0000

    partner knock exist official comeover

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-print("Hello, world!")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def append_to_array(array, item):
+        array.append(item)
