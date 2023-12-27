77db3aef5b107efec23a4444662d91ca052220e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:04:16 +0000

    cheap point lock correct nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
