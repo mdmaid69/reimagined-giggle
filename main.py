c0ef708a791dafe591802f99d91a85188f6fc0c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:57:45 +0000

    involve Ee attention manage cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
