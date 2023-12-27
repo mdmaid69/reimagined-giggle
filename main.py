6a46ae91c53db6f936e89bbc834fb28318d2a85b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:16:26 +0000

    bottom smooth rain mad explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
