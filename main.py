20f750af389af8d776b52670968aad96e15f2684
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:11:25 +0000

    down promise represent steam intelligent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
