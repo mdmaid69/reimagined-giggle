6ed04d099b6cf3bd70ca1fe7f6979a6197fe2a16
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:05:07 +0000

    address immediate left feel look

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def count_elements(lst):
+        return len(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
