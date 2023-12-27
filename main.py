b9bf6982ebd6f3fb1c7775764f26cc1f0ef40a81
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:22:46 +0000

    future cruelty drop Hh space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
