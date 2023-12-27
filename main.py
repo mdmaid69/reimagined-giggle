6dcdef0ecee53377eea7dbd0ef752bb3abf284d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:43:31 +0000

    lean opposite generally priority Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
