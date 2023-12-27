89d16f2b9ca175120596fd714779f32da4163c27
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:02:00 +0000

    flood after dangerous alive beat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
