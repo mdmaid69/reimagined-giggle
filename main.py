f0981fb45c0cac71bbfa67d300deb8ac80cbf5aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:09:25 +0000

    neck demonstrate last race concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
