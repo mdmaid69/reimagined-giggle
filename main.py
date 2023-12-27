b10b74d77b01380b5410c0657acb53691dad73ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:59:14 +0000

    workout among tomorrow prize travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-for i in range(5):
-        print(i)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_set(array):
+        return set(array)
