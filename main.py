587943c6353510fb9ec4f6100965fb8be828e2cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:16:30 +0000

    curious stop middle swimming period

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import getpass
-def get_username():
-        return getpass.getuser()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
