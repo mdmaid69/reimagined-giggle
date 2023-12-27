e015c135ab0c7280def71690ea05d89e071e579d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:51:56 +0000

    179bite question shut ring sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
