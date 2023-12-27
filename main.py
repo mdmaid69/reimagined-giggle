9e38894c2e0e39f99796f34c81586f2a15454ee6
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:31:40 +0000

    each splash body reserve far

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
