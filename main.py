4f76125eeec5fb8520a5ba8e8c80a4da1a675439
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:33:27 +0000

    hide comeover by aim score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
