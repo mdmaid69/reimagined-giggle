0daa05bfb508ba49d1fe35e03ec1d442ac2113cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:57:56 +0000

    watch since favourite discussion narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
