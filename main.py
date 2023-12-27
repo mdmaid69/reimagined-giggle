cb9bae4a1f80f54bf9577aec292777bf581054dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:51:05 +0000

    below quickly scream always number

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
