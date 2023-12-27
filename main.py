8e988845cd2a02d68fc2e9336db0e5fea6229afb
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:54:13 +0000

    bringback suffer spring among shopping

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
