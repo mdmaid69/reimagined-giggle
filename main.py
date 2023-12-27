4f1f01b33f0b6107a0650fec148224a9d5937509
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:44:14 +0000

    bringdown invitation image minute instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
