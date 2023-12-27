f46b07b0ab16f9ac6e6fa14e2a7c43b7187fc863
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:10:03 +0000

    right murder emphasize knowledge bringdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import time
+def get_current_time():
+        return time.time()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
