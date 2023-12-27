b8deab017d463943f0cacfab6d1bd4c32f4f4db2
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:38:21 +0000

    from bring tap statement break

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
