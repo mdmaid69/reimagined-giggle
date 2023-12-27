890cb319a274fce86e1989cbf29cc01ee11ebdd9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:23:27 +0000

    case friend train level backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
