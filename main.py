ed0fdec5c8b582875c5c2d0d09c281c2b04cbddb
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:48:02 +0000

    trade needle Jj event hospital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
