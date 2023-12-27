93b9fb37daa65ecf56b3cb147b896511e7d3fe19
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:13:17 +0000

    waste mine Ll spot rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pressure(force, area):
-        return force / area
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import sys
+print(sys.version)
