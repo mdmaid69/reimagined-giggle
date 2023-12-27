718eb23b1f17364c54cf665e4890783eec4f276f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:38:19 +0000

    excellent sport damage clever today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
