ede647a86ee1061e16ee7c017f909f27252693bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:19:26 +0000

    grand strength himself view difference

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
