0c9251423c170f2ea6ea243d4e83463afac97027
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:04:07 +0000

    smell no connect can Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
