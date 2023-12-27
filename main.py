94bac9d9d19394410636248425e8d55a729b654e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:13:30 +0000

    closed spite entrance favour too

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
