b6939f7ca4969f794e33fe04acade1f9fbbc99bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:09:29 +0000

    raise state spread figure dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_exponential(x):
+        return math.exp(x)
