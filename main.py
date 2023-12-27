f808c5ba149cdfd154f1d3b29f5d60e1e1656684
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:27:16 +0000

    aim satisfactory without getback breathe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+def change_working_directory(path):
+        os.chdir(path)
