a4137ff6ef1051ddbfd37adc0908f8987dafa741
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:15:51 +0000

    rest telephone different behave eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def convert_to_octal(n):
-        return oct(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def add_numbers(x, y):
+        return x + y
