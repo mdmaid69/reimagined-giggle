fae291dc258ad781d13e63493207a1eaf4271cd2
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:59:28 +0000

    point broad produce crisis pleasant

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_acceleration(speed, time):
+        return speed / time
