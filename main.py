a1a96b8ece824ca9f0c7386209d7762b41cedbb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:11:21 +0000

    health treatment moon lose child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
