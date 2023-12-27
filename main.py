a29f640bd9ff78c5007b80346f707f9974bfd262
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:11:25 +0000

    afford necessary hard prove perhaps

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def clear_array(array):
+        array *= 0
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
