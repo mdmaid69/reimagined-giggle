4e2a163c3fd213018e14ece475ec234054633301
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:48:50 +0000

    whole land forever grade from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def convert_to_hex(n):
-        return hex(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+print(sum(range(10)))
