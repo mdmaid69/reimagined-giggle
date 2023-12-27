d715a1a4e6fcd3b543e96c83293173e01b9fe3c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:35:08 +0000

    read butter argument achievement persuade

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
