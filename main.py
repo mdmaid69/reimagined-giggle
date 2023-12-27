c237792419b756476e4c9686c8c7ec414ddd2ac2
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:35:51 +0000

    flat until situation read please

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-def reverse_list(lst):
-        return lst[::-1]
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
