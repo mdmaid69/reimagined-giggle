e694760280a4e6df79917cac09454ce05fc1796b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:30:52 +0000

    load indeed scale repair boring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
