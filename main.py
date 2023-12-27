53d0e30c0a5fc4893240293402f4337cc67bd44a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:52:34 +0000

    wise square however exact unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import re
+  def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
