e69af49fbceb715ab6bcdc32ba9a6c15773d5001
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:00:02 +0000

    same after record letter sport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import datetime
+print(datetime.datetime.now())
