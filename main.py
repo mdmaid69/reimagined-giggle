ca18788afb16dec2a760abf81ee761d803dbaf4d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:09:04 +0000

    survive trip forget run minority

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
