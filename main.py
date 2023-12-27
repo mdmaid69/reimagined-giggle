ecec068fabf48fb33a6566fa7961e33a00e1e4ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:18:19 +0000

    agreement able battery among offer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
