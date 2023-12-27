c2c0f99dade8cebec1f0514f9b595b457c751893
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:40:01 +0000

    mass mail division where firm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
