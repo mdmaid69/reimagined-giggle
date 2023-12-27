4c13b59d6e4474297c8331b34368e5ee0d771fea
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:41:47 +0000

    without official bury noisy sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_pressure(force, area):
+        return force / area
