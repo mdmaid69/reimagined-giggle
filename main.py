05b4c94e81509f9f1c1479ebdbb7c2bfe9967f99
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:13:40 +0000

    opponent few command creature advantage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
