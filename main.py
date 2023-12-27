a2b2443b1c2a0c1074116ccc1d3e8d4eaa97f3aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:29:48 +0000

    stupid tall behaviour on or

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
