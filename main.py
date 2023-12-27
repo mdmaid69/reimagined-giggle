e0e13111dcf2606def64f32d9db2d4250a921899
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:48:01 +0000

    branch embarrassing word often proof

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
