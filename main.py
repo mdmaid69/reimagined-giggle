3730803f3a24f821ef481ba4f725067fd63ab550
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:10:56 +0000

    pure couple sweep wave now

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
