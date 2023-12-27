b8b04f4bd99aa93c1fe49e3864016e65315032a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:13:33 +0000

    clear expect emotion towards surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
