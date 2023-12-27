21593cb536c6b5cf480d5d7c9b0f5dbed37fcdfc
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:45:22 +0000

    throw hang grade holdout week

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def calculate_area_rectangle(l, w):
+        return l * w
