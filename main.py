b808e3022e48e23fca6357e078391b785e637326
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:47:28 +0000

    aim hang colour any 564encourage

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
