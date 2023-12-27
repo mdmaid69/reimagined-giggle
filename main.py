ddb83595ae3bdda88013510d80ebd5860c64bb65
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:42:55 +0000

    bringdown Zz mouth course stamp

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
