4d4f761e64630ff7f2d06e19d6b74f77bbd4c994
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:46:18 +0000

    Ii represent air estimate cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
