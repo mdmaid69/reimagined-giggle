ef22adc78e696977eaa3310a4469c55900cca089
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:25:32 +0000

    past workout quarter discuss hurt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def count_elements(lst):
+        return len(lst)
+def subtract_numbers(x, y):
+        return x - y
