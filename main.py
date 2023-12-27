257bc5d6c30ecc545e349d4e1a1d2c05df2fd2c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:04:44 +0000

    spring hot majority can although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
