3ba979f947a4741186045171226f007ac7ace7fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:29:10 +0000

    call lonely official tie punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
