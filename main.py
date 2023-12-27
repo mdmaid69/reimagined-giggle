5ced3ca45945547dfd0e14ec9e46f5ccecd4b1e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:05:27 +0000

    transparent sport poor seem news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
