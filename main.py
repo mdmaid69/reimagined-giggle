b170e9337b7f8a74ba5117d08800fefdf8bf266a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:50:07 +0000

    stick journey yet go time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
