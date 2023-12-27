ec8bcfb003423d4213c64b5731c0001a25729420
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:59:55 +0000

    pretend push trouble generally group

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
