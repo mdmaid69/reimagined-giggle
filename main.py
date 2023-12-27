76bc0ef0b25c61adc031d3495619d9023dc27459
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:45:47 +0000

    rule set private workout emotion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
