dcbe21fadf6a39f057fe71fe61cdff4794e5cc93
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:59:40 +0000

    estimate lay drawing base study

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
