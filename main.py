6b8a703ba287ef6900621543f5fb71da7dcf0b58
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:51:16 +0000

    pinch ignore high buy famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import re
+print(re.match("h.*o", "hello world"))
