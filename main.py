7ea13f87f0ed23442165cec4615b23aeda3af3c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:29:09 +0000

    confused crowded congratulate careless violent

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
