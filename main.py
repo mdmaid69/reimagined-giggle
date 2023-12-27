126328ba6f512dc0cf8e1789e15305f1888e75f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:52:05 +0000

    uncomfortable fact can unite sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import time
-def get_current_time():
-        return time.ctime()
+def convert_to_octal(n):
+        return oct(n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
