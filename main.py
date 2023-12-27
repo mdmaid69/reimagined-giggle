70dba14b449a2a525f3db838f8a392331503afa2
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:32:03 +0000

    slight crowd exist male somehow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def reverse_string(s):
-        return s[::-1]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
