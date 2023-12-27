8ae3ffb8be5f28211fd9dfc2d0df1a9fe9fc0999
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:14:18 +0000

    measure near across square need

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
