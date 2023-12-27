0c5fd6c350e710ffaa6eb0e96b40d0f3e2499502
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:32:52 +0000

    train admit live correction stay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
