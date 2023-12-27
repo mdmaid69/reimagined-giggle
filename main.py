55f612987afa5d416e43d89f49b4c276f1b86f30
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:57:45 +0000

    explanation Itself wonder sit develop

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_set(array):
+        return set(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
