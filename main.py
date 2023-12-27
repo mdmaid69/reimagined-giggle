a9c73d94fc5054806e08a7d011a4d72c77f7f7f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:30:29 +0000

    paper steady send leave turnaway

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
