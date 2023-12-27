a2fd59ebca1ada2b8558e030a8e7646f25190eb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:41:45 +0000

    spill file perfect shade rescue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
