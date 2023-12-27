c2db4bdf7dd5da8efeaaf3fbfafc2b359dacc899
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:38:05 +0000

    basic smile goback(to) their Tt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
