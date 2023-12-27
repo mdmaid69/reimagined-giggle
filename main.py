b0ba9e4d6424e5dad32683991895e9326ac20348
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:31:36 +0000

    tooth question exercise clear art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
