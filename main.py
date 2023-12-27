213ed5e9336037e38952e4be0e5b30cc991a7ceb
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:42:38 +0000

    whether ashamed often exercise string

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
