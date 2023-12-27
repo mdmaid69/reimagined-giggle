e9e6508e78f0890869f16c2682b424fe1f825399
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:26:43 +0000

    calmly minimum but why goodbye

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def add_numbers(x, y):
-        return x + y
+def get_bytes_from_array(array):
+        return array.tobytes()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
