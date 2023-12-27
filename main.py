35cb8400430d0494ab05cb3e13c14062b82646b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:10:52 +0000

    strict ago advice its average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import datetime
+print(datetime.datetime.now())
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
