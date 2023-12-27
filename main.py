eb6167c3d3a59ccd3b89c5ac110c5842c37a6a51
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:22:37 +0000

    test think possibly spread run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def cube_number(x):
-        return x**3
+  def create_numpy_array(lst):
+        return np.array(lst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
