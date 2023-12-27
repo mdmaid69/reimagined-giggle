56777e17ea4b1bc3780750d07d7caadf7bf399cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:40:43 +0000

    because wash often rubbish laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
