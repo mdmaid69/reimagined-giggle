3d5f741f2cd1b978a49583f8f2e903ac52711b4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:07:41 +0000

    combination jump view brown train

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
