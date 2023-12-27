4dba102886f144dbd2fbfa70aec3467dffb92cc7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:58:20 +0000

    sentence beat clear now laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import logging
-def log_message(message):
-        logging.info(message)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
