ee1f2307c6be7581a7832a63f452fe21dc7b7d0e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:36:51 +0000

    he cry stop Dd definite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
