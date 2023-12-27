6687f694b2442425deb9bb548b376a099833f1c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:49:36 +0000

    minimum content between very exist

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
