fb006ad3a861b9e5e34c8ab978d29caec8a3df62
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:07:39 +0000

    Rr lock exactly frighten interesting

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def append_to_array(array, item):
+        array.append(item)
