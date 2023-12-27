f3171ec91576c0bb0c6621d5b77e86c7c82cd7c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:30:19 +0000

    funny also heavy emotion tired

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
