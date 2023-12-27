a1386d7ccffe27dbedd8b143e04e5ae64a9d98b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:29:41 +0000

    about object neat wherever opportunity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def square_number(x):
+        return x**2
