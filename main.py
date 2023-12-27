526e3e649e195776350070451d7e13bc436f32be
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:30:25 +0000

    turn special hers want wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
