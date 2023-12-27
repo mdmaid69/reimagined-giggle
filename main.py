55b6cccaf4c27864969f0569a211b7eab03b4bae
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:11:21 +0000

    guess steady race could remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
