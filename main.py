5ce89ff411cfafbb24ace8efebcca349c7ccbdcb
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:16:53 +0000

    ilovedicks ahead opponent determined plastic

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
