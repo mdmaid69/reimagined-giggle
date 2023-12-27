71ceb9a0c51a8c648a049ae0e5c03566a18a5dc3
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:23:15 +0000

    arrangement also principle commit must

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+print(sum(range(10)))
