505f48faf679a48c9be5a96239604419ecb77707
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:42:31 +0000

    wind us empty them most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
