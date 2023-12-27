a4e9d83f4b9bfa7d7d0b43c36b48356de7282a37
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:32:24 +0000

    wheel aim and today undo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_work(force, distance):
+        return force * distance
