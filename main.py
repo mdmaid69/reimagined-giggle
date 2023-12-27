4e44f09a0eddd9b5b50da32fc01cc97e3b61208f
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:27:25 +0000

    post murder date grand condition

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def is_even(n):
+        return n % 2 == 0
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
