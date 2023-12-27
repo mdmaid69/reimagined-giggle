febcfa891ddba1b6f04b334470161c6367374c8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:48:51 +0000

    squeeze comedown treat breakup practical

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
