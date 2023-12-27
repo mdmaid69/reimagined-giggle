b50ffdb68f3ac01b08497a7ad431be6068c3737e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:11:56 +0000

    lean reasonable immediate shallow over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_floor(x):
+        return math.floor(x)
