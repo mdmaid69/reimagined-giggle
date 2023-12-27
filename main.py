d9be4976f05f2e8eb6cf26df9ac5c48a0232c3e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:57:10 +0000

    cost store friend path really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
