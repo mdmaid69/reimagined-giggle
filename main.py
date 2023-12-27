48add85798f067ce549db36b9d578c3ea0ae4d74
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:57:25 +0000

    pull polite string line tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_error_function(x):
+        return math.erf(x)
