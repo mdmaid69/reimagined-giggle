80cb2c82f00f56841e92501fe98dc8ab91c2acc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:47:39 +0000

    sort minor agreement building plate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
