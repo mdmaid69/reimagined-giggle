f64b0d0e570f9e9d4b06fe60451bd67a3d099dde
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:15:53 +0000

    still port creature only store

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
