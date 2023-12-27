a9cb2ad0e525780ac333f8159680fb3989696bbc
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:15:52 +0000

    factory even deep tough mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
