7548ad1608795b4873b9598e9445d934ea989650
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:21:59 +0000

    cost sing slip pride answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
