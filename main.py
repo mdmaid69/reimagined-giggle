aae77a3717bf53b939684c57f21cd1d4b4ea2db9
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:13:53 +0000

    keen impressed moment Itself news

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
