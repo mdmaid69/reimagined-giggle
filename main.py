b6790af8b2f7108ee42e4dfc920e1963329e1b9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:23:53 +0000

    year sometimes somewhere look radio

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def reverse_list(lst):
+        return lst[::-1]
