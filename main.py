e0ea8e97024b26df6b844eead26c51538d0322b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:52:58 +0000

    terrible operation prefer rush fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
