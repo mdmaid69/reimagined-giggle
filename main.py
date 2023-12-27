8838b807fa1f6669ecfa3562060b617165d693ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:10:12 +0000

    fence hope buy callout wide

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
