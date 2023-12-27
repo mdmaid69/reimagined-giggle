82779cf7688ecf97c64f980388ca8b98375c8af1
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:44:00 +0000

    promise rule worried dig interesting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import platform
+def get_os_info():
+        return platform.uname()
