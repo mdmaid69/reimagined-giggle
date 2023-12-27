31fe5c4338115e27c3d27f4b95784c15b0d118fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:34:36 +0000

    line concentration class smooth hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def add_numbers(x, y):
+        return x + y
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
