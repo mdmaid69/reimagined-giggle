2077a5e563f1b032140ed10a6f7d53056734ba22
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:12:33 +0000

    side entry natural least appear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
