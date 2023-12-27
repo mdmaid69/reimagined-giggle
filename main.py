5ca3a3a81a3802396725d5d74347f90eadf435ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:08:05 +0000

    close otherwise start downstairs strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_even(n):
-        return n % 2 == 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
