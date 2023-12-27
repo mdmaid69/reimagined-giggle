d8c8e33d51d7dc0f3a8dc09263754e19df3cccdc
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:58:36 +0000

    visit smell getaway risk like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
