7c493921bcf6fcfdfb746a30d12828942ce9d3be
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:01:51 +0000

    grand meanwhile succeed cause angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
