09ae7e8e32b53022ae8a5fd19c367ff19f1fe1a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:27:43 +0000

    sad except somewhere minor play

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def get_time_since_epoch():
+        return time.time()
