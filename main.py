d16e5d1b913312569887e64a4f2591e6524aec11
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:16:28 +0000

    record down pipe cheat under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
