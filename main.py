73c4fda9277a42adbd73f4ee6317cb12081712d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:28:58 +0000

    runover order understand inquire suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
