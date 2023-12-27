f3e44cfaaadae2ed845b1527e82824fbf8cfe7cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:04:09 +0000

    sentence bill first back pattern

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def multiply_numbers(x, y):
+        return x * y
