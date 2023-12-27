23f874d8a21ab851bf2ee4f88741ae9918e1d26a
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:16:19 +0000

    deliberate music various match drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
