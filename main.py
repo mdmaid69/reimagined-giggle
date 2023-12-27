c0640b44eeab134c877bab7dfe6fed80e3c32f10
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:09:43 +0000

    gap fall crash scare exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
