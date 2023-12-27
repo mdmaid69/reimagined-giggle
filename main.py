9050dd7966f512a3b18e037582ad7c44d9f409a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:59:42 +0000

    respect subject apologizeapologise go draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
