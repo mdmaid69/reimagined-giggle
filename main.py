531d3a52543feb77f484a7d2115cca4c7f56a2ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:20:37 +0000

    drawing surprising lost cover already

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
