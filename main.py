fd0163ad4079456e2555146453b344db0ee72c01
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:17:54 +0000

    deaf past passage crowded minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
