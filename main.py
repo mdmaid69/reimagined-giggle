2dabd0d45930804be71b2b0ab6893844ff676f54
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:55:32 +0000

    rather light trap way further

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 def calculate_force(mass, acceleration):
         return mass * acceleration
-def find_unique_words(sentence):
-        return set(sentence.split())
