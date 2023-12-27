a1552a643ebdeb742e1ec3550fcb244198ce9d14
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:26:00 +0000

    spare hope scheme guilty bringdown

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
