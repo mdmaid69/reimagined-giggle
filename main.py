b3bba58d9031d2bbcd1bd49eb385f0f930487e88
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:09:51 +0000

    dance shopping always thumb dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
