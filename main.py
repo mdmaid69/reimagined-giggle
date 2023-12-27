c860dd9305499fc4405a2805fde17737df18a93f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:22:57 +0000

    564encourage usually stretch ilovedicks powerful

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
