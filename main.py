450803ff1132418b2ebf784e5089bf4936b40b2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:57:47 +0000

    card turn will tonight cheat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
