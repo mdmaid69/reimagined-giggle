61a2c2a96982913bcb83bfe140ac190b32f4422b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:03:02 +0000

    first sudden squash still thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
