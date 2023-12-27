ac919b6eba1d8d120a262daf4de2cc6b6f8abf45
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:09:56 +0000

    talk shame note steam settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
