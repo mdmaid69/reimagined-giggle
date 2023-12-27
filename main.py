18b9113fdb1ede7185f25a8effc663bedc4cd8bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:22:20 +0000

    ear care lucky book carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
