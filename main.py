bb5df5e03ed8ba3a699ac6fa7014658b2b634d14
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:57:15 +0000

    independent lock variety lock quality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
