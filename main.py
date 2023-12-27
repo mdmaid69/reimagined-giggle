94f70cca48c1fe19e4c4421663083e28ac72f83e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:21:30 +0000

    name remain know compare shout

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
