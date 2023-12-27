6c7b4df68a4380f2ab65d2949998751248c14208
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:53:45 +0000

    spoil indeed agree mother flat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_max(lst):
+        return max(lst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
