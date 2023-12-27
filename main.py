a64bfe2c8a8b2da8d30319f354139fb6b42cfa81
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:53:56 +0000

    breathe Ii artificial nature present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
