6da438277df804f543db59fbfdf63e05340eeb02
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:03:24 +0000

    rain count doubt mess unable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
