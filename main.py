295fd9148efc94ad77fcf1bf5154247cd25da35f
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:37:03 +0000

    gradually cheat person serious definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_acceleration(speed, time):
+        return speed / time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
