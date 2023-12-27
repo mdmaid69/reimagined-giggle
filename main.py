5b488473d6c102b840a665877815839abf9b4b98
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:12:08 +0000

    through plenty wind holdon partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
