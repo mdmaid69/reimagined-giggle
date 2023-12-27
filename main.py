4789a08bac9c8255e82ff1c7d1e9944c2c26e035
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:31:27 +0000

    could occasion find mixture tidy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
