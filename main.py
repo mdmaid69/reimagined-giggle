23c01899a56a4da333ed27c8591b834bac43b626
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:24:26 +0000

    contain black sister dish rain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
