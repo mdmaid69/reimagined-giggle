035b6bd7697f33ed9811f71df72ffa895bd90a16
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:00:40 +0000

    getback plan 567enemy hit plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import sys
+def exit_program():
+        sys.exit()
