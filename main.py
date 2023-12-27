ce23a03bda68de0361e7d86784c6d76f933c22bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:03:24 +0000

    him unite ring bunch mix

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import array
-def get_array_length(array):
-        return len(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
