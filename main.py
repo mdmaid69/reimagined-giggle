036027ebf40a6683935948552a34efa18c6497c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:43:45 +0000

    old usually keepfrom mean break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
