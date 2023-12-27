2ba6655812b6c965e9ed8b103c307a268d284f3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:09:11 +0000

    boat plenty remember correct condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
