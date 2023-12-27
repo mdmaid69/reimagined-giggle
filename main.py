ad40837364819a7e845b8705b48c9f1b0334e22a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:02:58 +0000

    involve hate makeinto row moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import datetime
+def get_current_date():
+        return datetime.date.today()
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_int(array):
+        return int(array[0])
