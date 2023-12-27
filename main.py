8b46c137354a1fab4217c35029bd86525ccee66d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:29:47 +0000

    somewhere much act touch content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
