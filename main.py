afab8faf9cd53b323f87d3acf741b8fe855cfe91
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:56:38 +0000

    forward favourite dig cum extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
