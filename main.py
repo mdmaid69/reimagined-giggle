fc47f0230a0e4ede859f87f3f89d988c54080268
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:07:03 +0000

    shrink yourself really 572enter check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
