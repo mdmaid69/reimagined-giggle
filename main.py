bd139fa23ab06ce7387724334ca5ffea8b896608
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:12:32 +0000

    official programme thick interview repair

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
