b1d953315bc23277f86c9dd20632c6604a7108a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:59:48 +0000

    connect fresh journey confuse extraordinary

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def reverse_list(lst):
+        return lst[::-1]
