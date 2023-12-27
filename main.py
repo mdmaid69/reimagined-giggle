1d05e824787b13bfba3f740faf60fd09009adb1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:03:22 +0000

    divide indeed least prefer standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
