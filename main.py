cfee869ff8df2dcf93fe6820b22ed60795eedc8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:38:08 +0000

    everyone no block today support

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
