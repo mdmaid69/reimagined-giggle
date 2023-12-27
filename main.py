5d4e717033d5e9d06f8541788c5d4fb3a1c67e34
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:55:58 +0000

    bus generous spread almost direct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
