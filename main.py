3c3edb1b55b903017f7e7730606841ff5978472a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:32:21 +0000

    goodbye wild dance lock case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
