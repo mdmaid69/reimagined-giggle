192797ad6b63e653ff3ed7368f27ace7497b2b07
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:07:37 +0000

    fond entry get that possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
