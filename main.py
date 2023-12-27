77d0ed502225bd98d1ea9a4185bcd82d863d908d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:22:35 +0000

    seriously male such service attractive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def cube_number(x):
-        return x**3
+def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
