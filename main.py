1b720bb6dd82927f20bb142e4f2f606cdd7d2e95
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:28:28 +0000

    hope money upwards bright left

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_string_from_array(array):
+        return array.tobytes()
