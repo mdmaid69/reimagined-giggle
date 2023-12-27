2f9ca5256a0cf488a67c4ab5321da816d90d2acb
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:13:15 +0000

    dry way false sign cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
