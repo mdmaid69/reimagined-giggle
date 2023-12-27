51eef0f25de79879cb611ee8f5c504e39ab37b82
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:51:48 +0000

    unless force ear content attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def calculate_variance(arr):
+        return np.var(arr)
