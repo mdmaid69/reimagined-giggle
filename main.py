3dd9fa93ba7d5eb6abfa5431a865dcedaf6eacad
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:16:33 +0000

    follow natural regional sudden reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
