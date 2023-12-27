d16df2c10c9880958262b09ad2adbf0b04f10b2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:31:53 +0000

    fold right see bottom violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
