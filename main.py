cd7fb2fa63fccf009834f4848f5a3a570520ec46
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:19:13 +0000

    before turnout wide chin always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
