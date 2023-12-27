106af7c93165b6c109f5906e6b3f4f2d344cf52b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:16:17 +0000

    empty most tight near spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_length(array):
+        return len(array)
