abf9043531ab818457904df5e894a338ae270722
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:30:34 +0000

    miss shout survive intend try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
