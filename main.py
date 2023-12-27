bcf458a3a2b23c217b479294f9988b0ede224afc
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:59:50 +0000

    trial or aware easy plan

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
