e9f06a9e88bdb52efef66d009216e464fd919848
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:29:34 +0000

    murder geton welcome over practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
