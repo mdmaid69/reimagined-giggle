4d7f1f155e32ec942c984f003aae2c66e74d879a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:47:42 +0000

    breakinto gas ahead respect walk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_length(array):
+        return len(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
