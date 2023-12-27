de3b114f627bfd010e75140b3a988b8b51eb08b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:56:49 +0000

    hard yourself wing mark town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
