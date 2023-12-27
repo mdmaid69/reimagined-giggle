37e03dd3e1f823725cb1ee22dddb2779a2b80e6a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:49:04 +0000

    house related contrast content fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
