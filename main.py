1a221b12db97b81cd1b1fe316a200700e8178d26
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:40:05 +0000

    correct home back getback wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.time()
