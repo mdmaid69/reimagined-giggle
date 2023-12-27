5673ced958d575be49257d6e273e10113013a4ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:50:09 +0000

    direction fall usual comedown couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
