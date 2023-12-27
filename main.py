acc0f02d03e868034f4400f31d3678716823bb79
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:43:24 +0000

    goaway injury direct cut exit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_repr(array):
+        return repr(array)
