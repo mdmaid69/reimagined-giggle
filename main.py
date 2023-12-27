2eb0bc9393c8d86e9250142e48547c2ed3d86299
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:27:08 +0000

    rescue clue attach pleasure form

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_palindrome(s):
+        return s == s[::-1]
