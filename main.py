90dd21dc651de3d7d7e5d040e8fb6ef9ac2b6bb5
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:22:35 +0000

    answer buy getout suppose treatment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
