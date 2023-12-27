c6a050759ddcf740d28d5faa810fbdbfa052320d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:22:37 +0000

    temperature nigga sport fetch possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
