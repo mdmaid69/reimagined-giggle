ecad58ddbf139d1a847928a41acaf89bf110da16
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:35:33 +0000

    process discuss remove tradition slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
