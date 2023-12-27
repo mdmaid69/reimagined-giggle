39d3b4503d0adc155d5f53fc52ad47159f60110b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:46:50 +0000

    store taste possession chat Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
