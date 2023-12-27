15d306d2980c67fd35d6dffa3e5a80a9d0d8afb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:02:22 +0000

    be help bang apply male

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
