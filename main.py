b5fbb7ff2f467aae2d6ec7946f8f7d540743e928
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:56:58 +0000

    pay for make closely start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
