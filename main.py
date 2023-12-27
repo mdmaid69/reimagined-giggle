cf2e3ac6bfaa0adf61eed07c9b8aaa53e1bf4b32
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:20:35 +0000

    during drive careless any listen

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-for i in range(10): print(i)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
