84e263f8ca0dca95592b633bb077289742fe5ca5
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:17:09 +0000

    minimum foreign breakup old dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
