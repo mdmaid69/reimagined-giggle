e66f2d8a9145e8a2941cf12deca7f30546a21e36
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:25:57 +0000

    Gg unlucky Ww address whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def convert_to_octal(n):
+        return oct(n)
+import collections
+def create_queue():
+        return collections.deque()
