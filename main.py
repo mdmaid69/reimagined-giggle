5fc8bb3897da6769889566b87e90267eb90aeecc
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:31:32 +0000

    maybe side contrast brother whenever

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
