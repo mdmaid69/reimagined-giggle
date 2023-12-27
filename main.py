864fbd43e433dcb817b6aa726875814d8550cfbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:14:20 +0000

    lock same second place cough

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
