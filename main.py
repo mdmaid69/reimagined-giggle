913b371466d9646d9e7ea60d55d95187ab2f45c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:25:44 +0000

    wait cheat sweet area embarrassing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
