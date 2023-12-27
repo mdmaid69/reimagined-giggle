df9fc2dce53fbae4aa10287c0924395ff22e075d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:52:30 +0000

    really side terrible keepup protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def split_path(path):
+        return os.path.split(path)
