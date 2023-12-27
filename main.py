568cb726bb6c6875c0061016aef5f82af9684a1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:48:34 +0000

    factory underneath before claim switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
