c288b9a13781fde83cfc17a2a95910fb6fa0116e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:07:50 +0000

    early realize dangerous of tie

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def convert_to_octal(n):
-        return oct(n)
-def subtract_numbers(x, y):
-        return x - y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
