da35c6d1f004239659adb32a0b6ed9b7319da0de
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:50:25 +0000

    search judge Ii post criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
