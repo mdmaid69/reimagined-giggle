6bc67f69ca3e768067888ff792216ae994fa5018
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:01:53 +0000

    waste good study strike then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
 import array
-def reverse_array(array):
-        array.reverse()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
