5b741b7d74aaf445759e02c38c53123fe9f899ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:09:27 +0000

    finger contrast feel dare must

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
