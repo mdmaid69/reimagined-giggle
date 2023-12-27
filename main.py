5637d16bb88cd2b63aad0cf16358963aa53d0449
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:29:01 +0000

    recommend figure right defend wake

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
