7835cdb6348b3d896919d3e684ef12f1a1745a64
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:11:34 +0000

    down approval hurt able honest

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
