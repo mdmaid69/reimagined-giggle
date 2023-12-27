1c567d58da6b309856bc95c03bb68f7bb8838c8f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:56:00 +0000

    unkind pupil wild profit ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
