d7605dfee46bd07c2d5200b480681d23c9db97d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:32:26 +0000

    show offence murder win dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
