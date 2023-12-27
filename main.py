4a09f99fdfd6bcf6f1894d6a6c50d5e64ed3da47
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:19:32 +0000

    Kk confident himself chase air

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
