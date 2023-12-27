e1189b961b04dd38812d20e4ec4939050d0337a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:01:42 +0000

    strike switch valuable of payment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
