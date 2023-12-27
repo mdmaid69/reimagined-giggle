546562acb9fa64e41b89da0010255c2656b9e01b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:25:46 +0000

    justice second afternoon church complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_current_time():
-        return time.time()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_int(array):
+        return int(array[0])
