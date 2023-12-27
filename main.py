f6354161f4c17dfaf76ebfd7eaffb833f73fde11
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:48:58 +0000

    substance limit hour demonstrate impressive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
