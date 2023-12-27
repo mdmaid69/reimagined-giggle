7b3b3c308afdaef8c22cfa62c673efbc3c4be732
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:46:05 +0000

    cost sick ours along runout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
