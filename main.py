21c1a2222f5505fafdfc9c8a6e5d6ed865c97a2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:26:54 +0000

    over bus common power level

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
