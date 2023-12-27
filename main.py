c80dfe73a3f0bcd1ee5da3f2c5249461a32b7a2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:07:16 +0000

    price unable nice challenge stay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
