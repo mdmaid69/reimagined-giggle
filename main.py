e1be6a9fef587d2282641e0fbe3c29e8cc101d56
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:41:01 +0000

    flood respect bit eye well

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def convert_to_hex(n):
-        return hex(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
