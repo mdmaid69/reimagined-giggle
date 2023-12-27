99e6d9b12b141b65ddf334370fbe34b3bc7b6135
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:49:06 +0000

    material contribute tear schedule post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
