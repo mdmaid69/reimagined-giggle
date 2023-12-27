d0dc66b48ebda5131f0d660d335fcd5cfdb51adc
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:20:02 +0000

    warn appeal off complaint mistake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print("Hello, world!")
