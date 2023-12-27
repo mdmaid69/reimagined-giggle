a339e00b6dcf2ed343257e99658a93847e5b3c8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:42:40 +0000

    traffic content combine demand amount

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def reverse_array(array):
+        array.reverse()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
