a82d4afb69e3b62d3a198300531d169213b2ef9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:32 +0000

    speech guide backwards progress as

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def add_numbers(x, y):
+        return x + y
