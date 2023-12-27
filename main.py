66e8c49993b598a33efbac8fc6984009cb95f921
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:10:43 +0000

    quiet practice perhaps dull Zz

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
