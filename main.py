774008069eb251ed76e6da31f74f85032de7669d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:21:02 +0000

    position comefrom information room related

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def find_min(lst):
+        return min(lst)
