e14c0e06be3c51641f58cf2650681cfab4e71c5a
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:36:13 +0000

    pleasant pretty shadow alone run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_work(force, distance):
-        return force * distance
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
