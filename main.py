87cb12d2e2074dbe6456427b170c4c65e9acdc59
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:38:12 +0000

    wear earn storm will break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_bool(array):
+        return bool(array)
