daaa35c7927fe1ef38e0caee8989b5302cd8955e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:46:54 +0000

    break journey earth pool listenfor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def find_unique_words(sentence):
+        return set(sentence.split())
