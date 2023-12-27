491c6f956eb73309ecb9953e04ac25fff0be3255
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:36:17 +0000

    cope taste ordinary knowledge beginning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
