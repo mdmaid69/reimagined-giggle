ae7052110727f9d44ea036a7189c64135486f5c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:23:38 +0000

    squeeze space for take cool

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
+print([x**2 for x in range(10)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
