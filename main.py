4fbaa0bcfd2fe93d9e81b6138e5f3513af5e5203
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:22:57 +0000

    stranger shelter anywhere familiar age

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
