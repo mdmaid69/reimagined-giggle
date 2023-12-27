ac5b5206063c42f4eb047ec19aab2ed6cfdf4a3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:29:25 +0000

    lead instrument belief commit amount

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import sklearn.datasets
 print(sklearn.datasets.load_iris())
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
