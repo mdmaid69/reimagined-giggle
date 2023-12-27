cbaf2594b5b25db0aa3cf28662c031fac333005b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:53:06 +0000

    problem pin Zz clear so

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_area_rectangle(l, w):
-        return l * w
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
