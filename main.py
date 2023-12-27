18eec6ecebc1c16eeba1324dde7cd2ea07513ca7
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:01:36 +0000

    bank that shallow light shoulder

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
