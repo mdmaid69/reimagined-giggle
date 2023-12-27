b0253fd07144e1fee9f962b01c0921ccf06dc14f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:32:26 +0000

    can close send somebody behaviour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_density(mass, volume):
+        return mass / volume
