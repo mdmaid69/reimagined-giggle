d1c02d24ddf6349d68872b8a24c26dddeb50dec0
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:12:02 +0000

    question common relative thirsty bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
