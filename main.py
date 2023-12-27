1dfafd43a24e0baacf7be9ee75c5c05fae41f5d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:44:39 +0000

    threaten suit total distance whether

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
