71b2f12ca8fa2180b2a0271ab8f7f124b7e27e7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:31:06 +0000

    usually breakup Hh read pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
