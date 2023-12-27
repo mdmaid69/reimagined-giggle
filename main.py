9610819769cde5be09cea5b73046b1171bf14a6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:37:06 +0000

    person talk flat mix ago

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
