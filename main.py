a5c06744b84be0dcfd45b08a86f0e562d7ed7474
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:35:02 +0000

    get pen free find contact

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
