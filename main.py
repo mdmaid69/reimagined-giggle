f95bae992ed7acaac897b9efba977f02b45445be
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:19:32 +0000

    use recently turn grow flat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
