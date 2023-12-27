a336d0582a4f8c93cb328a3c9c35f311a0c29039
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:06:12 +0000

    community shape reply battery care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
