c5debfcfb35d42092c533a9cc9c0532f3e71a95e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:26:35 +0000

    government rent tune light pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def split_path(path):
+        return os.path.split(path)
