82f472670c7e2b4c2400e46e2b24bb6c6cb2f005
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:24:27 +0000

    stupid among getoff breakinto hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
