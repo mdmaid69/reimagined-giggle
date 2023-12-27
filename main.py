1d391006fa2e388ed015a3d8b3c7ef77e2bf5ae7
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:14:03 +0000

    everything example miss world talk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def sort_list(lst):
+        return sorted(lst)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
