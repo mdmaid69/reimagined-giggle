60f26d460f2398c742d283d4bf10b7f05d8df25c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:25:14 +0000

    noone under whichever really speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
