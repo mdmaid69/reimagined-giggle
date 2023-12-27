aea5f528eefee85c94070e0bb5a35342469679ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:38:51 +0000

    tall high match forever everybody

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def add_numbers(a, b):
+        return a + b
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
