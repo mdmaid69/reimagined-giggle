2e726446bcade40bf4ad3b47ca53000341211edc
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:24:20 +0000

    breath discover silent exhibition seriously

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
