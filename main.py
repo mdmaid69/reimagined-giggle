a41f345101a45c31477c4efccaa7c1542e26cdbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:59:53 +0000

    want joke stamp excuse majority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
