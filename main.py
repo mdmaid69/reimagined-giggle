4a163f3d3f6eebfcd2760afd30dd69c71cf7ada4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:06:26 +0000

    distant coat position party apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
