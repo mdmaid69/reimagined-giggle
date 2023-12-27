9027d6b6b34782734c49071fd0f30d83cb65542f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:40:22 +0000

    tie Qq mark wash mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
