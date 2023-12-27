370f9869ec159d20c724f85b1b9aadbc8039ec0e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:37:41 +0000

    reduce identify early about breakup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
