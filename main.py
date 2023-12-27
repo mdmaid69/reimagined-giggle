edceb7525393f20d4cd6eb1236561229f46cd790
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:35:24 +0000

    mainly change rich name share

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
