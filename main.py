1f40ecc3158e817ce7a56be084d1c7f5457bd429
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:49:46 +0000

    reaction finger range use defeat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Cube numbers:", [x**3 for x in range(n)])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
