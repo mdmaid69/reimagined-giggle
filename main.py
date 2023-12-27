43208dbfe3c2e17eb3fffa4b8eb1b49276b17762
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:07:34 +0000

    catch base godown sensible path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
