7a613a81aca982179f88720a70d81e7f22c98f46
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:36:54 +0000

    improvement switch ourselves obtain sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
