02a0a62878ede881ea5d62f5e022b07989b6b72b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:55:07 +0000

    profit divide base process worth

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
