6a00e8b41e368101e016bd39f77befba69551d83
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:28:43 +0000

    dangerous even see pretty cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
