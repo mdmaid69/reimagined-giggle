c03c8a908278032d99f909ca15da4305f7282a22
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:25:39 +0000

    distance opposite manner mainly hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
