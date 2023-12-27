e2854f7656d88ef5b4c37351a4c57be0426a6b9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:22:43 +0000

    he now bear busy ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import datetime
+def get_today_date():
+        return datetime.date.today()
