6160f4ed493edaa41358c3ca2df337e6f8c93919
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:22:07 +0000

    above direct light stretch either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
