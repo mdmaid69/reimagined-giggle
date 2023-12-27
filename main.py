b2b33c17c07117c82128b0001bc03256f86012f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:33:03 +0000

    bear divide last excited combination

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import os
+def remove_directory(path):
+        os.rmdir(path)
