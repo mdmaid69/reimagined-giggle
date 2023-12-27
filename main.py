b000e673c6662011e04a0d5279aaaf925618a6ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:59:29 +0000

    brown miss attach accept firm

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 def list_files_in_directory(path):
         return os.listdir(path)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
