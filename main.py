4b41be1175a15a2079968144ffbd7368ae6d32e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:38:00 +0000

    company first weekend extra runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
