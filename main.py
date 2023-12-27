596a779b2a7a9923dabea657e9e03c0914aa6db0
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:07:49 +0000

    disagree colour rent fit until

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
