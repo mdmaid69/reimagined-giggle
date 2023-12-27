1a9af6d0def0b623b532e4bf631c89c47252f722
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:59:09 +0000

    priority thirsty breath free who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
