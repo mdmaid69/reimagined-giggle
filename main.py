510698ba8437d4bdefc077d06a59d93f6cecb282
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:54:56 +0000

    owe Bb pencil advise float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
