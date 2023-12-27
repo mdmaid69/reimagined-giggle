134ab58535447a42f907926564f6bf63c0092a43
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:03:59 +0000

    practise cook choose bridge congratulations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
