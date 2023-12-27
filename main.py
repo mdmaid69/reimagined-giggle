92fd91bebeedbc1149c7bacd7343dc469698aef2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:31:38 +0000

    goup cheque low grand Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_user_string():
+        return collections.UserString()
