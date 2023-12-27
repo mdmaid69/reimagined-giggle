37a007470aff3fe85c5bb77bad3ab1d41cc8b61f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:05:17 +0000

    easy need behind leg wind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+x = 10
+y = 20
+print("Sum:", x + y)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
