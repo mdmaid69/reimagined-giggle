9067dfa4f7007523e017fee0a0b7286e6fdc3823
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:14:45 +0000

    bright swing fall kind disapprove

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
