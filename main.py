82470adf488b141391605241f9cacfda7be853fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:55:23 +0000

    question bury forward argue debt

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-for i in range(5):
-        print(i)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
