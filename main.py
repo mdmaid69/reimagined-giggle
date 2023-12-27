27122537830542daa4d11dacd44613ca0e7e54c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:58:55 +0000

    paper thanks pot possible take

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def get_array_itemsize(array):
+        return array.itemsize
