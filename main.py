e71a2c2a6a2a32cc54127caade3614ff5e41d80a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:13:11 +0000

    opposite peace practise loud or

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
