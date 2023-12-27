77c96c7cf65b2324fe091a9ea36eb6ae03227cb9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:55:11 +0000

    under trick niggaacademy who way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
