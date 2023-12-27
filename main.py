a995723e3a0be2536abd7ff0c2df369780810d88
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:24:05 +0000

    describe new left material allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
