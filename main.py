4c911f3f2f4aaa6399ec0186ea493de76f706b52
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:19:07 +0000

    this obtain yours question to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
