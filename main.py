b18a0fbeb094e57862186eed579290b553c86dd4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:30:43 +0000

    see callat otherwise by use

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
