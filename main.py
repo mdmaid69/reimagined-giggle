59a942106fece0cd503e7399e7d8101729261856
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:20:13 +0000

    show mysterious taste pool fail

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import time
-print(time.time())
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
