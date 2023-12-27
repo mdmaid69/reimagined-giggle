4667809a7b124b561327b22e976c37fa793b164a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:12:49 +0000

    from write intend flow division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
