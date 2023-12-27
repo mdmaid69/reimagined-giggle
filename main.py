c0989caae58854af1a8da92220432ac68a94f89b
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:28:07 +0000

    pool approve swimming peace offer

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
