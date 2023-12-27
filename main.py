c43427117d29b9242570ba65a220fd692cb24e1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:30:39 +0000

    keepfrom throughout whistle model letter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
