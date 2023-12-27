6efecb1937e24d7b96d7fc8db19c752b910ae045
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:26:06 +0000

    suppose taste pencil quantity mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
