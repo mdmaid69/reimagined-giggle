5c32ac64d7b0e26b832a7ec43811f78feb1c7efc
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:03:00 +0000

    later purpose birthday write reject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
