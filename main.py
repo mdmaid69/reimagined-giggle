36d7dfa35832cc33bf68e350306a4d052fabfadc
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:54:53 +0000

    still plenty reach mystery dry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_hex(n):
+        return hex(n)
