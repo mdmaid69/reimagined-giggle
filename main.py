56db2eec02bc609f6fca85ca0635161a3089a11f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:02:48 +0000

    love natural confuse connected lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def convert_to_binary(n):
-        return bin(n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def is_palindrome(s):
+        return s == s[::-1]
