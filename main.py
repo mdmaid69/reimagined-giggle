67d6573f47fa02390c07f7a13a09cb4102c83132
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:04:02 +0000

    dig recent world back mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_max(lst):
-        return max(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
