e3cbd19d08a251504bed06a19c3ab62239df25af
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:30:53 +0000

    comfort be angry prize step

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def convert_to_json(data):
+        return json.dumps(data)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
