6c425f65afe1aa42d32abbf0e477d6c4a3668f8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:26:56 +0000

    lunch guess callback speed perhaps

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def calculate_area_circle(r):
+        return 3.14 * r**2
