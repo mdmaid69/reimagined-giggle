03285b1a595d3c248d90b69d091fed31cb325ebf
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:17:03 +0000

    pour club write will takeover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
