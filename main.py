f6c09cf8a364a886f1719500b5586b8550156952
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:30:48 +0000

    my exchange row opposite street

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
