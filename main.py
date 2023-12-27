d7399597ee86bdc215ae4aeebe3b3c6384aa79c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:26:28 +0000

    evidence him female win stiff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
