82de15a2523d3a65b1234c1bfd250383cc99df11
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:00:10 +0000

    tall simple deep pleasure area

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_simple_interest(principal, rate, time):
         return principal * rate * time
-def is_palindrome(s):
-        return s == s[::-1]
+import time
+def get_time_since_epoch():
+        return time.time()
