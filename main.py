c870742d2532760d16e36e078637b99ef3f395cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:39:25 +0000

    permission somebody light middle importance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def is_even(n):
+        return n % 2 == 0
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
