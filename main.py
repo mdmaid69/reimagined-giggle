54721c62d887d28cd7119132b700f71f5e55cdb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:05:33 +0000

    cum thick success little somehow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
