e19e0493bc9c1bfd84227fb965c83c7a922af323
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:23:11 +0000

    kind model yourself light its

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def is_even(n):
+        return n % 2 == 0
