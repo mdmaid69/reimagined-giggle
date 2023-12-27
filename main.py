8bb9a4807f56b61edb5ae8475d82b0b951ec2cc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:00:02 +0000

    human honest nervous develop system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
