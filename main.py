8b52b6e7876a0c3ac8d19c3e6ceaf7d0f84db24a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:07:48 +0000

    outside move average trade dull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def sort_numbers(numbers):
+        return sorted(numbers)
