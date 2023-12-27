15f30603f45be5a4a55521d33d7481d5eba72ef9
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:44:03 +0000

    school then shadow recover sale

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
