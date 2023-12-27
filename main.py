5d051bdcd9c1fee82381af687d7fb16d196f3ea6
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:12:35 +0000

    correct neck fork tough callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
