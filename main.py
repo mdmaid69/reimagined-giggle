6c044137b703f10ac3fe748b556cfdfe44ae527e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:30 +0000

    very possession responsibility finally coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def convert_to_octal(n):
+        return oct(n)
