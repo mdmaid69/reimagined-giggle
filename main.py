3553cf52011678d3ab338a58bd2edcf6ba6f827f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:22:37 +0000

    accept hear fight tight limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
