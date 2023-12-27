5dc5004bc3fd4c13083fdfc3ad60fe2955413255
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:38:45 +0000

    rise work report simple fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
