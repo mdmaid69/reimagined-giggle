3b8ba88d75268ced1b462b35387b218952dcce21
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:50:56 +0000

    matter undo theirs guilty enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
