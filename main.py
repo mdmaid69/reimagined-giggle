86851e255e142588fffc89cdb782bd6bd47f5a1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:07:01 +0000

    man wash stay defend basic

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
