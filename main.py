374bbb1f3b8d851904dbb2f1160136bc5ab5baa9
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:23:11 +0000

    stranger tend open do exactly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
