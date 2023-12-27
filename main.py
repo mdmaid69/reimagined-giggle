679ff5ae39f274f51fc0c8c9bb9d07dade0aea1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:08:33 +0000

    ago tonight score ball Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
