f0dfc9357da9916e100af1fd52dad8e511176cbe
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:52:51 +0000

    scrap object sink effect about

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
