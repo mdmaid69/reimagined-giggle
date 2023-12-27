66c9bc53b7516fb890888f5c26dfbd58a96e586c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:25:57 +0000

    steam each understand sensitive could

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_power(work, time):
+        return work / time
