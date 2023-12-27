3515ec8a4baaed9aeda6179c05f1332e286f4355
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:12:31 +0000

    shall urgent walk its left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def convert_to_octal(n):
-        return oct(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
