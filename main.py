da6f6a4d9b74c8f7c216324f5530f803c5ddf21c
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:11:42 +0000

    turndown conversation play narrow perfect

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def count_elements(lst):
+        return len(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
