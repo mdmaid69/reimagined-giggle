f5b9619e171e574d367c6d39ec4e54843064f0b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:52:25 +0000

    clothes save consider Ll young

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
