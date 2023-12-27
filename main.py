918d8b97af46b9f6561c8d18e12e05c5b4c8f15d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:06:47 +0000

    relax suggestion tend properly backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
