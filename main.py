ff06a07c1c959dd7299cd5b02eecb72ceb7509fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:52:28 +0000

    shout target report regard petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
