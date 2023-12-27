6945e7ff1d78d3fdd3798383f8337859ab8e53c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:55:51 +0000

    subject sit mess trial rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
