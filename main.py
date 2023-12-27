5f11324c749239f4cc1cf98abb6d4698020d3344
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:18:17 +0000

    meeting voice interview second wear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
