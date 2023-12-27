a2ee0b5c390d1f81be9e83a8f6faf1db6c46bd4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:48:15 +0000

    phone broad fetch autumn coffee

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_pressure(force, area):
+        return force / area
