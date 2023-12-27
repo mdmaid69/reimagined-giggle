42ade1a96fc11d32d250e906727fed987a924f19
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:58:30 +0000

    ability vanish emergency ignore point

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
