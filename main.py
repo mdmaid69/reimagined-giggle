b99ca7d956ce3b6276f07e49c90fda91fe94597a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:24:23 +0000

    suppose authority full weekend goon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def count_words(sentence):
+        return len(sentence.split())
