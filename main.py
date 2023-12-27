70018ca9f717d058f34a3602d60350e060561063
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:48:01 +0000

    exchange beside coat table pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
