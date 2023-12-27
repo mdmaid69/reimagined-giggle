ffb8deb01b54dfe5cf0cf2347a1eae4df2379a83
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:30:21 +0000

    popular score please front from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
