420e97a8eb434cfdee2f0fa13acced4f8877bf54
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:53:58 +0000

    round report disagree into excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
