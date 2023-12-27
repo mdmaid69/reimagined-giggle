0be87f1caa801731acdd1654ee219fd092a8f498
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:06:05 +0000

    exercise tough test last strike

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def multiply_numbers(x, y):
+        return x * y
