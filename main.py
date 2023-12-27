624f1dd9d11d135f05f5ad77ce6710561e5d9a89
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:34:02 +0000

    this demonstrate plenty advantage suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
