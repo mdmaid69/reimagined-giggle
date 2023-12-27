79c42f8f43cd1604269983992552e25b28a10206
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:48:05 +0000

    fall doubt yet story embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
