78f1c34627c1a425ce7b67fb3aa642c3fa845f74
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:50:10 +0000

    poor ear physical music properly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
