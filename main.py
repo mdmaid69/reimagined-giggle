0930b4110d046fc331473270b82bfedce87efb08
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:32:09 +0000

    ready your badly combine say

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-for i in range(10): print(i)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
