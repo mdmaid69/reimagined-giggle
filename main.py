378efb1f6e4c6641aca249e2dd9c52d2c82374d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:44:20 +0000

    force picture Vv apologizeapologise mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
