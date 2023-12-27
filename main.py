6f0fd585ead4867958425c20d1490f4430924ef8
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:44:13 +0000

    look music ring talkback motion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
