ea1a80eebd6f716bcd8d1210b9e57d07906b95df
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:21:41 +0000

    store person sink hold report

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def multiply_numbers(x, y):
+        return x * y
