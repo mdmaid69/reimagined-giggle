f2a87c802861d9b809e3d328ad4c66c346a4ae86
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:23:57 +0000

    smooth spend crisis view wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
