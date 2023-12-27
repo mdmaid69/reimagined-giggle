22abc025b3e9beee8bbf7833e363536285564874
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:50:26 +0000

    discussion breakup regional runinto mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
