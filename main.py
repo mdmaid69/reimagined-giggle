50b09f8412cc742f23473d7b2b54da2a192e67d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:45:20 +0000

    lesson round information basically choose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
