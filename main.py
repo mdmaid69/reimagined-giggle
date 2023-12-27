063019c61af316b33861c7a559f1b4381e4c9419
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:28:37 +0000

    relationship boring tear emphasize shout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
