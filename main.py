9dc1fd601882cf415b850440d7b3f30c6e4a3737
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:18:59 +0000

    comment or maintain tap to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
