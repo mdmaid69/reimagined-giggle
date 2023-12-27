bff45322dfbd26b108571fbf904096ee3c335b8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:10:19 +0000

    progress advanced eye deserve declaration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import tensorflow as tf
-print(tf.__version__)
+for i in range(10): print(i)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
