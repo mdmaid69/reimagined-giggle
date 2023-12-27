6936895a9e1871d574e1600eea5360a68de73f38
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:13:24 +0000

    letter squash surface platform belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def convert_to_binary(n):
+        return bin(n)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
