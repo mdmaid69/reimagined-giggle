3d66c75dc6551f184900b05d8c4ef9e56239b2eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:27:27 +0000

    claim song present circumstance half

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_list_from_array(array):
+        return array.tolist()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
