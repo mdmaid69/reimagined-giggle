e78f56960b36f1a6f8547d617fb32a377d2d793c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:59:53 +0000

    favour back tend check form

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import numpy as np
+print(np.array([1, 2, 3]))
