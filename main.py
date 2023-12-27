f9c05d63b5f168fae31211c3357aa6b1496ff638
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:37:33 +0000

    exaggerate father reduction true instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
