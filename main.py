010d3cc8714db49f97a9a4529c91e4552d0f6fb4
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:51:02 +0000

    bridge regularly actual nice female

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def convert_to_binary(n):
+        return bin(n)
