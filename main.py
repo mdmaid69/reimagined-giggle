e53f534ce030144354c1a5023cb5228d2a14a2f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:50:01 +0000

    finish takeoff regular breath lock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_average(lst):
+        return sum(lst) / len(lst)
