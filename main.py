f77585aa42c4328478d2a27d42be736bad0482fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:29:08 +0000

    walk mail old note decision

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
