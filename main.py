faaf480ee45bbc6f05f48225430039ff3feb041f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:04:11 +0000

    method radio matter friend fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+for i in range(5):
+        print(i)
