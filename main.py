f8127ef8c4a4a370f3db50f98c54158736216bec
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:19:15 +0000

    regularly number now instrument confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
