a172bf0ccb04077661a23339a3dec3f9d822639b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:06:10 +0000

    whisper pen funny breakout flight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def get_list_from_array(array):
+        return array.tolist()
