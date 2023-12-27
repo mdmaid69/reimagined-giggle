9087a02e0b077e872d48504c881434149061644b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:00:33 +0000

    doubt split myself expensive flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def reverse_string(s):
+        return s[::-1]
