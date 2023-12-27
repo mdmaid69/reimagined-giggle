08d8dff209dd1f4e7551c608193787ae2c90f95b
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:06:31 +0000

    chance approval variety closely rise

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
