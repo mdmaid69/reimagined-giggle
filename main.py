c830fe5bc652245a45c4269af53e162076c25230
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:22:56 +0000

    trade brush all weigh hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def count_elements(lst):
+        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
