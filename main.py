e90acf8788dbe3f5a5faac5fec052ae894e302a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:46:08 +0000

    brave custom market show record

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import getpass
+def get_username():
+        return getpass.getuser()
