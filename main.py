5bd67d8602bd733f98fdad1aea486fdad362f880
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:15:47 +0000

    hardly keepup suffer model typical

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
