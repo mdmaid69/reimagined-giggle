3c48efffdfdf86d98ec7e0e7808c30cd49e68408
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:14:55 +0000

    manner national 567enemy 573entertainment prefer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
