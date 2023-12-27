fb3df8a2d3c17cb108d400fd76379ca082640991
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:04:52 +0000

    spoil OKorokay either sister accidental

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
