c8096cf6e84737fc07d7a46d37792e97728c2166
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:33:16 +0000

    guilty murder confusing neither of

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def multiply_numbers(x, y):
-        return x * y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
