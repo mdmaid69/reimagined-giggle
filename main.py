c794ffbaa372a2eb56a3869bf71d69486eda0988
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:00:14 +0000

    snow question say path collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
