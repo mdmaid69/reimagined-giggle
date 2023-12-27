7d42377d57a19cf42c328fb1a4a86bb8c36e0527
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:13:59 +0000

    considering little phone 572enter now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def convert_to_binary(n):
+        return bin(n)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
