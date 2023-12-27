0bea6a8e576a09dc47cc3581175379019e390dd1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:10:05 +0000

    time band use this forget

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
