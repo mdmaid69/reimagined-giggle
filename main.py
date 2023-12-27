c2d7699bc27fc7f2e7d5653416982ffb6d2cbf54
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:17:09 +0000

    plate cut rob Gg sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_speed(distance, time):
-        return distance / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def find_unique_words(sentence):
+        return set(sentence.split())
