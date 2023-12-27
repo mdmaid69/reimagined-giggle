84ad346d161e6c918b1ae513c833da8e194d2f43
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:51:24 +0000

    delight generous especially probably tidy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+print(math.pi)
