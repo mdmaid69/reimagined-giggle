2acce49cfcf77f68287ea0365a32919fc11e0519
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:34:17 +0000

    combine ask bring tough class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_current_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
