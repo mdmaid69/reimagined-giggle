cf40be3fe81d347187cd1313d0e34cffa7717917
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:46:49 +0000

    recover this report petrol suggestion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
