9a1c807ca370d176181acf8b5f8656e6c4fe76d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:51:38 +0000

    bill deliberately wrong switch spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
