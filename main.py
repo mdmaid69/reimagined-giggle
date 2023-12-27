ced297b23a56a2a78c02d9570d2ec8424210a7ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:53:27 +0000

    welcome wipe combine support official

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
