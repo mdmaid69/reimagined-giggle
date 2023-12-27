9fa7e7e3bbb48c4ab0308d90084663646348a9b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:11:05 +0000

    block very interesting partner attitude

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def split_path(path):
+        return os.path.split(path)
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
