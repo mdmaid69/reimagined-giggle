02e9955ae87e733fcdb03ed2ef17e0c70daca097
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:05:25 +0000

    her set 564encourage 572enter they

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
