8ff03e061e2aa58b115969445ac8d0979e7995f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:43:17 +0000

    hospital noisy pipe phone undo

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import random
+print(random.randint(0, 100))
