63cac264a2b701d3dff01f3db38f554b532bc29e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:21:53 +0000

    help goodbye unpleasant damp demonstrate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
