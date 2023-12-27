0572810e3b4ec19be81cc3a3d586cfe026e8ed09
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:26:22 +0000

    approve sea manner fold shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_counter():
-        return collections.Counter()
+  def split_path(path):
+        return os.path.split(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
