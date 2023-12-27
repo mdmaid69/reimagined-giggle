d030ee4b0b0a96f937bf0218f2eb98eaa9a159ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:28:33 +0000

    direct trust possibly wind now

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+print("Hello, world!")
