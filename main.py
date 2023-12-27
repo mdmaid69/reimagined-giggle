a9380b1729a9b3892efa1f484c367b518dc022cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:23:00 +0000

    content few side often instruction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
