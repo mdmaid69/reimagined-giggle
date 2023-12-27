b9707716d66026bbc7245fb39e3e5c44d88b1fc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:14:46 +0000

    care small usually spare its

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
