b10a2c6fa4b582122fea00e942f5c4fea4326ef0
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:22:49 +0000

    common guard occasion silent hear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
