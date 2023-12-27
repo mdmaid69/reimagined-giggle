6421071406918b9078c5824f8dd71180583a2be0
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:35:26 +0000

    nurse nice runout mark set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
