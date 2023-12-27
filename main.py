e920812f6e11edfdf0a3803931176e1ee41a2026
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:44:16 +0000

    right equal estimate brush problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
