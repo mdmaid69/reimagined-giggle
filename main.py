f7565a98b753e32d911de22fd8dbac80a2bfb2f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:31:02 +0000

    male later heavy expression life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
