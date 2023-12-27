d664aff247570f50d13f70ac524cca6456f1a4c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:36:29 +0000

    perform scale worry hello dead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import datetime
-print(datetime.datetime.now())
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
