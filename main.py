ea573b963940382a7c352866d9e22e126790833d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:04:45 +0000

    deliberate runover interfere dream suit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
