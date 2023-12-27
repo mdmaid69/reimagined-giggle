f952f55eb0b6866bd18910195c7f69c32b14b718
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:34:10 +0000

    further squeeze honest kind afraid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
