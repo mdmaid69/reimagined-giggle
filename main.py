74eab5e47e4b000451e0080a2bf3a763fac91232
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:15:59 +0000

    wealth proper violent organize view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
