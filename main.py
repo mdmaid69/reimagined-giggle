cf07c3da2883e3a30a6cd7445d0c2dd004d19c10
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:43:09 +0000

    relation man failure too evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
