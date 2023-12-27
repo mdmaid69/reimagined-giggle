8a46273b43446958af21bf9d4cb76fcc4b0ad18f
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:27:50 +0000

    during corner wish check somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import os
 def change_working_directory(path):
         os.chdir(path)
