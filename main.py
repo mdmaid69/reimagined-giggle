9885d302fcd1597ae25ecca2e3d3bdb33f58d76d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:08:09 +0000

    train able juice pleasant enthusiastic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
