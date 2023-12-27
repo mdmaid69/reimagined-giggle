1337364de093164e9652347937badb472e2211ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:19:42 +0000

    about solve maintain hard present

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
