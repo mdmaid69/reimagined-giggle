6b76feff8980da8760bd41032c5cbe2d4c22b6ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:44:15 +0000

    win welcome callat pride improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_today_date():
+        return datetime.date.today()
