c699cd78406be4430a7ea09986cb188f815a4aca
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:54:33 +0000

    pretty himself rest battery tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
