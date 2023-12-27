c959c3d5f18a197ab761cf2bd21925a8af3da4d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:02:03 +0000

    cruelty beneath ground carry conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
