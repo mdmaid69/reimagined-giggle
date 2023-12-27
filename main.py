1cdbed0010a6b84f61a21855eb9f9ff23a15819d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:31:39 +0000

    fat moment runinto talkoutof congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_item(array, i):
+        return array[i]
