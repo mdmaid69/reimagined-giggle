12596102d50370aecc2f3103eb6f52dfcd96e603
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:58:58 +0000

    have once across brother near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
