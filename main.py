ab11fa90aea17a70ab1a4b4f1a63eafeb07aaaa6
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:00:21 +0000

    narrow city convince confused brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
