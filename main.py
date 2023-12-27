3cde1fd7313fe431ea6d72fe28dfa09424c0b908
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:19:48 +0000

    turninto independent weekend able ability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
