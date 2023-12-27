5232620e2a072343093161c01e3222a723dcbc7f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:05:08 +0000

    rarely Gg little compare giveback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
