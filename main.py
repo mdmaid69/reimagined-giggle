ce37bef152d9e4672a6985f1f73549e7e8e94971
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:01:02 +0000

    commit that brown provide comefrom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        print(sys.version)
