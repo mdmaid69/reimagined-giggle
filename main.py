bf307ed82d6bffb2b7d1ec5aad08bbdc5aef293b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:53:53 +0000

    maximum in wash blow direct

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
