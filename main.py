ed8196a227d60233f5fc4c7f8f605493d6316771
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:09:56 +0000

    friend break luck rubbish night

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_length(array):
-        return len(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
