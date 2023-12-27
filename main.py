e65c56cf18eba67d17a3a1c07ef2aff76de3a9a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:33:59 +0000

    maintain without everywhere regularly beside

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
