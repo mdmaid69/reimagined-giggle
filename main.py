a5c79bf4f7a7131c1d8b2bacdbe24356e84f1e84
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:19:06 +0000

    honest trip many bringup list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
