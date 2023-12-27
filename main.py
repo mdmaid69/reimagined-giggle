59a6a74350e9073c8a7c50ed6fade0b8b1d4c6c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:47:26 +0000

    use organize basic point pour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
