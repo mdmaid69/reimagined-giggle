e5c1656997e52f598efb99629259573982beba9d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:04:26 +0000

    win someone uncertain over flow

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import sys
+def print_python_version():
+        print(sys.version)
