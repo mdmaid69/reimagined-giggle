252669a8b6d6e2eb8a44519f1655cd88f8487d5b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:20:15 +0000

    rush Ss deliver later turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
