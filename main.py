5bd0fb2498c59597613f63781454d6cd22d02c39
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:53:55 +0000

    once welcome communicate remember afford

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
