3057978082355dc75922e2302de636a8f700c3b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:18:03 +0000

    loss finish think destroy command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
