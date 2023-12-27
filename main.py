36cf024e0bc06397fc6dd8fbae494559a9d6872e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:20:25 +0000

    last turnoff no talkback so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
