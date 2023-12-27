d5c4009b57cf66b8669387c4f4de655e182ae6db
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:07:54 +0000

    sore sometimes study swim brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def convert_to_octal(n):
+        return oct(n)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
