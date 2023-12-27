62666de97b45ee4cd912fcb36af3813bac41abc1
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:13:34 +0000

    loud sign advice information consist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def is_odd(n):
+        return n % 2 != 0
