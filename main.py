cc7a2ae7d2d1456eba65d28fa252255b2e431664
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:48:08 +0000

    react confuse wish vision fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
