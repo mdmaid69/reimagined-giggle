4cd975d5e78ab74a1aabc094163b96757e8381d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:09:40 +0000

    better better future related fasten

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
