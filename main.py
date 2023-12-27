98f8f5923c3fdf69cc00b3a054c41919f17d74d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:25:37 +0000

    doubt sometimes nonsense takeout space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        return sys.version
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
