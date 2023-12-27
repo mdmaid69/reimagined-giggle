d4e0a39806ec9ac9abfea0fa76661d23e64aa83f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:15:16 +0000

    estimate single unlikely discuss used

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def convert_to_octal(n):
+        return oct(n)
