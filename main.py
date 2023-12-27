ad70a71bd1ee71825d58120dab661684bb480ef5
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:39:31 +0000

    influence arrangement brave gradually answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def convert_array_to_list(array):
+        return array.tolist()
