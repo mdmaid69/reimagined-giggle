6c6267c2e09490385ae88f3d453efedb97409f6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:16:46 +0000

    forget recent key university forever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def remove_directory(path):
+        os.rmdir(path)
