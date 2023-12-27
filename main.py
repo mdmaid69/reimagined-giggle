fc8f004d29835b58310c7b102bbcd40a75e6e281
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:32:46 +0000

    should by if hurt past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
