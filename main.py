cef69fe5b1b9c8574eb7baa078beac7ddf76cf8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:04:58 +0000

    slide mend story meaning thin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
