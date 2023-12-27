b498ee7b86241733b75db08a386f2ba76583fb02
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:52:50 +0000

    fall turnoff deep use beside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def reverse_list(lst):
+        return lst[::-1]
