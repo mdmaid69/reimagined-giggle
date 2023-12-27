f4e8e3e3f0b669172bb1b67dbeb93715e0e4e4e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:23:00 +0000

    why press drawing godown file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
