83a8996e8fcad3158c3445478f09bc76fe839328
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:53:26 +0000

    once shout off doctor male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
