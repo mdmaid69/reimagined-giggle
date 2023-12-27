d84f213179d4679c25e151843a259da61491f91e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:46:08 +0000

    excuse calmly everyone deal myself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import collections
+def create_user_list():
+        return collections.UserList()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
