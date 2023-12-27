264f56a384c8b232ae095af8b7984529e1b59217
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:41:55 +0000

    through realize rescue unite Uu

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
