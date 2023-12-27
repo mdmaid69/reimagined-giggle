f053fde5f61a8d61907b541502c7e3b98f2b6f95
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:15:17 +0000

    concerning bite small movement wrong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_power(work, time):
-        return work / time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
