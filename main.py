da18ead0adf6caebd42ef896bffa801807465c0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:03:24 +0000

    summer later contribute occasional smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
