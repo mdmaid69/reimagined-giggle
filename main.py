91e56f7ca308f4a9ad6042c70db5d976d65b9bbb
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:44:23 +0000

    achieve keep computer commit spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
