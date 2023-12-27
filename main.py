5963e761b930cc13a0fcefa83310bee3a8e1d758
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:15:06 +0000

    swing quick maintain somebody plenty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import os
+print(os.getcwd())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
