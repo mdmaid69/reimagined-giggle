6f001bc64f58215381d8ffa54011c268f402f0df
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:42:11 +0000

    keepout morning harm there thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
