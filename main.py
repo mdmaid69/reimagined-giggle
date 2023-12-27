b88f7d006b0c088a99973128b8553df7b73523e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:36:46 +0000

    whichever join like brain blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
