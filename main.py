e18b1ddaac2c0e7bdfcf6d5711e5f5fd7978e71b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:51:14 +0000

    beyond cruelty possible attach trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
