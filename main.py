efe6f6af27c97cec37888033a5b7c7d645d3341b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:58:51 +0000

    stretch before throughout criticism finger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
