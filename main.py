06f7f05705ee5ee1b6b248c4b4308c5fa342528e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:52:49 +0000

    home far on attempt take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+print(json.dumps({"name": "John", "age": 30}))
