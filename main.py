79371d06152d864833f856d5f65e99f6e3de3fbe
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:32:10 +0000

    no language wish chin popular

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_counter():
+        return collections.Counter()
