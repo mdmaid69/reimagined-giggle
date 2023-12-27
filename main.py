e892d43040a3ad1a8ea8c4fa9530749ff37e96e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:08:54 +0000

    nearly him circle ago school

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def sort_list(lst):
+        return sorted(lst)
+def calculate_speed(distance, time):
+        return distance / time
