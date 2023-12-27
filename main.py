c18a32b14951b197c82f0ed924d1164acfb23a95
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:40:25 +0000

    never 572enter test moral major

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
