b269595499b2e16bfd2203ba8df83b04a64c4033
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:43:26 +0000

    bye meeting gift edge term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
