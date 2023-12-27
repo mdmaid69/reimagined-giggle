9b6392a59fe7136459b33784ac726bb8ae518974
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:36:14 +0000

    immediately glad identify nurse increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
