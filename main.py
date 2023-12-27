18f161a9f7c21182e1a4efcffd002a423719f9bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:07:22 +0000

    worst proof track giveback speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
