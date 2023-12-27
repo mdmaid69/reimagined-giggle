4c9aa66c2a93db90aac40f532882b8480240d398
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:06:55 +0000

    meet up forget nice record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_density(mass, volume):
-        return mass / volume
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_item(array, i):
+        return array[i]
