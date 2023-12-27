c25e89c22969fc1065721350048b2c76f146f382
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:03:52 +0000

    give however pretty winter pollution

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
