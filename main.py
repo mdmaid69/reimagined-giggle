d922d68f1c456e0f962a2cb0c808c04c74fa9f53
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:21:15 +0000

    they mixture reliable slowly thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def remove_duplicates(lst):
+        return list(set(lst))
