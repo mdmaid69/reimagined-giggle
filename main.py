eb98ebc690461d8eac7055e9b846e935ba2a1af3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:12:44 +0000

    shake history consider age within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import datetime
+print(datetime.datetime.now())
