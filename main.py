4a7b11f38811da4b6e7276cb2a32968a866529aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:15:23 +0000

    start further ride deserve violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_density(mass, volume):
-        return mass / volume
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
