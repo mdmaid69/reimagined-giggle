93c0cd3e9f438e1f58f82794cb8e639f5bd9062e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:28:25 +0000

    sweep anywayanyhow need week boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
