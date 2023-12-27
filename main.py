1ddf31e6e6abbfffb30ab843ab972f1adb365566
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:32:20 +0000

    shrink cheap sing handle just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_pressure(force, area):
+        return force / area
