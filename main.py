97624ab001b1454bc4804e56bc8bd1e949a7216e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:07:19 +0000

    brother connection border address shame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
