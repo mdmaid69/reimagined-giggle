93f49030e9c072e96f99199a5980f5cab47ce9bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:42:14 +0000

    famous home variety next usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
