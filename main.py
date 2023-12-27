ed320cb8cd0931ecd21cb35abcaa9548e1dec3fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:07:52 +0000

    shallow brain live report gather

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
