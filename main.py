7d31dfea7da49fcd9e7dd762846d6310994f4147
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:03:42 +0000

    point tip man experience reliable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
