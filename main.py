7b463bf7da6e0572f5a31300ddc2b932910a8eff
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:43:15 +0000

    goon nervous separate cover pipe

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import sys
+def exit_program():
+        sys.exit()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
