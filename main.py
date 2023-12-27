092e7e54b21c34db807c1c42b003d3b5e4a19155
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:15:39 +0000

    check then loud sit permission

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
