6249c2522dc16e221cf1e231aa773e6dedb1c16e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:54:24 +0000

    shame knock physical event this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
