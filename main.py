b2141d4d46e8498e3f37839ec7cbbf52d4402f3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:23:44 +0000

    light afraid delight movement instead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
