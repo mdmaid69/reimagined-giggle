a8f784077ea7c7cf8b19b4b36e284b0b821dbb2a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:01:53 +0000

    out giveout birth flood normal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
