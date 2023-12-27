0e772156332eae8af63c1b89e093f0c0e86cbde9
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:19:22 +0000

    space attention regular from home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_density(mass, volume):
+        return mass / volume
