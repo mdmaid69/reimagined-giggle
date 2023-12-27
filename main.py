8c939df3b2cb13afa6f62c8869e8bc9d71564cdd
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:53:54 +0000

    temper juice through notice summer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
