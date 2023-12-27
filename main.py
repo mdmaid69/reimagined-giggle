793268e736d52cb14f50b2417e7cbd74b81d7c4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:22:22 +0000

    get beginning attach inside taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
