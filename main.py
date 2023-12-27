d576634a265763cdb21458a02852ad98e014c881
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:35:48 +0000

    but right declaration giveaway region

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
