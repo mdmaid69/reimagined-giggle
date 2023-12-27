403da9df2566d1a8f9c93fc9df66ad660aa9472e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:21:56 +0000

    divide car female Bb sick

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
