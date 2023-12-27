a65970ebc2d9f8fc27a97592b3027a58237585e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:15:44 +0000

    rest bill too improvement treat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def find_max(numbers):
+        return max(numbers)
