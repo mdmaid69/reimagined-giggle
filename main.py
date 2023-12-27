06d2c277d155b010fdfe968791112f570ff9fc37
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:05:10 +0000

    deliberately fasten Tt tongue culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def split_path(path):
+        return os.path.split(path)
