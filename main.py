3d896b69a50878e09a7bce90f94f2bcb6655ce1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:59:29 +0000

    plastic every usual division basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
