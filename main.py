ae1ee99470188361e9509725f187db02d7da7003
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:04:49 +0000

    error squash begin bringforward Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
