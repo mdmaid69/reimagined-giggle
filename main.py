a8d3efcb53fefb02362f777e48ddd5fc653e5a10
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:50:15 +0000

    personal hand takeaway harm famous

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
