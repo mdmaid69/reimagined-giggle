47d2de307dee9dd2def6f74f487be19bdbb3c9a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:57:22 +0000

    neat target cover burn standard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
