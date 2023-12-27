603189db80d29327dcd5914b81a2ca91d6899a00
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:50:41 +0000

    turnback holiday boat neck bridge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
