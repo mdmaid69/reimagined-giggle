184fcbad95d2ab4e6b62916d2faef8c87f59ca1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:10:57 +0000

    children loud conduct purpose circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
