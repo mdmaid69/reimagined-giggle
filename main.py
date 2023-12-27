96c1f5fc432a33dcb4133579abffc66a7764a6c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:09:02 +0000

    guard false floor approve attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_min(numbers):
-        return min(numbers)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
