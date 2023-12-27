f5e6467ad9ba3e38db1d589badb8bcf5836ebc92
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:07:56 +0000

    correct sorry cross surprise lucky

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def find_max(lst):
-        return max(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
