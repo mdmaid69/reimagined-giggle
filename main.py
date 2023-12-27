d6ab320beeefeb207e3a1521d62fc24b46850c96
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:25:23 +0000

    takeback male collect have Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_current_directory():
+        return os.getcwd()
+import os
+def change_working_directory(path):
+        os.chdir(path)
