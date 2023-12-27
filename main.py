701e5129b826fdbb082b2445dfa89b4636c7f56d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:54:14 +0000

    by watch sale second sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
