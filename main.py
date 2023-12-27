084e508745ef40b6a0e014e8dc629bedf372eac3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:42:02 +0000

    tip common study development separate

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-for i in range(10): print(i)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
