4662bf0f41bcb8e5492f7edc072741e96be7f871
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:17:32 +0000

    firm belief expression getout rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-print(sys.version)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
