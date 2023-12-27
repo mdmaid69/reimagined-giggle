925b77e314605deb0fe9b36290458256e5f24894
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:16:42 +0000

    wind stone rarely rough survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
