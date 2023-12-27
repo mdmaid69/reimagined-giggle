cce2c7b5398615686989e6c646d80221bddec162
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:35:02 +0000

    set attempt communication touch urgent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
