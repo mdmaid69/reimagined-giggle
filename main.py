24693d2772b3fefe1c78605921a5dfc9dd1dd379
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:22:18 +0000

    meet top shock spot instant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
