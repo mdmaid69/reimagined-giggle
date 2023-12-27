6cc01c42eb21dfed189dfc5bd75358874a59a163
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:24:29 +0000

    neat anybody cope check cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
