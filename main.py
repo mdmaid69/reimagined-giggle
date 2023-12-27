0febe4894edc95bca70438994cdacb90501878cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:08:19 +0000

    talkinto minute position feeling hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
