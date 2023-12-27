16c695815a2cda78e05239840e75475328332465
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:32:04 +0000

    hard suit judge trick come

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def square_number(x):
-        return x**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
