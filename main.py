96dd405676182e7f8aa1254cb820e1711bb2a950
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:36:01 +0000

    clear deaf thick 566end sail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
