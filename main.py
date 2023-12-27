436122f83cccdf977d5116a3f48c7ce4266ca70e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:20:51 +0000

    control rob escape loan far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_repr(array):
+        return repr(array)
