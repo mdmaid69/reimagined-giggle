9e9050857de25ef1cfdaf2139f73e85b7040227a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:09:08 +0000

    please let operate dress music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
