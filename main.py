8628fcd5248796ec4f2241afd99e099dc27ec93a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:30:32 +0000

    empty surprise pull inform control

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import sys
-def print_python_version():
-        return sys.version
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
