37e1f1840b444ec56a59fc7c2fffcb7e32032364
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:13:58 +0000

    powerful same terrible forward still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_itemsize(array):
+        return array.itemsize
