55856fbc84d09940094a6f32b09f3af1f73acbb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:27:42 +0000

    path too smart sight Ff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_acceleration(speed, time):
-        return speed / time
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
