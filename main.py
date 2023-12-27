ef2855a60d595481a9cf40e72b7e89e7a955f37e
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:57:48 +0000

    crash plant not illness measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_permutations(n, k):
+        return math.perm(n, k)
