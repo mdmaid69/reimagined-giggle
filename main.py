88a7d6e859f6013eedb833d94ef4205d39feba22
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:14:34 +0000

    guess Jj backwards OKorokay play

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def greet(name):
-        print(f"Hello, {name}!")
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_acceleration(speed, time):
+        return speed / time
