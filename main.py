2cad8656978ee8affceea9006f8498defbc9ddfd
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:44:14 +0000

    sentence platform concern oil straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_array_as_list(array):
-        return list(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
