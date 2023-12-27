4754a9bdc589690a9bd0ee20a554ccff29ce9e37
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:55:02 +0000

    sister up carry turninto explain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def find_max(lst):
+        return max(lst)
+def count_words(sentence):
+        return len(sentence.split())
