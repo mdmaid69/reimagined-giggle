df0d555e602da50e0d1891288752f380401b9363
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:19:44 +0000

    difference through exaggerate stamp callback

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_area_rectangle(l, w):
+        return l * w
