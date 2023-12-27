c9342c41e8f19a7b5909e1879a2872e08121f9b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:58:57 +0000

    regular negative interfere over level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
