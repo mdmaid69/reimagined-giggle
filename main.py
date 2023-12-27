3a8717257481de5d648b269764d976cccf3a0137
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:05:44 +0000

    interest turninto fit ball unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
