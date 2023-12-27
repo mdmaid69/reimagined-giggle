468bea73f027600524e10e5b298cd2daad088837
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:50:34 +0000

    turnon march food arrive contain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
