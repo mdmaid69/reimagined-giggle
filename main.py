45a1d108a44d7c74ee4087cae8b4e4ec627cbe92
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:23:28 +0000

    in morning swim oil Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
