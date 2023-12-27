914b25405672ffdb62bdb0b6729ada76a32057ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:20:08 +0000

    evidence without goodbye offend appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
