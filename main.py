6df4324562257a316d52886d00a716c3b3521b08
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:55:16 +0000

    custom congratulations Ff want ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def clear_array(array):
+        array *= 0
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
