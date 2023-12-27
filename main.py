826582300ac248f9b8e38992a8328d114a0b1ebf
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:14:21 +0000

    suggestion dark read tomorrow soil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
