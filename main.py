fe1e10a015db338e9fed74be0d3d04ce50646437
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:02:49 +0000

    eager card occasion contribute however

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-import time
-def get_current_time():
-        return time.ctime()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
