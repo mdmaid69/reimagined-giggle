48a5149734c0a0a3a26d456adbd156af60e59fa8
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:04:52 +0000

    divide near fine degree brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def clear_array(array):
-        array *= 0
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def remove_from_array(array, item):
+        array.remove(item)
+def sort_list(lst):
+        return sorted(lst)
