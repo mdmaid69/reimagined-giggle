0710f710e22096637b313a948bde28cd51e7fb79
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:37:01 +0000

    object keepoff lend situation content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
