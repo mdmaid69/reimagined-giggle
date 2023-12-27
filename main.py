69edd13e2c9a6ff0e38c194a30d253dff472bf5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:29:59 +0000

    fortunate stop same far very

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def sort_list(lst):
+        return sorted(lst)
