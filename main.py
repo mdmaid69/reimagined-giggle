e08889b2a0b8d7a2b78dff41935c516179ea6133
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:22:12 +0000

    survive blow curious car male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_current_directory():
+        return os.getcwd()
