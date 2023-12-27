34df9520df6a8341db46994ae744d662f0e6e462
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:52:03 +0000

    passenger splash block regular building

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def remove_duplicates(lst):
+        return list(set(lst))
