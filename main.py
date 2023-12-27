0833897bb122dce72d736f6c0450e5eb344ec07c
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:51:22 +0000

    black black medium poison piece

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
