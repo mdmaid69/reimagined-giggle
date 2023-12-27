74c7fd8ba3f1f9a0937724a39979b10c915030b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:18:26 +0000

    conversation noone somewhere keepup shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_str(array):
+        return str(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
