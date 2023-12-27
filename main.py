82261a1f7d64dfc494367e5141b984728017bc50
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:22:10 +0000

    takeout right Pp suppose subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_palindrome(s):
+        return s == s[::-1]
