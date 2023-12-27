d3c5f1f2a7cc4a953df4b5ac0c7032452fa933f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:24:06 +0000

    sight Ff personal broad wonder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
