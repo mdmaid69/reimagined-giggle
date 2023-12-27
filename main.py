49f2e606bd55005b69e476e4accbb303a919e613
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:34:20 +0000

    by yesterday hospital expression morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_item(array, i):
+        return array[i]
