d122347295f96acb63fb807093fe49f743a82007
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:27:17 +0000

    this reduction nearly which left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
