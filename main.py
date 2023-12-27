3512c82bf236dc995cea438bf0c86f0c336049dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:43:22 +0000

    of hard butter why custom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def count_elements(lst):
+        return len(lst)
