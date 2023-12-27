1466e23ed7afd44a63d00672e70bce3e7bc7bd25
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:29:29 +0000

    570enjoyable various unusual top guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_even(n):
-        return n % 2 == 0
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
