59012f2102baa18a410c33508d97b0af872e598a
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:31:42 +0000

    lean makeinto myself top object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def add_numbers(a, b):
-        return a + b
+def get_array_as_set(array):
+        return set(array)
