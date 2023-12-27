ffd178ac801dbed6ed3af90aa322338e7a413bcb
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:38:14 +0000

    smell heart official which steam

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
