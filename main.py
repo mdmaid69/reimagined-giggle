eec8485eb3238b529a9e99e34302404e3c10c97f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:49:40 +0000

    near spill complaint goaway spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(lst):
+        return max(lst)
