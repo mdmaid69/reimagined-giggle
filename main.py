1cb7e9af3b3ed0535f2d896431de5baf783adab7
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:09:27 +0000

    law stretch possession present history

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_area_rectangle(l, w):
+        return l * w
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
