0b0239398c7d2d3eb682f2751c5381e1ac06bf48
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:14 +0000

    big clearly powerful family warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
