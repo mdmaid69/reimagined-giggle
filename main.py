ee6b4b1fc21cfc833a8c567886fae3d6ac95d183
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:20:27 +0000

    row praise mean stand permission

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
