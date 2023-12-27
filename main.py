f3ef7911cdd2295f4bab35e59f91cb6e4d25bfc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:53:24 +0000

    waste use sail dance satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
