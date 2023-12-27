4682f48c87cdf9daa007f68688f728fd3341fea0
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:16:38 +0000

    neat broad strange scheme common

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
