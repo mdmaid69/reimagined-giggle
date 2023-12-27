cf5f385ec31e9adcac45ae66a434fa5cd79f9890
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:30:26 +0000

    eye wave rich woman admit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def find_min(numbers):
+        return min(numbers)
