47169f9119bd5af40ec9bd9a32f25af5fc0ad3c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:40:26 +0000

    still will scene pot feeling

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_itemsize(array):
+        return array.itemsize
