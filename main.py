d5d3978d1cab7468d1564b04c3df872e797c04d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:36:14 +0000

    begin damp reliable state cook

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
