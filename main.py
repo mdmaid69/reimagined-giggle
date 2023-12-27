33b8a9d5a01cb7d265fc3c0075c0d4127cc59ac2
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:35:11 +0000

    ours spite mass tip shine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
