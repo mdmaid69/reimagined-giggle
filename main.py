558dae617c090ef9ed95b61f7e79d85f931f3e35
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:53:15 +0000

    572enter notice blue again whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def split_path(path):
+        return os.path.split(path)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
