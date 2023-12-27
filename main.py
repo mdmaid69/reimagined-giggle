4889ea085632c4e945f124c9dd4085b112c07888
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:25:44 +0000

    take in honest hurt welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.time()
