179c03d3eeefc91e0192754cb93114debd52bbdf
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:53:34 +0000

    worst pair organization breathe field

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_acceleration(speed, time):
+        return speed / time
+def square_number(x):
+        return x**2
