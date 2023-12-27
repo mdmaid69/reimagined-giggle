af38d64bfb30619ad166bcd2dc32e3d0d31ff572
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:16:20 +0000

    finger Ff arm but rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
