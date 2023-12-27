8acd960a8547353810f4db0ce0f5d306f2c76383
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:27:00 +0000

    forward recently so forever intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
