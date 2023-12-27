6f6cf501632cdbfcc3fb7a46eed5e12e74e934bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:14:50 +0000

    light although chance unable recovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_max(lst):
+        return max(lst)
