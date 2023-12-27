41da9256600c2e39a1b2aff3da316dfe8167801d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:04:23 +0000

    love stamp slowly wet silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
