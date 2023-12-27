accb8901351a080605413f115fc0a73a26913f6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:07:33 +0000

    bringforward apart solve apparently taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
