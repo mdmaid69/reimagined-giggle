73fa61a7183b502f92d57f59fff1e9d1adbdea70
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:04:58 +0000

    fond my trousers remark shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
