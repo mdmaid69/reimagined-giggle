2683696dd30dd0b4437f936ac03020a47c1b0f4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:29:18 +0000

    share creature mean guess chief

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
