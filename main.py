3671b50d8a96b441d347501a3a4ecbe3b38c56cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:56:58 +0000

    amount slip introduction shut contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
