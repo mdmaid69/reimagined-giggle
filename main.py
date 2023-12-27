3b0825a17756530c34dfe44742760a9a79a9ebe2
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:01:40 +0000

    mind whoever song still runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def count_words(sentence):
+        return len(sentence.split())
