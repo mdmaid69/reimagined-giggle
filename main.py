dce4512916af7a0532cd8e69e544980cb1e05c10
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:19:05 +0000

    must mark switch drink opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+text = "Hello, world!"
+print("Words:", len(text.split()))
