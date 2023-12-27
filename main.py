902d283afa3d9917b614c6471ab3e796738383f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:24:38 +0000

    Jj later time score shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
