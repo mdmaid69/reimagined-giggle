e90bf9db017e54a908865f2284f2b64a710c2bf7
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:21:04 +0000

    punishment old question sad personal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_binary(n):
+        return bin(n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
