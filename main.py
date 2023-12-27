aed4112e5e98d80386bf42a67628e424443c0cce
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:39:04 +0000

    clear sense include at field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
