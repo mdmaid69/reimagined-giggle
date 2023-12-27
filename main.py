cedf2fb7f985e05ab23519a9d8911e415ba8a7f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:11:01 +0000

    wonder kind during pure usually

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
