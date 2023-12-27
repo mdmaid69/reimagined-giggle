fd74e9873990df5b47ac167fc338ee273eeea423
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:14:22 +0000

    comeoff load software tongue lot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
