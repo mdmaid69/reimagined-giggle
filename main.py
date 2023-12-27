b95d28b7f22f7f8ac349491eabee512781eab87a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:59:15 +0000

    bill now key opinion enthusiastic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
