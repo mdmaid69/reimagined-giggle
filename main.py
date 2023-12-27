dd2603dceee3534ca30436a2173dae9a58d0a6a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:32:47 +0000

    recommend announce duty adjective opponent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
