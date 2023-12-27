7d32a29f1e751c3d8d826f30a680711bedd04b62
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:08:16 +0000

    on successful hers basically hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
