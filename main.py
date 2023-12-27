ae36ebbe9d3b15d23a55ce45342dcfb041e67627
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:15:09 +0000

    report organization call fear bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
