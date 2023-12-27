63402c9eb5ca6a0546cb1d90a36f98d10e3a1450
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:33:41 +0000

    sound shall wide present subject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
