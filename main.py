aec15ca17897abde8affb441a8a9f3c090caa33a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:16:20 +0000

    Pp difficulty unkind bright right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
