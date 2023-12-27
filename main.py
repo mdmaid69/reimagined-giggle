8a30b7b45bb2a6e1eea5b9abf3ea0f1c4c9070b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:53:13 +0000

    admire shop legal smart large

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+print(time.time())
