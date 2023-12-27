1b6a145363af2297b33e63eab4fc9af030d00d4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:15:36 +0000

    label holiday dead strange team

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
