5cc6e17a3e9243155e294dd038ed9a88df4fb8fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:35:54 +0000

    fast organization various question chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_acceleration(speed, time):
+        return speed / time
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
