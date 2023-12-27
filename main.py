0edda05686a5b0bbf3a631aecab0efb3fdba3d17
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:02:48 +0000

    combination fact justice confuse change

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
