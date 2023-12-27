4526b5fa46e26372d02ce6a790620f63c80b2aeb
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:08:30 +0000

    soon contribute accidentally direct getaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
