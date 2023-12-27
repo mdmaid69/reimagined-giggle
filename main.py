802f542327af673f722af2a7e2112eb8306e9115
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:56:07 +0000

    over surprised mark slice discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
