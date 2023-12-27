7f3f7879a63768cfb7685274fd9add8b7ee0c96a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:46:52 +0000

    whether process climb cover plenty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import sys
 def add_to_python_path(path):
         sys.path.append(path)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
