ecc486b58414b98b2e6035a14cb2b9be6e2a7625
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:08:56 +0000

    letter mistake switch example rain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
