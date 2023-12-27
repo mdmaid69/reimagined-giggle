d44b2bf44fe845239da444e1a084bf853230eab3
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:02:41 +0000

    rest acceptable quantity relation spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def remove_duplicates(lst):
+        return list(set(lst))
