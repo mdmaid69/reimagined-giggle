be2b3df1cc7cd020a4dbd2821fd596305bc7ec68
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:33:43 +0000

    gooff obtain pleasure manner start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
