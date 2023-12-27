d7a3cf700f9186ef06e75af6e57491a2b8091406
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:32:09 +0000

    unite thought cheek wind ever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+print(math.pi)
