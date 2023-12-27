d92cbf5b56c064dae550295a040a635905016c02
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:14:48 +0000

    much contents challenge suckme dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
