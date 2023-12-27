f20d7766eec6a97eb045efe242675463a32ebb87
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:33:01 +0000

    nervous watch emphasize pleasure contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
