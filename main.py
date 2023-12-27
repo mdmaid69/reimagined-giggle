3158753c191d99b596b378ce57d781f10edd3cd5
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:38:25 +0000

    back vision accordingto behave force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
