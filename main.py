51ce411de4b7f45d5e55d23f451a8b1d87879b2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:30:22 +0000

    real arrival try game getback

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
