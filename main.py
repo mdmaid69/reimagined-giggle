0d665170f08f2b517cd5d1ae4a9de900f7d40cfd
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:43:25 +0000

    race trick over damage steer

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
