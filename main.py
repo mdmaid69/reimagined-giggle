721357cf5554c7add21d1c45497a6b9cdeb75a1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:46:36 +0000

    regularly total wash prepare message

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print(sum(range(10)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
