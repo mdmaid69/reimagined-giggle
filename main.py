80dd9d4e27fdf44424556c57af8a8286a9d26dad
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:20:35 +0000

    question come grow downstairs close

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-import re
-print(re.match("h.*o", "hello world"))
+  def reverse_list(lst):
+        return lst[::-1]
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
