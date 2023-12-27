275be31cfaabfb87101bb2d58e17c2dbb3b9f1d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:22:29 +0000

    protect correction powerful twist woman

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
