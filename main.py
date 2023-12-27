146cfb457068b7f3d2303557c2765b83f6455981
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:33:31 +0000

    eager flight mother probably my

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-print("Hello, world!")
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_item(array, i):
+        return array[i]
