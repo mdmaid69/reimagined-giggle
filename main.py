2000325ea5773878fc270288f349ebcd062220a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:22:45 +0000

    around related drawing shrink die

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_acceleration(speed, time):
+        return speed / time
