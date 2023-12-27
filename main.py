ff74993add569ab3635285b4b1f1d9c4bf53d142
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:17:16 +0000

    bang error alone effect fix

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_density(mass, volume):
+        return mass / volume
