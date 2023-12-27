102ffb25bdf5e5370a4476d785726673aa360b8f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:16:00 +0000

    concentrate answer talkinto through strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_area_rectangle(l, w):
-        return l * w
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
