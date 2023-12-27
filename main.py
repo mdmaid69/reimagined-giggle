578f0c4205072692391ddb4fc1eb33d372a53f79
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:35:35 +0000

    state majority habit breakthrough region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
