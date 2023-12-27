ce8b9fbe35dc3bee2f29ab7195b806679a1df130
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:31:21 +0000

    first circumstance put welcome plastic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_stack():
-        return collections.deque()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
