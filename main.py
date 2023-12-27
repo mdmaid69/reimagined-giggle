4400bb96e4728b4890864c4e2ee535db9d2c9540
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:09:13 +0000

    section steep special criticize teach

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_speed(distance, time):
-        return distance / time
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
