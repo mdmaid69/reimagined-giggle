f2f869a65c70ce18be2855dbef5cbcff838dcb98
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:20:41 +0000

    goaway before clearly begin eye

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import datetime
 print(datetime.datetime.now())
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
