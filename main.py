ff046cbd8116cbf36ffa71907f78a38a082a3c29
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:57:21 +0000

    injury handle doubt noisy mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
