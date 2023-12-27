4ef076249c77f59435d55df089533e4dec78ffe4
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:07:15 +0000

    dress say rub runaway wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
