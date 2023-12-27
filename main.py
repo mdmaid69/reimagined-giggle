c0efb3a5333a0f4889d63582077fc2209f9cf671
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:06:42 +0000

    state sign cruel distance joke

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
