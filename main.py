1c6d096a0d61357a0d8b14b4b7799c4b6720e0f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:50:12 +0000

    true book tune busy culture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def is_palindrome(s):
+        return s == s[::-1]
