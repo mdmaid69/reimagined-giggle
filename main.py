9506153cac37815d6caaa796af1eaffc3d61efc4
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:44:55 +0000

    wipe trick Zz disapprove left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def count_words(sentence):
+        return len(sentence.split())
