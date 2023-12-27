fe8a2bd421fa3ba6cacc2e9bff37b5f279c013a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:49:21 +0000

    nor false physical die herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
