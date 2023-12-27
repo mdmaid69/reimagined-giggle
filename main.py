466c50932898c8ccb69d4082427cf9bf5943608f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:21:42 +0000

    beginning pause heart turnover demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+print(time.time())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
