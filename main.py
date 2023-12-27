0706c65e1b4c22c15bb74b4b2336832c645db5f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:27:13 +0000

    turninto justice try drink influence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
