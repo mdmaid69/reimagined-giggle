44ccfbd045ef8f45ec468272ed1f0e352cbf7c04
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:42:33 +0000

    vote meeting shame pay operation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def add_numbers(x, y):
+        return x + y
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
