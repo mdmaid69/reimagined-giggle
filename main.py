fc8311af904c3a61b63501781262a29f504a57f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:10:32 +0000

    excellent die through worried offer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
