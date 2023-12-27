3e3c440e9256deb3f9614f09b048fc554e42dab0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:10:36 +0000

    fail behaviour comfort rub far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
