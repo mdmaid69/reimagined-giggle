17c747b716c35df4a5215905fc3424cadc22b41a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:20:37 +0000

    offence motion frame treat frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_arc_cosine(x):
+        return math.acos(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
