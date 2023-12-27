c425c3d7b0acb5929c4094ec2a402cb57d3449a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:32:53 +0000

    dick meanwhile smell scrap right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
