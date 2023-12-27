05a591c3f459fcb20a463fbe47f6b1c08c82d4d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:34:39 +0000

    concentrate satisfied sell before road

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
