3a47486ae00cc2687820b9f800ea0f38b74b9a05
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:46:51 +0000

    takeover apparently past Mm cheque

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
