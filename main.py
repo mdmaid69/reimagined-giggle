ad136b5ad935b082bd6abd70c9188aaacdd6c210
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:26:31 +0000

    difficult project right Dd affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import numpy as np
+print(np.array([1, 2, 3]))
