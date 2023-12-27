07ffc1727387467f421298766f37a7d2fde3dbe6
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:40:53 +0000

    board light across pleasure fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def clear_array(array):
+        array *= 0
