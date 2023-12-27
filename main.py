df9cbcbd33f3477d9259bcf7d3f26158a2ab4bb5
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:31:23 +0000

    wet anybody lift open instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
