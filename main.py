c6fdb6e28da44438e5797590e53dad8bff837a5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:57:00 +0000

    round thin now flavour absolutely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_repr(array):
+        return repr(array)
+def is_odd(n):
+        return n % 2 != 0
