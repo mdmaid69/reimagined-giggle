09bccfdbaaab98e7db0312f20dcd2f3562ca3214
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:47:21 +0000

    push unless leg himself defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
