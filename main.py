5ab2a20941e90a5957927eb04d3f2fa137e806fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:27:31 +0000

    passage vote emergency away key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
