495564c953714f3c73e07fce94f30d46d0f92719
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:14:11 +0000

    further reasonable centimetre moral 568energy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_error_function(x):
+        return math.erf(x)
