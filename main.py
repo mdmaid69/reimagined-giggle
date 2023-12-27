2ded1e3a86bd7e183014fb750bc322a6a6c84861
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:36:17 +0000

    aim payment still dry gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import os
+def get_current_working_directory():
+        return os.getcwd()
