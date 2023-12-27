7e80685125883697601f22bd4f8ca23364dea910
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:17:41 +0000

    organization quit fortunate base worst

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import time
-print(time.time())
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
