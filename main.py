cfdd1c347f65c552b0830a82d7f21cfa1e226826
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:35:24 +0000

    phone smoke connection or pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
