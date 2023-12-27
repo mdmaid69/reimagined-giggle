b23c51edb26504c5ba1e0529061582dba070ea36
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:49:23 +0000

    interfere better bar callup ashamed

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+print(datetime.datetime.now())
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
