c65c55789e2f771332a6ec361e8fc379e6f06ef5
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:40:54 +0000

    include doubt one deep drink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
