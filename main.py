c9468d531011c27e6e6902ce0970e289f8e0a7b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:03:12 +0000

    shall mean see tidy weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_stack():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
