f0ffc8a4cb529a0dfc6a6a8dadd050917407135e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:32:53 +0000

    surface genuine regard trust tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
