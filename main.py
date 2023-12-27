b9ea1a72906166ab61923b92cea7ce2a0b6a3b5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:57:38 +0000

    late crash information spill shut

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def find_min(lst):
-        return min(lst)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+print("Hello, world!")
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
