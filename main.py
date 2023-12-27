464f6a1cbb1b9aa8b4a026adae1ebf41702adc9e
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:04:26 +0000

    dangerous free should unpleasant nor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def delete_file(file_name):
+        os.remove(file_name)
