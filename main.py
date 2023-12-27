cfdcabb0cc1a028773785e9738e487332a625617
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:17:10 +0000

    have heavy over study sudden

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
