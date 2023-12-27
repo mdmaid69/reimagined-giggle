830e13d3a5d4b0d59440380ba551ba7b63186bfe
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:45:05 +0000

    popular wire bit off rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
