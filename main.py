d50ac57d67a7db15e6fedd5463a8a417b3122561
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:37:21 +0000

    mean sky emphasize date vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_int(array):
+        return int(array[0])
