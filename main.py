d6c65a2125ec2e75b351ea03a88b0806eaf4047d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:18:19 +0000

    block way fault trap compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
