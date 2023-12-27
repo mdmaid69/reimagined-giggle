dce862033710f58e4b07a3851909486c8a1c51f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:37:56 +0000

    letter between behaviour boat whatever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+print(time.time())
