76224417b5dfde93ae040686e92893bd6ce580f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:41:35 +0000

    artificial we food that emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
