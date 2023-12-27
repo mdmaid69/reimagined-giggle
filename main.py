3201dfff0579fd7b64f27c2ac55a5f8e84896b66
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:41:31 +0000

    label this gift year weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
