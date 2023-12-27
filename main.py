08b0bc4639d95c57b5c58bab3d85514728820edb
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:32:11 +0000

    slip leave react recognize suck

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
