62c7867b9d03ba11deace6c41992547ae2a7fefe
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:42:43 +0000

    check smoke present expensive child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
