7c893a8495fb860263104425b98861de6d70984c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:24:55 +0000

    suit cinema see light rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
