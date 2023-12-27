f585d9c77ee0eaf7b58b007fdbe811954152f1af
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:34:56 +0000

    Dd murder hold scrap field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
