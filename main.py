336fc8a8664f3989ef292d1093354eebf7f1af67
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:05:22 +0000

    circumstance 570enjoyable nor mention hardly

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def sort_list(lst):
+        return sorted(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
