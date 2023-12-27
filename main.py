c766390cbde58b4254da842b4be4542f620a2405
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:25:01 +0000

    means respect hurry talkback sensible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def sort_list(lst):
+        return sorted(lst)
