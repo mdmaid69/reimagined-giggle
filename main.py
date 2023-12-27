ea908cd97ded1dd5e62d1b8c9f314068b30f6ecb
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:20:59 +0000

    skirt invitation Ff equal write

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def is_even(n):
-        return n % 2 == 0
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import time
+print(time.time())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
