81a2e1d04881c285904c5178d71d0ed315937a7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:51:43 +0000

    secret fly unlikely often disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
