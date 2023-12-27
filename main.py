348a270f56a948b42445f0360dc999da4becd381
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:21:49 +0000

    breakin answer advice us group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+text = "Hello, world!"
+print("Words:", len(text.split()))
