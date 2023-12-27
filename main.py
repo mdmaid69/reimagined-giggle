16aea62dc8f78140e93b4c962e355a7af29e4dc6
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:00:18 +0000

    but set blue wind Nn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def multiply_numbers(x, y):
+        return x * y
+def multiply_numbers(x, y):
+        return x * y
