9cb44be9acdbedffa0c05168a78016c44926a5e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:31:46 +0000

    brother convinced news appear post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
