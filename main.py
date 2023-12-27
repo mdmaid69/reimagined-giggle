b8a404887e71da07aa21d5d0475193d3243f63da
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:40:01 +0000

    roll bank want hurry extra

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_str(array):
+        return str(array)
