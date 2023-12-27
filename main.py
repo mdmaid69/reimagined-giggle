b712fb35f43936ac327476b4375c54c20c59d3f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:23 +0000

    me shock comment tap tread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def sort_list(lst):
-        return sorted(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
