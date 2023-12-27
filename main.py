cca2c1743083c150f58d1793670b90725aa0735b
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:05:48 +0000

    hungry amount with bye interested

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_item(array, i):
-        return array[i]
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
