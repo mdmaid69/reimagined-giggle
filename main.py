e8bdad37cedcd695566a65dfaf97f7c9a818e4c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:49:07 +0000

    early actual generally shock generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
