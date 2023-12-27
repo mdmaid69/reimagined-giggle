89ddcae8cc232764144b6bdb6353940cdc7bcc88
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:56:47 +0000

    plenty unfair fact amount heart

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
