9735a557453c09162cee53f8f98f507a3a105be0
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:22:42 +0000

    come wide dangerous hair meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
