07a7102bfe1b477f9825f830d2883b5af59494f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:16:21 +0000

    transport brown height emergency expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
