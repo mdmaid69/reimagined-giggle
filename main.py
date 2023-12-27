bf95a8622672b63cb279c706f0d17dbc8b7a90b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:28:11 +0000

    sentence minimum think play carry

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def square_number(x):
+        return x**2
