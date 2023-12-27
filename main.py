5b120674f583d6e940d66db3af6c004f49ef65c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:15:02 +0000

    wheel seriously choose bye other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def add_numbers(a, b):
+        return a + b
