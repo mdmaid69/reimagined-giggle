f8e701b2667faedacf60376c639b8585a80b3df3
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:18:30 +0000

    hurt claim rain last personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
