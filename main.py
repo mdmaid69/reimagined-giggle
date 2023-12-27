38dc106c4406214a44ef10790df841db6d1afeac
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:10:32 +0000

    price major habit bringdown though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
