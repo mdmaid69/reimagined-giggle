ae2a948801a83ca28b4f3392a1ed967f5c1f11e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:19:45 +0000

    business behaviour thought oil report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
