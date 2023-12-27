0a102412506b29399989b17e70b947c5cc10fe54
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:58:22 +0000

    attractive turnover flight arrive attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_current_directory():
+        return os.getcwd()
