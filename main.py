bd42599d36376f6752e0c1a3a452ba40816c78af
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:20:27 +0000

    pretty identify show beautiful ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
