b06f1a60bfa84e065a2a3660f4593ea045b29470
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:02:50 +0000

    accommodation reliable house interesting child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
