b30f8d00f9ebe7653d7a4b0066f588f6255ef244
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:26:15 +0000

    injury cheek justice exhibition once

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
