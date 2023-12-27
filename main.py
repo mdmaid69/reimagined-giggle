9781ea77e8bfb01fd186abd1ed16790e57da5c44
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:57:46 +0000

    mistake priority relax comein sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def find_max(lst):
-        return max(lst)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
