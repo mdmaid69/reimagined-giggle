1e89a7f19442cdbfc173945290882d36e31886aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:35:13 +0000

    bang holdup exchange straight myself

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
