7df0c378d8b32b7a6f92cd1837129680388902c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:51:51 +0000

    store convenient average shout square

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-print([x**2 for x in range(10)])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
