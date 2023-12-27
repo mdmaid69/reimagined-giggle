14fc89d212c8f581ce5104fc1c167099425ccab1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:18:42 +0000

    inside distance different separate before

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def square_number(x):
-        return x**2
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
