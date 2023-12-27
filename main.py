d5ccd577db846829df78264ca7980e73de20a2ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:48:38 +0000

    nigga wing heart prepare key

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def sort_list(lst):
+        return sorted(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
