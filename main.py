924f010b10242bae72027938ad5cfd8fa122cb7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:24:48 +0000

    dinner bringdown phone hers male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def get_environment_variable(var):
+        return os.getenv(var)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
