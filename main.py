9f7c17d96fe95349b9eab6f2e9c6c56cfcd648e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:53:21 +0000

    such cold challenge life attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import getpass
+def get_username():
+        return getpass.getuser()
