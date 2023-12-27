3b8dd808d230d01032a4a951a114d44cf9e256ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:57:28 +0000

    573entertainment swallow patient figure confident

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
