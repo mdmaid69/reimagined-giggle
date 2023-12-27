458bf31ff1123751ab664ae77707905a5b4465a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:44:37 +0000

    across getoff ready standard mend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import time
+print(time.time())
