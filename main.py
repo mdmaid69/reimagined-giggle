ab186766297e31bc18bd66bcc9cb91a437f738b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:46 +0000

    touch bus unfortunate interview under

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
