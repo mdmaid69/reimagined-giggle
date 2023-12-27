4446a8bcc68d8e471fda42ed35ad2c0665f86f01
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:18:59 +0000

    secret gentle objection chin feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import numpy as np
+print(np.array([1, 2, 3]))
