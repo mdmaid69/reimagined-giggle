f2ba4b49038758537aa60b638de4c909a88f704b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:44:42 +0000

    interfere error sorry speed run

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
