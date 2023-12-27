4542090da6a49a5519dc86f1ce7bfcc0dc4a368f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:27:07 +0000

    sit operation software market exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
