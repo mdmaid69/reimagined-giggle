5bab2ed48ddf79bf8f036edf6755b29e64ff24a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:51:13 +0000

    over miss affect person hurt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
