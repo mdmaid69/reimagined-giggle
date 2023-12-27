3bf21cae4b29362ebd9679d861df49d2dba84643
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:19:37 +0000

    save cause practice calm satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_item(array, i):
+        return array[i]
