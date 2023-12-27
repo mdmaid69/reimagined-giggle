432972cd9e4d5d71fd0c2d6063681b12b04769eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:49:45 +0000

    concentrate print punish hit transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_factorial(n):
+        return math.factorial(n)
