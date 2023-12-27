7ac756cf2ce5bc1659c0b812a913c4ffb7043809
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:36:59 +0000

    squeeze tall easy love proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
