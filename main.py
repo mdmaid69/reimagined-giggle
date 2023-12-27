0a1d525553d87e9cbbaa6795ffe6e7afb239e2e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:05:15 +0000

    corner duty upset shrink look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
