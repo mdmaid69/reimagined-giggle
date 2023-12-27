fe906a229b29d3d1a33038a555e7add845925594
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:15:52 +0000

    close distant belong gap determined

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import getpass
+def get_username():
+        return getpass.getuser()
