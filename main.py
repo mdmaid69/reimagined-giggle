ab5b6b2de748ab98ad5700e5dead90d57a0435f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:14:14 +0000

    undo unlucky debt plate dull

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_area_rectangle(l, w):
+        return l * w
