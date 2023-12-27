0e202e878bef2372b07ec197baf83402dcbb2dbf
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:00:43 +0000

    wonder bad mail reply clever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
