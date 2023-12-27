9b4458a8b2afe5b79265cea55a3c95ef2211f4c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:48:40 +0000

    formal wipe please hardly capital

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
