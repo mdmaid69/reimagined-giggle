3737b76ad0aa2812345f2647c0676a809f7abcba
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:01:37 +0000

    search amount fashion defeat frighten

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
