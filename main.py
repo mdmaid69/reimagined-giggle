b19b6f7647f0f9bf6683fde8f5b7ecebc5f99b80
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:26:54 +0000

    precise wing late turninto door

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_max(lst):
-        return max(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+for i in range(10): print(i)
