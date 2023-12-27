96d43fa58e54a59e287665ba6ff28b7ab1eec274
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:55:44 +0000

    small help crime street pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
