9cb45c2e4db8a442e67f1056eb10076712bad07f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:39:54 +0000

    swallow pass drink recover you

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
