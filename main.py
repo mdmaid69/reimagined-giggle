459e388c5cc2a023c3427f44e2d8aa2f367fd638
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:16:46 +0000

    dream sad strict to guess

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+print(sum(range(10)))
+  def sort_list(lst):
+        return sorted(lst)
