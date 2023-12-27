435d097e7c6ef40b72a5f46b6d36effd031ef81e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:59:08 +0000

    negative unite file chief maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        return sys.version
