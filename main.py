24463b96381fcb464fd7fa52e8741debb1c65020
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:04:29 +0000

    start accidentally record practice decide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
