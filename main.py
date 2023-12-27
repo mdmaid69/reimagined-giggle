29f8644cc2d7e30785180012864c80362c4ff50c
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:03:03 +0000

    back port kind used waste

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
