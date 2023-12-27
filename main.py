f73dc506ce66fb2acc79e6c5ba537208353c762f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:54:59 +0000

    but drop threaten close Rr

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
