d831b457a53083bfe46d3bb93e602f4083b89272
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:59:59 +0000

    turn order smart plastic centre

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
