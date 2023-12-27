91381bdffacd3872ef258306da2418cb872be525
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:26:20 +0000

    rarely pity rush mysterious still

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
