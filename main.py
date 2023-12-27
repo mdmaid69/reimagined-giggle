18169c1c66d15d3c7b88ec7571a28ce5e4e9465c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:58:26 +0000

    spring answer detail your feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
