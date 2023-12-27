f7bc023eb184911bbb2e7f14c1c0ce07152aa5d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:03:32 +0000

    trust by ashamed coat tune

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def reverse_string(s):
+        return s[::-1]
