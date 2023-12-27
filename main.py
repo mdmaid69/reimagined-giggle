63307020dea27727d5e40a7df4954685e147af63
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:58:38 +0000

    shout godown under lose fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import re
+print(re.match("h.*o", "hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
