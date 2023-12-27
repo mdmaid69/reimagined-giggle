c7c730b6846204ddc9cc1fa2e8a1875306d6838b
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:51:56 +0000

    care possession careless laugh discover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
