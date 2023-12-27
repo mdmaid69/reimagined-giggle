a6b053910b0da246202413b96f8525362248a35d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:00:24 +0000

    bath people Rr everybody dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
 import urllib.request
 def download_file(url, filename):
         urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
