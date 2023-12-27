a649a7c4496303bd4f6d27fb669bd59944dc174c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:23:07 +0000

    quickly backwards threaten anywayanyhow surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
