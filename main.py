fa349aad3ad0e01af0e58b9a8d83ecc05e0c8586
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:43:22 +0000

    callup replace least evidence while

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def is_odd(n):
-        return n % 2 != 0
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
