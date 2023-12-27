e41d9e8dcca5f887fcbfb3101cc8eb575f6f7182
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:31:35 +0000

    experience contrast lean constantly house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
