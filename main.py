4fe52292077c144d08e12828fbaa60128c2c2bc1
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:07:34 +0000

    perhaps relation exaggerate rely instant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
