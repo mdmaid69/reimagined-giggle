d442a736eb50f7b19f604cbd04dfb4e5efb93461
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:40:37 +0000

    insist accordingto how allow nurse

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_current_working_directory():
+        return os.getcwd()
