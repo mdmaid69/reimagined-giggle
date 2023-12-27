c946f931fafbfcb505e8e39847a084355fea818d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:39:04 +0000

    party insist design eat support

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def is_odd(n):
+        return n % 2 != 0
