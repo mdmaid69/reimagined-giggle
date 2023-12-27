13da0955c78baf2c376caa0ae491f27c9abebbf9
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:17:12 +0000

    present interruption contact calmly embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
