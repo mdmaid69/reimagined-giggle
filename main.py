a804ce740e6ec745a4253b4068beea089a04d871
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:01:55 +0000

    birth sorry effect name them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
