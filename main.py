e1fd004b22e934c6612148286ae07e53bd523cec
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:01:35 +0000

    common basic thinkover comeround pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
