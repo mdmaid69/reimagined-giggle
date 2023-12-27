fe664f1af179fdbce29ff150edc6bea1bfe722b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:27:52 +0000

    left confident hold radio case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def convert_to_binary(n):
+        return bin(n)
