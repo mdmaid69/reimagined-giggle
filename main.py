97e281f600182167c5beb425dd24668fffbfcfdb
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:58:17 +0000

    future reserve noone prepare correct

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def log_message(message):
+        logging.info(message)
