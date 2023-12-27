f01d9e8e70aaaa0e04fe97778d83955fea130328
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:35:37 +0000

    backwards attempt heat content free

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
