3cba2004caa34735ff2ea516b001284c5c4fdb9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:11:19 +0000

    children cry size carry spare

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
 def factorial(n):
         if n == 0:
         return 1
         else:
         return n * factorial(n-1)
+def multiply_numbers(x, y):
+        return x * y
