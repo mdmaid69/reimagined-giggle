b9836431e5cad725198e2ed2f4e529125e3939ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:17:57 +0000

    ourselves back grade contain contain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def print_python_version():
+        return sys.version
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
