e9106d10a62cedd6f279bbb38870672ba5ed7e1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:44:41 +0000

    race goback(to) buy purpose contain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def convert_to_octal(n):
+        return oct(n)
