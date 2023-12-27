049e789cb1cc59dde30dee5b459b6651062ebac6
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:20:20 +0000

    float right ground might protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
