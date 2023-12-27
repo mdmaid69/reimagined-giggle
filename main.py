7c5452d42e496c703ec03cee579e54c67857adaf
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:07:03 +0000

    fortunately too song best judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
