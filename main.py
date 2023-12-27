c84d0effcbc82fca24e2faa03b019ef4ecd3d50a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:04:28 +0000

    confuse frightened decision production pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
