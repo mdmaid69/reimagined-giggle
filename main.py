07fe43455df4afc88093d1e749e1dc808f3c9dd0
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:07:58 +0000

    reaction who divide in reach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
