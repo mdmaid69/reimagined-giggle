91c8433e75d4cb57803ad80dbcac4aff9d9ba52b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:10:03 +0000

    translate circumstance combination will loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
