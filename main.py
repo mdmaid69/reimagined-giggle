9ee5be4e3f66fda2b62fb33e86bea01ae86b454b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:57:30 +0000

    copy tonight ready takeover report

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
