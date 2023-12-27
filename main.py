835dde048a0007f2c02a1238da97627adfc836ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:10:20 +0000

    book visit speed gap whether

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_unique_words(sentence):
+        return set(sentence.split())
+def add_numbers(a, b):
+        return a + b
