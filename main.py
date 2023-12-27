8376aa491c17b294eac08d611077ead2b5442a49
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:50:08 +0000

    value gentle lunch uponon slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
