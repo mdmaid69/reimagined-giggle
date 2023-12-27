2ff33c8952e4638856c3cd8d79cf9c0eef60bd55
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:20:21 +0000

    float mark bang hurt suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
