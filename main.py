4912f3ebfeefded178124f5e592083307ea19b2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:34:09 +0000

    schedule tomorrow Zz return underneath

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
