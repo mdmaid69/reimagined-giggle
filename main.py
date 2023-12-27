6a97fd656ab7f2698ef715401e377635ec5e6a88
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:02:14 +0000

    grade bunch left 565end hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
