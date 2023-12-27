cade624d235b14d4d0bd251001d02ed37d6f3349
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:57:31 +0000

    hello steam straight excited chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
