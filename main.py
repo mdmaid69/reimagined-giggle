6afc4504d5597e5151f2f7ea3aead45f2d6d2bcf
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:16:11 +0000

    use regularly exercise purpose car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
