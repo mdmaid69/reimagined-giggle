c46003990aed0ae0cb0375ab834367eaf146e1fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:11:37 +0000

    get skill suck mass square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
