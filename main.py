4516657aae0103d3093e572078f765b9f0feeadc
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:13:15 +0000

    approve survive fire uniform outside

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_pressure(force, area):
+        return force / area
