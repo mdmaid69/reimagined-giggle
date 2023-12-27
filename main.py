ec5d617169a48f2a2b4b5241b1ff18dde3806d56
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:15:46 +0000

    century safety market disturbing value

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
