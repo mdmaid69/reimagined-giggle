527b0b4e802a0cdce379b7ab98233b0e428947ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:20:47 +0000

    down that journey finger mixture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_palindrome(s):
+        return s == s[::-1]
