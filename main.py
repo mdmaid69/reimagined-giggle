aed0825051e40a72af42921a3f33d6446e8f5c3d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:30:18 +0000

    second without leave brush fierce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
