ee350eb8b2822a031a55f5015520172d3a07f968
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:00:10 +0000

    pattern hardly switch declare Qq

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import random
-print(random.randint(0, 100))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
