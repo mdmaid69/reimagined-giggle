a739d6cb8820a4f924da1fc5b3c0c186f3549ef5
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:59:08 +0000

    sometimes only thumb type certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import random
+print(random.randint(0, 100))
+import os
+def get_current_working_directory():
+        return os.getcwd()
