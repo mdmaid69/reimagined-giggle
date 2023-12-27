2acdc69ab34203d50fa8a3a1b3674db55f606228
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:07:58 +0000

    transport doctor tough worse beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
