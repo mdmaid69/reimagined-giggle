c9f2509e86bf24178897caac0342fdd6b30507bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:53:08 +0000

    opposite forgive runaway culture dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
