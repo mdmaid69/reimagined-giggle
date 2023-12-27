1796867e6cbdaa267153874fdd8d7ca7d20cdc22
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:17:58 +0000

    part fresh actual else once

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def convert_to_octal(n):
+        return oct(n)
+  def convert_to_binary(n):
+        return bin(n)
