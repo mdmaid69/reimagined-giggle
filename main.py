95a56fe5e69d3ab91c55097b25985910eab53e31
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:06:54 +0000

    uncertain single half strange seem

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_volume(length, width, height):
+        return length * width * height
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
