75e20dda528424d6c2ba8c2823721ac56dc0c7ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:16:31 +0000

    takeback operate division stand turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
