2e587ad55ed8574ecaa911d543f267c676c41deb
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:26:33 +0000

    design please comeout plenty half

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def add_numbers(x, y):
-        return x + y
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
