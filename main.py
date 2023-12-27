c56007addcd4a040ba15999d31649463c7512473
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:25:22 +0000

    rain turnoff sister everything perfect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
