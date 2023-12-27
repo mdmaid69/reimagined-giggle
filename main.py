72a4e892aae20c7bd26dacdaa5a042d9898a1d37
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:50:58 +0000

    several bound relief opinion light

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
