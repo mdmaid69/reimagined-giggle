8fd3c6f666d3f80da146aad0e679f0b054ff255d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:15:34 +0000

    there succeed send thing confident

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def sort_list(lst):
-        return sorted(lst)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
