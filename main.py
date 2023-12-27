cca7847d64bf46792341335e8c3dfbf487d1b4cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:35:46 +0000

    justice move order basic tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
