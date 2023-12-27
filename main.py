556016a95786583055db4f21c8aebc8f60cafde5
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:58:57 +0000

    Ii wander improvement should service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_speed(distance, time):
-        return distance / time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def remove_duplicates(lst):
+        return list(set(lst))
