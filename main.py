d579d2d247706820a443f36ce23c95e29c3bdce9
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:16:09 +0000

    Kk goout guide inside drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_power(work, time):
+        return work / time
