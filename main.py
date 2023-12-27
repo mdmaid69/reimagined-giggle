8b1ddff51e0b0ffac4256d6f0ec2bb04cd39530a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:11:05 +0000

    pretty hurry remark soil forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import getpass
+def get_username():
+        return getpass.getuser()
+import time
+print(time.time())
