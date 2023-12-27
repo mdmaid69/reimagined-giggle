9e261164b2da95ffbbf6ef622b592b0899dc784c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:35:52 +0000

    getaway unit earth test line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
