75fbb1a684905696adf4b64c12a78647a95b37c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:45:05 +0000

    limit help fix interview bend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
