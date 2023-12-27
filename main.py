ebd1ed6a0490fba155842c82c058a2d3e78c7dec
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:25:58 +0000

    carry challenge difference explanation kind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
