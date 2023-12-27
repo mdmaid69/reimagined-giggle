b221bde33d1b623229bf07a1fe38f6de4bff8b2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:32:00 +0000

    practical drink heart brown rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
