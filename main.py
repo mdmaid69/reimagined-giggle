48207fc24ebf05b589c959064e60fe74c9a76e00
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:08:35 +0000

    handle evening touch regional surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
