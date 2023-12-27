02fd5dade5375e507dcf27e87fbaeded7d77622b
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:29:20 +0000

    also straight little difficulty easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-for i in range(5):
-        print(i)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
