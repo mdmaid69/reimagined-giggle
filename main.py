c8716f022083097c383bed625a67ee9a7691f433
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:28:43 +0000

    accidentally collect mass attitude ago

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def split_path(path):
+        return os.path.split(path)
