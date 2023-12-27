5faa6c8b5bafd662eb278eaf03b9f94eefbdce6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:39:29 +0000

    bringup admire large wash home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def find_min(numbers):
+        return min(numbers)
