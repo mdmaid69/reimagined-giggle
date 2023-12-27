ea987732083d4d8be7fdcaf97bf1064c898aeafb
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:40:55 +0000

    dig write takeout gift centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
