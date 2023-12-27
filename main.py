31a65c51e77fd1ea42990829ca5c14f1c1848968
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:12:23 +0000

    standard present occur heart trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
