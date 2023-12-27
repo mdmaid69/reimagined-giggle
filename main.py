24ab19d291aee7d68ddf9b0daf4dcad96cc525f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:39:15 +0000

    market underneath phone tough muscle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+print(math.pi)
