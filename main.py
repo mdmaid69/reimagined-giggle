f47bd3997797caa9f355c5c803d0c20d656d1bad
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:33:43 +0000

    pump cope refuse happy force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
