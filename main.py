ba8e3298475e90234c005fd158902d9aa87a68b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:32:13 +0000

    wide opponent method doctor lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
