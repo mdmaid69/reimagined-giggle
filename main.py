19a303dba00193c5ea6c11f8d777f0c1b9edb797
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:49:33 +0000

    getoff beautiful comeoff attempt still

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
