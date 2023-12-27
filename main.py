1946972d2a3966520a05fec4df0ba3f680b64a24
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:29:23 +0000

    immediate certain target makeup refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
