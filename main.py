1b67c50a043c1debcc09ba5a88b74a6fde4a93ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:23:14 +0000

    well edge break possible know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
