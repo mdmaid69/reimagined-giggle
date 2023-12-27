2f8aae12c3ea7cd3c808e5647cd61235e03b0925
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:37:12 +0000

    track order priority shrink protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
