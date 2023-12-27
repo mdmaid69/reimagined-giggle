4b47af523d6e1fde7cab9cc0f40af4557307bc74
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:42:06 +0000

    admire unhappy can want sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
