26e0ac819d0086c299c7ddbfb204a910a5dc8373
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:14:19 +0000

    politics Kk opportunity hold print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_tangent(x):
+        return math.tan(x)
