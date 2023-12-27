2b23ae9ce6be885d6f1884ca20aad960e2ef3462
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:37:29 +0000

    as floor load heat drive

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_speed(distance, time):
+        return distance / time
