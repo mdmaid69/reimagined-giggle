f4b3639ca0866deaade9be1c0afaf8b59aa16b4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:23:33 +0000

    future plenty cheap tend argument

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def square_number(x):
-        return x**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
