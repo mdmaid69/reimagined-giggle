5ae78e031cd23afb5c2a4f1ea2a6b7f3f6033bd2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:11:50 +0000

    right or speech culture completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
