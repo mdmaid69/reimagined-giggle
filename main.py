377d03ddab79003ef0756e52be8f5ca31525e950
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:43:06 +0000

    shade talkback mail firm sick

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
