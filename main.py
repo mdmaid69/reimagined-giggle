78cdb0951b303b56dd681ed8a3df16615903de1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:31:16 +0000

    Oo excited read completely anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def convert_to_binary(n):
+        return bin(n)
+import time
+def get_current_time():
+        return time.time()
