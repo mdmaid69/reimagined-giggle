90b8b2c74f34d37fd0718d58d7802b25b903ad4d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:39:54 +0000

    after bath fortunately goout minimum

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_area_rectangle(l, w):
+        return l * w
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
