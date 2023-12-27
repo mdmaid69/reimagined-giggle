9364db4fe3085e764ffcf4d6e9d1f6355cabdc7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:32:40 +0000

    earn problem correct heart recognize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
