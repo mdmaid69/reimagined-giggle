d90f318133d7665fbd1a1afd97f42072bad5023e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:20:55 +0000

    pen take forever forget anywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
