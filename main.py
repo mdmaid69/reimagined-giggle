69a223de7b3dce0e9a6dd2d06c629d93cde579ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:52:20 +0000

    attract open goon involve past

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_power(work, time):
+        return work / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
