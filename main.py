c65362166f36c8cc7ee23e2b172a19233b73348f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:43:31 +0000

    centimetre pollution interest centre length

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def delete_file(file_name):
+        os.remove(file_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
