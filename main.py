3201ab0881376b469ea7df0d1ceca3ee7d16f9e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:09:12 +0000

    nervous bar as programme confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
