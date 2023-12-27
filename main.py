3a1d7ff6fd4b56b5b94cbc84b471a410049f6da3
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:38:52 +0000

    welcome form dangerous giveout experience

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
