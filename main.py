0a6c9b46abd7988cfeee354802283c24b27cc4a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:16:21 +0000

    niggaacademy phone empty pride save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
