ac49f1486370eeb61e0fac508f61f6edaf6392f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:06:21 +0000

    hurt peaceful low owe find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def convert_to_hex(n):
+        return hex(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
