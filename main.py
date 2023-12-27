fc8653567e86afdb769f84860beee67d9297248b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:05:47 +0000

    difference will justice dream excuse

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def square_number(x):
+        return x**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
