3f05172095cac7f7946d5eb35db05271acd32cf8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:35:35 +0000

    vote contain object confuse big

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
