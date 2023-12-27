8f1ec28137942ffb9ca1f279ff57acd40dc0e9b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:53:02 +0000

    switch trade treatment control long

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+x = 10
+y = 20
+print("Sum:", x + y)
