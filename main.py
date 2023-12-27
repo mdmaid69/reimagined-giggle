4fc006a279725c1884b722d67e92e26164c12ee3
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:31:46 +0000

    rain during operation getout evidence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
