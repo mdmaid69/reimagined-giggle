8dd3efc6d0d9e747afa6744dfef88207ecb577a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:17:22 +0000

    permission fast change fetch busy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print(sum(range(10)))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
