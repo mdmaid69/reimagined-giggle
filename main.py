13ef22af86dd21b2d88136c4f8daac091b132476
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:50:53 +0000

    unpleasant careless bit certain pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
