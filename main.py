73aa8c7892209c847826f8bb534db4da50297ef1
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:54:01 +0000

    relationship badly passenger thankyou fortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
