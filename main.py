7d70eee0847c1d36dd30c8ab5bf2755aa513291c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:56:40 +0000

    exploration break somewhere comeround extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
-import array
-def clear_array(array):
-        array *= 0
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
