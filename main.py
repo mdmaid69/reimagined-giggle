a2e79464c9ca6246ecea5d95daeac3e505534d48
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:28:20 +0000

    do influence equal chase along

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_max(numbers):
+        return max(numbers)
