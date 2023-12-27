a11121265b94a775ee3d19aa01b27fe0089142f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:44:34 +0000

    single black offence opponent duty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+for i in range(10): print(i)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
