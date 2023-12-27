7bc32bfe313cbba1b0e89f48716438bbefae1a36
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:59:31 +0000

    Vv comeout breathe empty sensible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def count_characters(sentence):
+        return len(sentence)
