b0693d108e2db26d511b4feeb444c0c6d4c137ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:24:40 +0000

    understand autumn factory loose spite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import time
+print(time.time())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
