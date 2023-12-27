bdefcba631891ac0879104fff29d27ab7c28a51d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:56:24 +0000

    convince confident same reliable plate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def remove_directory(path):
+        os.rmdir(path)
