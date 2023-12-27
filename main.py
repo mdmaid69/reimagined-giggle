0b5bba503f6fb8fd527ab7073b3e569dfb197943
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:05:30 +0000

    transport while basically rather company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_set(array):
-        return set(array)
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
