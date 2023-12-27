dc1a47e3f6660781e7d30274c7f383e935c7c1f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:41:38 +0000

    meeting runout hurry question bin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def reverse_array(array):
-        array.reverse()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
