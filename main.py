ce3ee706b730a1bbdebc71adea3eee63ed33b7b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:03:04 +0000

    crime demand sea room 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
