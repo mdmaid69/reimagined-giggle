00d8efe2fcd717dc554f7f77b94d607f8adbce3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:55:48 +0000

    could draw mistake interruption again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
