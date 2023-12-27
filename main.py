71d8bb89953e3b9de791f15cf88aff0da4d95687
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:13:16 +0000

    strict school miss school check

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def find_max(lst):
+        return max(lst)
