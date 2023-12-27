253f106f21a649d51c0addc11df213084966a2df
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:25:05 +0000

    appeal excuse Ss ring use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
