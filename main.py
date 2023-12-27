71c06e1309c7cbb80b4aaa9890e7400e97316312
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:58:14 +0000

    press measure guide prize justice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
