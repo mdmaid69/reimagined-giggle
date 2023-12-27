8122bb2560682335324164daed2feb7582fdc563
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:11:14 +0000

    reply tooth bringforward determined concentrate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_acceleration(speed, time):
+        return speed / time
