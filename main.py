5ddb785153442a54d1929fe4b9187669bb5e64ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:06:56 +0000

    whenever weak world extremely dinner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
