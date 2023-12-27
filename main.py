dc492916e300a4c7f37afba86515fc114535151d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:06:26 +0000

    lookup age answer read heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
