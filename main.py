8caa9c54b0479a4a63995dc4d4b140e6d6908c2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:49:53 +0000

    pity pipe shout cry gate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
