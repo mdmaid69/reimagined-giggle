f4784e81513d3a99344fc503389239bb259d7918
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:54:20 +0000

    object hand deserve congratulations convincing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def is_odd(n):
-        return n % 2 != 0
+def calculate_work(force, distance):
+        return force * distance
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
