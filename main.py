36b870ea19b7c2b9471f29b1dd453717d333fea6
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:06:54 +0000

    exchange total stupid shop attractive

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def subtract_numbers(x, y):
+        return x - y
