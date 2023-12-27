799c1859addcf7f49de89d8bdd0f827e86b93a57
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:17:28 +0000

    start comeback arrive mother appear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def subtract_numbers(x, y):
+        return x - y
