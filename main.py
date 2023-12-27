54db19c4dc0b6edc8dd0b8d080ef31a59ccd1abf
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:39:11 +0000

    total floor city else post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
