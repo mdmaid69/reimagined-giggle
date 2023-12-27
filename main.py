98dd6daf2aef4a77fcdc34d4b085e5faed2a7f28
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:26:10 +0000

    taste chin conversation course design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
