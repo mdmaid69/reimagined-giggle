3b646432e38df2beb3503faa36744049800c4d0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:59:16 +0000

    conclusion attitude swallow hand train

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
