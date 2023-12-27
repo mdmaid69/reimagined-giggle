d174d5b74dbf863173b686f5e525245560091ad7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:19:37 +0000

    print runover himself gap wing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-print(os.getcwd())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
