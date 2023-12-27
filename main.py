a901739149ae15f0eb2c71a1eb48ae4f8dcb1b67
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:06:53 +0000

    alone fetch anyone show compare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+print(os.getcwd())
+def convert_to_hex(n):
+        return hex(n)
