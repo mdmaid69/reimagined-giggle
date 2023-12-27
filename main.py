b05845b0d7defdc4e0b94022816177c3f96d6bd4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:55:25 +0000

    foreign ball tie fast base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
