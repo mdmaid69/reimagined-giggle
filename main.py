1eae65114e147ac645561e508d61d70b812b8abd
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:36:15 +0000

    night schedule deal so learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
