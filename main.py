de9e7901de6d15803baabd646ca08405efadfc17
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:47:11 +0000

    disturbing exchange independent makeinto fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
