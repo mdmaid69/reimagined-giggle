e04b1d723cf76afb68380ae6897b004930fa2d77
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:43:25 +0000

    operate evening sing 572enter joke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
