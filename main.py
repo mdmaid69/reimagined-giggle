18412e5e8d3971795cd37ee33f58b5e095247cee
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:38 +0000

    punishment negative turninto feature rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def convert_to_binary(n):
+        return bin(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
