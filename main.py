6e94d01e927b368837f9cc754c36d9ddb09b517a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:36:13 +0000

    truth bringforward perform paper intend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import getpass
+def get_username():
+        return getpass.getuser()
