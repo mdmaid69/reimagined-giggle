cae43c4d3de7c1f3986c294f31d36991ca8dd546
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:43:42 +0000

    extra plan Vv satisfied flood

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def find_min(lst):
-        return min(lst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
