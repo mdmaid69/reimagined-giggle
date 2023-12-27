c19375602f56a1b28e530cfefa4b13ce073e00b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:22:29 +0000

    age bit them receive work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def append_to_array(array, item):
+        array.append(item)
