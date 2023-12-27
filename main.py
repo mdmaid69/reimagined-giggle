6a312ebbb20f22bb0ec9d0a838c8f7964f9cbfff
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:02:22 +0000

    match trouble judge large manage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area_circle(r):
+        return 3.14 * r**2
