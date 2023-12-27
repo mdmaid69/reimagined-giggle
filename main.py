9775a4733a586ae5abbb59a765dee80826ae32f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:31:24 +0000

    release which sleep difficulty unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
