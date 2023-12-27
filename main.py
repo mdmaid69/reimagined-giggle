1cf29bb50a33bf1313d344672846be18a86edaef
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:42:35 +0000

    mend song aim yes disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import time
+print(time.time())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
