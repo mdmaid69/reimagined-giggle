c90a057b59d89fbdd74fa0dbaa7f69588816d365
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:43:03 +0000

    mean table drop operation always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
