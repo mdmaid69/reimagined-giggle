b20c639ef016e3cbb4373c8b43bd4907d7910059
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:12:25 +0000

    point waste classroom attitude tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
