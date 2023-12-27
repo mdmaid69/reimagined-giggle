5fc2560f956ea144e0514762cf2de3fa9354b061
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:03:38 +0000

    edge feature weak group pile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(10): print(i)
