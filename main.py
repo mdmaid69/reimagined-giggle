413fedcd0565a6248462c43efc656ec6c8782c18
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:32:36 +0000

    main Ee meanwhile male Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
