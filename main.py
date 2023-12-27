1f1cd5fed2c796e3616801a5c2667842b32d2c2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:15:30 +0000

    argument busy yet process crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
