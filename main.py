4ed4da54289bd86d1b8baa795776a91e26b140d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:27:24 +0000

    passage very space copy advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_str(array):
+        return str(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
