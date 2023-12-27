5a6640f5a057272baa3a465c04209ae50d0a5184
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:00:36 +0000

    error give thorough hold light

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
