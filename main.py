de722922fba2a37338af6bc70fdf94f4b3212331
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:48:36 +0000

    kind stare slice runinto know

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
