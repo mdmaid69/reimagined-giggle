e960cd8d286011c0db005ab4da77637e4b622443
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:20:50 +0000

    operation gate mass union each

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
