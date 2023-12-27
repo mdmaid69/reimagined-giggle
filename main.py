12c560e334fbe486314bf7ac02a82a8b7c28f827
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:09:38 +0000

    game stone avoid fortunate awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
