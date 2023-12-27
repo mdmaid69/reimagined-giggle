ccd8ae4648701c5c9939ead900014d8871cb1fc2
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:33:37 +0000

    mistake surprised today but people

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def cube_number(x):
-        return x**3
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
