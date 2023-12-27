8c09a6899b01cc6a8dbb3f4af2c06d455809c314
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:34:16 +0000

    boil comedown soon takeoff lonely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
