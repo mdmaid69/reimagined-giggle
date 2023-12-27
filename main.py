5bcffde8b736998049f62f4e96abefe2628dcaa5
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:12:36 +0000

    collect complete loss scale some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
