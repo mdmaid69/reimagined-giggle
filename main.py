0445a9fcdb8fe5fd666080ca16b0c535083721d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:28:03 +0000

    regularly passenger her test copy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def reverse_list(lst):
+        return lst[::-1]
