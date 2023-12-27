76c9cf1332581773c7545a1b812848cb86559501
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:44:52 +0000

    runout coffee only shame offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
