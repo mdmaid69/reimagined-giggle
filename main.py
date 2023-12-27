75ad0a851a4a0bacf2ecda01f887e00c29d6fb32
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:10:05 +0000

    black hungry task geton through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def set_array_item(array, i, item):
+        array[i] = item
