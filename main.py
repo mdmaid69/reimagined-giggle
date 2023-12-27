297a51c64baa34c2c09f3e5c36131a43e763c487
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:23:55 +0000

    hour Mm disappointed day broad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
