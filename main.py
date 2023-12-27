3371bb0bb8db949cb34a549d3184b6c3edaf63c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:23:36 +0000

    practical even health every within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_list(array):
+        return list(array)
