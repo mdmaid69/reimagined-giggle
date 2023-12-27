400971c7e286bfb30e7e5333ffebfba391df2335
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:21:45 +0000

    worst course recent rent factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def find_min(numbers):
+        return min(numbers)
