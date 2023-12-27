c213571925b80e43acaad9fbd189e4ce062e87fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:19:17 +0000

    keepfrom plant rather them press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+  def is_even(n):
+        return n % 2 == 0
