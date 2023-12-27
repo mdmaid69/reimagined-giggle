2eb4772c0eda0c781c6021c1622b188fda9aef0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:19:30 +0000

    one mad mess pressure shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
