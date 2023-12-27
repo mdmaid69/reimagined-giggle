7a48939c933ce87504e4d4a37f89677f1aaa9b6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:27:40 +0000

    professional pupil float upset autumn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
