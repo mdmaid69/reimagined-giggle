51938cbf8e59ba612c43eb68d9bf4ace4511d249
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:33:52 +0000

    pride basis temperature today generous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
