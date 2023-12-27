350133756aea0a538865d45cc247307a61398070
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:00:50 +0000

    kind speed cut each coat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
