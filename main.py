6a4d89af4cdbe87be8fa636e0aa52f8062962276
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:52:31 +0000

    allow exam turnback popular average

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(numbers):
+        return max(numbers)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
