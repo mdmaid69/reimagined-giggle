6448c2b6f711dd17a8e1346841e5a7506ee3f53f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:10:19 +0000

    mother prevent recovery confusing bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
