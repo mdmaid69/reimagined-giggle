00be5bc382f10b9af51b4c619017def999442859
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:00:50 +0000

    giveup body above bring delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
