c1f961a236714f1377b4fca0ee37259cac6a151d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:51:24 +0000

    welcome most properly across quick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
