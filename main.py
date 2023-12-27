42939389e547aa64dca3d976c3f8664337fbfd03
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:29:45 +0000

    move join turnaway gate operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
