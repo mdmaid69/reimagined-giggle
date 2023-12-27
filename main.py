e5c08e908190c3a7b5899fb09ab9a4e89ab0b30a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:20:02 +0000

    whoever key tape lie let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
