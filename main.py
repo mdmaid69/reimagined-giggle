214ab7f9e5a5a1ce6434910077c7274172766e61
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:32:28 +0000

    size mind relationship correct fashion

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
