cd765fe3e545b613062536e3d5ea228e7fd2f4c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:35:53 +0000

    summer turnon talkinto inquire more

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
