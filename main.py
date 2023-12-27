10296ae947a15e37ba366bff796406ba62368f31
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:56:19 +0000

    blind straight clue confident first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
