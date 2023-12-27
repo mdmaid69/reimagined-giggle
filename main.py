6a49bcf5bb48adaff5d40022107bf62d4c34ab65
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:52:12 +0000

    treat satisfactory warning business bother

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
