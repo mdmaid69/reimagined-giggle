a972b924004fd5582882db5785cdced4af210ae1
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:36:56 +0000

    public slide stay behind ours

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def multiply_numbers(x, y):
+        return x * y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
