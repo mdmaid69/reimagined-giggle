ff9e0a08cc74113809b0bd204b6d9fa7777b8b03
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:55:24 +0000

    hope number blow often begin

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
