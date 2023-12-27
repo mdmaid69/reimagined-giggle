9dd7d332d5566d367a7b411edf8693fadd133d77
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:49:49 +0000

    long by stretch turnon programme

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
