7c144c37fe13e68d00b0c0432115ae844bc85124
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:53:45 +0000

    everywhere pause mistake no gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
