f36f952d6c5a88c390e8e7349dda28f2c8704c8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:17:06 +0000

    respect soil sink near you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def is_odd(n):
+        return n % 2 != 0
