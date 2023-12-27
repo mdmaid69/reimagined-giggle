e0dc45a794e0961714f8429764ff0d69e2dd3882
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:31:44 +0000

    convince stare answer height nerve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
