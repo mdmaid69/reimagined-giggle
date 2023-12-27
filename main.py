e12ada7cde26423cb9911d1b7d7f5dc321c91e7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:34:57 +0000

    passage question trial correction maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
