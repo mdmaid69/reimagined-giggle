22d9b364bc44afd0260310a362023c90ed8365a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:14:17 +0000

    flat cause church godown ilovedicks

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import logging
+def log_message(message):
+        logging.info(message)
