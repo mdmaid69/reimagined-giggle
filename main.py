06a1091b417b06999c948630c77084364da977e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:38:23 +0000

    regret my class throw drop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
