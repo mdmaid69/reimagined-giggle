6fa271f58de5243e93cb23d50f5da739e8d853f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:01:06 +0000

    tune but find put success

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_cosine(x):
+        return math.cos(x)
