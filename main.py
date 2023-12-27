0fc47473a95672d95384350b482fe0d6b95ba52b
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:44:02 +0000

    catch bath calloff empty importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def add_numbers(a, b):
+        return a + b
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
