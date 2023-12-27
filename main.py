5808fcc2907384b012ef7eb20aa69e55f2a570b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:04:46 +0000

    next second yourself catch touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
