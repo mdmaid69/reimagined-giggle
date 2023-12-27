d1ceb1614b39c95454805246232edd3e86ef441f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:15:06 +0000

    stare charge bang drink room

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
