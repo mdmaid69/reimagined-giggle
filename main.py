1641159c6fe6e9320f6ff53437955cd98cb3e496
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:05:44 +0000

    emergency to mark preparation common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Characters:", len(text))
