3ce01c422b4e300c6e90e1b5d6bf5194ef1b9044
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:45:27 +0000

    university patience twist have dull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_acceleration(speed, time):
+        return speed / time
