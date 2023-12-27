1daf1f8466f55486163fe8685bde98f404c523eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:43:55 +0000

    region surprised organize congratulations surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import collections
-def create_user_string():
-        return collections.UserString()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
