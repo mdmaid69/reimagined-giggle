dfdf0db0b8d722a73d99fa6a053dcb715b386ff0
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:02:31 +0000

    city figure when best bank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def count_words(sentence):
-        return len(sentence.split())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
