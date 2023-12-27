4f49c335f001fc39204c475c1b58eb92ed52eb1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:10:06 +0000

    Uu develop sensitive nose loose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
