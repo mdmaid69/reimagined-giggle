dd901d0b035898bb5f1f53f8c57c327cd72d3f4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:45:22 +0000

    summer promise either boring question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
