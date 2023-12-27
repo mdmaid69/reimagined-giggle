65be9338cd7fc785724a66b512361dea3a307fdd
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:54:21 +0000

    corner peaceful hurt turnover all

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def cube_number(x):
+        return x**3
