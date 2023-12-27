f568be130c27eae0d7045a9d6864cbe557ba46be
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:08:07 +0000

    only dream month step finish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
