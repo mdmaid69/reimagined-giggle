4576abfa13a117d951c3c1bfd623897e1718d16d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:40:32 +0000

    impression crash eat recognize see

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
