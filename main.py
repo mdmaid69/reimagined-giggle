c4ef2a698f8ef0b315c28c5dcc82d66ca8edeea0
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:46:46 +0000

    tall shrink below unusual ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
