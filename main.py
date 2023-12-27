c323724e1b18d39079592eef3ab3d7f8ba27f020
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:57:25 +0000

    everywhere steady damage single issue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
