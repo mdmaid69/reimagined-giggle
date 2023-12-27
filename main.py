d2ffb82210991056a14bf87e681d2ef1a5e192b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:27:09 +0000

    necessary brief average wash narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
