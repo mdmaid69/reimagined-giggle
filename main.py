5e1dc316285e9e10ed689a316fa86f8ad9dbccc6
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:49:35 +0000

    hold movement work taste watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
