ed23cd89eccad870351705d38d2cf648de885730
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:56:06 +0000

    black gas complaint mess lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
