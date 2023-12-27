05b35fa795b217012ff15c82267193145ecaee33
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:18:20 +0000

    claim arrange emotion geton bus

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
