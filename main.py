7f0d55034399fd74e2450137b09f1e32316151ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:50:41 +0000

    our turndown jump figure speech

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
