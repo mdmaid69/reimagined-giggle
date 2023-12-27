9899bf65542cd8b7f8cf6b36ea4b5bb014012318
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:17:57 +0000

    share like sweep will influence

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def exit_program():
-        sys.exit()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
