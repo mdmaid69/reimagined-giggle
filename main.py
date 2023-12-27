db54e19f0fccbf77536deb173a63c7a55f71d68b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:50:08 +0000

    not habit keepoff suppose violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
