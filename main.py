c2e631da6025d5d2ba54b1168df7f0d9e60de4ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:10:36 +0000

    along surprise greeting receive accommodation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
