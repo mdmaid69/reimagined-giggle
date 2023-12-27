3c9247ae1f9937d5fcf692e9e1fa3da6e8950996
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:53:47 +0000

    attractive finger beside carry depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
