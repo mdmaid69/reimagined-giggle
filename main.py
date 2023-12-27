bc2f7b3f3419a21a6db6a03f45cfa70871c590fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:27:09 +0000

    tip hide sport environment so

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
