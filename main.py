752c476e4e152381e37b57c43b55837cc507820a
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:51:50 +0000

    solve improvement will today comedown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def find_min(numbers):
+        return min(numbers)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
