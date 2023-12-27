dc89e30d816eaa1a0712f588f6bc7a142b89d9a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:46:52 +0000

    comeup wire workupto myself cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
