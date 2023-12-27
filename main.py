e585b71d5fca0a0bb755cafaf1aaf9f224ad3711
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:33:13 +0000

    bang step punish peaceful paper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
