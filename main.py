05a775aeb2c920c7f45f8a6d15823fdc98e70190
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:15:29 +0000

    thought large post design fast

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
