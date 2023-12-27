bf67a9797eb191544bd719fd668fe0b95692e1c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:12:08 +0000

    throw expression under outside scene

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
