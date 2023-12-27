f3649bc36fc5ce859b9488691d0ed20d10b0d49d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:27:52 +0000

    visit movement practice firm cause

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
