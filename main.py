13980f6da1e576bd22f1a91d626fc6b355ee125b
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:39:43 +0000

    achieve warn unit calmly level

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
