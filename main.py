fda9d653b0d53c872efc8ed14788e067ee22b3b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:45:08 +0000

    ours with introduce Ee home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import collections
+def create_user_dict():
+        return collections.UserDict()
