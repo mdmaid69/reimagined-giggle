140503c313cbd3a6d7840a211431eaf228744107
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:03:44 +0000

    with nothing themselves anywayanyhow plant

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def add_numbers(x, y):
+        return x + y
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
