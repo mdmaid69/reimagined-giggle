8aedb55cb78551210e7dcd492267d3cb2f8e1d05
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:48:54 +0000

    catch near now memory air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
