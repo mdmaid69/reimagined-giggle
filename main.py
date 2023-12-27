25a02364ca72f4448b93719364a62f7b465e1c86
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:56:18 +0000

    somewhere basic terrible on relative

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
