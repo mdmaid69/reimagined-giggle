b83f6677fcbfb1f5abfab2db85781fd9f14846ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:28:11 +0000

    close cinema if answer always

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
