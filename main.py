a949890b7cfc9725566e3de06ba641970b679f98
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:05:45 +0000

    makeinto mean takeaway every improvement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
