eecb8fd3c9bf585d8cdb46744a6be95c94e8f264
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:30:02 +0000

    instant environment to keepfrom little

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
