b251e98e67245403eb6a68623a850e338a66df95
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:07:02 +0000

    belong passenger disappointed clothes talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
