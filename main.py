da9eea2502df994139cd96f5e3f27206bd138de5
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:28:56 +0000

    surface cold preparation state plastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def sort_list(lst):
+        return sorted(lst)
