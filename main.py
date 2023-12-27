a3a13439478d880876815c6f7a2bdb3e570c48bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:11:16 +0000

    giveout damp ahead by night

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
