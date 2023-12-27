3512bcfcefedf055503cd74ca726122b8a0ae60f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:18:53 +0000

    goaway attempt education 566end season

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def convert_to_hex(n):
-        return hex(n)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
