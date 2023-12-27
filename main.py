2ad6d85afd460ce78d3f13e5c4b3efb1cc493377
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:44:56 +0000

    success listen scare cheek increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
