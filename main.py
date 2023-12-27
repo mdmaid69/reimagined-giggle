7b08db1c68f149dbf212e5a52ac594163fef7135
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:30:32 +0000

    ordinary wing ago organize stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
