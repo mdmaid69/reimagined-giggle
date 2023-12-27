8feff87c7418e9f423d76dbb934f5afba9a00310
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:31:24 +0000

    can side 570enjoyable priority godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
