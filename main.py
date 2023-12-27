66a5c40138e68dbbf97247a3494818243623e5c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:33:47 +0000

    hour any possibly tall birthday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
