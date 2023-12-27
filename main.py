5649ceaaa5c4c600f22b31a0582defd0ade1baf8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:47:00 +0000

    move throw course live guide

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
