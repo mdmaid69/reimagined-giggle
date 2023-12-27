a9973c258a8253f93ed2cf05b2718a2e1a3f4b26
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:14:27 +0000

    cheque proud joke born such

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def get_array_typecode(array):
+        return array.typecode
