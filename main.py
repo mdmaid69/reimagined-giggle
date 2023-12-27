42f0f32b9047b2c9487b622b940281e21aa41f5a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:57:49 +0000

    every very like trouble please

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def convert_to_octal(n):
+        return oct(n)
