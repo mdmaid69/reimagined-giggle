aa9f47424c2e41c91544ea293b066b5d8a390d19
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:23:48 +0000

    excuse mysterious sky gentle little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
