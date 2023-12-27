1cfe21d6ef346f6687088e825482d23378268a71
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:02:53 +0000

    lookback(on) Rr wind powerful hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def extend_array(array, iterable):
+        array.extend(iterable)
