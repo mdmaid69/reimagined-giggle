d430f93b77ca47943d209eef6a657e4680b2e3a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:13:13 +0000

    tip distant loan opportunity represent

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
