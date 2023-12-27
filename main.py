f2949c894faa95ad85a30d385b5fcc12efc05848
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:51:08 +0000

    stop crisis direct set centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
