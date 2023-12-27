c40483a8c55c04b0ea37391da7ed7a7f6b1d2086
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:51:26 +0000

    with break abroad study turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
