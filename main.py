c27f02ca24e964fcc9c3c24d1daa20b5ba63c44b
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:25:56 +0000

    aim often anywhere impatient stupid

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
