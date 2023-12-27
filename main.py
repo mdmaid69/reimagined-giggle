fcc93c065b1455a71207ce0dd755febe8a0c5aa4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:19:49 +0000

    bottom range event first give

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
