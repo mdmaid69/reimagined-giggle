133602425451dddf5c3b218503bcc05adabf6fb1
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:58:36 +0000

    medium sing imagination lead lie

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+for i in range(10): print(i)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
