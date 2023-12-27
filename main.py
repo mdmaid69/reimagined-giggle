c14efa1e0edde8b82b315a24e78a0f2cd219c581
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:22:44 +0000

    belong crowd part wave take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import sys
+print(sys.version)
