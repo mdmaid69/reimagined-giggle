4ab236b499f2d060efc9248db8d91d3267b31d8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:18:24 +0000

    limit cut deserve forward convincing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
