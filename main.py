2f94db782bd052f199ee26dffe4583894c448df5
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:16:23 +0000

    understand speech open guard bringup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
