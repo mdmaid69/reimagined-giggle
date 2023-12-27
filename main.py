d77e0a4032e1269f9b0a28952ce00f775abe85d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:36:54 +0000

    share she question lay Dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
