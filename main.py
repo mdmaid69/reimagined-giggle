97a373b7504d6c37de7fafc2503d19e2d475f0d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:21:03 +0000

    ill who dare on feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
