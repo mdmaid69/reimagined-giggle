fe5a9c02bffb315eaeba422f379631c39dd76233
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:21:21 +0000

    exist workout hurry tour real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_min(lst):
+        return min(lst)
