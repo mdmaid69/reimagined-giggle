a62f0185ab60e059ceaa0e54cc1941ac3cdae15c
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:48:19 +0000

    check gather increase follow equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
