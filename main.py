23a74096fab2056e079e73fe41081d954f0d9f16
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:01:43 +0000

    hers radio face stick measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
