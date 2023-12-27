acd071408441b1cd384defb083fee8c94f3dd480
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:18:06 +0000

    generally fire giveup away neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import time
+def get_time_since_epoch():
+        return time.time()
