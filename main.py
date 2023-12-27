d63e4b84e00257801f83a809d43ffae6ea84258c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:39:58 +0000

    suddenly track crash welcome tough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
