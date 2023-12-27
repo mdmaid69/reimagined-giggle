3e5dfdbbe984aef06286a10cc40a67e54358c2aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:14:35 +0000

    speech goup open threaten worried

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
