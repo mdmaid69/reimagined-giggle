ae1bd1a77abdedd430a1cf30a8a07a98f76102d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:11:26 +0000

    himself maybe keen afternoon control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
