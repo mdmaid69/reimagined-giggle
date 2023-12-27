cc6a9b567caff589fea8d4bae4c8e4b3006045ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:55:12 +0000

    cover free poor lookback(on) whatever

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
