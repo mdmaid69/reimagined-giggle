d1ef4e503a98bea1e681beaa2fc3cc504c202527
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:05:19 +0000

    pen gather permission interest high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def is_even(n):
+        return n % 2 == 0
