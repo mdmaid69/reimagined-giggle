9e43451c99df254915cb50298f4e077fb1366df2
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:17:57 +0000

    invitation profit oil draw their

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(10): print(i)
