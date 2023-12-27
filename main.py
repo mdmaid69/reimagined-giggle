1193eecd75dddb1908871fc434e0cbb7d9634336
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:45:54 +0000

    side pride indeed history church

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_speed(distance, time):
+        return distance / time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
