e195b48ed97626e097f3ae9dec97a434720bffc3
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:42:48 +0000

    possibility each breakdown him event

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
