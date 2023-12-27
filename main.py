7a409f9a6b45a81599ecda93ee413c72f3061b5b
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:53:47 +0000

    claim please giveback voice store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
