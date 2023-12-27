bc8f243cd70a5136736c4b07a17e6b8806cdcc40
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:25:45 +0000

    freeze prove foreign keepfrom shame

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
