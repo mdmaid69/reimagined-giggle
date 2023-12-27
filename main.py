a11c3c118524a5bf4a9ecf0cd7f66eee6f545928
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:10:51 +0000

    present little rich however I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
