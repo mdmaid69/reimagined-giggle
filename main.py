227226cefbfa660b4c41588cf3ad86b0f85dbf46
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:45:16 +0000

    none otherwise stick rush match

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_cosine(x):
+        return math.cos(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
