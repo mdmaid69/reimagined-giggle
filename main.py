4ca006d323bf87afcdea6ab235c3b4f29405a0a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:57:21 +0000

    mess afterwards makeup relax beautiful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
