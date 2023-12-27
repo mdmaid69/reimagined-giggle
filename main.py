57efe97845ca52980238df19530a990b897a3fde
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:39:39 +0000

    woman of mend black wing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
