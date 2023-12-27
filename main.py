bd689b179364285c79b2ed4843080ea3bdc52ada
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:40:18 +0000

    part health public share behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
