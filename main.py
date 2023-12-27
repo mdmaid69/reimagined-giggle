97739b1b8292f46d13a233921617ac7ab034fb15
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:58:50 +0000

    post count touch connection direction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
