f06629e538d2663c85b2b8850ec8cbc2ddf36bf3
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:18:23 +0000

    ill male doubt takeout play

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
