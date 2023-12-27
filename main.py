b5a0f81818fca13edb14d4346e0d2efab750e9f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:34:40 +0000

    rid getoff cheek basis separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
