6ded74778ec6c944e576c7c365e96737304a3807
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:01:24 +0000

    jump movement behave breakup sentence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import collections
+def create_user_list():
+        return collections.UserList()
