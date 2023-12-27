66deb187f83f556933f08de02cca4be7c864c010
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:15:41 +0000

    snow treat cover different determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
