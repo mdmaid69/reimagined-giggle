cf22f198c9005153b4a4235903f83670a5046579
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:58:03 +0000

    lost will unpleasant reduce brush

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+print(sum(range(10)))
