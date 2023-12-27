a43b36c939826ce894cff94ad6e43a1c796c4369
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:50:05 +0000

    order control hour brain idea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
