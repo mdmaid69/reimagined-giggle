cae1fa4922fe118f6937e5b00502fa0414619f4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:42:09 +0000

    stare justice wrong damp pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
