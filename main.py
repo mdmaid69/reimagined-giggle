1c063bc61d5df11434d9ab5b2ec6f0d7a4233076
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:11:25 +0000

    callup period strike help both

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+print("Hello, world!")
