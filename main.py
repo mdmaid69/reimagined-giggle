69afbea20d1d0fea962c34789d3899710f5bc09c
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:30:31 +0000

    private against range scheme your

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def square_number(x):
+        return x**2
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
