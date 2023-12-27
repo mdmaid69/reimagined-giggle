73aac2fedcd598515074c3058ad7c9eacbb5157b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:13:19 +0000

    bin tense hide small bad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import sys
-print(sys.version)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
