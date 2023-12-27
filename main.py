0710f3e74c35a30053681acaa903ae02c495dba1
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:23:30 +0000

    brief along sting shop smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
