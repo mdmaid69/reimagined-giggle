2525dbf0737b31c9a5a70ba31e50f2de17bb7728
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:04:15 +0000

    dare negative sentence preparation practice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
