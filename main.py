84dc70669c4694350a16d2b3ae0ac739d658e94c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:53:29 +0000

    kick pause Yy main be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
