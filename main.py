b005f07d5da7138357bb2d5c32f7cd67e34c5857
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:51:33 +0000

    on phone move doctor holdon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
