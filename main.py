f7610f6a79bf405c049d5c8ef85c22a126a193b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:47:40 +0000

    event turn live relation food

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+print("Hello, world!")
