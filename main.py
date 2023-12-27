074434458704836ebe06787a4204d6f8fddd27a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:07:36 +0000

    treat turninto admit money should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
