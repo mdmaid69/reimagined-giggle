a1de80dd3887c52d2d037458f7ebbcc2f592c860
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:22:34 +0000

    frame period press thorough back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
