255c5b653b8f768d3d1d448f4c69249dcf7df5f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:32:36 +0000

    genuine as far serious summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
