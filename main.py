7818d6f98508b7f6bffd6647e2e4ade9a0f76d69
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:15:37 +0000

    rise early have they sort

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
