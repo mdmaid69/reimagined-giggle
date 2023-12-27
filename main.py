b8639405b1eda2923aa6444b5387e76d02fe411e
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:51:26 +0000

    control inquire once crowded mend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def is_palindrome(s):
+        return s == s[::-1]
