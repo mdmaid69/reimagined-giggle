691e09b34371852a272ac02c0701fba5095380cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:40:36 +0000

    holdup true repair Tt fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
