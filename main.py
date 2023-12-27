9d2d57586c720a86ecf7301a17dbff46eba01e5f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:04:49 +0000

    business arrange distance pleasure smile

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print("Hello, world!")
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
