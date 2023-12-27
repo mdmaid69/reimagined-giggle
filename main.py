10ea67aa7924b59f7c2b10222a5842c4bcd6340d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:22:19 +0000

    door complete appearance uncertain inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
