b59763bdaf44be11232e1396f74ba6f8c8008201
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:41:14 +0000

    while desperate opinion announce speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
