493982a591d0c96ccfda98dbb79095dc71c85322
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:21:06 +0000

    weak bottom genuine demonstrate politics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import sys
+def print_python_version():
+        return sys.version
