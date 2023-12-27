421099457e688b1aa2d1c2d0f743093fdfc0e1d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:46:41 +0000

    relationship convenient medium mainly shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import getpass
+def get_username():
+        return getpass.getuser()
