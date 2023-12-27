a9b81f4acc4e78528e68ce75d296853366860d3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:10:10 +0000

    combination achievement reply contrast tight

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_work(force, distance):
-        return force * distance
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
