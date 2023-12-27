32b77584cb5c2f2583ee8c3d5e4486d87c380a70
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:48:43 +0000

    thin sing escape sorry move

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import sys
+def print_python_version():
+        print(sys.version)
