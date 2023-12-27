5eb3993c3414e45e6869ab713c7f059981f1c9a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:24:27 +0000

    regret react common write interesting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
