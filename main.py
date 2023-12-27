bc14057ce0951031c5105186ff1ce16d04e6b8c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:11:07 +0000

    paper refuse smooth uponon proper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
