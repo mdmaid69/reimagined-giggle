cef4bb0a68ca0583433c80ed05f58162c5468aa9
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:51:24 +0000

    original flat period mistake scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def reverse_array(array):
+        array.reverse()
