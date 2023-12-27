2b6b0d689e0821f55a2886e14b97018d532de078
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:42:59 +0000

    another geton mother distance convinced

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
