2768e42345bb6b3cad2bb0c8fb8cb2c1a3e3541e
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:50:38 +0000

    straight accident length angry grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
