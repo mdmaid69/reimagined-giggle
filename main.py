cd90e239b77e2d331b1a13bac182fa33aaf02e8f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:45:48 +0000

    just organization love row notice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
