fb947de46e8ef321fe2ea4520f05d7802ff8f859
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:23:53 +0000

    pressure furniture disapprove away present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
