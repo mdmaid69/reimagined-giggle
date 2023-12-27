02962d3377b92c56b0af92be465c5102764c0f90
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:46:14 +0000

    wide memory small bush disapprove

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
