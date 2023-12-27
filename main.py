552f8c564559a876a6487479b5ca15178bc900fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:34:52 +0000

    seem subject reject detail couple

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
