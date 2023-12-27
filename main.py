49ffaa4978d7715e1136cef20082d47af7d5cfcd
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:52:18 +0000

    neat pocket copy involve container

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
