4e78e8ab7a9317102870d847f7ceebe69914f2e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:55:54 +0000

    reliable clean pronunciation convenient dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
