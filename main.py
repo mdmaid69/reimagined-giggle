43dc5b5c0e8878fa0d23ed0d9aecc9b2096cfe79
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:47:54 +0000

    solve exist tap cold travel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
