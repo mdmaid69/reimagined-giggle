4c886f761abbafd7951f1d773885fa220f7c1ea0
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:54:59 +0000

    bringforward play apart cover bridge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import tensorflow as tf
+print(tf.__version__)
+import getpass
+def get_username():
+        return getpass.getuser()
