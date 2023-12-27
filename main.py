a12874b7eba6caed14ec74e5a352e2c453f502cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:22:08 +0000

    fashion boat sleep decision tax

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print("Hello, world!")
