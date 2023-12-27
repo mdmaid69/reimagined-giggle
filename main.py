d7a66589ecf66187176073c633f4b82bd4fba8c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:48:10 +0000

    sky smile turnon back important

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_min(lst):
-        return min(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
