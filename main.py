1966b321470c700fb8367c9e990ed8eb822f21f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:49:17 +0000

    deal cause target goback(to) tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_tangent(x):
+        return math.tan(x)
