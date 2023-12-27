5084dbef1c0b2d7dd8ebdc3c57d0ff96283c8b67
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:36:56 +0000

    play serious song bear machine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
