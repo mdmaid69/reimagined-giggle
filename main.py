95ef8f716044c7df7b81167a1e14300901afbb22
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:58:07 +0000

    approve minimum Ff broad breakthrough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
