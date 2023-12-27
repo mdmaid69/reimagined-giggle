9f6cd7810d888df88ec5174e9d60aae7033340da
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:21:10 +0000

    them mess switch cross ordinary

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import sys
+def exit_program():
+        sys.exit()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
