4f601f0cca99f121528947b5cb88871a414713f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:07:27 +0000

    start sing sort back cry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import collections
+def create_counter():
+        return collections.Counter()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
