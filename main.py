4ca792ee34f28b793f2a6d035a549143b8576987
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:42:05 +0000

    violent unhappy unpleasant talkoutof uncertain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
