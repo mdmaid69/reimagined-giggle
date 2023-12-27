a2c85e56488d30e7b25d1386de2b12e00c6aa3fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:34:24 +0000

    comeout reject meanwhile drop hole

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
