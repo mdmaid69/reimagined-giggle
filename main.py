1324f7157dafc101fb6f4f2761ecbf8413da66cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:00:54 +0000

    cause attempt move include transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
