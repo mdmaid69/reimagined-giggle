0d717efe9a7cc26d64292b88a369ddab744e62ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:22:56 +0000

    future protection slightly break unlikely

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
