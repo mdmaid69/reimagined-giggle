5e43207c6c7cf7959a7a4d3d927a60b94cc2d678
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:56:36 +0000

    past interested during pretend well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_index(array, item):
+        return array.index(item)
