d991cab63c44fc67c62c7f89d858d13f9b7d923b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:56:16 +0000

    search row thin week print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
