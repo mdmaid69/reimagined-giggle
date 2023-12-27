3748da2e17f7370d7e923dc9da75b328f732fbe8
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:07:44 +0000

    unkind Gg hold edge appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
