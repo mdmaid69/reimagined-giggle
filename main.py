25b62af8513d114aed361a1fec2e4a2f2602ddc1
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:04:27 +0000

    sign but history path challenge

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
