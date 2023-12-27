2cc1242ef5f23cf3d51ee94d99748b94678ad601
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:00:41 +0000

    state comefrom achieve exploration trade

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def cube_number(x):
+        return x**3
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
