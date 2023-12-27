d3094a20f62204a5a0d4d6060650573e5255cee7
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:48:16 +0000

    post education damage pencil fire

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_list(array):
+        return list(array)
