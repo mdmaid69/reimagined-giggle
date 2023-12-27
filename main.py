62d54bf72e7cb27fb061cc631c2230c4e7cb3d69
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:09:22 +0000

    angry rescue material book advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_tangent(x):
+        return math.tan(x)
