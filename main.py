d3a5ac65174997a36c7d03f76cd95c2f40f459a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:44:35 +0000

    surface appeal but imagine hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
