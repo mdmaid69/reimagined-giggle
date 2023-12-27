cddc75c417e3c71a48e46103a04767282bf88f1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:47:32 +0000

    ring jump you pocket turnout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
