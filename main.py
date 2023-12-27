32222448bfd276048416841b155bcd0f1a748ab0
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:36:20 +0000

    welcome we sometimes wild public

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_floor(x):
+        return math.floor(x)
