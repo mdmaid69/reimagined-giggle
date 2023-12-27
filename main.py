e7deccd2e1152048c42225e0f5eb13f442786b96
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:30:56 +0000

    it extremely ring twist tend

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
