5575a4b386cefee6e26cffaf3527502830437696
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:21:27 +0000

    luck treat capital active bringback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import random
+def roll_die():
+        return random.randint(1, 6)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
