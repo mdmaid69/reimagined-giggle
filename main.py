db708673d60d801d8d90e2658d5811c31418fa76
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:47:23 +0000

    address during block plain considering

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
