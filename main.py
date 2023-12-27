11e457a4e350a0df9af66bf109f6b12a02938549
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:01:46 +0000

    wise expensive entrance identify hold

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
