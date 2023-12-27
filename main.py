1e3d747101ab4f16211478d371b48335bc7e99a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:44:18 +0000

    law classroom difficulty move pleasant

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-print(datetime.datetime.now())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import platform
+def get_python_version():
+        return platform.python_version()
