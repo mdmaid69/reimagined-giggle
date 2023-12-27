860cbe8f1c13d2a2cdcc2e04bd99ff5abd77bb42
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:21:08 +0000

    break last commit path stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
