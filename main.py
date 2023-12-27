a787ae93260f4ace98a581578de9d21ba0ecf26f
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:03:56 +0000

    actual distance equal represent city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_density(mass, volume):
-        return mass / volume
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
