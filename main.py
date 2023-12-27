c4b1a56e72270fdb19bc25c7ea37d767a4157203
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:41:18 +0000

    film pity book generally turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
