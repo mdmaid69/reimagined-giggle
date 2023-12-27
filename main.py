e4f8c48a571e5519495d5f3ad5d39124e23dc240
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:57:59 +0000

    include feel switch unfair luck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
