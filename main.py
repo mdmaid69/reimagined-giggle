7fe030b233163243e3acdbdc0a79de540d9d0d56
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:04:21 +0000

    glass pull slight build arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
