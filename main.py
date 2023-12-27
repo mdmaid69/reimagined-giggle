e86d699408b23f0a3ab8b413a6eaabe0f4711908
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:57:34 +0000

    rain sort true right natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_arc_sine(x):
+        return math.asin(x)
