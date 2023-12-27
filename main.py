9f82cf5307f3e97cd9d8dbbb2c868fd73e0c7d82
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:01:35 +0000

    production police chin left serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
