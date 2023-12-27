4a23dacab225c7099015de7a7a0f8cfda0cd8227
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:32:52 +0000

    neat fast bet top live

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
