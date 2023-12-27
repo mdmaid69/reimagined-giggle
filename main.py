08dbd6e44f1313b1b3a99b91723e920cb5415904
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:13:38 +0000

    fair advise post dangerous letter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def reverse_list(lst):
-        return lst[::-1]
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
