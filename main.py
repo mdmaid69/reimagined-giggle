77d05ef69a8a48b1f44679168152b2d0154df184
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:18:40 +0000

    nowhere serve information theirs behaviour

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
