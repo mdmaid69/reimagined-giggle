a24a3d5fafc986a857c7d215a4ee06406efad7c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:21:06 +0000

    keepfrom cover male genuine soil

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
