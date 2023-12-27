3e0c66d24e7e5d16493db969d3653cbe5cf966b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:24:14 +0000

    country loose secret connect absolutely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
