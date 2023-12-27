607c692d424ea659ec4d080c6f589258f8623584
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:16:03 +0000

    stare some circle marry club

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
