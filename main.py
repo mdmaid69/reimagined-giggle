e1cccf9a78694205d8b1e51d6550e91b7e68afcd
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:57:44 +0000

    considering group theirs delight right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
