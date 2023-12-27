cd4d55dd9dd5ef92c8e8033b6e0f4518d4635239
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:29:16 +0000

    everyone guard bath lift right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
