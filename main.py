0007555660d2c9e762193edf29efbb5c1e946096
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:40:04 +0000

    everyone entrance married tongue dead

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
