5f7a8e845e064062df268c52f43c7cc8e5550812
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:17:21 +0000

    Ss voice sell talk pen

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print("Hello, world!")
+import sys
+def exit_program():
+        sys.exit()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
