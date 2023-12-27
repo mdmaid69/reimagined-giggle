cd4ba579a970dcaa4349876a49edfdb9402eba17
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:59:35 +0000

    world well uniform file lead

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
