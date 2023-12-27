2c8ce6413b7f26292bf6cb07cc5e99dae6ec24aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:46:00 +0000

    count second characteristic bet task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_user_dict():
+        return collections.UserDict()
