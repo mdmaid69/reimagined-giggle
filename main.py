406cc9dc122ea58eaae3f3e6cd68109261b953a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:23:40 +0000

    runout breakup kind fact neither

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print([x**2 for x in range(10)])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
