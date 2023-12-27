68b92c12c443f8fd5416eb4f9eea316631bd1038
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:39:15 +0000

    uncertain group date cover remember

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
