82db0d8947031f48610c42f6f02e3fdf8aef2f66
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:22:42 +0000

    sink must least breakdown strike

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-print(math.pi)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
