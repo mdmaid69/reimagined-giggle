ea0ff1369b16576a7ebb71d73ebdb505ce698b28
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:01:04 +0000

    use mean heavy excited obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_min(lst):
+        return min(lst)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
