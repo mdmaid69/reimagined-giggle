19ace125541c1913811de881244a5e9bc05b0c9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:51:04 +0000

    rather before curious Qq morning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_max(lst):
+        return max(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
