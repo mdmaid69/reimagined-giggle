99576af5fab21fc54e60e8b93a8f95cdf232a21e
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:51:05 +0000

    sign downstairs seat mark snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import time
+print(time.time())
