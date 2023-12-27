5dcb76f678ab7dfb29540688bb9401c9212d55b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:53:17 +0000

    exam complaint guess disturbing yes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(numbers):
-        return max(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
