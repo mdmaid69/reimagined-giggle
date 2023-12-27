a944360a928c6eb2d8958ae5de05a8a44fc3cca9
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:10:05 +0000

    team soil payment vanish rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
