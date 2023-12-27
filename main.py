19e807d37d797ffadd2fc66a364fb892306ebf74
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:37:01 +0000

    worse passage eat runover watch

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+for i in range(10): print(i)
