f43c90c01044597b921a7cce069e4b5d8d81cdbe
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:00:55 +0000

    act worst risk good wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
