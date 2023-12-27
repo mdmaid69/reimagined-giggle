6597584e874f45bb70701308ee2015d452697a51
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:57:39 +0000

    mistake long raw mysterious coat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
