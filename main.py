0a871d5c1f04728b8667f52177f98e7994e0c57a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:29:21 +0000

    display Mm attempt strike just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_power(work, time):
+        return work / time
