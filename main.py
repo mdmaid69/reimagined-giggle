db2704e488f1eaa4e7fb914122ef784292cece16
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:48:14 +0000

    direction on balance through entrance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
