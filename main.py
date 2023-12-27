448c5a933f467784240628617b9fe5cbc8f0f501
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:58:37 +0000

    dry advanced action hide work

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
