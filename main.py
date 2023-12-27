0216db34faaa0fd416f24b680c3a2951563382ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:27:25 +0000

    what whichever next protection level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
