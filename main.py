66545ad442da5a24f8db96b8dbbc91e4c9a5b3e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:57:53 +0000

    lookup proud spoil minimum proud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
