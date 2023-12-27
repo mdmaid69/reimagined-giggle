0ba7f00a0ec34c4c402324c5a70db666af17744a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:45:19 +0000

    pupil sense furniture agreement destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import time
+def get_current_time():
+        return time.ctime()
