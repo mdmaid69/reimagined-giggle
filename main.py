549b4996bac79f24a4b107bae7d5f0e1177dbe0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:52:45 +0000

    tough fierce lunch length with

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
