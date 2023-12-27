38eae98c22d988b1dd9b29ee64ac1099ef3dffdd
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:37:26 +0000

    period touch tomorrow steam age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
