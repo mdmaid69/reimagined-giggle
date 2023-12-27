f5b5eaf66c59ac4b6052e58091051768df4dd406
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:39:10 +0000

    herself practise 566end rarely offend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
