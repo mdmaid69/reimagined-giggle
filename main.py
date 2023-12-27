89186fb1244cf07bbbb845b44705fd8ce6d0e198
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:13:43 +0000

    sting store key reach brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_itemsize(array):
+        return array.itemsize
