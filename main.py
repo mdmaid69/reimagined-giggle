e70f6d3977a05e5af4d78f8224821f689167d9d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:06:11 +0000

    model black direct especially accordingto

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def find_min(lst):
-        return min(lst)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
