37019e1e7b4e71e2562967980178e92882bc10da
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:39:32 +0000

    address television giveaway attempt slightly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_even(n):
-        return n % 2 == 0
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def convert_to_binary(n):
+        return bin(n)
