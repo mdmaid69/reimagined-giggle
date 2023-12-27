4ef5f1d653a15cf0903e9c37173b2a7ba790b08a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:59:16 +0000

    instead plastic objection invitation window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
