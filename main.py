1cc6d4107473bd9cebc1351519629f61b480a6d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:00:30 +0000

    demand odd goodbye attract purpose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
