ffa3d76e195deb7e0cb47006e3988bfb20d4cc1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:14:40 +0000

    dream practical somewhere stick past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
