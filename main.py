d7ed862441b56db0ca7cf1f63708ac73f56127bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:25:39 +0000

    wish whenever stranger comment object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
