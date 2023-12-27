7deb707b59cd7b1430ba81e408c1cbe164448ad8
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:35:09 +0000

    impatient damage which port when

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
