1f88c0e418b4c828c8d6c9dbc54c7bfbd42ed30c
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:03:39 +0000

    bang face skirt tend lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
