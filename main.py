a9a0e1a8570b6f8985d4022655ee862ff6a1f1e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:53:41 +0000

    pretend height once attempt breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
