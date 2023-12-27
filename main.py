ae1184a280c761a889a7a646269d4cebb13a87c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:25:47 +0000

    school public mix show obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
