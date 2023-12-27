8af239958e5232f04b84bf5166df80a1b8f0f93c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:37:33 +0000

    peaceful repeat suppose tough warm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
