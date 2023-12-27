532d97cc8ab94c2ca986755c15508b2f02fc2aae
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:50:19 +0000

    spare test Nn clean unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
