757079913f85ec046187a05b8a885783f05922af
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:09 +0000

    reason slice himself fun deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
