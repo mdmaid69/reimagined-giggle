941eb8d23869dc41c3d69f854d280ac51a05a076
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:31:41 +0000

    cum identify quiet pure tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
