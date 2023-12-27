3a1332fc3787f3fa7c12f7820009b346a8cc1f07
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:45:27 +0000

    anywhere land method fail emotion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+        return os.stat(file_name).st_size
+  def convert_to_hex(n):
+        return hex(n)
