232b25956a6c0370428a3dc1e30af6f21334f767
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:46:25 +0000

    especially consult please fortunately once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_list_from_array(array):
+        return array.tolist()
