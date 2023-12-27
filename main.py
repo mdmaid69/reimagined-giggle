4b367034b43fdc7ceac89bb5d48d2b69a7c1c1ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:33:47 +0000

    phone hers far confused health

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-print([x**2 for x in range(10)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
