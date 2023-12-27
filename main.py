d2ff25423c7a36356ea5dd2c2efc82721ac5ed3e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:49:24 +0000

    listen model speech used makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_sine(x):
-        return math.sin(x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
