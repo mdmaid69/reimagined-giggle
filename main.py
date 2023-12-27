3acefd7661eb093350b1c73dcb2711261a4c4bb9
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:43:17 +0000

    all hardly wet comfort trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
