2ffdad666ff179aa0ff9aa8e8878e932fdc4b676
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:25:11 +0000

    separate throw little force piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
