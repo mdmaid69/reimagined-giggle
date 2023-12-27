28be4f38eb878aafae91cddfb5d3688b7d06128d
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:21:29 +0000

    carry other apply sad lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
