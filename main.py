dbfc69c1f786ef55b9cd0fbe90b3fb8567253b7f
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:53:11 +0000

    organize number convince view wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import os
-print(os.getcwd())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
