08a2b6725635faf07f0cefbce06ccaa6ba4d619e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:53:34 +0000

    treat loud order suit during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
