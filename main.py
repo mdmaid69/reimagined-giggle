794ae626d5adeaab2e367752dc8bcf03b5f9e967
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:11:56 +0000

    another black recognize attention blame

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import numpy as np
+print(np.array([1, 2, 3]))
