0ca780a1ebd97a29650ce8252c935a93facdf867
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:25:02 +0000

    related wave interest present laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
