9b7d5651aa28dce42f67b442f9182cd5f75bc852
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:30:12 +0000

    body everybody possibly path interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
