031dc9aacc70c32d51211a9d5144a319b029acc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:53:07 +0000

    talk round new generous weak

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
