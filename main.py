42e236d670aa54c026cf44d9c298e72b25e435ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:35:58 +0000

    vision however edge pull exact

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_item(array, i):
-        return array[i]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
