4aca72f3ae7bc70583c99ba3aa6f1383b44368ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:05:00 +0000

    murder near its thumb uncertain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
