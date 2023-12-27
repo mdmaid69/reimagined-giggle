78765765b061de787a133d918f6df3e9fcce9525
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:38:17 +0000

    partner schedule protest influence off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
