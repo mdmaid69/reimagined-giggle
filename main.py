554c98d5a8335d8e30c3679836405d3cacd8a1e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:02:43 +0000

    corner shake nor plate memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
