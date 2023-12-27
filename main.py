c350302ae191a6274a36da16e2b0f5f3dac61e72
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:24:14 +0000

    complete warning recovery knowledge debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
