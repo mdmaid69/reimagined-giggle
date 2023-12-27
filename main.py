aa7e4c3d3008eba5107d3b48794080d5c8480c46
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:05:58 +0000

    share breakfast left independent takeoff

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def count_characters(sentence):
+        return len(sentence)
