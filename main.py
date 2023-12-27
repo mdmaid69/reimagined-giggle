fad8a8f51d120d9e780456cda071c7f8ecfabdf8
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:31:38 +0000

    fact comment service more workon

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  def convert_to_binary(n):
+        return bin(n)
