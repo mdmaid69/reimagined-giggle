0776873e4d1d12153da9c596d31e12edd3bf50d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:42:22 +0000

    fat otherwise turnout bet murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
