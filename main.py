91c845083ffd91f996ab9e28d0213d37ab88bb3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:35:51 +0000

    giveback side wrap piece fit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_str(array):
+        return str(array)
