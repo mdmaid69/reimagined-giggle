de1022d17f59b9a3bfe9e41baee50fbd096d4b4a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:10:14 +0000

    learn turndown straight shake serious

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_area_circle(r):
+        return 3.14 * r**2
