5bb4375ffc4f918b6448d18d55c08fe2b9ce6bcd
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:13:11 +0000

    road description stop object nose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
