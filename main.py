833ec102205c6c7bfa4e792c56757274cbf6d863
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:18:11 +0000

    complete embarrassing touch workupto on

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
