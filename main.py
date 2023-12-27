d8c3befcbf8a4d4c5a456823dd06f3b308b51326
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:20:41 +0000

    match peaceful dead its extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
