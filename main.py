54730e407ae46c257c183574729afe46901c1eed
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:54:28 +0000

    guide company getout human nigga

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
