4a5a58e6dcd3a7afe417e351c0396ca310953e72
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:17:39 +0000

    opportunity promise favourite measure clue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_set(array):
+        return set(array)
