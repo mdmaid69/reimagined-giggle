b6c44f64db55296c6e73aa6ec06a5151e2f05de4
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:54:30 +0000

    age still fat pool report

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+for i in range(5):
+        print(i)
+for i in range(5):
+        print(i)
