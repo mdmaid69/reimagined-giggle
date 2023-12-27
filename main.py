e2d065f04075b54e0d769010d4830643fabc656a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:19:54 +0000

    block ahead judge bet since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
