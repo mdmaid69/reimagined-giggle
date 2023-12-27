01b9dd2b428316c0f399d33e2622c35c264675df
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:03:27 +0000

    job rich action thirsty Bb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_area_rectangle(l, w):
+        return l * w
