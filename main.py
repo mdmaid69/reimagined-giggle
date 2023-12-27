c9fe37a9ab4e5d3486d06406ec8671df41045e81
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:58:41 +0000

    single creature rate uncomfortable estimate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+x = 10
+y = 20
+print("Sum:", x + y)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
