d6afdffef79b9342177e628b3213f78da905792a
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:17:46 +0000

    soak comedown bin extra prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
