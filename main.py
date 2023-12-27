257e4a9e2254350cbe85ff2121515bfbf4abe716
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:44:28 +0000

    rob print ride hello failure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_power(work, time):
+        return work / time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
