51e183832d85b2fba5d3ab488382ba3d0a07d182
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:49:30 +0000

    sound cut protest length comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
