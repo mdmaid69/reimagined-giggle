c012cf30a2fc926b00512d31c3c7992e429942b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:56:04 +0000

    complete pick breathe return because

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
