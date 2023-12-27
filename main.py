a3f2e4818b7113c2e8f54f0e1070d83a8fba4608
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:55:23 +0000

    rent warm trial fire wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
