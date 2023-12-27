7b3b35e626a0afed2ed9dbd90ce57ac5776726f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:43:08 +0000

    basically tap top book crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
