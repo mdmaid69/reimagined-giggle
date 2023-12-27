8b23dd90f9831a30bd4de8f85f4b05f01d5d0ade
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:35:05 +0000

    pencil point lift image bang

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
