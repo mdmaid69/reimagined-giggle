baae27e6ca35261ef9548fc13383c8da6a625c90
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:31:08 +0000

    reduce Itself emphasize minority original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import getpass
+def get_username():
+        return getpass.getuser()
