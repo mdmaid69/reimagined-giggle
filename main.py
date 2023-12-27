372aec0971f45f0482b2d4daddb3ea1b91c5684d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:18:35 +0000

    welcome worth odd lower search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
