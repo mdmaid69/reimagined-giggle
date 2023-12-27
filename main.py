a461c3b59afd97027793812f209a4e953f079345
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:36:25 +0000

    advice fit ashamed university best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+name = "Python"
+print("Hello,", name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
