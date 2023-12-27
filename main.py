04bb4b2c79bd3d3d4d55d6036ea45e70330cff1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:02:03 +0000

    point gooff together build born

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
