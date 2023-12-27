5b87aa0c55806cced8f5a63c99ac20896c17a634
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:24:35 +0000

    ring but professional press later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
