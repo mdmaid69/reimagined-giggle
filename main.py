63a8f8bc502269b12fbca82eb242f5c7543ae1fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:59:22 +0000

    talkback nerve just remain tear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_python_version():
+        return platform.python_version()
