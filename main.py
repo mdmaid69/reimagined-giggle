ab4f5905efd120b5faedc3ddec593cc47e42e863
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:24:37 +0000

    characteristic borrow store anyone neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import datetime
+print(datetime.datetime.now())
+import time
+def get_time_since_epoch():
+        return time.time()
