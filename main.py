b38d6d7a7e9d85e609def3f60c11fd38217ff0a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:37:56 +0000

    upstairs once rain deserve check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
