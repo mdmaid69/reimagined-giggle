0b4ec433f5066b5e0afc7e53093169ca96dffb7c
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:17:37 +0000

    know difficulty gas clean example

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_current_working_directory():
+        return os.getcwd()
