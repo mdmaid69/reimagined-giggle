95f7615e84826200283b3625dcfb16bb47253e4a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:03:16 +0000

    afford friend fight water lookaround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def convert_to_octal(n):
+        return oct(n)
