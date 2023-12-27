7f445c7f62385cb4a1e9c332570ac2682fbfdf48
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:40:05 +0000

    last word raise guilty object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
