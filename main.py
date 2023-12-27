81cd91744900cb5afb980e05c30d12c1d694cc55
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:45:51 +0000

    find month block patience form

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def is_odd(n):
+        return n % 2 != 0
