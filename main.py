85240a784dd58046da4cb6b24319a8c9ed655848
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:30:50 +0000

    find air pin culture lead

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
