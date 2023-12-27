a645622218b0fbdc4cde4c1bf10ef2af954992d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:31:24 +0000

    win stare cum progress thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
