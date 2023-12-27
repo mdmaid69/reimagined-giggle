a60068fbad82d2f00b545cf6a9873ba8353013f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:18:15 +0000

    573entertainment brief argue holiday list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
