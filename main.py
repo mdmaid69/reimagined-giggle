7cc378287dc7668e69031f2da3b0232f721d2cd2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:48:20 +0000

    thumb Dd warm spend continue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
