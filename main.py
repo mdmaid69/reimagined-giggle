e7ca7ec6105159236cacf7d3a4ca1139048b3063
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:33:34 +0000

    here pay them immediately achievement

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+print([x**2 for x in range(10)])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
