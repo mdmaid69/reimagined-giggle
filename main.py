0f6dee8099b10ea174ec17de5c7748f7b1553137
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:32:09 +0000

    out emotion stamp change discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
