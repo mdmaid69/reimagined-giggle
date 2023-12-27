dac787b709f5eb7c02db99595b8e6a85cff733bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:04:00 +0000

    holdup knock harm back present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
