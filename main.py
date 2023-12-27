e45d9596f710f52ce7d88aae8eb57e2067ca52f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:16:16 +0000

    callout prove question acceptable spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_power(work, time):
-        return work / time
+def calculate_sine(x):
+        return math.sin(x)
