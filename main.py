871046897f6c89ae9b4d0f2471b8ab019787c774
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:05:17 +0000

    alive congratulations storm joke care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
