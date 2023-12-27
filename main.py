3f852ee5c5f1d650e22aa40436db24fc674c25f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:25:16 +0000

    finger place better strong sister

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+print([x**2 for x in range(10)])
