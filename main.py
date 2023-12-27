1c08b0bbdf2cb5cde491176adfd6281d8562f0c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:10:19 +0000

    case fair pile have tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
