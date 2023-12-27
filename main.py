708b5f7053cfbcf1cedeb45917cd7e790b107294
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:21:04 +0000

    dig hospital throw instant modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
