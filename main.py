e850eedaf452fd63c7f3c540948451ad67f46308
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:36:26 +0000

    wish glad attract boring turnback

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_perpetuity(payment, rate):
+        return payment / rate
