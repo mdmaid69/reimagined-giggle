b96013be022e2481d09b79011f98df0d780de054
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:59:41 +0000

    begin system light demand peace

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
