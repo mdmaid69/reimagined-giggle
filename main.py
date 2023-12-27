cf78b69a2d1a279485334a79d3b69fb1e8d75117
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:53:53 +0000

    arrive mine shade ordinary whichever

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def remove_duplicates(lst):
+        return list(set(lst))
