8659b61ea09ee33a27821bdab8b3420c6e1e2d9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:44:32 +0000

    hope defend necessary view myself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
