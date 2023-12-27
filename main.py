a0298a805e90f92cb7ccb79847d68ca7e5707406
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:59:58 +0000

    plate flat present design interrupt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
