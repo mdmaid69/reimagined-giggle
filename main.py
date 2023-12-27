31f8a1699e2524a43539c6634581b215f6acb5c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:21:02 +0000

    mix brown intelligent tread battery

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+from collections import Counter
+print(Counter("hello world"))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
