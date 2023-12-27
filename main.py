9f9f7e2e126ff00841a219f0920f352c2093294b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:49:01 +0000

    keepout task your comeup tired

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
