70bcd797edf4bf18fead3609eac73fd692618e30
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:33:47 +0000

    body fashion act lend name

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_power(work, time):
-        return work / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def greet(name):
+        print(f"Hello, {name}!")
