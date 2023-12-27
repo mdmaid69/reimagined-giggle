66a75923506fd980f480e295b08eb6f82b906dba
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:40:13 +0000

    importance either gooff thinkthrough aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def reverse_string(s):
+        return s[::-1]
