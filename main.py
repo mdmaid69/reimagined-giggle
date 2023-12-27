b88febc950a1a86d881aa2684df4935444950f1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:12:31 +0000

    claim before quick secret warn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
