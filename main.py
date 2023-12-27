abbcc3fb9c6b85821329f357584577b276459b47
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:03:43 +0000

    success bet regard face arm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
