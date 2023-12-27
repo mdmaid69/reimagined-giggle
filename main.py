b929c095afd8feba016d56c384c746959b8f4af7
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:52:44 +0000

    information rubbish steal hospital Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
