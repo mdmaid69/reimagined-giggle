0f4ee02d803281ea0f83fb5c2edb4b9319195c61
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:47:17 +0000

    public value rob famous murder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
