9ac52e38cd056ae233cafd67b23006b15cc9b315
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:51:18 +0000

    face fetch deaf bound apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
