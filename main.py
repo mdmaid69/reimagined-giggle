804e1b703b53305fbf569d3becda5e7459f21a69
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:00:54 +0000

    advanced passenger difficulty Gg plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
