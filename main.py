eca600a8bc55c34707a6bb906872cc5d79c945ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:23:52 +0000

    year crazy Uu hard concentrate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
