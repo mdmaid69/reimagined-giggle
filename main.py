f780af7855ccc164cd0d5e4579a8d2defc70af00
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:54:42 +0000

    advanced share tie case while

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
