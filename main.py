75f1d32f5dc3e014c26f54f2a72fdd19849e101e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:55:04 +0000

    achievement admire trick underneath free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_length(array):
+        return len(array)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
