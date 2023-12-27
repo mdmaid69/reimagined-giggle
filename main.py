582c1bed3f4af2fb30e52aabfa655f655f84008a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:45:14 +0000

    lend nor urgent imagine suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
