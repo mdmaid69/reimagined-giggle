222ae456d25be0f4c19ab777959327bc03930936
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:28:01 +0000

    breath chat lonely minimum spite

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
