f8cf1883bc9944a86e770580100baaf7031b985b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:02:12 +0000

    treatment along interruption bet bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
