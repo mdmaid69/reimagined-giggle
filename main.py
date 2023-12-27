7aa25bc17e0b77dc440a0afaad17a98f2b991b27
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:54:40 +0000

    exist professional steady another unit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def count_characters(sentence):
-        return len(sentence)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
