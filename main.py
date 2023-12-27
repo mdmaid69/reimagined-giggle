f58194e4328ba390cd1a999774c58a0096cd5cc9
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:58:12 +0000

    Cc design contain the scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
