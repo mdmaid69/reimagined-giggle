2b43b35b95f1328e4ff72552c92de7550276ad9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:37:49 +0000

    disgusting sweep rich remain early

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
