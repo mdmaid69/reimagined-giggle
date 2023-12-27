bc64c69f9a819a67509b91df40d0727006e0dcfe
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:31:24 +0000

    funny rough Hh fun special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+x = 10
+y = 20
+print("Sum:", x + y)
