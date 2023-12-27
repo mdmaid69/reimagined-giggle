81aee904cf89f5e8f37660007d01d009c6aa745d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:09:42 +0000

    record belong basis declare finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def reverse_array(array):
+        array.reverse()
