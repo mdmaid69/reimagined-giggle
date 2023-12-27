517d8f948acc7760f21b56994cf12ceaa1ca7e70
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:15:49 +0000

    everything bag over waste scared

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
