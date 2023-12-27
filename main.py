9210776535647c4ba2278ab8101c0f332ebe11f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:40:30 +0000

    neither 571enough visit use target

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
