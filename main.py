0d05ca5d6944b1dec930af5ad96e443e24c50eaf
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:15:28 +0000

    injury book Ff fire world

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
