68515bec99f8c37971d7e4e6d0653a09e8f5313d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:01:01 +0000

    dangerous unite lock tidy always

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import sys
+print(sys.version)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
