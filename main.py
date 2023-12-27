71d67499f7ff9286fdc5c8aaeb80cf818ebbd826
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:00:50 +0000

    kick Hh mean birthday land

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
