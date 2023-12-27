94d1ef7f5d8a23e9edb7f0610ce431a16cf86b13
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:44:27 +0000

    threaten mix plenty breakup sink

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
