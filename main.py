13b83aac66b70b0eae738b7ec08cdcc84f27bd56
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:34:20 +0000

    stay side by talkinto drive

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
