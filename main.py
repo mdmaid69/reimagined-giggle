331bcea5cd4d5a9e174ae0f2aae5ed25414881ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:22:45 +0000

    probably estimate difficulty turn relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
