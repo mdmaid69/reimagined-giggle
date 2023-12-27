8825949092ebe9104f49e9eaf5ec00cc13476d66
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:46:43 +0000

    claim nowhere person fork between

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
