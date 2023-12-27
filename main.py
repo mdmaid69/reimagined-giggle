7f74c262c8ef3cd6c19da0c0e52e1cc89ac8ab38
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:53:26 +0000

    conclusion mark artificial switch back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
