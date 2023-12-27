0029aba73cebe1e12ffc3bbfdd3667b7cbe0188c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:52:33 +0000

    read dear exchange ear sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
