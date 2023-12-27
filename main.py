90cb15fb0249d93779d8260c946a2c135d356e61
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:23:51 +0000

    feel reasonable waste shelter accidentally

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+for i in range(10): print(i)
