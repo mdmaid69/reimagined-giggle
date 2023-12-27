51159c6e50a6e7113fef527aa220657f222ab99b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:46:43 +0000

    problem everywhere steady loud trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
