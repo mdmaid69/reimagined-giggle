e0677903844aea5e8cc9661f90b80a65bb75828a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:39:00 +0000

    react spot prepare order grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_exponential(x):
+        return math.exp(x)
