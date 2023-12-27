1f8d0a933c086d6ceb451991bd701c976e1180cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:12:27 +0000

    always gift war consult pour

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_density(mass, volume):
+        return mass / volume
