3c77e80d8807d975c276288c656d6964c1feafac
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:07:14 +0000

    pull touch relative no OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
