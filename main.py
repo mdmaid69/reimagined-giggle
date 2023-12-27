b5b942150b1b055990f58b898899c180c6fa0a45
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:32:40 +0000

    load rest nigga waste minimum

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
