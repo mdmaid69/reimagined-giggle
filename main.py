1c5b82ed2ca71d937f0b32303bcb84244e844ae6
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:30:09 +0000

    calm cool miss middle steer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
