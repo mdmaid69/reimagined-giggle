be814e198593ef75ab14033c25bacfc3ee169e78
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:39:10 +0000

    discovery deliver his exploration situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
