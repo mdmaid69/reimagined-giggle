4364cad25ea32f4de52e29565414b50e0a9fd49a
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:23:59 +0000

    watch work 570enjoyable act dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
