459e97eef97ffcf85637ebd4585a9791504d515e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:37:57 +0000

    closed breakin middle hardly safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def convert_to_hex(n):
+        return hex(n)
