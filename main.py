d674406419eacc5a23fe13871b0741e8e27a1542
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:38:33 +0000

    show oil exactly comeout accordingto

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
