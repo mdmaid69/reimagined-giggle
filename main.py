090da96552494b9742d3dfc0d1462bddc2939073
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:59:09 +0000

    spin combine sound refuse under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
