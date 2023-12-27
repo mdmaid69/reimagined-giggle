f9a09f9fa20add53e883f3243428d6bce9cae32b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:28:01 +0000

    weekend unlikely speed confidence stranger

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
