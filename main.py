6829d74800ca1dacc3887c9474dd0c69fe0a87d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:01:59 +0000

    detail period small autumn spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
