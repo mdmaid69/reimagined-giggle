87ac0f25e62f633c84bc0f2d4190ce67f19469a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:37:24 +0000

    switch quickly he tired Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
