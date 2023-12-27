4032fdc56bf11a83a2a2536bd084a7e4dfcc63ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:38:47 +0000

    stick die bag whisper stamp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+print([x**2 for x in range(10)])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
