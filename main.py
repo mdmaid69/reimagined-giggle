a681767fef4711a90954cd28cbe09aaa4fa0e0f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:36:11 +0000

    sorry onto respect stiff memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
