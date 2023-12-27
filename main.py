3682c8d228f99af9b0ccde0742ec879fe27a3c12
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:25:15 +0000

    fall detail hurry unhappy Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
