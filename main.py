9ff7c13bd029259077d9b5e6e0d9d1f5c941e24e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:15:07 +0000

    except match land add emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_array_to_list(array):
+        return array.tolist()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
