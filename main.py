7fd218b00b6e1409d3d853cbffa7cf4463e07173
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:54:37 +0000

    party pool talk place well

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
