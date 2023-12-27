b8af405a84d9434d19d30f7c0fdbaef686e80485
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:30:56 +0000

    house takeaway room uncertain market

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
