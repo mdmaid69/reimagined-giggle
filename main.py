cc5f36d8cd55b235d1ec74391ecfab99b98ccb1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:46:46 +0000

    run file statement book break

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def find_min(numbers):
+        return min(numbers)
