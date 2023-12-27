35c2a1145986d3f47aebdb1335132162ae98bf7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:45:45 +0000

    earth independent permission move jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+print(re.match("h.*o", "hello world"))
