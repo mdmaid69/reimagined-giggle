95a4745bac84a9d507ddbe39cc97206c805a2b7d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:41:39 +0000

    mix male neat rest soon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
