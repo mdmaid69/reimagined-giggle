bd9d0286d5200b6a33910cb22a5740c4a6e7bb9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:11:11 +0000

    Rr both tidy string regularly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
