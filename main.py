8deda568e1846d877163de3e96c7d02194a14480
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:22:22 +0000

    remove medium image emergency frighten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
