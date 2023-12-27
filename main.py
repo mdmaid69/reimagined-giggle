09dc3492c373b07b2877e03ecac5a66de0a3e85e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:38:28 +0000

    autumn do form circumstance content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
