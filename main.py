7c9e7442b2924350ac9cc6592330a0169b95d510
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:40:11 +0000

    form quickly admit off plastic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import getpass
-def get_username():
-        return getpass.getuser()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
