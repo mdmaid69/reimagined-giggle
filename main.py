ebe5f52fb325cba0ef77e3781fcdfd9e2300db8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:51:41 +0000

    flow practice house lead sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
