f4420dc8446077b9000c7c9e9c5e8a8550140a23
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:58:18 +0000

    arrival argue sound ignore law

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_work(force, distance):
+        return force * distance
