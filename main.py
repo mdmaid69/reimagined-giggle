2090e5cfc3e51db69aef6b61c95356723ba0729b
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:34:10 +0000

    someone act ear basis which

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import time
-print(time.time())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import json
+print(json.dumps({"name": "John", "age": 30}))
