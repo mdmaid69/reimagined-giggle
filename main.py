bcbe4bc17f48d1b98f65336222c50a120faabda0
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:39:47 +0000

    even always decision freedom kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
