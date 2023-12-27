683f04318b9281a24dae483115e758c519e0e124
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:52:01 +0000

    modern recommend cost read famous

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
