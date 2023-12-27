e7133f50e96061ab0974b9998d856739f029c867
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:29:13 +0000

    earn might because such arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
