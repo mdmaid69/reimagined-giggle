5e342421f181c60c01c506df72cd893bc9cc090b
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:11:20 +0000

    move shame silent cheek charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
