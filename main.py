5c01f0233eb121aacb62042519be4816dc850d80
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:42:50 +0000

    disturb action surprising bath settle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
