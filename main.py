2dcdf0676b92bc404c8af55c0ff168e513e794cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:46:12 +0000

    human shop getout empty order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
