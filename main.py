e32f61d77353135b171320ba078afac00c721c29
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:06:01 +0000

    time accommodation exciting shape equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_even(n):
-        return n % 2 == 0
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_repr(array):
+        return repr(array)
