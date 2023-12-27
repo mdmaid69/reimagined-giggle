42dd309a5480381b2215405971f02aad7c967329
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:09:23 +0000

    ashamed laugh exhibition close trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
