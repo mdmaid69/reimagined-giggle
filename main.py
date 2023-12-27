6e24b968e942be2b5bea4f9fcd9f2bbff46776fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:02:11 +0000

    fit occur extra crash turn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
