020f2501f09c01094a35875757b944624772989e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:52:10 +0000

    good anywhere home lookafter type

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_even(n):
+        return n % 2 == 0
