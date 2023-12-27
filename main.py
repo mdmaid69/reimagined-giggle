1fa07ff295c031a1525a85d3f148abfa07881f9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:09:31 +0000

    mixture breakdown wide dick your

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
