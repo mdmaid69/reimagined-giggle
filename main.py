bf0df91d06d454f8eb46680f3adb9019539c502d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:03:59 +0000

    mouth history beginning return supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
