c549d46277bc7df689c9a26ab1447e3cbda2cd02
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:44:43 +0000

    meet thin unit sit much

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
