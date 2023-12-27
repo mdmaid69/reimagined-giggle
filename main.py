f1c260f6b8547a231148bfa71a66cfba7f24a794
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:34:50 +0000

    attempt tax serious describe music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+print(sys.version)
