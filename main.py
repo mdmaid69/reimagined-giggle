ad355f3ca6bd66bec2c0c8bff334313bbb8a4470
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:10:49 +0000

    tune perhaps lookaround about hope

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
