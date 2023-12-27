c6a7ebdf8ce77e34ec3d01c99a53389c08ef6c75
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:28:49 +0000

    smart backwards behaviour eat uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
