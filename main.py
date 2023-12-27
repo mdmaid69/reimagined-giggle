5c7edfc935a9443792d21e626ad98940298a104a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:58:30 +0000

    share first owe wait shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import sys
+print(sys.version)
