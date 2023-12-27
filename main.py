f88ebabfc4fc389a7a900b4483bb89804988872d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:54:13 +0000

    improvement which last have another

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
