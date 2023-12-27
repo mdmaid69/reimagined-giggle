d04904851e9cc86fae3166034fa7af30adca5e32
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:57:15 +0000

    test fork treatment squeeze fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
