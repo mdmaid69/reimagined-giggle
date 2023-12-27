cc5022f9909f12daae4fb8f6d4fe5420467bb743
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:31:48 +0000

    between refuse stay threaten no

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+print(math.pi)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
