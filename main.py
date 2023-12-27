5f2db101e44f6cce23f988c08c745fc6b9ff0c20
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:44:43 +0000

    themselves lookback(on) black manage regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
