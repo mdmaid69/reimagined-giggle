8623806c9c93d66cbd33c8769f74adc276f4910f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:25:55 +0000

    declaration word step heat hair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
