86e87f15e14719e958b6a6466d2698ce8ad4b18b
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:27:03 +0000

    taste but charge later basic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
