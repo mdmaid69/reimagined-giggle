ed5fa9b8f8a9b857a14c6e7f8e79e9a6f76706ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:47:44 +0000

    file check weak neither build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_acceleration(speed, time):
+        return speed / time
