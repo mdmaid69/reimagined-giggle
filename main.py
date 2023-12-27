e12c93798dd95b8803297af7db72489c8e3e86e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:58:15 +0000

    accidentally display slip possible limit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
