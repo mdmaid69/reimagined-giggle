c701acf4a429e781c848bea45903af348c094d6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:16:39 +0000

    lunch world 568energy wild boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
