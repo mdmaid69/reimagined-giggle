acf4d44c86141fdbf8a621425e4810ec834421f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:57:10 +0000

    charge today public evening entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
