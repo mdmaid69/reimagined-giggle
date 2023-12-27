03ea13bba6cf024d88799f1e3125191da0c8fbda
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:03:41 +0000

    comfortable method earth most careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
