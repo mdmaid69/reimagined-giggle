a049e416e3a7a7d8d59b1cb51095e66d85b0abe1
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:19:14 +0000

    hang favour getback plan platform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
