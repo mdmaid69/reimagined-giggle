bcb23c8bbee9bf3fe8965862c023770069a87126
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:50:08 +0000

    hang just loan heavy goon

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_list(array):
+        return list(array)
