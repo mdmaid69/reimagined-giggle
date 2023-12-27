cc63e53720b9557ec54bee91b2fbce8063a152ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:13:33 +0000

    571enough brother considering should crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def get_array_index(array, item):
+        return array.index(item)
