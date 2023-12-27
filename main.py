dbc6fa56c7590a7ac190d5ae9b52a9ed9b5a359f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:29:21 +0000

    activity drop later plenty turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
