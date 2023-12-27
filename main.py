e98d00a069b38fa22393689932edf5c3a892af2a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:31:43 +0000

    then friendly comeoff Vv paper

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
