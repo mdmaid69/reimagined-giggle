440654da600cebd20daf9e91fd8188fe8720ef5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:25:36 +0000

    convinced value react tie earn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
