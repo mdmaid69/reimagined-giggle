243155024e7172c2a5d89181f83ffc721d03be68
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:27:21 +0000

    surface gift side consult he

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
