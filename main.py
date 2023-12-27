f59ed9b44d1b47080caaf3ceec7f2468a0c24a26
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:53:00 +0000

    loud upwards disturbing suck often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
