e69d7b8205c6b845bc05523a0015bb48aca9bbc2
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:23:14 +0000

    before telephone seriously achievement dear

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def remove_duplicates(lst):
-        return list(set(lst))
+print([x**2 for x in range(10)])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
