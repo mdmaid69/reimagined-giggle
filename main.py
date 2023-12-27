a69404f4fa1ccd354094bddb0ea9c5fe70079b20
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:22:26 +0000

    professional discuss scare workout birth

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
 import csv
 def read_csv_file(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
+import array
+def get_array_as_set(array):
+        return set(array)
