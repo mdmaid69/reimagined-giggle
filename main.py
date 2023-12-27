feb5d7dc4df1ebb495d812fcb6fa0e6ca278dce8
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:25:23 +0000

    rarely takeout back wash consider

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_power(work, time):
         return work / time
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
