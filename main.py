b021879e716d0038ef641e79a84fa6d9a0c966b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:32:21 +0000

    little but hers adjective goup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
