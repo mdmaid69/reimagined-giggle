5acde39de05e273c40ec8944779de6ba701c2c7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:13:23 +0000

    art control support partner related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
 import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def get_array_as_repr(array):
+        return repr(array)
