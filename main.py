3ac58053c7a48572f855bd68f054a49bc667fd03
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:36:41 +0000

    out tight suit goup opponent

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
