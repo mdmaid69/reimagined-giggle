04827a61dc8d82b0d3b737d025eb643175f2935a
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:41:50 +0000

    keep load willing correct tap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
