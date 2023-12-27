e65de9052da1e82e083916461e58cd147186525a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:44:48 +0000

    gooff tool respect rank piece

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
