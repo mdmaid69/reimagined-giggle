1ffde160429bcb5407ce84c7a83529aa06d0c787
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:34:38 +0000

    suggestion call sticky description rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_current_working_directory():
+        return os.getcwd()
