ed3f389ef5920bd2792f0a300e547f87739b87d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:10:45 +0000

    school for coffee across exploration

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import time
+def get_current_time():
+        return time.time()
