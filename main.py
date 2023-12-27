64f98caac070ae20833899bac8e5cee991555b24
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:22:40 +0000

    its quantity hello confuse model

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
