ea5d914ee76734c9c07d1a617195a121a34ecd22
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:37:04 +0000

    creature dish follow best account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
