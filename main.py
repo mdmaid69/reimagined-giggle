d73542d130b6d52076843d427b4f6eeef5be82ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:57:49 +0000

    big ought line before connected

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
