1a043a55c341c823b80fb3702b591cf6ddc9ed55
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:18:10 +0000

    wrong fold impressive movement downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
