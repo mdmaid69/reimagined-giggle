e2168e882958a350bc4c95007d7285a4cae50833
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:17 +0000

    creature dry fence form squash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_set(array):
+        return set(array)
