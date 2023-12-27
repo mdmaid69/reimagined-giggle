b3950ce90a2b8d819e90cca52f6001b7691950e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:36:42 +0000

    birthday lonely anything lonely official

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_item(array, i):
-        return array[i]
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
