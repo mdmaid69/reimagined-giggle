775f5c1febc3e33163c5aa0f5e1fe01c401111be
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:51:24 +0000

    content answer even though short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
