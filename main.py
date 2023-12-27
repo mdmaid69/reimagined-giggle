4f5328f1b3b41f8a5a82943826807aeee64540b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:46:40 +0000

    hollow practise home although purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(numbers):
+        return max(numbers)
