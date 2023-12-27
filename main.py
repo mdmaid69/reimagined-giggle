427e8cbe8d5b3a92adfa69de3a909283c014a6a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:16:52 +0000

    pronunciation trick equal calloff loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
