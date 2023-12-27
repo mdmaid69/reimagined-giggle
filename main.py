3b7f0fd1be5521f3bf419ed05da7fa0e4424a9b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:02:06 +0000

    politics part address kind plant

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
