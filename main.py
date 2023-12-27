f6def9f75181ca7dfd7c891b61c0ce4aaf4e31be
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:02:40 +0000

    emotion blow report probably course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
