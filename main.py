40722ebd74157f7577c60d3b20d661cb3bde4478
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:47:31 +0000

    giveout disapprove play goodbye try

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_typecode(array):
-        return array.typecode
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_pressure(force, area):
+        return force / area
