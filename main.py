682ab5fe7d970b8ebe240cc27a82d61143f7433d
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:06:57 +0000

    practice impatient thin care over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
