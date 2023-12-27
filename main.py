ad4ec5760508beba2ff8dd5f7ffe61ffd7826d28
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:08:35 +0000

    full continue can criticize convince

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_array_as_list(array):
-        return list(array)
+import time
+print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
