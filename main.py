54e6d26f2ad163c8734463ac27f1e25448d3cbcd
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:22:31 +0000

    there runinto movement plate guess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def append_to_array(array, item):
-        array.append(item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
