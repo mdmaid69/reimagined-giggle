f6ea38a1eb87c125dc3f62a1c113a0054e41410b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:28:32 +0000

    warning and bar bite basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
