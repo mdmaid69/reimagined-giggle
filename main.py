25f619fbe8186788b08fedc89242fa5b4f83166a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:38:59 +0000

    insist note always shallow back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
