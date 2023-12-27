9ecabd62ade3aea2aaec03629accc6739ed74a79
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:06:46 +0000

    tap temperature shame shut least

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def divide_numbers(x, y):
+        return x / y
