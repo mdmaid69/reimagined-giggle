38862a5ebe7cbcd8f25b1ccd8e7086344300a461
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:40:34 +0000

    scene principle whole serious connect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
