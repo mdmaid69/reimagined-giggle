b9a3c744d180af6205b41aa40f0b3e7d55685873
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:40:37 +0000

    scrape habit easy 572enter sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_base_name(path):
+        return os.path.basename(path)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
