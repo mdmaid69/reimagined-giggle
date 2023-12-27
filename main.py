01a04b3ee3c998b8d2b26b84e45be8a5ef4085d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:54:00 +0000

    takeaway person tune only turnup

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def multiply_numbers(x, y):
-        return x * y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
