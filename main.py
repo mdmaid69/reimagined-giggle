50f06d340377c03b23be3828ae2a65867c4f234b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:50:07 +0000

    battery dance event someone ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
