530ec708c76d1220f204fbc972f690b3e907f1f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:21:18 +0000

    quick sell target battery through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def add_numbers(a, b):
+        return a + b
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
