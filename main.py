c90f8957573775fc959263a3fae1fd8d0228d7d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:06:58 +0000

    benefit whistle runout block crowded

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def square_number(x):
+        return x**2
