310617b488d954f6b2720b3fd2e8047e899bdc5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:44:51 +0000

    sure broken play guilty respect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
