e86474e2b38cc44aa8115dc76b2c0ff314fb9c51
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:54:08 +0000

    dear head skin environment guess

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def reverse_list(lst):
+        return lst[::-1]
