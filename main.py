375909b4404560ae1c51692943fe2333f6c83c90
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:52:21 +0000

    win traffic production wish comeout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
