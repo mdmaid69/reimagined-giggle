2efd718eaf33eb2168ed3ea7328f815873345bbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:32:30 +0000

    full imagine change broken now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
