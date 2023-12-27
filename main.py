56e29d246b4226b7f990eb72ffa8473c8b24cda1
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:42:02 +0000

    release shut direction certainly comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
