d8713a1ab312ac6218de9bc57ded01150352bd8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:28:34 +0000

    both goodbye bet error owe

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
