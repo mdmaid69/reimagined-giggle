78b4f3dc32f35accd23bbd4a433dc07703951936
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:49:55 +0000

    can gentle class muscle gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
