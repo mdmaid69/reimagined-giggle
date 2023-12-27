7c1b63c4a077f77e0247cede68d55bbd9b25cf8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:41:33 +0000

    thirsty space swallow one comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
