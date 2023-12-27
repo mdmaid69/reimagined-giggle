f7adac17ed5f5cfcef07449fbe5cfecc90146179
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:50:38 +0000

    bend autumn share Ll keep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def find_min(lst):
-        return min(lst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
