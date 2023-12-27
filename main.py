63ac9e32528d9a18a4d2ffa581cb4b468a748e65
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:02:54 +0000

    type backwards section 571enough cold

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def convert_to_binary(n):
-        return bin(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
