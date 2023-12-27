e56f033d69f1dc72fa0dba43608ba7cad85f2765
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:05:43 +0000

    shut house circumstance risk classroom

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def count_elements(lst):
+        return len(lst)
+def is_palindrome(s):
+        return s == s[::-1]
