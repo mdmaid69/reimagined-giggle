9946c6149d2cf10b1ee041d924460ba7e8d44021
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:04:26 +0000

    operate spare soft loan another

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def is_palindrome(s):
+        return s == s[::-1]
