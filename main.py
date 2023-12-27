da0deaa1a787f4ac549a69ea2c7a654c172e0c18
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:32:16 +0000

    guess real lead relative job

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
