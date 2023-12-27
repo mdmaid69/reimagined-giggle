9eb2a4d33b46bcc311b691f4c561c37b49be3a40
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:04:14 +0000

    bet argument list copy care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
