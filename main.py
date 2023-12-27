804492b1b6d51a6af79cb5265b4cb3e6eeff23b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:18:39 +0000

    week tape secret well depend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
