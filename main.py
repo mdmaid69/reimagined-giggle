afca2aa60c95960ec201857957926983da9cd049
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:03:52 +0000

    separate track formal wheel now

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
