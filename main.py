3185665e1c623075c49351584389aad07cf818a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:20:04 +0000

    hurt 571enough clearly ear comment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
