6d86169567979e6b59d711702954148e07ec1697
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:10:38 +0000

    wish to hang idea movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
