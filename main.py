277b78a158cf98433d9eac07d28dc6ffa9027904
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:39:07 +0000

    vanish correction cause grade very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_floor(x):
+        return math.floor(x)
