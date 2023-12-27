cd97ff8fce2e79c68da788a95487e1b986bbdce7
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:47:48 +0000

    foreign generous coffee shame runover

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
