1dac124af951a041a98f816dd3777aec261bcc29
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:07:04 +0000

    fortunate delight off us tooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_work(force, distance):
+        return force * distance
