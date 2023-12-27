b55ecf93958c29c9c81c2179686e223ef4f8ed8f
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:53:25 +0000

    smart subject future perhaps unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
