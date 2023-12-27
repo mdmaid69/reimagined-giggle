e5e3328ac478f14e85cd4789c1e99912960ff8a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:21:13 +0000

    male whistle surprised seriously conclusion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import sys
-def print_python_version():
-        return sys.version
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
