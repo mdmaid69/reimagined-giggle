c58274a8ffe1668a6ac9a3eebdb2017d974435a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:34:49 +0000

    blame now soft check prove

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def sort_numbers(numbers):
+        return sorted(numbers)
