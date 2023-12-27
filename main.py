d12704c7b5fefdc496c38306a061b35d26c6da5f
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:30:03 +0000

    vision substance tune allow warning

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
