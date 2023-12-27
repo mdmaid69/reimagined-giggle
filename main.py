7eec93fb08b286cd36cfcd5aac6a57e14a9426aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:18:15 +0000

    miss suppose tip protection surprising

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
