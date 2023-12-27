ddb11c709997cfd9fc2079fe68490b27db6366bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:57:50 +0000

    occasion closely have object callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
