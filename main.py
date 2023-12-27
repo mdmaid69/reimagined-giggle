26229eeee67df5263d835661b9f32151e68be65d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:43:52 +0000

    dangerous level conclusion detail giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
