8880b7caa39da18efb4cd237883ac56d332e1df5
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:42:56 +0000

    innocent task translate behaviour division

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
