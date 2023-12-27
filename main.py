223dd33f48e7386646d652f36dd50383ce5a3ca6
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:01:30 +0000

    afraid polite push chase considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
