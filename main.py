aea3021cacd6af60e3180f191fcd4b7b932f30c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:20:14 +0000

    delicate likely back theirs answer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
