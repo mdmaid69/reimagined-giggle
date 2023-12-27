5bdf470a8c7ae30bbdcea5664ccb05f2677e759c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:55:20 +0000

    disturbing prepare special safety control

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
