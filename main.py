76249a2744c6057e2d56256d9b5616068496012b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:51:15 +0000

    very task base maybe building

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import getpass
+def get_username():
+        return getpass.getuser()
