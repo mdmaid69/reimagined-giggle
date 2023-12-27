19fc032449306fded2fe44c44ea51bdedb3b59cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:31:02 +0000

    region pull process guard comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def reverse_array(array):
+        array.reverse()
