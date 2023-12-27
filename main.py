1f58487fc048664001d24b5a5fe3a5414716eb6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:06:49 +0000

    takeback care hour splash test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(lst):
-        return max(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
