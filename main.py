4142051b9d6b4442ebbacdd2a3cf414f84f5a612
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:46:17 +0000

    emphasize also recovery human nurse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
