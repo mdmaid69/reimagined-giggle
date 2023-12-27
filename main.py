2004f4a064bed1cdc1859f21f20c275e75267529
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:17:37 +0000

    circle software slip callback catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
