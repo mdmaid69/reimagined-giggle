9d60a6f96c95d61e574be14890564c7abfb735fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:02:59 +0000

    total announce child situation needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
