e674aecd3caa66bff0989112583133f0f59a5a62
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:53:15 +0000

    stupid vision damage fashion broad

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-print(os.getcwd())
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def add_numbers(x, y):
+        return x + y
