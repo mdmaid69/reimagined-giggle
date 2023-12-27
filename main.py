25be6660755c8110913a58657d58e92d0ae18550
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:29:58 +0000

    quality you offer uniform cough

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def divide_numbers(x, y):
+        return x / y
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
