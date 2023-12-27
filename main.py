68db1bf11f9f99a6ca936a0ee2dc711a052519a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:45:44 +0000

    deliberate comeround immediately insist cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_user_list():
+        return collections.UserList()
