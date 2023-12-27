61cd305ee41335e0bb068a82e69662f033d37948
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:18:21 +0000

    memory rather ourselves takeoff neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
