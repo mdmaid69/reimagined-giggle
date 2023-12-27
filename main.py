294bf33f5a69a33d9ab3ea45824dc883dd8f076a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:11:43 +0000

    ours get little character performance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
