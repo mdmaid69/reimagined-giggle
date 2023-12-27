8c9e6687a616fe6c478d6e511b05f6e81cdce701
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:51:30 +0000

    trip tense empty shape spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
