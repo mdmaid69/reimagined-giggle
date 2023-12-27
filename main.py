10c42212ed192a3f1d60739a248cac8dd4953d3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:48:06 +0000

    its dear since crazy long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
