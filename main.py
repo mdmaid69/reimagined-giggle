77b0e45816f528c26d6ff0a7bf642d60ff9d63b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:26:08 +0000

    maintain downstairs crash mess lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
