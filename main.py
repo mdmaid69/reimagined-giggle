c5fb9a67342445153c53b334048e941206f775f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:54:14 +0000

    detail design grow wild what

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import logging
-def log_message(message):
-        logging.info(message)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
