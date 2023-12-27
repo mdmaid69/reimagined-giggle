70f183e886ac4f24f89224862cc824f0aabfc448
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:20:12 +0000

    similar clear Ww nature priority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_acceleration(speed, time):
+        return speed / time
+def square_number(x):
+        return x**2
