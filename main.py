8548d218e637066d36ea0d66818685125d412a96
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:59:36 +0000

    hear division extra getback delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
