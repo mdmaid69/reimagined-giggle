d1ca2b8184302407f9aa2af9539504b0eec7f655
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:45:35 +0000

    long both crash will float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
