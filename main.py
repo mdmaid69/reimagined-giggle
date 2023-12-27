009b7c0da2141c7cf704ec6f37e4e3fe352d9430
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:38:55 +0000

    polite read in advantage meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
