a3fc903b1806547305976c562b6a3103f56b5e6b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:25:32 +0000

    support search rank schedule thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
