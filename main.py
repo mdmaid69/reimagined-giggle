23f8f95d1f3154ecf463edf4726c9260fcd5a866
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:57:52 +0000

    knowledge vote building improve sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
