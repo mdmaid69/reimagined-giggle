0f73c499b8935b95f5e2689a29b1d2654773c5fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:26:04 +0000

    stick disturb recent shout stare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
