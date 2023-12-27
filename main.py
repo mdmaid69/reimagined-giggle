9ffb676cedfb5f0e034f094426fda4e08344c658
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:57:45 +0000

    scared innocent contain refer suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
