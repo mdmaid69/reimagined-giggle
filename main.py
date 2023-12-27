c1bbc05715da6a23d0adf1924ab04c5b4d7fe05c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:08:49 +0000

    mine quarter sense way breakout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
 import collections
 def create_user_dict():
         return collections.UserDict()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
