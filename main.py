f80348f998a0b2b07b011a513ad4aea031e3885b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:33:28 +0000

    further check blame welcome attract

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+def get_current_time():
+        return time.ctime()
