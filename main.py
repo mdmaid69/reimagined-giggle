896e2bb6a9b0ecd129a803dc54e350692897cedc
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:32:47 +0000

    onto organize murder extraordinary beautiful

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
