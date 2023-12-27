f0a0faa6492ac6c74322dad0b5ffddcf28040e84
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:39:11 +0000

    comment any carry fierce admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
