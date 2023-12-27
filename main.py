dca2fc493e6f6a7efbaa709c5bb1c54b1b308f55
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:22:23 +0000

    form steam any fasten beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
