ebe35caa4ce1cffd5ff4ec017a3826884bf99f93
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:49:13 +0000

    sting exact satisfied appeal mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
