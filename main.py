915ca1e92a2ccae65bc31a7faeabf7015805f37f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:26:37 +0000

    might colour crowded what worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_power(work, time):
+        return work / time
