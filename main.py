ba7562e71c79c7ce90de5a976dd367f1fd91fe0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:07:07 +0000

    song deep lower race emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
