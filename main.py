094ba30f972c3ea03bcc181fac735b1e172769d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:48:48 +0000

    fine phone rain thick hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def convert_to_hex(n):
+        return hex(n)
