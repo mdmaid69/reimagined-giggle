5c2107432a52c38261ce503d8bd48b80ff88307a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:02:49 +0000

    face release calmly fall strength

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_density(mass, volume):
-        return mass / volume
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
