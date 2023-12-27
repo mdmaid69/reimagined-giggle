300a98fa0e967abbe5129219cf50efa788397712
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:29:40 +0000

    someone wire great correct comeback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def find_max(numbers):
-        return max(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
