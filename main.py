3a4c8ffa3de5d0c2d89bf15fc1a9f36fb73d48d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:29:09 +0000

    hard course door come snow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
