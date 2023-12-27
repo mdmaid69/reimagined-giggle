8e96be0d386bfc5c30b1d8e1120d1037f28b0279
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:14:45 +0000

    better luck giveout rush account

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def load_csv(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
