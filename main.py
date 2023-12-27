8eb22e7ae29ebc5f5c7fa056669eedea28ee4bb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:16:01 +0000

    not letter after book ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
