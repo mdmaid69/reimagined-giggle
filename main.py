9aabf81ecb99a47175168f91631ed08c3f5b92f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:56:16 +0000

    boring comfortable negative contrast better

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
