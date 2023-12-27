e63480aa9c63d50f7e4a34aaba2d61e9bb4fbffd
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:37:43 +0000

    brief string go failure Oo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
