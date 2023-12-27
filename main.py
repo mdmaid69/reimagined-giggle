b5bf520a62e161613c299b3b8873e9a41de077b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:42:53 +0000

    gas really edge better rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
