52840ae5b885678678082b10cc969b57ccba584b
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:53:37 +0000

    kind common calloff breakin contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
