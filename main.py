6c69fa663be7ca7ed50f46ded14850f6c7fca8e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:46:00 +0000

    improve block change defend relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
