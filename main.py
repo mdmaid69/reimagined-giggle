4707289eb483fd57cee0ed31b000b87495bc7c93
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:05:28 +0000

    shoulder mistake shout warm together

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print("Hello, world!")
