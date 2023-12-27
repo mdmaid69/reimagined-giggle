f3a12bb5f18c0bde0386aab532e2104ac72c4983
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:33:31 +0000

    how price rough us takeaway

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
