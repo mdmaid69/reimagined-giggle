573f3b3e65a8dea6ef3ce08b537b580a2769c50f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:38:32 +0000

    concern government theirs same summer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
