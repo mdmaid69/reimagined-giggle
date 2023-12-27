d965056b00ca822d7b10f5e3afc85abd34c0d6aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:26:06 +0000

    education wash train trick sound

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
