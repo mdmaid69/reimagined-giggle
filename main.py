903ab3d88a60787f3842f049a74608691921b778
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:36:23 +0000

    sink forever listen flavour glad

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
