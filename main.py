9fcd1717cac7ecf539fb0ae1ade2b49393731902
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:36:07 +0000

    road OKorokay keepup permission terrible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
