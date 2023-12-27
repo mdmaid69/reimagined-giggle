313baaccd222b2ae5a193644b200d969f6723c82
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:23:09 +0000

    approve complaint environment event hold

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_density(mass, volume):
-        return mass / volume
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
