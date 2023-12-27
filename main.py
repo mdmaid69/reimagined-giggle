c2fc0d266933c72965e0390489a08100a5bc1684
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:45:37 +0000

    doubt wish sensible dance progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
