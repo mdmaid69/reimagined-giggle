da46f7d54a32db5458d04eb5cd5497a08ee7a2c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:10:20 +0000

    they Bb Jj whenever surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_item(array, i):
-        return array[i]
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
