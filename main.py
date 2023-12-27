b845fda05fa83572214eabede66a584ca535f317
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:47:07 +0000

    distant present dance church rubbish

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-name = "Python"
-print("Hello,", name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
