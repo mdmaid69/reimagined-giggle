370deb9ac0f6741ffab9e96c348ac0a9db622ea3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:06:47 +0000

    whatever urgent no powerful feed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
