30e8c349d3e190ab10292df5dec8ff0462e8c037
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:20:51 +0000

    brush destroy development guess cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
