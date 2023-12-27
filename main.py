256a590a984dd026e1e1694dd2e1b856e3a6eab4
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:09:04 +0000

    unusual giveup private contrast badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
