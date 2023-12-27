3a427dabde9a8be59ed408b5fcd6e64a09b7586f
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:56:07 +0000

    crisis pretty wing wide 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
