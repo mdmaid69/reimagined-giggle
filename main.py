59990f37330dd4e435a49214a408cb6be9631b54
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:38:12 +0000

    fresh complaint fashion concerning excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
