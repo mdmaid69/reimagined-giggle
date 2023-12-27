330fc0c8e546643bcb07de0b2e62c11e5753fea0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:10:38 +0000

    pretty workout rule lucky next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
