ca7a9e75922361f964bec0a19e7d233105afc9bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:00:04 +0000

    majority pin mend favourite behind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
