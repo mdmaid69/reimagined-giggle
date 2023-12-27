c8ad2b1a54ab15a35b46d47e47cd6942d4a74fad
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:55:37 +0000

    region own pleasant neither black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
