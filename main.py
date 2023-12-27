c9bda33ba3c5e7d55d2f48bc7c1675ac87b70ad0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:23:34 +0000

    tend open report his separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def find_min(numbers):
+        return min(numbers)
