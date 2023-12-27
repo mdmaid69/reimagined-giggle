92c175f932e2311eea64a46da6f3fbf78a0b81a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:56:14 +0000

    mail breathe laugh still feeling

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
