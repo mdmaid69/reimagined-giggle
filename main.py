3f6a0f3f4a5212d9b9845f8adc922028529e2e12
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:03:24 +0000

    though official most deliberately future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
