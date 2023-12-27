9510ea62235f1fe8d649c3b18db28be03ba9d166
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:53:47 +0000

    determined pain weather copy defeat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
