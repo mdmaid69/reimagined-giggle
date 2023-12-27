1ee97d0a755dde660975861f3c388055e720f516
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:44:21 +0000

    spill delicate exam use uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
