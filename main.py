29f003b79d063fa0b96a222cebf939df6ac75bfa
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:01:25 +0000

    arrange behaviour slight workupto mean

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_pressure(force, area):
+        return force / area
+i = 0
+while i < 5:
+        print(i)
+        i += 1
