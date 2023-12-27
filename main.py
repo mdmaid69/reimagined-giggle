5d00bf292939c5959020c26f75dff1bee290fa81
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:47:27 +0000

    takeout underneath reduce mean comein

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_stack():
+        return collections.deque()
+  def subtract_numbers(x, y):
+        return x - y
