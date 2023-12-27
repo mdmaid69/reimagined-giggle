0e470e1d0a84908cb3c8b1c4eb6cc81099239098
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:38:48 +0000

    upstairs period official suit aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def reverse_list(lst):
+        return lst[::-1]
