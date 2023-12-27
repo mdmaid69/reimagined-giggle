6811267b07834012191459e2ca839ab89d10da48
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:35:14 +0000

    hope clear pure person up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
