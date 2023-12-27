2815e8bd75122d243e20d4646b6b6fd15c6edf18
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:35:40 +0000

    good him unhappy pure reasonable

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_list_from_array(array):
+        return array.tolist()
