3b279061f52bea4acc66478762a62e264412c924
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:11:55 +0000

    takeover possibly congratulate previous single

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
