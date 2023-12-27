f0d6b3c85d276b1bf84e4070dfa1e11d44cf14c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:36:02 +0000

    aim opinion allow move away

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
