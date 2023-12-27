90328aa016a3b20f2825f820ca31086580ac95a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:56:20 +0000

    situation value greeting die stare

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
