c9a059f7fd72539425a7a602ce8580ab09530bd1
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:02:44 +0000

    capital workon computer influence minor

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
