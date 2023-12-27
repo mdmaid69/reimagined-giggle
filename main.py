81bc298316fd6abdd4fafd0f91e8373352f66694
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:51:18 +0000

    try study 179bite attempt door

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_current_working_directory():
+        return os.getcwd()
