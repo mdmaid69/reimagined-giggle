d93e1aef0933b9f7b5927a334f256fa982206195
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:36:53 +0000

    disgusting care pretend embarrassing issue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
