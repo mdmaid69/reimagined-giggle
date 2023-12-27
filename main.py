a0fa9ca63f68cbbfd946f1d8a3e787d79eb6f9d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:33:02 +0000

    holiday operation little generally whichever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
