ed4f65eb7bb858d0193908bd8da48a94a657bd86
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:27:49 +0000

    event introduce organize best what

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import sys
+print(sys.version)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
