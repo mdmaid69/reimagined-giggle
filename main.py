dd81daa0d0d731cc7e0c7762374e1099a0f73663
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:16:21 +0000

    Dd future result make lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_even(n):
+        return n % 2 == 0
