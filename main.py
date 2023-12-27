e796c83515bf465dede7ba08910c1d2ed3e02402
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:40:32 +0000

    beginning complain use trade switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
