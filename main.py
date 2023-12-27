7cecb968179f181b1214db5b84e75c17ba6c7be4
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:15:22 +0000

    look couple coat among law

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
-        a.fromunicode(unicode)
+        a.frombytes(bytes)
         return a
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
