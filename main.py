592e3cc5453fc64aae638254a99ef28838efcb4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:25:12 +0000

    bet house pass last slide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
