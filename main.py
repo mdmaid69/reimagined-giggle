c56a20d35763702f796032a0dfb2ac4d1d0371d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:37:06 +0000

    consider arrive thin rule concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
