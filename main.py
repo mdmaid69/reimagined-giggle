25b310ec833a92dc182dc18e08478e82280ba2ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:04:26 +0000

    report gate year information question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
