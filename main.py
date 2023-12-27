91e38d6310b78dbb6fed25222fec4733b7d27ceb
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:12:33 +0000

    comedown 179bite Rr follow food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
