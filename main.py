6e2e278f694ec471c1844a76a79e3ce7cc97f964
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:38:50 +0000

    coat bored challenge accident turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import time
+def get_time_since_epoch():
+        return time.time()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
