e2985bfa1fd7779d28f480430c12169dafa846b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:26:06 +0000

    thinkthrough check seat suppose defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
