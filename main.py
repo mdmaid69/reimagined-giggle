daf15fb4ac567b586ccd701901e3bd4d6864c65a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:31:04 +0000

    quiet professional intelligent sail forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
