40456f6ab433de43526474c21e6c89f36f2a2fce
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:59:34 +0000

    chase defend contrast close turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_queue():
+        return collections.deque()
