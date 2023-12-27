1b77060e89733536cd8f38f271bd06ad27bbe0f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:30:39 +0000

    information bin ashamed mind grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
