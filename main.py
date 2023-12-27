dda9835ceb1a6be6dfcc7dafc8000d3328146c8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:01:35 +0000

    minor summer second medium pay

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
