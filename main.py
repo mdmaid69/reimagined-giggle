9360094e0345ea390c62b8482bba1178901f3a77
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:13:32 +0000

    waste announce empty later politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_acceleration(speed, time):
+        return speed / time
