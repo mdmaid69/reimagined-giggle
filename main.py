7b10ba3b430e4d933b3bdd0d76e7268f700b9d52
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:37:47 +0000

    gooff offend escape education very

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
