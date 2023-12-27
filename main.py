7d77e91ecf7a642220d57d43aa6a9b485ff79134
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:41:51 +0000

    band occur thinkover run objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_device(file_name):
         return os.stat(file_name).st_dev
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
