3377056070435abb9d96032a239e63a09338ec48
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:16:34 +0000

    smell giveout seriously fair design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_work(force, distance):
+        return force * distance
