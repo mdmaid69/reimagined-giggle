805e699f6c0ad9ed10d383f5967d8978aeb0f00c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:31:56 +0000

    shut kind geton but interesting

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
