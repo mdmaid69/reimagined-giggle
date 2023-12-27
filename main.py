849585c46a433cbc23c272f804aaf7dce5481a38
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:55:02 +0000

    mark office education beautiful cold

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
