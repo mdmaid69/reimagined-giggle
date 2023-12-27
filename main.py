d34466b79459c779fce038dae8b5202d41acb494
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:13:09 +0000

    yes pupil suddenly sound straight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
