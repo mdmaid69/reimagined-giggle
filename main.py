a244ae60337c745aaaddd4cdc905bfdffb6322e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:55:54 +0000

    spin respect ticket nervous debt

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_ceiling(x):
+        return math.ceil(x)
