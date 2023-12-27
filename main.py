21c4970e46d482f2d7582d625840d21817223832
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:30:36 +0000

    cheek to trip cause here

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
