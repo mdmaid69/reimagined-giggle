4719525335a389d66db2b8ab4ee7f34559502fba
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:13:31 +0000

    ball first dare still gradually

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
