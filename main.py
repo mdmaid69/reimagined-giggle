ed542c6c3590c027b443207cd87e25856c4d5296
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:20:34 +0000

    neither whichever minute drive both

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
