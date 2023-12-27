2350f1fb5e2441feb82a868bf443a565abc01b53
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:38:53 +0000

    film accommodation story knowledge undo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def sort_list(lst):
+        return sorted(lst)
