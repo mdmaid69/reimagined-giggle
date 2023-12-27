b8416a87ef49ef493e332d15a943757e71c95d25
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:00:08 +0000

    shape schedule smell regret around

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
