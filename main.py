4ffde64fa2c30f94a293ee48558fb8c0152fdb34
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:19:21 +0000

    walk Vv design fetch thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
