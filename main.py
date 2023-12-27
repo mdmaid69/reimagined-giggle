585c74ca71a5a51a482d626c2e26fc4f3b0ef018
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:36:58 +0000

    track rough build have load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
