a690bed79dd9363625967b528b672cf1aacf6d29
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:34:52 +0000

    over production however winter welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
