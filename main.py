dc20be2a80a0c3531b194c57d93c088b45b1763c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:42:42 +0000

    reduce comein hair pause care

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
