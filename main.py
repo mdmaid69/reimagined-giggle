bd02836ec0f789861c45aadb0b91a4966842726a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:20:39 +0000

    miss damage someone invite back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
