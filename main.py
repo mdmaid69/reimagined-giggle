0d7cfc333bf03b255291b92ec2ef5d48060407c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:56:26 +0000

    peace base limit relationship stamp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
