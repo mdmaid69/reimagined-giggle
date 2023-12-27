0927f27f3209e95973b07f7e446718ec247ced3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:42:40 +0000

    way takeaway pronunciation shallow deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
