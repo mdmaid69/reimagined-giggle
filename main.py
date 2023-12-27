dc91401e8416d065ca2c3b2125b7d385dbf0eeb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:51:23 +0000

    allow definite somebody Rr ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
