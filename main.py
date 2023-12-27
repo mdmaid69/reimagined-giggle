d91fa7b3ca78967fc6902fa2a03c24184ecb1354
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:29:36 +0000

    which recover plastic general pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_repr(array):
+        return repr(array)
