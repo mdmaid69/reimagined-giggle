914036cc7f6871cd3efbf9e5415bbd145a7fd52b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:26:31 +0000

    work half attempt lucky smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_power(work, time):
+        return work / time
