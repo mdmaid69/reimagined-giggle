af27c0513ea27ec436a89b8a56db457bd8008b94
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:29:48 +0000

    clear strong brother attract motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
