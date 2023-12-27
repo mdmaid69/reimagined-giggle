2b0ef491099172e6188de3e5db783f0b8ba34a81
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:04:36 +0000

    exact offer rank circle this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def square_number(x):
+        return x**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
