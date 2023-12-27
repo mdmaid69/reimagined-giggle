bd299539ab5ea80b81ed4d40748b27e63104b686
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:38:23 +0000

    comedown thought cut coffee preparation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
