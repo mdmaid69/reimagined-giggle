c90d3a56bb4e4d21e9d7ae0296566b6cf29d0cc7
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:28:06 +0000

    start fact both high fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+print(os.getcwd())
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
