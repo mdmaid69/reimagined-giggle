dd3abaff3a6a98c97cb325976439585b9afa2e89
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:21:06 +0000

    569enjoy divide unhappy burst permission

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
