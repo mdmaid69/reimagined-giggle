23484baefd5d50aeacdbe8bdff68b745bb0bf71a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:21:11 +0000

    smell different dish trick go

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_unique_words(sentence):
+        return set(sentence.split())
