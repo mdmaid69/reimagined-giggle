c80331c90db106c45ae5814168589b687ff27e61
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:13:28 +0000

    band criticism reply prize persuade

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def reverse_list(lst):
+        return lst[::-1]
