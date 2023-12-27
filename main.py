a06fa3cbd947498774d8dc31e7706c58b497eb6b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:02:14 +0000

    exchange people no persuade feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def greet(name):
-        print(f"Hello, {name}!")
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+x = 10
+y = 20
+print("Sum:", x + y)
