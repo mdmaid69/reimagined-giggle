73b3e0e87872b315fc18db77e2f5e33e0abe863f
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:05:54 +0000

    pity less consider instruction relax

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def reverse_list(lst):
+        return lst[::-1]
