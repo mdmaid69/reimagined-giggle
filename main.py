980b1866f037249c6490ac196980ccf3d763f5a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:30:05 +0000

    expect blue tidy road goodbye

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
