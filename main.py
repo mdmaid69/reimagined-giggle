4f5ba85aa8ce7d8688e088bd1cf2ffaf304f3cdf
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:05:52 +0000

    rubbish back half emotion confidence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import array
-def get_array_length(array):
-        return len(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
