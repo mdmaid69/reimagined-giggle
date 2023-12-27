fd6537ac22716693ba38e867996b9ffec27ece68
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:00:36 +0000

    habit tip traffic bound usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
