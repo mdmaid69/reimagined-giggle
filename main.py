7552e21a4fbff6d09004a3cddb4911a12c12b20b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:21:50 +0000

    quiet giveup straight tie every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def reverse_string(s):
+        return s[::-1]
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
