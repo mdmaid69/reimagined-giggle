4591336c08de750131081001bd7bca07ee16c462
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:19:57 +0000

    Gg dress thanks air spot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def sort_list(lst):
+        return sorted(lst)
