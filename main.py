e096aa9c9eed4fa46d2aadff5b15c38fbf9e28b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:24:29 +0000

    close argument just fence visit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
