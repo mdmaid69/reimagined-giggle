3e55ca655a707af71b4057ac18fedb0132e28528
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:33:59 +0000

    around hurry invite behind mention

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
