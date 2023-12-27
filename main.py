2015fec9b798c9cb3e0223e7209280715c98e527
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:38:27 +0000

    stone happen during thirsty intelligent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def reverse_list(lst):
+        return lst[::-1]
+import time
+def get_current_time():
+        return time.ctime()
