62c836459cd9acad1580c594142b80ea00f3a08f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:14:30 +0000

    real book big 179bite score

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_work(force, distance):
+        return force * distance
