5c21a8126e29a3423874ced1a04951e26aa183c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:43:17 +0000

    offer miss moon prize service

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
