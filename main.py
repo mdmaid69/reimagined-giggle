08f7e0bc4e99629cfab172845146c2427e3b2063
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:37:10 +0000

    sentence fire board operation though

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+for i in range(10): print(i)
