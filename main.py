5702e0f2b4e00c8fd5b80185d4eaf9d6bcc1e0bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:03:59 +0000

    next apply finger press cough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
