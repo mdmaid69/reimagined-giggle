b8e8217288d9aa78a5a1c09851a0b3b546ff6969
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:42:42 +0000

    finish example sleep notice join

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-print([x**2 for x in range(10)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def sort_list(lst):
+        return sorted(lst)
