5a0af9f70c25c933ba83748a117ada501a8ba387
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:43:45 +0000

    edge whistle honest repeat difficulty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
