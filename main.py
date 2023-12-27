c19abf7e9286691853208ca4d0508195698ee169
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:18:55 +0000

    whom mean luck kind disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_str(array):
+        return str(array)
