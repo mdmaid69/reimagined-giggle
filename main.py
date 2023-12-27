8630431c36216f37ed275cdfa3ef55b6b8b27f69
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:57:38 +0000

    through dead shock amount Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_max(lst):
+        return max(lst)
