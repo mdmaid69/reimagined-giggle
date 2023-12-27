0944def29baf143f5be4ca58658f826a31429015
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:21:29 +0000

    reject correction weather body beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import tensorflow as tf
+print(tf.__version__)
