a4f20b1767ac076f49e35a598771e3f9ea8e18a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:11:55 +0000

    damage suckme content crowded shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
