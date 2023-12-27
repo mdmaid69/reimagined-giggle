98c53a7a6c0410b47872ec8f7cd674cc202e4af9
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:49:23 +0000

    forgive move considering increase giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
