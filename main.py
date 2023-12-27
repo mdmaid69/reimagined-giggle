4134fb243ad46e1030ec8f462f51db6c81bb3dfd
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:54:45 +0000

    slide ready little unit interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
