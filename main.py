4e0250cf0b46a3a13632e90511c9ac81b26f59c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:55:02 +0000

    smooth Vv cover talkoutof question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
