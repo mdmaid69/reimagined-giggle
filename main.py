bb6b268643f19483df203f9a62a52aaaa576f0fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:38:30 +0000

    thing division design cut entrance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
