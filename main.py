205bcc672b6856e7fefa7ffb16e636f37a01b685
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:42:21 +0000

    floor national report fact tax

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_work(force, distance):
+        return force * distance
