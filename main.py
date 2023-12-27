adfd6b001c8c7372de91293ec279583aec2c98d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:54:49 +0000

    stick create lonely glad wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
