766186ad71273de39c61a8b301b627ae097ee2b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:23:08 +0000

    tour round Tt touch rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_typecode(array):
-        return array.typecode
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
