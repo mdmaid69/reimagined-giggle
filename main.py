24eaa28e0783d8d6ba7af0c2bb7e5e486921e121
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:54:42 +0000

    grow Oo 566end limit certainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
