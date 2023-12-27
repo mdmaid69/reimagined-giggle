cdb823e88d6794c92767ff6591252eff6a0a9f86
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:33:24 +0000

    bang slice flat shout workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
