320ba0a016c4e9850332e1885a637769a8b8d7f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:27:28 +0000

    doubt throughout experience partner shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
