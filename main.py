408fe44d9f711bab93abe457c53d5e1e0a646826
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:25:11 +0000

    unable method slide boring be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
