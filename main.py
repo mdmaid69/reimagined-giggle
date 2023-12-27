c9c671204250b1025c6570326f7822af29797b11
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:27:47 +0000

    number value morning disturb slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
