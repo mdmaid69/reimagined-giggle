302b3510e935d5088b220b92c537fbadd1096e16
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:59:25 +0000

    worry stiff tip rescue better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import platform
-def get_python_version():
-        return platform.python_version()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
