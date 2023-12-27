8ff2771e14b6ec08a78d4b67ae1ac9fae03a9715
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:20:51 +0000

    workupto exploration intelligent he couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
