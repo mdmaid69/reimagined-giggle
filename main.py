7db2340188ac27e2371dea8edd8def4e6feb950e
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:53:30 +0000

    comeback deliberate various bridge forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
