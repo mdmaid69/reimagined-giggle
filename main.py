0a234650fc7dc5e41d1af94280e093ee80bf6ac6
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:43:01 +0000

    rent I appeal motion ahead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
