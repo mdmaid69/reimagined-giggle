5dade605ba9f3a9312532c7138a763699f9cc8e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:41:56 +0000

    post concerned skirt do tax

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
