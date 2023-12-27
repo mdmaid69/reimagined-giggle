7243034bb39db46bd370920a255dc0016f4f5e52
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:16:43 +0000

    fasten can job hardly message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
