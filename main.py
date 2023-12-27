43b6fb15c99e927cd5b38a779f683869ccf309e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:35:26 +0000

    intend unless description branch lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
