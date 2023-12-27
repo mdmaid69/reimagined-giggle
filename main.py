ecbe6bab66dee8f96ea1f50d9db5d6b0423e80c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:40:57 +0000

    this sad method sister serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
