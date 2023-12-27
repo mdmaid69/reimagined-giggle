2332a04ed0f03a8a7aa66535c3e3ec2b82a66910
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:38:20 +0000

    flow left sink slightly off

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import sys
-print(sys.version)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
