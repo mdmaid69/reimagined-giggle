7cb55a0081e61c248fe9730417b87411b0e51611
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:31:43 +0000

    pleasure stiff house kick whole

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+for i in range(5):
+        print(i)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
