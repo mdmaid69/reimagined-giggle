3fad9b1146ea26618c745a0b2b98736449fd0eee
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:41:41 +0000

    mention disagree glass Hh brave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
