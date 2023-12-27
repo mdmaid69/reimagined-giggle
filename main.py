eb1ce209238f808e5d4016dc62a723af0dbd637c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:08:41 +0000

    generous study holdup warm phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
