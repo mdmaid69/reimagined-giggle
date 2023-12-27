a56d1df4c1c6c80e7112103f7def2a549186fbd9
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:46:08 +0000

    surprise worry tomorrow convincing time

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def is_odd(n):
+        return n % 2 != 0
