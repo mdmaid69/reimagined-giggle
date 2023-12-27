38249dd086860773b1efe57b2f97e9faa91d1902
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:34:35 +0000

    still phone indeed no poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
