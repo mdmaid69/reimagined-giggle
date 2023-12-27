22ee5b038b1b98300bc0d5d6e56fa6b55b35288f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:47:53 +0000

    buy contrast production close have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
