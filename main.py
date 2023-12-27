477bc1386749e04fd42ccf40a20ba23ff52a0137
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:41:33 +0000

    invitation deserve emphasis ask before

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
