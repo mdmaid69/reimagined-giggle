a21e8bbc1e9c1cf7b555046d185f187ce288216d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:29:21 +0000

    right takeaway immediate avoid rubbish

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
