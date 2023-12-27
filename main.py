5823e45f6a86fcd903d3b908423c7a9a48f97820
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:29:20 +0000

    workout club climb then land

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-print(math.pi)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
