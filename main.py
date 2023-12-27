e59b06c9a46bfc2ca4d3a77192a5c88625a04e41
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:04:06 +0000

    spin split soil deep across

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_palindrome(s):
-        return s == s[::-1]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
