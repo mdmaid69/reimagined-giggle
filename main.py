5bdb974584e2733ce5c900623ab537c5ace76bb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:55:56 +0000

    correct funny crash this project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
