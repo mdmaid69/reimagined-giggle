9f96657bee954a71dc44a00b976d035177717c33
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:56:15 +0000

    close something memory field shut

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def convert_to_octal(n):
+        return oct(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
