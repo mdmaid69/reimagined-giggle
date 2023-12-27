2b7eda3d44a774213d93ffa7ac3a72cd2fabde77
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:08:01 +0000

    gate success round recent in

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
