5dfea03c77a4852a04a23e110f22c1af7f04e27a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:48:42 +0000

    low union seriously tone neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_int(array):
+        return int(array[0])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
