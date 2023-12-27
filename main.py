f98e32ed41a24534f6159e7daedafb7466e3acd4
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:27:11 +0000

    what know waste heavy accordingto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
