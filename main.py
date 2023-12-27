050c01760f547316e9e261a0e9e862a6b0446f2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:26:25 +0000

    top over past respect comment

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import random
-print(random.randint(0, 100))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+text = "Hello, world!"
+print("Reversed:", text[::-1])
