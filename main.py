4d9b51f5cef564ffe7044fa63c5cb53ee4cc1c98
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:26:15 +0000

    cruelty affect no century badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
