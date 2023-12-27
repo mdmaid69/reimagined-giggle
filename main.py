8924bfdc49863894c280c3138fb58955a2003d56
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:02:06 +0000

    shoulder show available rank anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
