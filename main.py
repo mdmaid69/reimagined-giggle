d59babd768809e855ae07904f08cdf48521b61a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:05:06 +0000

    honest herself use formal guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
