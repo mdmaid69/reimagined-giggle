7f8f3de4eb76ec54ce33b873ed9d7c864f25e34a
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:18:01 +0000

    suit butter part similar flat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  def convert_to_hex(n):
-        return hex(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
