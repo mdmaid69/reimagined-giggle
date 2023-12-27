6807c7050d0e2d27a3d9707b093f935c3b88a073
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:53:49 +0000

    drawing crowd note draw mine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
