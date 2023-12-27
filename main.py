db84d88487b99377d3c672f1bae6c5b006044232
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:37:58 +0000

    approval talk idea makeup influence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
