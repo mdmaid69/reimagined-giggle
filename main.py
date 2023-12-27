c916b81e916e6b3b269ba7f3ce56c0c4ef32ccbf
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:51:53 +0000

    healthy worry punish prove business

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
