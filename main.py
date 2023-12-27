d3b2a455de947baf5988b4c62d6ebee980ec0e83
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:35:14 +0000

    lock Zz leave task post

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+for i in range(5):
+        print(i)
