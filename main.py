6088a3038209726a3cb018d5fb6812247c3d6c9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:50:26 +0000

    delight substance definite loss compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
