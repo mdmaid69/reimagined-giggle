bbb526092bbf44439d5f04c65d45133714ad5e63
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:24:20 +0000

    murder experience which heavy down

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
