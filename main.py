abaaed0c748129946e593d24aa016bb7e4bd8b25
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:38:40 +0000

    dig worse method point whistle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
