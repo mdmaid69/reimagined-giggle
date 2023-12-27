624a777faf3e8dfe211fdc890496038aa31e32dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:50:31 +0000

    form Bb everyone crime sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
