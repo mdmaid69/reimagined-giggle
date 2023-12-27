cb87487a973709d3211ca934d61d268b506ea6e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:37:18 +0000

    beginning stiff file custom mine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def is_odd(n):
-        return n % 2 != 0
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
