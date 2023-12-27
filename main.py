7fca6c7d58bbdb5892359fc0ab9979bcccd6ffff
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:02:39 +0000

    fire generous sign average jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
