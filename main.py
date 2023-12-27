ed27d5f3cddf64e1c52a3b40587fa9adfbb82fb4
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:34:02 +0000

    spite health contact emphasis badly

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
