7f80a2a99f3db9489ab36bb1bcacf4e6b4d11126
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:58:42 +0000

    complicated sight soak getoff drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
