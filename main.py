fd38339d6d2424eee067fbedbf9b59db19373437
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:51:41 +0000

    crazy give it they both

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def divide_numbers(x, y):
+        return x / y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
