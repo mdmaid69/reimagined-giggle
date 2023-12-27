59700e0b32978de1f8ec6e7af06a2de1798e5487
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:39:32 +0000

    habit telephone unlucky shout inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
