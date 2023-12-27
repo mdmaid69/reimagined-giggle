4c3660d54ac90a5f564077de84221bae0555ad87
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:17:27 +0000

    demonstrate after night material government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
