0330682954ba3131f4844643d49816da49aa6fd7
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:03:22 +0000

    complicated nervous chat hurry new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def remove_duplicates(lst):
+        return list(set(lst))
