203b180b6efa53894a10384c0743ad2fd21dc50a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:52:07 +0000

    year get clue suck my

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import getpass
-def get_username():
-        return getpass.getuser()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
