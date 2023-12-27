80050574153e72be30b8117cdc773286092eecab
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:35:42 +0000

    move breakinto dish idea takeout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import sys
+def exit_program():
+        sys.exit()
+import re
+print(re.match("h.*o", "hello world"))
