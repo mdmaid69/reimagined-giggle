d9e49e1c22ad388c13123886e4bddb37d11c457d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:32:26 +0000

    concern bet hello break environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
