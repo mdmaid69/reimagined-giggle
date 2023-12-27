ba564c3e3bc6001b87e58bfac4b6a3644ef1d311
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:55:35 +0000

    spoil hold direction chat bound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
