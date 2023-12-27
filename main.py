e34ee59437b2a0a2125603dc4e4346b4f0125378
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:27:04 +0000

    discovery eat bringup drawing factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
   def set_environment_variable(var_name, value):
         os.environ[var_name] = value
