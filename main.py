ac51e0771391b35e9a2b1c86c2708a25c339bcfa
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:27:54 +0000

    bad clear computer motion smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import logging
+def log_message(message):
+        logging.info(message)
