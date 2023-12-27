a6ee6d500287da609ccec348d6e3822d1ea7b4d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:31:06 +0000

    depend bye maintain taste fetch

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
