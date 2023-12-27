62b317c0c25eb5574223c4265fcf47c3c0443f41
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:31:03 +0000

    store shall group bottom however

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
