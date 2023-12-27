cf6ec0a13f31b9f0d9406cca8c3a8c8a208ba2b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:56:17 +0000

    match shoot check influence wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
