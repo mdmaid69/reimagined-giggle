dde193de47792ca39b3f6bbc144498e433cffd9e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:58:57 +0000

    branch pick it common regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
