5afe0307c0cce8bec8a534efb72f00ab35325f7c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:44:13 +0000

    pair take key rough worry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
