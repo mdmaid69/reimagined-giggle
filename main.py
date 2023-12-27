2a802550677da9e767835acc4b4d6b96000bb829
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:44:13 +0000

    father smile speak hurt scream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_list_from_array(array):
+        return array.tolist()
