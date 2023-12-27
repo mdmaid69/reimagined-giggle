c05249bbe8aad6954fb177724ebd63c7491d9685
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:12:30 +0000

    sentence realize spite escape direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
