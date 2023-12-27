e76a10773b638c9c508b2a081121c8d5edd37d9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:01:06 +0000

    burn smile instrument well branch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def divide_numbers(x, y):
+        return x / y
