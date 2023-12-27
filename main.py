a82cc8b61d1abb546345f627b935b0722cb95626
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:55:00 +0000

    within development nothing Ii slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_index(array, item):
+        return array.index(item)
