497820d6480cca45a0dbcbbcf28e7abfd3a457a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:47:47 +0000

    quarter appeal praise organize pleasure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_item(array, i):
+        return array[i]
