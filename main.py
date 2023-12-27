016df2faaba4d53cce49a3fb73761014defcad69
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:41:23 +0000

    almost and brown properly excited

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
