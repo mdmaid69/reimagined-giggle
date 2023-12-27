ed8fcbf3d5169728c19aa755dac68a81188cf334
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:27:21 +0000

    familiar suffer permission idea admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
