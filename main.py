79054a457a441bd25d116e0b7ca068a332fe4cb9
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:53:45 +0000

    sentence tax proper increase bar

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
