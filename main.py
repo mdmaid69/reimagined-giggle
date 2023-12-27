62f09ee43042a0c523f2247570c15adcce822f59
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:41:42 +0000

    name medium concern switch phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def reverse_list(lst):
+        return lst[::-1]
