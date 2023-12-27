e3cbafb01e5cb11e1b50f45c7b516bff6ff55c2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:38:29 +0000

    suggestion cut exchange off answer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print(math.pi)
+import json
+print(json.dumps({"name": "John", "age": 30}))
