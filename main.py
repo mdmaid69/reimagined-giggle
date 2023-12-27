1f71aa6e27fe6146cb1f2b4e1c5e880687293186
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:12:56 +0000

    name discover sweet damp pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  def sort_list(lst):
+        return sorted(lst)
