43118a51bdb84c99dcb813004d56ac1b6c743a43
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:07:55 +0000

    scheme check hard separate sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
