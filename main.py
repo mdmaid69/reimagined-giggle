02d0dde8eb6ba118d2a107d9b36bfd73ce10b62a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:05:13 +0000

    race fight themselves smoke naturally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
