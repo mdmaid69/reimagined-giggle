66006d4d1425a860902feeb25946b060fceef2e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:30:18 +0000

    evening wash ring sign tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
