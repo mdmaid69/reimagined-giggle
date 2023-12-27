16e0daee1429c6a921597bad66e43e357a2f1dd6
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:34:03 +0000

    crowd television lookback(on) prepare small

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def find_min(lst):
+        return min(lst)
