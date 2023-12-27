d0159783fd309611f5638cc3a79a113dca381a67
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:11:53 +0000

    violent professional build combine negative

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
