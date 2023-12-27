262812b13586683977ded4943e3a9d39fc593ef1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:46:00 +0000

    join deliver Ll bar skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_acceleration(speed, time):
-        return speed / time
+def square_number(x):
+        return x**2
+import array
+def get_array_typecode(array):
+        return array.typecode
