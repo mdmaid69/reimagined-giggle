34a8dfc0d3d1f0cdcccf56cf5d19e62ed3305733
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:59:13 +0000

    distance perfect low promise present

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def multiply_numbers(x, y):
+        return x * y
