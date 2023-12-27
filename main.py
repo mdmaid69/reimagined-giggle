90e7020323b81eafe7d1160311b539cf84ef15bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:32:36 +0000

    like crash bye pause runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
