40b6e87981939e5e6fdaf9199b141183d66350df
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:44:21 +0000

    turnout begin pump steep spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
