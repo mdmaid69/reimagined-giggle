b983f5a7080b223f4277bac7dbab5c6d9cde5306
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:32:24 +0000

    natural supply announce behaviour national

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
