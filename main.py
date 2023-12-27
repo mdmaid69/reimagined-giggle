b99e140a225bbf456eca10090c5ff0b869663553
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:49:44 +0000

    tune temper division finish chase

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_set(array):
+        return set(array)
+print("Hello, world!")
