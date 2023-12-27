5119176e6c8d15daa13364b66e7b3f78242eb3fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:05:42 +0000

    practise watch bend hollow pain

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
