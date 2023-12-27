f778642bdd594d9f89504a69c1d5249fa0865163
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:23:10 +0000

    suggest throw wait cost dig

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
