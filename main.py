a995c91cc6757a62f1fbf0381e97a8919c4279f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:25:16 +0000

    dull deep downstairs deliberately nature

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
