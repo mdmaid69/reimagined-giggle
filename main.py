9619bcb72a988f7f97ef60ec5f9cc7e2d48c49e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:30:33 +0000

    willing couple store mad any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def split_path(path):
+        return os.path.split(path)
