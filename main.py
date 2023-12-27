d21da477f1d13ada4c08aa5c83f0895af6f051e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:08:01 +0000

    quickly it rain view major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
