5ca4b82c89ff00fd152c4a3597c3c7c79d939978
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:36:47 +0000

    settle sweep help use any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
+  def remove_duplicates(lst):
         return list(set(lst))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import collections
+def create_priority_queue():
+        return collections.deque()
