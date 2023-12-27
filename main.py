853a1bbea361d74ba067c73c854a31978c4c06fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:01:10 +0000

    pride they degree indeed decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
