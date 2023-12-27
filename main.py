0ee45ddd8c6ec5c2a18350725a9725da48d2d0b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:09:00 +0000

    name fight operation science classroom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
