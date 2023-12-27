bfc94689778163653eb2eafdc5a70432c6fe831e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:20:58 +0000

    stick Oo drop admit top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-for i in range(5):
-        print(i)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
