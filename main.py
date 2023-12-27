e4560ee913803b728a124d2b0909920eb4690f57
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:58:16 +0000

    home trade bread most politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def get_string_from_array(array):
+        return array.tobytes()
