b038ed9429b2084f7abc9ccb291608c2e08ed9c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:18:54 +0000

    emphasize sea strike friend lay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def subtract_numbers(x, y):
+        return x - y
+  def subtract_numbers(x, y):
+        return x - y
