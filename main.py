8d69847a9dc0fb622dfad1662fe557232e6a9a81
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:31:44 +0000

    about put tomorrow want runinto

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_area(radius):
+        return 3.14 * radius * radius
