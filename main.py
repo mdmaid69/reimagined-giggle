2339bc2b726ff05752310ec0eb7fa8a9b516ec89
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:10:10 +0000

    rarely machine belong shallow female

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
