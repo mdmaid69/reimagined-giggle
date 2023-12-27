6100252b50a5eb3c372280633879986490641419
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:55:23 +0000

    first rather trial attempt thin

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def multiply_numbers(x, y):
-        return x * y
-def convert_to_hex(n):
-        return hex(n)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
