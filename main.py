eafe06272ea8a255a46c68c9df5819c686f661b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:09:36 +0000

    thorough test foreign blind shine

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
