130a221507ac065c33f79d01d0cc61284c796d7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:10:58 +0000

    available forget force pot ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
