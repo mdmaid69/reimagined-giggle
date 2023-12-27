7a5d726be48c68ecb8c0b16c95d17c1e57e5d9bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:53:49 +0000

    sting low wrap nearly tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def reverse_string(s):
+        return s[::-1]
