d86f12f675773950b744f7a5e6d14ce8b3ffcd4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:39:04 +0000

    workout bus cheque she sea

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
