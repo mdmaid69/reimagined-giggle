0c1dd057afcc1dfa88fffda7f9e481a7ee29530e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:14:49 +0000

    silent change club safety strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
