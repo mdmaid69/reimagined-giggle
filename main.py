d1eead3af8470eb3f60b625f0929fc20e9091095
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:24:20 +0000

    fine attract embarrassed press nothing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def reverse_list(lst):
+        return lst[::-1]
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
