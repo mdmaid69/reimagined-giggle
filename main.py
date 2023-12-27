2027a748eebff07b2cc09b6c0eee99e01119c430
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:08:05 +0000

    sleep with only promise many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def reverse_string(s):
+        return s[::-1]
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
