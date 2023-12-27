3ad999574510ea210c3241e5cdacade7fe7b8f96
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:15:28 +0000

    short crisis work expensive across

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
