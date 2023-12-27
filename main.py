6881be40b767c851fad94bccf7f14478d5fede9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:56:26 +0000

    impressed innocent upwards influence eye

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_item(array, i):
-        return array[i]
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
