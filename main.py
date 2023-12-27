b4010cbd05f43b8c5601ea77afcdfd2b326cacdc
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:25:49 +0000

    can rid handle ball 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def add_numbers(a, b):
+        return a + b
