db6293d9de35875ca4f4d5fdf1c1a4092a168258
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:32:22 +0000

    stick come disagree recovery consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
