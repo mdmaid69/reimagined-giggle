1acadf22d08d757db5b9d44e150699714ab7b67e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:42:24 +0000

    usual doubt wheel twist nonsense

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def add_numbers(a, b):
-        return a + b
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
