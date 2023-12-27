04c1b7c125f6e13b4eacb9acd753299eaf80542e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:33:22 +0000

    public bridge 566end operation unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def add_numbers(x, y):
+        return x + y
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
