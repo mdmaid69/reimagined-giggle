2842a01a04891c68df6f759f459c0105f148f053
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:34:26 +0000

    share slow nose improve wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
