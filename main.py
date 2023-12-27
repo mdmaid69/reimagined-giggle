95e1bcd0b58d185c0e6564fdca7afddf6a252127
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:06:49 +0000

    about head close small on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
