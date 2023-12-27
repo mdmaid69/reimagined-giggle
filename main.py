2c04593da4919005faad69987f292abb6b10f9b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:17:39 +0000

    past interested cross father difference

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
