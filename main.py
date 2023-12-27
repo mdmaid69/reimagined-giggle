4ef1b326f4887c3f693503e03ed6ed19da77d633
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:19:37 +0000

    good could clear ring nowhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def reverse_string(s):
+        return s[::-1]
