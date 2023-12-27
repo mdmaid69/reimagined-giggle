3482e2131328cd5e7b07d6fb11d0d51d1edcbf9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:34:18 +0000

    estimate usual majority everybody authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
