31f3ac45827501de30ccf8c5aeb640dfd70ec0ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:06:45 +0000

    dead account opponent sky control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
