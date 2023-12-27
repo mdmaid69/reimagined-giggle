2ff933437b5bf9ef80f8ab039eb6f8b1034ce893
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:26:06 +0000

    way ground address city treat

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
