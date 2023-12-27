7b60fdec9401be03dfeaf7773718baf56b0cc234
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:42:11 +0000

    education pump plastic example point

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
