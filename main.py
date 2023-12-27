ae920d277ce23bb14d9a928f43c30429eb937ad5
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:01:28 +0000

    talkinto plain sense protection respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
