d247843828c4dd2dd7a129dbe7163d4fcbf15e7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:40:30 +0000

    risk report face long report

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
