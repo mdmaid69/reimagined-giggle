25b8401f7d12b7bebf8abc71054b83a1a78da25a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:27:54 +0000

    my cum laugh announce spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
