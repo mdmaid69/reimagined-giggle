1f51e51fe8fac075324bb53492df2d30c579e5cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:02:55 +0000

    empty if or urgent neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
