4feb4c935b89e0de34b666d0d278f85da71d4341
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:15:10 +0000

    sorry apparently glass smile building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
