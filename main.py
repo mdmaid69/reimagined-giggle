751a08e662e76d191aee5e3c13af36634b6b54ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:34:22 +0000

    however thick will watch swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
