e9c395e6a86b38df1ec02472a98898e3c6752207
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:45:55 +0000

    consist confused face achievement middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
