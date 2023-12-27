9881f10801f3e117f4b73860090b2e534375f668
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:58:27 +0000

    fierce considering give born let

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+for i in range(10): print(i)
+import os
+def change_working_directory(path):
+        os.chdir(path)
