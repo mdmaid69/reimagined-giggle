a3a0f28e7646eeccdd33a57b0ae77daa381f5b05
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:09:46 +0000

    spray bright male note tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
