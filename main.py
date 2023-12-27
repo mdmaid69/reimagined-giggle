27615fcfdf312bb375f769f9f7c26b6097160f3d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:30:56 +0000

    Ff rest telephone juice know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
