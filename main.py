30bcfb057b6311f2b7596915b50b87391ab2d732
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:00:44 +0000

    under trip drive flow family

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import re
+print(re.match("h.*o", "hello world"))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
