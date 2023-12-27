6eb1232e49f253f5324821efeae3ec5e399f0b75
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:06:56 +0000

    drive health invite second second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def count_elements(lst):
+        return len(lst)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
