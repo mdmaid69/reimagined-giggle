e3a10a5074ec994c5a0db6c4722b8070702ac581
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:00:23 +0000

    sweet freeze decide offend accordingto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
