9de14859284730bf4a2f9216b8099099bf50fc38
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:51:32 +0000

    first truth nose hard instead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_set(array):
-        return set(array)
+  def reverse_list(lst):
+        return lst[::-1]
+from collections import Counter
+print(Counter("hello world"))
