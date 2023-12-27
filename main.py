192f887c773889f50d6747051a537eb94e0753d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:44:11 +0000

    inquire far near private indeed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
