acb92ec60ec83ce3088196b6be26a9d06921b593
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:55:30 +0000

    tax reduce beyond total he

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
