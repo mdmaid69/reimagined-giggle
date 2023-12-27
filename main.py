d2cc76093737e2bd1c7241551c19568791886e52
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:25:28 +0000

    pile for would settle scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_length(array):
+        return len(array)
