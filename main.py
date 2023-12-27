f05fceaa6a0970a793493929824a101af09a369d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:51:47 +0000

    contact sight life pause transport

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+print(math.pi)
