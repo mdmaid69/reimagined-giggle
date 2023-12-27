a4ffc856580a34a1e8ce622c5e93c637d7337f3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:01:15 +0000

    ahead admit satisfied ashamed force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
