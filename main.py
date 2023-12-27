2762cb39930ca516c728f214344f0da24a5aa3dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:33:04 +0000

    goodbye dick lead excuse into

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
