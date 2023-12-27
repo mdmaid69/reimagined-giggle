49a3976afc80629c331300f4d2308060ad7c944b
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:48:16 +0000

    noone rescue complete turnon hospital

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
