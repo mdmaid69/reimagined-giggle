8f31fb6f89c528d6f4450503607b3778e831365d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:56:58 +0000

    succeed suffer test right shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
