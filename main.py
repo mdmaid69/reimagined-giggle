9cc69e37abe9508910875129fdf0c5279d5e5b9e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:25:51 +0000

    prove round long develop pressure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-for i in range(5):
-        print(i)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
