acbfd89f14107d1ac0f47320c1c0c0292193e500
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:40:26 +0000

    crime replace shut service approve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
