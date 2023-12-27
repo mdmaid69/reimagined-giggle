37af3fa41191350533451e300b15ccf882602e96
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:58:30 +0000

    give tradition leave over breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
