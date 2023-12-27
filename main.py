6198d7345bb43bf149827f3bac33c9fde2194d04
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:33:00 +0000

    sure forgive Ss brown statement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import platform
-def get_os_info():
-        return platform.uname()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
