fa1f41f2ff3a54c32e9521c86459e256153de20d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:51:20 +0000

    Kk whole male catch kind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
