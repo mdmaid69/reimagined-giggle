ab706e9074e4e19bce2eb8320006ab0cc8acc5a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:06:50 +0000

    anxious possibility model weekend become

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
