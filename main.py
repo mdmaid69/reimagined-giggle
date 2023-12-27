7ab1a53e5e3f3079ec449f0aea7acb850e39b1c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:55:40 +0000

    patience honest seem environment comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
