639918940eefad8efcc3c516ed2cd5639df7238d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:46:06 +0000

    opportunity they serve hurt scream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(lst):
-        return min(lst)
+def calculate_density(mass, volume):
+        return mass / volume
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
