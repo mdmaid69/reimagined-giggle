5e4bbe4fbcfae32755ef87588630688dd2709769
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:31:14 +0000

    stupid wear treatment post family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
