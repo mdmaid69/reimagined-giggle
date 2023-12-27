59f4cb534af722f3e26df09f0ddc63a4f67c3559
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:22:13 +0000

    home mystery insist experience sail

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
