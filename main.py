95cf9407ab1b825580c5cd3fdf6adce94b156c1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:56:35 +0000

    regular heavy boat up well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
