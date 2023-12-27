a062e0a6108f3dd795c27dfd7fe8718594a601d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:35:02 +0000

    mysterious join form himself debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
