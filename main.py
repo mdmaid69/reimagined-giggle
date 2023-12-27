c59f926e7043deb5ef6ccf7a0ebd7c47ebde05bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:38:06 +0000

    admire treatment event tape fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
