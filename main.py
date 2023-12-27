87e9de20d65bbe1446bad7196730e6c241263956
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:52:05 +0000

    goaway born sort comment study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
