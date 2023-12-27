f6c74d9caf2ddba18528a786f96a491137328a3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:07:30 +0000

    excited only interfere Yy influence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
