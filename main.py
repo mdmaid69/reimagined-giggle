f327155e515ef7373eae619177789492a38538eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:34:47 +0000

    strength lock card runinto difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_speed(distance, time):
+        return distance / time
