36de4c937c6b67556e36eaf5be05e2905a99d935
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:58:45 +0000

    better what contact minimum within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
