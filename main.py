71cc590f5ff230c9ba44d438b84db3619f22dd1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:21:54 +0000

    cover Jj comeback passenger stick

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
