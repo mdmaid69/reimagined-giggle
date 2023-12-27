bf6efa66bf97c112270a074a7143ae96c1165823
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:52:22 +0000

    Ww confuse hospital shock that

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
