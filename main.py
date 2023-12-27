a636e3d90db47e2a2d62d76d656d5429b4a7f502
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:07:27 +0000

    flat talk bother weather stage

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def convert_to_binary(n):
-        return bin(n)
-import collections
-def create_stack():
-        return collections.deque()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
