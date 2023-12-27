c2956653fc72df0848abd0caa246109fb87ff5fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:35:36 +0000

    everywhere tense 179bite less national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
