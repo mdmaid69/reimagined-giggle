04c8d89a977dbf68eb006e11ba6828633027c1a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:06:37 +0000

    miss no young minute hospital

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
