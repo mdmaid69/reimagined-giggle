a6c2cbcd3862af30dafca7cf3cec60e00a9ab52d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:35:54 +0000

    supply comefrom consult concentration dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
