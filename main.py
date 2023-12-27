09a19e25c5d9771ba5559f367e10ddbff923ef74
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:18:26 +0000

    recent raise brain majority shade

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def square_number(x):
+        return x**2
