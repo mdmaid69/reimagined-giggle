d7dbed5ec6b60ac4c25aa6f67feebf99dc05d38c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:55:04 +0000

    spare turnoff bank harm cry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def count_words(sentence):
-        return len(sentence.split())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
