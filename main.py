7975f2cc86a09b6c264336ecf4a1f68287e3724b
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:42:24 +0000

    breakthrough breakinto afternoon doubt club

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
