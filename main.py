1e2b1df5973bec7f349fe59362da9d48730c7350
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:55:48 +0000

    high history equal by picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
