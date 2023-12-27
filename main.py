b97e8601275e3240bf8711c02716bf0286dafe81
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:49:51 +0000

    eye unfair excuse now minor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+x = 10
+y = 20
+print("Sum:", x + y)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
