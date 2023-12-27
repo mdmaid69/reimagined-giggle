10799b2fbdb9a6c5c8231f3e449662fa89a980ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:42:39 +0000

    level year strike opponent different

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 import array
-def get_array_as_list(array):
-        return list(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
