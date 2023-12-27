d309dc20e7b67c5f4df306030c2798003f5a007b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:37:25 +0000

    level teach Dd Jj middle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
