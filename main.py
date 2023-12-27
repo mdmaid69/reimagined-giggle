771597bf0def74da2ce2321f7c0eb60217b763ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:01:39 +0000

    somewhere walk clean Vv chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
