9123e48065401b56233c78c7b275944cb4fd92de
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:05:49 +0000

    brother sell introduce survive tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
 print(np.array([1, 2, 3]))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import sys
+def exit_program():
+        sys.exit()
