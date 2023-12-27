5ce212dc687308c1cf9a226c56b1f37eb47d5acc
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:03:17 +0000

    must consist apply protest children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
