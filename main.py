5c63bf1762dda9dade22ac9c21a352fb63b27cdf
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:44:43 +0000

    public exercise attach within final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
