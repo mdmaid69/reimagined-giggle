dc8b8e7aa3be292143ecb47160960027faa14aa9
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:38:34 +0000

    drop waste score disturb tone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
