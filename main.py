8e558563c1758c82c42338dcaae6b76e74aeceb7
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:57:20 +0000

    special around increase way shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
