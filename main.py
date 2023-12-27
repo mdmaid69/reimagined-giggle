d85626bfb734b94d39167fdc18047f0c0ca7d313
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:42:13 +0000

    direction normal bag party shallow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
