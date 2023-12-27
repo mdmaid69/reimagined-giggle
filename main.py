1395aeb29fdfbc0ca945d0cb4e39c9b2d9dbac93
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:40:44 +0000

    ready warm word frightened rub

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import time
-print(time.time())
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
