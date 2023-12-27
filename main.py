c4eeabb2ad816721dea7c947d563a35ee03d6901
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:40:16 +0000

    smile happy kind classroom future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_float(array):
+        return float(array[0])
