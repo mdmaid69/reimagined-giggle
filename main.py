b19ddaa0134635a183da976ac159b188073c5c8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:04:25 +0000

    drink physical pencil insist away

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import getpass
+def get_username():
+        return getpass.getuser()
+  def reverse_list(lst):
+        return lst[::-1]
