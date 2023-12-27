e9f88d0d40f021438fc51801e6690bc650bf79f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:57:01 +0000

    upset justice naturally better hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
