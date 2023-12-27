3261857848a7fbb279b3bcc68928ae96b5f1899c
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:22:59 +0000

    arm allow wash example congratulate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import random
-print(random.randint(0, 100))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
