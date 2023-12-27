6e073ab5eb93038894625b987b3777caa76252c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:40:07 +0000

    alive reject low scheme bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_access_time(file_name):
         return os.path.getatime(file_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
