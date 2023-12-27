23733fe1b5f69c7ea78fb7f360dc85ad37e4b819
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:03:23 +0000

    nature buy surprising occasional classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
