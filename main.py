3ff0dc7c8d263637b22a386d3a1c91dcce4999a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:10:40 +0000

    stay mix lead arrive broken

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
