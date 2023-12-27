d911bb731452a4d286f6332eabe0fad65f13a7d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:30:41 +0000

    stamp force runout describe strict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Powers of 2:", [2**x for x in range(n)])
