b5f0ae6b469f670347cd608aa8e3a6f4836ecdd3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:04:48 +0000

    book test spill habit hurt

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+text = "Hello, world!"
+print("Characters:", len(text))
