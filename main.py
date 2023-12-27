2fb8e6ddd53623f420b5452d3c77718f143b8502
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:38:43 +0000

    approval respect Mm blind central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
