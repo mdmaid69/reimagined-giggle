31e159da82a8d5011a6a9f65061d4835a92f0048
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:06:04 +0000

    zero like company confidence pair

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
