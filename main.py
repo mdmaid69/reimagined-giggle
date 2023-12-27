2d5ea23e752e549771987fa34492de3b4228d525
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:52:50 +0000

    present crash release unable share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_memoryview(array):
+        return memoryview(array)
