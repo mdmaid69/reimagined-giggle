e012299111b76559908f57ccb6fe5cb0635d84bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:46:36 +0000

    upwards careless record anyone needle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
