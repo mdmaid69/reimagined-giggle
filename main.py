a423b2d8f29030aadb124fb2dbc89266f8c26e72
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:36 +0000

    model total way mother exercise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
