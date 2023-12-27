bdf93f426eba282c10f26f16f9c5b0e1123f7c17
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:24:35 +0000

    action truth declaration clearly sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
