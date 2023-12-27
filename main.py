84c7a4e0f3d7cc2ed2256263446ff331c284b8e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:04:13 +0000

    active ordinary violent famous unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
