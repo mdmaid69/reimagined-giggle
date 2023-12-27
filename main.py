cb2c5af8c60dbdc42730384c4575c18a35717d9d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:10:14 +0000

    object room who tap complaint

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def sort_list(lst):
+        return sorted(lst)
