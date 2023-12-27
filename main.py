b4110c05e34fd570c555f8cfc01690cc24734292
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:08:10 +0000

    appear much discussion turnback family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
