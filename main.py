3300c8bef7821188600099979e2108a03ac7905b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:27:37 +0000

    much stand beginning shout confident

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
