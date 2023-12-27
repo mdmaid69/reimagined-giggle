41b8dd46231fc637877485d704a4e5cacbbea12d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:55:22 +0000

    software tape which high mean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
