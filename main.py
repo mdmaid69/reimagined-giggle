4714d4fe0e93075aab9986b009c3bcd35d204de3
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:50:21 +0000

    receive gap this everything range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
