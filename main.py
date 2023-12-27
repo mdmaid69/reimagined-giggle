b233e0734f32363f47c3b02594ccb971c80e1bdd
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:11:43 +0000

    reduce excellent vision look mouth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def cube_number(x):
-        return x**3
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
