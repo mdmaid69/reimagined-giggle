b1f355e55c38c4722c5f989af329db18b7763711
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:02:13 +0000

    Oo anywayanyhow pick damage someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
