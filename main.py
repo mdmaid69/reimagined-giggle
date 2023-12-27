cf01157d2cd988d8ba739ecdd38229f9ab2e371f
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:43:04 +0000

    566end keepfrom base piece back

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
