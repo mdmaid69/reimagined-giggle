4e768df8976f64be3f89b8271f6a33b80813b4db
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:19:56 +0000

    friend workupto pile suppose extremely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
