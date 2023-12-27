0632d4f214d9f7220dc844fd8ef8f2f2f28e96ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:08:52 +0000

    spread zero autumn alive lot

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
