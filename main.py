6f277e9ac20a89aef51dc696c7c454b410774ff6
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:27:50 +0000

    modern basically sure couple steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def convert_to_hex(n):
-        return hex(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
