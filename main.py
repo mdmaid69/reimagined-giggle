233f5b035fab9a4542f696f61f66a81069e1b450
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:35:46 +0000

    kind anything round turnon cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
