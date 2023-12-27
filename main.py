e7eb874a4ee37c4c1706416219e7d2fffbf483b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:42:56 +0000

    among grow community worse broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
