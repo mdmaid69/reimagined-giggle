f80e014835e93eac35dba17094e2539692313670
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:00:31 +0000

    direct careful clear maybe here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
