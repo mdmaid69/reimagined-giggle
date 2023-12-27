5612ccce7d59b66207020e28cbae75ea550033c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:50:49 +0000

    temper violence soon hurt run

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+print([x**2 for x in range(10)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
