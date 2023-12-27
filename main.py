2df8c3c4f11956ff893f1378d224ed9450944ae0
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:28:41 +0000

    danger speak finally operate settle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_typecode(array):
+        return array.typecode
