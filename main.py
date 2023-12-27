3083c68fcf5b6d28459358a1d613a9008beea486
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:34:31 +0000

    and tongue game mail obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_current_working_directory():
+        return os.getcwd()
+def add_numbers(x, y):
+        return x + y
