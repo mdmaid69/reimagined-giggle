e7c6b11247cae1db91b2ffe37cb5b88e6bdc2460
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:59:38 +0000

    work arm bury want yours

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import tensorflow as tf
-print(tf.__version__)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import re
+print(re.match("h.*o", "hello world"))
