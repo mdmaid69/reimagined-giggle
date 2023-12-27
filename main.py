a931426661f7ff2486524c3fea37c5a572f846c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:30:49 +0000

    manage house there distance leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
