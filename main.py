a422957229abf48143a96288ac040801dc634100
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:07:33 +0000

    nervous hers style experience temperature

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 def find_min(lst):
         return min(lst)
