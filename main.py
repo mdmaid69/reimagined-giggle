f8cc834ec82724c8462f9f0fbb5b937a9ee7b546
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:06:19 +0000

    push regular confuse destroy book

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
