f7d971df692200a5e81b29e88ddfa7ed2a02101f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:38:07 +0000

    though weak if value match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
