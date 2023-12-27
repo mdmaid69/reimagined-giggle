f630b9fa012807fc92bf560fcbb4dd7b912cb29a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:06:57 +0000

    weigh gas wake crowded straight

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_bytes_from_array(array):
+        return array.tobytes()
