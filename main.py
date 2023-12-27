cd5415d0161923c4571c9bcac40b96e01c1a17bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:00:48 +0000

    promise comefrom block down hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
