6b680470d11f377668a07661546452e00ec1c2b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:42:29 +0000

    shoulder cry bridge price word

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
