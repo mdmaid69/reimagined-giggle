146ff7fe20d06af48de94ccb09461d4c4ccdac11
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:21:19 +0000

    model market education none far

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(sum(range(10)))
