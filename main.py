0a9573170969e9bda4a7a70d995847dd489347ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:42:47 +0000

    method giveaway agree protest close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
