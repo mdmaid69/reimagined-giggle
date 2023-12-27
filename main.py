efd45cc46726d579f56f0a874469a4c7603404da
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:49:49 +0000

    square tool duty bringback accidental

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+print([x**2 for x in range(10)])
