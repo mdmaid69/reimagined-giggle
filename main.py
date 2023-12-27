dfb5ae61f7636315308da2018fd7eea720d0aaad
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:57:39 +0000

    card rain ocean complain length

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import sys
+def print_python_version():
+        return sys.version
