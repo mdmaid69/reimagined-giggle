1683b75ba9098cb87e3324b30f32d9f931866161
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:53:29 +0000

    yours towards dry begin keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
