2c3eeff784434812e5b05c95d57223bd8af6557d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:45:42 +0000

    various trap form funny release

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
