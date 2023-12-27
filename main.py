57f87e8ac7a9c6dd427205bb83a656aaf3126a37
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:40:55 +0000

    desperate squash wear between above

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import re
+print(re.match("h.*o", "hello world"))
