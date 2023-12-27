85867aae007e73a9ded2bb6b5e9dc172f43f6e67
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:31:52 +0000

    peace season live born neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import sys
+def print_python_version():
+        return sys.version
