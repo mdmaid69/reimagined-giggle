4daee8b4a4486099a68a79ab2b38863c0b7db749
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:49:35 +0000

    right split regard makeinto amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
