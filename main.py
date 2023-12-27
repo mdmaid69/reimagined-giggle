7ce9e38925a06f98291c18b648185ed35ebe069c
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:13:56 +0000

    study leave connect grateful not

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def count_characters(sentence):
+        return len(sentence)
+import datetime
+def get_current_date():
+        return datetime.date.today()
