560f508188ad6c15723b5085677f54c6a74f9bbe
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:26:27 +0000

    before disapprove love lucky music

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
