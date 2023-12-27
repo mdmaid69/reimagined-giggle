6e3cece73bea00adf6d46a0eadf6790ba128bae7
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:22:39 +0000

    trap promise just ourselves measure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_density(mass, volume):
-        return mass / volume
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def calculate_speed(distance, time):
+        return distance / time
