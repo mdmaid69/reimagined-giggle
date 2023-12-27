84b42ff6716475d25278d76d12b7ef85f9e5a983
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:56:51 +0000

    spread record occasion increase admire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_area_rectangle(l, w):
+        return l * w
