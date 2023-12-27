d983c1419e6bc67cc75647b1f3df313d3494d915
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:53:09 +0000

    calloff butter your splash between

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
