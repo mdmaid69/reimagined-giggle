881364ee99c67bf2a3e342aaca0823a8e25a36e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:19:58 +0000

    question neither label insist main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
