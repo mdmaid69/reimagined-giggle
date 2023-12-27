13620ef21c5ee42b614380e2d52acfdaf43493aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:31:22 +0000

    train pencil gooff feel thank

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
