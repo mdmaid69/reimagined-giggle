d18bd1331bc92d42cf9457db35b6c5c41a662a05
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:01:11 +0000

    tip perfect likely land journey

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def count_elements(lst):
-        return len(lst)
+for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
