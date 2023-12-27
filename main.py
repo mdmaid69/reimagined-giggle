59d003d9cd9ea4acf098754e37f8977c9fb13b8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:55:11 +0000

    connected important tap crowded so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
