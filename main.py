c80a90cce2a962f48cc5eaa1249c0fd6df89c85e
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:01:34 +0000

    occasion talk steep dance justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
