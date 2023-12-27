e1e94eab77fb87408dc5d40fe01e2dd4388cf208
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:05:53 +0000

    correct please much their sky

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
