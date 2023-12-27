a792adc6fa5d60a202d2c73065f9d46d695015cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:48:20 +0000

    spill case friend criticism hurry

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
