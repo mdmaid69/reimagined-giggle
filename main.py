07cc51c015ed8cf0969c874b9b68130693953a35
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:23:11 +0000

    think line disturbing climb wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def convert_to_octal(n):
+        return oct(n)
