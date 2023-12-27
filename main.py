21237a468dedb1dd9b30c55b921d53a631338312
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:44:59 +0000

    pencil maximum getout level after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_current_directory():
+        return os.getcwd()
