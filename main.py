e46b4edcefb3543439f3a2cbcd2abce55110ca3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:04:59 +0000

    invitation cool please seriously alone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
