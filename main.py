41d109663c2546f9c0d860f1b7d97089e638b089
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:08:32 +0000

    Vv everybody board law comfortable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
