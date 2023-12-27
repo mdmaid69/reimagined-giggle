a23ce3e59445d8681f1def10d421e444cafce453
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:38:48 +0000

    present Oo worried grand concern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
