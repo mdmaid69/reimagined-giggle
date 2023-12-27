2296a5a6356d400e7afcf2aaf42460213e595de9
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:18:37 +0000

    sensitive build agree motion hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print("Hello, world!")
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
