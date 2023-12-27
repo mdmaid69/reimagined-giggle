b40999221cbf11399176359916482d9c62bed172
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:09:06 +0000

    impressive cheek delight free impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
