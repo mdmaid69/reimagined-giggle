25eed0b8464361133ad493eea5017418c4de48e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:01:19 +0000

    after complaint top file makeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
