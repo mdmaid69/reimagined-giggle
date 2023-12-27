e32c2d507b879b8f9eb610061900b1f12e76b4f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:09:04 +0000

    tip laugh among road post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
