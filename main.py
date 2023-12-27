9ab0aff781967d8bd2fdd2bf3e5e59e5b938fe06
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:48:56 +0000

    comeround figure lose stand delight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
