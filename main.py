7ec88fcbc4ac95bedf069b6685b96643c9f46d4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:24:39 +0000

    shame accept government disgusting win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
