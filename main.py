607b129b2870c0e31c9049f6e84272afbb1fafb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:09:27 +0000

    odd open sweet round area

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_acceleration(speed, time):
+        return speed / time
