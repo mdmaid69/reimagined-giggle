55c3f3f54ae3c3f520a02d6b474c4ff942f63192
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:20:04 +0000

    accidentally getout century circle trick

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+x = 10
+y = 20
+print("Sum:", x + y)
