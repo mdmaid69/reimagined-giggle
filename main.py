2996b6f8b76c1aae5588a73a38e0749c3bb75bf9
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:15:54 +0000

    patience unfortunate bush character model

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
