ed3363985b086518394c97266ef539530b5a6017
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:12:13 +0000

    comeup speech shake bang empty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
