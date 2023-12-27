cfe38557b76f3bcd1315973ce1f020abdc8da36c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:17:19 +0000

    study twist number spring basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def multiply_numbers(x, y):
+        return x * y
