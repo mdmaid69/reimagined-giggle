bd27a6d005843675f4d78d275b6bf8a487dc84a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:38:32 +0000

    on confident generous support below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
