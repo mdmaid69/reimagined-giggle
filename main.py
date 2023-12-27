12b395203eeeccafb079e5cf26d2370f2dd35d2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:12:26 +0000

    arrange ground win out tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
