6a7bd1d445cb40d958be59f879635c93f59954e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:49:28 +0000

    confusing type care face cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
