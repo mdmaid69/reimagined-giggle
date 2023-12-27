01c4e2683db5fae96594e177bc961f4cb2218ed1
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:23:03 +0000

    course criticism even disappear Ss

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import collections
+def create_queue():
+        return collections.deque()
