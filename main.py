ce52268131770cc89245ba18f75fba600d2f85b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:29:52 +0000

    takeaway even transport listenfor deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
