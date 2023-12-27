dcd72534533831eec568a33ea6ecdd687b3892d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:18:41 +0000

    because matter form closely contain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_min(lst):
-        return min(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
