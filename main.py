bcdf8a0041e4b1c86fdca9e020cdf5801af0424b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:02:27 +0000

    match warm me passenger leave

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_power(work, time):
-        return work / time
+def add_numbers(x, y):
+        return x + y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
