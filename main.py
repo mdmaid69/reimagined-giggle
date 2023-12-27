e302a45f7b4172027f91946de536e83f83924f47
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:18:57 +0000

    act eye emphasis necessary Dd

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
