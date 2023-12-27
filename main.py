a4460d8c53f54e05557fa2bf80752a572513bf75
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:26:56 +0000

    visit if like begin contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
