742e2a0c018dba41300a95157b346d5b276ddd6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:31:00 +0000

    pleased downstairs honest party emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
