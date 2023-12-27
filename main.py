456d35e00e4ac34de3059a957fcdede394c042c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:12:45 +0000

    keepfrom programme prevent experiment bar

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_floor(x):
+        return math.floor(x)
