6f9f567b0090fb14b295a96f191a5f498bb99fff
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:34:29 +0000

    crazy passage steal young pick

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-for i in range(10): print(i)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def convert_to_octal(n):
+        return oct(n)
+print("Hello, world!")
