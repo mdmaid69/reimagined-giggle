c36431151f6143470c38ec463c249427beb46776
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:52:47 +0000

    impressive ordinary goon forget deliberate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
