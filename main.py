9ba72ee4bbd35fe4f71c1686b381f807a26d43b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:03 +0000

    limit day lift deep blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_current_directory():
+        return os.getcwd()
