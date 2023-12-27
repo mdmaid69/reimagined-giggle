3ad7e4bcedcd866ed0cd31351bde930ebbe45c55
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:55:22 +0000

    wind high transparent yours belief

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
