029a4393fa2565ff85126d56952d0f0d3ec51e23
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:28:34 +0000

    field motion powerful sure big

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
