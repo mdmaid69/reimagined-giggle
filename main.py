c1a6ee427994c55586118bb2d38cba49b8dfba70
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:32:30 +0000

    born than makeinto carry coat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
