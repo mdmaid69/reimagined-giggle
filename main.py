2976590efb5ac26176a3a43c81a27aed70773ff4
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:04:20 +0000

    block clean fit cost bored

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
