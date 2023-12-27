6b4d1679f22552c038f755a42ed5c051d91d2506
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:11:10 +0000

    minimum hair passage song official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
