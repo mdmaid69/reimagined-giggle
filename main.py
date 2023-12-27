013a9ff21caaa0fc1f68c13c3961cae1b5887c0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:42:46 +0000

    shame replace large number as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
