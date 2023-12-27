8f932874872ada44a6b77decd137a793872168a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:07:03 +0000

    back against treat lean otherwise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
