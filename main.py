251ff0b654bc4f239a9586e58487c239445eb519
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:43:21 +0000

    positive drink undo fat choose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def square_number(x):
-        return x**2
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
