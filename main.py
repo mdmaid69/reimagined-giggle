b8d1696c568a00fd72da8f95f06c8f34975b461f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:50:22 +0000

    scheme knowledge test impressed maintain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-print(os.getcwd())
-  def convert_to_octal(n):
-        return oct(n)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
