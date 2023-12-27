cbb77de7693ea846993965e809dcfc198748d92e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:27:18 +0000

    572enter post will strong whoever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
