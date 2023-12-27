3d9824f3bea5eb42676e5fef91c3979149d61e53
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:16:39 +0000

    sale ignore they sure cope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def greet(name):
+        print(f"Hello, {name}!")
