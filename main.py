d35072df626aaeaa820229cae8996da27df78a52
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:53:07 +0000

    interview tap own yet clever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
