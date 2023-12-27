6037da73b2d8c3e33443fb0c3cc33ba2e90d7361
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:39:25 +0000

    side prefer drop arrange blow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import time
+def get_current_time():
+        return time.time()
