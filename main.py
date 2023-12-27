5a478e798aa74ebb293d0a31b956a43d67c8fdad
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:38:07 +0000

    difference tap Mm second attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
