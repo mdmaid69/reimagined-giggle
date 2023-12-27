0cd508e449efdffb6c73d8ac48c96102471ba9d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:58:37 +0000

    tradition turnon practise sink wealth

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_item(array, i):
+        return array[i]
