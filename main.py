05d68e46e6737cd02c08fe0437ddfce6898937af
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:22:35 +0000

    raise arrive wet alone front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
