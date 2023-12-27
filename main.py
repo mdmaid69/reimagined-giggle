d7a3cd1cfef502810419e022b8451f84d5358a00
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:23:41 +0000

    traffic fire back use contrast

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
