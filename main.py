5d869fa44e27c370dd45605d672b4d97b5199511
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:27:01 +0000

    unlikely ilovedicks print tired any

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
