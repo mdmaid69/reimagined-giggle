de6a59dbbd064a8c0c4c24bbc34e038912173bdb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:57:35 +0000

    nothing protection lock first shall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
