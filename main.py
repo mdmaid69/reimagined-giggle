641539a466a9c22e2088cb3ae8f807fe382e96e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:15:38 +0000

    pay drink related mend punish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
