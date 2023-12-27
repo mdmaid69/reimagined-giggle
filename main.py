c839ba5307a6db17eee6ebbef8808df32f011c20
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:05:18 +0000

    no anywayanyhow badly criticize provide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_max(numbers):
+        return max(numbers)
