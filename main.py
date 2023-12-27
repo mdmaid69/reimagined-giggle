4a679afc85bd70e31358d0f2adb37a70b3f130bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:32:23 +0000

    correct bend boat scream obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
