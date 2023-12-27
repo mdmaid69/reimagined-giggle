ba176a52b66d87f1ca84fff45a87c8e380192fc9
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:40:21 +0000

    and rely neat course professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_as_str(array):
-        return str(array)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import json
+print(json.dumps({"name": "John", "age": 30}))
