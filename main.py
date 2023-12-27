8d56a701db4e30170886e7f4b771ae368c9bed15
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:25:47 +0000

    favourite ride luck pour lunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
