9fb0788110614f532990fd41ffb38cf4efb2f953
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:36:16 +0000

    cover follow die peaceful downstairs

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
