9d8aa2afab6c3a6ca83038bc2da795a8433a623a
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:28:19 +0000

    Tt note since allow profit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
