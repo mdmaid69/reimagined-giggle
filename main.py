995a7581bde46ec78fadf2d545b11753baa15d4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:24:40 +0000

    address injury march fall might

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
