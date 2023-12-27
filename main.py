9710f309297940149ec3c6bd20fb57c231225078
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:50:22 +0000

    development spin boat truth yourself

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import collections
+def create_counter():
+        return collections.Counter()
