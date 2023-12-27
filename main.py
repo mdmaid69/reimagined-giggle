61d5631840baa9d8a1c2b214930b1c6da484dcf4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:45:30 +0000

    love power belong agree morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
