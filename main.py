b23f1df4ce42505021f12148f720099301867352
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:01:58 +0000

    tall naturally winter broad imagination

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
