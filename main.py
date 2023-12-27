c4140c24478a1a5f83d002c7d18fa69937dc630b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:52:05 +0000

    uniform recent hole grand watch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
