6e445081d105a4c7b3c0438217bf0e87e894763c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:30:06 +0000

    post wise spin blame bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
