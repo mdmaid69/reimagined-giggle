1dae1121b6aec39c0aa7695cd0ff79f9278afa68
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:12:26 +0000

    unless behave drawing dangerous movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
