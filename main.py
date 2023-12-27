a25efa190da0f74c2fdcf803d9359c0f8e93347c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:23:49 +0000

    further ought keepout kill comeback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
