785b215e1c7fec0c591f794428d312f4fc1116af
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:45:29 +0000

    doctor recently remove hope age

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
