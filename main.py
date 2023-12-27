97f0c15fde59807188c5cf959a981ca7f33861e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:34:21 +0000

    top base lonely rest create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
