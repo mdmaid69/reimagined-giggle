a14633599be5f5da8afd76c5d7e025e5b84001a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:18:50 +0000

    telephone in against lose tread

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
