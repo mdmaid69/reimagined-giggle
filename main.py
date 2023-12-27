fd0962552a74d96a361c06f7ffb46457ea4f4969
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:21:08 +0000

    turnoff type meal same well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
