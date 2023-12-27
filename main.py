6d122966b3a4ec70e7bda418673ee27688530cdf
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:56:47 +0000

    will direct divide noisy wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
