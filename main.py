68b4433a9336c65fe8a04cfcf0bb110d1aa58ba6
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:55:08 +0000

    brother normal improvement science breathe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
