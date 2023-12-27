04a25adf6b6a9117ce2a84cf77f1ab500430c86c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:19:39 +0000

    each wave after bet there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
