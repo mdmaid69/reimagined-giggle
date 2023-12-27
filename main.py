bd0b3ac601d5b452820ade00dacbd4615c2f61ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:55:33 +0000

    use lie learn right runaway

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-x = 10
-y = 20
-print("Sum:", x + y)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
