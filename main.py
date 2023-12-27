798267444bf1a833c89444a7b27005663c2ba9e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:10:33 +0000

    fairly might pupil matter separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def remove_directory(path):
+        os.rmdir(path)
