18fbc146cddfcee92b3629f7affe9b1e03ee5adb
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:46:05 +0000

    scared juice scene money no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
