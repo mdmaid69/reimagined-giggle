36344cb9e38a07c6951d7495fd526a8c99de9236
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:22:06 +0000

    touch while negative quit ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
