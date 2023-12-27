759390a4eea4b5042e0c0c7a86cd4aeffdfe3fd9
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:00:48 +0000

    information work politics partner sea

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
