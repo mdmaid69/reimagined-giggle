5e1ceac22e6e0aff426f37880d20c24640e69e44
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:48:43 +0000

    telephone front rest worry experience

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
