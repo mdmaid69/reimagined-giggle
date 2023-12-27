a1b34174929c94f7fad10a159f0c0e22f71ac6a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:05:18 +0000

    neither make use correction recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_typecode(array):
+        return array.typecode
