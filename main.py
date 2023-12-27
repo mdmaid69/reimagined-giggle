5cbc349e0a2b007b19b2e9245558d7372fa81a37
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:31:31 +0000

    even lock claim head further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
