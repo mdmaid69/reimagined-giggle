e6d3084dbbd5a387d5ce3ec7151ea7be339edeaa
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:18:26 +0000

    clue excited compare accidentally bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
