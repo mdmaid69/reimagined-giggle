e58ecb37a07adcaf03e5dec9a7d7842e97afebca
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:16:32 +0000

    cover genuine Yy operate rush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
