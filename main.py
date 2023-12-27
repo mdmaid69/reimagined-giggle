1dec1281c64348857a7ded3869945da9ae97d29b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:26:58 +0000

    sign experiment deal judge rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_as_set(array):
-        return set(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
