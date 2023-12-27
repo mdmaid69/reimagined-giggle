cf6ce9487ef2a13f3d17b35ea18b69a998bc2a8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:11:52 +0000

    complete workout influence prove female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
