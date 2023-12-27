2736d2cf09991dac864a18c0046fc119110aecf4
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:54:54 +0000

    Ee address conduct typical meaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_length(array):
+        return len(array)
