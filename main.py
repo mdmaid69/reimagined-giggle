891ed9e2f2d3b26a5f023879ef738545745d0c58
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:38:47 +0000

    workout petrol band needle movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
