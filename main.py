458022bbd8bb64d21a68f5871bfd3ab6bf080192
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:23:18 +0000

    home odd build pleased trade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
