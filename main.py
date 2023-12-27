6bd3f05fbf44469685c25c0cee450c8453f2d9eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:03:18 +0000

    bar figure event father onto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
