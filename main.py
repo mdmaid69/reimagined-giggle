80c1480776b29a99e7a183f309dd235d4486771f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:32:51 +0000

    though perfect spoil when science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
