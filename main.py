e29755fdd461e4a2ffde77b4753050f8a7297046
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:44:51 +0000

    copy conversation lay possibly laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
