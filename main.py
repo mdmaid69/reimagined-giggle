4354ef2481c3e04482cb4c511d67240ba4cc2aaf
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:25:37 +0000

    common beat fun stage niggaacademy

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
