42dd27d0e7126756b9f3ca31dd90542149dc20f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:09:08 +0000

    gate safety dish extraordinary only

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+print(sum(range(10)))
+print([x**2 for x in range(10)])
