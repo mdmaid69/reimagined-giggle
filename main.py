ce6a4f33d03ad5f32e6773658b0540474cc4897f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:50:41 +0000

    answer cum cough effort spot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
