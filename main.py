e37eef8fbdcda89e2e015991219c5b3ebffe78db
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:35:47 +0000

    news try sore sometimes motion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
