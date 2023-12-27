68ec8719bf4a76b310f3821025ab9d2796e4e33d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:46:54 +0000

    announce Ee use like compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
