3db97ec13be1ba3f482eb9b8d8679d08d83cdf4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:24:49 +0000

    tip matter convincing radio edge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
