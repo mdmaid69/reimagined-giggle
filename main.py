1ea4644f4800c61d316c7f9797e3cc40d46caa89
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:37:36 +0000

    extremely seriously rain pretty angry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
