99efa6d952f1e326e75f99b55543302ef57f180f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:45:57 +0000

    national independent 573entertainment nor clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def count_elements(lst):
-        return len(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def sort_list(lst):
+        return sorted(lst)
