1dcfa7c1cba650956c213b39067781c29ad0a8d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:00:14 +0000

    own tired breakdown talkoutof early

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
