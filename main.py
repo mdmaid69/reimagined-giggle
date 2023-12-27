4673b4d5b89643ab51abe6e3d49b257f3533a14e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:42:44 +0000

    wind strict freedom dead 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import datetime
+def get_current_date():
+        return datetime.date.today()
