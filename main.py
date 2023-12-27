de1e026bbff05aff948620141937e8e7048dd545
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:37:29 +0000

    pause slowly necessary important muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
