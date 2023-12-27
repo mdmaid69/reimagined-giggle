0023b787bca69463eefb5c34ad33366c93736fe7
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:48:36 +0000

    turnout quickly flood right pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
