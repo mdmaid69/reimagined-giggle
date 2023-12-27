b1768cdad11b5b4261e56e6bd72c6eb31b9b245d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:20:38 +0000

    temper still proof steam wave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import getpass
+def get_username():
+        return getpass.getuser()
