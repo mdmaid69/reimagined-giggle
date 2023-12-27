25c01686c50547b3edd55ee885831489999a6b91
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:32:02 +0000

    getup godown match mysterious impression

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import os
+def remove_directory(path):
+        os.rmdir(path)
