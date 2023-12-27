04e76ebd008f1e59b08e5e33dfc95f4e5c468bd9
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:27:22 +0000

    debt expression since match join

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
