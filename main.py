f4afa1b4a26e8b63e2cca4972766e9569b738141
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:07:58 +0000

    computer possibly learn turnout question

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
