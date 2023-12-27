2a0238d99dc5699a90b2a0693e1b3a1b7a4ac456
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:55:29 +0000

    bother combine correct law watch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
