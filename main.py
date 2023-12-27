3685ff3dd5fbfd8a7a1deea5580431ebe4dad8c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:37:24 +0000

    blind relax convincing dress finger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
