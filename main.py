5c8fed39c4ff698819df050c2390cc65ed86597c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:06:18 +0000

    next among anywayanyhow level protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
