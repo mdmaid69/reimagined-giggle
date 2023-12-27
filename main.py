72acc46768e9030c628d355529059d6f030b6c92
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:35:59 +0000

    true fix environment to rich

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
