74be4a4c8eede9c0e4931a90a2e2ba9a7840d48c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:12:46 +0000

    apologizeapologise for protect treatment anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
