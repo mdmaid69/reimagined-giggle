2a3dae42685f972cc3985cfe6824c4b76316681c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:27:59 +0000

    birthday organize nigga lie friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
