70b9e09cb235eeceded7c45ae6d065886873f49d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:38:10 +0000

    face turninto congratulations different switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
