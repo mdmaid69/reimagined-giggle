4880614b57a7ad6a6d7e599282b2786c0e4f9ed1
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:32:26 +0000

    ignore start keepaway rather 571enough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_work(force, distance):
-        return force * distance
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
