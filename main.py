3bef502ec048f8f43c5ce9bf42a8e516df6bf2b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:06:17 +0000

    danger petrol spray emergency vote

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
