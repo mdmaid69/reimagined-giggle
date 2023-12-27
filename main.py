2812deaceb57751642103f0f83ce9365dc824f14
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:18:39 +0000

    direct comeup oil serve further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import sys
+def exit_program():
+        sys.exit()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
