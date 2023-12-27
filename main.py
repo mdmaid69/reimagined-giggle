2e9017d6992ac518ab987779d45aad81471025a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:50:05 +0000

    coffee dangerous direct blue risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
