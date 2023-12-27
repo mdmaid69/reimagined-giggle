98f7cbd3eed93b1668ef791d9add079be3ccfcc0
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:51:22 +0000

    second whole damage properly test

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_counter():
+        return collections.Counter()
