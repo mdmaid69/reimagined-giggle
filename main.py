22532d75103d9108dd43666d03fe26d37e0e3f3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:18:55 +0000

    male middle sharp danger unkind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def sort_list(lst):
+        return sorted(lst)
