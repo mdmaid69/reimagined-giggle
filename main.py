6ec77e0c53e7820107a624fc0bcbabe2eb51842a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:09:15 +0000

    hard shoot crowded Vv deaf

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
