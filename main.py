d1c9f79d8d2cfade851ba4652632477b1e548699
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:40:10 +0000

    related some choice calmly Ee

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
