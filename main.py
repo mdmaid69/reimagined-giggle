d8839232d424c31e4de9e892b3f419e5886878c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:20:08 +0000

    otherwise score sudden interview afraid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_list(array):
+        return array.tolist()
