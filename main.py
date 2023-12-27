de30a24d10a8a479c0f1c443ec75dc7df69491cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:07:14 +0000

    separate definite guess turninto before

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
