6a6bbd2210a9dc5f80c4eaf9297636d390ef9bf2
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:12:37 +0000

    gas strike congratulate turnaway culture

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-print(math.pi)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def reverse_array(array):
+        array.reverse()
