71d675ef1dc95e1765edb6a701506dfd2230d182
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:34:47 +0000

    soon certain several wind will

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_power(work, time):
+        return work / time
