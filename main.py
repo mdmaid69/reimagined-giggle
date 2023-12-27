356373ffb10c8bbc9b2f4fbaf9ec033454ff7f30
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:00:27 +0000

    however during calmly getup smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
