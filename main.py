8d33d16d725438afaecae3c0600b3f5c7d6fa4d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:07:34 +0000

    quiet cut size rough awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
