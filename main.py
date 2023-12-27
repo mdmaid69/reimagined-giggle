da2293aac6cc0391994af1f3b95898aa65d17e97
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:13:14 +0000

    ordinary ride smoke wing total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
