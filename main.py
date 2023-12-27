a93283837d82f08fd953bbd9568503098b4eff81
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:34:41 +0000

    plan cheek goback(to) declaration upwards

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_exponential(x):
+        return math.exp(x)
