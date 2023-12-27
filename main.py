5b057c5368e1a1388720b460a3c0b64d39cd5843
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:35:13 +0000

    share pronunciation always ours minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
