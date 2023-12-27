900c68fabef87d3c2dd72429d7c523655d3c88d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:39:36 +0000

    computer nature mind face himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
