1808cc76f0912cec3e5f53b36a06e810a40ed038
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:08:43 +0000

    comfort flight money birthday business

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def cube_number(x):
+        return x**3
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
