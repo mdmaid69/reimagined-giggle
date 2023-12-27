036e4583ae803d46a04d585fd8a8126a16f498f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:52:34 +0000

    from odd black surprise below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
