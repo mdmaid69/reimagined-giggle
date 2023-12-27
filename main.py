6d8759673da351775db126381af1ff6db46a7bc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:55:05 +0000

    undo slice male maybe towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
