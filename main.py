42904d142431dbda4a2b1dea1017cff2723aa898
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:30:53 +0000

    move goout really father damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_even(n):
+        return n % 2 == 0
