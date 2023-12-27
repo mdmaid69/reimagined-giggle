a9dcbb95ce6abbc490869fa58dba8d28b070871b
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:14:23 +0000

    comefrom arrival clever club attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_acceleration(speed, time):
+        return speed / time
