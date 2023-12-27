fff86fbb5f4514a0c4f0ac804fd3c7a4975ee4df
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:02:09 +0000

    suck form popular lookup floor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import sys
+  def get_python_version():
+        return sys.version
