f4bbdad1288d56c760961b2d4fbfd28cdac6a348
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:33:13 +0000

    over possible colour later workout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+name = "Python"
+print("Hello,", name)
+def calculate_speed(distance, time):
+        return distance / time
