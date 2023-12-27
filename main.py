fe2a42b735a85154de7c238448e010a8dd6bd239
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:44:54 +0000

    amount like future joke completely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
