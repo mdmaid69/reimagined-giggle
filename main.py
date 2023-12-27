18ec0c2b54d1dfcbcaab92c8f01a8adc032ba8aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:22:12 +0000

    instead point could note well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
