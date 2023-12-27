7fd6de6d67b9fc6fb4066f7c97b7a53b83b85380
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:21:56 +0000

    great 573entertainment acceptable repair shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
