0f0872c696df05c017a0ed226539ecf7ff02afeb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:15:16 +0000

    moon type mind polite health

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
