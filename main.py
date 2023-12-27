f115537ef45870ea05e8d3b20d51804cd72b9467
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:06:40 +0000

    wipe target under minor oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
