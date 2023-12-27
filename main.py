5c3f99509ab7d310f3b7cd8bf176c07e9b8175cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:10:54 +0000

    clever prepare outside frighten bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
