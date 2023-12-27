09ff1da8bed249b60e81c9130a4efdc102f27509
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:02:34 +0000

    trip rubbish soak pretend violent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
