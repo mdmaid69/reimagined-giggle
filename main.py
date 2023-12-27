201cf31b8a8826713abf823fdc3e7f7d7e3dfa00
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:19:35 +0000

    class wear squeeze afternoon pay

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
