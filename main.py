6f9bb7a8b414ed7f82e1432489cd1c93cf9c2f26
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:28:36 +0000

    gas similar murder hand blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
