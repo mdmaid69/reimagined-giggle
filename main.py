dac7cf9deb3008043193acfed12b9681ead20130
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:14:53 +0000

    pleased although smoke terrible breakinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_current_directory():
+        return os.getcwd()
