566d7a76201d91e98c80bf1e228b918fbc09adca
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:25:58 +0000

    comfortable wise age understand complete

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_density(mass, volume):
+        return mass / volume
