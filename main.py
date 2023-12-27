043025c1d9e6077ff8a0c338b23c8de5176ed774
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:49:32 +0000

    emphasize likely bottom even knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
