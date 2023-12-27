ea233019510533af194b063818def0ff2fea0269
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:00:12 +0000

    post worse smoke process gooff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-from collections import Counter
-print(Counter("hello world"))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
