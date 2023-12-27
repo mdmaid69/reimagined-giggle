aee10f652130eb4be16cefe089e41e58826f7aa0
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:30:21 +0000

    wing classroom dull top detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
