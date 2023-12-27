519c2db85286399b9618606364f80ae83fedc4db
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:45:13 +0000

    hurt worse when each information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_hex(n):
-        return hex(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def reverse_string(s):
+        return s[::-1]
