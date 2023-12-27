dc21daf705d03ca3cdc90050505900ad13eff53d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:42:13 +0000

    surprised tap whenever lot share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
