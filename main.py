2a7e1bf876a32cc8e6ae5cfec0d194510fb9e29b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:45:40 +0000

    juice aim fair classroom get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
