529ee6c471435b4e2aea5221ddd27cdd35d2bd70
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:50:42 +0000

    hurry thin trust accidental world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
