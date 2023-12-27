88214708e1304377015288758acf75beb029ead9
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:18:49 +0000

    confidence study lunch urgent clean

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def reverse_string(s):
-        return s[::-1]
-for i in range(5):
-        print(i)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
