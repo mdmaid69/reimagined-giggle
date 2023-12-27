831af2aaed31b7e9aa243703c137da90e36cd0b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:03:04 +0000

    attract very own into search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
