55c2d6128331d7192131a93a4a1aca9254ddf7e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:53:10 +0000

    generous on direct relative cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def check_if_array_contains_item(array, item):
+        return item in array
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
