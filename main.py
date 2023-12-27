b48cc1a86674f7c6a0c29c8814a117a79987ce91
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:39:08 +0000

    beginning Cc hate no by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
