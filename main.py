37a4ea9217c4e7f70bb8bfc77bc7add7b4057361
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:33:27 +0000

    appear mean part trouble arrival

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
