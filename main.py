daaa2740fe80cd659d8b58a9eb7712d749e23d47
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:41:53 +0000

    rush compare ride change concentration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
