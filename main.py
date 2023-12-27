751a51f4b4e325990e7cb931e74dae541ebdf261
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:56:04 +0000

    task under university nervous borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_floor(x):
+        return math.floor(x)
