344ca31bfc995d8531f1f24324c973beab7661d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:09:48 +0000

    game nigga until increase hard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import time
+print(time.time())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
