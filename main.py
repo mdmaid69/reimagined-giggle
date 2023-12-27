7f2daf2100810fa4b4c382201eeadbfc69d824bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:53:32 +0000

    cut machine division not off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
