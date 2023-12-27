91564130ea0a42ada897b6b15e2bd6063dd8c0dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:19:33 +0000

    disgusting aim safety turndown book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def append_to_array(array, item):
+        array.append(item)
