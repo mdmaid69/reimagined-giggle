2c52673c68cc1c54f2dce94d5d57b97d67f3e025
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:33:25 +0000

    invite advise design ought joke

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
