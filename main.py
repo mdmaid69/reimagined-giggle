f037a90116e7995a68a32a452081d65e92294d59
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:09:15 +0000

    invitation consult state light aim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def find_min(lst):
+        return min(lst)
