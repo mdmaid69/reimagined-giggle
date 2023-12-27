c58b7762395b7c1ac744f6b1dc3d6c6ff694830b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:42:53 +0000

    rid cost hers Oo throw

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
+def calculate_circumference_circle(r):
         return 2 * 3.14 * r
-import time
-print(time.time())
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
