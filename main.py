165c1dacaae589799226160cadb21080cafe6cf7
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:58:36 +0000

    pause enthusiastic he explain talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+name = "Python"
+print("Hello,", name)
