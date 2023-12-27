ef5b76f968410b24b0892200d4091ebedb735fe8
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:26:56 +0000

    eager special surprise target judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
