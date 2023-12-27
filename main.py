c0bfefa748671ba1978f780c94cfc15d5ebf45ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:11:41 +0000

    start cost situation getoff way

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def create_counter():
+        return collections.Counter()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
