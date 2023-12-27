32533e5cab88733f3ea6411ba8a750aceb595c11
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:17:03 +0000

    condition once date run bury

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def divide_numbers(x, y):
+        return x / y
