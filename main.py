922115aac1209da400fff1a50806685f0dcdcadd
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:32:33 +0000

    during expensive suppose height clothes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def reverse_array(array):
-        array.reverse()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
