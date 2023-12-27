0c50d5451a8354e2fc9266869bb58f0587afe7aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:25:40 +0000

    pot every shape thinkof expensive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
