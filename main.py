b6de52eba3a886eae211f8d5864df41a4719145e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:45:17 +0000

    control weigh wide union address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def append_to_array(array, item):
+        array.append(item)
