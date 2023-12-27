5e2a408c5941c3de05102c22feecf76cc6ad341d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:16:48 +0000

    class your build lose comeround

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
