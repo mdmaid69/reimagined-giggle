9efda97dbabe737ea802b44744b3697645174284
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:58:08 +0000

    post through pull unite past

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_area(radius):
+        return 3.14 * radius * radius
