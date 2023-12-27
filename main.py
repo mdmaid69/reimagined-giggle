16fb0f8c0590cb1263d312ac2371b3b381682df0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:44:52 +0000

    rich better tidy use themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
