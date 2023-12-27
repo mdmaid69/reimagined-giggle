700af32548e85ec0ce6f4784f1f527f32323ca82
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:46:18 +0000

    fear notice hurry behind manage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
