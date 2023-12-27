7ab933b50260e719943e1e42273bb80e2603999c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:20:32 +0000

    help badly comefrom solve standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_length(array):
-        return len(array)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
