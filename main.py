2407c07ff4901451364fbbf328a96688128b8f99
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:26:38 +0000

    somewhere more anything project runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
