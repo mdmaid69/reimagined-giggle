1bbab627e19309f08c4fb1221ab8de50dde26d68
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:05:16 +0000

    once offer division life house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
