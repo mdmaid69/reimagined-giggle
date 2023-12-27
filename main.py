4f9a846853f02db464c3c4f60bcfaf8791702a38
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:24:38 +0000

    injury wall rarely second proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
