6a25e1540f0834b0f14b67b50f95b1fbf7e609aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:31:50 +0000

    good capital imagine believe fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def subtract_numbers(x, y):
+        return x - y
