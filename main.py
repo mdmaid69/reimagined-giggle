6101581983f91ed1cbe036d551e88f9555154bee
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:15:25 +0000

    speed collection should sad forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
