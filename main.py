b9767a32834a8cf227b858bb1d5f88dc3f37638f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:01:07 +0000

    better official obvious they aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_counter():
+        return collections.Counter()
