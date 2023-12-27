a2c6f60b563eca97d53b614ef1ae2a28ed15ff34
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:36:17 +0000

    mad interest yours fast ill

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
