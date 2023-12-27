6b179682fbe17ea9835070ba4386c9a78a8b53ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:54:08 +0000

    unkind properly room teach mark

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
