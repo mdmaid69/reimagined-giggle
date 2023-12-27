a400e43b20d88c9c8547db0bf486b522a0a2c06e
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:58:23 +0000

    only willing benefit tax sick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+name = "Python"
+print("Hello,", name)
