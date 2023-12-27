654795f7ab727bf6c83fbd65f822c6886f6324de
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:41:07 +0000

    rise workout force cap collection

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def find_min(lst):
+        return min(lst)
