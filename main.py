406108e2e2fa01e0fd8f24ef669508a1eb0878f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:29:37 +0000

    towards concentration rub sore anybody

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
