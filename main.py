18e72862fc40e856510af181666daa93caa177a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:38:39 +0000

    fall club fortunate unlucky stir

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
