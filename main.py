1f1b5dacb0014071af6573d921e1e2360ae77ea9
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:56:01 +0000

    image strange rough on react

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def find_max(lst):
+        return max(lst)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
