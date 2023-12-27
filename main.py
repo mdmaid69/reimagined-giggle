e9d7b60b7976f7f1ea0aa11b36545212d8a424c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:09:44 +0000

    properly bank rush address hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def square_number(x):
-        return x**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
