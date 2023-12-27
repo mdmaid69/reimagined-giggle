4558ddb06d1ffd665eb829bddd5c4a95b525bd09
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:25:47 +0000

    again tend early grow arrival

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
