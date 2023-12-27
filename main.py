2679e5c1f0433ce90246dc3d5d496fc6dea16cca
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:26:21 +0000

    comfortable reaction bridge new lock

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
