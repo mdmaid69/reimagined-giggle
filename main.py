11ae6dc618718add4b06ede3cdc1817cdecb0848
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:50:20 +0000

    wrong translate worth high phone

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import datetime
-print(datetime.datetime.now())
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
