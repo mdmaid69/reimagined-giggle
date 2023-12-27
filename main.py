1efc51a4cd8b803e42f585c810a9d9817d6d18fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:01:10 +0000

    disappointed every offence favour shelter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
