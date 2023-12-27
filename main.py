2159ac269ea2165f340722672c53ae750e4e38e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:36:04 +0000

    poor achieve either meeting want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import json
+def convert_to_json(data):
+        return json.dumps(data)
