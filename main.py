7531cc6754139bf142c8c85993ce7847dce8ba5c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:13:29 +0000

    ear pronunciation weekend culture friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def clear_array(array):
+        array *= 0
