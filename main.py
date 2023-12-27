145b5c49d4228e525218aae8e9f274e2d7d0e2e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:42:33 +0000

    stir pretty hate record soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_priority_queue():
+        return collections.deque()
