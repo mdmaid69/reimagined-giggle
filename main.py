ff986e1cb2fb13443c59bc1a3dcecbeb15ebbeaf
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:11:33 +0000

    shopping condition Jj floor maximum

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import sys
+print(sys.version)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
