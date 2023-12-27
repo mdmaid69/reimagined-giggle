17befeac403306d344c88112585642d57f83efa8
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:57:48 +0000

    react murder completely cinema respect

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
