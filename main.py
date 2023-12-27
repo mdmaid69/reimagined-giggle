1a71b9a879a86fe09b3356cef376898ccbbd4c29
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:02:11 +0000

    satisfactory front study value Ll

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import sys
-  def get_python_version():
-        return sys.version
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
