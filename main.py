fee97f2fbce4e8beea168a99dd1c07057a0601d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:40 +0000

    embarrassed compete black sink wish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
