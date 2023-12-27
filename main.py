3e6e85b830411a5b8b2c62f3b003819f9bc31ff5
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:42:07 +0000

    copy fail laugh female accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
