d07911f584d143036089345e3bbcfc519d10d8c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:21:20 +0000

    language excited quality hot move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
