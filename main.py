b8b11229c70bb107f57e52316091aadcff2d3477
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:41:22 +0000

    judge send reliable invite substance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
