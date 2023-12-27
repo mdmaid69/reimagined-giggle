2c0f6961cdd22f490ff155d6717627bdf0d16ee2
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:32:21 +0000

    high message giveout evidence pronunciation

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import time
+print(time.time())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
