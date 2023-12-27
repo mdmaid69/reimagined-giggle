1eef0b81e85523785b1c06ad5138fd77aa91c7d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:03 +0000

    raw mine hungry plan woman

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_list(array):
+        return list(array)
