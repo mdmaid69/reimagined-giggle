60ea0c783ae6898d4fb4d22c93817a84348d1b06
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:18:22 +0000

    doubt mad by have bringforward

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
