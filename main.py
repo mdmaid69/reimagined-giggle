1f3e3f17673cf518066a9e1b7b1ff044c14e5ffb
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:28:04 +0000

    clear study cruelty container worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
