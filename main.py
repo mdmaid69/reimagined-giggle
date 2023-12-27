9310fa5e0592b3e1a246285567a9f8b0be8e2762
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:08:19 +0000

    cap prepare contact Ff exact

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def count_elements(lst):
+        return len(lst)
