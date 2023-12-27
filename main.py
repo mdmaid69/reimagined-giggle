77fcdb1dc5be79cdce64d43516418de2f836b581
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:03:47 +0000

    cough another picture limit runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
