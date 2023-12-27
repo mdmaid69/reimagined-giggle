5e2531db684989d565cbccd11d43c3462b64df08
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:40:27 +0000

    comment set comeoff edge cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
