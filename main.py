e7f8aebce6b64064e8ebb1befa4a7c6c35483dbb
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:06:56 +0000

    wheel them maybe somewhere error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
