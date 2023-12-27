550890e2f80346a0d3f4744925d3e188a169ed3d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:11:46 +0000

    label sweet attempt distance independent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_list(array):
+        return list(array)
