3c6ef3063a7312909045b08505bb41b95cfc6f29
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:15:28 +0000

    evidence 564encourage summer nowhere doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
