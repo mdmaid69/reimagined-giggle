7a38535ad36e24aa359bf2de36d15a8092c4b8df
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:03:01 +0000

    really pin criticism treat conduct

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
