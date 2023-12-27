7255426906b51cbc6eb2a572ac5a01ad34f76db7
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:02:37 +0000

    will better it term second

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
