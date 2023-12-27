616847e954e9f70efbd8ca5358c956c50ae87dca
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:36:02 +0000

    finger trouble careless talkinto explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
