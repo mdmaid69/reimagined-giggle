fa88afcb5b1e4807cceffbd4a9ea08f5bf92d53b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:14:07 +0000

    room try take real present

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
