7ec36e838deee8e295868796b96dd4a6102518bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:55:35 +0000

    real turnback separate talkoutof no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
