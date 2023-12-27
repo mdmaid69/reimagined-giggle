9c9e05e4cdb82aa14c4c8db06cc8db051eef0e2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:31:33 +0000

    estimate check offence contact feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-print(sys.version)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
