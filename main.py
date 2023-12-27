4fcb5b71beb1b1c22cf707687c141ba25d9d6c27
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:46:21 +0000

    yes shop swing one pretend

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import time
-print(time.time())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
