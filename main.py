23208ff6d728d7aec0e93edd3c7b253b4f460284
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:53:16 +0000

    any early regret prepare payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
