36eed4291747f03e975f69f3f2c4be48d7bdfa64
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:15:58 +0000

    attractive lower culture team mainly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
