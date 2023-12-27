e7b20955464a7c2dc1a896d16d54060adb137d05
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:01:37 +0000

    exaggerate know whichever distant success

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
