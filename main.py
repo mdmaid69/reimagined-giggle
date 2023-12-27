b1545812cdcf2ec268a96fab6baa22b32ae78a7d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:14:45 +0000

    piece furniture even over street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
