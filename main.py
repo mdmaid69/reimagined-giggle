8cc0ee1352df05fbe9d958d01ec6e3f030223efc
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:30:43 +0000

    male no substance somewhere clean

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def remove_from_array(array, item):
+        array.remove(item)
