c0586e14bd2ca0a31d599abb4aa9515b12e4b539
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:20:57 +0000

    notice instant guilty rid 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
