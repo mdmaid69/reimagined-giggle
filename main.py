aaf63dc4d070053034c0dc50e14f12ee775f13c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:49:34 +0000

    awake glass small fix dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
