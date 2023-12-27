0addd6c1b9769937cfdb078da62e72c848338418
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:25:21 +0000

    want rough equal usually season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_density(mass, volume):
+        return mass / volume
