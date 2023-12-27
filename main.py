d1682de935afd3100300a19425deceb8b878e549
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:56:52 +0000

    wild attention general suit petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import sys
+def exit_program():
+        sys.exit()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
