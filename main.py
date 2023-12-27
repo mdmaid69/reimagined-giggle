275dfaf1caaf089930d02398bc3b52abdc92d572
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:30:43 +0000

    sleep flood how lend stamp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
