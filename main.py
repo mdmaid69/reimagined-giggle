e64839bff93a60223306306d2c8e9a6ec87e9cce
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:49:51 +0000

    with sister mistake become stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
