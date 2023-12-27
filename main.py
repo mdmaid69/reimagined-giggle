18f663c0ad31cc95aec0162462898faf9039c5a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:13:25 +0000

    getoff rank flood arrive fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def add_numbers(x, y):
-        return x + y
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
