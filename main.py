7e8c6aa6df79fade3db3352ff1a4e4c4742b2042
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:25:52 +0000

    plastic touch straight sleep impatient

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
