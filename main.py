b18473bfc621747e0cbcde782dd0daf8b08971db
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:43:29 +0000

    573entertainment beside maybe bet guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
