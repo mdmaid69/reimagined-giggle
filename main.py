3c79739a2f4ab52ea6e2f38aeb405a472dafd9a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:51:57 +0000

    treat message switch cause balance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
