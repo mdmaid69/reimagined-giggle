e0d7c50d855563592071997ddf62924cc8b7e02b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:15:06 +0000

    possibly clearly blame urgent miss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
