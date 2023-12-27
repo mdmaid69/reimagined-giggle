9d01c2b7e8e4e1a9db9fe490770351c683bf0c47
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:23:40 +0000

    congratulations nearly shine lock cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_string_from_array(array):
+        return array.tobytes()
