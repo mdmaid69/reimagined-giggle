ce3f25395ab2fef9e3aab23be631ef52d74bdc88
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:08:37 +0000

    lot basic make laugh cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
