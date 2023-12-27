5abff3598c2ae03f9d00c1b49238cd8dd8445e93
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:00:36 +0000

    fine so inquire plant stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
