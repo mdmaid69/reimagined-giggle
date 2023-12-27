b6991da46097031639a35d5dc1dd740b44599cf4
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:25:37 +0000

    what morning holdup fire 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
