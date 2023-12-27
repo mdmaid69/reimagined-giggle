361c388f3db605a1e998a8cf8792d9ec2978d6c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:05:48 +0000

    morning yourself closely flow likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
