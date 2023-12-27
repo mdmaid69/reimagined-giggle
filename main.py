1e8dc5a1b8f00ce1173d3af559bd5b340327c527
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:37:45 +0000

    write art move give neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
