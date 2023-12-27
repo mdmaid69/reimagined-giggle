5c6592abb02b8f5941af64dc487e1f1f171a4a65
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:54:06 +0000

    cheat space underneath keen thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
