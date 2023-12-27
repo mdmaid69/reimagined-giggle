d9919e0ed1029be8d00264f57267d47cb357b77d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:11:55 +0000

    getoff will patient scrape law

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
