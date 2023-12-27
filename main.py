939327a640def28159ce24920c08b47776dcdfce
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:35:00 +0000

    bush passage brush gate free

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_words(sentence):
+        return len(sentence.split())
