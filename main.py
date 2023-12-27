a348c13f4df26c636c9c0b201e3642fc2eb06613
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:18:21 +0000

    floor nothing attach equal better

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def reverse_string(s):
+        return s[::-1]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
