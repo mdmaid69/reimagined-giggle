8eea93aedff85f37c5b2b6403523b0e071a75a5b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:31:26 +0000

    keepoff knowledge invite excited laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import datetime
+def get_today_date():
+        return datetime.date.today()
