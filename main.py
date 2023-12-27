fb0ed6b51f7d3abe2a69daf7ea1f2e08148cc39b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:19:50 +0000

    finally would warning operation material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
