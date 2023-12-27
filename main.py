57770de012edeff5b7a02be6bc30d624a593447f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:31:23 +0000

    since mind deaf loan determined

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_area_rectangle(l, w):
-        return l * w
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
