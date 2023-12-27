46496914ca01287f71e923feeac0740b84b1ae0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:42:56 +0000

    little offer sport top smooth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+for i in range(10): print(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
