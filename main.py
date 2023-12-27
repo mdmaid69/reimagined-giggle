51934dbf453a0eeb8750dacf5853b1ae7b137e72
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:24:48 +0000

    long house generous couple see

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_work(force, distance):
+        return force * distance
