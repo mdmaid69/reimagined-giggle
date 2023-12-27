c6263d2761abbc666b18ff4a95c93b9a3ac3e51f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:57:49 +0000

    understand survive tune single congratulations

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
