5ee982d2ab36b2bb4e99c0d266dba1e48dfbdb6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:10:49 +0000

    apologizeapologise experiment water phone everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_floor(x):
+        return math.floor(x)
