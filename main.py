e1e38ba050c69f5b2466a7c7a66df7eb43a306c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:40:59 +0000

    ago match behind ill opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
