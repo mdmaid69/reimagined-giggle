96b5dd576d2657885678f84190ad9e867fe42d9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:44:15 +0000

    foreign roof foreign clean natural

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_density(mass, volume):
+        return mass / volume
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
