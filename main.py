3c901590acc3b9eb3d1951ae9799299bee89aa77
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:10:54 +0000

    question present lay case reliable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
