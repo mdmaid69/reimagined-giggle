a97453ea0f65802782a7ccd79ebf2512b4380abf
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:44:37 +0000

    Uu deep bite steer lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
