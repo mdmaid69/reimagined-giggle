6dda6055a31d0764326762628bf05a610f28b06a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:11:02 +0000

    news straight still getback both

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_length(array):
+        return len(array)
+def is_odd(n):
+        return n % 2 != 0
