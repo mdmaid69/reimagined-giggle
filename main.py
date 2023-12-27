7f29f3cb1b278649bff2a21caa6e17a885da2e94
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:30:01 +0000

    away bet ordinary black advise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
