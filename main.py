979dbabb41b80dabc5e37d35a16d034df36d7c17
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:11:25 +0000

    begin ring nurse such worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
