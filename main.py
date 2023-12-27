4860026b929df1add1a9cf797afffb6f6e5860d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:00:58 +0000

    discover criticism tradition tap charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
