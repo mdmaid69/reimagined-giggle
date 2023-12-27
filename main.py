e39af7e0ab4443b60f84e83b613cad41dab1d128
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:37:33 +0000

    before sister goon rubbish definite

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
