7d95b80c1b18ce02bc726535cc8981f07da36a30
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:28:09 +0000

    wheel check hold influence turnover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
