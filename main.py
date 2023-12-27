846004e854b2e01bf7e197225791c80e832fd04f
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:19:13 +0000

    characteristic equipment many build hear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_queue():
-        return collections.deque()
+import os
+print(os.getcwd())
+  def convert_to_hex(n):
+        return hex(n)
