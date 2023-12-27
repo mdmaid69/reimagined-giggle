3218d6751dc6504bb1c35145d8ed3bdb51b13009
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:31:49 +0000

    meanwhile swing similar workupto nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
