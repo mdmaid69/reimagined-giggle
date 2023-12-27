3ad8434a4ae8b1b33ec6f9e0022fa7ee3ccbe824
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:04:07 +0000

    count afraid test excuse save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
