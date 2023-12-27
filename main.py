fb6790dfc32814008e1bce3dacabff9078fa5066
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:16:50 +0000

    tradition expression actual question lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def convert_to_binary(n):
         return bin(n)
-import random
-def roll_die():
-        return random.randint(1, 6)
