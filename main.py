9b60b600a02c5df01b711dc1bbc520de950e8d42
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:22:38 +0000

    wing level damage black will

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
