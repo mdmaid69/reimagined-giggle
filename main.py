783da69cb76504d8c29d2625d8d583be385fe77a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:19:02 +0000

    goback(to) seriously towards trust slight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
