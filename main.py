739f06cd51fa63f324615cc4f0ddd8c307e44480
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:17:22 +0000

    black connection muscle medium step

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
