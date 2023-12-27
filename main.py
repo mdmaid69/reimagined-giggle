a2f66d1801718cadf2e56ad7359b2cd5dc4d414a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:04:28 +0000

    hello breakout but coat wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
