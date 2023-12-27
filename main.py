9faf61b6a5a2243cfe019724da731b2a914bc101
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:03:42 +0000

    suckme after concentrate film under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
