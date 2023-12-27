3bbf1c964e833a7af8c37de69aaffdf0199f7b1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:01:17 +0000

    priority mess sit lookup steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
