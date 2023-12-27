a0daed882cac91a37954f3a3e7250190964900e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:53:57 +0000

    Uu attempt goup behave late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
