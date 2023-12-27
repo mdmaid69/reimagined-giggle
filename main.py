95897339a45d1f023f8b43599a6d4730892b077b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:58:18 +0000

    certainly obtain out return well

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
