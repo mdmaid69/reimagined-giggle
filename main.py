125a8fbf0c9a035a832dc8c6650d8569b6d695ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:30:33 +0000

    disagree fond clear power murder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
