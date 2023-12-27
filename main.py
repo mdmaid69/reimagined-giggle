d04d72fa07315ff52803760aefbee3178a8580be
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:33:14 +0000

    heat conclusion tie load exist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_min(numbers):
+        return min(numbers)
