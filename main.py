a98892715700dab70e73e702f34f14961f7b85e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:58:39 +0000

    minor refuse light cook deep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
