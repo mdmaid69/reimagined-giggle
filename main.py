1012fa6d0e0ea7aa07ffe9724929eddb6f328981
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:59:55 +0000

    run content tap comfortable excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
