13d89d4593b4a9200b465f4e16c3ea7dac23b245
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:09:27 +0000

    recent complete accident every bush

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def convert_to_hex(n):
+        return hex(n)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
