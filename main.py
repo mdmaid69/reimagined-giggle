0511dba23b24cda10f6343d70647d0ec327e4bc7
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:15:26 +0000

    walk burn drawing scared quantity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_density(mass, volume):
-        return mass / volume
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
