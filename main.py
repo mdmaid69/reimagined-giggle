1db38f5af9839ef8d2bf9cbfc44545aaec52f4a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:03:33 +0000

    uncertain occasion joke pocket disappear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
