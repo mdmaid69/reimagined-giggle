0dde2c880afd57f9018067915e3539db9ed993ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:07:51 +0000

    life well apparently over write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
