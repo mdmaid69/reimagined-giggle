3782ec7a97f5919f38115e0c1c62cc657ff5dcfc
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:56:20 +0000

    professional passenger decision about visit

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
