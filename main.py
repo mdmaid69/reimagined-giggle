fa45edaf99ee1cf73edcdb01862f92889b30f8c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:10:45 +0000

    limit along bury rough problem

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_power(work, time):
+        return work / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
