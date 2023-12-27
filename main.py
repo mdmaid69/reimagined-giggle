57491ecd2546bae37c5314a2160128ff34b2126a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:27:13 +0000

    tread impression both way callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
