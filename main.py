c37deef95e60dbd35997b60508b2d4cbbdf5a188
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:10:55 +0000

    proof fact condition middle holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
 import array
-def get_array_length(array):
-        return len(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
