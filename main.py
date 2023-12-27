47ce6f4c2991723542356e54b04436fb116693ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:09:17 +0000

    view several somewhere punishment cheap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import re
-print(re.match("h.*o", "hello world"))
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
