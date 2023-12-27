242a749b514415a20819f207c0a61f86b935af04
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:41:31 +0000

    disapprove runinto variety refuse mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
