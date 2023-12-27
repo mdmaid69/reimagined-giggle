5a7803192f7c28802b3ca86a12dd86ac4f8c36ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:25:33 +0000

    thumb food means string far

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
