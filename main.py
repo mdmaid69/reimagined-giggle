2677a3b58087a7b885fede550b92f24dca904d4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:31:36 +0000

    look let past necessary split

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
