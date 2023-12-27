7bfeca09ac735f210519415b5538bfff57e6adb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:14:52 +0000

    honest heavy present wander reaction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
