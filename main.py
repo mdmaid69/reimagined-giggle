48f81258d68352c0581e9161c4f27948487b8e13
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:44:13 +0000

    laugh though anywayanyhow ride dish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
