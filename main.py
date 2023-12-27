e5ddbb40b9c111f2d6b30fcb1282adff1345dfb5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:15:18 +0000

    cough punishment extra band bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
