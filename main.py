2799a7ca58a7fb97b6e2ebbd8ee49d4e70488776
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:44:38 +0000

    call lost put whoever term

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import json
+print(json.dumps({"name": "John", "age": 30}))
