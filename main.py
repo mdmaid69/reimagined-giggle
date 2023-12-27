1cef30a8110deb1778c22125bcc5f2401fce434f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:20:58 +0000

    extraordinary bringup 567enemy sweet band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_array_as_set(array):
+        return set(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
