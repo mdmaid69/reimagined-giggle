4c476702d0a6f94e13836ab91ef60183090d7c5c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:34:21 +0000

    underneath attempt downstairs classroom politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
