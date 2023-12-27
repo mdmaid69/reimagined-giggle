1057e55867da03b331c7c69e672fd0d1b86e58bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:46:47 +0000

    away design pay moral contact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def sort_numbers(numbers):
-        return sorted(numbers)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
