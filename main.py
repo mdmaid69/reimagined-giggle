bbb6854976cb143caec931f8e7735d438d008be7
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:19:33 +0000

    break tell material get risk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
