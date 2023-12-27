b7b54db4dbfccc9823466c92aa1f6ad16aee8eae
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:37:12 +0000

    have closely base cough base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def cube_number(x):
-        return x**3
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
