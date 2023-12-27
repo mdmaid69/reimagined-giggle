76ab07dff97624f4c129710ce1f86a89f25ff1c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:30:55 +0000

    step understand ready side remain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
