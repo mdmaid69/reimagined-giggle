6b4c88efb4d162a9a98047da8d50ff2e025dc3b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:01:39 +0000

    history dead flat pattern spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import os
+def get_current_working_directory():
+        return os.getcwd()
