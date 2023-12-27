9e6eff5ad4548683f627daa656668123f88e95f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:28:48 +0000

    accidentally joke completely news first

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def count_characters(sentence):
+        return len(sentence)
