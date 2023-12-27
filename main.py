0835000df3171502e29e6d10243f341f16b6b098
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:47:42 +0000

    class occasion environment interrupt intend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def find_max(numbers):
         return max(numbers)
-import array
-def get_array_as_list(array):
-        return list(array)
+import time
+def get_time_since_epoch():
+        return time.time()
