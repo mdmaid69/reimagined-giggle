a56facdf5eaae245baf0f174a73ce1b91eb83f68
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:39:43 +0000

    popular suddenly visit protest stick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
