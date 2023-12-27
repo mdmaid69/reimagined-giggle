5234ebd46052d5114e0f20822705960092b22b1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:54:30 +0000

    appear moon injury rule minimum

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+for i in range(5):
+        print(i)
