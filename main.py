ad077f0266793968f1df0e475a41264950ba5f39
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:40:53 +0000

    takeout into along shade before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
