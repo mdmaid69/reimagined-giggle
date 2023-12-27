56bf4c7df85452a58fcfc442b114ec2838cba7d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:58:32 +0000

    left ear shake shut regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def remove_duplicates(lst):
+        return list(set(lst))
