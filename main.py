d4ade3cd8a27941199541d6dfd87cebda4150c63
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:31:48 +0000

    no separate experience stir minor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
