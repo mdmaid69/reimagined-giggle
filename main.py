56f3c648089fdf8487d94b820705c962cd276bb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:00:18 +0000

    environment brain moment back sensitive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
