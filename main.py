fbb6c2636264d881d0da1d5a88d9435284f58947
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:46:13 +0000

    unkind fit brown unusual underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
