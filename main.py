c018dda02d83c347c96f5f98f706f0952c79bfe8
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:05:44 +0000

    minority top complaint satisfactory still

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import json
+def convert_to_json(data):
+        return json.dumps(data)
