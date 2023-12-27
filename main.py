83441212c33510018dc980e14a6901314106c533
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:03:45 +0000

    future stage workout central government

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def multiply_numbers(x, y):
+        return x * y
