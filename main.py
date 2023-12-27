ca96818da66616831cd5f01e838684e0289eefe5
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:23:34 +0000

    trap school safe forward Ss

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def is_odd(n):
+        return n % 2 != 0
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
