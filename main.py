424efd6293b18f54c44614f75d3d15adb46683b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:10:28 +0000

    aware unusual another congratulations fierce

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import sys
+print(sys.version)
