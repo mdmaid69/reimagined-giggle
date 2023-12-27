672d46896ec15ccc246c2093d40db8b217bbd149
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:34:58 +0000

    either firm bringforward step though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
