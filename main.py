a3754f0c2c7d5a2a2377ef65bc862a4ab271adf3
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:41:42 +0000

    apply politics tool lonely apparently

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
