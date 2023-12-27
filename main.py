23b3e61f53c7a876b0b5a542ef20cdacd00ca241
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:36:22 +0000

    however distant gift effort add

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
