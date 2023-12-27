cad2b982de487b9d036e4eaa053aaa5f92260706
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:07:06 +0000

    any display Ww Bb stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def convert_to_binary(n):
-        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
