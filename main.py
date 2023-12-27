0874f8334f91a6c69780a6da136d5525a924b7ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:45:11 +0000

    570enjoyable show declaration your embarrassed

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print("Hello, world!")
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
