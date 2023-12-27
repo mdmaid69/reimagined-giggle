f4e295ef12afea99147ac431a77c559d1e1fe522
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:28:58 +0000

    official process nervous dark correct

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
