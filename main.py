6b64b02ee005306b1c3d52a408efd02c87b02111
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:07 +0000

    rank best actually once lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
