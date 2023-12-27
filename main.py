234425ff6e22a6510d61c6cc4e8227d6f078cb4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:56:24 +0000

    respect lucky whom earth suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def find_max(numbers):
+        return max(numbers)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
