4e1060a56a861fe0b160b5c853fc3055714cc387
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:44:58 +0000

    hit crowded maybe wrong tie

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_list(array):
+        return array.tolist()
