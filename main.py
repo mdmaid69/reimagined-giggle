31b7753eee5f2c3902adcd5413e554610be4ee1c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:05:07 +0000

    persuade by himself murder thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
