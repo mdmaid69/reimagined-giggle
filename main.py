085782fcb4896c516d4c2490a474c84b4c68329d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:11:18 +0000

    stay clothes art defend opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print("Average:", sum(numbers) / len(numbers))
