2ab4f8510a6b29400615ad24c1d710b31c19ef50
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:14:52 +0000

    bad manner 568energy result way

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
