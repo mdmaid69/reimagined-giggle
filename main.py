91ae3abf91c6691e0e79e8c1c40490b368b296a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:57:50 +0000

    agree loss government talk content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
