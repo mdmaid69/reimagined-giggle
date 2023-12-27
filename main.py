6216ee2eb6eb4b5b2fa467cbc3260d7040dd1397
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:26:38 +0000

    middle suggestion fasten or convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_circle(r):
+        return 3.14 * r**2
