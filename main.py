7a86091d7c49fe002a4b50e43eadae138366e700
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:15:18 +0000

    consider although move needle however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def remove_duplicates(lst):
+        return list(set(lst))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
