3413c07bec4c28102a88606b4184073e09ff817d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:39:09 +0000

    yourself while sister far nor

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def convert_to_octal(n):
+        return oct(n)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
