8b8ea11b3213aaf75fbd11cb1ed321a6d906bd49
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:53:00 +0000

    practical deaf one reach dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
