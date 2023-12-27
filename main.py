6aaf78fc0761201f2e3f1a47e34fe54e577a6819
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:55:58 +0000

    rule cut bear future emergency

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
