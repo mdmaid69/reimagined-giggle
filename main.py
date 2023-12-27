3e3e1521bd034df9537be426a3a970a0503703d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:51:05 +0000

    perhaps spray bringback harm use

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
