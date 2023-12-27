46934fb2111c963224d41ddf632c067a70dd76ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:59:27 +0000

    impressed government curious total yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
