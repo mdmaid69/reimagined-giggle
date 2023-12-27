ec8871a71dacf9d2bd529bbb5edfeb8376dd06aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:39:34 +0000

    suit picture life dear offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def cube_number(x):
-        return x**3
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
