07a1879f1695fe9ea4cc8a2a8e9f840227238392
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:55:52 +0000

    cry innocent long boat rate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
