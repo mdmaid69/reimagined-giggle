233cc85bc2909667131dccad80159b27113983f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:24:36 +0000

    unfortunate freedom cope translate typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_speed(distance, time):
+        return distance / time
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
