3d31019357ad468d41e878541bfa058ab8a118d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:05:26 +0000

    edge blue ago he present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
