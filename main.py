6db5cc666cadd76df72c6a1bf43de66113be8dce
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:19:30 +0000

    exaggerate under disagree crime man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
