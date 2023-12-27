be1c396bbacf3860e47a1fae36e2c666621c8f34
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:41:28 +0000

    bang comein space consist like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
