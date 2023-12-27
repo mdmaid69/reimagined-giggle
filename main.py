4477a720e37ebf6b92debef4207e08e6be412a3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:47:36 +0000

    smile strict there politics your

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def calculate_area_rectangle(l, w):
+        return l * w
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
