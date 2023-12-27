56534d2b7f5c77fdc8cbf14f19971383badca38e
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:24:41 +0000

    mark interruption offer else blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
