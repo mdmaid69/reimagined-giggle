8efc2caa31fa8583e0e2b0c21f5c5bec6ca070f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:01:28 +0000

    effect breath belong forgive scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
