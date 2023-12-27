a5574026093259c3dab6fd5231859350f43d9c99
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:38:21 +0000

    connect inform whatever theirs responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def is_palindrome(s):
-        return s == s[::-1]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
