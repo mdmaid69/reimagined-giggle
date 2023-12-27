8a53b0ba3153faace209977cb5a0d146d1c29c61
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:44:53 +0000

    breakout step fierce book crisis

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import sys
+def print_python_version():
+        print(sys.version)
