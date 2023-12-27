fe587a117285c45d7fc43d7c722ef66ae9afe74e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:34:02 +0000

    performance recently over shoulder whistle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def remove_duplicates(lst):
+        return list(set(lst))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
