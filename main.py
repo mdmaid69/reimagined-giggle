a345c56af7a295a0d55eac0ea0f8d4fea13b443a
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:53:50 +0000

    meeting sport human rid union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_repr(array):
+        return repr(array)
