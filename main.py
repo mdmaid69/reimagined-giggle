f6a3ae3ce842d0a87dedbeed394420cd9ca45978
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:33:49 +0000

    tired official 572enter down tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
