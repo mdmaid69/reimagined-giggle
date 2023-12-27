c2d0d9a6f3e1f5a5cfe031310f37676942c902ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:45:00 +0000

    what steer dull record deal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
