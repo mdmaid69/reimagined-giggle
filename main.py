8bc8bae0dd55f1b7313170ae782c4704d4662320
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:51:52 +0000

    swim recovery place dry area

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
