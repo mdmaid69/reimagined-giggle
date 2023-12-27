55048cd1e130eae6f07ed24b2c58f3d280289928
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:36:49 +0000

    clean load meet morning tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-name = "Python"
-print("Hello,", name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
