9e27bb04c37697aadf36d0715ea801c17050a72a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:23:35 +0000

    hurt behaviour getback content look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
