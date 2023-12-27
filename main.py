b0b713305083d6d63efe81452ede93756d45cf06
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:56:26 +0000

    difficult Bb sell callback nerve

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
