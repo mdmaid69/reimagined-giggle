3991ba1f98eb6f4444475ea087080f418c93d167
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:52:43 +0000

    concentrate threaten direct band care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
