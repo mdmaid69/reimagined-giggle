744424f4116445f8039e35bef87d9d21cc5a867f
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:47:38 +0000

    experience city goup pleasure back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
