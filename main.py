050f2bd3023956bbd6022e8ade37dfe853c4bdb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:37:43 +0000

    problem for project hello gooff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
