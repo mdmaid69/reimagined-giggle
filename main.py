03593b72b6d02c69ebdf50ef5f165135940586c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:38:39 +0000

    there congratulate anxious curious after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
