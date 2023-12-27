0691e8c617b2e503be3ec92cc63ead53244a8c3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:21:16 +0000

    still fit music business congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_array_length(array):
+        return len(array)
