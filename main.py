b744432ef56910401792686d33a77e6d20e78aff
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:45:58 +0000

    long copy rid neither relation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
