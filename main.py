2d2660d6f62da0f271d571f59bb17a7c6fec81af
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:12:12 +0000

    shine print float exit family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def convert_array_to_list(array):
+        return array.tolist()
