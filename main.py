e3ba1223c1046f058abbcabbf901d21be13c4cd7
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:19:20 +0000

    suffer remain butter order busy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_int(array):
+        return int(array[0])
