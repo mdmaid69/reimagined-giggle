9dfa3decf622b58135de8db9a3c4668d2087cd14
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:43:35 +0000

    reject test dark decision achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
