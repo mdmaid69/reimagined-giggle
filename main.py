e3a51a60dc0d9186ba9b403bfa4d58db60667f43
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:07:29 +0000

    comfort busy noise admit surface

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
