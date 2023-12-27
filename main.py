b7baffb364ba63cb174f0a4d4fa44e61eee73441
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:26:33 +0000

    talkinto nigga risk circumstance nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_current_working_directory():
+        return os.getcwd()
