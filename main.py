46ab53a573252bd4073702f67ae13a74eed4f2f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:35:22 +0000

    hollow radio without learn usually

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_density(mass, volume):
-        return mass / volume
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
