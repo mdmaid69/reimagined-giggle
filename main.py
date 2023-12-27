11f6a805d0d56bee4f2673436c207ad8782fa17d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:37:02 +0000

    sea hold know pollution pair

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
