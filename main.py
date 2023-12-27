a6e332de80d5e6776b9d62b225a5daf7cfcbf34a
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:41:51 +0000

    wave forever perform too sleep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import platform
-def get_python_version():
-        return platform.python_version()
+def find_max(numbers):
+        return max(numbers)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
