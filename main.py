7e3785a90abe979304ff795951c4bf1d351ac7c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:52:37 +0000

    fun design past moral mainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_binary(n):
+        return bin(n)
