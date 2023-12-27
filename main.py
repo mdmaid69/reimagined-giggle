ba625ad8a348cab1d87e68bc32f6743072b86c63
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:49:22 +0000

    disturb centimetre expensive summer gate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
