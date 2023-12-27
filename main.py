6c28c15dbc95499c4c9957b6d9d4b6446ce222d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:32:45 +0000

    wide once room fence music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
