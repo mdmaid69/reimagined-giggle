83df17484feb3d25e13b91a9bd057f6cda1aa226
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:27:35 +0000

    someone point between creature set

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+name = "Python"
+print("Hello,", name)
