9c15a860916682ba7048d50e31b867d6484b627d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:18:28 +0000

    Yy motion practical squash whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def square_number(x):
-        return x**2
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
