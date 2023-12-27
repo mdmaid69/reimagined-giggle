0b72dffecf4728dbd3a70aa17c02dc0dc7607264
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:24:21 +0000

    pile scream turndown narrow hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
