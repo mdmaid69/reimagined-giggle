8e7cbee33647899b7ad159bf53a6cabf48951fe6
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:36:13 +0000

    sink perfect settle wind science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
