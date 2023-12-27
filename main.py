1e22c19af33c949ea3cc10412b31bba563992c02
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:45:01 +0000

    admit unable fence downstairs correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
