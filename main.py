43113b0176be01583ab82a91a10e6bbcd939afe6
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:53 +0000

    somebody completely along smile cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_str(array):
-        return str(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def sort_list(lst):
+        return sorted(lst)
