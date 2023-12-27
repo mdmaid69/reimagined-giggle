9aba49759be6812957014c8c8ee7b22efcd201b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:08:46 +0000

    respect track fashion wipe bang

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
