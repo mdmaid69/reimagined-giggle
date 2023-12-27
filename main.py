4977bed5b4cf8dec00733f2c80c38ee0a7eaf389
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:01:23 +0000

    through here impressed what main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
