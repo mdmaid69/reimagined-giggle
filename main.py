fba331e0b40a82026a0bd13207fa0dbf6a732ce8
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:20:23 +0000

    sure ought basis emotion tired

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
