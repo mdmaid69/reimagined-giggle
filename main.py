3bd0e19aaeac71878af7faeb71cf66c5e3447207
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:03:09 +0000

    contact geton lose least stiff

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import datetime
+def get_today_date():
+        return datetime.date.today()
