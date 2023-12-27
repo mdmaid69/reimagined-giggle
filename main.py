ce4e87d1aec01f33e5159a380b10c350b4f05084
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:39:30 +0000

    takeout aware turn clear score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_even(n):
+        return n % 2 == 0
