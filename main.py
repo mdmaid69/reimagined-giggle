2339b7e1121a5061153a00c0e062a44ed61ac4fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:43:57 +0000

    way majority mysterious lookup instead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def convert_to_binary(n):
-        return bin(n)
+def count_elements(lst):
+        return len(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
