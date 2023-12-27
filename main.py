77b76a1dc2aafaf284a49c5eba74e0d6934c05a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:19:52 +0000

    stand smoke dance shock wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
