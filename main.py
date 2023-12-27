c3c96bf104edfd8b2813c99b21e7c2dcb733e6b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:32:04 +0000

    dig marry calloff foot vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
