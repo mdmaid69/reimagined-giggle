4aa64599af9cafdf3ed162bcaa7a39e57a105107
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:25:59 +0000

    switch opportunity 572enter grade answer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
