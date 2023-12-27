1d435dc2dc038cab764e7dcae32bcd4d5170f1fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:09:07 +0000

    proper to balance write children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+print(math.pi)
