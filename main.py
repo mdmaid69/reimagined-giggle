441429a3368ba948937cb9ff86df4c17c5c238e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:00:16 +0000

    benefit already criticize laugh pronunciation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
