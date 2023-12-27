906baceb2da20bd690ec00e16fd17fa15837d520
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:48:56 +0000

    later themselves price mad blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def multiply_numbers(x, y):
-        return x * y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
