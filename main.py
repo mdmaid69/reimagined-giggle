57b02ee016fc5034fd6da41a4ec5dc8913176ca0
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:13:05 +0000

    about he possibility right zero

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def convert_to_hex(n):
+        return hex(n)
