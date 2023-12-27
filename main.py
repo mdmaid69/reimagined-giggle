fc44a67c348bc20136218f6516266645e63371fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:18:15 +0000

    indeed separate everyone claim very

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
