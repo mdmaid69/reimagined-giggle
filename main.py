aefae8b9a7a9f3679ed21ab8ac75eb25fec82932
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:05:46 +0000

    occur lock mainly welcome ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
