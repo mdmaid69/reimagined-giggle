6515e0968b6fb38e375ef20e63e3e50e6d3fca04
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:54:47 +0000

    equipment tune still callback claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
