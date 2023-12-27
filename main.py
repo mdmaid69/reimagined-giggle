2b5ef20455a7cfe8b08143ed02c4053e08b4592c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:24:29 +0000

    recognize connect under goout recognize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def convert_to_binary(n):
+        return bin(n)
+def reverse_string(s):
+        return s[::-1]
