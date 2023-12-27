10832dcb47f29134bbb365daa4cd512538492555
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:28:09 +0000

    generous bunch available full careless

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_binary(n):
-        return bin(n)
+import sys
+print(sys.version)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
