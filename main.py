21a5c7fdb0c83fa3fca70731b68c8691225a87e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:17:24 +0000

    increase build whistle through public

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
