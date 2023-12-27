041d3990a22b94851ce910d775402e1ca15a014c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:38:34 +0000

    nerve why away fly issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
