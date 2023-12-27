86629b325cf673437a6b8aaf5cd88fe4e7de6d57
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:31:42 +0000

    number account great relax popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_current_working_directory():
+        return os.getcwd()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
