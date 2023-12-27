a53e0d822f62571da2f52a216630fc91acb2038b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:40:28 +0000

    glass care Dd measure wide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
