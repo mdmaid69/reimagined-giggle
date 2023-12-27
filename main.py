e5d5d1a4167ea38fef9a3d2ee029e82b423c838f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:01:07 +0000

    wealth far clothes moon occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import sys
+def exit_program():
+        sys.exit()
