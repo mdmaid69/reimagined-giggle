365c06964b7a8eb08f97aa63d436f8c47d1d1fc3
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:59:58 +0000

    bear important generous proper different

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import os
+def get_current_working_directory():
+        return os.getcwd()
