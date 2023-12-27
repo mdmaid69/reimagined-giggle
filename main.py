5480b5517cdbdac02edfd5e604490f7884b0f9d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:54:43 +0000

    while lookafter complain swing display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
