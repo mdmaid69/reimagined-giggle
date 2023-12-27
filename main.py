caeef9b9ec26b667fd603679758c8c42f90903a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:30:30 +0000

    pollution characteristic coat dinner sensitive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-import collections
-def create_counter():
-        return collections.Counter()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
