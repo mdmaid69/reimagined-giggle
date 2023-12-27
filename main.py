dd6d896c46d9b984cdc5fbf45ddc4ead7027f5aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:57:58 +0000

    yours chance pupil floor programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
