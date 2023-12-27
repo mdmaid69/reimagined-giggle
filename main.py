b189fdfc86fb9021b5c86d547051a5cb129b7563
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:07:10 +0000

    action deep subject spot impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
