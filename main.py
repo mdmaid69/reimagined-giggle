c34f86ed76dd29ed49eecacbe267547010706c80
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:57:22 +0000

    male fairly rank might share

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
