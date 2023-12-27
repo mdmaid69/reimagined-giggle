4a88ba09e617357f82c963b7033a7c9b5bff8f88
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:05:30 +0000

    represent love confidence limit print

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
