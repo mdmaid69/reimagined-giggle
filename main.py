933aa0ce97e65a37056a91a9f21efd4d270e8017
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:47:39 +0000

    rush makeup behaviour hole match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+x = 10
+y = 20
+print("Sum:", x + y)
