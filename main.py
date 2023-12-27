99be4eac2b8992011d6bc18839fd58e5e96792c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:45:50 +0000

    bath pour fold course rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def get_array_item(array, i):
+        return array[i]
