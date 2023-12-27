04edb473d98e8d148ae6697a1d68e09171bc26c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:09:59 +0000

    land pencil song burn alone

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import tensorflow as tf
+print(tf.__version__)
