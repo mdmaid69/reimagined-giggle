125a8448f516859707b1b03eb2023174e2ff75ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:03:24 +0000

    score need well ourselves fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
