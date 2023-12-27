b22a897ee5e7126643c9d6cdd7a621af6c837a63
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:32:13 +0000

    represent letter far comefrom level

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
