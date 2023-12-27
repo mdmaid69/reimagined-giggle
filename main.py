c939737d2da93877096d2978b7a1eb55cac4fcad
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:58:32 +0000

    trip separate listenfor advice bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def count_words(sentence):
+        return len(sentence.split())
+import datetime
+def get_current_date():
+        return datetime.date.today()
