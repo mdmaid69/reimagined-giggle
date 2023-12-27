9ec57cbc8029d0577e4351bf768a8cbc674c8640
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:39:12 +0000

    blow worse brother Pp open

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
