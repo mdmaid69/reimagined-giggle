d84bbb2daae1836257a41452e7e0197a43e98ae6
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:24:34 +0000

    case nowhere reason one perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_acceleration(speed, time):
+        return speed / time
