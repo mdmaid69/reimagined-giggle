d3658c57f2c04c1b437c878c29ba46b7551f6689
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:06:08 +0000

    hate advise no break familiar

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tensorflow as tf
+print(tf.__version__)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
