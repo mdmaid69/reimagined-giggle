cb4b31b1c64815e5230fede4364e5137960a4dbb
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:04:00 +0000

    spite wide create slide sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
