4a344dbe77ee3696691b9f5f80af3b944022c503
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:49:26 +0000

    force black lean promise telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def remove_from_array(array, item):
+        array.remove(item)
