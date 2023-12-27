3464c88f961e514bc0999bf655b54675c216ba2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:06:40 +0000

    frighten go share surface quality

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
