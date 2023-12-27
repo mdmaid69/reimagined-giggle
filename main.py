6fbe1b3fbbb75526b151b8ae2d7eeb2fa87b47d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:04:44 +0000

    start keepup level leave nor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_elements(lst):
-        return len(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def multiply_numbers(x, y):
+        return x * y
