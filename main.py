839d62f7b5e447cb36e29eb0bab782a62989cf92
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:17:15 +0000

    model spread disappointed another comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
