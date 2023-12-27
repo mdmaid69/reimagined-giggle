4dcec2d47fc88df4f799038300101eba33f0eb4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:27:31 +0000

    fall winter live offend state

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def add_numbers(a, b):
-        return a + b
+for i in range(10): print(i)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
