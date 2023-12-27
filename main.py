938ad4690027ebd70754d8ec3f461482ca52e37f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:16:44 +0000

    invite excuse contrast track exercise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def remove_from_array(array, item):
+        array.remove(item)
