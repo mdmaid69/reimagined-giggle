865b916097e5d888ce5bf9400a9cd1b429572e29
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:56:36 +0000

    pour Oo share every draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
