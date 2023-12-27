1b4a907fb385ad00b0c11416ec774f58a81200f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:20:52 +0000

    notice yes view previous floor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
