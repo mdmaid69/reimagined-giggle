cd8ac92cfc00df91f8fb15a86f0a6c9272577896
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:33:10 +0000

    thing total especially if square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
