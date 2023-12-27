b029f0fb7ce7afc78cd0c251e69d8de089647513
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:23:06 +0000

    money allow doctor strength class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
