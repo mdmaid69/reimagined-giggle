915a5ecce3bc30b9494b2cbacd003d4ea46467bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:08:38 +0000

    hardly safe gooff person poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
