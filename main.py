0349cb1001884d190d92b937c370eddd91063886
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:09:16 +0000

    awake rub pot rate fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
