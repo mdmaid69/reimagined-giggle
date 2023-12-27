e0be5b4eb787510038751d05d13e1fca2ad93f46
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:19:22 +0000

    steam past confused wet negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
