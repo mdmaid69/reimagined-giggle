575033a297250bba460c1c9e56977242385eb1a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:18:13 +0000

    injury excuse now pretend risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
