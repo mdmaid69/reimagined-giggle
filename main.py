79205ee058271f9075ecafb0b680e8bdb57ffe51
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:33:15 +0000

    might strike who coat comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
