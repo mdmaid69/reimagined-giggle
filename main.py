da9c4b11075b84db59c676aea1e25a93b967ebd5
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:36:33 +0000

    hardly ready danger into since

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def get_array_item(array, i):
-        return array[i]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def remove_duplicates(lst):
+        return list(set(lst))
