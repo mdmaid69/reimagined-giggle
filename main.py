1f9089d2cfccdca6c9cfc36d95870b97c6fb01a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:00:58 +0000

    name too rarely may spring

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
