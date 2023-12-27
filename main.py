836948977a431d4a0192b781f74c963f61f6a61f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:26:04 +0000

    turnup radio objection object stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
