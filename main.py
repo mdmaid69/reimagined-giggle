7fed0e9ac78a026a24366ad257859244b55aebfa
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:43:05 +0000

    song tidy rate first ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+print(math.pi)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
