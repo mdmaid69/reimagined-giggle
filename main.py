dabf67c87ab9b3cd788a7855b2c794cc809556ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:36:44 +0000

    who prepare squash really wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import collections
-def create_user_string():
-        return collections.UserString()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
