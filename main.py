ed9c386a3c4fb0cdc2360e0f8cd61c1ff6f10c0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:17:15 +0000

    matter criticize finger future neat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
