9cca71212d3264e09ff1a5878b21cc8efcc428f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:23:47 +0000

    neat goup impression object switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_hex(n):
+        return hex(n)
