800dabc2f54254c92f911f2780f774a5a14c5540
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:34:31 +0000

    country tidy themselves forget injury

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-print("Hello, world!")
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
