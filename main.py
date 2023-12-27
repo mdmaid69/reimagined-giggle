0556a81063c45f3eded7aeb520ba5b81cd663371
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:47:50 +0000

    lesson now kind term woman

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import array
+def get_array_as_set(array):
+        return set(array)
