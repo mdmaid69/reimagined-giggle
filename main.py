f58f3b3cc3e9b2255a99b2db20c2dda68d0a6699
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:57:00 +0000

    action concern business vision taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_max(lst):
+        return max(lst)
