c01d62aa51cdda309d735006caa425e948686d12
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:20:30 +0000

    excuse anywayanyhow obtain fashion legal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
