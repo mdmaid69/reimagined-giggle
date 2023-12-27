5b9627a05c77e1fe8107471b913c0adc6dfa80f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:10:52 +0000

    treat pleasant yourself dead content

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
