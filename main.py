a1580adc6f2ac12394d57badb8d3cdd745e939aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:09:19 +0000

    round recent light experiment protect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
