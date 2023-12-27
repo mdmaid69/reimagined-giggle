ff4f3b9d5c5b10603e7904814aac789f11ce259e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:22:44 +0000

    spite let address upwards wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
