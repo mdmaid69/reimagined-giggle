c536ab90ab6716179b9189514f0a242a8a35ff25
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:24:47 +0000

    substance method wealth apart contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
