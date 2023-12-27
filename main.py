1f89fbf8b46577cec089281825e7f17e09332082
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:51:53 +0000

    alive move direction fat hit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
