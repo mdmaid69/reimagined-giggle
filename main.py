5b004e81c727a9288a417e7317de3ea4decbc2d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:27:18 +0000

    bit personal record nurse length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
