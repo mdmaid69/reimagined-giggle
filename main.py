425a131c5894d7d1fc66eb97a1ad0d69a5634f6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:48:31 +0000

    heavy sometimes key smile battery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
