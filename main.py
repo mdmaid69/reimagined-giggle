19b9a93f63cf2b279ccb6ecec4169f27554cc562
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:38:41 +0000

    bottom fix sport climb actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import sys
+  def get_python_version():
+        return sys.version
