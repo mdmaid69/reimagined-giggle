42ca6cbebdf472fdff136fb7e724ae813ec6fa40
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:32:49 +0000

    correction press Hh aware indeed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
