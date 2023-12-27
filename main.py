942c73b90c7cbc2a299012f87fd2881a955ca7cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:30:41 +0000

    shallow answer circumstance reduction Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
