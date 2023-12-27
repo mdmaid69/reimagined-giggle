053a58267a49cd2f722cfa239328da3f695a535f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:36:49 +0000

    married bill birth lot instruction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
