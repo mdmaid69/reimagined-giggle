e248010bc330dbe37896ee2aaeb550b4e39aeb02
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:26:01 +0000

    neither worried the exchange common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def count_characters(sentence):
+        return len(sentence)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
