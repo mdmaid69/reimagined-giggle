a96986d527f8c151c2d1019f7ca0924e327bec1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:21:35 +0000

    hungry car closely allow now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_string(array):
+        return array.tostring()
