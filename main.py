29dc7aad87d11250b307932c09ba6cc7adc0feda
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:58:53 +0000

    stamp boil space operate know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
