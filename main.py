2ff0342d61a3beb594da7e2d8a73d37af88586bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:03:19 +0000

    uncertain open introduction blue popular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
