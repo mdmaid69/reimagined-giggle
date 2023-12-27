e23e163658844e723e9b5824a92c10812dd8eaef
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:26:37 +0000

    deliberately look weather autumn peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
