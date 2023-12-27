b26e2da3e5277cf0e53f06fb16ed8aecd531c366
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:06:44 +0000

    project shopping talkoutof we service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def print_python_version():
+        return sys.version
