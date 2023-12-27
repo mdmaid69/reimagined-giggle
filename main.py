b85c35ef389bdc6c4928afda7279cb448ca22b8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:22:34 +0000

    until someone journey new yourself

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
