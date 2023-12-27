cee3b042edbdcb50a5ab34fffc7e766cebb4ff73
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:31:48 +0000

    smoke brown birthday Hh office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
