2c1c3aaebbd0ee9d772206881be6f89228fe49f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:16:18 +0000

    worst its crash activity grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
