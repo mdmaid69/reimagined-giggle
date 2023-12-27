11454263931bfc1a21718e3ddb54aa623987d517
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:07:15 +0000

    peaceful takeaway shut key rest

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def find_min(numbers):
+        return min(numbers)
