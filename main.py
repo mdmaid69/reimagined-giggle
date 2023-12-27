d31eb97fd5aa2c806b718df39a2dfa371c4024a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:09:22 +0000

    nice instrument cheap permission couple

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
