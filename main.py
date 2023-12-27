c2dc2bb7d63db49697ae760d032ebb272942d643
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:16:54 +0000

    getoff put trap pause breakout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def subtract_numbers(x, y):
-        return x - y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
