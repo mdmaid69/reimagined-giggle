e8002a1c5eed286f98d097130be9f36d5b276321
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:50:28 +0000

    performance put juice perhaps Pp

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
