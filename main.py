794b5d98ac9a4021b22e150b168cafccebc9fc62
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:03:02 +0000

    straight or hand left range

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
