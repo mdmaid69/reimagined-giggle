d664548972972f8f3cab2684bfbf36540784957a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:14:04 +0000

    container should lookup pull sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
