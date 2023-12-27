b579bd436478fc62b0ac6d90d6414aa900132347
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:18:32 +0000

    muscle effort wind swing serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
