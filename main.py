4d68d72611ff5d5a39aeb8b2ff0f2f26d20ca255
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:01:40 +0000

    giveback absolutely nearly related so

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import random
-print(random.randint(0, 100))
+for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
