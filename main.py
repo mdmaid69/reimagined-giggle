9ee0a0d0bc4105cf151b273ab3fcd0fa801b3b99
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:36:09 +0000

    money introduce each favour violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
