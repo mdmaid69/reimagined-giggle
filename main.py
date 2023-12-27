d9f0114426676e23fa6f732dc13494d560f3417a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:18:56 +0000

    move persuade activity weigh colour

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
