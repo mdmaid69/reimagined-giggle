f14b21b36d949c8a91c42a47705724864e418d4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:25:29 +0000

    turnover difficult mood shop one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
