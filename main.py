895513af1b4f2c021cf46f8626eff076f40f5d45
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:11:10 +0000

    switch funny absolutely transport silent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def clear_array(array):
+        array *= 0
