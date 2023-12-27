e4daf47e3b22b97ee5336d3c0090edd2d89db501
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:48:13 +0000

    sport callback arrange capital prepare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import sys
-print(sys.version)
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
