5bc2aca7be26264c5af703dba8aa8f5a03b7e729
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:51:47 +0000

    bet create season supply large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def split_path(path):
+        return os.path.split(path)
