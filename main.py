f02e6dffee5acb1bd7f96b91957baab58ee1028e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:12:22 +0000

    now weekend sort tip prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
