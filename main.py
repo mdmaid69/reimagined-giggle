cdc3ebde4fea660b96a7a934c0a4cd1b7d08ab57
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:26:21 +0000

    criticize urgent release aware table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def convert_to_binary(n):
-        return bin(n)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
