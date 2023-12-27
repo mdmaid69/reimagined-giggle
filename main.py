2e888d21b1dcdf0b3314369c20ad1b9170cfed33
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:55:25 +0000

    exchange brother spare away gap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
