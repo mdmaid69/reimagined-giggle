b233586c3bffddf7fe51673bc020bac153757a33
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:04:37 +0000

    peaceful report properly independent brain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print("Hello, world!")
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
