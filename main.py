1d0b67c1c1f14c989d5bc3a76c47234014645c58
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:47:49 +0000

    sting independent noise crash train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import datetime
+print(datetime.datetime.now())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
