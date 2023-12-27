f0eaa94d4bea701852247bbb171a7aa271126ef6
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:28:57 +0000

    earn 570enjoyable draw comeoff sail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
