3928018bae88774c6152d5a908e77dd045abc4bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:49:50 +0000

    workon comeback sit meeting immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
