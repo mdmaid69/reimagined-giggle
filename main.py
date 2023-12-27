cd02075b406c0dc04aae9281b80d5ff69016d2ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:54:57 +0000

    thank space pump minority whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
