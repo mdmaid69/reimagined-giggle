5a1d006b52a625b4d099007780f56dbee09b53fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:26:41 +0000

    unusual war shop turndown face

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def add_numbers(x, y):
+        return x + y
