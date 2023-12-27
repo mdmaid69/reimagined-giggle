34d6f1074d40ce596262cce401c65fddcbbb0c4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:53:14 +0000

    steady pinch gas confident respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
