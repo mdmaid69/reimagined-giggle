279240d97c158b220ef117ef579e3b11d321ae60
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:30:01 +0000

    familiar silent over means introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_rectangle(l, w):
+        return l * w
