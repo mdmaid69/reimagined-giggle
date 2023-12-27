a53f5eb1977ede1d73066ab80ad929b1d549e00e
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:11:53 +0000

    share apologizeapologise gap feel warning

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def count_elements(lst):
+        return len(lst)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
