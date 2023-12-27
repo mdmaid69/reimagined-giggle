360b18f2a60d5fc6100d7e25a419ad5e1ea47bfb
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:44:37 +0000

    square medium mark honest describe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
