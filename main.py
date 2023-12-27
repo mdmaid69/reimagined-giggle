e44c30a58047f7ac6ef03633b31cbe611216e458
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:16:36 +0000

    arrange through inquire case ashamed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
