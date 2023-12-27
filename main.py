7de6e452552ad29e62ec12562baf1c436a84fe8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:04:58 +0000

    key front moment criticism more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
