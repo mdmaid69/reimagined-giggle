ae33e9af882bcae0a464ef74f9bb55fea05fb1b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:02:27 +0000

    bottom fire gradually doubt criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
