35536dd27a0293fa7810d25a89c83b3f967e8fa7
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:28:52 +0000

    choose simple today sweep reply

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
