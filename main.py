de78404afbaa7473a0d0b5ea934aca2f562b432b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:16:44 +0000

    spray picture combine we welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
