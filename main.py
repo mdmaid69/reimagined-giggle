12c713260049541b5728891c2e00bf2ccad6d7c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:00:59 +0000

    example last opposite ignore everyone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
