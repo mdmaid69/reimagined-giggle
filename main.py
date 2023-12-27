551fb2e0ad5fececabea8102c063b6c60177134a
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:48:47 +0000

    of education university exhibition Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def is_even(n):
+        return n % 2 == 0
