d6b9134648aa5c1efb977a6262cf8608bde45fdf
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:17:53 +0000

    take generous mind send sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-print(sys.version)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
