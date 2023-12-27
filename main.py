6c3e397787e6f5dc96c26505333e211c0e06f463
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:04:21 +0000

    worry middle grand nigga curious

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
