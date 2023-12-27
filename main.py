9b483d644ace3c40126d27625c78d1feec48427c
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:14:43 +0000

    cry moon face when react

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
