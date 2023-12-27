1dcf7d471be85adfde42811501607e9fb5aa07fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:40:24 +0000

    trick child soon square stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
