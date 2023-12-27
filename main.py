3cff474a800d07a1665ac4f4dd6551a16d50e28b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:39:40 +0000

    thinkover estimate print under shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+print("Union:", set(list1) | set(list2))
