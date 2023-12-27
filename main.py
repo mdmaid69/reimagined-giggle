bf4086597d57d11cc0f61faa37547b8378e41df2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:07:04 +0000

    pour history lot pleasure raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
