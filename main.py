f80c708e32b755d9847919406291a5ff46470e92
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:54:59 +0000

    mention aware familiar pretty gap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
