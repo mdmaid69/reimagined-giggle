e69857e3cb30c46bc93109e1b9ce9448c4dbe733
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:05:09 +0000

    else naturally date Mm follow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def count_elements(lst):
+        return len(lst)
