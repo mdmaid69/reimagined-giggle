4bed7621bbbbd2bd7a8a3f730db05927257fb86f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:52:16 +0000

    open door pronunciation turnout loud

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
