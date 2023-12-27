fee248bca5be155e52a70b838e6007ce2d1cf0f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:07:32 +0000

    getaway left lend Mm blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
