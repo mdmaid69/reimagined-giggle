d7e62ab7621a9636549cf8afcda445880803a145
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:45:41 +0000

    contact environment difference best custom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
