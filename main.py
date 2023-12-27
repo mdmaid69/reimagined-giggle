db8bfb2bfa81457e5955da963aa9202ea44a9f78
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:10:35 +0000

    violence earn breakin reply build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
