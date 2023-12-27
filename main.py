9eecaa7346fc45626181d87c1a1ae8ab293397fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:08:10 +0000

    yes try design reaction joke

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
