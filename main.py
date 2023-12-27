250a4b77c00bd2fe610de56103f67bfa15c8619d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:56:45 +0000

    spray strong regional brush tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
