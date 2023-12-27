059d3138a1b38390709f34c28e391fc2eb055f91
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:19:33 +0000

    buy clean hurry stare replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
