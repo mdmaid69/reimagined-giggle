352635bce3276fcaf0a92691ac316c7173d8d964
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:59:07 +0000

    tread opponent comfortable which care

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
