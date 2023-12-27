0b2eaa622d614a4420a27659c72f32db56fd238a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:27:03 +0000

    comeup large cough guess advantage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_as_set(array):
+        return set(array)
