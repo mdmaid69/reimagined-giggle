8b99a9e5dd0cf1a4cb1999af45dd1f62d721d927
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:59:12 +0000

    false naturally reserve greeting machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
