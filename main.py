fa17416d65ba78cd1db1e9a1fa957cd47fe78ccc
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:59:53 +0000

    cool knowledge abroad trade sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
