10861850aff6a669f977dbc72f9007d6e9868d6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:43:55 +0000

    join major coffee pretty dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  def convert_to_binary(n):
+        return bin(n)
