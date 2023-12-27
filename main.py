c648b6ab03b3367c092cf751b5e8bf234a46e6e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:30:51 +0000

    juice control interfere birth rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def sort_numbers(numbers):
+        return sorted(numbers)
