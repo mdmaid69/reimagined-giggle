2b6dfceb50c0770fc71811c65837fdd867189424
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:22:49 +0000

    after slice admit closed wet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
