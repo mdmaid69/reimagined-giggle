275bd46e647809c45f6217b50123adb8c60588eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:02:51 +0000

    below equal disappear morning by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
