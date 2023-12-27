265fe6322b489f53bc37dfb8af78431370f57f05
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:31:41 +0000

    sea butter education gradually taste

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
