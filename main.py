7a1aec892c8b92635b4847d167da3507d5ca4f34
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:26:11 +0000

    matter who car butter waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
