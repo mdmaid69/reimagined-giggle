6f63b0318071a5c125dd10c6a059d02ee7ab5158
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:04:57 +0000

    pretty hand weekend doubt wherever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
