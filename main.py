50f81a17f667863465acacb705b2af55ed4c4cb0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:41:10 +0000

    takeback nor help stamp delight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
