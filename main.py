f1a35937a26c0c377f6c5daf2603d1d45a167aa3
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:52:57 +0000

    roof very light distance watch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
