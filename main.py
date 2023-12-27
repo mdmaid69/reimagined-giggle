f1f9cd1a45a6dde774b14e2b6991381137652f71
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:04:20 +0000

    solve invite statement strike behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
