9c07b8d758ca815850d5e22dd86e2ad8c6fd756c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:42:35 +0000

    worried healthy steady connected imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
