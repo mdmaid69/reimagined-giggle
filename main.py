a261f906c89cb7bbcf9b57f750ca62fbbadf4575
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:38:56 +0000

    smile conversation grand suck listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
