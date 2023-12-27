7be927907ec2e2928cc6b8b96d4b17dfb159a74c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:05:40 +0000

    cinema declare school burn adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
