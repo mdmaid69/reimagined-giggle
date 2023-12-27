0bdceb753d4881532fbe080a683f508d4fc1d8c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:00:51 +0000

    hurry name catch though test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
