b7f1589f9d5f47a95d2d87177957e83bc12b7969
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:56:48 +0000

    room really exact mark closely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
