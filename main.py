c7e3d1ce4d3cddcff8a0862d3dc70cbee8844af7
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:14:29 +0000

    floor hospital change nurse pity

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_list(array):
+        return list(array)
