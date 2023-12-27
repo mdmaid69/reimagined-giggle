4d03d1430c4176c901b5736fa06fa815892c4015
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:03:03 +0000

    satisfied law match rely equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
