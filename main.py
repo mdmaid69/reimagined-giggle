4193ab0aa23dafd0cea3563408f8432d919a1a45
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:46:19 +0000

    below previous owe relative so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def get_time_since_epoch():
+        return time.time()
