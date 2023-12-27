643253b34ff67ee81908abf08039bf658afe1d9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:23:25 +0000

    bill unusual across hurry town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
