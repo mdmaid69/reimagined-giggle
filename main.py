37cc53cffa95e093a906b205387ce1a90ad2ee2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:58:54 +0000

    everything explain impossible nature normal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
