15083f11cd61356258c628cff58cedc68bcae1bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:36:31 +0000

    effort now difference match anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
