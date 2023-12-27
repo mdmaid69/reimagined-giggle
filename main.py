4d46688a948806aff56563603588a728ad015013
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:52:02 +0000

    swimming bang trip underneath getout

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
