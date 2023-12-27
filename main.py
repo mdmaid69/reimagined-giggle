a473661890dd15ef795b99af708d0fead1bb2f16
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:27:00 +0000

    label negative thinkof bath weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
