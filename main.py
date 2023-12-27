1d58e0ec0c1d4f5afa03f7b860184aacb1a96113
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:48:58 +0000

    post definite chief arm warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
