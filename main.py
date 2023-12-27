74184ed4329e58ed89f4cf1791cba17268adff87
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:51:37 +0000

    age hardly mood protest interested

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def square_number(x):
+        return x**2
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
