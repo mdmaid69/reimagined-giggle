9ce99949985c3978d548c554ad6589c09c10b2ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:58:57 +0000

    low keep goout look variety

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
