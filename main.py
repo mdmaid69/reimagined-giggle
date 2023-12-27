cdb286a89271fd76f76211a9aca0128b3ba40a4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:05:25 +0000

    keen shadow unusual progress lend

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+text = "Hello, world!"
+print("Characters:", len(text))
+def sort_list(lst):
+        return sorted(lst)
