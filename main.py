076f32fdc544943f623ed4f90e86e8bfef684128
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:30:36 +0000

    forgive grow talkinto send so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
