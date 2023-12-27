1f07ffa606fdae6140d56a9e1be0f52ff612eb0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:13:15 +0000

    key thick lock arm intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_file(file_name):
+        os.remove(file_name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
