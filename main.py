9c9502c4d11b4948b307636c045e1f11d85d8d91
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:53:34 +0000

    bother dark force suddenly estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import collections
+def create_priority_queue():
+        return collections.deque()
