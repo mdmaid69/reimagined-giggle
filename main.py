45d6372f9663a296bcb7c4cd1132e06642bad2b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:49:30 +0000

    test ability hope legal somebody

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_work(force, distance):
-        return force * distance
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
