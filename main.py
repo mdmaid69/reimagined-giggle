2f1d1190fd8850ee2a8c114bc1f53946a2f4ee78
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:09:36 +0000

    crash worth fill eye precise

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
