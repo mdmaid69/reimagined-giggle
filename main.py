e9a65c52da2acbe2008d170945cdb39ecc884330
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:22:59 +0000

    murder except direction walk muscle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
