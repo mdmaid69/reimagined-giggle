e96e860de37ad12c02a224ddf5244ed41617eeab
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:35:53 +0000

    point still fond blind damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
