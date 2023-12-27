10a0ce1650f43be3ab574b279d3e345074e5112f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:17:41 +0000

    terrible anxious after of future

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_density(mass, volume):
+        return mass / volume
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
