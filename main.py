8f2b79a8fbf8be6835d8e1cf658c4766fd0599a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:25:20 +0000

    giveout task imagine guilty sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
