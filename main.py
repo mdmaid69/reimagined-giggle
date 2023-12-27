f1109dfea1f7f26a5bcccbc5fef69f574dbf9a51
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:30:38 +0000

    creature thin forgive hurry surface

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def divide_numbers(x, y):
+        return x / y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
