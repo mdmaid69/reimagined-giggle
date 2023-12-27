fa6cb6b3761d516eab6f551d9ee50b5aacf9dd9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:41:53 +0000

    hardly whistle careful accept brother

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
