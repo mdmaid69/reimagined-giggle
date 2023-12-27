d0e8232a63b5e759b38125dae312a32498b1ab15
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:03:19 +0000

    wish concerning stop this fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
