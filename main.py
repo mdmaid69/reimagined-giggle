6e5894f8744cfe73960618edd9cead37c7386f4d
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:10:42 +0000

    find relax typical extra goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import sys
-def exit_program():
-        sys.exit()
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
