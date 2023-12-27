ee4f85a3f1607deeb1fb47688da045688d2f6a62
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:53:05 +0000

    benefit nigga at habit tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
