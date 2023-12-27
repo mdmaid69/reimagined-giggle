81e4651b950eaa5b6ce001c556d7d9e2cd1c304f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:14:05 +0000

    rule moon glad suppose point

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
