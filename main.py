cef25196e50c274ebfdeb80b4e00dc74d85d39eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:13:39 +0000

    either school charge quality bill

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import time
-def get_current_time():
-        return time.ctime()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
