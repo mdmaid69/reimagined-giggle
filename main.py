cbd70fdffa8bd464504c8fca87d1f2cb932ec261
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:31:04 +0000

    point 572enter combine sail Bb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-for i in range(10): print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
