7615cddba2c61f2832346beebb4cb834b3b9da43
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:32:12 +0000

    nice occasional swimming unfair careful

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
