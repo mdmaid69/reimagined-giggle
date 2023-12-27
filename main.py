7ca5d5b323d424348870f2c2224b507e258a8753
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:38:23 +0000

    wait culture concerned tour 565end

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
