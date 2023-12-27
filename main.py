772e4f9dd8191a0903b8f361cd8995c46100d888
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:32:50 +0000

    person frightened let course estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
