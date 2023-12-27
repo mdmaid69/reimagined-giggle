5eea34f77811837927269a9046191d0e7a2b18fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:03:48 +0000

    aim interfere finish boat Rr

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
