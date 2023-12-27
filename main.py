023f2b12f7cdbb8d14d4ea31fb5080c3e44850cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:58:56 +0000

    marry reply right wake almost

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
