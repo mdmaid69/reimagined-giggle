55d5b09079ed061ce46bf130e866bd94f302999a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:20:00 +0000

    age dress information wire 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_perpetuity(payment, rate):
+        return payment / rate
