808f53d7587cf34805e4413a8025fce8cf22c6f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:20:04 +0000

    equipment necessary entry payment impressed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
