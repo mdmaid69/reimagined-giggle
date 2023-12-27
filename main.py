6e6585d0524df5c838c80434e57e19ccfa199f93
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:07:49 +0000

    separate walk appearance sink takeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
