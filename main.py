b4e6736724edd289577cffa9d658417a41a40e4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:37:57 +0000

    anyone accident peaceful breakin dick

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
