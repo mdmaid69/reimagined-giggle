7b261c63c3853f340859f9843ed46a7698188696
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:13:01 +0000

    talkinto cause wheel position war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
