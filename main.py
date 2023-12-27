3bcd9d149613d928b8fd0b2d076085c8bab91a35
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:52:52 +0000

    sit stay complete offer news

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def is_palindrome(s):
+        return s == s[::-1]
