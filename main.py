2bcc72aa1c4a25d334617bfa2fd97a842910a81b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:59:18 +0000

    sudden pronounce butter pride cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def append_to_array(array, item):
+        array.append(item)
