dc2b1e2113a101a62cc230167e7de93b7c492aea
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:36 +0000

    firm frequent government clearly chance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def remove_duplicates(lst):
+        return list(set(lst))
