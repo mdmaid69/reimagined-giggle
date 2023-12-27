d397dfd62b26f35c3fe883b41ba227456199ff28
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:20:23 +0000

    cheap stretch mean may they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
