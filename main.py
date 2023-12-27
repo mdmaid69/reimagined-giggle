93d448c3f753aa451a5cb449bc661ca7807f4c95
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:27:47 +0000

    list road fire head occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
