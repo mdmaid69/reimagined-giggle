d31c3f2ba8df7f231ae462f147b9fa65c79f90cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:01:27 +0000

    exit charge neither pencil factory

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
