7fb73b0ebfa467c24b7df305d44a52f6d8845a9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:33:18 +0000

    fasten politics back around narrow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_floor(x):
+        return math.floor(x)
