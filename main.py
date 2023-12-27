a6a7de10c480f4e045beae644ac6b8477299c5dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:34:49 +0000

    sound naturally sure full concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
