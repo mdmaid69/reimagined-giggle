6805e45e8614d678763afd1a685186ba202a9899
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:07:34 +0000

    instruction impatient woman back goup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
