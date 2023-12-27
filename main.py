dd7adf5ff55084c34d842f49e7d55c3c7924386c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:31:30 +0000

    holdup condition Ll beside 179bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def divide_numbers(x, y):
-        return x / y
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
