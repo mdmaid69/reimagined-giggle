ec767bcb12ea7c938959bfcd6e3ed1c4d490337c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:00:14 +0000

    or personal however ours arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
