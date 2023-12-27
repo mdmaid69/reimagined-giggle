4e86c2fca798121d5edd89c583b2660502017ff1
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:10:11 +0000

    extremely thought tight takeaway doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
