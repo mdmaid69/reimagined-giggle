dca0c19459dcc8d7f89a8c676b66b4bdb4f38899
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:24:46 +0000

    outside perfect steer transport workupto

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
