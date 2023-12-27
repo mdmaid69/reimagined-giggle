e1b8717ae200ecbf65f2f2aee3dfaf76d37dd893
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:41:17 +0000

    spray Nn 569enjoy square free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
