cfeff88b378fb4c9e58bb53b19bd4013dfa4206a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:55:22 +0000

    treat mad minor handle often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
