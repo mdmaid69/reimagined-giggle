3e36c463ce8b4c01118f78b982548d0bc3a340ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:52:05 +0000

    first home drive muscle trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-def subtract_numbers(x, y):
-        return x - y
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
