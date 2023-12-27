25f07cba0f60a205b46b4d61d5d0e05fc204d488
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:32:31 +0000

    tense precise culture tear pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
