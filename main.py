352ee671efc8cf4bb56cfe5a89448d39b79d56e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:35:52 +0000

    bother failure forever walk ring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_index(array, item):
+        return array.index(item)
+  def remove_duplicates(lst):
+        return list(set(lst))
