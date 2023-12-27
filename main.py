b3545b4c0a976ed5b321eb400d97235a41ec34d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:29:54 +0000

    wild season job himself mouth

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
