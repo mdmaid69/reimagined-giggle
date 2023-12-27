28a4bcd6774822e4c013a0a40a7ad24b3b88d88c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:10:42 +0000

    load communicate stretch quarter drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
