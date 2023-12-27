6dfda1a7c4b5bbcc83114246c241ffeb302da476
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:33:11 +0000

    communication example whichever pour centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
