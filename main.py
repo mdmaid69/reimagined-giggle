54c67a498509171d90718168b66b872e43e79cab
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:16:48 +0000

    priority sometimes relief road comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
