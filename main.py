98cdc73644a9d0f0092a3b72dca0f4295fa55a47
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:07:24 +0000

    result demand fall guard consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
