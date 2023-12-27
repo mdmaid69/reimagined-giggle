d59ff04b356ba3598194eca57cbdaf907e837b9e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:30:13 +0000

    score advanced start feature limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
