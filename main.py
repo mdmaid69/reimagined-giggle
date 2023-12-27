1102d1b874f4d99f13f42bdfefa6f9c0aa71ed39
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:13:14 +0000

    exhibition point will light lay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
