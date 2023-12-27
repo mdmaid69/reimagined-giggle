4162c9de1b25c34ff1c372888b234c5b4d1e95be
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:52:43 +0000

    another general home something 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
