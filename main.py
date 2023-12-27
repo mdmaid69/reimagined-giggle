6a39499dda851e02648c5ddaf885590fd47477e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:51:11 +0000

    minor contact smooth tradition solve

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def find_min(lst):
+        return min(lst)
