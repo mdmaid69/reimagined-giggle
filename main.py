20954d794fc840d7601105cb083c5e07d19692b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:15:06 +0000

    hold hollow only perform used

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
