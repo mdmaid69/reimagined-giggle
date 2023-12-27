228d1b2d60a011ee4f2e2fab38807094fca8e097
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:16:36 +0000

    feel surprising wire offer strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
