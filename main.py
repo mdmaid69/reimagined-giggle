265fdfe60b53a004079de69d1431a06c5d2fe65f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:16:26 +0000

    shock relief interested skirt profit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
