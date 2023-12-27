d741bbef37bc6fa5624623f2f91bfbbfc5c98103
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:02:35 +0000

    sell possible show upstairs sad

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
