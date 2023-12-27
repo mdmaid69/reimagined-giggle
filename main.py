4986505bc40c9fec3412a42584f470aa608119ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:39:50 +0000

    exchange handle building really yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
