6d4e01948d6352d462b0eb4f3a8e14f9cbc09075
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:17:31 +0000

    criticize cost favour send tall

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
+def calculate_power(work, time):
+        return work / time
