89011e531f06f03cf6b64fd550f250a35e1ea728
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:54:01 +0000

    side knowledge wait sail consult

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
