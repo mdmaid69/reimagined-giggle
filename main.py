260de0d59a63568bedd4a2cbba1e849607cc4f3d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:13:30 +0000

    fierce mind wander dish frighten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def multiply_numbers(x, y):
-        return x * y
+def get_array_length(array):
+        return len(array)
