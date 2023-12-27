b11dbf1db4c73f82a1094737fdb2312e9a046af4
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:22:31 +0000

    stiff confused maybe he reaction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def remove_duplicates(lst):
+        return list(set(lst))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
