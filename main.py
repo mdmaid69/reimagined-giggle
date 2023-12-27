732e37bfda496985bc05e988af736c6519affc56
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:46:34 +0000

    stick young foot natural declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
