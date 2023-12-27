cb2af234c2c7b382a8d36faad462679caea23362
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:54:28 +0000

    allow reject bother service refer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
