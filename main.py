de4720737321312327ecd033e7c319991fd11d58
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:31:57 +0000

    difficulty stone destroy crazy attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
