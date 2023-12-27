3cdc41cac79c4acb5aab959edd58a25d3e88c831
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:56:13 +0000

    trap cinema stamp damage rid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
