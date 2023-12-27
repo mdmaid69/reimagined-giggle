6abf9a3e4e1e0d63a40780d9c4aea0d449fab4be
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:55:38 +0000

    squash season waste safety keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
