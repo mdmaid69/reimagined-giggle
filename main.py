7303423f901ac8590e27fd10eb9037019558c146
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:37:00 +0000

    pick dress evening Oo smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
