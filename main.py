ba816bccf1b4b8a7e3e8f8b8fbf2bceb8f0a9f1c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:41:10 +0000

    drive knowledge unpleasant example until

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
