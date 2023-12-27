35336959e578ad70fc03823d794699400a7fb8f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:02:36 +0000

    put properly face course lunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import collections
+def create_counter():
+        return collections.Counter()
