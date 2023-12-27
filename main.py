c5c8dfc786a317731630e7a91afcd6d042121691
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:10:13 +0000

    think arrangement Tt principle achievement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
