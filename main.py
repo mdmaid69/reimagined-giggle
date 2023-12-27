924a023daf6af680fd748a90482b743c30a57791
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:27:11 +0000

    high find appeal rest forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
