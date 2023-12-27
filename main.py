a5176211b12f9fd84f7726aeba14d5748a055ad6
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:30:46 +0000

    middle relation habit herself politics

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
