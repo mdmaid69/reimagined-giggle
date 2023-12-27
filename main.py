9f513e3fe2d877e613eaccb5a1a6bfab6326237f
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:24:00 +0000

    autumn belong laugh arrange star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import platform
-def get_os_info():
-        return platform.uname()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
