e2ded24391571d1bfe70ff3494c8757059e837ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:56:47 +0000

    light advanced force curious deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
