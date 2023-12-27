256298109ff6dde4a31bbfcf6afffb369f554bf3
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:48:30 +0000

    ask coat production power rest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import sys
+  def get_python_version():
+        return sys.version
+import time
+def get_current_time():
+        return time.ctime()
