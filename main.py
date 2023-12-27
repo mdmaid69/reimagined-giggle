dc4ed4cd269fdf2d5b5cb182e5e2a1907f989be2
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:18:44 +0000

    education neither meanwhile moon rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
