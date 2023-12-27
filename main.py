7b6b5503d445e6ecb2011aa33ac87a7e10999924
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:02:55 +0000

    accidentally within pretend play temperature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
