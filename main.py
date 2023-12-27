4dbec97c18035503d5279822dce3d5b137ce64ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:39:38 +0000

    middle murder below either everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
