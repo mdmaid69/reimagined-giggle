9fb74781b709ba89858f547876ef283f8afcac30
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:00:11 +0000

    Mm classroom who lost part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
