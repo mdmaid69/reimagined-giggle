eea8d76963e089ff0fa323f3903cf8635d251b91
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:29:55 +0000

    string occasion region intend generous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
