310582b661dfc5529d19723ce406559571ef1d1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:04:58 +0000

    part own strange less favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def find_min(numbers):
+        return min(numbers)
