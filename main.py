9dbbbcae29835c53421f0c2ea0673822a661472f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:16:15 +0000

    note solve fast getaway turnout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import random
+print(random.randint(0, 100))
