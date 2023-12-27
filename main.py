bb0840195385184a88afb3c0cca0e12b7bf9bef4
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:32:46 +0000

    nerve develop goout second post

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
