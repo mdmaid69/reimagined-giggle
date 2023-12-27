5ab4923ff131ab37628eb28c244df406ee3c6557
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:55:09 +0000

    suppose comeoff phone address condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
