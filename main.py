70e4a08ae3e273496f56554550c7f3ed4c81592c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:30:03 +0000

    cruelty evidence exchange proper connect

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import platform
+def get_python_version():
+        return platform.python_version()
