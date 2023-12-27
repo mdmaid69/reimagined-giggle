9c44d202fc8e5f9b6f7b4d7398ecaf3d04ec30ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:05:47 +0000

    exactly law push that pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
