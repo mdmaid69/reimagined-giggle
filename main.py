36a3ddee180c85b273b45bb27c020639f76abbf5
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:18:02 +0000

    tooth fit trip neither bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
