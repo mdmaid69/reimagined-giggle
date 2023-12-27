f4b167729bf149c298a47e4450062a77f2806969
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:44:44 +0000

    repeat scrap stage supply callout

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
