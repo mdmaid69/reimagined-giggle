e7889c8e44893fde77d603f5ca9135815b6aabb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:59:27 +0000

    false correction left 179bite that

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def find_min(lst):
+        return min(lst)
