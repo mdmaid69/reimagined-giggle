1df9c48a7644f602e941dd5e6cec6742cf27284c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:22:05 +0000

    almost bin callback apart cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
