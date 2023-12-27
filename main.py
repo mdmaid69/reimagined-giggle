2623e9c13ca9f7e381567ae718b8697018edb504
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:57:06 +0000

    find smoke several have limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
