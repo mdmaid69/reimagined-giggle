bff2e0c30ba10683952981141e38d2b0d22390fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:18:13 +0000

    before dark outside wrap healthy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
