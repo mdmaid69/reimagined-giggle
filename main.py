67fe0828405116779380c82dfb70b99f6a931022
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:28:09 +0000

    admit guess instruction boil complaint

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
