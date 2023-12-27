5274e77b7982e1c2622491229a4ad15f60b6797e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:54:08 +0000

    mixture owe pity theirs cinema

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print("Hello, world!")
