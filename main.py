54208e8f88587bed0ac3ce5048f9150fd13a044b
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:36:51 +0000

    urgent light embarrassed cause today

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import time
+def get_time_since_epoch():
+        return time.time()
