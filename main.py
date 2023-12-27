db64850c959a08253ac390313f2cf8a6a06c33c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:47:04 +0000

    path goback(to) stretch here government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_queue():
+        return collections.deque()
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def set_array_item(array, i, item):
+        array[i] = item
