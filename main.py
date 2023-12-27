92a575c091d00baaf27985a1e3636581cbd9852e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:20:01 +0000

    convincing opportunity production feature unlucky

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_memoryview(array):
+        return memoryview(array)
