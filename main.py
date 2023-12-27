8a70faa85f8d3d8d4484dee1d6a5b985a9254768
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:58:40 +0000

    doctor floor we face entry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
