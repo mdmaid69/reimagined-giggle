455ae074a966441c30f15be0914463cfc5deef22
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:36:03 +0000

    warn replace trust person date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
