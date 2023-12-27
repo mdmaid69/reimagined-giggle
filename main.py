9c6fb86b220f6e13b29bfe8ffb0e11b5104191c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:39:42 +0000

    hole clever whichever fortunately spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_typecode(array):
+        return array.typecode
