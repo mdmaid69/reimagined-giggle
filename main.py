79d6e06fa18c234ad3462ed2d004588c2ead6c12
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:26:15 +0000

    grow criticism left present call

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  def cube_number(x):
-        return x**3
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
