10536940d98ecca1185160a427ac23efec456454
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:58:02 +0000

    sign occasion treat influence deliver

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
