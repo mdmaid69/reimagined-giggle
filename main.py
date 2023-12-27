c46d7a9c7c56648b6610dcefb5c856d6a621438d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:25:04 +0000

    cough space welcome across callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
