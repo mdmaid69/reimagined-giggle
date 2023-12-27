84d003626e66925d08fe5f02164e67909b8df2b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:12:21 +0000

    frame other programme forward pump

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
