66aeabc55826d3d0c8c06001fabaa8f3082eebbb
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:12:37 +0000

    city pay after uniform direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
