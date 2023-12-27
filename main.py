69a6e9c999f218a5040d6840fe673045e75e446e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:11:53 +0000

    bye holdout season dress dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
