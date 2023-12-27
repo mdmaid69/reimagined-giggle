99fae2dbe683057ba5dfb2c33c7418f17a369003
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:31:27 +0000

    shrink answer cheek shrink stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
