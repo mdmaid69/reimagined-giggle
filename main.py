6dd05c6d953c980d2762373a8105ecf5ba2c338c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:26:43 +0000

    clever taste difference want medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
