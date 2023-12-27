d256a607b6ffadd09d402a03ae6202cda2ef7669
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:31:46 +0000

    prevent deliberate fair operate various

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
