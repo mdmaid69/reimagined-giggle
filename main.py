e0915e646cb2b11201d58b54a6ac2adaec59ba92
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:32:05 +0000

    size extraordinary breakin impatient always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def reverse_list(lst):
+        return lst[::-1]
