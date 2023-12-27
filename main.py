3943ce49ec1937c26616bd8830127a110fa28f2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:26:19 +0000

    voice weigh talk last respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_as_repr(array):
+        return repr(array)
