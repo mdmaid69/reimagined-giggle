12c6d4aa8b472f35422282dbcec24d9b348f33bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:32:38 +0000

    excellent suggestion complicated uniform space

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
