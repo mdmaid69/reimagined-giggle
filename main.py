b87d84bf8b9d46613d9a1baf4e6070ca8df4f44c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:30:48 +0000

    before instead runaway complaint characteristic

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
