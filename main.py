c49868ac6ed0edca6ab0f88bd24a5aa99c4f4dbe
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:01:17 +0000

    recent concentrate whisper load instead

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def sort_numbers(numbers):
-        return sorted(numbers)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
