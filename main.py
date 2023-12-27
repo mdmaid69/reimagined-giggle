3d71e15dac0fe9fcf9a4f0d2814da8376cbda8ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:08:12 +0000

    fault upset little but plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
