d711b6fbe5bfcc2d58c5085a80fa0784e57442ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:01:09 +0000

    spite pile drive by even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
