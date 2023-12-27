f4533a4db4c4094534aed191d72f74129407fd91
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:19:08 +0000

    generous keepaway afraid beneath criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
