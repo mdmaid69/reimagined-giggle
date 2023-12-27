4b5688c88c23fa2b2c01a1ba3956a352e3d7dfff
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:45:12 +0000

    young environment trick shopping shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def calculate_area_rectangle(l, w):
+        return l * w
