0045188ce70106097e4a169e1bdcb7c08d23869f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:13:33 +0000

    direct main mean plastic still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
