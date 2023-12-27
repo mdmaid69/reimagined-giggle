9e1ac76310f49164c1e6b5829e5c55082177b5c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:38:00 +0000

    under condition block make week

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
