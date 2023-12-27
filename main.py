1be2bc1b9e5cb6c1fdf08214e80bd7eb1610abe2
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:47:09 +0000

    meaning immediately smile phone black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
