79a1dd8cc3353edcd02a15bb59310931dba13551
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:48:46 +0000

    likely also spend let develop

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-from collections import Counter
-print(Counter("hello world"))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_power(work, time):
+        return work / time
