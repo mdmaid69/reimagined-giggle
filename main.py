f2e3e76408bc5d1a6d178f51b36ff5f55aa96748
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:48:38 +0000

    bill design out just wait

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
