d451321c75c836aeb74bfd3c3d4548a0ed9bb5a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:50:45 +0000

    glad record moon again card

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
