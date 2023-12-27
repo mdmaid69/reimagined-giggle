936aec74dc26aaaa0e56ebacdcc79125f1fc0df2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:41:05 +0000

    anywayanyhow some precise quarter blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
