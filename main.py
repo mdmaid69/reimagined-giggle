499702d46fa2e17eaf454728d649fd92e8bcfbac
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:36:42 +0000

    eager suckme tidy noise loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
