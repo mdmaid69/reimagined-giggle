13d8f6ef77530ab383b6404e723abd5c33088e2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:32:29 +0000

    dream cap class blind upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_current_date():
+        return datetime.date.today()
