405b2a70a8527c2406e58a6a567a1d9f16c4be64
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:16:47 +0000

    more pretty party march authority

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
