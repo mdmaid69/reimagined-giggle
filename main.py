8eb1c7efe167ed26e21d91a5282e9a874111f446
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:31:31 +0000

    almost 569enjoy call flat form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
