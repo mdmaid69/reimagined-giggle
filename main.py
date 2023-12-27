09941160b989a5f3400afa03e1ce513f233998ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:55:04 +0000

    thing study vote mine instrument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
