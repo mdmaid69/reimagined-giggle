defa1ae62e07f53386d2fc66e2c6c5b9bd16a8c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:10:38 +0000

    distance sweep for character journey

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
