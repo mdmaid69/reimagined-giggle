7d7038083b9e41e45a7a0bd03f9d270650866488
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:31:18 +0000

    on holdon crash runout always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
