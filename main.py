41a65d815c84ea088a079c3fe3b0f80a900ffe08
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:11:59 +0000

    represent Jj wander bye offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
