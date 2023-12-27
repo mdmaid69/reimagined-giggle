c63db46bc05c247e671a15ed5ad4ab841ca23e24
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:38:43 +0000

    trap only clear mark bin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_queue():
-        return collections.deque()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def sort_list(lst):
+        return sorted(lst)
