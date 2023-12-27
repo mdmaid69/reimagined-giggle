df23c6e22d15dc29dae9b5de02a626c3920ce279
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:09:15 +0000

    grateful care bad cost correct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
