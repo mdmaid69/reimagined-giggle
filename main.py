7b640a242c05af84d5085c3ad20a32c0d2f16080
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:18:22 +0000

    cause left bet chat snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
