dc68558be52cd8578c6ead7557aa4d6a882fb2de
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:17:33 +0000

    float tonight onto objection defend

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
