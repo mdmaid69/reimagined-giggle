4b34bf19374128a1fce47e877c082b1e09eacc49
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:51:44 +0000

    play doctor poor table everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
