2b3f8c395a1a371ddf34117c7c4f15ba349132a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:43:08 +0000

    seat right tidy fork large

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
