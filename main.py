6b229fe21133ee8989576367968e16327bef51ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:39:11 +0000

    start like whom tradition ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
