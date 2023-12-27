fbc2a072cfe3295cff0176fdb5e25b7adf503c72
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:06:33 +0000

    raw scared taste practise solve

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
