a48f507595c5497afc4040f7cf31a906d6e0be08
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:31:38 +0000

    criticism study before confidence subject

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+print([x**2 for x in range(10)])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
