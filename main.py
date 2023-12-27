5dfb03d8d5aa22046ac57052ba67a8ecbf3d78f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:07:33 +0000

    congratulations responsible callat active watch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
