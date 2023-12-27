4e32587e97862cacbeebbc93b2a50575b97440f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:55:25 +0000

    rush involve anybody surprise union

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
