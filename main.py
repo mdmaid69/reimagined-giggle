19fde4185974c975562b9a58c7c2cda156af4c90
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:45:24 +0000

    friend store getout shut make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_list_from_array(array):
+        return array.tolist()
