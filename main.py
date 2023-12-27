96a135086c386f76569781e688a1951573ff535e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:06:45 +0000

    well week turn dry point

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
