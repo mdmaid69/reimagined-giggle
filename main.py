f41d700287775ee7bffe279a46beb981aae59d37
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:56:30 +0000

    mark Ee valuable autumn combination

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
+def count_characters(sentence):
+        return len(sentence)
