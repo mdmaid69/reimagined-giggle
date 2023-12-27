2a1244f1ba868dae37f726808520c5118f51b928
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:00:41 +0000

    through contents free pity loan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import collections
+def create_priority_queue():
+        return collections.deque()
