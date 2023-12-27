3bfee94d2440ba4c23e0a40cd949356b7852ca07
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:18:07 +0000

    need television goodbye himself glad

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
