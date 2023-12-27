0733bdc7aff423f54122e10340d32d3e2e3068a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:41:09 +0000

    probably so history march head

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import random
+def roll_die():
+        return random.randint(1, 6)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
