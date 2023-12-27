86f51cec17fc54c7f47d156e19f57c46dee30ac1
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:05:04 +0000

    face clearly over godown purpose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_average(lst):
+        return sum(lst) / len(lst)
