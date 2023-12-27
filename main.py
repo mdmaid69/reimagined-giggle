3d7738d47e3d6a8b5da412104acaff312d17c3f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:39:52 +0000

    later take runaway service sometimes

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(10): print(i)
