2046ead2d0909d47ae52b37c2eaac7181cf0f1d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:41:10 +0000

    telephone workupto pronounce listenfor rubbish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
