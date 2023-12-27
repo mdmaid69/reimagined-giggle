f837c5a27be10897bf380e12720016fe287caa5c
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:13:44 +0000

    go crash promise match swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
