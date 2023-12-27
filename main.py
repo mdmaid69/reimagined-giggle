ef1b2a564af9bf6ce00d27e17c108a7d59417b6e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:40:14 +0000

    noone before course enthusiastic anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
