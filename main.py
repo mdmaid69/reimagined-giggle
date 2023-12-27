1c6700a24428580fbe27a5fca9411d63df0d4a90
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:19:36 +0000

    deliberate calm joke slow distant

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print(sum(range(10)))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
