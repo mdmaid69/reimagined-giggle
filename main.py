a3f8b019ddcb5ba44ac94a2a50c90bbf3bffccac
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:10:07 +0000

    ear dance mark condition pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
