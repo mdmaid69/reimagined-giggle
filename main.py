b7d881b3338d15ad82f9e85966952fbeb963dd2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:36:55 +0000

    interruption moon stick pair love

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def convert_to_hex(n):
+        return hex(n)
