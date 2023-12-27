031fedecda8482a1754a239229fc3005f6fb6fb0
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:16:35 +0000

    sleep sport fine on film

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
