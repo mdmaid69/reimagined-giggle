b315efd25dfe9becdad1744fde91a8640bac8206
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:42:13 +0000

    team upwards fill Itself shall

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
