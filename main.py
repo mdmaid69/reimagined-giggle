1e28add6934c33e36cfba94e108141d479799993
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:30:24 +0000

    remember grateful drop healthy its

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-def calculate_density(mass, volume):
-        return mass / volume
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
