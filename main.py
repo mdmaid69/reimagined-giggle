6e1b99cbaaf209ef4020554cb02ef650ec11b372
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:20:13 +0000

    passage mystery bunch lose bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_current_time():
-        return time.time()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
