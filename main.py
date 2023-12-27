dc70a63cbb75c5732fece95f89687b8df8adb78a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:23:56 +0000

    wind tune sort outside peace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
