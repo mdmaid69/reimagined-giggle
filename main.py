a1e43e9fd3e22a1a153fb0fe39f9b68fb90a898f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:24:08 +0000

    stir demand through gentle stiff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
