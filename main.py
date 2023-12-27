5ceda9705edb81cdefecf8ffadb9f9d636949e5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:18:23 +0000

    accidental always neat getup hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_base_name(path):
+        return os.path.basename(path)
