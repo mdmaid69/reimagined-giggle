0cf8e6899c823294da687dc7bf6ca0b2e347f0af
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:47:51 +0000

    how tip strength after maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
