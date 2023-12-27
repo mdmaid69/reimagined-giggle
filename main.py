7ff8cf84c54340a6baba619aae1c660a1fb3f16f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:28:53 +0000

    scene level great bother there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
