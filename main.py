b03571887f85916f6bdf2f8b289cfeed83f4b588
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:45:32 +0000

    pure generally threaten whisper scream

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
