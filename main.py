887fd0bc345e4b118b08f286f52bd4b9742b2428
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:51:59 +0000

    creature offend neither right up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
