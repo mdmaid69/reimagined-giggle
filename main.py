24fcf6618f315abc0aa593aca60216b055672be5
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:36:27 +0000

    politics avoid thing pattern club

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
