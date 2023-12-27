a0791b0b8dd1ba5dcf1ff5d2e5c7a67f326e8ce6
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:12:05 +0000

    lie purpose commit miss backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import platform
-def get_python_version():
-        return platform.python_version()
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
