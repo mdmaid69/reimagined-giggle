ec2326bd5359d2025b859d2daf6cf4549c7cddd2
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:41:22 +0000

    image valuable regional short say

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
