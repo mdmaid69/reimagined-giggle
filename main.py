449e656aabe469851f43a43f1a00602f9976f798
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:53:49 +0000

    themselves score between listenfor wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
