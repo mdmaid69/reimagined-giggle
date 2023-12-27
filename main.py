ba1773e4deea0a79f6b7bb00388699353e7aac30
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:28:50 +0000

    during care decision feeling wealth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
