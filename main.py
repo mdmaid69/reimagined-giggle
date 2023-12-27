d42817c5349a3e0adf3cf1ff38cae2212a4aafb0
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:55:54 +0000

    measure during Oo spray flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
