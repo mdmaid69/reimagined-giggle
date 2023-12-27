d1fb2a72243d853c9e592b74efd791ecdaf0ab36
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:03:48 +0000

    unlucky importance action classroom future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
