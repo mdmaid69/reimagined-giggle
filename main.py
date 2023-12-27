60bfbfaf51434c1dc9fc7d2e7072322d4e9de1ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:32:04 +0000

    cool usual speak long such

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
