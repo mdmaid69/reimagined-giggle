cf8c438c2faa5e97ee05be0c19e5069bdec5b6e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:18:28 +0000

    valuable right job rather yours

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def convert_to_hex(n):
+        return hex(n)
