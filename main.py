563075b4054ffdbd2a17df5cf66228ce7669225b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:04:43 +0000

    certainly standard break modern regret

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
