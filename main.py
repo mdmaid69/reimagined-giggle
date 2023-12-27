33d451182f2126c510f97d2cc937545a4ae3122a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:42:46 +0000

    human crowd intelligent form almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
