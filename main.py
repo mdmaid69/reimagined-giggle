f046fab24ef26de43987e4bec63b6737056605d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:40:47 +0000

    uncertain receive near giveout else

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_bytes(array):
+        return bytes(array)
