36d46b7d7f8a0792582b175621a3098ce1ecbcab
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:08:18 +0000

    only bank approve close giveback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
