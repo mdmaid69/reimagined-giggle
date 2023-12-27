532acf3b21c36d9443b4b5a37f28ca8c9aa5af56
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:07:02 +0000

    anybody violent let land shade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_as_repr(array):
-        return repr(array)
+for i in range(10): print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
