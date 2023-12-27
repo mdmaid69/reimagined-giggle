dc51e1fee902c3b4445e8b81c68f3ea2dcb27db6
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:08:37 +0000

    lock earn grade sink across

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def subtract_numbers(x, y):
+        return x - y
