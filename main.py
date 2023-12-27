37a1050ae90aebd2aa6b67e48ad9d412fe5b7d7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:48:29 +0000

    beat full breakout related cheque

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def square_number(x):
+        return x**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
