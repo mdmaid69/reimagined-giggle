7ad16cdef2f7d14527e047574feca28b25fd0c33
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:45:53 +0000

    each occur previous practise over

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
