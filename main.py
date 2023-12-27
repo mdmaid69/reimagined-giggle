d33b22436a95f43e117a4da3ad9b26b87f43263d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:27:07 +0000

    by hate year degree emotion

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def get_bytes_from_array(array):
+        return array.tobytes()
