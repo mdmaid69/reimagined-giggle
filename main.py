47c6f5ac48b0279882f978db101156fda5b16c14
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:24:12 +0000

    learn neither act closely half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
