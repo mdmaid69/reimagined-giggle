2732cf7bed76cc7a68b7bc7244e5fdebb1ab4e73
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:58:50 +0000

    peaceful march ordinary government schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_speed(distance, time):
+        return distance / time
