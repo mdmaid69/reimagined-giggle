d46ad14dc5d22e4198798d8c7752313802b8877f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:08:34 +0000

    mind believe rough slow tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
