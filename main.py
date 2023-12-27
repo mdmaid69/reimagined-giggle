6587db16481c3ad8f7fb767d8cc4eb1a37bc08c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:31:39 +0000

    buy listen study car standard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+text = "Hello, world!"
+print("Characters:", len(text))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
