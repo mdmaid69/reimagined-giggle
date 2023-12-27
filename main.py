96ac70c006f6bb628f89e6243823f69fd49c5d37
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:54:19 +0000

    precise provide name create phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
