4a8b95251c6966bbef13f59e2f6ca83eeda5d48b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:22:25 +0000

    health high throw will cheek

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
