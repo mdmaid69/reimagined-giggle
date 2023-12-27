c158c8463d3c51406097de40e5daa31784cca03e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:48:27 +0000

    kick cost neither test ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
 import sys
-def exit_program():
-        sys.exit()
+def add_to_python_path(path):
+        sys.path.append(path)
