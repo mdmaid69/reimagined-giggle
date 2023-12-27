2f12a0d92e09d792dab30237517918e856ede8a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:12:38 +0000

    can pump sign mix limit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def remove_duplicates(lst):
+        return list(set(lst))
