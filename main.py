933c9d402160cc4d00c558753c8b062e25d55d6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:31:02 +0000

    long straight slowly goon church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_sine(x):
+        return math.sin(x)
