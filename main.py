3c3fcf6cde707a14e764c80c7a7c94eed75168b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:19:43 +0000

    ignore eat opposite head likely

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
