da2204d4da9668559ce532b594cc57795a890ca1
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:37:08 +0000

    in reasonable benefit expect kick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
