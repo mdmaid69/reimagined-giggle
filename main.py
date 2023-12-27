d9e520a9ab9e7d46197d260b873a9c7f8a8ddeec
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:07:03 +0000

    real near coffee any similar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
