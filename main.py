eed4be1a2fece2b8df48ec108b90eb18758b6520
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:45:00 +0000

    excellent avoid fashion either today

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import datetime
+print(datetime.datetime.now())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
