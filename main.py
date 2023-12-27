1dbfa26cda75a7e407a175af36c5c191d054a386
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:22:53 +0000

    spray since escape dead strike

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
