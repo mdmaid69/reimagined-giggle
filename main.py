8493215f5cc37b53ff3534f7e43d1b5ed64143bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:40:52 +0000

    cross dry transparent night frighten

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
