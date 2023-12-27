dba41e317ad0bac29bbb0e8bce8901ac653df7b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:43:08 +0000

    plain flow call loss tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import time
-print(time.time())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
