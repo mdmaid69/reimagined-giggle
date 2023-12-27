062dfdcdf22edeb96f5e7b51288b1ab0f3c1b367
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:11:49 +0000

    pressure car door convenient limit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
