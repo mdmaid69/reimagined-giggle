db5a3b3cbce7f08c69c573f0cd63d6c00dcda767
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:10:20 +0000

    believe go pronunciation tall imagination

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
