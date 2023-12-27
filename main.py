085d01e890537df91a3abba6761c88c2307b76cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:52:38 +0000

    blind again plain shout already

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_speed(distance, time):
-        return distance / time
+  def get_base_name(path):
+        return os.path.basename(path)
