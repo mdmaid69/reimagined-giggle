6eba28d8483bbbd1a0a643e1d4fafe2bc80ee30f
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:36:25 +0000

    feed major congratulations force respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import http.client
 conn = http.client.HTTPSConnection("www.python.org")
 conn.request("GET", "/")
