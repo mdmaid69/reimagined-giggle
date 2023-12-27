af2cfc946138456bfe7a5c37aa7bb9150774c75b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:44:00 +0000

    thinkof regularly track obtain date

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
