516557f80e1131f5fe5807e0007834c1913fd787
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:30:06 +0000

    children movement fail thinkthrough history

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
