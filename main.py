e94da14bcd9d698dc0fdf93e99686cfd797a1c11
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:42:31 +0000

    illness whatever coffee married challenge

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+print(os.getcwd())
