7b994cbc3c75890c6dcbc5670e56d167e7a52551
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:19:13 +0000

    sharp extra relative rescue height

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def find_min(lst):
-        return min(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
