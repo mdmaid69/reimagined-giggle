21ca06d4552cb8e693bf31cd8d3a722d197d7ef5
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:35:06 +0000

    light recent by every tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
