7a4c615a6b4c35ea5e49eacdd204a4aabec6f31e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:22:45 +0000

    cheap petrol someone law trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
