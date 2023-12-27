c08239b48ba61886b6689f745c290f06c1809fc0
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:53:42 +0000

    important put his satisfied impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
