74bbf84df45d1cb7b461cedb656aea23ad2f43a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:43:17 +0000

    perform concerned very star gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
