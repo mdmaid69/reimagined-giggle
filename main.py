da230fb000a9d88834991b721bcbb2d4dcb5e9d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:17:52 +0000

    practise transport private total consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
