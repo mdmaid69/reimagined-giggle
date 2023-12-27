ca8583a5ee41144fe4608db54298bbdc4e388bc7
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:58:12 +0000

    wherever sound broad flavour congratulations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  def convert_to_hex(n):
+        return hex(n)
