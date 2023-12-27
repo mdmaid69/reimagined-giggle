c5bccc926f95af3a043362e709592cb0c464a177
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:47:09 +0000

    force out move acceptable comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import sys
+def exit_program():
+        sys.exit()
+for i in range(5):
+        print(i)
