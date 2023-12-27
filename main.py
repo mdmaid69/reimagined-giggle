9ada39207dfe1d656fd314fb25c36e62276ab90a
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:22:21 +0000

    skin maintain question Zz brain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+print("Hello, world!")
