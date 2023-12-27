1c6ff4e621ce27c0ce634f03dc504df8106df4ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:47:17 +0000

    before accept record clean top

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
