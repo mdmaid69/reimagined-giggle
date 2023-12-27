775ace64a5115caeb4ce0680e18a88ff51d922d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:50:56 +0000

    help Rr crowded wake number

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
