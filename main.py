0a0bc960b43af8f3b0f044978ca8a4569c433f44
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:48:25 +0000

    anybody ring arrive guilty soak

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+x = 10
+y = 20
+print("Sum:", x + y)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
