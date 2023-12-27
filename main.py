454aca2e2faa61af17cde6d4368a7892799aab15
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:06:19 +0000

    after explanation Ii declare wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import re
+print(re.match("h.*o", "hello world"))
