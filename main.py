3c365944a74b2302d084df912fb8362214aef35d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:26:40 +0000

    receive land on burn herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
