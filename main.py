ec8e72c9a830f5336d734d1669d58a37c5493e35
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:49:02 +0000

    listen grade programme turnover face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
