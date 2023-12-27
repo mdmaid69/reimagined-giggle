7bd6735b0ea947a356bf0ae9a6651978949e94be
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:02 +0000

    complicated statement spoil spin satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_working_directory():
+        return os.getcwd()
