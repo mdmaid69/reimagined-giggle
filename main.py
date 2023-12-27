02202c7059fea2936aa4ca8d8af913bc392d084b
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:24:58 +0000

    thumb cry gas lend further

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
