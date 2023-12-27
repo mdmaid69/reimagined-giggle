55d5328c9900cb44cda997b6513fbca44e72ddb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:59:35 +0000

    arrange trick centimetre female contents

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_queue():
-        return collections.deque()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
