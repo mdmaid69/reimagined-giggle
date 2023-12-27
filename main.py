1b1767a642eecd2ba24c1d9d32d04a3edf8f6b42
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:37:13 +0000

    bit speed beginning feed tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
