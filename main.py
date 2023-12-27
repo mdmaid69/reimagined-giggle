4a5d324bb954468e3c2469b0876910e072e1ad8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:46:31 +0000

    night somebody benefit extra across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
