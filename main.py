f43c1722d62f63ecce38c4cfcfdd6b25d98ee270
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:45:30 +0000

    butter deaf physical aim refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
