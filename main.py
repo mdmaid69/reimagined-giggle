7d6138c790bc28714d6b73712f28fd60ce013c96
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:22:11 +0000

    thinkthrough die thing major what

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def find_max(numbers):
+        return max(numbers)
