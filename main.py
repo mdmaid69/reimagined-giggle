e4165076819332360592a708d9e5a86a806121cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:54:42 +0000

    hollow cruel shut my tap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def sort_numbers(numbers):
+        return sorted(numbers)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
