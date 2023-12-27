f2e2ef30c144ee260dfe1993dcd5fb43c44dd2c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:29:14 +0000

    centre suckme content distance shall

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def greet(name):
-        print(f"Hello, {name}!")
+def sort_numbers(numbers):
+        return sorted(numbers)
+import os
+print(os.getcwd())
