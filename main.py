ba6b4eac5540334c9041e512527f5327d772c7e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:01:14 +0000

    expression draw pinch left keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
