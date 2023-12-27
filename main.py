77486d2c1bc34957acccf2c633f25f337dd63572
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:48:02 +0000

    uniform store offer whenever summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
