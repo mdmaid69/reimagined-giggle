df8257f48e63567ecc2ef3fab0ba93af91249376
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:22:27 +0000

    mine tidy wake fire song

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
