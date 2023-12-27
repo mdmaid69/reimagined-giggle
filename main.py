d112b6fb8a7f37f02dfa44a0c2a95b3517faf8cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:54:48 +0000

    beginning familiar ever evidence note

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
