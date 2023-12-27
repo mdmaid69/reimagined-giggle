80aed48db36bac545253230cc45ef816349066a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:47:08 +0000

    equal intelligent plenty quit fear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
