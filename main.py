18676d74c55b899eae1f6bc9f837aaa1ed01a716
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:00:22 +0000

    before general show suit cross

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
