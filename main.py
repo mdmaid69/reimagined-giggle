e1ade20593977444b2e604909e70473533fa019f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:35:49 +0000

    worried almost sister protection actual

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import tensorflow as tf
-print(tf.__version__)
+x = 10
+y = 20
+print("Sum:", x + y)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
