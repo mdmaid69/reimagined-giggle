0ca056af7df5a38fb85a04453b10215683c96bf4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:25:41 +0000

    several visit introduce comeoff company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
