e0a63963a596b74c94e3d39552ae7f04f54a5b28
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:10:11 +0000

    guess part imagination round clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
