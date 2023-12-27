a40609dc27ca808461183140f25cdeca73b4bccd
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:09:40 +0000

    enthusiastic mad attention her second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
