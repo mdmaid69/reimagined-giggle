6fd2d70ad4538ba76f64a8068541e944adbc1130
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:55:26 +0000

    steady return tired please declare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def multiply_numbers(x, y):
+        return x * y
