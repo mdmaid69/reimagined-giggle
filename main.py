c100a2ee1e52635570d8b97164403da00515996a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:07:02 +0000

    tune short visit who Ll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
