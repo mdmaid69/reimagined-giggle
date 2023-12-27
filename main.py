1e0ac2e0ca3b8558a9d6f85252671cdaa1e67857
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:26:04 +0000

    smart do wipe mix children

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-print(time.time())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+from collections import Counter
+print(Counter("hello world"))
