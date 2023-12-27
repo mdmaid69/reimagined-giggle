f771684f12f8c494a1982b2db7e21fc62e3fb3e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:25:02 +0000

    plenty slow fat build clearly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def is_odd(n):
+        return n % 2 != 0
