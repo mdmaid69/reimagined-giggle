72367fc226eb360e13a04dfa3d4642eaac788c48
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:22:28 +0000

    cut level except turninto further

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
