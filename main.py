f0cf902f8833e4136c61f91f17f252fd3f4a4baf
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:42:57 +0000

    steep colour occasion realize target

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
