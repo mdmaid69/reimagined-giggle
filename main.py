1ffb502fa4240d9e176910398f9aee6f110e3c6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:56:18 +0000

    hers corner event war instead

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
