3a8fa17e0882eeb489b8d04f53011d764dfae57e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:04:30 +0000

    muscle claim explain right business

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
