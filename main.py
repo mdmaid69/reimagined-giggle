2b5094ba087b6f9603d667db31a215c9627e68c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:05:47 +0000

    certainly left card calloff proper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
