44d8b073a88c5b6854376cb9dca13b0c3fb02e45
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:03:24 +0000

    coffee will difficulty exactly creature

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(10): print(i)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
