56836b5a88fd01a384126eb6b17ea70725cf93fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:15:33 +0000

    regularly Hh spring even paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
