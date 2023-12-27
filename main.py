dca308be2e8d7ec4a88f39a598b5dd4c084cae15
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:48:07 +0000

    giveback bush further before communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import sys
+def print_python_version():
+        print(sys.version)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
