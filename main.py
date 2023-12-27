7727d9049219b96dc2809aaee3bddfe7fc758f95
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:37:31 +0000

    road dream union film direction

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
