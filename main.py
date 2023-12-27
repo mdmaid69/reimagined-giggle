47fe24a2beba4d8b4f848cfe5244778aa08b5a0f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:52:49 +0000

    sit single mass part doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
