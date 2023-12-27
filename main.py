0bb16e8b21a8a27dc32e5c723b03ddc4b2233fad
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:12:19 +0000

    hate goodbye Jj owe detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_density(mass, volume):
+        return mass / volume
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
