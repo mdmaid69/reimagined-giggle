fcf49ce102e45cba52da53d87c266a3c714ff10d
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:36:15 +0000

    exchange any word nowhere sleep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
