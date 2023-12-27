58e455916d55804dd39b6fd1b3b287aff0e1f236
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:58:38 +0000

    help test send soon tidy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
