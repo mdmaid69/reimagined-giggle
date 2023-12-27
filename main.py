c1e4db257c12fd62129b76b57c9a11b5661832cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:38:42 +0000

    allow formal risk bored fear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def count_elements(lst):
+        return len(lst)
+  def square_number(x):
+        return x**2
