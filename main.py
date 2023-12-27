fd0354e274293b7ac7170803ba72b7dc80b7e50d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:02:23 +0000

    suggestion whom impressed table transport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
