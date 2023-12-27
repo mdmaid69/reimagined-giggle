80f0862c9c159490d330932ca945e894d1c2f1dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:15:56 +0000

    for callup slowly tragedy comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
