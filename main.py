9f3d8cbe1011a6799f5c3061c5e1cc90a4dd1222
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:18:09 +0000

    solve with tip intend all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
