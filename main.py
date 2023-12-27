f8bb12bf305e794a0027eb6369ecd0594722d029
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:39:34 +0000

    willing pour stir state ride

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+name = "Python"
+print("Hello,", name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
