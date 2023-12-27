73e2d606a481aa34c0f7fef2bb522938fd4c477f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:09:33 +0000

    Vv successful opponent hit total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import tensorflow as tf
+print(tf.__version__)
