4b1efa0294af0d7048bea96a4ab6bb7ee18ba319
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:23:22 +0000

    sister medium appear task test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
