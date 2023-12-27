e435e93cbcaca850899e4cb304282cacaf98db0e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:12:00 +0000

    interrupt their release home satisfactory

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+print(json.dumps({"name": "John", "age": 30}))
