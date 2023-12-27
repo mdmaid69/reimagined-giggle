f40838c2da210926f66f1853f34c2a8f2b38532c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:48:50 +0000

    knock petrol exactly once movement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def reverse_string(s):
+        return s[::-1]
