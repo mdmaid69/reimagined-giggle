e255aaf3a367052ee4a4eb584ca162a39ccd5bca
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:37:39 +0000

    damp march quality round respect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
