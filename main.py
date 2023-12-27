628e22fbb96a1cebc7afb81d32992cc652f43d87
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:29:38 +0000

    foot final constantly experiment bored

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+x = 10
+y = 20
+print("Sum:", x + y)
