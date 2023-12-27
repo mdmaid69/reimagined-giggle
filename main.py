02140f56994b192ffacbfab66923b29a88b43674
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:53:27 +0000

    name warm official brush achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
