a48442f4d4275766301440a4a2128250e26b9e4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:33:40 +0000

    pretend professional us limit need

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def reverse_string(s):
+        return s[::-1]
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
