ea9a350b944c55b8b5cd764e1e414df586ba9978
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:33:26 +0000

    advice car speed upwards high

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import sys
-def print_python_version():
-        print(sys.version)
+def add_to_python_path(path):
+        sys.path.append(path)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
