b5bf4ab7d952fa3d2522e8aac4f05b1678c638f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:39:28 +0000

    arrange quantity ignore raw switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def square_number(x):
-        return x**2
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
