0c1ff772871199a74a91ab2df3527cf2a79642ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:35:46 +0000

    pocket suppose problem absolutely responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
