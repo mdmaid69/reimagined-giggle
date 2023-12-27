3b29691a4fc97999288bbc4e38bdacd7d7e7435b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:56:46 +0000

    favour gap like expect except

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def remove_duplicates(lst):
-        return list(set(lst))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import datetime
+print(datetime.datetime.now())
