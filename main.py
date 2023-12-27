fcad46a3446c3aeda6a1744e8fffa6af0ed66542
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:04:35 +0000

    couple mine need shock early

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
