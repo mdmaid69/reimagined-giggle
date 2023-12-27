acee21950f5039991a636526a027acef8bfaa09e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:47:54 +0000

    summer along kick with charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
