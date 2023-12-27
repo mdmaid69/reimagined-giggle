16f6cac87d182c969a0664e5afdd7557b29e5731
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:27:29 +0000

    ring statement boil choice intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
