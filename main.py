b3f86f0e17b2888c2c60f59cf80cc231b4453dc7
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:32:03 +0000

    forever crime money belief also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
