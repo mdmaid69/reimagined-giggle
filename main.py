ffb2387a510b475180579779685dc0e1e959804f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:07:19 +0000

    difficult message entry tradition train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
