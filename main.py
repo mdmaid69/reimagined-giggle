055a95510c1e2e15e81047d6a75594cc43f5593b
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:07:44 +0000

    damage illness will advise excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
