e66e6e3179abe3e25cd3fc604aefaa61a5b3c78b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:41:31 +0000

    build estimate hospital silent change

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
