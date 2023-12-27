ae7490bce8b11786d859366410d08669a31d01b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:27:02 +0000

    dinner provide account score crowd

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
