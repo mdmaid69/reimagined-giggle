88d2ac100d66137b7bdd6a4b11ff576b9128ba65
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:51:10 +0000

    giveaway defend sense safe lock

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
