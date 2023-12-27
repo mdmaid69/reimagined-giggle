0e77f294004f456772486aa83e0c8acdcf82a164
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:13:16 +0000

    minimum anywhere accident face fairly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
