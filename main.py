1be313a60c957ff6568eddacfc44e587630a02fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:04:56 +0000

    terrible other surprised port roll

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_sign(x):
+        return math.copysign(1, x)
