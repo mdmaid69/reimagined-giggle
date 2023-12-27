08f09e96886dc61a2d8630fa47fadff1070f4e12
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:07:23 +0000

    shade hot perfect motion hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def remove_duplicates(lst):
+        return list(set(lst))
