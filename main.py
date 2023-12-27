34264747a14b0451ea117df38e163b941fa9a60c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:36:52 +0000

    face deserve come shout rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
