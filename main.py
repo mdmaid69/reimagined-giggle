296b34207d8bf01bafc6f35a80e8d4ba227b540f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:46:10 +0000

    question pupil now dish thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
