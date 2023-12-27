5492b4a75996e58b758aa29efd819255f48bc544
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:46:08 +0000

    mess succeed mess approval follow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
