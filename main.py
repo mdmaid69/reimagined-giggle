d343235a9e8903fa49167f1ad6f49a6d582f0889
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:31:12 +0000

    basis neat dry by factory

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  def square_number(x):
-        return x**2
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
