1020f7fd2c17f983d90e951ec7d8ae27305a1597
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:40:56 +0000

    share slip appeal getout blame

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        print(sys.version)
