9d0b9c27c02ccc164a293cfa984e0f1276108dde
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:37:44 +0000

    steep pleasant kind health service

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
