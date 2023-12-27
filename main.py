ade6c84ffdcd316cc72ff7a766d8138b8f17a5e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:17:26 +0000

    meaning accommodation paper seriously male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_str(array):
+        return str(array)
