baa7a53912c45b7676eea148cdfe8c57e1b241dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:01:23 +0000

    sweep strange hot ear summer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
