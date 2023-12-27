6359a5a454769d4f8e272f21805e7fc2b5e4e756
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:11:23 +0000

    pen sit convince try herself

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
