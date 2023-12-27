2776def30e8334b9307489cb432d45a8efcd238b
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:29:19 +0000

    range claim rank exist stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def clear_array(array):
-        array *= 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
