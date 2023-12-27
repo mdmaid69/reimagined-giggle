6b3e51e92ce5e5402d1bcaec1c48701093bbfa64
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:18:23 +0000

    sure lay intelligent mouth wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
