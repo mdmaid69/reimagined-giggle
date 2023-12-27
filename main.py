b87436bfbd306eb61dd06feda319f37df215e1d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:43:33 +0000

    smooth chin OKorokay last report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
