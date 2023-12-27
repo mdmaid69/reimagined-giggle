d9c92635b0964b607dedb18daa82a246512eb28d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:13:48 +0000

    fetch exercise brief experience region

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import random
+def roll_die():
+        return random.randint(1, 6)
