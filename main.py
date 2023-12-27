120929a2e4d63a7e0c0a84c83f15348634004611
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:53:18 +0000

    future equal straight exchange float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
