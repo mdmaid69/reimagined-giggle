f00a2f025b42a32919f38c80f497ced0880d6f8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:17:06 +0000

    lock exchange juice between interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
