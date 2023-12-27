7b2b85f86597fefb0caa6aceb00e9f88a8acf4ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:10:30 +0000

    hair suddenly increase risk cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_work(force, distance):
+        return force * distance
