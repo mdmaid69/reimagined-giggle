5034d647672eb6e1a0b861e175634ad67a2493dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:31:46 +0000

    quality ride warm when pollution

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_density(mass, volume):
+        return mass / volume
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
