9e49267f35a2b34b2f82b0cf0329df711039b957
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:08:00 +0000

    smile present string tongue figure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_current_directory():
+        return os.getcwd()
