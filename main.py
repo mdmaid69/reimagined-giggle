9bc5f9637d370264e95c14b41510cdfa1a78f044
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:36:46 +0000

    goout ball under Qq shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
