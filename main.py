8d436b52bdd4e1d0ed09c60a48b7f2ca783a6b6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:19:48 +0000

    Ww print circle bringback present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+x = 10
+y = 20
+print("Sum:", x + y)
