5c47c17d221cedffdf4a8a80d029d728a23e43b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:02:26 +0000

    room warn club strength left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
