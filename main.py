abac04cc4ea19e4341d06e8e7e2fd3ddeca36035
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:19:34 +0000

    sweep on principle foot morning

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
