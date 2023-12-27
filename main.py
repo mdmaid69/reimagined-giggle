0dc4afb0d551d119e7d7c33591adcdf229ce3775
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:51:00 +0000

    compare arrival joke use rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
