797e4c2c785e0f3882f40b3c465c8821d402e413
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:34:19 +0000

    pin breakthrough relationship pretty discover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
