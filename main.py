fc23de798dfdc7a397c2f95ca9e2d5ea30b4d4d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:08:46 +0000

    outside near visit centimetre content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
