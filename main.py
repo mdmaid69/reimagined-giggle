80b41bead149f97152964e661419338cea1be562
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:45:24 +0000

    police look range wait mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def remove_duplicates(lst):
+        return list(set(lst))
