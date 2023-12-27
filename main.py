b671670d5fb9b3424731d96f437ceeac8b7b71b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:47:34 +0000

    food full platform remember appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
