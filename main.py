b8fec38fa2a85613b99cb301dbc3e550ecae6c12
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:51:17 +0000

    improvement private hers cross organize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_pressure(force, area):
-        return force / area
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+for i in range(10): print(i)
