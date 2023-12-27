7e7867fdce49902074c83ef0ee0ba0ea6a70028d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:10:13 +0000

    quiet worse few flood lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import os
+def remove_directory(path):
+        os.rmdir(path)
