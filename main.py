638b4dbc09074afe3490f3a93b9b5ea3d31caa38
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:51:44 +0000

    welcome mine catch summer tend

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_logarithm(base, x):
+        return math.log(x, base)
