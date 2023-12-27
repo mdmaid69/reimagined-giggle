8000d602062bca11ec85b5c82df6b531007d783e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:41:52 +0000

    song trip broad force getaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
