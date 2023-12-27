6b33cbffa37115697a06d0446c4e8ed3a1b88c3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:42:15 +0000

    music violent ask begin lunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
