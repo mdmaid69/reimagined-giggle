d40f3cdcdf430fe2683221989662730bf9f090df
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:14:41 +0000

    occur scene enthusiastic tell break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
