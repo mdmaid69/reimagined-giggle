83edfea3145638ec14b66b5b36d14aca095ad1f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:06:02 +0000

    sore centimetre direct question zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_current_time():
+        return time.ctime()
