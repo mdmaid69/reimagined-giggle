4383278131780cabb79ea665ffe9f60aaa527f0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:37:37 +0000

    birthday coffee stay familiar surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+x = 10
+y = 20
+print("Sum:", x + y)
