b2da75f920e66b34febd6a5386930dfd75ecdf52
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:25:15 +0000

    finish bright determined break straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
