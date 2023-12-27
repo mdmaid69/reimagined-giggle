200cc736940087e431a6bf179eaf5b95e62a46aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:29:12 +0000

    crime start apply independent takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
