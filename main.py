5b163229f1a1e680f159a1a2414be597f8904644
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:13:06 +0000

    whisper holiday once challenge develop

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
