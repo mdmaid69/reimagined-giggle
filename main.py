17ca8dfbda06332cd44bc96e167ea36990c12550
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:05:43 +0000

    before clever on know reduction

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
