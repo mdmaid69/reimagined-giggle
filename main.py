74426ff7ca64bcfb48c7c25cb89d15a3b3bf359d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:37:18 +0000

    translate original figure take sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
