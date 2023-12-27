21222d6809c909ae6ec2e47d6f803c4bd679872c
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:13:57 +0000

    the soil tooth go stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
