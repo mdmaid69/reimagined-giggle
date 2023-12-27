ca42fec80dc14e1490c4132e68351bc1124890b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:15:16 +0000

    getback even wheel later move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-x = 10
-y = 20
-print("Sum:", x + y)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
