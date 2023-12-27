3032bd195e44869a69c2d9fc08ffda43fa149fd3
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:29:32 +0000

    teach lock brain excuse want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
