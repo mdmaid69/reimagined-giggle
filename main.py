e8543628e2e86054d8ffa6d6bf824fe3beee8f63
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:57:16 +0000

    now finger drive firm reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
