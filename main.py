c73648207eb525d0baec2c8a4ffaffae9bd58a2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:47:32 +0000

    separate corner turn getup load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
