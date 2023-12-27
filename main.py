010d45fca4dbcfd7851bad2823b0ae2f25d1908b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:49:13 +0000

    cap limit intend loud pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def find_min(lst):
-        return min(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
