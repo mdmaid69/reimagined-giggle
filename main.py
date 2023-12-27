1b25446843603f05b0dd316b9a54280f10f8c12c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:47:35 +0000

    size description score dry 565end

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
