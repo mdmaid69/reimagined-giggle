4ffebb90c91d66a759c2dc0815ca8384bdd733ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:27:19 +0000

    usually strong possibly young mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
