2b38c1b5a33f28b0823a08c6861fce0421e95440
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:50:16 +0000

    comfortable direction free achievement doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
