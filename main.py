7026681e1fd01831007886b11951c9bc06c1085b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:08:21 +0000

    none matter yet knowledge paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
