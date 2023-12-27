69d462e251a53f5d6feda8acfbce687ee26f384d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:21:04 +0000

    risk past sister claim cause

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def clear_array(array):
+        array *= 0
