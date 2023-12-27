5e15437ffe42717be1616adeed70b918a9f6d43e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:03:26 +0000

    comeover true generous reject leave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
