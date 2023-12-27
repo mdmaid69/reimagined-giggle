0e2e17bda24660fa4a3b546cff4ba14e060fdcea
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:33:24 +0000

    computer account pencil 573entertainment live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
