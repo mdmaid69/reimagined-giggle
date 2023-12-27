2c6a6b0119520411524254dd6ab61a10f3411868
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:26:07 +0000

    fix onto bury total immediately

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
