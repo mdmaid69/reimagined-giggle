3e0510919275a627c4b7f4f197b0c3c4c4cf9891
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:36:28 +0000

    search second disappear foot strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
