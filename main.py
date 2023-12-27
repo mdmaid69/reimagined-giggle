9c54ad8758d85cd90e415810bd2c7a572da5cc0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:19:58 +0000

    surprise transparent value straight occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
