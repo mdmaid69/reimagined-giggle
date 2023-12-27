297fc31822250b9c2d2ef8581336878835075d9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:27:19 +0000

    stay under cough slide before

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
