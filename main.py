bb7566256833688ed0512be6a80bfd1d0c28a94c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:38:09 +0000

    wish my adjective ear contrast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
