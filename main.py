fff3f66fdf9592e7f2c6c0b3b03457f50b82c50a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:10:50 +0000

    fall last rain book get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
