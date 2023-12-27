79eaa7e2390e330d3b79e48c3dea0df6d8de1891
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:45:34 +0000

    offence butter sharp rest instruction

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
