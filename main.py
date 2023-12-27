00d5abd9cb84624ccee9385643c317abf2a6c872
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:36:08 +0000

    punish payment store blind computer

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
