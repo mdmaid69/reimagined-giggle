3885742e35ca03268a65bdf833ad16358d37504a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:05:24 +0000

    himself continue on for ought

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
