fc0de3cfcfb6975cf4120fa0be0f24e379e1326e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:07:17 +0000

    lose pleasure home operation beginning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+i = 0
+while i < 5:
+        print(i)
+        i += 1
