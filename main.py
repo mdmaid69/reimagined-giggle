401fba241b0d186c0846422d4958883f61329995
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:25:19 +0000

    patience balance film principle weak

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import numpy as np
+print(np.array([1, 2, 3]))
