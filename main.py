6f9e7809fd7fae4b397672ed65dd2a062463c2d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:06:43 +0000

    theirs belong rich basic steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
