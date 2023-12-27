ce2db431964a435cf98d6383e68fd2b008501731
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:50:24 +0000

    firm pretty pleasant mysterious any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
