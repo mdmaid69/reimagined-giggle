84bbf261c6c5281591b3503c12f2f89e08669ff1
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:47:46 +0000

    question bored work communication plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_power(work, time):
+        return work / time
+import sys
+def exit_program():
+        sys.exit()
