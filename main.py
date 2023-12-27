a9320560fb2ba1e43ccaaf4a1b23693bbaa6386a
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:41:48 +0000

    sudden guide really your cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import collections
-def create_queue():
-        return collections.deque()
