c27b4d7857193cde902fec10cf87b10bf4006800
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:45:12 +0000

    charge coffee you achievement how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
