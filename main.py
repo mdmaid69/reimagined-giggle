92a980969d5b6006398648ccd554033699929a21
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:39:02 +0000

    getout calmly apologizeapologise blind excuse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
