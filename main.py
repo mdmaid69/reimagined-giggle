0e25705dc834d799d7b0d5e3fa474ec8b5ba0a14
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:13:00 +0000

    relative share butter occasion other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
