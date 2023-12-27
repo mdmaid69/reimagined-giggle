cbcbdd40d0149f76225c4a57619ef9c9ed76a285
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:01:26 +0000

    persuade hit pin anybody apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+name = "Python"
+print("Hello,", name)
