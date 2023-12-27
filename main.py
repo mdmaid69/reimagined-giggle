13ed2deced0a726c098aabe7d54e4a051677d59a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:16:35 +0000

    neither modern minimum dangerous interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
