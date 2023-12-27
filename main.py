fdf0ba642ad01cea891c2d71dd0026866144506c
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:57:46 +0000

    tool sick stiff success physical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
