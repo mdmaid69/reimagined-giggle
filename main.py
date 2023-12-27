4e53cd41f8f2162564b56107a190171d543a04f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:54:59 +0000

    relief wait hers gate development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def remove_duplicates(lst):
+        return list(set(lst))
