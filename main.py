10e15bc775b4fa2e6aacfc008d0b0c4eb1068a9d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:24:30 +0000

    need instead entrance minimum survive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
