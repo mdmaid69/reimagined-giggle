e84ec81d6461822b4be82129f4cc3224674d1199
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:11:44 +0000

    left upstairs favour guard outside

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import sys
+def print_python_version():
+        print(sys.version)
