38fa063273962fb42022d8771c46f966177b7cca
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:51:15 +0000

    method fill lesson might foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
