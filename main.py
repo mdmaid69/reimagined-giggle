44bdf6672ca395eb872033c06ff5c11e3c4d5988
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:48:21 +0000

    inquire touch keep me gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
