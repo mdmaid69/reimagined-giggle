a7a7c7d09c928ce6ac3838963d57986747cae454
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:44:32 +0000

    Yy delicate thin equal lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import collections
-def create_user_string():
-        return collections.UserString()
+def create_queue():
+        return collections.deque()
