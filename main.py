54f0e4d0ae9839baf187bbe94507b8f965730a41
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:30:49 +0000

    suckme shadow ball every town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-print(sys.version)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
