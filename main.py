e94b9519e0af7afc9e7a4d050cacb7b4d372dae9
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:34:42 +0000

    keepoff shade temper impressed taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
