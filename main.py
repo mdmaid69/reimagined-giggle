776ba9cc95b65237b01363872b6c1b7d197b287b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:50:19 +0000

    spend mess luck put undo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
