e19560c7c0149dfa8f940ce70dc66b5e9db6ded7
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:07:20 +0000

    beginning ground search takeover car

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import sys
+def exit_program():
+        sys.exit()
