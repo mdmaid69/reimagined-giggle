f6f5b69c343b425bed2d87e706b268cee799b7e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:55:40 +0000

    convince improvement who load finger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
