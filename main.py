1eb156293e9156f2d134bb4a0182efb6b8a6e460
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:23:57 +0000

    press plant disapprove introduction gradually

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
