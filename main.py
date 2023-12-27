5541975bd5b57d4b009246f41cac4c571562a2e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:40:09 +0000

    admire corner quarter impression odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
