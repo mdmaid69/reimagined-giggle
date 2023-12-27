10d882ce116183b82d2fb02d4b12cc319e0d844c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:05:46 +0000

    flat dare block guard sleep

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
