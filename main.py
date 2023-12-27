c17fc9146c529e31ca201c60fe5fc328b5f379d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:01:20 +0000

    stupid regular deep spot breakthrough

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
