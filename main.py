39fa9cd261fd9e8dc6712ccd3b5d744e369f5620
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:24:09 +0000

    guess taste away morning table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import time
-def get_current_time():
-        return time.time()
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
