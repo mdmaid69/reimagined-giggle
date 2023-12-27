fdb5fe6e84aada0cc22875969b5545b67961acf5
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:51:13 +0000

    ordinary runinto doctor mind as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import array
-def get_array_as_set(array):
-        return set(array)
+def reverse_array(array):
+        array.reverse()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
