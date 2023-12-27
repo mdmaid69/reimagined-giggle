589b701df904385051da31f1ad0ef9220377adff
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:13:06 +0000

    pick where equipment would slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
