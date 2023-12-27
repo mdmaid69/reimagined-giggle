fd033b4e2b49c73cb28ab357415f52d756434d3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:12:56 +0000

    track contain rest under able

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
