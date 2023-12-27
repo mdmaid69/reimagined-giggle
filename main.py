b27a45aaf321222c68238793060e0d03226a4147
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:14:09 +0000

    temper neat attractive visit Uu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
