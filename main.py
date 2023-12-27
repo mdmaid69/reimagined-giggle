6eff638ace0bcf39ded435db7cc1ddb6e0441335
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:28:25 +0000

    deep include entrance perform she

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_circle(r):
+        return 3.14 * r**2
