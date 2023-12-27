8613bebb5452d8ac724506fc28be74228a4e91fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:34:16 +0000

    mine ball spend float intend

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_array_to_string(array):
+        return array.tostring()
