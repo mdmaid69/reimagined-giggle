ad4e407682877bf21c693a38cbeff52ae82aa110
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:15:06 +0000

    nothing show success earn opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
