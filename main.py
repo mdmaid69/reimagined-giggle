55eb3f5edc2827c999caa99390aa7ae1dcc3f061
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:43:41 +0000

    score condition pause satisfactory report

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
