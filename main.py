c8323cf6e0ca57ceb5c86786a5834e52cbfc446a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:48:17 +0000

    runinto mistake morning air occur

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
