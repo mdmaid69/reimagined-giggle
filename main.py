ca7e54391ee44c73f4cb3c34af0a05edbfd62c55
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:56:45 +0000

    take nose modern disagree best

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import os
+print(os.getcwd())
