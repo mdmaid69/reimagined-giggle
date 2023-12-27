b8afcd590b83a6d80a72870ef4b572140329559e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:59:30 +0000

    speak clever cap except unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
