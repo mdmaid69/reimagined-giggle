621268ba53c07769ca12cd014c134f32015ace9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:40:42 +0000

    discover row criticism deaf area

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
