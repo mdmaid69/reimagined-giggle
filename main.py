60c549f3766b1ab9e71523574eceb0eae6489bed
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:45:00 +0000

    plan half public cry mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
