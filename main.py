86a6d38281446013f284da9b11640428db878af9
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:04:19 +0000

    successful strength minority guilty few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
