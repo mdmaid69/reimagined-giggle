dace4159f84e9acb4e9ccc8a824a064e5945537a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:45:16 +0000

    belief decide hurt production anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
