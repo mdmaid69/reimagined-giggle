f32a9137c97381c09bfaf648eb9e71a9aceec4c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:24:03 +0000

    work upstairs wish themselves thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
