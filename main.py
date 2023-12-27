9b5573de6de62ef6727f4ab9e6d5ab517fe417b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:36:30 +0000

    keepfrom anywayanyhow right between Pp

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def square_number(x):
+        return x**2
