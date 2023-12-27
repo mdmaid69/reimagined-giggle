7a286c2c7e9b7fa86aa2b6ca199209bb85c78225
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:10:41 +0000

    error responsible club nose clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
