41e24df187f21f07e75120bc092727cd52c9a21f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:06:57 +0000

    through private question cap have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def get_array_typecode(array):
+        return array.typecode
