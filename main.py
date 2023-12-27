946b014e75ac969265cce5a03c66d0f96464e4a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:44:06 +0000

    closely abroad trap road inform

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
