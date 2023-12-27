8e1ab57e3eee5f8b853ba538df376a9bfea0f26f
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:46:45 +0000

    crash far peaceful into glad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
