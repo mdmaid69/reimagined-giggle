1111094dec0b39bdbc4839cf070f700636e409a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:38:25 +0000

    trust print entry sail translate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
