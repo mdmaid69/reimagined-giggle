a2a8bd1947876e9d674555d95fdb38709068f7c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:16:12 +0000

    respect remain guess they count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-def is_odd(n):
-        return n % 2 != 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def count_elements(lst):
+        return len(lst)
