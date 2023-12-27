3a88c1919aa24a819ef0b097715becdd5812fce7
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:33:06 +0000

    quantity responsible left brown punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+i = 0
+while i < 5:
+        print(i)
+        i += 1
