57a141e37906724a2e105a7fbdf05ad27f473619
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:53:49 +0000

    public snow bear advise job

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
