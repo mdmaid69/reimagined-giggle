502a06afe450ae8c02a7b8704af57913e6344e16
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:07:00 +0000

    left entrance odd spring unable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
