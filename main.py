2838ec807cf5bda909f3ffdc36359dac7d9a8766
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:51:18 +0000

    basic replace feed blue section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
