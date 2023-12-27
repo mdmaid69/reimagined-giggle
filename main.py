18e66643f3a84fb20a9186d1f76882913c14e5d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:57:24 +0000

    disappear fail niggaacademy children continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
