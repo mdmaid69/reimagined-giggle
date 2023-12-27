1c99bcdf1322fd167e49920ec16bcff87e7abf7f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:39:16 +0000

    blame seem material listen couple

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-for i in range(5):
-        print(i)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
