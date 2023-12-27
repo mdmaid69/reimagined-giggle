f320bd2c36ef23a5697a0b62c5932116022910f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:02:21 +0000

    human foreign key report bury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
