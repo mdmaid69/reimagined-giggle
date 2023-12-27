df0c26b8a035e0cf0c070e6566e38cb8f8f33ac8
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:56:19 +0000

    confusing public attitude contact large

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
