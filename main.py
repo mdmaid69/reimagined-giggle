730fc339cc14d49d4cddc0a209de79ac288e82b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:38:31 +0000

    cover neither coffee rely meeting

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
