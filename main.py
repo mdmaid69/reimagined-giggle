792849d283750e1905bc6d42c03b695efe7c6665
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:02:46 +0000

    paper tread inform stage terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
