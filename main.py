00dd8510109ef9a5497c7a4da22f5d88fd5837df
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:08:03 +0000

    Ww ahead broken me chat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
