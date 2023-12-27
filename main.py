cc194ac4de5b1ec130d8485495d7b63d17aba3ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:44:01 +0000

    keepaway suppose yet dream file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_user_list():
+        return collections.UserList()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
