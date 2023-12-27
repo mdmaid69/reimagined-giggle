d6b4eec681839dcc598b75499d9d709cdddd83bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:53 +0000

    better loose direct improve chief

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
