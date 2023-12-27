d4ca1ba9b1a8b52fce2e90b0c2bd89b56adaa3fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:13:33 +0000

    greeting listen escape risk whisper

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
