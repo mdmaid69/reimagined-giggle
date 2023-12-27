86ddbe3365b080c1a80baffb8d0099cf93095fc6
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:55:59 +0000

    contrast slow increase priority discussion

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def append_to_array(array, item):
+        array.append(item)
