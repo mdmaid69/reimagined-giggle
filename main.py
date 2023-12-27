b3e4c8f42ab50620de0d5e06500578f1672116b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:02:04 +0000

    cold cut phone sure nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-for i in range(10): print(i)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
