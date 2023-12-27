f395d14d202c8d0256cd6ce43e9608ef01a00f1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:57:26 +0000

    deliberately go human considering present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
