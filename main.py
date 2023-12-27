bcec12604ab4d2fbc980d3aec86bbcd931d11bc1
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:53:00 +0000

    case be talkback bread force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
