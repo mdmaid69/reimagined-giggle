2678354ac7773cb8ea0ae69d2ecd56024b57275b
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:41:21 +0000

    thick consist degree nonsense prefer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
