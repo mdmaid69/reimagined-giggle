9347a818119d1a8d83e3a913a6b319af31b4d210
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:37:39 +0000

    any priority fast programme win

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
