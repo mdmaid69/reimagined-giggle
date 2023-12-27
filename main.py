11a7f29aef7c7aeadcb1bb59a38db4503d16660d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:55:45 +0000

    begin name commit especially light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
