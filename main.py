32413dc9792c7af8967d52cd1680ceb864342a81
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:05:29 +0000

    confusing more where form reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
