a7c8857fa2279d7c9738f1cc1e30c8a6e7deeeac
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:24:29 +0000

    old everyone work never talkback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
