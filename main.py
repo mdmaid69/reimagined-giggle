dc19975d8fc6e74a78ece35e09ce2eaafe134bb0
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:45:06 +0000

    pronounce pressure conduct gooff callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
