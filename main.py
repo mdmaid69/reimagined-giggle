1e6ed0daee7d6bea86402ea1676cc0ae09d8eb22
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:05:35 +0000

    your giveup unable trouble disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
