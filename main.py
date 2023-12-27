20825f18a089a219919251ac4e7613bb25b2113d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:40:06 +0000

    fly provide wall failure wherever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import time
-def get_time_since_epoch():
-        return time.time()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
