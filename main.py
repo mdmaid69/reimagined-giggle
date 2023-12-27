ed3e22c95ca935aaa4c37b2822cd06f9008b1302
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:29:02 +0000

    joke obtain secret brave pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
