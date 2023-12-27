424033582fdfdd78d674b4ce3b8fc5bfeee9aadf
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:03:05 +0000

    make climb estimate exciting that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
