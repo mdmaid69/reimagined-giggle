9b18d908f3f0e08af8d803ee9a707085320d1a8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:51:16 +0000

    every male band pretty miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
