410bbb68238ee45b275558b03f1f652461943236
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:51:09 +0000

    immediately plastic at clear turnaway

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def count_characters(sentence):
-        return len(sentence)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
