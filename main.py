8e83e2b829fcee45720d8aac54cd735bc0fa2a7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:14:29 +0000

    repair pleasure greeting right cut

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
