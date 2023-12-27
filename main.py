f7d959aaa8ff2429301d41986d75d9a0b87c36ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:36:35 +0000

    note bad great be weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Characters:", len(text))
