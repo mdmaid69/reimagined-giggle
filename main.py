4a7c03867ea41ad6ccccdee2012ab72de8e30922
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:25:40 +0000

    react loan belong grand difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
