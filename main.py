8837c963c8a014db49ccaa9285d001e34e4675f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:38:15 +0000

    string sticky slight difficulty help

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def multiply_numbers(x, y):
-        return x * y
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
