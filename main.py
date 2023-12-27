c1af00f65afe945740073d43315ac567c6037e53
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:23:48 +0000

    bound commit personal declaration count

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
