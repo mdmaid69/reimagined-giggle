48bf8aacd487c1a374da1496d9e8757502870325
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:04:57 +0000

    could neat worth worst trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_acceleration(speed, time):
-        return speed / time
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def get_list_from_array(array):
+        return array.tolist()
