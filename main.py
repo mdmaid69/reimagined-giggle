f0a1ba950d99783dd2c143abdcc0335567b65504
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:10:17 +0000

    search drop success let thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def split_path(path):
+        return os.path.split(path)
