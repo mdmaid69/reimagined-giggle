0d4681c1d4d380abb577c3308f74cb61e6e915c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:30:29 +0000

    health may room door exit

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
