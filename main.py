f976b0a1d2d3a8b855dbcbeecb222eeb377a46aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:18:47 +0000

    short pencil earn claim empty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def add_numbers(x, y):
+        return x + y
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
