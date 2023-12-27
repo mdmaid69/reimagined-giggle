2c4f803f968286f07837e8896adce22b420b0582
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:33:56 +0000

    ourselves movement similar after hit

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_area_rectangle(l, w):
+        return l * w
