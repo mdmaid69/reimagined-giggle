6dcb3c92df197787c2f3f46c2b6973b96cb89842
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:23:23 +0000

    half maximum memory hour private

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
