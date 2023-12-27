1f49679f55b2571eeb3eae8057935367d83263c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:35:26 +0000

    glad plant once exciting sentence

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
