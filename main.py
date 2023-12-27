140b35b70de2c9befb526fc94c453ce27862ff6a
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:57:42 +0000

    mysterious boil cover scene bottom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
