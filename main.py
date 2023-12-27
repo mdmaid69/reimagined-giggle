27a54ddce10fad8f3b444206cd83b6b06354301f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:20:56 +0000

    friendly turnaway spite cruelty themselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_directory_name(path):
+        return os.path.dirname(path)
