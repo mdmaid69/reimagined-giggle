97621f4a7188ad4fec0390361ec1da883e81c89e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:06:13 +0000

    clearly getup dull intelligent damage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
