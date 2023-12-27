49ae5e3584a04b284ecf16bd8e2369112478c27b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:04:47 +0000

    shopping make expensive shadow pin

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
