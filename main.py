a3aaf783978785c67dbc23897669923ee264f313
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:03:08 +0000

    person move hurry sell play

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
