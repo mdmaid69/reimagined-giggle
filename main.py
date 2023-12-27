8ddfe51eaa613544d0d6f6586638bfa409518f1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:04:53 +0000

    recognize operate influence someone breakdown

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_work(force, distance):
-        return force * distance
+def find_max(lst):
+        return max(lst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
