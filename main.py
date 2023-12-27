bc506440733d7b139cb965f83308346b0db0cba5
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:05:49 +0000

    environment giveback noise direct watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
