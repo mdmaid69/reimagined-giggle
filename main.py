64471acd3cc6a8964fc7f36b72627783e60cc7d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:36:02 +0000

    plant worst distant discussion keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  def convert_to_binary(n):
+        return bin(n)
