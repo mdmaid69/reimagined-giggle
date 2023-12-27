8a84059fcb552cd8006b25947f018da501f9a362
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:25:33 +0000

    breakthrough scared spot spare aim

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_density(mass, volume):
         return mass / volume
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
