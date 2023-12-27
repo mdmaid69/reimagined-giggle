6753ef80725718142a9c65c8444d009d095b4ea2
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:07:39 +0000

    black error threaten well interview

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
