c2108ba8fe05f9e59882e8dcb22eeb0c962058a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:52:12 +0000

    minor scared second straight communication

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
