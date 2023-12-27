2da2ddf6ea49574c1f34d6dc40471991b6fd4b24
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:22:12 +0000

    firm because seem Kk stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
