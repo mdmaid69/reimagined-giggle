7966f6945c82933b645a775746807a2e66cff181
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:19:04 +0000

    patience fire attempt process environment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_octal(n):
+        return oct(n)
