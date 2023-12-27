9d7687e023c06e6100023bfa5dd0d75b41533431
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:29:15 +0000

    through thumb system book transport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
