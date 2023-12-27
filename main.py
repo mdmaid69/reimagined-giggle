35e1b52f4f133e4a36a021441ddacf19ad3eec5f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:55:58 +0000

    aim trust single conversation determined

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import sys
-def print_python_version():
-        print(sys.version)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
