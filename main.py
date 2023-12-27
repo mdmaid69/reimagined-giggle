303c25073deeec38a2631fb5167307c6d7e998c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:48:00 +0000

    pool giveback sick sick hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
