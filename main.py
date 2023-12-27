5e077755fb36dddb2008aac063ca105a561b52a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:57:33 +0000

    guilty embarrassed region right force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
