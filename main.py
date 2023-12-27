0b081880793c1d1c2febf1eee6352fdd1cb1f56b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:31:29 +0000

    tape boring think phone about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def print_python_version():
-        print(sys.version)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
