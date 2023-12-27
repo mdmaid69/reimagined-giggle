dfcb1d2732048b7868438d9d86235e1ec21d00c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:54:55 +0000

    confused hide remark crisis discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
