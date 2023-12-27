0667bca7d10f679911e95eb0b34a3b729d0fc4f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:21:39 +0000

    past match during buy pride

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
