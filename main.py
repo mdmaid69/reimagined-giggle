e9ec8577a0006763ee30f320fd18e22a1e5aa883
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:00:23 +0000

    themselves previous style sweet feeling

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
