4f9b32d4c49fc118474c7eae837ae774216af8c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:03:54 +0000

    nor positive woman failure complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
