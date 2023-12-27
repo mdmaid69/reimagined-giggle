b37a8bdc2b06e245afc52bc9c92adcf35de6af46
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:00:20 +0000

    hate phone interfere satisfied crowd

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
