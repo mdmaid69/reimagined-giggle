2c78a750ff07fcde55d9361125836d8fd9626e9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:57:50 +0000

    rather fault conduct speed bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
