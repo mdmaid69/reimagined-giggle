398d229b62b36b65ba2138c521d43fe53947ba7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:19:45 +0000

    good transport of naturally disturb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_area_circle(r):
-        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def reverse_array(array):
+        array.reverse()
