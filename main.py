69dc3985bdea71cf72e67c78133150a62d225e3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:37:56 +0000

    remember blame keepoff pleasant lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
