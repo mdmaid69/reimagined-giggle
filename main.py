5afbd0b469bb904ba076fcef89bf89a8b67cfd80
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:13:36 +0000

    thinkover final continue with shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
