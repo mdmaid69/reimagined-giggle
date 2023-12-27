3ef5636f084489110d0556fa396ca285e668e4e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:34:20 +0000

    hot bridge stone mess shadow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
