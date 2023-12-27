1147f0b30ae1acaa0f298a949755b8abfbc7d2c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:09:10 +0000

    touch onto left mean talkback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
