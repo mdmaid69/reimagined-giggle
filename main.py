b7c50e2c85af8ff753aba73a7219104358e9d232
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:57:28 +0000

    breakin walk include importance thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
