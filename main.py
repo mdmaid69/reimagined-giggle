113d1e1a64bcb5ab2ffcc977eefdd9bf2488843f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:35:51 +0000

    load finish band occasional survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
