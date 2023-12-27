7d31c977a413f67ad32e04b2693c674b7eceb796
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:21 +0000

    movement crowd dare godown wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
