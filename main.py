cae270c82660c6222f6d307ab75539f06ad6a900
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:17:18 +0000

    wish circle regret previous see

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
