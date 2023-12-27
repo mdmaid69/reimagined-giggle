6520a6ae8c4e0fe7b7b5fe476b2db89f1e0e9ddb
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:36:32 +0000

    second callup come exchange way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
