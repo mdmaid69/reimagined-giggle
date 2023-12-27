6992df4cf7a17cb27256d9df89d92c8a9d02adc9
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:13:01 +0000

    sort catch tonight reliable date

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
