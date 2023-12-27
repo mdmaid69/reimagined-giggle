bf1876b9437e13b452fabf552ea738a9747e6c2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:48:03 +0000

    transparent steal behind suggest fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
