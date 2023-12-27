c19cc2886de7149385133e632f02fbaa55cb6e8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:28:45 +0000

    car do string exchange complaint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import time
+print(time.time())
