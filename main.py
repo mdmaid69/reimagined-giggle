3d3da95b5947dc4e4f3e9cfdc1992749934af4c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:56:37 +0000

    another work wide figure joke

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_even(n):
-        return n % 2 == 0
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
