a91b459ed62f5904b7bf3ebbdc6fc1c0c658baa4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:58:21 +0000

    day bread I nothing combine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_hex(n):
+        return hex(n)
