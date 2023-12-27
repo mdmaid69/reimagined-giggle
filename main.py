c62db0362b4af17f619c1bb711cfe70becbbee62
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:19:36 +0000

    name new dig would come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def add_numbers(x, y):
+        return x + y
