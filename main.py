30784bad805e5f79cb1c01a7be22434b1107cb22
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:10:42 +0000

    welcome maintain unlucky hello exchange

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def remove_directory(path):
+        os.rmdir(path)
+for i in range(10): print(i)
