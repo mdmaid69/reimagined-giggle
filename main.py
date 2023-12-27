33a750eb62061c29a3ba5cc1781c468223084066
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:15:03 +0000

    scream allow finally beyond previous

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
