ba6a9ed1c674c7629377fe84ad8ab4de2e3a824c
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:14:02 +0000

    classroom preparation successful after alive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
