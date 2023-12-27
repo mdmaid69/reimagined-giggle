2b39176c694ed7dac238a9428bc955f36697cb0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:33:55 +0000

    across takeaway believe block talk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
