b39ab6cc5f8a4d9b6ab136f2be02cc6b02ca08d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:01:21 +0000

    fail attempt very interview yes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
