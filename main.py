383955a7ee1e11275090c32adffa909dc4b66785
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:53:24 +0000

    protect wheel wander greeting meaning

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
