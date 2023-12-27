701cec97529f8246b6352f3fff72598a0fe207b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:43:43 +0000

    patient friendly sorry prevent honest

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
