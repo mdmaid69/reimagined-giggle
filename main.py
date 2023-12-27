d23e97cfcd7d9b1fced8be10b610f564d56dbb96
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:06:23 +0000

    past complete forward nervous contents

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
