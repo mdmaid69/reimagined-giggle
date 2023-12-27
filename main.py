1003ea67983ad77275bdc3ae23fd8b635fb0cc8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:56:21 +0000

    long position conversation accident unable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
