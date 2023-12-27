083ec56764f7d94ae518b0b20f594691af320821
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:00:02 +0000

    shout pause beneath settle wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
