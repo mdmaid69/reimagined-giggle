861ba441dcb2b69acdcc9e1976589804d66837ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:48:46 +0000

    cry cost unlikely activity straight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
