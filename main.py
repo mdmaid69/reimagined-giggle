cea8490864f612b229c8ef124fd1b2d0f4ac861d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:41:54 +0000

    nigga game long scene description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_bytes_from_array(array):
+        return array.tobytes()
