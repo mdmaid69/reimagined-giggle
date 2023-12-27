b8f24cb29addaa3a128444cd9bfb00a80f3122c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:51:06 +0000

    name weather hit listenfor situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_speed(distance, time):
+        return distance / time
