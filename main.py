a7f051371fa7ccb949371e5173e322e138904387
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:59:53 +0000

    though goout slight present dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
