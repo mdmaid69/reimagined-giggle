32af1d33e0c439401245aef1b40460a97ccd5952
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:00:01 +0000

    crazy limit major unlucky born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
