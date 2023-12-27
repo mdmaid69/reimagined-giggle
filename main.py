dc7e72784512405d02469175d77da57a20958f3d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:46:35 +0000

    bad apart get close visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
