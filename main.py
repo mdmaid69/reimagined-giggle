0ba6651fcff0b636cdb73d446ba2111e35c10744
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:15:02 +0000

    geton complaint fierce education coffee

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
