a444a88e0a2dcc2a28565666ab9b2abbf48b259f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:56:31 +0000

    holdon appear length worried read

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
