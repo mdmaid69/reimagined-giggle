3e53675b9c2ee76e0fce7d6c6f9947ecd0b78b25
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:38:15 +0000

    pen shout refuse late settle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_acceleration(speed, time):
+        return speed / time
