885670d9bb59497b53bf7b1065db4a4439d8ac79
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:30:31 +0000

    advice bright preparation container mean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
