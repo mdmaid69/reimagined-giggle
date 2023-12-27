36ddb110d56eb486fca12da2b568011b44f163c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:10:30 +0000

    turnoff otherwise drink pretend goout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
