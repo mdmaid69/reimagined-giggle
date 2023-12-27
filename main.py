ecefef7cdaed26d14673975da014c0ab90e3172b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:16:30 +0000

    first open complete choice pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
