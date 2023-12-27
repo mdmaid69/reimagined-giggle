87777dd2b56f3de2bcf584780fbe37221fad7f13
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:37:24 +0000

    introduce party mark limit safety

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import os
+print(os.getcwd())
+def square_number(x):
+        return x**2
