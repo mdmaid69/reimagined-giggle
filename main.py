8a33ee1c1e047eba57d208c00a0e902bbe1f36ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:02:19 +0000

    trust quickly mind niggaacademy fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
