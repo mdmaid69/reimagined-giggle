d1e04d0e9bba4d1f7126e4d9a411c255946d7982
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:39:50 +0000

    sting suppose doctor worth holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
