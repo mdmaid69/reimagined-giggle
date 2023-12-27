691c61eaabc1b22efddab0f696f8421821a045bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:50:47 +0000

    ring ourselves telephone describe but

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
