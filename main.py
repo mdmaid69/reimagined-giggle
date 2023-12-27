cac7f871a1598eb8e1158071efefd13b17200905
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:16:37 +0000

    face though sign single keepout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
