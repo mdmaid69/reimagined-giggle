e925d9136f1102f134bce732eda4c562acfa43b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:34:49 +0000

    scare generous difficulty promise keen

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
