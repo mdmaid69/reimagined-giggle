c59d5142ea296468659ae3f0bd82a47bf8e52a7d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:29:23 +0000

    range price pile improve community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def greet(name):
+        print(f"Hello, {name}!")
