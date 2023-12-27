1e32ebef49a8ee5a5e6e18f2278023d9ac218c17
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:50:53 +0000

    picture help blame late flood

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
