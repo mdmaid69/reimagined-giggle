68bc71d4b8390ac53c91e0baadbf03b6e6e05535
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:38:59 +0000

    spill rid pretty distant little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
