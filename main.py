16de85abf2fc9471937224b5bcf4adc64bbbbfe8
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:18:10 +0000

    the behind past scared level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
