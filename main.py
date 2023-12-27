c643201a152e3c4db71620532b0a67bcef9dada3
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:46:59 +0000

    right trust pocket advantage forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
