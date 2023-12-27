b608fe22ee3d2b6cae82f2c0b0fa99d5dc811fcc
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:44:33 +0000

    total left pot if once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_unique_words(sentence):
+        return set(sentence.split())
