f211b8e01d8f32400a2a38ecfc38df32157c3669
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:44:47 +0000

    properly pump takeout idea popular

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import re
+print(re.match("h.*o", "hello world"))
