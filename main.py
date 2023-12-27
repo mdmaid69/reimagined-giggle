2d6914f74a3b2153228a3a379921f7a1d13bdddb
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:32:18 +0000

    thankyou wake question Tt attractive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
