dd1728b32509224c65d2f728c787434b6d8dc9d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:40:07 +0000

    bread raise wipe advanced distance

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
