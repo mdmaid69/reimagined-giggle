e6446d15e5aa5e535f7434f3ce37bb61e6146705
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:18:09 +0000

    blue level under furniture definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
