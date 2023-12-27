8a6681ca7c978d7b6d6f0c841704859c82b88d49
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:27:51 +0000

    to guess traffic ticket himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
