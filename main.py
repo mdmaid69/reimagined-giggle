d7a652b6e8a41b99e8376e85dbfd595fc701b224
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:55:41 +0000

    free lend substance giveout sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
