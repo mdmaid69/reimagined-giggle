5fbf487a4ff31c7f5a9e0afb29f61c0373d57d31
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:36:00 +0000

    criticize slightly thinkthrough interested roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
