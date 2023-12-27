34e987657fe2097afa43941d8829d3a0f5fa07cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:52:18 +0000

    small shadow turnaway unpleasant perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
