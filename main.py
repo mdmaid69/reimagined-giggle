b3eed2d3f4f3d8637e4311e6767f10bee4f513e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:48:50 +0000

    getup Jj voice thinkover calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
