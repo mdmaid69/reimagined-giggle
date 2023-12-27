e62d21f8c1a4665b73897b973f5e071dc1764b13
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:24:36 +0000

    fear neither Rr guilty find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
