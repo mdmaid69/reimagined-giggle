233bf3a67173514d2f96b8232333cf8d5deb6ca7
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:12:45 +0000

    us welcome might back reject

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
