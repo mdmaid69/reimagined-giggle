227c03492da868b3da946fa27fd6dc98245cce66
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:05:19 +0000

    reduce party opponent pencil print

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
