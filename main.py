159ebeaeafa15594fa08062603e2306e51ece9c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:00:44 +0000

    complete holdup importance tomorrow pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
