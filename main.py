2bd5336841f121a15245069e33ef63f9f4400954
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:16:57 +0000

    bread sleep dry something suckme

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
