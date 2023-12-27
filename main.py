9cb143445f06716df41bffbe6769f1a1647a6649
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:13:08 +0000

    scale suffer sound firm claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
