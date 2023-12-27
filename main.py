560cefc4b6f1422acb55be79d8b2b62865b4d2c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:14:43 +0000

    proof crazy experience pocket by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
