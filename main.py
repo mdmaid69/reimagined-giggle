d2a475d9aeff41b6e3e70761da65dba501916652
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:16:33 +0000

    powerful voice cover mood stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def convert_to_binary(n):
+        return bin(n)
