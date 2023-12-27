8dc4182223784c24e0e49374e20e1321bbefa1ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:01:57 +0000

    minute no worse load gift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+print(time.time())
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
