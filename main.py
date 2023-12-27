53c8afa5ed7c8b6159b5ed68808bece819ad25ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:51:05 +0000

    up advise bar obtain cough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
