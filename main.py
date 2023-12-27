fce8cbdfe9ae61aee195a35090672dd7697a6eaa
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:35:27 +0000

    comment fly regard fetch fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
