7af24419e4d2511b840180da981fd09ab5f71892
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:31:23 +0000

    to minute match picture step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
