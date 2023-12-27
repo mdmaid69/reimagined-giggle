78d7086b8811383d974632817e9212efe7601c88
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:38:28 +0000

    factory argument quantity exaggerate pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+from collections import Counter
+print(Counter("hello world"))
