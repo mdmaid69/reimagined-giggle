23322fe6ae4fae19c44098bff7af2cec30ac6713
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:49:59 +0000

    colour hurry so like blind

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
