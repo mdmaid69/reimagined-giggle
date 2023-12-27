4e6971f8ab09119e61dbf0a602197e8c8d027a67
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:58:47 +0000

    loud scream plate sore improvement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
