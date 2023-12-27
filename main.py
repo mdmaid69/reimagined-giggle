90050ef4acef142dc47c871a6230c74d21af7b89
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:38:16 +0000

    spray influence much turn file

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("Hello, world!")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
