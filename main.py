fb68a42682817f8f84d72095958a856fc597cc02
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:25:10 +0000

    raw trap onto getout gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
