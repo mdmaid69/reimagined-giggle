6f498c1291724844e2cdd841c312a9a844e03819
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:58:32 +0000

    factory roof ready that Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
