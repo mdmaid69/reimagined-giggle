c6b0aac6d74fc898ad9d02980aab5084d4ebb31f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:57:05 +0000

    feature teach serious comfortable think

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import time
-print(time.time())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
