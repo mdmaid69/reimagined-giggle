24a9fd6850bcfea043c85457243576c4c8d327ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:23:37 +0000

    moon collect deliberate fit result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
