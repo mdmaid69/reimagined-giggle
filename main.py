dd2d255ad6f925edb9f4208ba43711fe3410fae2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:36:06 +0000

    present through whistle area oil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
