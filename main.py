0728aa1c042b3412faaa23a7721aa0c890dfe7fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:01:48 +0000

    come getaway accordingto dance beautiful

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
