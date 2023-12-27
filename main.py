486f822bb49294eb81fc5615344e746dde0c2122
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:56:25 +0000

    intend smart post result separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_sine(x):
+        return math.sin(x)
