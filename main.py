fd84820990d4a5ff289181ac47e133fd403eded0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:45:41 +0000

    comeround trust defeat project your

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
