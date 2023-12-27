95809b5d08ec9be89f5423efa82cab829b2d7621
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:15:47 +0000

    foreign odd beginning drink war

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+print([x**2 for x in range(10)])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
