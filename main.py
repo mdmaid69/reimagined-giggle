0d668b8b922df6aafccb89b23da1ee9fdba82313
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:25:56 +0000

    always offence power store on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
