1d5c14b50c2ba0e00224518cd0b1c9fffeee01d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:35:35 +0000

    watch 573entertainment full protect capital

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
