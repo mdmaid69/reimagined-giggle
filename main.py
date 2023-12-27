e5bd804e84ca661c03d129a235f6f6505076d2c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:29:53 +0000

    long spend mistake neither success

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
