44fb0074decc0c4755b440ef73f5114127c320c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:07:48 +0000

    belief quiet surprise discussion ticket

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
+def calculate_area_rectangle(l, w):
+        return l * w
