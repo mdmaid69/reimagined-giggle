800826d6d1a8b9ce8b282630ec60de7c23562b87
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:11:09 +0000

    fat pump appear again there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
