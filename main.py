fb995c9c3106e21cdb9be89cd7234b422cedc355
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:50:49 +0000

    comfortable fix rely paper play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
