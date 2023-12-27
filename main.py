7c714f9f5f9dd308cc263f2974779b14265e492d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:09:20 +0000

    maximum Kk keepaway earth turnup

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import time
+def get_current_time():
+        return time.time()
