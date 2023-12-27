be8314497aef73c683bf648c8044dbf24df78424
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:29:56 +0000

    contain push split behave let

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+print(math.pi)
