54e3ea8d1c69ca53912a7364bec8797deaade12a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:13:24 +0000

    block run left scene male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def remove_duplicates(lst):
+        return list(set(lst))
