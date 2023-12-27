4edc61dce8e7131f83ec85b41eb24daf852e2197
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:05:07 +0000

    continue effect content high quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def convert_array_to_unicode(array):
+        return array.tounicode()
