0da0d5adb5c6f13669999852eca9ba874e0e10ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:05:09 +0000

    consult nice guilty over treatment

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
