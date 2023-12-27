d88a11e0f11d0edff278e0331db2422141a06da7
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:14:53 +0000

    petrol damage sink tone quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
