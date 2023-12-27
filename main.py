0e3ef4851ec225a4e7d4057d70b91d0502f5de12
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:55:54 +0000

    disapprove attractive same little Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
