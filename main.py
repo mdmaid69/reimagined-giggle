f514cc81c72eabe4fd34beddcf24e9a99d9261ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:46:17 +0000

    improve practical war objection moral

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_area_rectangle(l, w):
+        return l * w
