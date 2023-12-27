984d684f74dd28bad48ed97fc17f3dff015caa63
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:52:58 +0000

    calm Uu mean swim finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
