027f3e65ee0bd1727f56cf32ba274da7e8443cd8
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:57:27 +0000

    journey glad seem whether practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
