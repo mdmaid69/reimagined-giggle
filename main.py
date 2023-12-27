5b06a02bc4577b6d554aad9494d1bc941c4db738
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:46:31 +0000

    expression none glass clearly they

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
