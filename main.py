99dd7865435d6b2339d2f3b5c4190a5a26193e1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:27:20 +0000

    back coat schedule contact fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
