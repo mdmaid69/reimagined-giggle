56822655bf1788dd6575b521394cda38c60f84bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:46:41 +0000

    capital break maximum fortunate pleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
