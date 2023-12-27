229a7a5a058182c0cd8c4be829c82a95cc267c81
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:16:23 +0000

    about ahead lucky smell over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
