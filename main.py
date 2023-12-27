33425ffd807f60ec6ebc58645d7c8ef4e501b71a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:04:09 +0000

    please offend blow odd Dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
