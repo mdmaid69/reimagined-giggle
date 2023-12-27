45e59ce9e55a29ea447aefa636a0ae29d734ac3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:31:48 +0000

    total friendly throughout tool poison

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def find_max(lst):
+        return max(lst)
