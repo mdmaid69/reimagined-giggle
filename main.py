35325af13a0f6102fa928d2589acd4d1e1eea3ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:46:08 +0000

    you drive catch report downstairs

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
