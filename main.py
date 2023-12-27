8b37f3244658f32ed8cab793a425f60217dc3b5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:14:16 +0000

    interview goup road calm large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
