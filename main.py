f53c27815ac41c9ff832208a4d801278e237d473
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:45:07 +0000

    nice wash smooth off reserve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-print("Hello, world!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
