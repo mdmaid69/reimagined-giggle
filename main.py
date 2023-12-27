2d51c4a38ab54adfe691564c1ebc1869346dbf12
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:42:25 +0000

    wall circle contents dress married

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
