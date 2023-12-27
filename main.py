997aec6a943057f95fdecd8d92c84c69953a37e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:57:47 +0000

    who risk spin tear male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
