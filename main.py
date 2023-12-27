4bc579bd061f5ac981be7404ac257e4e9b408869
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:21:00 +0000

    turnon mine visit flat calm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
