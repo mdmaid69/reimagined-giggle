a5bb563044893d083c5c35e24cd94300c8a9d0ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:02:23 +0000

    mass hard mix stop choose

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
