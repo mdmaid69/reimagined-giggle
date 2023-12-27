2c0228b521ca6e8b499c4e7123d6d6dda3d4397a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:47:27 +0000

    warning shout gap future pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_current_working_directory():
+        return os.getcwd()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
