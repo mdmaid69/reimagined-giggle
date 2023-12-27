503a7f3624730b58c026059e9be01fb75a93d7a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:15:51 +0000

    calm air equal intend failure

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
