3a4efe8e31df13287d5b49e2fa1b479c5c1b4da1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:28:05 +0000

    strange next something difficulty touch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_even(n):
+        return n % 2 == 0
