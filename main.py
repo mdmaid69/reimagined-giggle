68e15accc077963531d17a7f02fb2146eb082cca
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:46:52 +0000

    report step turnback responsible talkback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
