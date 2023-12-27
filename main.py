75e690772469b4515e5dd934206d58388d830911
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:54:42 +0000

    case deaf gradually shoulder their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_set(array):
-        return set(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
