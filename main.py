cbf2ded7776fd0dc61b3093012e17da292a742e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:36:30 +0000

    beat concentration hold before narrow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def cube_number(x):
+        return x**3
