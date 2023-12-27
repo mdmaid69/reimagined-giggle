b883f492fab43ddc6b15cbd19e116cb23e3abe6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:51:18 +0000

    mine account personal dinner meal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def cube_number(x):
+        return x**3
