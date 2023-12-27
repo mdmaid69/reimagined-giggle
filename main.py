fef9084d067189fbde0667ac186088bb878fe986
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:53:52 +0000

    getout brown possible block opportunity

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
