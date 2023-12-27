6b4ca6ac54b6ae04f71dd409d9323567cb3f2fec
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:43:29 +0000

    stop part force takeoff sensitive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
