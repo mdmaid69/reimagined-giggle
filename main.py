ecf35ca11ad69676248000deda62d3525895073f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:26:58 +0000

    pattern callat finish criticize sell

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area_rectangle(l, w):
+        return l * w
