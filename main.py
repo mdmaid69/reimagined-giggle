c85215f8bba6b76de9599dcfedd1e829937c428a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:23:48 +0000

    understand near complaint strong card

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
