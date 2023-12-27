2f0f0d0cdf65f841700ba14534e7e89a3f4691e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:03:36 +0000

    fill complicated drink comefrom full

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+print(math.pi)
