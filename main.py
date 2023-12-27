27dc3d7d11aa2acc05384e2bca73fb6fbd441d4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:11:41 +0000

    power agree opinion no feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
