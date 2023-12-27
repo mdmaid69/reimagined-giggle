e409d3e8b71db5defe7ba8d63b3c6e75cf03677c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:51:24 +0000

    surprise yourself return noone both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
