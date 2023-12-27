6e11f2c3c26ecd8570369cd06cffed99b5399eeb
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:29:08 +0000

    anyone wild freeze operate lock

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
