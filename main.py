72c4e38499522f77e4979f8b9c98d244e9cd168b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:46:34 +0000

    watch fast congratulations me escape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
