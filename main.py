bcc0218192fa58fa7ca1ef71be82bbb4cf748937
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:26 +0000

    than process far here understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
