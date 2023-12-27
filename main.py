2fd14b632a816a3873b8f368fe558a47812fc406
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:05:34 +0000

    drink joke mean towards whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
