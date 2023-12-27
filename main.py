ec678bed2cb3e9c07690e3de47967bd67141c692
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:33:28 +0000

    fire word friend look neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_str(array):
+        return str(array)
