bbf0d1345849f7a3db52eb137b4c78b1c0f4e28c
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:37:15 +0000

    food turnup swim general door

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
+  def cube_number(x):
+        return x**3
