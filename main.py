e6888bf57c3a871173eadd917d22f95f226ce3e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:15:22 +0000

    start 567enemy actually interested giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
