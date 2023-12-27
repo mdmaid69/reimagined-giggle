9b4ddc99b1dc14ac6e2f333f6787822bb476bb18
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:21:58 +0000

    criticism passage characteristic black so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
