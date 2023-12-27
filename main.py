fac0e333bbbaf6c517a552e443ac5331535c3731
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:07:50 +0000

    purpose dress no dangerous last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def remove_directory(path):
+        os.rmdir(path)
