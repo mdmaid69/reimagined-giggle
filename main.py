4e6d0ec3603738f5b19a7d09b3a857dfeefd459e
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:02:50 +0000

    Ee slip few term education

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_as_list(array):
-        return list(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
