4ec7e610080fe634cec94a4ed90974985b9fcb86
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:16:36 +0000

    issue question false later left

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import tensorflow as tf
-print(tf.__version__)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_int(array):
+        return int(array[0])
