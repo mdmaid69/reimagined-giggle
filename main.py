ad868c7584ce81025d591c440b359d6d822441cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:17:44 +0000

    description late during shoulder steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
