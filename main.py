44ac2f5e35f47ef5ffb27b78d4f40b2d10f30d90
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:40:30 +0000

    comeover hit sound callout meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
