ca78f6897a3cb89bba171bcda500864192cbf7df
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:16:20 +0000

    perhaps fact disagree pile command

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
