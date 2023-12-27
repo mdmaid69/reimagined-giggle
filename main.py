4c7f316089af99bc529c8a45cb9ad1b88faa9d11
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:37:51 +0000

    onto every then backwards otherwise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
