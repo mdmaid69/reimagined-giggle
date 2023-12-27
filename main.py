b84e4f90296f69942dc9858e4bce5d32470ebbf0
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:49:50 +0000

    skin serious study emphasize better

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+print(sum(range(10)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
