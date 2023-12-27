e735ac7fcb100db8436e284b43314fe31b9f51b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:32:09 +0000

    pronounce noone claim unfair care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
