ecf37aa73b59ed36ef18a154cdedbc3901d6b9a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:04:24 +0000

    hungry behaviour curious nervous around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
