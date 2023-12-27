b66e9bd12a171fe850045bfde54de40816c68578
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:29:55 +0000

    price bound think effort peace

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print("Hello, world!")
