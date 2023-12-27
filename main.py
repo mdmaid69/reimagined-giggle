8a71f193459d2350d790461837ffd47df9fd0469
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:37:47 +0000

    decision willing classroom excuse someone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def multiply_numbers(x, y):
+        return x * y
