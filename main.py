946474c1304723cd6cd7658668fe44fe12cf78db
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:53:04 +0000

    bear high paper near drawing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_directory_name(path):
+        return os.path.dirname(path)
