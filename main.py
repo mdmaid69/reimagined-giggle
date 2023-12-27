1fad0389ff7631c6b3f06c4ad52012d148bf6dbc
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:31:54 +0000

    expensive lower formal adjective rain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
