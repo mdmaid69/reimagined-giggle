9cc6910491b2a373195b3f7a32400b941d0b80b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:46:58 +0000

    respect true question effort bit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
