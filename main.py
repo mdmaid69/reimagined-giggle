a694a42c0b29ca9acae905db0897c6f58ef42730
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:10:49 +0000

    rarely easy up side damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def convert_array_to_list(array):
+        return array.tolist()
