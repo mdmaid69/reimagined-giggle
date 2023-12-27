70749a7f87dbd54599ea1a34194fa5261ee71977
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:09:38 +0000

    bus off stop law hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
