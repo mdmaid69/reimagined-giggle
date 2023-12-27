c38c56db312d3498a661f940632d09774d15100c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:50:35 +0000

    change uncomfortable moon ask view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
