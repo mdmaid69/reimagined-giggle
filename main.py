1920ed427189464ee9fb4f6d7089808bdf3d6132
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:43:44 +0000

    hurry gift ask arrival with

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+for i in range(10): print(i)
