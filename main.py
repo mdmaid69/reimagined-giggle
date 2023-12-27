5684b8473bc5af8d3062286d2b482a13f18003e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:41:20 +0000

    regional polite language again large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
