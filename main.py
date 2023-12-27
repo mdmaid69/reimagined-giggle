bf1ce62be2740c57d579f78379e482e80ace8bcf
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:04:54 +0000

    muscle oil regional finish relief

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import time
+def get_time_since_epoch():
+        return time.time()
