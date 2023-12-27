7f26ca51646ca46fdd9625efa19f038f0d1b5733
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:44:57 +0000

    Rr each rich open drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_string_from_array(array):
+        return array.tobytes()
