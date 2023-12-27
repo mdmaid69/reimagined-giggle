89b5961fc0c67c522d73c13ec9fd2dbc74e9183e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:09:17 +0000

    slice dry detail seat shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
