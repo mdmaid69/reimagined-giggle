cd411f8e9acde04528c849ff42908bd898ccc34a
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:58:49 +0000

    571enough bet stranger sticky own

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_as_set(array):
+        return set(array)
