2b17808c38a1b7f0d74a39aa5659813f4ab29b2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:32:47 +0000

    paper comeout wash bite waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_list(array):
+        return list(array)
