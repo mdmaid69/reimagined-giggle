febdaa947592ab6ac3519e65e7f151d430705bd0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:13:10 +0000

    it for success play other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
