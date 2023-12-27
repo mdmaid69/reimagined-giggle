8f22e795c2824521190deb7d5f19ebc0eb7bfd39
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:51:14 +0000

    sit middle recent basis band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
