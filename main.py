e646287207fd2c6b3d27cfb5ef71fda914009e56
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:28:54 +0000

    poison acceptable cry home foreign

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
