cecb0cb61340a9dd13b1e26ed7d45aa97d212c06
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:52:11 +0000

    related interview run interview thirsty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
