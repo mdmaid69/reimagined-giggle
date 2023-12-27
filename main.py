c4a0e0f828b88d277bea66836efaab74e0902e22
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:01:25 +0000

    pile freeze exactly evening forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
