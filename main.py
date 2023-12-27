6c514348a200cf3f3449b491e12e190f1ea8d00f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:07:44 +0000

    who display realize central use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
