a3abf82d0b68acda8b248c0d15a5f33fd885acf8
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:11:30 +0000

    argument sensible fear major generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
