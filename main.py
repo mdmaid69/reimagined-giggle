af5e47ebb0e6756f1347ce39944c2da13c1daf46
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:23:40 +0000

    flavour contain each for chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_itemsize(array):
+        return array.itemsize
