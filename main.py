63d88db1f834b7eb402129c0992632ea2f5dbef1
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:15:04 +0000

    shade container rank fight trick

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def sort_list(lst):
+        return sorted(lst)
