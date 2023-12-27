b0a51821e12f91f439b5dc2a659d823dc7f6e831
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:31:40 +0000

    task certain strong hate raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
