324f8f920dead5b4e11a54f6809cbc2cee38634c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:07:27 +0000

    trousers load instead machine here

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-for i in range(10): print(i)
+import array
+def get_array_as_set(array):
+        return set(array)
+import sys
+def exit_program():
+        sys.exit()
