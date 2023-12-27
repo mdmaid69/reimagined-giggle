834da4ee01bd3e75b864ea0f4f62b4be72eb2a5f
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:58:06 +0000

    limit herself confusing hide persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
