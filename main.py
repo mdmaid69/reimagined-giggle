31c920054b0e0b008cf8c0b8f9a9fade7695bc45
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:06:55 +0000

    flow concern single couple clear

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
