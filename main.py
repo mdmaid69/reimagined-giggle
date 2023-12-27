54ae8e9d11b69f1e11240be0219e393615d6fb8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:41:38 +0000

    meal sign into inquire month

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def is_even(n):
+        return n % 2 == 0
+  def convert_to_octal(n):
+        return oct(n)
