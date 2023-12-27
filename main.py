56f9f0318eae3d5f2a48971cf51a4a6f771fb6f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:40:37 +0000

    free bus thought tip far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def count_elements(lst):
-        return len(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
