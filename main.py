581938433e6cddb8769132c55623ba9bddf22f21
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:47:06 +0000

    spring us area confidence argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
