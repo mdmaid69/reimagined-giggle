12e05f3cd01112c15844174f898f3e1e8643c99a
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:47:47 +0000

    characteristic runaway shop fashion middle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+print(math.pi)
