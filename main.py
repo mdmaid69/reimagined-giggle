d06df7f1e8a56fecdecd2598d77fd203d3817de8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:43:57 +0000

    scream avoid stranger nonsense lucky

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def square_number(x):
-        return x**2
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
