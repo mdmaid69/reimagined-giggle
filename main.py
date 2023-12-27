6422843fccccdada9c70bd0719587a7d4bc22cd4
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:24:16 +0000

    example contribute roll secret hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-print(os.getcwd())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def clear_array(array):
+        array *= 0
