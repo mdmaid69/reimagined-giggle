2250bfa5667879991c703ce110bfdfe0cf41c20f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:50:19 +0000

    coat choose separate provide knock

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
