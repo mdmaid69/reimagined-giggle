c2dcc286b071961601b18e6fed705a6fb4aa71e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:57:42 +0000

    history accept goon but spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import sys
-def print_python_version():
-        return sys.version
+import collections
+def create_user_list():
+        return collections.UserList()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
