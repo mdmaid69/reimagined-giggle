20715fa79e6ada26b0efbf02853fc749e0ab7da2
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:01:40 +0000

    breakthrough needle union within carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
