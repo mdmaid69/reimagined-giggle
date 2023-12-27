188f275dfb8b11e95872a4bee6b010fa791490d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:34:18 +0000

    tend shut consult freedom concern

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
