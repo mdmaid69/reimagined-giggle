16f194b94f9414792a3b47b52a5272ac2cb86e75
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:25:42 +0000

    business workout justice fire keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
