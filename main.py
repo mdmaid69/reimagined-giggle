cf74de17700ce4fcf738dee570f08d52f08ddb94
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:55:15 +0000

    repeat sometimes rub clean feed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def convert_to_binary(n):
-        return bin(n)
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
