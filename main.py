8e79d10a3b1092f50067850c319129c5071474f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:08:04 +0000

    room left check professional test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-print(time.time())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
