95ee8433a11732d3c2c5e16110d46b98feccbc02
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:09:08 +0000

    influence discuss cum sad shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
