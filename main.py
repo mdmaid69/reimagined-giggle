db0300eaffe3f1605cf50309cdda30173d9d5089
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:26:12 +0000

    thirsty willing different band broad

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def add_numbers(x, y):
-        return x + y
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_floor(x):
+        return math.floor(x)
