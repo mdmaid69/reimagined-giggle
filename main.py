144c77222aeb507defd98d1ca1569762054a459b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:07:05 +0000

    lift bread tense crime emphasis

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
