81fa7797c385c020867873dce7207697370eb5f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:07:08 +0000

    upstairs example trick busy tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
