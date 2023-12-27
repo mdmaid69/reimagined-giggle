bb20c133099fc23966bff1f838a349b01580a464
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:48:21 +0000

    best standard Ee narrow evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
