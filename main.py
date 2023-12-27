dcbe5b89186bc97a75fa785ce2f616c9adbe6cfe
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:03:11 +0000

    gradually shelter improve pretty ocean

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def convert_to_octal(n):
-        return oct(n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
