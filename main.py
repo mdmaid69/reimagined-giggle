f34a180caebd4addd3559f3711e3154302a4b067
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:12:12 +0000

    issue between spot wind think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
