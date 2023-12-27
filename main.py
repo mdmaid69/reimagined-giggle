6f7e5a378f3f2eaec73b687840927fb99b4385c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:33:34 +0000

    bag face little rain everywhere

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
