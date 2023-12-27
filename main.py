dfdd8cc26143d547068153cd805b554679e4728e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:34:05 +0000

    female provide clever shut float

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
