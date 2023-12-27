06798a85f33573fa38e1f1cf6f69ad0d4a27ba5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:43:12 +0000

    science prove preparation ever fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print([x**2 for x in range(10)])
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_typecode(array):
+        return array.typecode
