83430d3674cf96429974c46f90afe1c6dab3e977
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:46:35 +0000

    after party name crash block

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
