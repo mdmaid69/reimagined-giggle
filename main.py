9d2fef2436c0cc8661f9a856d3560b8745fe1ed6
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:49:09 +0000

    car live pipe real error

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-print("Hello, world!")
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def sort_list(lst):
+        return sorted(lst)
