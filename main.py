bbaa2b62550910a23dc8ca1ca80516cf7f7227b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:00:06 +0000

    head recent squeeze same walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_set(array):
+        return set(array)
