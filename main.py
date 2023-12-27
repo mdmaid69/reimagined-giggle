e115671fb178e5a96c34dbedb751f01820f933df
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:00:58 +0000

    Zz only busy kind yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
