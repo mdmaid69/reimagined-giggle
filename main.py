ef0ecaf2e8d03c79538c327ba6c5acd2cd30cca8
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:07:20 +0000

    strike opinion national choose content

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def sort_list(lst):
+        return sorted(lst)
+  def subtract_numbers(x, y):
+        return x - y
