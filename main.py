8ee68bd3b6321946971cb8b482fd4b41801e591a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:15:26 +0000

    face escape another declaration goodbye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
