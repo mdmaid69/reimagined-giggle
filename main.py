d2423538170edb7808f6fb78a4b97a70753bb842
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:48:35 +0000

    intelligent sit approval level appeal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def reverse_array(array):
+        array.reverse()
