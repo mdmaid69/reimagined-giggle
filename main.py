b8665ce66a291adae2e51a048917ec58c7d1f6bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:49:02 +0000

    proof soon miss equipment pass

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
+def reverse_string(s):
+        return s[::-1]
