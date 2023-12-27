91d16c02719ea4042c1a50c58193a3841b602682
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:07:23 +0000

    sting warm attention get here

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def find_max(numbers):
+        return max(numbers)
+def divide_numbers(x, y):
+        return x / y
