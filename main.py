676d9c0b31ec7791c83abfc6056ce4e2cb52975c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:49:26 +0000

    win tonight escape narrow test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_palindrome(s):
+        return s == s[::-1]
+import json
+def convert_to_json(data):
+        return json.dumps(data)
