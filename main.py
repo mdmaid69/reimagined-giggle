1928cd4e0cc065de4e1072f106496cec3b2e2d7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:53:03 +0000

    takeoff no criticism report bank

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
