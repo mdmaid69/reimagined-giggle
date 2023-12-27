1c8534e2a0404e0cd9902ede0f894b1906aeaec9
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:20:52 +0000

    wander him mood Ii abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
