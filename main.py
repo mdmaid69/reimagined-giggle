9beffeea0c2322b3d2698d98192f4bd1d218a1a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:10:59 +0000

    tired male well gradually nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import getpass
+def get_username():
+        return getpass.getuser()
