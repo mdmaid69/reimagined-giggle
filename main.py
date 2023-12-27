bdaa9054fae477ec30349ee5d98092d91a1d495d
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:11:57 +0000

    trip vision someone scale reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
