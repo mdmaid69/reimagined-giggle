cd81cb5e6a28d7c97fec9fab0836d29f13495d66
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:51:32 +0000

    air passenger relative cheat certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
