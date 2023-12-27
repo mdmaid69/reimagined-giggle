a7ad68ce5c543a4fd34183b69b897ce7ffddb013
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:42:08 +0000

    objection government over muscle silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
