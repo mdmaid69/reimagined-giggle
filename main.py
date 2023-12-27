0db3a653851fabf7250591ca3467cccfb473e4c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:05:39 +0000

    question thought Ff file immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
