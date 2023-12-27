1f3df06d571ad4762380d4e8d10643dcca132933
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:17:59 +0000

    concerned destroy off instead meal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-for i in range(10): print(i)
+def find_min(lst):
+        return min(lst)
+  def convert_to_hex(n):
+        return hex(n)
