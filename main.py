b7e3c82ada46c34e82d231d1e7da5bb21f357dfc
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:14:48 +0000

    spray mistake build complaint block

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
