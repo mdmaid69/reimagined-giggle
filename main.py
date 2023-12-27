7df9377fda325dde1265cafddc48abf022a92f48
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:53:28 +0000

    free least last test convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
