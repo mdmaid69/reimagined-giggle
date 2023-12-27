1787527bbc87d36ec60cf89e3ce29e6a15155fba
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:07:36 +0000

    previous apparently relationship breakfast neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
