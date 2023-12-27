17266c573e9f68b79aba0150698c3192896b21a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:42:49 +0000

    die throughout free mood score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
