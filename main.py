5aad04244b3e7632654520a89608969c0000153c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:11:50 +0000

    kind our cost round giveout

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
