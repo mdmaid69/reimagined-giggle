03e3bdf6f0480c4498270adf3b472f903e9d21c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:37:43 +0000

    winter flat really breakdown name

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
