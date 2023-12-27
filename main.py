a09a8347237f962dc6e823325a71c3f530e0b31a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:53:51 +0000

    cheat quantity autumn plastic warning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
