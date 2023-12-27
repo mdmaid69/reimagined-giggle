46111acc2c0d5fbf1cb43fbab7572067ed0d0353
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:09:34 +0000

    bother fix touch determined towards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
