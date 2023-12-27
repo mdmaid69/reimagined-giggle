0e5899feb193bfdbbc273d47e5195e2b2ca856e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:28:09 +0000

    school my slight walk load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_set(array):
-        return set(array)
+def check_if_array_contains_item(array, item):
+        return item in array
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
