22487beda36f86eac13c4a26a3cdea8d8aa00e7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:03:28 +0000

    between confident short swallow full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
