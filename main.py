693fcb5f6352f05cbd4be762217715421813c608
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:11:18 +0000

    choice apply mark snow moral

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+for i in range(5):
+        print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
