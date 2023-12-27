23cdc2c22e7edc905428dce123c5849e7610fac9
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:35:32 +0000

    friendly maybe lucky crime brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def find_min(numbers):
+        return min(numbers)
