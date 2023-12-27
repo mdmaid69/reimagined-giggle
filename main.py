b183e1e7b2069b8ea7e67066bf9add22649161f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:45:00 +0000

    fun trick politics hide trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
