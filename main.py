e0ae5d3134db37b318c5fd4e0039a44e0690685a
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:16:49 +0000

    mind else now translate several

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
