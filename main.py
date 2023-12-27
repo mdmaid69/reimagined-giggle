871b29995c6e238ead5f1ec09e6a89e9decec12c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:25:24 +0000

    similar turninto language regret lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
