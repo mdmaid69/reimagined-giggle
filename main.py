1eaff708656ff157da9871b0ec2d9514fff01d5f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:53:15 +0000

    appeal print imagine doubt calmly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-print(time.time())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
