530e2d3b6132aa120c79d73fafd0fdee2580e37a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:02:06 +0000

    hold related talk bringback reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import os
+def remove_directory(path):
+        os.rmdir(path)
