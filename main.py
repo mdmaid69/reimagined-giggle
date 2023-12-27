ccfdd52f7310ee0dc570ca6c80d8c00cf80429be
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:04:03 +0000

    cope display right properly talk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
