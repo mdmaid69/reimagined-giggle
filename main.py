d7b601101ba7f0cb6d638a2a6dc1f573ceb36e5c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:40:35 +0000

    force on concentrate value world

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def add_numbers(a, b):
-        return a + b
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_acceleration(speed, time):
+        return speed / time
