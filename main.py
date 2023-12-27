f7286304e775d0f86c6ce7961b3446c25f36fdc2
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:52:32 +0000

    chase exam rest smile regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
