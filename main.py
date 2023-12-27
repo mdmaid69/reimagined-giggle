e7255e04b9035309a32fde5cbc6117565947a1da
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:21:49 +0000

    repair good instrument middle there

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
