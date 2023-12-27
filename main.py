bcda005c6b181084dcc33da2ff55ef925aec83c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:53:24 +0000

    block scream stop store call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_binary(n):
+        return bin(n)
