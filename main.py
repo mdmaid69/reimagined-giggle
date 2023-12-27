e7523869a4104f8c6eab16817f26cb09a6691ba4
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:20:25 +0000

    brave print meaning stupid down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
