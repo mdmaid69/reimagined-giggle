8c2943a44861fd9f1ab5b70c62dab5967ec381ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:57:26 +0000

    cheek appear thinkover fill with

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
