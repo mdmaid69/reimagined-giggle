3c6d8f3db4819f9627e284531d53e7e1e2040c67
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:03:58 +0000

    occasion such trick foot through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
