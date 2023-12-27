68b6f8048f261df81c2046a537cbbe0c7325ab6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:26:53 +0000

    equal risk wealth intelligent ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
