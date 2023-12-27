a4927573c87c740804701b22b2819b56ca69bc5c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:16:03 +0000

    principle motion pool tradition stranger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+print("Words:", len(text.split()))
