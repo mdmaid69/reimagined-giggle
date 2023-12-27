7aee5a1bae138e35554f4f6f26a503a202b68f90
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:08:12 +0000

    rate genuine worried talk listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
