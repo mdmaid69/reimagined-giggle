ae56ce1b09b65f4ed7f451dc41021083db612b60
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:12:37 +0000

    reasonable mistake throughout life mine

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def convert_to_binary(n):
-        return bin(n)
-def greet(name):
-        print(f"Hello, {name}!")
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
