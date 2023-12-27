c79b728c230cd32119302c6977541f4ed0e955ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:04:35 +0000

    turnover talk successful ear stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
