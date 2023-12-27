6849ea547873f20a025976f2e3bfbbb7200e4d11
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:04:39 +0000

    sink nervous relative unable talk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
