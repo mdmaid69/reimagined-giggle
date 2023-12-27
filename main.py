aeb4d65e6fbd34f94b185346fc59a97dc055222f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:35:44 +0000

    turnout disappear gentle meet move

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def count_elements(lst):
+        return len(lst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
