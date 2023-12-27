8ff5bd8b31b1a3d8bcf8897287e1113ecd2e2ca9
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:56:16 +0000

    mention conversation treat foot correct

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
