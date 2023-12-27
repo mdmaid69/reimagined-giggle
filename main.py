6fcd40a0824290bb5c8799c55240dbf9c2c9dbac
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:40:06 +0000

    entrance proper range quit suppose

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
