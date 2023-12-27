de4537cb62faae4e5fdfc8b03a4c22caf5eea483
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:09:15 +0000

    woman marry put show example

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def count_elements(lst):
+        return len(lst)
