33bf3897c203978a46924fb58fe9f9399252683e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:19:39 +0000

    bite gift opposite consider pour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
