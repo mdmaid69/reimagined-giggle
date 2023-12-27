51cbfd6bbc25bb84f3f1b8db6945009c9f0da709
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:19:45 +0000

    original guard promise dinner duty

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
