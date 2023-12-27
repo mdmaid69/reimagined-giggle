35521bf985a10b0584532e74c2de2b2c507d2f83
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:05:38 +0000

    pattern 573entertainment thinkthrough opposite disturbing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
