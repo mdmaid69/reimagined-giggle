223229be504b0ce50b058a159b96bde70d7448df
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:13:07 +0000

    opposite wall turnup rob turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
