61c031c3346dde9546170e7166acbc0f75c7bd84
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:16:19 +0000

    squeeze border dress win steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
