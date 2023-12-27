f93e1564296ba9a4747f93026b074e9caf81ad70
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:34:15 +0000

    help appeal rough brown may

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
