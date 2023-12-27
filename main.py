c04fec96af782f3cf3aa5b3173a83f9f748ac128
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:41:51 +0000

    runaway boil bound offer mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
