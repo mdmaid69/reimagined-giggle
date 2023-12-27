c58c9a286b2af25b1aada7d2031eaab427f63ed1
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:36:09 +0000

    arm wave blow row about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
