248e7518b0422e1c8a7b0a5acd73670c2dc5afe0
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:29:54 +0000

    language might scream getaway manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
