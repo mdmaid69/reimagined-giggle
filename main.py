ae980945fd779f4f3f19c30f080352b319b72217
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:14:46 +0000

    finger rank suddenly violence fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
