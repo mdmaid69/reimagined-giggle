b90c6dc57eab28fd8f12ab3c037e0d34e9dfadd0
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:14:45 +0000

    rarely mystery proud check maintain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
