ee5d67b409cd4ed39c0545471ece9d5e37532be9
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:51:16 +0000

    us pleasant always trouble comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
