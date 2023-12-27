abc654ea4e70806b0a1991e503e546d96f74f5dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:45:24 +0000

    close shut level difficulty colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cosine(x):
+        return math.cos(x)
