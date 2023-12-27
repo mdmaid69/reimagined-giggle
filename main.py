6d5ce392bf7781775d8332c9140714df8f79dd57
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:37:44 +0000

    juice dance score certainly sad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
