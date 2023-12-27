342b35ff79199116392c2f4ddc9fb533d20e8bdc
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:19:50 +0000

    admire second keep side aim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
