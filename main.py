bcfe649f963beefcc1f23096b2f5c6f748b93df6
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:32:48 +0000

    sad now shopping cheap name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
