3ff90f665dc859e1438a9203a633308ea68386da
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:42:12 +0000

    wish injury fierce do combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def add_numbers(x, y):
-        return x + y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
