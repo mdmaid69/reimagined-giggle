224073cd636c0edf94e835d482cec6fb31f18a08
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:48:01 +0000

    company tap worse breakinto opponent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
