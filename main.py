c1f2f66f9cd282751740a7f67292777dad1380ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:58:17 +0000

    sister town paper drawing notice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
