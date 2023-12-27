c8239bf7611c809e203b1d57ec5a7888a8dca479
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:27:54 +0000

    guess people suit holdout so

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def square_number(x):
-        return x**2
+def calculate_area_circle(r):
+        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
