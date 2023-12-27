edc42966cc5361a97277660c7bf08c5e78e847b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:55:13 +0000

    mine lose carry aware sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_working_directory():
+        return os.getcwd()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
