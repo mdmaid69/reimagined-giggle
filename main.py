2727891ce3535e92c1ed5cbe883a66a0a0660918
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:53:22 +0000

    Dd attach tone seem intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
