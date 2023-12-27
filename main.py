704a87e45e1869e4be0ad333a1c64a6186d80845
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:25:31 +0000

    backwards share again attractive autumn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
