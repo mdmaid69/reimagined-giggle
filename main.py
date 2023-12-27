0eb242e56d149fce6d3e6c0011e607dc8252ff9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:32:32 +0000

    never near might contents land

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+name = "Python"
+print("Hello,", name)
+  def square_number(x):
+        return x**2
