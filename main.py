bafa7673d867063ddfd7752b4b30d5287d9ed2ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:35:27 +0000

    want often gift day skin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
   import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
