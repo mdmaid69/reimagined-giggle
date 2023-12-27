c8c54c6f9f4a3bbdda7943c7d8af040bc10cea76
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:49:29 +0000

    respect lookup know health pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
