a32e352c67df0c4aa2df5afa564ce8cbdfebb25d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:37:53 +0000

    return makeup present understand comeback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
