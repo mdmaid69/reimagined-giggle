42f616a44378247bc809ea61ab5890b2ea5e6fda
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:36:42 +0000

    bush depend taste divide sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_os_info():
+        return platform.uname()
