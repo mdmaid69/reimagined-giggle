a64386551670179d403f2a0e6670695e1b6d22f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:36:24 +0000

    suckme anywhere spot excited climb

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
