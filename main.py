13badeaf7752f1fe5395f222e52e877df1140145
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:02:11 +0000

    hungry otherwise mix wide under

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def convert_to_hex(n):
+        return hex(n)
