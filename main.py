a3680af1f2cbdec74dee2aa21889fa34db61c5dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:23:56 +0000

    watch uponon forever none exercise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+def get_current_working_directory():
+        return os.getcwd()
