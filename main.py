1a0f18d36eb6e322fa956954c60e9b80980f3045
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:21:33 +0000

    bunch getup squeeze public noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
