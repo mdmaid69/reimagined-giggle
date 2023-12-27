737efcf8a537600dfd369b47e8ec65eff02ced67
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:59:29 +0000

    probably goup private back present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
