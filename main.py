2e738111a879b05efe7c2fedf88375a17240d5e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:36 +0000

    welcome lay programme breath sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
