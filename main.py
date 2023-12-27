2c8f2edc41fa4102983135b8df6eeff963c77471
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:43:48 +0000

    discuss further justice early fail

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+print(re.match("h.*o", "hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
