3d40a33d871f697b65926da5c5117604001569d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:20:10 +0000

    bit children complaint movement getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
