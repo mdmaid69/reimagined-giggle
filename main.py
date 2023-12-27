adc90e001b3def67782b70f26cade15cd545a7ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:55:42 +0000

    settle hole cope ilovedicks lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
