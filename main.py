823ce5722eb69682aa7ca3d77c613e6c12191ff0
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:38:49 +0000

    collect weak damp bored tight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_elements(lst):
+        return len(lst)
