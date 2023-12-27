e7d7f217feefa900a579d77d8c15efeac7237ffc
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:01:45 +0000

    wind ill separate tense situation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
