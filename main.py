91acbc4818cace2426ba3f5db2ce4087233cc8d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:43:30 +0000

    software floor doubt fasten be

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
