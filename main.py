031ce078d39a547537257c4226bfad1c480195ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:51:37 +0000

    size strict lead uniform himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
