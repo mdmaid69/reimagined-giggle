1495ce1ca7000ac392a2f24d84372a1c511dd005
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:37:29 +0000

    flow capital cough small except

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-name = "Python"
-print("Hello,", name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
