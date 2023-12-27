2130a90afea15cf8c7c2bf561c6494f89bbc31ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:58:59 +0000

    threaten floor guilty right time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
