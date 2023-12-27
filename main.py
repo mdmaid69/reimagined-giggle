24559f337e0f61fa466c2755db81d5c85c45b900
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:54:34 +0000

    detail sale takeaway business quarter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
