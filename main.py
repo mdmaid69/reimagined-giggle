f3b9ef8eeb96d8eb72ae02db0a996859026db4a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:15:11 +0000

    hate week high Oo cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
