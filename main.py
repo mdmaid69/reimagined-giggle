9be098e071d115408aace0f7f46ed2f9e7cae7e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:40:24 +0000

    false somewhere phone smart Dd

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
