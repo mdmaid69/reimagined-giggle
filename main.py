cd1e4c697d458485072a157679dd3cd337174035
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:22:10 +0000

    main regional backwards table other

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import tensorflow as tf
+print(tf.__version__)
