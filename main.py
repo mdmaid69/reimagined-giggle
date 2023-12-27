ccf0fd4eb49375af1f9b9090ce43485029a77933
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:30:43 +0000

    grow without tired environment information

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
