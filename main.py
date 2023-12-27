c53a5b4003d64ecbac7ccde1743cc9b38b5ae32e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:46:39 +0000

    club correction related please naturally

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_str(array):
+        return str(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
