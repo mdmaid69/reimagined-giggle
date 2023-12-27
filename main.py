d3d3f6df63c377075a5e2ab9b943d19ad0887670
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:12 +0000

    turn leave thorough failure himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
