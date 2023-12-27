fbceff73b618c024e0b9fec996f4e8cb1426e1e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:31:02 +0000

    shape in cheek murder distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
