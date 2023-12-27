e98275b59505a501a0aefb14768572e343d10722
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:11:52 +0000

    grow godown while live exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def sort_list(lst):
+        return sorted(lst)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
