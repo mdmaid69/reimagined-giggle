55d12f63488e34ff41cd1b035e07a3e819c25e57
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:10:14 +0000

    second opponent record disappear firm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
