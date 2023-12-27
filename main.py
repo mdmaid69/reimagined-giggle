65df6b2c223cf207612946d8a3e99192a3f5c0f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:20:21 +0000

    return pause comein both deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_acceleration(speed, time):
+        return speed / time
