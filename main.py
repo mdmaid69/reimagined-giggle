23b39fefc8f7cbc8f46e7b2f89071968e2cb4eb2
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:13:59 +0000

    perhaps familiar far scream sticky

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
