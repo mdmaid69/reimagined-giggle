2807a8257203067ce9e3c83b6b0e5cb9ffbaf3f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:19:02 +0000

    whisper success future bury tough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
