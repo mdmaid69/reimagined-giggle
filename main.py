096b16f4116ebe908872e795aba4bbeeb44b99c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:28:04 +0000

    fair guess perfect operation excellent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
