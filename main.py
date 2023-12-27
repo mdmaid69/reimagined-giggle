2dbeffa06bf2cd3366c95c14abca14e204f74e21
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:45:30 +0000

    impressed late reliable pleasure wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
