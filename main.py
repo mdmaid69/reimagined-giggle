3fc84a1ca6e8cd92fe3edc50199ad8cb692369ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:12:33 +0000

    have stupid cost naturally shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
