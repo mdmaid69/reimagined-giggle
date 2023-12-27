d1f1f94b5c9b73adfbd2f2c97f42e0c079f6c18f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:36 +0000

    accommodation activity length comeoff tip

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
