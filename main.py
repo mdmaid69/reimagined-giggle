22b66f7ed3c36fc1382f620e051bbe1fe0a5ba9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:52:36 +0000

    reduction fail charge comfort window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_area_rectangle(l, w):
+        return l * w
