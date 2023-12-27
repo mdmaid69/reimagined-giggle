eca0a0b6eb3bee44860ce7c04ca7454515053c86
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:44:08 +0000

    negative country positive afford more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
 import json
 def convert_to_json(data):
         return json.dumps(data)
+def count_characters(sentence):
+        return len(sentence)
