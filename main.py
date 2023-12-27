7c0fcb4d465e146b816a735984cbc1828abbd035
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:36:02 +0000

    likely business tidy mystery basic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
