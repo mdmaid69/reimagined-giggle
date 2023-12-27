d7e95541011b7e2437232724dc536d1e293dd061
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:28:11 +0000

    regular independent while attempt breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_item_count(array, item):
+        return array.count(item)
