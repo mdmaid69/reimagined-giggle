13e9203bb4e5618d1eb3de08dfd4ce1b1d16d4f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:32:55 +0000

    relative welcome my track crowd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
