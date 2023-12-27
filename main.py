fc9d3fe88474636d3bc465605c4d3fcb028c0f38
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:18:48 +0000

    achieve treat first careless nervous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
