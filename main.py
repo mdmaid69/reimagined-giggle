9448349024f5da1e9ca7ccd49788efd7ccbd397a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:36:00 +0000

    shoot walk funny neither lower

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
