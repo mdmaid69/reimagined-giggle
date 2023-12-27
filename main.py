3473e45e25bf61378d5270c34598a576bc86bb28
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:14:28 +0000

    keepfrom run bang expression damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
