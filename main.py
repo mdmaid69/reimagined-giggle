bdf0ba29c9f6e3883effc7cc1b38671b66a87cfd
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:03:51 +0000

    live lost everything thick calm

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
