46356252b1746784f4bf1027dd563ba60fa2e339
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:37:16 +0000

    hollow much slight joke Hh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_bytearray(array):
+        return bytearray(array)
