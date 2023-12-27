eecfcbaa87c15a699f4355faf7765c7e730bb7c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:37:25 +0000

    best finally cover safe comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
