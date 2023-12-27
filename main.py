405af342fca33866616997bc4e7718d153e4e4f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:19:06 +0000

    dead come least translate typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import time
-print(time.time())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
