88024264161834ea8931003506b7bc753908cef3
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:53:04 +0000

    influence Jj tonight book finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_min(numbers):
+        return min(numbers)
