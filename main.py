7e6f8973db146a6bec329c510a7a12df93febcaf
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:34:23 +0000

    reach copy bringforward active mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
