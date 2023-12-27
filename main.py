5304c6a5a745069abb232a6ef18fbec065fa5090
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:16:05 +0000

    564encourage sport Ll preparation argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
