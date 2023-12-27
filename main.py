011a7dc73291e430c2ee9c4b3b7b0c8b9de1f38b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:25:46 +0000

    once but frequent view lot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def is_even(n):
+        return n % 2 == 0
