c444854abf4ace57ccc6f8b16160d04f90ef398c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:27:49 +0000

    please people tidy understand possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
