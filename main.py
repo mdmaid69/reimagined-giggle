c8e5a5b5c5e46a8b0091de625246475739a74669
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:03:20 +0000

    half connected ability unpleasant report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_priority_queue():
+        return collections.deque()
