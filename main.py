2ba02095ff8dc8350e3212a4a062edc27d1c7c34
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:00:22 +0000

    chief advise mystery continue later

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_binary(n):
+        return bin(n)
