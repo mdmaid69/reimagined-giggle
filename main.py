3af36275b7839163027bed4835e5ef2a77bbcb47
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:36:36 +0000

    fine precise touch plenty invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
