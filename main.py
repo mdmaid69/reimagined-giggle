d39cbcbe0c76344a7128fe4e3e3dadaca6708424
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:07:14 +0000

    sea rough love section distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
