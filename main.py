d44ef486253fdf1ebfd7f532c5f2e886a3c826e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:31:09 +0000

    switch concerned comfort for morning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_max(lst):
+        return max(lst)
+  def square_number(x):
+        return x**2
