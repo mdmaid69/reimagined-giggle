f78d80962433ae4914a2a006a0424a5d77fbe54e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:38:45 +0000

    light move maximum off loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
