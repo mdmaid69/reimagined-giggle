df5ea1b5f5a5b0450895744206cc6db0ba10a394
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:13:42 +0000

    makeinto touch underneath embarrassing goon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
