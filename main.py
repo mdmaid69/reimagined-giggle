556f3bede2062709b7a81907f245ec732cb539ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:07:10 +0000

    freedom increase fire pause prize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_as_set(array):
+        return set(array)
