bb0fb5d5207922d9904dc2f562690351a60b0c82
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:40:20 +0000

    close difficulty flow send part

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
