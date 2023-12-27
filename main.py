03856fb4bcb4b10fae73cbc74557a59923d5ddf0
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:26:22 +0000

    relationship busy below Ww rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
