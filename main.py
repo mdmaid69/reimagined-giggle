e51ed226de099d64fd0e3052e0634a9187036e29
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:05:51 +0000

    official someone name play somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
