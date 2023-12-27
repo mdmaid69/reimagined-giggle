719897db622220c6bfa4edab5af7e388d4bf361d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:30:45 +0000

    lose their worse accidental organize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
