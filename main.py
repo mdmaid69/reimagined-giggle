f808e5a36f4ab479fcd37b7727580aaf9291b365
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:51:23 +0000

    stop turnback miss forgive land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
