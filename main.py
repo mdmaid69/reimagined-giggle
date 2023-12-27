966a3d7ea5ec50b621d27654ceb481f1eb491a99
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:04:38 +0000

    cap slice thankyou increase game

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def split_path(path):
+        return os.path.split(path)
