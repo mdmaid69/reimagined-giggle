2359df3ec221ae02787b14e25097d25dea7632f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:54:06 +0000

    swimming wrap clear fork mark

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import sys
+def exit_program():
+        sys.exit()
