8ab7e2e4878cc9827ed337fdfdb0473550fb4b80
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:53:34 +0000

    fierce action smooth regard the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
