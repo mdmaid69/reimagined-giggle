70e8fb32e5e8b231f2bcfba5f97949ff26a819a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:50:24 +0000

    belong means fire government like

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
