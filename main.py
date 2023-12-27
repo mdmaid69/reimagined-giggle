2490792853f5103751de63a1548c7f2eb5e1eb15
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:57:35 +0000

    tall holdout difference partner otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
