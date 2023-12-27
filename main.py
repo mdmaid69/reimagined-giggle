39e9f304d005a8c702e1a4671c6d2a8a39dfe757
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:48:23 +0000

    push fair detail language check

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def remove_duplicates(lst):
+        return list(set(lst))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
