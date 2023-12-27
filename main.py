e36311ef60b27f5af82fde3be14f797f84bed050
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:18:33 +0000

    comfortable doubt report silent cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
