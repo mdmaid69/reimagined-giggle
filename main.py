291a9a92f85f8f8a39a45d72932706ba6ca0de8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:19:43 +0000

    message add present mystery share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
