b95efba12346b51c60589eb58f6ef02dd5dfc3a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:16:20 +0000

    air permission announce next search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_item(array, i):
-        return array[i]
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def find_max(numbers):
+        return max(numbers)
