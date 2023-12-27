f53480835eb299fefeabbf5a5e41365fda8accd1
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:58:33 +0000

    nigga free minimum film best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
