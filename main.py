a1e4583543e5be8a68e0772193878387cb51a88c
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:52:59 +0000

    confused excuse board minority needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
 import array
-def reverse_array(array):
-        array.reverse()
+def remove_from_array(array, item):
+        array.remove(item)
