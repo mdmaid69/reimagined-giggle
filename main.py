fbae5c0b73197e903bf092375f2369cbcc1d47c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:00:38 +0000

    it community clever ashamed big

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
