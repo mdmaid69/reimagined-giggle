9d621c807bd41dedbb5e6827f2d85cb26aa6cea8
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:14:01 +0000

    platform set freedom secret glass

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+print(time.time())
