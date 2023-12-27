ae67d81b7bc7294ea80933a854d533e4d2957a3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:31:14 +0000

    deaf before order train up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
