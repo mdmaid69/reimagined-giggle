8762094686f870c54d0f198c0cd416a0cef9e815
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:43:59 +0000

    could best dry cope forward

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
