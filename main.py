befa9ab037d1f328a40b81e0cb394d3fb6c97b9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:25:53 +0000

    branch once Gg change but

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
