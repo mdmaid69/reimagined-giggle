e958b555635dc581e3a6a1e70c126977342d4a36
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:01:56 +0000

    escape declaration difficulty tough willing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
