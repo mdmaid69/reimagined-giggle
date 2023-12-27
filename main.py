4d2578358dcbd8f68d91d31621a9417648ccde66
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:19:08 +0000

    remain polite calm young skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
