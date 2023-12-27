5b89998fcbca157219f7e9a49828bbc44c048acf
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:07:26 +0000

    comeover insist question appeal interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
