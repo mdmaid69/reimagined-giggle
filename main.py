d4b0f3eb3b789b96bebc66062bbf1a5460f86b63
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:17:58 +0000

    upwards justice long bringback make

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def calculate_work(force, distance):
+        return force * distance
