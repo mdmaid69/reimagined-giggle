07f074f72ec01e4559080d663024dfcec5558a50
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:11:13 +0000

    ask blame forget really goup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
