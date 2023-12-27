205d89d9284ef464f87a0335e7c2c094c13ac14a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:33:02 +0000

    must equipment both fill around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
