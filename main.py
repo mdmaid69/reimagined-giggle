0afa0b7c174d6f45c3aba07cc97c6a22ba3e6598
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:45:58 +0000

    public but arrange pupil earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
