ff2761230923932ac3b06b2dec938abd9e8f24b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:23:31 +0000

    finish drawing material swallow cause

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_typecode(array):
+        return array.typecode
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
