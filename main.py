7681cb46e7c54842951baaafcbbb83e415cf72db
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:28:18 +0000

    within twist pen bite load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import random
+def roll_die():
+        return random.randint(1, 6)
