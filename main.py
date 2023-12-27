5f70d2b19dbaff3eea60b39ef153a9bbc2048afe
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:50:29 +0000

    small trousers importance deal goup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_min(numbers):
-        return min(numbers)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
