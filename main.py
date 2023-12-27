4d268fd328f72a588ce4954a7dfa951b3a1eb11a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:37:47 +0000

    mail mine clever forward adjective

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-print(os.getcwd())
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
