8e56dbe2e0982d8de7c7341a7f21a8b296472e07
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:24:03 +0000

    criticism taste steam boring style

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
