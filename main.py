ff4c723083c45b302e539dc637e953871b1fe2f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:55:33 +0000

    beyond prepare sentence gas she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
