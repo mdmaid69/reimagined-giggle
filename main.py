fa1d84ba48245c6328ff477bc42e450862e516d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:52:43 +0000

    post takeback too eat giveback

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
