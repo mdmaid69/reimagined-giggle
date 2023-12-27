798b3833be5d671a8492a59aa26f2e65ecfec10e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:00:22 +0000

    be sign develop wipe search

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import os
+print(os.getcwd())
+def calculate_acceleration(speed, time):
+        return speed / time
