726908950812266706fdf9bf71457016ce6bf8c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:54:23 +0000

    reserve down height careless plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def reverse_array(array):
+        array.reverse()
