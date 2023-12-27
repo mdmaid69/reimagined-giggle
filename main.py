52fac22ed0cda5fac9577c3dd333a84d1058e154
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:50:19 +0000

    situation contrast offer drink separate

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import time
-print(time.time())
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+print(sum(range(10)))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
