e35b46a47e3fc314ecf884bfe29ac9b144d709a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:28:20 +0000

    entrance drop say label window

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
