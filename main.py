d22664f03b13748d53b60385a066e17d25b3d2a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:38:00 +0000

    search soil unlucky spring far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
