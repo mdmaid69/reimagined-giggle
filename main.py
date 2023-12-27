d9ed6ec1d2fa9b6b0d1715b00c1508aad48c38cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:27:32 +0000

    tend that behave available goon

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
