b9db07312a694723e49067549c63bb8e937a530f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:02:38 +0000

    wave half adjective otherwise emphasis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
