046da693781486adc148d128bc2b3a24e6ededbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:56:05 +0000

    thirsty period substance trust fortunate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import platform
-def get_python_version():
-        return platform.python_version()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
