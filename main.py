17d902dfb84e56c05aee0233b889db5cf35e36e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:47:52 +0000

    satisfied physical sticky prevent like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import sys
-  def get_python_version():
-        return sys.version
+import collections
+def create_stack():
+        return collections.deque()
+  def remove_duplicates(lst):
+        return list(set(lst))
