d0e3c4e20fd5505279e52547d1ee79280d4e1b8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:36:45 +0000

    brush Uu follow beat themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
