b2155e67533c24a6f2031e9796b822e88e6752cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:08:35 +0000

    land wheel tongue fierce apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
