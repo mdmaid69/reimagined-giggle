f2fd5f314c1fc3f3444f2e72320f279e0a0e57a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:11:53 +0000

    zero criticism wind side sick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def find_min(numbers):
-        return min(numbers)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
