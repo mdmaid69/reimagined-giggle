7e45dfac4616db60aedd7747c17d1dbe029935f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:35:54 +0000

    stamp shop thin waste fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_elements(lst):
-        return len(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_typecode(array):
+        return array.typecode
