d7b993148e8455d64a1b03d51760f91296366f38
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:28:44 +0000

    will judge welcome shop let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import sys
-def exit_program():
-        sys.exit()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_current_directory():
+        return os.getcwd()
