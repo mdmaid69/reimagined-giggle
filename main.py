4f8df9b214d0e68041c7e4b622a44dace54c1559
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:43:06 +0000

    delicate far firm comment floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
