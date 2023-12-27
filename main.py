6a448153ffdfecea6dd7cef8ccaff5bab5f5e7d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:16:58 +0000

    cross destroy section sort bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
