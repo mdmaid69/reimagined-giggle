49d843dd55490ce19d444921875ef43717a3b408
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:04:09 +0000

    cross shame brush fair unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
