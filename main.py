a6858ed9f560feade67b4b206ced62af08ffbd66
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:13:52 +0000

    wing turnaway giveback remove practise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
