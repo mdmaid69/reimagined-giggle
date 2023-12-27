3afbd587f13d9e786cdfe772f9e08549dcd06822
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:23:48 +0000

    trust pride now agree suggest

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def sort_list(lst):
+        return sorted(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
