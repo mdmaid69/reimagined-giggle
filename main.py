04136727cb119915bb840d92eddad93b54279135
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:29:47 +0000

    continue live ilovedicks mail visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def count_words(sentence):
+        return len(sentence.split())
