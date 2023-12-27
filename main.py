e31c9bd5b1ce2b950a6214384ebd03a53d7a34cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:50:07 +0000

    reasonable nowhere receive objection temperature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
