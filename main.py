edd3dad3c02e7759c841f9d519b8b83081abec0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:17:01 +0000

    without to answer detail thankyou

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
