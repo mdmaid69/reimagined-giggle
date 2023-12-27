a032c0daad0a0b6766386ba2adf82b99b396c231
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:06:14 +0000

    bet act familiar union information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
