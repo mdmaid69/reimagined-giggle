c6d97286e814c1106e93d64a75ea02a553415c95
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:00:56 +0000

    often present cap disturbing treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+print(os.getcwd())
