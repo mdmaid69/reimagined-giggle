bc6615990e2872e3fb4f35a90c0aae2ec972fa24
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:39:36 +0000

    steer attitude noone by wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
