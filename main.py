bbb022e445a2fdefcd960d4601d585f02e3c76b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:43:25 +0000

    light giveaway beautiful disappointed tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
