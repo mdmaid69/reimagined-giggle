3e7ee5f75330b8129157c45cd6802728c8515069
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:47:06 +0000

    matter train bus major getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def reverse_array(array):
+        array.reverse()
