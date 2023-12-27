40ce3303876d426c3e48d0bce1061f7ac93fd14c
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:50:33 +0000

    front become comeoff place refer

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def get_array_as_bytearray(array):
+        return bytearray(array)
