2949a2eb24fc6facd76054eec85307bed22bfb2a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:09:50 +0000

    impression well thin press fit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_power(work, time):
+        return work / time
