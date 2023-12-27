3adba48348ce2909c7a97569dd3533bac662f94a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:54:52 +0000

    in giveback standard find exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
