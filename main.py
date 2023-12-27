ff2f0d651c8d60ba8f76c2163d7f31af12df15b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:01 +0000

    leg comeover me professional we

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
