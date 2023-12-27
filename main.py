03fafdce51a212960d1b0184536deaacd9afbc2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:21:37 +0000

    female substance afford programme degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
