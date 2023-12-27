eed17ed09598c2654aacf2d352523645eb89aefc
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:51:49 +0000

    famous once face surprised deep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-print(os.getcwd())
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
