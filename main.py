ceab0db76872a37bdfe16f1a6101faf2cf929f27
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:09:53 +0000

    trust purpose comeup properly far

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
