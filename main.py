f9b1cd87999f838db6d61cf7b8dd5641e3ff1b1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:38:03 +0000

    doubt modern knowledge runinto secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
