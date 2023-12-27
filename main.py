61f3bddef0a356988f7d7b3d1d9125f8d717d692
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:01:32 +0000

    worst university up fail like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
