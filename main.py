6d5789ba5da57a5c417fc96a1fbc9164e029080d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:26:01 +0000

    knock already past home wait

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def find_max(lst):
+        return max(lst)
