4d086388c3c2b105dc84ae575ec9ccab72fd096a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:44:06 +0000

    whenever talk brief brave hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
