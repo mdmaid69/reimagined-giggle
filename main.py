bcda27fd17721f5ce6d94fbb00dfac5b6d9f9203
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:01:44 +0000

    feeling takeoff write relation far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
