0df8e76205f51c480a86299699825d3975d9355a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:46:05 +0000

    peaceful always offer odd colour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
