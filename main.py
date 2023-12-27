ffe0405551e630b4e5636d7fdb24b84275a76743
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:24:56 +0000

    suck mistake bag Pp love

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def is_even(n):
+        return n % 2 == 0
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
