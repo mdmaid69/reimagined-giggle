28185587124e78296209db34368acaeea2bb6017
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:37:05 +0000

    cook pupil as during thought

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def add_numbers(a, b):
-        return a + b
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
