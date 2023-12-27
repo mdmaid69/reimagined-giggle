66d8e018dbd85be0ed89fe0ad403f1e2860f2044
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:03:17 +0000

    post watch worry transport press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
