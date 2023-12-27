1a56eb9f07f350e39628e913becde77f4ac90592
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:00:20 +0000

    reliable makeinto tie production slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
