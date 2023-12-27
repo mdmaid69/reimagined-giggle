e55a3705998a12357676b9204affb473280990c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:04:11 +0000

    method Vv fast excellent car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
