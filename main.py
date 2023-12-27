0809da0e0156fd0d3257ec2d5e497f1d5d9bdcc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:37:24 +0000

    force used at could dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
