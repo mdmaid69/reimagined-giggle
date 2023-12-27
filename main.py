c1588f0736dfeb59a8e49dcad94933ec10f1967e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:44:33 +0000

    wide frequent Oo relax possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
