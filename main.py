e4068e3a5baac593699061848499658c8a628ab9
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:45:10 +0000

    treat satisfactory die keepout interfere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
