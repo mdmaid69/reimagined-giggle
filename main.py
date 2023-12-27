b0a18f8a2159b0799e8ceaa1f50567d454b9f363
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:45:46 +0000

    see land worried friend fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
