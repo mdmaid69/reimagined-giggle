11d1db9f58c733bee59e4b8b66329acd383c2026
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:33:11 +0000

    work scared pull march communication

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def convert_to_octal(n):
+        return oct(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
