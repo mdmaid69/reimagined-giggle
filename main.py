3cc6bdc9eeb1d454b0ea1d025015a70001292d84
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:35:44 +0000

    successful receive address autumn treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+x = 10
+y = 20
+print("Sum:", x + y)
