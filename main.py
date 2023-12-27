3ade1cac3dd9d28b49c28566ca7e2287419be72d
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:37:51 +0000

    too pity zero Ii swimming

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def divide_numbers(x, y):
+        return x / y
