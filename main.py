48ebd1889a411b042578009c51ba4aad61b78a8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:41:51 +0000

    already strength door murder tooth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import collections
-def create_queue():
-        return collections.deque()
+import math
+print(math.pi)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
