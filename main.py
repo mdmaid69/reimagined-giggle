c0218fa8c1c362b0828106c4f3def15de8bceda3
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:10:33 +0000

    goaway connected store bye sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
