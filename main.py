0cc6989c58aecc64baf8cdda3ff922a031f84c6b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:04:22 +0000

    rob shout secret goback(to) system

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
