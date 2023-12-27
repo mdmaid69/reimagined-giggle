61ead73f4b5cd87264435d16ce26006e2b73f385
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:17:16 +0000

    perform brother holiday point the

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
   def get_file_name_without_extension(file_name):
         return os.path.splitext(file_name)[0]
