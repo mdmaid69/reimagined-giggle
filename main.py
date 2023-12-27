74cd53f1a9f01b15a1875df23dfadf30b2da4da8
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:42:03 +0000

    stiff 565end pause switch travel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
