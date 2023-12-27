60d7fdd1b00b48e542143a86c630834538df1662
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:48:15 +0000

    limit typical job friend mention

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
