b4ec99228d127097bd080f7edfd9100eea5d445d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:01:29 +0000

    sail side on record everyone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
