a533c33df618081115ac075c9f389b05354aa9b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:16:31 +0000

    whether giveup hide stay main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
