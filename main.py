b3a750fce23f0d77573fba69576dc2425e77034c
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:52:42 +0000

    pick contact completely arrange noise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def calculate_area_rectangle(l, w):
-        return l * w
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
