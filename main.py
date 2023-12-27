74d448323ae4f62931b093ba312493279cfef40f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:49:33 +0000

    confused needle phone comeover land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
