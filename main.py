18514f0bd0af45d0f00a2096092f0ed8ec37a682
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:32:20 +0000

    where afterwards combination drive attach

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def find_max(numbers):
-        return max(numbers)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
