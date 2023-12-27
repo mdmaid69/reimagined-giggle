d333a93694286234c4b3b537b19f19e78f28da6e
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:00:07 +0000

    correct badly considering brief talkback

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def convert_to_octal(n):
-        return oct(n)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
