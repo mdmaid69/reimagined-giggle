34711c31fde117fd56f13bdc7ea5a1e6da58edb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:24:43 +0000

    fall go fork meeting feed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  def count_elements(lst):
-        return len(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+text = "Hello, world!"
+print("Characters:", len(text))
