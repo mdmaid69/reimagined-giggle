3f9b8f837cb60c34cbf0b44811ad80636b1f007e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:40:13 +0000

    study term woman catch dream

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
