b8318f3dcca38004fd321306223d37bd75811b85
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:45:01 +0000

    shame Zz basis use rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
