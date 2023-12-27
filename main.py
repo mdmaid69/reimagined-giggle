3d1189e4fe5bfd362709f9f8ef2774cbaff95c71
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:52:23 +0000

    lonely luck responsible instead young

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
