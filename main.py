8c1cb16a20fd3b6b8f131a9e1e017985a009d29e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:40:38 +0000

    exist actual bet reaction Itself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import time
-def get_current_time():
-        return time.time()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
