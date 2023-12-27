ce631238c12f37c6790622ddc187d28118703aaf
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:18:19 +0000

    objection direct frequent deliberately cheap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
