14173c80def881af6e7b0bb75c4a7ce7f3699d6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:51:58 +0000

    make tidy anxious definite brief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
