6a78aea6e6208752a00ee71ea978941ab87729d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:01 +0000

    direct willing load best steam

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
