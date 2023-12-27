56a0d459758f0ddc42a17a640efb2659df72786e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:54:36 +0000

    name physical compare trick sight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+name = "Python"
+print("Hello,", name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
