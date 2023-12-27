c708fd9e7a1f0ad91f36e6a882ff4ad0ac1e9f85
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:11:24 +0000

    once night whoever write neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def square_number(x):
-        return x**2
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
