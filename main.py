9f3e634d654efb0e74d3fc07b92d7e67d5469af5
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:15:39 +0000

    remain general grade mention bring

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
