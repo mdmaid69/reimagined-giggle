b197610f34b887b00ed36f04a2d401ecfb52c6f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:45:11 +0000

    clear criticism human whatever believe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def find_max(lst):
+        return max(lst)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
