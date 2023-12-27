24e3a62e8fd3deaa8ac55a979d9322dd078c3640
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:00:14 +0000

    deal destroy through into which

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+for i in range(10): print(i)
+import array
+def get_array_typecode(array):
+        return array.typecode
