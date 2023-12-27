ba94446541462c7be23b174b02ce196284a13ebd
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:44:18 +0000

    range thumb plastic trick course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
