42f5d933f4baad11460183d915ce754a8d9443f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:07:55 +0000

    bad complete pure 571enough talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
