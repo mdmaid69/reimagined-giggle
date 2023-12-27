50cb875b2268b2caa1877c67a03208d441a7b7d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:36:21 +0000

    look care process turninto home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_hex(n):
+        return hex(n)
