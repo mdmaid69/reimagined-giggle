171a19a482f652fdc4820733231046b6b037eae7
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:25:47 +0000

    shade tread press to handle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_work(force, distance):
-        return force * distance
+def count_words(sentence):
+        return len(sentence.split())
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
