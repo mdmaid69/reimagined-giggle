3c8c62365fda43b32a40d766c2aaa3b0c3602e5b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:31:25 +0000

    back it slice sign hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
