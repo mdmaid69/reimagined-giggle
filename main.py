c569a0e06b1421418bfa6e566609b2abd54dd586
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:01:54 +0000

    justice promise read blame month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
