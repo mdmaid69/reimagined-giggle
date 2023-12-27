aaa1fc7f3073b4788405cb8efe10bce65dd12674
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:35:56 +0000

    finish level welcome grateful university

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def count_characters(sentence):
+        return len(sentence)
+  def convert_to_octal(n):
+        return oct(n)
