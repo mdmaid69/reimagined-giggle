2acd5d9128c14f4925f2e89d6c51e28b04410249
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:08:42 +0000

    ahead secret float central Ii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
