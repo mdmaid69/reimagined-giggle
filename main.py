28da827c250eb2251a8c954afbc5e600299d0bc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:50:18 +0000

    very affect continue length report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
