69b59b9647099c33ed547865173b34136762c092
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:33:16 +0000

    connection general furniture warn check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
