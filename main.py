02e2cc9df5b815160845f8456d0ec5d6fb3e4c37
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:53:08 +0000

    advantage border Kk here relief

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_cosine(x):
+        return math.cos(x)
