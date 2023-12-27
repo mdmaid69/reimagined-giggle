7e344c6488062d2e00375c9e7ac28d7f9cbea912
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:16:45 +0000

    trap apart load float even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
