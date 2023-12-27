f0965c34f6997a66550962dfdb5c7352b7fce4c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:46:46 +0000

    job play dark warning spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_exponential(x):
+        return math.exp(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
