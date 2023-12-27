7bc2d2a894535d71465f2d78e090fb96f2d615d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:28:37 +0000

    fierce Ff lost tip afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def reverse_list(lst):
+        return lst[::-1]
