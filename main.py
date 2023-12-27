d1eef232851fe7e1d43b23727700a4908dd969da
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:50:43 +0000

    room school contact personal apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def reverse_list(lst):
-        return lst[::-1]
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
