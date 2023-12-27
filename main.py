474cd20f20e3429151bf387fea223b4dc487fe52
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:17:31 +0000

    save onto row performance bringdown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def remove_duplicates(lst):
+        return list(set(lst))
