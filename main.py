275cee764d950e2e96f05ccb586215cc859f7e5a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:24:02 +0000

    progress emphasize temper damp minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
