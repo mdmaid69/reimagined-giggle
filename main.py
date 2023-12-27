a0c3b26ac8a53757e4a6bc6bb302e28b175df12b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:32:02 +0000

    skin stamp constantly pair proper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
