c625ed8e2211bef0abf1f11b7c877549b1c470f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:11:07 +0000

    wipe character at burst degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_pressure(force, area):
+        return force / area
