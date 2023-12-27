d4e67a36b8c78f792fb5b98752bb9ef6e320b933
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:00:57 +0000

    use plastic line must relax

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
