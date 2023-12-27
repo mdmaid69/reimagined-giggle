d2363b192bf5aad330886ec14fad222a0eb9b631
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:05:24 +0000

    exchange unlikely bored pot either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import random
+def roll_die():
+        return random.randint(1, 6)
