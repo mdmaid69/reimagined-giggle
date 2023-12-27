d74ac014bdb3d2133f3ea7a97cfe28c872cb7c8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:17:19 +0000

    undo ever journey test base

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-text = "Hello, world!"
-print("Characters:", len(text))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
