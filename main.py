87625316a89d628652e91173619f9ef616f111fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:13:57 +0000

    interested though beneath calmly heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def square_number(x):
+        return x**2
