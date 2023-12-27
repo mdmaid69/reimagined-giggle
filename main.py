ef2fe8ae9b08b344b21fda830b663117b524ca63
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:57:36 +0000

    loose remark appearance honest estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
