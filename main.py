9efb059c8b1494bbe01d70fdd99bdf663549b94a
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:28:47 +0000

    even gap rest attractive feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tensorflow as tf
+print(tf.__version__)
