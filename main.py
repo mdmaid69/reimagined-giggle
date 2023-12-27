1682d3a88bf6cd2f6fd10799e9dc355f4a4927f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:46:13 +0000

    before produce do sting cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
