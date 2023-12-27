36c61bc109931dea4c064651fc292356026c84cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:08:14 +0000

    shall woman fire freeze argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
