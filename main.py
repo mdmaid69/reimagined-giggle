717d96c46b903c48d2aa51ad02379abc1b1995f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:12:06 +0000

    at Ee really die today

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
