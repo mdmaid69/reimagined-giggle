ba8b04f4a72869ec86a6b6a021f837a682e1db7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:47:09 +0000

    contents loss hurry politics lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
