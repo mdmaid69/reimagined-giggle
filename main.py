a6c6ceeaab368cdcd4edad7e8e21093f75556117
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:33:28 +0000

    surface stand whole business apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_directory():
+        return os.getcwd()
