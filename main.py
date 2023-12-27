d6bc019d20a66991728e2e606f4e17cda7ef6465
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:53:05 +0000

    machine mess gooff illness anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_repr(array):
+        return repr(array)
