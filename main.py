0d672e29ffa8158978971cad2b1062f9f700b1c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:39:35 +0000

    sort make early eye control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
