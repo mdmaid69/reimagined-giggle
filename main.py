82167e9e04077070d2e033b6eb3b40ac92b9c099
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:06:42 +0000

    fire borrow drop live surprising

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-print("Hello, world!")
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
