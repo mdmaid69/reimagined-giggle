9677af728988c4cd380ef4538915463321500aa7
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:46:00 +0000

    right even afternoon brown remain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print(sum(range(10)))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
