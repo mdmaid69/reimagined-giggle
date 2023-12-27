d3b5eeed0ecc9eeda8a3607100b35090a7bd6fc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:32:02 +0000

    bunch organize minimum especially word

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_itemsize(array):
+        return array.itemsize
