570a712dfab97b86e88087fc929b23b206cf7bef
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:40:16 +0000

    afterwards several study boil doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
