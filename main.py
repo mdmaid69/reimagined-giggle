ef7672d3c931bc324ea389bde9d8101a7f23ce8f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:45:21 +0000

    possible bar for murder neat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def reverse_string(s):
+        return s[::-1]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
