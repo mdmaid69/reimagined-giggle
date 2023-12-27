238b421aa500c6cf94e187cd45e8fc095c22de50
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:28:05 +0000

    push callback pinch thought seem

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
