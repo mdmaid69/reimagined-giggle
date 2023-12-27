5d01dce70c620625fb68649478efa4d45c0c9304
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:12:09 +0000

    free into embarrassing declare together

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
