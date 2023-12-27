605afef0a323fa415daf5e4e3292b81720228058
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:39:14 +0000

    suddenly store scheme rank appearance

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-print(sum(range(10)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
