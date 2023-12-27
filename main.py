0a700469e6496a8f0af69ae93ab37f10daf82184
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:18:05 +0000

    down story should small snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
