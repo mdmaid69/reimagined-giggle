bca25c3f04e14dd7bfc52f83688ad0ece1074777
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:22:01 +0000

    slow exit rate content together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import sys
-def exit_program():
-        sys.exit()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
