584f208830f16215a691a1e3750b93256853add8
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:31:04 +0000

    battery to custom glass joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
