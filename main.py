53d372fed21fcb74b5f1f9c3af845033f1e206f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:36:15 +0000

    mess nerve sound Bb better

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
