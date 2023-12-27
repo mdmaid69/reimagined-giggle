fe7990ff8996634e7b52881d4840af72a6b3554f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:55:00 +0000

    exit probably protest chief sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+def calculate_average(lst):
+        return sum(lst) / len(lst)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
