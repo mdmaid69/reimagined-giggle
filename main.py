dace0e724a86fb8867082391e92bbfe3c18f65aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:49:56 +0000

    trip information level runover criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
