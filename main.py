eb322116d71ad3705d23780b85c37dba70984b84
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:59:00 +0000

    mine blow unfortunate uncomfortable gate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_acceleration(speed, time):
-        return speed / time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
