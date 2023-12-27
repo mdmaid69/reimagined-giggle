df580e668d5182c2620b1771b159f19864506b94
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:54:55 +0000

    relief call patient how wish

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
