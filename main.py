9c146ba1d41177fed0b02d9332bdcd44002cfe7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:38:55 +0000

    design pin prepare there gas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def is_palindrome(s):
+        return s == s[::-1]
