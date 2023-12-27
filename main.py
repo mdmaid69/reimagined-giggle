bfda4391ac3ae921aa5394afd051b6f958528836
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:16:06 +0000

    thick environment active central impatient

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+from collections import Counter
+print(Counter("hello world"))
+def is_even(n):
+        return n % 2 == 0
