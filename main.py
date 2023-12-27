46f09969601ec62b403edd55f11a7026bd43b909
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:07:03 +0000

    block public determined build mine

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_speed(distance, time):
+        return distance / time
