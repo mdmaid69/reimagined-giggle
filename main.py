1a4607607deae6830b7606d1cb83edf3d13ec9ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:27:10 +0000

    bush attention pile scene dish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
