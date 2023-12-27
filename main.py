55a9b4127744f7da6107064bad7a92dba63f8ff0
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:12:07 +0000

    feeling pure file this juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
