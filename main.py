cec1e3f2d2c11094fdf08a868d59fb14db6cc3af
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:42:27 +0000

    square wrap take pipe settle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
