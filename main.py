bc27d3ccf6c5d75205d5cabc56b3dcb880f03098
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:50:01 +0000

    load turnaway cheque bin affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
