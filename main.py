edebf51eba1609718fdf102b8f68dd1f6a8e6533
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:23:36 +0000

    show capital dull keepfrom star

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_list(array):
+        return list(array)
