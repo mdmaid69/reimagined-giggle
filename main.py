aeccad21fb72ab8819b49612eb34b8cf7ed7553f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:09:23 +0000

    balance ill sail record shelter

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
