44504a0228a33312926699a303ed5d080d2a73ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:34:52 +0000

    familiar advantage dark style hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
