2b5e935be5be2683bcf25ece9181f176915c2b2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:46:00 +0000

    opinion press wing send hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
