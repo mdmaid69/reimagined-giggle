6888d70ca788eac8ce2d72f5f642c29cf1618c35
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:40:53 +0000

    later tape man praise future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
