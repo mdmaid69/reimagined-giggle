b295901173655fb7916ee70e9be57e6cd5698eff
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:50:13 +0000

    account level mention provide somehow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_min(numbers):
+        return min(numbers)
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def list_files_in_directory(path):
+        return os.listdir(path)
