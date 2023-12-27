45e30eb12616726eeb1f01dfd8e67eb3c4f239e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:11:18 +0000

    straight suckme he repeat exploration

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import time
+def get_time_since_epoch():
+        return time.time()
