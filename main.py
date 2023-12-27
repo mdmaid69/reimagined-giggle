2cd6f07544618f672431ba3ae6f5898108bb537b
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:48:11 +0000

    mention pair eat shoot comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
