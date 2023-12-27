c04023c6ff1dfe5cad968d0651a2fdacb6f9ec25
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:47:42 +0000

    question tell card past get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
