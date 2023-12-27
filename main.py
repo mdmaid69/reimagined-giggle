67b255721708a78f1f6a3d054d07d2bc79637ac7
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:41:51 +0000

    mark seat instrument brush listen

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
