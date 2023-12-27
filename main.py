20751c2e44b96e61b1f694fc41f922832621a75b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:59:54 +0000

    plate lucky press sharp recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
