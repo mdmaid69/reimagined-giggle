f1a5fe56edff74783c95ae48b8cb37496dafad0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:09:39 +0000

    prize us settle thing education

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+for i in range(10): print(i)
