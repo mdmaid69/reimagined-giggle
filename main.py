3911eaa8f208c09f73efeb18a6650e3e36a0362f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:45:16 +0000

    brave how send smooth win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_queue():
+        return collections.deque()
