b5d703a6176398981f7050afe759c657a1dc11e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:03:24 +0000

    sea claim store although my

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
