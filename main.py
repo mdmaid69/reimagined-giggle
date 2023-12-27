f80e5ef052cfdb073631d5ccd47cd9d20d5a2127
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:34:59 +0000

    content sit before maybe keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
