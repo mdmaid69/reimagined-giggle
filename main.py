8211578267a25cf0bbacc049039770318756c785
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:39:24 +0000

    unhappy crowded soon paper whichever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-name = "Python"
-print("Hello,", name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
