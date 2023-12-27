7c4e9d7780d5ca2be2f2ca1ccabfdd0bf31b7226
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:02:05 +0000

    branch wall patience gooff law

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+for i in range(10): print(i)
