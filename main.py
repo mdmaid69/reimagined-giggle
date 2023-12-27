73feb9f7a8fbb386d08bec8bb326e088add023b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:55:29 +0000

    need this balance male spare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def reverse_string(s):
+        return s[::-1]
