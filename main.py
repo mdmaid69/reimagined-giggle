02e99fc9ba1a0e5c28f3a417a94bf89e61c61440
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:24:18 +0000

    to basic we month what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
