7ec1d2fcfac460815befd219adc4ab8b6107a30e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:03:16 +0000

    bread best pure on progress

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
