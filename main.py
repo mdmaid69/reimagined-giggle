c284e06700323e671d0a131604c95a93d6aabfd5
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:54:42 +0000

    quickly hide comment ring gate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
