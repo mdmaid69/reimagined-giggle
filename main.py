e1a5836d4220d176a9257b6853761fe862b21cd1
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:53:30 +0000

    pity fasten Hh finger climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
