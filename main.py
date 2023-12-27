f4c94a99b8e4f5899500a94704369de66f129b1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:21 +0000

    deliberate deliver escape offence seriously

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
