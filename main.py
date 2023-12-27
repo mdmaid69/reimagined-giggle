0d203df3ee177ac90145906ba289dd62a72fcdae
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:25:17 +0000

    door which appearance land whistle

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
