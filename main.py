eea4dff16de6c094e51ef7f82cdc1e93531b36fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:21:01 +0000

    government university dangerous set just

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_array_to_string(array):
+        return array.tostring()
