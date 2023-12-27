b2f5eab40e8cad297cb0757dd74b05505f8c4a14
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:58:13 +0000

    schedule warning error whoever count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
