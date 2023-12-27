192bf052da8c9e057ddf476320ed09136da84855
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:44:58 +0000

    spite around minor embarrassing burst

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
