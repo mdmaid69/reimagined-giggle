2b53bb34bcfd8b1df941cee0f7bbec66b9640978
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:46:46 +0000

    corner 568energy until Ff tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
