0772e39b049b6bdb3e48cfc66f1aacbb9fd15cff
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:23:27 +0000

    Ee tongue enthusiastic pretty doctor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def sort_list(lst):
+        return sorted(lst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
