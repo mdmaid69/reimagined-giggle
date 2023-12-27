1585681db76020faa78b20b50932733f37409d75
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:36:19 +0000

    wrong second Pp this honest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
