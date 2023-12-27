a7a0413b47349b9f28286a9984363f67f99f075c
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:10:57 +0000

    fold make age happen congratulations

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_odd(n):
+        return n % 2 != 0
