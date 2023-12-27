21dfece8db0a61c60c2d6db070eebf70e09a7130
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:58:19 +0000

    suffer realize mass point right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_typecode(array):
+        return array.typecode
