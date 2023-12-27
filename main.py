7460627b8d8b4b9a80d206372ed62a4ecd089153
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:23:28 +0000

    stranger prepare disappear lesson or

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
