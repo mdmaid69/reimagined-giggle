2d542e78b09db43c50a8d502b282406cfc24534e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:37:02 +0000

    week general content bet upwards

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
