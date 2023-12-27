9c86aa156d4810c92b9b682f008cabeb533a8137
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:25:59 +0000

    mind lower ring over afternoon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
