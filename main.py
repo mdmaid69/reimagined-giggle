4010ed0f15c600ef4e2e32cfecae8f8572438952
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:14:33 +0000

    birthday finish future bang strange

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
