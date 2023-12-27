1427ad6f6c50556202eb81bd1063f497f085b9e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:55:06 +0000

    present why plan rest Ll

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
