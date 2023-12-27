6079196b00c33dd8b8fbae6820f403f3aa7051b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:00:59 +0000

    university common opposite announce radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
