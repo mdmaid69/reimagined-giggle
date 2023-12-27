c7c33131f6e9c49e0425e03dd026d213d2bb3adb
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:13:07 +0000

    kill plant bunch point me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def convert_to_hex(n):
-        return hex(n)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
