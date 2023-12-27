dce8596bee50e504221b8208dd1b0b6b645183d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:41:41 +0000

    thin Ii brush bright warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import os
+def change_working_directory(path):
+        os.chdir(path)
