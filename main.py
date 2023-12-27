ef2afa237adbcfae928a7456df2c518112a9e291
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:09:19 +0000

    then 566end weekend habit scare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def multiply_numbers(x, y):
+        return x * y
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
