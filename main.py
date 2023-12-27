be9e5aa301dfaa02d345bdc5639be11b1bc54850
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:08:44 +0000

    block which card not event

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
