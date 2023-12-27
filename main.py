42153bb205947d071b668b939974db43986fbe3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:59:28 +0000

    evidence all copy free wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
