f2352480797afe40e8679ce0012aa5e93323c019
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:19:10 +0000

    drive country human instant permission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import re
+print(re.match("h.*o", "hello world"))
