ed26e8e6c6706bf45e9d73565effa6e89d60bd45
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:49:59 +0000

    continue hot except less note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
