a36d260e8234545d184f29c1883e35d4f58c5af3
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:14:50 +0000

    arm dangerous introduction squeeze excellent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_list(array):
+        return list(array)
