5e3756bcf9851c0afab9e44d6b6b6a1ab0b09c5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:29:09 +0000

    back evidence experiment either chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_user_list():
+        return collections.UserList()
