6ddcafd6e7792058ec4d1ecf53ed1eb4d0633311
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:39:01 +0000

    calmly spend rule yet comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
