4c44bef13bfee953c7bd19fff5a0359b448bfe6e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:57:51 +0000

    path trouble burn mail public

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def sort_numbers(numbers):
+        return sorted(numbers)
