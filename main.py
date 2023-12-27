b1335ead78c51a1f4f8b19a4ed4ae8f75b29b223
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:05:36 +0000

    order weigh straight chat unless

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
