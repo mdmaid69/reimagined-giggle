b63edd534d347245a89168b1d5c6b4d6f0131270
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:09:09 +0000

    takeout lead treat pretty wake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_dict():
+        return collections.UserDict()
