b72cc79f4da051c65e85ba120da711497d97ac68
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:04:39 +0000

    production ignore fact single search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
