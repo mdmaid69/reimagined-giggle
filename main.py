515313cbf545b3d44a4db62f2976c0685f6b9aee
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:31:40 +0000

    ground tell music which divide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
