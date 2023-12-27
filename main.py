f07994af1f3bd73ac61ee549fd89d0a1a0454bf7
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:27:42 +0000

    poor natural divide distant search

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
