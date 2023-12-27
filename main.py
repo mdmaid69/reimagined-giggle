2e54387289836ce0b7affc0e5fe8536532f23ea9
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:02 +0000

    point shallow thin fortunate we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import platform
+def get_os_info():
+        return platform.uname()
