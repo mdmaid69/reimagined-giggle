3627069cdd724de153a14ab2193cb6a0d8b735d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:25:14 +0000

    soil smart hard drawing intelligent

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+print([x**2 for x in range(10)])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
