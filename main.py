29d3349fb8f96af0fec6b36f42719e47501d8e22
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:37:03 +0000

    generally female turnaway crash straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
