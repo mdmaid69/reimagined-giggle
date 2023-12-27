039cb7c5a636d0f7b106dc096fea9b91eb5a1e76
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:37:28 +0000

    tear pattern disturb out moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def greet(name):
+        print(f"Hello, {name}!")
