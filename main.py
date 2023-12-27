be032c434cbdbf6ca2fdf66b980f81be6bfd717a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:17:16 +0000

    risk back by body joke

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
