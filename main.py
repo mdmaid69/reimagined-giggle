636366e6defcc0d3669534bb4a352c2581ff9ece
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:38:33 +0000

    main could formal runover mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
