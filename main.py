fdf3a7185fe1bff9039395d8f3a8e33cb4caf48f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:06:24 +0000

    slightly heat pressure rely priority

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
