4ec6f7b289c453fa48920363637809af9c330bbe
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:07:14 +0000

    possession basically score illness again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
