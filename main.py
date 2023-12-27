82ea7f00af78275a8b0850d6d94551aad50a42e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:26:29 +0000

    date unite meaning lend match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
