559ec5346b5ad2f93e8883b41a393d1638cbf06a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:51:41 +0000

    succeed Tt getoff guide stay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
