de20aa61f2b599abf55c90cd12da2f8aa381f9c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:49:17 +0000

    like arm ring false equal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
