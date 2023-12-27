8b1d73c6e857632a3ce59eedd74f0f9873e248c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:54:23 +0000

    politics show usually free lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
