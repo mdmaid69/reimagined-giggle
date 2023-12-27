f043761dc687a69e38bae816258972e5e7d9d536
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:45:28 +0000

    example sticky account legal physical

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
