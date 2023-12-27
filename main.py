446bedd1ece732d8e05d42c0aae6c1b16646ab31
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:48:01 +0000

    calm pleasure greeting reply knowledge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-def calculate_power(work, time):
-        return work / time
+import time
+print(time.time())
