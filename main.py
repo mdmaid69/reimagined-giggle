958045612e8ab1d57241ff777cfc1bbbbef2274b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:42:03 +0000

    start ill rather aim information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def convert_to_hex(n):
-        return hex(n)
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
