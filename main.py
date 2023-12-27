3114d63fd555d03449fae0a97710f40713cfdc6a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:53:23 +0000

    style congratulate normal worth yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
