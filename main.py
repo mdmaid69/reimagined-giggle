fa64815ac959a75e6a86dbd10e92f79c48e124ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:39:57 +0000

    push ilovedicks love probably unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
