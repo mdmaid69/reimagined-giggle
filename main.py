582c23f7b0cd4f928be3be0cbdf4f5adc5424f7f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:36:09 +0000

    base everything regard valuable whoever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_counter():
-        return collections.Counter()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
