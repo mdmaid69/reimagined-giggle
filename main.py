8f137069c474ecfe5c7ac89f3b2fde1c8f59f913
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:45:48 +0000

    attention pressure shout practise shadow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
