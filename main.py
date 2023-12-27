f94568caf4c6787c1d6647cf40cd92bdcb53b8b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:44:46 +0000

    peace coat swimming more convincing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
