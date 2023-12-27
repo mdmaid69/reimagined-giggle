c1c70bf2d3391ea86a62231bda747cd2334772db
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:43:48 +0000

    disappointed mood mention funny breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
