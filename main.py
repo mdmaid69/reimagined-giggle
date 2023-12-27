781e34c9c056f624180f087e59c7bd7a24019493
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:11:36 +0000

    before swimming fast fine risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
