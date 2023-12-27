d40154ca6404dd9f5093b8e51f3e539b97501923
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:26:52 +0000

    race yes nonsense born central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def multiply_numbers(x, y):
+        return x * y
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
