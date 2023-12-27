f020eb2664df104c2c029e8d5a95b510fd59796a
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:19:08 +0000

    approve money passage fond influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_user_list():
+        return collections.UserList()
