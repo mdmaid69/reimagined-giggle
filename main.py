0c33edbd4ee66986566f2442d009b0249c3d9f06
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:35:27 +0000

    minor it radio book ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
