93ae45a3127737d30530207608327519d12afb6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:35:02 +0000

    heavy question control blame know

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
