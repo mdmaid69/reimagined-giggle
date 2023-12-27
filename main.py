f359a6ea2bad19c9f3655fa6cbf2f6e1da8677a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:42:38 +0000

    round compete snow yours advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
