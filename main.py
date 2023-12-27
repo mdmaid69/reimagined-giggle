d2ea5b43eadb1e44c958ae6c1283fdd3bb13a43a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:44:21 +0000

    wash depend uncertain noone breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
