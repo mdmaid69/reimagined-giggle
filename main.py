ab65ece1cc6a5612632585a6aa15acb3c226f2b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:33:25 +0000

    tip smile duty reliable record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
