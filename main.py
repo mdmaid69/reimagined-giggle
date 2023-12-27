e637d432f249beaf83ae08b14dd92b71bfb6aec6
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:45:27 +0000

    Pp information scared clothes protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-print(time.time())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
