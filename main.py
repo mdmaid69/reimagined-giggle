456dd05ca0181f4bc07902d9f7ffcae2a8271cca
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:44:47 +0000

    invite one pleasure still traffic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+from collections import Counter
+print(Counter("hello world"))
