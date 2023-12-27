18ac09881ac37450138271b1dac4c77858ac4a93
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:33:53 +0000

    loud worried pay Yy store

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
