38d663c6a427c967cc3eb34ca5099d621eb9d65a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:54:24 +0000

    face store announce damp shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
