de333fb065bf1bc7b90d7c19b6c4e68198bd1de7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:26:40 +0000

    battery taste deep rain exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
