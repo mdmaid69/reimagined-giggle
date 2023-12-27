c6b1601eb8bb966eed9b03c90364b6cd42cad46b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:00:43 +0000

    everything yet like nurse Rr

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def square_number(x):
-        return x**2
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def convert_to_binary(n):
+        return bin(n)
