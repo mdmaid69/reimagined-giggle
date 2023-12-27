ec62563cb103a34edb48c5057569ba43614c56b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:32:29 +0000

    dream tax wide tape juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
