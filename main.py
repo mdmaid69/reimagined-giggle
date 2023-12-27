c7bad910c3787df6c2fac2cfd58d790a0f9785e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:36:53 +0000

    soon underneath wander power fast

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
