da10619f9f39828fc373ffab8929e3435cb3879e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:47:04 +0000

    after train comfort extremely getaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_speed(distance, time):
-        return distance / time
+import math
+print(math.pi)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
