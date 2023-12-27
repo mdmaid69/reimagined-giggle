1ffb34d4c641be4cf005ec08b2d92a058a236d13
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:19:28 +0000

    instruction tend even classroom sore

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
