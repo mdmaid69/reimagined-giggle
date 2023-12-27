573dbdab0b16edbb41a6700ac791d96b99cfc59a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:17:45 +0000

    cry last neither division guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
