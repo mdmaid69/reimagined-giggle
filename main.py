3f9f4e3ec74ea6c9eddd34837b969dbf58d27523
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:13:29 +0000

    confident exist experiment square calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
