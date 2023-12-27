a3e28c5c12191002f3af8292acf43085ab49c1d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:13:00 +0000

    offer test Bb bringup lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
