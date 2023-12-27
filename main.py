d5257bc413ea71577225ec3adba5b37fb2b03c87
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:51:53 +0000

    decision tragedy some hello could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
