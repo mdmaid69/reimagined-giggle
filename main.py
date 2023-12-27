e748271c58924a968d4deefa4a2fef1c5fcb8c50
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:14:39 +0000

    voice runout everywhere hot computer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def convert_to_hex(n):
+        return hex(n)
