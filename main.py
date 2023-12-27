ca749b171ef6e10ab733ad8eb0bb72d7c0d8300c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:22:06 +0000

    stir dream offer cope believe

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_power(work, time):
+        return work / time
