c1ef52cbb1a14a1dbc31542ce7433da0ad1fcac4
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:15:55 +0000

    Ii he hope child post

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
