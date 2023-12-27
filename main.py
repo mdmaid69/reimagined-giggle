dd7958cca9d70297096422869efbdcf65ae6dd16
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:51:47 +0000

    empty cinema call travel question

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def is_prime(n):
-        if n < 2: return False
+import os
+print(os.getcwd())
+  def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
+          if n % i == 0:
+        return False
         return True
