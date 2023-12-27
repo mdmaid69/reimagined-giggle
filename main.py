5f897c1152740943197eaf95a6bec47f8c179e2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:12:16 +0000

    certainly spin weather shelter serve

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
