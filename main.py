3db59c96ecadd26553614edac20232749bcd3cdd
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:18:33 +0000

    way him neither warm speech

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+print([x**2 for x in range(10)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
