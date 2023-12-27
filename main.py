3a9e8c776de2db91213b326ceb5ad66905c21303
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:13:43 +0000

    suggestion promise test 179bite good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
