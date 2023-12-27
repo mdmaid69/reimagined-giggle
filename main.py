49dbb7085d1d5fdf4cfd90110f1f27a0cdc6b4ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:04:05 +0000

    light noone will middle argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
