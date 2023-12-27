789607052bd2cc41573a84ad0612deddc856bf0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:10:59 +0000

    present character think wind flat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def reverse_list(lst):
+        return lst[::-1]
