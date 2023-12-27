e668106cc98b3e33460785cb7c3d4ada5f3813a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:25:37 +0000

    hers chat limit float where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
