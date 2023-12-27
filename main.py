9b91aec8879d3f67cba8af3396c110d6307742ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:23:53 +0000

    invite memory plan Uu message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
