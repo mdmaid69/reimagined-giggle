c8fc68cc0da1fb8e6fa05a578774f5285899f206
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:55:52 +0000

    authority tidy less strike sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
