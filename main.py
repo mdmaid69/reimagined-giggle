55e7abb8c3dd217a3a205e22574c7f598832fb7c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:53:05 +0000

    lead flavour community suit disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
