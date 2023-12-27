2e60c2fdcdbd86baa952218925ebea2f0fe5e8b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:51:35 +0000

    hold smile present declaration terrible

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_list(array):
+        return list(array)
