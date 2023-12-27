ccc618343bcb5eaa808fb6570684e34e975a542d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:21:58 +0000

    test valuable range goon practise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_min(lst):
+        return min(lst)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_array_as_list(array):
+        return list(array)
