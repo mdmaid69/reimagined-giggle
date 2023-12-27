dac84f80596c44d998c2a7871658b1008d53cfa2
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:24:19 +0000

    favour hold distance careless ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
