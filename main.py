886e74f7162121c1b66871d04f052babe35265bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:24:58 +0000

    introduce call top begin string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
