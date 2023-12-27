6e99a302f415c7a9d1fcc7ce03ffdad342eb2e4a
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:14:17 +0000

    fact remain necessary comment comeoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
