26ee52254bb6cce7031f66fc8dc9a36aae800a31
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:18:51 +0000

    interesting entry thank refuse thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def convert_to_hex(n):
+        return hex(n)
