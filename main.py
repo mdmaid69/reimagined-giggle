da51b5eca810be7f37e3f95cc970e69b8ed4c4af
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:16:12 +0000

    fix failure left fear murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
