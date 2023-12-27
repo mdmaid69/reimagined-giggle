97d16c7b788aeb49369a6eb5a110e8da92b0790b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:36:05 +0000

    takeback right break direct leave

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import tensorflow as tf
-print(tf.__version__)
+import sys
+def print_python_version():
+        return sys.version
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
