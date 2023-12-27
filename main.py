152d96de60e76100e5f905961da2e5f212a21843
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:16:55 +0000

    slow buy yours meaning know

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_pressure(force, area):
+        return force / area
