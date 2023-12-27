a362151b1a838e6c87a62bed1b6eaa6e7338c210
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:57:09 +0000

    identify rule do top talkoutof

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
