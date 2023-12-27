c7a611c866d9dabebf3e629dbd58b87d4540c124
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:26:45 +0000

    deliberately only only next enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
