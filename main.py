318af0f7340367e3740fdb5eb7f58f6323fb5d41
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:18:20 +0000

    far supply round past advanced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
