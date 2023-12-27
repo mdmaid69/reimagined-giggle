4da04072837def9dd3abf584cc358fd57e8a8009
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:31:40 +0000

    run keepoff furniture relationship introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
