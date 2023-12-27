01f1e8b0b7aaeb8cef1944247c8ab2f156d68124
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:04:21 +0000

    popular increase plenty together shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
 import array
-def convert_array_to_list(array):
+def get_list_from_array(array):
         return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
