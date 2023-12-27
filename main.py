0f4574907db6da7127c7db797ab7b1b3fb93592e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:29:24 +0000

    comeround attention boil long compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def is_odd(n):
+        return n % 2 != 0
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
