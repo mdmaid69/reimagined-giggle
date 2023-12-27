72dd54277905b6f3c6545c21e409eae0f473a50a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:51:26 +0000

    onto food danger cost now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
