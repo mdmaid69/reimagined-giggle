418806914a8b053aed69d4e441c14c1ed39028df
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:59:03 +0000

    want after takeout his year

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_string_from_array(array):
+        return array.tobytes()
