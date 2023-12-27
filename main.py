e37765d51e122aace05669709ef6f742f1e5594f
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:11:58 +0000

    union Zz medium pretty test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
