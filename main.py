2b3535ca66f99c3d60762792ce613e561976260d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:53:34 +0000

    warning achieve decision unlucky clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
