2ffb60e6e269bb496e09a1c7a51eb66bd58f771f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:34:20 +0000

    disappointed even niggaacademy take minute

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
