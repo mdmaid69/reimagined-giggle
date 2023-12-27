a5b0706d81bab77b2ac1899f75ee5f06c4996373
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:35:59 +0000

    bang legal least war typical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
