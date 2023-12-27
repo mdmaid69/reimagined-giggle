4ab7c2ab81e8fe0e67f71ec3ebc557d2c4dbc432
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:08:53 +0000

    unusual example print fold scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
