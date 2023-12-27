06dc550f175de41fe78c1b3be40c3dd631bd8a94
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:24:27 +0000

    yours communicate tongue lie entrance

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
