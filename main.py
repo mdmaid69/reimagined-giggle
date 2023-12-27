5ea10bfdc5c13c5517bf8741c4059a9b06db7780
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:25:50 +0000

    unpleasant majority mystery demonstrate voice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
