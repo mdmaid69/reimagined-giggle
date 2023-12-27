b21f938f428de6dcaae23fac46cd48e8a9b64897
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:02:20 +0000

    town fail obvious build born

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  def remove_duplicates(lst):
-        return list(set(lst))
