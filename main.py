08657703305c5e8db620e70ca1517d6b5fc9187c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:44:07 +0000

    plenty brush middle frame completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
