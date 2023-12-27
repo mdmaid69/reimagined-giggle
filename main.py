95ebc6aea0046c36b1f6c6a9572b3d25f80f8ece
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:58:51 +0000

    active century suffer wide flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
