277a0a23b77a056006e3826178772e9224928152
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:12:18 +0000

    hard safe priority so weigh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
