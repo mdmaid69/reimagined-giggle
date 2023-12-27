3bbdb153a63276838c8438b49ad612f8bc2f7ba9
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:32:15 +0000

    mass hide break ring model

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
