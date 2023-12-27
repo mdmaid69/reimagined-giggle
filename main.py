552fbc454b4d770a615400f2521520b74ef17e70
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:18:57 +0000

    plenty lie neat education exaggerate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def sort_list(lst):
-        return sorted(lst)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
