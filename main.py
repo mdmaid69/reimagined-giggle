54349224d29b9a771a9ddf73dfdd77779e0b95cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:03:07 +0000

    his breakdown exam task will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
