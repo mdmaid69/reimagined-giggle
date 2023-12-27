50f4efb1d3a327038ccbf9f7c89a1de13c2dabd4
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:29:07 +0000

    flight tidy game full shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
