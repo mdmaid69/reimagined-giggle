6c269b1fad41ef8af6d25f7c58d350aa3f98f553
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:19:53 +0000

    criticize confident truth previous definite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
