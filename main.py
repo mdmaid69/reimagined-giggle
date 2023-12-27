e57632a74e02bcf288c84ab93fc86ecce7108706
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:28:26 +0000

    early swim study part past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import time
+def get_current_time():
+        return time.ctime()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
