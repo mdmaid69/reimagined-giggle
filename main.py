47194529025761b20fd22bfc2168d685bc30f4f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:00:52 +0000

    will congratulations dick hold forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
