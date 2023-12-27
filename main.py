b9b6a5b557a51774e25a5f0320507e7aafd033ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:00:09 +0000

    regional sometimes voice mouth its

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
