c884facdee3a341a64c1f9324dfab4720bbba295
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:47:52 +0000

    when test tip intend calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
