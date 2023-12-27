a452673938b0275f9c70f8dd9c2a8b6d3226b974
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:56:07 +0000

    ill keen war deaf between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_hex(n):
+        return hex(n)
