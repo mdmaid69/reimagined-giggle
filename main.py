fc3c43fc0bcd51a60d72135f450dde317e1dec08
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:20:10 +0000

    method which occur use escape

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
