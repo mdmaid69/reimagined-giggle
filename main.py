08ea02a88291707fb26c41d912dfae3612aa069c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:31:35 +0000

    but recently programme excellent blind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
