6e99d726bfb26eee88730a7b827c5697f2837edb
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:58:06 +0000

    cross with roll song priority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
