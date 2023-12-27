60bc91d0718fa3313518cc863be9150243fbfc9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:03:58 +0000

    pump experience confused cheap plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
