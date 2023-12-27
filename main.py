d9f608e3ec70d7460392b31968b68006e22b3cf1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:20:52 +0000

    bin uncertain rate add goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
