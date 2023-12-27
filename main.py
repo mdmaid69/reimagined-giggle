1ef735396f4ff0050b16fe43e50ad932ad1d3333
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:45:39 +0000

    blind walk tidy opposite sure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+name = "Python"
+print("Hello,", name)
+import numpy as np
+print(np.array([1, 2, 3]))
