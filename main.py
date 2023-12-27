f5c9953327fe22f9baa0d589e1b19503d6551b7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:23:32 +0000

    concerned mouth hurry fall track

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
