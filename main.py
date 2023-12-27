6a18e21025648970361d9483d4886a1b022e3149
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:38:26 +0000

    trouble alive breath you matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
