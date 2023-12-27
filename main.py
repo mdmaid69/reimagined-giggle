f06d21f2f8352d6221a0a11c3d5c6e765115a38f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:39:14 +0000

    free to quality avoid bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+for i in range(5):
+        print(i)
