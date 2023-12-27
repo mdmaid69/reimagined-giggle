60aa17532a5e29e7d7c9a323c762db86566e2dc7
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:13:51 +0000

    smile say noise store boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
