ae39df38b257f10ebd8a7798e26c626f38053cc9
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:48:47 +0000

    occasion temperature comfort border another

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
