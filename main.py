63b1fb3a57a42b789bad2599fb6ace3cd1256204
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:51:21 +0000

    achieve mark occur expression niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
