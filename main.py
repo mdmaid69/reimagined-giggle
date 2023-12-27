4ebcfa550035e4b59e37650e9861101a553d3877
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:02:43 +0000

    flat better shall comment gradually

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-  def convert_to_binary(n):
-        return bin(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
