04d212f0d1a467d283aa60dcbdf7489464c50ca1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:24:14 +0000

    part turnout tap word tend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def divide_numbers(x, y):
-        return x / y
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
