5ee5e3aa89c8cfbb348d0adc844f472791a07e7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:26:42 +0000

    lot keepout you nowhere he

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
