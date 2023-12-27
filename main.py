3c2f25b07a2ed4d061c6f6cc3650aef5248b3381
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:14:47 +0000

    side unusual separate loan warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
