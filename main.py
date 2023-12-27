30415ef54e8255631dee129f4aaca67903827277
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:58:59 +0000

    point notice move wave wind

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
