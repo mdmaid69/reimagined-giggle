de4cecc13020a041593a25a99bd19885e34fa344
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:45:18 +0000

    talk change holdup cool rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
