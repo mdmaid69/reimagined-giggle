8d6172375639b54be9177c10e662028f591db742
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:29:05 +0000

    bang unable talk breakdown seriously

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
