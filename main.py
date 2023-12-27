d5ce1f7769171cae4795cceae8d284abbca6d548
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:36:23 +0000

    path demand stand settle wave

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_average(lst):
+        return sum(lst) / len(lst)
