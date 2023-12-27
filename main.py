5cb678411137bd22891127c914e0c52b3c9805bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:17:19 +0000

    date choose separate sky opinion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def divide_numbers(x, y):
-        return x / y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print("Hello, world!")
