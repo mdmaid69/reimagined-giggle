212cf116e50e0c8d3543c8c68ea705bb971f6446
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:11:57 +0000

    rely previous perhaps protest not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
