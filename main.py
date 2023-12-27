7c8fd41ca4abbdb184f6e71e6e64b959c91dc4f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:51:01 +0000

    wise blame arm kind debt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
