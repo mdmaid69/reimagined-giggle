b2b729678eada39cac24a982c69e35e8ff0a452f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:27:11 +0000

    bright accident school reserve process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
