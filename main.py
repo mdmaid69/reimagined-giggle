e77462750477f9803052c4077fcc40bbb4320590
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:01:34 +0000

    fair frame refer mean advanced

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import array
-def get_array_typecode(array):
-        return array.typecode
+def check_if_array_contains_item(array, item):
+        return item in array
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
