19550972064b9714c0b6eb8dc1e25e6c29916b33
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:35:01 +0000

    clearly professional offence satisfied star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
