65bc9c3d8b77993605b9c8cdd0d513809c00e3aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:44:27 +0000

    wire colour computer best sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
