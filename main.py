87489c63acf744d858078281cd5e869992f203d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:45:15 +0000

    myself love little themselves listen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
