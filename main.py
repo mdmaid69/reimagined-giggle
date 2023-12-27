d9d90f04f2ad98ca9e37aa7c0f56af88d83bf5da
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:59:31 +0000

    compare getaway black argument abroad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
 import sklearn.datasets
 print(sklearn.datasets.load_iris())
