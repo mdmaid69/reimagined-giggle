1959a8f4142881052827c1e2276d61375ad9fe9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:21:47 +0000

    brush muscle natural warm on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+for i in range(5):
+        print(i)
