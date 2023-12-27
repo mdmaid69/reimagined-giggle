befb818b421173553acc01dc522c624d64330c7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:05:22 +0000

    thin clear give reply shine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def clear_array(array):
+        array *= 0
