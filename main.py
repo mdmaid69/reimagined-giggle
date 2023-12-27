ba3db7db67c19db2ce0c90d4800b2ede618ddeef
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:16:22 +0000

    flood pay sentence earn represent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
