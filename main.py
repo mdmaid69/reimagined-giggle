96c7f7bf0cc73041141f6718e637cf4abf43e81a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:57:48 +0000

    drive arrangement feeling shade talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
