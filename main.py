7d8c740ec5b0600292e98d84534d09e42d2132a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:42:05 +0000

    passage hope follow address shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
