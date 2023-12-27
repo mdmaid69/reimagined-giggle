0305a6de79446cb280e6825c0639feb17aa9bfef
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:45:27 +0000

    identify knowledge holdout television file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
   def get_file_name_without_extension(file_name):
         return os.path.splitext(file_name)[0]
-text = "Hello, world!"
-print("Characters:", len(text))
