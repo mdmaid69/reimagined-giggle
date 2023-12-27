22eb1736070796409c1408087a17caed68153b94
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:15:01 +0000

    short along row finally apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_python_version():
+        return platform.python_version()
