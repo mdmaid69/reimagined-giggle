0cd154d9fa61ea60c2344a692376e45cce0b143b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:19:12 +0000

    main development rank drop load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
