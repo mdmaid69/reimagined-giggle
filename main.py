39c8b882263d50f5708a4cda0885939cdf148c21
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:29:36 +0000

    glass lie familiar workupto appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_list(array):
+        return list(array)
