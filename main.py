10acf8d52c2201a1bf3fb7b13c88ac18ee8481b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:32:14 +0000

    burst excuse Cc cold disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
