10e9753562ac2a7fe71108c1e42de60e13731a17
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:57:30 +0000

    learn hole free shelter neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
