faaa471ed1b5c43a22dc85cc8438af72e4591864
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:50:19 +0000

    commit murder ball fast ball

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
