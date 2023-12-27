6b6a052d88c064462b2159541c6fc7c5e1c46e52
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:28:55 +0000

    upwards sense limit sport pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
