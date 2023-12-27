a5f79cbed6003a205083332bb93e7b180a04c6a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:16:13 +0000

    sure over separate set fall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
