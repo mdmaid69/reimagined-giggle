f5fed97cf36ecf7c193bc09a28be85ae32873162
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:29:42 +0000

    minimum turn lookup stop talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def is_palindrome(s):
+        return s == s[::-1]
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
