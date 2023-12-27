55d76977a4071f89336ae9ac1103a6ce2a1164b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:28:43 +0000

    near cap dead pass rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
