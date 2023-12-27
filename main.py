5e0d7ebaad40066083e7b8cc410195cfe98dfbda
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:43:49 +0000

    false unit right birth tongue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-print("Hello, world!")
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import platform
+def get_os_info():
+        return platform.uname()
