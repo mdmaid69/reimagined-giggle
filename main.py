083050dbd429346879bf4027984d1465642f7341
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:58:31 +0000

    one turnup act me him

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
