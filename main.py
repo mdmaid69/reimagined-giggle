a9c2621068f345ff366081d007703c67c5c6caaf
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:36:57 +0000

    survive kind load bringback keepoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
