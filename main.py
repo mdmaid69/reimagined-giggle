2d39f6ad4de704273a3b21dcf76f7dc1fbf86d06
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:59:17 +0000

    reply apply closed kind keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
