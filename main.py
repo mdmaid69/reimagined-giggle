38a8f59d55130d9cad4de7d89c775ccfecfa091f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:47:00 +0000

    study bother stone pour at

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+from collections import Counter
+print(Counter("hello world"))
