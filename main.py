e6c3803068ea26b4011704354dcadbd6139d05a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:16:39 +0000

    real conduct be various conduct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_work(force, distance):
+        return force * distance
