0c5cfbc9c1a40ba300873ed8320fe2ad625fe241
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:48:31 +0000

    guide battery appeal matter sail

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-print("Hello, world!")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
