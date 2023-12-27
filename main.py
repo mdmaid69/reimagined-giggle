8953da34f93719a8ce2a938f883d4ef7cbecf2bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:43:56 +0000

    everyone law I minor when

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-print([x**2 for x in range(10)])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
