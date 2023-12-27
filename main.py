4835fbd8777d87d18c65d1dbbfe5586822ca3c3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:26:50 +0000

    invite complicated throw character but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def subtract_numbers(x, y):
-        return x - y
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
