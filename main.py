18583a517c89324d1957b53bee49d233970dee52
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:17:34 +0000

    Jj during suck decide last

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
