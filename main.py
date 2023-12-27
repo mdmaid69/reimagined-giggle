752baa92ef82718f2611eacbc69b637fcc6b70f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:53:39 +0000

    convenient generally best press move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def clear_array(array):
+        array *= 0
