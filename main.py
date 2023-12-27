9d099e07e7bca0d81d40a113915421afc5812b53
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:00:57 +0000

    mass world after any steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
