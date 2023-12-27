0c77970743fc202ee8ce44bc4280c46b37b5320f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:16:44 +0000

    if beat adjective lean mess

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
