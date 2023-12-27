18167f6eeebb110f4b6d37fbfc91998c7d603991
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:49:53 +0000

    alone fond goodbye congratulations train

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
