d83d78c199d2d410d33225a16a1e536f4171b8a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:50:43 +0000

    storm same cold bath swallow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
