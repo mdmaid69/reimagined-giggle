c29abd64d1d939e7d86f188fc7dba333024fb7d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:17:05 +0000

    clear print bottom violence damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
