c8a5a9030af591c810241c2348a048efd1cf38b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:58:26 +0000

    pile exist half squash side

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
