380312283bd22ad4052481b4e21d47f898ab694c
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:56:10 +0000

    organization young cause second out

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def reverse_string(s):
+        return s[::-1]
