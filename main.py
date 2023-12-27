668c3261f5c1810a2f9c117b7d082462e3fbfded
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:16 +0000

    university name upstairs produce familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
