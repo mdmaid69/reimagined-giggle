1268aff3c126dbdcc79302d90c9b2c85945fbd41
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:37:23 +0000

    square image form fair pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
