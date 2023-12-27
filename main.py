6db04aecdd9834475fe1a5f2a71506d018b527e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:47:49 +0000

    dig call market university I

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_pressure(force, area):
+        return force / area
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
