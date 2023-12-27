292110211eaadfdae8c032e667dac6d437851bb5
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:37:10 +0000

    general quarter pile opposite maintain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
