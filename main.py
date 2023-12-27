3aa4ed3e06f87f79cb459163cff548a02417cf30
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:26:04 +0000

    one quit whom uniform freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
