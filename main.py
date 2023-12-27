3c1d45370bf5c826bf00492fe0c14ace227ad257
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:22:37 +0000

    temper precise standard fence treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
