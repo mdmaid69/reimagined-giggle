376cea5bff52a16080c0ffd4a0130d2306cfb5e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:40:41 +0000

    wave possibility shock kill fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
