6d961664e2b47a2bbaf35a9caa6ef69518cd75f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:12:20 +0000

    slip flood abroad cruel distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
