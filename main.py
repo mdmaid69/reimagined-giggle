587efc9a1fb280f024e4c2404d5e6f933246c890
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:13:18 +0000

    model peaceful match be detail

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_float(array):
+        return float(array[0])
