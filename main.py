e311665f70789007fd2b09c2df7a4dfc7d4dc208
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:23:16 +0000

    better believe whether goback(to) favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
