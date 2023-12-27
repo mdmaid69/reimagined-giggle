a9bf27bf47fcfb9e35403fe29a667e70ed616f77
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:16:40 +0000

    minute spare emotion might release

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
