0537cf6ae42aa56dbe8559270c53bd6550d87e67
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:51:09 +0000

    damage answer praise more familiar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
