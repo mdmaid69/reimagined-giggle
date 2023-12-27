b243779a841ff3abf5f8eb8f7fe214fd0cde97ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:00:00 +0000

    reply very convenient coffee cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
