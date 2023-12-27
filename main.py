a18b3c876cdcf796a477f2108d07722ef56b4603
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:43:38 +0000

    drive insist bottom accidentally yesterday

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
