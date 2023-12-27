ea69e2ab11f723b7ac437a7a02979e8a05566378
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:35:45 +0000

    572enter sight love truth worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
