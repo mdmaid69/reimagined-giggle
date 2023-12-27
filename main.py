fcf4072ad28832f58e49465efd2eb033c3d69951
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:51:45 +0000

    holdout sing goon check again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
