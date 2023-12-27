cf9069511eb169f0b2b78b1b899c3310652f8f23
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:59:47 +0000

    record land female city forever

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_min(lst):
+        return min(lst)
