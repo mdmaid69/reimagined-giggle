dd3b280dd01720ed022c7844cff3008b685480ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:20:16 +0000

    same collect spend old unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
