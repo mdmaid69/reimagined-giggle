dfdea3de819996e3844424a15f80f0599eb67bec
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:10:24 +0000

    both dress aim this write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def get_today_date():
+        return datetime.date.today()
