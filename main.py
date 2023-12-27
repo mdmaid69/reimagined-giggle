a4c09a16c85855d21044e047899bfdd321fa1e72
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:20:15 +0000

    vote book send Jj rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
