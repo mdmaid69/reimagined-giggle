dbcd2965b4eef61b485037d3c399f9df0897b197
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:24:01 +0000

    birth expression total air entry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
