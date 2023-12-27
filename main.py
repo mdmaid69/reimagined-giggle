0f86e9a7538654876519f18c614022eca158b1e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:09:17 +0000

    taste present breakinto fault square

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
