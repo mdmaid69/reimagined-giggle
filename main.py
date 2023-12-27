f0b6c2711b03b4e8c0c014ecc4b5773e888c3591
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:51:17 +0000

    go high pocket account spin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
