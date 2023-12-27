48886da75588aafcf52945f9c6cd35514db3c460
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:56:56 +0000

    without unite copy container roof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 def calculate_volume(length, width, height):
         return length * width * height
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
