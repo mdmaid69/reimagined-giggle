ae00c4877b4b262f0d0741b520fabe4c4d17296a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:37:30 +0000

    runout freeze software official concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
