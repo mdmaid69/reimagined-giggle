94c147a35d7dd6b65616fa04479140296b042de4
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:45:34 +0000

    wipe base interesting under badly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
