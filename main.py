fb7e2921573d78be5a433fc4bb55c1e252cc9ecc
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:01:16 +0000

    substance spring I produce immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
