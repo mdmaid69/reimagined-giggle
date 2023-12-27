9ce4a7b81afa6814badb635d8e9f0f1ee2802379
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:31:05 +0000

    over mention best battery turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
