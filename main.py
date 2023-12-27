d98917c58002866cf516c92532338a2f69884c4a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:00:15 +0000

    maintain lookup patient whisper vision

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
