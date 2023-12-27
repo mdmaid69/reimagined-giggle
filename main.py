f5fadc6b1c23452eec39d3586fff4e126c5b9ddd
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:16:27 +0000

    pollution thick flavour service say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
