d7514b5041284180ea828bd54a10ff4358987723
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:24:03 +0000

    breakfast hold duty nerve rank

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
