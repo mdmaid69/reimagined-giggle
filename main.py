7868daca278901a75add4ea7984c7384e6c32712
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:43:49 +0000

    dig big attention simple it

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
