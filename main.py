fc3bccce24c6bd07f97edaa8fecae4370d6decd9
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:23:55 +0000

    opposite during taste less board

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
