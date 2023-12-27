99d6117e1026f484fe9c417593d61c618ed5ac28
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:05:55 +0000

    throughout wish scream anxious car

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
