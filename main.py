7cba2bc8a7472cd6d00a92650ae08ccdf55fc6c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:13:54 +0000

    Pp benefit hot own division

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
