7a5f6cd5e1ce26fecf37bdd2b166d5e80ddf0db9
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:32:54 +0000

    generous difficulty question copy unhappy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
