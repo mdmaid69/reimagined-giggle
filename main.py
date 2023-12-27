4388c6cf6b193ddedf53b04d6739edb022bc0e90
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:39:18 +0000

    slightly Gg hold release about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
