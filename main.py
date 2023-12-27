ba2e8d2ff233102dc2ff870dcfd85e68bab5fbe4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:56:42 +0000

    fault shut reserve market waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
