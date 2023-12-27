ea72b61f3a00d4fe8b2dd9ba114ad443db42ee9e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:01:04 +0000

    plan to knock watch bridge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import getpass
-def get_username():
-        return getpass.getuser()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
