dd5603c9b29357f1d91c94f9ddf6c13e1b4be2de
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:13:42 +0000

    turnback careful pot bottom leave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
