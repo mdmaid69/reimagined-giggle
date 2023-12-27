0207c3911f1a3ad410698568260370744e90cc80
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:33:10 +0000

    carry figure cross violence grow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
