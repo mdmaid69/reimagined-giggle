13bc8084e273c5ad0d2c31c0ef53fcc22a25f3a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:06:19 +0000

    whistle legal cap shop rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
