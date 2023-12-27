0bc7371b02b3ff46715f0e7429deac707a413a97
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:04:16 +0000

    tidy bear where scrap needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
