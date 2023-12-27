bc151897211c2fb9c5a635aeefbf1085df46f229
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:31:23 +0000

    performance computer destroy high occasional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
