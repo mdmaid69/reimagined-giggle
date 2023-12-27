ff3e9d61d6fd4713fefe36732b4e0f1eab3245c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:52:12 +0000

    sound invitation over right mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_itemsize(array):
+        return array.itemsize
