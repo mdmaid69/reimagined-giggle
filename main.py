f3a6bb891fc0db0191144bf06fc627a1950e5c5f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:52 +0000

    music bend impressed between load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
