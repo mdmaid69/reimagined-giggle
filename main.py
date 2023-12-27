8bbb53fe8a329e252d1a63dbee8ae8c4ae24a685
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:46:58 +0000

    final single suck recently do

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
