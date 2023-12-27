afe76ec88603c3ad0fa38edae162ac86378d1491
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:15:40 +0000

    direct turn mysterious season tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
