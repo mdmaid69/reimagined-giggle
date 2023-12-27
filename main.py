cf41c02728d35481ca6a021c96cabcbdcebbba4a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:50:15 +0000

    shrink have part ourselves swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_float(array):
+        return float(array[0])
