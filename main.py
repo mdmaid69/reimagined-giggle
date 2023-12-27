e8ca9cb9d6862d40fd0da2397277896ca3ed9390
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:30:24 +0000

    rarely substance upstairs travel moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
