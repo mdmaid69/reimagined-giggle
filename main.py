f41cc37b284ece2f6924faed1756cf4564aae9aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:27:52 +0000

    or pollution sleep mind plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
