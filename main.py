1861e2ced287a1889e1f7d1f58e7ba99f53df8a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:16:57 +0000

    exhibition evidence price makeinto bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
