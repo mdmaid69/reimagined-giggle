5411ff589073315c99589bfe73fbf004852dc5e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:11:42 +0000

    anyone concern soft possible final

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
