3225e8a9cccf22f0b244eee5b2b7f60361bba95e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:01:51 +0000

    standard exhibition group describe straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
