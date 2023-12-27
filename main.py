824632b067f3ceb4988d52691577a9710f3a62c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:07:15 +0000

    event crash answer visit lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
