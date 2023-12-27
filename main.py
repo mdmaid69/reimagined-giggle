e87cc4c2029e7d7fb29c395d8d185304baf58092
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:52:27 +0000

    gradually communicate swallow file shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
