60df58a6dae32c382296106d2d8758fea77c1948
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:52:21 +0000

    heat but performance offer be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
