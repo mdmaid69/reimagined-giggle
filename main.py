4afc46bec838c4de720e0a91ab8616fe94919460
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:22:50 +0000

    ask busy picture comefrom dinner

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
