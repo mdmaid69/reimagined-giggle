13a0d868571913438aa458e268cf4fcd2436f04b
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:23:39 +0000

    under speak behind could getback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
