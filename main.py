bd9049538ec9aa336d659330f4881b823b3270f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:41:31 +0000

    warn star great left touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
