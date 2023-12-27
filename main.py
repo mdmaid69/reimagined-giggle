f9085ba778a33547d01f3d065aff3a27afa4db63
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:14:04 +0000

    marry game temperature lift repeat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
