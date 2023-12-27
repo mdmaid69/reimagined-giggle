b7cd8bbd967f96108bda24ef18b99261e95c934c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:12:31 +0000

    practice person show know tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
