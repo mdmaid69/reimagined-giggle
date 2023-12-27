55ea81f45149491a05ade45d52cf4ea443af8f01
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:07:52 +0000

    moment way pin convince fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
