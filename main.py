5775d439c2ecc96f37b78db206541d90e5bc23c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:51:39 +0000

    callout vanish standard correct force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
