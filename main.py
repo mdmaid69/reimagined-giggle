8699a4134d6a611a7e19ecf86e81dcf7928ecd80
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:16:56 +0000

    carry steep size machine exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
