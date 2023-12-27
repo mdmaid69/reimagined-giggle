f27fd07f6ff40d5855e3c36c1b200596685836eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:11:58 +0000

    voice nurse glass apologizeapologise getout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def calculate_area_rectangle(l, w):
-        return l * w
+def convert_to_octal(n):
+        return oct(n)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
