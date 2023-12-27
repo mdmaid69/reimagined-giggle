5376825688ebea041e8d03e63ab48d8d2db58da9
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:13:34 +0000

    breath situation modern might also

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
