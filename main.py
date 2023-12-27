b879c1bc4b055c63c8431312d33252e6d738e51c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:36:54 +0000

    fire previous glass lift however

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
