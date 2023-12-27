b2c7f93d07ebfc6ae351995a050028600c97a2cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:25:12 +0000

    pure law nonsense bread into

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_string(array):
+        return array.tostring()
