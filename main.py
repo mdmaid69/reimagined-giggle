90beadf5a5649aee5f694eb1459487984f934e4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:38:51 +0000

    turnoff shame wave autumn mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import random
+print(random.randint(0, 100))
+import array
+def clear_array(array):
+        array *= 0
