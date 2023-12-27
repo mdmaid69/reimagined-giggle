5555f03d9e611d7fdb289bed603bf479ec982be8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:51:00 +0000

    explanation dick education style strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_as_str(array):
+        return str(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
