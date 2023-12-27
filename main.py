6f816b04132cb5f46dbba93c3157990fbb5b0648
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:00:29 +0000

    traffic who impressive Cc keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
