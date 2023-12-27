bb7b9cfae2e552ce6690a6508c0a33d234a3b376
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:11:22 +0000

    standard small 569enjoy quiet minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def sort_list(lst):
+        return sorted(lst)
