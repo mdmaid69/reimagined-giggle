bac53d774c99d9f01fc343f83b05857c0aa2bc9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:22:26 +0000

    tired walk mouth pay advanced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
