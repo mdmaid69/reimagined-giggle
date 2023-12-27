251174648a8705abc7d6696cdf8913b4b2194ad2
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:29:35 +0000

    when also region risk holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
