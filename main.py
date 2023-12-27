ffa81f9b270a9e6fd998ae00aca9ad36e8dc3f53
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:40:48 +0000

    winter laugh constantly mainly wealth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import os
+print(os.getcwd())
