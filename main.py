630a18efb2e3184e1036b12686e8a9eee54c26df
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:42:42 +0000

    there Bb makeup base drive

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_work(force, distance):
+        return force * distance
