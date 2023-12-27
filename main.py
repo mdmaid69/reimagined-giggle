96962a7ceb52a66ef9f2f3e6f7058a22194164bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:37:39 +0000

    solve sit precise skin whisper

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_as_set(array):
-        return set(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
