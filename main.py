a3429aaa72fa078882f81f475366efbd3f7cdac2
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:13:45 +0000

    obvious whether interrupt yet reduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+print(time.time())
+import array
+def get_array_as_bool(array):
+        return bool(array)
