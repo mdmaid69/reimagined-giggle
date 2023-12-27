05670538c0e961071cd55a5bb49e10b3a2828cc4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:48:34 +0000

    minute same form steep sink

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
