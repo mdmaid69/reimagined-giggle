20cf2e61ecd1777c859e4fd8b8280a7f76a23d09
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:26:25 +0000

    radio drive fast but undo

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
