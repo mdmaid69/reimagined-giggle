ef7f4e95bb2ce50310f732be8279298ddc5d6360
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:00:16 +0000

    getaway meet duty clean surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
