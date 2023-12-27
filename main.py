913431ab2e333105c30e9a9a0b0dcafd2fbce1f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:30:37 +0000

    regret unless best threaten deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
