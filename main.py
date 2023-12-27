b5b9d5a99f536e000499f31dc431191f4969c35f
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:45:54 +0000

    phone gate achievement makeup brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_speed(distance, time):
+        return distance / time
