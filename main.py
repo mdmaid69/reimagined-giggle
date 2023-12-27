7eca9c88b3af6509c22f76753b462125c3b5d06d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:50:48 +0000

    especially study centimetre ever hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
