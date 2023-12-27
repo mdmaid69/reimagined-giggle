0c1704588d9d4a61b26fea2a11972af0026c09af
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:59:25 +0000

    press worth chin unfortunate run

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def sort_numbers(numbers):
+        return sorted(numbers)
