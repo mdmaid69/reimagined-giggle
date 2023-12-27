ca39ea7a06014a142eddd5104efcd3fb01948fba
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:49:08 +0000

    pump physical pocket unable contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
