fbf58ca27fb78be68892ea825467a2db9ce87d50
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:46:51 +0000

    book unhappy whisper wish scream

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
