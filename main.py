34ef515aac3a9a87f22b7fc2045130aac7d4962b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:46:59 +0000

    rain you scale friend care

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def convert_to_hex(n):
+        return hex(n)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
