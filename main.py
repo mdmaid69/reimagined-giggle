923bdafc4f22c44762098ed273baddacb45872ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:40:39 +0000

    workupto Rr neck point sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_current_working_directory():
-        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
