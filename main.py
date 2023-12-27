80182cadb09e41386167a0c75cc836e3c0bcc172
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:10:01 +0000

    judge sail shine door hate

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
