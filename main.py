db74d088fed871a0f48bc0707cdcf39f892017b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:08:23 +0000

    opinion schedule smell persuade available

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
