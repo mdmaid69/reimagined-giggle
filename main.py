923581b718e3e0551b47c13b7d668bf32c6d2456
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:38:17 +0000

    explain happen face school joke

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
