22ada598359a4f205aaf30a30f3ddaca82586540
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:33:23 +0000

    any comein lie act invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 def get_file_creation_time(filename):
         return os.path.getctime(filename)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
