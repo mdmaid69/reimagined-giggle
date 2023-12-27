ffb18be491ad70d21c490e92d09d283740cae529
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:41:22 +0000

    in cross plate behaviour race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
