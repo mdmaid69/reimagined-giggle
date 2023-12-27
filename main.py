ee65b0dc4a4c271cc681dda3a6358d1e85948ae9
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:05:23 +0000

    underneath anywhere university list blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
