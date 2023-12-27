ab46067ecd42d5c6dcf563677a710903da9bee73
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:49:45 +0000

    tie perform some contain whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
