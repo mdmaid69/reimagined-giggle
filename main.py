18d48e98237317ad7528150dd1a2e7176051dd03
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:02:00 +0000

    split leave ahead high love

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
