798360628825d8881b7e9eaa4ba3f6bf4b746652
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:27:52 +0000

    occur hurt against giveout cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
