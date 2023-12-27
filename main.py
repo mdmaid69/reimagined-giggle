0b115d3a14e28b5f315207698371109608fe9d95
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:38:28 +0000

    real wide new fetch mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_stack():
+        return collections.deque()
