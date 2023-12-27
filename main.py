6d34f14aa90837dd72d9eddd6a013cdf10c02468
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:47:08 +0000

    form space suit character little

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import sys
+def print_python_version():
+        print(sys.version)
