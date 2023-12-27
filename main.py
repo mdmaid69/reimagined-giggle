53db1741896220d323162804629fe1ca59a74a94
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:36:29 +0000

    impression better everything giveback common

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
