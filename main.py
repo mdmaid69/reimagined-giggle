f1829710be6db05de64c1fae4a9ae46f1a55cdf4
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:44:54 +0000

    call size everybody afraid doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
