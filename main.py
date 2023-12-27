9576063a314f04e9273aa18995eba73aabea79ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:48:04 +0000

    swing exactly sight nervous generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
