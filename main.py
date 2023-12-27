72cd629a5047a94914cb7bfd83b0d852533cca85
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:27:14 +0000

    sister this dress hang connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
