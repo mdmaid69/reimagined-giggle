7ff8d1d9c6c800aec5c1bb1abd43f0cc89e78b4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:19:01 +0000

    worth suggestion include marry nice

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
