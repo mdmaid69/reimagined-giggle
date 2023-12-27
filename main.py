b5cb173c2002468a0f438eefdb15d7022faf9cb2
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:20:13 +0000

    hate mind connect deserve angry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
