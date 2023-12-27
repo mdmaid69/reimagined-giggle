cc7f8bf1ca50ce36207b7a41ff038262b1a0f4c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:34:48 +0000

    point fix opposite bringup comeback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_to_binary(n):
+        return bin(n)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
