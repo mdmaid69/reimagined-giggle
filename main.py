35681390cf224a8bc4ccbf260b743bc1f410f996
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:10:44 +0000

    ago yet invite national centre

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
