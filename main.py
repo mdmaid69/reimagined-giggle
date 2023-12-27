8f86c8bfc4918dee7d7ce20d8e159fc9a9279a94
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:31:28 +0000

    opposite goup thinkthrough machine pleased

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
