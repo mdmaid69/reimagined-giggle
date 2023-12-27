9f344ee86771189036e452fdf25cd2e97bafbec6
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:12:51 +0000

    Mm police familiar move eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
