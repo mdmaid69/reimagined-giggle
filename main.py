dcd75691e8891410a291e887d45113d9c1159f23
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:33:13 +0000

    friendly ordinary sweet sweet achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
