f08b681307df19cda81c2382824d134ae8bf250b
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:43:34 +0000

    many exchange conclusion official comeoff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
