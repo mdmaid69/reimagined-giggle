b91e3fb738abee7a43b9e9c8a2f6956d969baa33
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:06:48 +0000

    build survive round exit away

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
