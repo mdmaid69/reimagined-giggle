5c68545aad3611e925c915e384e3d20e75041b88
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:36:07 +0000

    argue cross genuine second but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
