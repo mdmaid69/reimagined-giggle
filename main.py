3feed455af640c4a07a36c5c46d43ba357885f45
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:15:41 +0000

    partner pleased treat plant they

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
