69ddacabef567b014d10b26257f280c48ee9503b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:09:17 +0000

    emphasize hold store type want

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import tensorflow as tf
+print(tf.__version__)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
