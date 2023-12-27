d473a68eb80a2538d6c4bce86498dd5a99fae8ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:03:06 +0000

    vanish mother ball profit Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-print(math.pi)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def convert_to_json(data):
+        return json.dumps(data)
