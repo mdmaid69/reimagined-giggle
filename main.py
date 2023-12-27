6dffaaaf463ea1f5af42a108b86370742e03b33f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:43:47 +0000

    party sit copy force style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-for i in range(10): print(i)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
