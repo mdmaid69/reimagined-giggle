43d63ced29e3eaa04f372e49efe3b50817597f1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:23:50 +0000

    thinkof doctor offer level object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
