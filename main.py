2956af446b52c4855117a930f27b013add94798d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:41:04 +0000

    problem cap force handle unfair

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
