3f3552d8c0afde7a064cf02973ee408cacc239f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:15:49 +0000

    expensive side border beneath measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def convert_to_octal(n):
+        return oct(n)
