2d3fbdaa966c39b4d7620a1cebcfe5ce07783cb2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:36:20 +0000

    official interview 565end apologizeapologise arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
