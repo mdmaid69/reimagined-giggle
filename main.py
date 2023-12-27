7bd7d67cfc853be007880eb735f0e1578ec6c9d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:29:48 +0000

    smile his move substance cool

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+name = "Python"
+print("Hello,", name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
