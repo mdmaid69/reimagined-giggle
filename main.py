b545af8417f471fa0a3dfe3d1958e9492a92a82f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:08:18 +0000

    university dangerous hot desperate slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
