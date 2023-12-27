01d193379d72ddd2018615b67bf017ef0a752fa1
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:57:13 +0000

    mother occasional guess relation check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  def remove_duplicates(lst):
-        return list(set(lst))
