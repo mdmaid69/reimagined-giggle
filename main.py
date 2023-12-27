6bb46fb5adb15f428b51bb0eee09b116c8aec966
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:39:56 +0000

    telephone wherever 573entertainment contact club

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
