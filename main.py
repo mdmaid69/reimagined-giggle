216ef97c689ac940acc88f7f4189ba9dbff9006f
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:18:42 +0000

    chin scale example relative attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+print(random.randint(0, 100))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
