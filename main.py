d8007c337103257d19cc57d5fc5ef7b1005fb30a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:14:25 +0000

    in door deal wing solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import collections
+def create_priority_queue():
+        return collections.deque()
+import collections
+def create_priority_queue():
+        return collections.deque()
