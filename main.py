d36b860f098f70f1b07d04e4813ce80bb11e4b46
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:39:21 +0000

    reaction strange loss together cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
