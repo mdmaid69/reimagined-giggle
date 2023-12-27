9362c035b200c33dc25842f0029c46235cd64911
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:01:48 +0000

    few harm Dd runover active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
