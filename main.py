a2cfec8f9a58d5fc011cd29c27eda86c21f35a56
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:39:02 +0000

    distance noisy when time conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_int(array):
+        return int(array[0])
