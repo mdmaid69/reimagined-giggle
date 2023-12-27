8bf2951e18d6854efd7773ec8ce6a8d25e1235aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:46:15 +0000

    deep hold bank tone slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
