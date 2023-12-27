79dcafd076b9fc30cea09bddfc1726bb0f9723b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:12:24 +0000

    tomorrow sleep will speak maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
