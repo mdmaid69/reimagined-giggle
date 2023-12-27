7f7b3bce861c7c88af3e1203da48ddf44f4d37a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:09 +0000

    production disturbing trade pity thank

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
