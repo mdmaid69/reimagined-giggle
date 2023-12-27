a33ba2c4104d82fb8f3840a26babd2bb5f503e18
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:04:13 +0000

    clean pile obtain recover bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
