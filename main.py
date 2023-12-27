cd9e05e0fb1fac8975b68140e40afdefc3a6702d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:23:59 +0000

    pipe who base bus health

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
