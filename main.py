c9c69a8f63361dcd0b1dbeee6a4efd9f5d8cf294
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:36:27 +0000

    meanwhile go snow ought port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
