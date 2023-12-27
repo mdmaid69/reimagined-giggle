66ab498c4359acd253b4be1257e85c9799a713a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:45:59 +0000

    pronunciation reserve wave physical all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return math.factorial(n)
