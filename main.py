4ae26fdc425f3ab6ce636ac43f3023994347cedb
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:27:50 +0000

    brother exit step prepare against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-print(time.time())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
