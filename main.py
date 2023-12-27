8a739d4857a50dd8222f12e91a54736b5ec8d59c
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:36:15 +0000

    or lookafter across spread than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_username():
-        return getpass.getuser()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
