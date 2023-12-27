ef877754ae599885570c8192b8a3ad582eaa7e2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:38:04 +0000

    picture disagree least train effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
