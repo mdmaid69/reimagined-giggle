a1c3c8f19c0421679b1db707732ce7f76e8c3a1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:54:30 +0000

    thorough learn store compare admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
