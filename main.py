e18ab00cf64f7b92aabc031dea7f278e4c668122
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:15:07 +0000

    lucky limit rent shop new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
