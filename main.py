2fbfc5168b40a890f436fe3e77a43ef0eae06913
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:51:52 +0000

    damp ground ours there neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
