e0cefb3a430cd6471351cd8cc78f9e31d866126f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:58:05 +0000

    proof holiday dull bar dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
