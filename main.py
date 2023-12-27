1c3673841c9aec3087b47d177649f623e5d76224
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:29:01 +0000

    will hole for exist full

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
