3ff6d10a8da25d632217f0f989810300733bed6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:06:38 +0000

    sink cry tough peaceful in

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
