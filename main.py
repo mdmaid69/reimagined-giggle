6c4e91d75de41ac3f9ae0844288f8c59c992f1af
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:25:11 +0000

    beyond pleasant worth cross 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
