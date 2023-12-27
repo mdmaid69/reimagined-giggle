53c4da45e99a2724504555254f06d0e128b082b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:50:39 +0000

    pinch callback success juice closely

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
