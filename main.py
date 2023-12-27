050b827450a677481e6b32f1fcd76fa12242b2ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:34:50 +0000

    success space offend thought sting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
