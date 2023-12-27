8b3e5c116e90319eb8429e15ff79afb76c0dea05
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:45:04 +0000

    lookup determined handle hour bet

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
