730a02dd7ac4d0d5edf34b6f566af2eaffa3f86a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:00:12 +0000

    have couple uncertain bunch Uu

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
