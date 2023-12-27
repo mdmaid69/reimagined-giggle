e7abe8a616184b128178d99aae6750b6c7aafc37
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:45:36 +0000

    noone square communicate nonsense it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_power(work, time):
+        return work / time
