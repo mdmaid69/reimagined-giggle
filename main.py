a31072f29c01782db823a7a44e6294c39a056ece
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:32:46 +0000

    convinced common refuse memory loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
