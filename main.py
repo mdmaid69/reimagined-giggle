e5d34a3c82c507b1cda4964c23cdb8d866b6608c
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:46:26 +0000

    store nonsense then gradually couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
