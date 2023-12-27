334ed331bb52f65f794f2fb8fa810225f4edb340
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:18:23 +0000

    Pp really somewhere emergency explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
