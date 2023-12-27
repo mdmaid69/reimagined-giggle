096c97228a28f267721cfa454a05453159c1c52f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:22:25 +0000

    especially never definite personal it

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
