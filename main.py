1623e78bba6ca26066752deec7988ec8bb815995
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:25:34 +0000

    Ll convinced schedule advice away

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+print(random.randint(0, 100))
