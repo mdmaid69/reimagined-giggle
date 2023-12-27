3ea62469096a49ffe3c86a66522a15f9beb5a08f
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:15:47 +0000

    frighten relax legal contribute air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
