6dc6dfb5486301919654049c093eb31d1e27257f
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:57:44 +0000

    pity children fast persuade round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
