664f863e9ed3a4ff7e9571514472f0152f1ad905
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:06:36 +0000

    heat float dinner deliberately open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
