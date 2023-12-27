8140f078cf4ce89a46943c28e0289a55fe3bfc33
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:50:42 +0000

    soil clear love distance load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def subtract_numbers(x, y):
+        return x - y
