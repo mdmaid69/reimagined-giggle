063e1ebb350d73eaac0bd37957673a9cba12e4f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:08:06 +0000

    type interruption close wait my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import logging
+def log_message(message):
+        logging.info(message)
