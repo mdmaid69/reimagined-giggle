ba51dc15af744f273a328595505610f886dacfbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:16:15 +0000

    rare settle contrast book temper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
