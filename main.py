0ebde87a9b31d3b2884420ba700c83069a8c6ab6
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:04:17 +0000

    anything getoff familiar best mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def convert_to_hex(n):
+        return hex(n)
