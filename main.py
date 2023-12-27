926160b9d0cc19e863a6357c36217fe4b9f958c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:54:19 +0000

    raw plenty exploration sad hurt

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
