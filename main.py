700d77aa3a56db60eb545b032b3569e7b603eab2
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:27:21 +0000

    nature unfair valuable this room

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+name = "Python"
+print("Hello,", name)
