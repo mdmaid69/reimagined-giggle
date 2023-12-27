494181bc0157f58b868d7d19d6211d8e87c9b4d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:10:11 +0000

    activity family cross style start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def reverse_string(s):
-        return s[::-1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
