1ce8e343c8772e9a9e0aa47b44ea44f85eee627a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:38:33 +0000

    hello office after later influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
