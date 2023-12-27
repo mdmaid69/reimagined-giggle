7f6dd90b7381005f9601703aed2ea90771768d79
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:18:28 +0000

    public shout Oo relative legal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
