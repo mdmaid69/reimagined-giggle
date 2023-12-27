57dcc1bea42e0f7ad24f43fd36858018043886fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:09:14 +0000

    collection clever their complete extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
