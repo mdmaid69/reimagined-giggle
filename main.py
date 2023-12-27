9df4ab484b15ef08889b9b2bdc15818f034f49bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:53:45 +0000

    impressive soon challenge before smell

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import re
-print(re.match("h.*o", "hello world"))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
