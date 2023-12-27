6f62460fb67604893a9e605a426a0a5976ce96ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:16:33 +0000

    wise now letter figure build

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
