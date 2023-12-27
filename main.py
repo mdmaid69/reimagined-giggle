08fe21263a17bb7f904d4126389df6f1e9d160ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:18:41 +0000

    uncomfortable accidental go least typical

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
