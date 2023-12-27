e70d154254a611af771e453cf7c0efd8f32cf1bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:08:59 +0000

    tape unlikely case satisfied note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
