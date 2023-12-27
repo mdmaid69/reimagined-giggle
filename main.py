c2fe952eebf7650b8e8ef5bb70bb997587c3d035
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:18:02 +0000

    turnaway separate post gap basically

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
