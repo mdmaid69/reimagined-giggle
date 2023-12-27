6554cfd41d4f5eee8cdc468401f002ece68926cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:00:26 +0000

    before pollution break country move

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
