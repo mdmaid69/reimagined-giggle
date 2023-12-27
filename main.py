eadc97a7d7a88680a4ca1673657757c62b8c0246
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:18:48 +0000

    up mad weigh tall fork

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_volume(length, width, height):
+        return length * width * height
