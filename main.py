ea21376d6f6c243ce09c0e12ed5ba851b9036f79
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:33:04 +0000

    marry Rr sink deliver include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
