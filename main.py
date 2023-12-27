d0469ce94c6f61c340f65740b7eaef6e9822e43e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:20:23 +0000

    give effect contain exam nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
