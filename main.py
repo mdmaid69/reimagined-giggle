a318551c8c74c795bb8b829f5b886eaddc9809cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:43:45 +0000

    joke record list band sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
