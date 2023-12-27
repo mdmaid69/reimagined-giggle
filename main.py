ef183ba4a49c6f4646377e8c7438426069a1a6db
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:03:14 +0000

    wave touch sleep coat whisper

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
