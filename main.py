cac4e7fbee5184c247858636496e59ce1904c4b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:10:37 +0000

    waste still satisfactory represent strict

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
