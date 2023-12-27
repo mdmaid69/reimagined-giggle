828fa8fcdbc4eb481686d6e3657ecf8c2a12eae3
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:39:27 +0000

    battery future convinced advice authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
