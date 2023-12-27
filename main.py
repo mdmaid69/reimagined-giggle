ec6a4e616ed596569a0798bc5bb91e8398b2e636
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:42:10 +0000

    related perhaps design break peaceful

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-def calculate_volume(length, width, height):
-        return length * width * height
