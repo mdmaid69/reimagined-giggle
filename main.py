2181a531787db69669415da3fe3088f7cf1ee370
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:54:14 +0000

    discuss any cold die hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
