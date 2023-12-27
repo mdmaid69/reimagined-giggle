c672304bd12d01094a05483e6f37368c50d14671
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:44:53 +0000

    programme getup wander hardly stand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-print(math.pi)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
