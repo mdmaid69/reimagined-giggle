5335b6382172a7942e17b91e6f3b44811e517c71
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:17:32 +0000

    yourself upset urgent scale bottom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
