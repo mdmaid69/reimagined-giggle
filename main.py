542597bbfc05d6878a9ce7b9150aa68b90a23213
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:10:34 +0000

    wash everywhere arrive treatment concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
