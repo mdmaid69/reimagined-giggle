26b29946567065852ea7b8e942a8cef9eea92b7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:59:02 +0000

    downstairs statement old across hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
