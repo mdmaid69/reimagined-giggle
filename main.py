3e1d001cabd44217bb3b93477f252a88860ea74d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:18:54 +0000

    rest burst really loss set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-print(datetime.datetime.now())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
