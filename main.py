b7635adb64d29a699ddd5de2955c6fee31d9a8f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:14:22 +0000

    world used fun giveaway snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
