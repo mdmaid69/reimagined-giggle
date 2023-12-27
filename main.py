60257f9c598cc3ad25e903f9c7524648bf58f39c
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:27:27 +0000

    attractive appear neither strange content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
