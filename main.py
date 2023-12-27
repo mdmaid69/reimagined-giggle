62b7d9a5ce212e8c82a4248ecb6b19d564fa6a05
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:43:16 +0000

    later Ee strong pollution evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
