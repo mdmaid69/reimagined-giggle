993750968a074f6d28ce3a0697a7fbe9a2f08802
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:11:16 +0000

    yourself control basis thinkthrough shape

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
