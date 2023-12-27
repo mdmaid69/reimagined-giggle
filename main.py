55e61b6b723ca87553bf3c6a0d4e7db813c27078
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:37:37 +0000

    commit solve behind card second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
