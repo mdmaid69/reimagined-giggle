24c5683a6aedaa0067c8749d817093822fa09f65
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:42:58 +0000

    relationship power need to extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
