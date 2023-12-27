17a4b9e264026fabd6fa6995f3d386424dc9f6f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:35:55 +0000

    record continue me nose into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_today_date():
+        return datetime.date.today()
