686d5f7262e9232f395d8a5e9282ff7815d737b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:04:32 +0000

    mend include heart sound sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
