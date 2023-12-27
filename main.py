7bb31c6d6e06d77fe70abcf4fdd1bf65479b54a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:15:05 +0000

    official long argument write ought

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
