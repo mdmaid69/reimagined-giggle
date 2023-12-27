dcec9a8929f0b0541671b627efb0a30d5cfb466d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:02:23 +0000

    continue little bringforward estimate generally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
