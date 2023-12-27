bd9ffe3cc23b9b986ff99a0258dc401376fc99dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:14:06 +0000

    control interrupt border sound switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
