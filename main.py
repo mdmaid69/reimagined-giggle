4b44e040169d8e353448bc9957aa03f5622c5b8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:52:19 +0000

    better sudden force branch here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
