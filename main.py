bc259fba38d859a05955f603bb0e07e9536c7c52
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:46:32 +0000

    division slide organize fork pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+print(sys.version)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
