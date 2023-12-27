20c5d3ea6f6fad8e83a366b449f3538f6397e4a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:36:48 +0000

    forget minority any suggest top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
