92d60babbfcdaa241e63eff78624cae826b35020
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:49:28 +0000

    moral table spot meeting central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
