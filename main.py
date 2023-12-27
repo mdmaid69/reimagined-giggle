41c4f6baa4d10d1bc77ed33d4fe0ae9771b4ec7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:37:06 +0000

    tear normal here on apparently

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
