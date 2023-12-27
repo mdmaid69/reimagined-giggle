4641b9d4cb950ac5f53a0645a06f4775394c8684
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:10:31 +0000

    hair centimetre warn original set

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
