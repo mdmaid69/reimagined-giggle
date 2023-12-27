b66f27ea76c668a60a91b426aa31d941ce230497
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:36:56 +0000

    prevent sing hungry drop only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
