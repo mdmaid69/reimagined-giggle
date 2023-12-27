f414bdc3a4236e11701ff69d25b26d9f50a90a16
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:02:50 +0000

    season tool arm sorry smart

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-import re
-print(re.match("h.*o", "hello world"))
-def find_min(numbers):
-        return min(numbers)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
