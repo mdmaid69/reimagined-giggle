19ab17e713c3daba9fa4363c16c7032d8116e202
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:24:10 +0000

    rise meeting male shout performance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
