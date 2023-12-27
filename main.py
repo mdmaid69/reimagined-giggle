30d1d3cebf91e3794ee162414d28f04ea1d1cc2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:23:01 +0000

    surprised afternoon wealth skin owe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
