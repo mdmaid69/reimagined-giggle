db750f37e831d86c0d45fd3b8e9a4da16f3f049f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:10:54 +0000

    connection variety drink wander image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
