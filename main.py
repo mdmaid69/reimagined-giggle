928b053bfcf1988d4a3ce7a323af71b8f3df34c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:39:11 +0000

    lookafter seriously bringback however question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def add_numbers(x, y):
+        return x + y
