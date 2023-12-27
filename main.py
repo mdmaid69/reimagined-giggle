0d26dfb5e2d3d66ce986411f8e7ecaa42aab3f42
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:12:33 +0000

    inquire list thorough habit warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
