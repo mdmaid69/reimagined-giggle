a9b2f9753ddf05417e6f1337e14bf90485cd77cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:47:51 +0000

    cook lost production glass shopping

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def sort_list(lst):
-        return sorted(lst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
