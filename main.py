9c72ba5c569b91ab0fa17c0748a403bba3fb8f53
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:01:43 +0000

    far much basis dance clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_sine(x):
+        return math.sin(x)
