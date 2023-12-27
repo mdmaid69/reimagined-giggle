c59596876a6085b3ec50616e5e99acd28c1a0977
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:00:37 +0000

    weekend against Bb in type

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_perpetuity(payment, rate):
+        return payment / rate
