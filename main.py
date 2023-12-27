2a4a01ff82cc669174e3bc450f9996c9f5a9daed
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:55:56 +0000

    clever intend consult like goon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
