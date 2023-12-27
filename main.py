59f020e6394b71c65381672b9fd257dd4abbd460
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:45:43 +0000

    correction dig machine characteristic recent

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
