c2b22eb20cb8e9c3ad4f49f250cff30327e3e6bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:49:42 +0000

    valuable treat present who necessary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
