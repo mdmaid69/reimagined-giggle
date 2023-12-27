9f1789f7d59f5e49c4330144377d2a616eb945db
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:49:13 +0000

    determined appear friend record hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
