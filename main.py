5cfa6d9d450aee871d8a52717c18f528d158072e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:44:23 +0000

    dinner climb across already temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
