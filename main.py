f5ebd84340fb3499d0de6af946cbf025125091cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:47:01 +0000

    detail callback greeting state before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
