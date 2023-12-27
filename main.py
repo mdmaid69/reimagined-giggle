2de9c1ceb5b736cb5bffedd14537d13a777a24d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:57:57 +0000

    own around give ever step

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
