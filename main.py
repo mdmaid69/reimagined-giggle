be46c3f57d1c2ab03747b83e05995590e4c7e6ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:58:39 +0000

    whisper discussion sky stop turndown

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
