ff7aede99a9b7594ec58553a70fea63a917ee3c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:48:48 +0000

    concern violent wet hope one

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
