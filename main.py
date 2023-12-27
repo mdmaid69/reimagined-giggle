71a97dc5c5b369843ef12a6777c6fefa3d28d2fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:21:42 +0000

    big pronounce protection exciting vote

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
