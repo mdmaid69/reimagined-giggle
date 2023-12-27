366d0837a92dca95ccd191f19dfd9612c31da8b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:56:23 +0000

    bury impressed hungry search closely

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_item(array, i):
+        return array[i]
