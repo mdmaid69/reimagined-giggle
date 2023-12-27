e59a82ebe12e7a7278e598142f17a0b271c46566
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:17:33 +0000

    afraid young cry point trust

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
