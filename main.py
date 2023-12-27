49a80263a4240ae450aa6ddffa7594751cc9ce36
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:36:22 +0000

    without phone fence trick be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_float(array):
+        return float(array[0])
