84c785ad167750a710151844c9b5ce3d399f6319
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:15:28 +0000

    cruelty grade fail themselves boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
