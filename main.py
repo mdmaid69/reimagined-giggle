079d8951a26c5cbdc870b48de3b68f2e578d70bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:03:50 +0000

    study weigh sail much available

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
