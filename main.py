c6a0e2cac67df62052ddc8afee39dae564447ad6
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:39:59 +0000

    activity convincing ever lean save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_list(array):
+        return list(array)
