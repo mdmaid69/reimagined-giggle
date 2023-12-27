a1235a6c566e6334835cf0703ee6623bcf631a96
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:06:50 +0000

    problem ring there in comment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
