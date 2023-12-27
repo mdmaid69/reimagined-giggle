4d74dde55dfe5810e7a36c3187f3c727f8d575e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:32 +0000

    lie star invite pass yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
