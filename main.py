3cd1c74238d8889c7600c2d2fea73b894e9a3108
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:40:11 +0000

    leg mouth interview post matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
