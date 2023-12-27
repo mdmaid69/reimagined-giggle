7f60bc4468f14762b84e55c1b8e8f37480b273a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:04:29 +0000

    system keepup any control progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
