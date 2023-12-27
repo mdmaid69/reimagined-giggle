66efa513f1a9184fca2d4e08e280f7988894edb1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:48:17 +0000

    pay polite see experiment section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
