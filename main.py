2e484c33f3c248dc6dc995ec3b259c154fce5f47
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:42:45 +0000

    task guide Gg before where

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def get_array_typecode(array):
+        return array.typecode
+def calculate_pressure(force, area):
+        return force / area
