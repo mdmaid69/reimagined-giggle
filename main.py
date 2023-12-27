28ece20ba2d8792f5cea2b8f111b77f5dc5fc171
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:37:34 +0000

    watch impatient turnaway accidental afraid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
