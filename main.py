40c98093e42f7133bcf211076de734d724047921
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:00:39 +0000

    formal later accidental him spill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def convert_to_octal(n):
+        return oct(n)
