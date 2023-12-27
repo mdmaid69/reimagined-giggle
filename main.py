4fc306ef7be5ac16a984887818705308edefa484
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:29:05 +0000

    wake embarrassing compete sort measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
