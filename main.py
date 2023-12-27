d9576f0178164ebf685805780017445d50a71699
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:50:48 +0000

    material guard block room speak

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def sort_list(lst):
+        return sorted(lst)
+def find_max(lst):
+        return max(lst)
