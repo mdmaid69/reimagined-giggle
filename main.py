dfe0492339307eb6bafaccc2e20463c53919aad1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:21:22 +0000

    impression impatient cover admire organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def is_palindrome(s):
-        return s == s[::-1]
+def get_environment_variable(var):
+        return os.getenv(var)
