73b818f45f306c3fb140aed3afe69fd34149699e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:34:47 +0000

    fast choose murder exactly rush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_item(array, i):
-        return array[i]
+def find_min(lst):
+        return min(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
