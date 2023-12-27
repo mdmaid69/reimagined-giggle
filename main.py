9bbceb0cda1ab6474d373383b9a8f1b59edccea8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:47:44 +0000

    recognize contain brush noise suddenly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
