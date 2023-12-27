b6154ea3a54965f8c0880aeff1e801ba1550955d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:23:33 +0000

    avoid probably print friendly matter

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
