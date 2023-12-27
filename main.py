6117e3cc1054c8ac53248859cca3dcec5a5ae644
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:00:09 +0000

    either fire brain conversation legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
