724896437ee1b1c04bda26e54718d5cfd7e6aaf2
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:46:13 +0000

    beside live immediately shrink protect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
