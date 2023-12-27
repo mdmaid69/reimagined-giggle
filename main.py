ac69a6f0f5d4d492448c70fe7d23364413bf35c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:36:18 +0000

    film exit trade pupil amount

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
