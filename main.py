63b54280f798c562f521db081cd56b143fcfbcdb
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:54:55 +0000

    sink pipe alone recognize journey

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
