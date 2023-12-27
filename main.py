d671cdfaa9c7d89d6a6a15dcbfaca0561a9e4e2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:40:59 +0000

    wind short capital authority design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
