e4b5b0dd6aa1b062a1763dffc5e7f0ff22b5ee7c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:49:30 +0000

    566end load inquire birthday Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def is_palindrome(s):
+        return s == s[::-1]
