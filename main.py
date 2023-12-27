a8f15a6be06793f31c66ec82b1c686d478d6569b
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:35:10 +0000

    fair lie sorry brain before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
