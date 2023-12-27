78e9e352a9596d9f77bacacae2dabff9f04560a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:21:17 +0000

    pain agree show score principle

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
