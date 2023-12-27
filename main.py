4528ef9548bdd43ef09585cc49f5a14aab36854a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:53:16 +0000

    steam club marry certainly punish

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import math
-print(math.pi)
-  def multiply_numbers(x, y):
-        return x * y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def subtract_numbers(x, y):
+        return x - y
