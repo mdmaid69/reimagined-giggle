312bc136da354debab1c7754309882468e5b9b22
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:22:37 +0000

    important cool paper direct open

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_binary(n):
+        return bin(n)
