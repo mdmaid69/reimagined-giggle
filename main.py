4a7f889a734f12576ce338208b586ba8bb07dfb0
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:16:02 +0000

    confusing Ll drink damage further

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
