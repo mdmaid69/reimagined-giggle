78a9a12f8819b16681d8b2309b5b6ac63d71e979
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:47:20 +0000

    deliver face train plastic feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
