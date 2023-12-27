af2dbcc17595a64e2633ff9b481f2f85365c8d88
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:09:21 +0000

    run care patience base win

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
