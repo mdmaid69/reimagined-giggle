60c3f6894e8455e0be4c76a8cb475c7a3c14db7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:59:03 +0000

    fit occasional pretty temper swimming

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
