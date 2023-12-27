443224cb1f6993f40d6c8bb687122107b4abe0ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:17:15 +0000

    wild bottom keepup curious distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
