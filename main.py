f198f946a61095a647a7b57bfc5a6c1a67830bef
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:08:54 +0000

    past plastic free mess getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
