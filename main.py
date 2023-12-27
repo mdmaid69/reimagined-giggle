1ff8bac34650c15bfdf8867c1a2b70ce44166492
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:10:27 +0000

    computer disturbing force impressed question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
