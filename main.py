33faf6fd74b287908dacb68c07e836add4beb14b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:06:31 +0000

    that previous keen uponon repair

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_square_root(x):
+        return math.sqrt(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
