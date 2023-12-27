8b7ec665b45e134b77763de19d30c001465cbedf
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:05:41 +0000

    range niggaacademy study tend somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
