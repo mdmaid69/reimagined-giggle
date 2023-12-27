c7e982f8a1468f9cf91a815fab79349c323af9aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:27:59 +0000

    people pinch blame unite child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def is_even(n):
+        return n % 2 == 0
