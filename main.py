92bb0fa78a38e7d43d874affedd666cebdfc361f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:57:49 +0000

    rate Ll sit take cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
