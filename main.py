c01b41f30362bb6e12409015faf8b92ae8e47e47
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:49:17 +0000

    sale valuable address path look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
