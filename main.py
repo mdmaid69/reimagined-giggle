6b64533f26878c9a70ebe5fcee18796251971988
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:47:31 +0000

    actual order pay embarrassed conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
