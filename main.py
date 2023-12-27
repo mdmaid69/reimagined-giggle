476f93b14c06e0fc5ef31fdfdeac6ab8d37d844a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:27:33 +0000

    bang clearly possibly children uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
