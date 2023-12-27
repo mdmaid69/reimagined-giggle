0b3ff3a42e7afed9553cbb7b232489611d55ed9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:11:14 +0000

    trip worried just operation such

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def convert_to_octal(n):
-        return oct(n)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
