9b98381051f8481345cfccd452592fcd7429b64a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:47:41 +0000

    pupil draw tap exist cut

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import numpy as np
+print(np.array([1, 2, 3]))
+  def count_elements(lst):
+        return len(lst)
