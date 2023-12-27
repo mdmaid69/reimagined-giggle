75379de896ca2d05cf8020a17448e9d166812f8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:11:28 +0000

    division beat finger tool guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
