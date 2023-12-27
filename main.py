a5ccad98d5c0c0a01512cfe993d0c29495da60c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:59:08 +0000

    subject hurry morning inside best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
