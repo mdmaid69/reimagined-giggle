230c5bd348acc5dbb19b24cc3641663b3b8d4cb2
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:58:27 +0000

    healthy crime something hole hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
