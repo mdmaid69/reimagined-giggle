17b69c88911e177d0b2971ba907d630b262bcb9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:02:26 +0000

    below card warn woman small

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
