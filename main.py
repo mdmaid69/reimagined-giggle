939cd00ccdc31c1b2ddebac3e8a571a717f9c3b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:17:36 +0000

    sister usual so through Tt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  def square_number(x):
-        return x**2
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
