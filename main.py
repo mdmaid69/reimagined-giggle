88ea322a23bf87812fe1eb5ff5f17b155aa0dc44
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:48:14 +0000

    die name promise hate male

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
