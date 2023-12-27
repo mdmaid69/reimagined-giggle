c2402254a34fad3a0c84200617b44f5200486c43
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:57:16 +0000

    least worst natural care thank

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def cube_number(x):
+        return x**3
