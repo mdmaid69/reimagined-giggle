d355a2c154789ba20e8ee40eee72bb0d169184ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:15:43 +0000

    wind aim attractive callback even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
