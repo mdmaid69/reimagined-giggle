baae391e86969a6a4b5e4a2353fdf00acb6d4cd6
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:19:45 +0000

    either serve keen back hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
