22dd341a8c9d5acefe6881b2a27d1df07420fed2
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:42:44 +0000

    certainly hello rob runaway mass

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_pressure(force, area):
+        return force / area
