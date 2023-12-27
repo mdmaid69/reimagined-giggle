f152f31536eda5318ae85d216e189b63628d88c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:30:37 +0000

    favourite contribute desperate spite dry

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
