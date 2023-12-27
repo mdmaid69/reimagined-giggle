51cf3dfc3b8649daa612a1ffd8802525a34eb050
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:50:57 +0000

    operation admire peaceful instead delicate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_power(work, time):
-        return work / time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
