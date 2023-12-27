9a57193c03f4bc16c5c569ee871fd04c7064387c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:47:28 +0000

    adjective sudden branch bar breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
