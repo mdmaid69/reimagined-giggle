ec1e26e95f3157041a3eb660407472c4757084c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:57:40 +0000

    normal talkoutof problem plate conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
