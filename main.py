e49516aa038770e3dcb644b01d1704dce78c7ffb
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:53:55 +0000

    shake union month nigga trial

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
