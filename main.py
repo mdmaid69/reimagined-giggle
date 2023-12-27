d1d0eaccb8e88e2fe079e2c98fdcd94fb973d09c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:02:53 +0000

    sure tune scare rare explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
