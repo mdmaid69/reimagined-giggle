a2d44ded7bda542a70d488e58a4688d501f807bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:30:15 +0000

    room bath pass knowledge through

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
