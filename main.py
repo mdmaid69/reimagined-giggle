f34dc4214a8b9204f939cf9b9bdc522c747c937c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:18:48 +0000

    admit normal holiday bit transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
