85e05b2d9d288edab8228df191a75ef756617fbc
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:40:51 +0000

    join target precise day moon

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
