310379fa7fe7397a8572f2b5f2f6b926fc3b2ae1
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:05:01 +0000

    time once answer poor blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
