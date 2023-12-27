619be793b470b1f77d8ed7801d72e2837e8174c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:27:27 +0000

    scared colour if stone future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
