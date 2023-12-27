77e690cfce6bddefd5b314bb52a5221a330e072c
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:13:30 +0000

    quickly mouth possibly hard bush

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
+  def square_number(x):
+        return x**2
