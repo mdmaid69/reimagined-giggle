125ea344e1f0b6c3ff0690690eb4752b5d9f0dce
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:40:16 +0000

    fresh pretend bin attach cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_odd(n):
+        return n % 2 != 0
