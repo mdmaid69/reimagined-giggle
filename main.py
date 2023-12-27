0e80bfe23da43d297451022236dffdd138d20479
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:06:31 +0000

    total factory strict clearly recognize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+for i in range(10): print(i)
