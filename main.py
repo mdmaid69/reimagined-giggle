4f126dc935c50eb50385cd7f3a4e95b1d5ed9675
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:00:54 +0000

    disturb blind relax anxious wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
 def get_array_as_set(array):
         return set(array)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
