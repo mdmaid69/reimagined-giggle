64c61ed74e1d5a2410f2b407101b63ff24564555
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:05:21 +0000

    guilty pull remain cause invitation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
