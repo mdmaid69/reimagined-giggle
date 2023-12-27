beab81eae6a8dbef951ea92b0510d70ebc1a37d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:46:42 +0000

    land enthusiastic study attitude memory

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def add_numbers(x, y):
+        return x + y
