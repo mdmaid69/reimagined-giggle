e52e4e28d008ed7d762bca0a8980c11da4c76ab1
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:43:44 +0000

    gooff free explain help study

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
 numbers = [1, 2, 3, 4, 5]
 print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
