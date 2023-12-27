d1aa16374f2a48295df79d3dab13753e94c184b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:59:48 +0000

    scrap wire classroom level holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
