b67f5389d94b3d95e08949fe42b7968ff70fb92c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:38:35 +0000

    bet argue view public offer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
