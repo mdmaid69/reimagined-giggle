97b4a2cea1177d3c3b08712983eccf0e8112af95
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:24:12 +0000

    class determined clean tune month

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def add_numbers(x, y):
-        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
