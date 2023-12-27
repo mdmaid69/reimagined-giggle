b3b8051a922f1d1736b8126077d1aeac48ca34af
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:43:45 +0000

    worry mistake climb present small

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_work(force, distance):
+        return force * distance
