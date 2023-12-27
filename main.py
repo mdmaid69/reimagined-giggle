4b727b394bf7e0cc5ed54a7b472f864e8b6be346
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:19 +0000

    tone play where bringforward fun

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
