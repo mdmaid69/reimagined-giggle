c19447c65121b43650802cc724faae312b879fd1
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:04:22 +0000

    interview nonsense side quantity behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
