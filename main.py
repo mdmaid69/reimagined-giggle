c89408f417dc7d440e4f80324ee053fe7454725b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:41:52 +0000

    lean previous its whatever one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_as_str(array):
+        return str(array)
