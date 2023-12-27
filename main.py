6ce65b4115a317576bcdf0439f4020203ebe811e
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:51:43 +0000

    use face offer shock service

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
