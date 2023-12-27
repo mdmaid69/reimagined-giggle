db5f1b4b271adbcb748a2baae0e22ecf8bc3e52a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:50:01 +0000

    butter below continue imagine loud

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
