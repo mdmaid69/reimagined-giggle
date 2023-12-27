aca2b65f501f831437376e5c4bd17a2b250b9f11
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:19:09 +0000

    crash noone joke not transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
