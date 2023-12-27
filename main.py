dd1ab05689e39eca63d725e6388d7b408626ea4a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:18:11 +0000

    influence secret finish hit rent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
