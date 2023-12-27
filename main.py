1d767f29759577e148f99e66d2b27bb2442b42e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:44:37 +0000

    test rain spring tidy about

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
