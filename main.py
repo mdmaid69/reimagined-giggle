06561d0694ff4b1f2876ce3148fe6ff7c4e00cb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:59:29 +0000

    callup equal punishment anywhere good

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
