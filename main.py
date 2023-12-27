c40ce6c8207ad39da33aaf748cf4537f1ad08edd
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:13:24 +0000

    trip lead without difficult clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
