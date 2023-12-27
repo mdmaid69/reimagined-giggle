d71433ec97ab482c21046295b52c1a0bb2dc1875
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:04:10 +0000

    artificial spill concerning reaction pretty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
