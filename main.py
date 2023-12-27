53038a911f2d165820806e280c9b15c303083f6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:45:07 +0000

    reduce limit fierce opposite summer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import tensorflow as tf
+print(tf.__version__)
