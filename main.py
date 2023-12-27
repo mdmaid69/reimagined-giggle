4831915d9dd85b3bb6e3a121379e281a22223315
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:19:08 +0000

    embarrassing violence team race upwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
