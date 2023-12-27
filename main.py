4cc7a385995a29956cb5fc16a1574b0890c6a263
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:09:18 +0000

    shut turnaway curious moral good

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import sys
-print(sys.version)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
