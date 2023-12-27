7c99ec32723fcfa2b8d297c757488a043ee92bf2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:56:44 +0000

    congratulate ours change book compete

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def find_max(numbers):
-        return max(numbers)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
