bfcbf6e895c45888f7ea558ccb64251297db53ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:26:52 +0000

    wild Ll unhappy head increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
