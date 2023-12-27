6d1086c0be1354f5ad270d5b92bb2bd893610924
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:23:09 +0000

    breathe equal broken get duty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
