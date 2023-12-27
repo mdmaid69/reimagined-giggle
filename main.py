aea8add212fedf0a0393ab0d88ac466af42f74a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:17:59 +0000

    ball marry thumb smoke protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
