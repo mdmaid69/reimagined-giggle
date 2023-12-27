3deb4cb979b6ac976e457d762a2b0abfab0e39e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:31:04 +0000

    sure apologizeapologise honest moral autumn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
