4a6a15f7783ebdf4a834817cc667269d91f6120b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:45:37 +0000

    private shoot borrow picture exciting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def split_path(path):
+        return os.path.split(path)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
