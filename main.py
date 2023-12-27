3df930279775ffd8a5d6a47a6ac4d95d3274931d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:59:48 +0000

    warning concerned call exam track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
