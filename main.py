3ca16fa01221317185d1e780de44c80e3ac1a9df
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:53:27 +0000

    ready stone behave murder possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
