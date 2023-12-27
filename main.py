168bd7a55e7d67e23b3099f7d367365fbb398a58
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:37:08 +0000

    undo variety time beside trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
