30fc47b36a23166a352b7c0a165f96d0719c47cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:01:03 +0000

    conclusion under buy charge slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
