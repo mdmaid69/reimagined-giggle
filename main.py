70e8258fe79058516cc0062d4611e5a95ff540f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:37:37 +0000

    pretty congratulate hear hurt bank

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
