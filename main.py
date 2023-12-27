6f97adf551017bc2675b5eb5f1855040a6fa0778
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:48:56 +0000

    happy area hers distance people

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def reverse_array(array):
+        array.reverse()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
