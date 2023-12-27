007e97cecee90b357ed79fdc126156eb396e37cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:07:56 +0000

    argue declare brush nose plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
