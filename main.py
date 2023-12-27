8b9f365d9e0d3fd93b190227714afabe7c290b3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:53:15 +0000

    divide discuss question surprising science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
