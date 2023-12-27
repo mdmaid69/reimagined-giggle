28db2b32eeffb3e3401be72922d0ee6376a2f3da
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:11:53 +0000

    wrap become temperature dance fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+for i in range(5):
+        print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
