9a005afdf65a45847681324a7eba420c59c41852
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:40:58 +0000

    interview string exchange benefit period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
