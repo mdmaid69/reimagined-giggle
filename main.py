5a491e5c5e6b9459807f9ee2b8fd2b2542d6bc1c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:59:10 +0000

    blame Mm behind press replace

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def print_python_version():
-        return sys.version
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
