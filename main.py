1748363be1db83975583ff73e0418bea35a43d9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:54:49 +0000

    wide ourselves sort sound goup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_base_name(path):
+        return os.path.basename(path)
