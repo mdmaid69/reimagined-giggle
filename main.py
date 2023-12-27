51e6de36b8bd2ad0d5f8587b9713c9fe605cacbf
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:14:33 +0000

    set jump arrangement plenty partner

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-print(random.randint(0, 100))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+for i in range(5):
+        print(i)
