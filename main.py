93386886d8b33bb9aa2a68736031108fcb5b7830
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:50:36 +0000

    Qq threaten result single fear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_list_from_array(array):
+        return array.tolist()
