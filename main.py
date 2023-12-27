324ea39b0a748b513ab61c0c2139ecc5dd1ee71e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:54:53 +0000

    pump willing age bound cheap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
