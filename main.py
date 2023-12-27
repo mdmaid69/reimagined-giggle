c733fe0d7909246a5428f0edacef8669af3f84f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:39:17 +0000

    beyond between bet occur disappointed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
