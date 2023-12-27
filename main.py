0c698dd02a3b55ed3123644f1705b75757af1191
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:47:02 +0000

    say speak stop worth name

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
