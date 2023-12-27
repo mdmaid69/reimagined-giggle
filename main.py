675b29960f183a9539ac5bb64daaceb615fda297
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:36:13 +0000

    deep even various rush favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import time
-def get_current_time():
-        return time.ctime()
+print(time.time())
