bdb5098d9224eac66868ff102bad9c9e4477af2a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:43:50 +0000

    offer government Ww test correct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import os
+print(os.getcwd())
+def is_odd(n):
+        return n % 2 != 0
