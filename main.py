366bbb36926af013e5020c5fade7f0c9e7afa3ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:18:44 +0000

    slightly half worry sale argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
