e20856868ce77c1e8e075ff7b9f389856762cfab
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:22:53 +0000

    clear rush wet quiet estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def convert_to_octal(n):
+        return oct(n)
