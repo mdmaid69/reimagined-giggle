da9a48945e07b9553cab45a5c33e8782e8aec2c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:36:11 +0000

    public live official disappointed push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
