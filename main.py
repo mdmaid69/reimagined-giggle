c83a449069888bcb2ea5de2630bf288aed28eb82
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:28:18 +0000

    holiday taste sort describe instrument

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
