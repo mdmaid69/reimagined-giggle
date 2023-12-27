872bfc85f303704f8bbf4d8492968bc04ac430c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:18:00 +0000

    like container speech face someone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
