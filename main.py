620559f4471608a2adba3aaa3137fb8ce4b405d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:45:23 +0000

    key sport people base behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(a, b):
+        return a + b
