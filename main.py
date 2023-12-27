d7545d2a22096082a48b841e53a5903d90ef2b0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:35:37 +0000

    practise none his suggest few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
