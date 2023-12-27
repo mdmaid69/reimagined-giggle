4458aa4d95e80e5e50153bafb5c90da5fe3200cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:08:52 +0000

    poison visit bear top precise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
