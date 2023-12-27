880c965e0d299fb26e48472f790ad3e29b48a031
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:17:20 +0000

    goodbye worried without left actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
