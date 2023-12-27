d032a6f7a11ac593003aa6e6d5806591601c6a7d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:49:12 +0000

    depend danger thank excited floor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
