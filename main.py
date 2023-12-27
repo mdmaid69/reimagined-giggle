b15283aa727289442201acca725a66453305a6f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:14:06 +0000

    path feed positive guilty mad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
