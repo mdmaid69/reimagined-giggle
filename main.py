2f33ca72eafcc7b3d27b0d47966dc14c3b475dd4
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:00:19 +0000

    decision organization especially phone test

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
