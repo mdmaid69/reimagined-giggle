0b52b5cebab4f960bdacb900209e7d86b7cf5153
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:09:55 +0000

    over very giveaway confusing front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
