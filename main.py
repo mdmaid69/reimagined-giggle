51d0bfde6ed44e7cecd1c10bafe960c2b810ef79
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:20:42 +0000

    flavour method mark stick best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
