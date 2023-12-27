06ae72066138cb3b179ef1d9d5dcc85ef70b0b09
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:44:00 +0000

    forgive truth swimming stiff guard

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
