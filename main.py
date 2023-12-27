ee7dbc27b1c9727c455bd8d3418b2f4e6f460e78
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:15:02 +0000

    combine spray back seem arrange

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
+from collections import Counter
+print(Counter("hello world"))
