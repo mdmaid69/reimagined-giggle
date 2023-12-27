50abc0ed566f89b60d8defc3c7d24a9896ca34c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:56:20 +0000

    something same broad loss wild

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
