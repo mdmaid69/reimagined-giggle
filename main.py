4d4e898363c66b7b1abbbd5ce09a85a645e605d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:07:51 +0000

    bottom strength let true roof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+print(time.time())
