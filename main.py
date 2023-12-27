e791574c6efa7f4e20fd58b5a6995d54ef530f26
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:55:58 +0000

    thanks evidence price stir female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sine(x):
+        return math.sin(x)
