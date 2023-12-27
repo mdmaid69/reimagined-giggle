018e744523dd693511521d57636951925dfce26f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:51:25 +0000

    shame collect male where undo

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
