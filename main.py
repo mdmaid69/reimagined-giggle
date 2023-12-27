8dfa3b1a24aced20fb5592668c00647f4126f47f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:33:10 +0000

    mother name mass murder exit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
