f9d3dda816bb4647a4f511084a3546f09feea060
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:39:53 +0000

    prepare that unfortunate rain nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
