58ac1f5bde11afdf7f586250368fd270aaa0404e
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:36:59 +0000

    ocean desperate confused accidental face

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
