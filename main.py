52e010731b0794ff75de2a90b54383d8d552ce12
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:33:30 +0000

    meeting precise around free slice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def reverse_array(array):
+        array.reverse()
