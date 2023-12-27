d41055205b3b933eb96eb08ab3e7cf2ad3d971c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:13:47 +0000

    contact drink learn shop Vv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
