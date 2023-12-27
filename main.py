5e457cccf6923ece934902f780d6122f3029d9ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:14:25 +0000

    use bath target tip tooth

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+print(sum(range(10)))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
