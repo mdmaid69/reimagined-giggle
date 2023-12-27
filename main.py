4168ba4a64401b69714b712123df96b7a1385ace
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:26:37 +0000

    after up certain that to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
