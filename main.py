65881d420af237b5fd5c19c198c4b0bbd56cca70
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:35:06 +0000

    opposite indeed boring ticket present

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-print("Hello, world!")
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
