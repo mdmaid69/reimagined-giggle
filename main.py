9883d3a39856028ec63a9de7fcaf837535da7e7f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:52:54 +0000

    tear model apparently post sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def is_palindrome(s):
-        return s == s[::-1]
+def get_list_from_array(array):
+        return array.tolist()
