32b14d865e41c7a76cb136ddb246cb2469813953
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:05:49 +0000

    turndown suit foreign makeup sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
