79bb939f3d7007f31095293471ffe913cfa1ed6a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:48:09 +0000

    action lock unfortunate lie design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
