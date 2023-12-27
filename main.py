efcf4d40dbfcfee4ba3ff68e90a159f3ffffff8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:25:00 +0000

    habit recover tight minute visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import sys
+  def get_python_version():
+        return sys.version
