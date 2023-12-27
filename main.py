73e472da179cbaf7ccee726421fa7b5b0090021e
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:45:50 +0000

    talkoutof ill protect pay frame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
