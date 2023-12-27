fba53b0ee946df740c0424449f0cb29319e8e830
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:11:28 +0000

    regard sure kick unlucky back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
