1ccb29370453bd564acd4968d89ba4ab8e328d0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:28:20 +0000

    sail account line broad blow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
