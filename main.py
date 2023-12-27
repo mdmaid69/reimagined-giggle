4c157eb8883f7d9bd10c2837f7ee37e0cdb58c3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:50:43 +0000

    beautiful suck any sorry kill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
