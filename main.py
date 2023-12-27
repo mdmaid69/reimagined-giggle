f95e2263c468b1f9bc8097b521162b6764391a7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:59:22 +0000

    opponent process I ticket meaning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
