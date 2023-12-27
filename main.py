3a70f32a654fe411adf9cfd92046f192ce82d0ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:17:09 +0000

    substance hurry expect sting practical

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
