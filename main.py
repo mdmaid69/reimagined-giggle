1d17f339205f7c1c2dced09e879c9e233dc5017d
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:53:52 +0000

    seriously breakfast fill trick tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
