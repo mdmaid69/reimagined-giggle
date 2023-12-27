44a54171b3887acf58445a7e5bd4b5549163ca64
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:02:55 +0000

    upset anything street sensible back

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
