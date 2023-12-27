69af9233ca7911c9d936d024bcdc744b55285f11
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:18:34 +0000

    no yourself small pretend cum

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
