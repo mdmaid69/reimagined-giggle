f27cf3c5ac79a01a2039f0d8f209b9d0c8404cf9
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:38:47 +0000

    basis rid somewhere remark excited

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import random
+print(random.randint(0, 100))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
