e62a329aa980adb00b2d494961aed703a66491ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:17:05 +0000

    only medium stone hospital steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
