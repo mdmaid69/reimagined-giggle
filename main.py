e0d48687ca81a21e6072337b565eb11321bbd90b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:56:12 +0000

    part adjective original edge plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-def get_time_since_epoch():
-        return time.time()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import sys
+print(sys.version)
