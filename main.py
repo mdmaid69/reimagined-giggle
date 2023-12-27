27a72361a9032ff3a3a255fc74ce8cd436e51ca4
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:19:45 +0000

    flow house first human trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
