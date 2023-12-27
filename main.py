81778d1780393eacb3ae95facff03cabfb0842c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:09:32 +0000

    somehow obvious empty finger love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
