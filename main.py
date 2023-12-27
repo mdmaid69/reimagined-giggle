6cc4ff5ee2d6bd6d5c5b2f7ec288ca963fb976d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:18:19 +0000

    pleasure stone some stranger thinkof

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
