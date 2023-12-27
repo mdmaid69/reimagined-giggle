c33394ff60ad5b8c587c5847b02dc0902a15cb29
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:54:44 +0000

    job Pp well precise nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
