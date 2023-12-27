8859015b6dad3e814a9c3e8d3453f43a233024a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:56:42 +0000

    fail wrong until maximum future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
