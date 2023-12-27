ca4640d710ca88eb63114d74f3adfa01a5a469bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:20:49 +0000

    mistake fetch open build inside

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def cube_number(x):
+        return x**3
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
