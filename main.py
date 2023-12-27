237ab7bc331dd3a70b8ff82960b2203b6dd21eac
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:38:28 +0000

    prepare autumn would finger fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
