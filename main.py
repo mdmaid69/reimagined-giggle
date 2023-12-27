79a8d44e3d7940458dfd3e4ff17a6c6996662375
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:14:14 +0000

    usually cause new breakin shine

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
