9fd0324fc86feaa4fdad98e7964cc898f01e41bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:19:56 +0000

    active whichever dream impression sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
