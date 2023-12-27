08f3f2402a9bd116cde2cc451c042ba4198c8834
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:49:42 +0000

    treat lookup today influence Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
