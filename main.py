5c98d8f57ec115b9be35a0400b1877641c57e49c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:47:44 +0000

    deep reason but inform build

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def reverse_array(array):
+        array.reverse()
