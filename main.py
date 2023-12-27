e8bf1fc22cfbe2ec1f7a7aa0e4aa39589db7818e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:13:54 +0000

    build dream behind history whenever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import time
+print(time.time())
