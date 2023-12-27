3cc325dd54a7f066cdcbc9939144a3ea116723ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:48:02 +0000

    perfect instead certain secret gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-for i in range(5):
-        print(i)
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
