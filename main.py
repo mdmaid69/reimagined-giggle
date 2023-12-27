6517e615ee60c6f11f14bb92f1564f4f721701fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:08:20 +0000

    it crime all damp war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def exit_program():
-        sys.exit()
+def convert_to_octal(n):
+        return oct(n)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
