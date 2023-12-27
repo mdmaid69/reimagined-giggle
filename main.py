de79c38306ee81f80fc55138bbbe96c6b4f59a0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:41:05 +0000

    bill wet strong explanation friend

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Characters:", len(text))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
