32720ff8c946ace6e1ab82c3b2741d02cf1578e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:20:11 +0000

    unfortunate announce positive love safety

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import time
+print(time.time())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
