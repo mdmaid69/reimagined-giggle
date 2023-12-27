324a6f8ac31c3676bc4b914efb4981540484ed8f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:51:06 +0000

    upset skin start concerned clear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_length(array):
+        return len(array)
+def calculate_acceleration(speed, time):
+        return speed / time
