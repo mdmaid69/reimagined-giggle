1c5291b75de4ba597791228b65fac618640cda83
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:05:23 +0000

    amount right grand along complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
