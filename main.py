c4c06c481a30e892561fceb5826a2d345dee38e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:19:38 +0000

    eye contact disturb pencil guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
