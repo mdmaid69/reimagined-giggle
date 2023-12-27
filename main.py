5b190ea9e8a9072d70f4494e4dcb2cf26ffecdae
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:28:14 +0000

    may unpleasant minor birthday wet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def is_even(n):
+        return n % 2 == 0
