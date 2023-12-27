46a1f669824d5a98ae49d70f775fae368a88699c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:00:33 +0000

    few blame plastic congratulations wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def reverse_array(array):
+        array.reverse()
