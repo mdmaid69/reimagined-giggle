fad78a3b9545a1c2b57400f424dd540153745022
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:53:57 +0000

    because second moment what pass

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
