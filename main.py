5631a619503e3c368a497c779180ab9f506fcf61
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:33:45 +0000

    Hh exercise passenger blame office

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
