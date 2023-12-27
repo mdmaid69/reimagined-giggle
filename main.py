e2aa99b6e56dc849daf50f2abef762b675177956
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:31:46 +0000

    glad limit confident claim close

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
