662f2bf19075535fd5a0a79de22094a69d8c4cdf
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:47:23 +0000

    world description ignore spray name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
