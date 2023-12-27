ca9a94aefc1a934bb24afb52b43931bf671f033a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:25:30 +0000

    holdout group advise you top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
