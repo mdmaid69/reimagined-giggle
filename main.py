1322d1201b3aa1ea6a86143f0c98057654962127
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:17:02 +0000

    surprising street physical belief listenfor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
