ae1a02bb4244af30466a10a12d4a815b118deba9
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:53:10 +0000

    answer float unkind unfair middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_palindrome(s):
-        return s == s[::-1]
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
