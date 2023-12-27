f741c86ce654471909fcb098113eab435aa7798e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:34:40 +0000

    make question 567enemy determined play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import datetime
-print(datetime.datetime.now())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
