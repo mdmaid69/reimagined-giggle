c9b724747d144dec7a37f7eeda5ee3aa4b5c2f0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:07:29 +0000

    close pronunciation fix turnover throw

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
