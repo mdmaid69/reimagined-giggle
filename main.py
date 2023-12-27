9eda7f36af157a0286880a7b35e223de87f9f5ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:17:54 +0000

    boil club medium comein stare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_pressure(force, area):
+        return force / area
