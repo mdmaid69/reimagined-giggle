eefea9036c2023103120b3eaa30827a82bfd2495
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:05:56 +0000

    ought him answer only manage

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_base_name(path):
+        return os.path.basename(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
