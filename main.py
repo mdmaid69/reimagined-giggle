32a5d880df99ea86c3fe5870e167da791a71bcfe
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:13:11 +0000

    afford first guilty speed close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
