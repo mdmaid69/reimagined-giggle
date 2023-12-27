4307c80c2ffcbd92259aaaece106183c1ff1fb82
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:58:26 +0000

    unkind embarrassed getout tool mainly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
