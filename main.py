c66a9ed94c4e21895af86ea32ae625406bb4d6ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:01:36 +0000

    Ff pronunciation wonder intend wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
