5c90c94b8a251c79a4112243e937344c44149f22
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:34:57 +0000

    minor fat action achievement comfort

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_pressure(force, area):
+        return force / area
