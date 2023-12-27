660513631edbf514c495f9d794b64702716e5390
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:29:56 +0000

    contrast frightened smile record light

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_volume(length, width, height):
-        return length * width * height
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
