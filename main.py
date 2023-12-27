80d3e61ee359b791600daa061b35d5b0cfe305a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:14 +0000

    unlikely 179bite ever change concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
