54094c66195a234af09a205c4d60da3b1902aeff
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:40:56 +0000

    practical commit wind fun otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
