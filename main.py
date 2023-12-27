054688f5ac4c50e26cd1365b2692ee4e31d1a6be
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:54:12 +0000

    spin exercise suffer wander apparently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
