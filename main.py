0a4e1ac56f4d1221dd3a4e7ce69b0d2185cffd84
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:03:20 +0000

    572enter travel measure loose water

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
