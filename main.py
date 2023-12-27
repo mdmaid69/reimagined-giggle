1df0c6043760fbfe184734d4b1c702b52556ea12
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:58:56 +0000

    market original copy hurt hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
