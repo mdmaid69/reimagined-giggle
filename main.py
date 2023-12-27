51a3e53305d6aed892e3fca8ae436ff9e9d480a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:04:16 +0000

    left sensible favourite sweet period

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
