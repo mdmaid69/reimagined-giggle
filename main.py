034679acb765544d2e0063d7a50d6f887e0e031a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:53:13 +0000

    patient far destroy bag pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_list_from_array(array):
+        return array.tolist()
