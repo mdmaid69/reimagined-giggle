9bce87df79018117cace10970b642b09cb11dc99
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:54:16 +0000

    visit smoke though murder side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
