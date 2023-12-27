3538b9b084e70377dcd4c6180337abd66cfdcf7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:13:46 +0000

    apply pour will development honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
