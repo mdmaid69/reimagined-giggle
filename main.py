ba2356bc9236d06b50e11e6d816556359f5ec582
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:14:42 +0000

    anxious behind skirt rent dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
