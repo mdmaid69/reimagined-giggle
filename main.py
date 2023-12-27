30f9569d63fb01ea913319845e0085172a0276ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:36:15 +0000

    play measure permission concerned roof

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_tuple(array):
+        return tuple(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
