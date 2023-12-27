1d81e0e174469005634a325cce48b89efed7bc28
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:40:31 +0000

    relative deserve large mass so

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+def convert_to_binary(n):
+        return bin(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
