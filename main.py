e56f470b7b12df52c2c704d725d5282b61df8ce4
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:10:47 +0000

    wind indeed deserve over range

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
