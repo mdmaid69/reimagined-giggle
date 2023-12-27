8b49aa96a8fadb711d137f4fd82a21f4e681deb4
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:49:43 +0000

    pinch responsible relief whom strike

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
