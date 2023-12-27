045a2d5f015ef54ec4182b45dc35c3e261b281e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:03:53 +0000

    character spin roll gift cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
