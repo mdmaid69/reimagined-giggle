f444f5f3eb1115679b92ec64a5bb9ecc859595f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:47:57 +0000

    terrible getup confused month exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
