2e6b72f99d0ec692773aa315e4d6e0921f6f1503
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:27:10 +0000

    wet embarrassed block cover Pp

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def subtract_numbers(x, y):
+        return x - y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
