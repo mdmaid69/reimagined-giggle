2625d2e4d02bb953ffd4ce44a76ae56fafaff17c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:16:49 +0000

    careless arm about suddenly best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def calculate_area_rectangle(l, w):
+        return l * w
