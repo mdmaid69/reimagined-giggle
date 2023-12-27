7565ca474d60d2fbefaace6fb365871506b11134
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:46:00 +0000

    maximum pleasure teach the bottom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def clear_array(array):
+        array *= 0
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
