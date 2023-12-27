cfc9f2153b2954f1d7f90cec40fd120121bee767
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:52:10 +0000

    instead above wet occasional discuss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def sort_list(lst):
+        return sorted(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
