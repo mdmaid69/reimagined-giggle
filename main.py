dc4538e3d16b39d37f8803a2bc6c2cff148e9d2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:50:18 +0000

    brush pocket flow few lonely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+print(json.dumps({"name": "John", "age": 30}))
