8adf3ba1284c57a39dfc1dff53cab2592606c915
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:22:31 +0000

    fairly holdout suck state apologizeapologise

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
