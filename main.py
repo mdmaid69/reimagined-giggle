85c60dfa479228be73c4256c736322d3e2775807
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:26:01 +0000

    attempt winter standard check boil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
