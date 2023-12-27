b4ba50264c7ace771d50d586aa25efaf96a7096f
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:16:48 +0000

    half beyond arrange plate flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def convert_to_binary(n):
+        return bin(n)
