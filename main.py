bb3839542f22bae48b8e93bf9306725d8d45f049
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:36 +0000

    climb cause company once try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
