8179215ab5f93116ab53bd29fbbd978a97f0cd0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:53:49 +0000

    identify small team office score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+print(math.pi)
