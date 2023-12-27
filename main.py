727e7455a2db24698f8b695801683a4566e94108
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:12:33 +0000

    task face view occur runover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def sort_list(lst):
+        return sorted(lst)
