ae0bdf752d425302d9bd9b0da9179d362d878505
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:18:21 +0000

    advise by final anywhere force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
