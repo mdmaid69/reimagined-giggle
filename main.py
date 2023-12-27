aa831e97a3411ede27cd6810290fcb7f85058eb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:36:26 +0000

    generally be shallow handle future

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
