573a3c1a28b0756c17df95d27f2d74361defe857
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:55:43 +0000

    turnback hot mention rough boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
 import csv
 with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
