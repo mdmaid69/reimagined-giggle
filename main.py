4838adb07cd0ae4252203bea53c2d6370ed1c570
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:11:54 +0000

    of before relax drive desperate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
