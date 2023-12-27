ba9a27d3fce0dea5b0a04cbea6e0f2b4d64b0265
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:21:48 +0000

    sink careless seriously beat discuss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def remove_duplicates(lst):
+        return list(set(lst))
