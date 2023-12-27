cedadebfd90ffd937b7502f5bd676fb82ad5de7f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:34:06 +0000

    early criticize tear rush comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
