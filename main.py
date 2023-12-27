e1f1725ffe8def4c8c9007c4e1d915f32d112768
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:02:46 +0000

    dance estimate keep bar strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def convert_to_binary(n):
+        return bin(n)
