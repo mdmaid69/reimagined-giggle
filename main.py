cb7b93925a3a8ae3e251dcb01d89532757f5f2f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:19:28 +0000

    valuable base available cut great

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def calculate_area_rectangle(l, w):
-        return l * w
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
