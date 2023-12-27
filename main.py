6c4d1813712a1c0d850d04a470204e31256aaf13
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:15:05 +0000

    plan Qq estimate instant vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
