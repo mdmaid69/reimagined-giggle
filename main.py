2d5125af844e0c07cabd923b1e0569e30d8f370b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:03:21 +0000

    your hurry worth attitude test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
