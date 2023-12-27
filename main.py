5e21bed56cd6624d80d9e3aa6c0f5ab8d4babfdd
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:57:15 +0000

    explanation interrupt needle flow tidy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
