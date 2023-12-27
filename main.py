25cd7f6ef3fc896eb678ec20fc12e47d7f10bdcf
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:52:20 +0000

    remain object shopping solve long

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
