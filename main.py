83d7200bde62915aa5ffd0e7204663473958566e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:23:30 +0000

    suppose case polite fast place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
