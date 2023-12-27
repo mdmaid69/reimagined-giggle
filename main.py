aa9b41b18d8eaa9a205c64d7c68d55765b0cdb91
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:44:21 +0000

    proud shelter name shut scheme

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
