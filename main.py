20343bca0cf151b6c239d4da3853348efc28bc3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:19:55 +0000

    attractive quiet drink central point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
