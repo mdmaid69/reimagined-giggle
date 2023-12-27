82a4628e8783c0fbc17c3dc9b9fe800c2a19b984
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:53:44 +0000

    heat together lead bath unable

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
