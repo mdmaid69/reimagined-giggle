c70a6d1ca66211026840fe68fb479f0227638fc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:36:09 +0000

    trial butter shape explain score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import time
+def get_current_time():
+        return time.ctime()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
