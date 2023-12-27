3ca1fcc76a3e03c68bc2fb22fc251444bfdb96d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:36:57 +0000

    party direct deal appear giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
