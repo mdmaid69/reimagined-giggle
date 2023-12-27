d256289120971ac15fd515e15c298c75c71faa24
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:50:28 +0000

    trousers rank little sticky deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
