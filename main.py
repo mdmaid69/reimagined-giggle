1ccff97a53a0c77e099044b709472ec2ee87769e
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:52:25 +0000

    copy shadow emphasize blame talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
