5a619602b29f8f7b4e811422dee76e6f334fe364
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:29:38 +0000

    worst follow introduction miss giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def find_max(numbers):
+        return max(numbers)
