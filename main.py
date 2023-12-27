899898ef0679a3fbabda20d897a78230d90240ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:32:43 +0000

    capital habit crash weekend lie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
