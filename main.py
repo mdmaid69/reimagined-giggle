2bb985f1d045d1c3827e97db34ba48ac2c8d2c77
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:17:43 +0000

    unkind fresh gift beautiful style

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
