95f4e58d0d43b62546f08cf7d9c15d31eedbd15f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:10:24 +0000

    concentrate reduction sport positive polite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
