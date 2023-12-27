16e24abdc840b99c0a10342a0eb6173c52e33b7d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:43:04 +0000

    angry full smile load honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
