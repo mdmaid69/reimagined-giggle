02cf380d60703abb4971ae43679a3608074dc020
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:29:32 +0000

    easy complete limit ask sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
