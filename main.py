7248e3a424d35e0cf1bbddc1652239506c027579
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:35:08 +0000

    cross organize cum recover pipe

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
