9b250c0cf27dbdc47a4ec3655d0ab9c6f7b6f48b
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:21:43 +0000

    food cool busy direct comeround

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
