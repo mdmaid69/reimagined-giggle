bbf3b9d9df91090959e20702b5bcbda0419abd25
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:17:12 +0000

    extraordinary land disagree lookback(on) occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_repr(array):
+        return repr(array)
