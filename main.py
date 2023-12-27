8f5a27dfda414bb6e34e29d096e42d7546ca27ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:03:22 +0000

    criticism tear pretend guess confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
