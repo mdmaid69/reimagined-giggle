b5d9dc1853249e4dd356c6a1aa3a5945a1346ba4
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:01:02 +0000

    neck worse measure rush glad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
