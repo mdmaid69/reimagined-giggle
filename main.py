41c04183183cd18b6c4cdc790d8edfc6bdbab416
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:59:05 +0000

    gooff serve dry lay action

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_speed(distance, time):
-        return distance / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
