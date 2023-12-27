6ed1fed36499ffbdd776b81986b54c16899ed96b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:10:29 +0000

    free foot treat excuse opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import os
+print(os.getcwd())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
