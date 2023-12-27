d4fc6995876520f3e76f3a472b41eee8c1695538
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:01:32 +0000

    sink come inform computer Jj

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_typecode(array):
+        return array.typecode
