8ba03d7474effdda60a9c12df49690bc243f1d2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:55:56 +0000

    ask spare load show possibly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
