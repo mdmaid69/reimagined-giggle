aedbd44f43811a75af59bef9f50a3fbb7a60bb5a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:06:30 +0000

    pupil damp personal minute inquire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
