f62844c90d8fb309e67568677df9d0261dccbbca
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:40:15 +0000

    cry with their spread himself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sys
+def print_python_version():
+        print(sys.version)
