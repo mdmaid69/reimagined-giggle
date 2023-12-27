16a10fafc6693fbf09aee54a13025248ac22004c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:43:50 +0000

    comefrom accidentally strike sleep guilty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_int(array):
+        return int(array[0])
