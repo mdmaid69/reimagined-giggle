40731ee7943f197a64508c5a44b571274e3fec13
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:05:10 +0000

    rate shrink separate space office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
