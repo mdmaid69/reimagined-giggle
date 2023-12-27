cac3364ee1aab03b2b682d760388d547b69bace6
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:31:25 +0000

    recent drop flight keen weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
