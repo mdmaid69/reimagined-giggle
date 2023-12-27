a97435467dc7910587c5ab18d3fecf385bdf74cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:42:20 +0000

    early worth wind hand watch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
