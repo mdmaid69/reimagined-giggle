6167d914350cdc33973f1f5879a6c6155b4b5e34
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:58:59 +0000

    basic term uncertain radio get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
