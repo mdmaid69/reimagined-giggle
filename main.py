ad2596ff8a2c5069fd106bc29f5e5155f095c7f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:22:44 +0000

    spite sink stamp match lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
