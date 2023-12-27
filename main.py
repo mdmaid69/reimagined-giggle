8abca8ffa9b7ec50ad7173573fc460d08f2e90e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:36:05 +0000

    giveout nice deaf speech play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
