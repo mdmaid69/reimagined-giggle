44f231b754356174df270a9ae0de2f8b382aeada
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:49:17 +0000

    unhappy because sky walk label

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
