183cbab44b20fff92e0b86c9f40cecd06e16897b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:02:28 +0000

    loose basic solve around occur

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
