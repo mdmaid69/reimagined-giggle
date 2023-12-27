3b7d2a311823771e83a3ac7db22305d28617fbed
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:56:54 +0000

    ring gas season listen away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
