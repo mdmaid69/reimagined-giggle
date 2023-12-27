5f11358651d6916b6c428558668dc3f19b7c1fd5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:49:56 +0000

    no turnover agreement agree lean

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sys
+print(sys.version)
+def calculate_density(mass, volume):
+        return mass / volume
