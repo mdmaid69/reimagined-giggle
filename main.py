5463bb18bc15743c05d995ce857693f7f4363df9
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:58:00 +0000

    related use fill actual each

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def find_max(numbers):
+        return max(numbers)
