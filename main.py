ac0b02c50b4d13bb9cfde610875c7f3a55e73519
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:03:21 +0000

    last sensible of slow minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
