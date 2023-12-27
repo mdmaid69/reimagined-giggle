4db59c38ddfd63b9d4777ddb4bd3113a6c19f9ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:18:17 +0000

    deep maybe trick scared rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
