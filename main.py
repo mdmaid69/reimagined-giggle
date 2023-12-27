6a44efff06c146e94980746d954105195d9d6591
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:38:04 +0000

    similar play value drive bother

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
