25aca4c1fcbf96b6ba4db91f6cbe2b21d59cb8ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:26:56 +0000

    recent rent Uu wide record

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
