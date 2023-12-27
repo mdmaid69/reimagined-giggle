5af5d28f91608a0965a8d8af577c00d42d3976b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:54:03 +0000

    properly niggaacademy inform turnon occur

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
