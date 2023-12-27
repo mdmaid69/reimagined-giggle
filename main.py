26f676d0b0573b29a4ccfdb85f1f175abccfae1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:56:57 +0000

    slice her such sharp choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
