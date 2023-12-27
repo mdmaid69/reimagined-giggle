21ba985a72657e2a599a163e999b9db987fdfc21
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:54:19 +0000

    hungry rather home immediate object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
