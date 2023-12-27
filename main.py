687c28c21ddbf183f39e223ab82062a5cb97b403
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:51:40 +0000

    treat concentrate content means reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
