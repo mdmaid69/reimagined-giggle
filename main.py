4aa9003938408f1662e5614f1e95a1531243767c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:22:59 +0000

    recover risk surprise no temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
