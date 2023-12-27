dbcd85146c6205d4629da07f2ab98390f183caca
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:07:05 +0000

    hold immediate to unit advice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
