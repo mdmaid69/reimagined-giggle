691ee2aa8a2823344012f53f97ca5c78f9c5b6ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:41:01 +0000

    Rr friend branch central time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
