49475f329cbfa4175f22ab299e44f478a53a8ac8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:17:10 +0000

    community bit future unpleasant freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
