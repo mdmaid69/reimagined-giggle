93b22f8193330ea6a80b649626e1bf6f93ddbfa2
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:48:35 +0000

    last past flow full design

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
