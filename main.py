dafbb6fd88083a6782ca6e82c0d0a4a6e156fa52
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:59:21 +0000

    sea area summer sudden organization

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
