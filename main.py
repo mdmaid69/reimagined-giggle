5a7c38c1505273b80d5dc5763d25abdf2f1d837c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:26:17 +0000

    high tear advise related roof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def reverse_string(s):
+        return s[::-1]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
