2f9da2b4c1ea72f4fc5d2d121cfeb38d65507f0f
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:44:31 +0000

    battery stop sport very repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
