5baf6f8a9a80688ea6c9d2dca7679cabd941e4f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:05:27 +0000

    before picture lesson illness fetch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
