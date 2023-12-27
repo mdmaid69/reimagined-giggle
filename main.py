6bb5621e769909a95e72e527d0a3f15e0a4b1233
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:17:05 +0000

    among exact tonight anywhere gate

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
