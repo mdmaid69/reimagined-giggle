cab4170dcd44d8d0997a21efd1db113460225a12
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:32:37 +0000

    back advice broad cum terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
