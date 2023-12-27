589388dacbd4a04bacae0e4483aa0c84f7936efb
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:15:14 +0000

    comfortable colour promise power guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
