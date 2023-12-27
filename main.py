086251af6cba5f84813762613a01db50dadb3986
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:46:25 +0000

    thing light rob inside speed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
 text = "Hello, world!"
-print("Uppercase:", text.upper())
+print("Characters:", len(text))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
