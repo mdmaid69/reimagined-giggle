ed7df6e7a950f7f7e5bd6f1fe3312c099cea7200
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:33:15 +0000

    enthusiastic twist late will always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
