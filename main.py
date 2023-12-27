54ca9d79e94aeeec08df3f43b7cfdf4d3bfb603d
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:04:34 +0000

    down situation sudden fit squeeze

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def convert_to_hex(n):
-        return hex(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
