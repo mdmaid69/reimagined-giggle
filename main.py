ace3893e7fb85d717842aeb49c20982e6eb2fcf3
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:23:04 +0000

    command shoot marry frighten agreement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
