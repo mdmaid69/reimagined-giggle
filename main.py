817963725db893ee7a9977ef4c3e33f9f01c31eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:33:51 +0000

    calm earn regular lend whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
