de2a9d50c62cf4e9ec57f1c586da7777bf9b3858
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:05:23 +0000

    show now own cold below

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def is_even(n):
+        return n % 2 == 0
+def is_even(n):
+        return n % 2 == 0
