5c749b3749ee3e70409eb99929b2f86dc8b42fa5
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:21:38 +0000

    breakdown breakfast content tough from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
