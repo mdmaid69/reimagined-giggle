4560ff59c5d1dc7916e6ce514ba5d5fd35764a0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:13:47 +0000

    telephone cheap reaction quality flight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import json
-print(json.dumps({"name": "John", "age": 30}))
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
