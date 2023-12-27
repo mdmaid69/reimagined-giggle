0938f4c24330ea7b6e607f67add6e81aaa0d0368
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:07:36 +0000

    fond only giveaway fasten back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
