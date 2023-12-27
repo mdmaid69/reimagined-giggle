446f970f7913d9281e6816555e73ca63d413e0f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:32:54 +0000

    special push spring sad giveout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import json
+print(json.dumps({"name": "John", "age": 30}))
