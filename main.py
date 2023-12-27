db050c4cd1ed9328269232f2db53fd6cb30a0937
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:55:02 +0000

    wash believe understand pupil cum

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
