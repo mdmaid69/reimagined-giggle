ac7183998a4ca85e63485b4ab6f237cd11c43277
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:25:31 +0000

    branch weigh use nowhere sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
