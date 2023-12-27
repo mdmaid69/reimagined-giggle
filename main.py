7087aa5e77b20631fbb4943c0fc5a2a69ffcfe95
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:03:02 +0000

    Ii evening remain accidental generous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
