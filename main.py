36357a373bcfdb3917b29c515764c4a5bbf47517
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:32:45 +0000

    awake burst danger fast way

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
