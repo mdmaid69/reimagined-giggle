4c0ad4bea0d8078a52ccf872b1fd12d37b10c5f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:45:40 +0000

    square chase rest comfortable doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def set_array_item(array, i, item):
+        array[i] = item
