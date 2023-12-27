cefb010f0a95aa371a7978be5a70e7c0f36e8b5f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:36:44 +0000

    floor someone practice circumstance take

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
