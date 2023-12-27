328368e4fa5c0aca0b3df3ca87c88dcaededfc0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:04:20 +0000

    congratulate imagination very direct think

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
