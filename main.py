6474629d2d6b36cd6880d69f5f19fad9f1088c87
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:52:53 +0000

    complete thank try level mother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
