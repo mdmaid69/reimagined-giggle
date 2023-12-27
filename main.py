15e02b906823ae1669bdad71a3475f878b5a409b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:45:14 +0000

    behaviour risk Qq brief shape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
