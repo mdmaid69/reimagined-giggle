d37c33382567c5b07b1d577f827b2dc42434aa96
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:07:44 +0000

    disgusting fine bring sky nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
