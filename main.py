d9b912024de8a270c85fd36a53782d86295e825f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:05:21 +0000

    knowledge fierce turn teach organization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
