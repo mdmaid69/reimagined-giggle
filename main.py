6fa72978998641f452711b65669c34a7e79ee1ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:10:26 +0000

    smile effort thinkthrough among journey

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
