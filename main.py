1ebd1c57c08fded85f33e9d087247a6f2baa37e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:16:06 +0000

    feel right accommodation split result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def count_elements(lst):
+        return len(lst)
