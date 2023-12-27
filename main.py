212e8ba5fb9436847e6bedb88285e54400f90330
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:30:24 +0000

    dinner tidy impatient feed compare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
