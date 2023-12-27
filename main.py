0e18b954f944327c72959ef21c3886f830d7e958
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:51:04 +0000

    rid education right platform often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_elements(lst):
-        return len(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
