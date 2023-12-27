783c6bab3bc8ddf6e737113ccd17e87af5f240de
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:44:05 +0000

    public bang generally begin makeup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
