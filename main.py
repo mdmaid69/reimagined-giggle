1d104686a150ef54b167881dc14fccededab5f70
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:13:44 +0000

    slice mean immediate change wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
