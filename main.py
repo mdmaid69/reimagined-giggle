40ec5fa81e4898a7c9a0974fdfbb2bd4eda37b46
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:16:43 +0000

    government production so exercise thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
