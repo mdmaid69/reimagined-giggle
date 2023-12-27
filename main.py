ee46c4bd917f1774aed690de604c60dd8b4b8b7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:31:21 +0000

    home use satisfied stiff object

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import sys
-  def get_python_version():
-        return sys.version
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
