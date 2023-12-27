e08e4647ddb079d8616fbde32dfbe2b8a302feb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:14:08 +0000

    store please any everyone communication

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
+def subtract_numbers(x, y):
+        return x - y
