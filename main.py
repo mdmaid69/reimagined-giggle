1bc9f6b6be51b156c46a4196789092f7b2f28398
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:44:53 +0000

    tragedy tear frame no freeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
