8545f491f0ad8464466e05ec10d1fbee0500c4ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:51:16 +0000

    fly switch love culture scheme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
