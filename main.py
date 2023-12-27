dc1e6b8a49b7b6c0d402c7801cd25fef5fa90a12
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:15:34 +0000

    seem neck pause first bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def multiply_numbers(x, y):
-        return x * y
+def get_array_as_tuple(array):
+        return tuple(array)
