2bc232e5351d74c30dd38013ffbd98b9a3b5f044
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:38:03 +0000

    thumb pollution cover field rich

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
