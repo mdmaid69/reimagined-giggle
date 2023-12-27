8725fcfa71ea728e8589bdf5e1a978e86c4e7b93
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:58:53 +0000

    whichever safe within stop schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
