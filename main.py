c59c45cae141802dec1867d2c6e5d32d19b4c039
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:42:15 +0000

    fork accidentally false opposite tired

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
