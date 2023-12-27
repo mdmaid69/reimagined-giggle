81dda3865e419f0991566df456c60282c924bd82
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:18:11 +0000

    quantity dress century hit shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
