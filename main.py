5dfbcb40be67ea52743bd37a3b7507f4cb441f53
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:39:53 +0000

    eat either leave risk fear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
