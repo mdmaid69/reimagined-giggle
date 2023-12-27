fb96b9f0be92c1c546a7e08cb7a71e6d6ee610f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:12:35 +0000

    smoke unkind score unable prefer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_binary(n):
+        return bin(n)
+def greet(name):
+        print(f"Hello, {name}!")
