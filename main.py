baee13a36f19b2d9b2c01c09cdbd99e731862fd8
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:02:13 +0000

    land show culture advantage suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
