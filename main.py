3df409772637e92d65b72b0d7647c4656449ae9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:48:09 +0000

    keepaway left union one national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
