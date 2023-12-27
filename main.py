32ce6abffe3f6a0b76d1fb6e556ff866b35a6137
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:15:25 +0000

    not fetch themselves wheel Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
