ab1b9bb9d382fdfa30e3e4eb6e8e40fc5f8fbd7c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:29:25 +0000

    worry connection contact meeting behind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_power(work, time):
+        return work / time
