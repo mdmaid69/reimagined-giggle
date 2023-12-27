51d7a4e00090afa5803d02a791436b0859eba43d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:50:44 +0000

    address centimetre week language distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
