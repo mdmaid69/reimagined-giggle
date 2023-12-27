af8ea6362f40b73e94493ac2eea55973b71ca4e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:09:31 +0000

    tap nor transport computer slice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
