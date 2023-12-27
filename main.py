a74028e2aecda2938c400a66781280f9f75c7a71
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:09:19 +0000

    concern meet distant uniform dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
