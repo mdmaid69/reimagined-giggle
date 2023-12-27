9c8575e853022a565cbed28b5d4707d748185aaa
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:56:02 +0000

    surprise offence lookafter passenger conduct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+x = 10
+y = 20
+print("Sum:", x + y)
