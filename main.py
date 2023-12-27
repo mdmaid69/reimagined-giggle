4b78d7956f8953d85663c0efb6c19029c7991d90
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:18:16 +0000

    holiday many learn arrive right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def clear_array(array):
-        array *= 0
-from collections import Counter
-print(Counter("hello world"))
+def get_array_as_tuple(array):
+        return tuple(array)
