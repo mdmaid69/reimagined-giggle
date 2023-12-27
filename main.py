b81173f203aa4f577ab31be5fcdfd35bddf0ccd8
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:20:04 +0000

    only pronunciation combination wise go

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
