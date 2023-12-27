612f713e1425c16b13f3d97e44f104f49d49f3f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:43:52 +0000

    dare information ability within workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
