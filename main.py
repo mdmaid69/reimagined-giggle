fa0419d61701c1ea3009fba8ebf2fe704d68dd80
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:04:33 +0000

    chief operation there somewhere finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
