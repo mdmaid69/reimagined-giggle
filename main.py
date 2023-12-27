a317639283f316f3caec97e93b75283f3d72587f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:35:20 +0000

    depend arrive division height by

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
