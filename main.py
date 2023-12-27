6c98107c3bd8d5aa21b794f644062ff55baef713
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:06:27 +0000

    shout grade welcome measure forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
