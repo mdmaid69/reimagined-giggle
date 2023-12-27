18be7432ece5df865bd88abeee9ce916bdb92853
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:07:11 +0000

    ilovedicks me dark out air

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
