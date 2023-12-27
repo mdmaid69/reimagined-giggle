857d3de7735cc6e2e6571624d45705942c4c9f49
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:13:59 +0000

    chief address wide Itself sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def square_number(x):
+        return x**2
