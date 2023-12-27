ebd3335a9298ce722355831b7323032d2647d551
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:09:15 +0000

    quiet tip fetch pour beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import re
+print(re.match("h.*o", "hello world"))
