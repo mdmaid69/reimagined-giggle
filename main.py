2f89043d881f63d62d707b86e936bbab0dc55acc
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:50:15 +0000

    tune doctor confused failure force

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
