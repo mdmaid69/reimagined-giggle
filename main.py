57ab78e2f3c636cac5f0f1490acb898a16adbebe
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:05:28 +0000

    collect delicate regret pencil the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_power(work, time):
-        return work / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
