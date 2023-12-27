e7cfe3f8a74f5f848cd659b291dc5705c44a36db
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:01:07 +0000

    favour neither capital step violent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+print(time.time())
