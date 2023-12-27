dfaaa695da7ca438afd9c6af5679be113369391f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:55:40 +0000

    unlikely comein float whistle figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+for i in range(5):
+        print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
