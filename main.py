852e0581e8b0577b6031f4b2ec14a1758a6efa13
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:05:26 +0000

    should shelter shop fault think

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
