bebf311d18e3260f31564dc46d08531d85725a87
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:34:40 +0000

    nigga track sore surprise which

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_as_int(array):
+        return int(array[0])
