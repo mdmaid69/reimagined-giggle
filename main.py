df6ac4535530934e82bf250b91adb7080f1376ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:42:07 +0000

    table test train take increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
