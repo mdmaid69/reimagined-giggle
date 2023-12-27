2b4997475b13baf58a01f96c1d46cfe63d25de38
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:43:06 +0000

    runout keepfrom although more improvement

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
