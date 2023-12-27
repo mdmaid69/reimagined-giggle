26c12d3a4fb0e5b7378e154e2b3da90c4b4672d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:30:20 +0000

    holdup new prepare meaning storm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
