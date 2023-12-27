8be075f29abdf0f48cc695273080feb05a82d222
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:59:23 +0000

    measure wheel do final distance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
