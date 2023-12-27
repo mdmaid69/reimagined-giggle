ef2b3080bfd4b33faaff1f3da05d13f8f50f87c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:33 +0000

    worry goout stay naturally achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
