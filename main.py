0d3ff80b4168d3b40b7408a78ed31248b34e24ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:02:26 +0000

    nature relationship quantity push nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-for i in range(5):
-        print(i)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def split_path(path):
+        return os.path.split(path)
