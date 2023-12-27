5db640fe6747d81b30b802b07decc0f95d39e0fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:41:32 +0000

    common connect tired business praise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
