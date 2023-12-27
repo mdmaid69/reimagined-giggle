7259dee417644a41ffd1baa2bfb83160429a3a4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:03:24 +0000

    study turnaway plant increase offence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
