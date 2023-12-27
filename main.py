96048f53c0c47bb7e22f75b6e13819c13b168d00
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:39:39 +0000

    home discuss right figure no

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def convert_to_octal(n):
+        return oct(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
