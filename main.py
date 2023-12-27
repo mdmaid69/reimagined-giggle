4e63755e0a16402877b5194ecfe763d9d161074a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:58:48 +0000

    organization niggaacademy present university mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
