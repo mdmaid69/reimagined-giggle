4189abd76bd3ddf7e63432ba5964fd9c39633815
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:31:48 +0000

    corner wrap hit must charge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
