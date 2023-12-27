d39a6ea03b335a4f00f4ed3f3396418bf975a5b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:59:35 +0000

    murder remark clear mind concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
