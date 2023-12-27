d3f1cc89f18fc2f083d91465632d1d029c306b26
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:40:26 +0000

    usually dry frequent remember study

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
