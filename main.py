46fd19c6700a05a8b242663d1e2ec7019ba2071f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:56:00 +0000

    skin guide spread normal thinkof

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
