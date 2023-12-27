8af43f047fe748c88edb8f547d523c8d7b3090ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:24:32 +0000

    direct confident onto answer interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def add_numbers(a, b):
+        return a + b
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
