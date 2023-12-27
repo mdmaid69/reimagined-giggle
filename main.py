21654e3904b81e3d81db96017012046d22ea5c37
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:53:54 +0000

    health average disagree operate sweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_min(numbers):
-        return min(numbers)
+  def split_path(path):
+        return os.path.split(path)
