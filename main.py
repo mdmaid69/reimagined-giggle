8956cb22b9589c21699e7bbb22888d41ad643242
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:46:41 +0000

    wind connected allow method smart

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import sys
+  def get_python_version():
+        return sys.version
