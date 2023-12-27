aef7a3989f0193a47a46ed5e250393f8945fb028
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:26:27 +0000

    defend traffic fork ring formal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import sys
+print(sys.version)
