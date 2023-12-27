a41f03b4c8c01d93e92457187df8e34a19079e30
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:51:20 +0000

    wire success 572enter answer explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
