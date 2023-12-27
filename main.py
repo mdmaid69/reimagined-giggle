3701b497882c2d8578d6bd598a8b7bd0448f357c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:01:50 +0000

    we warn building whistle breath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def count_words(sentence):
+        return len(sentence.split())
+def square_number(x):
+        return x**2
