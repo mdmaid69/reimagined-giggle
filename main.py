0ac18f280e7d75965329fad7b3476b48b9a541f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:39:10 +0000

    pretty instruction lookback(on) forgive children

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def count_characters(sentence):
-        return len(sentence)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import sys
+def print_python_version():
+        print(sys.version)
