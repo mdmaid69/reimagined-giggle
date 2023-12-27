d37665367e8006939257ac229e7f3f1b9026377c
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:11 +0000

    rare service before hope radio

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_as_list(array):
-        return list(array)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
