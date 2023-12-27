616623e312ee10061534b4de32585b33dd208888
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:22:27 +0000

    foreign violent nerve delight remove

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import collections
+def create_counter():
+        return collections.Counter()
