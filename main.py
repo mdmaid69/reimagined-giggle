0b6d97807151d6ec6e3006096eb774f256afc569
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:16:55 +0000

    sight describe set goout high

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+def get_time_since_epoch():
+        return time.time()
