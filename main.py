19d1c987aa6dc1de891b609d51b7bae34f162fda
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:33:37 +0000

    anywhere allow considering do black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+print("Hello, world!")
