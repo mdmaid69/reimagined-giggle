9ca130a4ddd60f1e8b9a9e096adb6c258101dd88
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:45:08 +0000

    treat attention road embarrassed beneath

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import random
-print(random.randint(0, 100))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
