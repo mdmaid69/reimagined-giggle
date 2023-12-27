eaeac562556bbc8eead28c65c3e9c7932cf64d36
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:28:22 +0000

    wrap custom load meet responsibility

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def reverse_string(s):
+        return s[::-1]
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
