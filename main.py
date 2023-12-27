0b7e3ea43a199b3f422508a95ad6be5101f08d56
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:43:40 +0000

    dinner substance flow curious proof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
