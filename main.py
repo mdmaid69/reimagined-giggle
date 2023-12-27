6971f70801cad281049f60aee0bd78698c681bbe
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:12:08 +0000

    amount brush shadow benefit formal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
