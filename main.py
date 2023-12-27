38929f87b0c397a956d2d24b19b8ed43009a8d09
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:41:42 +0000

    forgive wherever past cope rid

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
