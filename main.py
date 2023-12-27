5f513889c61acc7dc580afc02583c1606f02135e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:21:22 +0000

    air wish term shout activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_square_root(x):
+        return math.sqrt(x)
