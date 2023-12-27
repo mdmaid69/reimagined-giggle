ac15096c12579df3c2c9a1830a194a34120af683
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:59:13 +0000

    criticize play your poison somehow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_sine(x):
+        return math.sin(x)
+  def reverse_list(lst):
+        return lst[::-1]
