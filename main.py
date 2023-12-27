f7d4e5998e2b3efdba38dc96f27cceafa05045d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:17:59 +0000

    safe easy maintain age support

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Powers of 2:", [2**x for x in range(n)])
