ee02ff3811a971600ea144485a40cd04df8f7762
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:09:17 +0000

    brush talk call Oo smell

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def calculate_area_rectangle(l, w):
+        return l * w
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
