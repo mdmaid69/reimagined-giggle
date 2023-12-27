bb30f2c5d7458c99b390ef2988a055fb8b547abd
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:09:07 +0000

    black mother burst stick image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
