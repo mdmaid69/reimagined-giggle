a7dc7064712b12dd8498f9892098f908e4229973
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:42:05 +0000

    finish once talk proper alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def add_numbers(a, b):
+        return a + b
