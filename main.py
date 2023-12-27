3f59f9af4588bb00defe34dab0ab15ed96c31a3e
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:35:10 +0000

    frightened from patience join definite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
