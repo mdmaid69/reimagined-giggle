b85d7544e3cc99fa20d3bf2466db8020a1512c82
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:12:20 +0000

    speech wish peace live sign

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
