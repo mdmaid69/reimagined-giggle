78d22838b38e50d0004edfb190ada2b7e1cdbd01
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:11 +0000

    teach town such middle reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_min(numbers):
+        return min(numbers)
