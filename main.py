dfe4e439157572aebba84e9b1d7d8310a0eae195
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:03:40 +0000

    breakdown direction fast might reaction

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(10): print(i)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
