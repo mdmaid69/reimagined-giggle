f57afcd450b049673fb5ba91b8e7af7e81557ba5
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:12:32 +0000

    similar bar later pause increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
