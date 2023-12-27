da21af347597c71e0438ec3b48576ff85a3134e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:53:51 +0000

    back impatient love cool keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def clear_array(array):
+        array *= 0
