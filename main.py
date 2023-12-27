ad017a25d31847efe9086812501d2dcbe5fb6922
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:54:26 +0000

    scare nature complete profit fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
