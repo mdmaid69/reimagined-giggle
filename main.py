234401d6e5e46c42a7e68a16e918e60355c73b29
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:23:28 +0000

    little university block gather accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
