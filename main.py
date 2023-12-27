9e963e393996a3b9fd4ec07e2f5bbced0539a5f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:36:38 +0000

    then trust excuse change news

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
