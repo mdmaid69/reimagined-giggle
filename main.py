db5712265d965e7feb4dc02329774f7d455f9fe7
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:22:58 +0000

    awake Yy Uu search test

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
