57250c541645fa4aa8268656467f48d5c8776b18
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:23:19 +0000

    naturally defeat need feed generous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def count_characters(sentence):
+        return len(sentence)
