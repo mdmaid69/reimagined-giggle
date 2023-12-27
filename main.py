3edda0607fab955461186deaf419f0c468a5675c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:56:35 +0000

    bar accidental getout unit seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
+import array
+def set_array_item(array, i, item):
+        array[i] = item
