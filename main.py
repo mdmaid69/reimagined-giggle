fda626640efc4554a733925d0ce373d08b66bb75
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:15:19 +0000

    borrow Qq interruption turnover shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
