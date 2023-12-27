994a7a2edf031d70ce75603372b19c02a015711b
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:51:26 +0000

    might programme autumn conduct into

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
