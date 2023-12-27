14fa3a34217aa283da06bfe2a743df3776fb1cfa
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:26:04 +0000

    lookup fire teach shadow exit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_itemsize(array):
+        return array.itemsize
