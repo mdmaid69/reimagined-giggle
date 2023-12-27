42283e3122d13a86169f229f05cb888e74c14409
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:44:04 +0000

    gap begin hurry regularly compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import random
+print(random.randint(0, 100))
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
