dc1cd66ca6dfc766ab33c3d27741a54e615bee24
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:37:48 +0000

    giveout connection work basis rob

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
