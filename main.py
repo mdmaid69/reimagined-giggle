e1f3e10a053c01a75b038c07a1a04779267c8c41
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:19:53 +0000

    land tour apparently squeeze us

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
