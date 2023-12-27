79aabbe1e312efdfd594fe52951508619008e3e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:13:07 +0000

    keepout exist almost thirsty argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
