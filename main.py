492c008baf9583cdf4ad1ad89c3bca83f47248fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:24:09 +0000

    Ll value organize rough sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
