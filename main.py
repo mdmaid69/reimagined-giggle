939b444910d59930ad0cbd2882f3a49cce279489
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:43:37 +0000

    reason present drink welcome dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_acceleration(speed, time):
+        return speed / time
