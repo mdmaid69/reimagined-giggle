c72154fcc94237392d5b1a55becdd22138627c53
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:31:54 +0000

    ticket involve call equal suit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
