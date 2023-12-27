bc72e1f60727a126525e8419af83cde71860902c
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:18:19 +0000

    also smart know time innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
