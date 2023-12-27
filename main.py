9b41e5fd73fcf0b9cb832974a377c718653163c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:03:12 +0000

    across impossible feel cut cinema

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
