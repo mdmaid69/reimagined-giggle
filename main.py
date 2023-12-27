8ba3fc4db5c9a52ce23934f95f16936139c576f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:32:04 +0000

    pretend contrast remember standard clue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
