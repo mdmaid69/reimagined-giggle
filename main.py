03fe618baea17ce099beca1d736d2cea28e284f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:53:01 +0000

    invite unable brain available disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
