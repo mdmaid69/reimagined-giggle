f00654bf4754e7e03aa758c03da0ae26734c4653
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:13:18 +0000

    keepoff by quit payment wealth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
