daf303f61108b3b662be08edbd922ca68a70b67d
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:41:09 +0000

    traffic agree home born yet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def find_min(lst):
-        return min(lst)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
