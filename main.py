d69ccbe5c40e046bca511c5b48f3f2bc2fac50fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:16:22 +0000

    right weekend happen interfere somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
