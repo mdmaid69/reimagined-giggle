0a4fa99109ba02719c8bc13c2e7c8df419c41fc4
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:27:24 +0000

    environment goaway importance pretty long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+print(math.pi)
