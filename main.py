fd073ffd8795fad301d743b5c768f6ee5c753119
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:35:46 +0000

    piece discover early birth gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
