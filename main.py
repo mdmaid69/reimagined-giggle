c24980e06d07ab641230f95b764b86970f1417ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:22:11 +0000

    occur arm behind pocket runaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import numpy as np
-print(np.array([1, 2, 3]))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def reverse_list(lst):
+        return lst[::-1]
