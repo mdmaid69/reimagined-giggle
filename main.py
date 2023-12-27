3623b8bc01d914fd995020cdf884c2720a16334f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:34 +0000

    plate sink on finger recently

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def add_numbers(x, y):
-        return x + y
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_volume(length, width, height):
+        return length * width * height
