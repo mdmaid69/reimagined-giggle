64ceab46603b37c88c7885ae48a52c93d29c765e
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:44:02 +0000

    lookafter attitude getout base 568energy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print(sum(range(10)))
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
