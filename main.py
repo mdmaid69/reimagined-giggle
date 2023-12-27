6e92b032b954b3e5f2cacbc4eaa05265eecb5988
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:27:48 +0000

    collection measure ought memory nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
