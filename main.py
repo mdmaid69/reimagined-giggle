6f2ceb64380cc6970c4b1df75956e839bef58faf
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:34:53 +0000

    giveback hollow closely satisfied from

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_volume(length, width, height):
-        return length * width * height
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
