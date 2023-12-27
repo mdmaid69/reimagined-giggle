2d521e0c1163f67353ef54cf84ba13a032b8d68b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:47:07 +0000

    plenty discover later say little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
