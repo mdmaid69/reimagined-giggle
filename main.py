e670fde06bf58b6de626426ed503594eeddc76e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:32:06 +0000

    rate very circumstance land drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
