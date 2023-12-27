acae4e0fbd842d17580ad19b2b9cc05f245ae0ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:45:14 +0000

    needle runout marry doubt convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
