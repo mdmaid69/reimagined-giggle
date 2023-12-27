3b2fe008617a1c5c22c85069549eae7bc6b554f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:29:35 +0000

    then design glass wherever everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
