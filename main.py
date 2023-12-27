56b8930781b2cdb901de0d0b5f29f9ff187d3d68
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:05:30 +0000

    moral live rain nose hear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import platform
+def get_python_version():
+        return platform.python_version()
+print(sum(range(10)))
