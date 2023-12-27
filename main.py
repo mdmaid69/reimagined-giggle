44910470ee262dd4be503322b177432375773497
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:46:38 +0000

    same thirsty second serve average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
