8f45cd7eba27d8d3bf4cfd8dcaee203b4843c5d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:09:30 +0000

    replace the available on care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
