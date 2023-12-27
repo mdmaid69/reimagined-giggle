9597dac45292e21872b481191ab59c4fe6d73377
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:51:50 +0000

    on whistle plant start them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
