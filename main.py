be06b8e88eefdb3aaa6ca04165e27ede0ca47d93
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:20:06 +0000

    switch programme offence sister neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
