6c0ca30bf7aebb4b19bd7b3bc77169589225af0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:31:33 +0000

    where danger model prevent absolutely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
