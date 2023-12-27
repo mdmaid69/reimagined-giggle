a7ddb1747a03a5a65a6009527262caf45340500f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:45:03 +0000

    live fat principle once meet

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def square_number(x):
+        return x**2
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
