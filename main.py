9643180732743a327f95c97129b764c2eb8a6aeb
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:20:21 +0000

    ear middle not quarter vote

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_work(force, distance):
+        return force * distance
