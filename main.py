ed9a85bdd02532466692fe13a6188b85e10a2ce4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:54:23 +0000

    appeal place takeout nigga doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_list_from_array(array):
+        return array.tolist()
