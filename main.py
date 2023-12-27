2895997f4cfe74798c6b4629b4291477cfe9694a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:56:32 +0000

    height disappear priority break peaceful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
