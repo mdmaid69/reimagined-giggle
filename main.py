1a189fb4ebd157372958c96320d8102fa399d06d
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:41:16 +0000

    guide introduction frightened out best

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def square_number(x):
+        return x**2
+import collections
+def create_stack():
+        return collections.deque()
