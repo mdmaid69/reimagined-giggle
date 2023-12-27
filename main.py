7921ca82930eb15ec847b00eb0dfe7b1406b6a11
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:46:23 +0000

    charge copy degree language control

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
