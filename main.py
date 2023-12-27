73b588427e56f6b087c0dc77224a78a0fcc2e95b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:10:01 +0000

    tie position opposite off feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
