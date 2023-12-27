74d5987e30711267ace6d51029304ffb3e60bc79
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:42:40 +0000

    Kk innocent around fear cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
