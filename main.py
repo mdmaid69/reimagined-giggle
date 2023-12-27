e942a1d74b662b3a1915b0b9f58e902c992de4c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:58:34 +0000

    practise clever bound light lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
