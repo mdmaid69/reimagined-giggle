7226256557d6b0e379a766799281cd17bfdbdbeb
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:26:46 +0000

    object disappointed brain letter tough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
