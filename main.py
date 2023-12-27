2385c66c4c60a55078b816d9322201333929bb70
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:19:45 +0000

    undo agree interested uponon bet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_min(numbers):
-        return min(numbers)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
