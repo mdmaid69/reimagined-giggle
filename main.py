b9834f579e6ebd770b581b69563bc3edca4bd627
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:51:03 +0000

    judge trust example tomorrow phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
