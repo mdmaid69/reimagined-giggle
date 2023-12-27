65558380409aabc9122bd181e73bb1b38f3962fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:05:27 +0000

    suit room sick burst turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
