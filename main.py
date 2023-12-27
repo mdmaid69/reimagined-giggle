6ddb3c135bace68f2336b3f6bb7fe972c9c2cb27
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:23:21 +0000

    cruelty commit fit make injury

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
