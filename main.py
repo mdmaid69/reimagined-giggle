72d489820058e24346fe514c97c63d7c7c7ce160
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:09:44 +0000

    for poison target suckme trouble

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
