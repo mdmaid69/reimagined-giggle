28dec50ff0aa6972912e3a8b4e51fa2a8eebc146
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:47:23 +0000

    point talkinto really bank full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_hyperbolic_tangent(x):
         return math.tanh(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
