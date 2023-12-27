a72ec993cbd52ea49f3596821b4d715d7393225b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:47:49 +0000

    exist by loud like convinced

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def sort_list(lst):
+        return sorted(lst)
