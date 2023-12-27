63cf8a7e9cda259de5b7d47c20099a10eab73929
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:25:20 +0000

    although could card past such

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
