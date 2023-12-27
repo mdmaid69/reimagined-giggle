224fab6bb968431e36e183d958b4618c69bd011a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:13:27 +0000

    black unless other medium face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
