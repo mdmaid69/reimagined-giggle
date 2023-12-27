f562dce81ed5ef66a29601374abd0ac4a3d382fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:45:18 +0000

    difference real pin yours transparent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_queue():
-        return collections.deque()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import sys
+def exit_program():
+        sys.exit()
