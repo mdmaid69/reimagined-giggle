12d4acc4081088dfa97625d79c0ce5712bd57e8f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:26:18 +0000

    few success drop climb without

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
