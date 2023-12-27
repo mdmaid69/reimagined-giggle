c48781a86edaf336a7d934c02da78c676e861f92
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:18:47 +0000

    Pp base forward cum bank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
