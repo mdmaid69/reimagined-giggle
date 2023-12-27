46858ef7eb90890c7311fddcf150e03bbdf0a638
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:21:21 +0000

    would little factory division relative

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
+def multiply_numbers(x, y):
         return x * y
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
