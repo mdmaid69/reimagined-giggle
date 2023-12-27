98dbbaf7e0841b0c0edb89e71552ddbcaf33e215
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:48:33 +0000

    nerve needle powerful accordingto event

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
