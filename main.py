5105713c0ab187df25bb3e27d7939fd1fb612b2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:08:54 +0000

    hair plenty 571enough turnback bin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
