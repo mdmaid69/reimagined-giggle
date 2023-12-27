55d5a7fa0088d9b0a80c503d0c04a0125dcec9b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:40:30 +0000

    pollution behind tidy worse experience

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
