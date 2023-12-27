88888f54d4bebd1de955abcceb26a21c27f24333
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:06:05 +0000

    include about furniture mean generous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def sort_list(lst):
+        return sorted(lst)
