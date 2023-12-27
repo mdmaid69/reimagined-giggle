f714b0543f5b4754866f0dcf1813cbe9fa00d069
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:38:40 +0000

    actually hit Rr whistle breakin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_set(array):
+        return set(array)
