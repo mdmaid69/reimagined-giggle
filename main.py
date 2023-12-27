351793b79a10eddd6657ae5cb9821030f5880fde
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:11:59 +0000

    container suit same talkinto keepaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
