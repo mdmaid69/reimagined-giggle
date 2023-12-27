7c02939dde6de40b706c7860269c69fcbcf52dcd
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:53:27 +0000

    earn bury by really keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def is_even(n):
+        return n % 2 == 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
