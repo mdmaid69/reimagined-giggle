84d6d5e0d51d2266d79e22fdea4daeb3416ee4cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:09:28 +0000

    reject breakdown wire left authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_hex(n):
+        return hex(n)
