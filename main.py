48ade39d62eee302894f636a2a6df118ddd1ffa8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:42:29 +0000

    little however slow gentle child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_list_from_array(array):
+        return array.tolist()
