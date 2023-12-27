2ec4a23642faede738cb3bc8e8789118e0d11543
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:31:40 +0000

    content interested although deliberately used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
