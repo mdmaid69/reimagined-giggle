cf1cdbf12d231312c382ec4d59c465e7f6e861ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:08:42 +0000

    raise tie exam complete fast

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 def calculate_return_on_equity(net_income, total_equity):
         return net_income / total_equity
+def calculate_force(mass, acceleration):
+        return mass * acceleration
