bcb32f31fde0d98f6ef7513b01ed6f44b2e0af8f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:08 +0000

    receive closely tour clothes thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
