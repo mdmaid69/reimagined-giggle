16d0422fe3b95953f1358e880c19b84de11fbecd
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:18:29 +0000

    exploration something impression wake great

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
