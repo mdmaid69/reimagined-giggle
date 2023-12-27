75e034a865cf32aad93fa99b5f0b06f6926ee3e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:42:30 +0000

    first evening sign prize this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def subtract_numbers(x, y):
+  def subtract_numbers(x, y):
         return x - y
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
