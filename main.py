65b89de432a1de5252802c040196243a0fbc31f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:38:07 +0000

    raw unhappy sink course worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
