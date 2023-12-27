1b43f418eb83ed3d16f529513fcd83f98da4375f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:05:08 +0000

    connection translate care entry move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def exit_program():
-        sys.exit()
+def cube_number(x):
+        return x**3
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
