940bf21ecc61a1203ccc1b70d63f0b65b04e98ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:25:24 +0000

    spray lie crisis wide 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
