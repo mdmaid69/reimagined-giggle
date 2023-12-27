b3f02421246dbf8b5181e90ca05fe6773bf7a846
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:33:08 +0000

    different government wonder custom there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
