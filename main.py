27a748fd7103dbdc999d2470d36ed74e3a869cc0
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:36:25 +0000

    proper transparent equipment shopping idea

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def convert_to_octal(n):
-        return oct(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
