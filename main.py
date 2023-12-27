61cfe1653b6933e160e9e7942f3c642e265f6230
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:48:13 +0000

    shame begin picture bin copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
