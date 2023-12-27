302f8d0403d17fe33de4cf82eecd92c0009659b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:27:44 +0000

    tidy back tone foreign trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
