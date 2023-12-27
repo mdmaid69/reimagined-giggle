ddee2902de99caa3b9e0f7c617b21ca484e95e53
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:03:00 +0000

    whether crazy service inside stand

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import datetime
+print(datetime.datetime.now())
+  def is_odd(n):
+        return n % 2 != 0
