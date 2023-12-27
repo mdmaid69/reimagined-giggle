a2da94e3d088328d3a97fa0b5f622ae73101684a
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:09:47 +0000

    peaceful urgent keepout recover picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def sort_list(lst):
+        return sorted(lst)
