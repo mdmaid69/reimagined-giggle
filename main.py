b0a8a23455ffa8425da0836e1b0ed6c358af7661
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:37:13 +0000

    destroy company will worth safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
