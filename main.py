c25d3991b4024ef8e9b0fee1e5b5ac7948754cec
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:46:09 +0000

    traffic check ahead sweep runinto

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
