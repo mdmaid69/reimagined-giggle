8bd07bb5d1a8b1f453115d6540b26852c02f677f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:07:44 +0000

    indeed shop mainly we bus

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def convert_to_json(data):
+        return json.dumps(data)
