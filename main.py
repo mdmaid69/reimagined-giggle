9d376677c10668d44d96b17247ccd463c53212e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:44:52 +0000

    566end special unit belief excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
