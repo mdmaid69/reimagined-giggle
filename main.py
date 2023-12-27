4bf109e32eaf90a508c79e47cb038f12594604bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:32:19 +0000

    plenty neck stir practical score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pressure(force, area):
+        return force / area
