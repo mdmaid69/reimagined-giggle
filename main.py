fbf2575e56257ad6e680e5e6ec5d2fa05e378149
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:19:02 +0000

    criticize show declare ignore factory

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
