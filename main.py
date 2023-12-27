ac3000b427fb7bef9a0bd64af23e2c180eb7a8f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:46:53 +0000

    pencil narrow calloff wall type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
 import math
 def calculate_circle_circumference(radius):
         return 2 * math.pi * radius
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
