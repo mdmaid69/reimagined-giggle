af3b17dbf42c1e6ecdf39ce18276c00f9da38cb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:58:48 +0000

    play clever trip nose should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_priority_queue():
+def create_stack():
         return collections.deque()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
