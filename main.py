e8494c290b21e2e7967ee9a58cb03350c35fe3c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:12:46 +0000

    crazy frightened unlikely somewhere expensive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def cube_number(x):
-        return x**3
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
