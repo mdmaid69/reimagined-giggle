f32b6e4dc0a699ef6b88960941ef4eb4292c15d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:08:21 +0000

    guess physical freedom stay each

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_area_rectangle(l, w):
+        return l * w
