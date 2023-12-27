d29e4f9b33293529fe0c75eb18e65b06c1231aee
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:24:58 +0000

    tonight cruel tape side even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
