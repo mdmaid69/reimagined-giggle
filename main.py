555e80664bdaf49b9eef6795a25d22821a7f80ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:06:07 +0000

    change bag thought combination walk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
