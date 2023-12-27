84c3aaf07566f648a2ab8b27bcec2d0f7c3b1d2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:39:32 +0000

    stiff wherever love mysterious pronounce

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import tensorflow as tf
+print(tf.__version__)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
