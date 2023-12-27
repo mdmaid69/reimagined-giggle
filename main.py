c21b5898cf14217c76f0586239527da46682566f
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:52:49 +0000

    soft cinema many appeal refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
