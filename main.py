1c8c609f00103ec7068eecedf7c3195c8e5250cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:37:51 +0000

    crash concerning mass something couple

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
