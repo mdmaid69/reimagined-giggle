7873d4a6d3cb332f78eae2f63d39113f6601b2d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:06:04 +0000

    sweep meal secret general popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
