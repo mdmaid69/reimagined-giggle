975498dcec333f43325050d8122a8712db541caa
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:34:49 +0000

    balance sad test distant fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
