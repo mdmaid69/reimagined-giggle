8a8c107d65cdde5e74b148add0ef7a9fc810bc30
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:05:04 +0000

    satisfied control yet it best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
