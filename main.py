53abcd7b44c9ca5fcb74752aed89411e1b3faba5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:49:51 +0000

    human expensive promise about distant

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
