a34a997de07926598c47d64c7d12ee36fd474ac4
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:00:19 +0000

    ground very border slip urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
