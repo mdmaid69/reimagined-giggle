c1922b4190e7abadf9c3ca2e7bd45a56d7d9b487
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:54:12 +0000

    freedom excellent improvement come tired

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
