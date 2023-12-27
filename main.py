221803f0f29753b0f618443c3a154a1071539ce1
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:56:33 +0000

    past mind part crazy also

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
