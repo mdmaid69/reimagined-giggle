a8b78743b6589329c221783302082dd607cc4b7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:08 +0000

    create Cc may floor gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
