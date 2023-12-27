9a9b0a9fcb1f89bce048eaa81d0bf92e1601ef7d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:23:42 +0000

    improvement name whole brain slice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
