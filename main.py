3f7206800ceafa438f5e95f53948e9371b4e23ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:24:02 +0000

    temper everyone score pain thin

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
