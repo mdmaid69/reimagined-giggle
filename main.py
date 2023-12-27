419642abb1b569f6506cb97eb1e0ac69f628ceda
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:52:23 +0000

    probably region custom close difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
