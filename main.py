7590ebed25d8ec045743074a4ec44a2980a0d981
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:16:52 +0000

    risk like example uponon talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+x = 10
+y = 20
+print("Sum:", x + y)
