9a9f8b18cc908a96fd0ed7a1e19da3b52b47247c
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:03:14 +0000

    clever stiff shout home limit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
