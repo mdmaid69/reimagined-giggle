289f9301467e265468cb68901060980caf2542be
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:00:10 +0000

    Itself later compare position miss

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
