8ac8e06dfef180d81f1b3ef94437f4efad2eb3ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:21:42 +0000

    country runinto promise later agreement

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
