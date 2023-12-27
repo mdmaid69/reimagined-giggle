171cd916ea417124f29e5fb98195d10111f6e2f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:06:59 +0000

    second although charge give hollow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
