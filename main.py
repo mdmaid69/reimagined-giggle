5e6011d203cc306439c610088e0a5b92721ea89b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:34:27 +0000

    ball tour giveback he point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
