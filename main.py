63b1d5bc2ae5a8ae0fb85575e81c2de7574f4f72
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:39:13 +0000

    safe to confidence clean getback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
