006ab338ee63a86fd8d5d139b6d8d8d97c5fec5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:40:29 +0000

    check temperature keepoff attitude every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
