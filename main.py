ec2b753c8f658f9638036f8ec888d2a75f995562
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:33:36 +0000

    satisfactory borrow learn block ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
