e0a388ba6878521902e2a11f685c72117d8fa038
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:37:46 +0000

    scrap where other little surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
