4ee6952fd1b64f5694922351c91d5eec42b9b89d
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:54:05 +0000

    basically promise pay hurry whatever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
