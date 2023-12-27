eded7c535fc86d5b595dcba8a5365dead579ffb2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:30:16 +0000

    general count proper stay bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+from collections import Counter
+print(Counter("hello world"))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
