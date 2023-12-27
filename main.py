51d50e9a2557ce890de7600afbc94c3b9e9a063a
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:20:49 +0000

    congratulations tour drop high odd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
