910dab36dda275657590c665b828a6e37bd999cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:12:31 +0000

    tune means behind achievement past

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
