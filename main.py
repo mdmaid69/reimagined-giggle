8630ec1e153754a80ed33d5498d2089441d8cc1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:43:16 +0000

    support active pronunciation might attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
