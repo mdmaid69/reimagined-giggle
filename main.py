9621f232d6b9f52f33d8c3221ae9faf9b70bd328
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:46:46 +0000

    himself Mm live foot butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
