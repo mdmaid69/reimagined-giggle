7854455e457aac994b5b3123984d4a0298e66856
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:02:24 +0000

    minimum one land wipe minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def sort_list(lst):
-        return sorted(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
