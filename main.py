7c1e2368a66210c01b55ec2e5e042dc371ee9840
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:13:35 +0000

    successful practice breath tired typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
