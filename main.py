7e8eae1dac8d9d28ff5bb5a60993cc5b756b16b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:48:15 +0000

    noone yourself fact smooth exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def check_if_array_contains_item(array, item):
+        return item in array
