4a8fd57edb73009e413720a8e9baf180dd581336
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:50:37 +0000

    across ashamed pollution content bang

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def convert_to_hex(n):
-        return hex(n)
+for i in range(10): print(i)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
