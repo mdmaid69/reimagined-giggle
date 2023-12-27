a36b173bec4942abcf52fc4179939395aef3339a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:25:59 +0000

    wish taste programme afterwards downstairs

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
