1b4c2a8358ade9408294db8bccb7d6a024283e70
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:17:53 +0000

    affect worth ought frequent take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
