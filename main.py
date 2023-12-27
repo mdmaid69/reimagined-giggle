625755811c52062bb66478e65c515a6370e9de95
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:16:36 +0000

    pull creature somehow crash flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
