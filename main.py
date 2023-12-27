76fc7e3e49b1f117d72c8e8c8cc8a6e1ed18aa7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:27:52 +0000

    giveup thinkof brief achieve comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def convert_to_binary(n):
+        return bin(n)
