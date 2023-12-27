12c2ec94311b782b0f250dcd7d1227dd4d771b62
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:58:56 +0000

    prefer middle eye already precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import collections
+def create_user_list():
+        return collections.UserList()
