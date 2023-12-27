a20c3e63b7c13e8300007b5fe1f05290a1de2ee4
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:58:23 +0000

    familiar excited ready collection weigh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
