dd3f07c9e60609fc4e6b2cf79071d73e06cc29da
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:38:37 +0000

    OKorokay suit unusual boil active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
