1bdf8e34431b73838e1dceba5c8dead2db3b098b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:01:34 +0000

    loud use convenient breakout other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_str(array):
+        return str(array)
