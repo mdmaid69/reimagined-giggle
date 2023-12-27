f8153f5a46c532ba1c6abfa377550c360c289e3d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:49:08 +0000

    play some dare continue comeup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
