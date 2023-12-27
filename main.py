4d80fab1ba0b96634000c66ab17141091d9a8996
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:54:56 +0000

    indeed rush sleep wind section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_current_directory():
-        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
