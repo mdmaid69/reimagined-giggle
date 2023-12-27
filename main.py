fe79e4bbeb649825430e39611fb2d80e01a80026
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:06:56 +0000

    follow tense birth quality food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
