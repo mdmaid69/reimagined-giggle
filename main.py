0696983ff2890423a657486e9f4199f2f9576699
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:50:22 +0000

    note low treatment error probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_tangent(x):
+        return math.tan(x)
