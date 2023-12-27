c8fc52fd4f35a8568d9cd71ab4629f0eb10385c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:51 +0000

    fold payment their gentle affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import collections
 def count_elements(iterable):
         return collections.Counter(iterable)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
