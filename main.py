36f0caccfb40a960b14bdc7d71abd4997f636e12
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:17:41 +0000

    repair professional point cause write

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_speed(distance, time):
+        return distance / time
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
