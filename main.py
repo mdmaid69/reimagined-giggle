02c29b4253823fa429910a3bcc30df95aef8cf7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:54:16 +0000

    phone care disappear drive basis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
