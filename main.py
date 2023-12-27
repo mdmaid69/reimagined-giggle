7b195555e7d5e6f084a0a86e698df39fcf8027f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:19:22 +0000

    equal judge force bank roof

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_work(force, distance):
+        return force * distance
+def reverse_list(lst):
+        return lst[::-1]
