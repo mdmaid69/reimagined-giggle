d8c31da17b58f5896e7630b685d8c2a82ee7df5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:06:03 +0000

    murder wild paper in command

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
