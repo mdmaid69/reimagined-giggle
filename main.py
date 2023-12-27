f6e70e90d493031d92586710e1e8121d4659d913
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:15:22 +0000

    exercise home murder convincing polite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def convert_to_octal(n):
-        return oct(n)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
