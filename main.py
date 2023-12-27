a64faf91dfe17e1d103fb53946de99381999e05f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:29:30 +0000

    listenfor period always week taste

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def cube_number(x):
+        return x**3
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
