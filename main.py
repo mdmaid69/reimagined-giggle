50bf657b5c1688c6036aaa05f1a32fe8b4537c9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:11:25 +0000

    gap range definite know recover

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+print(time.time())
