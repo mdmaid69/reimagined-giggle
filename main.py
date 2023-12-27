994076f2096174e1312629723c31ce3a514de4a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:54:06 +0000

    rather mysterious house nurse blow

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
