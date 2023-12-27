1c2d9fb9542ae7dd14929d97c7f062ddea0f661d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:15:31 +0000

    level well repeat effort rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import collections
+def create_queue():
+        return collections.deque()
