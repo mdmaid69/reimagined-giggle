1873e2f23da7588223b72ccb9dab69411881c177
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:07:07 +0000

    confuse sale usual blame upset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_set(array):
+        return set(array)
