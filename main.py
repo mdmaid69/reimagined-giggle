67f6479b7f1094e59d4c230f91b41505a3dded70
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:12:12 +0000

    gradually whoever imagination pile guess

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
