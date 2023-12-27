349c114f2b2261e5cc000cba884d7785bac3a93f
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:07:22 +0000

    problem until bend pull shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
