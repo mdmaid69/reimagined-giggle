db19f928e7b2a59b940a80399dbcb6da07906d15
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:33:41 +0000

    generally worried remark aim fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
