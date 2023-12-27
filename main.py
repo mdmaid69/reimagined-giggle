5a298078dc05d50a68a6a8fd86822b295109f005
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:57 +0000

    no might ashamed upstairs train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
