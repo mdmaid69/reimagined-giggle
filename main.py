f52b4b983f316008dcd9dd8cb2c798fabb5dff4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:00:10 +0000

    evening final fear extraordinary tone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
