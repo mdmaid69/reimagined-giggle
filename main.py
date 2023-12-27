a0bd0d9b90f978c8d7bd9ffe3b7ef2e353b35ee7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:28:17 +0000

    port threaten wide behind Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
