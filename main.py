6f88d272abbdfeb0fa0ca7dcbae7311bd9e53dd4
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:18:04 +0000

    line unhappy until word shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
