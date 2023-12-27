8c4d8408f25ac6e5287a3233840349fd43342eb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:45:13 +0000

    meal respect side hungry let

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def remove_duplicates(lst):
+        return list(set(lst))
