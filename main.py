fcd04852fa80fcd643f6b877b738eeadb1f3728e
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:54:02 +0000

    admit education Jj steer friend

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-print(math.pi)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def remove_duplicates(lst):
+        return list(set(lst))
