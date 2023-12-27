738ce8c7d4449e63ab870ee02dcae1e41a0d8293
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:23:19 +0000

    as react neither radio back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sys
+def print_python_version():
+        return sys.version
