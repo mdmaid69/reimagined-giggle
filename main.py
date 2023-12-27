2fdc4f96b4a0949bc14bcd1072dfb1486992c910
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:34:01 +0000

    hurry keepout moment question expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
