3e3b0a90a0ec0f0e1b823cc4c7021e9457a7bf33
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:49:19 +0000

    ticket fair talk organize test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
