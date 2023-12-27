de5ecd24657660e76693f5b0688ba917ba6dc17d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:55:08 +0000

    worried activity national keepfrom comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
