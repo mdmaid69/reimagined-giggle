5e3e3dcc2a4f3d7b64cb0591c83fb14aa68a1360
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:27:31 +0000

    glass comeout joke mistake dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
