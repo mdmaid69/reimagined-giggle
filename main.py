36da379cdabbdde368a68be9f1ab1df37a200ed7
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:26:50 +0000

    across weather prevent closed disagree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
