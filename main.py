45a43b67445721b6c3f7174973a83b1a87902223
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:50:32 +0000

    inside persuade worry strike apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
