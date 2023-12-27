8ade76cd7715d8486728d9371ebab022fc58d2c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:00:37 +0000

    stone before bringup most treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
