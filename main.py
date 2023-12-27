ff9e102282067841338449fc99769ff0245815ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:24:58 +0000

    fetch original pupil plain rather

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
