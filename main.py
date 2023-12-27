fe4bd00da6b2a785e8343d45eb226f5399407630
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:12:01 +0000

    flight entry conduct knock small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
