b4b401428d6274c5f9d97c19a5152935232d6415
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:43:54 +0000

    try spill hand area lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+print([x**2 for x in range(10)])
