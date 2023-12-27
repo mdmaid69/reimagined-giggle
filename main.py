3974846cc4a666881519517a1975d182ce723047
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:54:30 +0000

    pay rare trap comment unpleasant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_list_from_array(array):
+        return array.tolist()
