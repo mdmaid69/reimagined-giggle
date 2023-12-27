e34ab8690a703ed0d00bbb31469b5cd4b85c2b02
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:15:26 +0000

    fire ourselves fill week rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
