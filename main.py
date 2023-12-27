43ff7a0b1918254d4d2ea818ddca8b6d255703e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:48:41 +0000

    add somewhere today on seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_current_directory():
-        return os.getcwd()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
