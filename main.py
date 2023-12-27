fefc5a668ef4194327c2c84753a97bedadb2c70d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:20:58 +0000

    nowhere pride better stir out

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import numpy as np
+print(np.array([1, 2, 3]))
