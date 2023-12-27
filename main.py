862045a4dc0c07a59398eb9c2a32004fb00cd878
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:54:05 +0000

    me immediate difference roll apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
