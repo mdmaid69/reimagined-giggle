2b5f00d8ee24ae9787abb6cf8a10aab2581d3711
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:44:14 +0000

    brother shock please long accidental

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def is_even(n):
-        return n % 2 == 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
