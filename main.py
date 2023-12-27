03922c44abc7d0fa5b64456ecb769b34a9abbd6a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:29:25 +0000

    advice many thumb after period

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
