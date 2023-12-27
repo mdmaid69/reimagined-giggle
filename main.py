990fc356c96e875395880bc854732e904dc965b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:38:41 +0000

    scrap church best about tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
