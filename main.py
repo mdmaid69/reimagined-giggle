78bfc94da1baed7add8500a4889dfdbba4556c4d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:01:29 +0000

    recovery rain climb protest beautiful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
