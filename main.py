3a18a9e026dd575f1be6708e1914c8f8e3669df4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:20:37 +0000

    crowd voice line connected murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
