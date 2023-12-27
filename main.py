ce3f9624b95a19a72e25b863a42192802a4ad466
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:00:11 +0000

    clean left light plastic keepup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
