c57346fd0555e105a3e021d97723c5a0a31de2e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:37:04 +0000

    address city holiday herself sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
