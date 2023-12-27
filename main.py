a073d1d8229c107937fc167d7168f70e67a4bee9
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:46:40 +0000

    getup tired home fault music

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_work(force, distance):
+        return force * distance
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
