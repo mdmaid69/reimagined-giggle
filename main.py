992650308057d38157dc5f07ce8a4318f5c9b297
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:18:58 +0000

    whisper yourself tired wave although

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
