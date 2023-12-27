6b678bc0065ade62c0fa7eccc2cb587bac56f567
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:18:23 +0000

    illness fair quit bread block

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
