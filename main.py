e6a23dd4ca0bf64b4b7c5445eb5c21e8038a46ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:09:11 +0000

    surprised club shall cheat law

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print(sum(range(10)))
-print([x**2 for x in range(10)])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_speed(distance, time):
+        return distance / time
