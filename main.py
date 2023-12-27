c39a49cd3e9dab0e42f7d2b92fb5c90adb02fbe5
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:36:13 +0000

    description throw definite remember temperature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
