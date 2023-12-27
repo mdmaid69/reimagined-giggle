158fc4cf8332268c7b71f1d1643634457003d7a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:47:13 +0000

    life dead male bottom fork

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
