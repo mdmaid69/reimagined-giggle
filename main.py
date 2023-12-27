9cf0439969dce7bf7acf3f01ced06f48c3d0964d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:50:37 +0000

    fold trust hang low stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
