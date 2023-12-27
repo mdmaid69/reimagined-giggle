0c7d19ff73da20f03150273dd3c1d5cb694018b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:51:58 +0000

    comment squash shout jump actually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
   def is_odd(n):
         return n % 2 != 0
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
