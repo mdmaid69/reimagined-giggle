437b2c68c97af922b11e6b37b0b53d310d96a57f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:19:21 +0000

    willing knowledge cough take period

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import collections
-def create_priority_queue():
-        return collections.deque()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
