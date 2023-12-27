ac1d08c28385fdcb0b46e0e074aedba9f0cbfa90
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:46:02 +0000

    search hello our sweet provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
