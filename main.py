8fe52bb72c3d260b87bde68779550ad769da0722
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:44:20 +0000

    word comefrom unfortunate splash wheel

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_work(force, distance):
+        return force * distance
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
