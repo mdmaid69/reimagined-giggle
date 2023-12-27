6f17e448c3f7a2a90daeb8951f4e6ad82559b2d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:38:10 +0000

    would let mix tight right

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-print(math.pi)
+def get_array_itemsize(array):
+        return array.itemsize
