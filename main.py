2aae800b113d0c6fd1a8a6a78cd9e229cbdf1087
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:28:35 +0000

    increase cheap tomorrow fly preparation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-print("Hello, world!")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Uppercase:", text.upper())
