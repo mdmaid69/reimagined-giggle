33b173866fd9a79359c4ca9e45a9928a94d15ee9
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:44:31 +0000

    shade hour decide wake properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_octal(n):
-        return oct(n)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
