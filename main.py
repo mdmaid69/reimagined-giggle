9c98bfcee26a70e1023311ddd398538235495847
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:06:22 +0000

    passage very variety pressure autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
