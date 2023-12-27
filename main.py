5a6543ad192e7e60276d507176c8ff8b28d910f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:45:25 +0000

    land risk use fork her

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
