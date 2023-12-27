5cb61ad2fdc3751b4da188c1355ce9e55574fec1
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:13:08 +0000

    stranger suppose legal scare surprised

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_volume(length, width, height):
+        return length * width * height
