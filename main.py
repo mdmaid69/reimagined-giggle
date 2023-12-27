1109c8cc52b6be9d634c1fc3ce8fbdd1ab7f6cc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:24:43 +0000

    suit knock cross phone party

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
