575b1daca8490da812544e6a4e888492b21c2d5a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:19:16 +0000

    hardly down repeat properly recovery

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
