f7910a4add95e7e6c8d652016614fac26ba08ca5
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:46:42 +0000

    bend mainly law steal possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_max(numbers):
-        return max(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
