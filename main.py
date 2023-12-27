3232ddf956f2e65e20adb42d3a2e45a24160c2c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:27:23 +0000

    club let occasion close neck

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+i = 0
+while i < 5:
+        print(i)
+        i += 1
