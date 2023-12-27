ba4002a87ef03b98cdb4e1b259f4654d4c80248a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:18:17 +0000

    ability street cheap sting distance

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
