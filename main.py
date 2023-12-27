f1443c076e88bbb2eb91f1b917101c381d9dc6a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:59:45 +0000

    limit remove sell argument hour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
