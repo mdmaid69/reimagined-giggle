d5937074f584fd1de894629a915f7a9620231a36
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:36:53 +0000

    late machine comfort meet fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
