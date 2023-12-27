f601d82325fff09b10275b6801bcebb2c88d7f0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:34:53 +0000

    instrument ought chief right single

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
