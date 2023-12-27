f94b0b1a6fbab70b6994e48cb1a9bc19881929ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:16:09 +0000

    well face area loan time

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-print(sum(range(10)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
