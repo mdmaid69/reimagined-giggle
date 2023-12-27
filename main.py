bde2d9982098cfde10f77a0e236a605ea2dbc837
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:04:04 +0000

    project welcome ever bend close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
