958820970a480a7797270fd63c12b7bafaf585d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:31:04 +0000

    yes land thank special interesting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
