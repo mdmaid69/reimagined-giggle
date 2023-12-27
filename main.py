cd80280dc27a9b0fa81b3d130ac12b6852f4fa3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:13:14 +0000

    influence report even confidence trouble

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def is_odd(n):
+        return n % 2 != 0
