86403b85ca81ddcd398b38ac7ec2bfeceffff972
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:01:45 +0000

    wonder aim getaway fortunately pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def add_numbers(x, y):
-        return x + y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
