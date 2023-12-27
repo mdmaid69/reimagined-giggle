5db32ecfc90ff751d631682c2784293522f20da4
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:12:29 +0000

    getoff move comeup freeze life

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_pressure(force, area):
+        return force / area
