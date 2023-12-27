54a6501cd3d29f3330d1acabba8db1cd768dca36
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:47:37 +0000

    them look practise awake desperate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-print("Hello, world!")
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
