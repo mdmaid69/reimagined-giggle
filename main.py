44c6efe70ad8443710a5f0ec355900702d91f4af
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:59:50 +0000

    convince fat road message have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
