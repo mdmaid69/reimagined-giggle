01da5613116423114a79056aaa82c58a80ee4953
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:26:05 +0000

    job over contrast human strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
