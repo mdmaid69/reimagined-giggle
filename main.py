57cd6679cf8503e12f8f6b348e54da0ace0c118a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:20:55 +0000

    ride fold hello his afternoon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
