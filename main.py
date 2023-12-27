e5239ace75b1f03f8ce61ca426d19ff464b79150
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:14:05 +0000

    police very refuse occasional burst

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def count_elements(iterable):
+        return collections.Counter(iterable)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
