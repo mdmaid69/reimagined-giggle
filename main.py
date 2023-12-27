a601e6404c0ab1cd3922773db6000c21216c4c02
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:01:16 +0000

    somewhere fight display print basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_power(work, time):
+        return work / time
