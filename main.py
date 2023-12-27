4dabc08e518c6dfcfcdbc973ba3cd6f52da15acd
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:51:05 +0000

    target mind sore build mean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_floor(x):
+        return math.floor(x)
