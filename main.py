d5502d7a4530da5d4a70e75678d4bddb1c39e171
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:09:20 +0000

    lead part minority embarrassing short

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
