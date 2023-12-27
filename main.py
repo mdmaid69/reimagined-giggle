0b3d83e78da99c1c9d42133c95b4cf1167e8436b
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:00:44 +0000

    573entertainment woman bank rescue right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
