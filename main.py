1280add8a8b50aa8f6bf0f73a9b446a29cdc883b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:21:17 +0000

    disagree off stone popular joke

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+print(os.getcwd())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
