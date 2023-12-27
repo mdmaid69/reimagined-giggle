dbc7a229481f16ba9c3b15e6527e97a8e2f0b245
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:19:35 +0000

    pool skirt contribute trick train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_list(array):
+        return list(array)
