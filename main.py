08a5c6e96a75f1132fbeb4130d0564eb36e94739
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:59:20 +0000

    lookaround cruel least jump grand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
