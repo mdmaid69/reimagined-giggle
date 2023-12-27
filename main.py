0abdc464f7084dacc92fb6d5df34e77c82c699f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:41:02 +0000

    callback freedom takeaway situation sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def reverse_array(array):
+        array.reverse()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
