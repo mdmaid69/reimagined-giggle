07c97965832a0dca8e10d9a7228c868ece5acc98
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:32 +0000

    congratulations arrive minimum along press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
