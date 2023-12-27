028c0aa2ee70cac57015e72b29f11b99dacdb62f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:05:21 +0000

    indeed godown news become round

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
