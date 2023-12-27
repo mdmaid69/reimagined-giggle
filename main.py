c16b3f0ef76c62237a25a3aaad794ad198c009cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:41:49 +0000

    fashion steam imagine ride yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
