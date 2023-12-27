d168385e5ed6dd73f572a7cf100c981617c57170
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:42:52 +0000

    fit test perhaps man private

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(5):
+        print(i)
+import getpass
+def get_username():
+        return getpass.getuser()
