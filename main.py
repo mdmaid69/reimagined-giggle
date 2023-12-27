73238d9af8c064c72bb47ae06cc99885e339cf00
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:00:41 +0000

    confused attach pool means beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
