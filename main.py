c357f9f28433bd245f030bd599708a069a8a3b7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:53:11 +0000

    eye but warning say sea

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import random
+def roll_die():
+        return random.randint(1, 6)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
