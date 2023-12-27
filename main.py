e08b011722a76cc7401650e00c276a64f6ff5dbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:55:45 +0000

    to fear cap Tt sort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def add_numbers(x, y):
+        return x + y
