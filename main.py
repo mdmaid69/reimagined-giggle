dc199ab12e33e6b71f4b3b270085f9b9bfbf738a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:48:03 +0000

    left finger lock note rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
