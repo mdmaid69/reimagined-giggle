3106b98eaabb1f25d53dd0f652b3616e6865c777
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:14:49 +0000

    apologizeapologise accident wing crisis sweep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
