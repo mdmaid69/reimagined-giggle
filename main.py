8016db3e4627543a72162a4b4db52637c58265c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:50:05 +0000

    good contents important grateful exact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
