81cf6f31b8b510fc7655be5a6e7a6f36ec224e8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:06:01 +0000

    couple moment Cc anybody punish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
