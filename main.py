385532b0d304c40761504481f0bb7cb4805edb41
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:50:54 +0000

    violent worse central tonight ask

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
