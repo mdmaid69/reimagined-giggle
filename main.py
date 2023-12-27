20e50727058d42efd4102535db7d1c0cb403dfe4
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:23:12 +0000

    hurt reply flood priority wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
