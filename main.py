cd5d5e513815d4088186ded37913bd066034818f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:16:15 +0000

    average loose common noise fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
