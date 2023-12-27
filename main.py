8b1b47c2c50cd170fa94468be39508a0c6db609d
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:02:31 +0000

    Ii man by turnon unless

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def clear_array(array):
-        array *= 0
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
