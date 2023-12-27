48238a579b57f2675564944defb082a77b39fd99
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:15:41 +0000

    height Tt in high later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def count_elements(lst):
+        return len(lst)
