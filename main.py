c9d9679bf26f6bc855e174060400ce847b6e2937
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:24:28 +0000

    fit common opposite guess positive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
