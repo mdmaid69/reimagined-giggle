8785b463313c5c240191a1405519176f3fe54b22
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:44:47 +0000

    how pain difficult joke pretend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
