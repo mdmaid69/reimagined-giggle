28329929d7dbf264405abf0db1a352928b921ea8
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:16:38 +0000

    meet oil same maybe division

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_max(lst):
+        return max(lst)
