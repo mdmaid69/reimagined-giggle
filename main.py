7c34682979e75ab7f90f6a28b12135af97e0e900
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:20:54 +0000

    Ll able by day recommend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
