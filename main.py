c3c749919225af220ce2e103a5637504c6c53540
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:29:42 +0000

    bad command wish live guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
