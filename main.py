528b5aab41ccbe2766dbc504359f663cfb145fa1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:39:29 +0000

    expect wake probably accident that

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def is_even(n):
+        return n % 2 == 0
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
