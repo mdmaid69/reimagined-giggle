f3fd6a04dd5d6522bccc7fe838290abc895d3075
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:44:02 +0000

    hang print container unless develop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
