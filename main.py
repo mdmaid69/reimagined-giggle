dac1346197d8d233a8f3ff0349d7bfd2da69af8f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:06:58 +0000

    getback contact before still come

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
