3cc6a8555e98ffee1d27b707a699991070441599
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:25:49 +0000

    hit black preparation mystery excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
