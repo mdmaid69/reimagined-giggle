de5af3a91a7f8c8952e53a831034aef79f34ea5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:07:51 +0000

    personal shoot contact hide food

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_str(array):
+        return str(array)
