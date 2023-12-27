4e4ed0116ac3c770022ee987d042441c3905b84a
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:03:10 +0000

    remain muscle perform fair free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_length(array):
-        return len(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
