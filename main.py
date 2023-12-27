f7d208fb65125be7abc9dcdc0a80fff81bd7d422
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:16:07 +0000

    cost return thorough recovery nowhere

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
