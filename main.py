18c09b68a8566ea7764bc432400d77c11ead2970
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:45:06 +0000

    difference avoid turnup poison form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
