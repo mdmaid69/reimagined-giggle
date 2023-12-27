22c13cdc89770961a8514d3447eaf1e43a4f0035
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:29:14 +0000

    will cruelty world several extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
