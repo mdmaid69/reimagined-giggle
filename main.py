a7a20936eda4ef05cda576655b653930836fcd9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:33:17 +0000

    comeup wave round invite excited

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
