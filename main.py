d041433cba240414231299857b0d9b78581ab1ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:50:33 +0000

    generous name takeout 564encourage spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
