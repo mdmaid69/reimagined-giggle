368b562048ede40a1195b404f7878d77b9a75b4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:10:29 +0000

    target live lookaround experiment top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
