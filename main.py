7be0355e285bc3b2f0eab2e1793172ad9693be53
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:36:49 +0000

    sound summer unit work people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
