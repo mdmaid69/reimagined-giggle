d15a72379e56b3ad277992dc9e30ffc80cac8dc4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:37:52 +0000

    empty line switch arrive goodbye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
