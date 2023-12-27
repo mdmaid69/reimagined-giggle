994d26f7f381c8b00c661754238467170f8d19b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:20:14 +0000

    cross alive juice spare male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
