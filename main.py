eb13a97d20eee608e5ae7fd5274c005540dc9b30
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:00:26 +0000

    frequent yourself makeinto sweep pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_area_rectangle(l, w):
+        return l * w
