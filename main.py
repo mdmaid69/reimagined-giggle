eedf723e9a2d08941f3bd8256e0b2664f8641389
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:19:43 +0000

    market sky copy start trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
