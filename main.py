3a4c7c754d6e40bb21e12082de9d3eb060810f8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:46:11 +0000

    yourself doubt pool our peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
