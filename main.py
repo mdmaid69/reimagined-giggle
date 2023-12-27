69c9cddc10826823e0e798077dffcff0bfb9b236
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:10:16 +0000

    flavour health hold protest above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
