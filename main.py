adcfc0a8fc0bd91934f5d4af98df24ff14944c62
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:29:54 +0000

    rob second clothes burst nurse

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
