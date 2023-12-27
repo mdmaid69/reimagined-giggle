1b75f8ea3686a8ee69bbcaa0ebea073b036fb649
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:47:33 +0000

    argue even thinkover cry study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
