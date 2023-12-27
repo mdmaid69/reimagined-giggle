c755c749ca672d8dc6cba0c8664768a91baec7a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:32:17 +0000

    progress run down pay plan

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
