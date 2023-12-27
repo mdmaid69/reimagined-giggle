c00470c70d3fd9daed59f496e7b4fdf77f727427
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:51:06 +0000

    earth giveup sister bar transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
