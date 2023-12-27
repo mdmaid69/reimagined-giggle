c2c3bc8ffd02d45116740de1cf64d5102de39e77
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:37:20 +0000

    ticket importance art forever book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
