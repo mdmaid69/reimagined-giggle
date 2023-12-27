f43e33a4fc35a4210ca669a31defd0ef3d1939c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:08:36 +0000

    wait workupto callback thin side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
