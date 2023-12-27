c7a73a9e3218d56f48549196f6b452805bdb6e2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:28:45 +0000

    most board replace hurt cheap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_min(lst):
-        return min(lst)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
