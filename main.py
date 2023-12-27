f337e73637e1a4fffba0c0f1875531b618aee665
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:23:51 +0000

    fill draw meal adjective movement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import heapq
 def pop_from_heap(heap):
         return heapq.heappop(heap)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
