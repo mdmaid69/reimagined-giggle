e64cb2934cd1e0331517fbe7d6fed9d411b34c22
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:22:26 +0000

    holiday passage swimming win describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
