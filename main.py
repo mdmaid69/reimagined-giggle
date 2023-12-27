4651da32829c5d1244b5754b889339e1c5e91fab
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:42:15 +0000

    children official how extraordinary shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
