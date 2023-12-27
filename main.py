4a0a9c5a39d65e8d600b4c8b62217388326774e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:58:52 +0000

    fight connection further 572enter ocean

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
