b6f64720c96570118f3bd659b6829641f9009a47
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:25:55 +0000

    unkind brief total pupil interesting

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+for i in range(5):
+        print(i)
