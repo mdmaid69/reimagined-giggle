d2676e589140e788c87c67196f6b9ad5b758e647
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:37:08 +0000

    certainly possession drop possible squash

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def cube_number(x):
+        return x**3
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
