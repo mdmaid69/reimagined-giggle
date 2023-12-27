7aeb59a35f8198c9e07556970aa142e1e896f4e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:24:21 +0000

    region perfect good sort pull

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def count_elements(lst):
-        return len(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import re
+print(re.match("h.*o", "hello world"))
