8f6901c49f579fdc1500385dfaa22fa10a172b44
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:23:46 +0000

    Mm sale at getup necessary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def convert_to_binary(n):
-        return bin(n)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
