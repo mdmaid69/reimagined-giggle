cb7ed4df1479fafbb83fb0832ac399126c16da72
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:11:18 +0000

    change minimum anybody keepout neat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import logging
+def log_message(message):
+        logging.info(message)
