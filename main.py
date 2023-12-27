05afa12825ff45823b12e64f655b6b9508630517
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:22:21 +0000

    harm instrument mind back lose

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
