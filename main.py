a9d98343422d5894b8e6e9043e87914001596535
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:11:09 +0000

    level giveup yet warning hang

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
