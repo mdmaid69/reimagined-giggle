6795e567d65a81ea184e6bce44afcad75cf24170
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:20:35 +0000

    today reach market short different

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_power(work, time):
-        return work / time
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
