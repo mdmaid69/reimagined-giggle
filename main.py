66cf6c23c68c2a12c18c615b6288c49119c53c0e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:46:12 +0000

    reject weekend closely print underneath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
