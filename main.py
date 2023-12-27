3d5ab6f61c13ac23dc8f1abcc4679f60e7144cd7
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:20:34 +0000

    respect away disturb disappointed she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
