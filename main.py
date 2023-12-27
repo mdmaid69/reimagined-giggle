2dcf528bdddd7299035c50c18a9df69a2e8e97f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:49:09 +0000

    indeed above taste runinto confused

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
