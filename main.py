c09dffcecc239df2270417d924537050174cda87
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:09:57 +0000

    public wish light us catch

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def is_odd(n):
+        return n % 2 != 0
