7cfe80a261058a22429fc456b732295956b7ca3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:34:08 +0000

    travel breakthrough duty even peace

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
+import time
+print(time.time())
