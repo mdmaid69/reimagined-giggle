c854f63b86dce8ee9855eae74571b0c0b68922ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:20:26 +0000

    join stone plate crowded unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import array
-def get_array_typecode(array):
-        return array.typecode
+def remove_from_array(array, item):
+        array.remove(item)
