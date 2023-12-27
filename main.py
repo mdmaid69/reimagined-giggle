06d30a0e1e4a5b3a66e24897d7a8c760c1d1048b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:03:17 +0000

    OKorokay 564encourage between concerned cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
