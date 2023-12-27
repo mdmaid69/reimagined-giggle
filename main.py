da05ff7bbfed2750f54d3aad6ad91add8dae74b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:49:19 +0000

    arrange thinkof negative course rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
