a5808dea5725885beca1abf57ee73f5fccd08541
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:28:56 +0000

    Itself remark regard announce thinkof

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
