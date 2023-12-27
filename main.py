2d581a47ca7dc345df229da89199660cc68a4fea
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:21:11 +0000

    vanish arrange rest tell along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
