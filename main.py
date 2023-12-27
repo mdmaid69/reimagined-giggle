c2832514396360c95314297ac0eda060749ae237
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:27:26 +0000

    beat deliver comeover draw Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
