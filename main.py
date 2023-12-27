f44c3873ecd96666eab46a79b91770dbe8b30bf7
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:32:28 +0000

    thinkthrough detail coffee keen much

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
