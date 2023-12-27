1c56205f7093ee96ddb606753de8fd779f899ed1
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:35:05 +0000

    ring distant certainly above injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def change_working_directory(path):
+        os.chdir(path)
