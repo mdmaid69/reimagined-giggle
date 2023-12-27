72a0bc6a098e07175247e919097b7f1cdb5c965a
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:06:59 +0000

    duty 571enough since remember thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def remove_duplicates(lst):
-        return list(set(lst))
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
