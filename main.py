4d29c09bf43e4282b7e885cc67f3385d14111eb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:36:38 +0000

    pencil wide dance everywhere crash

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
