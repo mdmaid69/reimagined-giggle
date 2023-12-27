2b80d93159dde96ed8fc167f624a86fddb9b096f
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:22:16 +0000

    train gooff pick occasion laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
