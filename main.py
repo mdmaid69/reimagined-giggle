4b1f2b4aca865f44819fdc514ddf02c1e48e1953
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:04:27 +0000

    development plastic silent comment achieve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def remove_duplicates(lst):
+        return list(set(lst))
