90eefd61454500c1d85880577765abedeb01ba0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:13:44 +0000

    anywhere kind several long beyond

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
