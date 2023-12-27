58c101ead6e57852aabd364ffc8813a42aee6f68
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:06:28 +0000

    key apparently means old inform

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
