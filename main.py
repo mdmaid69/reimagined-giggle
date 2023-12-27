90eb0a7bb410530e22bcd127ad2a2ae38c330bfb
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:22:49 +0000

    pronunciation substance much cover everywhere

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def add_numbers(x, y):
+        return x + y
