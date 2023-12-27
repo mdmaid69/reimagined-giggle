0ff868a0ea1aaa42b818d8fc42968ba514f6800f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:52:02 +0000

    talk whichever manner central tear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
