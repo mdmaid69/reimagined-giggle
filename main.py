8a824d7d7490f86f1208ac6b89f1f564a79ab912
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:23:50 +0000

    unfortunate getaway blow getback anyone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
