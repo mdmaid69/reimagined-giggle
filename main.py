0144841d2eef94b3d10182287ac9d5ee6dbec1c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:33:54 +0000

    violence purpose comfort calm crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
