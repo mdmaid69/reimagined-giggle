76d90ad6e7ba5fa0496bf79d89fa3f81f98a997d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:11:59 +0000

    past apply hide star speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
