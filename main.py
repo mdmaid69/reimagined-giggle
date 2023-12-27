7620db0b774eaf5024400eab46b9ffea8ba1789f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:27:40 +0000

    what again morning friend forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def convert_to_binary(n):
+        return bin(n)
