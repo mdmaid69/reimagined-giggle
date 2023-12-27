77f9b1a2d0958b4968af36b5d89424f8a4241d95
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:35:26 +0000

    explanation apparently dark since range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
