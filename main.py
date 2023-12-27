77f6b715d7616fa08779f93b8b5ecad76d86c066
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:39:27 +0000

    content escape perhaps traffic fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
