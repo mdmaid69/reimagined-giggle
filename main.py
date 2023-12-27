de74c25019cdf6377565d50e44eb7816eec0cd48
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:33:55 +0000

    trap quarter argue amount difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def find_max(lst):
+        return max(lst)
