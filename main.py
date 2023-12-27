02a738cc33c786cd058edb93b8af5e810a7bbbc3
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:35:06 +0000

    line tie apply sink left

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def convert_to_binary(n):
-        return bin(n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
