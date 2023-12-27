2d3266efd222d56e9a132b7994461e780f58a027
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:35:21 +0000

    clear curious glad conclusion fire

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def add_numbers(a, b):
-        return a + b
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
