a22516258f85950a9c3032331f9af9c4a5688220
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:43:58 +0000

    noone best university tight complete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import os
+def remove_directory(path):
+        os.rmdir(path)
