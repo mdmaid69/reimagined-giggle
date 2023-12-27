bbc3599e6fb9f591369c58e1cb422f57a5203996
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:43:15 +0000

    only row getout claim hard

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
