2d73c6d502262a523551ebfe8e737f019a19a23f
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:36:52 +0000

    watch summer region easy shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
