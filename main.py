9208ca69685b2a6dabd7be8205f2aaa86b2c4d08
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:18:18 +0000

    either invitation thinkof anyone wheel

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
