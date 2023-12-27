647b02c23c238dc4dee3ae194036e1ee65e81a73
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:31:37 +0000

    middle related each decide ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_speed(distance, time):
+        return distance / time
