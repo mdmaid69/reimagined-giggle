fe53eeed101c8b411681cb947708771362ed0da1
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:56:30 +0000

    insist dance ordinary and treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def get_array_as_bytes(array):
+        return bytes(array)
