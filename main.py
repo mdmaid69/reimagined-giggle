1f046005a6ba2772d63ffa604b83ffb809cb8a14
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:39:52 +0000

    use middle rarely worth satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
