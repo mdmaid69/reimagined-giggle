63629613847c044bbbee73698768b8807d8d8bf7
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:37:05 +0000

    constantly actually eager recently face

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
