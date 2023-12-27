43840777be84c1806d1ec68c62205f388047a0f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:47:36 +0000

    reduce exact design shout call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_power(work, time):
+        return work / time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
