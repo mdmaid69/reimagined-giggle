faa35947f4fc621ee5ac20fd7fceb5277b13ac01
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:46:58 +0000

    base intelligent pocket time luck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
