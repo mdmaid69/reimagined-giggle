d5ee1829dcec8731476094363a448bae0a9fa642
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:07:20 +0000

    concentration shallow much area grand

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
