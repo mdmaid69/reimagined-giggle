3cbc3b8c553ecc20b420e34fe6bb496824c10234
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:43:25 +0000

    ahead belief tend affect field

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
