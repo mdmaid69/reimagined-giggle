abe71b3ea1b335966c44124c29d044217af84622
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:24:53 +0000

    often Yy into seem confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
