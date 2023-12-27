769b0f36bf7152c66af17d132e0456acfb59efae
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:30:20 +0000

    gate character temper custom major

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
