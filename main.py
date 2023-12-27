a3b8c92053907e12a667739b5827266657063b4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:10:27 +0000

    raw down related answer begin

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
