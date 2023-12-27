f33a644289fbc6b42f2744f7efa7576ddcea2a7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:17:42 +0000

    sail comein book excited aim

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
