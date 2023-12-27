90a50df6674fd8b3cdf2962a8c336339bd85b627
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:55:21 +0000

    injury dick bringforward mine get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
