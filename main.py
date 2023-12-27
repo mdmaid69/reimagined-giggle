b0569ad6f9209fa56ddb8c3104ac28d2ae83f39d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:23:23 +0000

    waste makeup lead contrast dance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
