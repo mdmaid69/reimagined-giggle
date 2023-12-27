b077e60fb4c1f2df004ffa70d2ce85093deee089
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:24:11 +0000

    force with stick dress far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
