ea970a5d4010530606ff5e19ef1212dc899ba490
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:11:17 +0000

    now heavy shape sign nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
