22db4090dd969b5be4964bb7e95f4a523ba35995
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:31:42 +0000

    lookback(on) stare cry lucky check

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
