267d1ba483aa3e6aac713470ed79b3ca3368e593
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:44:40 +0000

    part together about accommodation refuse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+for i in range(5):
+        print(i)
