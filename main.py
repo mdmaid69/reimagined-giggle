f135eab110f5f97bd8a55461edb6f31dd9ece9aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:04:24 +0000

    various draw broken favour load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
