23b68daee1357d0d55eeb0ab9b563cb300c71d68
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:00:21 +0000

    scrape cry earth watch smooth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import platform
-def get_os_info():
-        return platform.uname()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
