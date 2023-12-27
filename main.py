87d88c255dc9540ef9fea5c3a74750bca5db1e56
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:24:42 +0000

    add dig sail soak ask

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
