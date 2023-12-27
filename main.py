996eece6c3cca48b45a45598616b2575a0f97eb7
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:27:26 +0000

    day accommodation sell back side

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-print(math.pi)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
