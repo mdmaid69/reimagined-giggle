7085cca3f4360c8893ef19b78ab295f0d9b7d44a
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:34:51 +0000

    kill unless neither just smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
