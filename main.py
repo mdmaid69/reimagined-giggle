0f11c6cd33fb3f369cce65d5c7246f982b3dd557
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:55:59 +0000

    description it like careless out

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+print("Hello, world!")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
