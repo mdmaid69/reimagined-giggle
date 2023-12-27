a078eab2ec7325ee84c604bc95c3e8a70815c576
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:01:46 +0000

    hello influence collect prefer past

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+print(math.pi)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
