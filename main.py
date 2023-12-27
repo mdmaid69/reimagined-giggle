befee1da65fae9d7f4b3e94838a74e6b56d8ef3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:41:27 +0000

    introduce throughout part thank rush

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
+  def multiply_numbers(x, y):
+        return x * y
