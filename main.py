99a190b009af030b3f702ace1b21ce7d77616d46
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:34:06 +0000

    unhappy beneath abroad attractive trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
