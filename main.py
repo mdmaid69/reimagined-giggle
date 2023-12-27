2f74e49db8c8364edd4b60277f6bd49efcb479cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:52:05 +0000

    study small anywhere defeat succeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+print(datetime.datetime.now())
