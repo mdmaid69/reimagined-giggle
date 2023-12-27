5904904f92ace0e305f715b77b4960cc6d40c8af
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:38:10 +0000

    frightened acceptable near takeover second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
