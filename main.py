f7dafe69affcdf85206df527f17294e379b7283c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:47:25 +0000

    challenge bringforward keen few whisper

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+print(time.time())
