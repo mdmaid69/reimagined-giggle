f7f4d170e2a449868bf6b365c045e9fa99711444
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:39:17 +0000

    greeting ago identify ask star

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
