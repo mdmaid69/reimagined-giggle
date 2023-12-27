059500dedfe00cec52d6d8adebb98cd5a221893e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:21:31 +0000

    likely please light view worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
