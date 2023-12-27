aa48dbb5b20d93049f1c225725689ebce3bb1302
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:36:16 +0000

    nice nonsense shall woman factory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
