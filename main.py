a9e3729b75702dd107cd1b4cfb2f523ddd4febdb
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:50:01 +0000

    only trouble deep medium now

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
