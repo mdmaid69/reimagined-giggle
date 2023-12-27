c362ca2f41ac3c83fbd195d5b241bf51a0756d67
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:27:39 +0000

    female organize most tip ring

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def count_elements(lst):
-        return len(lst)
+import collections
+def create_counter():
+        return collections.Counter()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
