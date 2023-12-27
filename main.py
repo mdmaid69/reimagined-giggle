2b71d02ab7eb7ac97d9ffb03bf9535d6a9c04439
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:51:53 +0000

    form die police opposite mean

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
