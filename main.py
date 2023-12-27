ba7b27930bf427d615c94a4e5cc6390d1d8f30b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:49:41 +0000

    spray choose settle female somehow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def convert_to_octal(n):
+        return oct(n)
