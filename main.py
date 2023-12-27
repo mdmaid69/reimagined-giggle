308aaf3a63e08c5a6cf3b5a771c81c7f7d75d770
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:01:49 +0000

    conduct statement maintain chief love

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
