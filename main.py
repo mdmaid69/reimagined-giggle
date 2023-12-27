694aef9c77f31497465a07441e5beeb8c365be7d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:06:08 +0000

    though development contain nurse future

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def divide_numbers(x, y):
+        return x / y
