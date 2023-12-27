47d82a5b4fbd92e19ce52ad9b6bef2db9c343e58
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:27:50 +0000

    fight rain nearly above history

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
