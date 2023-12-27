0bb7ec78cb562e52d8b601f0564d86a153414339
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:08:05 +0000

    surprising offence consist pleasant beat

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
