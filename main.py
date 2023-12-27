8931e64cc83d2bb7e43cd6ee3bbbf224e17ff83c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:27:31 +0000

    office adjective form total advise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
