008de1f4aa1ec8d96e8f0f551805e547af212607
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:41:11 +0000

    cause him excuse day listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
