e417d0227f8c29ec30fc34d8a7df2e3a90eb5eca
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:40:12 +0000

    yes politics effort authority geton

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def reverse_list(lst):
+        return lst[::-1]
