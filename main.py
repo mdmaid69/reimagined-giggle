1d5ade8261ba36375d6940b96ebdc41d0ca80d54
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:13:57 +0000

    describe centimetre notice date workon

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
