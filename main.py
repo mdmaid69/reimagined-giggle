d12e3111e8ea512e767b12366a246550ae3b65dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:10:34 +0000

    low crime state especially accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
