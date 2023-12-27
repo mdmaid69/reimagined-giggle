6f0c1fcb0e0ed11ac7f7fd0e35c4351f16dd3789
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:48:31 +0000

    offer any arrival Itself official

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
