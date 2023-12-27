7bae8c215c494acbd8144e08a3ca9b79a98ac65c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:58:08 +0000

    climb read opposite worth still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def get_current_time():
-        return time.time()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
