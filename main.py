a2d01af8d5252115fe414ba4c758636efe71d7a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:18:15 +0000

    poor look fit certain which

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
