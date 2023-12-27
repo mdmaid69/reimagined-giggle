8db4884f1257dd58256f0cdf221b400b78ee8bae
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:09:56 +0000

    sentence terrible exist related violent

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-for i in range(10): print(i)
-def is_odd(n):
-        return n % 2 != 0
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_sine(x):
+        return math.sin(x)
