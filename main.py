d88b2fafff2e46368633d4508d9c6676baaee78f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:19:13 +0000

    then question grand admit run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_bool(array):
+        return bool(array)
