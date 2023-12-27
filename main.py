448c9f02d2c8340df6c22247395fe38d4e3d2c6e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:53:24 +0000

    tense worst gift operate print

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
