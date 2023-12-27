91bd56dbf01bd28e7901752af3db2acae6381574
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:24:33 +0000

    degree rough comfortable minority what

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
