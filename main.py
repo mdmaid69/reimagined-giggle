9861c78bf21fbae0bb154e20eca5e0f4a6902b26
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:21:13 +0000

    sink challenge bend nice explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
