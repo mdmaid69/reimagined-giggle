4bba6e1fb51f51a03a4c65b23b867aa789971079
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:32:21 +0000

    feature payment affect considering callback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
