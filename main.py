335e0416154d92ac86835279ed77b6dab73c9f04
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:46:04 +0000

    drawing Ee jump thinkover taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
