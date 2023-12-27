804b22477c55fb4d115a6d02b8e749e343762dee
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:44:19 +0000

    depend flight sticky of later

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
