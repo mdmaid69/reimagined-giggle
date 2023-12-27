8a4e986005ade277a742b2666edb9c71833187c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:21:39 +0000

    exam wave place again reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
