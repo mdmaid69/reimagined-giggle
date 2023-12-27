bd349c759157c03dc9a964fc85a4451b34220ad2
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:13:45 +0000

    follow get declare punish sink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def count_characters(sentence):
+        return len(sentence)
