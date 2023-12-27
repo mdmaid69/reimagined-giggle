d0de9e0eef2ae2c4b5d86cf56ecb952a1317355f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:08:24 +0000

    breakout bite false solve none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+print(re.match("h.*o", "hello world"))
