699fd60fd8649f46482dc1ff2c77a738cf678276
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:40:40 +0000

    bring earth concerning through drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
