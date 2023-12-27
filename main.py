cb5fb965c2fcc77a541f95f869edd3f1f48268b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:20:00 +0000

    past compare switch unable peace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def convert_to_binary(n):
+        return bin(n)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
