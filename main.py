131a29521504471a11a4c0d6848b69af2854dfae
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:01:42 +0000

    popular standard no sea man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
