044bb70542a99a37f5230585e9c134afda4e12fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:23:04 +0000

    already address splash try miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
