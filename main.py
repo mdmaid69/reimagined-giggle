f6fb41167c6dd6a05b080ddcb388f19489c52bf5
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:59:43 +0000

    forever quality tell slice spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
