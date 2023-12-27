ecd14f1f194a61b6ea1e1ca2ba9d96795d534a8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:43:23 +0000

    borrow central pick shrink interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def reverse_string(s):
+        return s[::-1]
