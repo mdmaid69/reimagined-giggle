36c4794b51e39a06ed449c188b13f37f7b09474c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:42:02 +0000

    book public suck appear pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
