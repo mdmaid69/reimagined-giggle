d0c42ec67853ff6da4029bd8887805622117cc8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:26:31 +0000

    affect reduce avoid her be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
