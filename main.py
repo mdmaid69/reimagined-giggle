08a7a7f869a6e70382d321dba84c5c7904850fd3
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:12:19 +0000

    visit wind goon hurt tonight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
