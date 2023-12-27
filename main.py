a6e4720c8697442b0f6a16a41696f76eed3c0153
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:18:05 +0000

    to bar pool better open

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+print(math.pi)
