96e67882550517b0dbefd709c8fadf9d0ed1eee2
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:44:23 +0000

    breakfast thick crime understand comedown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def clear_array(array):
+        array *= 0
