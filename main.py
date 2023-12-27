be5454ff2e91aa2291666ed60c1a893ff55436dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:25:10 +0000

    trial pain size rich fierce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_current_directory():
+        return os.getcwd()
