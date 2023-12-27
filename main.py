75eada203a4476bc7c2d0588fa29b7a60ef1ec7f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:50:18 +0000

    forgive arrange entrance fortunate admit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
