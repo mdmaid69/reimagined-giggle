5b1aab737973dbdd84b4d1b9480e14a36c89efd6
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:45:39 +0000

    exciting harm goback(to) instant effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def square_number(x):
-        return x**2
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
