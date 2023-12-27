a0e4f463f80f01ed65e85d446d806879bcdfe7a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:00:31 +0000

    general fortunately grand usually may

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
