71bfbab7c45695c5e5068db08bb75e196312d7be
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:25:33 +0000

    danger well meaning press dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
