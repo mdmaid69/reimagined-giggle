b4e003f2b46ccd8f4cf29e5b2356e7adb0a27057
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:08:25 +0000

    one criticism land left workupto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
