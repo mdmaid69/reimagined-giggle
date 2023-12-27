9dd23deaf159705760c3acfbcdc973a5d86018fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:52:54 +0000

    imagine race gas impressed release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
