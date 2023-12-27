0361d052d636742d2f7f2b675526b63131f34aa4
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:31:38 +0000

    Vv no safe job upwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import collections
+def create_user_list():
+        return collections.UserList()
