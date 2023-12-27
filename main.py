332c9161bd5b6751880607d578b9576c688168de
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:00:12 +0000

    plenty classroom meanwhile complicated right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
