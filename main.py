f2e0399ab9d699f7134898c816a9c1d31af07857
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:48:33 +0000

    gap steep discovery crash splash

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
