1b3b96e3da41fdd1acd3ab762a3919dd3846e942
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:46:09 +0000

    every tear principle plastic branch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
