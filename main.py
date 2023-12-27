e5e4c9e3753ecf815ae0756484673ab230a39696
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:39:23 +0000

    worth tragedy middle freedom handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
