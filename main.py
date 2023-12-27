01ef22411e33f52516cbb6896d6a10d7eb0607ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:20:58 +0000

    wash aim fear share exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
