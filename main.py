ea4967b569b139f55a83d262ccd802bd399f3e04
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:32:24 +0000

    fall payment long strange final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
