77ed9b88ac6190520e08eb490e1737e2978fb56b
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:34:15 +0000

    port among lean ordinary area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_str(array):
+        return str(array)
+import logging
+def log_message(message):
+        logging.info(message)
