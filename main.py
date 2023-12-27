c167b32b630ebb96c6b7adabcd0a14de270a985d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:12:31 +0000

    survive loan through what used

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_power(work, time):
+        return work / time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
