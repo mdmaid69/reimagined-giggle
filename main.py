ba24caf2106e6b5e5bdf6587a0ae75668be6d0c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:31:48 +0000

    recent hit jump pretty summer

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
