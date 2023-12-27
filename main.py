8ba024986dcc63dc2d8f89ce8e0143db918b5bf1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:16:49 +0000

    experiment refer check angry unpleasant

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
