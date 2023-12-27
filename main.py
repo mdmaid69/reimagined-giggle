9768325601f2a2f4895f3468c154572fbecbb9ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:27:55 +0000

    protest up open rub strange

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
