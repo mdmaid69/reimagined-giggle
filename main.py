f972b8bbbd72cc826d9b4246f3dd459930edafe4
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:41:09 +0000

    match everything call closed precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
