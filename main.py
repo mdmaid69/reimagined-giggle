fbe8a6902dc9b9d059adabb54d0a4c5adbf7bc66
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:48:09 +0000

    minor change report train handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
