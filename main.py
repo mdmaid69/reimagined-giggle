129a696082a1f732dc34e543de4a4a1fb5ffbfde
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:13:14 +0000

    authority bill human quick if

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
