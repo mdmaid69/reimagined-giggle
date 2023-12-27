bfef52f1b3637c47782b7407819a48f2c354ca7c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:25:29 +0000

    beautiful arrange care steal children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
