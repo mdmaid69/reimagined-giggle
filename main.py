cb1143a41ade91eaf16eb2a606c8bdfe97174e1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:50:33 +0000

    angry keepoff plate somebody base

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
