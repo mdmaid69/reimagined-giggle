d57cbc29042d8acf1c043fcf2233031f5b663d23
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:36 +0000

    wish take wet tip definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import math
 def calculate_ceiling(x):
         return math.ceil(x)
+import getpass
+def get_username():
+        return getpass.getuser()
