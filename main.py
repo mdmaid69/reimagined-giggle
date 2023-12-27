224f812dd58d939469f2737a7f6edfb5a8d45978
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:55:43 +0000

    would holdup piece neither look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
