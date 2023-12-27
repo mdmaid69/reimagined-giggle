2e44693d464e65a8c5019b2cd9232e46b4e40e9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:07:29 +0000

    always convince surprise about statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
