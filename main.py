23227efb45cb440f38bbfd6625287f498a59a671
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:26:05 +0000

    exactly strong send sense coffee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
