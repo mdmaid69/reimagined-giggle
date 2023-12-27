af6e809c482cb6fbe261082c4b2fba717d436886
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:16:57 +0000

    centre lift disturbing introduction telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def clear_array(array):
-        array *= 0
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
