980bcfad469564afc436dc117b105e88ed3ac3b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:45:24 +0000

    whether mind service claim turnover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
