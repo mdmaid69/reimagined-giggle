cd103f7338f1d9389ecb1feddf1e2df7621ce5be
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:45:46 +0000

    around best later lower lead

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
