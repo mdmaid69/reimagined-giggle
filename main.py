06681fd249c82ef1dca6bf6122d77e56b2900557
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:24:28 +0000

    their region substance bottom answer

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_area_rectangle(l, w):
+        return l * w
