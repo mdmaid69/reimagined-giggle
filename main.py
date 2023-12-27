a7b1dbe5eb4a2ca1f987b2123a617b67f7238119
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:35:49 +0000

    fat bus clue suppose let

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
