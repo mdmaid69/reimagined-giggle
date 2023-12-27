93ecc1204836525aa415376c5bcaecf9873c667d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:44:02 +0000

    passage whenever hospital check off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
