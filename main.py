665b5d0e0a9d2d33f78c3e884fabc1c87b138784
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:57:40 +0000

    unlucky dig for half circle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def divide_numbers(x, y):
-        return x / y
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
