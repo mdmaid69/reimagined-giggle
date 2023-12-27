eed48e2b7079987e93d83234735f743b3af297c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:49:12 +0000

    name within announce worry file

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def find_min(lst):
+        return min(lst)
