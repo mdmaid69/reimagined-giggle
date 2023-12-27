d404ad7e643bd2f912f13642f2710cde4faa10d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:57:56 +0000

    home nature lean by trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import numpy as np
-print(np.array([1, 2, 3]))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
