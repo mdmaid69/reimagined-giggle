849dd4e899259042cf1e19046b1acefd92cd7346
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:35:42 +0000

    wander afraid along computer pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
