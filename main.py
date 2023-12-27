f4c7ab3bc894cfa6bbdf7a194f45a49018f42d62
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:10:21 +0000

    tongue back especially kind tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
