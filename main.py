b19b4d9e68cbd91eeff1d6a50a43317a3530f157
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:21:56 +0000

    future write top female sharp

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
