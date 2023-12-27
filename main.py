7c376ada50c2d2684d782e126f56c630509f59eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:22:39 +0000

    peace niggaacademy injury attempt balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
