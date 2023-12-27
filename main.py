9cd0da73083c5cc97da4709528364becc30050ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:59:07 +0000

    play translate wet excuse stop

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
