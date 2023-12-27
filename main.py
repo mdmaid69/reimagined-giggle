a780aa6ee9671f673eecdc2e69a0535815329273
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:16:50 +0000

    beginning scare convinced choice then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
