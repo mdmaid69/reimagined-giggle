f6aaebdce1d5e296e420af061fcd1a201e03780d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:21:35 +0000

    prepare argue takeaway art warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
