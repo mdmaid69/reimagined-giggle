faded404c970b40582c31e8799caba07e5740985
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:26:49 +0000

    hit thumb confused amount polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
