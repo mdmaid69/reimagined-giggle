86fa74f1566ab90d3e78515d98d1330e3d250e63
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:33:20 +0000

    school star collect serve phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
