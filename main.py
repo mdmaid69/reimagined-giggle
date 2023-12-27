619f6dc7982a5a37f9c8c372f431758e4b5ea2f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:59:38 +0000

    smart lead total curious gradually

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import sys
+print(sys.version)
