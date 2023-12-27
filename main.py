48994c0c274b5dffe68268eed3118b0c4f32a9b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:33:24 +0000

    action if whistle cause joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        print(sys.version)
