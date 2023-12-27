bc5d224c2e08cc50260af29002e05b97d9ee24cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:29:49 +0000

    bet communicate pass fun soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
