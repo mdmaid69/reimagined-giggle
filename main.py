5e42a97ef59199fdb62e416e8ed74e348bcca978
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:15:34 +0000

    price believe play advantage shock

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def is_odd(n):
+        return n % 2 != 0
