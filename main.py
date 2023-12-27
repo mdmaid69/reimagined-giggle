b025e6782c0f413fd5727c218ab928fd7a72b1ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:04:23 +0000

    name section right left getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
