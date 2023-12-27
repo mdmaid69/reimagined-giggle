475686a42227258791728d5d86dd0745aa575d6a
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:49:18 +0000

    drop off space dare tool

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
