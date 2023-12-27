0ee6a28dc84dbd8e6b7eb4d78addad5692e708b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:11:48 +0000

    either none possession loose behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
