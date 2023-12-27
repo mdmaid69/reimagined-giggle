111a858c15526d929969a777287507d9de5c7929
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:41:29 +0000

    mean attractive dream objection wear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_work(force, distance):
+        return force * distance
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
