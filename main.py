470954629a592a9607ab4ff5d5fcdab72a220868
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:46:29 +0000

    compete generally accidental yes feed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
