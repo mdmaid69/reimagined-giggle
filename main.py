446e4b68042b24d5cd55db3852d9e9697a23e907
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:33:16 +0000

    contact slow division whenever giveback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_as_bool(array):
+        return bool(array)
