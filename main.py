185782c61b0fe63fb5d7551039e6dd51e1d20b0e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:45:24 +0000

    mix worst charge about design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
