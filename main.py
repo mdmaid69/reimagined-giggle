96aab22ff6bcf54eb233dd91f8a06c636a26f21a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:21:34 +0000

    disgusting off ready accident nature

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
