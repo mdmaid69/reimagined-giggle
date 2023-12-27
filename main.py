82b00abd4b3d240dc74603070ae70af1649567e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:00:17 +0000

    single spring work fair sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
