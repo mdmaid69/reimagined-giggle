c4aeff1c14c3484396255367c48d6d023f5f24b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:05:40 +0000

    bin reliable suckme brown takeaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_palindrome(s):
+        return s == s[::-1]
