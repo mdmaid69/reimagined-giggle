11a87dc49fb5fbc85a9bc8b257b08a12c6032fb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:29:36 +0000

    basically stupid foreign serve any

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import random
-def roll_die():
-        return random.randint(1, 6)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
