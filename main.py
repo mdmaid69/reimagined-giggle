bbced76f2c2a340a5d84cecc90656789f870dc9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:34:03 +0000

    neck complete turnup someone across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def is_palindrome(s):
+        return s == s[::-1]
