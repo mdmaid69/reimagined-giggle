bc53ee5a01f79083731ae10bd7d05735ca8d7f30
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:34:13 +0000

    important travel government relative talkinto

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import re
+print(re.match("h.*o", "hello world"))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
