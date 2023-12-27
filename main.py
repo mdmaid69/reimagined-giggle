72ee87745a9b01f914fde80979269787f73c0506
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:07:02 +0000

    firm negative behave who rather

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
