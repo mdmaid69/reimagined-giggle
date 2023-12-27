0a4c2b98d9b90bbd5b2e775c4e8ad497b0ba091f
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:49:55 +0000

    turndown true passage unable best

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def square_number(x):
+        return x**2
