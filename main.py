be72f7269ae0d680595384de6958343e4a3b80d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:54:05 +0000

    rub disagree drop ready forward

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_density(mass, volume):
+        return mass / volume
