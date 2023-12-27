c128b2f6d754674f6b11c5811ca2c2913b23bdc4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:20:43 +0000

    damp callout carry ill pass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
