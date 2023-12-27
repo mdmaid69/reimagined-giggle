08a2c1c3065ed45945c30106b17ee923d993a4b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:21:27 +0000

    arrive surprise discover angry trip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+print([x**2 for x in range(10)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
