756bd51aa4f2a118d040bef46bf232064a51af84
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:38:04 +0000

    performance behind before close mark

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_acceleration(speed, time):
-        return speed / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
