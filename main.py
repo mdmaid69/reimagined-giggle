d79be84774d9416496ef7176fbca786b1b902b7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:13:04 +0000

    race tape last back whether

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def count_characters(sentence):
-        return len(sentence)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_max(lst):
+        return max(lst)
