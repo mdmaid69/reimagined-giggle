0851f48ed29c2d86b57e74b1f569aae99aa59e3e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:14:03 +0000

    design fair border spin build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
