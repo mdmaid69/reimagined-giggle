dacf9e8bad8bf54e48e6eb70065ca2adedcb8359
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:21:02 +0000

    above bush shade television thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_repr(array):
+        return repr(array)
