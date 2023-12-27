a56bebae22276f02496df3911fa69c81658b46de
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:28:07 +0000

    fail offend genuine separate pure

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
