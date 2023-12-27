bb8d6f631136647b72f766418c26fa2685c85182
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:46:23 +0000

    pretty murder history meet probably

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
