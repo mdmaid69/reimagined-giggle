919682f5c9ae562bfa57408ef25c4ed7b950b435
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:43:35 +0000

    earn scream experience search Dd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
