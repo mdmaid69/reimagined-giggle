c03e082927ba48fe9ceb5d0c4f9114cab9d70547
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:11:35 +0000

    welcome pour male push news

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
