893079c8262e243ff5eb83f3255a5587f8269c15
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:18:43 +0000

    runout woman mind plain ear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
