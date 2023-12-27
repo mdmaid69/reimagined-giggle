cc4de93606b7a2542e9b065063c8d7088da94cd3
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:18:04 +0000

    path culture risk thinkof but

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
