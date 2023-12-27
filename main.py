33327d955255b06ca8802ef26d413c83e0054af2
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:04:01 +0000

    once near meaning plenty space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
