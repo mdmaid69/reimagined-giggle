8da5001d9f3627bb0822473c2313aae4989aa6c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:37:02 +0000

    confused rank phone begin blue

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
