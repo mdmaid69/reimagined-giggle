be149c9d3a3b76bdac8fd446cde65c5d3803cd79
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:13:56 +0000

    lunch condition that succeed help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+x = 10
+y = 20
+print("Sum:", x + y)
