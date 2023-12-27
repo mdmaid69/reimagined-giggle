37cfb120e2237fbe7976057b22773846f14d34ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:59:29 +0000

    rest near live whether bridge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def find_min(lst):
-        return min(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
