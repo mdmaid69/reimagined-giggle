5c18a926ee8828a810c9933d6193a46a0d664c08
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:38:44 +0000

    572enter seriously friend still price

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
