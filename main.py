4748ce9590fdc86b6b4b1727bf00f77235c072fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:56:23 +0000

    cheat transport mail bread comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import sys
+print(sys.version)
