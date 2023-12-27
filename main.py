af993a1434254f824a2d28ccf423aadb708527e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:35:17 +0000

    relationship eat accident responsible lead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+name = "Python"
+print("Hello,", name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
