ab4a564c12daf6d875ed992b17214479cf2734b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:03:15 +0000

    attempt cheque goup tend deliver

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
