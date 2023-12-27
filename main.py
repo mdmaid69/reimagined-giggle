b38cc5794572d323844485488ca5fc703a6f3beb
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:16:55 +0000

    law supply sudden if centre

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
