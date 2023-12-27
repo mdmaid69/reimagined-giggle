5bdc7eaedca61c1667a6e7a9c4c88baa1f153b3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:17:20 +0000

    ought nose foreign refer paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
