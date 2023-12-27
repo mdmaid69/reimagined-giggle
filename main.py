36743bc72dc4361e88edba81ebbea0b7fffa0b47
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:09:21 +0000

    reason perform advantage when greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def reverse_list(lst):
-        return lst[::-1]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def square_number(x):
+        return x**2
