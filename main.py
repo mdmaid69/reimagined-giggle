cbbc88d706608595757b20b4af2355c06a7a93e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:13:04 +0000

    bury anywayanyhow formal whether giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
