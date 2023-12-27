a38108f3a0ccf1d705533b0b7449e522611a4618
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:59:44 +0000

    sense comeback outside lucky pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
