2517f60eb1a083ad640b36fe3f4ee51feaa7cbd6
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:28:40 +0000

    invite no minimum us standard

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def count_words(sentence):
+        return len(sentence.split())
