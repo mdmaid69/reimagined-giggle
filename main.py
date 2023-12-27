5e444b95f6633961817332c2fbdef4dca0b7063d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:16:11 +0000

    care main takeaway improvement plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
