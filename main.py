53c3ff6bd6322e03d7a021e20a90e1cbb258ebbb
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:37:33 +0000

    burst yours disgusting last either

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
