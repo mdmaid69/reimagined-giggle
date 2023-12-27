321e8837b08d3486d93f3d012f15515e2add965e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:42:57 +0000

    common inquire anybody crash difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
