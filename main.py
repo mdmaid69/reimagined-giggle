ad36a69e85760bb2605b95fb665e6ae2f3947b87
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:00:06 +0000

    market female bill inform before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
