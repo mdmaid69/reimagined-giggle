c7fb9069b4b17f22f4730392beac3cd04445766a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:10:56 +0000

    illness personal movement doctor foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
 import random
-def roll_die():
-        return random.randint(1, 6)
+print(random.randint(0, 100))
