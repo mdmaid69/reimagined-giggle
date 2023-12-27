0ffcfa86363078f5c083b948e45a0f2ac2928874
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:38:24 +0000

    nurse letter desperate early miss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_density(mass, volume):
-        return mass / volume
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
