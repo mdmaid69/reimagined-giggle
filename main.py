43d8a023736e850df56da28d7c88ea601ebd1e16
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:19:41 +0000

    memory discuss fond destroy except

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def cube_number(x):
-        return x**3
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def convert_to_binary(n):
+        return bin(n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
