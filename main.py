d809e41347f37bbb9865d3044b1fcfec39c41f52
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:08:58 +0000

    eat offence wind protest ever

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import re
+print(re.match("h.*o", "hello world"))
+import os
+print(os.getcwd())
