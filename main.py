35b47d91440b2469a4f5a3fc04125319ce714fd3
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:16:22 +0000

    anything right before at wait

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
