ddde58e10682aa8e1edb83f56a1b7bfa05bdc8da
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:15:50 +0000

    win environment cinema side circle

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_current_time():
-        return time.time()
+print(sum(range(10)))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
