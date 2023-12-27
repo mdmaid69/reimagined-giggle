291f1981873b820937f721634c5d466dc0d918f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:38:44 +0000

    part danger typical warn to

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-  def square_number(x):
-        return x**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
