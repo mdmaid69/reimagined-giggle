c18549433e1abbcf86173ad7e6cc5432d7784217
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:52:21 +0000

    project damage soil pretty apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
