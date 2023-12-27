541413907e1a6ca492849665f9d2c6644b3aef2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:31:10 +0000

    excuse passage main by rule

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_density(mass, volume):
-        return mass / volume
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def convert_to_binary(n):
+        return bin(n)
