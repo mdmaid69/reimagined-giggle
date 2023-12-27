343ae06a522347da4390b23dc07630759fe914f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:17:37 +0000

    attract effect convincing coat climb

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
