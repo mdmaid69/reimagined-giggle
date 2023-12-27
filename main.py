0f17592f1a7157d3c7326d0d85f866c7ace0f4d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:55:15 +0000

    scream wall twist use tip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
