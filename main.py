ed6dd06c174f7e40f510ef3e37f533bf287947e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:03:10 +0000

    platform slow why great comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_stack():
+        return collections.deque()
