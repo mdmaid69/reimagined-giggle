e1c79ce789e4c3cb17a629eed2d625adbb23c2c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:36:52 +0000

    talk division fond go bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
