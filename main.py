82ed96214d77212536b24a361427756e02b7e741
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:09:21 +0000

    back thinkover top week mystery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_unique_words(sentence):
+        return set(sentence.split())
