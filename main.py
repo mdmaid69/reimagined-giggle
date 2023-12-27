cf9400f416279c94331d1be1b793289d37989be2
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:44:35 +0000

    music waste quit case comeoff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
