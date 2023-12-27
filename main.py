11dda3ef6cacd1975b96da19e521affe11916e65
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:42:17 +0000

    shock join talkoutof record OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
