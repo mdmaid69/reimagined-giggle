72eb7ff8561d87a6cb597e49addf266ad897774e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:52:55 +0000

    border gift matter give estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
