f15cd5c61166d346e957784af7ce9eb8b0f1b539
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:48:10 +0000

    instruction finger increase drive friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
