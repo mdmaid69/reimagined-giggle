4723e2411c4fd09260d67b0d3126e1ea457990e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:08:40 +0000

    event undo whichever disturbing bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
