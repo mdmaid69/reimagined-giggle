5c7c108b83d9ec90d5bd719012b7bcd26447c1e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:57:27 +0000

    relation expression everybody spray say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
