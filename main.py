5906ab0170ffb504480b92b67fcee14ddae89647
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:25:28 +0000

    dream content cheque recent print

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+def print_python_version():
+        print(sys.version)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
