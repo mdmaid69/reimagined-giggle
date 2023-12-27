c2658f63ba2300a6d168f85c1a6989cb98ab2220
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:32:05 +0000

    emotion just peaceful lookup activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
