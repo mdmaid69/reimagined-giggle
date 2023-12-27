6543ba6483355edfc6c4cd76dc22a53821430eee
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:27:46 +0000

    estimate extra translate step nothing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def square_number(x):
-        return x**2
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
