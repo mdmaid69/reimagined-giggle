91d7b370f76f8442658d545362f306bd2fe93cd3
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:26:50 +0000

    before Vv pin passage brief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_repr(array):
+        return repr(array)
