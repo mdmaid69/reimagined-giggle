35e9d99a4842d412e51b0432b7716bf4059cc506
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:35:19 +0000

    instead before risk weekend yourself

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_odd(n):
+        return n % 2 != 0
