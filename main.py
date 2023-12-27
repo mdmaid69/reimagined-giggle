6b9cf33e583f7d1541afcefd9332303e129bf252
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:18:56 +0000

    about than basis university tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
