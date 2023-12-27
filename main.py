f042c5c132d36c321a0188f310d7b859d204d268
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:14:54 +0000

    see peaceful lead order ticket

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
