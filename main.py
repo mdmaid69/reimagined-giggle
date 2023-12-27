6119c131d1e78acd8db8671c583e664b4ad48842
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:00:23 +0000

    answer OKorokay everywhere confused difficulty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
