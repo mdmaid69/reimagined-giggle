fa9f78806a8c4112841fd2e330f4ae78aeac0228
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:55:41 +0000

    pretend lean indeed holdon reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
