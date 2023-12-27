9c90017814e59f82580463751dba8aeb97c6c761
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:13:54 +0000

    lay tap pronunciation curious supply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import random
-print(random.randint(0, 100))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
