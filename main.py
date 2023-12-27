7c55041801f63b849d0dd7b736164566678bf623
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:46:47 +0000

    gap past serve judge unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import sys
-print(sys.version)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
