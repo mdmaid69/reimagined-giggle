dc911cdba8db9a2e929e39e6a935dc9fb9894c2a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:31:27 +0000

    but else distance flavour nothing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
