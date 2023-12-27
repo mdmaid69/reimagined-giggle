e3a81c217bfabe5b9c494e412c39709047c39805
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:26:19 +0000

    whichever pay argument keepout object

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-print("Hello, world!")
+def find_min(numbers):
+        return min(numbers)
+name = "Python"
+print("Hello,", name)
