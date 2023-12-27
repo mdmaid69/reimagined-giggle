7626e32ccd77d2b8843cb34bd9b1806e4e0736b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:54:44 +0000

    stare game idea basically standard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
