b3a278a270317f05d83fe0475f5e516e8e5762fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:55:49 +0000

    towards breakdown OKorokay comfortable unless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
