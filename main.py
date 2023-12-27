b98ff8a88614ee81e07f657b7164dd6200cb9a8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:33:18 +0000

    makeinto emphasis nurse contribute band

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
