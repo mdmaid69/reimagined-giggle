417d0df4f15ab3f2aeec74c7fe84b74b98d1ab19
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:53:06 +0000

    change suit might fast mine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
