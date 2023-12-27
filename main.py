0a99dd74be70231d797b17dbcaa370800f1418dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:12:58 +0000

    threaten shade thanks attach difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import logging
-def log_message(message):
-        logging.info(message)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
