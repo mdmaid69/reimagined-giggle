f12624e01998123fc1f2621156847e8dfa4a5c6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:38:31 +0000

    pour please rush very reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
