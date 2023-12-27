b4c77129aac64942a673eac047c05b0b0b2c7f7c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:14:51 +0000

    money mention hurt know rid

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def subtract_numbers(x, y):
+        return x - y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
