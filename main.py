b9ad578ac19012e83158b34b164e058df14a9f86
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:16:18 +0000

    print tone information repair quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
