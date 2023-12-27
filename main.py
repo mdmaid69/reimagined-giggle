6620af2bc5934a0b1e146e135e0872ff4bfa13ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:00:53 +0000

    reach invite sea clear obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
