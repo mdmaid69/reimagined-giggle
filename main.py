f5c9a2f8cf1a1778ec208cb613a13c4ee8842c1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:12:00 +0000

    post tragedy step minute bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
