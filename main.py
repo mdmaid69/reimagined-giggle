4f06b662a8ed35cab995840e427357444ec3b2de
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:22:49 +0000

    recently since centre winter sudden

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def read_csv_file(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
