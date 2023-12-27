39a6b49b05f7ffa96cc8d3870a20dcd7e0250e1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:10:10 +0000

    beat past tax much might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_float(array):
+        return float(array[0])
