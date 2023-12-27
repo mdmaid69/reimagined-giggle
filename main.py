d8c98c8b5a8586aa0df5212ed63395a79a2c1cca
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:45:58 +0000

    principle whistle near laugh criticize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_min(numbers):
-        return min(numbers)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def square_number(x):
+        return x**2
