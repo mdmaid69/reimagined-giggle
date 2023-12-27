3f8ced6a5f7ad3ae3121ac2c0bd7ca7a559cba57
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:42:58 +0000

    habit store trick join circle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def is_even(n):
-        return n % 2 == 0
+import sys
+def exit_program():
+        sys.exit()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
