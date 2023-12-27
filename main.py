2f02b5e1fc2ef6a9fc2a0476fe77b8aa822467e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:53:52 +0000

    building regional pollution example street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
