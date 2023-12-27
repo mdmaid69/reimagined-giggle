0885adba0c2d56c30b7287526c7002dc17a43ccd
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:41:58 +0000

    here backwards squash inside sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+name = "Python"
+print("Hello,", name)
