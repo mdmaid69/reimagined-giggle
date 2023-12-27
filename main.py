6c5dc998d52f41074a634567668297f484b43c88
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:16:38 +0000

    recovery clean surprise they prepare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
