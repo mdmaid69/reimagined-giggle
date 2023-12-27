c9a04a5442ed2b5b82597bcaaa29dbfc7c3e506c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:44:55 +0000

    month conduct myself better still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
