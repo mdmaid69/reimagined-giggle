86a9fea8b08b495c3cdd31355fe0a6058930d1b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:19:08 +0000

    can interest tie rough dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
