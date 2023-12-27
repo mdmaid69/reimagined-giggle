1d130f1760491317ce214d80188445ef45b2c81f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:52:38 +0000

    opposite pin sharp new piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
