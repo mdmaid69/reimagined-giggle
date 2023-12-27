4f64c642e20fa98ef85cbf719edc4c9d209b0751
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:50:26 +0000

    luck hope spray usually taste

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
