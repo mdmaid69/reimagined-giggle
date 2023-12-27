e3d34d81084aac883e9ce2abad9fba798ddddb17
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:31:10 +0000

    crash stay dark than team

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
