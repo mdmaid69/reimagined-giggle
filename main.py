bbe076cce20a769908ea427d89dd21bb0140acff
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:18:46 +0000

    die high rich punishment obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
