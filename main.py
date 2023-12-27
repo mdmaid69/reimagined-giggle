89ff895535c0a598a5ca678c4c934775a928a41c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:19:33 +0000

    the hers statement people improve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def reverse_list(lst):
+        return lst[::-1]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
