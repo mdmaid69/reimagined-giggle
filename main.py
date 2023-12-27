3edc7e491a4b3e96f09cd402151ace191bb4d699
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:08:43 +0000

    classroom culture basically throw long

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
