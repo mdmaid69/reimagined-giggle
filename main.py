f2e1e4f984726b5882ad8d9ac0561c760f79e551
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:53:22 +0000

    lend start roll well mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
