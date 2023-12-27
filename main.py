97642ad3a42ce4fd3c3018f590bdf28e1bdced21
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:10:03 +0000

    quick intend Uu computer winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
