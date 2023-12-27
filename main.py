7b3800b2b079526ad2fd9dc8e45cfc1b0ce5c8e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:06:40 +0000

    murder what yet deep none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
