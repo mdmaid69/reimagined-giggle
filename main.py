3d14bd1d58a83001e1550c32b2fa8b17c0c5317f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:23:20 +0000

    estimate getout runout operation truth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
