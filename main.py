ebe99df25e81ddfb520087ec5fc0542394dc1117
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:33:43 +0000

    idea journey behind count black

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
