8799c1dfacb67685abd43cac95766455df1c84e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:32:40 +0000

    lookback(on) shelter influence bringback accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
