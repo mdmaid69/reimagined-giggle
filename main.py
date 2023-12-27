ceb27b00631a28704a7a2590c36876670687e898
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:25:39 +0000

    colour strict until unkind geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import time
+def get_time_since_epoch():
+        return time.time()
