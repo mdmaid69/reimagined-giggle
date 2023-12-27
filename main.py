c88181c44cab170e04c02268a8eefe0558f83574
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:45:17 +0000

    memory note suggestion male sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
