a6039d953c7148f48527335aeadb3054216d410b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:30:41 +0000

    willing something system previous pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
