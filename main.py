766658da06aa025f47438e07c9f9e9d570b70f4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:34:08 +0000

    Ss listenfor touch equal birth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
