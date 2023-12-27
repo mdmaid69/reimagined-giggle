81708b9efcc43f3105cadc0eb2d09542b4b256c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:24:49 +0000

    limit common bus consult means

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
