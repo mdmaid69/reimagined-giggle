2ca10144db77f0e562e1a09a126e4569c3859ae0
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:54:53 +0000

    pressure tidy activity satisfied so

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
