300c65b808abf70112a8a7e3f96a1172c4d48cc2
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:46:01 +0000

    game occasional spot mother professional

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
+def convert_to_binary(n):
+        return bin(n)
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
