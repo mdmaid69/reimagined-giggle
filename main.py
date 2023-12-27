8728fec118c13d72f13a8c582322caa04fb94b10
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:29:13 +0000

    swing flat story takeover criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
