950236b3eeec1402cb5d94092697a0912102594d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:09:34 +0000

    content vote carry blue colour

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
