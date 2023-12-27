f51d26b04a98bc90c7f969094aae69c18581be3d
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:17:40 +0000

    other mystery during off action

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
