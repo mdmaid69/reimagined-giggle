1e2109eebdda8f1932b2888a9ca879989988ad23
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:47:33 +0000

    cut pronounce admit watch winter

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
