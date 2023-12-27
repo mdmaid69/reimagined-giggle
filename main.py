3ad02e089666f33e18f071403e0e63e40fcddcbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:12:39 +0000

    remove talk copy long relax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
