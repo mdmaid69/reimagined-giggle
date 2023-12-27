ee6c0135c353f117d11b3263bcbcd1ff78958bcc
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:02:07 +0000

    hold matter exam gather burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
