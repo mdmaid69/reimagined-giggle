96a37d536e9e473700b81edb2be1a47462679535
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:10:15 +0000

    cry story term interest risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
