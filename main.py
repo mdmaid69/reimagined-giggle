edec7b0cf82a68f14bebb1f1e5fb2e3cb33d92a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:29:11 +0000

    taste raise area spray play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def reverse_list(lst):
+        return lst[::-1]
