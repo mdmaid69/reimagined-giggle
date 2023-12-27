e0f50d925db12542fc963e19e883c23ac44ea40c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:21:47 +0000

    fix plastic shame priority almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
