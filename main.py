08427fd29475c05ef85e8d9be3e124cde9a64223
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:03:39 +0000

    relationship Qq actually bath detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
