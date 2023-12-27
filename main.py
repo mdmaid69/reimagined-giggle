ac829c397c77005822b5e4bd2daec77b34b15215
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:04:29 +0000

    better touch himself family exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
