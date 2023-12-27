1c027d7ab6fbadbbf6db9fcfbfc72dbfb7ea6f9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:51:14 +0000

    general fast picture burst result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-print("Hello, world!")
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
