ad04fc35e895a97c313a56934da25ad46d18583e
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:02:09 +0000

    bang operation favour visit getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_os_info():
+        return platform.uname()
