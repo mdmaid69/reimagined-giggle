970ffcf1a76a9d05e1d70c6151c1bd3b52b76b5c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:11:07 +0000

    race ahead knowledge criticize path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def is_odd(n):
+        return n % 2 != 0
