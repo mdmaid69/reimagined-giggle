812d2d76082c9297c2b3fac35875c5cef036793d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:07:29 +0000

    bury spring frequent best possible

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
