9067be81555cb06e9079260f969921c2fcc9cffe
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:39:44 +0000

    soak imagination circumstance dream eye

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_working_directory():
+        return os.getcwd()
