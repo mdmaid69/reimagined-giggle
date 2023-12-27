abf0b54e2023013c988afa5e23535a280a92615a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:45:55 +0000

    adjective organize free sick tax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_elements(lst):
+        return len(lst)
