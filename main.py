e7767a6c81a7f016d26dd2f8c6d9aa7382bb9a32
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:00:21 +0000

    own otherwise love kind habit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def multiply_numbers(x, y):
+        return x * y
+def convert_to_octal(n):
+        return oct(n)
