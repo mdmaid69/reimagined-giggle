222d8fa6e616d4d8404940c36982f0347c62de10
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:24:57 +0000

    small smart colour cover plenty

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
