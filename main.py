bbdf60dfac0da613c83a1676dda7d35db0e10317
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:19:06 +0000

    poor bet song form tough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
