7e40c6b87786f825dc68319649ee19b7341f5948
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:08:18 +0000

    dull rush group interruption call

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import sys
+def print_python_version():
+        return sys.version
