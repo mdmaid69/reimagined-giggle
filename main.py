def7971c4b8a0c4da59fb42f7b9d7a67c437db61
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:53:28 +0000

    cruel alone idea catch hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import getpass
+def get_username():
+        return getpass.getuser()
