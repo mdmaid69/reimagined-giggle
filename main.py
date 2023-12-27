bebdacbd4c4caef53ac2c1f7a1066d6ace472f81
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:40:51 +0000

    luck read thinkthrough attract content

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+for i in range(10): print(i)
