284b023169bf7db417c62a6f9c9fb0e1544a9d14
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:48:59 +0000

    sore tragedy describe guess remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
