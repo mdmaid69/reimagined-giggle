44a46cca0444b2f5f8fad1e85a44ab850cd7c85e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:41:57 +0000

    middle lead warning whoever shut

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
