1e50efb06dc677d6e69022b95e74a120a613e4f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:33:52 +0000

    callat trip lot coat phone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def convert_to_hex(n):
+        return hex(n)
