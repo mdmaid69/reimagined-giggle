e6988fdc3bfadef2414a94273a259b5791feef70
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:06:38 +0000

    scared intend unkind combine giveaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+name = "Python"
+print("Hello,", name)
