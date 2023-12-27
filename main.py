902a66cce99ad1720ea2f4115cf95b9fd8cfa925
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:01:52 +0000

    keepfrom butter object exist character

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
