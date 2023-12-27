9b06f9bce1d771aedc3b2a3fcad376b47599da9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:28:54 +0000

    reject talk pretty murder gather

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_rectangle(l, w):
-        return l * w
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+print(sum(range(10)))
