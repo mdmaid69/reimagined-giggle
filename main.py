f54d2d83cc9d60015ffe8e52950333a5f9d98fbc
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:07:16 +0000

    now maybe butter circumstance occasional

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
