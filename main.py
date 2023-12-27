240c8415505fbfbe6646de194183a66b66c777ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:10:48 +0000

    thinkof movement only load back

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def find_max(lst):
+        return max(lst)
