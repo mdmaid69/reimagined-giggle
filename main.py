7ef41cbc86481a2a961852086b9e69aafd0c2049
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:22:00 +0000

    minimum Ss long soft history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
