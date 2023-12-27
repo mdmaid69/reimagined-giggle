865750ecea85f26759fc35ea4533e43359978114
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:43:07 +0000

    influence notice second break fit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import sys
-print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
