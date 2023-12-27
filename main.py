617a9d38b10d7cb9394dc8c151ee0cbaa1ed2075
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:14:44 +0000

    speed shut absolutely buy reasonable

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
