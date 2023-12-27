ef2bf81e26bce00d69bee782eca2b80b68a4ec80
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:00:42 +0000

    listen past happy satisfactory makeinto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import sys
-def exit_program():
-        sys.exit()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
