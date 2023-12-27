0c636893082fc4247eab5aeb945a778fac380944
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:16:16 +0000

    disappear pen attempt quarter repair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
