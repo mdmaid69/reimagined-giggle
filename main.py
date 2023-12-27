38709bb8240667daaa11fbf6c9786cb0c83ceca9
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:22:46 +0000

    sleep for adjective about invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
