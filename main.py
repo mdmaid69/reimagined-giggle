eedb474cd220c157f6fe6a59c9aa8cfa9c4e3b78
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:41:40 +0000

    substance government deep fresh actual

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
