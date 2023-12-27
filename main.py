d22d94a862ae489a376fa9768fbeb41d25ee8ff0
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:00:01 +0000

    weekend hurry shrink Uu equipment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
