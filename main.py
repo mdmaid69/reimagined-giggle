168186f1be3b340692a5f855924dbb390982fb94
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:57:17 +0000

    regular hand recovery weigh convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
