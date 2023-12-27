20830a920f8b9abc39d65659ca937dd58f72ecc4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:09:44 +0000

    development shadow tip full precise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
