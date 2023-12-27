e9f6cd56fdccd20f99eb37b35ac6c90f3cdb3982
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:01:59 +0000

    interesting improve male attempt attract

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def add_numbers(x, y):
+        return x + y
