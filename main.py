5ba7f8a011864f915c6932d1ca096bb0efeaa780
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:54:35 +0000

    in away record pile him

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+print(math.pi)
+def count_words(sentence):
+        return len(sentence.split())
