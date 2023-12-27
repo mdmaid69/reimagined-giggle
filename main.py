320ca08524d404d2383c31a810058b03f20d81d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:25:18 +0000

    question 569enjoy mix move clothes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def clear_array(array):
+        array *= 0
