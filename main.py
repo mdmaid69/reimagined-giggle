4d5a986cc956bf8210d9a7da9143f5f9a5608cf4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:38:35 +0000

    off project pleasure himself reduction

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
