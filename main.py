9a1df101783f2570572837958c534f809d6933ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:18:28 +0000

    meet dance chase will unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
