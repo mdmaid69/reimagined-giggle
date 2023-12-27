31c03cc5882d862dc92261ae991596868cad4dac
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:37:35 +0000

    thinkof occur learn exploration home

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
