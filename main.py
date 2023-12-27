ba65303befcb94c7b613397534f8f8fcae5cfd98
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:10:07 +0000

    close even figure unpleasant rare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
