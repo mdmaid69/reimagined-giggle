48fea02276f5df347c084b80cdc22e1788ac5cb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:56:12 +0000

    bread connection comedown ignore forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
