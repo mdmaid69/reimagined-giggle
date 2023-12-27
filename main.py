2e4669a4abb346807cbdac5f64437c1969055e1c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:01:19 +0000

    brother run the it edge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+print(datetime.datetime.now())
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
