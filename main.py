593eb8592c378c3b1e5a99a15f2f362f6bd05988
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:21:26 +0000

    early murder secret imagine suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
