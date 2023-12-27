7f6984910ae72d275292f8ae36fc55762a9091e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:44:05 +0000

    various decide earn owe clearly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def reverse_list(lst):
-        return lst[::-1]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
