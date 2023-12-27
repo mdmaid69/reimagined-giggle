c63206f6d0bd05748d6dc19f7f85c801da133078
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:05:01 +0000

    obvious sight public treatment explain

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
