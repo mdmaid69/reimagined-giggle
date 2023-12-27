8d87d213502a41086956a850de59dd307840370f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:52:07 +0000

    underneath unusual surprise admit still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
