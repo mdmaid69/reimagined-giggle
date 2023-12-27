0319fcaeb3a8e48127e4fb9579154d969884485c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:49:04 +0000

    love talk scrape power pay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
