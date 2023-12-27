8e59b8b2ee8f577f0719381d2de90dd8cca48bf1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:42:37 +0000

    fall nothing agree shallow thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
