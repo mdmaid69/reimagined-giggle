0fbfedd7ce5e0ee7cb4f43d4d6e00c7cd692dbf9
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:53:53 +0000

    fire goaway shut public work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def convert_to_octal(n):
-        return oct(n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
