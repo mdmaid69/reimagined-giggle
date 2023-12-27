df848c4fb132f1dcda9bb66fc803337b4e25d132
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:52:14 +0000

    by comeup centre criticism vote

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print([x**2 for x in range(10)])
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
