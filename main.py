848b3c5ee382939424d514e197a595ab7f468981
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:33:38 +0000

    or suppose petrol rely artificial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
