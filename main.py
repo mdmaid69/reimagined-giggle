6454989692dc860c4745c9e661150ab930d23a9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:46:22 +0000

    march occasional fat definite season

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
