49c3fe50902db5efc17bddb5a380394a11091005
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:03:23 +0000

    natural frequent possible rare left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
