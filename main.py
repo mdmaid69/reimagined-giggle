e1416518969d4eb8e85d350996a041e332a20ccd
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:11:21 +0000

    surprise smooth cross emotion run

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def multiply_numbers(x, y):
-        return x * y
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
