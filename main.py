885460ea2770b0f0d3c09bd010c0590a4e968cc1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:31:48 +0000

    direct explanation hardly crowded general

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
