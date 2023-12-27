578df4e42de8b4f08213ba4f5c4a7b9d756b66cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:37:55 +0000

    watch pinch adjective prevent push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
