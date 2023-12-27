cd302dbb548a30658002e74854ac18b503370bfd
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:07:41 +0000

    joke bringforward up improvement dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
