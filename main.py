712ca3734e8c6d49dbe134cbbb29495de746c140
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:48:48 +0000

    once mood get sign bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
