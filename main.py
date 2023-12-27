ca8e530fc8ed11373f3c5e98f3b76500d6973bd5
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:29:53 +0000

    rush steep operation public Dd

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
