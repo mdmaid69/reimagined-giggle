e2f7d999827b236635e6187755e8225e2970b3f2
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:51:24 +0000

    crime bang moon tomorrow blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
