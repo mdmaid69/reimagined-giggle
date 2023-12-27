cb5217ab808af2b4eb1430c616f9b69cd68356e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:54:38 +0000

    disturbing takeaway backwards themselves basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
