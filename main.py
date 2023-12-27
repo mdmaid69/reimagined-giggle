2fc0b7ad610f214224390029a80927cb535ef819
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:49:48 +0000

    file shadow wave understand agreement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_int(array):
+        return int(array[0])
