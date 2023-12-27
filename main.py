414db72dcedee4c4457f8689dfd5534c13653515
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:42 +0000

    whether scream reasonable quarter project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
