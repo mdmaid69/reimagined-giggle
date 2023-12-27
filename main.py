092e51404d0bef8d043a5e5187983f3efe472edf
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:27:10 +0000

    interruption dress object defend begin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
