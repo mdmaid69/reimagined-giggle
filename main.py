b6a3c452ac09ea7b96bef30776ca4e3b836ff168
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:51:47 +0000

    fasten Ss object force fail

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-print(sum(range(10)))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import platform
+def get_os_info():
+        return platform.uname()
