ab4000d38f5fee243067cbd7c2da8c7910fc0d3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:27:41 +0000

    wind empty because uponon design

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
