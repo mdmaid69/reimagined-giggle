0bdefc0d74ed8f666800eb7e7cb8e62e8785083a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:40:11 +0000

    thought advise official accident pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
