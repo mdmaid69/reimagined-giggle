3700db53bb5e305ba20e3d5bd39e49b6a1ca9165
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:16:45 +0000

    warm Mm method variety charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import array
-def get_array_index(array, item):
-        return array.index(item)
