02febd529ce592cfe4ffcced4e5d4f617b2c5660
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:30:20 +0000

    see discussion finger flow she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
