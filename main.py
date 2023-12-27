2417f61b2c903aaadadb2f0fceff5fa0852edffa
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:30:57 +0000

    without swallow giveup their ring

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_pressure(force, area):
+        return force / area
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
