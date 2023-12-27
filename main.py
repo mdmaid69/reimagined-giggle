968eee2c13a30a8c1846661fb1450b6a3f161269
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:27:07 +0000

    broad pencil before sell callout

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def remove_from_array(array, item):
+        array.remove(item)
