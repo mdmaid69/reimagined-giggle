09b29e96169b98b4fc3bc26b3ffa77f655384931
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:00:50 +0000

    side broad should number quick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def is_even(n):
+        return n % 2 == 0
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
