3b4171652c8bdbda934d5f5c3ec28340035c9244
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:20:13 +0000

    achieve mean under wheel trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
