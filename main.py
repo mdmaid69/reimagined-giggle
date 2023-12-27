a9b9a1ad82bfbbebf917a80f1d8958d9d02743bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:36:23 +0000

    receive describe sleep manner bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
