b429090a71778863c81fa82cac511d8f5217d44f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:33:14 +0000

    over business inside evening house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
