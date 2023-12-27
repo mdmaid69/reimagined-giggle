7a3c02854586ff509b61075abe323ed3100908e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:58:26 +0000

    564encourage moral bill lonely space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
