18ceed0b2b6ec43e5aec8b94d3fd2dbfe46b8add
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:43:03 +0000

    anything holiday apply clue discovery

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
