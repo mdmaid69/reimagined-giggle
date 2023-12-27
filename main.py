35c9efea3a37859585b115b7302c506dafc98a3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:07:55 +0000

    base this operation exact hard

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def subtract_numbers(x, y):
+        return x - y
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
