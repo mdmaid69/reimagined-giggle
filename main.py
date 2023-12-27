ddabf28cebe2137354af683ce370dfd5fea714cc
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:04:33 +0000

    meal instrument hurt drink live

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
