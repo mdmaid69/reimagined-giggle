5919ff381c6be980c197a1dc99166ee9630217cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:38:13 +0000

    empty opposite who warm Kk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_item(array, i):
-        return array[i]
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import datetime
+def get_today_date():
+        return datetime.date.today()
