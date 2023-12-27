de271ef7afc1c9c05df585a9da5ab5fdbfd8a739
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:34:22 +0000

    feeling scream honest mixture question

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
