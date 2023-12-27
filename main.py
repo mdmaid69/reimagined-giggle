b226bb6419aceb17163fbd2176f59e8f9fa5fcca
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:55:58 +0000

    wet round turn yet many

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
