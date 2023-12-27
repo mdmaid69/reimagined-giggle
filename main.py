4deeb5af0a48570f66f74369537aa8d9ecc7ae1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:19:35 +0000

    mess its tax term sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_base_name(path):
+        return os.path.basename(path)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
