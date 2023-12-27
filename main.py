33d40b60cffd0edfe9683a0bac04aec09a96cb2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:51:05 +0000

    suffer survive command union anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
