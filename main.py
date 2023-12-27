14f68c44ccc449dccb261bc9add2d1e79a778c92
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:11:42 +0000

    practical movement average Ff arm

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def find_unique_words(sentence):
+        return set(sentence.split())
