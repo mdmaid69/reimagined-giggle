b62ada45cf333096360ad7f9e9a18170ffdbf198
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:07:45 +0000

    price upwards easy same proof

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def square_number(x):
+        return x**2
