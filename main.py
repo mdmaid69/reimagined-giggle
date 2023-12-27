f8526b204bf4aea047fc80aaf24a8f47f60ea009
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:33:57 +0000

    comeup discovery rub neither favourite

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import sys
+def print_python_version():
+        print(sys.version)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
