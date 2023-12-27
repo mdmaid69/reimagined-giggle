c56cbb2d978096f997dbd790fabbdc3992d7a4b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:39:21 +0000

    rid admit prefer lock read

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def reverse_list(lst):
+        return lst[::-1]
