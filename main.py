62921a5f17823efa880cfbe2bb70afd5be41eac0
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:52:40 +0000

    pollution repeat object collection steep

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def calculate_area_rectangle(l, w):
+        return l * w
