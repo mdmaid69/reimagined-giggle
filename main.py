d469d5ca1dcbbb612884e671f92eb2fbaa539898
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:25:40 +0000

    past phone healthy church glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
