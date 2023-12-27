16b78e65a0d09c515db4dcd5e831e7350304e077
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:54:13 +0000

    statement gift part but print

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+print("Hello, world!")
