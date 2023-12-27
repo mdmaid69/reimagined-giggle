6ac944127d7f2214708b6e6f5622b64559575636
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:00:45 +0000

    at say onto scale greeting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
