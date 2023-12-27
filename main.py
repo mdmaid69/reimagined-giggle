148c21dfee68037e49b6518faec4a29ec6964f65
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:13:35 +0000

    work size scare pain own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
