bc406e6de2a0b24e65c0ea8ac1ce5139caf3335c
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:14:35 +0000

    period OKorokay well write combine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-for i in range(5):
-        print(i)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def is_palindrome(s):
+        return s == s[::-1]
