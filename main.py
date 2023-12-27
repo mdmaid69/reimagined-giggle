15259cf838d27c6f5d6d8acaa10262d0c1707e1c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:28:02 +0000

    talk safety block news afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def sort_list(lst):
+        return sorted(lst)
