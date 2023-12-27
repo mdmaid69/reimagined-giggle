88f3be263640df89bdf9dbab83e2620509c66041
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:44:57 +0000

    charge dark to by serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
