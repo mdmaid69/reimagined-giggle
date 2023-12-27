dfd749aef7d8fb296a1abe10def32b65fad09038
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:13:59 +0000

    difference Dd bear trousers provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
