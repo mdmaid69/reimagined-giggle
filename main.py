0cc62b76e45b3852329e04e2f2324c88fddb731e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:52:26 +0000

    lose frighten party but mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
