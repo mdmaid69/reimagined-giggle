e86286a3ee862b110f3b47f868af82b27f314cad
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:16:47 +0000

    old get desperate day stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
