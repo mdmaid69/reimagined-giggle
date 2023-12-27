5dd1f53c6736ce057d7fd1c85b7d21bf59ec98ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:44:13 +0000

    Ss actually character suck foot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
