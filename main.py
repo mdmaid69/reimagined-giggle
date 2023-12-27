35309b26e172e92674364caa7f60d856f09da65b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:12:51 +0000

    convincing slice ourselves general square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
