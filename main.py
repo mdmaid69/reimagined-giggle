3d240b782ea960c6080246dcf49f90e1f133741a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:26:58 +0000

    crash worried power tip base

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def get_array_as_set(array):
+        return set(array)
