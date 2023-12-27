5458dc36e13cd28313307ec312909a8899f06656
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:07:30 +0000

    towards method look past one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
