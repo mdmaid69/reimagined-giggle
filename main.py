7a537fb1c184e166495acb32c7e5dc4486ced0ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:18:38 +0000

    round myself fortunate who rain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_speed(distance, time):
+        return distance / time
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
