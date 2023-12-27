9b380cc5e44b82ce6b30d481a42ab7dc246ad84c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:04:41 +0000

    sell yesterday dig past night

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
