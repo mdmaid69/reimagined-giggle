828f7e85d94c78ffd4e5cb9705a66701763b44f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:28:05 +0000

    Vv rub mouth naturally Nn

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
