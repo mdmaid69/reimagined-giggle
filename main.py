8b023bc0f9703d585f8beceea2956530385d0981
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:30:56 +0000

    Bb cheque wild learn ashamed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_volume(length, width, height):
-        return length * width * height
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_typecode(array):
+        return array.typecode
