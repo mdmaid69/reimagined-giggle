8940559a6ad01f0ca43782e7dc409c4a4fac687c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:26:42 +0000

    pity negative lose in recommend

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
