8a80788213e9c09be06afd746b49e8efaa939646
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:37:24 +0000

    him over wrong exhibition present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_list():
-        return collections.UserList()
+  def cube_number(x):
+        return x**3
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
