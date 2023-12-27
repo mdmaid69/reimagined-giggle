2573471fc8e7ff0ad16ac538d327fb695e43f7c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:11:31 +0000

    mend or comfort disturbing result

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
