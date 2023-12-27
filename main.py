29cbb2753584861dce09e7ad48b6c74927830470
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:50:00 +0000

    rather sudden ago choice partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
