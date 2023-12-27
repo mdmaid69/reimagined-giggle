c2344016786bf32cb9119a4dc2d82f9c7c800964
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:48:31 +0000

    your brief touch surface male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
