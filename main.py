c473aacd06c6078f470d15def3a8cff5f6242078
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:43:22 +0000

    confusing workout strange rush handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
