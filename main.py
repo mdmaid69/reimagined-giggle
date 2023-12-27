2a22c36eee725596ab4e7d526b33305373e6a151
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:31:17 +0000

    where tap 571enough Ll float

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+from collections import Counter
+print(Counter("hello world"))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
