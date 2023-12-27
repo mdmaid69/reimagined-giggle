e25212a6c351c35319ff4fa47c2bf5f907c9d7ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:06:09 +0000

    hit wonder pull embarrassing fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
-print(re.match("h.*o", "hello world"))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
