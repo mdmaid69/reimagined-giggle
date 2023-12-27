c0082f13749bfba6928418f48a6a26a33f9feaf7
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:05:58 +0000

    pour follow cruel smoke turnaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+print(sys.version)
+def remove_duplicates(lst):
+        return list(set(lst))
