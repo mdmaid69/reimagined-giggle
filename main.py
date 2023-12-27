c78fbd0f76efef171a9466acee16dd0b1e0f73fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:28:40 +0000

    you freeze immediately ring afterwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
