ab10fb97cf73bdc024b224639006fdfcd7f6f2f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:49:55 +0000

    spoil fast wide university birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
