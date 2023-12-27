828f684011f4384238059281ea530bce4298273e
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:40:55 +0000

    finish almost nose continue bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import datetime
+def get_today_date():
+        return datetime.date.today()
