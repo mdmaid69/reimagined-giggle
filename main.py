6dbb0c0401fe5ee8a6effccb6d75322116695f3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:07:03 +0000

    quit stage scream successful wheel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
