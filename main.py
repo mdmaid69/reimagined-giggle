7d32001086e7d007a845e8229db6b0401d09bf26
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:03:17 +0000

    fear cruel board worst pride

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
