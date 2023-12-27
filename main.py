d665bcb268e5fdcc05348bedb6d5768c34a61fa5
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:12:18 +0000

    lookafter apologizeapologise exam region boat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_int(array):
+        return int(array[0])
