2f86632de76e468c0fb7ab558feeb084f8876043
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:59:40 +0000

    scrape active press straight accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
