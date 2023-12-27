22db3821158bc9a3701f94cfd70522a3859ffc02
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:35:41 +0000

    boat any conduct wise challenge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_even(n):
+        return n % 2 == 0
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
