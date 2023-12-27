f76b8c887861e4d3f94b464c069aff503dba0a1c
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:46:26 +0000

    connect something look draw value

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
