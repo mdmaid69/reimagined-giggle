3649d613f74168320f6e94f769382a6e7d159afe
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:39:35 +0000

    lesson left hot successful with

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def convert_to_binary(n):
+        return bin(n)
