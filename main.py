6a5bc385502da385a2f257ec4c5c16bd1d226270
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:24:50 +0000

    noisy you lost perhaps place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
