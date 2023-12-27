8e27f3494f320f91c4c277f730d2bd96836b73a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:51:53 +0000

    pocket eye because suckme hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
