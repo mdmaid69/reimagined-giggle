84debedfa70a6a7054541efa78b71dd89f4523fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:12:44 +0000

    powerful thankyou concerning develop display

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
