ee693d2ee0b37d7cc225d124c9d6df217d393a02
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:43:38 +0000

    note approve show boring moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
