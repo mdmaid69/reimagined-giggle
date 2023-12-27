904c6c79847f87b912b0ce66c332cd7f9d78e673
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:27:50 +0000

    dream print work impatient section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
