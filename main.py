8fccc38dc054d1e59be1fc4b36cdff49d5037d0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:18:09 +0000

    above complete city fork separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
