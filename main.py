2f965bf7d0127c6e3eeed39e9212d6e49c17a761
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:24:44 +0000

    compare pencil bringforward should brown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
