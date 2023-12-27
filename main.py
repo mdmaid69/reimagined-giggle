8c214266a4084ef661855042440f62acf1aecba5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:00:35 +0000

    while change scared soft too

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def reverse_string(s):
-        return s[::-1]
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
