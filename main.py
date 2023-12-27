86d4abcb93d3121879b79ffb7073bcf22bed79ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:35:42 +0000

    embarrassing later circle fortunate bunch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
