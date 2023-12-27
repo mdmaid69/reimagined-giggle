6b47070edb4758a79f69c85ad0c07bb782757fe0
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:35:21 +0000

    conduct please company mad basis

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
