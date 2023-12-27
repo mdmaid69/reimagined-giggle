2a787d50d320d804c086d95b8024ffa03a863b95
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:52:03 +0000

    sense why search perform comeup

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Words:", len(text.split()))
