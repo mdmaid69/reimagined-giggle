efb1232803f5f83ffb3128919282884fc00c6525
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:40:04 +0000

    advice Oo eager ground describe

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
