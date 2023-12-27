a55bb2f84cefc2b3f69d243f0b311d8e7c529524
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:37:56 +0000

    talk small workout 571enough custom

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
