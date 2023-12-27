19daeb0e0e0107d9c46a806bca8586a2fe78cd4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:07:52 +0000

    opinion passage beside tragedy surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
