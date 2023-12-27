39239540e1c293816dac1ee31e8ac528153fe83d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:50:28 +0000

    unfair search original order confused

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
