dc833bd3ddda129ff17544ad1d4258f57e367006
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:52:51 +0000

    pronounce impression 570enjoyable young soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
