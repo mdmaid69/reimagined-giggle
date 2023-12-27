f82793f0c13ce9ad15af1d7d2237df78582ed506
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:24:45 +0000

    become practice taste reliable upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
