2c0a86eae68902bf14a702680eef172c9764c506
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:38:42 +0000

    far communication production influence with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
