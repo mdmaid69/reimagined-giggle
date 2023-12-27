d2d7871510540b4165444713e544996c92f0fff4
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:19:02 +0000

    gift 572enter duty persuade married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
