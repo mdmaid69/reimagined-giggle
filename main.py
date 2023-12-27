c2847c0af0c2cce520b27f20ecc7fbe846467b74
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:59:43 +0000

    suit probably real Ww group

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
