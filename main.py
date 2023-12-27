1ce4883445d87eec1f3584913128b77d33e2d9a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:08:26 +0000

    board tap trick trust border

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-print(math.pi)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
