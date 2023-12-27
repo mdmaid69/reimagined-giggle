0216516b4b3d2a1e62439ce438d72fdf7a839527
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:52:06 +0000

    convince container backwards account opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_memoryview(array):
+        return memoryview(array)
