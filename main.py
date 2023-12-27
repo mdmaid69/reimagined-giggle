af03036fcae9cca75c84e67afe7a3877c1def0b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:30:42 +0000

    squeeze can popular whichever fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-def add_numbers(x, y):
-        return x + y
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
