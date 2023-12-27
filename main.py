d1ea09497bcdc408dd0f67c4f6bcbc3c96c860b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:03:14 +0000

    congratulations clear poison crash one

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def is_even(n):
+        return n % 2 == 0
+def calculate_power(work, time):
+        return work / time
