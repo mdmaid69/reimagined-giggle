5101cde713ad58082493c8c34fd938bf4d74e669
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:33:31 +0000

    properly whisper objection waste turn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
