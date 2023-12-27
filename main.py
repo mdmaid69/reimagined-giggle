08fe1d26d0e3c07daa640123d308f0ec5341eae8
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:49:38 +0000

    frightened hold condition fill genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
