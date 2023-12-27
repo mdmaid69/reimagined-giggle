f439c81c88db8bccfed6c8840dd4cb037e851396
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:57:00 +0000

    lower distance family goback(to) substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
