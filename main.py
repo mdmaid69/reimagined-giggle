17fddc6f3b41d24dffd5deab4d63336ffc08ba87
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:35:19 +0000

    noisy yesterday wash emergency slow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
