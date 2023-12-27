a0213a8a8f80a1144900574627fb7a6d0a456afd
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:23:16 +0000

    dish frame press declare manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
