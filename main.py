549a20c991c83d0108c29b863c4f1cdce117e2c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:15:40 +0000

    knowledge relationship future figure test

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def cube_number(x):
+        return x**3
