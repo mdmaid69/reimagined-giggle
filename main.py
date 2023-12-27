8a972067555297fd5fede7d4cd5026e213bdcf34
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:29:56 +0000

    avoid priority forward free turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
