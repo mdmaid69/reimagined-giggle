717872cf75127a011eb913cf149779c44c8062f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:56:22 +0000

    few shelter necessary switch Qq

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
