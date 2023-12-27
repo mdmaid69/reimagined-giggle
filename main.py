172640d43f4599b16c9e6f92902d30a455286ce2
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:37:29 +0000

    run keepup week medium involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_floor(x):
+        return math.floor(x)
