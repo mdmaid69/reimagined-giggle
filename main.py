47418c1bb17c3a1b439acbf84a38414f89d6c54c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:45:19 +0000

    convenient support pair convince less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
