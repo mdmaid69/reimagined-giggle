cfe03f1c2f38dd18e60d9734394cd3ed7f7d6372
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:47:09 +0000

    callback company settle tell really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
