1ce389bc145e610c68571c7687573a6b3c6f1356
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:29:46 +0000

    achieve couple later slide fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
