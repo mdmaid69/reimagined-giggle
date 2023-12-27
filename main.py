2e8cf6e24e64df11d6502da95b8f34cc93ae3799
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:35:34 +0000

    seem calm disturb talk also

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
