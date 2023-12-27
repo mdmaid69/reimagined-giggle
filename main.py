2097ccfe1eb9384f61ead6c7f1db3e3a35b90e11
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:15:18 +0000

    blame your cross mean town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
