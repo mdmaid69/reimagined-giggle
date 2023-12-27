8c2e52e02f4d6ea39193c67c54f8cfebbed179a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:22:03 +0000

    interfere badly fix pen boat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def get_time_since_epoch():
+        return time.time()
