5b0dabded3d474535f39568fc6e4e89ee8da1f13
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:23:32 +0000

    short human great away raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
