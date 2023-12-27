ebdbff94a73f807d2787c2ac987a88bf9d951720
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:33:04 +0000

    Qq weather gooff quantity twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
