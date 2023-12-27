d4f221c80e3d8d9922106c1d5b09d60aaa7f3764
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:53:44 +0000

    contact then game out fat

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import random
-print(random.randint(0, 100))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
