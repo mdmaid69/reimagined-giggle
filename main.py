5706e97ad5310733c771f03a60404f1a2ea8b5ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:47:02 +0000

    breathe top target dream tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
