b334150465c81e95821819820cdd02aaae74a69e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:43:58 +0000

    that old sensitive win forget

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
