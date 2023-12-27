8944cc8eee9ede1da4615a78661d0ea3658fc1eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:10:07 +0000

    generous telephone takeaway straight recognize

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_power(work, time):
-        return work / time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_work(force, distance):
+        return force * distance
