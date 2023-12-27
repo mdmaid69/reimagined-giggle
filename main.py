5a27281fa0e1c51b2fea15e24b8b75d226dd1c3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:31:58 +0000

    payment say treatment suit factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
