cfef7a6fcb49e601de86c04de79dcaf89ecd428c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:06:08 +0000

    when near bill instead behind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_speed(distance, time):
+        return distance / time
