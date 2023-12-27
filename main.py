f9c92fb72a8f137ccc004b4027a12c7497dc9c9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:04:52 +0000

    comeup kind subject cheat kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def reverse_string(s):
+        return s[::-1]
