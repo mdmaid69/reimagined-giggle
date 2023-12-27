f5d018f57cfa70d4f3c9d8a3cd316982fc678799
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:36:15 +0000

    finger event question date collection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
