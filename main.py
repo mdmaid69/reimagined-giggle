0934fb3ed4634d3f5c0d8b8aadf40f26b219a499
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:52:29 +0000

    prepare precise should flight might

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def append_to_array(array, item):
-        array.append(item)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
