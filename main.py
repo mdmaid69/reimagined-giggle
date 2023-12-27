a14a7d44a67d39260ef3a853f88da3baf4628a81
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:20:26 +0000

    play sport keep turnback switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
