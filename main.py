ac1b34f08744573c84aa49c3f3e085bd97c67d8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:21:13 +0000

    giveout contact minimum outside aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
