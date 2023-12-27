e68d914b30fda9aa432558e8902174fa80533bc7
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:45:23 +0000

    plenty snow step near claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
