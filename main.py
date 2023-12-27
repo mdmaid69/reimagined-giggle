0e2e3e3dd952361779e00847a700b01beff08902
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:59:45 +0000

    slice moral disapprove spare but

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
