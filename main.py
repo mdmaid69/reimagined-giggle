cecc3394e52d19173e98589cf280d26f4e56df66
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:47:17 +0000

    tax worth breathe blue score

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_length(array):
-        return len(array)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
