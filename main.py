b07df3cf7d2794b382c74aab9c99fd76b94525ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:03:48 +0000

    prefer female promise further fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
