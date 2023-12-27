f0cfbd3964e24adbc6cdfa33b5ae770c88de00f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:54:14 +0000

    sorry fit spoil choice fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
