79180483df98d9f30df1e91ce51b604d1687fa25
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:56:50 +0000

    himself tip find giveaway letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
