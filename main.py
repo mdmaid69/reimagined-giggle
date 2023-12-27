ee10fcfe31737b20292abc243401be184a5658d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:25:47 +0000

    mark each closed something temperature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
