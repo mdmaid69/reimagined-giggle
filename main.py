1e6cd5b24cbf39398f769bc8894db2209e8b8f75
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:28:56 +0000

    accidentally hole soon provide world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
