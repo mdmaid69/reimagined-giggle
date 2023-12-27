6a99f595962678101d66cf570b469c3a0da73ef2
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:14:27 +0000

    even game minimum confusing person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
