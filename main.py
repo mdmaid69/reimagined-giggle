cea90ed2301a4c7fe8faba16ac011357d0c0097a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:21:10 +0000

    shine unfortunate use blow comeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
