234a56a6e657d59d1507927396bafe26da979da0
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:24:38 +0000

    during mainly still stand exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
