55ed7ab98f21ef60700f1733f4bace8d417f62dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:32:07 +0000

    comfortable lead injury ashamed mystery

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
