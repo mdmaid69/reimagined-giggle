0418a511b8cb86a96a4d71bac36adcabf7fa5274
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:57:21 +0000

    never walk strange wander carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
