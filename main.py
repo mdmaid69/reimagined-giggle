34a1554b289464610d060a7d1c366d674df65e3e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:04:07 +0000

    belong state peace interview cover

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
