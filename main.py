34a589e044a2d6c3b991970bef81ecb49f22fbd5
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:15:59 +0000

    go dream spot comedown memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
