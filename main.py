3deaab39813dd8067652a88a14002cb4632e4856
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:54:54 +0000

    term peaceful minority public show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
