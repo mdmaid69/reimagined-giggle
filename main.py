db700fd1e9c3ca715d2499837d4d5bbd5f217afb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:33:26 +0000

    avoid life young strong bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
