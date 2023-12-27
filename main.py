945fcb85116c771f85be7b106d76a2cac46a3510
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:19:24 +0000

    several underneath distant minority depend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
 def calculate_density(mass, volume):
         return mass / volume
