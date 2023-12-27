218e0dcd09f8c743199ba89cd9c9a5e81fa71bfb
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:19:20 +0000

    instrument oil meaning order blame

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def is_palindrome(s):
+        return s == s[::-1]
