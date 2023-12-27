4ba075567d548046167ce1677195203ea6546a2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:24:52 +0000

    race male occasional address afterwards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def sort_numbers(numbers):
+        return sorted(numbers)
+def find_max(lst):
+        return max(lst)
