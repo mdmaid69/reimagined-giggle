a1c686689d7e394c610b5aad682b9a93af989f70
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:04:36 +0000

    count edge kill strange region

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def find_min(lst):
+        return min(lst)
