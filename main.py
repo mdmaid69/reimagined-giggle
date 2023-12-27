391681ef042848da0140728035d324fd06b139cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:53:26 +0000

    underneath shelter manage on too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
