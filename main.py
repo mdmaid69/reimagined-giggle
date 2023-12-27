698f81683348d01791df38774618103d4e318427
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:31:27 +0000

    prove cross separate hour file

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
