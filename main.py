797112182dda913299d3ca4f7805fe2048514301
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:38:55 +0000

    goup guard godown reaction possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
