04f1802b21db84b2ad553b06fd02d5bc313c02f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:58:13 +0000

    Ee aware workon guess suggestion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
