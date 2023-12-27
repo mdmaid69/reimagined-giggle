7fdffb718fa9a0f5f5ddc1785fb05b504d6e765d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:54:50 +0000

    brush tie send simple behind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
