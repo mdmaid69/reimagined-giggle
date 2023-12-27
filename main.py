6884db10515b444ec654bf12bd7e2bc4143ae5ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:57:48 +0000

    book voice herself improve climb

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_bool(array):
+        return bool(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
