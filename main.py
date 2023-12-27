511b4513b744c112e605e8e82c6d79f7c5e329f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:35:53 +0000

    store zero cap money behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
