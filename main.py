34161cb1471fd7319187752e4f4006ad6a8a5e11
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:56:16 +0000

    lookafter dry sell hold have

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
