8a35b5a28820b0357960fd027a3071858cc03f61
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:50:24 +0000

    bringup copy small tradition lunch

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import collections
-def create_counter():
-        return collections.Counter()
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
