cbd74e4346ab79afcaad92057cd86cb1d87e474d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:17:58 +0000

    talk takeout contact normal unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
