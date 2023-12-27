9fa31d5319f92e451854ebbf6b317efb40075b84
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:45:57 +0000

    fair permission joke operate empty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
