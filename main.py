aa6f1b9515bb0ff45759b428c1f595fac362bdbb
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:22:24 +0000

    song brown smart die even

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
