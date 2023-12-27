5b5eeecdfc447df6c46f554fe93b38a3149397b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:17 +0000

    rest would find aim recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
