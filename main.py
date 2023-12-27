5b6c50e15be56741531c3015e95ac3779b4003f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:28:06 +0000

    influence opponent please time tough

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
