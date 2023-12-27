58b89231c0f50314f77b0ff7dec9d1bd29ae6189
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:49:51 +0000

    goodbye test instrument 570enjoyable exam

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def convert_to_binary(n):
-        return bin(n)
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
