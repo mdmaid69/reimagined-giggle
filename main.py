14c85f009ac427339ffb8c1725c1218c2fbc1d7d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:50:54 +0000

    copy hers content cover rubbish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def convert_to_octal(n):
+        return oct(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
