910121c038b35ebe51f4f8ef6967a976020267ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:52:28 +0000

    powerful rest impressed war there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def convert_to_binary(n):
+        return bin(n)
