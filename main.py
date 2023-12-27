70e31d467fe187fe0a1f468696f48baaf531f321
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:22:24 +0000

    yesterday thinkof even afterwards bottom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
