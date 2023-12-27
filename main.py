c96bccdf1222267d10279d75f0fa94e20ff2c46b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:11:36 +0000

    detail any crash practise sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
