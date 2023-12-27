b21ce6892440aade14b4fca15af01ccb6d421b4d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:30:30 +0000

    question any thin cum case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
