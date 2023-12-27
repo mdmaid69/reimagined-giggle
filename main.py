9ef287a206f56286c0dca61f7c033cfa3d19e0ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:59:27 +0000

    dress door occasion thick they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_rectangle(l, w):
+        return l * w
