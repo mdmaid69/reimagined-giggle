93871ee6d4f17185eb55d9876efc039cf2a330de
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:14:59 +0000

    unpleasant accommodation yet bear lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
