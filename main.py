878b4b9a419b7a52d04633ccef350442cd8fd1bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:58:05 +0000

    talkoutof mistake treatment tap intelligent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
