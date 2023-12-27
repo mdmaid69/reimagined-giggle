99bedf28ba5e5dae747486444b1eaa92ffb73fe3
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:54:33 +0000

    please break argue rich anybody

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
