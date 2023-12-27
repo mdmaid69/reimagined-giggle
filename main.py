eac5642667a389b50dbd0c0b593b0df14ea1947e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:21:32 +0000

    patient duty one block grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def clear_array(array):
-        array *= 0
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
