cc64d2c02505d27029577b8da2c10b464b29ef99
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:39:56 +0000

    fortunately middle thinkthrough damage yes

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
