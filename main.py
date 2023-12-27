d9e5341b4fcb4da343e925248406997aa638df08
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:54:25 +0000

    estimate few pride ask conduct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
