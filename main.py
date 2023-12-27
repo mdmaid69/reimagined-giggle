fa27f9ed0cb1bddcc5411d94c9ea7ef83711366d
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:45:21 +0000

    claim press press difficulty valuable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
