1d9e78029fafd0afb27398f60e19bd45211bd9b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:39:51 +0000

    task partner birth cook building

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
