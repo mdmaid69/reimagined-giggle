eee467c9bb2103f3cc62cd1bd284e509904d5168
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:23:31 +0000

    period earn spare heart peaceful

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def convert_to_hex(n):
+        return hex(n)
