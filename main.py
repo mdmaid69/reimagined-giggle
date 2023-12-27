1c470f9524aa18635d9d2d2e48429a13e3ff9cc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:24:45 +0000

    breakinto reserve however priority goaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_length(array):
-        return len(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
