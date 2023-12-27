05f9d44e0323f497f7627e0f8f732274fe7ee2e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:18:44 +0000

    respect prepare share rest minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def count_characters(sentence):
-        return len(sentence)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
