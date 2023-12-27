6fa4af29f8970c03b2551d88a7ba08cbffbd1e5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:39:19 +0000

    outside bottom pleasant about against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
