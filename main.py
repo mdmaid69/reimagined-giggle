2e54584697bf4ba2e7b859d66736d8cf410ed038
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:38:13 +0000

    walk sort report fit note

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
