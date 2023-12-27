e2306663ab453605df3b424ae2dae014bd35ee17
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:17:13 +0000

    patient no perfect badly empty

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def reverse_string(s):
+        return s[::-1]
+def calculate_average(lst):
+        return sum(lst) / len(lst)
