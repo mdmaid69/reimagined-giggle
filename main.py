ce23175bc2e1c8ef07ec326d2c2d4f2665dcf364
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:16:31 +0000

    worst straight forget partner consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
