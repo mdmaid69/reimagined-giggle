6096698d491b6f91ac69b696810224c1b0803fce
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:59:00 +0000

    awake capital gather male national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
