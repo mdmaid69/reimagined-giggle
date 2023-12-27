bf015fd598846c949ad57f74e5fd98729a208550
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:57:32 +0000

    Oo television tone my common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+for i in range(10): print(i)
