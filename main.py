6c9b1bacd782a24616fa83cf9eab182f40e1b088
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:20:05 +0000

    remove if variety exit write

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def reverse_array(array):
+        array.reverse()
