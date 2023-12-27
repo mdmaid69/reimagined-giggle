c86c97d728aa70d3d6c70e441e751d1a1cea22c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:02:18 +0000

    whistle organize Zz skill goout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
