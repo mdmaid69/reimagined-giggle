b9c66bd2a80e6a4407717dec2633aa6b34ee0803
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:55:04 +0000

    expensive shadow class rest continue

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
