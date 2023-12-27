0bb297ac1d28bbd773c9564bddbf9cb302ebd7bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:43:21 +0000

    stay choice rent normal put

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import sys
-def exit_program():
-        sys.exit()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
