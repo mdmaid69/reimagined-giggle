36196e5dfa79b53cd8442e12efc83d89acdd4802
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:44:59 +0000

    closely Yy spill hour war

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
