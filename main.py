64c1c0ab19d8434366de413eb3b76bc6cb36f9b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:23:01 +0000

    where straight estimate check afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def is_odd(n):
+        return n % 2 != 0
