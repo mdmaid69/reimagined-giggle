59c0d9e96dfe710daa4f5085796d2122dd857c91
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:48:51 +0000

    cover cheat fun prize production

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
