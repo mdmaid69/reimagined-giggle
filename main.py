d093286c6165495e9a0b1c30d22438cc512f194b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:48:03 +0000

    goback(to) share prove attitude keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_slice(array, i, j):
+        return array[i:j]
+from collections import Counter
+print(Counter("hello world"))
