643e7b794fa76ddfa5a908f0682f6e0bed59171f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:01:15 +0000

    by there sound seriously flight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def is_odd(n):
+        return n % 2 != 0
