b53a08461620270472c3e1305c14f99c2cdc35bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:59:15 +0000

    shut smile rest owe nose

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
