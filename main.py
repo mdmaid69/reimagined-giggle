8e836e7cfcf8727d870d77500fa656298a7745b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:51:30 +0000

    action brush rise name opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def clear_array(array):
+        array *= 0
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
