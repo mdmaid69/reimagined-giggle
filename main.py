36e557432db2f151d85d08cebe30187da59f2320
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:49:15 +0000

    government thin possible condition seat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
