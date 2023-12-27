26a62e649082ac172a4637be3ea56fcea30a053f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:21:22 +0000

    either ask influence under fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
