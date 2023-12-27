fafbf1065b4eeeb862ef37cc4a740aac06345a5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:17:54 +0000

    exactly store study give consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
