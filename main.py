38f37f8b837b1abe54fccf0aa6961af026b3d13e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:02:13 +0000

    deep spring challenge nothing confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
