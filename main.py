4f77b1c28805beae02b6ab8e3115734c30cd3634
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:36:16 +0000

    swallow anybody wait what surprise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def add_numbers(x, y):
+        return x + y
+def find_union(list1, list2):
+        return set(list1) | set(list2)
