81fefe8f2abdc75ec19ba328a42ae32e3109673a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:40:04 +0000

    break clothes sting alive relative

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
