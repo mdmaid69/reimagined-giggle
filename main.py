806381b0615a4455cc9c340f020d7ae039bb4b4a
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:27:33 +0000

    minimum return cruel offer last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
