45b4eb5701d0f870dbe277fe8ecbe347341feaf3
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:27:54 +0000

    extra again ahead character between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
