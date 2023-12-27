60c6dfdcf673a619e862ac992151d97783ae5c2a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:41:25 +0000

    sudden lend person entrance main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
