e98c27249376f37dcb36dea6c37ba8e27e57d1fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:17:14 +0000

    far standard race criticism intend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
