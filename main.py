d82a81d4be24b52a2a6fdcfc2a63d47f3df13d35
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:15:24 +0000

    emphasize point favour disgusting shall

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def is_odd(n):
+        return n % 2 != 0
