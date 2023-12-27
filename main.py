ab85189428db4175e9261817df79cace8788e9af
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:07:27 +0000

    punishment clue mark true side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def delete_file(file_name):
+        os.remove(file_name)
