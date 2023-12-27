fd71634dbfd2865d2fc31fc415d8ff5de596c46e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:20:12 +0000

    impressed square otherwise bite lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
