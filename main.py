ec55702541ffe364e6c8b55ff2931ac17a95bad4
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:21:23 +0000

    clear but chat answer spray

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
