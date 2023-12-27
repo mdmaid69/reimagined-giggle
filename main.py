d00d21571bc95156fc0d54c235a4b6cb52bfe4d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:37:57 +0000

    excellent thumb work behind manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_list_from_array(array):
+        return array.tolist()
