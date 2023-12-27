4116bfa1c8e54c47fe1a614819445a7e492b50f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:22:07 +0000

    fierce Mm thinkthrough actual flood

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def add_numbers(x, y):
-        return x + y
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
