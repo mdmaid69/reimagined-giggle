569eb8d47c1601d30ff50c7464c3261039cee37a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:22:19 +0000

    any cope reject wall bag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
