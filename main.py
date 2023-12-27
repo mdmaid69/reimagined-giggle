1e62725e607b19354d61dfe6e6456fc6e4dd3b8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:27:12 +0000

    suck possession exam fair expect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
