27b731786b85c560235a3e9401acc8ae95f52a19
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:38:42 +0000

    impressed satisfied convincing basically cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
