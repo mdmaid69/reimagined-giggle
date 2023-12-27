5d65de8037fc8dcb69afe59d174cedfd14b7328e
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:21:41 +0000

    pay keepoff positive house manage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_set(array):
+        return set(array)
+def reverse_string(s):
+        return s[::-1]
