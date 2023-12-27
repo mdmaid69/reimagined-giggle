20102fee83b7a8cce96d1c21730bf4279e18e94a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:26:39 +0000

    head unless shut giveup godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
