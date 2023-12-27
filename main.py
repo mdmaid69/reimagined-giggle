7b60849f225d73f5bb66b0e33305329c94d126f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:39:13 +0000

    destroy back heavy Uu port

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
