df9ff02cdbda799935de047096765d905346fc22
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:41:08 +0000

    runaway workout habit start apart

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_speed(distance, time):
-        return distance / time
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_item(array, i):
+        return array[i]
