405a16b3a5347712f6b5b7b1fc57bed76f85b3e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:38:23 +0000

    about lonely goback(to) exact noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
