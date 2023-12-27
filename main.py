7951210cb33d0d306dc43d7c4f3b5475ecf4406c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:29:35 +0000

    operation ability accidental plenty our

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
