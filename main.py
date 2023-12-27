91fa0beee6c6115fd2886286711d0d2464b025c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:43:25 +0000

    basis know wire trade challenge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
