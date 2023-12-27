ca4a5b4a0005ea784690ec236bdb4699c50d30b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:11:20 +0000

    laugh stretch discover work power

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_bool(array):
+        return bool(array)
