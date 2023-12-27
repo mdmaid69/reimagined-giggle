6e3e7b489fa87b3dc38d166ef4c7297fb989ca4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:07:51 +0000

    authority regular ocean contribute warn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
