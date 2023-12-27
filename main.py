a7bb27f5b1bb12df48810ca29e85971ddb2d3217
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:19:43 +0000

    without increase standard never thirsty

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
