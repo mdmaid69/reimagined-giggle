80b711706ca0895554017c7f010c2dbed1cf33ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:31:02 +0000

    twist shine him press company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def get_array_as_bool(array):
+        return bool(array)
