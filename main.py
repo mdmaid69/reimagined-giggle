6bebe7c64d7b2c75a51a31a4993a027806f81b77
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:59:51 +0000

    heat design sail glad rate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
