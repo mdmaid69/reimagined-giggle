7daba4d54f4bc3e628bd886ffffdd0a18c6fb222
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:03:00 +0000

    out tie create worth street

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
