5a46181d7d2e341a5b09be082483c418c9647709
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:06:48 +0000

    wish born lean communicate answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
