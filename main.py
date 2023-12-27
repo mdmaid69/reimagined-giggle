67a802fc3e7657fc897f76e83cf81a340c65e19a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:27:15 +0000

    better rub imagination rid body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
 import array
-def get_array_item(array, i):
-        return array[i]
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
