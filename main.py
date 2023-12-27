64c64946d27defb9e48d2a7e864113ef53b1d951
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:08:55 +0000

    common bright reasonable shoulder scrape

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def greet(name):
+        print(f"Hello, {name}!")
