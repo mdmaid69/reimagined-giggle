980fdf09a3e5cc4bde51071409ce08a7e4aa9e68
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:06:05 +0000

    create development either it Kk

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
