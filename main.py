1372e92b7faf864467cf112e1621b2e379aae753
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:14:04 +0000

    file natural cause hit muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
