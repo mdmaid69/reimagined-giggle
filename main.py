3d73f37c4ba8ac8d2896e8d5067926d418ae26d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:28:03 +0000

    climb turnup practise tip noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
