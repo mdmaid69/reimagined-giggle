0a2ecc6ec9984c93a87cc49576d055391478cde2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:05:03 +0000

    drink trade fear soil belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
