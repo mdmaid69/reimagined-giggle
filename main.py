78bdf7fd1db293ac0133bdb2294b02d4ff2c3ad4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:00:13 +0000

    office nonsense desperate too conclusion

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def count_characters(sentence):
+        return len(sentence)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
