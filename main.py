7c959c1ba17ae02e2b43033f0b5eb1faca18d89c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:37:40 +0000

    getout well partner her fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def subtract_numbers(x, y):
+        return x - y
