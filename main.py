c8705ab3b307674ec19ee0386be9523695d50e49
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:53:17 +0000

    organization medium rate certain find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
