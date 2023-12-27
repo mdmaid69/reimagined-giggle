e5b277620a1e1917538a9ca167749234263519cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:25:22 +0000

    rule flow muscle keepaway 571enough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
