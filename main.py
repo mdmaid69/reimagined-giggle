1d217b6fb077cf154fe287ec107d0b17ef25f23f
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:59:56 +0000

    comefrom shake order class Zz

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
