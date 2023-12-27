2e6dea006031e7ab74b5e9394e4f91da6407ed5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:23:36 +0000

    muscle brush enthusiastic increase fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
