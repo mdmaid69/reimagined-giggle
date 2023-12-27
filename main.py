e1ece651effac88c5ef388d5b4893b9fd1a02c41
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:38:02 +0000

    needle label like sink feed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
