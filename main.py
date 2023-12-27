5a0cc89eb671b7f1c2d1aa1f2ad0c945518c8cb2
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:17:47 +0000

    joke destroy comeover short contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
