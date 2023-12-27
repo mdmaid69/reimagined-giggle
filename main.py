6da30e34cbd955c3ba242b9e682cfc1f6f385e20
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:50:17 +0000

    act yesterday cook bring aim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
