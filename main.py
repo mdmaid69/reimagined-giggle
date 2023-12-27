46b85784b05f5cabe800e4cd43580ace6d197aff
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:32:56 +0000

    appearance fall scare class tight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
