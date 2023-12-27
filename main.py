7152bfe72a6e874b2e0667c5986816d46af74d5c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:35:45 +0000

    image pour flavour reduce tongue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
