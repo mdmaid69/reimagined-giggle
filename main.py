a53c4811ff9dc9fccf4786f09e8f7fa02769097b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:25:53 +0000

    address glass fast feel repeat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
