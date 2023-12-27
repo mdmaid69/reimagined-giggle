d5b0dbf69463fcf9c5ab495dcf931bd868ca4085
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:43:09 +0000

    high unless conclusion ask inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import re
+print(re.match("h.*o", "hello world"))
