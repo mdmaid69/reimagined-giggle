d84d800b7b243bb37ada8011bee9b7565c38c7b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:43:37 +0000

    fly walk shock fold manner

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
