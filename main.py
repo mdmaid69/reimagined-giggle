27c72c58d5cb8fa4e5daf72b2eff16fa38c1fe5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:28 +0000

    odd consist surprised awake careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
