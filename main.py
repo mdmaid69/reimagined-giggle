9ff03d5a136134b975b636cec7710a1df861847b
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:52:45 +0000

    this feeling solve lower snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import sys
+def print_python_version():
+        print(sys.version)
