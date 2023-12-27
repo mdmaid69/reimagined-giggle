6f49130ab3d879bdd493b6f2f50f58ed2b17d448
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:05:10 +0000

    cry Tt forward score emphasize

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
