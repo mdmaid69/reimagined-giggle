d156b053811c1fd8e03bab1281daf6f1f234b5a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:06:13 +0000

    seriously everything worth report order

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
