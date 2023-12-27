04f2067805b01c0fd2a426eb39523c6e4bbdb806
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:55:06 +0000

    maximum fashion bet physical choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import re
+print(re.match("h.*o", "hello world"))
+x = 10
+y = 20
+print("Sum:", x + y)
