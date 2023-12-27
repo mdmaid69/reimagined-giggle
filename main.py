05053f6c4cbd442c74f7d5c2a97259cf35a909ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:02:12 +0000

    market sign flood arrival warning

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
