b8fe6b62143e5e164942c8dddbd701ba7b750d71
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:09:23 +0000

    flow listenfor deal Rr price

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_circle(r):
+        return 3.14 * r**2
