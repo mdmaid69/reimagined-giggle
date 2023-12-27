9869531ba4d7fefcf3c64e8d658a93c30257b506
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:13:25 +0000

    discover climb measure summer break

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
