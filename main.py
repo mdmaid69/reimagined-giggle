9e65c94511388d9701789522adcd8a3b032d9d7a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:08:15 +0000

    juice error home workout bye

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_sine(x):
+        return math.sin(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
