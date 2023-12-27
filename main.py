b81862a12edda07a1050d54c10812692f69f1eb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:53:14 +0000

    careful stop mess failure keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
