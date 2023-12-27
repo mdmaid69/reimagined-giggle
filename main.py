6eeafbd9461ace7190cfa8c3860a1ad3441bf7ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:19:53 +0000

    from male mind knowledge vote

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def find_unique_words(sentence):
+        return set(sentence.split())
