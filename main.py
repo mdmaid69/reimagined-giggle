d638a69f99e411d2a0a4e8ca54b291cefabee373
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:18:28 +0000

    press joke hello professional Kk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
