824d2531ba251241f86f6e21d4a9fa40982c6238
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:44:35 +0000

    contrast effect priority content modern

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
