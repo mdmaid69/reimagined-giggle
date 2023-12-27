3dd5e4d7885c1bde84b10d1a40d391908f1bf1bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:27:28 +0000

    fairly getback flow plant pain

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
