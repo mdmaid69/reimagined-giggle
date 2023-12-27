5ba7c3d6074d38171ed402e39a18e1656d1716ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:47:17 +0000

    school through length talk shoulder

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
