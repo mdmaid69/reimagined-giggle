78300b963bc141ccf14531d2ce1551aeba57eaed
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:46:09 +0000

    569enjoy phone loud run absolutely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
