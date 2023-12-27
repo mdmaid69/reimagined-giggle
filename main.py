503e1650c4b7e005a3d22d3d888b757cc1f6a7df
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:06:43 +0000

    rule message computer bridge swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
