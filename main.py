3037bea0e371d015c6b222909fc50fd7d5ed8007
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:34:59 +0000

    weather bear breakinto medium worst

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_pressure(force, area):
-        return force / area
+def calculate_acceleration(speed, time):
+        return speed / time
+import time
+print(time.time())
