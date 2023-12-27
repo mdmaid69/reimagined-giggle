6e9ee1709879780c85738e9278f2c89e410ec0f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:04:05 +0000

    middle spring pin young breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
