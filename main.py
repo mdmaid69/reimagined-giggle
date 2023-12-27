cff9fc7a6fc7c83d5848c1fb23cfbab2aabc06b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:36:58 +0000

    doctor corner release square positive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import tensorflow as tf
-print(tf.__version__)
+import os
+print(os.getcwd())
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
