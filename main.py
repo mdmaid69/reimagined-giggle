c86c69304bfc01c332176f3e530849d7340c00b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:38:53 +0000

    adjective right damage sit public

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
