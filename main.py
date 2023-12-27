db3e9af2ca36f8cc6c69ff228b7f08e8fa8a5566
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:30:34 +0000

    hurt brain interview rush length

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
