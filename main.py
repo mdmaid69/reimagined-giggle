3f516a740abf3e953e090913ccf12334b7b3f7b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:43:54 +0000

    trick control moral various exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
