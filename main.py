a4ce5ac53da3af057b4b2d232f9831d941119771
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:07:03 +0000

    equal whether accidental nose roll

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
