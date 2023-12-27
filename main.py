b8f35b1df424e45ef23aa36de2aef6915a0f8d89
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:10:35 +0000

    rich agreement summer mention near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
