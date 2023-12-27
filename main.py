6ea0bbf6f6168651c294a056ed706a9b85c1d436
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:53:22 +0000

    recover course false cap lookback(on)

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def sort_list(lst):
-        return sorted(lst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
