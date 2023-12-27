6e25c962581e7f7a505e2c0949aead86c4075b98
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:57:37 +0000

    bother pleasant basis rank damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
