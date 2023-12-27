1bfd35f6e6e136fa036a4061111153f4e302a2af
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:03:48 +0000

    crisis extra poison want Ff

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def is_even(n):
+        return n % 2 == 0
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
