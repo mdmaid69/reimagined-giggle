ad683bf78194def5543edce5e7f729666346436b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:19:31 +0000

    flat like low declare development

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
