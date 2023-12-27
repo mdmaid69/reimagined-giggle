99b6c0217b85c5e22a3020e3ee1d228b6f2e68a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:22:55 +0000

    shut plastic seat thirsty thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
