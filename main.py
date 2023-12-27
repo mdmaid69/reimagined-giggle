c92e26f88c3cee604ea67709ebdbad9d3716d87d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:50:58 +0000

    seriously defeat look dead coat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
