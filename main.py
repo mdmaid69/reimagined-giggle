7f55dcb90e0ee9814f7e3f04fda10bc974363574
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:41:14 +0000

    material Gg generally deliberately justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
