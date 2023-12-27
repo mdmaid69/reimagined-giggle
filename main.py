d23e6775f2430114951eb34bb87bc63e7659c8a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:40:03 +0000

    flight loan set method fat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def convert_array_to_string(array):
+        return array.tostring()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
