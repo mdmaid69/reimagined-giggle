61ccfe8e2f4f977aa3d1073e070142cee480d202
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:24:59 +0000

    name serve rate brain late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
