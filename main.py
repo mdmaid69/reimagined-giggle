6dac5a37674641486a848ed5235bc33eb606e9f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:02:44 +0000

    passage drink every manner skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
