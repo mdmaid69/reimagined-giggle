8bd0970c7ac5ce3416fec76b6235e3898c9c1ba6
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:32:16 +0000

    share must mine regional note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
