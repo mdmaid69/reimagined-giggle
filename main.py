6d3ac905695028c36d5c421e862c38c536baa343
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:39:09 +0000

    advanced raise recently problem exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import sys
-def exit_program():
-        sys.exit()
+from collections import Counter
+print(Counter("hello world"))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
