36fe91b8b04c4041cc017a9d244c94a014df7d45
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:59:54 +0000

    high centre culture content under

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
