8d2938fed24e81129c9f5e622938da6a409a8591
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:07:24 +0000

    breakinto space cheque strength shut

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_acceleration(speed, time):
+        return speed / time
