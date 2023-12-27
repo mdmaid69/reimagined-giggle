2479bd7fe40c76b20fa73491295ef98ab6864ccc
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:55:01 +0000

    quiet country influence open callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
