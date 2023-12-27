2296e870739c7070116c0fcf51e514c93ca648ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:55:11 +0000

    still leg behind successful previous

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import collections
+def create_stack():
+        return collections.deque()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
