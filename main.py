0057fd2a7ae7fbf0c161a04c299e37c5cd257fd1
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:55:21 +0000

    sure knock draw rare soak

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_acceleration(speed, time):
-        return speed / time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
