f2d33998c49994c2618ce4c6a624aff7d26edf5a
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:58:59 +0000

    calmly keepup mistake press way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
