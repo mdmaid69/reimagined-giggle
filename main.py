e3aabbc6d472e065dfe74ed61a0a4aeb7e24cb27
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:05:00 +0000

    section comeout poison pay poor

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
