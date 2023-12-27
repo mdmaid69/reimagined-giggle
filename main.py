d528c2823e63da013f8314bcf981831eb2f9c032
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:05:13 +0000

    chat sail project difficulty man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
