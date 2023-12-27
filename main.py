1e1c3292f3144996af46263a19e68e103cbd2879
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:39:04 +0000

    pass choice lookback(on) limit Qq

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
