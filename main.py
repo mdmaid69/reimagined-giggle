42524ba51b75488d6ec3f44233811d735ee78181
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:35:52 +0000

    price without forever stone road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
