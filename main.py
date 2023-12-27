d32b892b4f631dc9042d62b867b4299c18e0490a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:38:17 +0000

    usual recovery connection pleasure sure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def add_numbers(a, b):
+        return a + b
