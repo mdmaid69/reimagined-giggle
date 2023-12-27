e060ec6c37926b421cab09c90056f513bcaf17ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:47:54 +0000

    Oo whether lose wheel plenty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def square_number(x):
-        return x**2
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
