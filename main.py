6bb3d8c99703db819593549812790036805f165a
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:54:39 +0000

    content inform young guard storm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def is_odd(n):
+        return n % 2 != 0
