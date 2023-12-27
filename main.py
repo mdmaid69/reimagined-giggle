42bd2b06054798dc4392c0ce83a357f07cc15d6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:00:10 +0000

    steer shape shock over face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
