87ed0fa7abaaf6e27ea0d9b89ffba9793935afce
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:52:19 +0000

    raise yesterday bite alive base

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
