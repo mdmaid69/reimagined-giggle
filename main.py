829be725e6862f535ed4f614fe849e8dce6c20c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:12:20 +0000

    threaten grade equal lock man

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_min(lst):
+        return min(lst)
