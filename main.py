47fb4861d6d075c24628574f1f7843a5ee61026e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:35:03 +0000

    last react man include post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bool(array):
+        return bool(array)
