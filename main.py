6d801002e492101a582964e6a5fc86e7d4f6fd4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:27:35 +0000

    brush grow sign criticize field

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
