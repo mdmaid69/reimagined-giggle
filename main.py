c3b77f02714d0279d418560204e2e78a30bafa6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:34:38 +0000

    image sail cause little produce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
