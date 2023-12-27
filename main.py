ea2e48b99e76d0352bd8eaff93857aa9089c79e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:15:33 +0000

    road human influence Yy steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
