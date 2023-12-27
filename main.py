a918392914e0cc9c353d6d80aa7ee5237141e153
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:40:35 +0000

    dear union judge capital earn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
