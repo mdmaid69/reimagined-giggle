b7790aaedd212d5b3fca9c033fd1e621691d6db8
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:09:25 +0000

    thick people react conduct opportunity

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import sys
-def exit_program():
-        sys.exit()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def square_number(x):
+        return x**2
