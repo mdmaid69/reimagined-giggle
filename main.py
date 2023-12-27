c18d768b3457856508de36012c5fb21db9d64ec6
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:10:17 +0000

    straight that frequent call in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_repr(array):
+        return repr(array)
