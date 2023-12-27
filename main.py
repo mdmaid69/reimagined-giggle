cfcf2547f32e8b2992456718a8cd835b0d311fe2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:31:34 +0000

    lie branch mixture ahead accommodation

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
