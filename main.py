e2a4b500acf4b15d91f9e7b02e9c9f60065c628a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:03 +0000

    prize discovery settle call mind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def convert_to_hex(n):
+        return hex(n)
