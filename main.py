eb78248088a779c0a878ef23fb814671e357384b
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:15:29 +0000

    disturb object character difficult sorry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def split_path(path):
+        return os.path.split(path)
