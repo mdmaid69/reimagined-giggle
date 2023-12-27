b8455c8191000617878d56cb4506d9b155f0915c
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:00:46 +0000

    moment generous explanation pity welcome

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_interest(principal, rate, time):
+import sys
+def exit_program():
+        sys.exit()
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
