8fe6949e1e636771082fea7944c5ebe7be79822d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:32:49 +0000

    regular furniture simple increase bath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
