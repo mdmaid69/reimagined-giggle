f6db88678802af8b3b0a755629274a65861e59de
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:20:16 +0000

    match tone Cc closely above

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_work(force, distance):
-        return force * distance
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
