d4411f1021fd2cbcdbaba531bcb273f287ba5dd1
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:12:02 +0000

    flow perfect better shut rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
