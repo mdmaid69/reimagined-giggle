043499c14cd93f8e7b13f4fd88c321615be215f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:49:46 +0000

    answer criticize office contrast comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
