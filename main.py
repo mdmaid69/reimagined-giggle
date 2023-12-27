dd5be06e4249b1c72964a699029df43919d363cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:36:13 +0000

    cook command near their scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
