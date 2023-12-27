6f085129a82e4b6b24bc36f237375a6a683d5e91
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:00:19 +0000

    learn inquire up case can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
