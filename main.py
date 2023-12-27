8928c13353ae95a784272bcec5ea700b8d97dda0
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:44:37 +0000

    getoff sort badly neck lucky

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
