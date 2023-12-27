a7ffb006352c4129e42e3a9815d910d09e2c1625
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:17:07 +0000

    score future office welcome sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
