576fcb6045531828fb8afa25db126de0af0b6c82
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:59:22 +0000

    guide fasten yes mass print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
