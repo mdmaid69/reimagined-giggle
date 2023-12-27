9a99d59bf1de7aec6deafa4e99c5cd7dc9b3bb81
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:59:46 +0000

    strong convince reasonable open search

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+for i in range(10): print(i)
+def is_odd(n):
+        return n % 2 != 0
