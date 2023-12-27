e994b8433ef1aa253300d9d88bc886d8947bf636
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:10:08 +0000

    certain breath detail smell win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
