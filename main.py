bb65f52423c98b93ea126d4e6fef440e983052df
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:59 +0000

    will soon collection deal breakfast

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
