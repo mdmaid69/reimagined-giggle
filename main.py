5e499429b83ed4b3540f6406a76ab9c8995de1f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:59:23 +0000

    region sail apparently grand lookafter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def is_odd(n):
+        return n % 2 != 0
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
