578bcfee6de079a211c9d67b15f571215e6f6913
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:32:42 +0000

    decision container relationship somehow soak

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
