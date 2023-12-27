cd78b4addeecc031c2a4fb7123eb8e704c3de2ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:33:41 +0000

    thumb holdup month official guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
