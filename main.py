2824cf7e30d16cdbbed02a90d0d4c6d29801bcea
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:38:42 +0000

    board choice glad feature offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
