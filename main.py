35f9150f4c7f4c86e4d533d43aab2079b62e9479
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:29:00 +0000

    decide formal borrow doubt fashion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
