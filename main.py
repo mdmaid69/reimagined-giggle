37d411aef92407572619af402939f933853c70c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:11:08 +0000

    react possible lose rough thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import time
-print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
