fa801cf3af51d7a34a41411be70f58401243079e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:39:59 +0000

    rather complete gift runover rise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
