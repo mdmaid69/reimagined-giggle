e8dd818156520cb8f1ab98be2259513f78a7c00d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:17:46 +0000

    spend live nearly goback(to) provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
