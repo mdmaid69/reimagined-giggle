c23f3829700a9039262b4e8114a0930f078af869
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:52:46 +0000

    half tomorrow bet message keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
