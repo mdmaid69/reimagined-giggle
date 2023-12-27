ff7c5169261d060e63c4bf147c736a047ee86d7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:22:22 +0000

    comment downstairs any bringup tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
