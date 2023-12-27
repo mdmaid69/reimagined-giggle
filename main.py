3a3428c6c608d178d2e2abdd0a6821c0639369d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:44:24 +0000

    feel brain block big seriously

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
