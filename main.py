a9995ba44b87e069f858864c70b703a500b0d044
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:37:47 +0000

    during careful objection reply tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def count_elements(lst):
+        return len(lst)
