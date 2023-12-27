d5113afcd73513580f9a6895735bf50fb4881565
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:15:20 +0000

    wide spill plenty glad just

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
