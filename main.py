f2d072637740a903b1ab6d94e12f35ace4280952
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:26:12 +0000

    check balance lean funny walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
