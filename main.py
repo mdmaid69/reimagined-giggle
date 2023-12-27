827b383e2d7a28374549e1cdd34e5025a594ccc6
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:44:39 +0000

    pump discussion effect pinch favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
