bbf820eeeb34c368b3b528dc6a1576b92a47b593
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:12:53 +0000

    profit mistake range switch shape

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
