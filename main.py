be08249b914bb41b211b1b61865990dc731036e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:35:25 +0000

    reason experiment tradition frequent invitation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
