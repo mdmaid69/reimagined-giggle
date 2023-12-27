0f31715eaab15f7cdb752f0a47f4d1279f0929ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:26:42 +0000

    narrow bank anything advice position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
