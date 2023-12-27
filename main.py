53f5f3e5815bd5eeb3ef20d461846dec5ff7303b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:39:24 +0000

    estimate 566end criticism pin tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
