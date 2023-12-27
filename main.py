80da13aab3be90f5fb12f1a72c89a4c8e57133d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:09:05 +0000

    exit marry watch opportunity unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def print_python_version():
+        print(sys.version)
