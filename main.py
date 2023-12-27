64b7b4f2d219a2a19962af5875a8ac84977a8bd8
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:02:18 +0000

    night bringback interested like touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_python_version():
+        return platform.python_version()
