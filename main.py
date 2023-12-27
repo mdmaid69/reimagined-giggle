8e508ca863fe31d026f543ccb85bdd85c0910933
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:56:39 +0000

    split trial somewhere false accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
