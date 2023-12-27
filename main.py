0892441d1ce067fc481b53b47b89782c25de7909
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:12:19 +0000

    discover geton dream breakinto splash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
