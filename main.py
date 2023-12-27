0db814d279c67767b9a76fbbe8e8a428bb8581d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:04:39 +0000

    close appeal rush bar from

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def add_numbers(x, y):
-        return x + y
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
