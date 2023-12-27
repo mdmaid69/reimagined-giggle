27eddb7a8d757bf6fbc7863162b1bd1f89c055e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:37:12 +0000

    uncertain poison of Hh different

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
