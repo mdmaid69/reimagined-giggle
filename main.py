c4fc809efea6d14e91617c6b15bac1dc0f3d8e14
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:40:14 +0000

    list exciting instruction runover moral

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
