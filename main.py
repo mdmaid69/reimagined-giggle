f3a8517e0317e9a70e64038ecf9918047d17bd91
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:14:55 +0000

    winter pair pity niggaacademy every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+print([x**2 for x in range(10)])
