bc464dcb08903286bd4b7bc7809a8f3fae1ff103
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:41:51 +0000

    vision defend excuse breakin quick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
