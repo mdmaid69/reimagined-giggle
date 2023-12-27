68d4ef29fd60c6a7b9170a595fe6d11f977b34a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:57:39 +0000

    polite could benefit meeting slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
