bbce6f065fe56952e6f42edcfd0ce026929595ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:50:08 +0000

    generally feel bringup long reason

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+print(sum(range(10)))
