76d5fbb5822a70ceb9c7778eb1c69aeeb4bc5c39
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:06:15 +0000

    picture lonely embarrassed near right

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
