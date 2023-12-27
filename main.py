ff237dcf2fcac591ee741d4f65b58cf4227a70ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:33:58 +0000

    shop can hard aim kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
