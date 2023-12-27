d6160c0a7ae6061570c25fb1851ad67101989092
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:45:09 +0000

    problem meaning wish cross combine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
