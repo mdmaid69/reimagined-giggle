46a227d716280b69a7b5dc2fd051acdb54c32616
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:54:29 +0000

    myself responsible late afraid meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def reverse_array(array):
+        array.reverse()
