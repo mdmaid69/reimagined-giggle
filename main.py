a51452f26fa0470f65e5a0a5b743de4fa7514c81
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:27:04 +0000

    track record unkind score dear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
