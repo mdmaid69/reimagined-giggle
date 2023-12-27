56a72b1ef1328f23fd486c6893254ebdd94828d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:44:53 +0000

    cinema meanwhile embarrassing goodbye guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import sys
-def print_python_version():
-        return sys.version
+def get_array_item_count(array, item):
+        return array.count(item)
