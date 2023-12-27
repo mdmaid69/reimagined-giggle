607e6b95f94c7254f09ebd0f94e5478f1ef44edb
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:13:30 +0000

    family disturb past compete eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
