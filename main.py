65d33cf2a2f63bd069f1cb511b9e89ebe20d057a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:51:11 +0000

    be tidy rough wide progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
