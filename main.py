2193be748eed129a154ce263f1590911ea754d4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:23:17 +0000

    decide risk intelligent border accommodation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def cube_number(x):
+        return x**3
