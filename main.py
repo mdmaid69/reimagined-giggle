f04a31ed77e5ce01a04247baaaf117be6bba1627
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:01:26 +0000

    carry process contrast rescue sing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print("Hello, world!")
