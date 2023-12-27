dddd9527b388d7dde6d9fec3b6117b0cca600d2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:48:35 +0000

    unpleasant stick basically reply deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
