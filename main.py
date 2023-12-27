20ddc33f10c8ba4056e4dfdb4cab20113390de71
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:49:45 +0000

    loose behind rich birthday autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
