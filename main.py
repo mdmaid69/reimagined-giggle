7283edeb6af6bb514a6a71ee640c199f3dece59f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:42:51 +0000

    still dark illness stand everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
