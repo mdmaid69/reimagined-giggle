8e22981e552fb805bfef7eeae8049d5df54a11ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:31:06 +0000

    somebody expensive earth price fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_typecode(array):
+        return array.typecode
