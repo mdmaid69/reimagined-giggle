0c5a68d430d8e60f70e9f9eda72887194f7c52ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:19:31 +0000

    perfect sister through spin fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
