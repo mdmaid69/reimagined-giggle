c7312a21211dd418e072555722268bbd56f363bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:50:03 +0000

    suckme history press rely release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
