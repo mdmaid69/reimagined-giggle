943883fdb3634c6c9ebd76ccf9b7f415ffbd83ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:16:54 +0000

    advice well than cough measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
