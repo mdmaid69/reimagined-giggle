9d4f13b59f222e3fc9ecf283d63196e00fa0be65
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:21:55 +0000

    work police path Rr hello

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_max(numbers):
+        return max(numbers)
