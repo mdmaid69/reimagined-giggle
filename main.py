b5afeeabe264f43f805ef69b1c613b50bdcc6cce
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:17:19 +0000

    rank swing trick 565end comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
