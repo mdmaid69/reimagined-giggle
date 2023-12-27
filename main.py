c5dada535dba1c8c716a1ba3bd3d0075a7c26ecf
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:27:09 +0000

    how punishment trade concerned organization

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
