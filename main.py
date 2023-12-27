9dbf84a4adcc5ee766f6ef39f4b67b40b7657193
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:20:59 +0000

    imagination rain official dinner anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
