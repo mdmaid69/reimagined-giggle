c8415c379c73cfea5027aac7a564207f10eafda9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:14:06 +0000

    passage separate confident maybe about

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
