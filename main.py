03abd2bbd3c7b4f4c08c9893a4d02458e4d78ea5
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:27:11 +0000

    Itself face mark drop play

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
