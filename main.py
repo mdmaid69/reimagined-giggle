1e2c96632cc787a255da0afb3f2ef6d917976e1a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:31:50 +0000

    drawing thin discuss across least

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
