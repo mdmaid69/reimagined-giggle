d61d3027df03a107b6c3ff147629bed391a8ce55
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:53:52 +0000

    relax type grow sharp weekend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
