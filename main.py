c06f9e21456671012fcb9d69b7d3e574dd5f056c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:24:52 +0000

    news climb shrink practice worth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
