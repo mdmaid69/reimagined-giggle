3d814cfac7290fc570cb109b04dbb2ad8b635bc6
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:38:42 +0000

    genuine deal several constantly sorry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def reverse_array(array):
+        array.reverse()
