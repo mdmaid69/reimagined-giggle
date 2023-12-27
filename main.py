26d251c47d8add71f3bb60c5e7bbed7ff2201d80
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:12:03 +0000

    goout careless defend number begin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import collections
-def create_user_string():
-        return collections.UserString()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
