944bb3ca605dc74111f3ab476cd521342650ce6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:15:49 +0000

    spread even promise action settle

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-print([x**2 for x in range(10)])
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+print("Hello, world!")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
