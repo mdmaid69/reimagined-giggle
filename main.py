21a77bf8e786464c990baa806191e6e481660149
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:44:08 +0000

    so discovery under port rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
