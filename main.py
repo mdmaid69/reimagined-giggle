61cd3b37166bb9edefc504460ee3443607af1588
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:24:51 +0000

    lookaround comeup improvement fall away

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def clear_array(array):
-        array *= 0
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def find_union(list1, list2):
+        return set(list1) | set(list2)
