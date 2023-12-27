42e3844d1dbca8614611a9332cc312d225b579f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:35:21 +0000

    pure spread consult in satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  def reverse_list(lst):
+        return lst[::-1]
