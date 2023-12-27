13c66465b7fd29dd1ff5cbb5235420b5812e3212
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:17:52 +0000

    doubt be embarrassing memory touch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
