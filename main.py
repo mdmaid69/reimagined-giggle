712ab1ea63ef2106de09a60841cdb2a7086ba457
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:39:12 +0000

    practical drawing form live turnout

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_speed(distance, time):
+        return distance / time
+print(sum(range(10)))
