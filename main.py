dab9d1405d7ccdbae54c36b1660be0fcacfee42e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:30:13 +0000

    stage unusual everyone pin spare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
