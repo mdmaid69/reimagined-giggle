6f67d9053ba24a16203fc83c7c00b4feaf15e0aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:26:13 +0000

    back mention wise gap refuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+for i in range(10): print(i)
