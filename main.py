f28ddf3fc00533f9c1af104844a04f9052faa308
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:41:22 +0000

    address deep centimetre stop impression

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
