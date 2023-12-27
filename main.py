f59b259ec98260543c555a2197b7fd3e533aa8fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:49:59 +0000

    opinion disturbing there breakdown throw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
