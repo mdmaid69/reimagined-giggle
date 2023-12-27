8c3f65d82c2223ba2805543a219370cce4ded5b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:56:29 +0000

    fear exaggerate flow keepfrom patient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
