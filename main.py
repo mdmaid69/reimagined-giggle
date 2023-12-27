820f87be113ea679f466c50de329f9ca98ca4a0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:28:12 +0000

    operate considering fat valuable Bb

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def square_number(x):
+        return x**2
