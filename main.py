0bc01c45ac8aed26d733e548af91f78b12000934
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:14:39 +0000

    help arrangement around fit notice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_floor(x):
+        return math.floor(x)
