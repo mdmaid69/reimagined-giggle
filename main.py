c5c375788bfcb76e694105e62f88b42db34bf4c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:34:43 +0000

    example artificial consist waste should

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def divide_numbers(x, y):
-        return x / y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+name = "Python"
+print("Hello,", name)
