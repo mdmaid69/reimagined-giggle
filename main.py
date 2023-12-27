8b58f80ea98d8610751a2a24afc3a3b763889e9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:14:55 +0000

    confuse tradition future snow original

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
