d8bff02d92e34165947455205f30060cc1f104f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:36:41 +0000

    guide whole dance Zz avoid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def cube_number(x):
-        return x**3
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def is_odd(n):
+        return n % 2 != 0
