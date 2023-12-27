bd39f73bbffced0a4097a9676f8e65e5f1194c55
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:00:21 +0000

    improve strength gate cook present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
