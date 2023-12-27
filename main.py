f4f29817ee9742b7ff5136aaec92bfc894ea41cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:02:36 +0000

    slightly try lock repair ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
