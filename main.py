9e5bb3f8dcaf2d8fd70d8a2b92836fcbb69551c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:41:32 +0000

    danger store sure material worried

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
