464ef27719c39707eb38eff85da5f01766cb3ecf
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:10:44 +0000

    introduce recover swimming free anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
