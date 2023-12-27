7a344071329938ef80860ca63422b4443bfae58a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:52:25 +0000

    fall ability during convinced neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
