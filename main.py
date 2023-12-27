ff779ffe72adbb513278db127e6dee93f787854e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:46:20 +0000

    comeout to argue fail getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
