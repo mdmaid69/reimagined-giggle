c37c61df0ce2546a6dab47450759d85e45db3961
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:29:46 +0000

    straight list alone petrol winter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
