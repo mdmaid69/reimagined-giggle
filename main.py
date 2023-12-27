5e7453ac2eac5be1544fc3dcea86061845d1b9db
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:00:02 +0000

    offer Gg learn tax account

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
