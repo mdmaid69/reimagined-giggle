ac8d920a14ec9f0c32c22d0f04a1f4272f5d8622
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:40:20 +0000

    ourselves over bin check tooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
