7521c49b1f3c7ffa96d299ff255aac9439cbec41
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:11:21 +0000

    fear spin damp embarrassing classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def square_number(x):
+        return x**2
