48625dcc99c31b7deb702ac8836a974a31a8a540
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:14:29 +0000

    under ill soon right mind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def find_union(list1, list2):
+        return set(list1) | set(list2)
