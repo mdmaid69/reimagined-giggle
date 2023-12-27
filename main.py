f8ee14915c67842d9555ae9979e9f645f0739c2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:12:52 +0000

    noisy brave 569enjoy lend some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
