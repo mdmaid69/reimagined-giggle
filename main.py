2b7920234506712dac8d2feaa12031790fba19fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:50:39 +0000

    else draw average question community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
