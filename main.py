c3f86afee6b576d707a185b79a1e0d9a12e07e48
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:18:20 +0000

    colour pretty collection finish connected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def reverse_array(array):
+        array.reverse()
