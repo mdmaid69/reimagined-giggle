2801a667b714dcaba4830e971d23849be199dd9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:54:09 +0000

    day culture afraid ready wish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  def subtract_numbers(x, y):
+        return x - y
