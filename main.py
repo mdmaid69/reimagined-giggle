9f5eae228ba253cf9d8baf04285514a33c5e5642
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:45:23 +0000

    attempt yesterday bet factory fortunate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import json
+print(json.dumps({"name": "John", "age": 30}))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
