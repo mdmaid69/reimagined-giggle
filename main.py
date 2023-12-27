5625ca0919aab6c99a4febe6903763cb86d2ff70
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:33:44 +0000

    reduce stir answer over sleep

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
