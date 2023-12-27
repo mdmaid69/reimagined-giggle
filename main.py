8bb5fa3e07e9b57f5c670b39c59964213cdc396b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:22:19 +0000

    straight error neck basis matter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
