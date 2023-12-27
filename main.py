a594cc03fd11b67dce58db4d0959286155338fde
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:04:19 +0000

    tape difficult around criticize judge

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
