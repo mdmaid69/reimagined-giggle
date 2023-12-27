346f38f50ecb95f9aa097eef80ed2b9da7a555eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:43:32 +0000

    alone word rain around use

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
