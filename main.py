c73b5c0d4f557e917192259bc12917d1df7eec04
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:04:52 +0000

    fail tidy satisfactory probably knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
