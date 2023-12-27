6235747ead7c291455fab35e07adcd9df6140e19
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:43:25 +0000

    measure behind secret love Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+print(math.pi)
