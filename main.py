0240095dc803353b61ccff562d4d387f1f747600
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:52:27 +0000

    shall finally everybody tongue offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
