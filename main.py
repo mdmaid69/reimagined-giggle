54f985a3c8debcee1d81f710430da8dc8c09f62e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:45:22 +0000

    feeling run swallow warn free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
