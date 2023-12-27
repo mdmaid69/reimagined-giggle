235bd59f5cd86233986495f2e6e16602f7928ece
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:58:45 +0000

    himself wrong reaction unpleasant both

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
