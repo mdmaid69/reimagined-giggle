2eb8d1128ae540f7b60a3d175513f0520e925db9
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:11:03 +0000

    rather otherwise name marry story

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
