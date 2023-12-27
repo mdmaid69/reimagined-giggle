d077022ef9f254ac99efedd3453c3af277c95363
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:33:10 +0000

    tear father neither keepoff hardly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
