9bc7f24fc5930b8949fff18f9941efadb5ada618
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:31 +0000

    grateful objection ashamed polite formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def cube_number(x):
+        return x**3
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
