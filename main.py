7f06d14d40acf5b78357152401cc7df8280a5312
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:11:39 +0000

    mark mysterious throw fight guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_int(array):
+        return int(array[0])
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
