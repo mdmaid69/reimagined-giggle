3450993a2eadcc0e734a05cb4bb8188ece143476
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:35:54 +0000

    brief mean comeup against proper

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
   def get_directory_name(path):
         return os.path.dirname(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
