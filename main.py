8d4c96399e2e480ac1dfced8b83ac50deb099dd7
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:00:22 +0000

    square phone along please story

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
