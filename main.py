acd700899f9714c745f051906b4536d625261aa8
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:14:01 +0000

    task getaway arrangement represent office

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
