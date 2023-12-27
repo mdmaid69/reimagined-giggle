f772e78a66f93daf6dc4e317d7f33ac98383110a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:22:37 +0000

    turnon mother gradually human sail

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
