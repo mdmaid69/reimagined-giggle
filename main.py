8f881e8ac6179e0ef83d9098143f15133f972aea
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:18:53 +0000

    surface bet in less form

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+from collections import Counter
+print(Counter("hello world"))
+def reverse_string(s):
+        return s[::-1]
