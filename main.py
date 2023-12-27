8bc0c251e8b1d87f2bd42049058487f3e6543496
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:57:13 +0000

    can favour behind tonight lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
