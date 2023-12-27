6d4e92f705c5c2506e21829e883bc5f71beabab1
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:39:21 +0000

    lost harm plenty cost range

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_future_value(principal, rate, time):
+def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
