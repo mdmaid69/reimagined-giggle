6dd2bb01598985be48c05c838a2c9ccd5b294c12
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:44:25 +0000

    protect suggestion without interrupt wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
