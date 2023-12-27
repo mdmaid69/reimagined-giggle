ced443dcf627a39270e921abbf83de885e92dab1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:57:41 +0000

    pupil summer operation often phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
