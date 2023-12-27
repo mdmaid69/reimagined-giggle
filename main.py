f66d9606f6c4136dec56866e862cabc833f87289
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:38:46 +0000

    comeoff set quick uniform float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
