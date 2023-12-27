7de47063bf459987a08d5759cbe7080ea229ba93
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:05:45 +0000

    unable remove realize extra nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
