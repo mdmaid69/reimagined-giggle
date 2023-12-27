4465f7f109c44bbe47fc9e43d802cda98404fc2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:50:18 +0000

    earn leg prepare idea talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
