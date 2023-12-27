80167842cf5ebb78607601eea2dc3f7481c9d127
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:41:08 +0000

    wise shallow possible exact ago

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
