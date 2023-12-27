58b650f5f10711886505c0df05c4be4f78d154d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:28 +0000

    possibly aim 179bite position order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_max(numbers):
+        return max(numbers)
