95eb165f6a03b283d3af19d8d75cb34ddfcdbb33
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:32:13 +0000

    mean finish joke surprise so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+print(datetime.datetime.now())
