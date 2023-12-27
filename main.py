d8f26f9f678cfe71078d12f64ef90f048878cc9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:25:01 +0000

    waste preparation anything experiment taste

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
