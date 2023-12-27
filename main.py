3f3d59bf5a8ba4cf1217c86fde19a0fb6621da62
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:25:18 +0000

    talk actually relative post unfair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
