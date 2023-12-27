8dcb5a2b9a1c1e6d3fef5a324d86ca44e3541dd7
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:46:30 +0000

    female onto firm chief describe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def count_elements(lst):
-        return len(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
