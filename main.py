0d1f89fb603e8b3e44b3b9ac31b49e68e21fbaff
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:22:51 +0000

    break picture arrive comeover while

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
