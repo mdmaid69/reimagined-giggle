dbcf01fc46e7cd24665f7835984114e66ee33683
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:37:35 +0000

    goodbye bill good spring round

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
