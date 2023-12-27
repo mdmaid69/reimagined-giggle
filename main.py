7e27abf8e0426f6ac8422c89fb370389a398968e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:53:13 +0000

    though impressive accident wait rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def sort_list(lst):
+        return sorted(lst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
