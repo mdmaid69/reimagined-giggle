9e760ac7f8c7921d7aebf726c581a253749f7c9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:26:56 +0000

    whatever then wash die willing

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def subtract_numbers(x, y):
+        return x - y
