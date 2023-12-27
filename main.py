84ccde33eb6e05bc7efd7c0f3b3e51ccd2cdc371
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:39:52 +0000

    plain precise justice priority exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
