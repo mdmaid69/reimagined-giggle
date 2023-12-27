b2265827199c9474774e92885324b16fe3de0937
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:46:01 +0000

    method theirs house provide though

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
