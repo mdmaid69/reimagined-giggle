abc428457692a6a9d81c4b43f67a14eefb475290
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:51:35 +0000

    achievement lead floor backwards content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
