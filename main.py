28e05815b3481f8a786d04e1ce6c418a1e84c848
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:39:24 +0000

    autumn question noise present way

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
