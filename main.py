66c5bff9fe631257ada0c85809da4fd43b9376c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:52:46 +0000

    available possession good uncertain stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def is_odd(n):
-        return n % 2 != 0
+  def get_base_name(path):
+        return os.path.basename(path)
