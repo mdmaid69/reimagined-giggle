1f4e356f71f3527fd36f11cca95a9ed95b444e67
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:42:46 +0000

    comefrom pile popular vanish early

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_rectangle(l, w):
+        return l * w
