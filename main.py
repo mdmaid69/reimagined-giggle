2a1be0c04e05256d78fc8e72c0254bbd791d4122
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:32:12 +0000

    private wish colour meaning rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
