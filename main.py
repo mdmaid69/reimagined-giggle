393866d824d88348fd70781510cf7b48e649d31a
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:39:37 +0000

    beat force odd language here

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
