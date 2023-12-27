9ebda3c38e8bd62e32b08e3b6b353c92003c8309
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:36:43 +0000

    taste fear spring bury himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def is_odd(n):
-        return n % 2 != 0
+def convert_array_to_list(array):
+        return array.tolist()
