03c483f9ddfce1ae7fe34ee2b7bcaa422fb7e21b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:20:17 +0000

    proper judge trouble callup chin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def convert_to_octal(n):
+        return oct(n)
