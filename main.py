1bfa74a59d95f924a1a4265e1f11383e99aea807
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:09:45 +0000

    although effect desperate coat wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
