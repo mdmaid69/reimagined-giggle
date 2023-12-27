71e7683322b6d82c268eddd7947b3290f06fef43
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:57:01 +0000

    chief while responsibility worse nerve

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
