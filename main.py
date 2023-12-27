429d067bfef7ab424f3615f87d363b28e0dc4689
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:56:11 +0000

    matter into long talk giveback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(lst):
-        return min(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
