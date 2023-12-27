86e599c1a33dc122f4851af817483f55ddf5cfc6
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:33:35 +0000

    demonstrate comment fast tape impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
