fa04a44dbada9db535bcf2feaf1173b2de1d17b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:15:33 +0000

    entrance top difficulty border mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
