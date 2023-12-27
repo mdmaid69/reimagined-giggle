fcc25d475378d3cdf12b2340a78f5f91648a0d44
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:25:39 +0000

    path near free force afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def add_numbers(a, b):
+        return a + b
