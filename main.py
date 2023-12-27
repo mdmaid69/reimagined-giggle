5f5c839ff393f18e52efc8ce0179e5936c595167
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:53:58 +0000

    tall concentrate uncomfortable man fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
