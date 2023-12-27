7887df93d11da732453e92e09fff717e1dff01c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:30:03 +0000

    interruption later section mention fit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def count_elements(lst):
-        return len(lst)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
