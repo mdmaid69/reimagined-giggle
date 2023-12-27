bd8a01d896624e284a56eaf98efd035aad7b3b13
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:20:05 +0000

    love march birth smoke exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
