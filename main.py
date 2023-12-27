1ff372746e528456dc3865f789c9c1c61d229d9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:13:22 +0000

    child getback shape shop out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
