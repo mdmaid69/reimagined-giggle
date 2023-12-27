1b9681bc6945db66e64dde295ed47c91c3c86a1f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:56:41 +0000

    theirs modern suggest perfect refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def convert_to_hex(n):
+        return hex(n)
