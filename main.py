08c84491eb7d5bd310db107a2cf011e6ead05d60
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:32:22 +0000

    number cum culture successful do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def sort_list(lst):
+        return sorted(lst)
