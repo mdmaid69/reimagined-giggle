9dd370430a9d4dca6baae306f2b82e938aeebc6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:16:44 +0000

    human our lock however complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
