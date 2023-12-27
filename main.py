227440532da0be8f377131802f01c071863ee8f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:24:35 +0000

    entrance slice catch joke bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
