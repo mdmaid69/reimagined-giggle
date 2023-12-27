3da4a5165251b5198bd91bac0897280e8180700d
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:28:21 +0000

    peaceful ball beside command eat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def is_odd(n):
-        return n % 2 != 0
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_bool(array):
+        return bool(array)
