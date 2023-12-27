0e4ce2a541183a4b3d8f2de0696933332472332e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:33:10 +0000

    hurry shout autumn common strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
