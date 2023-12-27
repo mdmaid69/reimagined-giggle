8108c3b2902c339409e8bd210c2a8b1e8f6586b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:26:12 +0000

    indeed understand rule compete might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
