afb46bdc5fec8f5cb71d124aff0cd6872361de35
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:21:00 +0000

    drink rain uncomfortable plain brown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
