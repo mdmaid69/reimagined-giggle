ee7d0cada9d2041ef1a5543f8c2d9a13a74e365f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:13:18 +0000

    damp left one underneath ask

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
