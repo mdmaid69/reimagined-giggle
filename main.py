72f0c5805dd2603986e5e09b68a9cdc467f22a9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:01:57 +0000

    crowd less bringdown suppose behave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_str(array):
-        return str(array)
+import numpy as np
+print(np.array([1, 2, 3]))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
