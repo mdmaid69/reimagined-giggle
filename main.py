9fa3c7baaa9191b3979b911bb90d32a9a4e9d40b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:24:09 +0000

    share crash then conclusion surface

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-name = "Python"
-print("Hello,", name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
