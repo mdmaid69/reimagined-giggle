70ba20b02300408c08f654eb57c160422f24163e
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:25:43 +0000

    school show escape remain confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
