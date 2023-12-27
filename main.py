eaa58dc359fde64fe7343330e6635f0ec1401cfc
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:22:56 +0000

    force decision feel clue myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
