83abcfa068ccd5d04c9e75d280319b1803b797b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:45:20 +0000

    no bend powerful display bag

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def find_min(numbers):
+        return min(numbers)
+  def remove_duplicates(lst):
+        return list(set(lst))
