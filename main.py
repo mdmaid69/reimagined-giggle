2eb84282c4a00fcfc1e9bdea5382c6ac016c0c01
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:12:31 +0000

    last unusual criticism embarrassing get

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
