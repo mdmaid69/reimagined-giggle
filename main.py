aac1924bda62d995618f23e0c2c8afbe0d058253
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:34:55 +0000

    dress silent warning comeout careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
