c79400e1337b643c8b86e8f490241c75793daa2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:02:53 +0000

    refer funny feed variety public

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-print(sum(range(10)))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
