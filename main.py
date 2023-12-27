b66fa1e4c34e8536f288942f5386fcc9422b1d92
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:37:18 +0000

    hard computer mention confused priority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def divide_numbers(x, y):
+        return x / y
+def calculate_perpetuity(payment, rate):
+        return payment / rate
