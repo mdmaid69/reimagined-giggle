a57bcb701ec25c88a7c6453a6e8ca05611f88658
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:38:38 +0000

    suppose distance train Cc past

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
