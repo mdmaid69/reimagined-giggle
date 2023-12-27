fae875b61fd0a440650bd4f87dc7b0dce0224ab5
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:36:20 +0000

    bill major film cough produce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
