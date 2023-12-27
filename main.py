48c7a417c9c1f0ef41e2f042db875aab94cabb2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:26:18 +0000

    attempt fear television dare fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
