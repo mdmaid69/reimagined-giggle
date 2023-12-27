fa62ab8beab626faaf67ce07c10a553a51e3fa35
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:45:22 +0000

    wind star relationship 570enjoyable turnaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
