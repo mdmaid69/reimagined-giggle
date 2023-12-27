c8add4f9424019049beabf6f0b863c12249db5ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:46:10 +0000

    birth bother instead scream artificial

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+i = 0
+while i < 5:
+        print(i)
+        i += 1
