e9d6ca9c8319ab74f718d4692ba1a8e3fd1b1c99
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:22:26 +0000

    cheat turnon takeout treatment regular

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
