773a9d63f95f5c608e289f5bafcfa0cc42c1e59c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:33:40 +0000

    then draw inquire look common

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def square_number(x):
+        return x**2
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
