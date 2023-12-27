14e3131148c9d3215e445f0af9affc96dfcfbc4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:11:31 +0000

    frighten temperature wind noise giveaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-print(math.pi)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
