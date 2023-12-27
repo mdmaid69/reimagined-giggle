6879f1a2645823e83b36c4d46cc52e1b7415b64c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:17:26 +0000

    instead Qq pity curious unusual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
