0bb7447bf811c89431f500c3d80f43272c54cbeb
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:49:56 +0000

    pass suckme know land its

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print("Hello, world!")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
