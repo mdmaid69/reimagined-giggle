163564d750073f667fc7a7624766baf48a699d03
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:06:59 +0000

    check hang mouth successful talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
