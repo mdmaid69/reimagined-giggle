14e9a17fc4073e5069053f4a0a65173795d83ec6
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:06:22 +0000

    taste file name unfair confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
