e5de5a6f7c62ee84a6a660fe5f18efefe85f85a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:57:07 +0000

    will practical Bb born married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
