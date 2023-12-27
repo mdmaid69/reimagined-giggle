e7efb5d235ffaa575d9ef0052351ba38e7541cbc
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:26:52 +0000

    shelter breathe Ww strict recovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
