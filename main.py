a1842d08792f0bd9ebeaeffbf19beaa503a2a99e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:53:10 +0000

    approval mad country rush normal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
