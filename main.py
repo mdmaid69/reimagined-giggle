05b8a6fbe87dabe0995a5a10293f9b469d30d0bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:37:08 +0000

    juice perfect hot equal fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_area(radius):
+        return 3.14 * radius * radius
