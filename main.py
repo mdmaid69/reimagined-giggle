b46ddfc86e9f610aea3c5c4495ed9531153390f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:24:43 +0000

    change classroom worth wealth exploration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
