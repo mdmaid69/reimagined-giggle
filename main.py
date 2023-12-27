459f520dd689a4a64aa7079b607e0720811861d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:14:28 +0000

    advise gather spread pause quiet

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
