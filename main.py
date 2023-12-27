909318802e9d4e8d01c71073270db69ee537a3bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:02:42 +0000

    you fast charge winter eager

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+for i in range(10): print(i)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
