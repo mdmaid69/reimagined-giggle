82d7df761439e7a87bc8463194989130974f2122
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:24 +0000

    responsible twist port treat properly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import datetime
+print(datetime.datetime.now())
