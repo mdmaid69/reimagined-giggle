38b2512d3964fa2ced8f50c00fe9e1ba3446103b
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:14:31 +0000

    goaway cinema remark phone term

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
