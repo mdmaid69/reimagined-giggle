e47ff88895174e877b57ce233a49c484e1811e8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:55:50 +0000

    difference stare bag rubbish means

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
