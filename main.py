d80e6ce9752e284597e1ad71ee9d7473c582eaf3
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:20:54 +0000

    really hard check reduction picture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
