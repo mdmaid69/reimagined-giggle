518da3e16febe3ec0f6cc93393609cc9572f2bce
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:02:32 +0000

    wild moment forget either float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_item(array, i):
+        return array[i]
