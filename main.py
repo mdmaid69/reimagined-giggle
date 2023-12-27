5529c61f32e4d76e05e5cb0fbb39eeaf23788819
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:38:10 +0000

    owe content well dear calmly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
