358e1011569fc60220ceab6668136da8cf5cc7d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:42:01 +0000

    yourself wash use wish before

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def convert_to_hex(n):
+        return hex(n)
