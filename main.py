1c0165d1857a537a3ec48dfbd5d2327563c46d1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:59:01 +0000

    approval downstairs second destroy mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
