5a9e1d5f6c19296ce0fd58763b12b78eca352d26
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:43:14 +0000

    period correct jump expression different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
