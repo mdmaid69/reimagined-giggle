ee2af4de146ba3e42d9e2dbc6fe417a89598c49e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:04:09 +0000

    unite reason charge damp getout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
