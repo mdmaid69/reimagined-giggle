af449274a899301ff224fed66576e43eed1b3c7f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:19:01 +0000

    interest introduction standard seriously chin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def sort_list(lst):
+        return sorted(lst)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
