b37883d6bd2b07e70f3da4a4fec0b5cdbe14d245
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:50:43 +0000

    married whistle breath area study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
