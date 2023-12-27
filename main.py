5d25be5c56877f90ce42c4d6d58f8cd21d65a690
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:51:13 +0000

    position slice circle challenge food

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def find_min(lst):
-        return min(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
