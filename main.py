2020beb31caed325d567275fe3eb3b8745129a31
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:23:57 +0000

    harm none secret next mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_item_count(array, item):
+        return array.count(item)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
