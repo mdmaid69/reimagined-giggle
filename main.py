1d3dc3ea8d401ccfeeac925ff2bd7bf2be298fa4
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:55:35 +0000

    exploration properly fast accident commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
