37dd6efb82fa33633712c6eb7bc69a8bd4c6edd1
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:24:55 +0000

    progress sensible point convinced better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
