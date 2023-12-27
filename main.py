0d858ff251eec739e560b25d4fe3855ec8653f89
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:49:21 +0000

    doubt shopping copy attempt bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
