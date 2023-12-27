aafbb45dedf22f7ea605db2f77683ad7b662c88f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:23:23 +0000

    practical defeat serve fail balance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_dict():
+        return collections.UserDict()
