0c6a3d4fb192107296618ae290b4e3a58f80a7fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:29:47 +0000

    talkinto pattern married copy lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
