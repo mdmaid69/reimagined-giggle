5acd7dc8f9f202c2d522bea87324647f1b6e2a41
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:30:20 +0000

    bottom whoever love happen black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def add_numbers(a, b):
+        return a + b
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
