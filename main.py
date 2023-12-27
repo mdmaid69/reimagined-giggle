ddcd4d18477318110fe10de019812e2dd2a85219
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:41:39 +0000

    wrap contents announce game tidy

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
