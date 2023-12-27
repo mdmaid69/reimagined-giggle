a8f595f9c6f57e19b7319554100338a6c4570168
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:21:23 +0000

    train tread nothing stop capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
