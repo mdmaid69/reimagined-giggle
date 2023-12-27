de5ddcbc17716e390ed4c087ce02f41184f093e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:30:33 +0000

    laugh hard press control slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
