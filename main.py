7df7a9127e79f0038a62c6f2b5e700d2e6f40992
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:53:45 +0000

    whether stamp everybody use emotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
