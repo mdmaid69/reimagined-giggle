02f54603b86e76ea283faf90b64bdb8b34087b00
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:28:16 +0000

    her bound answer breakinto interfere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def add_numbers(x, y):
+        return x + y
+import sys
+def exit_program():
+        sys.exit()
