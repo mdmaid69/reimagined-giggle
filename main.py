1861e276675bc7e975e352a4e2750d9e9b82fa0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:40:33 +0000

    order holdon bright foreign run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
+import re
+def find_pattern(pattern, string):
         return re.findall(pattern, string)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
