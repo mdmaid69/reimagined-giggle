73b710bfe933d8b073a7dfc3bf4acc45c81071fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:16:30 +0000

    talk switch welcome open plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import getpass
-def get_username():
-        return getpass.getuser()
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
