d8142db1872642d584b2eec3286d709b3cd24582
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:26:51 +0000

    believe bunch challenge opposite aim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
