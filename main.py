646a861474e68e7d4b8dbea93a88e4f9319d547b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:34:04 +0000

    getout past also notice square

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
