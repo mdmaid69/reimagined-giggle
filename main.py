c7af42775f8fa227ee9750b218748041a4d9b237
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:50:32 +0000

    check body way recognize everything

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_pressure(force, area):
+        return force / area
