5513061663187d2457b411b0cbcfa4a06df2fa75
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:19:19 +0000

    face operate environment oil clear

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def is_even(n):
+        return n % 2 == 0
