bbca911b50560c201b4417a5d53d55159087db82
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:46:39 +0000

    consist row stretch image tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
