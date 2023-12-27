55f326b56870ff45e624cace6131d7dd313ddb6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:36:35 +0000

    bring far fight admit frightened

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def split_path(path):
+        return os.path.split(path)
