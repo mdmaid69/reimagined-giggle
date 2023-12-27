4709d94f783b13d1ab6f79a2a64cc33aaa0f8524
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:05:46 +0000

    shock undo shop present wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Characters:", len(text))
+def find_max(lst):
+        return max(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
