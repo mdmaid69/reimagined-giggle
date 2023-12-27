e7e8bcc73d3a4cff5f8fffe4db7d9c85c487dadd
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:05:25 +0000

    house drink comeoff failure polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
