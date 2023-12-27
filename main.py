58413644c0bd141878d00d90f74daa25ca8b3603
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:49:17 +0000

    takeoff boat appearance scheme suffer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
