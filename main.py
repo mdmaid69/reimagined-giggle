14421d3bed6aaad842632e06e2e36e62a37ae6fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:05:00 +0000

    lean indeed tell direct scale

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
