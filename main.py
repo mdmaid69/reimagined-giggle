7524570882cd0580083eedd8b90f7bb2d603bf4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:35:11 +0000

    bet smoke compare possibly shoulder

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
