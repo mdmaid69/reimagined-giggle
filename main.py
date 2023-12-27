edbd2703e0e89a08dd255926098c9ba87b20506c
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:13:50 +0000

    confident minute lay punishment contain

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
