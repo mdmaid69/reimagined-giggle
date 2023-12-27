6f65a5f34bf50cfff3ad7a786de1086d140d902c
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:17:06 +0000

    comeoff she hungry why lead

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
