769895a7e68cc66949e7c3ebb339e896053d15a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:22:26 +0000

    find achievement of careless treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
