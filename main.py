c783bcca43164c3f4a9beb0e3b4563a37729e9ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:52:42 +0000

    strength operate neat exact regret

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_speed(distance, time):
-        return distance / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
