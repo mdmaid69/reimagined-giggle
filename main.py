a586cd37ac43b7f49eb953533120418931aa1b41
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:41:27 +0000

    characteristic each activity tax private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
