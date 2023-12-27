d1551ce8e7716781eb6ba6e3436b1f6fdeba7731
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:02:41 +0000

    decision something today round experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
