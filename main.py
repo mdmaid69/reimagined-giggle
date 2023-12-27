61c26065181040caa0726deca8c79f3c5b7e6b90
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:46:57 +0000

    lesson nature hit interview introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
