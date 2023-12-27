e74d4ca76358f2fdd073fe6ca8c4df6479abbbd2
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:40:58 +0000

    offence workon cruelty together voice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import re
-print(re.match("h.*o", "hello world"))
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
