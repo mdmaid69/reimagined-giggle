faaf338a71b157d213dade920b82f16840c25f4a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:49:30 +0000

    quarter whom paper children goout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
