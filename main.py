2683b5c8575a578c4c1bbd6a7f212cda46f47a91
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:57:21 +0000

    generally chase new wild coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
