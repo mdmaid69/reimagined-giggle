248f24d667a831d83f41fe459368700b7bafdd29
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:08:09 +0000

    guard dig sure fact complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  def cube_number(x):
-        return x**3
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
