5385670e480bd0a52c75d3b25d9307fc9c34ab55
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:57:17 +0000

    crash friend moment long your

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
