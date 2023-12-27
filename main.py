3542a91b36eed54cc24a8462572ebcd9e2e2f861
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:13:10 +0000

    move unless instant greeting blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
