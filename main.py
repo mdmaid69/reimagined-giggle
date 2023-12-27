a922292a8af20dc0c91a174f6c0e996d1d7a66ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:24:01 +0000

    hot recognize proper oil congratulations

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print([x**2 for x in range(10)])
