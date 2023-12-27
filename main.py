50cf309c0ae14ca606a119a9e848e3a505db13c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:46:02 +0000

    worth ignore promise behind rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
