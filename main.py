ac13070b8221abf4e89b671a192b7b568baaae20
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:30:59 +0000

    trip less rent bus relative

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def find_unique_words(sentence):
+        return set(sentence.split())
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
