957b4a7c15a1b2b015b5b5785206a75a65a97d12
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:11:02 +0000

    basically body fill hit confuse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
