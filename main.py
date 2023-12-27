61a941f3fbc537dc2ca81fc67ce0e66b3840ca6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:50:34 +0000

    573entertainment soft tongue scale mouth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
