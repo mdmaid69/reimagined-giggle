1a0ba8970e79978a72f3c4ef569f0c7fc41c58ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:27:12 +0000

    invite defeat freedom not temper

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def divide_numbers(x, y):
+        return x / y
+def square_number(x):
+        return x**2
