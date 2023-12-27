d66fdddbafc7efcd9a003fe702da6e858ab2a342
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:57:57 +0000

    fit alone chin history lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
