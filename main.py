69ba2b8bc699fd5e98a503a75253972e0789425b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:58:45 +0000

    minimum weak voice pleased sometimes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
