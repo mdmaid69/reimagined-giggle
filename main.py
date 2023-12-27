62deccf6a1065acd021ccb56dd79d547aa476b33
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:51:56 +0000

    second understand convince urgent report

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
