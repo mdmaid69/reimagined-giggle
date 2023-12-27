f210f022c21d1673ee3da190a06af468b5374ad2
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:33:00 +0000

    present escape it reduce repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
