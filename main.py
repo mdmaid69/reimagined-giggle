e1358df83e02da21913da8e52a6321b7db53a7a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:36:13 +0000

    central mail large lay giveback

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
