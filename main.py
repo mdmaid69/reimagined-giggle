dbd8bec22c57dc843e7572fb88f3bb285b2fd540
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:47:28 +0000

    apart thinkthrough condition suppose still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
