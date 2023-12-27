37ae93a8d2c9d26567528a97b6274b8ce55b4397
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:21:56 +0000

    degree emotion comeround comment among

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+print([x**2 for x in range(10)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
