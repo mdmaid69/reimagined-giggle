195cbb298300737ce131d97c5b66d0de0b5da0e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:20:06 +0000

    up term frequent argument clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
