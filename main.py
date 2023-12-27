8634776d889d1978a454e064d3b90ad3f29d3deb
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:47:27 +0000

    bang range performance with fortunate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
