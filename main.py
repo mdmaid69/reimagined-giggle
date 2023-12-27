6f12de3228776c7cf14f7f10d05e88b523b73c0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:19:09 +0000

    matter cook thanks guard course

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_min(numbers):
+        return min(numbers)
