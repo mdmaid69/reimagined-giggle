ce203690eee5df93fd878494ff3a998488156dab
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:14:35 +0000

    callout breakfast pour debt wall

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
