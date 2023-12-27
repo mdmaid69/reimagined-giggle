91ffeb41257b14e2e60cceb7a7f653a65c2f3104
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:19:42 +0000

    dare high lower steady backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
