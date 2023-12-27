2126fe9b6a1e589d51c75ad51e57692ab0f50042
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:11:44 +0000

    remove ball through badly temperature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import collections
-def create_counter():
-        return collections.Counter()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
