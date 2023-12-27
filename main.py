4b5ca3e71e095c02743e09bdbc1efdcd2481eeb1
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:10:04 +0000

    murder concentrate taste result serve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def divide_numbers(x, y):
-        return x / y
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
