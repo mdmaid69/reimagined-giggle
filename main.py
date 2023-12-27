d1f2bcfec88576f222f223bd853e06614ab017d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:22:19 +0000

    comment seat record blame tap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
