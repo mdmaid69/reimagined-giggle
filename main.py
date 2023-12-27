859b0053bf35ed98d508b5b524e5201427ce17ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:07:19 +0000

    action love meal children bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
