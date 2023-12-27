7d3aa602abb6323253ddfaf443fe4ac41a24be09
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:03:29 +0000

    suck strike operate whatever embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import sys
+def exit_program():
+        sys.exit()
