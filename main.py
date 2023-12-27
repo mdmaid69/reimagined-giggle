cba98845e6de4aa76157a3b5da1c20fa09358bb1
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:24:07 +0000

    centre minor reply important basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
