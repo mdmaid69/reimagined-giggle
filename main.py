db885bb2c85809d3be569262710773d3766f68fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:38:26 +0000

    fence mixture together OKorokay pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
