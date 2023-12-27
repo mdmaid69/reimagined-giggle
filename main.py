cc1e452104759ebef22d095ef94ac49529d52fb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:00:13 +0000

    sister pass brother seem too

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def convert_to_octal(n):
+        return oct(n)
