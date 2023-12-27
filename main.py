a468e0d9a02bb5611738818186f37942563cf3c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:21:06 +0000

    equal basis story ought block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_work(force, distance):
+        return force * distance
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
