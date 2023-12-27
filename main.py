d9167df3deaf1aa2f20ee06ea2571f781f3a1ac5
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:35:58 +0000

    doubt back let makeinto fine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def count_elements(lst):
+        return len(lst)
+def count_words(sentence):
+        return len(sentence.split())
