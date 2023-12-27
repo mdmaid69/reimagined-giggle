2ff7c91e87616700dc8aaa98504924135f09a531
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:50:52 +0000

    claim flow since guard cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
