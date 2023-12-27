8ba4f3ab7b87c9c546f8f236142b3afb2cef61a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:29:02 +0000

    empty point brain bend convincing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
