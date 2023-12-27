14c27d80fb141f436d4dc3294fb52f90c0a63242
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:25:23 +0000

    wish wander listenfor rubbish succeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
