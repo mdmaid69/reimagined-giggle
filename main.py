5e1035443291f3799f463c77f2971b2f0bd098f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:49:55 +0000

    print wave neat far comein

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def square_number(x):
+        return x**2
