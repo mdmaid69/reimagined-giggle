17196950ac945d1a0425c9bc1be4c4f2d5700fac
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:30:28 +0000

    fresh exercise project reason creature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_index(array, item):
+        return array.index(item)
