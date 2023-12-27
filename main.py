a57b8f97e451e9e7d67338a63403da08c4905305
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:07:29 +0000

    name trust bringback concerning reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-print(math.pi)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
