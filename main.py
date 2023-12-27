90d40e6abbde83e02996a1328c259437ef8e73ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:39:56 +0000

    here respect eat ashamed arm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
