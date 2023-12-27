c2f6dd886707d0ab863eece99bed521aa8e4f792
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:11:10 +0000

    uncomfortable motion female appeal easy

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
