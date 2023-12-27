9fccffa8de3413b74ffaf81db4dd471998e5d0b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:08:53 +0000

    turnback woman have kick hers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import re
-print(re.match("h.*o", "hello world"))
+def get_array_index(array, item):
+        return array.index(item)
+def count_elements(lst):
+        return len(lst)
