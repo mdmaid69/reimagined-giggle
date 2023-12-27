c6a27367ea17cd339d37d6c1db0b48932ed98e35
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:32:28 +0000

    responsible except pair pause concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
