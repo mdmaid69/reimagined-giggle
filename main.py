bfe48e8161194527fb19972e4da9a753b2c8fcf6
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:15:24 +0000

    round tread divide another sight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def add_numbers(a, b):
-        return a + b
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_bool(array):
+        return bool(array)
