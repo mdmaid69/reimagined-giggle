dc2e91deca28fe6a9b32bc36f00cdf964f291ef4
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:47:25 +0000

    slightly scare defend direction last

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
