b5660bd68c99fb4f1ff6ccd73dd52037d1c1ffa7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:26:10 +0000

    female turnout nerve clearly suck

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
