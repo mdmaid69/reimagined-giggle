28d00806283dd0be2986975cd9d5db667febdb67
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:05:05 +0000

    improvement hold feature agreement bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
