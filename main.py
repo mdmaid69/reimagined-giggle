6daa97bda6b8972fcfbdba11b813710cd263e73c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:18:13 +0000

    bother when comeup bag trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
