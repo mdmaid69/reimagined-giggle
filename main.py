13d21430830831ffbfcda72af8ceb988c5b44426
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:49:39 +0000

    unable support collect imagination fire

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
