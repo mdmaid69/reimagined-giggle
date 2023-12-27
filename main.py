1c8b0068f798851219410a21d7c09a3f905c8605
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:03:53 +0000

    total entrance less each do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
