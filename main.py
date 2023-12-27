6983327f09fd5fbba34d83cfea6bc666dddfa921
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:48:41 +0000

    after clean plenty sink patient

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
