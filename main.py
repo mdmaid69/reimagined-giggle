fd77e98a9f8d775f4f0959d017d2929bc579c6e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:26:43 +0000

    line not outside dish computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_current_time():
-        return time.time()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
