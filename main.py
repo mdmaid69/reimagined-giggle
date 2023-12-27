b0be4ebc71c93a8eaeb8fd25c015bbec6734e407
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:33:33 +0000

    awake summer science cut bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def append_to_array(array, item):
+        array.append(item)
