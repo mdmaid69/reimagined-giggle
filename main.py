2b7764febc9bd6ad957c5c9a1cc4a7f652db01c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:57:57 +0000

    favourite up track murder prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
