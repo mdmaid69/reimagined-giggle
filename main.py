f344a54f28602d3415a14400edf6ea5c1f3da767
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:34:18 +0000

    home skirt shame compare winter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
