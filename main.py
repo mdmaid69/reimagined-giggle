5abe868fe482f3e3ac8ae10053657962b1fd1eee
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:22:39 +0000

    thick rubbish create mother entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
