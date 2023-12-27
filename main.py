761577464f80df7c817d013c30efdb4ec768fc68
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:56:47 +0000

    belief deaf present play getup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
