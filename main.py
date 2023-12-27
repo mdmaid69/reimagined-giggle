6c876f89469e461997b026a160dde7c43792aa0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:12:10 +0000

    lean thirsty direct loss continue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
