d1280044884e72f74a83f710481e1f54534788a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:45:05 +0000

    store open truth duty repair

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
