4bdcd63a5f0fc32a3daf1541dca21f7bcf332df8
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:59:35 +0000

    state convenient morning unlikely flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
