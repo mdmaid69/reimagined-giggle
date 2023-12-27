118353a3b6aebbf9062a83d466cd8e7869739539
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:06:22 +0000

    trick stick dress press believe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
