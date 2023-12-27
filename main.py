18a08f185d3a7dea0a87c58759bd8bd7c7688486
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:20:19 +0000

    holdon slice love brush cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
