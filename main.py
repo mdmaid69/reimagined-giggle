f5efadb13d88bac4abffb726c76e0dc403cd679b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:47:25 +0000

    excuse whichever pretty beyond blame

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_power(work, time):
+        return work / time
