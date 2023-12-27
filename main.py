b654d26f0dc647c6ed06b5d401f81fb8fe9c6008
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:20:19 +0000

    tap rich sticky tool birth

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-print([x**2 for x in range(10)])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
