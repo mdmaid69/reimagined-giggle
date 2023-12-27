a2183710e54e29955f1fa9a96ca7192346e882f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:08:29 +0000

    wrong calmly vanish borrow hers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def reverse_string(s):
+        return s[::-1]
