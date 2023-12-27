79cb9b4db3e8324ced84074c1e429f0732a8d6dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:49:58 +0000

    return pain free smoke damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
