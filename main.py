c0a7913ee5b45e2359f8c84fb2048758e8531422
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:06:07 +0000

    such instead television interview plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
