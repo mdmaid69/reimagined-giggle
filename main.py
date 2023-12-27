56338d8492a4bae6142dd1f5380dd50ae623c5de
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:08:13 +0000

    name pupil high favour brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
