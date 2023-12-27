3728587c12ecc4aa1032e50fb0d8e7735a5d611d
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:15:16 +0000

    feeling reply spend no somewhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
