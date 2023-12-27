7194788e5fddaedf18b12573d01de31538949cf3
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:36:39 +0000

    bread although support dare breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
