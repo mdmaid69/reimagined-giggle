ba4cb72768df794adf4ce2fb7095c8d0f5167741
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:09:40 +0000

    definite instant meanwhile intend bye

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def reverse_list(lst):
+        return lst[::-1]
