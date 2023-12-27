327ae9470c404b017b52e1487475214f8c04c22a
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:49:01 +0000

    interrupt challenge joke tongue meaning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
