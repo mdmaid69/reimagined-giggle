44fc85f02b236aed5d05e413e32990004b207c3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:00:28 +0000

    introduction order notice visit experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_power(work, time):
-        return work / time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_volume(length, width, height):
+        return length * width * height
