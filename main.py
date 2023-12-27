2570c7003ddf71ca88b28bc1d509c831d0701df1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:52:52 +0000

    uponon complete lend different sort

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
