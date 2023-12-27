f637c27d1e8be158f541d99b17b3350764213bd2
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:52:29 +0000

    prefer almost up bringup back

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
