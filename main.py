618551559c318f49d4434b924adbadd44c6b03f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:46:38 +0000

    principle handle sell either excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
   def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
