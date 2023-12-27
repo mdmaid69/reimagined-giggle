82122dcff505c51bacfb2852d379aa60bc16ff94
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:08:19 +0000

    spend face release uncomfortable ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
