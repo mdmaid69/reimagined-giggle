ccf6857849b56e1899ed8752dd1b0310d2c24e60
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:29:35 +0000

    fact live many onto pinch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def count_elements(lst):
+        return len(lst)
