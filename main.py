80c85071b0799447ee8fd3286ffa633fb279afc4
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:13:03 +0000

    pass difference defeat university operation

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
