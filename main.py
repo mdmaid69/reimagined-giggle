d3a16afd9203fad9ddc833b019e163f114570c5b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:49:59 +0000

    attempt comeover flow rank escape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
