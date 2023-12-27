35e56ed1c4af3249e8c4f6d7596bba41adc47150
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:01:10 +0000

    exchange factory tear breakinto smoke

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cosine(x):
+        return math.cos(x)
