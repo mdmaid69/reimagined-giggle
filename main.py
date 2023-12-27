ac8d12993af6ac79e73c98dbfedaef29d4a79320
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:43:16 +0000

    invite grow agreement warning tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
