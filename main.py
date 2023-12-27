8925972b67c62a5461f97eb53cc918215105126b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:21:40 +0000

    problem giveout achieve balance inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
