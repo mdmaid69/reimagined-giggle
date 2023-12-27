dfb0495ecdd73f6d9bc86d264226ce557b03840b
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:28:39 +0000

    direction so crash shoulder work

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import json
+print(json.dumps({"name": "John", "age": 30}))
