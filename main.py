d82eb9cad931f9f353876c2095f642cba2549a53
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:38:18 +0000

    admit takeout business today deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
