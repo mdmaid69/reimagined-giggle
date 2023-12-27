ccfde8479336c16a9e01be89d417d6d290a9c575
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:41:48 +0000

    bother please plant expensive over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
