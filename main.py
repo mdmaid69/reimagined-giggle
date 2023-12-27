5cb155bc82dc69300f60837fb2fac8bbe7f6b348
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:49:19 +0000

    reduce about class bottom black

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
