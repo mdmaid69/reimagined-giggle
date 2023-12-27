b9956d335b6b0ef32436e47e9ea9f9b2e45b290b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:08:17 +0000

    against morning scrap trade fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
