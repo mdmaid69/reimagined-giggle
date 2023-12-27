82888978b1782ceb4a49569ce43494cd100feaf6
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:01:39 +0000

    guide winter mail boat future

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
