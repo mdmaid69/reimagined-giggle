9347ec56adff88fd0adab37ff2cfbfc02332c58f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:01:38 +0000

    wish breakinto none march window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
