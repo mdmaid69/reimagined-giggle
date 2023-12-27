ca97da13a75c492c187819e5cb0a8f534cf12ac8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:57:55 +0000

    during deal mess Gg degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
