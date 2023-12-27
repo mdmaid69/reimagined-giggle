289ce434a534d8dde746164a0c4db8340ae40034
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:38 +0000

    uncertain sense each soon minimum

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
