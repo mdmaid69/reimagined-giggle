dbe2e8ace30d88640e8e2bb690175bd2f134f572
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:49:52 +0000

    show hide possession talk answer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
