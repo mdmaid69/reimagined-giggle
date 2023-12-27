e443c8b9125a2a1fc12069d7cbb57390d5c09a8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:53:57 +0000

    knowledge course kick listen someone

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_area_rectangle(l, w):
+        return l * w
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
