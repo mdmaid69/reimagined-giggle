1d85a7ac1fed27dc9750dc8dd7b254408677364f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:30:10 +0000

    here edge slice turn smile

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
