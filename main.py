735acf2f8dfbbfde5bbfcdc59564bc127900b9f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:08:56 +0000

    exaggerate many run over blame

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
