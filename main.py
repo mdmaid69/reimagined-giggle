c10a369010a3521659ce02943355612c9fe04cbb
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:29:25 +0000

    modern thorough learn bridge argue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
