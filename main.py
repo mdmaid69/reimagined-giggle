f9a1480e53bc5584cae329f8873827d21cc0b6f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:35:56 +0000

    top eat more opinion brown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
