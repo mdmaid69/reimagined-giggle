f4c7153e34c7de78a7a16ea16138ad307f997b56
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:10:32 +0000

    test workon breakout damage deserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
