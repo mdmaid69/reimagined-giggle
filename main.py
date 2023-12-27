5474326de44fb816051a01ce817b76f34aa8578b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:27:52 +0000

    respect before public evidence bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
