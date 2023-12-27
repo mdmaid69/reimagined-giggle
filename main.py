8d5177ccdf118df564d00dc078dc2f308f65fbe9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:05:37 +0000

    half with roof rescue copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
