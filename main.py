1e8adb3321cd638ae6bc9abaffd8d5922d5de70d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:54:48 +0000

    next buy month rough speech

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_bool(array):
+        return bool(array)
