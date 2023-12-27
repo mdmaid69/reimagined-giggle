59e59a11d1f7aa26912e62711b9e19a8a840bdb9
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:22:45 +0000

    welcome understand fork form then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
