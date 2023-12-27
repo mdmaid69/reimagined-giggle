ec9e52f17e4275868a7c46f9fde9b87fb492242a
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:50 +0000

    throw field considering basic corner

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_work(force, distance):
+        return force * distance
