71e01d69958e6b3582ae9b787d33a996fa761dc7
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:30:13 +0000

    hollow breakfast pretend part measure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
