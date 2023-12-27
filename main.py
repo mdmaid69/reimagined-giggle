c386fb2695c69b14a668c3eeabb9519fc5b13ca6
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:07:20 +0000

    cold rough copy freeze turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import logging
+def log_message(message):
+        logging.info(message)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
