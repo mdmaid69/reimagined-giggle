3a5ec218717d8972efbf8545c022b15372d07466
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:22:06 +0000

    let successful bringforward maintain stage

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
