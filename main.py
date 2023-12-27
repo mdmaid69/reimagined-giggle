eb0378dc9d4fc414b49998c69d7b486f8e763662
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:09:16 +0000

    content trick live language branch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+print(sys.version)
