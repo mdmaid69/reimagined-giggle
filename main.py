dd5343e22af23c12d58dbc123c22aab7bf553fac
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:29:33 +0000

    Rr boring development they admit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
