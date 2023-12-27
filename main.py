0c8df0be44b22d74e3c1e1eb9042a9a706de815d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:10:08 +0000

    whenever community task safe character

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 def change_working_directory(path):
         os.chdir(path)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
