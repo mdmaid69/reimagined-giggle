86512a0912cebe124a230529c03c6408c5aafbb0
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:11:20 +0000

    exam black freeze schedule before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
