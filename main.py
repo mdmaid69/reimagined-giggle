3f7b0b804a4012ab5cc258dcd9d0cbfcec8f3c18
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:56:02 +0000

    open bound shopping possible loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_power(work, time):
+        return work / time
