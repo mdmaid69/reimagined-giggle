ef4099b9dff1df5f8d922502dac405229f90e303
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:07 +0000

    side repair bet start perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
