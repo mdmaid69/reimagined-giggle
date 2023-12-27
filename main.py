458e75b4527d486bedf2ae0f6bf9d99cfe00678c
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:59:06 +0000

    land pair somehow but pure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def convert_to_octal(n):
+        return oct(n)
