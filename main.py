c8fb7925c3984bbc7310d215b9ffd4547570080a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:50:28 +0000

    guess statement blame happy night

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
+import platform
+def get_os_info():
+        return platform.uname()
