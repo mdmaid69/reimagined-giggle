962b904f87d0845ddf5f96604f7dea88bd772aef
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:52:58 +0000

    minor bother situation regularly after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_item(array, i):
+        return array[i]
