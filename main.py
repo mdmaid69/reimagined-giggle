c1fb05ffb1fe592c09c4c4f1ccac865441d931ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:23:28 +0000

    rather help Mm tune band

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def sort_list(lst):
+        return sorted(lst)
