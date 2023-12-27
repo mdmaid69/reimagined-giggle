928d57d6798933033ca0a51010db193c4aebd769
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:37:12 +0000

    experiment interruption year sure practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
 def append_to_array(array, item):
         array.append(item)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
