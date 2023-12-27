21edcdc1ec06656ef27ba65121d6167589e6c968
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:18:30 +0000

    anywhere original disappear battery less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
