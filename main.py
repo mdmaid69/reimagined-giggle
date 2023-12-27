ab9696b5c668c4b19304dc03ea7a3d166a0fa4f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:41:33 +0000

    mystery loan brief estimate uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
