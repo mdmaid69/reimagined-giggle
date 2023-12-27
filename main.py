c6ca7b68c5e76fc57531d3a11723561e308fbe2a
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:44:51 +0000

    better hurry neck point lunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def find_union(list1, list2):
+        return set(list1) | set(list2)
