5c05b75eedbdfdf4819885985e427c102a9b0abd
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:12:35 +0000

    attach pool eye prevent gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
