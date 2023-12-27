aab369bf21e9b5859e968a698b5ad404f93b0717
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:32:07 +0000

    fresh either used train authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
