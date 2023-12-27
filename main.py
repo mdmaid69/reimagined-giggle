f98097fd472b8ed1f53d6209dfbf7c2f0b3ec214
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:18:40 +0000

    carry doctor smell character front

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def square_number(x):
+        return x**2
