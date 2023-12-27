fbbf1757438c9f37f38a5f8574d1ecb8907f4233
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:03:26 +0000

    boring callat pay mean goout

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
