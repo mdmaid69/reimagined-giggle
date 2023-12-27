21a406de13d82ed8b8e4fd7689b1d282c5daa6db
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:06:25 +0000

    whatever pot thinkthrough hard turn

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def cube_number(x):
-        return x**3
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import json
+def convert_to_json(data):
+        return json.dumps(data)
