7d4c7e8f7b29f09b7066bec8844ff5bd37ce3ab8
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:19:02 +0000

    evidence room move discuss if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
