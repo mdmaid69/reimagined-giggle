2d5f5b372a107c559c953eb9afe0c9ec238b014e
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:03:01 +0000

    create even answer although scrap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
