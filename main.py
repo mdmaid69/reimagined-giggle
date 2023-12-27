99edbe92b74a112cd6b0a552b0b8511ff599ae58
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:54:36 +0000

    your Mm clever dress 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
