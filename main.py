3d3d59aa725e0777ed9603cf94bcb450926789c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:22:16 +0000

    round firm government boat size

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
