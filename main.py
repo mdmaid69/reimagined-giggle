5c6aae8f6a8722cfd7026e8c890b96ca5b89e4f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:10:33 +0000

    almost address breakin play grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_length(array):
+        return len(array)
