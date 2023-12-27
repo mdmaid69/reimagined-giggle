bf7964d49e7e2eb3e5271a527b6dd6b87f0dfc90
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:05:50 +0000

    hold negative nowhere best bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def multiply_numbers(x, y):
-        return x * y
+import sys
+print(sys.version)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
