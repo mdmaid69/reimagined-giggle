e56cd475e1c4bc2677c374190f1d4f13b6ff65ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:34:08 +0000

    family Mm born order myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
