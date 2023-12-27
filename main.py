7dda5e2f9dda4ee469f89b33996d73bdc777aa3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:58:44 +0000

    treat break fair content whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
