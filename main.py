ea5e469af5603fdb5e778c72ff5cafb510991443
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:33:35 +0000

    extra must safety calm Dd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_pressure(force, area):
+        return force / area
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
