0bd0242623488183f31eac49bb4bff1aaac8decb
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:28:15 +0000

    begin worse cool discussion balance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def subtract_numbers(x, y):
-        return x - y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
