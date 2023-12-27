c915ac349379db54f957d9273812eb9aa2465f97
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:15:47 +0000

    avoid neither winter please never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
