453c3e5303f1459aee9cf2ddc2aa7860d72efa3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:46:15 +0000

    plate pile forgive study relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
