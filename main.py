8a0ec8ee43f6a01806cd0332f400a538d83348b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:28:44 +0000

    because simple artificial fact pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def add_numbers(a, b):
+        return a + b
