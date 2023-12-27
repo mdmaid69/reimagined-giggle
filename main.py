41647854a41f76ea467ba059db2879731e7ad354
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:31:14 +0000

    dick shelter design file earth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
