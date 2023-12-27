a3843cd84d7b821f0d152814743d2fc0b38f0054
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:23:44 +0000

    hurry professional protest rub black

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import tensorflow as tf
-print(tf.__version__)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
