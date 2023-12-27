a6179ca64497824c5cd2cefa801eaa64fff772c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:08:57 +0000

    occasional reach no let constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def reverse_list(lst):
+        return lst[::-1]
