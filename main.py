36aefa2fb3c5617e7243a2d07269db807945f119
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:06:59 +0000

    tape goout anything clue else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
