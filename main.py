a938ca9520b500e43bc9bbcaa43d78cbf9de62a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:38:24 +0000

    trust reaction discover justice sign

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def square_number(x):
-        return x**2
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
