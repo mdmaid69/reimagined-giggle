f2dfda3c06d8d60b67aa341f4bf108f56b05ede1
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:09:55 +0000

    fit statement future already minority

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
