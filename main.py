6d87841e2d48a54119cb7e665028b66ef6780ab9
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:58:21 +0000

    uncomfortable firm design flat city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
