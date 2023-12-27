79985147a1d88ae25983c2eed939c2eb419c5298
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:40:03 +0000

    generally blue tend spoil dig

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import random
-print(random.randint(0, 100))
-def subtract_numbers(x, y):
-        return x - y
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
