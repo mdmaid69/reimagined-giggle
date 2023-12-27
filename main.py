d5541c6269884d380016f5132cb84e87381fa15b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:13:54 +0000

    meal their must mean push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
