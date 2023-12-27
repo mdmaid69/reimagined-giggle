91ee6c0dc1e1a872311272a47680b39aeebc0cf1
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:27:21 +0000

    question progress design undo now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import sys
+def print_python_version():
+        return sys.version
