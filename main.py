e0f5594628ad377b4d13947a70911ef16d13cc59
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:29:11 +0000

    tip minute instead condition mistake

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_list():
-        return collections.UserList()
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
