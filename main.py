b9abfcfb4584051cf5222812a36607ae3b57c699
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:40:42 +0000

    then immediate young refuse test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
