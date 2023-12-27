29e26a85207cd6bf9a0ead238e6d2856a9de5fd0
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:00:12 +0000

    sit sentence succeed turnback Uu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
