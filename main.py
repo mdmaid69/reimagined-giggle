52408c0d6e9c668368e46420b840346ba9bd14da
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:46:04 +0000

    themselves match offend tread modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_index(array, item):
+        return array.index(item)
