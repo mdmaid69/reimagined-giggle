2aeafad93e71f8d6dbd7ae21bbdecb0a2b0ad963
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:59:14 +0000

    walk possible miss communication rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
