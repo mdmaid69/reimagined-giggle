022297fa55ed407f92e33cb087fc97aab42a1f3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:58:24 +0000

    interfere study fall handle opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
