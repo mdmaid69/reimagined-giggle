4fe96faaac6b90d578ff71fdd9e2adfe68f2f5a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:34:47 +0000

    bury pocket bar charge yet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
