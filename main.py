4c4fab9cb31a016fbcd15f5ceafd6bb790a790e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:30:54 +0000

    perfect old road feel wild

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
