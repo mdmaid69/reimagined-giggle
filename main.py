a716d919a0817572c0e5b7163536b74d0ab6d630
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:01:26 +0000

    both line wise seriously every

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def is_palindrome(s):
-        return s == s[::-1]
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
