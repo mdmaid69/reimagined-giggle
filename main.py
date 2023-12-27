cda8b3353d4412fa0c853df89c25710210772636
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:26:33 +0000

    Jj mad rule before towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def append_to_array(array, item):
+        array.append(item)
