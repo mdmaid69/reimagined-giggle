225de342565b8bfa584bd4b256df079cad10280e
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:17:33 +0000

    environment change Pp getoff drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
