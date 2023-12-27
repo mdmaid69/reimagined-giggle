3d5bfc08d460360d2a2a85efaa4865e231f3cc12
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:36:31 +0000

    different cross collect unless comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def clear_array(array):
+        array *= 0
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
