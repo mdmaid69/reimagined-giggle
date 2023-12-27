d0e70ccb1f15e14c051b93f4f7936b831a3c03b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:09:42 +0000

    throughout model save with middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_repr(array):
+        return repr(array)
