03a11e1217f0326f8c4277e0c32c45b1cab63432
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:11:49 +0000

    comeover night worry proud public

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def multiply_numbers(x, y):
+        return x * y
