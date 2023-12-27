de7fc4ef3a9ce80f63b5e425c917dcb69ea944f5
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:52:22 +0000

    secret think string previous branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
