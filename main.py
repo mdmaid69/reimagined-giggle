009c0f7ecb920cad9d8f1027868c9e7aac8333e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:36:28 +0000

    prevent pronounce wire remark outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
