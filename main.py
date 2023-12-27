fb8730f53c2e4fd0fa198b94074f5ff4705acf02
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:11:59 +0000

    dance small state journey rescue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
