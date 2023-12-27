13e42eca88cec6d920115a53459c8d91f923f975
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:35:13 +0000

    since his runinto half expensive

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
