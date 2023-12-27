840ed18dc9b52442f302210e7931c13e07a8c12a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:58:07 +0000

    bar deliver crowded behave slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
