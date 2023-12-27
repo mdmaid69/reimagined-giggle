a8463ac813ff39cefda1c9f38c907ed202638737
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:33:07 +0000

    clean nature flavour impressive dream

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
