44c388414cb1085204f529c46a973f081753a138
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:05:15 +0000

    left sister undo recent right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
