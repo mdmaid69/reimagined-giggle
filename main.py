fbed91738a02c76cb0bace62548d3fb25b64fa98
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:28:21 +0000

    confused glass ride urgent preparation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
