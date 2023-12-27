cf8c6426eafd4fd22fe7f4aaca46aed1fc4f3263
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:14:57 +0000

    sensible floor good club exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
