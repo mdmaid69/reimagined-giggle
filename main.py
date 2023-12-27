cc319949a19818d6cb260807c9da8c94ff8e58cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:05:12 +0000

    far music exam majority split

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
