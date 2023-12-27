7eef9889da0a30dfb7c6b1ccaac34ae3a65a240a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:28:10 +0000

    accommodation flight unfair 572enter drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_hex(n):
+        return hex(n)
