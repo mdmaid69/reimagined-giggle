7527b604de8a8d4ad84cf279651b68a847c61ba4
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:01:54 +0000

    borrow weekend ability score trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
