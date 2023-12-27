482bdd35e19fca4993a9beaccee83bda2935717c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:06:20 +0000

    gas runinto up exam wrap

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
