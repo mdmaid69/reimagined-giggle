dca964197f0b612acca7fcbc835b73630fe8abb5
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:13:27 +0000

    even age funny whether final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
