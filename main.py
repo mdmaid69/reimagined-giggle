a136b588b9bb8a9943e8c2c871608cc50fffc542
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:08:15 +0000

    trap march moral top comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
