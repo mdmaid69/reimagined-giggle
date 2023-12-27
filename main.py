eb8ed72c515e11d5ee28ed46a67d7925fc8c64c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:09:02 +0000

    handle sort anxious level below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
