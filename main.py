a109c69056499d10c5823db11c7d815bff529daf
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:21:21 +0000

    increase mark pot actual fit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import sys
-def exit_program():
-        sys.exit()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
