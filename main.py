46273a6f94e8168ede40f0989dcf476bcd750162
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:02:02 +0000

    argument turnon rough gas congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+for i in range(10): print(i)
