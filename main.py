2f9608974d962d9c2d2b55205960560a6377812f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:45:18 +0000

    evidence eye include pin pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def convert_to_json(data):
+        return json.dumps(data)
