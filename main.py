9dca87840cdebf51a91164123729a7f6fe50b26d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:31:53 +0000

    friendly on tall knock mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def sort_list(lst):
+        return sorted(lst)
