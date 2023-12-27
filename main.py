abc80fce6c45c53cca822761de6f23592a520a0e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:12:07 +0000

    pattern rid bar aim important

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
