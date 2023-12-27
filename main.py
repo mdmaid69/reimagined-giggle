bfee613c1099a51bfaeb7b576d1ae423debab859
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:49:10 +0000

    ring breathe rent inquire person

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def reverse_string(s):
-        return s[::-1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
