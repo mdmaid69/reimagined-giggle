52531da3c7ad59168ac2fabb1912f93e0316dc8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:20:56 +0000

    on one score show afraid

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def is_odd(n):
+        return n % 2 != 0
