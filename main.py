28a06906512ff00cd511bddc9fc133c62305277b
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:15:00 +0000

    communicate direction Gg Ww along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
