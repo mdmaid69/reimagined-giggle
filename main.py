52f96d82a55b584a95b3cbb46a9e289f58edbdcf
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:16:16 +0000

    wish Kk cheap exist exhibition

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
