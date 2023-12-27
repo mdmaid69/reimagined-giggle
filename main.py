9849534babc7884b51304b01dd231106c9dbe6a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:37:14 +0000

    while bright solve goout swimming

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
