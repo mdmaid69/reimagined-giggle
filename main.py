c4d993b968ca544fcf0d052f331972c2aa355830
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:15:36 +0000

    Ww thankyou permission sound maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
