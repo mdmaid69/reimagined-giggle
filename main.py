2985f357c76a2d5bfa1be9592eb6d72e8ebdd1c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:18:06 +0000

    innocent supply healthy warn consider

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
