7d7f5ae8481f40dc1c0c98a87c323b480e69b896
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:53:59 +0000

    live neat system share rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
