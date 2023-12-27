29bb74f091add07db971cc73afc496572fa58ab1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:10:56 +0000

    case squeeze bar mix whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
