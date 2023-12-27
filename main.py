43b029fd9bc7b2cbaa7c1db42f536a22118aaee7
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:32:25 +0000

    space meanwhile stop normal even

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
