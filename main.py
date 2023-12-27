62d10a65933206229e5ec364cccbcb50d87b552e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:28 +0000

    off purpose yet degree container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def reverse_array(array):
+        array.reverse()
