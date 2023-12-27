e965afd4b5d0980567d37edaa8d17405ff6c794d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:09:23 +0000

    punish disappear drop trap guess

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sys
+def exit_program():
+        sys.exit()
