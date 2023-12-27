b8c219e050b49b6b284c1092f9cd69fb4aa9dcf1
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:08:37 +0000

    wall flat use common talkback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
