59dc38942806d8325bf7b9c33d8830d64b109068
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:30:58 +0000

    slight this decide length office

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
