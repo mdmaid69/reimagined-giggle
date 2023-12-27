d63da18f9dc254aedf8387ae6973bc9143ebcb89
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:34:16 +0000

    objection trip confuse early heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
