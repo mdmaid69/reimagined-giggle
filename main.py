dbc7c30a9aec1c18b7fd84fd4ee514688ebd3e4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:59:13 +0000

    flight hello copy wipe interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
