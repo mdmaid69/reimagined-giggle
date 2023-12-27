6ff7f6770a231a2775db4039057ef17af2ec004d
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:55:56 +0000

    intelligent march reach arm band

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
