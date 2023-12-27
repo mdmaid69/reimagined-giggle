b0259d2b1449b9dd4837c0f853a3f3e7f11e6aea
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:23:53 +0000

    speed happy anyone hurt slide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
