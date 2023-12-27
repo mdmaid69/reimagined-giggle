fc2316278452166bdbc015d6a09f238bd4969bec
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:13:04 +0000

    rob arrival will otherwise drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
