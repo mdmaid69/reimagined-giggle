6395e9ff6e3a32453a126fb858737601cbeb17f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:42:03 +0000

    popular after performance fetch mouth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-print(math.pi)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
