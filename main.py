e345cac79be1fddec81757c143ab2ca49b5340d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:22:00 +0000

    again exactly on produce Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
