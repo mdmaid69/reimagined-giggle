1ec72cf51f2b7b5d0ccf7a75a79bd422962e3573
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:30:49 +0000

    loss fail ignore pattern chase

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
