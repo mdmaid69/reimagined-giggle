ebd9498eaf26c0b2d1fdf000f52bedad9c34fe05
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:44:39 +0000

    for successful range majority address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
