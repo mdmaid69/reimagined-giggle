11889507c3dfd9450be5d043e06c211a4a45ae53
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:02:31 +0000

    experience material 573entertainment forever art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
