f10e5a4f635988907224f70a13cc678c7f5be348
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:02:02 +0000

    cheap immediate load usually Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
