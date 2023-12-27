9f4b936ef4b54fe4851ae86d49850b085696f023
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:14:22 +0000

    bite alive sight properly just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
