dfaf22210b35ba1dd7c21cfd9796d6bea3d88e3e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:11:56 +0000

    perfect Yy our Ww awake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
