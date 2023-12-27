d1c85092164b78536b3c06727728b057368cbabe
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:03:42 +0000

    sorry section snow test bringback

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import logging
-def log_message(message):
-        logging.info(message)
+for i in range(10): print(i)
+  def is_odd(n):
+        return n % 2 != 0
