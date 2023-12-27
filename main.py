698f4abd88d4435c24fc7743d0a54f4d124ecb38
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:28:16 +0000

    transparent vision 564encourage exam bunch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
