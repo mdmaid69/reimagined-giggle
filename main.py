7774bc691bf8f87329699d664b7833d3d18b5612
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:16:57 +0000

    month strength base word gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
