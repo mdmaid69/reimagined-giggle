959af403f3ff0814ec4d2b4fe02bf6a3af064492
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:43:18 +0000

    promise boil scream live cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
