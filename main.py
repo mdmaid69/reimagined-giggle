40e9d4310749a9d4416c87d79af320a63f42af5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:42:17 +0000

    sore brother so mind autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def reverse_array(array):
+        array.reverse()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
