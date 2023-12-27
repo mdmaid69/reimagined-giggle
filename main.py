63cb50736a5164d23b896d106ce28ec81345a6c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:08:40 +0000

    father goout home dark have

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def count_words(sentence):
+        return len(sentence.split())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
