d853cabaf3c8fc09c7932139410e42c5098ae80b
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:31:08 +0000

    free bet achievement herself benefit

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
