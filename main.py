6ab36aa3587fc62574a456c0aad4331fa59717c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:18:00 +0000

    talk hit sleep block succeed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
