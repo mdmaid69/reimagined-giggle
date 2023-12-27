bbab821d71dbc2860e68af79e93a1a0a8da9c005
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:56:37 +0000

    clean splash Qq smooth closed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
