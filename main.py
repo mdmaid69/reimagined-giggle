3bd6f9fd43227fba7ffc30387ff80e668e3e0399
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:09:43 +0000

    unit form inside priority rub

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def is_odd(n):
+        return n % 2 != 0
