cbfc61813967018b72388266c8dc7b6dc75f6247
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:22:38 +0000

    hungry everywhere peace concern well

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import collections
+def create_counter():
+        return collections.Counter()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
