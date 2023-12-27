ec3dea443a5a1b6773f403c517aa29b1c4299cc6
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:45:53 +0000

    their 571enough Ll pupil then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
