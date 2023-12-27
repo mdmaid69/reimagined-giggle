fa5b920bc39d2bc52530550275bc8c3e31ca136a
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:59:04 +0000

    someone slice hurry admire another

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def remove_duplicates(lst):
+        return list(set(lst))
