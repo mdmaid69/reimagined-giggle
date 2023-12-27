1f982250d47de31f6e11de1c092d9ad6d3045519
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:25:54 +0000

    miss reply someone exam total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
