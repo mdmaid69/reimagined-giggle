9117ac3aeeee43e825be479718fd77d55736f621
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:45:10 +0000

    muscle honest pollution shadow getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
