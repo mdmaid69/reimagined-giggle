da126373a92bb450cc9ea439e4792d4d3a8ddee6
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:31:41 +0000

    water last mess workout flight

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
