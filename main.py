28f7300f500e410f99a5f575c70904a2685e3a04
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:40 +0000

    beyond say mess music fierce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import getpass
+def get_username():
+        return getpass.getuser()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
