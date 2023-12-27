0da42b9ee703e9c135a529196d3506209a38873e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:05:14 +0000

    full bar move match read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
