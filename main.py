77d731c66f9f871a84fcfa61c96d4879a467efb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:02:38 +0000

    supply shoulder mine miss human

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
