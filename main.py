65ad9cb264618a66b6bfae8d503e877c372239ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:13:26 +0000

    bit comeup keen change return

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_item(array, i):
+        return array[i]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
