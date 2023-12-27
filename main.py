30f0bfab0d3ab539b43bd2a34af9c06e8d427c47
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:31:18 +0000

    kind relation fly agree finish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
