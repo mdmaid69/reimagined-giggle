bb1f282527e9ff9fdcfcd02e4c72955d8d6b05fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:37:02 +0000

    car their wind feed sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
