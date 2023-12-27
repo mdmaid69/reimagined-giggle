7f9a2e6ad5012e9f231a4a380dffef4501025f8f
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:06:54 +0000

    tomorrow terrible level afraid train

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
