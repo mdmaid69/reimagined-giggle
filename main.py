d4ec1c0bb4429f0203cecac474cb76a5ecc0aee0
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:06:42 +0000

    each doctor comfortable count fit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
