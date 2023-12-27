e166fef0d183df01aca79f9b91e2aeab0f7c73d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:55:07 +0000

    commit obvious born fine country

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
