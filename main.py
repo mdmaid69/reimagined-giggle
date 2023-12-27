fd6eeed580887661f82a4b51d4783230c4c15995
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:44:55 +0000

    fierce cover any pay fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def is_odd(n):
+        return n % 2 != 0
