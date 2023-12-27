6fd29689e7e0806f20a6a79498a365dba8d8f9c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:20:27 +0000

    we extraordinary behind sorry search

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
