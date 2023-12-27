c13d73cf7a5f6756861aa01917a2407b0b0ce957
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:50:52 +0000

    comment basic news eager their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
