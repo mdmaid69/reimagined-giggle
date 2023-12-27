80d91d358588395756b76ddc23eadcc2da0be64d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:57:05 +0000

    anxious generous pin floor action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
