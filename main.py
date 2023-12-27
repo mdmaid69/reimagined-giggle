ea1e0bf0fd4d4ab00d8efda829ce3c79731d68fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:04:24 +0000

    only spoil object across unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_set(array):
+        return set(array)
