85a73f6d2d56a8232a4c45c7bbf74f22ad174957
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:33:28 +0000

    about miss turnup content nurse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
