c4a816bd8ffe1a5c874690d56808b9de2c826ba6
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:55:48 +0000

    talk scared criticism watch laugh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
