dca7e005e0d476b6abe1e109c968a806224f1076
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:46:29 +0000

    street back round mysterious every

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_repr(array):
+        return repr(array)
