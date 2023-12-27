5376b87022c39e290bb6cdcb4a6a4970e6b225f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:24:19 +0000

    concerning wish question sick progress

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
