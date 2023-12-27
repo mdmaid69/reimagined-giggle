d20468d96f14e67738bf07b417839f8374337a12
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:13:06 +0000

    question geton cover callout dead

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
