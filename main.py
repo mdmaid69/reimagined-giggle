be6aa2275d86e00e81fe34e44c303d3ad785f89e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:42:39 +0000

    schedule but why narrow area

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
