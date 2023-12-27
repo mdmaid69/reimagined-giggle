cf53f15d58cb59aa62829102a95a5aea49629e3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:27:23 +0000

    female that roof to getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
