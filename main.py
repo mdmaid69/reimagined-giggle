4446e0b97ae19929f7ccd3c08b52a5ede8574490
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:59:49 +0000

    hurry must than sore soak

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
