e24dc4fe919b727af57ba74618afa4c83ddd6bb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:57:55 +0000

    usually stand on arrangement roll

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
