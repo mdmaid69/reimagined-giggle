45583a00893dd1e2a38098abd48a0b3e962b9ae3
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:25:31 +0000

    gas frequent wrap reach whether

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
