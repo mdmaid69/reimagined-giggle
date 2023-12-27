02dd8c3002296941d66bd62cd37d478de92a5a00
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:45:29 +0000

    polite police left dream short

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
