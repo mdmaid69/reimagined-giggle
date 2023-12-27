26bc295e4b0e97cdc0cd95232577b9cbbc3b76da
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:35:31 +0000

    weekend afterwards fit without care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
