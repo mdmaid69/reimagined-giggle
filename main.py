29b8c78b1127709cbddf9b652ce12e2c1ebc67f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:34:38 +0000

    black practical wait sore thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_cosine(x):
+        return math.cos(x)
