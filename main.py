0dbedb7309bf5493c590a2170d798ea42477276d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:53:47 +0000

    please back distance explain crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
