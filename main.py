60d826a69cb02e873c9ef2785d50dadbc11214a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:51:17 +0000

    build not pretty generally discover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
