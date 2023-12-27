17559728fe4c4abf8f2b1b7282da3ba03fadfa50
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:53:42 +0000

    callat this sharp soak concerning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
