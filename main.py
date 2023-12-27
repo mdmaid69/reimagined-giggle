45e5934d4b0c29e81487175092fc35d89854ab2e
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:36:21 +0000

    spring lock mad confuse uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
