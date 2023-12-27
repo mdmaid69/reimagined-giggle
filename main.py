3c9bbe3ea03ba71782e8473acba1ccdec689ab1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:13:17 +0000

    ilovedicks strike clear emotion dead

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
