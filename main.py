0e656db5725a8352d6edde523af8c53eb043cf7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:51:11 +0000

    eager dinner breath afraid university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
