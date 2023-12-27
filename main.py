662951effd8fc8f72fb9a17cdba3c0c8bc0968d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:28:20 +0000

    close block load cry muscle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
