ecbf2f6f8c6d3baaeec8d40a3a292652f96016b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:46:30 +0000

    hurt fall sensitive mother raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
