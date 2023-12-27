72273e433d4e7eddd7a8553d6f730ae7e9edb00c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:26:37 +0000

    friendly occasion really voice price

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
