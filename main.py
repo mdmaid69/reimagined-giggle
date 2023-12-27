2179b8c5b84884f39d5243ac81a1a9194e7587af
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:23:34 +0000

    failure apparently bunch track hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
