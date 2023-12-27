f177e589b0f48dcbaf540fab6b1dd7be3ec017ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:15:38 +0000

    release mend copy bottom much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
