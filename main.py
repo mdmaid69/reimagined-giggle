c1671833dee81dcff693cf6b125fe5591c54d8af
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:41:31 +0000

    tip suffer apologizeapologise continue workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
