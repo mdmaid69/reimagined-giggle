0240d83c232873dc23d44d1ad6d197174fcdd37a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:47:16 +0000

    broken comment form sometimes exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
