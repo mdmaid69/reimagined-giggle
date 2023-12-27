af5df3f43640521c8108c32ee770c04045e2cdd4
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:17:52 +0000

    unable curious fill quality head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
