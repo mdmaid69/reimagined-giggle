9233a98ca121c42260172e0d237a4e445573400b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:04:39 +0000

    I mood interested film male

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
