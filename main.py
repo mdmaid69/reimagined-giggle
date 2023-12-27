ca62d1dc766a8a058a5083e4ec81f43243204efc
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:03:14 +0000

    hurt familiar search help beginning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
