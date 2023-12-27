926a77f3dcb25b221ae2104865c65134dc508d2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:51:03 +0000

    poor march tip mixture rich

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import numpy as np
+print(np.array([1, 2, 3]))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
