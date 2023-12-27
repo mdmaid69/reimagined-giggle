80ad1cae9165d9a97eb807adf234faca5edccd4a
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:48:09 +0000

    another show move recommend water

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
