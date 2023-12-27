b1a2a90b45d65e74f2355b1eb70fd8c0bb6ccd9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:56:11 +0000

    experience convincing soil error hour

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
