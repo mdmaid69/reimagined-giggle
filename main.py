4d39d02a51094cfe7c9cbf21e58f309456fc015c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:09:09 +0000

    shop development still plastic before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
