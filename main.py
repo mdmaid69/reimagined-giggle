a1f6ef410e176a501edb339dc308c97880f9ff44
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:00:00 +0000

    brown bad sorry nice challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
