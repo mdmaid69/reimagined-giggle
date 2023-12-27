37e32527b544a52cd2c11bed4b97fa20313a8700
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:42:37 +0000

    noise remain prevent show harm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def square_number(x):
+        return x**2
