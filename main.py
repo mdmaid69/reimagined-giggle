3e43e3eeb10549c1fdfd0134e9baeda2dd412b9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:01:41 +0000

    brown confuse trade very public

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def is_odd(n):
+        return n % 2 != 0
