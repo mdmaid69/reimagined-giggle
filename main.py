b3310305faeede11c5fb10d47e51d3ed6dd1a66d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:18:41 +0000

    round respect comeup belief hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
