cb58e751316a4b29e2dd3cf5b099fc24111b01aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:05:48 +0000

    bringback use sit advantage sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def reverse_string(s):
+        return s[::-1]
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
