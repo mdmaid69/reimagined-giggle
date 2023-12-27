05b28e2048118d865649e8840d1e8b1ad73713cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:48:13 +0000

    anywhere winter walk official for

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
