8268860e5541433d7bc1e54376031282bfc03c77
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:18:32 +0000

    address beginning disturbing hurt disturbing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
