ab7559ebfa2d4d6778681970612a01957990f7e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:42:15 +0000

    community intend turninto to science

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
