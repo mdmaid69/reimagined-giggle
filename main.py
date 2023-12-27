130dc0a47024cb6eea6e322594cee689c319dfec
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:57:35 +0000

    allow minute manner floor spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
