220e1b64611f59662cd862c4a1024b3d45d1c7e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:50:11 +0000

    picture drink benefit plastic tune

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
