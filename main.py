555003cd8e145db2270afe8b6bdd5f92a075ec2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:29:31 +0000

    identify 570enjoyable debt one out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
