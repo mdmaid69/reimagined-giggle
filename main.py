479025336e48d55facfb7d34658b286554f7e629
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:20:45 +0000

    phone thirsty abroad later passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
