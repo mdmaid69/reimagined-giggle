56f3f1872463526881fbe6b0193a9320e3aa3e18
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:22:37 +0000

    club unpleasant there boat emergency

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
