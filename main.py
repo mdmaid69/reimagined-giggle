f422d9a944714fecb7ecf441173b6a51b3b8ac02
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:37:31 +0000

    disagree lot ocean offence then

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
