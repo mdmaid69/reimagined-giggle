2019c5f452231bfa097c71013107417f0c227483
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:12:15 +0000

    style approval maybe emphasis occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def clear_array(array):
+        array *= 0
