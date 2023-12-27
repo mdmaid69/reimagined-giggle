f383a29bcf9b83f9f185a0ed2cfd605c0d5a707f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:15:54 +0000

    least convenient create plastic float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
