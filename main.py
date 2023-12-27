c7e1214cdc39978fcb939e39f096daaa15f0b8ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:59:18 +0000

    number whisper breath defend accidental

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
