61a59e773ea8a6e7e18a1b354ecba0b512be7db9
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:09:49 +0000

    wash education charge damp strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_length(array):
+        return len(array)
