132026401f9cfafb992847569f9176cd5bd203a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:21:47 +0000

    least smile question sea edge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-name = "Python"
-print("Hello,", name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
