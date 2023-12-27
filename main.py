5c155a1f6e0141da6a73a248c1264982b333b2f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:23:53 +0000

    either right solve worth improvement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
