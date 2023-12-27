3547bd03bc4c9f1e768b125fbde0791c67f473d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:23:26 +0000

    can basic very error message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
