9994037ead573c65d279d6583d4e29785da8341b
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:42:52 +0000

    plan matter narrow advise wherever

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
