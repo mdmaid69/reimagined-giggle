ac2023ae45e047682d1342cb346b38a4e5004958
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:22:17 +0000

    demonstrate worse whistle disturb along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
