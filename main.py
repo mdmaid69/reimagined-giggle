aa71188fdb7a4d5fcef12bebc237637df7dd4f28
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:16:42 +0000

    obtain build relationship health whisper

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
