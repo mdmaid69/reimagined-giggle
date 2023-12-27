4f015c9a70aa618de9418446643ed4e78120ceb2
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:02:53 +0000

    flood stay bet afternoon female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
