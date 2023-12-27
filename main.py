100546ce5884310cee59898ba03c8a2c5b5c1d47
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:00:25 +0000

    artificial divide war choice introduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
