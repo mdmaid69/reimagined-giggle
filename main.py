0933c9ca855be357982007a8514527fbd60e05e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:04:40 +0000

    organize skill work size herself

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
