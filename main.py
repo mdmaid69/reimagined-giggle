7319a0c5f9a27a3dd1628b698d77ed5b2e76da4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:52:14 +0000

    rest replace us trick basically

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
