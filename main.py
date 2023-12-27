bb4c6302636a735ffdb809e0f477a5497bc4d323
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:11:55 +0000

    journey Oo practise value cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
