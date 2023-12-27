65a53c9ce760d0f8e9ce094ce7445c8d9f0e4c39
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:54:22 +0000

    wild above polite debt feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
