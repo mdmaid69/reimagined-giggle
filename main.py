947d17ca26cab94308fcad8d62bb72f69d324376
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:32:45 +0000

    affect building plate between mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
