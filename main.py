45a48fbebeb66964f07b75f1147b503bb8de93f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:37:47 +0000

    sink scrap together turnoff feeling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def add_numbers(a, b):
+        return a + b
+def calculate_acceleration(speed, time):
+        return speed / time
