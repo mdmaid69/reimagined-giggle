73d5548a62e663f82096d7e36bbf784836d1f909
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:39:40 +0000

    along more zero although activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
