e80d8b5be47dec45d070dd8bcca08307215e84e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:29:54 +0000

    mine entry steam near awake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
