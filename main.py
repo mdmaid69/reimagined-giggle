c1d794ceb9e7f99c2e8109f2d06a4e6599591d4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:07:37 +0000

    rise exist face light finally

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
