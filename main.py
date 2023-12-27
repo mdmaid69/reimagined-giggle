fb0fef7c53ac351e845eeab45574aedb21f93c80
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:27:18 +0000

    offend properly feature dance half

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
