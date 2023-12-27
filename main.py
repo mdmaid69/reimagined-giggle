f39b852bfe213d81ee6f35e7b4931922aaf522a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:50:57 +0000

    part bright bag advanced search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
