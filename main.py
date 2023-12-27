f594f05338cce9724718c0d974d7250e297577bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:17:37 +0000

    worse getoff home suck autumn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
