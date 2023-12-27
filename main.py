1a67e5d24beb16808d6c6910befc2b84918aa6c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:00:29 +0000

    experience treat age breakup approval

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
