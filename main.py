29d3944c0c827fa6aa2e4f0d3e4f0af12c6fa99c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:59:50 +0000

    position pencil figure though lesson

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def reverse_list(lst):
-        return lst[::-1]
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
