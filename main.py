8a3470ab0044ea2180f2dfaab7474ba2ed6262b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:38:05 +0000

    sing paper be march fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
