64b8d1fdff4e84f2a10a68d1002222b52b7319c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:43:38 +0000

    art basis trick prevent introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def print_python_version():
-        print(sys.version)
+def convert_to_octal(n):
+        return oct(n)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
