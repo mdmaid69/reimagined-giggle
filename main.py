0a8c5e2e01fee6187282da699bf63fb200c2a8d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:21:58 +0000

    march partner perhaps field official

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
