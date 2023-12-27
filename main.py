56ff80eeaef23db86bf727cc48f874f2f5fdb9b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:31:54 +0000

    you example war someone claim

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import time
+def get_time_since_epoch():
+        return time.time()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
