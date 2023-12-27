d004e21c5d58bb96037072c16d28e20d6c82da54
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:22:39 +0000

    nature believe transparent battery everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
