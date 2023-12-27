1c1835a51940052c30b777676ba5db1eba3143d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:14:37 +0000

    job different look climb club

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
