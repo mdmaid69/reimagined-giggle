83f29a1d4c712906987874e71f8aab0fff8a4123
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:11:12 +0000

    cause slide century call repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import os
-print(os.getcwd())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def convert_to_hex(n):
+        return hex(n)
