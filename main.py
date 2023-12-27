3b6d28733d9569a4d75274e3c8646bae23e2ab5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:54:02 +0000

    grow difficult block into whether

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import tensorflow as tf
+print(tf.__version__)
+import os
+def remove_directory(path):
+        os.rmdir(path)
