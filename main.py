03a3b7fb3ab7e4f56050392fb93631b2c044dfb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:06:07 +0000

    suddenly except compete private prefer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
