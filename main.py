01de8483abb34e73f44aa40725e6db3b534b36ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:26:56 +0000

    pronunciation answer design prefer drop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+print([x**2 for x in range(10)])
