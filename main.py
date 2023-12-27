03b78b853b5533ffcd863213a0a38ac2ce8a918d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:38:47 +0000

    oil 566end lookup detail trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
