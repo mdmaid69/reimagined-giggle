7874e196a08f5aaa2a7dd52e95b06f87860802a9
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:45:35 +0000

    path confusing illness smell very

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
+  def calculate_area_rectangle(l, w):
+        return l * w
