883784ecf9765a278c06e1f8d75973769ce6969f
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:24:01 +0000

    patient way across exhibition frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import getpass
+def get_username():
+        return getpass.getuser()
