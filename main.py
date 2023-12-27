23c346dbe51368efc0cd8f94327afe1bacb8e878
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:00:12 +0000

    zero save bringdown behave contain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
