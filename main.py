f07a9a02ea80d315cd088d6922522236661519e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:39:25 +0000

    shut deliberately never throughout left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
