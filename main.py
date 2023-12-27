50281b6c63dc952b565db0b3072ea0449e2653f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:43:19 +0000

    mix backwards past freedom fill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
