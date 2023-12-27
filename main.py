e156242220944252e4ccef19ecafcc087d32ce95
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:04:39 +0000

    connect move prove exaggerate goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
