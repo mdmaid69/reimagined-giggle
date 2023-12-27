bb65f04151cc929f9c4d4aacd542c655c8fa126a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:46:53 +0000

    free suggest trust whistle exercise

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
