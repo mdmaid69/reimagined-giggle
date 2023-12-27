9ad98b6acd1905cbc11648773485bba16732cb72
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:22:55 +0000

    spread damage damage listenfor container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
