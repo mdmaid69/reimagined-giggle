7b93769cd7bc24a6b42dec9e63247c18e648d3bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:38:46 +0000

    strange breathe finish afterwards human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
