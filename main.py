6279d926cf71c4715e48b8c03c44838d48685198
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:48:44 +0000

    among advantage cruel nonsense always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import os
+def remove_directory(path):
+        os.rmdir(path)
