cf8e3b0f7456a81e0593aa57e3ba7e53e2e346ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:36:58 +0000

    correct area cough bin turnon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
