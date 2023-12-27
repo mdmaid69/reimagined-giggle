fd56670a035f490e756f2e1e6f37891fcf5a0a8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:42:07 +0000

    contents loud forget label introduction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
