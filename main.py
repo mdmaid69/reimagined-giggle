654af443f48b3dff5be9b31adf51b6df5509789b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:48:37 +0000

    argue pen broad myself act

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def reverse_array(array):
-        array.reverse()
-def find_max(lst):
-        return max(lst)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
