cf1abef4e386f9fdb6f79ee8696883a3fdd291eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:08:20 +0000

    treatment position apply them concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
