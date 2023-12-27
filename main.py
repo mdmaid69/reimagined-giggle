125af43e11d4e6702181507682d7d796480ce22c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:58:20 +0000

    previous fond relax question sound

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
