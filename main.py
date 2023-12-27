f5c2b0a301d1a68a3b921f5579297b9553290647
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:33:09 +0000

    workon runover how culture discussion

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def reverse_list(lst):
+        return lst[::-1]
