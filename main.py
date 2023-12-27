f62ca33aec6d460fda6812c35134e380831e3766
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:00:58 +0000

    brain month arrangement interview visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
