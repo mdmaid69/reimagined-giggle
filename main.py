0205533fd800994b932b990e22978aeb10b0f41c
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:33:04 +0000

    Ff him strength opinion receive

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
