8ae33ee32d8301ee8edde6e6edb638a5bbb999b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:20:55 +0000

    previous prize offence makeup perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
