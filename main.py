d5703922ba295ad13dc20d61bf37411208a6e98f
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:43:43 +0000

    proud sweet classroom neither polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def cube_number(x):
+        return x**3
