9124f9bb22d6b38fbbb7d500b3ba9cd155deb300
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:12:56 +0000

    city trick possession culture settle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(lst):
-        return max(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_set(array):
+        return set(array)
