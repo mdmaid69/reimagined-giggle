78e0b79032a336bf0a9adbe2d91a011bdbb15a63
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:57:33 +0000

    beginning song weigh plate getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
