be0cc371d921b9ddbf2fc3b2e8919ca9c9686e9b
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:35:56 +0000

    remove without short open sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
