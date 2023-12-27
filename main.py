efb029fd14d6486f169cf3bde5e9eff2678e05d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:05:14 +0000

    such Dd music priority thick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
