978e3297cdb6a604c5601e046efe0465addb8501
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:30:09 +0000

    develop sister compare occasion dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
