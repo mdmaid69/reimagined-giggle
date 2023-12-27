2a7ec754095547b1486ee8f752395bdab02b23dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:41:24 +0000

    press region break conduct increase

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
