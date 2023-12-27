423641b141f290781d0a64b666b0f34d37b0f412
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:04:19 +0000

    concern responsible wake cool grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
