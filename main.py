17258cc0b0b81d484563cc7c5cd608eb4d26577e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:26 +0000

    any table definite invitation own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
