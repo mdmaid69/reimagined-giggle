39db06586f82f2751d9af7f1557b2f13f4dbb35f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:11:30 +0000

    exam peace trip pick strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def count_words(sentence):
+        return len(sentence.split())
