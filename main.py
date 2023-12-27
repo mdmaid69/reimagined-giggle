dfa9e4843a4352052c728f91679453f68fb09c50
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:09:57 +0000

    turn late deep right angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
