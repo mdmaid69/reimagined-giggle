0a9ab529caf1e5ded91ee2df2d0bfdfbbf305197
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:06:23 +0000

    pretty loan trick bunch probably

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
