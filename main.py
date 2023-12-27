bca184c6b9e124d4a6da0f4a4379af952ec35408
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:52:11 +0000

    happy nowhere attitude attempt breakdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def delete_file(file_name):
+        os.remove(file_name)
