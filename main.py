b2a92ccc40c050003febebcb703c01c65709a286
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:25:35 +0000

    quick start fall interview unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
