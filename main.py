bd3ba6b4473aff1cb04a4fc6189ca8f7f94f0441
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:19:03 +0000

    report length tear inquire machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
