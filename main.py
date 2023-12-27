71db7b0e692e9f19339276accf7fc4b1d8a8e242
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:31:40 +0000

    dish sound drink gate taste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_power(work, time):
+        return work / time
