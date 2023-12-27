4ba79cf0810111ada8f05a27684e0844fa8e9229
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:40:54 +0000

    choose murder wash land secret

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print("Hello, world!")
