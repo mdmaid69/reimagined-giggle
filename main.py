80520f40341b0be530fdee55e9016fb04a84e0ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:10:40 +0000

    false shall appearance sleep violent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-print(time.time())
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import getpass
+def get_username():
+        return getpass.getuser()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
