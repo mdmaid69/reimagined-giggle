637f94abbc917f1880a6c455987b4d5696989afa
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:58:54 +0000

    stage inquire downstairs funny summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_power(work, time):
+        return work / time
