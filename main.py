af359cc43f2abd59d2adfd6f1c259253159d5067
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:29:44 +0000

    please front explain stamp yours

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
