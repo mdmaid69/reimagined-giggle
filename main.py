d153e75905c0185fd96bfea21ba6d68696fc9f1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:07:11 +0000

    juice offer process separate can

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
