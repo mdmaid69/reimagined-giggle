46b8f10b432740a1236b2aa76d4dc0107cac6ba4
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:52:36 +0000

    wave please quick occur standard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
