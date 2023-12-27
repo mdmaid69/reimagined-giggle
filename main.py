83ff4c4c909b0802bc6c4f4daf25b7983ed6e31d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:51:03 +0000

    air memory difficulty crisis special

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
