8a6da64f6dbb3b2a4fcf315b930d76a2fdb287bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:57:10 +0000

    home card mean software aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
