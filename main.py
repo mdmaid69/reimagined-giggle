6fe639376b01b3b7f7bfb510cec2e62590c9aa6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:06:31 +0000

    can minimum last thinkover home

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
