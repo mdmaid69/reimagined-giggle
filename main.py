f9fd7ef7f1424f25eb0c4f1c881f56e2d712dd4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:58:18 +0000

    hers total address neither know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
