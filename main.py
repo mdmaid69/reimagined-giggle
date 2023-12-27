d9603547350d91b3df1c969a1f1e44b169c689ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:28:50 +0000

    race unfortunate mistake female new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
