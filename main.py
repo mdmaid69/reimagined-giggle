0f240ca7666d4fd2a4331ac87c7998a4f08371ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:08:10 +0000

    ilovedicks this impossible ask account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import json
+print(json.dumps({"name": "John", "age": 30}))
