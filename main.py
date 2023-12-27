add41935c729ab5f3d7546d9483fbd9bb2c06bd3
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:40:57 +0000

    standard increase alive test reserve

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import time
+def get_time_since_epoch():
+        return time.time()
+import collections
+def create_user_list():
+        return collections.UserList()
