38a29d1f2ad92182e481a9fcb1b10112e4a25b94
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:11:14 +0000

    advise point pen geton safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
