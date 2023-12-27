39d4903a8d53606dcc375b353428e332fb1d939b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:11:50 +0000

    minor law glad keen chase

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
