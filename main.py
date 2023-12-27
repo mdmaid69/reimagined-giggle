9c9f1fa648988869114645dd18b908bd06996b07
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:39:25 +0000

    clean more usual work disappear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
