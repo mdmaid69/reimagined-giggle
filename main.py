a7cd520f5492d385e9f03060d8fcfe3f54315a6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:45:54 +0000

    plant whistle mind each getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
