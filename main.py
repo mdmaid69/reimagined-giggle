e42b808fdc53719b707558505b46ddc0e5b3e90e
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:52:24 +0000

    traffic closed dig fire air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
