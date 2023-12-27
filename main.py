e358e864e523c62590ecc50fdcfe701572285cdf
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:35:53 +0000

    persuade description getout children forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_pressure(force, area):
+        return force / area
