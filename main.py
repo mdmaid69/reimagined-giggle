46ffd5e0f34f2adbc6352e9fdc9eca8c51e382ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:59:44 +0000

    road another pleasant stage turninto

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-for i in range(5):
-        print(i)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
