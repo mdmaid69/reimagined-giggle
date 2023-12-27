f008c6228d356d1808e82abec302b64037ca1f2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:25:28 +0000

    date runover supply interview once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
