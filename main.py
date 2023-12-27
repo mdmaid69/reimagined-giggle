3482f9e69717e888c14bf1f013448ec6f69dc61d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:57:55 +0000

    sound comedown behaviour tape crime

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+from collections import Counter
+print(Counter("hello world"))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
