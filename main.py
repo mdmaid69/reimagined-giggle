6c817f437b73cc3a6f4d06efafbd4b6e6a7efa04
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:19:40 +0000

    mood in meal ride contribute

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_current_working_directory():
+        return os.getcwd()
