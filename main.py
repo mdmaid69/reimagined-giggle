62fadb0d90477e5d01d3e5c0b086863bc8b4c964
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:22:33 +0000

    his bunch pick turn arm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
