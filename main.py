64f07adde53ab1c08c64699904a9c5a968aea3ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:23:11 +0000

    copy settle schedule criticism no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
