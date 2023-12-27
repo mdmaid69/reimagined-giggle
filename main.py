907e9e22c22ee37391eca3528ee4d721ce605d27
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:02:54 +0000

    practice curious into yours date

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
