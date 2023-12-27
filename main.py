11b8f67b043a3282fc4e7e251a50876500ec68a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:24 +0000

    usually unite future buy sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
