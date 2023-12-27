4b5600b8a7b816844c07adee2a0b45495adafa67
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:40:17 +0000

    set equal it popular cold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-print(sum(range(10)))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
