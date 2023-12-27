2aff5e6bf9f79da23376860074fdb2453815c88c
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:44:25 +0000

    concern child of difficult reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_rectangle(l, w):
+        return l * w
