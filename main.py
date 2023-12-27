f1db9bc22325d01d947b0768643b138eecff14c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:57:01 +0000

    think last treatment Yy personal

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-def print_python_version():
-        return sys.version
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
