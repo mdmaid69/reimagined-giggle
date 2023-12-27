424182e9fb617925e357e47c24e2f1b3ff2210fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:55:41 +0000

    unit form convenient you translate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
