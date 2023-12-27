447a67dfb608f4edf0a02ce2d58fc7b4a07565f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:26:50 +0000

    birth hurt spare less its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
