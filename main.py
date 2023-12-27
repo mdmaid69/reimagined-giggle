1f3ab908035daa103f0dd57e49b272fc1bf018e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:29:47 +0000

    wise post be argument protest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_volume(length, width, height):
-        return length * width * height
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def reverse_string(s):
+        return s[::-1]
