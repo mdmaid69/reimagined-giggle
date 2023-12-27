215ff38707bb63d1ab9a1cc3d94fcfd6dbc3b6cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:57:04 +0000

    by passenger goon wonder doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_pythagorean_theorem(a, b):
         return math.sqrt(a**2 + b**2)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import collections
+def create_user_list():
+        return collections.UserList()
