82c9761c852ce7204192fcc19883eada4774899b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:41:35 +0000

    workon Ss flood rain uponon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import getpass
+def get_username():
+        return getpass.getuser()
