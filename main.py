d236bd0cb41cccbc19663153ec31f8ba428e13ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:28:40 +0000

    state level smooth store wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
