388c7bcd6ced4e23fb51b469c1c78059a7dfba3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:39:02 +0000

    uponon detail brush warn mail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
