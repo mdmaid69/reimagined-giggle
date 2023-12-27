ea30ad0c1ab1f15142650bfc67cbac108ef821e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:17:15 +0000

    seem comeback flow famous commit

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
