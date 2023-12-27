2fb6e5eefcd9ded430ed3ed4dc845e4b5d1f5cfe
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:59:46 +0000

    sell best this cruel describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import os
+print(os.getcwd())
