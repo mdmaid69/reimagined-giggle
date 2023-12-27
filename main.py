9303847484888aa3ae8686a719e6c354de630f3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:16:25 +0000

    repeat contain something avoid move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import json
+print(json.dumps({"name": "John", "age": 30}))
