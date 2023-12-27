514f773b2b428016fb47b536ec7405167de801ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:42:25 +0000

    female skill your luck instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
