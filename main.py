484ad57ec502250fe6269eed316ba010ccd15ff5
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:55:39 +0000

    display whenever taste combine plenty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
