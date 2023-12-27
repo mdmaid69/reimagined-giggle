6810eea771246ac8c940850f9a292cace8c958ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:09:50 +0000

    bus conduct getup undo better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
