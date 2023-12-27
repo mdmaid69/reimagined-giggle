c605507f61a0771888e85ccb518225432fa2b788
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:21:54 +0000

    dig cap ought edge equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
