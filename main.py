fd84bbe48f6a282fb772e805e341e4ef258c0a54
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:15:05 +0000

    achieve as discovery opportunity underneath

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
 n = 5
 result = 1
 for i in range(1, n + 1):
         result *= i
 print("Factorial:", result)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
