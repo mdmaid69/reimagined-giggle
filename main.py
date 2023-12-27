3cf09d246f84b1ddd46255f95ae29ae74caa1d27
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:37:21 +0000

    far finally tap soon cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
