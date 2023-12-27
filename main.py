18fe4898ec12e2cc4c3a0f6db9e93d5495d91d2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:15:26 +0000

    broad exactly belief theirs bridge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-for i in range(5):
-        print(i)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
