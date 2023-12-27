790f2c24906ea8501a1919dbddd3669696cb6e4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:37:15 +0000

    than permission right meet subject

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
