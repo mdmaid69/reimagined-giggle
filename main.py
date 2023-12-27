b87e4eccb3617e79b515b1c181096cc52024b3d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:47:36 +0000

    immediate inquire tense hurt over

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
