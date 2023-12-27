10ef9148ff40c8f33f0c4be1ec56a1d94911e030
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:54:12 +0000

    guilty comfortable than we serious

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
