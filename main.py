32cc11b5ff7eceea48556746ee1fc4ce87b332e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:05:09 +0000

    of blame might Ff autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
