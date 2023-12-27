cd808392cccce34a6a66f1940149d25512e8972e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:19:12 +0000

    hurt dead stop top material

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
