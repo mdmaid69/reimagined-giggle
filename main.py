e294890c9384c18cbc5cbab324c9d4337d9a09f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:53:11 +0000

    declaration much description brush hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_repr(array):
+        return repr(array)
