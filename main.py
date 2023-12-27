7d3bd522f9d24e45ed0e27b1c14a179c73311b14
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:34:51 +0000

    laugh and should usual speak

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
