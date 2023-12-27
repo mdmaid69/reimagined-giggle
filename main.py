19d593f92c5a5775819dc0f514c1b1022c5ef39d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:46:44 +0000

    repair rest either hide journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
