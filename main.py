fc4860ff1417c2a3d74c72e61369077d78add063
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:10:47 +0000

    see plenty second treat thin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
