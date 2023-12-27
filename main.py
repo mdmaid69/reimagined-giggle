1eb8b969915985ec3f8f295b2fd87e3ef3191582
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:19:12 +0000

    platform declare rely string defend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
