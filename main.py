77e0305facf96a8879d68e13f8e739d9f5d14671
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:47:40 +0000

    object second owe destroy make

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
