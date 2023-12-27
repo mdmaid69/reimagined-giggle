f38d8791afdf8bd9be25d00af869d7e81396f35e
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:41:57 +0000

    exact content fill actually prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def sort_list(lst):
-        return sorted(lst)
+def get_array_as_bool(array):
+        return bool(array)
