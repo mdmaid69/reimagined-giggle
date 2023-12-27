736943670bfa7bd6bf97326f4afeb34c3b60d6e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:36:41 +0000

    number rush yours rubbish offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
