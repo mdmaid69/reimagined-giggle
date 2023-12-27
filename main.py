c57bd5a6c85dd49c5fa3fe5179f04aa02820c116
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:34:39 +0000

    hold difficulty deaf model care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
