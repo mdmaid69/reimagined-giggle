4c7d1ac3028896bdbdbba4e8712c5c6e0ca7d1b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:02:00 +0000

    fit comment smell whom recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_str(array):
-        return str(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
