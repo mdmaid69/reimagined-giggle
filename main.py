6026e4dccbf17df0f19cbef4578bb863b2004042
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:15:09 +0000

    person bet value squash turndown

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
