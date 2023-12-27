026529fdc77296fd56fda72d78c5af1d91ef097f
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:48:31 +0000

    occur down clearly scale figure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
