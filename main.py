584bf7c8ed3f3d3a038c742355b61494522541df
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:04:02 +0000

    possibly turnout Oo pass remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
