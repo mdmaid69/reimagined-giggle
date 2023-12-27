3df0af3964746ef1906f7079f7f8cd0c590838d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:17:35 +0000

    book Gg sink stop fine

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
