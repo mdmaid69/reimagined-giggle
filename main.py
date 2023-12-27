50ca087b9ec2d7fde6d12087c46b45ebf9f852d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:24:03 +0000

    area laugh born own trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
