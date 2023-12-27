8b3ab5739d7cf498e767b6d8c6ca3beabe9df655
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:10:47 +0000

    score match stretch ground abroad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
