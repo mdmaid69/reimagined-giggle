74ec0c1a2f4e8967df689ce17be98b493d4934da
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:31:13 +0000

    joke sting plenty butter event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
