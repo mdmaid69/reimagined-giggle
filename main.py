00cf6548b6bf824485d8be59cfc0733c03918c66
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:11:38 +0000

    weather smell house once organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
