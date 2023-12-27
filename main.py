602d89f1c659af99b73b63a9a01081d25d1d902c
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:31:52 +0000

    likely Ll goout rough grand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def count_elements(lst):
+        return len(lst)
