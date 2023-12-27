6ea3d616e19df9d00ffd0e611311b42c666061bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:41:59 +0000

    present intelligent doubt throughout warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
