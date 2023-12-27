5eb9f832d322430fe8b1dd0e9b539ce03e43905d
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:58:49 +0000

    comeup arrival finger certain size

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
