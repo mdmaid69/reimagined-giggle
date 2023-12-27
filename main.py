2a5fd7e2bb8ae92e55bb5c7910af19f821d20f6b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:32:23 +0000

    pattern board notice stage continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
