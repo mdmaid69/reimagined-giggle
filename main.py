a27314da867c8838f42f41eca27b963de4d2bc48
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:26:59 +0000

    tell against turnback present underneath

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
