b982d72b7deeb98f7fccfbfd6a248d9cea2f5b31
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:11:43 +0000

    plant contribute bang uncomfortable absolutely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
