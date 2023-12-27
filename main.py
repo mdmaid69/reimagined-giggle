4dadbf8871debb22b5cd046c031f5f73827e7264
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:58:15 +0000

    wide experiment move tune rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_base_name(path):
+        return os.path.basename(path)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
