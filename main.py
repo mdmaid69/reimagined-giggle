6b8601b04dd31593ceba2f6c7cfc2dd27efd5c88
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:33:04 +0000

    pinch instrument model subject start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_python_version():
+        return platform.python_version()
