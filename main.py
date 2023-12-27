b0a84cccf997fcc22c768cc93694723c87e80be3
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:27:05 +0000

    high thank rule tip impressed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_list():
+        return collections.UserList()
