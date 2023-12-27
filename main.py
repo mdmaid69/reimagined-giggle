52c1e67e9e128056bbb8fc76c1cfcd42c13ae25b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:48:21 +0000

    answer greeting I seem year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
