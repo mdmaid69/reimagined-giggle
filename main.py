9f420d631c64fc9736ce1ac2fe7b303032746b0e
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:35:19 +0000

    proper giveaway sound float float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
