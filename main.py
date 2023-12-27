aba68fd206409cf972d6d80b40276d29966f2c58
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:32:52 +0000

    physical makeinto die use admit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def cube_number(x):
-        return x**3
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
