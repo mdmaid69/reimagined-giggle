d62cd7ebcd16ad3809d102acfe12e481cd88934f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:19:38 +0000

    bottom sensitive imagination manner he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def square_number(x):
+        return x**2
