3b179ee97eeda32eeff5981e8a7f0653857ada45
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:18:30 +0000

    wide bang intend neck neat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
