1851e8f5d29cd9953fde533dfa58141ed87cdf3f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:14:47 +0000

    fight turnup behave tap offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
