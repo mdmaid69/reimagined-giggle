248c4e5924ff2d158528ae76d083b175655bc740
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:27:30 +0000

    office 569enjoy team active touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
