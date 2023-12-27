cc5552a1120b69d14a13c57085fd7bbb7b6ac8f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:43:45 +0000

    satisfactory perform comeround activity match

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
