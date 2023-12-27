12b9aa513b7f856ec8df9219d82ba156e5f9cdb0
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:54:57 +0000

    spray row describe base pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import time
-def get_current_time():
-        return time.time()
+def reverse_array(array):
+        array.reverse()
