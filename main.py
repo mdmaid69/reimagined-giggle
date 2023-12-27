451fb87f409d1dc28c84f7fe654638e57f953f06
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:02:58 +0000

    arrangement region could shade quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
