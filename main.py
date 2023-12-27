44fcc2aa211cb4a093b247a3e391b748901eece7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:42:34 +0000

    laugh confident come phone see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
