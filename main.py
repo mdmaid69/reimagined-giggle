16a8b7f609f0f2dbd54c886094560eff1f1e9d2c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:55:39 +0000

    transparent anywayanyhow related culture Oo

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+print(datetime.datetime.now())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
