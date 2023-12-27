663c792424532f303b716c0b16de35492a57e363
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:17:29 +0000

    above shout push steer butter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
