c242c3b96103a8b5e87861e69b3edc943db93219
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:05:38 +0000

    add still different well stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
