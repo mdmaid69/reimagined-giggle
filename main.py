b6dc6391b7aabe4a137300243df1f840343b08dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:27:43 +0000

    break count favour mix mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
