6567dfa6fa9962fc66089f7d150a73e1fa537c51
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:33:00 +0000

    type sometimes single rain imagination

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
