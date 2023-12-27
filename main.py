0fb0a11b0372c92202eaaab212f087967d6d19e5
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:49:28 +0000

    scream die swim track decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
+import sys
+print(sys.version)
