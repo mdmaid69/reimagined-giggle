e0a9a853c2b7d64975c4f4e28a9f74cd5c8aff11
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:45:08 +0000

    passage big interested raw tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
