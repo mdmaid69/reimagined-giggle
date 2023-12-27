cdc4ddd29ba921305a9048c9357f8f686b7d4dd0
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:57:01 +0000

    apply threaten scrap make really

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
