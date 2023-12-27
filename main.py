dbce2628d77fcbbf28749b842aa6ffc07b1d1fa2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:18:02 +0000

    179bite house loose bound quit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
