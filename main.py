71800c45b9c590da93e5280d8b02c5bbe1715669
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:50:37 +0000

    table mixture suffer cry ring

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_index(array, item):
+        return array.index(item)
