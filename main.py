57d14f700ac7c8e5a9eca3030c0d9adc05a85b35
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:09:00 +0000

    work tidy shoulder children comeout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import os
-print(os.getcwd())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
