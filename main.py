1e95b840c1c70b33e5f5009acc02ed7aa4e76912
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:47:17 +0000

    destroy laugh distant trousers complicated

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+def get_current_time():
+        return time.time()
