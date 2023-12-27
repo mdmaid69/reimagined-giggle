bece8f2325667216430618b88578795d2cb3bb2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:13:33 +0000

    forever discovery spite better entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
