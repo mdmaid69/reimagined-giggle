c4e32b6192d0d87e304229a7d3d8cd317e142380
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:32:04 +0000

    suit mood middle weigh turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
