a5ef37c1b71711365273d8d28b03623cb084e76d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:31:31 +0000

    firm brother beat 566end little

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
