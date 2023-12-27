f49640e40a1cfa1c841471730dcb297f48c9dcbf
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:09:54 +0000

    interview relative when impressive try

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+for i in range(10): print(i)
+def is_odd(n):
+        return n % 2 != 0
