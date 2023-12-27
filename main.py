dd3f7e4eabcc30566b11d0fdbeafc6b2500e0e7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:18:05 +0000

    they lucky silent nice reply

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
