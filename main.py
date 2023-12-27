226b24fdc78d460ceb108153daee1a4b014312e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:59:52 +0000

    majority apparently hole thankyou nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
