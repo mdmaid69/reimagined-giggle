a418572ba02fa962857898a95a80614277dd9f64
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:25:00 +0000

    Oo share brother enthusiastic stiff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+print(math.pi)
