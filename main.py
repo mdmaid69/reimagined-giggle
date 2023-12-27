f3af6fc360f7a502a6aa6432087aeb9ba8d9fc4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:03:46 +0000

    note damage trip speed success

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
