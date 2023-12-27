024742d75d396a6403d2309cd344618d33db2569
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:04:10 +0000

    introduction forever talkoutof keep control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def set_array_item(array, i, item):
+        array[i] = item
