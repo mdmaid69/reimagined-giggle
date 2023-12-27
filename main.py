87a18262688921e9f3fa91da77b890505b17e6f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:51:22 +0000

    print beneath fire disturbing free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
