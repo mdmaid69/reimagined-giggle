09a6c92ce8bc4afa616983442aaf890f898bda60
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:36:35 +0000

    among related in almost thick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
