b07a96d402ef50733fce1f496c193c6ea8d662d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:07:26 +0000

    contents judge discuss hers dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
