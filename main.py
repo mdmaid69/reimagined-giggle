02cc5c0149eec6ef07dcb9c0c88e02fcb2a9e51c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:29:52 +0000

    everywhere crisis turninto rain already

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-name = "Python"
-print("Hello,", name)
+def calculate_density(mass, volume):
+        return mass / volume
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
