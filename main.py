ebf9b1e25f401b527686bf7a3079f387344d63cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:22:22 +0000

    then open family plan geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
