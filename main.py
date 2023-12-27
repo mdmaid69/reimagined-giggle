17b5ffa2c6bfa3a7ea72ce9d27c52672c0f49966
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:41:36 +0000

    Bb lean appeal if language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_python_version():
+        return platform.python_version()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
