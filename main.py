3ec77a885e83beb07e4d090ca3e075575011e6b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:59:28 +0000

    pupil government straight switch happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import sys
-def exit_program():
-        sys.exit()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
