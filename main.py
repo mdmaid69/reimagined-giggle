706c9e04d31c182590d430eda28fe52700096cca
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:57:18 +0000

    no around spin grow lookup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
