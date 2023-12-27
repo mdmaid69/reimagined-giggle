cbb6de63592e3d504a47d44c31dd924a721d6fa2
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:09:29 +0000

    just protect finish shock unless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
