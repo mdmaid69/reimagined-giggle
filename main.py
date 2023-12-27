619a544181f44d5b56e160ecc2cfbd36da24c280
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:03:09 +0000

    free pleasant forgive holdon arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
