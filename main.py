6d94141bbe2d39ab60b556cc2a75e45f983c3044
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:22:23 +0000

    arrangement deliberate coffee tradition honest

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
