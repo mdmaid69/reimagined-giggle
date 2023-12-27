c050c215bab79d5f0c78afa5f42dc6424b0facd8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:02:48 +0000

    exploration bet stare autumn leave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
