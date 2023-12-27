fa3ae6c18dc1b6490776b39c14549e7421aaec02
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:26:51 +0000

    climb minimum think rid rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
