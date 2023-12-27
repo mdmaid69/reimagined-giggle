0a2293e00e1be981657502e092b8dde24bdca247
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:19:57 +0000

    common slice bunch steam traffic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
