35d4b853bcc15b8999f10484cf781c9c29735c14
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:12:04 +0000

    fire nearly dance approve need

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
