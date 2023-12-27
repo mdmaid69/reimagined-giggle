15aac4602982ead7dfbe29ef532a7d0596d7a244
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:14:03 +0000

    Hh show search relief pause

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
