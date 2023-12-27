db49200f4d18aa5c8ffa3a7dc954fd01026a85bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:41:27 +0000

    moon dress inform performance precise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
