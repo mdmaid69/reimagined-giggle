562fe0fa685e09ab3c656d325e0464fc49fef2c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:46:36 +0000

    world foot colour spot minimum

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_as_repr(array):
-        return repr(array)
+from collections import Counter
+print(Counter("hello world"))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
