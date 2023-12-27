dbb997b5220c4f86f84f1684c2e9beed973e77d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:31:57 +0000

    steer still better bringup eye

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import collections
+def create_stack():
+        return collections.deque()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
