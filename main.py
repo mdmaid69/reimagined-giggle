50d5d333a9585aaabf3a386ffcfb4b9ac32d7063
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:52:20 +0000

    abroad strange port stranger corner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_bool(array):
+        return bool(array)
