d4e7853cf69a9a0d012be3a3faf0261ca2272c3c
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:42:44 +0000

    impatient opposite exactly get sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_binary(n):
+        return bin(n)
