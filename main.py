86cf59953743eef1caac5ee847dfdee2b98bc0c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:58:15 +0000

    busy cost zero many complete

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
