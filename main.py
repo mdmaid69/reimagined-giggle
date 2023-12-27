ff03769a21cf48dfbdf77f07220e6d256142edc0
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:54:49 +0000

    spoil which petrol club surface

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
