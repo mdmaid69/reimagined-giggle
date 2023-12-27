03b78508a28e36dddef63d84dec432a190028693
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:13:39 +0000

    geton exactly cut slide opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
