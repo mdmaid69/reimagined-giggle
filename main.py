ad2f4577907a108b00b4392336cbe92933ea7c71
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:30:41 +0000

    centimetre office only container separate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import random
-def roll_die():
-        return random.randint(1, 6)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
