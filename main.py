c1f7525913c7514c8bbc91771fc52fc6948307d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:08:26 +0000

    prove lookup priority strong father

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
