2e8e68d98fcaf58464c7d9349491a66f24bab00a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:18:54 +0000

    post goodbye yesterday shopping state

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+x = 10
+y = 20
+print("Sum:", x + y)
