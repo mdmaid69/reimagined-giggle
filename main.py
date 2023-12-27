16ebbd56f18c0ab750be9156477aa302d23f9f48
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:16:51 +0000

    lift fast environment 571enough rush

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
