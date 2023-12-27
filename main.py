db50a7bb93d4f7d6484a150a04ce77841c876761
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:51:19 +0000

    benefit include embarrassed hungry boil

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-for i in range(10): print(i)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
