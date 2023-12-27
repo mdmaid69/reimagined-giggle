a85bdf4d9591963ce629718fad340a117f1a0a0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:39:58 +0000

    exchange part exhibition list benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
