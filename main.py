4666c20405d8c5ac650d5c156118c4cf68d82a05
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:10:12 +0000

    warning book confuse punish second

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
