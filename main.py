dcef3a0b370cb08bedb164ab402677bc68173b02
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:37:51 +0000

    cap maximum leave embarrassing early

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
