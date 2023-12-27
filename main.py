8257c28ff938c1cf724a7838eb7045cc6a78973e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:34:26 +0000

    mysterious load busy check sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
