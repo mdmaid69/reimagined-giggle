e9df9333fff88a944b61640f9624a67f013e9f87
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:30:54 +0000

    smoke expect used shade month

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
