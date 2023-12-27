075f9c096e06722557882ea76b478db00e51abe8
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:26:48 +0000

    goup past better comfortable see

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def reverse_array(array):
+        array.reverse()
