9b5ddd708360c89aae626b704d71b233eee296bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:25:00 +0000

    Uu scrap dance Ss part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_area(radius):
+        return 3.14 * radius * radius
