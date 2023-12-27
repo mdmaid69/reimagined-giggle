c0536f6be2cdcba223c85a1f50960162cc253bf7
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:30:11 +0000

    hard pump hurt fall dress

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
