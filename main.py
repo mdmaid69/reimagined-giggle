2f10a092bfd472f0eafe7548be8a43c759cf9888
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:30:57 +0000

    one injury invitation goon scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
