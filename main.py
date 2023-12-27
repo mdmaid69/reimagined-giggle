b3126bf647a1f311c0d0c7bc6d0e422069972e6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:06:44 +0000

    stone belief fresh remove taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
