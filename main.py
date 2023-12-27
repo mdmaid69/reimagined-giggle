d1ad168044562e80700a3bf50b716590a4b91f66
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:33:58 +0000

    goback(to) everything know issue forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
