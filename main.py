18706818208f313196a9a91f5054cf9a3ccc2ca6
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:31:54 +0000

    healthy hour walk journey just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
