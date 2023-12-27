e7fcb695add8d86d15a36a401ad1e678d0ecbe05
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:45:09 +0000

    escape contents instant march juice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
