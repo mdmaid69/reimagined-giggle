71a45a6e794674dc261b87aba5af817da5fca768
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:23:53 +0000

    throw soil sea describe bound

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
