e7a049f109076364a368f49cf5abbc6f3b173462
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:20:10 +0000

    crash rubbish instrument sale level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_length(array):
+        return len(array)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
