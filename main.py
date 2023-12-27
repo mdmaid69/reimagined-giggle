0afc7d6bb89e4724c98ba595e431946ed94f61ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:04:39 +0000

    getaway rank hole exercise test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
