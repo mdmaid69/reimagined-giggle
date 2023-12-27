761525a5aeed368f0bc05043c27144c56e908041
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:42:21 +0000

    post soil many approve bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
