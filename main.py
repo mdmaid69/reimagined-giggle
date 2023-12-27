3ac49c4799e4341fe2ea42d3f2491334d8476ee5
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:09:21 +0000

    play fortunate table soon task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
