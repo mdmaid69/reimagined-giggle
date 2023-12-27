08ecead0d794a082a0e532c67c4e315b610609d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:37:43 +0000

    butter board almost Cc Oo

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
