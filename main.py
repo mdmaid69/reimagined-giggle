ce32f38c9e874933e2226f02c231bc21a90564d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:56:01 +0000

    off getoff all characteristic involve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
