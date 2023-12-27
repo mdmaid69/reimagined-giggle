2bb9c13989bcbc2ee96cfbe244bcf60b7b22090a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:15:09 +0000

    ordinary workupto owe among start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
