f9df03946f8f988bdfe027032e2492265155d770
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:39:54 +0000

    usually whisper runaway dry reach

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_ceiling(x):
+        return math.ceil(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
