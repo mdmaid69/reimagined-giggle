23ac955ced7d7a9be72f4b579f536d49632db4ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:13:41 +0000

    drink passage odd blame do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
