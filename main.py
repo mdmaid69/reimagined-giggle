9e59d84025033df22e9947fb813e850fad3d5c4a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:55 +0000

    my runaway instruction shame area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
