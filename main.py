124ffb27850995e7dd6702a64f6fd7ab3df88ce4
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:19:06 +0000

    judge blame ride hide compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import sys
-def exit_program():
-        sys.exit()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
