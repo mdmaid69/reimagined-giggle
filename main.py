5bb25457d7cb794de9a31b14c4f2450a3a5d3591
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:36:56 +0000

    description possible combination exhibition blue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def convert_to_hex(n):
+        return hex(n)
