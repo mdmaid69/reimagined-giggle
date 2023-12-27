babe84583922bc07929765eb786c46cd70a74bac
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:24:28 +0000

    continue satisfied deep science present

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-name = "Python"
-print("Hello,", name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
