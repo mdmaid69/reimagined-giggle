d6a35845714a40a87f0fc02089562db6d22356b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:17:01 +0000

    split season crash keepfrom region

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
