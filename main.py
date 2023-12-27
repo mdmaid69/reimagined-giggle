2b8f199ee4cd64ad64a3f3866c4e00f1c59ed64b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:21:06 +0000

    arrangement tone admire motion valuable

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
