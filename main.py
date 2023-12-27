798b420191e1a1d348f8b9397914ac87aa4dd98e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:35:44 +0000

    quantity help burst sink often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
