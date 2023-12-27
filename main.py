e33143c81196757c780beb57e0e8a3ae5c045921
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:06:54 +0000

    surface along mend understand mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def square_number(x):
+        return x**2
