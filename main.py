d644ffedb0be202f9f556ec19a8c287f4f6b6d6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:17:12 +0000

    disagree ought whether minimum squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
