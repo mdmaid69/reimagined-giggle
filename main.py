e08ef3d94903594326cd5fd845ca1f4a4bea8515
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:06:17 +0000

    sale history catch compare calm

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_max(numbers):
+        return max(numbers)
