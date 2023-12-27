dd1ea5ee98d0622459ab1cbc30c635ba80a8c072
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:28:34 +0000

    left command table clearly prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
