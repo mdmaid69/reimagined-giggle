e1430ce8c695b28cdf1c8f31ee34489aa83eea54
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:49:05 +0000

    since convincing choice tight bit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
