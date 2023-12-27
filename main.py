54338108fd9c352fe8fb90894cb72c16eefce734
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:38:02 +0000

    on crash cross around cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
