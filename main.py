3f5839d1d7a13d1d5ae4cfa0cea60bdcee31d481
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:53:11 +0000

    safe nurse sick pick best

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
