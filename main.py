3b533073c9e30ef1e3aefe0d1d7503fa652906ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:12:04 +0000

    properly 572enter giveup sorry careless

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
