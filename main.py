d3c7d050c009d0b1de4f078e065f2a8974f69439
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:21:02 +0000

    stiff I bring well fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
