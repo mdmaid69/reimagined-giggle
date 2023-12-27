2e26b0835bf19ab0b2333d65f06e40a4bcdadb56
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:55:18 +0000

    success fall light smell square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
