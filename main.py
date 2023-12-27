ccef17e0ec557893656dc8d3c94753a43a04e384
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:36:02 +0000

    row grand exercise best collection

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import getpass
+def get_username():
+        return getpass.getuser()
