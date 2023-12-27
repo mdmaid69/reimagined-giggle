2ac4fa23564e82cf91cb5e72cd84194a26007fbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:25:54 +0000

    feel deliver room fit pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
