7e91a69e00aa1b2b90d6513daf7a48f73a7809d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:03:22 +0000

    suddenly burn owe guess pupil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
