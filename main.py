da5b02ec8b1aa6acb550a3ed85e71172f982d579
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:08:28 +0000

    battery rid sore door comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
