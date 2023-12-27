70273e59b339afa012cf763e96398117f47db68c
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:11:51 +0000

    split introduce muscle draw brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
