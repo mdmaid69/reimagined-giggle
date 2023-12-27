c1fee71b74a120f8a2a66a6901907ba4331daefa
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:29:33 +0000

    most high recommend whole throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_circle(r):
+        return 3.14 * r**2
