e9104b031385ba9474928af5827db1724b9a85d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:11:36 +0000

    check moral rule bath hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
