bd6db8ad9d9fceb1e8a0ececbccc6bc7c5177ec4
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:05:21 +0000

    pronunciation spring Qq treatment comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
