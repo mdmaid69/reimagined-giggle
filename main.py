3148ee7b21d5d46a2d508e74d7173b902930f246
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:42:55 +0000

    arrival spring example pile like

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import re
+print(re.match("h.*o", "hello world"))
