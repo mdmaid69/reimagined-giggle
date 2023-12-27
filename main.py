8ea4f8c07fd2ad6aa28d36572c3e83de501f1c8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:43:29 +0000

    undo talkoutof bury past opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
