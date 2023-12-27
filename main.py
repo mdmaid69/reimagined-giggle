5715a76ccfe355ae697ce4beffb65a9a54938cae
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:52:20 +0000

    life permission history honest round

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
