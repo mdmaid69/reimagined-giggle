9e3329be33e0eb22970a9f4053a7bf7b24845093
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:50:15 +0000

    scrap freedom freeze soft however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
