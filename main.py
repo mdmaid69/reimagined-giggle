ceee587a9e59aea3fc1802af39abb359a13556ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:54:04 +0000

    well lookback(on) shout habit anywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
