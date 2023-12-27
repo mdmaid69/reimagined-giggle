a29fb23fe2fee5a22cc3264caf4c133b431b16fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:53:53 +0000

    temperature slight mention debt reduction

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
