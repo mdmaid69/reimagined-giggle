5ac2c4a76a7d26e99dd386bdce97f352d05dd2b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:27:28 +0000

    play Hh along waste adjective

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+print(sum(range(10)))
