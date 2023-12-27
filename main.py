5b40ae3ec60177fe3444d67f44ac50815cf7cbd1
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:19:19 +0000

    maintain busy mix field flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
