26e737960b902329086a7ceacb5eaaa6646d4af2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:52:33 +0000

    next switch worth however fierce

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
