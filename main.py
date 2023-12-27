5f9166897831b7bb763ad44d91ddb264124eeabb
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:26:23 +0000

    fault goaway either cut around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
