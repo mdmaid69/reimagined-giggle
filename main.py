35d834257359c13375ef5b1cce00f33611f25194
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:45:46 +0000

    survive everywhere tongue equal software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
