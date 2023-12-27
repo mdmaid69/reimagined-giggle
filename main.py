5750cb5ecbb7ab4359a8f363fc825eb945e35a5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:18:56 +0000

    anyone aware during convincing warning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+for i in range(5):
+        print(i)
+def calculate_area_rectangle(l, w):
+        return l * w
