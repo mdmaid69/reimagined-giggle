6b1a8a9cc63aae20451eb2bc0eced2e312cf757e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:30:40 +0000

    sign giveout practice little still

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def greet(name):
+        print(f"Hello, {name}!")
