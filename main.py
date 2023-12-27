25a2a25d710caa9834f61e2adf797491cecf2328
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:06:50 +0000

    weekend phone earth clear neither

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
