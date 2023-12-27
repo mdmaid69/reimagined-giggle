42fecbfeb7640a1e786d23808f2ec9e8b1652254
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:15:21 +0000

    money swim Yy instrument decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
