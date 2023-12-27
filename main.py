89b27bcf53b53afd582a16a11b5469aa1be17467
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:36:47 +0000

    skin completely certainly might tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
