5e3b4dd17dd9b4bcbe7844783d10077227fdbff0
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:04 +0000

    way straight service hurt though

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
