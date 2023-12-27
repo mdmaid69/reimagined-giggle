d5c6604692c3cf713e040b14cbf21b1f9729556f
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:34:01 +0000

    boil peace keepaway long finish

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+text = "Hello, world!"
+print("Characters:", len(text))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
