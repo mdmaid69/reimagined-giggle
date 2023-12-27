2b11585bb54183d8175dd9fb34b2e036fa9526e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:46:27 +0000

    evidence risk thin score pretty

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
