e6c87288b5fd2c3700cc147975e8bbf08428fb8a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:22:03 +0000

    surface tongue tax confused greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
