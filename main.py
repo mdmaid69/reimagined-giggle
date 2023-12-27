abad3a7b4f0faedaa085a284e6aca3d19e28f6f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:36:18 +0000

    objection down thumb just interruption

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print("Sum:", sum(numbers))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
