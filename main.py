f407f595643b3da2d9944747a11ffae1c892900b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:48:09 +0000

    brush interested grateful may culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_set(array):
+        return set(array)
