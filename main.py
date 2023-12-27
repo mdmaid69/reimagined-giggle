5a30e6822c315d09cc70544b0258fee5af6291ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:07:09 +0000

    wing quiet hang march Ee

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_work(force, distance):
-        return force * distance
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
