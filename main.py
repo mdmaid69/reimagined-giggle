7ee9c5d5119c21021381d4b15264a51802a09aa5
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:48:18 +0000

    threaten party takeout popular flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
