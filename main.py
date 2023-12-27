be7fbc0246dcad4d0bfec93c676aa4d7b36073b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:15:45 +0000

    precise unlikely neither face similar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
