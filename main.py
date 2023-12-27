ba2246e3089fb06fee0eb8cfa61ade086dda89f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:08:55 +0000

    lie tour off speed some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
