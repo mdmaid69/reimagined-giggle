70e94e2f3d1b2a36e29f0be75848445c61b2f302
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:19:24 +0000

    entrance laugh action cruelty permission

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_work(force, distance):
-        return force * distance
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
