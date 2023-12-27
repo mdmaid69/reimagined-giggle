86c77111aa9995cb12377668e4b3d956992c8df6
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:13:55 +0000

    OKorokay than sure peace measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
