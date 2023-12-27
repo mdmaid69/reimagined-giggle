dec5c8be1e2c86080611a7309101e8263f679725
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:45:39 +0000

    scared getoff less nearly impressed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_queue():
+        return collections.deque()
