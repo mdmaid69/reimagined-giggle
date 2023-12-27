dc5c08600928836a9e240f9938eb7b8bfeba31f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:42:20 +0000

    sad send could present please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
