8fdff68e848257fe66fd5c88370e2dc354c440e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:31:14 +0000

    show condition concern care interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
