c78c66981c76f872bcfc6b938862980d2353c17e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:15:06 +0000

    nose broken attempt period might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
