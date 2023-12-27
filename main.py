6f13957f041d0de7e77c950ed54fecbcd2742124
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:55:20 +0000

    Yy estimate achieve drive empty

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def convert_to_hex(n):
-        return hex(n)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
