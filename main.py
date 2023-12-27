ce2f6287e23ebee4f75294709f0e5d66c173a88b
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:28:09 +0000

    try paper around whenever deep

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
