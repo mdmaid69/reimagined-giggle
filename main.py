5f28e94ebf8c860cbc33e5cef219e2e8c5b20ce2
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:52:14 +0000

    half your stop belief blind

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
