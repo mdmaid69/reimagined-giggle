6ee10fdd3efea85dfbe70cd39cc7339f07be046b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:47:38 +0000

    nor stupid recovery holdon mystery

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
