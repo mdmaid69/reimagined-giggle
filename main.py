c751a076086dc2b871cc3c81bda42d5e447560b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:23:54 +0000

    opposite workon react interview hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
