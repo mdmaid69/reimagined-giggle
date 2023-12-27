960788b182916dd4be56410a5439d878e4dadf89
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:16:11 +0000

    any switch wake cool formal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
