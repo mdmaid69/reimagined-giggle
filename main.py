283e8f6a403e55af7de738b2fbb8236b58bbc5e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:35:23 +0000

    under similar organization crash soft

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def multiply_numbers(x, y):
+        return x * y
