1cd224d70dfd1cd0312cd867f538f52a2407ea11
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:59:26 +0000

    risk turninto its tap successful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_density(mass, volume):
+        return mass / volume
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
