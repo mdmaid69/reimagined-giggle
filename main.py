b7ee6ebecdb47957b10bff4826258aaecad49638
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:06:20 +0000

    standard account appeal laugh snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
