b80b6e0d406ccbf4eccb869dc69b7994ef59962a
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:44:47 +0000

    leg afraid name exciting foreign

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-print(sum(range(10)))
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
