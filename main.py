c60056f1577e6fe89c76f23d6594b4ce246952e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:24:26 +0000

    tight plenty fix heavy cruel

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+name = "Python"
+print("Hello,", name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
