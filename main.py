e684b49ad3e7724093353aad9432a86c5653d8bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:27:10 +0000

    pity exploration stop eye connection

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def convert_to_octal(n):
+        return oct(n)
