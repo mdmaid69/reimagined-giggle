5c9b1d76e74ddd85ce851c3510c3237c734033eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:02:38 +0000

    interfere manner forgive goout violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_binary(n):
+        return bin(n)
