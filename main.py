7a92b9ce6fc1ea72245703685922bff0efa235ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:27:21 +0000

    dick slowly hope adjective destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
