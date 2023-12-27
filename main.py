cf85c0af0fb099204ff5ac5e161fd838d6f9bbbf
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:24:41 +0000

    put normal runover holiday Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
