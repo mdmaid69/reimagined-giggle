afbb27c8985ce087aa47f9c3c8f68ec456b78a94
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:10:19 +0000

    national store passenger lose recovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
