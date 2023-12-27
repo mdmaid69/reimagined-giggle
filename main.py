7e956eefae0787a0933eb320e323cb03e5976b47
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:44:35 +0000

    sink delicate reasonable consist minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_current_working_directory():
+        return os.getcwd()
