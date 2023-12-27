2ba34a19a9fc072177f8613ab1d26b9286512ac2
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:30:47 +0000

    murder stone fierce there excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+print(sys.version)
