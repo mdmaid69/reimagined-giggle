779793c1a81af5e3050b15fb71b896911a1083df
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:11:06 +0000

    central production snow country godown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def is_even(n):
-        return n % 2 == 0
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import tensorflow as tf
+print(tf.__version__)
