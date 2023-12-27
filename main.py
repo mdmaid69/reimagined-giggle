9c06984858964511d891da086abf0e4e24d3775a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:52:47 +0000

    world thought comefrom card reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
