5d25e7650ff031174619c3d482396a9088647da4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:32:21 +0000

    exaggerate advantage cheap immediate everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
