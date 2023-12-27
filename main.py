80be77648eb04153220ca322d72932c90a956870
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:04:30 +0000

    march stop confident smile guide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
