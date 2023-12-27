1eeb27f56428ce224c054324650c0f09b8531442
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:57:03 +0000

    measure Mm practice round write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
