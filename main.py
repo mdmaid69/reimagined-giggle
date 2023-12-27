dad0ac0669aff8981db4dc3a0f8dea9d7463fdcf
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:39:09 +0000

    considering confident steam shame warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
