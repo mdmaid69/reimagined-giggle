0330d93f61248a9b1075f37a091caaba8215fa7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:54:52 +0000

    prefer direct sport deliberately admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
