4b2a7598050b612dc7e3ef19b136f6fae7ff9247
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:35:39 +0000

    mother aware deaf relax age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
