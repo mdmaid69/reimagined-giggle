a0a8e9916776df1569d5ef71c6409f032cd8e2e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:34:32 +0000

    punish result accommodation talkback pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
