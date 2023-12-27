6bea8c0d5ec3b22596919b54bb27cae8b2173304
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:58:44 +0000

    cover famous purpose patient top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def append_to_array(array, item):
+        array.append(item)
