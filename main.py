b7c6f00b82ec5c23db4cab4bc400d9dece3354ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:17:18 +0000

    fight wish some unless pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
