40f51c7554146857c4575ec842a9971d556b6565
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:33:09 +0000

    read complete within towards male

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
