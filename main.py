761c89066fd6c184845133ea102d28a0c795383e
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:12:48 +0000

    plastic shake rubbish upstairs ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
