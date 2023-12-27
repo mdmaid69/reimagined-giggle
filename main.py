ec7997f17b0ac10535dfb97145a33fa6fd84e527
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:47:36 +0000

    uniform happy laugh choose keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
