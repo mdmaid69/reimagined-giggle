3fb94c5dfcd81e95963626dcb3df32af67822fff
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:06:09 +0000

    rain here its read surprised

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
