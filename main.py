a59788c24554d0fa16767cc77e33bff1048693b0
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:09:54 +0000

    seem spare challenge early onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_power(work, time):
+        return work / time
+import time
+def get_current_time():
+        return time.time()
