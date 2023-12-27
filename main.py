3dffd5b6afacdfec3826652f92abf55c23de14b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:05:55 +0000

    taste door introduction better poor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cosine(x):
+        return math.cos(x)
