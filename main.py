ab45a7daaffc5963db6060c63adda341d39bed0a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:08:32 +0000

    convince many bite change offer

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
