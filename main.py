9e03ad3c97dd2e8d508becd1b168fa6b0b344376
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:57:37 +0000

    store freeze gate brain build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
