372798761a5c2a8c398bdd69607f1b048471132c
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:01:40 +0000

    split list favour healthy middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_unique_words(sentence):
-        return set(sentence.split())
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_list_from_array(array):
+        return array.tolist()
