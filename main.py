3696525303165ac9a58d6f1907053899679cbb94
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:24:03 +0000

    thumb expect accident famous quit

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
