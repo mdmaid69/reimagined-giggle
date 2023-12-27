3fa22455db9c827f2b7fd4cc314df1e95966f4aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:29:17 +0000

    nice store storm somewhere lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
