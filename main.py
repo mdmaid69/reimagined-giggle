5db5fc4974bcab2fc06e90d8ac23eab6fecb48b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:34:38 +0000

    section dull substance never shrink

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def cube_number(x):
+        return x**3
