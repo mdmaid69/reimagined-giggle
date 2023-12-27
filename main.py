f6edee3e93a0e53dd4c1795b3668f6d133712552
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:45:03 +0000

    manage pin bound after breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
