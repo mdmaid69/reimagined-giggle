bd23daab0d1645ec0233b89744fc79ca7fb24bee
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:08:27 +0000

    conduct exactly miss possibility innocent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
