ece6eabddd81490be08797f00e0710ffbbed659b
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:45:51 +0000

    clever view turnover during pain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def set_array_item(array, i, item):
+        array[i] = item
+def convert_to_hex(n):
+        return hex(n)
