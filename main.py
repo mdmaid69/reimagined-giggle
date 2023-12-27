b89a12c84b988c32001c276ffc2627012bf84292
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:36:56 +0000

    everybody name exchange medium interested

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import tensorflow as tf
+print(tf.__version__)
