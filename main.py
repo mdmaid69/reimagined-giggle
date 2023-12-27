30749fee6e539c1770547a136cd7fe2fe4c6bb31
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:39:07 +0000

    plant imagination behave sudden great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
