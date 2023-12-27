3dd97a83e3820198110b04e9607c0b8487455a05
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:02:40 +0000

    push live official one turnout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
