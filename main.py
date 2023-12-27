df21023b1e0455bcc2b8d6363740609d2a82df98
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:10:44 +0000

    nonsense juice speed freedom discussion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
