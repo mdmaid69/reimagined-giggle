6becdaa9341085f5364d2069692f384273e64e61
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:42:38 +0000

    side add slight calmly dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
