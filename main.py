c265451970fef6005d9371d19e805781a2935d53
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:24:30 +0000

    Mm bright bye treatment wheel

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def is_even(n):
+        return n % 2 == 0
