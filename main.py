4251b9f0ca531bf01624ddfe4117e3c27703b66d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:23:06 +0000

    quickly although correct shame shut

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
