dab997a47f7b28fe22d6eb0fba9bd3103636ed11
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:44:08 +0000

    everyone again squeeze unlikely secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
