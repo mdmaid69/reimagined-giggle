a8c3c1153a66b08d66d40e2117e997f3781960bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:59:45 +0000

    into score official escape goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def find_unique_words(sentence):
+        return set(sentence.split())
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
