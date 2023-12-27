b47f181514c32202a73783a8ebea4c5d04ae52c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:23:15 +0000

    away method within splash path

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+for i in range(10): print(i)
