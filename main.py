c3f457cf6cd03e9e09d6562873c51d1003a06d4d
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:16:01 +0000

    scare change yours actually move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def multiply_numbers(x, y):
-        return x * y
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
