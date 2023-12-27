a796f7e1da3277918bace6af7c67cf620b349e42
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:56:26 +0000

    state satisfied wet takeoff cheek

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
