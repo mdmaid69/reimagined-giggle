04cb9092619512bfad053505b3bf2d58a1960882
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:12:16 +0000

    nothing reach takeoff cover unite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_even(n):
-        return n % 2 == 0
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
