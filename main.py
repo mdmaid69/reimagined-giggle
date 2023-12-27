510d10685a2c525c7b92380fb2882a1e6699e16a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:23:15 +0000

    feature strike variety dick fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
