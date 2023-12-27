02678619f5675c0b135726d48cbe505e06828ec1
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:45:15 +0000

    not centimetre scheme doctor away

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
