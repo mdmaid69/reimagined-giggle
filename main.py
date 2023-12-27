620f0a0d3f0d4642476f941f8b1181099b990b84
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:36:04 +0000

    really breathe offence breakinto possible

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def add_numbers(a, b):
+        return a + b
+import getpass
+def get_username():
+        return getpass.getuser()
