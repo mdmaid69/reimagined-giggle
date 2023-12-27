eb66729a479f10ecb6a26dbe0339f5e3ca4dad5a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:48:35 +0000

    surprising cold relax consider holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
