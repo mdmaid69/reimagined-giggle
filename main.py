df0bcb4a01572420d58a80356d4c53637bb66bdc
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:12:52 +0000

    colour not easy but trick

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def count_elements(lst):
-        return len(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
