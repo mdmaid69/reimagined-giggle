90b8a5c784c660c6b122c32610ada2999a595ae9
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:53:46 +0000

    home feature belong car runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
 import sys
-def print_python_version():
-        print(sys.version)
+def add_to_python_path(path):
+        sys.path.append(path)
