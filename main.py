d58e8e1c490c9b7d84634e236c0fd10d87d91704
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:10:27 +0000

    fly runinto seem shadow little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_sine(x):
+        return math.sin(x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
