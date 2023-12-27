313a13dc8a0dd223615d3ddc8cc9b808cd47300a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:30:22 +0000

    occur visit holdup upwards passage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
