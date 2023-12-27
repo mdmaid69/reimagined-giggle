cbe112c87133337a7b048ee9146dbd5e07519cf8
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:11:30 +0000

    late wash communicate Tt value

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
