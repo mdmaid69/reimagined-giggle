c8d3c85dcb5956dee649842dde3427170bbd29c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:33:50 +0000

    during raw anybody different opposite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_index(array, item):
+        return array.index(item)
