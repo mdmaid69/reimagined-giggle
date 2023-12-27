7ea6c6f86e93ddda27c3ecc12b42c90c61b296a5
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:32:41 +0000

    turnback whom rare write risk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def count_elements(lst):
+        return len(lst)
