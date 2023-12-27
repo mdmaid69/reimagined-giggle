3ef7dab15117bf7c928e0ac7489173d9d58a5259
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:07:18 +0000

    plate good flow distance different

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
