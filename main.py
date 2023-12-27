a4e37d57961f28ef99514b83ba8055b7754aa111
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:34:01 +0000

    create usual fetch mess would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
