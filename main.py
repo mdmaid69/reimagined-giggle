ac016c3b631c8529ee89187c0b69dbc8f8ec4401
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:43:32 +0000

    recently not forward keepfrom turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_time_since_epoch():
-        return time.time()
+  def split_path(path):
+        return os.path.split(path)
