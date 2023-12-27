a5a7c9756e04ff6c3ed3660efcaa99603f4f43ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:44:32 +0000

    advantage design detail goup before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
