e495d2ee1c6453b6b55911a74c103dffb922f2b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:55:10 +0000

    tip enthusiastic strike achieve natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
