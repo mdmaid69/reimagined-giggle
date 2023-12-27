b513970fd93249f5a26e24e40700eff67f744f44
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:52:01 +0000

    sale represent awake impatient cut

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
