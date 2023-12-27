1a4ac8cc71c2a4df87f16697eee4e433ce51aef5
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:49:58 +0000

    run game underneath test decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
