a8109a6721403f7bd191f6233bf43e00dc32ad56
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:55:12 +0000

    play plastic once transport explain

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def reverse_list(lst):
+        return lst[::-1]
