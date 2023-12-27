eff578bb2b79cfcf5a96787078a6706a4c668b75
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:24:17 +0000

    part weekend unable fast yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
