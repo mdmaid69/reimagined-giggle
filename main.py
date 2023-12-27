3a0277e507d599106a2edbadb7a9ac6a24c342e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:53:54 +0000

    touch neck depend ever creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
