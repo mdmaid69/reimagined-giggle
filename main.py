531a3b4aa86958cfdd58baa620e4369f977990d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:22:52 +0000

    trick stiff smoke father Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
