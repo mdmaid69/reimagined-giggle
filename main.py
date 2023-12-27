4bb589a84d1235052c7487254131a7e3a3bc7188
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:31:51 +0000

    getaway ought year any positive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_current_directory():
-        return os.getcwd()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
