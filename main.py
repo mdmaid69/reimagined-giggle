5dda605ab437c545ffd7d1e08ab6d3ccbd774afd
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:03:44 +0000

    thin old care wet sure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_stack():
-        return collections.deque()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
