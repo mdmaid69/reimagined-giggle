dd6d837793ea9d0e16b335e5b3e9ae04198e1ef0
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:08:10 +0000

    calloff phone neither tread forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
