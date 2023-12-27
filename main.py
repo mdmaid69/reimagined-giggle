9c18ae442aeffa683dbd0e2f6b7506db2e131c49
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:03:47 +0000

    shout change serious accommodation unless

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def multiply_numbers(x, y):
-        return x * y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
