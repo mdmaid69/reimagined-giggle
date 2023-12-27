7fa47b7103ed623c9b8c2e6861f6b0ba412c9f87
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:09:02 +0000

    wide late pinch me fence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_power(work, time):
+        return work / time
