3ceb5cd87680fa4b10078ec703178f996c0c0e24
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:26:24 +0000

    miss attach meeting silent dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
