b7d70ccfbe09801011866aaeb22f4bdc443b8da7
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:36:31 +0000

    occasional about whom she boil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
