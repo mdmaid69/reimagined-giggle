a6b757304efe674e578421495c1f4674905d0489
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:23:53 +0000

    soil up handle explain them

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
