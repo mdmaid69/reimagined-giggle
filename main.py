d274f5942f06fc61c75a877553d7705947ed128b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:23:42 +0000

    history advanced in form determined

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def subtract_numbers(x, y):
-        return x - y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import collections
+def create_stack():
+        return collections.deque()
