70ada9a39e6b6b4ac2b1835fa3e801a800700446
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:34:20 +0000

    understand cut noone unkind yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
