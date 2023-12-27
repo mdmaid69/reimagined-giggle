ffde4338f7e5348bd71650ca021c3717a2436306
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:06:30 +0000

    repair target polite choose final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
