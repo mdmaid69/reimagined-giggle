5e009cbcd2b89c22f193ebb332e88a9242c5ecbe
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:39:58 +0000

    gentle discuss just mystery feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
