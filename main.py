3735099625b2a8b3d7e4006d9a96e853a80a1a36
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:14:04 +0000

    impression criticize situation place stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
