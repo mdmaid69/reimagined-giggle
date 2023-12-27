2bcf14edc46789fb8316688822905b1070aea541
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:44:53 +0000

    convince point such downstairs early

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import getpass
+def get_username():
+        return getpass.getuser()
+  def convert_to_binary(n):
+        return bin(n)
