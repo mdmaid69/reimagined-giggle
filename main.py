6fec7961462f2c07d44c9a1ce0d8f7f28aecad70
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:17:45 +0000

    figure really stupid attach expect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import tensorflow as tf
-print(tf.__version__)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
