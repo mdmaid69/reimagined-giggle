7a7c86c9808adfaf33634ecc959cf4b9209a340a
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:58:49 +0000

    may cap certain division outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
