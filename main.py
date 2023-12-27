2deb17e4e9f8643bc7c1ff5d0b09334432e1f82f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:50:03 +0000

    advice correct drive new brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
