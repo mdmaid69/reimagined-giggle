27d3601c799762f696441944fbcec5daa207f922
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:59:25 +0000

    low odd while exchange sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(5):
-        print(i)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def cube_number(x):
+        return x**3
