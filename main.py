2539b78a65b6b7a58eeb6f8d07e668ab10d22d1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:46:20 +0000

    bet own breakup behind track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import platform
+def get_os_info():
+        return platform.uname()
