eb4ccd304eddabfb421005039c494f4a545151b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:32:42 +0000

    goaway standard promise frame food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
