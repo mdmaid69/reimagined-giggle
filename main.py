320ce0fa4e52f0b6785b3e9d0f8b2d7851860346
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:49:18 +0000

    card control scheme excellent become

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+print(json.dumps({"name": "John", "age": 30}))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
