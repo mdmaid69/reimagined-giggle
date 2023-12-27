a36e57edc22193bb25f8f49e32e250477fc03c47
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:12:10 +0000

    organize have soil model first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
