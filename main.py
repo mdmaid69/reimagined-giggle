73fa188a998a021a403e885f07cb49085ac64c41
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:37:05 +0000

    better surface game workupto platform

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_typecode(array):
+        return array.typecode
+def find_min(numbers):
+        return min(numbers)
