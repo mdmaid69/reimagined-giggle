f0653b8070147ff4bf17af8e98f9052ed8747f59
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:57:27 +0000

    face special morning however square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
