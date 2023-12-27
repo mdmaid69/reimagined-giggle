8414be08059b019c72d9cf0df0543c456617011a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:25:27 +0000

    Nn further knowledge choose respect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
