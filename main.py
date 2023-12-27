1b6b87145dc6854dd6ce5d6309667965cd405f78
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:52:37 +0000

    basis indeed full lose mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
