6b0cf44c952b0273ecadeaaa449d58bc181fbeef
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:26:35 +0000

    short politics wonder teach borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
