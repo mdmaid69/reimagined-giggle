f82e458b3ec8b44f68d0dbd7c1dc0b21339c2dff
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:35:15 +0000

    runaway drink could order sorry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
