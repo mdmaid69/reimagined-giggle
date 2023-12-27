6a379e983b6dd49953ab7ab967a2b6493edc08c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:14:34 +0000

    speak comeout more teach shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
