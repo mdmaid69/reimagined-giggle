4dcdc103c9dfe5449f28af6be44a0cd4cb4bf7c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:50:18 +0000

    different minimum finally string thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
