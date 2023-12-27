2020f19635134ee1df5c2525940e3fc815070540
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:07:13 +0000

    turnover reserve priority escape hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
