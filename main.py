dba32ba4f0d32f206b28a9c12eba41649248dce9
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:37:47 +0000

    hold each run satisfied holiday

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
