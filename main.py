0aa1ad58c514cc05f3f6309aa703e84231942af6
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:25:57 +0000

    present family other light awake

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import sys
+  def get_python_version():
+        return sys.version
