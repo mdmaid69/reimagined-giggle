35dfec48b416c0882dae026a9ab90bc18f999bb1
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:20:11 +0000

    century guess reduce right himself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def get_current_time():
+        return time.time()
