74339903d3bdce4bf7abcdaa704ee239b8201f28
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:15:01 +0000

    everybody anxious whisper towards outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def is_odd(n):
+        return n % 2 != 0
+import collections
+def create_user_list():
+        return collections.UserList()
