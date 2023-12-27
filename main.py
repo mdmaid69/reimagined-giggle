f5e35fca2dba6cbcf3915b0873eba71132fe1b33
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:31:57 +0000

    male furniture picture beautiful hair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
