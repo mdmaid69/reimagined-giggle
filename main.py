0dfb4506f292e89f8124f04446c1c975f9aee705
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:24:22 +0000

    move listen leg train hole

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
