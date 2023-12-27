cc068aa0e4cef55b8cea2979f3a10d3555dbdeb9
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:12:10 +0000

    stir bar ride store normal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_float(array):
+        return float(array[0])
