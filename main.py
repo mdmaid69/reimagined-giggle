d8ac2eb6493d9ba7506f4290fcf50468f4be1662
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:06:58 +0000

    character pay embarrassing thick my

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
