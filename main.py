9f5cf31225471cc237919fbf43eb426e7f818850
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:32:50 +0000

    mark sound interested book please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
