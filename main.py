2affad88775a45c20321f26d07184d0b6cf99676
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:05:26 +0000

    finish juice fear beat sure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
