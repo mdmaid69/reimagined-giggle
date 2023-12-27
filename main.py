9589a065af5066e530f63eecb8bc254d1b23d647
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:46:54 +0000

    Rr move deaf rush more

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
