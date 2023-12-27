b6bda7d2b8cc81775387a59363d2f0e7c4ae93eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:08:22 +0000

    radio spring plate course case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
