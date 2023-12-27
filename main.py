75a78187295d7ea26a340db5877e124d3ed4e97a
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:11:33 +0000

    regard scrape bottom will every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
