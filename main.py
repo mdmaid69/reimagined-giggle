78470a5142eae96a1db021adc5c358be5fa8756f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:39:39 +0000

    high regard cover increase remark

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
