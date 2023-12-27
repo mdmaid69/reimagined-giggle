bec81a788649d80957cef38446021d7ee086b9c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:57:30 +0000

    affect rob she whistle Ww

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_density(mass, volume):
+        return mass / volume
