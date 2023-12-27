14da89f27f6501c6e5259d126f4af1beeb7f70df
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:54:57 +0000

    play mistake success steam card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
