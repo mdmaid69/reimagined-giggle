27f2297cda112e0406e71d011dd9eb52260ecf32
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:18:10 +0000

    book star fortunately 571enough guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def convert_to_hex(n):
+        return hex(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
