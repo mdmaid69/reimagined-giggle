bf4f740b289423c5cde1409a585ec5b98b7f46de
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:01:47 +0000

    create winter shape term neither

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
