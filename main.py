fdc767e2be4a9581bbc3c798081089b0763cf4bd
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:32:52 +0000

    miss instead national experiment doubt

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+print(sum(range(10)))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
