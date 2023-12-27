d6e8b4e361dd938fee4f3475b9fbaf51698524d6
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:27:00 +0000

    back fortunate sense well fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
