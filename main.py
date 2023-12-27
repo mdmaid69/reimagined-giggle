39bb219ccc8be986174340c2e56f8d46456d5bfa
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:14:40 +0000

    software drink basic personal matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
