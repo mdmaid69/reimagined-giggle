c839e4a8a7699ca5e06947ef9fe08abae053365d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:54:22 +0000

    deliberate none comment calmly winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import getpass
+def get_username():
+        return getpass.getuser()
