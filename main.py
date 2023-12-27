6cb3ab23129bf93055dc9755af08c364e5ce5a3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:49:28 +0000

    set keepfrom live crisis copy

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import sys
+def print_python_version():
+        print(sys.version)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
