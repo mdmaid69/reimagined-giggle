3020dee17ddfea2e971139101d31baeb00d5f809
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:17:27 +0000

    serious dark insist university immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
