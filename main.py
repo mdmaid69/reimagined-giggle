04d955dd50f9edd60968271b2cf65a41088822cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:40:48 +0000

    complicated set before comein mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
