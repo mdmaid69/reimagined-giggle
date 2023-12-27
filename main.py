8f57f0ca2620e71eef5002f9c038542daf30f985
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:37:41 +0000

    shake thing disapprove ordinary stiff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-def divide_numbers(x, y):
-        return x / y
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
