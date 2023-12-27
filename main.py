3c7e166dca0a8391ebf8b7f8660d6ce1ed2bb2a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:33:20 +0000

    early danger opposite wash steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
