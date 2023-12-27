7e27aec9b4e6a633232b88b4d9ce0121c488ea83
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:52:27 +0000

    pause trick suck godown cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
