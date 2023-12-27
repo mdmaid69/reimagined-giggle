9925084dd40ef276aaa7a3f87c9874620f4e29ed
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:44:58 +0000

    mine damp conversation bringdown month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-print(random.randint(0, 100))
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_length(array):
+        return len(array)
