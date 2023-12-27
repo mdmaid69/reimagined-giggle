3b68f6c8c4a76da906048c965565d9bb40104244
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:22:51 +0000

    in later from persuade buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
