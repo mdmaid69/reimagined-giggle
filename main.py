1666bbe2809ed0a66a10a2c5ff0cb81e7fb0bb35
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:57:18 +0000

    damp town hers bread stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
