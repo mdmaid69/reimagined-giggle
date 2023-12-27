589493596bd206e40b52b7fd77dbf635389b355d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:51:43 +0000

    convincing strange number protection weak

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_pressure(force, area):
+        return force / area
