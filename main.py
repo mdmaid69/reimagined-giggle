6e84bb4a085ef2c920e9c2efd6a494896b62d46a
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:42:27 +0000

    left knock suggestion sore workupto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print("Hello, world!")
+import array
+def clear_array(array):
+        array *= 0
