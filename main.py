96509c605d2d7408592165b6805349e8cbcb5057
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:42:19 +0000

    noise impressed danger clever record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def reverse_array(array):
-        array.reverse()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def get_bytes_from_array(array):
+        return array.tobytes()
