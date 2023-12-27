5d4420bd7a4248a8d1e337c58edbcefc6f5e5f65
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:26:35 +0000

    aim ball otherwise vote mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_today_date():
+        return datetime.date.today()
