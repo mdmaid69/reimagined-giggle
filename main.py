e83dece99702d7f580c9cc46b222a5f3e13c3a44
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:18:27 +0000

    harm your description mistake back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def find_max(lst):
+        return max(lst)
