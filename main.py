b8b8727a13babe16a5f49dbb8ace7eda8c69cb77
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:57:29 +0000

    gather unlikely operation loan pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
