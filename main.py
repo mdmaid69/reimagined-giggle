f12fe26084d99cba2d616f7c338b51c2d6d4eb3e
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:36:36 +0000

    tune he common welcome mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
