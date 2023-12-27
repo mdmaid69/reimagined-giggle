cafb595fa41df33b5f8af437c5c2665ffc3b17e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:39:40 +0000

    extra deaf limit ilovedicks risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import array
-def convert_array_to_bytes(array):
+def get_string_from_array(array):
         return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
