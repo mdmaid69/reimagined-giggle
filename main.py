320918d929d26640f2595fe7272e8ece7f67987f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:24:54 +0000

    good reduce length heart difficulty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
