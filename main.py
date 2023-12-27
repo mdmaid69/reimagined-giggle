71a45899ae9e2c48d16e4f9debb0b74eabe4b1aa
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:21:47 +0000

    catch the impatient finish theirs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
