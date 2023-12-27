fa42c94a46bec3f94ab1f7e0222369262ce928ae
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:38:52 +0000

    frightened write petrol protest nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_item(array, i):
-        return array[i]
+def cube_number(x):
+        return x**3
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
