9144c9aba39b1d93ba54c70dedfac3e499c2e62c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:12:23 +0000

    maximum colour majority bank getout

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
