f70bf4fa00114f05e50db1bc3ef306ac41d889c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:07:54 +0000

    trap mind model imagination ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-print(time.time())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
