4f620b584ab5923dd34af464148731c2afe162ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:58:59 +0000

    address onto shout comeoff door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_working_directory():
+        return os.getcwd()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
