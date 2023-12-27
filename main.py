cedc9146f7cb308bf95bb31247d39a97d7ffffe2
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:39:52 +0000

    couple right surface pour tie

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def convert_to_hex(n):
+        return hex(n)
