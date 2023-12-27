37543bbb46806503b0642644965ab7e16d1a2c0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:39:51 +0000

    on direct cum direct winter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-print([x**2 for x in range(10)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+x = 10
+y = 20
+print("Sum:", x + y)
