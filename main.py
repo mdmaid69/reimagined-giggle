e1e77e5232d00f023096c791167ce64268fbf06b
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:52:24 +0000

    meanwhile move understand raise not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
