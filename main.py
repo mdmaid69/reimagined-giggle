a5230b123d3135b0029bbf07afba780fc167ce17
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:27:29 +0000

    car could 568energy another distant

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_density(mass, volume):
+        return mass / volume
