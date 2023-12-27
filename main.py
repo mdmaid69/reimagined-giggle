848cd78cdf730840a9fbfe22e7ab3f3958772705
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:23:17 +0000

    second shut downstairs score collect

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
