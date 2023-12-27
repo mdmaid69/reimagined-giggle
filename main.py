00f05f0868c4fd5149afe21c5d77126551108c37
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:21:43 +0000

    trick tax roll body by

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def convert_to_hex(n):
+        return hex(n)
+import tensorflow as tf
+print(tf.__version__)
