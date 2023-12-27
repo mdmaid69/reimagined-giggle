e619951774bd960f2de880452e42526a3ccde0ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:34:16 +0000

    promise book improve brave price

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_min(numbers):
+        return min(numbers)
