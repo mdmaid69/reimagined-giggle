1c92b4b367a2e5bf5e4cae151525f40af74bb45d
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:17:16 +0000

    task chat attractive sure stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_set(array):
+        return set(array)
