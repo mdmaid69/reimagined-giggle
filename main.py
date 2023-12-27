f24c051a85f45ab8499fd438d4c03d40cb614593
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:46:19 +0000

    live subject before activity commit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def calculate_area_rectangle(l, w):
-        return l * w
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
