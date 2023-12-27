f58c9e6ef22fd94b95c4ef9bb3c3a35f1851c7ac
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:01:14 +0000

    regular fashion service connected stiff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
