f7e84e61241a40919980205fa7361f34e0058e6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:39:27 +0000

    loan silent fire land throw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
