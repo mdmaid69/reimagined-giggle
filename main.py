c795b02552ca7b39e835e86f802603125a7405a2
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:22:56 +0000

    street weigh narrow lookback(on) surprising

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
