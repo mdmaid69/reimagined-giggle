d731eb4bc618ce488a08ab90f4d355c64f1dc28b
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:19:16 +0000

    today reserve mine finally family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def subtract_numbers(x, y):
-        return x - y
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
