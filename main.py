641885251c7ae17e5f952c5e12675ddcc399ff25
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:22:04 +0000

    out goout within very centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_work(force, distance):
+        return force * distance
