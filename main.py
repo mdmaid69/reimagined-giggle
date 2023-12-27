13401ad6b155472ebd7c089f7aa3d4c5e0cff143
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:05:31 +0000

    point lean issue wave maybe

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def sort_list(lst):
+        return sorted(lst)
