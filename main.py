ad0a3912330159a7756415a1809d907ac358a8d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:14:47 +0000

    preparation blind recent ear pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
