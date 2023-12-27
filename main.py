8b8249aceebbd125a08fc5440324d3d30cd9899e
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:20:06 +0000

    about pressure fold strike raw

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
