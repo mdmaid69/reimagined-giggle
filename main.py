5bed9f05befbc7fd0465041ec0cf7f757c900cb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:42:18 +0000

    flat off offence persuade neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
