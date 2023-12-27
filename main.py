4dd9ed420d6c9e485d4b98b0c86ba62bd37de1db
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:11:06 +0000

    road welcome regret television successful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_even(n):
+        return n % 2 == 0
