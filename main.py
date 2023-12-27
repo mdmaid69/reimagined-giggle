11165f94cdd96f111c64f54929e62c69d2883dac
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:08:25 +0000

    its success down born soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
