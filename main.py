04b4a503fd6d0159f93f36750ae6a99e0010f3cb
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:28:38 +0000

    mistake health Ii charge yours

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import time
+def get_current_time():
+        return time.time()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
