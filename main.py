7613f9f58c70a5b1b28dd564d2c165ea48048336
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:02:33 +0000

    attempt which disgusting charge neither

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
