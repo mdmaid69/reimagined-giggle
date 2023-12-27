6be64d7a894dacaac025cf085ff62f95769ee5ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:02:10 +0000

    cheek century rare minimum single

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def convert_array_to_list(array):
+        return array.tolist()
+import random
+def roll_die():
+        return random.randint(1, 6)
