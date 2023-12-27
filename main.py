cfe52f4d479fc10db034342147704bed6c761f22
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:00:06 +0000

    strong whenever set boat autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import json
+print(json.dumps({"name": "John", "age": 30}))
