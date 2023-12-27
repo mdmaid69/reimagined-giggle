72ac4164055f385a4f11e7150daa7872574453bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:46:54 +0000

    rid consult bet figure date

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
