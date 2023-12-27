206622b68c1897b70b66b840b19592739723e9b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:28:17 +0000

    hit check importance read trade

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
