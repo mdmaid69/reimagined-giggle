b04fce1e5eedf561ef8a65cc9844ce70c695e589
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:22:43 +0000

    mind clean cruelty direct morning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import time
-def get_current_time():
-        return time.ctime()
+def get_array_as_complex(array):
+        return complex(array[0])
