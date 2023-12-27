d8656a5226fcd567f585e7c813fccb2149d91036
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:41:16 +0000

    roof sleep announce private personal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
