862b92de00bbe5ad074355bf11823c4079e3f7d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:11:27 +0000

    accident dance final above guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
