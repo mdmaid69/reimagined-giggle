c08c56dbeb141e0490864db5632b5e4ca6448ebc
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:16:08 +0000

    firm blue machine report brief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def is_palindrome(s):
+        return s == s[::-1]
