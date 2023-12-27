fb7d1866d668a9c73f4ddb2c45bdb3cefc2b3cdc
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:05:35 +0000

    present group charge rubbish they

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
