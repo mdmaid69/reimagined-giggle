29a91d83c23d8c54598928c02917ce91f76d7422
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:03:43 +0000

    stand guess height basis dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def is_palindrome(s):
+        return s == s[::-1]
+import os
+def get_current_working_directory():
+        return os.getcwd()
