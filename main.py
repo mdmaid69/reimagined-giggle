d5e12e89703c8557e0e2dac88e0c260cf508ce39
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:29:52 +0000

    smile rather decide protection return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
