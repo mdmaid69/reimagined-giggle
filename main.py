d966dd141982a9fcfaa9e5a71a51cdbf27df5a9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:24:32 +0000

    interfere steady Tt replace ball

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
