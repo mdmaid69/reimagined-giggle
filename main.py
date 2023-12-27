66b3a0262f6c8766348a38f7f8de4fd6d8a4254d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:06:22 +0000

    storm worst gooff pressure spend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_sine(x):
+        return math.sin(x)
