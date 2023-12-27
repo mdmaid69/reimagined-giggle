2bc2ad2662a3875a035a8aedb23438140d2f3063
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:14:35 +0000

    frame where recently wander secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
