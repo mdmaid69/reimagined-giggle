d7b65e3a5801f216f4db7a77d3f4a4fec83dc795
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:39:06 +0000

    child really long thick he

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import sys
+def print_python_version():
+        print(sys.version)
