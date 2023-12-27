f37c2b3879d0326043c119b87bcd6ac557de0284
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:23:06 +0000

    except side bother mind well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import time
+print(time.time())
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
