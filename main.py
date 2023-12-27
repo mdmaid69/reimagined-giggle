eb3e69f53c303273d139ba11b8dedaa0166bbde7
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:50:07 +0000

    raise present though pollution thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_as_float(array):
+        return float(array[0])
