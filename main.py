53a86e6cb98a1e4e8516265362ecd6f65a32a1d5
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:38:37 +0000

    principle the subject cum finger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def convert_array_to_string(array):
+        return array.tostring()
