3baaf5469c24666e02a796458fe227d40562eede
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:09:30 +0000

    sad better regret confuse occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
