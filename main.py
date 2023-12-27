f5b0ac92cca90cc25752229c16ff5e419db51ec8
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:30:22 +0000

    last week previous number break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
