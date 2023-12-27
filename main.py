d76762d2a2eb6f17a96a653ae66cbe3bd25bb5d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:20:38 +0000

    this buy principle sad strike

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
