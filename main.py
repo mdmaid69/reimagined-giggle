ed7e74c0b7a2f0756de1d0a500bd1b409af43862
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:43:09 +0000

    clue bread contrast every confidence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
