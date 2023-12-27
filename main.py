a039614923b21fb4fb4c07c836f76dbe01763556
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:41:54 +0000

    fact note yours interrupt next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
