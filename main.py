b93358ae84fe4268dc727f60e9486fad4766bb86
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:59:33 +0000

    sort wherever collect holdon strict

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
