fb310413c261c3c6c51e2131775d5c84ebfb1dcf
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:29:16 +0000

    whoever border closed joke basic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
