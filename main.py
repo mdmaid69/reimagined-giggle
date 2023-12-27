1cb93c1bf9f510ce7842392c9a84a2b4b70db6fa
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:44:26 +0000

    sight bar own mind nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
