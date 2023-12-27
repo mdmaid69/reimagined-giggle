2d71484ff387d555212dc3722cb7825f3d843074
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:23:13 +0000

    chat scream favourite shape programme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def cube_number(x):
+        return x**3
