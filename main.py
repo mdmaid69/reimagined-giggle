e8bb9c3fff71ef076fa9a2144106ec9c422b510d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:14:33 +0000

    grade quantity rise course class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
