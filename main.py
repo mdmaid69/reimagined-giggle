55625a239f79afa61ead958c94d7d6c9d05fa5fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:01:31 +0000

    various thankyou cough sister rub

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_binary(n):
-        return bin(n)
+  def convert_to_octal(n):
+        return oct(n)
+print(sum(range(10)))
