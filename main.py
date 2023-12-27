5c48b8f4ecafa8ec6cb371287244384586922254
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:22:11 +0000

    blue bringback shrink greeting any

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import os
-print(os.getcwd())
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
