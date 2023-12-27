aa51edc4277a1db2fc8804039c985406c287b6bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:58:29 +0000

    separate seem reasonable increase Bb

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
