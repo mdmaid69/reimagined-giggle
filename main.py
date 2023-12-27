2b76399f22abf5995d897ef2a54205586d0ba9d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:09:10 +0000

    forward calloff sink replace block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_current_working_directory():
+        return os.getcwd()
