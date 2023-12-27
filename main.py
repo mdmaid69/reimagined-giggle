0311cbd8b2ce10da0e71bc8a7faca3d36f7b3826
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:32:36 +0000

    closed mistake failure pure birthday

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def find_unique_words(sentence):
+        return set(sentence.split())
+from collections import Counter
+print(Counter("hello world"))
