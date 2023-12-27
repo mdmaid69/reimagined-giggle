e7863ef9a8fc78409ccbdac56e7a22b468ac6d96
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:53:04 +0000

    excuse makeup present group price

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
