0618846f3420ee066b6083fc9aa9d8bd4003a373
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:25:53 +0000

    emotion plastic birthday rain nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
