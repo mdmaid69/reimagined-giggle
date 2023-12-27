633e5c7f4dc6fcce6c1dc4b14156a14f125faed8
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:28:14 +0000

    hope mind few quickly scene

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
