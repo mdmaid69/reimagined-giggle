8ee4ec2bb7e5ae1073c9561f724dc475b43daed5
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:58:40 +0000

    worried substance matter direct wing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_directory():
+        return os.getcwd()
