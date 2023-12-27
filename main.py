3029bb4aba2dbd2d2c647d967a7cefa96e50399d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:13:28 +0000

    trade bill claim year whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import os
 def create_directory(path):
         os.makedirs(path, exist_ok=True)
