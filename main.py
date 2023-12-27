4e3a08e4d9512a7fcc30905532248c9c90872031
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:10:51 +0000

    anything edge callat clean lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
