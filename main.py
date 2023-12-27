ff37b8a41cfecc7344db687113b642ce460224ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:24:34 +0000

    spend impressive use callup loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_current_directory():
+        return os.getcwd()
