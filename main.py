370437849985a8e5839441dbc564cb68a7168cc6
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:50:26 +0000

    regularly some matter hard press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_set(array):
+        return set(array)
