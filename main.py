970cdab6f826adcc9d041fafee9c3d60bb263096
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:37:55 +0000

    pen sorry introduction 564encourage either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
