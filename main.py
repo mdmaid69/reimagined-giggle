ad2ac4c34efbe6ed891650ce064d2af54d836f96
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:59:46 +0000

    comeback flavour check regard error

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_floor(x):
+        return math.floor(x)
