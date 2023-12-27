8e8f836c721c80618f708ea898690ad447ab54ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:33:59 +0000

    modern remember broken myself film

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def sort_list(lst):
+        return sorted(lst)
