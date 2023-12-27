ad04077d3a6e8060b5094e7820d150e79010228c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:04:21 +0000

    owe introduction judge then drink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def get_list_from_array(array):
+        return array.tolist()
