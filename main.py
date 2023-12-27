b9347093871e03538a08682daea97f2d7c89d133
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:43:53 +0000

    Vv oil close direction concentrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
