43d0ec15639356e0751a86c6c0cc16f24052b021
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:30:15 +0000

    next leg suit container reasonable

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
