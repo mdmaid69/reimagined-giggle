506a371e5e846917ba7e9ff7d500db7041a56680
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:40:03 +0000

    free none prove figure public

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
