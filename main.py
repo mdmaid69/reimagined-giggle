668fc0afbf73295d8f3a03161cd1426fd5a50a4d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:54:00 +0000

    city occasion platform considering hit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def find_max(lst):
+        return max(lst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
