a76b8bb04ed74e0e79d129e86d22683f345798a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:37:45 +0000

    many best disapprove exactly begin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+print(os.getcwd())
