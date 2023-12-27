52cb51fe17618a609ea26819610cc1254c25a898
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:49:40 +0000

    original whistle combine disappear workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
