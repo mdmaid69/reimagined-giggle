b4f1182c5fbd3c4db6e3d72f6a03eca484489231
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:47:17 +0000

    land total only cover extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
