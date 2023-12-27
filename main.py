f44d7371ad2e333faf740e07078422df405a200d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:51:40 +0000

    contrast arrange emergency save hurry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def convert_to_octal(n):
-        return oct(n)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
