73c46ba127d581a44079cd91803a8206cc3de4a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:18:12 +0000

    difficulty thinkover bound passenger branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
