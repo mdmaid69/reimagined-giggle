85e540bd17b8eebf6fcaacc6fb6646c9077b1df5
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:15:48 +0000

    second in acceptable cut learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
 print("Union:", set(list1) | set(list2))
