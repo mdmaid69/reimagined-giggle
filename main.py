a33cc1b862dc7690c1be296a65c112687e2f50f7
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:06:19 +0000

    by comfortable heart fond talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
