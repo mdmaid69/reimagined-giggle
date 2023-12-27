f29f91ba6a2faff8189b4609b2332f90309cb373
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:29:00 +0000

    piece rather ourselves surprise scrape

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
