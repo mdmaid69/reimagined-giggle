1f057605cc9da5eb7e72cf6558226fa3bd65fc2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:27:11 +0000

    sharp phone refer difficulty tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+print("Hello, world!")
