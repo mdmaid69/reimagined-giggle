de1d4e7fa97da73ad4113202e996ccb25d8b2c10
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:44:34 +0000

    steal sink possibility runaway little

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
