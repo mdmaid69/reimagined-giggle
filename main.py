2e89d54008dc9049a057f76d5c9dd4b2db54fbef
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:00:38 +0000

    callback pride time use happy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
