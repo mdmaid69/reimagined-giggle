b6ebaa78081bb7a2316e5d358782da500e9025eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:56:26 +0000

    mention calmly meal mass win

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
