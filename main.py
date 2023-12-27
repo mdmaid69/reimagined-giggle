0d6e3f9343472cd1d5d056c237558d61dc346e7e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:11:21 +0000

    doubt hospital band balance clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
