310a6b7bbf1b02255c60714a781af49c66eddf46
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:04:50 +0000

    hear mean young accidentally early

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+print([x**2 for x in range(10)])
