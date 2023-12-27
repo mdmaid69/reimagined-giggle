33f238798913050dc6fe0e34ba49e2e45ef56e29
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:12:53 +0000

    size with on board guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
