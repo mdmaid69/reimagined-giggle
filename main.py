7c8c40c143d2f817c40b311d3af6e3735ac97e6f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:15:15 +0000

    workout pool live story bag

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
