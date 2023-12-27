de1db4440a15c73db5afe558f10cf99acc2b1363
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:29:02 +0000

    about soon calmly judge introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
