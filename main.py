a9e9d0d9ca0184455ba359e47ddb5fc0446a71bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:04:05 +0000

    foot who ever drawing lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_python_version():
+        return platform.python_version()
