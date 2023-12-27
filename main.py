7d49878d313531f365560c5c270680035959bfc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:04:23 +0000

    bridge maybe care pick prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
