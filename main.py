7f2ad9b707859283f021e3217e421b13fb46fe8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:26:19 +0000

    danger family purpose meet beyond

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
 def convert_to_hex(n):
         return hex(n)
+def convert_to_binary(n):
+        return bin(n)
