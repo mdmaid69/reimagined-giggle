e0bc7bee975ed29546c0943aa591601f566f0e16
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:35:57 +0000

    might dress flow hang organize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
