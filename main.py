8e9132f17d359263cb66a39b7e488ef078fec61b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:46:25 +0000

    sweep butter main reduce meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
