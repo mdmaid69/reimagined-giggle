854db676527d46fbc3696975376c5f2b77509c69
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:51:54 +0000

    hour knowledge entrance cap different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
