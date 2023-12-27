f215c0fb9dcba6c07391f15a495c934d904695f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:35:13 +0000

    cruelty remain account approve address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_list(lst):
+        return lst[::-1]
