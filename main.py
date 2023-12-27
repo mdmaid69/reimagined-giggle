a0225c33f176b23b859621edc568e6ba687d5b21
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:48:28 +0000

    come summer slip love happy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
