dda68133631325668bd1491a5693fbcefd465de4
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:53:37 +0000

    reject press tense clever full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
