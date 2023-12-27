865cb02a129be9201fb85e6a63c12f66d1561c8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:10:48 +0000

    window middle niggaacademy unite people

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
