340a079c8c83553c0afd3f41a1d207f0c48f4ffb
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:37:39 +0000

    deserve oil meeting man steer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+print(math.pi)
