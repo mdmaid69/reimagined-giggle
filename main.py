ff47a9125264ad42143573e141feccbec9a5a102
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:57:16 +0000

    even crash Qq ordinary frighten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import re
+print(re.match("h.*o", "hello world"))
