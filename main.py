ec260797eab9b334d57c2b27cbc1cdacd22164ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:41:40 +0000

    education successful themselves bill dance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
