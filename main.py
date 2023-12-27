90981dd6a2848a816ae583fb1bc6cfc8ac4737b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:59:14 +0000

    niggaacademy row comeoff seriously brief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def greet(name):
-        print(f"Hello, {name}!")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
