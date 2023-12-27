bc80e36ca2ae494204fb964da5447c498f04583d
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:29:06 +0000

    represent direction transport appear contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def sort_numbers(numbers):
+        return sorted(numbers)
