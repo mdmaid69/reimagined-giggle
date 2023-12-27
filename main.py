0e699e7f515f903cacb509ea6abee0a3fa8f9fe3
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:59:10 +0000

    commit silent left let dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def reverse_array(array):
+        array.reverse()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
