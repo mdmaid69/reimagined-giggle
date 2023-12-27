4dc70d94f62fbe6e6f23c4d8441c4fda1f75147d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:18:17 +0000

    smile noone it achieve hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_length(array):
+        return len(array)
