de2fdad29284da335fb19fee9ffa9593f59aae5f
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:04:20 +0000

    usually judge care delicate view

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_odd(n):
-        return n % 2 != 0
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
