bd6c566463308c3b5dd04bad77267ce7e88b4272
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:17:20 +0000

    cope boring glad low Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
