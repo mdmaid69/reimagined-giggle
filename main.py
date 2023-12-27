068a0b3c141fd869f43147bdf462ff2c96f8f4a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:18:58 +0000

    because bend wet now seat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
