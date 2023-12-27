168f409df6c6d0a27bbb247feb1e6f4865075b37
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:41:36 +0000

    once few pay between they

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
