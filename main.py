feaa28df1f8883f4554c81f2a2b1fd6f8e9bb825
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:27:26 +0000

    key tip nor typical correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
