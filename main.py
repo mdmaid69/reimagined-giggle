b3e18304ae9bbb7341ef19ccc4ab066f77e4e769
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:11:55 +0000

    distance sometimes hair tape thumb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def reverse_list(lst):
+        return lst[::-1]
+def multiply_numbers(x, y):
+        return x * y
