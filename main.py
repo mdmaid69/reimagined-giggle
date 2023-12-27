db4a9732236297f353d4449b03d5b786f5f93ebc
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:11:07 +0000

    safety breakdown marry persuade under

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_power(work, time):
+        return work / time
