64e975c30d766d258051e341a697a94200ec8b6d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:19:44 +0000

    report pen acceptable terrible though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
