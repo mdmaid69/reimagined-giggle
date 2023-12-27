7bdf49b6ca54fc56ef5c8e6b704b21715b7e47dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:04:22 +0000

    slight minority boring name reply

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import re
-print(re.match("h.*o", "hello world"))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
