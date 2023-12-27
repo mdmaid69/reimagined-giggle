2c0cac8b84a13e93934285776bab5731285847b4
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:15:46 +0000

    accommodation fit sign weekend Zz

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
