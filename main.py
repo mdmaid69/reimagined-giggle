331366139b7c0fb12972513ab133a185ae34ec75
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:52:07 +0000

    hit power broken drive still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_current_directory():
+        return os.getcwd()
