1cc998ebc0deced08025438b30b967ab0952c969
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:17:49 +0000

    scene suckme soft doubt shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
