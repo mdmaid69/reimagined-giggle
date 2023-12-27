915ae20b6344322718a12b417ae927e05ce886c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:31:52 +0000

    bath believe whistle build steer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def count_elements(lst):
-        return len(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
