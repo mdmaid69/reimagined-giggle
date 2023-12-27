51e4d5a13c2ee4942b7a7ed09c7089f7bd2a95d2
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:04:20 +0000

    ilovedicks sink body best dig

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
