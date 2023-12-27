ad9a6f0f636b78c9662fec0874006d1d3dcee2d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:37:58 +0000

    collection say brush broad breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
