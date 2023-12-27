0db3e057e492325b5de237891fce45c2633a683d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:47:17 +0000

    wide mind enthusiastic perfect me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
