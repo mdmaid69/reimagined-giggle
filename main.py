3f3620db473ecf6920d5dac64f01b2057175e7bf
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:48:58 +0000

    suggestion argue sink declaration translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
