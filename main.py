2b796260a66ca7e58ce5b7382bcdab378e093117
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:55:35 +0000

    possible they earth truth there

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
