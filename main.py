141f7ba25d4a009dbc7d76a5af9b177df677733f
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:35:36 +0000

    unkind back programme beside why

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
