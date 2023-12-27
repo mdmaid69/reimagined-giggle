bf8318a6f1bdb3312c63fa632224ad593ba22981
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:18:44 +0000

    lesson control personal right cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
