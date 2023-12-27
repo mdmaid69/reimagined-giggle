e0e1ccb64e66efef41dcce33ccb3c5f8d4bf6e10
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:16:18 +0000

    earn loose drink splash keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
