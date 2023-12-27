380699231fe12349fab2490780f5b51cb0c1c409
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:17:48 +0000

    store stop invite look exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
