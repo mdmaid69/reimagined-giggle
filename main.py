b1bf70067470c9d0d0835eae649808cb3303dc56
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:11:57 +0000

    new callup embarrassing deliberate touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_current_directory():
+        return os.getcwd()
