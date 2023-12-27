2aa79a3ee2db175a4f267c3f4284254075919e10
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:58:09 +0000

    bring plan unpleasant heat yourself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
