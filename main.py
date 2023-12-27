87be737723a881c3b7f704d0b7c61b45e76c2b37
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:12:59 +0000

    most judge square collect thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
