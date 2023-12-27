9f28e99fc884f4d9ebb011bd4d0ea3899521ca35
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:11:05 +0000

    different declaration bring either mouth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
