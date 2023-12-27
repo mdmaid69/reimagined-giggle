e9a6963bcce18311bba183f601e8fa6a47d1bb6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:51:24 +0000

    back talkinto begin pity reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
