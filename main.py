8698b745945f108986d4d777be131d2700f6df28
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:42 +0000

    after else complete order machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
