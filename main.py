9d368ad1b07962c78d083b502a8b36c06dbfc76b
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:00:36 +0000

    course suggest night rent geton

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
