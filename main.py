ea9600050cfda90ebe62078d8a734983cb2a07ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:48:11 +0000

    bang keepoff together statement beautiful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def convert_to_octal(n):
+        return oct(n)
+import collections
+def create_priority_queue():
+        return collections.deque()
