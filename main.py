c3f135c3e8a672f6ee5bc68e631d8fce7c534913
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:32:44 +0000

    claim far one matter half

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
