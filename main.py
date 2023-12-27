6b385bce99545efeb6061edce25602779d28d89e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:43:36 +0000

    impressed string minor target comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import sys
-def exit_program():
-        sys.exit()
+def print_python_version():
+        print(sys.version)
