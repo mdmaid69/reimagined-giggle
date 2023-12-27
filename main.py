040762ecdb18e42a65553c39916891895e63b0f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:40:49 +0000

    Mm bus thank safety brave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
