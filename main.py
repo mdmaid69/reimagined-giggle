8bb4352de4e21e9a890f1c1f0dd96a3cf21f893d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:20:31 +0000

    left rain joke aim quiet

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def square_number(x):
-        return x**2
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
