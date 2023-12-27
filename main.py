b36ecd496b4588441d7b6ee2383703cb704dc15b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:10:56 +0000

    welcome year Mm favour light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
