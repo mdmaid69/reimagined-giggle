1dbca8aa23eebfdaab9defd81c4931083750231c
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:13:38 +0000

    callat since live round increase

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
