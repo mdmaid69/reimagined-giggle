f5f891aaf3580d792c0d7750f9976c9a98288961
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:41:36 +0000

    exercise move position stage modern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
