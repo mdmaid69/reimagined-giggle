4edf2448ba18bbe8f3e44a0ff0e3f970cc29c532
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:06:55 +0000

    port steal disappointed declare land

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def convert_to_hex(n):
+        return hex(n)
+  def cube_number(x):
+        return x**3
