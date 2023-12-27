af1e68b8d57860383bc7314538c42c3e59e9d244
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:38:16 +0000

    question talkinto push keen acceptable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import time
-print(time.time())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
