9ab050c9740e96adf1c85a97ba8d7b96acf5dbe5
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:40:24 +0000

    declaration count her them belief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
