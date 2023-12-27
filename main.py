e4952d7dbf0a5d78497d75117465727726b1721d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:45:56 +0000

    keepfrom suckme breakup comeoff can

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
