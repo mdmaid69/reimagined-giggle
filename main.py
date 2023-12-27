712200400eff18cc684e2f0e055d1127d8c8770d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:10:30 +0000

    free record apologizeapologise splash order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
