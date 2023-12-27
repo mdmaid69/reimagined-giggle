af7296226fd9ad275ff84f72461360feb97773e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:44:36 +0000

    dress point drive yourself cut

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
