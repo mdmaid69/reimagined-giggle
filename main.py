10195a151bb2e210bb2955355c1cd7eade200d8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:57:56 +0000

    hit miss takeaway steam something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
