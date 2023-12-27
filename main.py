6fbed914e373a9111870e69e1eaba3474433efda
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:33:18 +0000

    attention complete land lunch birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
