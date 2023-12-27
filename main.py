4f851d5aec33d37e45feb3b07685a690602e1571
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:22:07 +0000

    solve pity brother afraid middle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
