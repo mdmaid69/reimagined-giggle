5d820f89d5f133cd6bf5b68c29cf88c4e8fcadb4
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:25:53 +0000

    myself breakfast rest top there

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
