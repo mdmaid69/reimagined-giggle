3ed6c0a6557fae8c010f9222162ec60b91aeaf4c
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:00:05 +0000

    balance fast important politics Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
