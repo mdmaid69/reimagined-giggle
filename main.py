9b14fe660947e33fe17fedcd98ccb5fbd38f7c4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:01:19 +0000

    system family free shoulder false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
