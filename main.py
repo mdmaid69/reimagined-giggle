2e3dc4b349a0bdd18689963d6271c5a74cc830b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:43:32 +0000

    together ride environment situation bound

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def convert_to_binary(n):
+        return bin(n)
