53e910cb236014449e0dc2ec9af4e8ee46e59fdb
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:42:09 +0000

    young typical high getoff travel

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
