8e3fda1664a5e49d4d55d94c3d5bd1a319a7fbef
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:20:50 +0000

    accidental extra mention independent Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
