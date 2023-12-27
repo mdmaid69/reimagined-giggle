43c760d114e8d6b46465053cb5c4fe6691f3a8d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:02:58 +0000

    unhappy hole else neck flow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
