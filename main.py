ba2144151456f4d88227260f35e4990cd86758fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:04:24 +0000

    reach mind wise share no

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
