77829ebe3719a52483c51b84161860bcbccb16b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:27:46 +0000

    hardly fierce life narrow couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
