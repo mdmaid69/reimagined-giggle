ff3131bc777732fc145954b8b7cf8ae145dadb9e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:37:27 +0000

    firm describe century walk attractive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def convert_to_hex(n):
+        return hex(n)
