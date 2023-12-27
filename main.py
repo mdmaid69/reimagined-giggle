cefc450c1f7e22787f71c50a7cb00510f7ecd9b7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:26:26 +0000

    beside most decide sign comfortable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+def get_current_working_directory():
+        return os.getcwd()
