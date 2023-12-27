ac9f5992d18fb71574a16e33f10b90d88bed0d5f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:20:37 +0000

    spill print react early noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_user_list():
+        return collections.UserList()
