d15f11f6f2eab56a1aedee69e7d69efed2597e0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:01:04 +0000

    murder unpleasant cold day tradition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
