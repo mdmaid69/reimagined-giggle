94dbd6d6d07d483f456869b20cc327483015d5c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:18:21 +0000

    meet remember autumn trust serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
