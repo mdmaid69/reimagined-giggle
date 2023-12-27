03384cc408a44ac77b0c72fed0b59bda47b9049a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:59:39 +0000

    seriously handle damage shallow comedown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
