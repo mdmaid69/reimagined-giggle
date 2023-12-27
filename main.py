8bdb8b1bb343384980eb5063bd25fc12d2810434
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:33:02 +0000

    whom trick study flat explain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
