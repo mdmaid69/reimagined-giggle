df2dd7baaad6cd660e183a3dfaf27591e59727ad
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:57:45 +0000

    generally stone desperate fair depend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def count_elements(lst):
+        return len(lst)
+def convert_to_binary(n):
+        return bin(n)
