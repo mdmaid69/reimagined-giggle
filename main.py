c7c4494996bfc0655008385e1fe62e8fe0d7daf5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:24:17 +0000

    care meal moral send frighten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
