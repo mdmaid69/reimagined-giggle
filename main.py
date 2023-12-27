426437c1a726904bafa942b6b1c8a7b7b11a8b46
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:34:54 +0000

    pronunciation bury black commit learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_hex(n):
+        return hex(n)
