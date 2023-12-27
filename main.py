2ce48a16487beb57fc2ffa1b7d99f5c8aef56bf9
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:12:30 +0000

    suit gas constantly anybody come

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
