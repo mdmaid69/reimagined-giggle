66bfcccd50110b48adb5b3db2ca46fb1e2ff21b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:09:38 +0000

    adjective fly round world snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def delete_file(file_name):
+        os.remove(file_name)
