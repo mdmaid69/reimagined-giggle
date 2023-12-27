81b8e3c68b8153fcc2114754dbd8cb0be093c45e
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:53:30 +0000

    open off control best soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_volume(length, width, height):
+        return length * width * height
