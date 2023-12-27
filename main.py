a86265dd68c5c7330e6b7fde6a51c7d5e4bdbbb7
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:11:56 +0000

    plant equal neat speech careless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
