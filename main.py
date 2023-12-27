dd73918ba15e73eb3fd45eaace0b969a4df31b75
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:30:08 +0000

    invitation cap off rank downstairs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
