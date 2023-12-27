a6890af1f96939c4e046cac05a4b2f65b2122b10
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:55:57 +0000

    clearly train wash connected burst

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
