7706a4489778f66bd4a566cd523681ade6c1e038
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:00:38 +0000

    church rest plenty defeat dangerous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
