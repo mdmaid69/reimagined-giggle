425db973e65f3667339646be9ecc9a453e80aa4f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:39:22 +0000

    apart exchange touch combination really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
