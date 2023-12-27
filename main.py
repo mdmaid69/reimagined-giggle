9f924d396c2d13b7d0a061abd9cdb0e6e91079f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:21:16 +0000

    power payment his trust crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
