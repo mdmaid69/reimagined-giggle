cd8a209f980c1300824267e3f4c91378071904f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:56:32 +0000

    downstairs emphasis ask majority introduction

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_floor(x):
+        return math.floor(x)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
