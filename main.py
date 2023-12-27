119cbbc6d66a7d9309e9c9ad3ceb65fec9c0f5f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:30:16 +0000

    middle body guide wire pity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+for i in range(5):
+        print(i)
