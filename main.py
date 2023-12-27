5367a6267d15380c394d5e9da16ec57e2c9b8bb7
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:40:20 +0000

    importance successful crisis impressed supply

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
