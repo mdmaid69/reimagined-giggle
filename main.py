dc132243c3cc1c31aa62e98654c5de8e6c86b6b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:38:41 +0000

    safe pronounce term own method

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
