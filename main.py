8ba7f842faed976dcdd6e3ff0ba37b79dfa69af8
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:16:18 +0000

    564encourage direct past bringback fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
