c4a3a9dfbf0ee3113ace9bee8c706461bf69dae2
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:45:53 +0000

    lay cause buy history press

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
