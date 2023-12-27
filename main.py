ee745d8dc36a49d5f452b5857d408c38c3ad03bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:47:43 +0000

    anxious hair recover future bread

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
