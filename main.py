fee4757c0675ae6a07cdd9556fb6b9b0db257d99
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:12:05 +0000

    suddenly responsibility gooff skill degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
