fac72caa7050a92bf5d8bd0da6c3870d27c61bc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:53:55 +0000

    down business borrow further probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def count_elements(lst):
+        return len(lst)
