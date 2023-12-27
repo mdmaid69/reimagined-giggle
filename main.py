6def0aa54b27a879896bb8a881642b2c5911af46
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:39:29 +0000

    pupil good next emotion battery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_speed(distance, time):
+        return distance / time
