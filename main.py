b31706ebfc42c9dfedeacbb53ee0cb53a7ffd563
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:37:52 +0000

    before connection again class radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import sys
+def exit_program():
+        sys.exit()
