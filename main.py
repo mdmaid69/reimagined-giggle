858458e37a99c3125c0e9fb3d10e449851039a0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:01:38 +0000

    trip means colour period strict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def find_max(lst):
-        return max(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import random
+print(random.randint(0, 100))
