5b426da094e199fa1d47d0308af88e452c562689
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:33:22 +0000

    train offer talkinto backwards pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def is_odd(n):
-        return n % 2 != 0
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
