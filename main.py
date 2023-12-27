e72cb6d2eac107bb2554334622ba7f9c75014a9e
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:18:51 +0000

    Tt rid soil breakthrough visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
