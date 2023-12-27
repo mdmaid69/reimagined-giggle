c9510f8b322f87edc980a4b7bc6bbf89486d5bad
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:09:36 +0000

    score carry ignore out damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
