6793ab755a42870f18ae50cf1283952eb8f68b17
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:57:34 +0000

    any mend uncomfortable confidence mess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def append_to_array(array, item):
-        array.append(item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
