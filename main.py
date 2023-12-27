26228381a10bd666558ef15cb80ecad72e636399
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:42:12 +0000

    pretty correct count repeat love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import random
-print(random.randint(0, 100))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
