a961001bc9fa6503d8c6ef66234c9dc74242fdba
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:48:14 +0000

    around close clothes stretch crash

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
