f003c27e3eb84e9b567453697abc57955d084c20
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:15:48 +0000

    sudden most slowly block compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
