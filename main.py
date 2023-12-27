c5b547ecb0b0f3a0030feb62a83b2fe444b284f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:34:41 +0000

    patience bound scheme slightly bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+for i in range(10): print(i)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
