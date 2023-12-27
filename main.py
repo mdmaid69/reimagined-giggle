ec47c4cbfbfc77d3e613ec4a249406e994846948
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:36:01 +0000

    smell university hungry large escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
