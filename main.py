6c51d51c000f16313a328a436337d6d8198e006d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:35:58 +0000

    sure fork trick string importance

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
