5e54a601350cfed9feafbc02f60aa54003053393
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:24:05 +0000

    generally nonsense meanwhile comfort guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_user_dict():
+        return collections.UserDict()
