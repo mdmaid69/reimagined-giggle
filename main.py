a6481bbd570186f2dc07ceff5973e30e602622da
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:03:13 +0000

    ours unpleasant practice attempt centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def square_number(x):
-        return x**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
