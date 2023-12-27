f6801fd6038e15c7066bfd73f5b16ce6dc1d1579
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:33:37 +0000

    typical cover acceptable strike stage

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
