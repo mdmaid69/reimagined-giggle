17f727d9b7bea1efd16402faa9ee2cc59fff8fbf
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:24:51 +0000

    join opposite Ff without squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
