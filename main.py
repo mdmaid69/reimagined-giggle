c976914bd0bc9e714dc919467a306bb562f027f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:33:36 +0000

    abroad pair closely apologizeapologise say

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+print([x**2 for x in range(10)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
