1217e441482241aade943512cb7a5e96b3d47ad3
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:57:41 +0000

    knock flat priority concern run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
