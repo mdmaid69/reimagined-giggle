ab8aed7fb188437d81976b24969cc121badcf139
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:43:11 +0000

    mood hold answer bother secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
