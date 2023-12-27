a797faab3f717f29c0663eea702116dc305f0d73
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:01:41 +0000

    develop middle area book accordingto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
