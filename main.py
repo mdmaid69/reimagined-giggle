c64efb72fd06d326df0cbcf059f72194435ef091
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:31:44 +0000

    unless whom direct hand against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import sys
+print(sys.version)
