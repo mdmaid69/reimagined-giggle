e2c7a90b0e402e2bdc855f953544232897130753
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:56:41 +0000

    Kk unlucky pause somehow odd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
