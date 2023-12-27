eceeb2f9fd288bb19d9bde645e87c2801ee910e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:11:55 +0000

    Tt glass half match sharp

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
