126c570c078b9f0a4076762550274177f54155c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:24:55 +0000

    public interruption respect project arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_itemsize(array):
+        return array.itemsize
