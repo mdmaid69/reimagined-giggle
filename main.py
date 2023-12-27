cc319419444b5c51993f76a15a9342faae68b355
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:06:09 +0000

    bill maintain afford shallow clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
