ef818f54241e888ab1504b35d53eb20cb954fc69
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:14:37 +0000

    mine wait development still admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def convert_array_to_list(array):
+        return array.tolist()
