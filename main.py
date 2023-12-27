177a5f3c949c42c50dd2e01eb09e05181edf6f75
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:45:29 +0000

    connect send boat popular judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
