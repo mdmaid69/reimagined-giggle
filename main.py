62185711e4ab3f39001818a38d16b6089ac3dda8
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:09:17 +0000

    as total name decision shall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def count_elements(lst):
-        return len(lst)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
