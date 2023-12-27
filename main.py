3b83ea745f2c5f2b5e8770e8f6aa13783d8b856d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:24:28 +0000

    walk stop slice tough walk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def is_even(n):
-        return n % 2 == 0
+def calculate_speed(distance, time):
+        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
