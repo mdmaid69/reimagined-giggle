bfbea1a45c7da22a4cabee361215ca97dd691f39
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:32:47 +0000

    true train treat slight rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
