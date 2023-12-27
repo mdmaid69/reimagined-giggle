ce22572a83c6c6d1f349dd30edadcc646f613a9c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:58:04 +0000

    official innocent turn lock room

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
