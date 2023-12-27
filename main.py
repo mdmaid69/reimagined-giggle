da277dad3ef76eef5148c3149b71021ffc0d8f83
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:38:43 +0000

    giveback pick whoever criticize activity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def reverse_list(lst):
+        return lst[::-1]
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Cube numbers:", [x**3 for x in range(n)])
