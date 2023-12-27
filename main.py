2287e6fdf6d6e904489468f9d79ef6ddf99f3a23
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:50:31 +0000

    most disturbing no go begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def divide_numbers(x, y):
+        return x / y
