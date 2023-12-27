0d149c25f7a00d2f921c2a7ff04968a3210312c6
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:46:16 +0000

    callup free kind what across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
