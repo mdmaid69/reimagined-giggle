eb2ce75da5de582912317f0dedab995250339475
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:47:02 +0000

    lift snow happen total match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
