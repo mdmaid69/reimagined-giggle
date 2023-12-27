0aef9345f56ddd478568e7d728f2863d2bf2a06a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:47:29 +0000

    thirsty march anything sit deal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
