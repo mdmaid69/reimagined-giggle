8228e676ecb609ace2f4f11489c31e0a0a81c421
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:46:56 +0000

    discuss herself work extraordinary pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
