adc9b9d238841b4640e2a7eee344cce5338e7481
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:17:13 +0000

    either dance treat time public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def sort_list(lst):
+        return sorted(lst)
