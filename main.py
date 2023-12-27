a3c74cf65f9ee9ebcb393aed38824bf16bd8c4da
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:09:10 +0000

    change protect immediately wind view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def sort_list(lst):
+        return sorted(lst)
