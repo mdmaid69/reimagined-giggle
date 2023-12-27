5854bee6d587fda3c53ed7fbf9174c30116c6204
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:10:46 +0000

    weather argument fetch question usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
