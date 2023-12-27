60a5625820eef50b96bb330e12686b4c703b120c
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:35:45 +0000

    uncertain interest interview rush wing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import time
+print(time.time())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
