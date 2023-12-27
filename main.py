25958d48be1f49d609d9202b1e35ef447b40949a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:22:52 +0000

    move invitation children muscle slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import sys
+  def get_python_version():
+        return sys.version
