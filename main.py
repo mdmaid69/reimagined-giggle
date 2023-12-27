7dff4218d1b2367bb8ce059f3ea682e4106aaf89
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:48:53 +0000

    stranger border loud pay account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
