753fa3373b03d6467d8a0180d2d7c6ef8ef45a2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:43:48 +0000

    ago fun communication 566end breakdown

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_volume(length, width, height):
+        return length * width * height
+import math
+def calculate_error_function(x):
+        return math.erf(x)
