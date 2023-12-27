7e2b3b9a8b29bac3dc5b3468a2fac0ed0655cf17
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:32:01 +0000

    sure familiar loss room accept

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
