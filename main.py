d25472a87cbb8cba7df6fe9abf983b78778da4c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:56:18 +0000

    Pp accidental hole burst move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
