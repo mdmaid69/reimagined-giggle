2352ee0f5740c86aa546d8a5d4c772818aab36b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:40:57 +0000

    on team question fond punishment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
