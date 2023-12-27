d7eda6a6f8a746f293380fd4c15596457e7c6869
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:38:32 +0000

    slip badly proof human minimum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
