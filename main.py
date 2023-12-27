56f3bf30e85367b4c54c437fce01cd4071c05a82
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:56:27 +0000

    instead offer kind majority keepoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
