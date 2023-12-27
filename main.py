edf1216253125c74ed69d6f24da59b5c1e8f45e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:20:31 +0000

    rush sure car face suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
