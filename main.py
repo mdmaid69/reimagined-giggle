be1466548927a0a141d863c55d2539a2906ec31f
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:47:34 +0000

    awake drawing course sleep handle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
