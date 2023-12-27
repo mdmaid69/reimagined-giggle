6ed23fa4205fc8636be98e67a69888b40fc97fa8
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:10:53 +0000

    clue fact cause grateful meeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
