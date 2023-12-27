199ad797f210e27427538cc4f96f492570d0e130
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:19:49 +0000

    history team copy gas fast

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
