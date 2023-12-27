eb0edb8deb726948eb6fe55ef5ce96b39c7c4c93
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:27:35 +0000

    sort this shoulder guide break

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
