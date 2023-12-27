5e91aedf832270b01d044c8facd599f89b076b2f
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:04:31 +0000

    hers fight illness learn lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
