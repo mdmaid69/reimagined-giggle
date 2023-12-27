e48d84f70aa5fcba51e5ef5c1d2ca58eae45dbbc
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:19:13 +0000

    holdon opposite powerful discuss bar

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
