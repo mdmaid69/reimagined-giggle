191cee6a1bd32ae1df5395b9dbb32ec7ca954b17
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:56:55 +0000

    deep breathe need by price

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_length(array):
+        return len(array)
