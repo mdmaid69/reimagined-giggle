2e99d17a26c8d17df6550e54bd104f46109af33b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:48:29 +0000

    rely section convinced star interesting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+print("Hello, world!")
