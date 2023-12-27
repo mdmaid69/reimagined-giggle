dc28ce7559e38709b05928c5463f01b0a368fa3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:44:09 +0000

    right suppose become passenger once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
