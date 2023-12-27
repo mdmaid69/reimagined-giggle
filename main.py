910518ad75ce55efe2ffbfc16c0c851d34dc79a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:29:08 +0000

    trousers intend error outside step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_floor(x):
+        return math.floor(x)
