1f0bbe0e78b5947b4abebee98ec952a4c0285b93
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:19:30 +0000

    breakinto connected cost meeting since

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
