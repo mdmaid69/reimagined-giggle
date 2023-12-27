76ff0c3da4b18a2d38599c0d4c445d7d0a49947d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:31:06 +0000

    unpleasant keepoff duty charge protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
