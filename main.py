4df3663ed63c681cba418bebca00e72f30f76bc8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:04:38 +0000

    underneath book cross send abroad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import time
+def get_time_since_epoch():
+        return time.time()
