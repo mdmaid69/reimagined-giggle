ebca212c217e79fde7be6ecd84dd865cf8091af6
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:41:02 +0000

    thank emergency whether true thumb

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
