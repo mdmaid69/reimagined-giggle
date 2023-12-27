d7f2383cdb2fed984968db15d82b6bdc92cb1005
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:37:48 +0000

    machine zero callout silent shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
