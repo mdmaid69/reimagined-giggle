7a08baf3d48af0b148ad4f52932d2f5cae660ff1
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:34:23 +0000

    release follow brother hour light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
