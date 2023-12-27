89a454ed40e359fc6653ba74b8a1fde5a387bfb4
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:14:09 +0000

    winter home murder base passage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
