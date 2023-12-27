2e04eae31d11c61e1bfb4c8c514575d081bd669e
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:32:51 +0000

    pretty float makeup body basis

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def square_number(x):
-        return x**2
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
