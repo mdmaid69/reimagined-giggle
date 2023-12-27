29a3f5a0a25f1b084f95a070d89ac3a16918fc29
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:08:30 +0000

    ocean comeround official slowly appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
   def create_directory(dir_name):
         os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
