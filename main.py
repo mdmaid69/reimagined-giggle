7a424f27f104be633fefe88c82c045004e18d38f
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:42:14 +0000

    arrive fly speed whatever middle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
