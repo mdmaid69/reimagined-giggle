f0b73e8a152368f2219ec8dc4a953c7844a5c920
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:01:13 +0000

    power distant special empty exact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print("Hello, world!")
