3c7bab469b8c0d637b8a88527167065fee17c661
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:01:15 +0000

    rush plain classroom although pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def sort_numbers(numbers):
+        return sorted(numbers)
