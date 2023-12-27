c02e14289008ed014580b7ff8063d82745c665a4
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:39:29 +0000

    Uu command bringdown takeover course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_rectangle(l, w):
+        return l * w
