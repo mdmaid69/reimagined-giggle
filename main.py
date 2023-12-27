f1b39917a3007795498c1456b5d32bd8ac5c937a
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:29:09 +0000

    cope correction ago telephone strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
