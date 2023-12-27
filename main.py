cbfecc18be4ec2616a9707d94cec990b5ddf9c1c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:40:02 +0000

    comment straight 572enter should public

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
