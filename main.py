70a2ee4337b4f8cdafc954c67a88e24d0f631829
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:31:43 +0000

    some urgent concentrate repair tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
