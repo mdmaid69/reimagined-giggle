e17a177fb5d79cc1f63db348b5092a6366b654f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:48:19 +0000

    scene off nice bringback finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_min(lst):
-        return min(lst)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
