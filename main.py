3d38e6d7ead909b38043978a3d0dff0480fbde49
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:42:09 +0000

    myself most drive bar keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
