09beb90082f4dce913fc5a6a40d709904d0bb4fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:28:07 +0000

    track noisy disappear shout later

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
