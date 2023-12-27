57651090c31229d5ed3cd375b71fb756c2402a78
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:35:54 +0000

    stir injury name wait house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
