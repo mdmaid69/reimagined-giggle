04a478946ed6b74fcddcfc594e24c096e4c9fd2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:37:39 +0000

    profit tip calm active include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
