daa27e3ce5b8bfef147fca908dd71005ee70ee2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:42:48 +0000

    correct bye platform fat giveaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-print(sys.version)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
