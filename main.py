28eca4c9971879a5211857cc5bd096cfa740e376
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:20:45 +0000

    thankyou admit forget immediately thorough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
