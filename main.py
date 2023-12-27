33a82c019c1aadb8caf81c872033df17ca634ef5
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:24:55 +0000

    authority battery fight achieve anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
