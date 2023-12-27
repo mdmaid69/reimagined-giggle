03530223439b16ffc4f9423ecbfc4704d39b7f48
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:06:40 +0000

    themselves us one receive behaviour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_power(work, time):
+        return work / time
+import tensorflow as tf
+print(tf.__version__)
