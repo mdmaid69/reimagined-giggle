5d7d9f40fa75436d711f2c385e3b85afbe22e900
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:39:06 +0000

    regard cut fall put keepfrom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
