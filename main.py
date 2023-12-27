6a1777f98d3e80667c37b684bf62d477bb5dd713
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:44:18 +0000

    so recovery vote tap battery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def is_palindrome(s):
+        return s == s[::-1]
+  def remove_duplicates(lst):
+        return list(set(lst))
