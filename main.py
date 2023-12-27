beb1434f56c2656d11e9dcd67530bfb89359d845
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:29:46 +0000

    occasion shock by throughout high

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print("Hello, world!")
