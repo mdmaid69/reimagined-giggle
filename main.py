1f10d0f5660e26011de75d727da0f9beada84038
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:11:52 +0000

    completely skin more excuse pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
