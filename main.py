b6df0585e498cd32176c01122f87beee73ae96ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:31:14 +0000

    age than sad zero cut

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
