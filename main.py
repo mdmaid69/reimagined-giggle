67642041af2e2f9910ade3f499fd38d2b0219e5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:17:13 +0000

    other exciting measure my hold

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
