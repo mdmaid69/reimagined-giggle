b65d62795fdcb27f45e13ddbf47afdd26c7c0300
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:58:50 +0000

    light steady shop their Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def remove_duplicates(lst):
+        return list(set(lst))
