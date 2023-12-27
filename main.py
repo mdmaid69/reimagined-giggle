cba619068e441015feb3f8161678a1a633c8aa6c
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:06:46 +0000

    dear succeed terrible down Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_length(array):
+        return len(array)
