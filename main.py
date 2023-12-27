d30d7d81efe9bd6d5dc97122b7238793c2232f2b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:44:10 +0000

    him arrange test shake glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_length(array):
-        return len(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
