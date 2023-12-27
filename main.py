5fe845587cbaa59f1e451e2f52512500983de00b
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:58:53 +0000

    involve set hour turndown task

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
