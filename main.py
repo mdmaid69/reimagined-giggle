6b5c1fa01484192705a3737bdbcf2d9816c3c38f
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:35:11 +0000

    criticism conclusion possibly whoever size

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import array
 def get_array_as_int(array):
         return int(array[0])
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
