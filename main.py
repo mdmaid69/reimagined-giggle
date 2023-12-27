e11b3860de19b031057271e59b08d1db8801b47b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:25:42 +0000

    similar identify valuable connection exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import getpass
+def get_username():
+        return getpass.getuser()
