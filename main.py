2a353f4bd10d8be19857a792223cd24366360a38
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:38:42 +0000

    factory shallow detail quantity lower

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
