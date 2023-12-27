9e777871bcb7190ac244fccfe2a79192f37ef4b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:48:12 +0000

    far proper reply copy range

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print([x**2 for x in range(10)])
