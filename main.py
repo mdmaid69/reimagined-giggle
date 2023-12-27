478e9de189931c348ea5119d5e9066aca7a28177
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:02:47 +0000

    confident turn keen better settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
