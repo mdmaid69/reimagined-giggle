aa00379217ef5eb3ab010a60c37ab3b50e45aa81
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:42:49 +0000

    keepaway season more wash general

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def count_elements(lst):
+        return len(lst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
