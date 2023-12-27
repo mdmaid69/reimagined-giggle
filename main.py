4d54dc6c6a794a0bf9c8ab11a3f17c075c951688
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:10:38 +0000

    choice less live limit book

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
