1e9d259d46dcaf2d9dea8e9f4fc163eb43977444
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:02:23 +0000

    keepoff honest shrink behind but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
