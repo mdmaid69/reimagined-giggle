b3c49b27ee3a82a50b26fb7aea3ff7d208113c19
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:02:07 +0000

    thirsty pretty lock breakinto dream

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
