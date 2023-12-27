a9c780aa68cbb5ceeb0c4d4c8520438d87b090cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:21:40 +0000

    impressed hand whom flavour equipment

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def cube_number(x):
-        return x**3
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
