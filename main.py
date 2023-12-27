1dbcb64afd489ce2606a22f67d7e61ab52dd8330
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:48:59 +0000

    belief sink excuse mistake miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
