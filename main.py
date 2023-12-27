5ca67a2688263cf8a6fa9b0dafeace895b02684a
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:27:47 +0000

    bar damage basis position begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
