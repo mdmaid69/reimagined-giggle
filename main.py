1897913e9ca85a312b33005615273621aace47b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:29:33 +0000

    zero slowly pause swallow pass

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
