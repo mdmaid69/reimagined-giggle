6467da90e691800f632aeaca629f7da3981949d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:13:14 +0000

    instrument fortunate meal spill him

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
