62fe08a4783f0978e81e214b1f6a21d247f2a76a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:04 +0000

    outside lookup he length tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def convert_to_hex(n):
+        return hex(n)
