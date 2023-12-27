ff8616ebd1e12d581215b64fe7525da8cde7e82b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:47:03 +0000

    dangerous short process convenient raw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
