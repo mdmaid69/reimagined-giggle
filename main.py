eb4c678f84f4ba802b66b8b7f426866af95acc82
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:43:58 +0000

    exist or form never walk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
