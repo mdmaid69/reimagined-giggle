ee091db17b7b38e8e13db260f4821bebf8e166dd
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:37:51 +0000

    both path sleep mark attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
