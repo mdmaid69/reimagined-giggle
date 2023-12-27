98336874175392bd43edac2c699ba009f15bfcac
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:59:29 +0000

    always break pretend private visit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
