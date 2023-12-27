e6d6e73d1802ba9860fa1ee253be354603e88b7c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:15:40 +0000

    tight content hole press central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
