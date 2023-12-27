5a7fbc11a51dc124963cb4eec24eb32e82e50d20
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:49:15 +0000

    sorry treat instant charge train

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_typecode(array):
+        return array.typecode
