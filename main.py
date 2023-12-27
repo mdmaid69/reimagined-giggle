92cc46586b10d4e653c5182e3261e88733bfc51e
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:46:48 +0000

    adjective lock opinion manner say

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
