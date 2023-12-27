f69bf8fc231f2156abcff086a9a076c4577af637
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:05:10 +0000

    plastic tap uncomfortable getoff fortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
