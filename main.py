f48e0c90e184c02e9022b3dbdd1a7d4d4e1f2e99
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:50:25 +0000

    pain discover remark yet concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
