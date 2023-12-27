dd2fbf509f193912f3cd876ff1e6ca2c5adc827e
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:32:27 +0000

    sister accommodation soak responsibility safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
