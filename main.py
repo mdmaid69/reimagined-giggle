9f5f1ff7863c4e5761b5279acb341bdf03ab7d4a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:34:36 +0000

    continue freeze party as equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
