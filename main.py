7726034eefa50693d04adf3157432663bbb5c27d
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:54:04 +0000

    care runout fly rate question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_factorial(n):
+        return math.factorial(n)
