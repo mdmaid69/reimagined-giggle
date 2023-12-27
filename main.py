0653ac008433fb5ce768a8544c9c21dc61a742de
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:18:13 +0000

    me shape knowledge build one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
