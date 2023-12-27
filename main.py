33d18fe062d58619da165ea9cb84387ca32aebe4
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:28:32 +0000

    here damage apologizeapologise politics worst

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
