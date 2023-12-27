213d7a6c12ae98f34fce0f68f27b2e671feac3e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:14:31 +0000

    start date continue borrow wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
