9000c525c683f0842b3a4ea344ea04108c6abcc6
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:08:41 +0000

    model fasten ours almost heavy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
