d6f16bbfdce87c18d9fed554a2edca6e1c2262c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:28:05 +0000

    afterwards male convince point limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
