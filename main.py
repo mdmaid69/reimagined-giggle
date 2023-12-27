e8f242051b53e7a656ec2f5449a670d1857adc72
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:30:17 +0000

    fierce muscle pause issue right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
