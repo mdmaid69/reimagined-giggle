93692a7b26a9c8c64add188ac3167e7691648882
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:13:42 +0000

    strange also majority very obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
