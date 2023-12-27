d49900aad327b1bdb4c139c958c0bd0974313419
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:01:56 +0000

    spread slice seem tough shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
