e9e19ca2bb1416c4a8ca7153ac2fded6e05eff9f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:40:58 +0000

    wide fly interfere now properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def print_python_version():
-        print(sys.version)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_set(array):
+        return set(array)
