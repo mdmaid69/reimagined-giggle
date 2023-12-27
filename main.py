7bc4a3b3b24657eb6a7728b2213238bddeaa30da
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:33:45 +0000

    smart bringback shoot persuade turnup

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
