d4aea69afd6da3902f6aa7223a00bf3c8cd920fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:48:03 +0000

    holdon begin talk crazy begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        print(sys.version)
