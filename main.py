5fc83d131b998b009ebedef9888e22df56e6dc5e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:10:54 +0000

    original damage score may pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def is_odd(n):
+        return n % 2 != 0
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
