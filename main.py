37cf4c9cfb15358bd9e5b5ac8a49f665cfcbece9
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:13:33 +0000

    accidental negative bury walk appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
