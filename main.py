f259d157c28ad74a18764514b6b3bb81bf4a1fb1
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:16:12 +0000

    shall crazy set afraid Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
