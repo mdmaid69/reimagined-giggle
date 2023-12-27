db5ef2df9c064b227ce6798d3484d5534374155e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:32:07 +0000

    month fun thinkover contribute destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
