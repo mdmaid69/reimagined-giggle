95e37f583d41b5fcd02aa2295ba10c4c258ac4c7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:40:28 +0000

    autumn behind jump stage comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
