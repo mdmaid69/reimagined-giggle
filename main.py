6d277b4d280cf1e3e1ba8f84ea26082167aa8d8e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:33:09 +0000

    wish conduct bin walk hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import random
+print(random.randint(0, 100))
