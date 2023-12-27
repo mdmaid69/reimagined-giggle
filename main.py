6131ba9ac35986a2c76dc78a64a27cf4a03d6556
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:28:19 +0000

    statement passenger opposite scale nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
