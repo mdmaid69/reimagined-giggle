74405838b6b51700bfe6072ee3bf3308048a943c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:05:09 +0000

    date edge comefrom memory drink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
