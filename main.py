2307a2c6351c9177655007384d8cc08b69e06743
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:20:11 +0000

    arm our comeover brother transport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_floor(x):
+        return math.floor(x)
