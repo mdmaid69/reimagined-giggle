e8a73a146b0267d58afcc63d792038b01e2f7195
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:20:17 +0000

    callout breakthrough bin let dead

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
