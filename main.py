d421124198ce7865b92649ae8c40f5876a4888ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:40:00 +0000

    phone match colour crisis offer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
