0a6a0c8a1f27e951bd86defa579770b630aaea55
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:06:56 +0000

    runinto delight sell care play

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
