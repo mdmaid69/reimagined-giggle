d5e10fe57392b69cb0f022398785868b71e67e43
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:41:16 +0000

    community but sign wander behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
