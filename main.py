2648fcfbe74e512673e77e83aa34dacd24e21132
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:40:17 +0000

    increase moment generally fetch Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
