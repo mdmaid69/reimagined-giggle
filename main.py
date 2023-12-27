6e08b88e913200da5b071f8865e57172f79671cf
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:54:35 +0000

    steady maybe tonight claim record

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  def is_even(n):
-        return n % 2 == 0
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
