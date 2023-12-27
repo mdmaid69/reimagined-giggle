342a916639addffe8abe7682a55191f54de9f66a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:18:46 +0000

    work branch increase dance emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
