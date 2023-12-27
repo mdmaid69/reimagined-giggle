d8f9385e29e0c6717df0d403a6c22ae03e459fc0
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:04:56 +0000

    deaf serve instant steal murder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
