5962c6860ee1464dae0ab357195ff619e7847f20
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:13:33 +0000

    far but until back bringforward

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
