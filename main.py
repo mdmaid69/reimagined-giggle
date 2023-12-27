196b0b6b32c062417984139b5eb7e945a955bc44
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:32:54 +0000

    plenty old fix breath steady

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_today_date():
+        return datetime.date.today()
