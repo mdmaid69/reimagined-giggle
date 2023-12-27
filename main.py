7929820e71347d67144409e2b13244a8eb356270
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:20:55 +0000

    past whisper beside government go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import collections
+def create_counter():
+        return collections.Counter()
