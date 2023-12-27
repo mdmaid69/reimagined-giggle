580743a7d06cd9f76bf9a0f5c036b9572c73f695
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:28:01 +0000

    pronounce unit once ashamed spray

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print([x**2 for x in range(10)])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
