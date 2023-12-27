4cc1606731bef380e50de49e150ff9b9c2188c99
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:13:36 +0000

    weak set refuse message serious

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-print(sys.version)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
