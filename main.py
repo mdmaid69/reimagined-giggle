242d36bf422ce5fe3bae28bdb13016bd00b7964e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:46:33 +0000

    talkinto getout human kick file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
