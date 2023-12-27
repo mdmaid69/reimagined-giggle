5a5f2ac879d4f44b4cd4aaa93a73877c129f3f83
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:14:01 +0000

    wander add occur wait exist

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
