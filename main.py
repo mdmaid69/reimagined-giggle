4c7b8046eabd537fe5007461fc1b52a0f034210f
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:32:30 +0000

    learn roll past division whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
