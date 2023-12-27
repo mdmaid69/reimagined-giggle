7d39878ca9fe14f2b967a53b44083a14f12530f4
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:59:39 +0000

    actually in keen last kill

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-import sys
-print(sys.version)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def sort_list(lst):
+        return sorted(lst)
