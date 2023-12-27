a5df91dfd32614ed4898de9792e60cf5c20a545b
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:14:17 +0000

    for tend opposite make connected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
