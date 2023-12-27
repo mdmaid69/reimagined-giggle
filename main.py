f9e24c3811ec861e73eabfceddb0623ad91efaa6
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:41:54 +0000

    heart capital promise tap disturb

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Reversed:", text[::-1])
