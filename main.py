40a9fa1e4409f67209a64796c46e42e4f02cfe17
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:21:04 +0000

    suck wing equipment attract either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
