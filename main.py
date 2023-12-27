b87f20a996a1a7e397af5a1eec82e437b7e34223
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:07:19 +0000

    kind adjective programme interfere easy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
