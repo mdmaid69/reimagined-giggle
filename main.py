c4a838b99c565fc204ebc6f8446e3dc2cd88a2c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:40:24 +0000

    emphasize interview send into accidental

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
