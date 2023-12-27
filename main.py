a6006363d87a6482a58118ea2f17cdf617d1e37e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:54:56 +0000

    arm telephone embarrassed look thick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_set(array):
-        return set(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
