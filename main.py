8e6880d8db4bb88229bff0cbd8701cf78b1e6b34
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:52:15 +0000

    who match anxious official send

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import random
-print(random.randint(0, 100))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
