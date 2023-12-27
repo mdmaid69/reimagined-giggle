b8760592e71d6647217c11654f12380556c02c83
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:22:00 +0000

    back single spend before behaviour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
