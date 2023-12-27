2785ee190c28f6ab004d8f52c3479a6ad70a4c18
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:25:18 +0000

    occasion meaning idea clean government

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
