e2b5d25b764ba586c3f9d1b1162b9deb55e0c177
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:58:22 +0000

    566end sure bend allow flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
