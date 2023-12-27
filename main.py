4e4e3a06aa7f41bc1fbce7baf12c21c1a1714cd7
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:27:02 +0000

    actual bet apply Vv wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
