688092d0d37c0f8fab2f3dd188d3588fb958167d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:54:21 +0000

    town proud invitation probably band

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
