6844fb0d59b171134cff5bcf35e9a6c6235a8d66
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:04:15 +0000

    march trick pour very success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
