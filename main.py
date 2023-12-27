ffbdc061d90d5421b9db5fd6be8008b6845f72e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:32:25 +0000

    invite slice murder visit plain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def add_numbers(x, y):
+        return x + y
