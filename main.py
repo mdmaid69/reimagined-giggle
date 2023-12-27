14e76d834d05fcc8f085fe41ff1fca5a70fb5239
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:14:32 +0000

    phone along sore deaf exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
