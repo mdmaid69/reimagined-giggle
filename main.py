14abf456733febc691a4941fefeb040677aea697
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:20:19 +0000

    soon forward female smile silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
