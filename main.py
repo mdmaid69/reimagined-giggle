31b1b8309337e902f7ba316e27fea29180699518
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:19:26 +0000

    reaction trip share develop soil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_even(n):
-        return n % 2 == 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
