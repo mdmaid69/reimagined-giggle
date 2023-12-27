f095536473305fb3f1f38f67b74e5b119a4a3808
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:55:52 +0000

    priority bound listenfor sail depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_area_rectangle(l, w):
+        return l * w
