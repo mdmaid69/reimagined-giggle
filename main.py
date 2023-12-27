cf48f6452ed03efcd0bf119c50c892ef17bb5c0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:25:28 +0000

    next door like persuade strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
