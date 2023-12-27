80aedc9df91041744569349b7e1cc7a0bbec85df
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:25:32 +0000

    central path boil cinema between

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def greet(name):
+        print(f"Hello, {name}!")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
