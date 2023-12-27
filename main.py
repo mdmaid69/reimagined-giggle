f7015efa8a575657f7ce3d7fafc70c3ee4782c2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:27:08 +0000

    personal gather oil uncomfortable art

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def add_numbers(a, b):
-        return a + b
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
