9c22260ea16cade0af7fb37882323ceb226df221
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:06:33 +0000

    especially church temper ordinary uniform

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_set(array):
+        return set(array)
