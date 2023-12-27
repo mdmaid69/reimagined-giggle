70b9866e7de34806910aa0d62ae4f20136c0e9e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:07:11 +0000

    original year extra action health

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def cube_number(x):
+        return x**3
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
