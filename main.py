390b0cffd38afc4b8b86ddd5dd3cd72830739c14
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:11:23 +0000

    force concentrate each match scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
