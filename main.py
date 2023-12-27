1e6a25c041f16938d20ab8f0c6436b343dbf1d30
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:49:57 +0000

    narrow come turnout invitation laugh

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
