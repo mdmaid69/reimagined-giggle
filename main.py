b158f90d936659bb28f3c5239ca89613b8362c5b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:55:39 +0000

    free welcome will cool race

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(5):
-        print(i)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
