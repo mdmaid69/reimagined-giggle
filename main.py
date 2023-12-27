b60f6aa7f6505e070f39ff644c82b7160af3925b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:39:19 +0000

    quantity goon relax record makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
