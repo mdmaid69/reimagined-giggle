0ebdbb3f4b358b37e17b5970c049ba13ba418631
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:11:58 +0000

    estimate never punish stop getout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
