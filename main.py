12de8c319450160a47492f14d961c0181109043e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:04:34 +0000

    disturbing label confident breakdown excellent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
