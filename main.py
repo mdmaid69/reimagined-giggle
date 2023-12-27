b4d795ed900a0410d37e384f003e93d0e5ef93a3
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:48:17 +0000

    Cc sleep fact recover ear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
