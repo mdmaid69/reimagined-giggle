f3b461bbc681083629b8712c8e10efcf30419e41
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:08:05 +0000

    port tone mind arrival become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
