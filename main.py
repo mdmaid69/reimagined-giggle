10bd8ed4c79f13ec082126e9daaf755e3a11a39a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:43:07 +0000

    advanced release label game let

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def iterate_over_array(array):
+        for item in array:
+        print(item)
