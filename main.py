0d77ee57a0b85fc515ba2f49223e0210a6284d46
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:08:30 +0000

    destroy involve impatient excellent for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
