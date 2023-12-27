a1db0c9f69b69e62d08dcc77e3bfb79bc514f8da
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:31:33 +0000

    565end today male send disturbing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
