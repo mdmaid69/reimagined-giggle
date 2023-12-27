9f9f03b6c8e1d2d965162d4d7f81782ef22cad74
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:10:19 +0000

    happen temperature state polite deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
