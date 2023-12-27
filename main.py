afe61bcf61ddb6c399c1068ab613cdf67c257781
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:37:08 +0000

    point cook original note opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
