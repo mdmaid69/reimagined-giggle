1ad9b35d7987dd6c59ef7d6c56f765a3153dc7fc
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:36:12 +0000

    public less certain pour cry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
