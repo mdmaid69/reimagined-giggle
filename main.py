f34c6a9335f1a80f88141fda21910610d297b0c8
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:28:29 +0000

    sink approve later name wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def remove_duplicates(lst):
+        return list(set(lst))
