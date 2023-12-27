cd9a8d0091f2ca401dae9455f48f1454d4038a4a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:02:54 +0000

    adjective thinkthrough definite opportunity hang

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import collections
-def create_queue():
-        return collections.deque()
+print(sum(range(10)))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
