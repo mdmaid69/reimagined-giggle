e59c36c94b89ce1ab6fb7930ecaea4ffa40bcb1b
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:36:39 +0000

    gradually often match somewhere machine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def convert_to_octal(n):
+        return oct(n)
+def calculate_density(mass, volume):
+        return mass / volume
