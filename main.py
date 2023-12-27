7beb7418f13db60d94099a896b99e7e5b337c898
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:52:06 +0000

    justice in getback level well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
