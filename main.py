6ccca9ab28c6a327412745e60ebfb3f56cf9e22d
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:03:27 +0000

    language thing practice blind exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
