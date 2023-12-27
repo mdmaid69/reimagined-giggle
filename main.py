ecedf25541edfd5d51c8e91f8e202a1802c0d8d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:46:29 +0000

    behind precise material instant gap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
