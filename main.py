0713e75369f9423df36985c929add97211e0a2d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:10:46 +0000

    sure advice negative female advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
