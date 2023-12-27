0ba2cc3e2fa26ad1d4a10b51d3e4c1dc6a55c7ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:59:39 +0000

    fresh dry start famous interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
