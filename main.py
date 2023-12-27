1c07b9c98d054ca0aeba3bd5353c696140b86d52
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:02:26 +0000

    illness cold Bb occur point

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def reverse_array(array):
+        array.reverse()
