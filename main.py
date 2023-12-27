9997fa51cb0dd9ce7f91d281cfbfd763bb6b0aea
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:15:51 +0000

    mark wall well official convinced

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
