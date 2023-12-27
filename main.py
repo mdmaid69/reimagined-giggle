756e62385a485827d7a198d54dbe3df3c367cb99
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:55:20 +0000

    clear imagination mean task Hh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
