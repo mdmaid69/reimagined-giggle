00afd6ee0b9c89bebfdbcb24f57c2cee3e78d09f
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:13:01 +0000

    label kick announce subject noone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_volume(length, width, height):
+        return length * width * height
