c84a8b3bb4dd1d194051bc0ece476ab6f0c07c80
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:07:53 +0000

    miss meanwhile clearly how birth

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(sum(range(10)))
+def sort_numbers(numbers):
+        return sorted(numbers)
