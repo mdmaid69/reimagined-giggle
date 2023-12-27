491f666dbb7bda5952fc73f07f9b94a78a4c1c05
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:03:42 +0000

    slightly float whistle fork clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
