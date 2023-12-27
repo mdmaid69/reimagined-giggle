7f871c3061944d544cf00e5e70458bdffc5f9f82
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:35:58 +0000

    mainly ours clearly least anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
   def get_current_working_directory():
         return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
