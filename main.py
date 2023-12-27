6b33ec10171d0661d92f9b80a7f8c32931cc8a09
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:38:14 +0000

    slightly listen hear wave rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
