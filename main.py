fc12ce5b24404bfb2cd108a63e60f155221a49a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:56:33 +0000

    exhibition strike cap bottom balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
