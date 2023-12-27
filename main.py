9e161814bdc4dc55ab07c9f48a28c41dbf59a3b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:06:08 +0000

    night cut upset wise will

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
