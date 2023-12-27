0eb2d1aceadb5bed76d56932403ced0b276eebc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:18:43 +0000

    left in furniture pretty strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
