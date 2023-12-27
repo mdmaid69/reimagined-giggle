28d448bed2cd5b671edaacb0ff80e04925f3c82d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:02:11 +0000

    beat criticize accidentally workon nor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_power(work, time):
+        return work / time
