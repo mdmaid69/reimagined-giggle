988a6b6bf41ff2080bd63c15cab9a3b2864dc251
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:20:54 +0000

    clothes about apologizeapologise 564encourage learn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_item(array, i):
+        return array[i]
