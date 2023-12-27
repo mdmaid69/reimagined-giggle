256874215af2ad8d8a1d854f7bfbfc418f856a09
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:31:15 +0000

    improvement fit question she borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
