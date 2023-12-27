f57021f16e01f6712d74ba53e17d939bf83d5bd8
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:57:28 +0000

    world mark healthy mess wing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
