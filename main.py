5590a4726a243ff0fc000408324dbeb6596cccc9
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:02:33 +0000

    worse disappear meal general polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
