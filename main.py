7f685a8839ffe4efe658aceeab07a1621304028e
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:34:40 +0000

    thinkthrough private workout anxious practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
