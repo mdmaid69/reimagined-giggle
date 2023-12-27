bb99913710b26bf9098105716637de95c7a19d5c
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:34:10 +0000

    high as speed properly unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
