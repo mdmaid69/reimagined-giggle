aa48a10851b1b0eac7c655f36e82d9059bd9c498
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:46:49 +0000

    scream path comment complete instrument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_bool(array):
+        return bool(array)
