127bbce8a607cda9da47f02c43cc03a3b8c76cdf
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:50:22 +0000

    point dead blame kind strength

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def convert_to_octal(n):
+        return oct(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
