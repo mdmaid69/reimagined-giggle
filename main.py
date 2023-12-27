3906dc5b3368f27e99ecc7ad18f31c8598f6ec3a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:36:09 +0000

    risk watch blind pipe support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
