4afc96e105fe3ddcee222a193275df42b204fb65
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:19:52 +0000

    stare arrangement official female anxious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def subtract_numbers(x, y):
+        return x - y
