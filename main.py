c8259d268271952b0f7ace14730135c1a20579f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:57:46 +0000

    arrive remove land bringforward area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
