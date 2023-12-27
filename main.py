f2e3af6312578c14ea64e60b982a3b03a30fcad6
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:41:42 +0000

    far drop guide means poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
