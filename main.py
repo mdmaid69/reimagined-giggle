e28c023babf25585e53c5f7681bb1868a4225e36
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:05:13 +0000

    accommodation fill moment track post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
