fd2cd7fa7b1bb2a000110b2e36503ab78d7afa0f
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:44:06 +0000

    serious getaway check nerve nor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def cube_number(x):
-        return x**3
+from collections import Counter
+print(Counter("hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
