da4a006868b20b6793acc65190be996276cc628a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:34:54 +0000

    teach no train swing blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
