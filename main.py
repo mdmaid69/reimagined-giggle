b5d903d60983453648d8325aa923657559b86920
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:16:13 +0000

    mess maybe uncertain callat influence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_string(s):
+        return s[::-1]
