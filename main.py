e1f8e81016bea0e3a5fcba9da6b1f98cb9d6a2e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:35:14 +0000

    choice delicate practice smart interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
