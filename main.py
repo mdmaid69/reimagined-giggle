ccd0bda91e439522c9ac895eddc5e80251821c62
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:50:54 +0000

    close board section goback(to) keen

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
+def convert_to_binary(n):
+        return bin(n)
