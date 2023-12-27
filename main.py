8d9827e06643e8d3324b84b99452928c16aba897
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:07:06 +0000

    earth scale wise pile worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
