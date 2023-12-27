c74b12c89c7792c6743a7afb158965536c4afbbe
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:06:15 +0000

    shoot rate often no include

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
