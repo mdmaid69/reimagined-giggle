c3b2c99d32d94a9c860c360724beffc6220f4e03
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:45:56 +0000

    near 572enter at absolutely bright

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import os
-print(os.getcwd())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_current_directory():
+        return os.getcwd()
