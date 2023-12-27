53932af2ff63c270bb97319acdaa76195fa5bf80
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:58:22 +0000

    considering welcome clean dance fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
