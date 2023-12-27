9ab904e56305bc94a909984ce4b14f4e391e72a8
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:47:46 +0000

    maybe silent decision lesson flood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
