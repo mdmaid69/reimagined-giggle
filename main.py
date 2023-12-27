1b9b6e5e950b0ba75bc1e30b568ec70427881a56
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:00:18 +0000

    improvement play drive direct generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
