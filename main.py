c905e760fcd62ae9be31f39885cb1f9653c3566a
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:05:49 +0000

    breakin identify do show vision

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def square_number(x):
-        return x**2
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
