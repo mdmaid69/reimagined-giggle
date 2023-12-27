ad65b20c7071745a2639b87168ea0cbf79c39d9d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:56:14 +0000

    statement comment wise your broad

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import getpass
+def get_username():
+        return getpass.getuser()
