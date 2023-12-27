9cfa12ec4f8ac3a2d93b010b82e58a9752524782
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:46:35 +0000

    actually star mend tend hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
