252645e2bbcdc8f15d6a7373bdf86c91bfa31d8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:18:27 +0000

    object another container national over

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_list(array):
+        return list(array)
