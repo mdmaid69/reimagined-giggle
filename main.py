0bc9e40a35f29362d2cdbf7947d27ad8cc7c7dcb
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:53:49 +0000

    ride tell statement mass uniform

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
