000e1980e47c3da0601739ceb42b3c4cf296ccbd
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:19:23 +0000

    hear large bringdown tie shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
