1cd18a9c697f447ba32f4ea1902fcefd4cd8dc21
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:20:39 +0000

    deaf similar free spot charge

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
