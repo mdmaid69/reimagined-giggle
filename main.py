0ad3212050ca86ca4de3ebd43ed80fbcedc06a17
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:09:50 +0000

    fight why object goaway row

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
