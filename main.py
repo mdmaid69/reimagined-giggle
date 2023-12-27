c515f43c363551631248e99546853898d7c6702f
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:37:36 +0000

    tool positive boil mend moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import datetime
+def get_today_date():
+        return datetime.date.today()
