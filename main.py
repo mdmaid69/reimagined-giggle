2403d77edbc2bc0ef6b41e152509da0f760cac47
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:35:01 +0000

    damage insist depend sure phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import time
-print(time.time())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
