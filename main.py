85ce28a56f1463fc8753d1f297e2afac74347a51
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:20:29 +0000

    communication about deliberately money bury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
