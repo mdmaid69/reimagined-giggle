bfc6b70b08a65ab7c5a9a7255ad3b92fffa1fe58
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:55:35 +0000

    geton throughout clearly relation university

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_speed(distance, time):
+        return distance / time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
