917fed4d28e0e6d1acf89cdcdb39f2d7d78fa9e9
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:19:39 +0000

    ready yesterday ahead passage regional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_power(work, time):
-        return work / time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
