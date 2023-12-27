7ea74068e628a4068871863419903a9fae6fd421
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:06:57 +0000

    consist original company trouble formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_array_item(array, i):
-        return array[i]
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
