14f32a061736450b3218cae1a409c5aa32bf6148
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:06:38 +0000

    answer tip afraid fetch among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_os_info():
+        return platform.uname()
