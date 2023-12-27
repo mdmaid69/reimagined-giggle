98760fa78e1292ad7d7110cecd1931d5b2c37c94
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:48:16 +0000

    relationship slice behind expect on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  def square_number(x):
+        return x**2
