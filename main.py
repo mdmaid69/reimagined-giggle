6cb0cb8095577fb5c24886dd36ccb093227e1746
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:35:57 +0000

    tall computer available comfortable memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
