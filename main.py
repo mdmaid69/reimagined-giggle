b5be3879af2c9536caf01a52d11ad8dd71f20eca
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:23:27 +0000

    tidy care extra possible violence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
