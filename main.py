529cc678b6c2ba1c04c85820ebdeb92e0d58f695
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:16:18 +0000

    task no moral tip tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_item(array, i):
+        return array[i]
