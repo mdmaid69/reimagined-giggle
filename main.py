769247010ea78918c4f47683da5d4bbf0bad8efc
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:07:28 +0000

    beyond hardly compare present brief

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
