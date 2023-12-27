92712faf93590ced38f78ce5a43c053791f80dc1
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:13:59 +0000

    service get throw Uu occasional

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def square_number(x):
-        return x**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
