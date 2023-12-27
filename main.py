c0db0a645a6674f8bb1ade778d3c56d616d8eb49
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:02:15 +0000

    direction woman weigh suit description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
