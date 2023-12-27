7a2d7a4abe88b5a3380edf30b258a208116a8309
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:30:29 +0000

    safety dick exploration detail forgive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
