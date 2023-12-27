b006f14222c37efc29ce2c2f8b754d18ba66af25
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:29:53 +0000

    forever Cc public lower seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
