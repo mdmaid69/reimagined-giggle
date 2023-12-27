36a26cfb8e02dc601f86b896fa71f85b77ca166d
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:52:09 +0000

    delicate healthy understand deserve describe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import sys
-print(sys.version)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
