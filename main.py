d15e904c2a3de81365ef6cf4acea82fca548a82a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:43:34 +0000

    large scale inside getback release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import random
+def roll_die():
+        return random.randint(1, 6)
