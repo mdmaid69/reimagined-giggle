38db181c27bee5ca1056f24ecfed63d83c1f28fe
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:11:26 +0000

    Ff rush shall surprised post

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
