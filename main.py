19dd500a44981348074822eb8a1f537607445671
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:46:32 +0000

    left discussion criticize turn smile

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_min(lst):
-        return min(lst)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
