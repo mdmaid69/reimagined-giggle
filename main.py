198c0163ee63563855bf1d2a832dd4b7b7a4b2ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:56:41 +0000

    careful straight correction borrow charge

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
