f655a59e545e5bbd1f9bd8edf639ab75abdbaf10
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:55:42 +0000

    nothing sky opportunity fear regret

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
