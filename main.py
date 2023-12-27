2fc11303e59edb9d6ca50c9be5ea6665ddef062a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:52:44 +0000

    thought unfortunate step bus consist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
