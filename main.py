8f954360245da10bb7787ea7b2cff01765d781b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:10:28 +0000

    feel prepare holdon soon suddenly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
