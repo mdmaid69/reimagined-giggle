cbf32c5670acdf8f98e798d8b3a11b23ae6617c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:33:15 +0000

    hang shock start certain one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import datetime
-print(datetime.datetime.now())
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
