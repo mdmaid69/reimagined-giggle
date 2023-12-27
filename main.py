fbb5c04c385f7f55c201ed369841cbf3f5e7bc07
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:42:59 +0000

    fall use quarter goout dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
