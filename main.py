2dfe2766be7c0a2bbb788c115d9c7b498ead7a25
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:47:14 +0000

    violence innocent come quality emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_min(numbers):
+        return min(numbers)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
