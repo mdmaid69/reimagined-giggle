9d82a002f8966128e167b0268a48a0db7530d396
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:49:25 +0000

    hot cap choose main small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
