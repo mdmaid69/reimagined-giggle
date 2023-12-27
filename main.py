ee665750eab8222e444979e90186cbcf28b5e4e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:03:12 +0000

    once thick immediate certain key

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print([x**2 for x in range(10)])
-def calculate_power(work, time):
-        return work / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
