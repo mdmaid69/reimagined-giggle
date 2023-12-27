1b6f8aa4f53395cd94712f9f1b527b5a577d912b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:19:38 +0000

    fault getoff time realize name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
