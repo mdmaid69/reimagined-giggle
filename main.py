23d47aace8b1e1552a93272f4081b0c7d11d9617
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:03:08 +0000

    from cross pity underneath send

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_bool(array):
+        return bool(array)
