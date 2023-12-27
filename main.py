cfcc95c4cca0d885fd760981e407819a23b24e7b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:39:43 +0000

    curious wear pencil raw yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
