6328d45957c635e443e6cd90efd4959c8ca139c3
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:40:32 +0000

    contact busy branch worth speech

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
