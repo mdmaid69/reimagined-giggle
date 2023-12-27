47dafabca1c5a0e887d58f578eef473a8ba03b4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:16:03 +0000

    mention present your problem pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
