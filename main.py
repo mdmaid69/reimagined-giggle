ebc44284ccb4bedde070d06c93b2b7b8e478564d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:56:55 +0000

    dig mystery punish complete kill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
