203d00f12ab5855637ae6a410ec474d8de28ea82
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:26:58 +0000

    body begin great bread traffic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
