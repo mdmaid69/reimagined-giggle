ef627571d02364d0beab68ec16ec934e1dbbf6f0
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:53:03 +0000

    place behind belong wait when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_index(array, item):
+        return array.index(item)
