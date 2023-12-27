e0583546f23e5d865746970fa3fcf9dfb9d271ba
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:27:10 +0000

    replace fat tool respect regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
