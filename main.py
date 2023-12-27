3fb4f62882642bec9ac90620c290d676059d6096
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:02:24 +0000

    thirsty when appear human blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def convert_to_binary(n):
+        return bin(n)
