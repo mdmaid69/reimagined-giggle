4189ec5519b6d1135c705fab6d159a78372acc5b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:40:36 +0000

    example near wash thing feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
