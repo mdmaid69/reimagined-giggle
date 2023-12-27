3c8d78aa8b1de3d7f6a93dbe6570a1ffdad2f6ca
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:56:55 +0000

    thinkover let content pile bringup

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
