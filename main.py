a205bea062f71e39aae8099355d2e8294b5e3405
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:06:40 +0000

    workon discovery lower ride vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def find_max(lst):
+        return max(lst)
