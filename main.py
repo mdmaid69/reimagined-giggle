ce8cc85eb2a282d6fe15b0bbf74cc6a7baf04773
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:13:04 +0000

    hungry gentle ordinary shame taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
