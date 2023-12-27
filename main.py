47eb8bbfba10c3a247d0c3db83946b3e55772dc2
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:47:57 +0000

    wander welcome minor tidy available

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def is_odd(n):
+        return n % 2 != 0
