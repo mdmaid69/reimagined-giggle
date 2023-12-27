e7637cc2c381874d23020d7a011bb36c9a0b3a15
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:24:53 +0000

    increase manner hardly last content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
