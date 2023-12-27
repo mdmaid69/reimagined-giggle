c2ce5c30324580b0e64877f072e8eaf63e4c0144
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:48:39 +0000

    tap type food crash achievement

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
