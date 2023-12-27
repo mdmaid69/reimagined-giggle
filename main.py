9a09b0bbb04d80ace65821d5d4c3632d2e47e8e3
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:40:46 +0000

    visit walk movement patience hospital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
