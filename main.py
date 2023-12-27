26ecce113454153ea3a68628d13c90c18cf1f32b
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:04:03 +0000

    check by smell objection awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def set_array_item(array, i, item):
+        array[i] = item
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
