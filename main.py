d0480ff5554d30e6e738fe98ec3ac803ba114853
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:35:12 +0000

    pollution lonely authority write favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
