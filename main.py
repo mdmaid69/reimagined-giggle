7df00cc225f0acc59eb91e5ca5c919b9d8d6be1c
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:20:29 +0000

    thin claim warm me promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def square_number(x):
+        return x**2
