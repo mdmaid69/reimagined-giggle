79476fde8c6fab2315dd2d636be40c1c800cc2c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:01:18 +0000

    wild black however balance free

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
