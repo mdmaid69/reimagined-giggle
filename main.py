cff32d21730bf315a36073e0b6d04beaa9421a08
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:55:39 +0000

    sort outside twist example tell

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
