613a1730622b0567ad7496a6ec35e7a541872693
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:07:21 +0000

    available road communicate regard table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
