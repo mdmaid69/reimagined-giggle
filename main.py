b963f7fa033e7647e3cdaaacb6a853f8282b96a0
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:59:36 +0000

    Qq shop method design mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
